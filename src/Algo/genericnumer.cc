2911e676173520f6002629ea0659cf72068f1d0d
Bert Bril <bert.bril@dgbes.com>
1999-11-18 16:40:04 +0000

    Changed to MathFunction
    git-svn-id: https://svn.opendtect.org/od/trunk@10 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Algo/genericnumer.cc
+++ src/Algo/genericnumer.cc
@@ -3,15 +3,15 @@
  * DATE     : 9-3-1999
 -*/
 
-#include <genericnumer.h>
+#include "genericnumer.h"
+#include "mathfunc.h"
 #include <math.h>
-#include <xfunc.h>
     
 #define ITMAX 100
-#define EPS 3.0e-8
+#define EPS 6e-8
    
-bool findValue( const MathXFunction& func, float x1, float x2, float& res,
-		   float targetval, float tol)
+bool findValue( const MathFunction& func, float x1, float x2, float& res,
+		float targetval, float tol)
 { 
     int iter;
     float a=x1,b=x2,c,d,e,min1,min2;
@@ -20,72 +20,64 @@ bool findValue( const MathXFunction& func, float x1, float x2, float& res,
     float fc,p,q,r,s,tol1,xm;
     void nrerror();
 
-    if (fb*fa > 0.0) return false;
-    fc=fb;
-    for (iter=1;iter<=ITMAX;iter++) {
-	    if (fb*fc > 0.0) {
-		    c=a;
-		    fc=fa;
-		    e=d=b-a;
-	    }
-	    if (fabs(fc) < fabs(fb)) {
-		    a=b;
-		    b=c;
-		    c=a;
-		    fa=fb;
-		    fb=fc;
-		    fc=fa;
-	    }
-	    tol1=2.0*EPS*fabs(b)+0.5*tol;
-	    xm=0.5*(c-b);
-	    if (fabs(xm) <= tol1 || fb == 0.0) 
-	    {
-		res = b;
-		return true;
-	    }
+    if ( fb*fa > 0 ) return false;
+    fc = fb;
+    for ( iter=1; iter<=ITMAX; iter++ )
+    {
+	if ( fb*fc > 0 )
+	    { c = a; fc = fa; e = d = b - a; }
+	if ( fabs(fc) < fabs(fb) )
+	    { a = b; b = c; c = a; fa = fb; fb = fc; fc = fa; }
+	tol1 = EPS * fabs(b) + 0.5*tol;
+	xm = 0.5 * (c - b);
+	if ( fabs(xm) <= tol1 || fb == 0 ) 
+	{
+	    res = b;
+	    return true;
+	}
 
-	    if (fabs(e) >= tol1 && fabs(fa) > fabs(fb)) {
-		    s=fb/fa;
-		    if (a == c) {
-			    p=2.0*xm*s;
-			    q=1.0-s;
-		    } else {
-			    q=fa/fc;
-			    r=fb/fc;
-			    p=s*(2.0*xm*q*(q-r)-(b-a)*(r-1.0));
-			    q=(q-1.0)*(r-1.0)*(s-1.0);
-		    }
-		    if (p > 0.0)  q = -q;
-		    p=fabs(p);
-		    min1=3.0*xm*q-fabs(tol1*q);
-		    min2=fabs(e*q);
-		    if (2.0*p < (min1 < min2 ? min1 : min2)) {
-			    e=d;
-			    d=p/q;
-		    } else {
-			    d=xm;
-			    e=d;
-		    }
-	    } else {
-		    d=xm;
-		    e=d;
-	    }
-	    a=b;
-	    fa=fb;
-	    if (fabs(d) > tol1)
-		    b += d;
+	if ( fabs(e) < tol1 || fabs(fa) <= fabs(fb) )
+	    { d = xm; e = d; }
+	else
+	{
+	    s = fb / fa;
+	    if ( a == c )
+		{ p = 2 * xm * s; q = 1 - s; }
 	    else
-		    b += (xm > 0.0 ? fabs(tol1) : -fabs(tol1));
-	    fb=targetval - func.getValue(b);
+	    {
+		q = fa / fc; r = fb / fc;
+		p = s * (2*xm*q*(q-r) - (b-a)*(r-1));
+		q = (q-1) * (r-1) * (s-1);
+	    }
+	    if ( p > 0 ) q = -q;
+	    p = fabs(p);
+	    min1 = 3 * xm * q - fabs( tol1 * q );
+	    min2 = fabs( e * q );
+	    if ( 2*p < (min1 < min2 ? min1 : min2) )
+		{ e = d; d = p/q; }
+	    else
+		{ d = xm; e = d; }
+	}
+	a = b;
+	fa = fb;
+	if ( fabs(d) > tol1 )
+	    b += d;
+	else
+	    b += (xm > 0.0 ? fabs(tol1) : -fabs(tol1));
+
+	fb = targetval - func.getValue(b);
     }
+
     return false;
 }
+
 
 #undef ITMAX
 #undef EPS
 
-float findValueInAperture( const MathXFunction& func, float startx, 
-			      float aperture, float dx, float target, float tol)
+
+float findValueInAperture( const MathFunction& func, float startx, 
+			   float aperture, float dx, float target, float tol )
 {
     bool centerispositive = target - func.getValue( startx ) > 0;
     const float halfaperture = aperture / 2;
@@ -127,7 +119,7 @@ float findValueInAperture( const MathXFunction& func, float startx,
 }
 
 
-float similarity( const MathXFunction& a, const MathXFunction& b, 
+float similarity( const MathFunction& a, const MathFunction& b, 
 			 float a1, float b1, float dist, int sz )
 {
     float val1, val2;
