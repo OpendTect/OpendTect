db8b20eae5d31df53d98c038a910ee92896763de
Bert Bril <bert.bril@dgbes.com>
2000-05-29 10:33:38 +0000

    More general setup
    git-svn-id: https://svn.opendtect.org/od/trunk@75 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/callback.h
+++ include/Basic/callback.h
@@ -7,71 +7,101 @@ ________________________________________________________________________
  CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
  Author:	A.H.Bril
  Date:		8-11-1995
- Contents:	Callbacks for any CallBackClass
- RCS:		$Id: callback.h,v 1.3 2000-05-27 16:22:48 bert Exp $
+ Contents:	Callbacks for any CallBacker
+ RCS:		$Id: callback.h,v 1.4 2000-05-29 10:33:38 bert Exp $
 ________________________________________________________________________
 
-You must define the CallBackClass before including this header file. This can
-be done on the compiler command line ( ... -DCallBackClass=MyClass ... ) or
+If you want a specific class to be the 'callback base class', you must define
+the CallBacker before including this header file. This can
+be done on the compiler command line ( ... -DCallBacker=MyClass ... ) or
 simply by defining it with #define .
 
-If you don't define it yourself, you'll get the dGB CallBackClass which is the
-infamous UserIDObject.
+If you don't define it yourself, you'll get the dGB CallBackClass which
+is (almost) empty.
+
+A simple list of CallBacks is also required. Again, define CallBackList if
+needed. In this case, you'll need to support:
+
+1) An 'operator +=' to add a callback to the list
+2) An 'operator -=' to remove a callback from the list
+3) An 'operator [](int)' const that returns the i-th callback in the list.
+4) A method 'size() const' that returns the number of callbacks in the list
+5) A method 'doCall(CallBacker*)' that simply calls all the CallBacks' doCall
+   methods.
+
+If you do not define CallBackList you will get dGB's CallBackSet, which is
+based on a simple dumbed-down implementation a bit like the std lib's vector
+class.
 
 -*/
 
-#include <sets.h>
+#ifndef CallBacker
 
-#ifndef CallBackClass
+#   define CallBacker CallBackClass
+    class CallBackClass
+    { public: virtual void _a_dummy_virtual_enabling_dynamic_cast_() {} };
 
-# define CallBackClass UserIDObject
+#else
+
+    class CallBacker;
 
 #endif
 
 
-class CallBackClass;
-
-
-typedef void (CallBackClass::*CallBackFunction)(CallBackClass*);
+typedef void (CallBacker::*CallBackFunction)(CallBacker*);
 #define mCBFn(clss,fn) ((CallBackFunction)(&clss::fn))
 #define mCB(obj,clss,fn) CallBack(obj,mCBFn(clss,fn))
 
 
 class CallBack
 {
-    friend class	CallBackClass;
-
 public:
-			CallBack( CallBackClass* o=0, CallBackFunction f=0 )
+			CallBack( CallBacker* o=0, CallBackFunction f=0 )
 			{ obj = o; fn = f; }
     int			operator==( const CallBack& cb ) const
 			{ return obj == cb.obj && fn == cb.fn; }
     int			operator!=( const CallBack& cb ) const
 			{ return obj != cb.obj || fn != cb.fn; }
 
-    void		doCall( CallBackClass* o )
-			{ if ( obj && fn ) (obj->*fn)( o ); }
     bool		willCall() const
-			{ return obj && fn ? YES : NO; }
+			{ return obj && fn; }
+    void		doCall( CallBacker* o )
+			{ if ( obj && fn ) (obj->*fn)( o ); }
+
+    CallBacker*		cbObj()			{ return obj; }
+    const CallBacker*	cbObj() const		{ return obj; }
+    CallBackFunction	cbFn() const		{ return fn; }
 
 protected:
 
-    CallBackClass*	obj;
+    CallBacker*		obj;
     CallBackFunction	fn;
 
 };
 
-class CallBackList : public TypeSet<CallBack>
+
+#ifndef CallBackList
+
+
+#define CallBackList CallBackSet
+#include <sets.h>
+
+class CallBackSet : public TypeSet<CallBack>
 {
 public:
-    void	doCall(CallBackClass*);
+
+    void	doCall(CallBacker*);
+
 };
 
-inline void CallBackList::doCall( CallBackClass* obj )
+inline void CallBackSet::doCall( CallBacker* obj )
 {
     for ( int idx=0; idx<size(); idx++ )
 	(*this)[idx].doCall( obj );
 }
+
+
+#endif
 
 
 #endif
