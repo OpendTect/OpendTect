78b4162b9d2ee2030d5c6568964afced03975e3c
Bert Bril <bert.bril@dgbes.com>
2000-07-27 09:54:03 +0000

    Added line/markerappearance and fontdata
    git-svn-id: https://svn.opendtect.org/od/trunk@93 a985855d-d384-4b45-ae33-31a410998c3c

--- include/General/draw.h
+++ include/General/draw.h
@@ -6,21 +6,53 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Bril
  Date:          26/07/2000
- RCS:           $Id: draw.h,v 1.1 2000-07-26 15:35:40 bert Exp $
+ RCS:           $Id: draw.h,v 1.2 2000-07-27 09:54:03 bert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "enums.h"
+#include "color.h"
+#include "fontdata.h"
 
 class Draw
 {
 public:
 
-    enum MarkerType	{ NoMarker, Circle, Square, Cross };
+    enum MarkerType	{ NoMarker, Square, Circle, Cross };
 			DeclareEnumUtils(MarkerType)
     enum LineStyle	{ NoLine, Solid, Dash, Dot, DashDot, DashDotDot };
 			DeclareEnumUtils(LineStyle)
+};
+
+
+class MarkerAppearance
+{
+public:
+
+			MarkerAppearance( Draw::MarkerType t=Draw::Square,
+					  Color c=Color(0,0,0,0) )
+			: type(t), color(c)	{}
+
+    Draw::MarkerType	type;
+    Color		color;
+    FontData		fontdata;
+
+};
+
+
+class LineAppearance
+{
+public:
+
+			LineAppearance( Draw::LineStyle s=Draw::Solid,
+					int w=1, Color c=Color(0,0,0,0) )
+			: style(s), width(w), color(c)	{}
+
+    Draw::LineStyle	style;
+    int			width;
+    Color		color;
+
 };
 
 
