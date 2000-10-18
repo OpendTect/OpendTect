c2a4016f7a8703a9f59bb5c765a7e098f6ae3276
Bert Bril <bert.bril@dgbes.com>
2000-10-18 08:01:25 +0000

    update
    git-svn-id: https://svn.opendtect.org/od/trunk@142 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Basic/settings.cc
+++ src/Basic/settings.cc
@@ -5,13 +5,14 @@
  * FUNCTION : Default user settings
 -*/
  
-static const char* rcsID = "$Id: settings.cc,v 1.7 2000-08-16 13:28:25 bert Exp $";
+static const char* rcsID = "$Id: settings.cc,v 1.8 2000-10-18 08:01:25 bert Exp $";
 
 #include "settings.h"
 #include "filegen.h"
 #include "ascstream.h"
 #include "errh.h"
 #include <fstream.h>
+#include <filegen.h>
 
 
 Settings* Settings::common_ = 0;
@@ -45,18 +46,35 @@ bool Settings::reRead()
 {
     ifstream* strm = new ifstream( fname );
     bool do_write = false;
+    BufferString usedfname = (const char*)fname;
     if ( !strm || !strm->good() )
     {
-	delete strm;
-	strm = new ifstream( GetDataFileName( "dgbSettings" ) );
 	do_write = true;
+	usedfname = (const char*)fname;
+	usedfname += ".old";
+	delete strm;
+	strm = new ifstream( usedfname );
+	if ( !strm || !strm->good() )
+	{
+	    delete strm;
+	    usedfname = GetDataFileName("dgbSettings");
+	    strm = new ifstream( usedfname );
+	}
     }
     if ( !strm || !strm->good() )
-	{ delete strm; return false; }
+    {
+	ErrMsg( "Cannot find any valid user settings file" );
+	delete strm; return false;
+    }
 
     ascistream stream( *strm, true );
     if ( !stream.isOfFileType( sKey ) )
-	{ delete strm; return false; }
+    {
+	BufferString emsg = "User settings file '";
+	emsg += usedfname;
+	emsg += "' seems to be corrupted.";
+	delete strm; return false;
+    }
 
     clear();
     while ( !atEndOfSection( stream.next() ) )
@@ -67,7 +85,7 @@ bool Settings::reRead()
     }
 
     delete strm;
-    if ( do_write ) write();
+    if ( do_write ) write( false );
     return true;
 }
 
@@ -77,14 +95,45 @@ Settings::~Settings()
 }
 
 
-bool Settings::write() const
+bool Settings::write( bool do_merge ) const
 {
-    ofstream strm( fname );
-    if ( strm.fail() || strm.bad() ) return NO;
+    BufferString newfname = (const char*)fname;
+    newfname += ".new";
+    ofstream strm( newfname );
+    if ( strm.fail() || strm.bad() )
+    {
+	ErrMsg( "Cannot open '.new' settings file" );
+	return false;
+    }
+
+    if ( do_merge )
+    {
+	IOPar dup( *this );
+	Settings* me = const_cast<Settings*>( this );
+	me->reRead();
+	me->merge( dup );
+    }
 
     ascostream stream( strm );
     stream.putHeader( sKey );
     putTo( stream, false );
 
+    BufferString oldfname = (const char*)fname;
+    oldfname += ".old";
+    if ( File_exists(oldfname) && !File_remove(oldfname,NO,NO) )
+    {
+	ErrMsg( "Cannot remove '.old' settings file" );
+	return false;
+    }
+    if ( !File_rename(fname,oldfname) )
+    {
+	ErrMsg( "Cannot rename settings file to '.old'" );
+	return false;
+    }
+    if ( !File_rename(newfname,fname) )
+    {
+	ErrMsg( "Cannot rename new settings file" );
+	return false;
+    }
     return true;
 }
