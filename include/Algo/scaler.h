785705005d2df7236c9be54aec831808fff8acce
Bert Bril <bert.bril@dgbes.com>
2000-12-14 16:23:53 +0000

    added AsymptScaler
    git-svn-id: https://svn.opendtect.org/od/trunk@164 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Algo/scaler.h
+++ include/Algo/scaler.h
@@ -8,7 +8,7 @@ ________________________________________________________________________
  Author:	A.H.Bril
  Date:		8-9-1995
  Contents:	Scaler objects
- RCS:		$Id: scaler.h,v 1.2 2000-11-21 13:37:21 bert Exp $
+ RCS:		$Id: scaler.h,v 1.3 2000-12-14 16:23:53 bert Exp $
 ________________________________________________________________________
 
 @$*/
@@ -18,6 +18,10 @@ ________________________________________________________________________
 #define sLinScaler	"Linear"
 #define sLogScaler	"Logarithmic"
 #define sExpScaler	"Exponential"
+#define sAsymptScaler	"Asymptotic"
+
+/*!\brief scaling floating points.
+*/
 
 class Scaler
 {
@@ -35,6 +39,9 @@ public:
     virtual void	fromString(const char*)	= 0;
 };
 
+
+/*!\brief linear scaling
+*/
 
 class LinScaler : public Scaler
 {
@@ -71,6 +78,7 @@ public:
 			{ return new LogScaler(ten); }
 
     double		scale(double) const;
+    double		unScale(double) const;
     const char*		toString() const;
     void		fromString(const char*);
     bool		operator==( const LogScaler& b ) const
@@ -90,6 +98,7 @@ public:
 			{ return new ExpScaler(ten); }
 
     double		scale(double) const;
+    double		unScale(double) const;
     const char*		toString() const;
     void		fromString(const char*);
 
@@ -97,6 +106,41 @@ public:
 			{ return ten==b.ten; }
     
     bool		ten;
+};
+
+
+class AsymptScaler : public Scaler
+{
+public:
+			AsymptScaler( double c=0, double w=1, double l=0.95 )
+			: center_(c), width_(w), linedge_(l), factor(1)
+						{ set(c,w,l); }
+    const char*		type() const		{ return sAsymptScaler; }
+    virtual AsymptScaler* duplicate() const
+			{ return new AsymptScaler(center_,width_,linedge_); }
+
+    double		scale(double) const;
+    double		unScale(double) const;
+    const char*		toString() const;
+    void		fromString(const char*);
+
+    bool		operator==( const AsymptScaler& b ) const
+			{ return mIS_ZERO(center_-b.center_)
+			      && mIS_ZERO(width_-b.width_)
+			      && mIS_ZERO(linedge_-b.linedge_); }
+
+    void		set(double,double,double);
+    double		center() const		{ return center_; }
+    double		width() const		{ return width_; }
+    double		linedge() const		{ return linedge_; }
+
+protected:
+
+    double		center_;
+    double		width_;
+    double		linedge_;
+
+    double		factor;
 };
 
 
