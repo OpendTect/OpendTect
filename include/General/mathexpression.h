93829054d217688ab0ba41d1f3d9c81bc2ed830b
Bert Bril <bert.bril@dgbes.com>
2001-01-19 11:25:26 +0000

    Abs added
    git-svn-id: https://svn.opendtect.org/od/trunk@168 a985855d-d384-4b45-ae33-31a410998c3c

--- include/General/mathexpression.h
+++ include/General/mathexpression.h
@@ -1,19 +1,27 @@
 #ifndef mathexpression_h
 #define mathexpression_h
 
-/*@+
+/*+
 ________________________________________________________________________
 
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        Kristofer Tingdahl
  Date:          10-12-1999
- RCS:           $Id: mathexpression.h,v 1.2 2000-05-01 15:38:43 bert Exp $
+ RCS:           $Id: mathexpression.h,v 1.3 2001-01-19 11:25:26 bert Exp $
 ________________________________________________________________________
 
-MathExpression can parse a string with a mathematical expression. The expression
-can consist of constants, variables and operators. A constant can be any
-number like 3, -5, 3e-5, or pi. Everything that does not start with a digit
-is treated as a variable. An operator can be either +, -, *, / or ^.
+-*/
+
+#include <sets.h>
+#include <gendefs.h>
+
+
+/*!\brief parses a string with a mathematical expression.
+
+The expression can consist of constants, variables and operators.
+A constant can be any number like 3, -5, 3e-5, or pi. Everything that does not
+start with a digit is treated as a variable. An operator can be either
++, -, *, / or ^.
 
 If the parser returns null, it couldn't parse the expression.
 
@@ -23,14 +31,12 @@ each variable's name can be queried with getVariableStr( int ).
 When a calculations should be done, all variables must be set with
 setVariable( int, float ). Then, the calculation can be done with getValue().
 
-@$*/
-
-#include <sets.h>
-#include <gendefs.h>
+-*/
 
 class MathExpression
 {
 public:
+
     static MathExpression* 	parse( const char* );
     virtual float		getValue() const		= 0;
 
@@ -44,6 +50,7 @@ public:
 				~MathExpression();
 
 protected:
+
     int				getNrInputs() const { return inputs.size(); }
     bool			setInput( int, MathExpression* );
     void			copyInput( MathExpression* target ) const;
@@ -52,6 +59,8 @@ protected:
     ObjectSet<TypeSet<int> >	variableobj;
     ObjectSet<TypeSet<int> >	variablenr;
     ObjectSet<MathExpression>	inputs;
+
 };
+
 
 #endif
