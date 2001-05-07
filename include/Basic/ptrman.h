6349f89621dadc4cc1a6a21d0e62f6380fd78436
Bert Bril <bert.bril@dgbes.com>
2001-05-07 11:38:32 +0000

    Make more generally usable
    git-svn-id: https://svn.opendtect.org/od/trunk@321 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/ptrman.h
+++ include/Basic/ptrman.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        Kristofer Tingdahl
  Date:          10-12-1999
- RCS:           $Id: ptrman.h,v 1.4 2001-03-21 11:02:40 bert Exp $
+ RCS:           $Id: ptrman.h,v 1.5 2001-05-07 11:38:32 bert Exp $
 ________________________________________________________________________
 
 -*/
@@ -15,37 +15,66 @@ ________________________________________________________________________
 #include <general.h>
 
 
+#define mDefPtrMan1(Clss,ArrBrIfNec) \
+\
+template<class T> \
+class Clss \
+{ \
+public: \
+\
+				Clss( T* ptr=0 ) : ptr_( ptr )	{} \
+				~Clss()		{ erase(); } \
+    inline Clss<T>&		operator=( T* ptr ) \
+				{ erase(); ptr_ = ptr; return *this; } \
+\
+    inline			operator T*()	{ return ptr_; } \
+    inline T*			operator ->()	{ return ptr_; } \
+    inline T&			operator *()	{ return *ptr_; } \
+
+#define mDefPtrMan2(Clss,ArrBrIfNec) \
+    inline bool			operator ==( const Clss& p ) const \
+				{ return ptr_ == p.ptr_; } \
+    inline bool			operator ==( const T* ptr ) const \
+				{ return ptr_ == ptr; } \
+    inline bool			operator !=( const Clss& p ) const \
+				{ return ptr_ != p.ptr_; } \
+    inline bool			operator !=( const T* ptr ) const \
+				{ return ptr_ != ptr; } \
+\
+    inline bool			operator !() const { return !ptr_; } \
+
+#define mDefPtrMan3(Clss,ArrBrIfNec) \
+private: \
+\
+    T*				ptr_; \
+\
+    Clss<T>&			operator=(const T& p) const; \
+\
+    void			erase() \
+				{ delete ArrBrIfNec ptr_; ptr_ = 0; } \
+\
+};
+
+
 /*!\brief a simple autopointer.
 
 It is assigned to a pointer, and takes over
 the responsibility for its deletion.
+For Arrays, use the ArrPtrMan class.
 
 */
+mDefPtrMan1(PtrMan,)
+mDefPtrMan2(PtrMan,)
+mDefPtrMan3(PtrMan,)
 
-template<class T>
-class PtrMan
-{
-public:
-				PtrMan( T* ptr_ = 0 )
-				: ptr( ptr_ )
-				{}
+/*!\brief a simple autopointer for arrays.
 
-				~PtrMan()
-				{ erase(); }
+For Non-arrays, use the PtrMan class.
 
-    T*				getPtr() const { return ptr; }
+*/
+mDefPtrMan1(ArrPtrMan,[])
+mDefPtrMan2(ArrPtrMan,[])
+mDefPtrMan3(ArrPtrMan,[])
 
-    T*				operator=( T* ptr_ )
-				{ erase(); ptr = ptr_; return ptr; }
-
-    bool			operator!() const { return !ptr; }
-
-
-private:
-    void			erase()
-				{ delete ptr; ptr = 0; }
-
-    T*				ptr;
-};
 
 #endif
