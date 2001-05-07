089cc2be038df8a296e5cab0f08f2f9e857bfc8f
Bert Bril <bert.bril@dgbes.com>
2001-05-07 09:23:08 +0000

    now with obj selector
    git-svn-id: https://svn.opendtect.org/od/trunk@318 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiIo/uiioobjsel.cc
+++ src/uiIo/uiioobjsel.cc
@@ -4,26 +4,122 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        Bert Bril
  Date:          25/05/2000
- RCS:           $Id: uiioobjsel.cc,v 1.2 2001-05-05 16:32:52 bert Exp $
+ RCS:           $Id: uiioobjsel.cc,v 1.3 2001-05-07 09:23:08 bert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uiioobjsel.h"
 #include "iodirentry.h"
+#include "uigeninput.h"
+#include "uilistbox.h"
+#include "uimsg.h"
+#include "ioman.h"
+#include "ioobj.h"
+#include "transl.h"
 
-uiIOObjSelect::uiIOObjSelect( uiObject* p, CtxtIOObj& c )
-	: uiIOSelect(p,mCB(this,uiIOObjSelect,doObjSel),"Select an object",true)
+
+uiIOObjSelDlg::uiIOObjSelDlg( uiObject* p, const CtxtIOObj& c )
+	: uiDialog(p)
+	, ctio(c)
+	, nmfld(0)
+{
+    BufferString nm( "Select " );
+    nm += ctio.ctxt.forread ? "input " : "output ";
+    nm += ctio.ctxt.trgroup->name();
+    setName( nm );
+
+    IOM().to( MultiID(IOObjContext::getStdDirData(ctio.ctxt.stdseltype)->id) );
+    entrylist = new IODirEntryList( IOM().dirPtr(), ctio.ctxt.trgroup,
+				    ctio.ctxt.maychdir );
+    if ( ctio.ioobj )
+        entrylist->setSelected( ctio.ioobj->key() );
+
+    listfld = new uiListBox( this, entrylist->Ptr() );
+    if ( !ctio.ctxt.forread )
+    {
+	nmfld = new uiGenInput( this, "Name" );
+	nmfld->attach( alignedBelow, listfld );
+    }
+
+    //TODO
+    //listfld->selection.notify( mCB(this,uiIOObjSelDlg,selChg) );
+    //listfld->doubleclick.notify( mCB(this,uiDialog,accept) );
+    listfld->notify( mCB(this,uiIOObjSelDlg,selChg) );
+
+    setOkText( "Select" );
+}
+
+
+uiIOObjSelDlg::~uiIOObjSelDlg()
+{
+    delete entrylist;
+}
+
+
+void uiIOObjSelDlg::selChg( CallBacker* )
+{
+    entrylist->setCurrent( listfld->currentItem() );
+    ioobj = entrylist->selected();
+    if ( nmfld )
+	nmfld->setText( ioobj ? (const char*)ioobj->name() : "" );
+}
+
+
+bool uiIOObjSelDlg::acceptOK( CallBacker* )
+{
+    if ( !ioobj ) { uiMSG().error( "Please select an object" ); return false; }
+    if ( !nmfld ) return true;
+
+    const char* seltxt = nmfld->text();
+    if ( ioobj->name() == seltxt ) return true;
+
+    int selidx = entrylist->indexOf( seltxt );
+    if ( selidx >= 0 )
+    {
+	entrylist->setCurrent( selidx );
+	ioobj = entrylist->selected();
+	return true;
+    }
+
+    // create new entry
+    CtxtIOObj newctio( ctio );
+    newctio.ioobj = 0; newctio.setName( seltxt );
+    newctio.fillObj();
+    ioobj = newctio.ioobj;
+    if ( !ioobj )
+    {
+	uiMSG().error( "Cannot create object with this name" );
+	return false;
+    }
+
+    return true;
+}
+
+
+uiIOObjSel::uiIOObjSel( uiObject* p, CtxtIOObj& c, const char* txt,
+			      bool wclr )
+	: uiIOSelect( p, mCB(this,uiIOObjSel,doObjSel),
+		     txt ? txt : (const char*)c.ctxt.trgroup->name(), wclr )
 	, ctio(c)
 {
+    updateInput();
 }
 
 
-uiIOObjSelect::~uiIOObjSelect()
+void uiIOObjSel::updateInput()
 {
+    if ( ctio.ioobj )
+	setInput( (const char*)ctio.ioobj->key() );
 }
 
 
-void uiIOObjSelect::doObjSel( CallBacker* )
+void uiIOObjSel::doObjSel( CallBacker* )
 {
+    uiIOObjSelDlg dlg( this, ctio );
+    if ( dlg.go() )
+    {
+	ctio.setObj( dlg.ioObj()->clone() );
+	updateInput();
+    }
 }
