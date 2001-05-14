eca167f10c020362a0bbcac37208b697751da6fb
Bert Bril <bert.bril@dgbes.com>
2001-05-14 13:57:54 +0000

    implemented read
    git-svn-id: https://svn.opendtect.org/od/trunk@356 a985855d-d384-4b45-ae33-31a410998c3c

--- src/General/pickset.cc
+++ src/General/pickset.cc
@@ -5,10 +5,35 @@
  * FUNCTION : CBVS I/O
 -*/
 
-static const char* rcsID = "$Id: pickset.cc,v 1.1 2001-05-14 13:21:51 bert Exp $";
+static const char* rcsID = "$Id: pickset.cc,v 1.2 2001-05-14 13:57:54 bert Exp $";
 
 #include "pickset.h"
 #include "picksettr.h"
+#include "ascstream.h"
+
+
+bool PickLocation::fromString( const char* s )
+{
+    if ( !s || !*s ) return false;
+
+    BufferString bufstr( s );
+    char* str = bufstr.buf();
+    char* endptr = strchr( str, ' ' );
+    if ( !endptr ) endptr = strchr( str, '\t' );
+    if ( !endptr ) return false;
+    *endptr++ = '\0';
+    double x = atof( str );
+
+    str = endptr; endptr = strchr( str, ' ' );
+    if ( !endptr ) endptr = strchr( str, '\t' );
+    if ( !endptr ) return false;
+    *endptr++ = '\0'; if ( !*endptr ) return false;
+
+    pos.x = x;
+    pos.y = atof( str );
+    z = atof( endptr );
+    return true;
+}
 
 
 void PickSet::add( PickGroup*& grp )
@@ -61,10 +86,39 @@ const char* dgbPickSetTranslator::read( PickSet& ps, Conn& conn )
     if ( !conn.forRead() || !conn.hasClass(StreamConn::classid) )
 	return "Internal error: bad connection";
 
-    // PickSetTranslator::classdef.name()
-    // ((StreamConn&)conn).iStream()
+    ascistream astrm( ((StreamConn&)conn).iStream() );
+    istream& strm = astrm.stream();
+    if ( !strm.good() )
+	return "Cannot read from input file";
+    if ( !astrm.isOfFileType(PickSetTranslator::classdef.name()) )
+	return "Input file is not a Pick Set";
+    if ( atEndOfSection(astrm) ) astrm.next();
+    if ( atEndOfSection(astrm) )
+	return "Input file contains no pick groups";
 
-    return 0;
+    ps.setName( astrm.value() );
+    astrm.next(); if ( atEndOfSection(astrm) ) astrm.next();
+    if ( atEndOfSection(astrm) )
+	return "Input file contains no picks";
+
+    do
+    {
+	PickGroup* newpg = new PickGroup( astrm.getValue() );
+	PickLocation loc;
+	while ( !atEndOfSection(astrm.next()) )
+	{
+	    if ( !loc.fromString( astrm.keyWord() ) )
+		break;
+	    *newpg += loc;
+	}
+	while ( !atEndOfSection(astrm) ) astrm.next();
+	astrm.next();
+
+	ps.add( newpg );
+
+    } while ( !atEndOfSection(astrm) );
+
+    return ps.nrGroups() ? 0 : "No valid picks found";
 }
 
 
@@ -73,8 +127,5 @@ const char* dgbPickSetTranslator::write( const PickSet& ps, Conn& conn )
     if ( !conn.forWrite() || !conn.hasClass(StreamConn::classid) )
 	return "Internal error: bad connection";
 
-    // PickSetTranslator::classdef.name()
-    // ((StreamConn&)conn).oStream()
-
-    return 0;
+    return "Pick Set write not implemented yet";
 }
