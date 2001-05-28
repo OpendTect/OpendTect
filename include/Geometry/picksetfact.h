a412a3e0c92d22d45c068c5a166f7e9894aa8c12
Bert Bril <bert.bril@dgbes.com>
2001-05-28 20:57:01 +0000

    Right name
    git-svn-id: https://svn.opendtect.org/od/trunk@390 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Geometry/picksetfact.h
+++ include/Geometry/picksetfact.h
@@ -7,14 +7,14 @@ ________________________________________________________________________
  CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
  Author:	A.H. Bril
  Date:		May 2001
- RCS:		$Id: picksetfact.h,v 1.2 2001-05-28 15:50:32 bert Exp $
+ RCS:		$Id: picksetfact.h,v 1.3 2001-05-28 20:57:01 bert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include <picksettr.h>
 
-defineTranslatorGroup(PickSet,"Pick Sets");
+defineTranslatorGroup(PickSet,"Pick Set");
 defineTranslator(dgb,PickSet,mDGBKey);
 
 #endif
