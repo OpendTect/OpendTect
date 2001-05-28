8ea70219c93caa2a80966ca2c4ed32aac685d5df
Bert Bril <bert.bril@dgbes.com>
2001-05-28 15:50:32 +0000

    replaced 'Attribute' leftover
    git-svn-id: https://svn.opendtect.org/od/trunk@385 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Geometry/picksetfact.h
+++ include/Geometry/picksetfact.h
@@ -7,14 +7,14 @@ ________________________________________________________________________
  CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
  Author:	A.H. Bril
  Date:		May 2001
- RCS:		$Id: picksetfact.h,v 1.1 2001-05-14 13:22:26 bert Exp $
+ RCS:		$Id: picksetfact.h,v 1.2 2001-05-28 15:50:32 bert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include <picksettr.h>
 
-defineTranslatorGroup(PickSet,"Attribute definitions");
+defineTranslatorGroup(PickSet,"Pick Sets");
 defineTranslator(dgb,PickSet,mDGBKey);
 
 #endif
