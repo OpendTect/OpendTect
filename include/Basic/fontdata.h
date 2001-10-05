9f6002fdd20def67278d8207a7ace2f15ed2efdf
Bert Bril <bert.bril@dgbes.com>
2001-10-05 10:41:35 +0000

    oops
    git-svn-id: https://svn.opendtect.org/od/trunk@673 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/fontdata.h
+++ include/Basic/fontdata.h
@@ -6,7 +6,7 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Bril
  Date:          26/07/2000
- RCS:           $Id: fontdata.h,v 1.3 2001-10-05 10:41:08 bert Exp $
+ RCS:           $Id: fontdata.h,v 1.4 2001-10-05 10:41:35 bert Exp $
 ________________________________________________________________________
 
 -*/
@@ -72,7 +72,7 @@ public:
     enum StdSz		{ Control=0, 
 			  GraphicsMed, GraphicsSmall, GraphicsLarge,
 			  ControlSmall, ControlLarge };
-    inline static const char* key( StdSz ss ) const
+    inline static const char* key( StdSz ss )
 			{ return defaultkeys[(int)ss]; }
  
 protected:
