dc7c3cdd8c7e2313ade577e4b091423a4155232a
Bert Bril <bert.bril@dgbes.com>
2001-12-04 16:34:35 +0000

    added
    git-svn-id: https://svn.opendtect.org/od/trunk@774 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiTools/uiselsimple.h
@@ -0,0 +1,61 @@
+#ifndef uiexecutor_h
+#define uiexecutor_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
+ Author:        A.H. Bril
+ Date:          Dec 2001
+ RCS:           $Id: uiselsimple.h,v 1.1 2001-12-04 16:34:35 bert Exp $
+________________________________________________________________________
+
+-*/
+
+#include "uidialog.h"
+
+class uiListBox;
+class PtrUserIDObjectSet;
+
+/*!\brief Select entry from list
+
+Use setTitleText to set a message different from caption.
+
+*/
+
+class uiSelectFromList : public uiDialog
+{ 	
+public:
+			uiSelectFromList(uiParent*,
+					 const ObjectSet<BufferString>&,
+					 const char* cur=0,
+					 const char* captn="Please select");
+			uiSelectFromList(uiParent*,
+					 const PtrUserIDObjectSet&,
+					 const char* cur=0,
+					 const char* captn="Please select");
+			uiSelectFromList(uiParent*,
+					 const char**,int nritems=-1,
+					 const char* cur=0,
+					 const char* captn="Please select");
+			~uiSelectFromList()	{}
+
+    int			selection() const	{ return sel_; }
+    			//!< -1 = no selection made (cancelled or 0 list items)
+
+    uiListBox*		selfld;
+
+protected:
+
+    int			sel_;
+
+    bool		acceptOK(CallBacker*);
+
+private:
+
+    void		init(const char**,int,const char*);
+
+};
+
+
+#endif
