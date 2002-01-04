6a1e5491232124b3811d36986d0485f86119c1c0
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2002-01-04 10:25:50 +0000

    *** empty log message ***
    git-svn-id: https://svn.opendtect.org/od/trunk@826 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ plugins/AttribExp/expdipview.cc
@@ -0,0 +1,127 @@
+/*+
+ * COPYRIGHT: (C) de Groot-Bril Earth Sciences B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Oct 1999
+-*/
+
+static const char* rcsID = "$Id: expdipview.cc,v 1.1 2002-01-04 10:24:16 kristofer Exp $";
+
+
+#include "expdipview.h"
+#include "attribprovider.h"
+#include "seistrc.h"
+
+#include <math.h>
+
+#define M_360_2_PI       57.2957795131   // 360/(2*PI)
+
+DipViewAttrib::DipViewAttrib( Parameters* params )
+    : aspect( params->aspect )
+    , AttribCalc( new DipViewAttrib::Task( *this ) )
+{ 
+    params->fillDefStr( desc );
+    delete params;
+    AttribInputSpec* spec = new AttribInputSpec;
+    spec->setDesc( "Inline dipangle");
+    spec->forbiddenDts += Seis::Ampl;
+    spec->forbiddenDts += Seis::Dip;
+    spec->forbiddenDts += Seis::Frequency;
+    spec->forbiddenDts += Seis::Phase;
+    spec->forbiddenDts += Seis::AVOGradient;
+    inputspec += spec;
+
+    spec = new AttribInputSpec;
+    spec->setDesc( "Crossline dipangle");
+    spec->forbiddenDts += Seis::Ampl;
+    spec->forbiddenDts += Seis::Dip;
+    spec->forbiddenDts += Seis::Frequency;
+    spec->forbiddenDts += Seis::Phase;
+    spec->forbiddenDts += Seis::AVOGradient;
+    inputspec += spec;
+
+    prepareInputs();
+}
+
+
+DipViewAttrib::~DipViewAttrib( )
+{
+}
+
+
+bool DipViewAttrib::Task::Input::set( const BinID& pos,
+			     const ObjectSet<AttribProvider>& inputproviders,
+			     const TypeSet<int>& inputattribs,
+			     const TypeSet<float*>&)
+{
+    inldip = inputproviders[0]->getTrc(pos.inl, pos.crl);
+    crldip = inputproviders[1]->getTrc(pos.inl, pos.crl);
+
+    if ( !inldip || !crldip ) return false;
+
+    inlattrib = inputproviders[0]->attrib2component( inputattribs[0] );
+    crlattrib = inputproviders[1]->attrib2component( inputattribs[1] );
+
+    return true;
+}
+
+
+int DipViewAttrib::Task::nextStep()
+{
+    if ( !outp ) return 0;
+
+    const DipViewAttrib::Task::Input* inp = 
+			(const DipViewAttrib::Task::Input*) input;
+
+    const SeisTrc* inldip = inp->inldip;
+    const SeisTrc* crldip = inp->crldip;
+    const int inlattrib = inp->inlattrib;
+    const int crlattrib = inp->crlattrib;
+
+    BinID aspect = calculator.aspect;
+
+    float curt = t1;
+
+    for ( int idx=0; idx<nrtimes; idx++ )
+    {
+	float inldipval = inldip->getValue(curt,inlattrib) / M_360_2_PI;
+	float crldipval = crldip->getValue(curt,crlattrib) / M_360_2_PI;
+
+	float a1 = -1;			// One vector in the dipplane
+	float b1 = -1;
+	float c1 = a1 * sin( inldipval ) + b1*sin( crldipval );
+
+	float a2 = -1;			// Another vector in the dipplane
+	float b2 = 1;
+	float c2 = a2*sin( inldipval ) + b2*sin( crldipval );
+
+	float a3 = b1*c2-b2*c1;		// The planes normal vector
+	float b3 = -a1*c2+a2*c1;
+	float c3 = a1*b2-a2*b1;
+
+	if ( c3 < 0 )
+	{
+	    a3 = -a3;
+	    b3 = -b3;
+	    c3 = -c3;
+	}
+
+	float a4 = 0;			// The lights vector
+	float b4 = 0;
+	float c4 = 1;
+
+	if ( aspect.inl!=90 )
+	{
+	    a4 = cos( aspect.crl / M_360_2_PI );
+	    b4 = sin( aspect.crl / M_360_2_PI );
+	    c4 = tan( aspect.inl / M_360_2_PI );
+	}
+
+	float val = 1 - (a3*a4+b3*b4+c3*c4)/ sqrt( (a3*a3+b3*b3+c3*c3) *
+					  (a4*a4+b4*b4+c4*c4 ));	
+	outp[idx] = val > 0 ? val : 0;
+
+	curt += step;
+    }
+
+    return 0;
+}
---
+++ plugins/AttribExp/expwavelet1d.cc
@@ -0,0 +1,203 @@
+/*+
+ * COPYRIGHT: (C) de Groot-Bril Earth Sciences B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Oct 1999
+-*/
+
+static const char* rcsID = "$Id: expwavelet1d.cc,v 1.1 2002-01-04 10:25:50 kristofer Exp $";
+
+
+#define mEPSILON 1E-9
+
+#include "expwavelet1d.h"
+#include "attribprovider.h"
+#include "seistrc.h"
+#include "simpnumer.h"
+#include "arrayndimpl.h"
+#include "ptrman.h"
+
+
+DefineEnumNames(Wavelet1DAttrib, WaveletLen,0,"WaveletLen")
+{	  "2"
+	, "4"
+	, "8"
+	, "16"
+	, "32"
+	, "64"
+	, "128"
+	, "256"
+	, "512"
+	, "1024"
+	, "2048"
+	, "4196"
+	, 0
+};
+
+
+Wavelet1DAttrib::Wavelet1DAttrib( Parameters* param)
+    : minwaveletlen( (Wavelet1DAttrib::WaveletLen) ((int) param->minwaveletlen))
+    , maxwaveletlen( (Wavelet1DAttrib::WaveletLen) ((int) param->maxwaveletlen))
+    , wavelet( (WaveletTransform::WaveletType) ((int) param->wavelet) )
+    , AttribCalc( new Wavelet1DAttrib::Task( *this ) )
+{ 
+    param->fillDefStr( desc );
+    delete param;
+
+    WaveletLen tmp;
+    if ( maxwaveletlen<minwaveletlen ) mSWAP(minwaveletlen, maxwaveletlen, tmp);
+    scalelen = intpow( 2, maxwaveletlen );
+    dsg = Interval<int>( -(scalelen-1), (scalelen-1) );
+
+    AttribInputSpec* spec = new AttribInputSpec;
+    spec->setDesc("Data which should be analysed");
+    inputspec += spec;
+
+    prepareInputs();
+}
+
+
+Wavelet1DAttrib::~Wavelet1DAttrib()
+{  ; }
+
+
+int Wavelet1DAttrib::Task::nextStep()
+{
+    Wavelet1DAttrib::Task::Input* inp = (Wavelet1DAttrib::Task::Input*)input;
+    const SeisTrc* trc = inp->trc;
+    const float inpstep = trc->info().sampling.step;
+    const int component = inp->attrib;
+
+    const int scalelen =  calculator.scalelen;
+
+    int len = nrtimes + scalelen;
+    while ( !isPower( len, 2 ) ) len++;
+
+    Array1DImpl<float> signal( len );
+    int off = (len-nrtimes)/2;
+
+
+    for ( int idx=0; idx<len; idx++ )
+    {
+	signal.set(idx, trc->getValue(t1+(idx-off)*inpstep, component) );
+    }
+
+    Array1DImpl<float> transformed( len );
+    DiscreteWaveletTransform transform( calculator.wavelet );
+
+    transform.setInputInfo( signal.info() );
+    transform.init();
+    transform.transform( signal, transformed );
+
+    const int nrscales = isPower( len, 2 ) + 1;
+    ArrPtrMan<float> spectrum =  new float[nrscales];
+    spectrum[0] = fabs(transformed.get(0)); // scale 0 (dc)
+    spectrum[1] = fabs(transformed.get(1)); // scale 1 
+
+    for ( int idx=0; idx<nrtimes; idx++ )
+    {
+	for ( int scale=2; scale<nrscales; scale++ )
+	{
+	    int scalepos = intpow(2,scale-1) + ((idx+off) >> (nrscales-scale));
+	    spectrum[scale] = fabs(transformed.get(scalepos));
+	}
+
+	int maxid = -1;
+	float max = 0;
+
+	float sum = 0;
+	float wsum = 0;
+	float sqsum = 0;
+	float wsqsum = 0;
+
+	float sumbeforemax = 0;
+	float sumaftermax = 0;
+
+	int nrstatscales = 0;
+
+	int firstscale = nrscales-calculator.maxwaveletlen-1; // Reversed order
+	int lastscale = nrscales-calculator.minwaveletlen-1; // Reversed order
+	// The numbering of scales is not straighforward.
+	// The transform and all literature counts the scales
+	// from 0 (dc) to M (where N=2^(M+1)=nr samples). The user,
+	// does not know how long the transform will be, and will thus
+	// specify the scales as the length of the wavelet, i.e.
+	// waveletlength = 2^(userscale+1). The conversion between the two
+	// formats are done above.
+
+	for ( int scale=firstscale; scale<=lastscale; scale++ )
+	{
+	    nrstatscales++;
+	    float val = spectrum[scale];
+	    sum += val;
+	    sqsum += val*val;
+	    wsum += scale*val;
+	    wsqsum += scale*val*val;
+	    if ( val>max ) { maxid=scale; max=val; }
+	    else if ( maxid==-1 ) sumbeforemax += val;
+	    else sumaftermax += val;
+	}
+
+	if ( outp[0] ) (outp[0])[idx] = wsum/sum;
+	if ( outp[1] ) (outp[1])[idx] = wsqsum/sqsum;
+	if ( outp[2] ) (outp[2])[idx] = maxid;
+	if ( outp[3] )
+	{
+	    float scale = wsum/sum;
+
+	    int intoff = (int) scale;
+	    float pos = scale - intoff;
+
+	    float res = polyInterpolate(
+			intoff ? spectrum[intoff-1] : mUndefValue,
+			intoff > lastscale ? mUndefValue :spectrum[intoff],
+			intoff+1 > lastscale ? mUndefValue :spectrum[intoff+1],
+			intoff+2 > lastscale ? mUndefValue :spectrum[intoff+2],
+			pos );
+
+	    if ( mIsUndefined( res ) ) res = 0;
+	    (outp[3])[idx] = res/sum;
+	}
+
+	if ( outp[4] ) (outp[4])[idx] = spectrum[maxid];
+
+	if ( outp[5] )
+	{
+	    float scale = wsqsum/sqsum;
+
+	    int intoff = (int) scale;
+	    float pos = scale - intoff;
+
+	    float res = polyInterpolate(
+			intoff ? spectrum[intoff-1] : mUndefValue,
+			intoff > lastscale ? mUndefValue :spectrum[intoff],
+			intoff+1 > lastscale ? mUndefValue :spectrum[intoff+1],
+			intoff+2 > lastscale ? mUndefValue :spectrum[intoff+2],
+			pos );
+
+	    if ( mIsUndefined( res ) ) res = 0;
+	    (outp[5])[idx] = res/sum;
+	}
+
+	if ( outp[6] ) (outp[6])[idx] = sumbeforemax/sum;
+	if ( outp[7] ) (outp[7])[idx] = sumaftermax/sum;
+	if ( outp[8] ) (outp[8])[idx] = sqrt((sqsum-sum*sum/nrstatscales) /
+	    				(nrstatscales-1));
+
+    }
+
+    return 0;
+}
+			
+
+
+bool Wavelet1DAttrib::Task::Input::set(const BinID& pos, 
+	const ObjectSet<AttribProvider>& inp, const TypeSet<int>& inpattrib,
+			     const TypeSet<float*>&)
+{
+    trc = inp[0]->getTrc( pos.inl, pos.crl ); 
+
+    attrib = inp[0]->attrib2component( inpattrib[0] );
+    return trc ? true : false;
+} 
+
+
