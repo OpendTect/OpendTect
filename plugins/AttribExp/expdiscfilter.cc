95eb738db4806f35aa4d17c800ec875b37de42ba
Nanne Hemstra <nanne.hemstra@dgbes.com>
2002-01-04 11:25:59 +0000

    Moved discfilter to experimental factory
    git-svn-id: https://svn.opendtect.org/od/trunk@827 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ plugins/AttribExp/expdiscfilter.cc
@@ -0,0 +1,298 @@
+/*+
+ * COPYRIGHT: (C) de Groot-Bril Earth Sciences B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Oct 1999
+-*/
+
+static const char* rcsID = "$Id: expdiscfilter.cc,v 1.1 2002-01-04 11:25:59 nanne Exp $";
+
+
+#define mEPSILON 1E-9
+
+#include "expdiscfilter.h"
+#include "simpnumer.h"
+#include "attribprovider.h"
+#include "seistrc.h"
+#include "ptrman.h"
+#include "arrayndutils.h"
+#include "attribdescsetproc.h"
+
+
+bool DiscFilterAttrib::Parameters::update()
+{
+    if ( constantvel && velocity.getMode() == AttribParameter::Disabled )
+    {
+	velocity.setMode( AttribParameter::Required );
+	return true;
+    }
+    else if ( !constantvel && velocity.getMode() == AttribParameter::Required )
+    {
+	velocity.setMode( AttribParameter::Disabled );
+	return true;
+    }
+
+    return false;
+}
+
+
+DiscFilterAttrib::DiscFilterAttrib( Parameters* param )
+    : sg( -param->radius, param->radius )
+    , velocity( param->velocity )
+    , constantvel( param->constantvel )
+    , radius( param->radius )
+    , fast( param->fast )
+    , stepout( param->radius, param->radius )
+    , AttribCalc( new DiscFilterAttrib::Task( *this ) )
+{ 
+    param->fillDefStr( desc );
+    delete param;
+
+    AttribInputSpec* spec = new AttribInputSpec;
+    spec->setDesc( "Data to filter");
+    inputspec += spec;
+
+    spec = new AttribInputSpec;
+    spec->setDesc("In-line dip");
+    spec->forbiddenDts += Seis::Ampl;
+    spec->forbiddenDts += Seis::Frequency;
+    spec->forbiddenDts += Seis::Phase;
+    spec->forbiddenDts += Seis::AVOGradient;
+    spec->forbiddenDts += Seis::UnknowData;
+    inputspec += spec;
+
+    spec = new AttribInputSpec;
+    spec->setDesc("Cross-line dip");
+    spec->forbiddenDts += Seis::Ampl;
+    spec->forbiddenDts += Seis::Frequency;
+    spec->forbiddenDts += Seis::Phase;
+    spec->forbiddenDts += Seis::AVOGradient;
+    spec->forbiddenDts += Seis::UnknowData;
+    inputspec += spec;
+
+    if ( !constantvel )
+    {
+	spec = new AttribInputSpec;
+	spec->setDesc( "Velocity" );
+	inputspec += spec;
+    }
+
+    prepareInputs();
+}
+
+
+DiscFilterAttrib::~DiscFilterAttrib( ) {}
+
+
+Seis::DataType DiscFilterAttrib::dataType(int val,
+				const TypeSet<Seis::DataType>& dts) const
+{
+    if ( val == mDiscFilterStdDev || val == mDiscFilterVar )
+        return Seis::UnknowData;
+
+    return dts[0];
+}
+
+
+bool DiscFilterAttrib::init()
+{
+    inldist = common->inldist*common->stepoutstep.inl;
+    crldist = common->crldist*common->stepoutstep.crl;
+
+    return AttribCalc::init();
+} 
+
+
+void DiscFilterAttrib::Task::set( float t1_, int nrtimes_, float step_,
+				      const AttribCalc::Task::Input* inp,
+				      const TypeSet<float*>& outp_)
+{
+    t1 = t1_;
+    nrtimes = nrtimes_;
+    step = step_;
+    input = inp;
+    avg = outp_[mDiscFilterAvg];
+    med = outp_[mDiscFilterMed];
+    stddev = outp_[mDiscFilterStdDev];
+    variance = outp_[mDiscFilterVar];
+    min = outp_[mDiscFilterMin];
+    max = outp_[mDiscFilterMax];
+    max = outp_[mDiscFilterMostFreq];
+
+    indata.setSize( calculator.radius*2+1, calculator.radius*2+1, calculator.radius*2+1 );
+
+}
+
+
+bool DiscFilterAttrib::Task::Input::set( const BinID& pos,
+			     const ObjectSet<AttribProvider>& inputproviders,
+			     const TypeSet<int>& inputattribs,
+			     const TypeSet<float*>&)
+{
+    const int radius = calculator.radius;
+
+    if ( !trcs )
+	trcs = new Array2DImpl<const SeisTrc*>( radius*2+1, radius*2+1 );
+
+    int nrtrcs = 0;
+    for ( int idx=-radius; idx<=radius; idx++ )
+    { 
+	for ( int idy=-radius; idy<=radius; idy++ )
+	{
+	    SeisTrc* trc = inputproviders[0]->getTrc( 	pos.inl + idx, 
+							pos.crl + idy );
+
+	    if ( !calculator.fast && !trc ) return false;
+	    if ( trc ) nrtrcs++;
+	    trcs->set(idx+radius,idy+radius,trc);
+	}
+    }
+
+    if ( !nrtrcs ) return false;
+
+    inldiptrc = inputproviders[1]->getTrc( pos.inl, pos.crl);
+    crldiptrc = inputproviders[2]->getTrc( pos.inl, pos.crl);
+
+    if ( !calculator.constantvel )
+    {
+	veltrc = inputproviders[3]->getTrc( pos.inl, pos.crl);
+	if ( !veltrc ) return 0;
+
+	velattrib = inputproviders[3]->attrib2component( inputattribs[3] );
+    }
+
+    dataattrib = inputproviders[0]->attrib2component( inputattribs[0] );
+
+    if ( inldiptrc )
+	inldipattrib = inputproviders[1]->attrib2component( inputattribs[1] );
+
+    if ( crldiptrc )
+	crldipattrib = inputproviders[2]->attrib2component( inputattribs[2] );
+
+    return true;
+}
+
+
+int DiscFilterAttrib::Task::nextStep()
+{
+    const DiscFilterAttrib::Task::Input* inp = 
+			(const DiscFilterAttrib::Task::Input*) input;
+
+    const int radius = calculator.radius; 
+    const int sz = radius*2+1;
+    const int radius2 = radius*radius;
+    const SeisTrc* inldiptrc = inp->inldiptrc;
+    const SeisTrc* crldiptrc = inp->crldiptrc;
+    const SeisTrc* veltrc = inp->veltrc;
+    const float inldist = calculator.inldist;
+    const float crldist = calculator.crldist;
+    const float unitdist = inldist>crldist?crldist:inldist;
+
+    int const dataattrib = inp->dataattrib;
+    int const velattrib = inp->velattrib;
+    int const inldipattrib = inp->inldipattrib;
+    int const crldipattrib = inp->crldipattrib;
+
+    Array2DImpl<const SeisTrc*>& trcs = *inp->trcs;
+
+    const SeisTrc* dttrc = 0;
+    for ( int idx=0; idx<sz; idx++ )
+    {
+	for ( int idy=0; idy<sz; idy++ )
+	{
+	    dttrc = trcs.get(idx,idy);
+	    if ( dttrc ) break;
+	}
+
+	if ( dttrc ) break;
+    }
+	
+
+    const float inpstep = dttrc->info().sampling.step;
+ 
+    float curt = t1;
+
+    for ( int idx=0; idx<nrtimes; idx++ )
+    {
+	float velocity = veltrc ? veltrc->getValue( curt, velattrib )
+				: calculator.velocity; 
+
+	if ( !calculator.fast )
+	{
+	    for ( int idi=0; idi<sz; idi++ )
+	    {
+		for ( int idc=0; idc<sz; idc++ )
+		{
+		    for ( int idt=0; idt<sz; idt++ )
+		    {
+			float val = trcs.get(idi,idc)->getValue(curt+
+					  inpstep* (idt-radius),dataattrib);
+			indata.set(idi,idc,idt,val);
+		    }
+		}
+	    }
+	}
+
+	float inldip = inldiptrc ? inldiptrc->getValue(curt,inldipattrib) : 0;
+	float crldip = crldiptrc ? crldiptrc->getValue(curt,crldipattrib) : 0;
+
+	float angle0 = atan2( crldip, -inldip );
+	float angle1 = atan(sqrt(inldip*inldip+crldip*crldip)*1e-6*velocity);
+
+	stat.clear();
+
+	for ( int pos0=-radius; pos0<radius; pos0++ )
+	{
+	    for ( int pos1=-radius; pos1<radius; pos1++ )
+	    {
+		if ( pos0*pos0+pos1*pos1 > radius2 )
+		    continue;
+
+		float p = pos0 * unitdist;
+		float q = pos1 * unitdist;
+		float qz = q * sin(angle1);
+		float qic = q * cos(angle1);
+
+
+		float rho = atan2( qic, p ); 
+		float r = sqrt( qic*qic + p*p );
+
+		float crl = r*cos(angle0+rho);
+		float inl = r*sin(angle0+rho);
+
+		float val;
+
+		if ( !calculator.fast )
+		{
+		    val = Array3DInterpolate( indata,
+					      radius+inl/inldist,
+					      radius+crl/crldist,
+					      radius+qz/velocity/inpstep);
+		    stat += val;
+		}
+		else
+		{
+		    const SeisTrc* trc = trcs.get(radius+mNINT(inl/inldist),
+					      radius+mNINT(crl/crldist));
+
+		    if ( trc )
+		    {
+			val = trc->getValue(curt+qz/velocity,dataattrib); 
+			stat += val;
+		    }
+		}
+	    }
+	}	
+
+	if ( avg )	avg[idx] = stat.mean();
+	if ( med )	med[idx] = stat.median();
+	if ( stddev )	stddev[idx] = stat.stdDev();
+	if ( variance )	variance[idx] = stat.variance();
+	if ( min )	min[idx] = stat.min();
+	if ( max )	max[idx] = stat.max();
+	if ( mostfreq )	mostfreq[idx] = stat.mostFreq();
+
+	curt += step;
+    }
+		
+    return 0;
+}
---
+++ plugins/AttribExp/expdiscfilter.h
@@ -0,0 +1,235 @@
+#ifndef expdiscfilter_h
+#define expdiscfilter_h
+
+/*@+
+________________________________________________________________________
+
+ CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
+ Author:        Kristofer Tingdahl
+ Date:          07-10-1999
+ RCS:           $Id: expdiscfilter.h,v 1.1 2002-01-04 11:25:47 nanne Exp $
+________________________________________________________________________
+
+DiscFilter radius= planefilter=true [constantvel=true] [velocity=] [fast=]
+
+Discfilter places a disc with the "radius" along the dip specified on the
+input. The filter extracts samples along the disc and derives statistical
+properties from it. If planefilter is enabled, the filter will go the all
+traces within the radius. If planefilter is turned off, it will cut out a
+disc through the data regardless of which trace it might be. At high
+dips, it might equally well take many samples from the same trace.
+This option makes it neccecary to convert the time-domain to distance, and
+this is what the velocity is for. Velocity can either be stated on commandline
+or be given for each sample. The 'fast' option is use when planefilter is
+turned off and specifies wether the filter should snap each sample to the
+closest trace, or do an interpolation between the traces.  
+
+Input:
+0	Data
+1	Inline dip
+2	Crossline dip
+3	Velocity (only if not stated on command-line)
+
+Outputs:
+0	Mean
+1	Median
+2	Stddev
+3	Variance
+4       Min
+5       Max
+6       Most Frequent
+
+
+@$*/
+
+#include <attribcalc.h>
+#include <basictask.h>
+#include <position.h>
+#include <limits.h>
+#include <seistrc.h>
+#include <arrayndimpl.h>
+#include <runstat.h>
+#include <attribparamimpl.h>
+
+#define mDiscFilterAvg		0
+#define mDiscFilterMed		1
+#define mDiscFilterStdDev	2
+#define mDiscFilterVar		3
+#define mDiscFilterMin		4
+#define mDiscFilterMax		5
+#define mDiscFilterMostFreq	6
+
+#define mDiscFilterNrVals	7
+
+
+class DiscFilterAttrib : public AttribCalc
+{
+public:
+    mAttrib5Param( DiscFilterAttrib
+	, "DiscFilter"
+	, IntAttribParameter
+	    , radius
+	    , IntAttribParameter( "radius"
+		, AttribParameter::Required
+		, 3
+		, Interval<int>(0,100)
+	    )
+	, BoolAttribParameter
+	    , planefilter
+	    , BoolAttribParameter( "planefilter"
+		, AttribParameter::Default
+		, false
+	    )
+	, BoolAttribParameter
+	    , constantvel
+	    , BoolAttribParameter( "constantvel"
+		, AttribParameter::Default
+		, true
+	    )
+	, FloatAttribParameter
+	    , velocity
+	    , FloatAttribParameter( "velocity"
+		, AttribParameter::Required
+		, 4000
+		, Interval<float>(0,mUndefValue)
+	    )
+	, BoolAttribParameter
+	    , fast
+	    , BoolAttribParameter( "fast"
+		, AttribParameter::Default
+		, true
+	    )
+	, mAttribParamFormHasUpdate);
+
+			DiscFilterAttrib( Parameters* );
+			~DiscFilterAttrib();
+
+    bool		init();
+
+    int                 nrAttribs() const { return mDiscFilterNrVals ; }
+    const char*		attribName(int val) const
+			{
+			    switch (val)
+                            {
+				case mDiscFilterAvg:     return "Average";
+				case mDiscFilterMed:     return "Median";
+				case mDiscFilterStdDev:  return "StdDev";
+				case mDiscFilterVar:     return "Variance";
+				case mDiscFilterMin:     return "Min";
+				case mDiscFilterMax:     return "Max";
+				case mDiscFilterMostFreq:return "MostFreq";
+			    }
+
+			    return 0;
+			}
+
+    Seis::DataType	dataType(int,const TypeSet<Seis::DataType>&) const;
+
+    const BinID*	reqStepout(int inp, int) const
+			{ return inp ? 0 : &stepout; }
+    const Interval<int>* reqExtraSamples(int inp, int) const
+			{ return inp ? 0 : &sg; }
+
+    const Interval<float>* inlDipMargin(int,int) const { return 0; }
+    const Interval<float>* crlDipMargin(int,int) const { return 0; }
+
+    Seis::DataType	dataType(int val, int) const;
+
+    const char* 	definitionStr() const { return desc; }
+    void		setCommonInfo( const AttribProcessCommonInfo& ni )
+			{ common = &ni; }
+
+protected:
+    
+    BufferString		desc;
+    Interval<int>		sg;
+    BinID			stepout;
+
+    float			inldist;
+    float			crldist;
+
+    int 			radius;
+    float			velocity;
+    bool			constantvel;
+    bool 			fast;
+    const AttribProcessCommonInfo*	common;
+
+    class Task : public AttribCalc::Task
+    {
+    public:
+	class Input : public AttribCalc::Task::Input
+	{
+	public:
+				Input( const DiscFilterAttrib& calculator_ )
+				    : calculator ( calculator_ )
+				    , inldiptrc( 0 )
+				    , crldiptrc( 0 )
+				    , veltrc( 0 )
+				    , trcs( 0 ) {}
+
+	    bool                set( const BinID&, 
+				    const ObjectSet<AttribProvider>&, 
+				    const TypeSet<int>&,
+				    const TypeSet<float*>& );
+
+	    AttribCalc::Task::Input* clone() const
+			{ return new DiscFilterAttrib::Task::Input(*this); }
+
+	    Array2DImpl<const SeisTrc*>*	trcs;
+	    const SeisTrc*			veltrc;
+	    const SeisTrc*			inldiptrc;
+	    const SeisTrc*			crldiptrc;
+
+	    int					dataattrib;
+	    int					velattrib;
+	    int					inldipattrib;
+	    int					crldipattrib;
+
+	    const DiscFilterAttrib&		calculator;
+	};
+
+			    Task( const DiscFilterAttrib& calculator_ )
+				: avg( 0 )
+				, med( 0 )
+				, stddev( 0 )
+				, variance( 0 )
+				, min( 0 )
+				, max( 0 )
+				, mostfreq( 0 )
+				, indata( 0,0,0 )
+				, calculator( calculator_ ) {}
+
+	void		    set( float , int , float ,
+				 const AttribCalc::Task::Input* ,
+                                 const TypeSet<float*>& );
+
+	AttribCalc::Task*    clone() const 
+			    { return new DiscFilterAttrib::Task(*this); }
+
+	int		    getFastestSz() const { return 25; }
+
+	int		    nextStep();
+
+	AttribCalc::Task::Input* getInput() const
+		{ return new DiscFilterAttrib::Task::Input( calculator ); }
+
+    protected:
+	float*          avg;
+	float*          med;
+	float*          stddev;
+	float*          variance;
+	float*          min;
+	float*          max;
+	float*          mostfreq;
+
+	Array3DImpl<float>		indata;
+	RunningStatistics<float,float>	stat;
+
+	const DiscFilterAttrib&	calculator;
+    };
+
+    friend DiscFilterAttrib::Task;
+    friend DiscFilterAttrib::Task::Input;
+};
+
+#endif
