e167d7e8c3c0c02cae9cdbad5a2b4c2816648c9f
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2002-01-04 09:37:49 +0000

    Created factory for experimental attribs
    git-svn-id: https://svn.opendtect.org/od/trunk@822 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ plugins/AttribExp/expdeconv.h
@@ -0,0 +1,195 @@
+#ifndef expdeconv_h
+#define expdeconv_h
+
+/*@+
+________________________________________________________________________
+
+ CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
+ Author:        Kristofer Tingdahl
+ Date:          07-10-1999
+ RCS:           $Id: expdeconv.h,v 1.1 2002-01-04 09:37:49 kristofer Exp $
+________________________________________________________________________
+
+DeConvolve wavelet=
+
+Performs a deconvolution in fourier domain with the specified wavelet.
+
+Input:
+0	Real data
+1	Imag data
+
+Output:
+0	Real data
+1	Imag data
+
+@$*/
+
+#include "attribcalc.h"
+#include "basictask.h"
+#include "position.h"
+#include <limits.h>
+#include "seistrc.h"
+#include <complex>
+#include "arrayndimpl.h"
+#include "fft.h"
+#include "attribparamimpl.h"
+
+#include <arrayndutils.h>
+
+    
+class DeConvolveAttrib : public AttribCalc
+{
+public:
+    mAttrib5Param(DeConvolveAttrib,"DeConvolve",
+	TimeGateAttribParameter, gate,
+	   TimeGateAttribParameter(	"samplegate",
+					AttribParameter::Required,
+					TimeGate(-64,64),
+					TimeGate(-mUndefValue, mUndefValue)),
+	BinIDAttribParameter, pos1,
+	    BinIDAttribParameter(	"pos1",
+					AttribParameter::Required,
+					BinID(0,1),
+					Interval<int>(-100,100),
+					Interval<int>(-100,100)),
+	BinIDAttribParameter, neighbourhood,
+	    BinIDAttribParameter(	"neighbourhood",
+					AttribParameter::Required,
+					BinID(1,1),
+					Interval<int>(1,10),
+					Interval<int>(1,10)),
+	EnumAttribParameter, window,
+            EnumAttribParameter(	"window",
+					ArrayNDWindow::WindowTypeNames,
+					AttribParameter::Required,
+					0),
+	BoolAttribParameter, steering,
+	    BoolAttribParameter(	"steering",
+					AttribParameter::Default,
+					true),);
+
+
+				DeConvolveAttrib( Parameters* );
+				~DeConvolveAttrib();
+
+    bool			init();
+
+    const Interval<float>*	reqInterval( int, int ) const {return &gate;}
+    const BinID*		reqStepout( int i, int ) const
+				{ return i ? 0 : &neighbourhood; }
+
+    int                 	nrAttribs() const { return 2; }
+
+    const char*         	attribName(int val) const
+				{
+				    switch (val)
+				    {
+					case 0:     return "1";
+					case 1:     return "2";
+				    }
+
+				    return 0;
+				}
+
+    Seis::DataType		dataType(int,const TypeSet<Seis::DataType>&) const
+				{ return Seis::UnknowData; }
+
+    const char* 		definitionStr() const { return desc; }
+    void                	setCommonInfo( const AttribProcessCommonInfo& ni )
+				{ common = &ni; }
+
+
+protected:
+    Interval<float>		gate;
+    bool			steering;
+    BinID			neighbourhood;
+    BinID			pos1;
+    
+    FFT				fft;
+    FFT				ifft;
+    int 			fftsz;
+    ArrayNDWindow::WindowType	windowtype;
+    ArrayNDWindow*		window;
+    float			inpstep;
+    float			df;
+
+    BufferString		desc;
+    const AttribProcessCommonInfo*	common;
+
+    class Task : public AttribCalc::Task
+    {
+    public:
+	class Input : public AttribCalc::Task::Input
+	{
+	public:
+				Input( const DeConvolveAttrib& calculator_ )
+				    : calculator ( calculator_ )
+				    , trcs( 0 )
+				    , inldiptrc( 0 )
+				    , crldiptrc( 0 )
+				{}
+
+				~Input();
+
+	    bool                set( const BinID&, 
+				    const ObjectSet<AttribProvider>&, 
+				    const TypeSet<int>&,
+				    const TypeSet<float*>& );
+
+	    AttribCalc::Task::Input* clone() const
+			{ return new DeConvolveAttrib::Task::Input(*this); }
+
+	    Array2DImpl<SeisTrc*>*	trcs;
+	    SeisTrc*			inldiptrc;
+	    SeisTrc*			crldiptrc;
+
+	    int				dataattrib;
+	    int				inldipattrib;
+	    int				crldipattrib;
+
+	    const DeConvolveAttrib& 	calculator;
+	};
+
+			    Task( const DeConvolveAttrib& calculator_ );
+			    ~Task();
+	
+	void		    set( float t1_, int nrtimes_, float step_, 
+					    const AttribCalc::Task::Input* inp,
+                                            const TypeSet<float*>& outp_)
+				{
+				    t1 = t1_;
+				    nrtimes = nrtimes_; 
+				    step = step_;
+				    input = inp; 
+				    out0 = outp_[0];
+				    out1 = outp_[1];
+				}
+
+	AttribCalc::Task*    clone() const 
+				{ return new DeConvolveAttrib::Task(*this); }
+
+	int		    getFastestSz() const { return 25; }
+
+	int		    nextStep();
+
+	AttribCalc::Task::Input* getInput() const
+		    { return new DeConvolveAttrib::Task::Input( calculator ); }
+
+    protected:
+	float*				out0;
+	float*				out1;
+
+	const DeConvolveAttrib&		calculator;
+	Array2D<Array1D<float_complex>*>*	tracesegments;	
+	Array1D<float_complex>*		spectrum0;
+	Array1D<float_complex>*		spectrum1;
+	Array1D<float_complex>*		spectrumaverage;
+	Array1D<float_complex>*		spectrumoutput;
+	Array1D<float_complex>*		traceoutput;
+    };
+
+    friend DeConvolveAttrib::Task;
+    friend DeConvolveAttrib::Task::Input;
+};
+
+#endif
