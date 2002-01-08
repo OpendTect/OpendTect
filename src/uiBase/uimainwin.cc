c269caf1bc98932a83ecb2c107be2091293129d6
Nanne Hemstra <nanne.hemstra@dgbes.com>
2002-01-08 13:36:30 +0000

    help button only when using GDI
    git-svn-id: https://svn.opendtect.org/od/trunk@858 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiBase/uimainwin.cc
+++ src/uiBase/uimainwin.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Lammertink
  Date:          31/05/2000
- RCS:           $Id: uimainwin.cc,v 1.32 2002-01-08 10:36:08 arend Exp $
+ RCS:           $Id: uimainwin.cc,v 1.33 2002-01-08 13:36:30 nanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -525,7 +525,7 @@ void uiDialogBody::finalise()
 	}
 	if ( withsavebut && saveText != "" )
 	    saveBut = new uiCheckBox( centralWidget_, saveText );
-	if ( helpId != "" )
+	if ( helpId != "" && GetDgbApplicationCode() == mDgbApplCodeGDI )
 	    helpBut = new uiPushButton( centralWidget_, "?" );
 
         if ( !withmenubar )
