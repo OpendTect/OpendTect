f4630f929122c382121e62c10d3112fac24c126f
Nanne Hemstra <nanne.hemstra@dgbes.com>
2002-01-18 16:27:07 +0000

    setSzPol medvar to combobox
    git-svn-id: https://svn.opendtect.org/od/trunk@908 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiTools/uisettings.cc
+++ src/uiTools/uisettings.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        Nanne Hemstra
  Date:          November 2001
- RCS:           $Id: uisettings.cc,v 1.3 2002-01-08 11:25:56 nanne Exp $
+ RCS:           $Id: uisettings.cc,v 1.4 2002-01-18 16:27:07 nanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -30,6 +30,7 @@ uiSettings::uiSettings( uiParent* p, const char* nm )
     }
 
     itemfld = new uiLabeledComboBox( this, "Item", "", true );
+    itemfld->box()->setSzPol( SzPolicySpec().setHSzP( SzPolicySpec::medvar ) );
     itemfld->box()->addItems( items );
     itemfld->box()->setCurrentItem( 0 );
     itemfld->box()->selectionChanged.notify( mCB(this,uiSettings,itemSel) );
