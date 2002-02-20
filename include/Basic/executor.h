af0babacd8749cd80b4a7425d03afa12858972b0
Bert Bril <bert.bril@dgbes.com>
2002-02-20 17:14:48 +0000

    minor things
    git-svn-id: https://svn.opendtect.org/od/trunk@992 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/executor.h
+++ include/Basic/executor.h
@@ -7,8 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
  Author:	A.H.Bril
  Date:		11-7-1996
- Contents:	Executor
- RCS:		$Id: executor.h,v 1.3 2001-05-31 12:55:04 windev Exp $
+ RCS:		$Id: executor.h,v 1.4 2002-02-20 17:14:48 bert Exp $
 ________________________________________________________________________
 
 -*/
@@ -19,8 +18,19 @@ ________________________________________________________________________
 
 /*!\brief specification to enable chunkwise execution a process.
 
-The execute() utility executes while logging message() etc. to a stream.
+Interface enabling separation of the control of execution of any process from
+what actually is going on. The work is done by calling the nextStep() method
+until either ErrorOccurred or Finished is returned. To enable logging and/or
+communication with the user, two types of info can be made available (the
+methods will be called before the step is executed). Firstly, a message.
+Secondly, info on the progress.
+It is common that Executors are combined to a new Executor object. This is
+the most common reason why totalNr() can change.
+
 If nextStep returns -1 (Failure) the error message should be in message().
+
+The execute() utility executes the process while logging message() etc. to
+a stream. Useful in batch situations.
 
 */
 
@@ -32,17 +42,21 @@ public:
 			: UserIDObject(nm)		{}
     virtual		~Executor()			{}
 
-    virtual const char*	message() const			= 0;
     virtual int		nextStep()			= 0;
-			/*!< return value > 1 should mean a warning is
-			     available */
+    			//!< Essentially returns as required by BasicTask
+    static const int	ErrorOccurred;		// -1
+    static const int	Finished;		// 0
+    static const int	MoreToDo;		// 1
+    static const int	WarningAvailable;	// 2
 
+    virtual const char*	message() const			{ return "Working"; }
     virtual int		totalNr() const			{ return -1; }
+    			//!< If unknown, return -1
     virtual int		nrDone() const			{ return 0; }
     virtual const char*	nrDoneText() const		{ return "Nr done"; }
 
     virtual bool	execute(ostream* log=0,
-				bool isfirst=YES,bool islast=YES);
+				bool isfirst=true,bool islast=true);
 
 };
 
