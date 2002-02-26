66eb5b06b642750b2ff794c18321533467986e92
Bert Bril <bert.bril@dgbes.com>
2002-02-26 15:46:45 +0000

    added comment
    git-svn-id: https://svn.opendtect.org/od/trunk@1014 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiBase/uiobj.h
+++ include/uiBase/uiobj.h
@@ -7,10 +7,80 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Lammertink
  Date:          25/08/1999
- RCS:           $Id: uiobj.h,v 1.19 2002-01-10 11:14:52 arend Exp $
+ RCS:           $Id: uiobj.h,v 1.20 2002-02-26 15:46:45 bert Exp $
 ________________________________________________________________________
 
 -*/
+
+/*! \mainpage
+
+  \section intro Introduction
+
+This module is a set of cooperating classes that enable creating User
+Interfaces. This layer on top of the already wonderful Qt package was created
+because of the following problems:
+
+- Qt provides an enormous set of tools, of which we need only a fraction
+- On the other hand, Qt does not offer things like:
+   - Selection of widget on basis of data characteristics
+   - Automated 'black-box' layouting (enabling full grouping into new classes)
+   - Integration with our data types
+   - Generalised Callback/Notifier support (instead of Qt's Signal/Slot)
+- If we use Qt directly, no isolation is present.
+
+Therefore, as with most external libraries, we chose to make a new layer to
+combine the power of Qt with the flexibility of more generalised design
+principles.
+
+
+\section usage Usage
+
+The basic principles are:
+- Objects are linked to each other in the window by attaching.
+- Events are notified by specifically asking a Notifier in the class.
+- All objects can be grouped; every group must be prepared to be attached.
+- 'Simple' data input should preferably be done through the uiGenInput
+  class (see uiTools module).
+
+The Qt window painting facilities are only used for quick sketching, the
+code generation capacity is not used. Example:
+
+<code>
+    IntInpSpec spec( lastnrclasses );<br>
+    spec.setLimits( Interval<int>( 0, 100 ) );<br>
+    nrclassfld = new uiGenInput( this, "Number of classes", spec );<br>
+<br>
+    FloatInpSpec inpspec( lastratiotst*100 );<br>
+    inpspec.setLimits( Interval<float>( 0, 100 ) );<br>
+    perctstfld = new uiGenInput( this, "Percentage used for test set",
+	    				inpspec );<br>
+    perctstfld->attach( alignedBelow, nrclassfld );<br>
+    <br>
+    defbut = new uiPushButton( this, "Default" ); <br>
+    defbut->activated.notify( mCB(this,ThisClass,setPercToDefault) ); <br>
+    defbut->attach( rightOf, perctstfld );
+</code>
+
+Note that all objects could have been made:
+- Conditional (only if a condition is met)
+- Iterative (any number that may be necessary)
+- As part of a group (e.g. to later treat as one uiObject)
+- From a Factory (for example an object transforming a string into a uiObject).
+
+
+\section design Design
+
+In the uiBase directory, you'll find classes that directly communicate with Qt
+to implement (parts of) this strategy. To keep the header files uncoupled
+from the Qt header files, there is a mechanism where the 'ui' class has a
+'ui'-Body class that is a subclass of a Qt class.
+
+Almost every 'visible' object is a uiObject. Besides the different subclasses,
+there is also the uiGroup which is just another uiObject. The windows holding
+these are (like uiObjects) uiObjHandle's. The uiMainWin is a subclass, and
+the ubiquitous uiDialog.
+
+*/
 
 
 #include "uihandle.h"
