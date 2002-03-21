e50c57eb328ac7ca80fbfca261cdafd1d3bd3dbc
Nanne Hemstra <nanne.hemstra@dgbes.com>
2002-03-21 16:06:28 +0000

    use of groups and changed icons
    git-svn-id: https://svn.opendtect.org/od/trunk@1125 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiIo/uiconvpos.cc
+++ src/uiIo/uiconvpos.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        Nanne Hemstra
  Date:          June 2001
- RCS:           $Id: uiconvpos.cc,v 1.7 2002-01-14 07:06:53 nanne Exp $
+ RCS:           $Id: uiconvpos.cc,v 1.8 2002-03-21 16:06:28 nanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -22,26 +22,28 @@ uiConvertPos::uiConvertPos( uiParent* p, SurveyInfo* si )
 	, survinfo(si)
 
 {
+    uiGroup* flds = new uiGroup( this );
     IntInpSpec iis; iis.setHSzP(SzPolicySpec::small);
     DoubleInpSpec dis; dis.setHSzP(SzPolicySpec::small);
-    inlfld = new uiGenInput( this, "In-line", iis );
-    crlfld = new uiGenInput( this, "Cross-line", iis );
-    xfld = new uiGenInput( this, "X-coordinate", dis );
-    yfld = new uiGenInput( this, "Y-coordinate", dis );
-
-    const ioPixmap right( GetDataFileName("rightarrow.png") );
-    const ioPixmap left( GetDataFileName("leftarrow.png") );
-    docoordbut = new uiToolButton( this, "", right );
-    docoordbut->activated.notify( mCB(this,uiConvertPos, getCoord) );
-    dobinidbut = new uiToolButton( this, "", left );
-    dobinidbut->activated.notify( mCB(this,uiConvertPos, getBinID) );
-
+    inlfld = new uiGenInput( flds, "In-line", iis );
+    crlfld = new uiGenInput( flds, "Cross-line", iis );
+    xfld = new uiGenInput( flds, "X-coordinate", dis );
+    yfld = new uiGenInput( flds, "Y-coordinate", dis );
     crlfld->attach( alignedBelow, inlfld );
-    dobinidbut->attach( rightAlignedAbove, inlfld );
-    docoordbut->attach( rightOf, dobinidbut, 0 );
     xfld->attach( rightTo, inlfld );
     yfld->attach( alignedBelow, xfld );
     yfld->attach( rightTo, crlfld );
+
+    uiGroup* buts = new uiGroup( this );
+    const ioPixmap right( GetDataFileName("forward.xpm") );
+    const ioPixmap left( GetDataFileName("back.xpm") );
+    docoordbut = new uiToolButton( buts, "", right );
+    docoordbut->activated.notify( mCB(this,uiConvertPos, getCoord) );
+    dobinidbut = new uiToolButton( buts, "", left );
+    dobinidbut->activated.notify( mCB(this,uiConvertPos, getBinID) );
+    docoordbut->attach( rightOf, dobinidbut, 0 );
+
+    flds->attach( centeredBelow, buts );
 
     setOkText("Quit");
     setCancelText("");
