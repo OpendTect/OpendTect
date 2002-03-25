e7f1b867e277bbf20c34940f4e47f50e45296740
Bert Bril <bert.bril@dgbes.com>
2002-03-25 16:07:55 +0000

    added
    git-svn-id: https://svn.opendtect.org/od/trunk@1136 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/uiVis/uivispartserv.cc
@@ -0,0 +1,23 @@
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
+ Author:        N. Hemstra
+ Date:          Mar 2002
+ RCS:           $Id: uivispartserv.cc,v 1.1 2002-03-25 16:07:55 bert Exp $
+________________________________________________________________________
+
+-*/
+
+#include "uivispartserv.h"
+
+
+uiVisPartServer::uiVisPartServer( uiApplService& a )
+	: uiApplPartServer(a)
+{
+}
+
+
+uiVisPartServer::~uiVisPartServer()
+{
+}
