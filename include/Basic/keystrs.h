27f2e129f1699632f1f3c718feb118f5da94885b
Bert Bril <bert.bril@dgbes.com>
2002-03-27 08:18:09 +0000

    reverse logic ifndef --> ifdef
    git-svn-id: https://svn.opendtect.org/od/trunk@1142 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/keystrs.h
+++ include/Basic/keystrs.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
  Author:	A.H.Bril
  Date:		Mar 2002
- RCS:		$Id: keystrs.h,v 1.1 2002-03-26 17:02:30 bert Exp $
+ RCS:		$Id: keystrs.h,v 1.2 2002-03-27 08:18:09 bert Exp $
 ________________________________________________________________________
 
 -*/
@@ -17,32 +17,40 @@ ________________________________________________________________________
 
 #undef mImpl
 
-#ifndef KEYSTRS_IMPL
+#ifdef KEYSTRS_IMPL
 # define mImpl(s) = s
 #else
 # define mImpl(s) /* empty */
 #endif
 
+/*!\brief is used for defining key strings that are 'global'.
+
+Some standard key strings are shared between otherwise unrelated modules.
+To make sure no artificial dependencies are created, such a key can be added
+to this namespace.
+
+*/
+
 namespace sKey
 {
 
-    const char*		Yes		mImpl("Yes");
-    const char*		No		mImpl("No");
-    const char*		Type		mImpl("Type");
-    const char*		Undef		mImpl("Undefined");
-    const char*		Factor		mImpl("Factor");
-    const char*		Name		mImpl("Name");
-    const char*		Title		mImpl("Title");
-    const char*		Value		mImpl("Value");
-    const char*		FileName	mImpl("File name");
-    const char*		IOSelection	mImpl("I/O Selection");
+    extern const char*	Yes		mImpl("Yes");
+    extern const char*	No		mImpl("No");
+    extern const char*	Type		mImpl("Type");
+    extern const char*	Undef		mImpl("Undefined");
+    extern const char*	Factor		mImpl("Factor");
+    extern const char*	Name		mImpl("Name");
+    extern const char*	Title		mImpl("Title");
+    extern const char*	Value		mImpl("Value");
+    extern const char*	FileName	mImpl("File name");
+    extern const char*	IOSelection	mImpl("I/O Selection");
 
-    const char*		Minimum		mImpl("Minimum");
-    const char*		Maximum		mImpl("Maximum");
-    const char*		Average		mImpl("Average");
-    const char*		Median		mImpl("Median");
-    const char*		Sum		mImpl("Sum");
-    const char*		StdDev		mImpl("StdDev");
+    extern const char*	Minimum		mImpl("Minimum");
+    extern const char*	Maximum		mImpl("Maximum");
+    extern const char*	Average		mImpl("Average");
+    extern const char*	Median		mImpl("Median");
+    extern const char*	Sum		mImpl("Sum");
+    extern const char*	StdDev		mImpl("StdDev");
 
 };
 
