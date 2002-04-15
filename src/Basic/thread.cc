ee11e7416b7b7f46731f1173322d6aebd7089ca3
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2002-04-15 12:08:12 +0000

    New simpler interface and some functionality moved to threadwork.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@1211 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Basic/thread.cc
+++ src/Basic/thread.cc
@@ -4,13 +4,9 @@
  * DATE     : Mar 2000
 -*/
 
-static const char* rcsID = "$Id";
+static const char* rcsID = "$Id: thread.cc,v 1.5 2002-04-15 12:08:12 kristofer Exp $";
 
-#include <thread.h>
-#include <basictask.h>
-
-//
-//! Platform dependent implementations
+#include "thread.h"
 
 Threads::Mutex::Mutex()
 {
@@ -43,7 +39,6 @@ int Threads::Mutex::unlock()
 #endif
     return 0;
 }
-
 
 
 Threads::ConditionVar::ConditionVar()
@@ -82,23 +77,25 @@ int Threads::ConditionVar::signal(bool all)
 }
 
 
-
-int Threads::Thread::start()
+Threads::Thread::Thread(void (func)(void*), void* data )
 {
-   is_running = true;
 #ifdef __pthread__
-   return pthread_create( &id, 0, (void* (*)(void*)) func, this );
+    pthread_create( &id, 0, (void* (*)(void*)) func, data );
 #endif
-    return 0;
 }
 
 
-int Threads::Thread::join()
+void Threads::Thread::destroy(bool wait, void** ret_val)
 {
 #ifdef __pthread__
-    return pthread_join( id, &ret_val );
+    if ( wait )
+	pthread_join( id, ret_val );
+    else
+    {
+	pthread_detach( id );
+    }
+    delete this;
 #endif
-    return 0;
 }
 
 
@@ -110,154 +107,3 @@ void Threads::Thread::threadExit( void* rv )
 }
 
 
-//
-//! Platform independent implementations
-
-Threads::Mutex::Locker::Locker( Threads::Mutex& mutex_ )
-    : mutex( mutex_ )
-{ mutex.lock(); }
-
-
-Threads::Mutex::Locker::~Locker()
-{ mutex.unlock(); }
-
-
-int Threads::ConditionVar::unlock( bool all )
-{
-    unlock();
-    return signal( all );
-}
-
-
-int Threads::ConditionVar::unlock()
-{ return Mutex::unlock(); }
-
-
-Threads::Thread::Thread()
-    : func( 0 )
-    , exitflag( false )
-    , ret_val( 0 )
-    , is_running( false )
-{}
-
-
-Threads::Thread::~Thread()
-{ if ( is_running ) stop(); }
-
-
-bool Threads::Thread::setFunction( void (nf)(void*)  )
-{
-    if ( func ) return false;
-
-#ifdef __msvc__
-    func = nf;
-#else
-    func = (void*) nf;
-#endif
-
-    return true;
-}
-
-
-void Threads::Thread::stop()
-{
-    exitcond.lock();
-    exitflag=true;
-    exitcond.unlock(false);
-
-    join();
-
-    is_running = false;
-}
-
-
-
-Threads::TaskRunner::TaskRunner()
-{
-    data.exitcond.lock();
-    data.stat = Idle;
-    data.stopflag = true;
-    data.exitcond.unlock();
-    data.task=0;
-
-    data.setFunction( Threads::TaskRunner::threadFunc );
-    data.start();
-}
-
-
-Threads::TaskRunner::~TaskRunner()
-{ }
-
-
-Threads::TaskRunner::Status Threads::TaskRunner::status() const
-{ return data.stat; }
-
-
-bool Threads::TaskRunner::setTask( BasicTask* nt )
-{
-    if ( data.stat!=Idle ) return false;
-
-    Threads::Mutex::Locker locker( data.exitcond );
-    data.task = nt;
-    data.lastval = 0;
-    data.stopflag = true;
-
-    return true;
-}
-
-
-bool Threads::TaskRunner::start()
-{
-    if ( data.stat==Running ) return false;
-
-    Threads::Mutex::Locker locker( data.exitcond );
-    data.stat=Running;
-    data.stopflag = false;
-
-    return true;
-}
-
-
-bool Threads::TaskRunner::stop()
-{
-    if ( data.stat!=Running ) return false;
-
-    Threads::Mutex::Locker locker( data.exitcond );
-    data.stopflag = true;
-    data.stat=Stopped;
-
-    return true;
-}
-
-
-void Threads::TaskRunner::threadFunc(void *ptr)
-{
-    Threads::TaskRunner::Data* data = (Threads::TaskRunner::Data*) ptr;
-
-    while ( true )
-    {
-	data->exitcond.lock();
-
-	while ( !data->exitflag && data->stopflag ) data->exitcond.wait();
-
-	if ( data->exitflag )
-	{
-	    data->exitcond.unlock();
-	    data->threadExit();
-	}
-	else if ( !data->stopflag )
-	{
-	    data->exitcond.unlock();
-
-	    data->lastval = data->task->nextStep();
-
-	    if ( data->lastval>0 )
-		continue;
-
-	    Threads::Mutex::Locker locker( data->exitcond );
-	    data->stopflag = true;
-	    data->stat = Finished;
-	}
-
-    }
-};
