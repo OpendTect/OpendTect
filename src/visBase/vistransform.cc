c70080fae384e1475aec9219d612e2cf8d2f1c44
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2002-04-22 09:08:01 +0000

    Changed the rotation of the matrix
    git-svn-id: https://svn.opendtect.org/od/trunk@1263 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visBase/vistransform.cc
+++ src/visBase/vistransform.cc
@@ -4,7 +4,7 @@
  * DATE     : Oct 1999
 -*/
 
-static const char* rcsID = "$Id: vistransform.cc,v 1.2 2002-02-26 19:58:19 kristofer Exp $";
+static const char* rcsID = "$Id: vistransform.cc,v 1.3 2002-04-22 09:08:01 kristofer Exp $";
 
 #include "vistransform.h"
 #include "Inventor/nodes/SoMatrixTransform.h"
@@ -28,10 +28,10 @@ void visBase::Transformation::setA( float a11, float a12, float a13, float a14,
 				   float a31, float a32, float a33, float a34,
 				   float a41, float a42, float a43, float a44 )
 {
-    transform->matrix.setValue( a11, a12, a13, a14,
-	    			a21, a22, a23, a24,
-				a31, a32, a33, a34,
-				a41, a42, a43, a44 );
+    transform->matrix.setValue( a11, a21, a31, a41,
+	    			a12, a22, a32, a42,
+				a13, a23, a33, a43,
+				a14, a24, a34, a44 );
 }
 
 
