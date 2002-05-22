bec4699ef58d6e4c54a1a9b0efc63a6c074ed833
Nanne Hemstra <nanne.hemstra@dgbes.com>
2002-05-22 11:03:59 +0000

    added boolean to constructor for color selection
    git-svn-id: https://svn.opendtect.org/od/trunk@1487 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiTools/uisellinest.h
+++ include/uiTools/uisellinest.h
@@ -7,13 +7,12 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Lammertink
  Date:          08/08/2000
- RCS:           $Id: uisellinest.h,v 1.2 2001-08-23 14:59:17 windev Exp $
+ RCS:           $Id: uisellinest.h,v 1.3 2002-05-22 11:03:59 nanne Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uigroup.h"
-#include "draw.h"
 #include "uidobjset.h"
 
 class uiComboBox;
@@ -25,7 +24,7 @@ class uiSelLineStyle : public uiGroup
 { 	
 public:
 			uiSelLineStyle(uiParent*,const LineStyle&,
-				       const char* txt=0);
+				       const char* txt=0,bool withcolor=true);
 
     LineStyle		getStyle() const;
 
--- src/uiTools/uisellinest.cc
+++ src/uiTools/uisellinest.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Lammertink
  Date:          08/08/2000
- RCS:           $Id: uisellinest.cc,v 1.5 2001-08-23 14:59:17 windev Exp $
+ RCS:           $Id: uisellinest.cc,v 1.6 2002-05-22 11:03:52 nanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -14,20 +14,25 @@ ________________________________________________________________________
 #include "uicombobox.h"
 #include "uicolor.h"
 #include "uidset.h"
+#include "draw.h"
 
 
 uiSelLineStyle::uiSelLineStyle( uiParent* p, const LineStyle& l,
-				const char* txt )
-	: uiGroup( p, "Line style selector" )
-	, ls( l )
+				const char* txt, bool wcol )
+	: uiGroup(p,"Line style selector")
+	, ls(l)
+	, colinp(0)
 {
     UserIDSet itms( LineStyle::TypeNames );
     itms.setName( "Line Style" );
     stylesel = new uiComboBox( this, itms );
     stylesel->setCurrentItem( (int)ls.type );
     new uiLabel( this, txt, stylesel );
-    colinp = new uiColorInput( this, ls.color );
-    colinp->attach( rightOf, stylesel );
+    if ( wcol )
+    {
+	colinp = new uiColorInput( this, ls.color );
+	colinp->attach( rightOf, stylesel );
+    }
 
     setHAlignObj( stylesel );
     setHCentreObj( stylesel );
@@ -38,6 +43,6 @@ LineStyle uiSelLineStyle::getStyle() const
 {
     LineStyle ret = ls;
     ret.type = (LineStyle::Type)stylesel->currentItem();
-    ret.color = colinp->color();
+    if ( colinp ) ret.color = colinp->color();
     return ret;
 }
