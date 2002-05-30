7cc8637caf7057aeb51baadb438e5bb201c6be9a
Bert Bril <bert.bril@dgbes.com>
2002-05-30 22:09:59 +0000

    small intf chgs
    git-svn-id: https://svn.opendtect.org/od/trunk@1543 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiSeis/uiseisfmtscale.h
+++ include/uiSeis/uiseisfmtscale.h
@@ -6,13 +6,14 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Bril
  Date:          May 2002
- RCS:           $Id: uiseisfmtscale.h,v 1.1 2002-05-24 14:38:21 bert Exp $
+ RCS:           $Id: uiseisfmtscale.h,v 1.2 2002-05-30 22:09:59 bert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uigroup.h"
 class uiGenInput;
+class uiScaler;
 class Scaler;
 class IOObj;
 
@@ -31,7 +32,7 @@ public:
 protected:
 
     uiGenInput*		imptypefld;
-    uiGenInput*		scalefld;
+    uiScaler*		scalefld;
 
 };
 
--- include/uiTools/uiscaler.h
+++ include/uiTools/uiscaler.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        Nanne Hemstra
  Date:          June 2002
- RCS:           $Id: uiscaler.h,v 1.1 2002-05-30 15:03:19 nanne Exp $
+ RCS:           $Id: uiscaler.h,v 1.2 2002-05-30 22:09:59 bert Exp $
 ________________________________________________________________________
 
 -*/
@@ -21,7 +21,7 @@ class uiScaler : public uiGroup
 {
 public:
 
-			uiScaler(uiParent*,const char* txt="Value scaling",
+			uiScaler(uiParent*,const char* txt=0, //"Value scaling"
 				 bool linear_only=false);
 			~uiScaler();
 
