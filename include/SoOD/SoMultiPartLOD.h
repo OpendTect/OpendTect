775a1ffe8b59cd2091746bd5e8c970c5fde2b998
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2002-07-08 07:55:52 +0000

    *** empty log message ***
    git-svn-id: https://svn.opendtect.org/od/trunk@1609 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/SoOD/SoMultiPartLOD.h
@@ -0,0 +1,81 @@
+#ifndef vissomultirespart_h
+#define vissomultirespart_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
+ Author:	Kristofer Tingdahl
+ Date:		4-11-2002
+ RCS:		$Id: SoMultiPartLOD.h,v 1.1 2002-07-08 07:55:52 kristofer Exp $
+________________________________________________________________________
+
+
+-*/
+
+#include <Inventor/nodes/SoGroup.h>
+#include <Inventor/fields/SoMFVec3f.h>
+#include <Inventor/fields/SoSFInt32.h>
+#include <Inventor/fields/SoMFFloat.h>
+
+/*! \brief 
+is a SoLOD that is used for shapes that has more than one part. The 'normal'
+SoLOD does only have one center and the distance to the camera is calculated
+from there. On a multi-part horizon this in not good enogh, since we might view
+a detail on one of the parts, while the distance to the center still is large.
+This is solved my allowing multiple centers. Each part adds its own center
+to the centers field.
+
+At render the distance between all centers and the camera is calculated, and the
+shortest distance determines which child should be rendered in the same manner
+as the normal SoLOD.
+
+The user can set the whichChild field to disable the 'automatic' resolution selection.
+
+*/
+
+
+class SoMultiPartLOD : public SoGroup
+{
+    typedef SoGroup	inherited;
+public:
+			SoMultiPartLOD(void);
+
+    SoMFVec3f		centers;
+    SoMFFloat 		range;
+    SoSFInt32		whichChild;
+
+    virtual void	doAction(SoAction * action);
+    virtual void	callback(SoCallbackAction * action);
+    virtual void	GLRender(SoGLRenderAction * action);
+    virtual void	GLRenderBelowPath(SoGLRenderAction * action);
+    virtual void	GLRenderInPath(SoGLRenderAction * action);
+    virtual void	GLRenderOffPath(SoGLRenderAction * action);
+    virtual void	rayPick(SoRayPickAction * action);
+    virtual void	getBoundingBox(SoGetBoundingBoxAction * action);
+    virtual void	getPrimitiveCount(SoGetPrimitiveCountAction * action);
+
+    static SoType	getClassTypeId(void);
+    virtual SoType	getTypeId(void) const;
+    static void		initClass(void);
+
+protected:
+				~SoMultiPartLOD();
+    virtual int			whichToTraverse(SoAction *);
+
+    static const SoFieldData**	getFieldDataPtr(void);
+    virtual const SoFieldData*	getFieldData(void) const;
+
+private:
+    void			commonConstructor(void);
+    static SoType		classTypeId;
+    static const SoFieldData**	parentFieldData;
+    static SoFieldData*		fieldData;
+
+    static unsigned int		classinstances;
+
+    static void*		createInstance(void);
+};
+
+
+#endif
