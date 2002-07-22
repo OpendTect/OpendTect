b780a2b34bce61bc9870fd578df3df61eec0a3ca
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2002-07-22 07:23:08 +0000

    *** empty log message ***
    git-svn-id: https://svn.opendtect.org/od/trunk@1628 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiCoin/uicmain.cc
+++ src/uiCoin/uicmain.cc
@@ -4,12 +4,13 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Lammertink
  Date:          06/02/2002
- RCS:           $Id: uicmain.cc,v 1.1 2002-02-07 14:58:34 arend Exp $
+ RCS:           $Id: uicmain.cc,v 1.2 2002-07-22 07:23:08 kristofer Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uicmain.h"
+#include "visinventorinit.h"
 #include <Inventor/Qt/SoQt.h>
 
 uicMain::uicMain(int argc,char** argv)
@@ -19,6 +20,7 @@ uicMain::uicMain(int argc,char** argv)
 void uicMain::init(QWidget* mw)
 {
     SoQt::init(mw);
+    visBase::initdGBInventorClasses();
 }
 
 int uicMain::exec()
