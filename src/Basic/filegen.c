e8862689bae6dc73a089a848360dbd80ba952fa1
Bert Bril <bert.bril@dgbes.com>
2002-09-06 15:17:06 +0000

    added File_isRemote
    git-svn-id: https://svn.opendtect.org/od/trunk@1732 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Basic/filegen.c
+++ src/Basic/filegen.c
@@ -4,7 +4,7 @@
  * FUNCTION : file utilities
 -*/
 
-static const char* rcsID = "$Id: filegen.c,v 1.25 2002-08-05 11:11:10 bert Exp $";
+static const char* rcsID = "$Id: filegen.c,v 1.26 2002-09-06 15:17:06 bert Exp $";
 
 #include "filegen.h"
 #include "genc.h"
@@ -33,7 +33,7 @@ int File_exists( const char* fname )
 #ifdef __win__
     return fname && *fname && PathFileExists( fname );
 #else
-    return !fname || stat(fname,&statbuf) < 0 ? NO : YES;
+    return fname && *fname && stat(fname,&statbuf) > 0 ? YES : NO;
 #endif
 }
 
@@ -536,4 +536,38 @@ const char* File_getCurrentDir()
     static FileNameString pathbuf;
     getcwd( pathbuf, PATH_LENGTH );
     return pathbuf;
+}
+
+
+#ifdef lux
+#include <sys/statfs.h>
+/* These header files don't compile!
+#include <linux/nfs_fs.h>
+#include <linux/smb.h>
+*/
+#else
+#include <sys/statvfs.h>
+#endif
+
+int File_isRemote( const char* fname )
+{
+#ifdef lux
+    static struct statfs fsstatbuf;
+    if ( !File_exists(fname)
+      || statfs(fname,&fsstatbuf) )
+#else
+    static struct statvfs fsstatbuf;
+    if ( !File_exists(fname)
+      || statvfs(fname,&fsstatbuf) )
+#endif
+	return NO;
+
+
+#ifdef lux
+    /* return fsstatbuf.f_type == NFS_SUPER_MAGIC
+	|| fsstatbuf.f_type == SMB_SUPER_MAGIC; */
+    return fsstatbuf.f_type == 0x6969 || fsstatbuf.f_type == 0x517B;
+#else
+    return fsstatbuf.f_basetype[0] == 'n' && fsstatbuf.f_basetype[1] == 'f';
+#endif
 }
