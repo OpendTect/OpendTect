7e899f6a310dd4466a72908900a84316b303de99
Bert Bril <bert.bril@dgbes.com>
2002-10-07 14:57:30 +0000

    added X11 shut up env variable
    git-svn-id: https://svn.opendtect.org/od/trunk@1793 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiCoin/uicmain.cc
+++ src/uiCoin/uicmain.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Lammertink
  Date:          06/02/2002
- RCS:           $Id: uicmain.cc,v 1.3 2002-10-03 08:09:40 bert Exp $
+ RCS:           $Id: uicmain.cc,v 1.4 2002-10-07 14:57:30 bert Exp $
 ________________________________________________________________________
 
 -*/
@@ -18,9 +18,11 @@ uicMain::uicMain(int argc,char** argv)
 : uiMain( argc, argv )
 {}
 
-void uicMain::init(QWidget* mw)
+void uicMain::init( QWidget* mw )
 {
-    putenv( "SOQT_KILL_X11ERROR_WARNINGS=1" );
+#ifndef __debug__
+    putenv( "SOQT_BRIL_X11_SILENCER_HACK=1" );
+#endif
     SoQt::init(mw);
     visBase::initdGBInventorClasses();
 }
