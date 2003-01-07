094b24c4248727c27ce244b15253ea1fe305cf02
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2003-01-07 10:27:35 +0000

    Simplified a lot and made use of new classes in IndexedShape
    git-svn-id: https://svn.opendtect.org/od/trunk@2001 a985855d-d384-4b45-ae33-31a410998c3c

--- include/visBase/vistristripset.h
+++ include/visBase/vistristripset.h
@@ -7,91 +7,22 @@ ________________________________________________________________________
  CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
  Author:	Kristofer Tingdahl
  Date:		4-11-2002
- RCS:		$Id: vistristripset.h,v 1.8 2002-11-15 08:14:32 kristofer Exp $
+ RCS:		$Id: vistristripset.h,v 1.9 2003-01-07 10:27:31 kristofer Exp $
 ________________________________________________________________________
 
 
 -*/
 
-#include "visobject.h"
-
-class Coord3;
-class SoCoordinate3;
-class SoSwitch;
-class SoMaterial;
-class SoIndexedTriangleStripSet;
-class CallBacker;
-
-
-namespace Geometry
-{ class TriangleStripSet; };
+#include "visshape.h"
 
 namespace visBase
 {
-class VisColorTab;
 
-
-/*!\brief
-
-
-*/
-
-class TriangleStripSet : public VisualObjectImpl
+class TriangleStripSet : public IndexedShape
 {
 public:
     static TriangleStripSet*	create()
 				mCreateDataObj( TriangleStripSet );
-
-    void		setStrips( Geometry::TriangleStripSet*, bool connect );
-    			/*!< If connect is true, the object will use the
-			     stripsets callbacks to keep itself updated.
-			     stripset must then stay alive.
-			*/
-
-    void		setValues( const float* data );
-    			/*!< The data must be arranged in the same order as
-			     the coords, i.e. data[n] should correspond to
-			     coord[n]
-			*/
-
-    void		getPositions( TypeSet<Coord3>& ) const;
-
-    void		setColorTab( VisColorTab* );
-    const VisColorTab*	getColorTab() const { return colortable; }
-    VisColorTab*	getColorTab() { return colortable; }
-
-    void		setAutoscale(bool yn) { autoscale = yn; }
-    bool		autoScale() const { return autoscale; }
-
-    void		setClipRate( float n );
-    			/*!< Should be between 0 and 0.5 */
-    float		clipRate() const { return cliprate; }
-
-    void		useTexture(bool);
-    bool		usesTexture() const;
-			     
-protected:
-			~TriangleStripSet();
-    void		clipData();
-
-    void		updateCoords(CallBacker* =0);
-    void		updateIndexes(CallBacker* =0);
-    void		updateTexture();
-
-    bool 		autoscale;
-    float		cliprate;
-
-    VisColorTab*	colortable;
-
-    float*		data;
-
-    SoCoordinate3*		coords;
-    SoMaterial*			ctmaterial;
-    SoSwitch*			textureswitch;
-    SoIndexedTriangleStripSet*	strips;
-
-    Geometry::TriangleStripSet*	stripset;
-
 };
 };
 
