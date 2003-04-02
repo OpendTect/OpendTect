63fcc775620ca10399e516ca0ceb4bb4427d9b62
Bert Bril <bert.bril@dgbes.com>
2003-04-02 15:07:05 +0000

    addded
    git-svn-id: https://svn.opendtect.org/od/trunk@2324 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ spec/ODSeis/od_cbvs_swap_inlcrl.cc
@@ -0,0 +1,85 @@
+#include "seistrc.h"
+#include "seiscbvs.h"
+#include "cbvsreadmgr.h"
+#include "conn.h"
+#include "iostrm.h"
+#include "strmprov.h"
+#include "progressmeter.h"
+#include "filegen.h"
+#include <iostream>
+#include <math.h>
+
+#include "prog.h"
+defineTranslatorGroup(SeisTrc,"Seismic Data");
+defineTranslator(CBVS,SeisTrc,"CBVS");
+
+
+int main( int argc, char** argv )
+{
+    if ( argc < 3 )
+    {
+	cerr << "Usage: " << argv[0] << " inpfile outpfile"<< endl
+	     << "Format: CBVS." << endl << endl;
+	return 1;
+    }
+    else if ( !File_exists(argv[1]) )
+    {
+        cerr << argv[1] << " does not exist" << endl;
+        return 1;
+    }
+
+    FileNameString fname( argv[1] );
+    if ( !File_isAbsPath(argv[1]) )
+    {
+	fname = File_getCurrentDir();
+	fname = File_getFullPath( fname, argv[1] );
+    }
+    CBVSSeisTrcTranslator tri;
+    if ( !tri.initRead(new StreamConn(fname,Conn::Read)) )
+        { cerr << tri.errMsg() << endl;  return 1; }
+
+    const CBVSReadMgr& rdmgr = *tri.readMgr();
+    const CBVSInfo::SurvGeom& geom = rdmgr.info().geom;
+
+    fname = argv[2];
+    if ( !File_isAbsPath(argv[2]) )
+    {
+	fname = File_getCurrentDir();
+	fname = File_getFullPath( fname, argv[2] );
+    }
+
+    SeisTrc trc;
+    CBVSSeisTrcTranslator tro;
+    int nrwr = 0;
+    ProgressMeter pm( cerr );
+
+    for ( int linenr = geom.start.crl; linenr <= geom.stop.crl;
+	    linenr += geom.step.crl ) 
+    {
+	for ( int trcnr = geom.start.inl; trcnr <= geom.stop.inl;
+		trcnr += geom.step.inl ) 
+	{
+	    pm.update( nrwr );
+	    if ( !tri.goTo(BinID(linenr,trcnr)) )
+		continue;
+
+	    else if ( !tri.read(trc) )
+		{ cerr << "Cannot read " << linenr << '/' << trcnr
+		       << endl; return 1; }
+
+	    Swap( trc.info().binid.inl, trc.info().binid.crl );
+	    trc.info().coord = SI().transform( trc.info().binid );
+
+	    if ( !nrwr
+		    && !tro.initWrite(new StreamConn(fname,Conn::Write),trc) )
+		{ cerr << "Cannot start write!" << endl;  return 1; }
+
+	    if ( !tro.write(trc) )
+		{ cerr << "Cannot write!" << endl;  return 1; }
+
+	    nrwr++;
+	}
+    }
+
+    return nrwr ? 0 : 1;
+}
