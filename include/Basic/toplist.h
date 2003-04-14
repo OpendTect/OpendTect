725bff015f0682d328b34a9edc097cbc1d0bd7b9
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2003-04-14 14:45:30 +0000

    *** empty log message ***
    git-svn-id: https://svn.opendtect.org/od/trunk@2338 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Basic/toplist.h
@@ -0,0 +1,85 @@
+#ifndef toplist_h
+#define toplist_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
+ Author:	K Tingdahl
+ Date:		April 2003
+ RCS:		$Id: toplist.h,v 1.1 2003-04-14 14:45:30 kristofer Exp $
+________________________________________________________________________
+
+-*/
+
+#include <sets.h>
+
+/*!\brief
+is a class that holds a "top N" list with the N highest (or lowest) values
+that is added. Each value has an associated value that can be used as an
+identifier of where the value come from or something like that.
+*/
+
+template <class VT, class AVT>
+class TopList
+{
+public:
+			TopList( int maxsize_, VT undefval_, bool istop_)
+			    : istop( istop_ )
+			    , maxsize( maxsize_ )
+			    , undefval( undefval_ )
+			{ }
+    virtual		~TopList() {}
+
+    VT			getValue(int pos) const { return values[pos]; }
+    AVT			getAssociatedValue(int pos) const { return avals[pos]; }
+
+    virtual int		size() const { return values.size(); }
+    VT			getBottomValue() const
+			{
+			    if ( size() )
+				return values[size()-1];
+			    return undefval;
+			}
+
+    void		addValue( VT val, AVT aval )
+			{
+			    int pos = 0;
+			    const int mysize = size();
+			    if ( istop )
+				while ( pos<mysize && values[pos]>val ) pos++;
+			    else
+				while ( pos<mysize && values[pos]<val ) pos++;
+
+			    if ( pos==mysize )
+			    {
+				if ( mysize>=maxsize )
+				    return;
+
+				values += val;
+				avals += aval;
+			    }
+			    else
+			    {
+				values.insert( pos, val );
+				avals.insert( pos, aval );
+
+				if ( mysize==maxsize )
+				{
+				    values.remove(mysize);
+				    avals.remove(mysize);
+				}
+			    }
+			}
+private:
+
+    TypeSet<VT>		values;
+    TypeSet<AVT>	avals;
+    const bool		istop;
+    const int		maxsize;
+    VT			undefval;
+
+};
+
+
+#endif
