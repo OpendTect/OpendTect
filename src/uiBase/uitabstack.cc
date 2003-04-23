02e42549820df53d9a2f960ea1ebdc7dce58156b
arend <arend@a985855d-d384-4b45-ae33-31a410998c3c>
2003-04-23 12:14:43 +0000

    oops
    git-svn-id: https://svn.opendtect.org/od/trunk@2364 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiBase/uitabstack.cc
+++ src/uiBase/uitabstack.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Lammertink
  Date:          17/01/2002
- RCS:           $Id: uitabstack.cc,v 1.5 2003-04-23 12:11:21 arend Exp $
+ RCS:           $Id: uitabstack.cc,v 1.6 2003-04-23 12:14:43 arend Exp $
 ________________________________________________________________________
 
 -*/
@@ -39,7 +39,6 @@ uiTabStack::uiTabStack( uiParent* parnt, const char* nm, bool mnge )
 
 void uiTabStack::tabSel( CallBacker* cb )
 {
-cout << "o" << endl;
     int id = tabbar_->currentTabId();
     uiGroup* selgrp = page( id );
     ObjectSet<uiTab>& tabs = tabbar_->tabs_;
