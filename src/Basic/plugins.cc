73b7c55e76428e907d1f5bab2bde16aad8c7ef65
Bert Bril <bert.bril@dgbes.com>
2003-08-20 10:10:41 +0000

    added
    git-svn-id: https://svn.opendtect.org/od/trunk@2653 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/Basic/plugins.cc
@@ -0,0 +1,131 @@
+/*+
+ * AUTHOR   : A.H. Bril
+ * DATE     : Aug 2003
+-*/
+
+static const char* rcsID = "$Id: plugins.cc,v 1.1 2003-08-20 10:10:41 bert Exp $";
+
+#include "plugins.h"
+#include "filegen.h"
+#include "dirlist.h"
+
+#include <dlfcn.h>
+#include <iostream>
+
+static ObjectSet<BufferString> loaded;
+
+extern "C" {
+
+    typedef int (*VoidIntRetFn)(void);
+    typedef const char* (*ArgcArgvCCRetFn)(int*,char**);
+
+};
+
+static const char* getHDir()
+{
+    const char* ret = getenv( "binsubdir" );
+    return ret ? ret : getenv( "HDIR" );
+}
+
+
+static const char* getFnName( const char* libnm, const char* fnend )
+{
+    static BufferString ret;
+
+    if ( *libnm == 'l' && *(libnm+1) == 'i' && *(libnm+2) == 'b' )
+	libnm += 3;
+    ret = libnm;
+    char* ptr = strchr( ret.buf(), '.' );
+    if ( ptr ) *ptr = '\0';
+    ret += fnend;
+    return ret.buf();
+}
+
+
+static bool loadPlugin( const char* libnm, int* pargc, char** argv,
+			int inittype )
+{
+    if ( inittype == PI_AUTO_INIT_NONE ) return false;
+
+    void* handle = dlopen( libnm, RTLD_LAZY );
+    if ( !handle ) return false;
+
+    const BufferString libnmonly = File_getFileName(libnm);
+    if ( inittype > 0 )
+    {
+	VoidIntRetFn fn = (VoidIntRetFn)dlsym( handle,
+				getFnName(libnmonly,"GetPluginType") );
+	if ( !fn || inittype != (*fn)() )
+	    { dlclose(handle); return false; }
+    }
+
+    ArgcArgvCCRetFn fn2 = (ArgcArgvCCRetFn)dlsym( handle,
+				getFnName(libnmonly,"InitPlugin") );
+    bool rv = false;
+    if ( !fn2 )
+	cerr << "Cannot find InitPlugin() function in "
+	     << libnm << endl;
+    else
+    {
+	const char* ret = (*fn2)(pargc,argv);
+	if ( ret )
+	    cerr << libnm << ": " << ret << endl;
+	else
+	{
+	    cerr << "Successfully loaded plugin "
+		 << libnmonly << endl;
+	    loaded += new BufferString( libnmonly );
+	    rv = true;
+	}
+    }
+
+    dlclose(handle);
+    return rv;
+}
+
+
+bool LoadPlugin( const char* libnm, int* pargc, char** argv )
+{
+    return loadPlugin(libnm,pargc,argv,-1);
+}
+
+
+static void loadPlugins( const char* dirnm, int* pargc, char** argv,
+			 int inittype )
+{
+    DirList dl( dirnm, -1 );
+    dl.sort();
+    for ( int idx=0; idx<dl.size(); idx++ )
+    {
+	BufferString libnm = dl[idx]->name();
+	if ( indexOf( loaded, libnm ) >= 0 )
+	    continue;
+
+	BufferString fulllibnm = File_getFullPath( dirnm, libnm );
+	loadPlugin(fulllibnm,pargc,argv,inittype);
+    }
+}
+
+
+static void loadPIs( const char* dnm, int* pargc, char** argv, int inittype )
+{
+    BufferString dirnm = File_getFullPath( dnm, getHDir() );
+    if ( !File_exists(dirnm) )
+	return;
+
+    const BufferString prognm( File_getFileName(argv[0]) );
+    BufferString specdirnm = File_getFullPath( dirnm, prognm );
+    loadPlugins( specdirnm, pargc, argv, inittype );
+    loadPlugins( dirnm, pargc, argv, inittype );
+}
+
+
+extern "C" void LoadAutoPlugins( int* pargc, char** argv, int inittype )
+{
+    const BufferString homedir( getenv("HOME") );
+    BufferString dirnm = File_getFullPath( homedir, ".dgbPlugins" );
+    loadPIs( dirnm, pargc, argv, inittype );
+    const BufferString swdir( GetSoftwareDir() );
+    dirnm = File_getFullPath( swdir, "plugins" );
+    loadPIs( dirnm, pargc, argv, inittype );
+}
