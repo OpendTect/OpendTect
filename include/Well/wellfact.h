0dbe9310c50bb04cfca6e9a87c60d7cd3debc705
Nanne Hemstra <nanne.hemstra@dgbes.com>
2003-08-22 11:22:40 +0000

    added wellfact / removed emwelltransl from emfact
    git-svn-id: https://svn.opendtect.org/od/trunk@2658 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Well/wellfact.h
@@ -0,0 +1,21 @@
+#ifndef wellfact_h
+#define wellfact_h
+
+/*@+
+________________________________________________________________________
+
+ CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
+ Author:	A.H. Bril
+ Date:		24-11-1995
+ RCS:		$Id: wellfact.h,v 1.1 2003-08-22 11:22:40 nanne Exp $
+________________________________________________________________________
+
+@$*/
+
+#include "welltransl.h"
+
+defineTranslatorGroup(Well,"Well");
+defineTranslator(dgb,Well,mDGBKey);
+
+
+#endif
