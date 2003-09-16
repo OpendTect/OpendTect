ea5c38064c30bfc28d8aee75a6ab3eb82ad537ac
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2003-09-16 09:22:39 +0000

    Added SoForegroundTranslation
    git-svn-id: https://svn.opendtect.org/od/trunk@2725 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/SoOD/SoForegroundTranslation.cc
@@ -0,0 +1,87 @@
+/*
+___________________________________________________________________
+
+ * COPYRIGHT: (C) de Groot-Bril Earth Sciences B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Jul 2003
+___________________________________________________________________
+
+-*/
+
+static const char* rcsID = "$Id: SoForegroundTranslation.cc,v 1.1 2003-09-16 09:22:39 kristofer Exp $";
+
+
+#include "SoForegroundTranslation.h"
+
+#include "Inventor/actions/SoGLRenderAction.h"
+#include "Inventor/actions/SoGetBoundingBoxAction.h"
+#include "Inventor/actions/SoCallbackAction.h"
+#include "Inventor/actions/SoGetMatrixAction.h"
+#include "Inventor/actions/SoPickAction.h"
+#include "Inventor/actions/SoGetPrimitiveCountAction.h"
+#include "Inventor/elements/SoModelMatrixElement.h"
+#include "Inventor/elements/SoViewVolumeElement.h"
+
+
+SO_NODE_SOURCE(SoForegroundTranslation);
+
+void SoForegroundTranslation::initClass()
+{
+    SO_NODE_INIT_CLASS(SoForegroundTranslation, SoNode, "Node" );
+
+    SO_ENABLE(SoGLRenderAction, SoModelMatrixElement );
+    SO_ENABLE(SoGLRenderAction, SoViewVolumeElement );
+
+    SO_ENABLE(SoGetBoundingBoxAction, SoModelMatrixElement );
+    SO_ENABLE(SoGetBoundingBoxAction, SoViewVolumeElement );
+
+    SO_ENABLE(SoCallbackAction, SoModelMatrixElement );
+    SO_ENABLE(SoCallbackAction, SoViewVolumeElement );
+
+    SO_ENABLE(SoGetMatrixAction, SoModelMatrixElement );
+    SO_ENABLE(SoGetMatrixAction, SoViewVolumeElement );
+
+    SO_ENABLE(SoPickAction, SoModelMatrixElement );
+    SO_ENABLE(SoPickAction, SoViewVolumeElement );
+
+    SO_ENABLE(SoGetPrimitiveCountAction, SoModelMatrixElement );
+    SO_ENABLE(SoGetPrimitiveCountAction, SoViewVolumeElement );
+}
+
+
+SoForegroundTranslation::SoForegroundTranslation()
+{
+    SO_NODE_CONSTRUCTOR(SoForegroundTranslation);
+    SO_NODE_ADD_FIELD( lift, ( 1 ) );
+}
+
+void SoForegroundTranslation::doAction( SoAction* action )
+{
+    const float liftvalue = lift.getValue();
+    if ( !liftvalue ) return;
+
+    SoState* state = action->getState();
+    const SbViewVolume& vv = SoViewVolumeElement::get(state);
+
+    SbVec3f projectiondir = vv.getProjectionDirection();
+    projectiondir.normalize();
+    SoModelMatrixElement::translateBy(state, this,projectiondir*(-liftvalue));
+}
+
+    
+#define mImplAction( func, actiontype ) \
+void SoForegroundTranslation::func( actiontype* action ) \
+{ \
+    SoForegroundTranslation::doAction( action ); \
+}
+
+
+mImplAction( GLRender, SoGLRenderAction );
+mImplAction( getBoundingBox, SoGetBoundingBoxAction );
+mImplAction( callback, SoCallbackAction );
+mImplAction( getMatrix, SoGetMatrixAction );
+mImplAction( pick, SoPickAction );
+mImplAction( getPrimitiveCount, SoGetPrimitiveCountAction );
+
+
+
