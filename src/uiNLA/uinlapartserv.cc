a4b39e2f79162ed39327e3d2747699b0d28b77c6
Bert Bril <bert.bril@dgbes.com>
2003-10-03 14:35:37 +0000

    NLA and uiNLA defined; no NN and license dep anymore
    git-svn-id: https://svn.opendtect.org/od/trunk@2801 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiNLA/uinlapartserv.cc
+++ src/uiNLA/uinlapartserv.cc
@@ -4,14 +4,13 @@ ________________________________________________________________________
  CopyRight:     (C) de Groot-Bril Earth Sciences B.V.
  Author:        A.H. Bril
  Date:          May 2001
- RCS:           $Id: uinlapartserv.cc,v 1.1 2003-10-01 12:51:43 bert Exp $
+ RCS:           $Id: uinlapartserv.cc,v 1.2 2003-10-03 14:35:37 bert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uinlapartserv.h"
 #include "featset.h"
-#include "uidset.h"
 
 const int uiNLAPartServer::evPrepareWrite	= 0;
 const int uiNLAPartServer::evPrepareRead	= 1;
