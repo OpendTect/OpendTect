85ca61dfae1a47cc4f61425a635c1f9306cb95be
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2003-10-21 08:27:33 +0000

    Added uitreeitemmanager.h/.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@2876 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiTools/uitreeitemmanager.h
@@ -0,0 +1,203 @@
+#ifndef uitreeitemmanager_h
+#define uitreeitemmanager_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
+ Author:	Kristofer Tingdahl
+ Date:		4-11-2002
+ RCS:		$Id: uitreeitemmanager.h,v 1.1 2003-10-21 08:27:30 kristofer Exp $
+________________________________________________________________________
+
+
+-*/
+
+#include "sets.h"
+#include "iopar.h"
+#include "callback.h"
+#include "bufstring.h"
+
+/*!\brief
+are helping classes for uiListViews, wich makes it easy to bundle inteligence
+and data to treeitems. Usage is normally to create the uiTreeTopItem, run the
+its init() and add Childrens.
+*/
+
+class uiListViewItem;
+class uiListView;
+class uiParent;
+
+class uiTreeItem	: public CallBacker
+{
+public:
+
+    				uiTreeItem( const char* name__ );
+    virtual			~uiTreeItem();
+
+    const char*			name() const;
+
+    virtual int			selectionKey() const { return -1; }
+    virtual bool		select();
+    				/*!<Selects this item */
+				 
+    virtual bool		addChild( uiTreeItem* child );
+    				/*!<Adds a child. If the child does not fit
+				    (i.e. the child's parentType() is not
+				    the same as this), it will try to find
+				    a valid parent somewhere else in the tree.
+				    \note child becomes mine regardless of
+				    	  return value.
+				*/
+    virtual void		removeChild( uiTreeItem* );
+    virtual const uiTreeItem*	findChild( const char* name ) const;
+    				/*!<Finds a child in the tree below
+				    this item.
+				*/
+
+    template<class T> inline void setProperty(const char* key, const T&);
+    				/*!<Sets a keyed value that has been retrieved
+				    with getProperty().
+				    \note		Should not be used if T
+				    			is a pointer. Use
+							setPropertyPtr(
+							const char*, T& )
+							instead.
+				*/
+    template<class T> inline void setPropertyPtr(const char* key, const T&);
+    				/*!<Sets a keyed pointer that has been retrieved
+				    with getPropertyPtr().
+				*/
+    template<class T> inline bool getProperty(const char* key, T& res) const;
+    				/*!<Gets a keyed value that has been stored
+				    with setProperty().
+				    \retval true	the key was found and
+				    			res is set
+				    \retval false	the key was not found
+				    			and res is not set
+				    \note		Should not be used if T
+				    			is a pointer. Use
+							getPropertyPtr(
+							const char*, T& )
+							instead.
+				*/
+    template<class T> inline bool getPropertyPtr(const char* key, T& res) const;
+    				/*!<Gets a keyed pointer that has been stored
+				    with setPropertyPtr().
+				    \retval true	the key was found and
+				    			res is set
+				    \retval false	the key was not found
+				    			and res is not set
+				*/
+
+protected:
+    virtual int			uiListViewItemType() const;
+    				/*!<\returns the uiListViewItem::Type that
+				    should be created */
+    virtual uiParent*		getUiParent();
+
+    virtual bool		addChild( uiTreeItem*, bool downwards );
+    				/*!< Adds a child to this item. If the child
+				    does not fit (i.e. its parentType() is not
+				    equal to this), the object tries to add
+				    it to its parent if downwards is false.
+				    If downwards is true, it tries to add it
+				    to its children if it does not fit.
+				*/
+
+    virtual const char*		parentType() const = 0;
+    				/*!<\returns typeid(parentclass).name() */
+    virtual bool		init() { return true; }
+				
+    virtual bool		rightClick(uiListViewItem* item);
+    virtual bool		anyButtonClick(uiListViewItem* item);
+    virtual void		setListViewItem( uiListViewItem* );
+    uiListViewItem*		getItem() { return uilistviewitem; }
+
+    virtual bool		showSubMenu() { return true; }
+    virtual bool		select(int selkey);
+
+    virtual bool		isSelectable() const { return false; }
+    virtual bool		isExpandable() const { return true; }
+    
+    virtual void		updateSelection(int selectionKey,
+	    					bool dw=false );
+    				/*!< Does only update the display */
+    virtual void		updateColumnText(int col);
+
+
+    IOPar			properties;
+
+    uiTreeItem*			parent;
+    BufferString		name_;
+
+    uiListViewItem*		uilistviewitem;
+    ObjectSet<uiTreeItem>	children;
+    friend			class uiTreeTopItem;
+};
+
+
+class uiTreeTopItem : public uiTreeItem
+{
+public:
+    			uiTreeTopItem(uiListView*);
+    virtual bool	addChild( uiTreeItem*);
+    virtual void	updateSelection(int selectionkey, bool=false );
+    			/*!< Does only update the display */
+    virtual void	updateColumnText(int col);
+
+
+			~uiTreeTopItem();
+protected:
+    virtual bool	addChild( uiTreeItem*, bool downwards);
+    uiListView*		listview;
+    void		rightClickCB(CallBacker*);
+    void		anyButtonClickCB(CallBacker*);
+    virtual const char*	parentType() const { return 0; } 
+    virtual uiParent*	getUiParent();
+};
+
+
+template<class T>
+bool inline uiTreeItem::getProperty( const char* propertykey, T& res ) const
+{
+    if ( properties.get( propertykey, res ))
+	return true;
+
+    return parent ? parent->getProperty( propertykey, res ) : false;
+}
+
+
+template<class T>
+bool inline uiTreeItem::getPropertyPtr( const char* propertykey, T& res ) const
+{
+    int tres = 0;
+    if ( properties.get( propertykey, tres ))
+    {
+	res = (T) tres;
+	return true;
+    }
+
+    return parent ? parent->getPropertyPtr( propertykey, res ) : false;
+}
+
+
+template<class T>
+void inline uiTreeItem::setProperty( const char* propertykey, const T& val )
+{
+    if ( typeid(T)==typeid(void*) )
+	properties.set( propertykey, (long long) val );
+    else
+	properties.set( propertykey, val );
+}
+
+
+template<class T>
+void inline uiTreeItem::setPropertyPtr( const char* propertykey, const T& val )
+{
+    properties.set( propertykey, (int) val );
+}
+
+
+
+#endif
---
+++ src/uiTools/uitreeitemmanager.cc
@@ -0,0 +1,279 @@
+/*
+___________________________________________________________________
+
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Jul 2003
+___________________________________________________________________
+
+-*/
+
+static const char* rcsID = "$Id: uitreeitemmanager.cc,v 1.1 2003-10-21 08:27:33 kristofer Exp $";
+
+
+#include "uitreeitemmanager.h"
+#include "uimenu.h"
+#include "errh.h"
+#include "uilistview.h"
+
+
+uiTreeItem::uiTreeItem( const char* name__ )
+    : parent( 0 )
+    , name_( name__ )
+    , uilistviewitem( 0 )
+{
+}
+
+
+uiTreeItem::~uiTreeItem()
+{
+    while ( children.size() )
+	removeChild( children[0] );
+}
+
+
+const char* uiTreeItem::name() const
+{
+    return name_.buf();
+}
+
+
+
+bool uiTreeItem::rightClick( uiListViewItem* item )
+{
+    if ( item==uilistviewitem )
+    {
+	showSubMenu();
+	return true;
+    }
+
+    for ( int idx=0; idx<children.size(); idx++ )
+    {
+	if ( children[idx]->rightClick(item) )
+	    return true;
+    }
+
+    return false;
+}
+
+
+bool uiTreeItem::anyButtonClick( uiListViewItem* item )
+{
+    if ( item==uilistviewitem )
+	return select();
+
+    for ( int idx=0; idx<children.size(); idx++ )
+    {
+	if ( children[idx]->anyButtonClick(item) )
+	    return true;
+    }
+
+    return false;
+}
+
+
+void uiTreeItem::updateSelection( int selid, bool downward )
+{
+    if ( uilistviewitem )
+	uilistviewitem->setSelected( selid!=-1 && selid==selectionKey() );
+
+    if ( downward )
+    {
+	for ( int idx=0; idx<children.size(); idx++ )
+	    children[idx]->updateSelection(selid,downward);
+    }
+    else if ( parent )
+    {
+	parent->updateSelection( selid, false );
+    }
+}
+
+
+bool uiTreeItem::select(int selkey)
+{
+    return parent ? parent->select(selkey) : false;
+}
+
+
+bool uiTreeItem::select()
+{
+    return select(selectionKey());
+}
+
+
+
+
+void uiTreeItem::updateColumnText( int col )
+{
+    for ( int idx=0; idx<children.size(); idx++ )
+	children[idx]->updateColumnText(col);
+
+    if ( !uilistviewitem ) return;
+
+    if ( !col )
+    {
+	uilistviewitem->setText( name_, col );
+    }
+}
+
+
+const uiTreeItem* uiTreeItem::findChild( const char* nm ) const
+{
+    if ( !strcmp( nm, name_ ) )
+	return this;
+
+    for ( int idx=0; idx<children.size(); idx++ )
+    {
+	const uiTreeItem* res = children[idx]->findChild(nm);
+	if ( res )
+	    return res;
+    }
+
+    return 0;
+}
+	
+
+int uiTreeItem::uiListViewItemType() const
+{
+    return uiListViewItem::Standard;
+}
+
+
+uiParent* uiTreeItem::getUiParent()
+{
+    return parent ? parent->getUiParent() : 0;
+}
+
+
+void uiTreeItem::setListViewItem( uiListViewItem* item )
+{
+    uilistviewitem=item;
+    uilistviewitem->setExpandable(isExpandable());
+    uilistviewitem->setSelectable(isSelectable());
+}
+
+
+#define mAddChildImpl( uiparentlist ) \
+if ( newitem->parentType()==typeid(*this).name() ) \
+{ \
+    children += newitem; \
+    newitem->parent = this; \
+ \
+    newitem->setListViewItem( new uiListViewItem(uiparentlist, \
+		  uiListViewItem::Setup(newitem->name(), \
+		  (uiListViewItem::Type)newitem->uiListViewItemType())) ); \
+ \
+    if ( !newitem->init() ) \
+    { \
+	removeChild( newitem ); \
+	return true; \
+    } \
+ \
+    if ( uilistviewitem ) uilistviewitem->setOpen( true ); \
+    updateColumnText(0); updateColumnText(1); \
+    updateSelection(selectionKey(),false);  \
+    return true; \
+} \
+ \
+if ( downwards ) \
+{ \
+    for ( int idx=0; idx<children.size(); idx++ ) \
+    { \
+	if ( children[idx]->addChild( newitem, downwards ) ) \
+	    return true; \
+    } \
+} \
+else if ( parent ) \
+    return parent->addChild( newitem, downwards ); \
+ \
+return false; 
+
+
+bool uiTreeItem::addChild( uiTreeItem* newitem )
+{
+    return addChild( newitem, false );
+}
+
+
+bool uiTreeItem::addChild( uiTreeItem* newitem, bool downwards )
+{
+    mAddChildImpl( uilistviewitem );
+}
+
+
+void uiTreeItem::removeChild( uiTreeItem* treeitem )
+{
+    const int idx=children.indexOf( treeitem );
+    if ( idx<0 )
+	return;
+
+    uilistviewitem->removeItem( treeitem->getItem() );
+    delete children[idx];
+    children.remove( idx );
+}
+
+
+uiTreeTopItem::uiTreeTopItem( uiListView* listview_ )
+    : uiTreeItem( listview_->name() )
+    , listview( listview_ )
+{
+    listview->rightButtonClicked.notify(
+	    		mCB(this,uiTreeTopItem,rightClickCB) );
+    listview->mouseButtonClicked.notify(
+	    		mCB(this,uiTreeTopItem,anyButtonClickCB) );
+}
+
+
+uiTreeTopItem::~uiTreeTopItem()
+{
+    listview->rightButtonClicked.remove(
+	    		mCB(this,uiTreeTopItem,rightClickCB) );
+    listview->mouseButtonClicked.remove(
+	    		mCB(this,uiTreeTopItem,anyButtonClickCB) );
+}
+
+
+bool uiTreeTopItem::addChild( uiTreeItem* newitem )
+{
+    return addChild( newitem, true );
+}
+
+
+
+bool uiTreeTopItem::addChild( uiTreeItem* newitem, bool downwards )
+{
+    downwards = true;		//We are at the top, so we should go downwards
+    mAddChildImpl( listview );
+}
+
+
+void uiTreeTopItem::rightClickCB( CallBacker* )
+{
+    rightClick( listview->itemNotified() );
+}
+
+
+void uiTreeTopItem::anyButtonClickCB( CallBacker* cb )
+{
+    anyButtonClick( listview->itemNotified() );
+}
+
+
+void uiTreeTopItem::updateSelection( int selectionkey, bool dw )
+{
+    uiTreeItem::updateSelection( selectionkey, true );
+    listview->triggerUpdate();
+}
+
+
+void uiTreeTopItem::updateColumnText(int col)
+{
+    //Is only impl to have it nicely together with updateSelection at the
+    //public methods in the headerfile.
+    uiTreeItem::updateColumnText(col);
+}
+
+
+uiParent* uiTreeTopItem::getUiParent()
+{
+    return listview->parent();
+}
