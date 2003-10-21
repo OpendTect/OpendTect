c4ab82e697dc516ff4a9b29ba686370bdd59c0b5
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2003-10-21 18:27:03 +0000

    *** empty log message ***
    git-svn-id: https://svn.opendtect.org/od/trunk@2887 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/SoOD/SoIndexedTriangleFanSet.h
@@ -0,0 +1,66 @@
+#ifndef SoIndexedTriangleFanSet_h
+#define SoIndexedTriangleFanSet_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) de Groot-Bril Earth Sciences B.V.
+ Author:	Kristofer Tingdahl
+ Date:		4-11-2002
+ RCS:		$Id: SoIndexedTriangleFanSet.h,v 1.1 2003-10-21 18:27:03 kristofer Exp $
+________________________________________________________________________
+
+
+-*/
+
+#include "Inventor/nodes/SoIndexedShape.h"
+
+
+/*!\brief
+A shape class that is similar to IndexedTriangleStripSet. The organized like
+a fan, where the first index is in the center of the fan, and the following
+indexes was the surrounding coordinates.
+
+1   2   3   4   5
+
+       6
+
+I.e. a coordIndex sequence of : 6, 1, 2, 3, 4, 5, -1 creates the following
+triangles: 6-1-2, 6-2-3, 6-3-4, 6-4-5.
+This saves quite much mem compared with stripsets.
+
+*/
+
+class SoIndexedTriangleFanSet : public SoIndexedShape
+{
+    SO_NODE_HEADER(SoIndexedTriangleFanSet);
+public:
+
+    static void			initClass();
+    				SoIndexedTriangleFanSet();
+
+protected:
+    void			generatePrimitives(SoAction*);
+    SbBool			generateDefaultNormals(SoState* state,
+						       SoNormalCache* nc);
+    				/*! Does not work properly (TODO), use 
+				    own normals instead */
+
+    void			GLRender(SoGLRenderAction*);
+
+private:
+    enum			Binding { OVERALL = 0,
+					  PER_FAN,
+					  PER_FAN_INDEXED,
+					  PER_TRIANGLE,
+					  PER_TRIANGLE_INDEXED,
+					  PER_VERTEX,
+					  PER_VERTEX_INDEXED };
+
+
+    Binding			findMaterialBinding(SoState*) const;
+    Binding			findNormalBinding(SoState*) const;
+};
+
+#endif
+
