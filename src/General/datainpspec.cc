5564e5dd8a94d670d3d05e0f14481651996281b1
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2004-01-12 13:20:10 +0000

    Added fillPar and usePar on datainpspec, and move impl of non template classes to new cc-file
    git-svn-id: https://svn.opendtect.org/od/trunk@3269 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/General/datainpspec.cc
@@ -0,0 +1,376 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : 12-1-2004
+-*/
+
+static const char* rcsID = "$Id: datainpspec.cc,v 1.3 2004-01-12 13:20:10 kristofer Exp $";
+
+#include "datainpspec.h"
+#include "iopar.h"
+#include "ptrman.h"
+
+
+const char* DataInpSpec::valuestr = "Val";
+
+
+DataInpSpec::DataInpSpec( DataType t )
+    : tp_(t), prefempty_(true)
+{}
+
+
+DataInpSpec::DataInpSpec( const DataInpSpec& o )
+     : tp_(o.tp_), prefempty_(true)
+{}
+
+
+DataType DataInpSpec::type() const
+{ return tp_; }
+
+
+bool DataInpSpec::preferEmpty() const
+{ return prefempty_; }
+
+
+bool DataInpSpec::hasLimits() const
+{ return false; }
+
+
+void DataInpSpec::fillPar(IOPar& par) const
+{
+    for ( int idx=0; idx<nElems(); idx++ )
+    {
+	IOPar subpar;
+	subpar.set(valuestr, text(idx) );
+	const BufferString key(idx);
+	par.mergeComp(subpar, key);
+    }
+}
+
+
+bool DataInpSpec::usePar(const IOPar& par)
+{
+    ObjectSet<const char>	values;
+    for ( int idx=0; idx<nElems(); idx++ )
+    {
+	const BufferString key(idx);
+	BufferString value;
+	PtrMan<IOPar> subpar = par.subselect(key);
+	if ( !subpar ) return false;
+	const char* valptr = (*subpar)[valuestr];
+	if ( !valptr || !*valptr )
+	    return false;
+
+	values += valptr;
+    }
+
+    for ( int idx=0; idx<nElems(); idx++ )
+	setText( values[idx], idx );
+
+    return true;
+}
+
+
+int DataInpSpec::getIntValue( int idx ) const
+{
+    int res = 0;
+    const char* valstr = text(idx);
+    return valstr && getFromString(res, valstr,0);
+}
+
+
+double DataInpSpec::getValue( int idx ) const
+{
+    double res = 0;
+    const char* valstr = text(idx);
+    return valstr && getFromString(res,valstr,0);
+}
+
+
+float DataInpSpec::getfValue( int idx ) const
+{
+    float res = 0;
+    const char* valstr = text(idx);
+    return valstr && getFromString(res,valstr,0);
+}
+
+
+bool DataInpSpec::getBoolValue( int idx ) const
+{ return (bool)getIntValue(idx); }
+
+
+void DataInpSpec::setValue( int i, int idx )
+{ setText( toString( i ),idx); }
+
+
+void DataInpSpec::setValue( double d, int idx )
+{ setText( toString( d ),idx); }
+
+
+void DataInpSpec::setValue( float f, int idx )
+{ setText( toString( f ),idx); }
+
+
+void DataInpSpec::setValue( bool b, int idx )
+{ setValue( ((int)b), idx ); }
+
+
+void DataInpSpec::setType( DataType t )
+{ tp_ = t; }
+
+
+StringInpSpec::StringInpSpec( const char* s )
+    : DataInpSpec( DataTypeImpl<const char*>() )
+    , isUndef_(s?false:true), str( s )
+{}
+
+
+bool StringInpSpec::isUndef( int idx ) const
+{ return isUndef_; }
+
+
+DataInpSpec* StringInpSpec::clone() const
+{ return new StringInpSpec( *this ); }
+
+
+const char* StringInpSpec::text() const
+{ 
+    if ( isUndef() ) return "";
+    return (const char*) str;
+}
+
+void StringInpSpec::setText( const char* s, int idx )
+{
+    str = s; isUndef_ = s ? false : true;
+}
+
+
+const char* StringInpSpec::text( int idx ) const
+{
+    return text();
+}
+
+
+FileNameInpSpec::FileNameInpSpec( const char* fname )
+    : StringInpSpec( fname )
+{
+    setType( DataTypeImpl<const char*>( DataType::filename ) );
+}
+
+
+DataInpSpec* FileNameInpSpec::clone() const
+{ return new FileNameInpSpec( *this ); }
+
+
+BoolInpSpec::BoolInpSpec( const char* truetxt, const char* falsetxt,bool yesno)
+    : DataInpSpec( DataTypeImpl<bool>() )
+    , truetext(truetxt && *truetxt ? truetxt : "Yes")
+    , falsetext(falsetxt && *falsetxt ? falsetxt : "No")
+    , yn(yesno)
+{}
+
+
+BoolInpSpec::BoolInpSpec( const BoolInpSpec& oth)
+    : DataInpSpec( oth )
+    , truetext( oth.truetext )
+    , falsetext( oth.falsetext )
+    , yn( oth.yn )
+{}
+
+
+bool BoolInpSpec::isUndef( int idx ) const
+{ return false; }
+
+
+DataInpSpec* BoolInpSpec::clone() const
+{ return new BoolInpSpec( *this ); }
+
+
+const char* BoolInpSpec::trueFalseTxt( bool tf ) const
+{ return tf ? truetext : falsetext; }
+
+
+void BoolInpSpec::setTrueFalseTxt( bool tf, const char* txt )
+{ if ( tf ) truetext=txt; else falsetext=txt; }
+
+
+bool BoolInpSpec::checked() const 
+{ return yn; }
+
+
+void BoolInpSpec::setChecked( bool yesno )
+{ yn=yesno; }
+
+
+const char* BoolInpSpec::text( int idx ) const
+{
+    return yn ? (const char*)truetext
+	      : (const char*)falsetext;
+}
+
+
+void BoolInpSpec::setText( const char* s, int idx )
+{
+    yn = s && strcmp(s,falsetext);
+}
+
+
+bool BoolInpSpec::getBoolValue( int idx ) const
+{ return yn; }
+
+
+void BoolInpSpec::setValue( bool b, int idx )
+{ yn = b; }
+
+
+StringListInpSpec::StringListInpSpec( const BufferStringSet& bss )
+    : DataInpSpec( DataTypeImpl<const char*> (DataType::list) )
+    , strings_(bss)
+{}
+
+
+StringListInpSpec::StringListInpSpec( const char** sl )
+    : DataInpSpec( DataTypeImpl<const char*> (DataType::list) )
+    , cur_(0)
+{
+    if ( !sl ) return;
+    while( *sl )
+	strings_ += new BufferString( *sl++ );
+}
+
+StringListInpSpec::StringListInpSpec( TypeSet<char*> sl )
+    : DataInpSpec( DataTypeImpl<const char*> (DataType::list) )
+    , cur_(0)
+{
+    for ( int idx=0; idx<sl.size(); idx++ )
+	strings_ += new BufferString( sl[idx] );
+}
+
+StringListInpSpec::StringListInpSpec( const StringListInpSpec& oth)
+    : DataInpSpec( oth )
+    , cur_(oth.cur_)
+{ deepCopy( strings_, oth.strings_ ); }
+
+
+StringListInpSpec::~StringListInpSpec()
+{ deepErase(strings_); }
+
+
+bool StringListInpSpec::isUndef( int idx ) const
+{ return !(strings_.size() && cur_ >= 0); }
+
+
+DataInpSpec* StringListInpSpec::clone() const
+{ return new StringListInpSpec( *this ); }
+
+
+const BufferStringSet& StringListInpSpec::strings() const
+{ return strings_; }
+
+
+void StringListInpSpec::addString( const char* txt )
+{ strings_ += new BufferString( txt ); }
+
+
+const char* StringListInpSpec::text( int idx ) const
+{
+    if ( isUndef() ) return "";
+    else return (const char*)*strings_[cur_];
+}
+
+
+void StringListInpSpec::setItemText( int idx, const char* s )
+{ *strings_[cur_] = s; }
+
+
+void StringListInpSpec::setText( const char* s, int nr )
+{
+    for ( int idx=0; idx<strings_.size(); idx++ )
+	if ( *strings_[idx] == s )
+    { cur_ = idx; return; }
+}
+
+int StringListInpSpec::getIntValue( int idx ) const
+{ return cur_; }
+
+
+double StringListInpSpec::getValue( int idx ) const
+{ return cur_; }
+
+
+float StringListInpSpec::getfValue( int idx ) const
+{ return cur_; }
+
+
+void StringListInpSpec::setValue( int i, int idx )
+{ if ( i < strings_.size() ) cur_ = i; }
+
+
+void StringListInpSpec::setValue( double d, int idx )
+{
+    if ( (int)(d+.5) < strings_.size() )
+	cur_ = (int)(d+.5);
+}
+
+
+void StringListInpSpec::setValue( float f, int idx )
+{
+    if ( (int)(f+.5) < strings_.size() )
+	cur_ = (int)(f+.5);
+}
+
+
+BinIDCoordInpSpec::BinIDCoordInpSpec( bool doCoord, bool isRelative,
+					bool withOtherBut, double inline_x,
+					double crossline_y,
+					const BinID2Coord* b2c )
+    : DataInpSpec( DataTypeImpl<int>(DataType::binID) )
+    , withOtherBut_( withOtherBut )
+    , inl_x( inline_x )
+    , crl_y( crossline_y )
+    , doCoord_( doCoord )
+    , isRelative_( isRelative )
+    , b2c_( b2c )
+{}
+
+
+DataInpSpec* BinIDCoordInpSpec::clone() const   
+{ return new BinIDCoordInpSpec( *this ); }
+
+
+int BinIDCoordInpSpec:: nElems()  const { return 2; }
+
+
+double BinIDCoordInpSpec::value( int idx ) const
+{ return idx ? crl_y : inl_x; }
+
+
+bool BinIDCoordInpSpec::isUndef( int idx ) const
+{
+    if ( idx<0 || idx>1 ) return true;
+    return idx ? isUndefined( crl_y )
+	       : isUndefined( inl_x );
+} 
+
+
+const char* BinIDCoordInpSpec::text( int idx ) const
+{
+    if ( isUndef() )        return "";
+    else            return toString( value(idx) );
+}
+
+
+void BinIDCoordInpSpec::setText( const char* s, int idx ) 
+{ getFromString( (idx ? crl_y : inl_x), s); }
+
+
+const char* BinIDCoordInpSpec::otherTxt() const
+{
+    if ( !withOtherBut_ ) return 0;
+    if ( doCoord_ ) { return "Inline/Xline ..."; }
+    return isRelative_? "Distance ...":"Coords ...";
+}
+
+
+const BinID2Coord* BinIDCoordInpSpec::binID2Coord() const
+{ return b2c_; }
