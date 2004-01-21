689066fbbf3f5b0d2181c71ef06b0a3064e20871
dgb <dgb@a985855d-d384-4b45-ae33-31a410998c3c>
2004-01-21 10:21:28 +0000

    use exitProgram to, eh, exit.
    git-svn-id: https://svn.opendtect.org/od/trunk@3321 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiODMain/od_main.cc
+++ src/uiODMain/od_main.cc
@@ -4,17 +4,19 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H.Bril
  Date:          Mar 2002
- RCS:           $Id: od_main.cc,v 1.3 2004-01-15 16:17:03 dgb Exp $
+ RCS:           $Id: od_main.cc,v 1.4 2004-01-21 10:21:28 dgb Exp $
 ________________________________________________________________________
 
 -*/
 
 
 #include "prog.h"
+#include "genc.h"
 
 extern int ODMain(int,char**);
 
 int main( int argc, char** argv )
 {
-    return ODMain( argc, argv );
+    int ret=ODMain( argc, argv );
+    exitProgram(ret);
 }
