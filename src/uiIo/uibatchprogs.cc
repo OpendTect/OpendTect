3fed02fb507d64ed39921963d6ce744b20d6571f
dgb <dgb@a985855d-d384-4b45-ae33-31a410998c3c>
2004-03-23 16:28:17 +0000

    oops
    git-svn-id: https://svn.opendtect.org/od/trunk@3509 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiIo/uibatchprogs.cc
+++ src/uiIo/uibatchprogs.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne Hemstra
  Date:          January 2002
- RCS:           $Id: uibatchprogs.cc,v 1.15 2004-03-23 16:04:45 bert Exp $
+ RCS:           $Id: uibatchprogs.cc,v 1.16 2004-03-23 16:28:17 dgb Exp $
 ________________________________________________________________________
 
 -*/
@@ -272,13 +272,7 @@ bool uiBatchProgLaunch::acceptOK( CallBacker* )
     const BatchProgInfo& bpi = *pil[selidx];
 
     BufferString comm( "@" );
-#ifdef __win__
     comm += mGetExecScript();
-#else
-    BufferString exbpscript = GetSoftwareDir();
-    exbpscript = File_getFullPath( exbpscript, "exec_prog" );
-    comm += exbpscript;
-#endif
 
     comm += " --inxterm+askclose ";
     if ( bpi.issys ) comm += "--sys ";
