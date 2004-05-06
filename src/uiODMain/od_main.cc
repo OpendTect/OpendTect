ea4f93ec12bfac0085296817ab6f9d926265ccc5
macman <macman@a985855d-d384-4b45-ae33-31a410998c3c>
2004-05-06 14:20:14 +0000

    Explicitely include factory on mac
    git-svn-id: https://svn.opendtect.org/od/trunk@3655 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiODMain/od_main.cc
+++ src/uiODMain/od_main.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H.Bril
  Date:          Mar 2002
- RCS:           $Id: od_main.cc,v 1.4 2004-01-21 10:21:28 dgb Exp $
+ RCS:           $Id: od_main.cc,v 1.5 2004-05-06 14:20:14 macman Exp $
 ________________________________________________________________________
 
 -*/
@@ -12,6 +12,11 @@ ________________________________________________________________________
 
 #include "prog.h"
 #include "genc.h"
+
+// TODO : Is there a better way to force linking with attribute factory?
+#ifdef __mac__
+# include "attribfact.h"
+#endif
 
 extern int ODMain(int,char**);
 
