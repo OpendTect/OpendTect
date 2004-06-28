2b94fdbd307274eca0777cdc35f64423a357cded
Bert Bril <bert.bril@dgbes.com>
2004-06-28 09:35:20 +0000

    upda
    git-svn-id: https://svn.opendtect.org/od/trunk@3899 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiSeis/uiseissubsel.h
+++ include/uiSeis/uiseissubsel.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H. Bril
  Date:          June 2004
- RCS:           $Id: uiseissubsel.h,v 1.1 2004-06-18 15:54:30 bert Exp $
+ RCS:           $Id: uiseissubsel.h,v 1.2 2004-06-28 09:35:20 bert Exp $
 ________________________________________________________________________
 
 -*/
@@ -29,6 +29,8 @@ public:
     			//!< Trace number range
     void		setInput(const StepInterval<float>&);
     			//!< Z range
+    void		setInput(const BinIDRange&);
+    			//!< The pseudo-ranges; converted to trace ranges
     void		setInput(const char* linename);
     void		setInput( const StepInterval<int>& tr,
 	    			  const StepInterval<float>& zr )
@@ -39,10 +41,10 @@ public:
     bool		getRange(StepInterval<int>&) const;
     bool		getZRange(StepInterval<float>&) const;
 
-    void		setSelText(const char*);
     bool		isAll() const;
 
     int			expectedNrSamples() const;
+    int			expectedNrTraces() const;
 
 
 protected:
