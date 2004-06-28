a5b054adf053e104eadded7cb52170190573e522
Bert Bril <bert.bril@dgbes.com>
2004-06-28 07:43:06 +0000

    Made more general including 2D
    git-svn-id: https://svn.opendtect.org/od/trunk@3897 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiSeis/uiseissel.h
+++ include/uiSeis/uiseissel.h
@@ -6,13 +6,14 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H. Bril
  Date:          July 2001
- RCS:           $Id: uiseissel.h,v 1.2 2003-11-07 12:21:54 bert Exp $
+ RCS:           $Id: uiseissel.h,v 1.3 2004-06-28 07:43:06 bert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uiioobjsel.h"
 class uiBinIDSubSel;
+class ui2DSeisSubSel;
 
 
 class uiSeisSelDlg : public uiIOObjSelDlg
@@ -20,16 +21,28 @@ class uiSeisSelDlg : public uiIOObjSelDlg
 public:
 
 			uiSeisSelDlg(uiParent*,const CtxtIOObj&,
-				     bool withz=false,
-				     const char* selectlistannot=0);
+				     const SeisSelSetup&);
 
     virtual void	fillPar(IOPar&) const;
     virtual void	usePar(const IOPar&);
-    void		selChg(CallBacker*);
+
+    static const char*	standardTranslSel(int pol2d);
+    			//!< returns "CBVS`2D" for pol2d == 0
 
 protected:
 
+    SeisSelSetup	setup;
+    bool		is2d;
+
     uiBinIDSubSel*	subsel;
+    ui2DSeisSubSel*	subsel2d;
+
+    void		entrySel(CallBacker*);
+    void		fillFlds(CallBacker*);
+
+private:
+
+    const CtxtIOObj&	getCtio(const CtxtIOObj&,const SeisSelSetup&);
 
 };
 
@@ -38,9 +51,8 @@ class uiSeisSel : public uiIOObjSel
 {
 public:
 
-			uiSeisSel(uiParent*,CtxtIOObj&,const char* txt=0,
-				  bool wthz=false,bool wthclear=false,
-				  const char* selectlistannot=0);
+			uiSeisSel(uiParent*,CtxtIOObj&,const char* txt,
+				  const SeisSelSetup&,bool wthclear=false);
 
     virtual bool	fillPar(IOPar&) const;
 			//!< Extra, ctio will be filled anyway
@@ -49,7 +61,7 @@ public:
 
 protected:
 
-    bool		withz;
+    SeisSelSetup	setup;
 
     virtual void	newSelection(uiIOObjRetDlg*);
     virtual uiIOObjRetDlg* mkDlg();
