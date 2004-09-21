b8ec219fd4ca07c7b38a2a1a765671f4f087046e
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2004-09-21 15:57:39 +0000

    Made inherit new RCol class
    git-svn-id: https://svn.opendtect.org/od/trunk@4216 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/rowcol.h
+++ include/Basic/rowcol.h
@@ -7,32 +7,31 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	A.H. Bril
  Date:		12-8-1997
- RCS:		$Id: rowcol.h,v 1.15 2004-08-02 08:54:21 kristofer Exp $
+ RCS:		$Id: rowcol.h,v 1.16 2004-09-21 15:57:39 kristofer Exp $
 ________________________________________________________________________
 
 -*/
+
+#include "basictask.h"
+#include "rcol.h"
 
 
 /*!\brief Object with row and col. */
 
 template <class T> class TypeSet;
 
-class RowCol
+class RowCol : public RCol
 {
 public:
-		RowCol( int r, int c ) : row(r), col(c)		{}
+		RowCol( int row_, int col_ ) : row(row_), col(col_)	{}
+		RowCol( const RCol& rc ) : row( rc.r() ), col ( rc.c() )			{}
 		RowCol() : row( 0 ), col ( 0 )			{}
 
-    void	operator+=( const RowCol& rc )
-		{ row += rc.row; col += rc.col; }
-    void	operator-=( const RowCol& rc )
-		{ row -= rc.row; col -= rc.col; }
-    void	operator*=( const RowCol& rc )
-		{ row *= rc.row; col *= rc.col; }
-    void	operator*=( int factor )
-		{ row *= factor; col *= factor; }
-    void	operator/=( const RowCol& rc )
-		{ row /= rc.row; col /= rc.col; }
+    int&	r() { return row; }
+    const int&	r() const { return row; }
+    int&	c() { return col; }
+    const int&	c() const { return col; }
+
     RowCol	operator+( const RowCol& rc ) const
 		{ return RowCol( row+rc.row, col+rc.col ); }
     RowCol	operator-( const RowCol& rc ) const
@@ -45,22 +44,6 @@ public:
 		{ return RowCol( row*factor, col*factor ); }
     RowCol	operator/( const RowCol& rc ) const
 		{ return RowCol( row/rc.row, col/rc.col ); }
-    int		operator==( const RowCol& rc ) const
-		{ return row == rc.row && col == rc.col; }
-    int		operator!=( const RowCol& rc ) const
-		{ return ! (rc == *this); }
-
-    float	angleTo(const RowCol&) const;
-    		/*!<\returns the smallest angle between the vector going from
-		     0,0 to the object and the vector going from 0,0 to rc.*/
-    float	clockwiseAngleTo(const RowCol& rc) const;
-    		/*!<\returns the angle between the vector going from 0,0 to the
-		     object and the vector going from 0,0 to rc in the
-		     clockwise direction.*/
-    float	counterClockwiseAngleTo(const RowCol&) const;
-    		/*!<\returns the angle between the vector going from 0,0 to the
-		     object and the vector going from 0,0 to rc in the
-		     counterclockwise direction.*/
 
     RowCol	getDirection() const;
     		/*!\returns a rowcol where row/col are either -1, 0 or 1 where
@@ -68,21 +51,11 @@ public:
 		    positive. 
 		*/
 
-    bool	isNeighborTo( const RowCol&,
-	    		      const RowCol& step=RowCol(1,1),
-			      bool eightconnectivity=true ) const;
-    		/*!<\returns true if the object is a neighbor with the 
-		     provided RowCol. The neighborhood is defined with either
-		     eight- or four-connectivity */
-
     int		row;
     int		col;
 
     static const TypeSet<RowCol>&	clockWiseSequence();
-    static void				makeLine( const RowCol& start,
-					    const RowCol& stop,
-					    TypeSet<RowCol>& output,
-					    const RowCol& step=RowCol(1,1) );
+
 private:
 };
 
