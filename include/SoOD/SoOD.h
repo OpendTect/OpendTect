1d79f3afe1c9c76b144b20bc15c5f38ef89cd200
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2004-12-08 10:16:04 +0000

    *** empty log message ***
    git-svn-id: https://svn.opendtect.org/od/trunk@4605 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/SoOD/SoOD.h
@@ -0,0 +1,24 @@
+#ifndef SoOD_h
+#define SoOD_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	Kristofer Tingdahl
+ Date:		4-11-2002
+ RCS:		$Id: SoOD.h,v 1.1 2004-12-08 10:16:04 kristofer Exp $
+________________________________________________________________________
+
+
+-*/
+
+/*!\brief A function that initiases the OpendTect Inventor classes. */
+
+class SoOD
+{
+public:
+    static void		init();
+};
+
+#endif
