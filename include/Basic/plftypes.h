8b8a952abea8ef3bda67aea77d21cade3a18d065
arend <arend@a985855d-d384-4b45-ae33-31a410998c3c>
2004-12-17 12:53:12 +0000

    should be ok on win32
    git-svn-id: https://svn.opendtect.org/od/trunk@4641 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/plftypes.h
+++ include/Basic/plftypes.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	A.H.Bril
  Contents:	Platform dependent types
- RCS:		$Id: plftypes.h,v 1.3 2004-12-17 12:38:43 bert Exp $
+ RCS:		$Id: plftypes.h,v 1.4 2004-12-17 12:53:12 arend Exp $
 ________________________________________________________________________
 
 */
@@ -17,9 +17,7 @@ ________________________________________________________________________
 #if defined(__sun__) || defined(__sgi__)
 # include <sys/types.h>
 #else
-# ifndef __win__
-#  include <stdint.h>
-# endif
+# include <stdint.h>
 #endif
 
 /* 16 bits short is standard. Only use to emphasise the 16-bitness */
@@ -30,14 +28,8 @@ ________________________________________________________________________
 #define int32 int
 #define uint32 unsigned int
 
-/* 64 bits is int64_t on UNIX. The definition is in various header files. */
-#ifndef __win__
-# define int64 int64_t
-# define uint64 uint64_t
-#else
-# define int64 TODO_AREND_see_Basic_plftypes_h
-# define uint64 TODO_AREND_see_Basic_plftypes_h
-#endif
-
+/* 64 bits is int64_t. The definition is in various header files. */
+#define int64 int64_t
+#define uint64 uint64_t
 
 #endif
