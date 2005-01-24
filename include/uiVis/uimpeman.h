3f7d8d9715c84e088ef01f40a7a3050724631c5d
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2005-01-24 08:31:26 +0000

    Numerous interface changes
    git-svn-id: https://svn.opendtect.org/od/trunk@4839 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiVis/uimpeman.h
+++ include/uiVis/uimpeman.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne Hemstra
  Date:          March 2004
- RCS:           $Id: uimpeman.h,v 1.1 2005-01-17 08:34:17 kristofer Exp $
+ RCS:           $Id: uimpeman.h,v 1.2 2005-01-24 08:31:26 kristofer Exp $
 ________________________________________________________________________
 
 -*/
@@ -44,14 +44,14 @@ public:
     				{ return destrackertype; }
 
 protected:
+    visSurvey::MPEDisplay*	getDisplay(int sceneid, bool create=false );
+
     void			seedPropertyChangeCB(CallBacker*);
     void			createSeedMenuCB(CallBacker*);
     void			handleSeedMenuCB(CallBacker*);
     int				seedmnuid;
-    void			createMPEDisplay();
-    uiVisPartServer*		partserver;
+    uiVisPartServer*		visserv;
 
-    visSurvey::MPEDisplay*	mpedisplay;
     visSurvey::SeedEditor*	seededitor;
 
     uiComboBox*			attribfld;
@@ -59,9 +59,9 @@ protected:
 
     uiSpinBox*			nrstepsbox;
 
-    void			selCB(CallBacker*);
-    void			deselCB(CallBacker*);
-    void			selectCB(CallBacker*);
+    void			cubeDeselCB(CallBacker*);
+    void			cubeSelectCB(CallBacker*);
+    void			showCubeButtonPushCB(CallBacker*);
     void			handleInterpreterSelection();
 
     void			displayCB(CallBacker*);
@@ -76,7 +76,7 @@ protected:
     void			trackForward(CallBacker*);
 
     void			seedMode(CallBacker*);
-    void			extendMode(CallBacker*);
+    void			extendModeButtonPushCB(CallBacker*);
     void			retrackMode(CallBacker*);
     void			eraseMode(CallBacker*);
     void			mouseEraseMode(CallBacker*);
@@ -87,7 +87,7 @@ protected:
 
     int				seedidx;
     int				extendidx, retrackidx, eraseidx;
-    int				selectidx, displayidx;
+    int				showcubeidx, displayidx;
     int				mouseeraseridx;
     int				undoidx, redoidx;
     int				trackforwardidx, trackbackwardidx;
