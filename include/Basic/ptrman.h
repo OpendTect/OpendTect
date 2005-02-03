488a7aaf4015963b3a69b3b7d0475c4c91f5e68a
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2005-02-03 09:11:32 +0000

    Changed macro-definition so refcounted objects can make a class from them
    git-svn-id: https://svn.opendtect.org/od/trunk@4882 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/ptrman.h
+++ include/Basic/ptrman.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Kristofer Tingdahl
  Date:          10-12-1999
- RCS:           $Id: ptrman.h,v 1.9 2004-06-03 14:51:35 bert Exp $
+ RCS:           $Id: ptrman.h,v 1.10 2005-02-03 09:11:32 kristofer Exp $
 ________________________________________________________________________
 
 -*/
@@ -15,17 +15,17 @@ ________________________________________________________________________
 #include <general.h>
 
 
-#define mDefPtrMan1(Clss,ArrBrIfNec) \
+#define mDefPtrMan1(Clss,PostSet, EraseFunc) \
 \
 template<class T> \
 class Clss \
 { \
 public: \
 \
-				Clss( T* p=0 ) : ptr_( p )	{} \
+				Clss( T* p=0 ) : ptr_( 0 )	{ set(p); } \
 				~Clss()		{ erase(); } \
     inline Clss<T>&		operator=( T* p ) \
-				{ erase(); ptr_ = p; return *this; } \
+				{ set(p); return *this; } \
 \
     inline T*			ptr()			{ return ptr_; } \
     inline			operator T*()		{ return ptr_; } \
@@ -34,7 +34,7 @@ public: \
     inline const T*		operator ->() const	{ return ptr_; } \
     inline T&			operator *()		{ return *ptr_; } \
 
-#define mDefPtrMan2(Clss,ArrBrIfNec) \
+#define mDefPtrMan2(Clss,PostSet, EraseFunc) \
     inline bool			operator ==( const Clss& p ) const \
 				{ return ptr_ == p.ptr_; } \
     inline bool			operator ==( const T* p ) const \
@@ -47,9 +47,11 @@ public: \
     inline bool			operator !() const { return !ptr_; } \
 \
     void			erase() \
-				{ delete ArrBrIfNec ptr_; ptr_ = 0; } \
+				{ EraseFunc; ptr_ = 0; } \
+    void			set( T* p ) \
+				{ erase(); ptr_=p; PostSet; }
 
-#define mDefPtrMan3(Clss,ArrBrIfNec) \
+#define mDefPtrMan3(Clss,PostSet, EraseFunc) \
 private: \
 \
     T*				ptr_; \
@@ -66,18 +68,18 @@ the responsibility for its deletion.
 For Arrays, use the ArrPtrMan class.
 
 */
-mDefPtrMan1(PtrMan,)
-mDefPtrMan2(PtrMan,)
-mDefPtrMan3(PtrMan,)
+mDefPtrMan1(PtrMan, , delete ptr_ )
+mDefPtrMan2(PtrMan, , delete ptr_ )
+mDefPtrMan3(PtrMan, , delete ptr_ )
 
 /*!\brief a simple autopointer for arrays.
 
 For Non-arrays, use the PtrMan class.
 
 */
-mDefPtrMan1(ArrPtrMan,[])
-mDefPtrMan2(ArrPtrMan,[])
-mDefPtrMan3(ArrPtrMan,[])
+mDefPtrMan1(ArrPtrMan, , delete [] ptr_)
+mDefPtrMan2(ArrPtrMan, , delete [] ptr_)
+mDefPtrMan3(ArrPtrMan, , delete [] ptr_)
 
 
 #endif
