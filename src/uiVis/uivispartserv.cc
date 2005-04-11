b4a69a60c2c9dea8fde392f2731ab4a4377740e3
Nanne Hemstra <nanne.hemstra@dgbes.com>
2005-04-11 15:41:28 +0000

    update QC texture after reloading data
    git-svn-id: https://svn.opendtect.org/od/trunk@5146 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiVis/uivispartserv.cc
+++ src/uiVis/uivispartserv.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        N. Hemstra
  Date:          Mar 2002
- RCS:           $Id: uivispartserv.cc,v 1.255 2005-04-06 10:54:54 cvsnanne Exp $
+ RCS:           $Id: uivispartserv.cc,v 1.256 2005-04-11 15:41:28 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -999,6 +999,7 @@ void uiVisPartServer::deselectObjCB( CallBacker* cb )
 	    eventmutex.lock();
 	    eventobjid = oldsel;
 	    sendEvent( evGetNewData );
+	    showMPEToolbar();
 	    return;
 	}
 
