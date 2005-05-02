338cb624c0d78ce3243c40509122e592ccd259f6
Nanne Hemstra <nanne.hemstra@dgbes.com>
2005-05-02 13:15:26 +0000

    update for reTrack
    git-svn-id: https://svn.opendtect.org/od/trunk@5258 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiMPE/surfacecutter.cc
+++ src/uiMPE/surfacecutter.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        N. Hemstra
  Date:          March 2005
- RCS:           $Id: surfacecutter.cc,v 1.4 2005-04-29 15:40:06 cvsnanne Exp $
+ RCS:           $Id: surfacecutter.cc,v 1.5 2005-05-02 13:15:26 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -20,6 +20,10 @@ ________________________________________________________________________
 #include "consistencychecker.h"
 
 #include "emtracker.h"
+#include "sectiontracker.h"
+#include "sectionselector.h"
+#include "sectionextender.h"
+#include "sectionadjuster.h"
 #include "mpeengine.h"
 #include "trackplane.h"
 #include "trigonometry.h"
@@ -99,7 +103,7 @@ bool SurfaceCutter::doTerminate( bool positiveside )
     checker.nextStep();
     cuttedsurf->geometry.checkSections();
 
-//  reTrack();
+    reTrack();
     return true;
 }
 
@@ -149,17 +153,85 @@ bool SurfaceCutter::reTrack()
     EMTracker* tracker = engine().getTracker( trackeridx );
     if ( !tracker ) return false;
 
-    BinID start;
-    BinID stop;
-    BinID step( 2, 0 );
-    float zstart, zstop;
+    CubeSampling cs;
+    getBoundingBox( cs );
+
+    RowCol dir;
+    getSurfaceDir( dir );
+
+    BinID start, stop, step;
+    if ( dir.row > 0 )
+    {
+	BinID start0 = cs.hrg.start; start0.inl += cs.hrg.step.inl;
+	BinID start1 = cs.hrg.start; start1.inl -= cs.hrg.step.inl;
+	BinID start2(cs.hrg.stop.inl+cs.hrg.step.inl,cs.hrg.start.crl);
+	BinID start3(cs.hrg.stop.inl-cs.hrg.step.inl,cs.hrg.start.crl);
+	stop = cs.hrg.stop;
+	step = cs.hrg.step; step.inl = 0;
+	if ( cuttedsurf->geometry.isDefined(cuttedsectionid_,start0) )
+	{ start = start0; stop.inl = start0.inl; step *= -1; }
+	else if ( cuttedsurf->geometry.isDefined(cuttedsectionid_,start1) )
+	{ start = start1; stop.inl = start1.inl; }
+	else if ( cuttedsurf->geometry.isDefined(cuttedsectionid_,start2) )
+	{ start = start2; stop.inl = start2.inl; step *= -1; }
+	else if ( cuttedsurf->geometry.isDefined(cuttedsectionid_,start3) )
+	{ start = start3; stop.inl = start3.inl; }
+    }
+    else
+    {
+	BinID start0 = cs.hrg.start; start0.crl += cs.hrg.step.crl;
+	BinID start1 = cs.hrg.start; start1.crl -= cs.hrg.step.crl;
+	BinID start2(cs.hrg.start.inl,cs.hrg.stop.crl+cs.hrg.step.crl);
+	BinID start3(cs.hrg.start.inl,cs.hrg.stop.crl-cs.hrg.step.crl);
+	stop = cs.hrg.stop;
+	step = cs.hrg.step; step.inl = 0;
+	if ( cuttedsurf->geometry.isDefined(cuttedsectionid_,start0) )
+	{ start = start0; stop.crl = start0.crl; step *= -1; }
+	else if ( cuttedsurf->geometry.isDefined(cuttedsectionid_,start1) )
+	{ start = start1; stop.crl = start1.crl; }
+	else if ( cuttedsurf->geometry.isDefined(cuttedsectionid_,start2) )
+	{ start = start2; stop.crl = start2.crl; step *= -1; }
+	else if ( cuttedsurf->geometry.isDefined(cuttedsectionid_,start3) )
+	{ start = start3; stop.crl = start3.crl; }
+    }
+
+    float zstart = cs.zrg.start;
+    float zstop = cs.zrg.stop;
+    start += step;
+    stop += step;
     TrackPlane plane( start, stop, zstart, zstop );
     plane.setMotion( step.inl, step.crl, 0 );
-    while ( tracker->trackSections(plane) )
+    while ( true )
     {
+	SectionTracker* sectiontracker = 
+			    tracker->getSectionTracker( cuttedsectionid_,true);
+	if ( !sectiontracker ) return true;
+
+	sectiontracker->reset();
+	sectiontracker->selector()->setTrackPlane( plane );
+	if ( sectiontracker->selector()->selectedPositions().size() == 0 )
+	    break;
+
+	sectiontracker->extender()->setDirection( plane.motion() );
+	sectiontracker->select();
+	if ( sectiontracker->extend() && sectiontracker->adjusterUsed() )
+	    sectiontracker->adjust();
+
+	EM::PosID posid( cuttedsurf->id(), cuttedsectionid_ );
+	ConsistencyChecker checker( *cuttedsurf );
+	const TypeSet<EM::SubID>& addedpos =
+	    			sectiontracker->extender()->getAddedPositions();
+	for ( int posidx=0; posidx<addedpos.size(); posidx++ )
+	{
+	    posid.setSubID( addedpos[posidx] );
+	    checker.addNodeToCheck( posid );
+	}
+
+	checker.nextStep();
+
 	CubeSampling& cs = plane.boundingBox();
-	cs.hrg.start.inl += step.inl;
-	cs.hrg.stop.inl = cs.hrg.start.inl;
+	cs.hrg.start += step;
+	cs.hrg.stop += step;
     }
 
     return true;
