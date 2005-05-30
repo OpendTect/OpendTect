080f5f9575e84b3ddb6dc4b2ef53810f121406c4
Nanne Hemstra <nanne.hemstra@dgbes.com>
2005-05-30 19:09:33 +0000

    added
    git-svn-id: https://svn.opendtect.org/od/trunk@5322 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Attributes/hilbertattrib.h
@@ -0,0 +1,59 @@
+#ifndef hilbertattrib_h
+#define hilbertattrib_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        N. Hemstra
+ Date:          May 2005
+ RCS:           $Id: hilbertattrib.h,v 1.1 2005-05-30 19:09:33 cvsnanne Exp $
+________________________________________________________________________
+
+-*/
+
+#include "attribprovider.h"
+#include "runstat.h"
+#include "valseries.h"
+#include "valseriesinterpol.h"
+#include "mathfunc.h"
+
+namespace Attrib
+{
+
+
+class Hilbert : public Provider
+{
+public:
+    static void			initClass();
+				Hilbert(Desc&);
+
+    static const char*		attribName()	{ return "Hilbert"; }
+    static const char*		halflenStr()	{ return "halflen"; }
+
+protected:
+    static Provider*		createInstance(Desc&);
+    static void			updateDesc(Desc&);
+    static Provider*		internalCreate(Desc&,ObjectSet<Provider>& exis);
+
+    bool			getInputOutput(int inp,TypeSet<int>& res) const;
+    bool			getInputData(const BinID&);
+    bool			computeData(const DataHolder&,const BinID& pos,
+					    int t0,int nrsamples) const;
+
+    const Interval<float>*	reqZMargin(int input,int output) const;
+
+    const DataHolder*		inputdata;
+
+    Interval<float>		gate;
+    int				halflen;
+    int				hilbfilterlen;
+    const float*		hilbfilter;
+    static float*		makeHilbFilt(int);
+};
+
+}; // namespace Attrib
+
+
+#endif
+
---
+++ src/Attributes/hilbertattrib.cc
@@ -0,0 +1,154 @@
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        N. Hemstra
+ Date:          May 2005
+ RCS:           $Id: hilbertattrib.cc,v 1.1 2005-05-30 19:09:14 cvsnanne Exp $
+________________________________________________________________________
+
+-*/
+
+#include "hilbertattrib.h"
+
+#include "attribdataholder.h"
+#include "attribdesc.h"
+#include "attribfactory.h"
+#include "attribparam.h"
+#include "datainpspec.h"
+#include "datainpspec.h"
+#include "genericnumer.h"
+
+namespace Attrib
+{
+
+void Hilbert::initClass()
+{
+    Desc* desc = new Desc( attribName(), updateDesc );
+    desc->ref();
+
+    Param* halflen = new Param( halflenStr(), new IntInpSpec() );
+//  halflen->setDefaultVal( 30 );
+
+    InputSpec inputspec( "Input data", true );
+    desc->addInput( inputspec );
+    desc->addOutputDataType( Seis::UnknowData );
+    desc->init();
+
+    PF().addDesc( desc, createInstance );
+    desc->unRef();
+}
+
+
+Provider* Hilbert::createInstance( Desc& desc )
+{
+    Hilbert* res = new Hilbert( desc );
+    res->ref();
+
+    if ( !res->isOK() )
+    {
+	res->unRef();
+	return 0;
+    }
+
+    res->unRefNoDelete();
+    return res;
+}
+
+
+void Hilbert::updateDesc( Desc& desc )
+{
+}
+
+
+Hilbert::Hilbert( Desc& desc_ )
+    : Provider( desc_ )
+{
+    if ( !isOK() ) return;
+
+    halflen = desc.getParam( halflenStr() )->getIntValue();
+    hilbfilterlen = halflen * 2 + 1;
+    hilbfilter = makeHilbFilt( halflen );
+    gate = Interval<float>( -halflen-3, halflen+3 );
+}
+
+
+bool Hilbert::getInputOutput( int input, TypeSet<int>& res ) const
+{
+    return Provider::getInputOutput( input, res );
+}
+
+
+bool Hilbert::getInputData( const BinID& relpos )
+{
+    inputdata = inputs[0]->getData( relpos );
+    return inputdata;
+}
+
+
+class Masker
+{
+public:
+Masker( const DataHolder* dh, int shift, float avg )
+    : data_(dh )
+    , avg_(avg)
+    , shift_(shift)	{}
+
+float operator[](int idx) const
+{
+    const int pos = shift_ + idx;
+    if ( pos < 0 )
+	return data_->item(0)->value(0) - avg_;
+    if ( pos >= data_->nrsamples_ )
+	return data_->item(0)->value(data_->nrsamples_-1) - avg_;
+    return data_->item(0)->value( pos ) - avg_;
+}
+
+    const DataHolder*	data_;
+    const int		shift_;
+    float		avg_;
+};
+
+
+bool Hilbert::computeData( const DataHolder& output, const BinID& relpos, 
+			   int t0, int nrsamples ) const
+{
+    if ( !inputdata ) return false;
+
+    const float shift = t0 - inputdata->t0_;
+    Masker masker( inputdata, mNINT(shift/refstep), 0 );
+    float avg=0;
+    for ( int idx=0; idx<nrsamples; idx++ )
+	avg += masker[idx];
+
+    masker.avg_ = avg / nrsamples;
+    float* outp = output.item(0)->arr();
+    GenericConvolve( hilbfilterlen, -halflen, hilbfilter, nrsamples,
+		     0, masker, nrsamples, 0, outp );
+
+    return true;
+}
+
+
+const Interval<float>* Hilbert::reqZMargin( int inp, int ) const
+{ return &gate; }
+
+
+float* Hilbert::makeHilbFilt( int hlen )
+{
+    float* h = new float[hlen*2+1];
+    h[hlen] = 0;
+    for ( int i=1; i<=hlen; i++ )
+    {
+	const float taper = 0.54 + 0.46 * cos( M_PI*(float)i / (float)(hlen) );
+	h[hlen+i] = taper * ( -(float)(i%2)*2.0 / (M_PI*(float)(i)) );
+	h[hlen-i] = -h[hlen+i];
+    }
+
+    return h;
+}
+
+
+}; // namespace Attrib
+
+
