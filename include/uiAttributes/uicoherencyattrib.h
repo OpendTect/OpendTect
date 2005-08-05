090a4060b14b45d0c7c967b1775c11d09a82f750
Helene Huck <helene.huck@dgbes.com>
2005-08-05 12:20:05 +0000

    linking coherency with the new engine
    git-svn-id: https://svn.opendtect.org/od/trunk@5566 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiAttributes/uicoherencyattrib.h
@@ -0,0 +1,54 @@
+#ifndef uicoherencyattrib_h
+#define uicoherencyattrib_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        A.H. Bril
+ Date:          April 2001
+ RCS:           $Id: uicoherencyattrib.h,v 1.1 2005-08-05 12:20:05 cvshelene Exp $
+________________________________________________________________________
+
+-*/
+
+#include "uiattrdesced.h"
+
+namespace Attrib { class Desc; };
+
+class uiAttrSel;
+class uiImagAttrSel;
+class uiGenInput;
+class uiStepOutSel;
+
+
+/*! \brief Coherency attribute description editor */
+
+class uiCoherencyAttrib : public uiAttrDescEd
+{
+public:
+
+			uiCoherencyAttrib(uiParent*);
+
+protected:
+
+    uiImagAttrSel*	inpfld;
+    uiGenInput*		is1fld;
+    uiGenInput*		tgfld;
+    uiGenInput*		maxdipfld;
+    uiGenInput*		deltadipfld;
+    uiStepOutSel*	stepoutfld;
+
+    void		is1Sel(CallBacker*);
+
+    bool                setParameters(const Attrib::Desc&);
+    bool                setInput(const Attrib::Desc&);
+
+    bool                getParameters(Attrib::Desc&);
+    bool                getInput(Attrib::Desc&);
+    virtual void	set2D(bool);
+
+};
+
+
+#endif
---
+++ src/uiAttributes/uicoherencyattrib.cc
@@ -0,0 +1,104 @@
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        A.H. Bril
+ Date:          April 2001
+________________________________________________________________________
+
+-*/
+
+static const char* rcsID = "$Id: uicoherencyattrib.cc,v 1.1 2005-08-05 12:20:05 cvshelene Exp $";
+
+#include "uicoherencyattrib.h"
+#include "coherencyattrib.h"
+#include "attribdesc.h"
+#include "attribparam.h"
+#include "uiattrsel.h"
+#include "uisteeringsel.h"
+#include "uigeninput.h"
+#include "uistepoutsel.h"
+
+using namespace Attrib;
+
+uiCoherencyAttrib::uiCoherencyAttrib( uiParent* p )
+	: uiAttrDescEd(p)
+{
+    inpfld = getImagInpFld();
+
+    is1fld = new uiGenInput( this, "Type", BoolInpSpec("1","2",false) );
+    is1fld->attach( alignedBelow, inpfld );
+    is1fld->valuechanged.notify( mCB(this,uiCoherencyAttrib,is1Sel) );
+
+    tgfld = new uiGenInput( this, gateLabel(), FloatInpIntervalSpec() );
+    tgfld->attach( alignedBelow, is1fld );
+
+    maxdipfld = new uiGenInput( this, "Maximum dip", FloatInpSpec() );
+    maxdipfld->attach( alignedBelow, tgfld );
+
+    deltadipfld = new uiGenInput( this, "Delta dip", FloatInpSpec() );
+    deltadipfld->attach( alignedBelow, maxdipfld );
+
+    stepoutfld = new uiStepOutSel( this );
+    stepoutfld->attach( alignedBelow, deltadipfld );
+
+    setHAlignObj( tgfld );
+}
+
+
+void uiCoherencyAttrib::set2D( bool yn )
+{
+    inpfld->set2D( yn );
+    stepoutfld->set2D( yn );
+}
+
+
+void uiCoherencyAttrib::is1Sel( CallBacker* )
+{
+    stepoutfld->display( !is1fld->getBoolValue() );
+}
+
+
+bool uiCoherencyAttrib::setParameters( const Attrib::Desc& desc )
+{
+    if ( strcmp(desc.attribName(),Coherency::attribName()) )
+	return false;
+
+    mIfGetInt(Coherency::typeStr(),type,is1fld->setValue(type==1? true:false));
+    mIfGetFloatInterval( Coherency::gateStr(), gate, tgfld->setValue(gate) );
+    mIfGetFloat( Coherency::maxdipStr(),maxdip,maxdipfld->setValue(maxdip) );
+    mIfGetFloat( Coherency::ddipStr(),ddip,deltadipfld->setValue(ddip) );
+    mIfGetBinID(Coherency::stepoutStr(), stepout, stepoutfld->setBinID(stepout))
+
+    is1Sel(0);
+    return true;
+}
+
+
+bool uiCoherencyAttrib::setInput( const Attrib::Desc& desc )
+{
+    putInp( inpfld, desc, 0 );
+    return true;
+}
+
+
+bool uiCoherencyAttrib::getParameters( Attrib::Desc& desc )
+{
+    if ( strcmp(desc.attribName(), Coherency::attribName()) )
+	return false;
+    
+    mSetBool( Coherency::typeStr(), is1fld->getBoolValue() ? 1 : 2 );
+    mSetFloatInterval( Coherency::gateStr(), tgfld->getFInterval() );
+    mSetFloat( Coherency::maxdipStr(), maxdipfld->getfValue() );
+    mSetFloat( Coherency::ddipStr(), deltadipfld->getfValue() );
+    mSetBinID( Coherency::stepoutStr(), stepoutfld->binID() );
+    return true;
+}
+
+
+bool uiCoherencyAttrib::getInput( Desc& desc )
+{
+    inpfld->processInput();
+    fillInp( inpfld, desc, 0 );
+    return true;
+}
