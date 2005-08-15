11a52d607f0506da03f1adf438a59272f69c85a5
Bert Bril <bert.bril@dgbes.com>
2005-08-15 11:27:50 +0000

    added mDefSetupMemb macro
    git-svn-id: https://svn.opendtect.org/od/trunk@5627 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/general.h
+++ include/Basic/general.h
@@ -8,7 +8,7 @@ ________________________________________________________________________
  Author:	A.H. Bril
  Date:		23-10-1996
  Contents:	Extension of genc.h with C++ stuff.
- RCS:		$Id: general.h,v 1.8 2004-10-15 09:43:17 bert Exp $
+ RCS:		$Id: general.h,v 1.9 2005-08-15 11:27:50 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -39,10 +39,44 @@ inline const char* className( const T& t )
 }
 
 
-		//! Defines policy for selection of things
+//! Defines policy for selection of 2D vs 3D seismics
 enum Pol2D	{ No2D=-1, Both2DAnd3D=0, Only2D=1 };
 
+//! Define members in setup classes (see e.g. uidialog.h)
+/* Usage typically like:
 
-#endif /* ifdef cpp */
+ class SomeClass
+ {
+ public:
+ 
+	 class Setup
+	 {
+		 Setup()
+		 : withformat_(true)	//!< Can user select storage?
+		 , withstep_(true)	//!< Can user specify steps?
+		 , title_("")		//!< Title for plots
+				{}
+
+		mDefSetupMemb(bool,withformat)
+		mDefSetupMemb(bool,withstep)
+		mDefSetupMemb(BufferString,title)
+	 }
+	
+	SomeClass(const Setup&);
+	// etc.
+};
+
+The point of this is clear when SomeClass is constructed:
+
+    SomeClass sc( SomeClass::Setup().withformat(false).title("MyTitle") );
+
+ */
+
+#define mDefSetupMemb(typ,memb) \
+	typ	 memb##_; \
+	Setup&	memb( typ val )		{ memb##_ = val; return *this; }
+
+
+#endif
 
 #endif
