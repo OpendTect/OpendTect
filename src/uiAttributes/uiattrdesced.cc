7ee7573e8f41cd41e4889dbd778c52d4a1b3e1d3
Helene Huck <helene.huck@dgbes.com>
2005-10-05 13:09:42 +0000

    add errmsg to prevent the user from giving an input with the wrong data type.
    git-svn-id: https://svn.opendtect.org/od/trunk@6067 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiAttributes/uiattrdesced.cc
+++ src/uiAttributes/uiattrdesced.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        N. Hemstra
  Date:          May 2005
- RCS:		$Id: uiattrdesced.cc,v 1.7 2005-10-05 07:57:59 cvsnanne Exp $
+ RCS:		$Id: uiattrdesced.cc,v 1.8 2005-10-05 13:09:42 cvshelene Exp $
 ________________________________________________________________________
 
 -*/
@@ -57,9 +57,6 @@ void uiAttrDescEd::setDesc( Attrib::Desc* desc, Attrib::DescSetMan* adsm )
 
 void uiAttrDescEd::fillInp( uiAttrSel* fld, Attrib::Desc& desc, int inp )
 {
-    if ( inp >= desc.nrInputs() )
-	return;
-
     fld->processInput();
     const DescID attribid = fld->attribID();
 
@@ -69,7 +66,12 @@ void uiAttrDescEd::fillInp( uiAttrSel* fld, Attrib::Desc& desc, int inp )
     else
 	chtr.setChanged( true );
 
-    desc.setInput( inp, desc.descSet()->getDesc(attribid) );
+    if ( !desc.setInput( inp, desc.descSet()->getDesc(attribid) ) )
+    {
+	errmsg += "The suggested attribute for input"; errmsg += inp;
+	errmsg += " is incompatible with the input (wrong datatype)";
+    }
+    
     mDynamicCastGet(const uiImagAttrSel*,imagfld,fld)
     if ( imagfld )
 	desc.setInput( inp+1, desc.descSet()->getDesc(imagfld->imagID()) );
@@ -78,9 +80,6 @@ void uiAttrDescEd::fillInp( uiAttrSel* fld, Attrib::Desc& desc, int inp )
 
 void uiAttrDescEd::fillInp( uiSteeringSel* fld, Attrib::Desc& desc, int inp )
 {
-    if ( inp >= desc.nrInputs() )
-	return;
-
     const DescID descid = fld->descID();
     const Attrib::Desc* inpdesc = desc.getInput( inp );
     if ( inpdesc )
@@ -88,7 +87,11 @@ void uiAttrDescEd::fillInp( uiSteeringSel* fld, Attrib::Desc& desc, int inp )
     else
 	chtr.setChanged( true );
 
-    desc.setInput( inp, desc.descSet()->getDesc(descid) );
+    if ( !desc.setInput( inp, desc.descSet()->getDesc(descid) ) )
+    {
+	errmsg += "The suggested attribute for input"; errmsg += inp;
+	errmsg += " is incompatible with the input (wrong datatype)";
+    }
 }
 
 
@@ -102,7 +105,11 @@ void uiAttrDescEd::fillInp( uiSteerCubeSel* fld, Attrib::Desc& desc, int inp )
     else
 	chtr.setChanged( true );
 
-    desc.setInput( inp, desc.descSet()->getDesc(inlid) );
+    if ( !desc.setInput( inp, desc.descSet()->getDesc(inlid) ) )
+    {
+	errmsg += "The suggested attribute for input"; errmsg += inp;
+	errmsg += " is incompatible with the input (wrong datatype)";
+    }
 
     const DescID crlid = fld->crlDipID();
     inpdesc = desc.getInput( inp+1 );
@@ -218,7 +225,7 @@ const char* uiAttrDescEd::commit( Attrib::Desc* editdesc )
     editdesc->updateParams();
     getInput( *editdesc );
     getOutput( *editdesc );
-    return 0;
+    return errmsg.size() ? errmsg.buf() : 0;
 }
 
 
