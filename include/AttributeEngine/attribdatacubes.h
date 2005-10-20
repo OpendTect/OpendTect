9efe793158aae4351398640d6a9af33ae53b25e5
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2005-10-20 19:07:49 +0000

    Renamed attribdatacube.h/.cc to attribdatacubes.cc/.h
    git-svn-id: https://svn.opendtect.org/od/trunk@6234 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/AttributeEngine/attribdatacubes.h
@@ -0,0 +1,73 @@
+#ifndef attribdatacubes_h
+#define attribdatacubes_h
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        A.H. Bril
+ Date:          Apr 2002
+ RCS:           $Id: attribdatacubes.h,v 1.1 2005-10-20 19:07:49 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+#include "arrayndimpl.h"
+#include "cubesampling.h"
+#include "refcount.h"
+#include "samplingdata.h"
+
+namespace Attrib
+{
+
+/*!\brief A class for holding cubes with attribute data. All the cubes have
+the same sampling and size.
+ */
+
+class DataCubes
+{ mRefCountImpl(DataCubes);
+public:
+    				DataCubes();
+
+    int				nrCubes() const { return cubes.size(); }
+    void			addCube();
+    void			removeCube(int);
+
+    void			setSize( int nrinl, int nrcrl, int nrz );
+    				/*!<\note that all data will be lost. */
+    void			setValue( int array, int inlidx, int crlidx,
+	    				  int zidx, float val );
+
+    const Array3D<float>&	getCube(int idx) const;
+    				/*!<The attrib data. The data is always
+				    organized with inl as the slowest dim,
+				    and z as the fastest dim. Data can thus
+				    be accessed by:
+				    \code
+				    float val = getCube(idx).get(inlidx,crlidx,
+				    				  zidx);
+				    \endcode */
+
+    SamplingData<int>		inlsampling;
+    SamplingData<int>		crlsampling;
+    int				z0;
+    double			zstep;
+    CubeSampling		cubeSampling() const;
+    				/*!<For convenience. The samling is changed
+				    by setting inlsampling, crlsampling, z0 and
+				    zstep. */
+
+    static int			cInlDim()	{ return 0; }
+    static int			cCrlDim()	{ return 1; }
+    static int			cZDim()		{ return 2; }
+
+protected:
+    ObjectSet<Array3DImpl<float> >	cubes;
+    int					inlsz;
+    int					crlsz;
+    int					zsz;
+};
+
+
+}; //namespace
+
+#endif
---
+++ src/AttributeEngine/attribdatacubes.cc
@@ -0,0 +1,85 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Oct 2005
+-*/
+
+
+static const char* rcsID = "$Id: attribdatacubes.cc,v 1.1 2005-10-20 19:07:49 cvskris Exp $";
+
+#include "attribdatacubes.h"
+#include "survinfo.h"
+
+namespace Attrib
+{
+
+DataCubes::DataCubes()
+    : inlsampling( SI().inlRange(true).start, SI().inlRange(true).step )
+    , crlsampling( SI().crlRange(true).start, SI().crlRange(true).step )
+    , z0( mNINT(SI().zRange(true).start/SI().zRange(true).step) )
+    , zstep( SI().zRange(true).step )
+    , inlsz( 0 )
+    , crlsz( 0 )
+    , zsz( 0 )
+{ mRefCountConstructor; }
+
+
+DataCubes::~DataCubes()
+{ deepErase( cubes ); }
+
+
+void DataCubes::addCube()
+{
+    Array3DImpl<float>* arr = new Array3DImpl<float>( inlsz, crlsz, zsz, false);
+    cubes += arr;
+}
+
+
+void DataCubes::removeCube( int idx )
+{
+    delete cubes[idx];
+    cubes.remove(idx);
+}
+
+
+void  DataCubes::setSize( int nrinl, int nrcrl, int nrz )
+{
+    inlsz = nrinl;
+    crlsz = nrcrl;
+    zsz = nrz;
+
+    for ( int idx=0; idx<cubes.size(); idx++ )
+	cubes[idx]->setSize( inlsz, crlsz, zsz );
+}
+
+
+void DataCubes::setValue( int array, int inlidx, int crlidx, int zidx,
+			  float val )
+{
+    cubes[array]->set( inlidx, crlidx, zidx, val );
+}
+
+
+const Array3D<float>& DataCubes::getCube( int idx ) const
+{ return *cubes[idx]; }
+
+
+CubeSampling DataCubes::cubeSampling() const
+{
+    CubeSampling res(false);
+
+    if ( inlsz && crlsz && zsz )
+    {
+	res.hrg.start = BinID( inlsampling.start, crlsampling.start );
+	res.hrg.stop = BinID( inlsampling.atIndex(inlsz-1),
+	crlsampling.atIndex(crlsz-1) );
+
+	res.zrg.start = z0 * zstep;
+	res.zrg.stop = (z0+crlsz-1) * zstep;
+    }
+
+    return res;
+}
+
+
+}; // namespace Attrib
