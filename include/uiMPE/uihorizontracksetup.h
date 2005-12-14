ff0aba48d11850c351401f703186f53c01f8c9a6
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2005-12-14 18:58:48 +0000

    1. Removed horizontrackdlg from uimpesetup.h/.cc and moved it to    ui uifaulttracksetup.h/.cc under new name. 2. Added uifaulttracksetup.h/.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@6569 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiMPE/uihorizontracksetup.h
@@ -0,0 +1,82 @@
+#ifndef uihorizontracksetup_h
+#define uihorizontracksetup_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        K. Tingdahl
+ Date:          December 2005
+ RCS:           $Id: uihorizontracksetup.h,v 1.1 2005-12-14 18:58:48 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+#include "uimpe.h"
+#include "valseriesevent.h"
+
+
+class uiAttrSel;
+class uiGenInput;
+class uiPushButton;
+
+namespace MPE
+{
+
+
+class HorizonAdjuster;
+
+
+/*!\brief Horizon tracking setup dialog. */
+
+class uiHorizonSetupDialog :  public uiSetupDialog
+{
+public:
+    static void		initClass();
+			/*!<Adds the class to the factory. */
+			~uiHorizonSetupDialog();
+
+    void		enableApplyButton( bool yn );
+    NotifierAccess*	applyButtonPressed();
+
+    bool		commitToTracker() const;
+
+protected:
+
+    uiGenInput*			trackatallfld;
+    uiAttrSel*			inpfld;
+    uiGenInput*			usesimifld;
+    uiGenInput*			thresholdtypefld;
+    uiGenInput*			evfld;
+    uiGenInput*			srchgatefld;
+    uiGenInput*			ampthresholdfld;
+    uiGenInput*			simithresholdfld;
+    uiGenInput*			variancefld;
+    uiGenInput*			compwinfld;
+    uiGenInput*			extriffailfld;
+    uiGroup*			maingrp;
+    uiPushButton*		applybut;
+
+				uiHorizonSetupDialog( uiParent*,SectionTracker*,
+						      const Attrib::DescSet* );
+    void			initWin(CallBacker*);
+    void			selTrackAtAll(CallBacker*);
+    void			selUseSimilarity(CallBacker*);
+    void			selAmpThresholdType(CallBacker*);
+
+    const Attrib::DescSet*	attrset_;
+    SectionTracker*		sectiontracker_;
+    HorizonAdjuster*		horadj_;
+
+    bool			acceptOK(CallBacker*);
+
+    static const char**		sKeyEventNames();
+    static const VSEvent::Type*	cEventTypes();
+    static uiSetupDialog*	create( uiParent*, SectionTracker*,
+				        const Attrib::DescSet* );
+};
+
+
+}; //Namespace
+
+#endif
