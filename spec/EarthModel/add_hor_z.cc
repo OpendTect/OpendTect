4cde84672dcccae69f68da3c42337b786396a36b
cvsdgb <cvsdgb@a985855d-d384-4b45-ae33-31a410998c3c>
2005-12-21 16:39:17 +0000

    added add_hor_z.cc create_randpicks_2d.cc (from spec/EMAttrib)
    git-svn-id: https://svn.opendtect.org/od/trunk@6598 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ spec/EarthModel/add_hor_z.cc
@@ -0,0 +1,95 @@
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        Nanne Hemstra
+ Date:          August 2004
+ RCS:           $Id: add_hor_z.cc,v 1.1 2005-12-21 16:39:17 cvsdgb Exp $
+________________________________________________________________________
+
+-*/
+
+#include "emsurfacegeometry.h"
+#include "emhorizon.h"
+#include "emmanager.h"
+#include "prog.h"
+#include "position.h"
+#include "ioman.h"
+#include "ioobj.h"
+#include "strmprov.h"
+#include "sets.h"
+#include "bufstring.h"
+#include "ptrman.h"
+#include "survinfo.h"
+#include "executor.h"
+#include <iostream>
+
+static int prUsage( const char* msg = 0 )
+{
+    std::cerr << "Usage: Horizon_ID input_XY_file output_XYZ_file [--incudf]";
+    if ( msg ) std::cerr << '\n' << msg;
+    std::cerr << std::endl;
+    return 1;
+}
+
+static int doWork( int argc, char** argv )
+{
+    if ( argc < 4 ) return prUsage();
+    bool incudf = argc > 4 && strcmp(argv[4],"--incudf");
+
+    IOObj* ioobj = IOM().get( argv[1] );
+    if ( !ioobj ) return prUsage( "Horizon_ID not OK" );
+    MultiID ioobjkey( ioobj->key() );
+    delete ioobj;
+
+    EM::EMManager& em = EM::EMM();
+    PtrMan<Executor> exec = em.objectLoader( MultiID(argv[1]) );
+    exec->execute( &std::cerr );
+    EM::EMObject* emobj = em.getObject( em.getObjectID(ioobjkey) );
+    mDynamicCastGet(EM::Horizon*,horizon,emobj)
+    if ( !horizon ) return prUsage( "ID is not horizon" );
+
+    StreamData sd = StreamProvider(argv[2]).makeIStream();
+    if ( !sd.usable() ) return prUsage( "input_XY_file not OK" );
+
+    Coord c0;
+    TypeSet<Coord> coords;
+    while ( sd.istrm->good() )
+    {
+	Coord c;
+	*sd.istrm >> c.x >> c.y;
+	double sqdist = c.sqDistance(c0);
+	if ( mIsZero(sqdist,1e-4) )
+	    break;
+	coords += c;
+    }
+    sd.close();
+    if ( coords.size() < 1 ) return prUsage( "No valid coordinates found" );
+
+    sd = StreamProvider(argv[3]).makeOStream();
+    if ( !sd.usable() ) return prUsage( "output_XYZ_file not OK" );
+
+    for ( int idx=0; idx<coords.size(); idx++ )
+    {
+	Coord c( coords[idx] );
+	const BinID bid = SI().transform( c );
+	TypeSet<Coord3> positions;
+	horizon->geometry.getPos( bid, positions );
+	bool isudf = !positions[0].isDefined();
+	if ( isudf && !incudf ) continue;
+
+	BufferString str;
+	str += c.x; str += "\t"; str += c.y; str += "\t";
+	if ( isudf )	str += sUndefValue;
+	else		str += positions[0].z;
+	*sd.ostrm << str << std::endl;
+    }
+
+    return 0;
+}
+
+
+int main( int argc, char** argv )
+{
+    return ExitProgram( doWork(argc,argv) );
+}
---
+++ spec/EarthModel/create_randpicks_2d.cc
@@ -0,0 +1,121 @@
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        Nanne Hemstra
+ Date:          October 2001
+ RCS:           $Id: create_randpicks_2d.cc,v 1.1 2005-12-21 16:39:17 cvsdgb Exp $
+________________________________________________________________________
+
+-*/
+
+#include "prog.h"
+
+#include "conn.h"
+#include "emhorizon.h"
+#include "emmanager.h"
+#include "emsurfacegeometry.h"
+#include "errh.h"
+#include "executor.h"
+#include "ioobj.h"
+#include "ioman.h"
+#include "ptrman.h"
+#include "segposinfo.h"
+#include "seis2dline.h"
+#include "stats.h"
+#include "strmprov.h"
+#include "survinfo.h"
+
+#include <math.h>
+
+
+#define mErrRet(err) { std::cerr << err << std::endl; return 1; }
+
+static int doWork( int argc, char** argv )
+{
+    if ( argc < 3 )
+    {
+	std::cerr << "Usage: " << argv[0] << " lineset_id horizon_id nrpicks "
+	    	     "xyz_filename " << std::endl;
+	return 1;
+    }
+
+    const int nrpicks = atoi( argv[3] );
+    BufferString fname( argv[4] );
+    StreamData outsd = StreamProvider( fname ).makeOStream();
+    if ( !outsd.usable() )
+	mErrRet( "Cannot open output file" );
+
+    PtrMan<IOObj> ioobj = IOM().get( argv[1] );
+    if ( !ioobj ) mErrRet( "Lineset ID not OK" );
+    PtrMan<Seis2DLineSet> lset = new Seis2DLineSet( ioobj->fullUserExpr(true) );
+    if ( !lset )
+	mErrRet( "Cannot find input lineset" );
+    if ( lset->nrLines()==0 )
+	mErrRet( "Input lineset is empty" );
+
+    PtrMan<IOObj> horioobj = IOM().get( argv[2] );
+    if ( !horioobj ) mErrRet( "Horizon ID not OK" );
+    EM::EMManager& em = EM::EMM();
+    PtrMan<Executor> exec = em.objectLoader( horioobj->key() );
+    exec->execute( &std::cerr );
+    EM::EMObject* emobj = em.getObject( em.getObjectID(horioobj->key()) );
+    mDynamicCastGet(EM::Horizon*,horizon,emobj)
+    if ( !horizon ) mErrRet( "ID is not horizon" );
+
+//  Get 2D geometry
+    ObjectSet<PosInfo::Line2DData> geoms;
+    for ( int lineidx=0; lineidx<lset->nrLines(); lineidx++ )
+    {
+	PosInfo::Line2DData* geometry = new PosInfo::Line2DData;
+	if ( !lset->getGeometry(lineidx,*geometry) )
+	{
+	    delete geometry;
+	    continue;
+	}
+
+	geoms += geometry;
+    }
+
+    TypeSet<Coord3> picklocations;
+    const int nrlines = geoms.size();
+    int nrpicksadded = 0;
+    while ( true )
+    {
+	const int lineidx = Stat_getIndex( nrlines );
+	PosInfo::Line2DData& geometry = *geoms[lineidx];
+	const int nrcoords = geometry.posns.size();
+	const int crdidx = Stat_getIndex( nrcoords );
+	const Coord& pos = geometry.posns[crdidx].coord;
+	const BinID bid = SI().transform( pos );
+	TypeSet<Coord3> positions;
+	horizon->geometry.getPos( bid, positions );
+	if ( !positions.size() || !positions[0].isDefined() )
+	    continue;
+
+	const Coord3 pickpos( pos, positions[0].z );
+	if ( picklocations.indexOf(pickpos) < 0 )
+	    picklocations += pickpos;
+
+	if ( picklocations.size() == nrpicks )
+	    break;
+    }
+
+    for ( int idx=0; idx<picklocations.size(); idx++ )
+    {
+	const Coord3& pos = picklocations[idx];
+	*outsd.ostrm << pos.x << ' ' << pos.y << ' ' << pos.z << '\n';
+    }
+
+    outsd.close();
+    std::cerr << nrpicks << " positions written to " << fname.buf() 
+	      << std::endl;
+
+    return 0;
+}
+
+
+int main( int argc, char** argv )
+{
+    return ExitProgram( doWork(argc,argv) );
+}
