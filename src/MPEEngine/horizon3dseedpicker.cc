bca6f08d47d0698799bed92bdad5f0bb7a2ba18f
Jaap Glas <jaap.glas@dgbes.com>
2006-02-27 11:07:15 +0000

    First implementation of the new seed picking modes.
    git-svn-id: https://svn.opendtect.org/od/trunk@6900 a985855d-d384-4b45-ae33-31a410998c3c

--- src/MPEEngine/horizon3dseedpicker.cc
+++ src/MPEEngine/horizon3dseedpicker.cc
@@ -8,15 +8,15 @@ ___________________________________________________________________
 
 -*/
 
-static const char* rcsID = "$Id: horizon3dseedpicker.cc,v 1.2 2006-01-25 14:49:53 cvsjaap Exp $";
+static const char* rcsID = "$Id: horizon3dseedpicker.cc,v 1.3 2006-02-27 11:07:15 cvsjaap Exp $";
 
 #include "horizonseedpicker.h"
 
 #include "autotracker.h"
 #include "emhistory.h"
 #include "emhorizon.h"
-//#include "emfault.h"
 #include "emmanager.h"
+#include "sectiontracker.h"
 #include "executor.h"
 #include "mpeengine.h"
 #include "survinfo.h"
@@ -28,30 +28,31 @@ namespace MPE
 {
 
 HorizonSeedPicker::HorizonSeedPicker( MPE::EMTracker& t )
-    : tracker( t )
-    , firsthistorynr( mInitialEventNrNotSet )
-    , interpolmode( false )  
+    : tracker_( t )
+    , firsthistorynr_( mInitialEventNrNotSet )
+    , seedmode_( TrackFromSeeds )
+    , frozen_( false )
 {}
 
 
 bool HorizonSeedPicker::setSectionID( const EM::SectionID& sid )
-{ sectionid = sid; return true; }
+{ sectionid_ = sid; return true; }
 
 
 #define mGetHorizon(hor) \
-    const EM::ObjectID emobjid = tracker.objectID(); \
+    const EM::ObjectID emobjid = tracker_.objectID(); \
     mDynamicCastGet( EM::Horizon*, hor, EM::EMM().getObject(emobjid) ); \
     if ( !hor ) \
 	return false;\
 
 bool HorizonSeedPicker::startSeedPick()
 {
-    firsthistorynr =  EM::EMM().history().currentEventNr();
-    seedlist.erase();
-    seedpos.erase();
+    firsthistorynr_ =  EM::EMM().history().currentEventNr();
+    seedlist_.erase();
+    seedpos_.erase();
 
     mGetHorizon(hor);
-    didchecksupport = hor->geometry.checkSupport(false);
+    didchecksupport_ = hor->geometry.checkSupport(false);
 
     return true;
 }
@@ -60,26 +61,29 @@ bool HorizonSeedPicker::startSeedPick()
 	
 bool HorizonSeedPicker::addSeed(const Coord3& seedcrd3 )
 {
+    if ( frozen_ ) return true;
+
     BinID seedbid = SI().transform(seedcrd3);
     const HorSampling hrg = engine().activeVolume().hrg;
     const StepInterval<float> zrg = engine().activeVolume().zrg;
+
     if ( !zrg.includes(seedcrd3.z) || !hrg.includes(seedbid) )
 	return false;
 
     // Set seed positions
-    const EM::ObjectID emobjid = tracker.objectID();
+    const EM::ObjectID emobjid = tracker_.objectID();
     EM::EMObject* emobj = EM::EMM().getObject(emobjid);
-    const EM::PosID pid( emobj->id(), sectionid, seedbid.getSerialized() );
+    const EM::PosID pid( emobj->id(), sectionid_, seedbid.getSerialized() );
 
-    const int idx = seedlist.indexOf(pid);
+    const int idx = seedlist_.indexOf(pid);
     if ( idx==-1 )
     {
-	seedlist += pid;
-	seedpos += seedcrd3;
+	seedlist_ += pid;
+	seedpos_ += seedcrd3;
     }
     else
     {
-	seedpos[idx] = seedcrd3;
+	seedpos_[idx] = seedcrd3;
     }
 
     return reTrack();
@@ -88,11 +92,11 @@ bool HorizonSeedPicker::addSeed(const Coord3& seedcrd3 )
 
 bool HorizonSeedPicker::removeSeed( const EM::PosID& pid)
 {
-    int idx = seedlist.indexOf(pid);
-    if ( idx == -1 )	return false;
+    int idx = seedlist_.indexOf(pid);
+    if ( idx == -1 ) return false;
     
-    seedlist.remove(idx);
-    seedpos.remove(idx);
+    seedlist_.remove(idx);
+    seedpos_.remove(idx);
 
     return reTrack();
 }
@@ -101,12 +105,14 @@ bool HorizonSeedPicker::removeSeed( const EM::PosID& pid)
 bool HorizonSeedPicker::reTrack()
 {
     removeEverythingButSeeds();
-    if ( !seedlist.size() )
+    if ( !seedlist_.size() )
 	return true;
 
-    if ( interpolmode )
+    if ( frozen_ ) return true;
+   
+    if ( seedmode_ == DrawBetweenSeeds )
 	return interpolateSeeds();
-    
+   
     const TrackPlane::TrackMode tm = engine().trackPlane().getTrackMode();
     engine().setTrackMode( TrackPlane::Extend );
 
@@ -129,8 +135,11 @@ bool HorizonSeedPicker::reTrack()
 	    mDynamicCastGet( AutoTracker*, autotrk, exec );
 	    if ( !autotrk )	break;
 
-	    autotrk->setNewSeeds( seedlist );
+	    if ( seedmode_ == TrackBetweenSeeds )
+		autotrk->setTrackBoundary( getSeedBox() );
+	    autotrk->setNewSeeds( seedlist_ );
 	    autotrk->execute();
+	    autotrk->unsetTrackBoundary();
 	}
     }
 
@@ -142,19 +151,28 @@ bool HorizonSeedPicker::reTrack()
 }
 
 
+int HorizonSeedPicker::nrSeeds() const
+{ return seedpos_.size(); }
+
+
+int HorizonSeedPicker::isMinimumNrOfSeeds() const
+{ return getSeedMode()==TrackFromSeeds ? 1 : 0 ; }
+
+
+
 bool HorizonSeedPicker::removeEverythingButSeeds()
 {
-    if ( firsthistorynr != mInitialEventNrNotSet )
+    if ( firsthistorynr_ != mInitialEventNrNotSet )
 	EM::EMM().history().unDo(
-	    EM::EMM().history().currentEventNr() - firsthistorynr );
+	    EM::EMM().history().currentEventNr() - firsthistorynr_ );
 
-    const EM::ObjectID emobjid = tracker.objectID();
+    const EM::ObjectID emobjid = tracker_.objectID();
     EM::EMObject* emobj = EM::EMM().getObject(emobjid);
 
-    for ( int idx=0; idx<seedlist.size(); idx++ )
+    for ( int idx=0; idx<seedlist_.size(); idx++ )
     {
-	emobj->setPos( seedlist[idx], seedpos[idx], true );
-	emobj->setPosAttrib( seedlist[idx], EM::EMObject::sSeedNode, true );
+	emobj->setPos( seedlist_[idx], seedpos_[idx], true );
+	emobj->setPosAttrib( seedlist_[idx], EM::EMObject::sSeedNode, true );
     }
 
     return true;
@@ -165,27 +183,26 @@ bool HorizonSeedPicker::stopSeedPick( bool iscancel )
 {
     if ( iscancel )
     {
-	if ( firsthistorynr != mInitialEventNrNotSet )
+	if ( firsthistorynr_ != mInitialEventNrNotSet )
 	    EM::EMM().history().unDo(
-		EM::EMM().history().currentEventNr() - firsthistorynr );
-	EM::EMObject* emobj = EM::EMM().getObject(tracker.objectID());
-	for ( int idx=0; idx<seedlist.size(); idx++ )
-	    emobj->setPosAttrib(seedlist[idx], EM::EMObject::sSeedNode, false);
+		EM::EMM().history().currentEventNr() - firsthistorynr_ );
+	EM::EMObject* emobj = EM::EMM().getObject(tracker_.objectID());
+	for ( int idx=0; idx<seedlist_.size(); idx++ )
+	    emobj->setPosAttrib(seedlist_[idx], EM::EMObject::sSeedNode, false);
     }
     mGetHorizon(hor);
-    hor->geometry.checkSupport( didchecksupport );
-    seedlist.erase();
-    seedpos.erase();
-    firsthistorynr = mInitialEventNrNotSet;
+    hor->geometry.checkSupport( didchecksupport_ );
+    seedlist_.erase();
+    seedpos_.erase();
+    firsthistorynr_ = mInitialEventNrNotSet;
     return true;
 }
-
 
 #define mSortSeeds(rorc) \
 { \
     for ( int idx=0; idx<nrseeds; idx++) \
     { \
-	rorc##value[idx] = SI().transform( seedpos[idx] ).rorc(); \
+	rorc##value[idx] = SI().transform( seedpos_[idx] ).rorc(); \
 	rorc##index[idx] = idx; \
     } \
     sort_coupled( rorc##value, rorc##index, nrseeds ); \
@@ -196,20 +213,19 @@ bool HorizonSeedPicker::stopSeedPick( bool iscancel )
 { \
     mGetHorizon(horptr); \
     EM::EMObject* emobj = EM::EMM().getObject(emobjid); \
-    const RowCol step = horptr->geometry.step(); \
+    const int step = horptr->geometry.step().rorc(); \
     \
     for ( int vtx=0; vtx<nrseeds-1; vtx++ ) \
     { \
 	const int diff = rorc##value[vtx+1] - rorc##value[vtx]; \
-	for ( int idx=step.rorc(); idx<diff; idx+=step.rorc() ) \
+	for ( int idx=step; idx<diff; idx+=step ) \
 	{ \
 	    const double frac = (double) idx / diff; \
-	    const Coord3 interpos = (1-frac) * seedpos[ rorc##index[vtx] ]  \
-				    + frac * seedpos[ rorc##index[vtx+1] ]; \
-	    const EM::PosID interpid( emobj->id(), sectionid, \
+	    const Coord3 interpos = (1-frac) * seedpos_[ rorc##index[vtx] ]  \
+				    + frac * seedpos_[ rorc##index[vtx+1] ]; \
+	    const EM::PosID interpid( emobj->id(), sectionid_, \
 				SI().transform(interpos).getSerialized() ); \
 	    emobj->setPos( interpid, interpos, true ); \
-	    emobj->setPosAttrib( interpid, EM::EMObject::sSeedNode, true ); \
 	} \
     } \
 }
@@ -233,6 +249,20 @@ bool HorizonSeedPicker::interpolateSeeds()
 
     return true;
 }
+
+
+CubeSampling HorizonSeedPicker::getSeedBox() const
+{
+    CubeSampling seedbox( true );
+    seedbox.hrg.init( false );
+    for ( int idx=0; idx<nrSeeds(); idx++ )
+    {
+	BinID seedbid = SI().transform( seedpos_[idx] );
+	seedbox.hrg.include( seedbid );
+    }
+    return seedbox;
+}
+
 
 }; // namespace MPE
 
