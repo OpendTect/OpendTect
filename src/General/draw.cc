a162c1c4e5927e4e2c5a37c30afe2ec3565601f7
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2006-03-01 19:51:57 +0000

    Added MarkerStyle3D::Cross
    git-svn-id: https://svn.opendtect.org/od/trunk@6930 a985855d-d384-4b45-ae33-31a410998c3c

--- src/General/draw.cc
+++ src/General/draw.cc
@@ -4,7 +4,7 @@
  * DATE     : 18-4-1996
 -*/
 
-static const char* rcsID = "$Id: draw.cc,v 1.47 2006-02-20 18:49:49 cvsbert Exp $";
+static const char* rcsID = "$Id: draw.cc,v 1.48 2006-03-01 19:51:57 cvskris Exp $";
 
 /*! \brief Several implementations for UI-related things.
 
@@ -32,7 +32,7 @@ The main chunk is color table related.
 DefineEnumNames(MarkerStyle2D,Type,2,"Marker type")
     { "None", "Square", "Circle", "Cross", 0 };
 DefineEnumNames(MarkerStyle3D,Type,0,"Marker type")
-    { "Cube", "Cone", "Cylinder", "Sphere", "Arrow", 0 };
+    { "Cube", "Cone", "Cylinder", "Sphere", "Arrow", "Cross", 0 };
 DefineEnumNames(LineStyle,Type,0,"Line style")
     { "None", "Solid", "Dashed", "Dotted", "Dash-Dotted", "Dash-Dot-Dotted",0 };
 DefineEnumNames(Alignment,Pos,3,"Alignment position")
