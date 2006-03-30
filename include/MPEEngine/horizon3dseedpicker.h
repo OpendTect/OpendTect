f7603dd1bfb719fdff817afe31d1b2a6fc6c1d0b
Jaap Glas <jaap.glas@dgbes.com>
2006-03-30 15:58:21 +0000

    Redesign of many horizon seedpicker functions.
    git-svn-id: https://svn.opendtect.org/od/trunk@7097 a985855d-d384-4b45-ae33-31a410998c3c

--- include/MPEEngine/horizon3dseedpicker.h
+++ include/MPEEngine/horizon3dseedpicker.h
@@ -8,7 +8,7 @@ ________________________________________________________________________
  Author:        A.H. Bril
  Date:          23-10-1996
  Contents:      Ranges
- RCS:           $Id: horizon3dseedpicker.h,v 1.7 2006-03-21 10:30:13 cvsjaap Exp $
+ RCS:           $Id: horizon3dseedpicker.h,v 1.8 2006-03-30 15:58:21 cvsjaap Exp $
 ________________________________________________________________________
 
 -*/
@@ -21,16 +21,17 @@ namespace MPE
 class HorizonSeedPicker : public EMSeedPicker
 {
 public:
-    			HorizonSeedPicker( MPE::EMTracker& );
+    			HorizonSeedPicker(MPE::EMTracker&);
 
     bool		canSetSectionID() const		{ return true; }
-    bool		setSectionID( const EM::SectionID& sid );
+    bool		setSectionID(const EM::SectionID&);
     EM::SectionID	getSectionID() const		{ return sectionid_; }
 
     bool		startSeedPick();
-    bool		addSeed( const Coord3& );
+    bool		addSeed(const Coord3&);
     bool		canAddSeed() const		{ return true; }
-    bool		removeSeed( const EM::PosID& );
+    bool		removeSeed(const EM::PosID&,
+	    			   bool retrack);
     bool		canRemoveSeed() const		{ return true; }
     bool		reTrack();
     int			nrSeeds() const;
@@ -38,39 +39,37 @@ public:
 
     bool		stopSeedPick(bool iscancel=false);
 
-    enum SeedModeOrder	{ TrackFromSeeds, TrackBetweenSeeds, DrawBetweenSeeds };
-    int			getSeedMode() const		{ return seedmode_; }
-    void		setSeedMode( int sm )		{ seedmode_ = sm; }
-    void		setLocalErase( bool yn )	{ localerase_ = yn; }
-    void		freezeMode( bool yn )		{ frozen_ = yn; }
-    bool		isModeFrozen() const		{ return frozen_; }
-    bool		isInVolumeMode() const;
-    bool		isInDrawMode() const;
+    enum SeedModeOrder	{TrackFromSeeds,TrackBetweenSeeds,DrawBetweenSeeds};
+
+    int			getSeedConnectMode() const	{ return seedconmode_; }
+    void		setSeedConnectMode(int scm)	{ seedconmode_ = scm; }
+    void		blockSeedPick(bool yn)		{ blockpicking_ = yn; }
+    bool		isSeedPickBlocked() const	{ return blockpicking_;}
+    bool		doesModeUseVolume() const;
+    bool		doesModeUseSetup() const;
 
 protected:
-    void		eraseFromSeed( const BinID& startbid, bool upwards );
-    void		checkJunctions();
-    bool		retrackActiveLine();
-    void		repairDisconnections();
-    bool		clearActiveLine();
+    bool		retrackOnActiveLine( BinID startbid, 
+					     bool startwasdefined,
+					     bool eraseonly=false);
+    bool		retrackFromSeedList();
+    int			nrLateralConnections( const BinID& ) const;
     bool		interpolateSeeds();
-    BinID		activeLineStep() const;
     CubeSampling	getSeedBox() const;
 
     TypeSet<EM::PosID>	seedlist_;
     TypeSet<Coord3>	seedpos_;
-    TypeSet<EM::PosID>	junclist_;
-    TypeSet<EM::PosID>	crosspid_;
-    TypeSet<Coord3>	crosspos_;
-    int			firsthistorynr_;
     bool		didchecksupport_;
-
     EM::SectionID	sectionid_;
     MPE::EMTracker&	tracker_;
 
-    int			seedmode_;
-    bool		localerase_;
-    bool		frozen_;
+    int			seedconmode_;
+    bool		blockpicking_;
+private: 
+    void		extendSeedListEraseInBetween( 
+			    bool wholeline,const BinID& startbid,
+			    bool startwasdefined,const BinID& dir,
+			    TypeSet<EM::PosID>& candidatejunctionpairs);
 };
 
 
