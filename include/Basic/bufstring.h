b061f538d17446f8c0fac906e9fe8c5cb76a0ba1
Nanne Hemstra <nanne.hemstra@dgbes.com>
2006-04-24 12:07:57 +0000

    take min size when memcopying buffers
    git-svn-id: https://svn.opendtect.org/od/trunk@7195 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/bufstring.h
+++ include/Basic/bufstring.h
@@ -8,7 +8,7 @@ ________________________________________________________________________
  Author:	A.H.Bril
  Date:		12-4-2000
  Contents:	Variable buffer length strings with minimum size.
- RCS:		$Id: bufstring.h,v 1.19 2006-03-22 12:56:29 cvshelene Exp $
+ RCS:		$Id: bufstring.h,v 1.20 2006-04-24 12:07:57 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -124,7 +124,7 @@ inline void BufferString::setBufSize( unsigned int newlen )
 
     char* oldbuf = buf_;
     buf_ = new char [newlen];
-    memcpy( buf_, oldbuf, newlen );
+    memcpy( buf_, oldbuf, mMIN(newlen,strlen(oldbuf)) );
     delete [] oldbuf;
 
     len = newlen;
