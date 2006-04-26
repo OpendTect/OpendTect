e8d3c28724535917b5f5165f9557559255c71e7a
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2006-04-26 21:10:42 +0000

    Added rowcolsurface.cc and horizon2dline.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@7225 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/Geometry/horizon2dline.cc
@@ -0,0 +1,160 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Dec 2004
+-*/
+
+static const char* rcsID = "$Id: horizon2dline.cc,v 1.1 2006-04-26 21:10:42 cvskris Exp $";
+
+#include "horizon2dline.h"
+#include "undefval.h"
+
+namespace Geometry
+{
+
+Horizon2DLine::Horizon2DLine()
+    : firstrow_( 0 )
+{}
+
+
+Horizon2DLine::Horizon2DLine( const TypeSet<Coord>& path, int start, int step )
+    : firstrow_( 0 )
+{
+    addRow( path, start, step );
+}
+
+
+Horizon2DLine::Horizon2DLine( const Horizon2DLine& ln )
+    : firstrow_( ln.firstrow_ )
+    , colsampling_( ln.colsampling_ )
+{
+    deepCopy( rows_, ln.rows_ );
+}
+
+
+Horizon2DLine::~Horizon2DLine()
+{
+    deepErase( rows_ );
+}
+
+
+Horizon2DLine* Horizon2DLine::clone() const
+{ return new Horizon2DLine( *this ); }
+
+
+int Horizon2DLine::addRow(const TypeSet<Coord>& coords, int start, int step )
+{
+    const int res = rows_.size()+firstrow_;
+    rows_ += new TypeSet<Coord3>;
+    colsampling_ += SamplingData<int>( start, step );
+    setRow( res, coords, start, step );
+    return res;
+}
+
+
+void Horizon2DLine::removeRow( int id )
+{
+    const int rowidx = id-firstrow_;
+    if ( rowidx<0 )
+	return;
+
+    delete rows_[rowidx];
+    rows_.removeFast( rowidx );
+    colsampling_.removeFast( rowidx );
+    if ( !rowidx )
+	firstrow_++;
+    else
+    {
+	//TODO notify
+    }
+}
+
+
+void Horizon2DLine::setRow( int id,
+		const TypeSet<Coord>& path, int start, int step )
+{
+    const int rowidx = id-firstrow_;
+    if ( rowidx<0 )
+	return;
+
+    const bool samedimensions =
+	path.size()==rows_[rowidx]->size() &&
+	start==colsampling_[rowidx].start && step==colsampling_[rowidx].step;
+
+    rows_[rowidx]->setSize( path.size(), Coord3::udf() );
+
+    for ( int idx=path.size()-1; idx>=0; idx-- )
+	(*rows_[rowidx])[idx] = Coord3( path[idx], mUdf(float) );
+
+    if ( samedimensions )
+	triggerMovement();
+    else
+    {
+	colsampling_[rowidx].start = start; colsampling_[rowidx].step = step;
+	triggerNrPosCh();
+    }
+}
+
+
+StepInterval<int> Horizon2DLine::rowRange() const
+{ return StepInterval<int>( firstrow_, firstrow_+rows_.size()-1, 1 ); }
+
+
+StepInterval<int> Horizon2DLine::colRange( int rowid ) const
+{
+    const int rowidx = rowid-firstrow_;
+    if ( rowidx<0 )
+	return StepInterval<int>(INT_MAX,INT_MIN, 1 );
+
+    const SamplingData<int>& sd = colsampling_[rowidx];
+    return StepInterval<int>( sd.start, sd.atIndex(rows_[rowidx]->size()-1),
+	    		      sd.step );
+}
+    
+
+Coord3 Horizon2DLine::getKnot( const RCol& rc ) const
+{
+    const int rowidx = rc.r()-firstrow_;
+    const int colidx = colIndex( rowidx, rc.c() );
+    return colidx>=0 ? (*rows_[rowidx])[colidx] : Coord3::udf();
+}
+
+
+bool Horizon2DLine::setKnot( const RCol& rc, const Coord3& c )
+{
+    const int rowidx = rc.r()-firstrow_;
+    const int colidx = colIndex( rowidx, rc.c() );
+
+    if ( colidx<0 ) return false;
+
+    (*rows_[rowidx])[colidx] = c;
+
+    return true;
+}
+
+
+bool Horizon2DLine::isKnotDefined( const RCol& rc ) const
+{
+    const int rowidx = rc.r()-firstrow_;
+    const int colidx = colIndex( rowidx, rc.c() );
+    return colidx>=0 ? (*rows_[rowidx])[colidx].isDefined() : false;
+}
+
+
+int Horizon2DLine::colIndex( int rowidx, int colid ) const
+{
+    const SamplingData<int>& sd = colsampling_[rowidx];
+    int idx = colid-sd.start;
+    if ( idx<0 || idx%sd.step )
+	return -1;
+
+    idx /= sd.step;
+    if ( idx>=rows_[rowidx]->size() )
+	return -1;
+
+    return idx;
+}
+
+
+}; //Namespace
+
