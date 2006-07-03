6fbbc616e58fbe0bd207e498a117171a2beda031
Bert Bril <bert.bril@dgbes.com>
2006-07-03 16:39:33 +0000

    support storing source filename
    git-svn-id: https://svn.opendtect.org/od/trunk@7676 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiBase/pixmap.cc
+++ src/uiBase/pixmap.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H. Lammertink
  Date:          08/12/1999
- RCS:           $Id: pixmap.cc,v 1.13 2006-05-02 19:40:00 cvsnanne Exp $
+ RCS:           $Id: pixmap.cc,v 1.14 2006-07-03 16:39:33 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -17,6 +17,7 @@ ________________________________________________________________________
 #include "arrayndimpl.h"
 #include "arrayrgb.h"
 #include "colortab.h"
+#include "separstr.h"
 
 #include <qpixmap.h>
 #include <qbitmap.h>
@@ -24,39 +25,60 @@ ________________________________________________________________________
 
 
 ioPixmap::ioPixmap( const ioPixmap& pm )
+    : qpixmap_(new QPixmap(*pm.qpixmap_))
+    , srcname_(pm.srcname_)
 {
-    qpixmap = new QPixmap( *pm.qpixmap );
 }
 
 
 ioPixmap::ioPixmap( const ArrayRGB& anImage )
+    : qpixmap_(new QPixmap)
+    , srcname_("[ArrayRGB]")
 {
-    qpixmap = new QPixmap;
     convertFromArrayRGB( anImage );
 }
 
 
 ioPixmap::ioPixmap( const char* xpm[] )
-    { qpixmap = new QPixmap( xpm ); }
+    : qpixmap_(new QPixmap(xpm))
+    , srcname_("[xpm]")
+{
+}
 
 
 ioPixmap::ioPixmap( int w, int h )
-    { qpixmap = new QPixmap( w, h ); }
+    : qpixmap_(new QPixmap(w,h))
+    , srcname_("[created]")
+{
+}
 
 
 ioPixmap::ioPixmap( const QPixmap& pm )
-    { qpixmap = new QPixmap( pm ); }
+    : qpixmap_(new QPixmap(pm))
+{
+}
 
 
-ioPixmap::ioPixmap( const char* fileName, const char * format )
-    { qpixmap = new QPixmap( fileName, format ); }
+ioPixmap::ioPixmap( const char* filenm, const char* fmt )
+    : qpixmap_(new QPixmap(filenm,fmt))
+    , srcname_(filenm)
+{
+    if ( fmt )
+    {
+	FileMultiString fms( filenm );
+	fms += fmt;
+	srcname_ = fms;
+    }
+}
 
     
 ioPixmap::ioPixmap( const char* tablename, int width, int height )
+    : qpixmap_(0)
+    , srcname_("[colortable]")
 {
     if ( !tablename || !*tablename )
     {
-	qpixmap = new QPixmap( width, height );
+	qpixmap_ = new QPixmap( width, height );
 	fill( Color(255,255,255) );
 	return;
     }
@@ -74,51 +96,57 @@ ioPixmap::ioPixmap( const char* tablename, int width, int height )
 	for ( int idx2=0; idx2<rgb.info().getSize(0); idx2++ )
 	    rgb.set( idx2, idx1, color );
     }
-    qpixmap = new QPixmap;
+    qpixmap_ = new QPixmap;
     convertFromArrayRGB( rgb );
 }
 
 
 ioPixmap::~ioPixmap()
-    { if ( qpixmap ) delete qpixmap; }
+{
+    if ( qpixmap_ )
+	delete qpixmap_;
+}
 
 
-void ioPixmap::convertFromArrayRGB( const ArrayRGB & theImage )
+void ioPixmap::convertFromArrayRGB( const ArrayRGB& theImage )
 {
     releaseDrawTool();
 
-    if( !qpixmap ) qpixmap = new QPixmap;
+    if( !qpixmap_ ) qpixmap_ = new QPixmap;
 #ifdef USEQT4
-    *qpixmap = QPixmap::fromImage( theImage.Image(), Qt::OrderedAlphaDither);
+    *qpixmap_ = QPixmap::fromImage( theImage.Image(), Qt::OrderedAlphaDither);
 #else
-    qpixmap->convertFromImage( theImage.Image(), Qt::OrderedAlphaDither);
+    qpixmap_->convertFromImage( theImage.Image(), Qt::OrderedAlphaDither);
 #endif
 }    
 
 
 QPaintDevice* ioPixmap::mQPaintDevice()
-    { return qpixmap; }
+{
+    return qpixmap_;
+}
 
 
 int ioPixmap::width()
 {
-    return qpixmap->width();
+    return qpixmap_->width();
 }
 
 
 int ioPixmap::height()
 {
-    return qpixmap->height();
+    return qpixmap_->height();
 }
 
 
 void ioPixmap::fill( const Color& col )
 {
-    qpixmap->fill( QColor(col.r(),col.g(),col.b()) );
+    qpixmap_->fill( QColor(col.r(),col.g(),col.b()) );
 }
 
 
-ioBitmap::ioBitmap( const char* fileName, const char * format )
-    { qpixmap = new QBitmap( fileName, format ); }
-
-
+ioBitmap::ioBitmap( const char* filenm, const char * format )
+{
+    qpixmap_ = new QBitmap( filenm, format );
+    srcname_ = filenm;
+}
