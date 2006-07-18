2d4e4ecea845ad5a25454f2e542c2d8ce8ccd6ea
Jaap Glas <jaap.glas@dgbes.com>
2006-07-18 12:06:16 +0000

    Definition of function lineTrackDirection() which enables tracking/drawing in right direction not only when seedpicking on inl/crl but on tracker plane too.
    git-svn-id: https://svn.opendtect.org/od/trunk@7756 a985855d-d384-4b45-ae33-31a410998c3c

--- include/MPEEngine/horizon3dseedpicker.h
+++ include/MPEEngine/horizon3dseedpicker.h
@@ -8,7 +8,7 @@ ________________________________________________________________________
  Author:        A.H. Bril
  Date:          23-10-1996
  Contents:      Ranges
- RCS:           $Id: horizon3dseedpicker.h,v 1.14 2006-06-26 07:49:48 cvsjaap Exp $
+ RCS:           $Id: horizon3dseedpicker.h,v 1.15 2006-07-18 12:06:16 cvsjaap Exp $
 ________________________________________________________________________
 
 -*/
@@ -65,6 +65,7 @@ protected:
 					     bool startwasdefined,
 					     bool eraseonly=false);
     bool		retrackFromSeedList();
+    bool		lineTrackDirection(BinID& dir) const;
     int 		nrLateralNeighbors(const EM::PosID& pid) const;
     int 		nrLineNeighbors(const EM::PosID& pid,
 	    				bool pardir=true) const;
