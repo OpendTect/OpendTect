2e699347c59d9d4bcb28e81d9aea845b1d721afe
Nanne Hemstra <nanne.hemstra@dgbes.com>
2006-07-18 08:37:19 +0000

    define M_PI stuff if not already defined
    git-svn-id: https://svn.opendtect.org/od/trunk@7745 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/commondefs.h
+++ include/Basic/commondefs.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	A.H.Bril
  Date:		Mar 2006
- RCS:		$Id: commondefs.h,v 1.4 2006-07-14 21:04:58 cvskris Exp $
+ RCS:		$Id: commondefs.h,v 1.5 2006-07-18 08:37:19 cvsnanne Exp $
 ________________________________________________________________________
 
  Some very commonly used macros.
@@ -33,6 +33,18 @@ ________________________________________________________________________
 #define	YES 1
 #define	NO 0
 #define mMaxUserIDLength 127
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#ifndef M_PI_2
+#define M_PI_2 1.57079632679489661923
+#endif
+
+#ifndef M_PI_4
+#define M_PI_4 0.78539816339744830962
+#endif
 
 
 #ifdef __msvc__
