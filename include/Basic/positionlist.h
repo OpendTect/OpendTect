f4b92a16b149473cfc64ac8e844dd18bcf8456d6
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2006-08-28 09:33:04 +0000

    *** empty log message ***
    git-svn-id: https://svn.opendtect.org/od/trunk@7983 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/positionlist.h
+++ include/Basic/positionlist.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H. Bril
  Date:          Jan 2003
- RCS:           $Id: positionlist.h,v 1.2 2006-08-28 09:08:22 cvskris Exp $
+ RCS:           $Id: positionlist.h,v 1.3 2006-08-28 09:33:04 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -24,7 +24,7 @@ class CoordList
 public:
     virtual int		add(const Coord3&)				= 0;
     virtual Coord3	get(int) const					= 0;
-    virtual void	set(int,const Coord3&) const			= 0;
+    virtual void	set(int,const Coord3&)				= 0;
     virtual void	remove(int)					= 0;
 };
 
