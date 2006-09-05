6832cb16c2e622fab7e5708d3b6d3a26efe1b29b
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2006-09-05 20:34:59 +0000

    Added vismultiattribsurvobj.cc/.h
    git-svn-id: https://svn.opendtect.org/od/trunk@7996 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/visSurvey/vismultiattribsurvobj.h
@@ -0,0 +1,110 @@
+#ifndef vismultiattribsurvobj_h
+#define vismultiattribsurvobj_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	Kristofer Tingdahl
+ Date:		4-11-2002
+ RCS:		$Id: vismultiattribsurvobj.h,v 1.1 2006-09-05 20:34:59 cvskris Exp $
+________________________________________________________________________
+
+
+-*/
+
+#include "vissurvobj.h"
+#include "visobject.h"
+
+namespace visBase { class MultiTexture2; }
+
+namespace visSurvey
+{
+
+/*!Base class for objects with multitextures. Class handles all texture handling
+   for inheriting classes, which avoids code duplication.
+*/
+
+
+class MultiTextureSurveyObject : public SurveyObject,
+			         public visBase::VisualObjectImpl
+{
+public:
+    void			turnOn(bool yn);
+    bool			isOn() const;
+
+    virtual int			nrResolutions() const			= 0;
+    virtual void		setResolution(int)			= 0;
+    int				getResolution() const;
+
+    bool			canHaveMultipleAttribs() const;
+    int				nrAttribs() const;
+    bool			addAttrib();
+    bool			removeAttrib(int attrib);
+    bool			swapAttribs(int attrib0,int attrib1);
+    void			setAttribTransparency(int,unsigned char);
+    unsigned char		getAttribTransparency(int) const;
+
+    const Attrib::SelSpec*	getSelSpec(int) const;
+    void			setSelSpec(int,const Attrib::SelSpec&);
+    void			clearTextures();
+    				/*!<Blanks all textures. */
+    bool 			isClassification(int attrib) const;
+    void			setClassification(int attrib,bool yn);
+    bool 			isAngle(int attrib) const;
+    void			setAngleFlag(int attrib,bool yn);
+    void			enableAttrib(int attrib,bool yn);
+    bool			isAttribEnabled(int attrib) const;
+    const TypeSet<float>*	getHistogram(int) const;
+    int				getColTabID(int) const;
+
+    bool			canHaveMultipleTextures() const { return true; }
+    int				nrTextures(int attrib) const;
+    void			selectTexture(int attrib, int texture );
+    int				selectedTexture(int attrib) const;
+
+    void			fillPar(IOPar&, TypeSet<int>&) const;
+    int				usePar(const IOPar&);
+
+protected:
+    				MultiTextureSurveyObject();
+				~MultiTextureSurveyObject();
+    void			getValueString(const Coord3&,
+	    				       BufferString&) const;
+
+    virtual float		getCacheValue(int attrib,int version,
+					      const Coord3&) const	= 0;
+
+    void			updateMainSwitch();
+    virtual void		addCache()				= 0;
+    virtual void		removeCache(int)			= 0;
+    virtual void		swapCache(int,int)			= 0;
+    virtual void		emptyCache(int)				= 0;
+    virtual bool		hasCache(int) const			= 0;
+
+    visBase::MultiTexture2*	texture_;
+    int				resolution_;
+    visBase::Material*		material_;
+
+private:
+    ObjectSet<Attrib::SelSpec>	as_;
+    BoolTypeSet			isclassification_;
+    bool			onoffstatus_;
+
+    static const char*		sKeyResolution();
+};
+
+}; // Namespace visSurvey
+
+
+/*! \mainpage 3D Visualisation - OpendTect specific
+
+  This module contains front-end classes for displaying 3D objects. Most 
+  functions in these classes deal with the geometry or position of the object, 
+  as well as handling new data and information about the attribute 
+  displayed.
+
+*/
+
+
+#endif
---
+++ src/visSurvey/vismultiattribsurvobj.cc
@@ -0,0 +1,395 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Jan 2002
+-*/
+
+static const char* rcsID = "$Id: vismultiattribsurvobj.cc,v 1.1 2006-09-05 20:34:59 cvskris Exp $";
+
+#include "vismultiattribsurvobj.h"
+
+#include "attribsel.h"
+#include "visdataman.h"
+#include "vismaterial.h"
+#include "viscolortab.h"
+#include "vismultitexture2.h"
+#include "iopar.h"
+
+
+namespace visSurvey {
+
+const char* MultiTextureSurveyObject::sKeyResolution()	{ return "Resolution"; }
+
+MultiTextureSurveyObject::MultiTextureSurveyObject()
+    : VisualObjectImpl(true)
+    , texture_( visBase::MultiTexture2::create() )
+    , onoffstatus_( true )
+    , resolution_( 0 )
+{
+    texture_->ref();
+    addChild( texture_->getInventorNode() );
+    texture_->setTextureRenderQuality(1);
+
+    material->setColor( Color::White );
+    material->setAmbience( 0.8 );
+    material->setDiffIntensity( 0.8 );
+
+    addAttrib();
+}
+
+
+MultiTextureSurveyObject::~MultiTextureSurveyObject()
+{
+    deepErase( as_ );
+    setDataTransform( 0 );
+    texture_->unRef();
+}
+
+
+void MultiTextureSurveyObject::turnOn( bool yn )
+{
+    onoffstatus_ = yn;
+    updateMainSwitch();
+}
+
+
+bool MultiTextureSurveyObject::isOn() const
+{ return onoffstatus_; }
+
+
+int MultiTextureSurveyObject::getResolution() const
+{ return resolution_; }
+
+
+void MultiTextureSurveyObject::updateMainSwitch()
+{
+    bool newstatus = onoffstatus_;
+    if ( newstatus )
+    {
+	newstatus = false;
+	for ( int idx=nrAttribs()-1; idx>=0; idx-- )
+	{
+	    if ( isAttribEnabled(idx) )
+	    {
+		newstatus = true;
+		break;
+	    }
+	}
+    }
+
+    VisualObjectImpl::turnOn( newstatus );
+}
+
+
+bool MultiTextureSurveyObject::canHaveMultipleAttribs() const
+{ return true; }
+
+
+int MultiTextureSurveyObject::nrAttribs() const
+{ return as_.size(); }
+
+
+bool MultiTextureSurveyObject::addAttrib()
+{
+    as_ += new Attrib::SelSpec;
+    addCache();
+
+    texture_->addTexture("");
+    texture_->setOperation( as_.size()-1, visBase::MultiTexture::BLEND );
+
+    updateMainSwitch();
+    return true;
+}
+
+
+bool MultiTextureSurveyObject::removeAttrib( int attrib )
+{
+    if ( as_.size()<2 || attrib<0 || attrib>=as_.size() )
+	return false;
+
+    delete as_[attrib];
+    as_.remove( attrib );
+
+    removeCache( attrib );
+    texture_->removeTexture( attrib );
+
+    updateMainSwitch();
+    return true;
+}
+
+
+bool MultiTextureSurveyObject::swapAttribs( int a0, int a1 )
+{
+    if ( a0<0 || a1<0 || a0>=as_.size() || a1>=as_.size() )
+	return false;
+
+    texture_->swapTextures( a0, a1 );
+    as_.swap( a0, a1 );
+    swapCache( a0, a1 );
+
+    return true;
+}
+
+
+void MultiTextureSurveyObject::clearTextures()
+{
+    for ( int idx=nrAttribs()-1; idx>=0; idx-- )
+    {
+	Attrib::SelSpec as; setSelSpec( idx, as );
+
+	for ( int idy=nrTextures(idx)-1; idy>=0; idy-- )
+	{
+	    texture_->setData( idx, idy, 0, false );
+	}
+    }
+}
+
+
+void MultiTextureSurveyObject::setAttribTransparency( int attrib,
+						      unsigned char nt )
+{ texture_->setTextureTransparency( attrib, nt ); }
+
+
+unsigned char
+MultiTextureSurveyObject::getAttribTransparency( int attrib ) const
+{ return texture_->getTextureTransparency( attrib ); }
+
+
+const Attrib::SelSpec* MultiTextureSurveyObject::getSelSpec( int attrib ) const
+{ return attrib>=0 && attrib<as_.size() ? as_[attrib] : 0; }
+
+
+void MultiTextureSurveyObject::setSelSpec( int attrib,
+					   const Attrib::SelSpec& as )
+{
+    if ( attrib>=0 && attrib<as_.size() )
+	*as_[attrib] = as;
+
+    emptyCache( attrib );
+
+    const char* usrref = as.userRef();
+    if ( !usrref || !*usrref )
+	texture_->enableTexture( attrib, false );
+}
+
+
+bool MultiTextureSurveyObject::isClassification( int attrib ) const
+{
+    return attrib>=0 && attrib<isclassification_.size()
+	? isclassification_[attrib] : false;
+}
+
+
+void MultiTextureSurveyObject::setClassification( int attrib, bool yn )
+{
+    if ( attrib<0 || attrib>=as_.size() )
+	return;
+
+    if ( yn )
+    {
+	while ( attrib>=isclassification_.size() )
+	    isclassification_ += false;
+    }
+    else if ( attrib>=isclassification_.size() )
+	return;
+
+    isclassification_[attrib] = yn;
+}
+
+
+bool MultiTextureSurveyObject::isAngle( int attrib ) const
+{ return texture_->isAngle( attrib ); }
+
+
+void MultiTextureSurveyObject::setAngleFlag( int attrib, bool yn )
+{ texture_->setAngleFlag( attrib, yn ); }
+
+
+bool MultiTextureSurveyObject::isAttribEnabled( int attrib ) const 
+{
+    return texture_->isTextureEnabled( attrib );
+}
+
+
+void MultiTextureSurveyObject::enableAttrib( int attrib, bool yn )
+{
+    texture_->enableTexture( attrib, yn );
+    updateMainSwitch();
+}
+
+
+const TypeSet<float>*
+MultiTextureSurveyObject::getHistogram( int attrib ) const
+{
+    return texture_->getHistogram( attrib, texture_->currentVersion( attrib ) );
+}
+
+
+int MultiTextureSurveyObject::getColTabID( int attrib ) const
+{
+    return texture_->getColorTab( attrib ).id();
+}
+
+
+int MultiTextureSurveyObject::nrTextures( int attrib ) const
+{
+    return texture_->nrVersions( attrib );
+}
+
+
+void MultiTextureSurveyObject::selectTexture( int attrib, int idx )
+{
+    if ( attrib<0 || attrib>=nrAttribs() ||
+	 idx<0 || idx>=texture_->nrVersions(attrib) ) return;
+
+    texture_->setCurrentVersion( attrib, idx );
+}
+
+
+int MultiTextureSurveyObject::selectedTexture( int attrib ) const
+{ 
+    if ( attrib<0 || attrib>=nrAttribs() ) return 0;
+
+    return texture_->currentVersion( attrib );
+}
+
+
+void MultiTextureSurveyObject::fillPar( IOPar& par,
+					TypeSet<int>& saveids ) const
+{
+    visBase::VisualObject::fillPar( par, saveids );
+
+    par.set( sKeyResolution(), resolution_ );
+    par.setYN( visBase::VisualObjectImpl::sKeyIsOn(), isOn() );
+    for ( int attrib=as_.size()-1; attrib>=0; attrib-- )
+    {
+	IOPar attribpar;
+	as_[attrib]->fillPar( attribpar );
+	const int coltabid = getColTabID(attrib);
+	attribpar.set( sKeyColTabID(), coltabid );
+	if ( saveids.indexOf( coltabid )==-1 ) saveids += coltabid;
+
+	attribpar.setYN( visBase::VisualObjectImpl::sKeyIsOn(),
+			 texture_->isTextureEnabled(attrib) );
+
+	BufferString key = sKeyAttribs();
+	key += attrib;
+	par.mergeComp( attribpar, key );
+    }
+
+    par.set( sKeyNrAttribs(), as_.size() );
+}
+
+
+int MultiTextureSurveyObject::usePar( const IOPar& par )
+{
+    const int res =  visBase::VisualObject::usePar( par );
+    if ( res!=1 ) return res;
+
+    par.get( sKeyResolution(), resolution_ );
+
+    int nrattribs;
+    if ( par.get(sKeyNrAttribs(),nrattribs) ) //current format
+    {
+	bool ison = true;
+	par.getYN( visBase::VisualObjectImpl::sKeyIsOn(), ison );
+	turnOn( ison );
+
+	bool firstattrib = true;
+	for ( int attrib=0; attrib<nrattribs; attrib++ )
+	{
+	    BufferString key = sKeyAttribs();
+	    key += attrib;
+	    PtrMan<const IOPar> attribpar = par.subselect( key );
+	    if ( !attribpar )
+		continue;
+
+	    int coltabid = -1;
+	    if ( attribpar->get(sKeyColTabID(),coltabid) )
+	    {
+		visBase::DataObject* dataobj= visBase::DM().getObject(coltabid);
+		if ( !dataobj ) return 0;
+
+		mDynamicCastGet(const visBase::VisColorTab*,coltab,dataobj);
+		if ( !coltab ) coltabid=-1;
+	    }
+
+	    if ( !firstattrib )
+		addAttrib();
+	    else
+		firstattrib = false;
+
+	    const int attribnr = as_.size()-1;
+
+	    as_[attribnr]->usePar( *attribpar );
+	    if ( coltabid!=-1 )
+	    {
+		mDynamicCastGet( visBase::VisColorTab*, coltab, 
+		       		 visBase::DM().getObject(coltabid) );
+		texture_->setColorTab( attribnr, *coltab );
+	    }
+
+	    ison = true;
+	    attribpar->getYN( visBase::VisualObjectImpl::sKeyIsOn(), ison );
+	    texture_->enableTexture( attribnr, ison );
+	}
+    }
+
+    return 1;
+}
+
+
+void MultiTextureSurveyObject::getValueString( const Coord3& pos,
+						BufferString& val ) const
+{
+    val = "undef";
+    BufferString valname;
+
+    for ( int idx=nrAttribs()-1; idx>=0; idx-- )
+    {
+	if ( !isAttribEnabled(idx) ||
+		texture_->getTextureTransparency(idx)==255 )
+	    continue;
+
+	const int version = texture_->currentVersion(idx);
+	const float fval = getCacheValue( idx, version, pos );
+
+	bool islowest = true;
+	for ( int idy=idx-1; idy>=0; idy-- )
+	{
+	    if ( !hasCache(idy) ||
+		 !isAttribEnabled(idy) ||
+		 texture_->getTextureTransparency(idy)==255 )
+		continue;
+
+	    islowest = false;
+	    break;
+	}    
+
+	if ( !islowest )
+	{
+	    const Color col = texture_->getColorTab(idx).color(fval);
+	    if ( col.t()==255 )
+		continue;
+	}
+
+	if ( !mIsUdf(fval) )
+	{
+	    val = fval;
+	    if ( nrAttribs()>1 )
+	    {
+		BufferString attribstr = "(";
+		attribstr += as_[idx]->userRef();
+		attribstr += ")";
+		val.insertAt( cValNameOffset(), (const char*)attribstr);
+	    }
+
+	    return;
+	}
+    }
+
+    return;
+}
+
+
+}; // namespace visSurvey
