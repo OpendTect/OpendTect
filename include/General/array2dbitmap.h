7d2751557b63e13273182a5abe89bd47c6d847b9
Bert Bril <bert.bril@dgbes.com>
2006-09-07 18:50:19 +0000

    Compiles, and it's almost 9 o'clock so I didn't test it. Comprende?
    git-svn-id: https://svn.opendtect.org/od/trunk@8012 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/array2dbitmap.h
@@ -0,0 +1,203 @@
+#ifndef array2dbitmap_h
+#define array2dbitmap_h
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        A.H. Bril
+ Date:          Sep 2006
+ RCS:           $Id: array2dbitmap.h,v 1.1 2006-09-07 18:50:19 cvsbert Exp $
+________________________________________________________________________
+
+-*/
+
+#include "ranges.h"
+#include "sets.h"
+#include "arraynd.h"
+
+
+typedef Array2D<char>	A2DBitMap;
+#define A2DBitMapImpl	Array2DImpl<char>
+
+
+/*! \brief Array2D Bitmap generation parameters */
+
+struct A2DBitmapGenPars
+{
+		A2DBitmapGenPars()
+		  : nointerpol_(false)
+		  , autoscale_(true)
+		  , clipratio_(0.025)
+		  , scale_(0,1)			{}
+
+    bool	nointerpol_;	//!< Do not interpolate between actual samples
+    bool	autoscale_;	//!< If not, use the scale_
+    float	clipratio_;	//!< ratio of numbers to be clipped before
+    				//!< determining min/max, only when autoscale_
+    Interval<float> scale_;	//!< Used when autoscale_ is false
+
+    static const char cNoFill;	//!< -127, = background/transparent
+
+};
+
+
+/*! \brief Array2D<float>& + statistics */
+
+class A2DBitMapInpData
+{
+public:
+
+    			A2DBitMapInpData( Array2D<float>& d )
+			    : data_(d)	{ collectData(); }
+
+    Array2D<float>&	data()		{ return data_; }
+    const Array2D<float>& data() const	{ return data_; }
+    int			nrPts() const	{ return statpts_.size(); }
+    Interval<float>	scale(float clipratio) const;
+    virtual float	midVal() const
+					{ return statpts_[nrPts()/2]; }
+
+    void		collectData(); //!< again.
+
+protected:
+
+    Array2D<float>&	data_;
+    TypeSet<float>	statpts_;
+
+};
+
+
+/*! \brief Array2D Bitmap generation setup
+
+The first dimension may be irregularly sampled. The second dimension must
+be 100% regular. For the first dimension, you set up the axis by providing
+the positions in a float array. If you don't provide that array, one will be
+generated, the postions are assumed to be: 0 1 2 ..., which is also what
+is done for the second dimension (which has to be regular).
+
+Then, you can zoom in by setting the different ranges. The default ranges will
+be -0.5 to N-0.5, i.e. half a distance between the cols/rows is added on all
+sides as border.
+
+The positions in dim 0 *must* be sorted, only the distances may vary.
+The average distance between the positions is used to calculate the
+default border.
+
+Dim 0 <-> X
+Dim 1 <-> Y
+
+*/
+
+
+class A2DBitmapPosSetup
+{
+public:
+
+			A2DBitmapPosSetup(const Array2DInfo&,float* dim0pos=0);
+				// If passed, dim0pos becomes mine
+    virtual		~A2DBitmapPosSetup();
+
+    void		setPositions(float* dim0positions);
+				//!< dim0posistions will become mine
+    inline const float*	dim0Positions() const
+    			{ return dim0pos_; }
+    int			dimSize( int dim ) const
+    			{ return dim ? szdim1_ : szdim0_; }
+
+    			// 'zoom in' (or out)
+    void		setDim0Range( const Interval<float>& r ) { dim0rg_ = r;}
+    void		setDim1Range( const Interval<float>& r ) { dim1rg_ = r;}
+
+    inline const Interval<float>& dimRange( int dim ) const
+    			{ return dim ? dim1rg_ : dim0rg_; }
+    inline float	avgDist( int dim ) const
+    			{ return dim ? 1 : dim0avgdist_; }
+    inline float	dimEps( int dim ) const
+			{ return 1e-6 * avgDist(dim); }
+
+    void		setBitmapSizes(int,int) const;
+    inline int		nrXPix() const		{ return nrxpix_; }
+    inline int		nrYPix() const		{ return nrypix_; }
+
+    inline float	getPixPerDim( int dim ) const
+			{ return dim ? pixperdim1_ : pixperdim0_; }
+    inline float	getPixOffs( int dim, float pos ) const
+			{ return  (pos - (dim ? dim1rg_ : dim0rg_).start)
+				* getPixPerDim( dim ); }
+
+    int			getPix(int dim,float) const;
+    			// Nr of pixels from (0,0), always inside bitmap
+    bool		isInside(int dim,float) const;
+    			// Is position in dim inside bitmap?
+
+protected:
+
+
+    float*		dim0pos_;
+    int			szdim0_;
+    int			szdim1_;
+
+    Interval<float>	dim0rg_;
+    Interval<float>	dim1rg_;
+
+    float		dim0avgdist_;
+
+    // Vars for current bitmap
+    int			nrxpix_;
+    int			nrypix_;
+    float		pixperdim0_;
+    float		pixperdim1_;
+
+};
+
+
+/*!\brief Generates Array2D bitmap from Array2D<float> */
+
+class A2DBitmapGenerator
+{
+public:
+
+    virtual		~A2DBitmapGenerator()	{ delete &pars_; }
+
+    void		setBitmap(A2DBitMap&);
+    A2DBitMap&		bitmap()		{ return *bitmap_; }
+    const A2DBitMap&	 bitmap() const		{ return *bitmap_; }
+    int			bitmapSize(int dim) const;
+
+    void		initBitmap();	//!< with A2DBitmapGenPars::cNoFill
+    void		fill();
+    virtual bool	dump(std::ostream&)	{ return false; }
+
+    A2DBitmapGenPars&		pars()		{ return pars_; }
+    const A2DBitmapGenPars&	pars() const	{ return pars_; }
+    const A2DBitMapInpData&	data() const	{ return data_; }
+    const A2DBitmapPosSetup&	setup() const	{ return setup_; }
+
+protected:
+
+				A2DBitmapGenerator(const A2DBitMapInpData&,
+						   const A2DBitmapPosSetup&,
+						   A2DBitmapGenPars&);
+				   //!< pass a new instance of (subclass of)
+				   //!< A2DBitmapGenPars
+
+    const A2DBitMapInpData&	data_;
+    const A2DBitmapPosSetup&	setup_;
+    A2DBitmapGenPars&		pars_;
+    A2DBitMap*			bitmap_;
+
+    // handy vars, duplicated from respective objects
+    int				szdim0_;
+    int				szdim1_;
+    const float*		dim0pos_;
+    Interval<float>		dim0rg_;
+    Interval<float>		dim1rg_;
+    Interval<float>		scalerg_;
+    float			scalewidth_;
+
+    virtual void		doFill()		= 0;
+
+};
+
+
+#endif
---
+++ src/General/array2dbitmap.cc
@@ -0,0 +1,207 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : A.H. Bril
+ * DATE     : Sep 2006
+-*/
+
+static const char* rcsID = "$Id: array2dbitmap.cc,v 1.1 2006-09-07 18:50:19 cvsbert Exp $";
+
+#include "array2dbitmap.h"
+#include "arrayndimpl.h"
+#include "sorting.h"
+#include "stats.h"
+#include <math.h>
+
+
+const char A2DBitmapGenPars::cNoFill = -127;
+#define mMaxNrStatPts 2000
+#define mMinNrStatPts 100
+
+
+Interval<float> A2DBitMapInpData::scale( float clipratio ) const
+{
+    const int nrstatpts = nrPts();
+    int nroffs = (int)(clipratio * nrstatpts + .5);
+    if ( nroffs > nrstatpts / 2 - 1 )
+	nroffs = nrstatpts / 2 - 1;
+    if ( nroffs < 0 )
+	nroffs = 0;
+
+    return Interval<float>( statpts_[nroffs], statpts_[nrstatpts-nroffs-1] );
+}
+
+
+void A2DBitMapInpData::collectData()
+{
+    const int szdim0 = data_.info().getSize( 0 );
+    const int szdim1 = data_.info().getSize( 1 );
+    const int totnrsamps = szdim0 * szdim1;
+
+    if ( totnrsamps > mMaxNrStatPts )
+    {
+	const int maxtries = mMaxNrStatPts * 1000;
+	Stat_initRandom( 0 );
+	for ( int itry=0; itry<maxtries; itry++ )
+	{
+	    const int totidx = Stat_getIndex( totnrsamps );
+	    const float val = data_.get( totidx / szdim1, totidx % szdim1 );
+	    if ( !mIsUdf(val) )
+	    {
+		statpts_ += val;
+		if ( statpts_.size() >= mMaxNrStatPts )
+		    return;
+	    }
+	}
+    }
+
+    if ( statpts_.size() >= mMinNrStatPts ) return;
+
+    for ( int idim0=0; idim0<szdim0; idim0++ )
+    {
+	for ( int idim1=0; idim1<szdim1; idim1++ )
+	{
+	    const float val = data_.get( idim0, idim1 );
+	    if ( !mIsUdf(val) )
+	    {
+		statpts_ += val;
+		if ( statpts_.size() >= mMaxNrStatPts )
+		    return;
+	    }
+	}
+    }
+
+    if ( statpts_.size() < 1 )
+	{ statpts_ += 0; statpts_ += 0; }
+    else if ( statpts_.size() < 2 )
+	statpts_ += statpts_[0];
+}
+
+
+A2DBitmapPosSetup::A2DBitmapPosSetup( const Array2DInfo& i, float* p )
+    	: szdim0_(i.getSize(0))
+    	, szdim1_(i.getSize(1))
+	, nrxpix_(0)
+	, nrypix_(0)
+	, dim0pos_(0)
+{
+    dim1rg_.start = -0.5; dim1rg_.stop = szdim1_-0.5;
+    setPositions( p );
+}
+
+
+A2DBitmapPosSetup::~A2DBitmapPosSetup()
+{
+    delete [] dim0pos_;
+}
+
+
+void A2DBitmapPosSetup::setPositions( float* p )
+{
+    if ( szdim0_ < 1 ) return;
+
+    delete [] dim0pos_; dim0pos_ = p;
+
+    Interval<float> posbounds;
+    dim0avgdist_ = 1;
+    if ( !dim0pos_ )
+    {
+	dim0pos_ = new float [szdim0_];
+	for ( int idx=0; idx<szdim0_; idx++ )
+	    dim0pos_[idx] = idx;
+	posbounds.start = 0; posbounds.stop = szdim0_-1;
+    }
+    else
+    {
+	posbounds.start = dim0pos_[0]; posbounds.stop = dim0pos_[szdim0_-1];
+	if ( szdim0_ > 1 )
+	{
+	    float totdist = 0;
+	    for ( int idx=1; idx<szdim0_; idx++ )
+		totdist += fabs(dim0pos_[idx] - dim0pos_[idx-1]);
+	    dim0avgdist_ = totdist / (szdim0_ - 1);
+	}
+    }
+
+    posbounds.sort();
+    dim0rg_.start = posbounds.start - dim0avgdist_ * 0.5;
+    dim0rg_.stop = posbounds.stop + dim0avgdist_ * 0.5;
+}
+
+
+void A2DBitmapPosSetup::setBitmapSizes( int n0, int n1 ) const
+{
+    A2DBitmapPosSetup& self = *(const_cast<A2DBitmapPosSetup*>(this));
+    // Don't like to declare all mutable as they shld be const everyhwere else
+    self.nrxpix_ = n0; self.nrypix_ = n1;
+    self.dim0rg_.sort(); self.dim1rg_.sort();
+    self.pixperdim0_ = nrxpix_ / dim0rg_.width();
+    self.pixperdim1_ = nrypix_ / dim1rg_.width();
+}
+
+
+int A2DBitmapPosSetup::getPix( int dim, float pos ) const
+{
+    const float fpix = getPixOffs( dim, pos );
+
+    int pix = mNINT(fpix);
+    if ( pix < 0 )
+	pix = 0;
+    else if ( pix >= (dim?nrypix_:nrxpix_) )
+	pix = (dim?nrypix_:nrxpix_) - 1;
+
+    return pix;
+}
+
+
+bool A2DBitmapPosSetup::isInside( int dim, float pos ) const
+{
+    const float fpix = getPixOffs( dim, pos );
+
+    return fpix + 1e-6 > 0 && fpix - 1e-6 < (dim?nrypix_:nrxpix_) - 1;
+}
+
+
+A2DBitmapGenerator::A2DBitmapGenerator( const A2DBitMapInpData& dat,
+					const A2DBitmapPosSetup& setp,
+					A2DBitmapGenPars& gp )
+	: data_(dat)
+    	, setup_(setp)
+    	, pars_(gp)
+    	, bitmap_(0)
+{
+}
+
+
+void A2DBitmapGenerator::initBitmap()
+{
+    const int totsz = bitmapSize(0) * bitmapSize(1);
+    if ( totsz > 0 )
+	memset( bitmap_->getData(), A2DBitmapGenPars::cNoFill,
+		totsz*sizeof(char) );
+}
+
+
+int A2DBitmapGenerator::bitmapSize( int dim ) const
+{
+    return bitmap_ ? bitmap_->info().getSize( dim ? 1 : 0 ) : 0;
+}
+
+
+void A2DBitmapGenerator::fill()
+{
+    setup_.setBitmapSizes( bitmapSize(0), bitmapSize(1) );
+    if ( !setup_.nrXPix() || !setup_.nrYPix() )
+	return;
+
+    szdim0_ = setup_.dimSize( 0 );
+    szdim1_ = setup_.dimSize( 1 );
+    dim0pos_ = setup_.dim0Positions();
+    dim0rg_ = setup_.dimRange( 0 );
+    dim1rg_ = setup_.dimRange( 1 );
+
+    scalerg_ = pars_.autoscale_ ? data_.scale( pars_.clipratio_ )
+				: pars_.scale_;
+    scalewidth_ = scalerg_.stop - scalerg_.start;
+
+    doFill();
+}
