6f91da294404f7d50224587241013bcb4649cdd4
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2006-09-12 18:40:54 +0000

    Moved keyenum to General and added mouseevent.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@8044 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/General/keyenum.cc
@@ -0,0 +1,30 @@
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        H. Payraudeau
+ Date:          30/01/2006
+ RCS:           $Id: keyenum.cc,v 1.1 2006-09-12 18:40:54 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+
+#include "keyenum.h"
+
+DefineEnumNames(OD,ButtonState,0,"Button State")
+{
+	"NoButton",
+	"LeftButton",
+	"RightButton",
+	"MidButton",
+	"MouseButtonMask",
+	"ShiftButton",
+	"ControlButton",
+	"AltButton",
+	"MetaButton",
+	"KeyButtonMask",
+	"Keypad",
+	0
+};
+
---
+++ src/General/mouseevent.cc
@@ -0,0 +1,57 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : September 2006
+-*/
+
+static const char* rcsID = "$Id: mouseevent.cc,v 1.1 2006-09-12 18:40:54 cvskris Exp $";
+
+#include "mouseevent.h"
+
+MouseEvent::MouseEvent(OD::ButtonState st, int xx, int yy )
+    : butstate_( st )
+    , pos_( xx, yy )
+{}
+
+
+bool MouseEvent::operator ==( const MouseEvent& ev ) const
+{ return butstate_ == ev.butstate_ && pos_==ev.pos_; } 
+
+
+bool MouseEvent::operator !=( const MouseEvent& ev ) const
+{ return !(*this == ev); }
+
+MouseEventHandler::MouseEventHandler()
+    : buttonPressed( this )
+    , buttonReleased( this )
+    , movement( this )
+    , doubleClick( this )
+{}
+
+
+void MouseEventHandler::triggerMovement( const MouseEvent& ev )
+{
+    event_ = &ev;
+    movement.trigger();
+}
+
+
+void MouseEventHandler::triggerButtonPressed( const MouseEvent& ev )
+{
+    event_ = &ev;
+    buttonPressed.trigger();
+}
+
+
+void MouseEventHandler::triggerButtonReleased( const MouseEvent& ev )
+{
+    event_ = &ev;
+    buttonReleased.trigger();
+}
+
+
+void MouseEventHandler::triggerDoubleClick( const MouseEvent& ev )
+{
+    event_ = &ev;
+    doubleClick.trigger();
+}
