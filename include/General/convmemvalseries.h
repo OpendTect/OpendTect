62b247dfae3b39c00ceedfd317b8cf669bd2b67a
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2006-11-14 12:38:21 +0000

    Moved implementation from class definition to end of file.
    git-svn-id: https://svn.opendtect.org/od/trunk@8337 a985855d-d384-4b45-ae33-31a410998c3c

--- include/General/convmemvalseries.h
+++ include/General/convmemvalseries.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Kris Tingdahl
  Date:          Oct 2006
- RCS:           $Id: convmemvalseries.h,v 1.1 2006-10-20 21:35:27 cvskris Exp $
+ RCS:           $Id: convmemvalseries.h,v 1.2 2006-11-14 12:38:21 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -27,37 +27,22 @@ class ConvMemValueSeries : public ValueSeries<T>
 {
 public:
 
-    		ConvMemValueSeries( int sz, const BinDataDesc& stortype )
-		    : ptr_( 0 )
-		    , size_( sz )
-		    , interpreter_( DataCharacteristics(stortype) )
-		{
-		    T dummy;
-		    rettype_ = BinDataDesc( dummy );
-		    ptr_ = new char[sz*interpreter_.nrBytes()];
-		}
+    inline	    	ConvMemValueSeries(int sz, const BinDataDesc& stortype);
 
-		~ConvMemValueSeries() { delete [] ptr_; }
+    inline		~ConvMemValueSeries();
 
-    T		value(int idx) const
-    		{ return interpreter_.get( ptr_, idx ); }
+    inline int		size() const;
+    inline bool		writable() const;
+    inline T		value(int idx) const;
+    inline void		setValue( int idx, T v );
 
-    bool	writable() const		{ return true; }
+    inline const T*	arr() const;
+    inline T*		arr();
 
-    int		size() const 			{ return size_; }
+    inline char*	storArr();
+    inline const char*	storArr() const;
 
-    void	setValue( int idx, T v )
-		{
-		    interpreter_.put( ptr_, idx, v );
-		}
-
-    const T*	arr() const
-    		{ return interpreter_.dataChar()==rettype_ ? (T*) ptr_ : 0; }
-    T*		arr()
-		{ return interpreter_.dataChar()==rettype_ ? (T*) ptr_ : 0; }
-
-    char*	storArr()	{ return ptr_; }
-    const char*	storArr() const	{ return ptr_; }
+    inline BinDataDesc	dataDesc() const;
 
 protected:
 
@@ -67,6 +52,68 @@ protected:
     char*		ptr_;
     int			size_;
 };
+
+
+template <class T> inline
+ConvMemValueSeries<T>::ConvMemValueSeries( int sz, const BinDataDesc& stortype)
+    : ptr_( 0 )
+    , size_( sz )
+    , interpreter_( DataCharacteristics(stortype) )
+{
+    T dummy;
+    rettype_ = BinDataDesc( dummy );
+    ptr_ = new char[sz*interpreter_.nrBytes()];
+}
+
+
+template <class T> inline
+ConvMemValueSeries<T>::~ConvMemValueSeries()
+{ delete [] ptr_; }
+
+
+template <class T> inline
+T ConvMemValueSeries<T>::value(int idx) const
+{ return interpreter_.get( ptr_, idx ); }
+
+
+template <class T> inline
+bool ConvMemValueSeries<T>::writable() const
+{ return true; }
+
+
+template <class T> inline
+int ConvMemValueSeries<T>::size() const
+{ return size_; }
+
+
+template <class T> inline
+void ConvMemValueSeries<T>::setValue( int idx, T v )
+{ interpreter_.put( ptr_, idx, v ); }
+
+
+template <class T> inline
+const T* ConvMemValueSeries<T>::arr() const
+{ return interpreter_.dataChar()==rettype_ ? (T*) ptr_ : 0; }
+
+
+template <class T> inline
+T* ConvMemValueSeries<T>::arr()
+{ return interpreter_.dataChar()==rettype_ ? (T*) ptr_ : 0; }
+
+
+template <class T> inline
+char* ConvMemValueSeries<T>::storArr()
+{ return ptr_; }
+
+
+template <class T> inline
+const char* ConvMemValueSeries<T>::storArr() const
+{ return ptr_; }
+
+
+template <class T> inline
+BinDataDesc ConvMemValueSeries<T>::dataDesc() const
+{ return interpreter_.dataChar(); }
 
 
 #endif
