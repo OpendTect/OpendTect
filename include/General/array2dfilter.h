18730640419dbc1dfa55660fa916145c8defc329
Bert Bril <bert.bril@dgbes.com>
2006-11-17 13:09:41 +0000

    now compiles. Not tested.
    git-svn-id: https://svn.opendtect.org/od/trunk@8355 a985855d-d384-4b45-ae33-31a410998c3c

--- include/General/array2dfilter.h
+++ include/General/array2dfilter.h
@@ -1,22 +1,23 @@
-#ifndef array2dinterpol_h
-#define array2dinterpol_h
+#ifndef array2dfilter_h
+#define array2dfilter_h
 
 /*@+
 ________________________________________________________________________
 
  CopyRight:     (C) dGB Beheer B.V.
- Author:        Kristofer Tingdahl
- Date:          April 2005
- RCS:           $Id: array2dfilter.h,v 1.1 2006-11-17 12:17:16 cvsbert Exp $
+ Author:        Bert Bril
+ Date:          Nov 2006
+ RCS:           $Id: array2dfilter.h,v 1.2 2006-11-17 13:09:41 cvsbert Exp $
 ________________________________________________________________________
 
 
 @$*/
 
 #include "arraynd.h"
-#include "stattype.h"
+#include "statruncalc.h"
 #include "executor.h"
 #include "iopar.h"
+#include "rowcol.h"
 #include "errh.h"
 #include <math.h>
 
@@ -33,7 +34,7 @@ struct Array2DFilterPars
 
 			Array2DFilterPars( Stats::Type t=Stats::Average,
 					   RowCol rc=RowCol(1,1),
-			       		   bool filludf=false )	{}
+			       		   bool filludf=false )
 			: type_(t)
 			, stepout_(rc)
 			, filludf_(filludf)
@@ -67,11 +68,11 @@ public:
     inline Array2D<T>&	arr()			{ return arr_; }
     inline const Array2D<T>& arr() const	{ return arr_; }
     inline const Array2DFilterPars& pars() const { return pars_; }
-    inline const Stats::RunCalc& calc() const	{ return *calc_; }
+    inline const Stats::RunCalc<float>& calc() const { return *calc_; }
 
     inline int		nextStep();
     inline const char*	message() const		{ return "Filtering data"; }
-    inline int		nrDone() const		{ return nrdone_; }
+    inline int		nrDone() const		{ return nrcolsdone_; }
     inline const char*	nrDoneText() const	{ return "Columns handled";}
     inline int		totalNr() const		{ return colsize_; }
 
@@ -79,20 +80,20 @@ protected:
 
     Array2D<T>&		arr_;
     Array2DFilterPars	pars_;
-    Stats::RunCalc*	calc_;
-    T**			buff_;
+    Stats::RunCalc<float>* calc_;
+    T**			bufs_;
     int*		idxs_;
 
     const int		rowsize_;
     const int		colsize_;
     const int		nrcols_;
     const int		nrrows_;
-    int			nrdone_;
+    int			nrcolsdone_;
 
     inline void		releaseAll();
     inline bool		manageBufs(int);
-    inline void		filterCol(int);
-    inline void		doPoint(int,int);
+    inline void		filterCol();
+    inline void		doPoint(int);
 
 };
 
@@ -102,72 +103,73 @@ Array2DFilterer<T>::Array2DFilterer( Array2D<T>& a, const Array2DFilterPars& p )
     : Executor("2D Filtering")
     , pars_(p)
     , arr_(a)
-    , buff_(0)
+    , bufs_(0)
     , calc_(0)
     , rowsize_(a.info().getSize(0))
     , colsize_(a.info().getSize(1))
     , nrcols_(2 * p.stepout_.c() + 1)
     , nrrows_(2 * p.stepout_.r() + 1)
-    , nrdone_(0)
+    , nrcolsdone_(0)
 {
     if ( rowsize_ < 2 * p.stepout_.r() + 1
       || colsize_ < 2 * p.stepout_.c() + 1
-      || (pars_.stepout.c() < 1 && pars_.stepout.r() < 1) )
-	{ nrdone_ = colsize_; return; }
+      || (pars_.stepout_.c() < 1 && pars_.stepout_.r() < 1) )
+	{ nrcolsdone_ = colsize_; return; }
 
-    buff_ = new T* [ nrcols_ ];
+    bufs_ = new T* [ nrcols_ ];
     idxs_ = new int [ nrcols_ ];
     for ( int idx=0; idx<nrcols_; idx++ )
     {
-	buff_[idx] = new T [rowsize_];
+	bufs_[idx] = new T [rowsize_];
 	idxs_[idx] = idx;
-	int inpcol = idx < pars_.stepout.c() ? 0 : idx - pars_.stepout.c();
+	int inpcol = idx < pars_.stepout_.c() ? 0 : idx - pars_.stepout_.c();
 	if ( inpcol >= colsize_ ) inpcol = colsize_ - 1;
 	for ( int irow=0; irow<rowsize_; irow++ )
-	    buff_[idx][irow] = arr_->get( irow, inpcol );
+	    bufs_[idx][irow] = arr_.get( irow, inpcol );
     }
-    if ( !buff_[nrcols_-1] )
-	{ releaseAll(); ErrMsg("Memory full"); nrdone_ = colsize_; return; }
+    if ( !bufs_[nrcols_-1] )
+	{ releaseAll(); ErrMsg("Memory full"); nrcolsdone_ = colsize_; return; }
 
     Stats::RunCalcSetup setup( !mIsUdf(pars_.rowdist_) );
     setup.require( pars_.type_ );
-    calc_ = new Stats::RunCalc( setup );
+    calc_ = new Stats::RunCalc<float>( setup );
 }
 
 
 template <class T> inline
-void Array2DFilterer<T>::~Array2DFilterer()
+Array2DFilterer<T>::~Array2DFilterer()
 {
     releaseAll();
 }
 
 
+template <class T> inline
 void Array2DFilterer<T>::releaseAll()
 {
     delete calc_; calc_ = 0;
-    if ( !buff_ ) return;
+    if ( !bufs_ ) return;
 
     for ( int idx=0; idx<nrcols_; idx++ )
-	delete [] buff_[idx];
-    delete [] buff_; buff_ = 0;
+	delete [] bufs_[idx];
+    delete [] bufs_; bufs_ = 0;
 }
 
 
 template <class T> inline int Array2DFilterer<T>::nextStep()
 {
-    if ( !buff_ || nrdone_ >= colsize_ )
+    if ( !bufs_ || nrcolsdone_ >= colsize_ )
 	return Executor::Finished;
 
-    if ( !manageBufs(nrdone_) )
+    if ( !manageBufs(nrcolsdone_) )
 	return Executor::Finished;
 
-    filterCol( nrdone_ );
-    nrdone_++;
+    filterCol();
+    nrcolsdone_++;
     return Executor::MoreToDo;
 }
 
 
-template <class T> inline void Array2DFilterer<T>::manageBufs( int col )
+template <class T> inline bool Array2DFilterer<T>::manageBufs( int col )
 {
     if ( col == 0 || col >= colsize_ )
 	return false;
@@ -176,7 +178,7 @@ template <class T> inline void Array2DFilterer<T>::manageBufs( int col )
     for ( int idx=0; idx<nrcols_; idx++ )
     {
 	if ( idxs_[idx] != 0 )
-	    idxs_[idx]--
+	    idxs_[idx]--;
 	else
 	{
 	    idxs_[idx] = nrcols_ - 1;
@@ -189,7 +191,7 @@ template <class T> inline void Array2DFilterer<T>::manageBufs( int col )
 	inpcol = colsize_ - 1;
 
     for ( int irow=0; irow<rowsize_; irow++ )
-	fillbuf[irow] = arr_->get( irow, inpcol );
+	fillbuf[irow] = arr_.get( irow, inpcol );
 
     return true;
 }
@@ -199,7 +201,6 @@ template <class T> inline void Array2DFilterer<T>::filterCol()
 {
     for ( int irow=0; irow<rowsize_; irow++ )
     {
-	const T v00 = bufs_[ idxs_[nrcols_/2] ][irow];
 	if ( !pars_.filludf_ )
 	{
 	    const T v00 = bufs_[ idxs_[nrcols_/2] ][irow];
@@ -238,6 +239,9 @@ inline void Array2DFilterer<T>::doPoint( int row )
 		calc_->addValue( buf[irow], wt );
 	    }
 	}
-	arr_.set( irow, realcol, calc_->getValue(pars_.type_) );
+	arr_.set( row, realcol, calc_->getValue(pars_.type_) );
     }
 }
+
+
+#endif
