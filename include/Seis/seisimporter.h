881443678a70bd055169b7d3a6349aa1d8df614e
Bert Bril <bert.bril@dgbes.com>
2006-12-05 12:09:26 +0000

    added
    git-svn-id: https://svn.opendtect.org/od/trunk@8430 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Seis/seisimporter.h
@@ -0,0 +1,86 @@
+#ifndef seisimporter_h
+#define seisimporter_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	A.H. Bril
+ Date:		Nov 2006
+ RCS:		$Id: seisimporter.h,v 1.1 2006-12-05 12:09:26 cvsbert Exp $
+________________________________________________________________________
+
+-*/
+
+#include "seistype.h"
+#include "executor.h"
+#include "bufstring.h"
+class IOObj;
+class SeisTrc;
+class SeisTrcBuf;
+class SeisTrcWriter;
+
+
+/*!\brief Helps import or export of seismic data. */
+
+class SeisImporter : public Executor
+{
+public:
+
+    /*!<\brief provides traces from the import storage
+
+      fetch() must return false at end or when an error occurs.
+      On error, the errmsg_ must be filled.
+
+      */
+
+    struct Reader
+    {
+	virtual			~Reader()			{}
+
+	virtual const char*	name() const			= 0;
+	virtual bool		fetch(SeisTrc&)			= 0;
+	virtual int		totalNr() const			{ return -1; }
+
+	BufferString		errmsg_;
+    };
+
+
+    			SeisImporter(Reader*,SeisTrcWriter&,Seis::GeomType);
+				//!< Reader becomes mine. Has to be non-null.
+    virtual		~SeisImporter();
+
+    const char*		message() const;
+    int			nrDone() const;
+    const char*		nrDoneText() const;
+    int			totalNr() const;
+    int			nextStep();
+
+    bool		removenulltrcs_;
+
+protected:
+
+    enum State		{ ReadBuf, WriteBuf, ReadWrite };
+
+    Reader*		rdr_;
+    SeisTrcWriter&	wrr_;
+    SeisTrcBuf&		buf_;
+    SeisTrc&		trc_;
+    Seis::GeomType	geomtype_;
+    State		state_;
+    int			nrread_;
+    int			nrwritten_;
+    bool		crlsorted_;
+    Executor*		postproc_;
+
+    int			doWrite(const SeisTrc&);
+    int			readIntoBuf();
+    Executor*		mkPostProc();
+
+    mutable BufferString	errmsg_;
+    mutable BufferString	hndlmsg_;
+
+};
+
+
+#endif
---
+++ src/Seis/seisimporter.cc
@@ -0,0 +1,171 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : A.H. Bril
+ * DATE     : Nov 2006
+-*/
+
+static const char* rcsID = "$Id: seisimporter.cc,v 1.1 2006-12-05 12:09:14 cvsbert Exp $";
+
+#include "seisimporter.h"
+#include "seisbuf.h"
+#include "seistrc.h"
+#include "seiswrite.h"
+#include "ptrman.h"
+
+
+SeisImporter::SeisImporter( SeisImporter::Reader* r, SeisTrcWriter& w,
+			    Seis::GeomType gt )
+    	: Executor("Importing seismic data")
+    	, rdr_(r)
+    	, wrr_(w)
+    	, geomtype_(gt)
+    	, buf_(*new SeisTrcBuf)
+    	, trc_(*new SeisTrc)
+    	, state_(ReadBuf)
+    	, nrread_(0)
+    	, nrwritten_(0)
+    	, crlsorted_(0)
+    	, postproc_(0)
+    	, removenulltrcs_(false)
+{
+}
+
+
+SeisImporter::~SeisImporter()
+{
+    buf_.deepErase();
+    delete postproc_;
+    delete rdr_;
+    delete &buf_;
+    delete &trc_;
+}
+
+
+const char* SeisImporter::message() const
+{
+    if ( postproc_ ) return postproc_->message();
+    if ( !errmsg_.isEmpty() )
+	return errmsg_;
+
+    if ( hndlmsg_.isEmpty() )
+	{ hndlmsg_ = "Importing from "; hndlmsg_ += rdr_->name(); }
+    return hndlmsg_;
+}
+
+
+int SeisImporter::nrDone() const
+{
+    if ( postproc_ ) return postproc_->nrDone();
+    return state_ == ReadBuf ? nrread_ : nrwritten_;
+}
+
+
+const char* SeisImporter::nrDoneText() const
+{
+    if ( postproc_ ) return postproc_->nrDoneText();
+    return state_ == ReadBuf ? "Traces read" : "Traces written";
+}
+
+
+int SeisImporter::totalNr() const
+{
+    if ( postproc_ ) return postproc_->totalNr();
+    return rdr_->totalNr();
+}
+
+
+#define mDoRead(trc) \
+    bool atend = false; \
+    if ( rdr_->fetch(trc) ) \
+	nrread_++; \
+    else \
+    { \
+	errmsg_ = rdr_->errmsg_; \
+	if ( !errmsg_.isEmpty() ) \
+	    return Executor::ErrorOccurred; \
+	atend = true; \
+    }
+
+
+int SeisImporter::nextStep()
+{
+    if ( postproc_ ) return postproc_->doStep();
+
+    if ( state_ == WriteBuf )
+    {
+	if ( buf_.isEmpty() )
+	    state_ = ReadWrite;
+	else
+	{
+	    PtrMan<SeisTrc> trc = buf_.remove( ((int)0) );
+	    return doWrite( *trc );
+	}
+    }
+
+    if ( state_ == ReadWrite )
+    {
+	mDoRead( trc_ )
+	if ( !atend )
+	{
+	    if ( crlsorted_ )
+		Swap( trc_.info().binid.inl, trc_.info().binid.crl );
+
+	    return doWrite( trc_ );
+	}
+
+	postproc_ = mkPostProc();
+	return postproc_ ? Executor::MoreToDo : Executor::Finished;
+    }
+
+    return readIntoBuf();
+}
+
+
+int SeisImporter::doWrite( const SeisTrc& trc )
+{
+    if ( removenulltrcs_ && trc.isNull() )
+	return Executor::MoreToDo;
+
+    if ( wrr_.put(trc) )
+    {
+	nrwritten_++;
+	return Executor::MoreToDo;
+    }
+
+    errmsg_ = wrr_.errMsg();
+    if ( errmsg_.isEmpty() )
+    {
+	pErrMsg( "Need an error message from writer" );
+	errmsg_ = "Cannot write trace";
+    }
+
+    return Executor::ErrorOccurred;
+}
+
+
+int SeisImporter::readIntoBuf()
+{
+    SeisTrc* trc = new SeisTrc;
+    mDoRead( *trc )
+    if ( atend )
+    {
+	delete trc;
+	if ( nrread_ == 0 )
+	{
+	    errmsg_ = "No valid traces in input";
+	    return Executor::ErrorOccurred;
+	}
+
+	return buf_.isEmpty() ? ReadWrite : WriteBuf;
+    }
+
+    //TODO use the info we have to make importing safer
+    buf_.add( trc );
+    return WriteBuf;
+}
+
+
+Executor* SeisImporter::mkPostProc()
+{
+    return 0;
+}
