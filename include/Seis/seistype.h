f6406db0fa145a951f1d463e6f611f789c07a02a
Bert Bril <bert.bril@dgbes.com>
2006-12-05 16:48:45 +0000

    added utility functions for GeomType
    git-svn-id: https://svn.opendtect.org/od/trunk@8438 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Seis/seistype.h
+++ include/Seis/seistype.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	K. Tingdahl
  Date:		29-5-99
- RCS:		$Id: seistype.h,v 1.8 2006-08-24 14:34:09 cvskris Exp $
+ RCS:		$Id: seistype.h,v 1.9 2006-12-05 16:48:45 cvsbert Exp $
 ________________________________________________________________________
 
 */
@@ -16,8 +16,19 @@ namespace Seis
 {
 
     enum SelType	{ All, Range, Table, TrcNrs };
-    enum GeomType	{ Vol, VolPS, Line, LinePS };
+
     enum ReadMode	{ PreScan, Scan, Prod };
+
+    enum GeomType	{ Vol, VolPS, Line, LinePS };
+    inline bool		is2D( GeomType gt )
+    			{ return gt > VolPS; }
+    inline bool		is3D( GeomType gt )
+    			{ return gt < Line; }
+    inline bool		isPS( GeomType gt )
+    			{ return gt == VolPS || gt == LinePS; }
+    inline int		dimSize( GeomType gt )
+			{ return gt == Line ? 2 : (gt == VolPS ? 4 : 3); }
+
 
     enum DataType       { Ampl, Dip, Frequency, Phase, AVOGradient, 
 			  Azimuth, Classification, UnknowData };
