c1fece86040d6f0f321c061ad61291323e144aab
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2007-01-24 14:22:37 +0000

    Made all functions inline and added implementation at end of file.
    git-svn-id: https://svn.opendtect.org/od/trunk@8701 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/samplingdata.h
+++ include/Basic/samplingdata.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	A.H. Bril
  Date:		23-10-1996
- RCS:		$Id: samplingdata.h,v 1.8 2006-04-27 16:07:06 cvskris Exp $
+ RCS:		$Id: samplingdata.h,v 1.9 2007-01-24 14:22:37 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -21,32 +21,79 @@ template <class T>
 class SamplingData
 {
 public:
-		SamplingData( T sa=0, T se=1 )
-		: start(sa), step(se)			{}
-		SamplingData( const StepInterval<T>& intv )
-		: start(intv.start), step(intv.step)	{}
+    inline				SamplingData(T sa=0,T se=1);
+    template <class FT>	inline		SamplingData(const SamplingData<FT>&);
+    template <class FT>	inline		SamplingData(const StepInterval<FT>&);
 
-    void	operator+=( const SamplingData& sd )
-		{ start += sd.start; step += sd.step; }
-    int		operator==( const SamplingData& sd ) const
-		{ return start == sd.start && step == sd.step; }
-    int		operator!=( const SamplingData& sd ) const
-		{ return ! (sd == *this); }
+    inline void				operator+=(const SamplingData& sd);
+    inline int				operator==(const SamplingData& sd)const;
+    inline int				operator!=(const SamplingData& sd)const;
 
-    StepInterval<T> interval( int nrsamp ) const
-		{ return StepInterval<T>( start, start+(nrsamp-1)*step, step); }
-    template <class IT> float	getIndex( IT val ) const
-		{ return (val-start) / (float) step; }
-    int		nearestIndex( T x ) const
-		{ float fidx = getIndex(x); return mNINT(fidx); }
-    template <class IT>
-    T		atIndex( IT idx ) const
-		{ return start + step * idx; }
+    inline StepInterval<T>		interval(int nrsamp) const;
+    template <class IT> inline float	getIndex(IT val) const;
+    inline int				nearestIndex(T x) const;
+    template <class IT> inline T	atIndex(IT idx) const;
 
-    T		start;
-    T		step;
-
+    T					start;
+    T					step;
 };
+
+
+template <class T> inline
+SamplingData<T>::SamplingData( T sa, T se )
+    : start(sa), step(se)
+{}
+
+
+template <class T> 
+template <class FT> inline
+SamplingData<T>::SamplingData( const SamplingData<FT>& templ )
+    : start( templ.start ), step( templ.step )
+{}
+
+
+template <class T>
+template <class FT> inline
+SamplingData<T>::SamplingData( const StepInterval<FT>& intv )
+    : start(intv.start), step(intv.step)
+{}
+
+
+template <class T> inline
+void SamplingData<T>::operator+=( const SamplingData& sd )
+{ start += sd.start; step += sd.step; }
+
+
+template <class T> inline
+int SamplingData<T>::operator==( const SamplingData& sd ) const
+{ return start == sd.start && step == sd.step; }
+
+
+template <class T> inline
+int SamplingData<T>::operator!=( const SamplingData& sd ) const
+{ return ! (sd == *this); }
+
+
+template <class T> inline
+StepInterval<T> SamplingData<T>::interval( int nrsamp ) const
+{ return StepInterval<T>( start, start+(nrsamp-1)*step, step); }
+
+
+template <class T>
+template <class IT> inline
+float SamplingData<T>::getIndex( IT val ) const
+{ return (val-start) / (float) step; }
+
+
+template <class T> inline
+int SamplingData<T>::nearestIndex( T x ) const
+{ float fidx = getIndex(x); return mNINT(fidx); }
+
+
+template <class T>
+template <class IT> inline
+T SamplingData<T>::atIndex( IT idx ) const
+{ return start + step * idx; }
 
 
 #endif
