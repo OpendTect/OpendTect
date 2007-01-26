73ea62e66dc2d38a17859d7f2cfc6b678fa4e7c3
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2007-01-26 17:50:54 +0000

    Added SoColTabMultiTexture2.
    git-svn-id: https://svn.opendtect.org/od/trunk@8742 a985855d-d384-4b45-ae33-31a410998c3c

--- src/SoOD/SoOD.cc
+++ src/SoOD/SoOD.cc
@@ -4,7 +4,7 @@
  * DATE     : Oct 1999
 -*/
 
-static const char* rcsID = "$Id: SoOD.cc,v 1.7 2007-01-03 19:35:40 cvskris Exp $";
+static const char* rcsID = "$Id: SoOD.cc,v 1.8 2007-01-26 17:50:54 cvskris Exp $";
 
 
 #include "SoOD.h"
@@ -14,12 +14,12 @@ static const char* rcsID = "$Id: SoOD.cc,v 1.7 2007-01-03 19:35:40 cvskris Exp $
 #include "SoArrow.h"
 #include "SoCameraInfo.h"
 #include "SoCameraInfoElement.h"
+#include "SoColTabMultiTexture2.h"
 #include "SoDepthTabPlaneDragger.h"
 #include "SoForegroundTranslation.h"
 #include "SoIndexedTriangleFanSet.h"
 #include "SoLODMeshSurface.h"
 #include "SoManLevelOfDetail.h"
-#include "SoMultiTexture2.h"
 #include "SoMFImage.h"
 #include "SoPerspectiveSel.h"
 #include "SoPlaneWellLog.h"
@@ -48,12 +48,12 @@ void SoOD::init()
     //Then nodes
     SoArrow::initClass();
     SoCameraInfo::initClass();
+    SoColTabMultiTexture2::initClass();
     SoDepthTabPlaneDragger::initClass();
     SoForegroundTranslation::initClass();
     SoIndexedTriangleFanSet::initClass();
     SoLODMeshSurface::initClass();
     SoManLevelOfDetail::initClass();
-    SoMultiTexture2::initClass();
     SoPerspectiveSel::initClass();
     SoPlaneWellLog::initClass();
     SoIndexedLineSet3D::initClass();
@@ -65,5 +65,4 @@ void SoOD::init()
     SoGridSurfaceDragger::initClass();
     UTMCamera::initClass();
     UTMPosition::initClass();
-
 }
