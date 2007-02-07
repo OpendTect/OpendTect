b9430bbcb7cc5c34689a1c0bdcdd341952a9cea0
Nanne Hemstra <nanne.hemstra@dgbes.com>
2007-02-07 16:44:02 +0000

    cursor fix
    git-svn-id: https://svn.opendtect.org/od/trunk@8891 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiBase/uicursor.cc
+++ src/uiBase/uicursor.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H. Lammertink
  Date:          12/05/2004
- RCS:           $Id: uicursor.cc,v 1.4 2006-12-08 15:58:03 cvsnanne Exp $
+ RCS:           $Id: uicursor.cc,v 1.5 2007-02-07 16:44:02 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -17,7 +17,8 @@ ________________________________________________________________________
 
 void uiCursor::setOverride( uiCursor::Shape shape, bool replace )
 {
-    QApplication::setOverrideCursor( QCursor((int)shape), replace );
+    Qt::CursorShape qshape = (Qt::CursorShape)(int)shape;
+    QApplication::setOverrideCursor( QCursor(qshape), replace );
 }
 
 
