d0baab036c50d49a547df341fe633af82affe79f
Nanne Hemstra <nanne.hemstra@dgbes.com>
2007-03-06 07:46:48 +0000

    Qt3 -> Qt4
    git-svn-id: https://svn.opendtect.org/od/trunk@9091 a985855d-d384-4b45-ae33-31a410998c3c

--- include/General/keyenum.h
+++ include/General/keyenum.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        H. Payraudeau
  Date:          27/01/2006
- RCS:           $Id: keyenum.h,v 1.1 2006-09-12 18:39:23 cvskris Exp $
+ RCS:           $Id: keyenum.h,v 1.2 2007-03-06 07:46:48 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -19,17 +19,17 @@ class OD
 public:
     enum		ButtonState 
     			{ //!< Qt's mouse/keyboard state values
-			    NoButton        = 0x0000,
-			    LeftButton      = 0x0001,
-			    RightButton     = 0x0002,
-			    MidButton       = 0x0004,
-			    MouseButtonMask = 0x00ff,
-			    ShiftButton     = 0x0100,
-			    ControlButton   = 0x0200,
-			    AltButton       = 0x0400,
-			    MetaButton      = 0x0800,
-			    KeyButtonMask   = 0x0fff,
-			    Keypad          = 0x4000
+			    NoButton        = 0x00000000,
+			    LeftButton      = 0x00000001,
+			    RightButton     = 0x00000002,
+			    MidButton       = 0x00000004,
+			    MouseButtonMask = 0x000000ff,
+			    ShiftButton     = 0x02000000,
+			    ControlButton   = 0x04000000,
+			    AltButton       = 0x08000000,
+			    MetaButton      = 0x10000000,
+			    KeyButtonMask   = 0xfe000000,
+			    Keypad          = 0x20000000
 			};
     			DeclareEnumUtils(ButtonState);
 };
