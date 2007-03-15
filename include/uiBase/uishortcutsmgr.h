ab0c4f1c46b0bff963082d74757c0077f5d04171
Bert Bril <bert.bril@dgbes.com>
2007-03-15 16:18:58 +0000

    Big rework
    git-svn-id: https://svn.opendtect.org/od/trunk@9215 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiBase/uishortcutsmgr.h
+++ include/uiBase/uishortcutsmgr.h
@@ -7,88 +7,107 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        H. Payraudeau
  Date:          20/01/2006
- RCS:           $Id: uishortcutsmgr.h,v 1.5 2006-05-08 07:06:31 cvshelene Exp $
+ RCS:           $Id: uishortcutsmgr.h,v 1.6 2007-03-15 16:18:48 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "iopar.h"
 #include "enums.h"
+#include "keyenum.h"
 #include "bufstringset.h"
-
-
-class uiShortcutsList;
-class uiShortcutsMgr
-{
-public:
-			uiShortcutsMgr()			{};
-
-    IOPar*		readShortcutsFile(const char*);
-    			// IOPar becomes yours!
-    
-    uiShortcutsList*	getList(const char*);
-};
-
-uiShortcutsMgr& SCMgr();
-
-
-
 class QKeyEvent;
-class uiKeyDesc;
 
-class uiShortcutsList
-{
-public:
-    			uiShortcutsList(const char*);
-    			~uiShortcutsList() 		{ deepErase(keyslist_);}
 
-    void		init(const char*);
-    
-    bool		getKeyValues(const IOPar&,int,
-	    			     BufferString&,BufferString&);
-    bool		getSCNames(const IOPar&,int,BufferString&);
-    const char*		getAct(QKeyEvent*);		
-    
-    ObjectSet<uiKeyDesc>& getKeysList()			{ return keyslist_; }	
-    BufferStringSet& 	getNamesList()			{ return nameslist_; }
-
-    static const char*  ODSceneStr()			{ return "ODScene"; }   
-
-protected:
-				
-    ObjectSet<uiKeyDesc> keyslist_;
-    BufferStringSet	nameslist_;
-};
+class uiShortcutsMgr;
+uiShortcutsMgr& SCMgr(); //!< This is where you get your shortcuts
 
 
 class uiKeyDesc
 {
 public:
-			uiKeyDesc(QKeyEvent*);
-			~uiKeyDesc();
-			uiKeyDesc(const char*,const char*);
+			uiKeyDesc(const char* statestr=0,const char* keystr=0);
 			
     bool		operator==(const uiKeyDesc& ev) const
 			{ return key_==ev.key_ && state_==ev.state_; }
 
-    bool		set( const char* str1, const char* str2 );
-    bool		set(QKeyEvent*);
+    bool		set(const char* statestr,const char* keystr);
     
-    int			state() const 		{ return state_; }
+    OD::ButtonState	state() const 		{ return state_; }
+    void		setState( OD::ButtonState bs )	{ state_ = bs; }
     int			key() const 		{ return key_; }
-    QKeyEvent*		keyEv() const		{ return qkeyev_; }
-    void		setState(int);
+    void		setKey( int k )		{ key_ = k; }
     char 		asciiChar() const;
     bool		isSimpleAscii() const;
+
+    static const char**	sKeyKeyStrs();
+    const char*		stateStr() const;
+    const char*		keyStr() const;
+
+    			uiKeyDesc(QKeyEvent*);
 
 protected:
 
     int			key_;
-    int			state_;
-    QKeyEvent*		qkeyev_;
-    bool		qkeyevmine_;
+    OD::ButtonState	state_;
 
     void		handleSpecialKey(const char*);
+
+};
+
+
+class uiShortcutsList
+{
+public:
+
+    			uiShortcutsList( const uiShortcutsList& scl )
+							{ *this = scl; }
+    			~uiShortcutsList() 		{ empty(); }
+    uiShortcutsList&	operator =(const uiShortcutsList&);
+    bool		write(bool usr=true) const;
+
+    void		fillPar(IOPar&) const;
+    
+    TypeSet<uiKeyDesc>& keyDescs()		{ return keydescs_; }	
+    const TypeSet<uiKeyDesc>& keyDescs() const { return keydescs_; }	
+    BufferStringSet& 	names()			{ return names_; }
+    const BufferStringSet&  names() const	{ return names_; }
+    uiKeyDesc		keyDescOf(const char*) const;
+    const char*		nameOf(const uiKeyDesc&) const;
+
+    void		empty();
+
+protected:
+
+    friend class	uiShortcutsMgr;
+    			uiShortcutsList(const char* selkey);
+
+    BufferString	selkey_;
+    TypeSet<uiKeyDesc>	keydescs_;
+    BufferStringSet	names_;
+
+    bool		getKeyValues(const IOPar&,int,
+	    			     BufferString&,BufferString&) const;
+    bool		getSCNames(const IOPar&,int,BufferString&) const;
+};
+
+
+class uiShortcutsMgr
+{
+public:
+			uiShortcutsMgr()			{};
+    
+    const uiShortcutsList& getList(const char* key) const;
+    bool		setList(const uiShortcutsList&,bool usr=true);
+
+protected:
+
+    BufferStringSet	keys_;
+    ObjectSet<uiShortcutsList>	lists_;
+
+    friend class	uiShortcutsList;
+    IOPar*		getStored(const char*);
+    bool		putStored(const char*,const IOPar&);
 
 };
 
--- src/uiBase/uishortcutsmgr.cc
+++ src/uiBase/uishortcutsmgr.cc
@@ -15,82 +15,139 @@ ________________________________________________________________________
 #include "settings.h"
 #include "keyenum.h"
 #include "keystrs.h"
+#include "strmprov.h"
+#include "ascstream.h"
+#include "errh.h"
 #include <qevent.h>
 
 uiShortcutsMgr& SCMgr()
 {
-    static PtrMan<uiShortcutsMgr> scmgr = 0;
-
-    if ( !scmgr ) 
-	scmgr = new uiShortcutsMgr();
-	    
+    static uiShortcutsMgr* scmgr = 0;
+    if ( !scmgr ) scmgr = new uiShortcutsMgr;
     return *scmgr;
 }
 
 
-IOPar* uiShortcutsMgr::readShortcutsFile( const char* subsel )
+const char** uiKeyDesc::sKeyKeyStrs()
 {
-    IOPar tmppars;
-    IOPar* iopar;
-    bool isdefaultfile = false;
-    BufferString firstsc;
-    BufferString basekey = IOPar::compKey( sKey::Shortcuts, subsel );
-
-    basekey = IOPar::compKey( basekey, 0 );
-    if ( !mSettUse(get,basekey,"Name",firstsc) && 
-	 ( !mSettUse(get,"Shortcuts.0","Name",firstsc)//compat with old files
-	   && !strcmp( subsel, uiShortcutsList::ODSceneStr() ) ) )
-    {
-	tmppars.read( GetDataFileName(sKey::Shortcuts), 0 );
-	isdefaultfile = true;
-    }
-    else
-	tmppars = Settings::common();
-
-    if ( isdefaultfile )
-	iopar = tmppars.subselect( subsel );
-    else
-    {
-	iopar = tmppars.subselect( IOPar::compKey( sKey::Shortcuts,subsel ) );
-
-	if ( !iopar && !strcmp( subsel, uiShortcutsList::ODSceneStr() ) )
-	    iopar = tmppars.subselect( sKey::Shortcuts );
-    }
-
-    return iopar;
+    static const char* strs[] =
+{ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S",
+  "T","U","V","W","X","Y","Z","Plus","Minus","Asterisk","Slash","Up","Down",
+  "Left","Right","Delete","PageUp","PageDown", 0 };
+    return strs;
 }
 
 
-uiShortcutsList* uiShortcutsMgr::getList( const char* subsel )
-{
-    static PtrMan<uiShortcutsList> scodscenelist = 0;
-    //if some shortcuts are added to other places, like 2D viewer for instance,
-    //just create another case here. example:
-    //static PtrMan<ShortcutsList> sc2dviewerlist = 0;
-   
-    if ( !strcmp( subsel, uiShortcutsList::ODSceneStr() ) )
-    {
-        if ( !scodscenelist )
-	    scodscenelist = new uiShortcutsList( uiShortcutsList::ODSceneStr());
+const int speckeystransbl[] =
+{ 0x2b, 0x2d, 0x2a, 0x2f,
+  0x1013, 0x1015, 0x1012, 0x1014, 0x1007, 0x1016, 0x1017 };
 
-	return scodscenelist;
+
+#define mQtbaseforA 0x41
+#define mQtbasefor0 0x30
+
+
+uiKeyDesc::uiKeyDesc( QKeyEvent* ev )
+    : key_(ev->key())
+    , state_((OD::ButtonState)ev->state())
+{
+    if ( state_ >= OD::Keypad )
+	state_ = (OD::ButtonState)((int)state_ - (int)OD::Keypad);
+}
+
+
+uiKeyDesc::uiKeyDesc( const char* str1, const char* str2 )
+    : state_(OD::NoButton)
+    , key_(0)
+{
+    set( str1, str2 );
+}
+
+
+bool uiKeyDesc::set( const char* statestr, const char* keystr )
+{
+    if ( !statestr )
+	statestr = "";
+
+    if ( *statestr == 'S' || *statestr == 's' )
+	state_ = OD::ShiftButton;
+    if ( *statestr == 'C' || *statestr == 'c' )
+	state_ = OD::ControlButton;
+    // Not considering 'Alt' button because that's usually OS-related
+
+    if ( !keystr || *keystr == '\0' )
+	return false;
+
+    if ( keystr[1] != '\0' )
+    {
+	const char** strs = sKeyKeyStrs();
+	for ( int idx=26; strs[idx]; idx++ )
+	{
+	    if ( !strcmp( keystr, strs[idx] ) )
+		{ key_ = speckeystransbl[idx-26]; break; }
+	}
+    }
+    else
+    {
+	char charfound = *keystr;
+	if ( (charfound >= 'a' && charfound <= 'z') )
+	    key_ = mQtbaseforA + charfound - 'a';
+	else if ( (charfound >= 'A' && charfound <= 'Z') )
+	    key_ = mQtbaseforA + charfound - 'A';
+	else if ( charfound >= '0' && charfound <= '9' )
+	    key_ = mQtbasefor0 + charfound - '0';
     }
 
+    return key_ != 0;
+}
+
+
+const char* uiKeyDesc::stateStr() const
+{
+    return OD::nameOf( state_ );
+}
+
+
+const char* uiKeyDesc::keyStr() const
+{
+    const char** strs = sKeyKeyStrs();
+
+    if ( key_ >= mQtbaseforA && key_ < mQtbaseforA + 26 )
+	return strs[ key_ - mQtbaseforA ];
+    if ( key_ >= mQtbasefor0 && key_ < mQtbasefor0 + 10 )
+	return strs[ key_ - mQtbasefor0 ];
+
+    for ( int idx=26; ; idx++ )
+    {
+	if ( speckeystransbl[idx-26] == key_ )
+	    return strs[idx];
+    }
     return 0;
 }
 
+#define mDefQKeyEv \
+    QKeyEvent qke( QEvent::KeyPress, key_, 0, (int)state_ )
 
-uiShortcutsList::uiShortcutsList( const char* elementsel )
+
+bool uiKeyDesc::isSimpleAscii() const
 {
-    init( elementsel );
+    mDefQKeyEv;
+    return state_== 0 && isascii( *(qke.text()) );
 }
 
 
-void uiShortcutsList::init( const char* elementsel )
+char uiKeyDesc::asciiChar() const
 {
-    deepErase( keyslist_ );
-    nameslist_.deepErase();
-    PtrMan<IOPar> pars = SCMgr().readShortcutsFile( elementsel );
+    if ( !isascii(key_) ) return 0;
+    mDefQKeyEv;
+    return qke.ascii();
+}
+
+
+uiShortcutsList::uiShortcutsList( const char* selkey )
+	: selkey_(selkey)
+{
+    PtrMan<IOPar> pars = SCMgr().getStored( selkey );
     if ( !pars ) return;
     
     int index = 0;
@@ -104,16 +161,51 @@ void uiShortcutsList::init( const char* elementsel )
 	if ( !getKeyValues( *pars, index, val1, val2 ) )
 	    return;
 
-	nameslist_.add( name );
-	uiKeyDesc* ev = new uiKeyDesc( val1, val2 );
-	keyslist_ += ev;
+	names_.add( name );
+	keydescs_ += uiKeyDesc( val1, val2 );
 	index++;
     }
 }
 
 
+uiShortcutsList& uiShortcutsList::operator =( const uiShortcutsList& scl )
+{
+    if ( this == &scl ) return *this;
+    empty();
+    selkey_ = scl.selkey_;
+    keydescs_ = scl.keydescs_;
+    names_ = scl.names_;
+    return *this;
+}
+
+
+void uiShortcutsList::empty()
+{
+    keydescs_.erase(); names_.deepErase();
+}
+
+
+bool uiShortcutsList::write( bool usr ) const
+{
+    return SCMgr().setList( *this, usr );
+}
+
+
+void uiShortcutsList::fillPar( IOPar& iop ) const
+{
+    for ( int idx=0; idx<names_.size(); idx++ )
+    {
+	BufferString basekey = IOPar::compKey(selkey_,idx);
+	iop.set( IOPar::compKey(basekey,sKey::Name), names_.get(idx) );
+	iop.set( IOPar::compKey(basekey,sKey::Keys),
+			keydescs_[idx].stateStr(), keydescs_[idx].keyStr() );
+    }
+}
+
+
 bool uiShortcutsList::getKeyValues( const IOPar& par, int scutidx,
-				    BufferString& val1, BufferString& val2 )
+				    BufferString& val1,
+				    BufferString& val2 ) const
 {
     BufferString scutidxstr = scutidx;
     BufferString key = IOPar::compKey( scutidxstr.buf(), sKey::Keys );
@@ -121,8 +213,30 @@ bool uiShortcutsList::getKeyValues( const IOPar& par, int scutidx,
 }
 
 
+uiKeyDesc uiShortcutsList::keyDescOf( const char* nm ) const
+{
+    for ( int idx=0; idx<names_.size(); idx++ )
+    {
+	if ( names_.get(idx) == nm )
+	    return keydescs_[idx];
+    }
+    return uiKeyDesc(0,0);
+}
+
+
+const char* uiShortcutsList::nameOf( const uiKeyDesc& kd ) const
+{
+    for ( int idx=0; idx<names_.size(); idx++ )
+    {
+	if ( keydescs_[idx] == kd )
+	    return names_.get( idx );
+    }
+    return 0;
+}
+
+
 bool uiShortcutsList::getSCNames( const IOPar& par, int scutidx,
-				  BufferString& name ) 
+				  BufferString& name ) const
 {
     BufferString scutidxstr = scutidx;
     BufferString key = IOPar::compKey( scutidxstr.buf(), sKey::Name );
@@ -130,148 +244,63 @@ bool uiShortcutsList::getSCNames( const IOPar& par, int scutidx,
 }
 
 
-const char* uiShortcutsList::getAct( QKeyEvent* event )
+static const char* sFileKey = "shortcuts";
+
+IOPar* uiShortcutsMgr::getStored( const char* subsel )
 {
-    uiKeyDesc kd( event );
-    if ( kd.state() >= (int)OD::Keypad )
-	kd.setState( kd.state() - (int)OD::Keypad );
-    
-    const int index = indexOf( keyslist_, kd );
-    return index>-1 ? nameslist_.get(index).buf() : 0;
-}
+    Settings& setts = Settings::fetch( sFileKey );
+    IOPar* ret = setts.subselect( subsel );
+    if ( ret && ret->size() )
+	return ret;
+    delete ret;
 
-//---------------------------------------------------------------------------
-#define mQtbaseforA 0x41
-#define mQtbasefor0 0x30
+    StreamData sd = StreamProvider(GetDataFileName("ShortCuts")).makeIStream();
+    if ( !sd.usable() )
+	{ sd.close(); return 0; }
 
+    ascistream astrm( *sd.istrm );
+    IOPar* iop = new IOPar( astrm );
+    sd.close();
+    if ( iop && iop->isEmpty() )
+	{ delete iop; return 0; }
 
+    ret = iop->subselect( subsel );
+    delete iop;
+    if ( ret && ret->isEmpty() )
+	{ delete ret; return 0; }
 
-uiKeyDesc::uiKeyDesc( QKeyEvent* event )
-	: state_( event->state() )
-	, key_( event->key() )
-   	, qkeyev_( event )
-   	, qkeyevmine_( false ) 
-{
-    set( event );
-}
- 
-
-uiKeyDesc::uiKeyDesc( const char* str1, const char* str2 )
-    : state_(0)
-    , key_(0)
-    , qkeyev_(0)
-    , qkeyevmine_( false )
-{
-    set( str1, str2 );
+    return ret;
 }
 
 
-uiKeyDesc::~uiKeyDesc()
+const uiShortcutsList& uiShortcutsMgr::getList( const char* key ) const
 {
-    if ( qkeyev_ && qkeyevmine_ ) delete qkeyev_;
+    const int idx = keys_.indexOf( key );
+    if ( idx >= 0 ) return *lists_[idx];
+
+    uiShortcutsMgr& self = *(const_cast<uiShortcutsMgr*>(this));
+    uiShortcutsList* newlist = new uiShortcutsList( key );
+    self.keys_.add( key );
+    self.lists_ += newlist;
+    return *newlist;
 }
 
 
-bool uiKeyDesc::set( QKeyEvent* event )
+bool uiShortcutsMgr::setList( const uiShortcutsList& scl, bool usr )
 {
-    bool isudf;
-    if ( mIsUdf(state_) )
+    if ( !usr )
     {
-	state_ = 0;
-	isudf = true;
-    }
-    if ( mIsUdf(key_) )
-    {
-	key_ = 0;
-	isudf = true;
+	pErrMsg( "Needs implementation" );
+	return false;
     }
 
-    return !isudf;
-}
-
-
-bool uiKeyDesc::set( const char* str1, const char* str2 )
-{
-    if ( *str1 == 'S' || *str1 == 's' )
-	state_ = OD::ShiftButton;
-    if ( *str1 == 'C' || *str1 == 'c' )
-	state_ = OD::ControlButton;
-    // Not considering 'Alt' button because that's most often an OS-related key
-
-    if ( *str2 == '\0' )
+    IOPar iop; scl.fillPar( iop );
+    Settings& setts = Settings::fetch( sFileKey );
+    setts.merge( iop );
+    if ( !setts.write(false) )
 	return false;
 
-    int keypressed = 0;
-    if ( str2[1] == '\0' )
-    {
-	char charfound = *str2;
-	if ( (charfound >= 'a' && charfound <= 'z') )
-	    key_ = mQtbaseforA + charfound - 'a';
-	else if ( (charfound >= 'A' && charfound <= 'Z') )
-	    key_ = mQtbaseforA + charfound - 'A';
-	else if ( charfound >= '0' && charfound <= '9' )
-	    key_ = mQtbasefor0 + charfound - '0';
-    }
-    else
-	handleSpecialKey(str2);
-
-    if ( qkeyev_ && qkeyevmine_ ) delete qkeyev_;
-    
-    if ( key_!=0 && state_!=0 )
-	qkeyev_ = new QKeyEvent( QEvent::KeyPress, key_, 0, state_ );
-    else
-	qkeyev_ = new QKeyEvent( QEvent::KeyPress, 0, 0, 0 );
-    
-    qkeyevmine_ = true;
-    return key_ != 0;
-}
-
-
-void uiKeyDesc::handleSpecialKey( const char* str )
-{
-    if ( !strcmp( str, "Plus" ) )
-	key_ = 0x2b;
-    else if ( !strcmp( str, "Minus" ) )
-	key_ = 0x2d;
-    else if ( !strcmp( str, "Asterisk" ) )
-	key_ = 0x2a;
-    else if ( !strcmp( str, "Slash" ) )
-	key_ = 0x2f;
-    else if ( !strcmp( str, "Delete" ) )
-	key_ = 0x1007;
-    else if ( !strcmp( str, "Left" ) )
-	key_ = 0x1012;
-    else if ( !strcmp( str, "Up" ) )
-	key_ = 0x1013;
-    else if ( !strcmp( str, "Right" ) )
-	key_ = 0x1014;
-    else if ( !strcmp( str, "Down" ) )
-	key_ = 0x1015;
-    else if ( !strcmp( str, "PageUp" ) )
-	key_ = 0x1016;
-    else if ( !strcmp( str, "PageDown" ) )
-	key_ = 0x1017;
-}
-
-
-bool uiKeyDesc::isSimpleAscii() const
-{
-    return state_== 0 && isascii( *(qkeyev_->text()) );
-}
-
-
-char uiKeyDesc::asciiChar() const
-{
-    if ( !isascii(key_) ) return 0;
-    return qkeyev_->ascii();
-}
-
-
-void uiKeyDesc::setState( int state )
-{
-    state_  = state;
-    if ( qkeyev_ && qkeyevmine_ ) delete qkeyev_;
-
-    qkeyev_ = new QKeyEvent( QEvent::KeyPress, key_, 0, state_ );
-    qkeyevmine_ = true;
+    uiShortcutsList& myscl = const_cast<uiShortcutsList&>(getList(scl.selkey_));
+    myscl = scl;
+    return true;
 }
