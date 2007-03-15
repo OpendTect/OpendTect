f1bca8391d748ee49c5c009fbaf0f4127f9f13cd
Bert Bril <bert.bril@dgbes.com>
2007-03-15 16:15:06 +0000

    Fully improved implementation that actually works
    git-svn-id: https://svn.opendtect.org/od/trunk@9208 a985855d-d384-4b45-ae33-31a410998c3c

--- src/General/keyenum.cc
+++ src/General/keyenum.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        H. Payraudeau
  Date:          30/01/2006
- RCS:           $Id: keyenum.cc,v 1.1 2006-09-12 18:40:54 cvskris Exp $
+ RCS:           $Id: keyenum.cc,v 1.2 2007-03-15 16:15:06 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -12,19 +12,53 @@ ________________________________________________________________________
 
 #include "keyenum.h"
 
-DefineEnumNames(OD,ButtonState,0,"Button State")
+static const char* nms[] = 
 {
-	"NoButton",
-	"LeftButton",
-	"RightButton",
-	"MidButton",
-	"MouseButtonMask",
-	"ShiftButton",
-	"ControlButton",
-	"AltButton",
-	"MetaButton",
-	"KeyButtonMask",
+	"----",
+	"Left Mouse",
+	"Right Mouse",
+	"Mid Mouse",
+	"Button Mask/Mouse",
+	"Shift",
+	"Control",
+	"Alt",
+	"Meta key",
+	"Button Mask/Key",
 	"Keypad",
 	0
 };
 
+const int transtbl[] =
+{ 0x00000000, 0x00000001, 0x00000002, 0x00000004, 0x000000ff, 0x02000000,
+  0x04000000, 0x08000000, 0x10000000, 0xfe000000, 0x20000000 };
+
+
+OD::ButtonState OD::stateOf( const char* nm )
+{
+    if ( !nm || !*nm ) return OD::NoButton;
+
+    for ( int idx=0; nms[idx]; idx++ )
+    {
+	if ( !strcmp(nms[idx],nm) )
+	    return (OD::ButtonState)(transtbl[idx]);
+    }
+
+    for ( int idx=0; nms[idx]; idx++ )
+    {
+	if ( *nms[idx] == *nm )
+	    return (OD::ButtonState)(transtbl[idx]);
+    }
+
+    return OD::NoButton;
+}
+
+
+const char* OD::nameOf( OD::ButtonState state )
+{
+    for ( int idx=0; nms[idx]; idx++ )
+    {
+	if ( transtbl[idx] == (int)state )
+	    return nms[idx];
+    }
+    return nms[0];
+}
