bf69173f8040e1081a5b5b2a2f876bb838cac7b6
Bert Bril <bert.bril@dgbes.com>
2007-03-15 16:15:21 +0000

    enum utils didn;t work
    git-svn-id: https://svn.opendtect.org/od/trunk@9209 a985855d-d384-4b45-ae33-31a410998c3c

--- include/General/keyenum.h
+++ include/General/keyenum.h
@@ -7,31 +7,33 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        H. Payraudeau
  Date:          27/01/2006
- RCS:           $Id: keyenum.h,v 1.2 2007-03-06 07:46:48 cvsnanne Exp $
+ RCS:           $Id: keyenum.h,v 1.3 2007-03-15 16:15:21 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "enums.h"
 
-class OD
+namespace OD
 {
-public:
-    enum		ButtonState 
-    			{ //!< Qt's mouse/keyboard state values
-			    NoButton        = 0x00000000,
-			    LeftButton      = 0x00000001,
-			    RightButton     = 0x00000002,
-			    MidButton       = 0x00000004,
-			    MouseButtonMask = 0x000000ff,
-			    ShiftButton     = 0x02000000,
-			    ControlButton   = 0x04000000,
-			    AltButton       = 0x08000000,
-			    MetaButton      = 0x10000000,
-			    KeyButtonMask   = 0xfe000000,
-			    Keypad          = 0x20000000
-			};
-    			DeclareEnumUtils(ButtonState);
+    enum ButtonState 
+    { //!< Qt's mouse/keyboard state values
+			NoButton        = 0x00000000,
+			LeftButton      = 0x00000001,
+			RightButton     = 0x00000002,
+			MidButton       = 0x00000004,
+			MouseButtonMask = 0x000000ff,
+			ShiftButton     = 0x02000000,
+			ControlButton   = 0x04000000,
+			AltButton       = 0x08000000,
+			MetaButton      = 0x10000000,
+			KeyButtonMask   = 0xfe000000,
+			Keypad          = 0x20000000
+    };
+
+    const char*		nameOf(ButtonState);
+    ButtonState		stateOf(const char*);
+
 };
 
 #endif
