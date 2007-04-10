608552005e004c52a4ea8c78d5b2857c53abe2f5
Bert Bril <bert.bril@dgbes.com>
2007-04-10 08:00:59 +0000

    signal names changed in Qt4
    git-svn-id: https://svn.opendtect.org/od/trunk@9376 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiBase/i_qlistbox.h
+++ include/uiBase/i_qlistbox.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H. Lammertink
  Date:          16/05/2000
- RCS:           $Id: i_qlistbox.h,v 1.2 2006-04-28 15:23:20 cvsnanne Exp $
+ RCS:           $Id: i_qlistbox.h,v 1.3 2007-04-10 08:00:59 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -18,9 +18,7 @@ ________________________________________________________________________
 #include <qwidget.h>
 #include <string.h>
 
-#include <q3listbox.h>
-#define Q3ListBox	Q3ListBox
-#define Q3ListBoxItem  Q3ListBoxItem
+#include <QListWidget>
 
 //! Helper class for uiListBox to relay Qt's 'activated' messages to uiMenuItem.
 /*!
@@ -32,22 +30,17 @@ class i_listMessenger : public QObject
     friend class	uiListBoxBody;
 
 protected:
-			i_listMessenger( Q3ListBox*  sender,
+			i_listMessenger( QListWidget*  sender,
 					 uiListBox* receiver )
 			: _sender( sender )
 			, _receiver( receiver )
 			{ 
-			    connect( sender, SIGNAL( selectionChanged ()),
-				     this,   SLOT( selectionChanged ()));
+			    connect( sender,SIGNAL( itemSelectionChanged()),
+				     this,SLOT( selectionChanged ()));
 
-			    connect( sender, 
-				     SIGNAL( doubleClicked(Q3ListBoxItem*) ),
-				     this, SLOT( doubleClicked(Q3ListBoxItem*)));
-
-			    connect( sender, SIGNAL( 
-			      rightButtonClicked(Q3ListBoxItem*,const QPoint&)),
-			      this, SLOT( 
-			      rightButtonClicked(Q3ListBoxItem*,const QPoint&)));
+			    connect( sender,
+				    SIGNAL(itemDoubleClicked(QListWidgetItem*)),
+				    this,SLOT(doubleClicked(QListWidgetItem*)));
 
 			}
 
@@ -56,39 +49,23 @@ protected:
 private:
 
     uiListBox* 		_receiver;
-    Q3ListBox*  	_sender;
+    QListWidget*  	_sender;
 
 private slots:
 
 
-/*!
-    Handler for selectionChanged events.
-    \sa QListBox::selectionChanged
-*/
     void 		selectionChanged() 
-			{_receiver->selectionChanged.trigger(*_receiver);}
-    
-/*!
-    Handler for doubleClicked events.
-    \sa QListBox::doubleClicked
-*/
-    void		doubleClicked( Q3ListBoxItem * item )
 			{
-			    int idx = _sender->index(item);
+			    _receiver->selectionChanged.trigger( *_receiver );
+			}
+    
+    void		doubleClicked( QListWidgetItem * item )
+			{
+			    int idx = _sender->row(item);
 			    _receiver->lastClicked_ = idx;
 			    _receiver->doubleClicked.trigger(*_receiver);
 			}
 
-/*!
-    Handler for rightButtonClicked events.
-    \sa QListBox::rightButtonClicked
-*/
-    void		rightButtonClicked( Q3ListBoxItem* item, const QPoint&)
-			{
-			    int idx = _sender->index(item);
-			    _receiver->lastClicked_ = idx;
-			    _receiver->rightButtonClicked.trigger(*_receiver);
-			}
 
 };
 
