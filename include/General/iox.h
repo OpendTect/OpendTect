b19a6024f818138d3faec96dd10943590859948e
Nanne Hemstra <nanne.hemstra@dgbes.com>
2007-05-04 11:40:49 +0000

    added again
    git-svn-id: https://svn.opendtect.org/od/trunk@9472 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/iox.h
@@ -0,0 +1,54 @@
+#ifndef iox_h
+#define iox_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	A.H.Bril
+ Date:		25-7-1997
+ Contents:	IOObj on other IOObj
+ RCS:		$Id: iox.h,v 1.13 2007-05-04 11:40:49 cvsnanne Exp $
+________________________________________________________________________
+
+-*/
+
+#include <ioobject.h>
+
+
+/*\brief is a X-Group entry in the omf, e.g. Seismic data based upon a
+Feature Set. */
+
+class IOX : public IOObject
+{
+public:
+			IOX(const char* nm=0,const char* ky=0,bool =0);
+    virtual		~IOX();
+    bool		bad() const;
+
+    void		copyFrom(const IOObj*);
+    const char*		fullUserExpr(bool) const;
+    void		genDefaultImpl()		{}
+
+    bool		implExists(bool) const;
+
+    const char*		connType() const;
+    Conn*		getConn(Conn::State) const;
+    bool		slowOpen() const;
+    IOObj*		getIOObj() const;
+
+    const MultiID&	ownKey() const			{ return ownkey_; }
+    void		setOwnKey(const MultiID&);
+
+protected:
+
+    MultiID		ownkey_;
+
+    int			getFrom(ascistream&);
+    int			putTo(ascostream&) const;
+
+    static int		prodid; //!< for factory implementation
+};
+
+
+#endif
---
+++ src/General/iox.cc
@@ -0,0 +1,135 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : A.H. Bril
+ * DATE     : Oct 1995
+ * FUNCTION : Translator functions
+-*/
+
+static const char* rcsID = "$Id: iox.cc,v 1.14 2007-05-04 11:36:44 cvsnanne Exp $";
+
+#include "iox.h"
+#include "iostrm.h"
+#include "iolink.h"
+#include "ioman.h"
+#include "ascstream.h"
+#include "conn.h"
+
+class IOXProducer : public IOObjProducer
+{
+    bool	canMake( const char* typ ) const
+		{ return !strcmp(typ,XConn::sType); }
+    IOObj*	make( const char* nm, const MultiID& ky, bool fd ) const
+		{ return new IOX(nm,ky,fd); }
+};
+
+int IOX::prodid = IOObj::addProducer( new IOXProducer );
+
+
+IOX::IOX( const char* nm, const char* ky, bool )
+	: IOObject(nm,ky)
+	, ownkey_("")
+{
+}
+
+
+IOX::~IOX()
+{
+}
+
+
+void IOX::setOwnKey( const MultiID& ky )
+{
+    ownkey_ = ky;
+}
+
+
+const char* IOX::connType() const
+{
+    return XConn::sType;
+}
+
+
+bool IOX::bad() const
+{
+    return ownkey_ == "";
+}
+
+
+void IOX::copyFrom( const IOObj* obj )
+{
+    if ( !obj ) return;
+    if ( obj->isLink() ) obj = ((IOLink*)obj)->link();
+    if ( !obj ) return;
+
+    IOObj::copyFrom(obj);
+    mDynamicCastGet(const IOX*,trobj,obj)
+    if ( trobj )
+	ownkey_ = trobj->ownkey_;
+}
+
+
+const char* IOX::fullUserExpr( bool i ) const
+{
+    IOObj* ioobj = IOM().get( ownkey_ );
+    if ( !ioobj ) return "<invalid>";
+    const char* s = ioobj->fullUserExpr(i);
+    delete ioobj;
+    return s;
+}
+
+
+bool IOX::slowOpen() const
+{
+    IOObj* ioobj = IOM().get( ownkey_ );
+    if ( !ioobj ) return false;
+    bool ret = ioobj->slowOpen();
+    delete ioobj;
+    return ret;
+}
+
+
+bool IOX::implExists( bool i ) const
+{
+    IOObj* ioobj = IOM().get( ownkey_ );
+    if ( !ioobj ) return false;
+    bool yn = ioobj->implExists(i);
+    delete ioobj;
+    return yn;
+}
+
+
+Conn* IOX::getConn( Conn::State rw ) const
+{
+    IOObj* ioobj = getIOObj();
+    if ( !ioobj ) return 0;
+
+    XConn* xconn = new XConn;
+    xconn->conn_ = ioobj->getConn( rw );
+    if ( xconn->conn_ ) xconn->conn_->ioobj = 0;
+    xconn->ioobj = const_cast<IOX*>( this );
+
+    delete ioobj;
+    return xconn;
+}
+
+
+IOObj* IOX::getIOObj() const
+{
+    return ownkey_ == "" ? 0 : IOM().get( ownkey_ );
+}
+
+
+int IOX::getFrom( ascistream& stream )
+{
+    ownkey_ = stream.value();
+    stream.next();
+    return YES;
+}
+
+
+int IOX::putTo( ascostream& stream ) const
+{
+    stream.stream() << '$';
+    stream.put( "ID", ownkey_ );
+    return YES;
+}
