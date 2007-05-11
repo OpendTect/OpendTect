aa86fb67dc6ccefa86ce6d644362b54dceab313b
Bert Bril <bert.bril@dgbes.com>
2007-05-11 12:43:42 +0000

    Compiles & loads. Doesn't do anything and may crash
    git-svn-id: https://svn.opendtect.org/od/trunk@9521 a985855d-d384-4b45-ae33-31a410998c3c

--- plugins/Tut/tutseistools.cc
+++ plugins/Tut/tutseistools.cc
@@ -5,20 +5,81 @@
  * DATE     : Mar 2007
 -*/
 
-static const char* rcsID = "$Id: tutseistools.cc,v 1.1 2007-05-09 15:58:49 cvsbert Exp $";
+static const char* rcsID = "$Id: tutseistools.cc,v 1.2 2007-05-11 12:43:42 cvsbert Exp $";
 
 #include "tutseistools.h"
-#include "seistrcprop.h"
+#include "seisread.h"
+#include "seiswrite.h"
+#include "ioobj.h"
 
 
-Tut::SeisTools::SeisTools( float fact, float shft )
-    : factor_(fact), shift_(shft)
+Tut::SeisTools::SeisTools()
+    : Executor("Tut Seismic Tools")
+    , inioobj_(0), outioobj_(0)
+    , action_(Scale)
+    , factor_(1), shift_(0)
+    , weaksmooth_(false)
+    , totnr_(-1)
+    , nrdone_(0)
+    , rdr_(0)
+    , wrr_(0)
 {
 }
 
 
-void Tut::SeisTools::apply( SeisTrc& trc ) const
+Tut::SeisTools::~SeisTools()
 {
-    SeisTrcPropChg pc( trc );
-    pc.scale( factor_, shift_ );
+    delete inioobj_; delete outioobj_;
+    delete rdr_; delete wrr_;
+}
+
+
+void Tut::SeisTools::setInput( const IOObj& ioobj )
+{
+    delete inioobj_; inioobj_ = ioobj.clone();
+}
+
+
+void Tut::SeisTools::setOutput( const IOObj& ioobj )
+{
+    delete outioobj_; outioobj_ = ioobj.clone();
+}
+
+
+const char* Tut::SeisTools::message() const
+{
+    static const char* acts[] = { "Scaling", "Squaring", "Smoothing" };
+    return errmsg_.isEmpty() ? acts[action_] : errmsg_.buf();
+}
+
+
+int Tut::SeisTools::totalNr() const
+{
+    if ( totnr_ >= 0 || !inioobj_ )
+	return totnr_;
+
+    // SeisIOObjInfo ioobjinfo( *inioobj_ );
+    return totnr_;
+}
+
+
+#define mOpenIO(clss,obj,ioobj) \
+    if ( !obj ) \
+    { \
+	obj = new clss( ioobj ); \
+	const char* emsg = obj->errMsg(); \
+	if ( emsg && *emsg ) \
+	{ \
+	    errmsg_ = emsg; \
+	    return Executor::ErrorOccurred; \
+	} \
+	return Executor::MoreToDo; \
+    }
+
+int Tut::SeisTools::nextStep()
+{
+    mOpenIO( SeisTrcReader, rdr_, inioobj_ );
+    mOpenIO( SeisTrcWriter, wrr_, outioobj_ );
+
+    return Executor::Finished;
 }
--- plugins/Tut/tutseistools.h
+++ plugins/Tut/tutseistools.h
@@ -5,34 +5,66 @@
  * COPYRIGHT: (C) dGB Beheer B.V.
  * AUTHOR   : R.K. Singh
  * DATE     : Mar 2007
- * ID       : $Id: tutseistools.h,v 1.1 2007-05-09 15:58:49 cvsbert Exp $
+ * ID       : $Id: tutseistools.h,v 1.2 2007-05-11 12:43:42 cvsbert Exp $
 -*/
 
-#include "gendefs.h"
+#include "executor.h"
+#include "bufstring.h"
+class IOObj;
 class SeisTrc;
+class SeisTrcReader;
+class SeisTrcWriter;
 
 
 namespace Tut
 {
 
-class SeisTools
+class SeisTools : public Executor
 {
 public:
 
-    			SeisTools(float factr=1,float shft=0);
+    enum Action		{ Scale, Square, Smooth };
 
+    			SeisTools();
+    virtual		~SeisTools();
+
+    const IOObj*	input() const		{ return inioobj_; }
+    const IOObj*	output() const		{ return outioobj_; }
+    inline Action	action() const		{ return action_; }
     inline float	factor() const		{ return factor_; }
-    inline void		setFactor( float f )	{ factor_ = f; }
     inline float	shift() const		{ return shift_; }
-    inline void		setShift( float s )	{ shift_ = s; }
+    inline bool		weakSmoothing() const	{ return weaksmooth_; }
 
-    void		apply(SeisTrc&) const;
+    void		setInput(const IOObj&);
+    void		setOutput(const IOObj&);
+    inline void		setAction( Action a )	{ action_ = a; }
+    inline void		setScale( float f, float s )
+						{ factor_ = f; shift_ = s; }
+    inline void		setWeakSmoothing( bool yn )
+    						{ weaksmooth_ = yn; }
+
+			// Executor compliance functions
+    const char*		message() const;
+    int			nrDone() const		{ return nrdone_; }
+    int			totalNr() const;
+    const char*		nrDoneText() const	{ return "Traces handled"; }
+			// This is where it actually happens
+    int			nextStep();
 
 protected:
 
+    IOObj*		inioobj_;
+    IOObj*		outioobj_;
+    Action		action_;
     float		factor_;
     float		shift_;
+    bool		weaksmooth_;
 
+    SeisTrcReader*	rdr_;
+    SeisTrcWriter*	wrr_;
+    int			nrdone_;
+    mutable int		totnr_;
+    BufferString	errmsg_;
 };
 
 } // namespace
