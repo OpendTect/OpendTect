01c4c10d20efb1df5e455dcbc5a336eea555c01a
Nanne Hemstra <nanne.hemstra@dgbes.com>
2007-05-14 06:56:08 +0000

    Show errormessage when file to open does not exist
    git-svn-id: https://svn.opendtect.org/od/trunk@9529 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiBase/uidesktopservices.cc
+++ src/uiBase/uidesktopservices.cc
@@ -4,12 +4,16 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne Hemstra
  Date:          August 2006
- RCS:           $Id: uidesktopservices.cc,v 1.1 2007-02-06 21:26:50 cvsnanne Exp $
+ RCS:           $Id: uidesktopservices.cc,v 1.2 2007-05-14 06:56:08 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uidesktopservices.h"
+
+#include "bufstring.h"
+#include "filegen.h"
+#include "uimsg.h"
 
 #include <QDesktopServices>
 #include <QUrl>
@@ -22,6 +26,13 @@ uiDesktopServices::uiDesktopServices()
 
 void uiDesktopServices::openUrl( const char* url )
 {
+    if ( !File_exists(url) )
+    {
+	BufferString msg( "Cannot open file:\n" ); msg += url;
+	uiMSG().error( msg );
+	return;
+    }
+
     QUrl qurl( url, QUrl::TolerantMode );
     qdesktopservices_->openUrl( qurl );
 }
