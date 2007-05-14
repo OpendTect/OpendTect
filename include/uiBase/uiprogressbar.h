7f3052227acf6f9694cc371ea8cfb5518cf3bf90
Nanne Hemstra <nanne.hemstra@dgbes.com>
2007-05-14 06:55:01 +0000

    Moved to Qt4
    git-svn-id: https://svn.opendtect.org/od/trunk@9528 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiBase/uiprogressbar.h
+++ include/uiBase/uiprogressbar.h
@@ -1,5 +1,5 @@
-#ifndef uiprogressbar_H
-#define uiprogressbar_H
+#ifndef uiprogressbar_h
+#define uiprogressbar_h
 
 /*+
 ________________________________________________________________________
@@ -7,12 +7,12 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H. Lammertink
  Date:          17/1/2001
- RCS:           $Id: uiprogressbar.h,v 1.6 2003-11-07 12:21:54 bert Exp $
+ RCS:           $Id: uiprogressbar.h,v 1.7 2007-05-14 06:55:01 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
 
-#include <uiobj.h>
+#include "uiobj.h"
 
 class uiProgressBarBody;
 
@@ -20,18 +20,18 @@ class uiProgressBar : public uiObject
 {
 public:
 
-                        uiProgressBar( uiParent*,const char* nm="ProgressBar", 
-				       int totalSteps=100, int progress=0);
+                        uiProgressBar(uiParent*,const char* nm="ProgressBar", 
+				      int totalSteps=100,int progress=0);
 
     void		setProgress(int);
-    int			Progress() const;
+    int			progress() const;
     void		setTotalSteps(int);
     int			totalSteps() const;
 
 private:
 
     uiProgressBarBody*	body_;
-    uiProgressBarBody&	mkbody(uiParent*, const char*);
+    uiProgressBarBody&	mkbody(uiParent*,const char*);
 };
 
 #endif
--- src/uiBase/uiprogressbar.cc
+++ src/uiBase/uiprogressbar.cc
@@ -1,33 +1,33 @@
 /*+
 ________________________________________________________________________
 
- CopyRight:     (C) dGB Beheer B.V.
- Author:        A.H. Lammertink
- Date:          17/1/2001
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	Arend Lammertink
+ Date:		2001
+ RCS:		$Id: uiprogressbar.cc,v 1.14 2007-05-14 06:55:01 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
 
 
-#include <uiprogressbar.h>
-#include <uiobjbody.h> 
+#include "uiprogressbar.h"
+#include "uiobjbody.h"
 
 #ifdef USEQT3
-# include	<qprogressbar.h> 
-# define	mQProgressBar	QProgressBar
+# include	<qprogressbar.h>
 #else
-# include	<Q3ProgressBar> 
-# define	mQProgressBar	Q3ProgressBar
+# include	<QProgressBar>
 #endif
 
 
-class uiProgressBarBody : public uiObjBodyImpl<uiProgressBar,mQProgressBar>
+#ifdef USEQT3
+class uiProgressBarBody : public uiObjBodyImpl<uiProgressBar,QProgressBar>
 {
 public:
 
                         uiProgressBarBody( uiProgressBar& handle, 
 					   uiParent* parnt, const char* nm )
-			    : uiObjBodyImpl<uiProgressBar,mQProgressBar>
+			    : uiObjBodyImpl<uiProgressBar,QProgressBar>
 				( handle, parnt,nm)
 			    { 
 				setStretch( 1, 0 );
@@ -37,6 +37,24 @@ public:
 
     virtual int 	nrTxtLines() const			{ return 1; }
 };
+#else
+class uiProgressBarBody : public uiObjBodyImplNoQtNm<uiProgressBar,QProgressBar>
+{
+public:
+
+                        uiProgressBarBody( uiProgressBar& handle, 
+					   uiParent* parnt, const char* nm )
+			    : uiObjBodyImplNoQtNm<uiProgressBar,QProgressBar>
+				(handle,parnt,nm)
+			    { 
+				setStretch( 1, 0 );
+				setHSzPol( uiObject::MedVar );
+			    }
+
+    virtual int 	nrTxtLines() const			{ return 1; }
+};
+
+#endif
 
 
 uiProgressBar::uiProgressBar( uiParent* p, const char* txt, 
@@ -47,31 +65,40 @@ uiProgressBar::uiProgressBar( uiParent* p, const char* txt,
     setTotalSteps( totalSteps );
 }
 
-uiProgressBarBody& uiProgressBar::mkbody(uiParent* p, const char* txt)
+
+uiProgressBarBody& uiProgressBar::mkbody( uiParent* p, const char* txt )
 {
-    body_= new uiProgressBarBody(*this,p,txt);
+    body_= new uiProgressBarBody( *this, p, txt );
     return *body_; 
 }
 
+
+#ifdef USEQT3
+
 void uiProgressBar::setProgress( int progr )
-{ 
-    body_->setProgress( progr );
-} 
+{ body_->setProgress( progr ); } 
 
-
-int uiProgressBar::Progress() const
-{
-    return body_->progress();
-}
-
+int uiProgressBar::progress() const
+{ return body_->progress(); }
 
 void uiProgressBar::setTotalSteps( int tstp )
-{ 
-    body_->setTotalSteps( tstp > 2 ? tstp : 2 );
-} 
-
+{ body_->setTotalSteps( tstp > 2 ? tstp : 2 ); } 
 
 int uiProgressBar::totalSteps() const
-{
-    return body_->totalSteps();
-}
+{ return body_->totalSteps(); }
+
+#else
+
+void uiProgressBar::setProgress( int progr )
+{ body_->setValue( progr ); } 
+
+int uiProgressBar::progress() const
+{ return body_->value(); }
+
+void uiProgressBar::setTotalSteps( int tstp )
+{ body_->setMaximum( tstp > 2 ? tstp : 2 ); } 
+
+int uiProgressBar::totalSteps() const
+{ return body_->maximum(); }
+
+#endif
