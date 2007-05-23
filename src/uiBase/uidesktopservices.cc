b83ee860fdd74e85a4bb89975dc0fee79b36b1b2
cvsdgb <cvsdgb@a985855d-d384-4b45-ae33-31a410998c3c>
2007-05-23 12:20:07 +0000

    check if file is windows filename, if so start url with "file:///"
    git-svn-id: https://svn.opendtect.org/od/trunk@9607 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiBase/uidesktopservices.cc
+++ src/uiBase/uidesktopservices.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne Hemstra
  Date:          August 2006
- RCS:           $Id: uidesktopservices.cc,v 1.4 2007-05-23 10:27:21 cvsnanne Exp $
+ RCS:           $Id: uidesktopservices.cc,v 1.5 2007-05-23 12:20:07 cvsdgb Exp $
 ________________________________________________________________________
 
 -*/
@@ -21,6 +21,18 @@ ________________________________________________________________________
 
 bool uiDesktopServices::openUrl( const char* url )
 {
-    QUrl qurl( url, QUrl::TolerantMode );
+    BufferString myurl = url;
+    if ( !strncasecmp(url,"c:",2) )
+    {
+	myurl = "file:///";
+	myurl += url;
+    }
+    else if ( !strncasecmp(url,"www.",4) )
+    {
+	myurl = "http://";
+	myurl += url;
+    }
+
+    QUrl qurl( myurl.buf(), QUrl::TolerantMode );
     return QDesktopServices::openUrl( qurl );
 }
