b8785bb1e4e14b74ad78228aa605c75d962b37f1
Raman Singh <raman.singh@dgbes.com>
2007-06-12 11:23:25 +0000

    Utility for Auto-loading of Attribute Sets...
    git-svn-id: https://svn.opendtect.org/od/trunk@9716 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiAttributes/uiautoattrdescset.h
@@ -0,0 +1,93 @@
+
+#ifndef uiautoattrdescset_h
+#define uiautoattrdescset_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        R. K. Singh
+ Date:          June 2007
+ RCS:           $Id: uiautoattrdescset.h,v 1.1 2007-06-12 11:23:25 cvsraman Exp $
+________________________________________________________________________
+
+-*/
+
+#include "uidialog.h"
+#include "multiid.h"
+#include "bufstringset.h"
+
+class uiAttrDescEd;
+class uiCheckBox;
+class uiGenInput;
+class uiLabel;
+class uiLabeledListBox;
+class uiPushButton;
+class uiIOObjSelGrp;
+class CtxtIOObj;
+class IOObj;
+
+
+/*! \brief class for selecting Auto-load Attribute Set  */
+
+
+class uiAutoAttrSelDlg : public uiDialog
+{
+public:
+    				uiAutoAttrSelDlg(uiParent* p);
+				~uiAutoAttrSelDlg();
+
+	IOObj*			getObj();
+	bool			useAuto();
+	bool			loadAuto();
+
+protected:
+	
+	CtxtIOObj&          	ctio_;
+
+	uiGenInput*         	usefld_;
+	uiIOObjSelGrp*      	selgrp_;
+	uiLabel*            	lbl_;
+	uiCheckBox*         	loadbutton_;
+
+	void			useChg(CallBacker*);
+	bool			acceptOK(CallBacker*);
+
+};
+
+
+class uiAutoAttrSetOpen : public uiDialog
+{
+public:
+				uiAutoAttrSetOpen(uiParent*,BufferStringSet&,
+							BufferStringSet&);
+				~uiAutoAttrSetOpen();
+	IOObj*			getObj();
+	const char*		getAttribname();
+	const char*		getAttribfile();
+	bool			isUserDef()		{ return usrdef_; }
+	bool			isAuto()		{ return isauto_; }
+	
+protected:
+	
+	CtxtIOObj&              ctio_;
+
+	uiIOObjSelGrp*          selgrp_;
+	uiLabel*                lbl_;
+	uiLabeledListBox*	defattrlist_;
+	uiGenInput*		defselfld_;
+	uiGenInput*		autoloadfld_;
+
+	BufferStringSet		attribfiles_;
+	BufferStringSet		attribnames_;
+	int			defselid_;
+	bool			usrdef_;
+	bool			isauto_;
+
+	void			setChg(CallBacker*);
+	bool                    acceptOK(CallBacker*);
+
+};
+
+
+#endif
