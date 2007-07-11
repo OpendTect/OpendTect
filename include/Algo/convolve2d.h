3fe5455019cfa0f72b5455cb50d75ebf833b59ce
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2007-07-11 20:51:37 +0000

    Major bugfixes. Not that covolution is not tested.
    git-svn-id: https://svn.opendtect.org/od/trunk@9873 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Algo/convolve2d.h
+++ include/Algo/convolve2d.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Kristofer Tingdahl
  Date:          07-10-1999
- RCS:           $Id: convolve2d.h,v 1.1 2007-07-09 21:05:25 cvskris Exp $
+ RCS:           $Id: convolve2d.h,v 1.2 2007-07-11 20:51:37 cvskris Exp $
 ________________________________________________________________________
 
 
@@ -15,19 +15,26 @@ ________________________________________________________________________
 
 #include "arraynd.h"
 #include "basictask.h"
+#include "rowcol.h"
+
+/*!Convolves (or correlates) two 2D signals. */
 
 
 template <class T>
-class Convolver2D : public ParalellTask
+class Convolver2D : public ParallelTask
 {
 public:
     inline		Convolver2D();
 
     inline void		setX(const Array2D<T>&,int first0=0,int first1=0);
-    inline void		sety(const Array2D<T>&,int first0=0,int first1=0);
+    inline void		setY(const Array2D<T>&,int first0=0,int first1=0);
     inline void		setZ(Array2D<T>& z )		{ z_ = &z; }
     void		setNormalize( bool n )		{ normalize_ = n; }
+    			/*!<If true, the sum will be divided with the number
+			    of valid samples. */
     void		setCorrelate( bool yn )		{ correlate_ = yn; }
+    			/*!<If true, the convolution will be replaced by a
+			   correllation. */
 
 protected:
     inline bool		doWork( int, int, int );
@@ -37,7 +44,6 @@ protected:
     const Array2D<T>*	y_;
     RowCol		yshift_;
     Array2D<T>*		z_;
-    RowCol		zshift_;
     bool		normalize_;
     bool		correlate_;
 };
@@ -48,9 +54,8 @@ Convolver2D<T>::Convolver2D()
     : x_( 0 )
     , xshift_(0,0)
     , y_( 0 )
-    , yshift(0,0)
+    , yshift_(0,0)
     , z_( 0 )
-    , zshift_( 0, 0 )
     , normalize_( false )
     , correlate_( false )
 {}
@@ -60,13 +65,13 @@ template <class T> inline
 void Convolver2D<T>::setX( const Array2D<T>& x, int first0, int first1 )
 {
     x_ = &x;
-    shiftx_.row = first0;
-    shiftx_.col = first1;
+    xshift_.row = first0;
+    xshift_.col = first1;
 }
 
 
 template <class T> inline
-void Convolver2D<T>::sety( const Array2D<T>& y, int first0, int first1 )
+void Convolver2D<T>::setY( const Array2D<T>& y, int first0, int first1 )
 {
     y_ = &y;
     yshift_.row = first0;
@@ -85,17 +90,17 @@ bool Convolver2D<T>::doWork( int start, int stop, int )
 
     for ( int idz0=start; idz0<=stop; idz0++ )
     {
-	const int zdim0 = idz0+zshift_.row;
+	const int zdim0 = idz0;
 	for ( int idz1=0; idz1<dim1zsz; idz1++ )
 	{
-	    const int zdim1 = idz1+zshift_.col;
+	    const int zdim1 = idz1;
 
 	    T sum = 0;
 	    int nrsamples = 0;
 	    for ( int idx0=0; idx0<dim0xsz; idx0++ )
 	    {
 		const int xdim0 = idx0+xshift_.row;
-		const int ydim0 = zdim0-xdim0;
+		const int ydim0 = correlate_ ? zdim0+xdim0 : zdim0-xdim0;
 		const int idy0 = ydim0-yshift_.row;
 		if ( idy0<0 || idy0>=dim0ysz )
 		    continue;
@@ -103,23 +108,27 @@ bool Convolver2D<T>::doWork( int start, int stop, int )
 		for ( int idx1=0; idx1<dim1xsz; idx1++ )
 		{
 		    const int xdim1 = idx1+xshift_.col;
-		    const int ydim1 = zdim1-xdim1;
-		    const int idy0 = ydim1-yshift_.col;
+		    const int ydim1 = correlate_ ? zdim1+xdim1 : zdim1-xdim1;
+		    const int idy1 = ydim1-yshift_.col;
 		    if ( idy1<0 || idy1>=dim1ysz )
 			continue;
 
-		    const T yval = correlate_
-			? y->get( dim0ysz-1-idy0, dim1ysz-1-idy1 )
-			: y_->get( idy0, idy1 );
+		    const T yval = y_->get( idy0, idy1 );
+		    if ( mIsUdf(yval) )
+			continue;
 
-		    sum += x_->get( idx0, idx1 ) * yval;
+		    const T xval = x_->get( idy0, idy1 );
+		    if ( mIsUdf(xval) )
+			continue;
+
+		    sum += xval * yval;
 		    nrsamples++;
 		}
 	    }
 
 	    if ( !nrsamples )
 		z_->set( idz0, idz1, 0 );
-	    else if ( normalize )
+	    else if ( normalize_ )
 		z_->set( idz0, idz1, sum/nrsamples );
 	    else
 		z_->set( idz0, idz1, sum );
