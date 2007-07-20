359a7bd70628e025d8a3c7047440b19150a09e94
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2007-07-20 19:36:04 +0000

    Made window with symmetrical.
    git-svn-id: https://svn.opendtect.org/od/trunk@9913 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiPreStackProcessing/uiprestackagc.cc
+++ src/uiPreStackProcessing/uiprestackagc.cc
@@ -4,13 +4,14 @@
  * DATE     : April 2005
 -*/
 
-static const char* rcsID = "$Id: uiprestackagc.cc,v 1.1 2007-03-26 21:12:02 cvskris Exp $";
+static const char* rcsID = "$Id: uiprestackagc.cc,v 1.2 2007-07-20 19:36:04 cvskris Exp $";
 
 #include "uiprestackagc.h"
 
 #include "uiprestackprocessor.h"
 #include "prestackagc.h"
 #include "uigeninput.h"
+#include "uimsg.h"
 
 namespace PreStack
 {
@@ -34,11 +35,10 @@ uiAGC::uiAGC( uiParent* p,AGC* sgagc )
     : uiDialog( p, uiDialog::Setup("AGC setup",0,"dgb:104.2.0") )
     , processor_( sgagc )
 {
-    windowfld_ = new uiGenInput( this, "Window", FloatInpIntervalSpec(false) );
+    windowfld_ = new uiGenInput( this, "Window width",
+			     FloatInpSpec(processor_->getWindow().width() ));
     lowenergymute_ = new uiGenInput( this, "Low energy mute", FloatInpSpec() );
     lowenergymute_->attach( alignedBelow, windowfld_ );
-
-    windowfld_->setValue( processor_->getWindow() );
     lowenergymute_->setValue( processor_->getLowEnergyMute() );
 }
 
@@ -47,7 +47,14 @@ bool uiAGC::acceptOK( CallBacker* )
 {
     if ( !processor_ ) return true;
 
-    processor_->setWindow( windowfld_->getFInterval() );
+    const float width = windowfld_->getfValue();
+    if ( mIsUdf(width) )
+    {
+	uiMSG().error("Window width is not set");
+	return false;
+    }
+
+    processor_->setWindow( Interval<float>( -width/2, width/2 ) );
     processor_->setLowEnergyMute( lowenergymute_->getfValue() );
     return true;
 }
