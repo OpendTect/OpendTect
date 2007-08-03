dc0678abd1f48d519960e61e4e9a7f2fe8ea7328
Raman Singh <raman.singh@dgbes.com>
2007-08-03 09:50:11 +0000

    New formatted Pickset import...
    git-svn-id: https://svn.opendtect.org/od/trunk@9961 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiIo/uiimppickset.h
+++ include/uiIo/uiimppickset.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne Hemstra
  Date:          June 2002
- RCS:           $Id: uiimppickset.h,v 1.4 2004-06-23 11:18:46 nanne Exp $
+ RCS:           $Id: uiimppickset.h,v 1.5 2007-08-03 09:49:13 cvsraman Exp $
 ________________________________________________________________________
 
 -*/
@@ -15,10 +15,12 @@ ________________________________________________________________________
 #include "uidialog.h"
 
 class CtxtIOObj;
+class uiLabeledComboBox;
 class uiFileInput;
 class uiGenInput;
 class uiIOObjSel;
-
+class uiTableImpDataSel;
+namespace Table { class FormatDesc; }
 
 /*! \brief Dialog for pickset selection */
 
@@ -30,17 +32,23 @@ public:
 
 protected:
 
-    uiIOObjSel*		objfld;
-    uiGenInput*		xyfld;
-    uiFileInput*	filefld;
+    uiIOObjSel*		objfld_;
+    uiGenInput*		xyfld_;
+    uiLabeledComboBox*	zfld_;
+    uiGenInput*		constzfld_;
+    uiTableImpDataSel*  dataselfld_;
+    uiFileInput*	filefld_;
+
 
     virtual bool	acceptOK(CallBacker*);
+    void		formatSel(CallBacker*);
     bool		checkInpFlds();
     bool		doImport();
     bool		doExport();
 
-    bool		import;
-    CtxtIOObj&		ctio;
+    bool		import_;
+    CtxtIOObj&		ctio_;
+    Table::FormatDesc&  fd_;
 };
 
 
--- src/uiIo/uiimppickset.cc
+++ src/uiIo/uiimppickset.cc
@@ -4,64 +4,126 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne Hemstra
  Date:          June 2002
- RCS:           $Id: uiimppickset.cc,v 1.23 2007-03-29 15:08:14 cvskris Exp $
+ RCS:           $Id: uiimppickset.cc,v 1.24 2007-08-03 09:50:11 cvsraman Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uiimppickset.h"
+#include "uicombobox.h"
 #include "uifileinput.h"
 #include "uiioobjsel.h"
 #include "uimsg.h"
+#include "uiseparator.h"
+#include "uitblimpexpdatasel.h"
 
 #include "ctxtioobj.h"
 #include "ioobj.h"
 #include "strmdata.h"
 #include "strmprov.h"
 #include "survinfo.h"
+#include "tabledef.h"
 #include "filegen.h"
 #include "pickset.h"
 #include "picksettr.h"
 
 #include <math.h>
 
+static const char* zoptions[] =
+{
+    "Input file",
+    "Constant Z",
+    0
+};
+
 
 uiImpExpPickSet::uiImpExpPickSet( uiParent* p, bool imp )
     : uiDialog(p,uiDialog::Setup(imp ? "Import Pickset" : "Export PickSet",
 				 "Specify pickset parameters",
 				 imp ? "105.0.1" : "105.0.2"))
-    , ctio(*mMkCtxtIOObj(PickSet))
-    , import(imp)
-    , xyfld(0)
+    , ctio_(*mMkCtxtIOObj(PickSet))
+    , import_(imp)
+    , fd_(*PickSetAscIO::getDesc(true,true))
+    , xyfld_(0)
 {
-    BufferString label( import ? "Input " : "Output " );
+    BufferString label( import_ ? "Input " : "Output " );
     label += "Ascii file";
-    filefld = new uiFileInput( this, label, uiFileInput::Setup()
-					    .withexamine(import)
-					    .forread(import) );
-    filefld->setDefaultSelectionDir( 
+    filefld_ = new uiFileInput( this, label, uiFileInput::Setup()
+					    .withexamine(import_)
+					    .forread(import_) );
+    filefld_->setDefaultSelectionDir( 
 			    IOObjContext::getDataDirName(IOObjContext::Loc) );
 
-    ctio.ctxt.forread = !import;
-    ctio.ctxt.maychdir = false;
-    label = import ? "Output " : "Input "; label += "PickSet";
-    objfld = new uiIOObjSel( this, ctio, label );
+    ctio_.ctxt.forread = !import_;
+    ctio_.ctxt.maychdir = false;
+    label = import_ ? "Output " : "Input "; label += "PickSet";
+    objfld_ = new uiIOObjSel( this, ctio_, label );
 
-    if ( import )
+    if ( import_ )
     {
-	xyfld = new uiGenInput( this, "Positions in:",
+	xyfld_ = new uiGenInput( this, "Positions in:",
 				BoolInpSpec(true,"X/Y","Inl/Crl") );
-	xyfld->attach( alignedBelow, filefld );
-	objfld->attach( alignedBelow, xyfld );
+	xyfld_->valuechanged.notify( mCB(this,uiImpExpPickSet,formatSel) );
+	xyfld_->attach( alignedBelow, filefld_ );
+
+	zfld_ = new uiLabeledComboBox( this, "Get Z values from" );
+	zfld_->box()->addItems( zoptions );
+	zfld_->box()->selectionChanged.notify( mCB(this,uiImpExpPickSet,
+		    		formatSel) );
+	zfld_->attach( alignedBelow, xyfld_ );
+
+	constzfld_ = new uiGenInput( this, "Specify Constatnt Z value",
+				FloatInpSpec(0) );
+	constzfld_->attach( alignedBelow, zfld_ );
+	constzfld_->display( zfld_->box()->currentItem() == 1 );
+
+	uiSeparator* sep = new uiSeparator( this, "H sep" );
+	sep->attach( stretchedBelow, constzfld_ );
+
+	dataselfld_ = new uiTableImpDataSel( this, fd_, "100.0.0" );
+	dataselfld_->attach( alignedBelow, constzfld_ );
+	dataselfld_->attach( ensureBelow, sep );
+
+	sep = new uiSeparator( this, "H sep" );
+	sep->attach( stretchedBelow, dataselfld_ );
+
+	objfld_->attach( alignedBelow, sep );
+	objfld_->attach( ensureBelow, sep );
     }
     else
-	filefld->attach( alignedBelow, objfld );
+	filefld_->attach( alignedBelow, objfld_ );
 }
 
 
 uiImpExpPickSet::~uiImpExpPickSet()
 {
-    delete ctio.ioobj; delete &ctio;
+    delete ctio_.ioobj; delete &ctio_;
+}
+
+
+void uiImpExpPickSet::formatSel( CallBacker* cb )
+{
+    const bool isxy = xyfld_->getBoolValue();
+    const int zchoice = zfld_->box()->currentItem(); 
+    const bool iszreq = zchoice == 0;
+    constzfld_->display( zchoice == 1 );
+    Table::FormatDesc& fd = dataselfld_->desc();
+    fd.bodyinfos_[0]->setName( isxy ? "X Coordinate" : "Inl No." );
+    fd.bodyinfos_[1]->setName( isxy ? "Y Coordinate" : "Crl No." );
+    if ( iszreq )
+    {
+	if ( fd.bodyinfos_.size() == 2 )
+	    fd.bodyinfos_ += new Table::TargetInfo( "Z Values", FloatInpSpec(),
+		    		 Table::Required, PropertyRef::surveyZType() );
+    }
+    else
+    {
+	if ( fd.bodyinfos_.size() == 3 )
+	{
+	    Table::TargetInfo* ti = fd.bodyinfos_.remove( 2 );
+	    delete ti;
+	}
+    }
 }
 
 #define mErrRet(s) { uiMSG().error(s); return false; }
@@ -69,7 +131,7 @@ uiImpExpPickSet::~uiImpExpPickSet()
 
 bool uiImpExpPickSet::doImport()
 {
-    const char* fname = filefld->fileName();
+    const char* fname = filefld_->fileName();
     StreamData sdi = StreamProvider( fname ).makeIStream();
     if ( !sdi.usable() ) 
     { 
@@ -77,44 +139,18 @@ bool uiImpExpPickSet::doImport()
 	mErrRet( "Could not open input file" )
     }
 
-    const char* psnm = objfld->getInput();
+    const char* psnm = objfld_->getInput();
     Pick::Set ps( psnm );
+    bool isxy = xyfld_ ? xyfld_->getBoolValue() : true;
+    const int zchoice = zfld_->box()->currentItem();
+    const float constz = zchoice==1 ? constzfld_->getfValue() : 0;
     ps.disp_.color_ = Color::DgbColor;
-
-    bool doxy = xyfld ? xyfld->getBoolValue() : true;
-    bool firstpos = true;
-    bool doscale = false;
-    Pick::Location ploc;
-    char buf[1024];
-    const float zfactor = SI().zFactor();
-    while ( true )
-    {
-	sdi.istrm->getline( buf, 1024 );
-	if ( !ploc.fromString(buf,doxy) )
-	    break;
-
-        if ( firstpos )
-        {
-            firstpos = false;
-            doscale = ploc.pos.z > 2 * SI().zRange(false).stop &&
-                      SI().zRange(false).includes( ploc.pos.z / zfactor );
-        }
-
-	if ( doscale )
-	    ploc.pos.z /= zfactor;
-	if ( ploc.hasDir() )
-	{
-	    ploc.dir.theta *= M_PI/180;
-	    ploc.dir.phi *= M_PI/180;
-	}
-
-	ps += ploc;
-    }
-
+    PickSetAscIO aio( fd_ );
+    aio.get( *sdi.istrm, ps, isxy, zchoice==0, constz );
     sdi.close();
 
     BufferString errmsg;
-    if ( !PickSetTranslator::store(ps,ctio.ioobj,errmsg) )
+    if ( !PickSetTranslator::store(ps,ctio_.ioobj,errmsg) )
 	mErrRet(errmsg);
 
     return true;
@@ -125,10 +161,10 @@ bool uiImpExpPickSet::doExport()
 {
     Pick::Set ps;
     BufferString errmsg;
-    if ( !PickSetTranslator::retrieve(ps,ctio.ioobj,errmsg) )
+    if ( !PickSetTranslator::retrieve(ps,ctio_.ioobj,errmsg) )
 	mErrRet(errmsg);
 
-    const char* fname = filefld->fileName();
+    const char* fname = filefld_->fileName();
     StreamData sdo = StreamProvider( fname ).makeOStream();
     if ( !sdo.usable() ) 
     { 
@@ -152,21 +188,21 @@ bool uiImpExpPickSet::doExport()
 bool uiImpExpPickSet::acceptOK( CallBacker* )
 {
     if ( !checkInpFlds() ) return false;
-    bool ret = import ? doImport() : doExport();
+    bool ret = import_ ? doImport() : doExport();
     return ret;
 }
 
 
 bool uiImpExpPickSet::checkInpFlds()
 {
-    BufferString filenm = filefld->fileName();
-    if ( import && !File_exists(filenm) )
+    BufferString filenm = filefld_->fileName();
+    if ( import_ && !File_exists(filenm) )
 	mErrRet( "Please select input file" );
 
-    if ( !import && filenm.isEmpty() )
+    if ( !import_ && filenm.isEmpty() )
 	mErrRet( "Please select output file" );
 
-    if ( !objfld->commitInput( true ) )
+    if ( !objfld_->commitInput( true ) )
 	mErrRet( "Please select PickSet" );
 
     return true;
