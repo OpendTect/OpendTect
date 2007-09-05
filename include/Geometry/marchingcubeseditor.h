c55ea25ec2478c302d0dcdc87e1379a365ebc357
Yuancheng Liu <yuancheng.liu@dgbes.com>
2007-09-05 15:24:13 +0000

    Interface changes.
    git-svn-id: https://svn.opendtect.org/od/trunk@10167 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Geometry/marchingcubeseditor.h
+++ include/Geometry/marchingcubeseditor.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        K. Tingdahl
  Date:          August 2007
- RCS:           $Id: marchingcubeseditor.h,v 1.1 2007-09-04 18:02:28 cvskris Exp $
+ RCS:           $Id: marchingcubeseditor.h,v 1.2 2007-09-05 15:24:13 cvsyuancheng Exp $
 ________________________________________________________________________
 
 -*/
@@ -27,7 +27,7 @@ MarchingCubesSurfaces.
 
 The modification of the implicit representation is guided by:
 
-NewImplicitShape = OriginalShape * Kernel * factor
+NewImplicitShape = OriginalShape + Kernel * factor
 
 The Kernel is a scalar field (values between 0 and 255) represents the shape of 
 the movement, and the factor is set by from the outside.
@@ -41,11 +41,15 @@ public:
     			MarchingCubesSurfaceEditor(MarchingCubesSurface&);
     virtual		~MarchingCubesSurfaceEditor();
 
+    bool		setKernel(Array3D<unsigned char>*,
+	    			  int xpos, int ypos, int zpos );
+    			//!<Kernel becomes mine
+
     bool		setFactor(int); //!<a value of 255 is one voxel if
     					//!<the kernel is 255
     int			getFactor() const { return factor_; }
 
-    virtual void	affectedVolume(Interval<int>& xrg,
+    virtual bool	affectedVolume(Interval<int>& xrg,
 	    			       Interval<int>& yrg,
 				       Interval<int>& zrg) const;
 
@@ -57,19 +61,17 @@ protected:
 				    if it has changed kernel size or
 				    kernel position. */
 
-    virtual Interval<int>	kernelXRange() const			= 0;
-    virtual Interval<int>	kernelYRange() const			= 0;
-    virtual Interval<int>	kernelZRange() const			= 0;
-
-    virtual bool		computeKernel(Array3D<unsigned char>&) const =0;
-
     MarchingCubesSurface&	surface_;
     int				factor_;
     int				prevfactor_;
     Array3D<unsigned char>*	kernel_;
-    Array3D<int>*		implicitsurface_;
+    Array3D<int>*		changedsurface_;
     Array3D<int>*		originalsurface_;
     float			threshold_;
+
+    int				xorigin_;
+    int				yorigin_;
+    int				zorigin_;
 
 private:
     int				nrTimes() const;
--- src/Geometry/marchingcubeseditor.cc
+++ src/Geometry/marchingcubeseditor.cc
@@ -4,7 +4,7 @@
  * DATE     : August 2007
 -*/
 
-static const char* rcsID = "$Id: marchingcubeseditor.cc,v 1.1 2007-09-04 18:02:28 cvskris Exp $";
+static const char* rcsID = "$Id: marchingcubeseditor.cc,v 1.2 2007-09-05 15:24:13 cvsyuancheng Exp $";
 
 #include "marchingcubeseditor.h"
 #include "marchingcubes.h"
@@ -22,19 +22,75 @@ MarchingCubesSurfaceEditor::MarchingCubesSurfaceEditor(
     : surface_( surface )
     , shapeChange( this )
     , kernel_( 0 )
-    , implicitsurface_( 0 )
+    , changedsurface_( 0 )
     , originalsurface_( 0 )
     , factor_( 0 )
     , prevfactor_( 0 )
     , threshold_( mUdf(float) )
+    , xorigin_( 0 )
+    , yorigin_( 0 )
+    , zorigin_( 0 )		   				   
 { }
 
 
 MarchingCubesSurfaceEditor::~MarchingCubesSurfaceEditor()
 {
     delete kernel_;
-    delete implicitsurface_;
+    delete changedsurface_;
     delete originalsurface_;
+}
+
+#define mErrRet \
+{ \
+    delete kernel_; \
+    kernel_ = 0; \
+    delete changedsurface_; \
+    changedsurface_ = 0; \
+ \
+    delete originalsurface_; \
+    originalsurface_ = 0; \
+ \
+    return false; \
+}
+
+
+bool MarchingCubesSurfaceEditor::setKernel( Array3D<unsigned char>* arr,
+					    int xpos, int ypos, int zpos )
+{
+    if ( !arr || arr->isOK() )
+	mErrRet;
+
+    delete kernel_;
+    kernel_ = 0;
+
+    delete changedsurface_;
+    changedsurface_ = 0;
+
+    delete originalsurface_;
+    originalsurface_ = 0;
+
+    xorigin_ = xpos;
+    yorigin_ = ypos;
+    zorigin_ = zpos;
+    kernel_ = arr;
+
+    changedsurface_ = new Array3DImpl<int>( kernel_->info() );
+    if ( !changedsurface_->isOK() )
+	mErrRet;
+
+    originalsurface_ = new Array3DImpl<int>( kernel_->info() );
+    if ( !originalsurface_->isOK() )
+	mErrRet;
+
+    MarchingCubes2Implicit mc2i( surface_, *originalsurface_,
+	xorigin_, yorigin_, zorigin_ );
+
+    if ( !mc2i.compute() )
+	mErrRet;
+
+    threshold_ = mc2i.threshold();
+
+    return true;
 }
 
 
@@ -52,70 +108,36 @@ bool MarchingCubesSurfaceEditor::setFactor( int nf )
 }
     
 
-void MarchingCubesSurfaceEditor::affectedVolume(Interval<int>& xrg,
+bool MarchingCubesSurfaceEditor::affectedVolume(Interval<int>& xrg,
 				      Interval<int>& yrg,
 				      Interval<int>& zrg) const
 {
-    xrg = kernelXRange(); yrg = kernelYRange(); zrg = kernelZRange();
+    if ( !kernel_ )
+	return false;
+
+    xrg =  Interval<int>(xorigin_, xorigin_+kernel_->info().getSize(mX)-1 );
+    yrg =  Interval<int>(yorigin_, yorigin_+kernel_->info().getSize(mY)-1 );
+    zrg =  Interval<int>(zorigin_, zorigin_+kernel_->info().getSize(mZ)-1 );
+    return true;
 }
 
 
 int MarchingCubesSurfaceEditor::nrTimes() const
 {
-    return (kernelXRange().width()+1) * (kernelYRange().width()+1) *
-	   (kernelZRange().width()+1);
-}
+    if ( !kernel_ )
+	return 0;
 
-
-#define mErrRet \
-{ \
-    delete kernel_; \
-    kernel_ = 0; \
-    delete implicitsurface_; \
-    implicitsurface_ = 0; \
- \
-    delete originalsurface_; \
-    originalsurface_ = 0; \
- \
-    return false; \
+    return ( kernel_->info().getSize(mX) ) * 
+	   ( kernel_->info().getSize(mY) ) *
+	   ( kernel_->info().getSize(mZ) );
 }
 
 
 bool MarchingCubesSurfaceEditor::doPrepare( int nrthreads )
 {
-    if ( !kernel_ )
-    {	
-	kernel_ = new Array3DImpl<unsigned char>( kernelXRange().width()+1,
-						  kernelYRange().width()+1,
-						  kernelZRange().width()+1 );
-	if ( !kernel_->isOK() || !computeKernel( *kernel_ ) )
-	    mErrRet;
-    }
-
-    if ( !implicitsurface_ )
-    {
-	implicitsurface_ = new Array3DImpl<int>( kernelXRange().width()+1,
-						 kernelYRange().width()+1,
-						 kernelZRange().width()+1 );
-	if ( !implicitsurface_->isOK() )
-	    mErrRet;
-
-	MarchingCubes2Implicit mc2i( surface_, *implicitsurface_,
-	    kernelXRange().start, kernelYRange().start, kernelZRange().start );
-
-	if ( !mc2i.compute() )
-	    mErrRet;
-
-	threshold_ = mc2i.threshold();
-
-	originalsurface_ = new Array3DImpl<int>( kernelXRange().width()+1,
-						 kernelYRange().width()+1,
-						 kernelZRange().width()+1 );
-	if ( !originalsurface_->isOK() )
-	    mErrRet;
-    }
-
-    return true;
+    return kernel_ && changedsurface_ && !originalsurface_ &&
+	 kernel_->isOK() && changedsurface_->isOK() &&
+	 originalsurface_->isOK();
 }
 
 #undef mErrRet
@@ -130,8 +152,8 @@ bool MarchingCubesSurfaceEditor::doFinish( bool success )
 	    new ArrayValueSeries<float,int>(originalsurface_->getData(),false);
 
 	if ( convarr.setStorage( valseries ) )
-	    surface_.setVolumeData( kernelXRange().start, kernelYRange().start,
-				    kernelZRange().start,  convarr, threshold_);
+	    surface_.setVolumeData( xorigin_, yorigin_, zorigin_,  
+		    		    convarr, threshold_ );
     }
 
     return true;
@@ -140,23 +162,20 @@ bool MarchingCubesSurfaceEditor::doFinish( bool success )
 
 bool MarchingCubesSurfaceEditor::doWork( int start, int stop, int )
 {
-    const int difffactor = prevfactor_-factor_;
-    if ( !difffactor ) return true;
-
-    int* implicitptr = implicitsurface_->getData();
-    const int* stopptr = implicitsurface_->getData()+stop;
+    int* changedsurfptr = changedsurface_->getData();
+    const int* stopptr = changedsurface_->getData()+stop;
     const int* origptr = originalsurface_->getData();
     unsigned const char* kernelptr = kernel_->getData();
 
-    implicitptr += start;
+    changedsurfptr += start;
     origptr += start;
     kernelptr += start;
 
-    while ( implicitptr<=stopptr )
+    while ( changedsurfptr<=stopptr )
     {
-	*implicitptr = *origptr + (difffactor*(*kernelptr)>>8);
+	*changedsurfptr = *origptr + (factor_*(*kernelptr)>>8);
 	kernelptr++;
-	implicitptr++;
+	changedsurfptr++;
 	origptr++;
     }
 
@@ -168,10 +187,7 @@ void MarchingCubesSurfaceEditor::reportShapeChange( bool kernelchange )
 {
     if ( kernel_ )
     {
-	if ( kernelchange ||
-	     kernelXRange().width()+1!=kernel_->info().getSize(mX) ||
-	     kernelYRange().width()+1!=kernel_->info().getSize(mY) ||
-	     kernelZRange().width()+1!=kernel_->info().getSize(mZ) )
+	if ( kernelchange )
 	{
 	    delete kernel_;
 	    kernel_ = 0;
