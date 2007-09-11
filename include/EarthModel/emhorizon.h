4495502892799c5f6872953f15371c6335bcd3a2
Nanne Hemstra <nanne.hemstra@dgbes.com>
2007-09-11 16:39:28 +0000

    call baseclass' usePar and fillPar
    git-svn-id: https://svn.opendtect.org/od/trunk@10229 a985855d-d384-4b45-ae33-31a410998c3c

--- include/EarthModel/emhorizon.h
+++ include/EarthModel/emhorizon.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Nanne Hemstra
  Date:		May 2007
- RCS:		$Id: emhorizon.h,v 1.2 2007-09-07 12:27:13 cvshelene Exp $
+ RCS:		$Id: emhorizon.h,v 1.3 2007-09-11 16:39:28 cvsnanne Exp $
 ________________________________________________________________________
 
 
@@ -39,9 +39,20 @@ public:
 					{ return const_cast<Horizon*>(this)
 					    			->geometry(); }
 								
-    void				setTiedToLvl( const char* str )
-    					{ stratlevelnm_ = str; }
+    void			setTiedToLvl( const char* str )
+    				{ stratlevelnm_ = str; }
 
+    virtual void		fillPar( IOPar& par ) const
+				{
+				    Surface::fillPar( par );
+				    par.set( sKey::StratRef, stratlevelnm_ );
+				}
+
+    virtual bool		usePar( const IOPar& par )
+				{
+				    par.get( sKey::StratRef, stratlevelnm_ );
+				    return Surface::usePar( par );
+				}
 
 protected:
     				Horizon( EMManager& emm )
@@ -51,13 +62,6 @@ protected:
 
     BufferString		stratlevelnm_;
 
-    virtual void		fillPar( IOPar& pars ) const
-				{ pars.set( sKey::StratRef, stratlevelnm_ ); }
-    virtual bool		usePar( const IOPar& pars )
-    				{ 
-				    pars.get( sKey::StratRef, stratlevelnm_ );
-				    return true;
-				}
 };
 
 } // namespace EM
