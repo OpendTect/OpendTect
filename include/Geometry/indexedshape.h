e642d9e99c9be20fe7d4b001f542d9fa4732914c
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2007-09-12 20:05:34 +0000

    Initial add.
    git-svn-id: https://svn.opendtect.org/od/trunk@10246 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Geometry/indexedshape.h
@@ -0,0 +1,89 @@
+#ifndef indexedshape_h
+#define indexedshape_h
+                                                                                
+/*+
+________________________________________________________________________
+CopyRight:     (C) dGB Beheer B.V.
+Author:        K. Tingdahl
+Date:          September 2007
+RCS:           $Id: indexedshape.h,v 1.1 2007-09-12 20:05:13 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+#include "sets.h"
+#include "thread.h"
+
+class CoordList;
+
+namespace Geometry
+{
+
+/*!A geomtetry that is defined by a number of coordinates (defined outside
+   the class), by specifying connections between the coordiates. */
+
+class IndexedGeometry
+{
+public:
+    enum	Type { Lines, TriangleStrip, TriangleFan };
+    enum	NormalBinding { PerVertex, PerFace };
+
+    		IndexedGeometry(Type,NormalBinding=PerFace,
+				CoordList* coords=0, CoordList* normals=0);
+		/*!<If coords or normals are given, used indices will be
+		    removed when object deleted or removeAll is called. If
+		    multiple geometries are sharing the coords/normals, 
+		    this is probably not what you want. */
+    virtual 	~IndexedGeometry();
+
+    void	removeAll();
+
+
+    Threads::Mutex	lock_;
+
+    Type		type_;
+    NormalBinding	normalbinding_;
+
+    TypeSet<int>	coordindices_;
+    TypeSet<int>	normalindices_;
+
+    mutable bool	ischanged_;
+
+protected:
+    CoordList*		coordlist_;
+    CoordList*		normallist_;
+};
+
+
+/*!Defines a shape with coodinates and connections between them. The shape
+   is defined in an ObjectSet of IndexedGeometry. All IndexedGeometry share
+   one common coordinate and normal list. */
+
+class IndexedShape
+{
+public:
+    virtual 		~IndexedShape();
+
+    virtual void	setCoordList(CoordList* cl,CoordList* nl=0);
+    virtual bool	needsUpdate() const		{ return true; }
+    virtual bool	update(bool forceall)		{ return true; }
+
+    virtual void	setRightHandedNormals(bool);
+    virtual void	removeAll();
+
+    const ObjectSet<IndexedGeometry>&	getGeometry() const;
+
+protected:
+    				IndexedShape();
+
+    Threads::ReadWriteLock	geometrieslock_;
+    ObjectSet<IndexedGeometry>	geometries_;
+
+    CoordList*			coordlist_;
+    CoordList*			normallist_;
+    bool			righthandednormals_;
+};
+
+}; //namespace
+
+#endif
---
+++ src/Geometry/indexedshape.cc
@@ -0,0 +1,112 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : March 2006
+-*/
+
+static const char* rcsID = "$Id: indexedshape.cc,v 1.1 2007-09-12 20:05:34 cvskris Exp $";
+
+#include "indexedshape.h"
+
+#include "positionlist.h"
+
+namespace Geometry
+{
+
+
+IndexedGeometry::IndexedGeometry( Type type, NormalBinding nb,
+				  CoordList* coords, CoordList* normals )
+    : coordlist_( coords )
+    , type_( type )
+    , normalbinding_( nb )
+    , normallist_( normals )
+    , ischanged_( true )
+{
+    if ( coordlist_ ) coordlist_->ref();
+    if ( normallist_ ) normallist_->ref();
+}
+
+
+IndexedGeometry::~IndexedGeometry()
+{
+    removeAll();
+
+    Threads::MutexLocker lock( lock_ );
+    if ( coordlist_ ) coordlist_->unRef(); coordlist_ = 0;
+    if ( normallist_ ) normallist_->unRef(); normallist_ = 0;
+}
+
+void IndexedGeometry::removeAll()
+{
+    Threads::MutexLocker lock( lock_ );
+    if ( coordlist_ )
+    {
+	for ( int idx=coordindices_.size()-1; idx>=0; idx-- )
+	{
+	    if ( coordindices_[idx]<0 )
+		continue;
+
+	    coordlist_->remove( coordindices_[idx] );
+	}
+    }
+
+    if ( normallist_ )
+    {
+	for ( int idx=normalindices_.size()-1; idx>=0; idx-- )
+	{
+	    if ( normalindices_[idx]<0 )
+		continue;
+
+	    normallist_->remove( normalindices_[idx] );
+	}
+    }
+
+    coordindices_.erase();
+    normalindices_.erase();
+}
+
+
+IndexedShape::IndexedShape()
+    : coordlist_( 0 )
+    , normallist_( 0 )
+    , righthandednormals_( true )
+{}
+
+
+IndexedShape::~IndexedShape()
+{
+    setCoordList( 0, 0 );
+}
+
+
+void IndexedShape::setCoordList( CoordList* cl,CoordList* nl )
+{
+    removeAll();
+
+    if ( coordlist_ ) coordlist_->unRef();
+    coordlist_ = cl;
+    if ( coordlist_ ) coordlist_->ref();
+
+    if ( normallist_ ) normallist_->unRef();
+    normallist_ = nl;
+    if ( normallist_ ) normallist_->ref();
+}
+
+
+void IndexedShape::setRightHandedNormals( bool yn )
+{ righthandednormals_ = yn; }
+
+
+void IndexedShape::removeAll()
+{
+    geometrieslock_.writeLock();
+    deepErase( geometries_ );
+    geometrieslock_.writeUnLock();
+}
+
+
+const ObjectSet<IndexedGeometry>& IndexedShape::getGeometry() const
+{ return geometries_; }
+
+
+}; //namespace
