e2ef301387640dd2d343a6ca1ca463164c06a5a4
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2007-10-03 14:03:13 +0000

    Added prestackstacker.cc/.h
    git-svn-id: https://svn.opendtect.org/od/trunk@10396 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/PreStackProcessing/prestackstacker.h
@@ -0,0 +1,60 @@
+#ifndef prestackstacker_h
+#define prestackstacker_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	Bert Bril
+ Date:		Nov 2006
+ RCS:		$Id: prestackstacker.h,v 1.1 2007-10-03 14:03:13 cvskris Exp $
+________________________________________________________________________
+
+
+-*/
+
+#include "prestackprocessor.h"
+#include "multiid.h"
+
+
+template <class T> class Interval;
+
+
+namespace PreStack
+{
+
+class Stack : public Processor
+{
+public:
+
+    static void			initClass();
+    static Processor*		createFunc();
+    static const char*		sName()			{ return "Stack"; }
+
+ 				Stack();
+    				~Stack();
+
+    const char*			name() const		{ return sName(); }
+    void			fillPar(IOPar&) const;
+    bool			usePar(const IOPar&);
+    const char*			errMsg() const;
+
+    void			setOffsetRange(const Interval<float>*);
+				//!<Null pointer means all offsets
+    const Interval<float>*	getOffsetRange() const;
+				//!<Null pointer means all offsets
+
+protected:
+    static const char*		sKeyOffsetRange() { return "Offset Range"; }
+    Gather*			createOutputArray(const Gather&) const;
+    int				nrTimes() const;
+    bool			doWork(int,int,int);
+
+    BufferString		errmsg_;
+    Interval<float>*		offsetrg_;
+};
+
+
+}; //namespace
+
+#endif
---
+++ src/PreStackProcessing/prestackstacker.cc
@@ -0,0 +1,129 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : April 2005
+-*/
+
+static const char* rcsID = "$Id: prestackstacker.cc,v 1.1 2007-10-03 14:03:13 cvskris Exp $";
+
+#include "prestackstacker.h"
+
+#include "ranges.h"
+//#include "ioobj.h"
+//#include "ioman.h"
+#include "iopar.h"
+//#include "muter.h"
+#include "prestackgather.h"
+//#include "prestackmutedef.h"
+//#include "prestackmutedeftransl.h"
+
+
+using namespace PreStack;
+
+void Stack::initClass()
+{
+    PF().addCreator( Stack::createFunc, Stack::sName() );
+}
+
+
+Processor* Stack::createFunc()
+{ return new Stack; }
+
+
+Stack::Stack()
+    : offsetrg_( 0 )
+{ }
+
+
+Stack::~Stack()
+{
+    delete offsetrg_;
+}
+
+
+const char* Stack::errMsg() const
+{ return errmsg_.isEmpty() ? 0 : errmsg_.buf(); }
+
+
+#define mErrRet(s) \
+{ delete muter_; muter_ = 0; errmsg_ = s; return false; }
+
+
+void Stack::setOffsetRange( const Interval<float>* offrg )
+{
+    if ( offrg )
+    {
+	if ( !offsetrg_ ) offsetrg_ = new Interval<float>( *offrg );
+	else *offsetrg_ = *offrg;
+    }
+    else
+    {
+	delete offsetrg_;
+	offsetrg_ = 0;
+    }
+}
+
+
+void Stack::fillPar( IOPar& par ) const
+{
+    if ( offsetrg_ )
+	par.set( sKeyOffsetRange(), offsetrg_->start, offsetrg_->stop );
+}
+
+
+bool Stack::usePar( const IOPar& par )
+{
+    Interval<float> rg;
+    setOffsetRange( par.get( sKeyOffsetRange(), rg ) ? &rg : 0 );
+
+    return true;
+}
+
+#undef mErrRet
+#define mErrRet(s) { errmsg_ = s; return false; }
+
+bool Stack::doWork( int start, int stop, int )
+{
+    const int nroffsets = input_->data().info().getSize(Gather::offsetDim());
+    for ( int idz=start; idz<=stop; idz++ )
+    {
+	int nrvals = 0;
+	float stack;
+	for ( int ioff=0; ioff<nroffsets; ioff++ )
+	{
+	    const float offset = input_->getOffset(ioff);
+	    if ( offsetrg_ && !offsetrg_->includes( offset ) )
+		continue;
+
+	    const float val = input_->data().get( ioff, idz );
+	    if ( mIsUdf(val) )
+		continue;
+
+	    if ( !nrvals ) stack = val;
+	    else stack += val;
+
+	    nrvals += 0;
+	}
+
+	output_->data().set( 0, idz, nrvals ? stack/nrvals : mUdf(float) );
+    }
+
+    return true;
+}
+
+
+Gather* Stack::createOutputArray( const Gather& input ) const
+{
+    Gather* res = new Gather( input );
+    if ( !res->setSize( 1, input.size(false) ) )
+    {
+	delete res;
+	return 0;
+    }
+
+    return res;
+}
+
+
+int Stack::nrTimes() const
+{ return input_->data().info().getSize( Gather::zDim() ); }
