e3b65d20b0fe77fb0519b4c6f7de42d08fd83407
Yuancheng Liu <yuancheng.liu@dgbes.com>
2007-11-09 16:28:34 +0000

    Fixed a bug.
    git-svn-id: https://svn.opendtect.org/od/trunk@10701 a985855d-d384-4b45-ae33-31a410998c3c

--- src/AttributeEngine/attribdesc.cc
+++ src/AttributeEngine/attribdesc.cc
@@ -5,7 +5,7 @@
 -*/
 
 
-static const char* rcsID = "$Id: attribdesc.cc,v 1.60 2007-11-08 17:54:21 cvsyuancheng Exp $";
+static const char* rcsID = "$Id: attribdesc.cc,v 1.61 2007-11-09 16:28:34 cvsyuancheng Exp $";
 
 #include "attribdesc.h"
 
@@ -546,7 +546,7 @@ bool Desc::getAttribName( const char* defstr_, BufferString& res )
 
     int stop = start+1;
     while ( stop<strlen(defstr) && !isspace(defstr[stop]) ) stop++;
-    defstr[stop+1] = 0;
+    defstr[stop] = 0;
 
     res = &defstr[start];
     return true;
