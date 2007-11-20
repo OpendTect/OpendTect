10a45085c6d471e483d7b94c9f2782c585d91600
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2007-11-20 18:20:20 +0000

    Added MultiArrayValueSeries.
    git-svn-id: https://svn.opendtect.org/od/trunk@10756 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/valseries.h
+++ include/Basic/valseries.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Bert Bril & Kris Tingdahl
  Date:          Mar 2005
- RCS:           $Id: valseries.h,v 1.13 2007-09-13 19:38:39 cvsnanne Exp $
+ RCS:           $Id: valseries.h,v 1.14 2007-11-20 18:20:20 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -109,6 +109,39 @@ protected:
 
 #undef mImplArr
 
+#define mChunkSize	0x20000000
+
+
+/*!Valueseries that allocates its data in smaller chunks
+   (default is 512MB). Bydoing this, it performs better in environments
+   where the memory is fragmented (i.e. windows 32 bit). */
+
+template <class RT, class AT>
+class MultiArrayValueSeries : public ValueSeries<RT>
+{
+public:
+    		MultiArrayValueSeries(od_int64);
+    		~MultiArrayValueSeries();
+
+    bool	isOK() const			{ return ptrs_.size(); }
+
+    RT		value( od_int64 idx ) const;
+    bool	writable() const		{ return true; }
+    void	setValue(od_int64 idx, RT v);
+
+    const RT*	arr() const;
+    RT*		arr();
+
+    bool	reSizeable() const		{ return true; }
+    inline bool	setSize(od_int64);
+    od_int64	size() const			{ return cursize_; }
+
+protected:
+    ObjectSet<AT>	ptrs_;
+    od_int64		cursize_;
+    const unsigned int	chunksize_;
+};
+
 
 
 template <class T> inline
@@ -170,13 +203,13 @@ ArrayValueSeries<RT,AT>::ArrayValueSeries(AT* ptr, bool memmine,od_int64 cursz )
 
 template <class RT, class AT>
 ArrayValueSeries<RT,AT>::ArrayValueSeries( od_int64 sz )
-    : mine_(true), cursize_( sz )
+    : mine_(true), cursize_( -1 )
 {
-    mTryAlloc( ptr_, AT[sz] )
+    setSize( sz );
 }
 
 
-template <class RT,class AT>
+template <class RT,class AT> inline
 bool ArrayValueSeries<RT,AT>::setSize( od_int64 sz )
 {
     if ( cursize_!=-1 && cursize_==sz && ptr_ ) return true;
@@ -188,5 +221,96 @@ bool ArrayValueSeries<RT,AT>::setSize( od_int64 sz )
     return ptr_;
 }
 
+
+template <class RT, class AT> inline
+MultiArrayValueSeries<RT,AT>::MultiArrayValueSeries( od_int64 sz )
+    : cursize_( -1 )
+    , chunksize_( mChunkSize/sizeof(AT) )
+{
+    ptrs_.allowNull( true );
+    setSize( sz );
+}
+
+
+template <class RT, class AT> inline
+MultiArrayValueSeries<RT,AT>::~MultiArrayValueSeries()
+{
+    deepEraseArr( ptrs_ );
+}
+
+
+template <class RT, class AT> inline
+RT MultiArrayValueSeries<RT,AT>::value( od_int64 idx ) const
+{
+    const od_int64 arridx = idx/chunksize_;
+    if ( arridx>=ptrs_.size() )
+	return RT();
+
+    idx -= arridx*chunksize_;
+    return ptrs_[arridx][idx];
+}
+
+
+template <class RT, class AT> inline
+void MultiArrayValueSeries<RT,AT>::setValue( od_int64 idx, RT v )
+{
+    const od_int64 arridx = idx/chunksize_;
+    if ( arridx>=ptrs_.size() )
+	return;
+
+    idx -= arridx*chunksize_;
+    ptrs_[arridx][idx] = v;
+}
+
+
+template <class RT, class AT> inline
+RT* MultiArrayValueSeries<RT,AT>::arr()
+{
+    return typeid(RT)==typeid(AT) && cursize_<=chunksize_
+	? (RT*) ptrs_[0] : (RT*) 0;
+}
+
+
+template <class RT, class AT> inline
+const RT* MultiArrayValueSeries<RT,AT>::arr() const
+{
+    return typeid(RT)==typeid(AT) && cursize_<chunksize_
+	? (RT*) ptrs_[0] : (RT*) 0;
+}
+
+
+template <class RT, class AT> inline
+bool MultiArrayValueSeries<RT,AT>::setSize( od_int64 sz )
+{
+    if ( cursize_==sz )
+	return true;
+
+    od_int64 lefttoalloc = sz;
+    deepEraseArr( ptrs_ );
+
+    while ( lefttoalloc>0 )
+    {
+	const od_int64 allocsize = lefttoalloc>=chunksize_
+	    ? chunksize_ : lefttoalloc;
+
+	AT* ptr;
+	mTryAlloc( ptr, AT[allocsize] );
+	if ( !ptr )
+	{
+	    cursize_ = -1;
+	    deepEraseArr( ptrs_ );
+	    return false;
+	}
+
+	ptrs_ += ptr;
+
+	lefttoalloc -= allocsize;
+    }
+
+    cursize_ = sz;
+    return true;
+}
+
+#undef mChunkSize
 
 #endif
