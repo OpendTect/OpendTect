9702f94926abf2f976471ce042eefa5074c12b55
Raman Singh <raman.singh@dgbes.com>
2007-12-07 12:11:35 +0000

    added color table
    git-svn-id: https://svn.opendtect.org/od/trunk@10852 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiAttributes/uiattr2dsel.h
+++ include/uiAttributes/uiattr2dsel.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        R. K. Singh
  Date:          Nov 2007
- RCS:           $Id: uiattr2dsel.h,v 1.1 2007-12-06 11:10:27 cvsraman Exp $
+ RCS:           $Id: uiattr2dsel.h,v 1.2 2007-12-07 12:11:35 cvsraman Exp $
 ________________________________________________________________________
 
 -*/
@@ -20,6 +20,8 @@ namespace Attrib { class Desc; class DescSet; class SelInfo; class SelSpec; };
 
 class MultiID;
 class uiButtonGroup;
+class uiCheckBox;
+class uiComboBox;
 class uiGenInput;
 class uiListBox;
 class uiRadioButton;
@@ -36,6 +38,8 @@ public:
     int			getSelType()		{ return seltype_; }
     const char*		getStoredAttrName()	{ return storednm_.buf(); }
     Attrib::DescID	getSelDescID()		{ return descid_; }
+    bool		needToSetColTab()	{ return usecoltab_; }
+    const char*		getColTabName()		{ return coltabnm_.buf(); }
 
 protected:
 
@@ -43,7 +47,10 @@ protected:
     const MultiID&	setid_;
     Attrib::DescID	descid_;
     int			seltype_;
+    bool		usecoltab_;
     BufferString	storednm_;
+    BufferString	curnm_;
+    BufferString	coltabnm_;
 
     uiButtonGroup*	selgrp_;
     uiRadioButton*	storfld_;
@@ -51,12 +58,16 @@ protected:
 
     uiListBox*		storoutfld_;
     uiListBox*		attroutfld_;
+    uiCheckBox*		coltabfld_;
+    uiComboBox*		coltabsel_;
 
     void		createSelectionButtons();
     void		createSelectionFields();
+    void		createColorFields();
 
     void		doFinalise( CallBacker* );
     void		selDone(CallBacker*);
+    void		colTabSel(CallBacker*);
     virtual bool	acceptOK(CallBacker*);
     int			selType() const;
 };
