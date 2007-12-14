317eb1dd074fb7cac42d3109b1688c30e9fe828e
Nanne Hemstra <nanne.hemstra@dgbes.com>
2007-12-14 05:52:55 +0000

    typo in filename
    git-svn-id: https://svn.opendtect.org/od/trunk@10913 a985855d-d384-4b45-ae33-31a410998c3c

--- src/AttributeEngine/initattributeengine.cc
+++ src/AttributeEngine/initattributeengine.cc
@@ -4,12 +4,12 @@
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Satyaki
  Date:          December 2007
- RCS:           $Id: initattributeengine.cc,v 1.1 2007-12-14 05:10:16 cvssatyaki Exp $
+ RCS:           $Id: initattributeengine.cc,v 1.2 2007-12-14 05:52:55 cvsnanne Exp $
         ________________________________________________________________________
 
 	-*/
 
-#include "initatrributeengine.h"
+#include "initattributeengine.h"
 #include "attribstorprovider.h"
 
 void AttributeEngine::initStdClasses()
