afae3e9869e1ee652ca18f8dfa08eec50a59001e
Nanne Hemstra <nanne.hemstra@dgbes.com>
2007-12-19 08:59:52 +0000

    Added some doc
    git-svn-id: https://svn.opendtect.org/od/trunk@10942 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiBase/uisplitter.h
+++ include/uiBase/uisplitter.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne Hemstra
  Date:          May 2007
- RCS:           $Id: uisplitter.h,v 1.1 2007-05-04 05:49:35 cvsnanne Exp $
+ RCS:           $Id: uisplitter.h,v 1.2 2007-12-19 08:59:52 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -17,15 +17,31 @@ ________________________________________________________________________
 class uiGroup;
 class uiSplitterBody;
 
+/*! \brief Provides a splitter object
+
+A splitter lets the user control the size of its children by dragging the
+handle between them. A default splitter lays out its children horizontally
+(side by side).
+Example:
+\code
+    uiGroup* leftgrp = new uiGroup( 0, "Left Group" );
+    uiGroup* rightgrp = new uiGroup( 0, "Right Group" );
+    uiSplitter* splitter = new uiSplitter( this );
+    splitter->addGroup( leftgrp );
+    splitter->addGroup( rightgrp );
+\endcode
+
+*/
+
 class uiSplitter : public uiObject
 {
 public:
-
                         uiSplitter(uiParent*,const char* nm="Splitter", 
 				    bool hor=true);
+			//!< Set hor to false to layout vertically
 
-    void		addObject(uiObject*);
-    void		addGroup(uiGroup*);
+    void		addObject(uiObject*); //!< Object becomes my child
+    void		addGroup(uiGroup*); //!< Group becomes my child
 
 private:
 
