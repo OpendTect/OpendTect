0042d4146a4f7acab1b9da85477cca1cd803f324
Nanne Hemstra <nanne.hemstra@dgbes.com>
2007-12-21 09:38:32 +0000

    moved some initStdClasses to uiodmain
    git-svn-id: https://svn.opendtect.org/od/trunk@10953 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiCoin/uicmain.cc
+++ src/uiCoin/uicmain.cc
@@ -4,18 +4,17 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H. Lammertink
  Date:          06/02/2002
- RCS:           $Id: uicmain.cc,v 1.19 2007-12-14 05:15:23 cvssatyaki Exp $
+ RCS:           $Id: uicmain.cc,v 1.20 2007-12-21 09:38:32 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uicmain.h"
-#include "initsood.h"
-#include "initvisbase.h"
-#include "initvissurvey.h"
+
 #include "debug.h"
 #include "debugmasks.h"
 #include "envvars.h"
+
 #include <Inventor/Qt/SoQt.h>
 #include <Inventor/SoDB.h>
 
@@ -33,25 +32,9 @@ void uicMain::init( QWidget* widget )
 	SetEnvVar( "COIN_FULL_INDIRECT_RENDERING", "1" );
 
     if ( DBG::isOn(DBG_UI) )
-	DBG::message( "SoQt::initStdClasses() ..." );
+	DBG::message( "uicMain::init()" );
     SoQt::init( widget );
-
-    SoOD::initStdClasses();
-
-    if ( DBG::isOn(DBG_UI) )
-	DBG::message( "done. visBase::initStdClasses() ..." );
-    visBase::initStdClasses();
-
-    if ( DBG::isOn(DBG_UI) )
-	DBG::message( "done. visSurvey::initStdClasses() ..." );
-    visSurvey::initStdClasses();
-
-    if ( DBG::isOn(DBG_UI) )
-	DBG::message( "done. SoDB::initStdClasses() ..." );
     SoDB::init();
-
-    if ( DBG::isOn(DBG_UI) )
-	DBG::message( "done." );
 }
 
 
