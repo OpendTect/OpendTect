1538f73f1ae9cd09ffbf8580119826e6726ac4c9
Bert Bril <bert.bril@dgbes.com>
2007-12-31 15:57:58 +0000

    added maskfile_, got rid of datapath_
    git-svn-id: https://svn.opendtect.org/od/trunk@10994 a985855d-d384-4b45-ae33-31a410998c3c

--- plugins/Madagascar/madio.cc
+++ plugins/Madagascar/madio.cc
@@ -4,7 +4,7 @@
  * DATE     : June 2007
 -*/
 
-static const char* rcsID = "$Id: madio.cc,v 1.3 2007-12-20 16:18:54 cvsbert Exp $";
+static const char* rcsID = "$Id: madio.cc,v 1.4 2007-12-31 15:57:58 cvsbert Exp $";
 
 #include "madio.h"
 #include "keystrs.h"
@@ -12,9 +12,10 @@ static const char* rcsID = "$Id: madio.cc,v 1.3 2007-12-20 16:18:54 cvsbert Exp 
 #include "filepath.h"
 #include "strmprov.h"
 #include "envvars.h"
+#include "oddirs.h"
 #include "iopar.h"
 
-const char* ODMad::FileSpec::sKeyDataPath = "Data Path";
+const char* ODMad::FileSpec::sKeyMaskFile = "Mask File Name";
 const char* ODMad::sKeyMadagascar = "Madagascar";
 const char* ODMad::sKeyMadSelKey = "909090";
 
@@ -25,45 +26,59 @@ ODMad::FileSpec::FileSpec( bool fr )
 }
 
 
-static BufferString getDataFnm( const char* fnm, const char* dir, bool forread )
+bool ODMad::FileSpec::fileNameOK( const char* fnm ) const
 {
-    FilePath fp( fnm ); fp.setPath( dir );
-    const BufferString ret( fp.fullPath() );
-    if ( !forread )
+    if ( !forread_ )
     {
-	if ( File_isWritable(dir) )
-	    return ret;
+	FilePath fp( fnm );
+	if ( !File_isWritable(fp.pathOnly()) )
+	{
+	    errmsg_ = "Directory '";
+	    errmsg_ += fp.pathOnly();
+	    errmsg_ += "' is not writable";
+	    return false;
+	}
     }
-    else if ( !File_isEmpty(ret) )
-	return ret;
+    else if ( fnm && *fnm && File_isEmpty(fnm) )
+    {
+	errmsg_ = "File '";
+	errmsg_ += fnm;
+	errmsg_ += "' is not readable";
+	return false;
+    }
 
-    return BufferString();
+    return true;
 }
 
 
-static BufferString getDataPath( const char* dp, const char* fnm, bool forread )
+bool ODMad::FileSpec::set( const char* fnm, const char* maskfnm )
 {
-    BufferString ret( dp );
-    if ( dp ) return ret;
+    if ( !fnm || !*fnm )
+	{ errmsg_ = "No file name provided"; return false; }
+    FilePath fp( fnm );
+    if ( !fp.isAbsolute() )
+	fp.set( GetDataDir() ).add( sKeyMadagascar ).add( fnm );
+    fnm_ = fp.fullPath();
 
-    ret = getDataFnm( fnm, ODMad::FileSpec::defDataPath(), forread );
-    ret = FilePath( ret ).pathOnly();
-    return ret;
-}
+    if ( !maskfnm || !*maskfnm )
+	maskfnm_.setEmpty();
+    else
+    {
+	fp.set( maskfnm );
+	if ( !fp.isAbsolute() )
+	    fp.set( GetDataDir() ).add( sKeyMadagascar ).add( maskfnm );
+	maskfnm_ = fp.fullPath();
+    }
 
-
-void ODMad::FileSpec::set( const char* fnm, const char* dp )
-{
-    fname_ = fnm;
-    datapath_ = getDataPath( dp, fnm, forread_ );
+    return fileNameOK( fnm_ ) && fileNameOK( maskfnm_ );
 }
 
 
 void ODMad::FileSpec::fillPar( IOPar& iop ) const
 {
     iop.set( sKey::Type, forread_ ? "Read" : "Write" );
-    iop.set( sKey::FileName, fname_ );
-    iop.set( sKeyDataPath, datapath_ );
+    iop.set( sKey::FileName, fnm_ );
+    iop.set( sKeyMaskFile, maskfnm_ );
 }
 
 
@@ -72,64 +87,63 @@ bool ODMad::FileSpec::usePar( const IOPar& iop )
     const char* res = iop.find( sKey::Type );
     forread_ = !res || (*res != 'w' && *res != 'W');
 
-    BufferString fnm( fname_ );
+    BufferString fnm = fnm_, maskfnm = maskfnm_;
     iop.get( sKey::FileName, fnm );
-    if ( fnm.isEmpty() )
-    {
-	errmsg_ = "No filename specified";
-	return false;
-    }
-
-    BufferString dp( datapath_ );
-    iop.get( sKeyDataPath, dp );
-    set( fnm, dp );
-    if ( datapath_.isEmpty() )
-    {
-	errmsg_ = "Cannot find a valid DATAPATH";
-	return false;
-    }
-
-    return true;
+    iop.get( sKeyMaskFile, maskfnm );
+    return set( fnm, maskfnm );
 }
 
 
-const char* ODMad::FileSpec::defDataPath()
+const char* ODMad::FileSpec::defPath()
+{
+    static BufferString* ret = 0;
+    if ( ret ) return ret->buf();
+
+    FilePath fp( GetDataDir() ); fp.add( sKeyMadagascar );
+    ret = new BufferString( fp.fullPath() );
+    return ret->buf();
+}
+
+
+const char* ODMad::FileSpec::madDataPath()
 {
     static BufferString* ret = 0;
     if ( ret ) return ret->buf();
 
     ret = new BufferString( GetEnvVar("DATAPATH") );
-    if ( File_isDirectory(ret->buf()) )
-	return ret->buf();
-
-    //TODO support ~/.datapath
-    // ~/.datapath may contain: [machine_name] datapath=/path/to_file/
-
+    if ( ret->isEmpty() )
+	*ret = defPath();
     return ret->buf();
+}
+
+
+StreamData ODMad::FileSpec::open() const
+{
+    return doOpen( fnm_ );
+}
+
+
+StreamData ODMad::FileSpec::openMask() const
+{
+    return maskfnm_.isEmpty() ? StreamData() : doOpen( maskfnm_ );
 }
 
 
 #define mErrRet(s1,s2,s3) \
 	{ errmsg_ = s1; errmsg_+= s2; errmsg_ += s3; return StreamData(); }
 
-StreamData ODMad::FileSpec::open() const
+StreamData ODMad::FileSpec::doOpen( const char* fnm ) const
 {
-    FilePath fp( fname_ );
-    if ( !fname_.isEmpty() && !fp.isAbsolute() )
-	fp.setPath( datapath_ );
-
-    const BufferString fname( fp.fullPath() );
-    if ( fname_.isEmpty() )
-	const_cast<ODMad::FileSpec*>(this)->fname_ = StreamProvider::sStdIO;
-    else if ( forread_ )
+    if ( forread_ )
     {
-	if ( !File_exists(fname_) )
-	    mErrRet("File '",fname_,"' does not exist")
-	else if ( File_isEmpty(fname_) )
-	    mErrRet("File '",fname_,"' is empty")
+	if ( !File_exists(fnm) )
+	    mErrRet("File '",fnm,"' does not exist")
+	else if ( File_isEmpty(fnm) )
+	    mErrRet("File '",fnm,"' is empty")
     }
     else
     {
+	FilePath fp( fnm );
 	const BufferString dirnm( fp.pathOnly() );
 	if ( !File_isDirectory(dirnm) )
 	    mErrRet("Directory '",dirnm,"' does not exist")
@@ -137,10 +151,10 @@ StreamData ODMad::FileSpec::open() const
 	    mErrRet("Directory '",dirnm,"' is not writable")
     }
 
-    StreamData ret = forread_ ? StreamProvider(fname_).makeIStream()
-			      : StreamProvider(fname_).makeOStream();
+    StreamData ret = forread_ ? StreamProvider(fnm).makeIStream()
+			      : StreamProvider(fnm).makeOStream();
     if ( !ret.usable() )
-	mErrRet("File '",fname_,"' cannot be opened")
+	mErrRet("File '",fnm,"' cannot be opened")
 
     return ret;
 }
--- plugins/Madagascar/madio.h
+++ plugins/Madagascar/madio.h
@@ -4,7 +4,7 @@
  * COPYRIGHT: (C) dGB Beheer B.V.
  * AUTHOR   : Bert
  * DATE     : Sep 2007
- * ID       : $Id: madio.h,v 1.3 2007-12-20 16:18:54 cvsbert Exp $
+ * ID       : $Id: madio.h,v 1.4 2007-12-31 15:57:58 cvsbert Exp $
 -*/
 
 #include "bufstring.h"
@@ -18,23 +18,26 @@ namespace ODMad
 extern const char* sKeyMadagascar;
 extern const char* sKeyMadSelKey;
 
+/*!\brief Specifies file name and optional mask filename */
+
 class FileSpec
 {
 public:
 
     			FileSpec(bool forread);
-    void		set(const char* fnm,const char* datapath=0);
-				//!< If not absolute: uses datapath
-				//!<	then, if datapath==null -> $DATAPATH
+    bool		set(const char* fnm,const char* maskfnm=0);
 
-    const char*		fileName() const	{ return fname_; }
-    const char*		dataPath()		{ return datapath_; }
+    const char*		fileName() const	{ return fnm_; }
+    const char*		maskFileName()		{ return maskfnm_; }
 
     StreamData		open() const;		//!< if !usable() -> errMsg()
+    StreamData		openMask() const;	//!< if !usable() -> errMsg()
     const char*		errMsg() const		{ return errmsg_; }
 
-    static const char*	defDataPath();
-    static const char*	sKeyDataPath;
+    static const char*	defPath();	//!< returns Madagascar dir in survey
+    static const char*	madDataPath();
+
+    static const char*	sKeyMaskFile;
 
     void		fillPar(IOPar&) const;
     bool		usePar(const IOPar&);	//!< sets errMsg() if failed
@@ -42,10 +45,12 @@ public:
 protected:
 
     bool		forread_;
-    BufferString	fname_;
-    BufferString	datapath_;
+    BufferString	fnm_;
+    BufferString	maskfnm_;
     mutable BufferString errmsg_;
 
+    StreamData		doOpen(const char*) const;
+    bool		fileNameOK(const char*) const;
 };
 
 } // namespace
