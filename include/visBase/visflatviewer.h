5fb473f30c467493ddb296d922e8fb6f2bef00b5
Yuancheng Liu <yuancheng.liu@dgbes.com>
2008-01-07 20:49:37 +0000

    Added splitting capability.
    git-svn-id: https://svn.opendtect.org/od/trunk@11023 a985855d-d384-4b45-ae33-31a410998c3c

--- include/visBase/visflatviewer.h
+++ include/visBase/visflatviewer.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Yuancheng Liu
  Date:		5-11-2007
- RCS:		$Id: visflatviewer.h,v 1.1 2007-09-10 07:54:57 cvsyuancheng Exp $
+ RCS:		$Id: visflatviewer.h,v 1.2 2008-01-07 20:49:37 cvsyuancheng Exp $
 ________________________________________________________________________
 
 -*/
@@ -16,9 +16,14 @@ ________________________________________________________________________
 #include "visobject.h"
 
 template <class T> class Array2D;
+class SoSplitTexture2Part;
+
+
 
 namespace visBase
 {
+
+class Coordinates;    
 class MultiTexture2;
 class FaceSet;
 class VisColorTab;
@@ -28,25 +33,42 @@ class VisColorTab;
 class FlatViewer : public FlatView::Viewer, public VisualObjectImpl
 {
 public:
-    static FlatViewer*		create()
-				mCreateDataObj(FlatViewer);
-    				~FlatViewer();
+    static FlatViewer*	create()
+			mCreateDataObj(FlatViewer);
+    			~FlatViewer();
 
-    Notifier<FlatViewer>	dataChange;    
+    Notifier<FlatViewer> dataChange;    
 
-    void			handleChange(FlatView::Viewer::DataChangeType);
-    void			updateTextureCoords(const Array2D<float>*);
-    void			setPosition(const Coord3& c00,const Coord3& c01,					   const Coord3& c10,const Coord3& c11);
-  
-    visBase::Transformation* 	getDisplayTransformation();
-    void			setDisplayTransformation(mVisTrans*);
+    void		handleChange(FlatView::Viewer::DataChangeType);
+    void		setPosition(const Coord3& c00,const Coord3& c01,
+				    const Coord3& c10,const Coord3& c11);
+ 
 
-    void			allowShading(bool yn);  
+    mVisTrans* 		getDisplayTransformation();
+    void		setDisplayTransformation(mVisTrans*);
+
+    void		allowShading(bool yn);  
    				
 protected:
-    FaceSet*			faceset_;
-    MultiTexture2*		texture_;
 
+    static int		nrBlocks(int totalnr,int maxnr,int overlap); 
+    void		updateFaceSets(int rowsz,int colsz); 
+    void		updateCoordinates(); 
+
+    MultiTexture2*		texture_;
+    Coordinates*		coords_;
+    TypeSet<float>		c00factors_;
+    TypeSet<float>		c01factors_;
+    TypeSet<float>		c10factors_;
+    TypeSet<float>		c11factors_;
+
+    Coord3			c00_;
+    Coord3			c01_;
+    Coord3			c10_;
+    Coord3			c11_;
+
+    ObjectSet<FaceSet>		facesets_;
+    ObjectSet<SoSplitTexture2Part> splittextures_;
 };
 
 }; // Namespace
--- src/visBase/visflatviewer.cc
+++ src/visBase/visflatviewer.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Yuancheng Liu
  Date:		5-11-2007
- RCS:		$Id: visflatviewer.cc,v 1.3 2007-12-12 15:44:41 cvsbert Exp $
+ RCS:		$Id: visflatviewer.cc,v 1.4 2008-01-07 20:49:37 cvsyuancheng Exp $
 ________________________________________________________________________
 
 -*/
@@ -13,12 +13,19 @@ ________________________________________________________________________
 #include "arraynd.h"
 #include "colortab.h"
 #include "flatview.h"
+#include "simpnumer.h"
 #include "viscolortab.h"
 #include "viscoord.h"
 #include "visfaceset.h"
 #include "vismaterial.h"
 #include "vismultitexture2.h"
 #include "vistexturecoords.h"
+
+#include "SoSplitTexture2.h"
+
+
+#define mMaxRowSz 256 
+#define mMaxColSz 256
 
 mCreateFactoryEntry( visBase::FlatViewer );
 
@@ -27,33 +34,37 @@ namespace visBase
    
 FlatViewer::FlatViewer()
     : VisualObjectImpl( false )
-    , faceset_( FaceSet::create() )
-    , dataChange( this )				   
+    , dataChange( this )
     , texture_( MultiTexture2::create() )
 {
     texture_->ref();
     addChild( texture_->getInventorNode() );
+
     if ( texture_->nrTextures()<1 )
     {
     	texture_->addTexture( "Flat Viewer" );
     	texture_->enableTexture( 0, true );	
-    }	
+    }
 
-    faceset_->ref();
-    faceset_->setVertexOrdering( 
-	    visBase::VertexShape::cCounterClockWiseVertexOrdering() );
-    faceset_->setShapeType( visBase::VertexShape::cUnknownShapeType() );
-    faceset_->removeSwitch();
-    
+    coords_ = Coordinates::create();
+    coords_->ref();
+    addChild( coords_->getInventorNode() );
     getMaterial()->setColor( Color( 255,255,255 ) , 0 );
-    addChild( faceset_->getInventorNode() );
 }
 
 
 FlatViewer::~FlatViewer()
 {
-    faceset_->unRef();
-    texture_->unRef();
+    if ( texture_ ) 
+	texture_->unRef();
+
+    if ( coords_ )
+	coords_->unRef();
+
+    deepUnRef( facesets_ );
+
+    for ( int idx=0; idx<splittextures_.size(); idx++ )
+	splittextures_[idx]->unref();
 }
 
 
@@ -73,22 +84,25 @@ void FlatViewer::handleChange( FlatView::Viewer::DataChangeType dt )
 		if ( dt!=All )
 		    break;
 	case VDData:
-	{
-	    const FlatDataPack* dp = pack( false );
-	    if ( dp )
 	    {
-		texture_->setData( 0, 0, &dp->data(), true ); 
-		texture_->turnOn( appearance().ddpars_.vd_.show_ );
-		updateTextureCoords( &dp->data() );
-		texture_->turnOn( true );
-	    }
-	    else
-		texture_->turnOn( false );
+		const FlatDataPack* dp = pack( false );
+		if ( dp )
+		{
+		    const int colsz = dp->data().info().getSize(0);
+		    const int rowsz = dp->data().info().getSize(1);
+		    
+		    texture_->setData(0, 0, &dp->data(), true);
+		    texture_->turnOn( appearance().ddpars_.vd_.show_ ); 
+		    updateFaceSets( rowsz, colsz );
+		    texture_->turnOn( true );
+		}
+		else
+		    texture_->turnOn( false );
 
-	    dataChange.trigger();
-	    if ( dt!=All )
-		break;
-	 }
+		dataChange.trigger();
+		if ( dt!=All )
+		    break;
+	    }
 	case VDPars : 	
 		const char* ctabname = appearance().ddpars_.vd_.ctab_.buf();
 		visBase::VisColorTab& vct = texture_->getColorTab( 0 );
@@ -107,85 +121,210 @@ void FlatViewer::handleChange( FlatView::Viewer::DataChangeType dt )
 }
 
 
-void FlatViewer::updateTextureCoords( const Array2D<float>* array )
+int FlatViewer::nrBlocks( int totalnr, int base, int overlap )
 {
-    if ( array )
+    int res = 0;
+    while ( totalnr>base )
     {
-	const int x1sz =  array->info().getSize( 0 );
-	const int x2sz =  array->info().getSize( 1 );
-
-	const float x1translation = 0.5/( x1sz );
-	const float x2translation = 0.5/( x2sz );
-
-	const Coord c00( x2translation, x1translation );
-	const Coord c01( x2translation, 1-x1translation );
-	const Coord c10( 1-x2translation, x1translation );
-	const Coord c11( 1-x2translation, 1-x1translation );
-	if ( !faceset_->getTextureCoords() )
-	    faceset_->setTextureCoords( TextureCoords::create() );
-
-	if ( !appearance().annot_.x1_.reversed_ &&
-	     !appearance().annot_.x2_.reversed_ )
-	{
-	    faceset_->getTextureCoords()->setCoord( 0, c10 );
-	    faceset_->getTextureCoords()->setCoord( 1, c11 );
-	    faceset_->getTextureCoords()->setCoord( 2, c01 );
-	    faceset_->getTextureCoords()->setCoord( 3, c00 );
-	}
-	else if ( appearance().annot_.x1_.reversed_ &&
-		  !appearance().annot_.x2_.reversed_ )
-	{
-	    faceset_->getTextureCoords()->setCoord( 0, c11 );
-	    faceset_->getTextureCoords()->setCoord( 1, c10 );
-	    faceset_->getTextureCoords()->setCoord( 2, c00 );
-	    faceset_->getTextureCoords()->setCoord( 3, c01 );
-	}
-	else if ( !appearance().annot_.x1_.reversed_ &&
-		  appearance().annot_.x2_.reversed_ )
-	{
-	    faceset_->getTextureCoords()->setCoord( 0, c00 );
-	    faceset_->getTextureCoords()->setCoord( 1, c01 );
-	    faceset_->getTextureCoords()->setCoord( 2, c11 );
-	    faceset_->getTextureCoords()->setCoord( 3, c10 );
-	}	    
-	else
-	{
-	    faceset_->getTextureCoords()->setCoord( 0, c01 );
-	    faceset_->getTextureCoords()->setCoord( 1, c00 );
-	    faceset_->getTextureCoords()->setCoord( 2, c10 );
-	    faceset_->getTextureCoords()->setCoord( 3, c11 );
-	}
+	res++;
+	totalnr = totalnr - base + overlap;
     }
+
+    return res+1;
 }
 
-	
-void FlatViewer::setPosition( const Coord3& c00,const Coord3& c01, 
-			      const Coord3& c10,const Coord3& c11 )
+
+void FlatViewer::updateFaceSets( int rowsz, int colsz )
 {
-    faceset_->getCoordinates()->setPos( 0, c00 );
-    faceset_->getCoordinates()->setPos( 1, c01 );
-    faceset_->getCoordinates()->setPos( 2, c11 );
-    faceset_->getCoordinates()->setPos( 3, c10 );
-    
-    if ( faceset_->nrCoordIndex()<4 )
+    const bool dosplit =texture_->canUseShading() || texture_->splitsTexture();
+
+    const int nrrowblocks = dosplit ? nrBlocks( rowsz, mMaxRowSz, 1 ) : 1;
+    const int nrcolblocks = dosplit ? nrBlocks( colsz, mMaxColSz, 1 ) : 1;
+    if ( nrrowblocks > 1 || nrcolblocks > 1 )
+	texture_->splitTexture( true );
+
+    ObjectSet<FaceSet> unusedfacesets = facesets_;
+    ObjectSet<SoSplitTexture2Part> unusedsplittextures = splittextures_;
+
+    for ( int row=0; row<nrrowblocks; row++ )
     {
-	faceset_->setCoordIndex( 0, 0 );
-	faceset_->setCoordIndex( 1, 1 );
-	faceset_->setCoordIndex( 2, 2 );
-	faceset_->setCoordIndex( 3, 3 );
+	const int firstrow = row * (mMaxRowSz-1);
+	int lastrow = firstrow + mMaxRowSz-1;
+	if ( lastrow>=rowsz ) lastrow = rowsz-1;
+
+	for ( int col=0; col<nrcolblocks; col++ )
+	{
+	    const int firstcol = col * (mMaxColSz-1);
+	    int lastcol = firstcol + mMaxColSz-1;
+	    if ( lastcol>=colsz ) lastcol = colsz-1;
+	    
+	    FaceSet* fs = 0;
+	    SoSplitTexture2Part* sp = 0;
+
+	    if ( unusedfacesets.size() )
+	    {
+		fs = unusedfacesets.remove( 0 );
+		if ( dosplit )
+		    sp =  unusedsplittextures.remove( 0 );
+	    }
+	    else
+	    {
+		if ( dosplit )
+		{
+		    sp = new SoSplitTexture2Part;
+		    sp->ref();
+		    addChild( sp );
+		    splittextures_ += sp;
+		}
+
+		fs = FaceSet::create();
+		fs->ref();
+		fs->setCoordinates( 0 );
+		fs->setVertexOrdering( visBase::VertexShape::
+			cCounterClockWiseVertexOrdering() );
+		fs->setShapeType( visBase::VertexShape::cUnknownShapeType() );
+		fs->setTextureCoordIndex( 0, 0 );
+		fs->setTextureCoordIndex( 1, 1 );
+		fs->setTextureCoordIndex( 2, 2 );
+		fs->setTextureCoordIndex( 3, 3 );
+		fs->removeSwitch();
+		if ( !fs->getTextureCoords() )
+		    fs->setTextureCoords( TextureCoords::create() );
+		addChild( fs->getInventorNode() );
+		facesets_ += fs;
+	    }
+
+	    const int rowsz = lastrow-firstrow+1;
+	    const int colsz = lastcol-firstcol+1;
+	    const int texturerowsz = dosplit ? nextPower( rowsz, 2 ) : rowsz; 
+	    const int texturecolsz = dosplit ? nextPower( colsz, 2 ) : colsz;
+
+	    if ( sp )
+	    {
+    		sp->origin.setValue( firstrow, firstcol );
+		sp->size.setValue( texturerowsz, texturecolsz );
+
+		const TypeSet<int>& units = texture_->getUsedTextureUnits();
+		for ( int idx=0; idx<units.size(); idx++ )
+		    sp->textureunits.set1Value( idx, units[idx] );
+
+		sp->textureunits.deleteValues( units.size() );
+	    }
+
+	    const float rowstartmargin = 0.5/texturerowsz;
+	    const float colstartmargin = 0.5/texturecolsz;
+	    const float rowendmargin = (float)rowsz/texturerowsz-rowstartmargin;
+	    const float colendmargin = (float)colsz/texturecolsz-colstartmargin;
+
+	    const Coord c00( rowstartmargin, colstartmargin );
+	    const Coord c01( rowstartmargin, colendmargin );
+	    const Coord c10( rowendmargin, colstartmargin );
+	    const Coord c11( rowendmargin, colendmargin );
+	    
+	    fs->getTextureCoords()->setCoord( 0, c00 );
+	    fs->getTextureCoords()->setCoord( 1, c01 );
+	    fs->getTextureCoords()->setCoord( 2, c11 );
+	    fs->getTextureCoords()->setCoord( 3, c10 );
+
+	    fs->setCoordIndex( 0, row*(nrcolblocks+1) + col );
+	    fs->setCoordIndex( 1, row*(nrcolblocks+1) + col + 1 );
+	    fs->setCoordIndex( 2, (row+1)*(nrcolblocks+1) + col + 1);
+	    fs->setCoordIndex( 3, (row+1)*(nrcolblocks+1) + col );
+	}
     }
+
+    c00factors_.erase();
+    c01factors_.erase();
+    c10factors_.erase();
+    c11factors_.erase();
+
+    for ( int idx=0; idx<=nrrowblocks; idx++ )
+    {
+	const float rowfactor = (float) idx/nrrowblocks;
+
+	for ( int idy=0; idy<=nrcolblocks; idy++ )
+	{
+	    const float colfactor = (float) idy/nrcolblocks;
+
+	    c00factors_ += (1-rowfactor)*(1-colfactor);
+	    c01factors_ += (1-rowfactor)*colfactor;
+	    c10factors_ += rowfactor*(1-colfactor);
+	    c11factors_ += rowfactor*colfactor;
+	}
+    }
+
+
+    for ( int idx=unusedfacesets.size()-1; idx>=0; idx-- )
+    {
+	facesets_ -= unusedfacesets[idx];
+	removeChild( unusedfacesets[idx]->getInventorNode() );
+	unusedfacesets[idx]->unRef();
+	unusedfacesets.remove( idx );
+
+	splittextures_ -= unusedsplittextures[idx];
+	removeChild( unusedsplittextures[idx] );
+	unusedsplittextures[idx]->unref();
+	unusedsplittextures.remove( idx );
+    }
+
+    updateCoordinates();
+}
+
+
+
+void FlatViewer::updateCoordinates()
+{
+    Coord3 c00 = c00_;
+    Coord3 c10 = c10_;
+    Coord3 c11 = c11_;
+    Coord3 c01 = c01_;
+    if ( appearance().annot_.x1_.reversed_ )
+    { 
+	Swap( c00, c01 );
+	Swap( c10, c11 );
+    }
+
+    if ( appearance().annot_.x2_.reversed_ )
+    { 
+	Swap( c00, c10 );
+	Swap( c01, c11 );
+    }
+
+    for ( int idx=0; idx<c00factors_.size(); idx++ )
+	coords_->setPos( idx, c00factors_[idx]*c00 + c01factors_[idx]*c01 +
+	       		      c10factors_[idx]*c10 + c11factors_[idx]*c11 );
+    
+    coords_->removeAfter( c00factors_.size() );
+}
+
+
+void FlatViewer::setPosition( const Coord3& c00, const Coord3& c01, 
+			      const Coord3& c10, const Coord3& c11 )
+{
+    c00_ = c00; c01_ = c01;
+    c10_ = c10; c11_ = c11;
+
+    updateCoordinates();
 }    
 
 
 visBase::Transformation* FlatViewer::getDisplayTransformation()
-{ return faceset_->getDisplayTransformation(); }
+{ 
+    return facesets_.size()>0 ? facesets_[0]->getDisplayTransformation() : 0;
+}
 
 
 void FlatViewer::setDisplayTransformation( mVisTrans* nt )
-{ faceset_->setDisplayTransformation( nt ); }
+{ 
+    for ( int idx=0; idx<facesets_.size(); idx++ )
+	facesets_[idx]->setDisplayTransformation( nt );
+}
 
 
 void FlatViewer::allowShading( bool yn )
-{ texture_->allowShading( yn ); }
+{
+    texture_->allowShading( yn );  
+}
+
 
 }; // Namespace
