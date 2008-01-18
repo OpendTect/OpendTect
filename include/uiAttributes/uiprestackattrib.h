b0212363ee51e49d5c7df584b7f593ff54a2c133
Bert Bril <bert.bril@dgbes.com>
2008-01-18 14:40:37 +0000

    Steadily improving
    git-svn-id: https://svn.opendtect.org/od/trunk@11093 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiAttributes/uiprestackattrib.h
+++ include/uiAttributes/uiprestackattrib.h
@@ -7,18 +7,20 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        B.Bril & H.Huck
  Date:          Jan 2008
- RCS:           $Id: uiprestackattrib.h,v 1.4 2008-01-18 11:37:02 cvsbert Exp $
+ RCS:           $Id: uiprestackattrib.h,v 1.5 2008-01-18 14:40:37 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uiattrdesced.h"
 
+class CtxtIOObj;
 namespace Attrib { class Desc; };
 
-class CtxtIOObj;
+class uiLabel;
 class uiGenInput;
 class uiSeisSel;
+
 
 /*! \brief PreStack Attribute ui */
 
@@ -32,12 +34,20 @@ public:
 protected:
 
     uiSeisSel*		inpfld_;
-    uiGenInput*         stattypefld_;
+    uiGenInput*		calctypefld_;
+    uiGenInput*		stattypefld_;
+    uiGenInput*		lsqtypefld_;
+    uiGenInput*		offsaxtypefld_;
+    uiGenInput*		valaxtypefld_;
+    uiGenInput*		useazimfld_;
+    uiLabel*		xlbl_;
 
     CtxtIOObj&		ctio_;
 
     bool		setParameters(const Attrib::Desc&);
     bool		getParameters(Attrib::Desc&);
+
+    void		calcTypSel(CallBacker*);
 
     			mDeclReqAttribUIFns
 };
--- src/uiAttributes/uiprestackattrib.cc
+++ src/uiAttributes/uiprestackattrib.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        B.Bril & H.Huck
  Date:          Jan 2008
- RCS:		$Id: uiprestackattrib.cc,v 1.5 2008-01-18 11:37:02 cvsbert Exp $
+ RCS:		$Id: uiprestackattrib.cc,v 1.6 2008-01-18 14:40:37 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -22,6 +22,7 @@ ________________________________________________________________________
 #include "uiattribfactory.h"
 #include "uiseissel.h"
 #include "uigeninput.h"
+#include "uilabel.h"
 
 
 mInitAttribUI(uiPreStackAttrib,PreStack,"PreStack",sKeyBasicGrp)
@@ -33,9 +34,32 @@ uiPreStackAttrib::uiPreStackAttrib( uiParent* p, bool is2d )
 {
     inpfld_ = new uiSeisSel( this, ctio_, uiSeisSel::Setup(is2d,true) );
 
+    calctypefld_ = new uiGenInput( this, "Calculation type",
+		   StringListInpSpec(SeisPSPropCalc::CalcTypeNames) );
+    calctypefld_->attach( alignedBelow, inpfld_ );
+    calctypefld_->valuechanged.notify( mCB(this,uiPreStackAttrib,calcTypSel) );
+
     stattypefld_ = new uiGenInput( this, "Statistics type",
-				    StringListInpSpec(Stats::TypeNames) );
-    stattypefld_->attach( alignedBelow, inpfld_ );
+				   StringListInpSpec(Stats::TypeNames) );
+    stattypefld_->attach( alignedBelow, calctypefld_ );
+
+    lsqtypefld_ = new uiGenInput( this, "LSQ output",
+		  StringListInpSpec(SeisPSPropCalc::LSQTypeNames) );
+    lsqtypefld_->attach( alignedBelow, calctypefld_ );
+
+    valaxtypefld_ = new uiGenInput( this, "Axis transformations",
+		     StringListInpSpec(SeisPSPropCalc::AxisTypeNames) );
+    valaxtypefld_->attach( alignedBelow, lsqtypefld_ );
+    xlbl_ = new uiLabel( this, "X:" );
+    xlbl_->attach( rightOf, valaxtypefld_ );
+    offsaxtypefld_ = new uiGenInput( this, "",
+		    StringListInpSpec(SeisPSPropCalc::AxisTypeNames) );
+    offsaxtypefld_->attach( rightOf, xlbl_ );
+
+    useazimfld_ = new uiGenInput( this, "X = Azimuth", BoolInpSpec(false) );
+    useazimfld_->attach( alignedBelow, valaxtypefld_ );
+
+    calcTypSel(0);
     setHAlignObj( inpfld_ );
 }
 
@@ -50,8 +74,16 @@ bool uiPreStackAttrib::setParameters( const Attrib::Desc& desc )
 {
     RefMan<Attrib::Desc> tmpdesc = new Attrib::Desc( desc );
     RefMan<Attrib::PreStack> aps = new Attrib::PreStack( *tmpdesc );
+
     inpfld_->setInput( aps->psID() );
+    calctypefld_->setValue( (int)aps->setup().calctype_ );
     stattypefld_->setValue( (int)aps->setup().stattype_ );
+    lsqtypefld_->setValue( (int)aps->setup().lsqtype_ );
+    offsaxtypefld_->setValue( (int)aps->setup().offsaxis_ );
+    valaxtypefld_->setValue( (int)aps->setup().valaxis_ );
+    useazimfld_->setValue( aps->setup().useazim_ );
+
+    calcTypSel(0);
     return true;
 }
 
@@ -62,7 +94,31 @@ bool uiPreStackAttrib::getParameters( Desc& desc )
     if ( !ctio_.ioobj )
 	{ errmsg_ = "Please select the input data store"; return false; }
 
-    mSetString("id",ctio_.ioobj->key());
-    mSetEnum(Attrib::PreStack::stattypeStr(),stattypefld_->getIntValue());
+    mSetString("id",ctio_.ioobj->key())
+    const int calctyp = calctypefld_->getIntValue();
+    mSetEnum(Attrib::PreStack::calctypeStr(),calctyp)
+    const bool isnorm = calctyp == 0;
+    if ( isnorm )
+    {
+	mSetEnum(Attrib::PreStack::stattypeStr(),stattypefld_->getIntValue())
+    }
+    else
+    {
+	mSetEnum(Attrib::PreStack::lsqtypeStr(),lsqtypefld_->getIntValue())
+	mSetEnum(Attrib::PreStack::offsaxisStr(),offsaxtypefld_->getIntValue())
+	mSetBool(Attrib::PreStack::useazimStr(),useazimfld_->getBoolValue())
+    }
+    mSetEnum(Attrib::PreStack::valaxisStr(),valaxtypefld_->getIntValue())
     return true;
+}
+
+
+void uiPreStackAttrib::calcTypSel( CallBacker* )
+{
+    const bool isnorm = calctypefld_->getIntValue() == 0;
+    stattypefld_->display( isnorm );
+    lsqtypefld_->display( !isnorm );
+    offsaxtypefld_->display( !isnorm );
+    xlbl_->display( !isnorm );
+    useazimfld_->display( !isnorm );
 }
