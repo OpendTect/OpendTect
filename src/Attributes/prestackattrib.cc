d4dd38d3050098191a54c5323d274394d4a1d8e8
Bert Bril <bert.bril@dgbes.com>
2008-01-18 08:06:56 +0000

    Added some macros
    git-svn-id: https://svn.opendtect.org/od/trunk@11089 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Attributes/prestackattrib.cc
+++ src/Attributes/prestackattrib.cc
@@ -4,7 +4,7 @@
  * DATE     : Jan 2008
 -*/
 
-static const char* rcsID = "$Id: prestackattrib.cc,v 1.2 2008-01-16 16:16:29 cvsbert Exp $";
+static const char* rcsID = "$Id: prestackattrib.cc,v 1.3 2008-01-18 08:06:56 cvsbert Exp $";
 
 #include "prestackattrib.h"
 
@@ -26,17 +26,17 @@ void PreStack::initClass()
 {
     mAttrStartInitClass
 
-#define mDefEnumPar(str,enm) \
-    epar = new EnumParam( str() ); \
-    epar->addEnums( enm##Names ); \
+#define mDefEnumPar(var,typ) \
+    epar = new EnumParam( var##Str() ); \
+    epar->addEnums( typ##Names ); \
     desc->addParam( epar )
 
     EnumParam*
-    mDefEnumPar(calctypeStr,SeisPSPropCalc::CalcType);
-    mDefEnumPar(stattypeStr,Stats::Type);
-    mDefEnumPar(lsqtypeStr,SeisPSPropCalc::LSQType);
-    mDefEnumPar(valaxisStr,SeisPSPropCalc::AxisType);
-    mDefEnumPar(offsaxisStr,SeisPSPropCalc::AxisType);
+    mDefEnumPar(calctype,SeisPSPropCalc::CalcType);
+    mDefEnumPar(stattype,Stats::Type);
+    mDefEnumPar(lsqtype,SeisPSPropCalc::LSQType);
+    mDefEnumPar(valaxis,SeisPSPropCalc::AxisType);
+    mDefEnumPar(offsaxis,SeisPSPropCalc::AxisType);
 
     BoolParam* useazimpar = new BoolParam( useazimStr() );
     useazimpar->setDefaultValue( false );
@@ -63,17 +63,16 @@ PreStack::PreStack( Desc& ds )
 {
     if ( !isOK() ) return;
 
-    BufferString str;
-    mGetString( str, calctypeStr() );
-    setup_.calctype_ = eEnum(SeisPSPropCalc::CalcType,str.buf());
-    mGetString( str, stattypeStr() );
-    setup_.stattype_ = eEnum(Stats::Type,str.buf());
-    mGetString( str, lsqtypeStr() );
-    setup_.lsqtype_ = eEnum(SeisPSPropCalc::LSQType,str.buf());
-    mGetString( str, offsaxisStr() );
-    setup_.offsaxis_ = eEnum(SeisPSPropCalc::AxisType,str.buf());
-    mGetString( str, valaxisStr() );
-    setup_.valaxis_ = eEnum(SeisPSPropCalc::AxisType,str.buf());
+#define mGetSetupEnumPar(var,typ) \
+    int tmp_##var = (int)setup_.var##_; \
+    mGetEnum(tmp_##var,var##Str()); \
+    setup_.var##_ = (typ)tmp_##var
+
+    mGetSetupEnumPar(calctype,SeisPSPropCalc::CalcType);
+    mGetSetupEnumPar(stattype,Stats::Type);
+    mGetSetupEnumPar(lsqtype,SeisPSPropCalc::LSQType);
+    mGetSetupEnumPar(valaxis,SeisPSPropCalc::AxisType);
+    mGetSetupEnumPar(offsaxis,SeisPSPropCalc::AxisType);
 
     bool useazim = setup_.useazim_;
     mGetBool( useazim, useazimStr() ); setup_.useazim_ = useazim;
