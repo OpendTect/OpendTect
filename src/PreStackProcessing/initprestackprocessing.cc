aa3d3595023407b1033ccdfae5a4cc199ca650ed
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2008-02-05 20:23:55 +0000

    Added VerticalStack.
    git-svn-id: https://svn.opendtect.org/od/trunk@11236 a985855d-d384-4b45-ae33-31a410998c3c

--- src/PreStackProcessing/initprestackprocessing.cc
+++ src/PreStackProcessing/initprestackprocessing.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Kristofer Tingdahl
  Date:          December 2007
- RCS:           $Id: initprestackprocessing.cc,v 1.1 2007-12-06 20:05:45 cvskris Exp $
+ RCS:           $Id: initprestackprocessing.cc,v 1.2 2008-02-05 20:23:55 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -13,9 +13,11 @@ ________________________________________________________________________
 
 #include "prestackagc.h"
 #include "prestackmute.h"
+#include "prestackverticalstack.h"
 
 void PreStackProcessing::initStdClasses()
 {
     PreStack::Mute::initClass();
     PreStack::AGC::initClass();
+    PreStack::VerticalStack::initClass();
 }
