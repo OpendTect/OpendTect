aab1bda1f83f019f057b25f99d2068f335d63f68
Raman Singh <raman.singh@dgbes.com>
2008-02-05 09:53:18 +0000

    minor corrections...
    git-svn-id: https://svn.opendtect.org/od/trunk@11225 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiODMain/uiodeditattribcolordlg.cc
+++ src/uiODMain/uiodeditattribcolordlg.cc
@@ -4,7 +4,7 @@ ___________________________________________________________________
  CopyRight: 	(C) dGB Beheer B.V.
  Author: 	R. K. Singh
  Date: 		Jan 2008
- RCS:		$Id: uiodeditattribcolordlg.cc,v 1.1 2008-02-04 07:31:37 cvsraman Exp $
+ RCS:		$Id: uiodeditattribcolordlg.cc,v 1.2 2008-02-05 09:53:18 cvsraman Exp $
 ___________________________________________________________________
 
 -*/
@@ -52,7 +52,7 @@ uiODEditAttribColorDlg::uiODEditAttribColorDlg( uiParent* p,
     }
 
     ColorTable ctab = ctabobj->colorSeq().colors();
-    ctab.scaleTo( ctabobj->getInterval() );
+    ctab.scaleTo( Interval<float>(0,1) );
     coltabed_ = new ColorTableEditor( this, ColorTableEditor::Setup()
 	    			      .editable(true).withclip(true)
 				      .vertical(true), &ctab );
@@ -71,6 +71,7 @@ uiODEditAttribColorDlg::uiODEditAttribColorDlg( uiParent* p,
 void uiODEditAttribColorDlg::doApply( CallBacker* )
 {
     ColorTable newct = *coltabed_->getColorTable();
+    newct.scaleTo( Interval<float>(0,1) );
     const bool autoscale = coltabed_->autoScale();
     const Interval<float> intv = coltabed_->getColorTable()->getInterval();
     const float cliprate = coltabed_->getClipRate();
@@ -87,7 +88,7 @@ void uiODEditAttribColorDlg::doApply( CallBacker* )
 	mDynamicCastGet(visBase::VisColorTab*,obj,visBase::DM().getObject(id))
 	if ( !obj ) continue;
 
-	if ( !(obj->colorSeq().colors()==newct) )
+	if ( !(newct == obj->colorSeq().colors()) )
 	{
 	    obj->colorSeq().colors() = newct;
 	    obj->triggerSeqChange();
@@ -95,16 +96,15 @@ void uiODEditAttribColorDlg::doApply( CallBacker* )
 	if ( obj->autoScale()!=autoscale || obj->clipRate()!=cliprate
 	  				 || obj->getSymmetry()!=symm )
 	{
+	    obj->autoscalechange.enable( false );
 	    obj->setAutoScale( autoscale );
 	    obj->setClipRate( cliprate );
 	    obj->setSymmetry( symm );
+	    obj->autoscalechange.enable( true );
 	    obj->triggerAutoScaleChange();
 	}
 	if ( !autoscale && obj->getInterval() != intv )
-	{
 	    obj->scaleTo( intv );
-	    obj->triggerRangeChange();
-	}
 
 	items_[idx]->updateColumnText( uiODSceneMgr::cColorColumn() );
     }
