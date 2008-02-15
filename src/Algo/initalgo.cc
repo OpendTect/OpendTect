4426e0a3b42fe9e06bfc52c586a85e96076681cf
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2008-02-15 20:29:24 +0000

    Added gridding classes.
    git-svn-id: https://svn.opendtect.org/od/trunk@11341 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Algo/initalgo.cc
+++ src/Algo/initalgo.cc
@@ -4,13 +4,14 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne and Kristofer
  Date:          December 2007
- RCS:           $Id: initalgo.cc,v 1.4 2008-02-05 14:25:26 cvsbert Exp $
+ RCS:           $Id: initalgo.cc,v 1.5 2008-02-15 20:29:24 cvskris Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "initalgo.h"
 #include "windowfunction.h"
+#include "gridding.h"
 
 void Algo::initStdClasses()
 {
@@ -20,4 +21,7 @@ void Algo::initStdClasses()
     BlackmanWindow::initClass();
     BartlettWindow::initClass();
     CosTaperWindow::initClass();
+
+    TriangulatedGridding::initClass();
+    InverseDistanceGridding::initClass();
 }
