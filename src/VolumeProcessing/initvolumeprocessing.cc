031e37a1080189108549aad09000a79540cffe23
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2008-02-22 23:17:48 +0000

    Added Smoother.
    git-svn-id: https://svn.opendtect.org/od/trunk@11428 a985855d-d384-4b45-ae33-31a410998c3c

--- src/VolumeProcessing/initvolumeprocessing.cc
+++ src/VolumeProcessing/initvolumeprocessing.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Kristofer Tingdahl
  Date:          December 2007
- RCS:           $Id: initvolumeprocessing.cc,v 1.1 2007-12-06 20:05:44 cvskris Exp $
+ RCS:           $Id: initvolumeprocessing.cc,v 1.2 2008-02-22 23:17:48 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -13,11 +13,13 @@ ________________________________________________________________________
 
 #include "horinterfiller.h"
 #include "volumeprocessingattribute.h"
+#include "volumeprocessingsmoother.h"
 #include "marchingcubesprocessing.h"
 
 void VolumeProcessing::initStdClasses()
 {
     VolProc::HorInterFiller::initClass();
+    VolProc::Smoother::initClass();
     VolProc::ExternalAttribCalculator::initClass();
     VolProc::MarchingCubesProcessing::initClass();
 }
