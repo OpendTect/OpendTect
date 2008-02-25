964cadd5421221c7b4c03da5fcd03a725e925cb3
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2008-02-25 17:04:17 +0000

    Moved AttribDataCubesWriter from VolProc.
    git-svn-id: https://svn.opendtect.org/od/trunk@11445 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/AttributeEngine/attribdatacubeswriter.h
@@ -0,0 +1,59 @@
+#ifndef attribdatacubeswriter_h
+#define attribdatacubeswriter_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	Y.C. Liu
+ Date:		April 2007
+ RCS:		$Id: attribdatacubeswriter.h,v 1.1 2008-02-25 17:04:17 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+#include "cubesampling.h"
+#include "executor.h"
+#include "multiid.h" 
+
+class BinID;
+class IOPar;
+class SeisTrcWriter;
+class SeisTrc;
+
+namespace Attrib
+{
+    
+class DataCubes;
+
+class DataCubesWriter : public Executor
+{
+public:
+    			DataCubesWriter(MultiID&,Attrib::DataCubes&,
+				       const TypeSet<int>& cubeindices);
+			~DataCubesWriter();
+
+    int			nrDone() const;
+    int			totalNr() const;		
+    const char*		message() const		{ return "Writing out data!"; }
+    const char*		nrDoneText() const	{ return "Traces written:"; }
+    int			nextStep();
+ 	
+
+private:
+
+   int			nrdone_;    
+   int			totalnr_;
+   Attrib::DataCubes&	cube_;
+   HorSamplingIterator	iterator_;
+   BinID		currentpos_;
+   MultiID		mid_;
+   SeisTrcWriter*	writer_;
+   SeisTrc&		trc_;
+   TypeSet<int>		cubeindices_;
+};
+
+}; //namespace
+
+
+#endif
---
+++ src/AttributeEngine/attribdatacubeswriter.cc
@@ -0,0 +1,100 @@
+/*+
+ *CopyRight:	(C) dGB Beheer B.V.
+ *Author:	Y.C. Liu
+ *Date:		April 2007
+-*/
+
+static const char* rcsID = "$Id: attribdatacubeswriter.cc,v 1.1 2008-02-25 17:04:17 cvskris Exp $";
+
+#include "attribdatacubeswriter.h"
+
+#include "attribdatacubes.h"
+#include "arraynd.h"
+#include "ioman.h"
+#include "ioobj.h"
+#include "seiswrite.h"
+#include "seistrc.h"
+#include "survinfo.h"
+
+namespace Attrib
+{ 
+
+
+DataCubesWriter::DataCubesWriter( MultiID& mid, Attrib::DataCubes& dc,
+       				const TypeSet<int>& cubeindices )
+    : Executor( "Attribute volume writer" )
+    , nrdone_( 0 )
+    , totalnr_( dc.cubeSampling().totalNr() )
+    , cube_( dc )
+    , iterator_( dc.cubeSampling().hrg )
+    , currentpos_( 0 )
+    , mid_( mid )
+    , writer_( 0 )
+    , trc_( *new SeisTrc( dc.getZSz() ) )
+    , cubeindices_( cubeindices )
+{ 
+    cube_.ref();
+    const CubeSampling cs = cube_.cubeSampling();
+
+    trc_.info().sampling.start = cs.zrg.start;
+    trc_.info().sampling.step = cs.zrg.step;
+    trc_.info().nr = 0;
+
+    for ( int idx=1; idx<cubeindices_.size(); idx++ )
+	trc_.data().addComponent( dc.getZSz(), DataCharacteristics() );
+}
+
+
+DataCubesWriter::~DataCubesWriter()
+{
+    cube_.unRef();
+    delete &trc_;
+    delete writer_;
+}    
+
+
+int DataCubesWriter::nrDone() const
+{ return nrdone_; }    
+
+
+int DataCubesWriter::totalNr() const
+{ return totalnr_; }
+
+
+int DataCubesWriter::nextStep()
+{
+    if ( !writer_ )
+    {
+	PtrMan<IOObj> ioobj = IOM().get( mid_ );
+	if ( !ioobj ) return ErrorOccurred; 
+
+	writer_ = new SeisTrcWriter( ioobj );
+    }
+
+    if ( !iterator_.next( currentpos_ ) )
+	return Finished;
+
+    trc_.info().binid = currentpos_;
+    trc_.info().coord = SI().transform( currentpos_ );
+    const int inlidx = cube_.inlsampling.nearestIndex( currentpos_.inl );
+    const int crlidx = cube_.crlsampling.nearestIndex( currentpos_.crl );
+
+    for ( int idx=0; idx<cubeindices_.size(); idx++ )
+    {
+	for ( int zidx=0; zidx<cube_.getZSz(); zidx++ )
+	{
+	    const float value =
+		cube_.getCube(cubeindices_[idx]).get( inlidx, crlidx, zidx );
+	    trc_.set( zidx, value, idx );
+	}
+    }
+    
+    if ( !writer_->put( trc_ ) )
+	return ErrorOccurred;
+
+    nrdone_++;
+    trc_.info().nr++;
+    return MoreToDo;
+}  
+
+}; //namespace
