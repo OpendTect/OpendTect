6b5af22b1c2b9cf769e9614b0ec4d1dc3d1a3713
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2008-03-17 14:38:47 +0000

    Initial add.
    git-svn-id: https://svn.opendtect.org/od/trunk@11618 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Algo/agc.h
@@ -0,0 +1,200 @@
+#ifndef agc_h
+#define agc_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	K. Tingdahl
+ Date:		March 2008
+ RCS:		$Id: agc.h,v 1.1 2008-03-17 14:38:46 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+#include "sorting.h"
+#include "task.h"
+#include "thread.h"
+#include "valseries.h"
+
+/*!Computes an AGC over a ValueSeries. */
+
+template <class T>
+class AGC : public ParallelTask
+{
+public:
+    			AGC();
+			~AGC();
+    void		setInput(const ValueSeries<T>&,int sz);
+    void		setOutput(ValueSeries<T>&);
+    			//!<Output can be the same as input
+
+    void		setSampleGate(const Interval<int>&);
+    const Interval<int>& getSampleGate() const;
+
+    void		setMuteFraction(float lvmf)	{ mutefraction_ = lvmf;}
+    			//!<The lowest fraction will be muted
+    float		getMuteFraction() const		{ return mutefraction_;}
+
+    bool		doPrepare(int nrthreads);
+
+protected:
+    void		computeEnergyMute();
+    bool		doWork(int,int,int);
+    int			minThreadSize() const { return 200; }
+    int			totalNr() const { return size_; }
+
+    const ValueSeries<T>*	input_;
+    int				size_;
+    ValueSeries<T>*		output_;
+    Interval<int>		samplerg_;
+    float			mutefraction_;
+    TypeSet<T>			energies_;
+    T				energymute_;
+
+    int				threadsinenergycalc_;
+    Threads::ConditionVar*	lock_;
+};
+
+
+template <class T> inline
+AGC<T>::AGC()
+    : input_( 0 )
+    , output_( 0 )
+    , samplerg_(-5,5)
+    , lock_( 0 )
+    , size_( 0 )
+    , mutefraction_( 0 )
+{}
+
+
+template <class T> inline
+AGC<T>::~AGC()
+{ delete lock_; }
+
+
+template <class T> inline
+void AGC<T>::setInput( const ValueSeries<T>& nvs, int sz )
+{ input_ = &nvs; size_ = sz; }
+
+
+template <class T> inline
+void AGC<T>::setOutput( ValueSeries<T>& nvs )
+{ output_ = &nvs; }
+
+
+template <class T> inline
+void AGC<T>::setSampleGate( const Interval<int>& nrg )
+{ samplerg_ = nrg; }
+
+
+template <class T> inline
+const Interval<int>& AGC<T>::getSampleGate() const
+{ return samplerg_; }
+
+
+template <class T> inline
+bool AGC<T>::doPrepare( int nrthreads )
+{
+    if ( !input_ || !output_ || output_->setSize(size_) )
+	return false;
+
+    energies_.setSize( size_, mUdf(T) );
+
+    if ( nrthreads )
+    {
+	if ( !lock_ ) lock_ = new Threads::ConditionVar;
+	threadsinenergycalc_ = nrthreads;
+    }
+    else
+    { delete lock_; lock_ = 0; }
+
+    return true;
+}
+
+
+template <class T> inline
+void AGC<T>::computeEnergyMute()
+{
+    energymute_ = 0;
+    if ( mIsZero(mutefraction_,1e-5) )
+	return;
+
+    const int sample = mNINT(size_*mutefraction_);
+    if ( sample<0 || sample>=size_ )
+	return;
+
+    ArrPtrMan<T> energies = new T[size_];
+    memcpy( energies.ptr(), energies_.arr(), size_*sizeof(T) );
+    sortFor( energies.ptr(), size_, sample );
+    energymute_ = energies[sample];
+}
+
+
+template <class T> inline
+bool AGC<T>::doWork( int start, int stop, int threadidx )
+{
+    for ( int idx=0; idx<=stop; idx++ )
+    {
+	const T value = input_->value(idx);
+	energies_[idx] = mIsUdf( value ) ? mUdf(T) : value*value;
+    }
+
+    if ( lock_ )
+    {
+	lock_->lock();
+	threadsinenergycalc_--;
+	if ( !threadsinenergycalc_ )
+	    lock_->signal(true);
+	else while ( threadsinenergycalc_ )
+	    lock_->wait();
+
+	if ( !threadidx )
+	{
+	    computeEnergyMute();
+	    lock_->signal(true);
+	    threadsinenergycalc_--;
+	}
+	else while ( threadsinenergycalc_!=-1 )
+	    lock_->wait();
+
+	lock_->unLock();
+    }
+    else
+    {
+	computeEnergyMute();
+    }
+
+    for ( int idx=start; idx<=stop; idx++ )
+    {
+	int nrenergies = 0;
+	float energysum = 0;
+	for ( int energyidx=idx+samplerg_.start;
+		  energyidx<=idx+samplerg_.stop;
+		  energyidx++ )
+	{
+	    if ( energyidx<0 || energyidx>=size_ )
+		continue;
+
+	    const T energy = energies_[energyidx];
+	    if ( mIsUdf(energy) )
+	    energysum += energy;
+	    nrenergies++;
+	}
+
+	if ( nrenergies ) energysum /= nrenergies;
+
+	float outputval = 0;
+	if ( energysum<energymute_ )
+	{
+	    const T inpval = input_->value(idx);
+	    outputval = mIsUdf( inpval ) ? inpval : inpval/sqrt( energysum );
+	}
+
+	output_->setValue( idx, outputval );
+    }
+
+    return true;
+}
+
+#endif
---
+++ include/Algo/smoother2d.h
@@ -0,0 +1,192 @@
+#ifndef smoother2d_h
+#define smoother2d_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	K. Tingdahl
+ Date:		Feb 2008
+ RCS:		$Id: smoother2d.h,v 1.1 2008-03-17 14:38:47 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+#include "task.h"
+#include "convolve2d.h"
+#include "arrayndimpl.h"
+#include "iopar.h"
+#include "windowfunction.h"
+
+/*!Smothes a 2d signal with an an operator. */
+
+
+template <class T>
+class Smoother2D : public Task
+{
+public:
+
+				Smoother2D();
+
+    void			setInput(const Array2D<T>&);
+    void			setOutput(Array2D<T>&);
+   		 		/*!Must be at least the size of input.*/
+    bool			setWindow(const char* nm,float param,
+	    				  int sz0,int sz1);
+    int				getWindowSize(int dim) const;
+    const char*			getWindowName() const;
+    float			getWindowParam() const;
+
+    inline void			fillPar(IOPar&) const;
+    inline bool			usePar(const IOPar&);
+
+    inline void			setProgressMeter(ProgressMeter* pm);
+    inline void			enableNrDoneCounting(bool yn);
+    inline bool			execute();
+    inline void			enableWorkControl(bool);
+    inline void			controlWork(Task::Control);
+    inline Task::Control	getState() const;
+
+    static const char*		sKeyWinFunc() { return "Window function"; }
+    static const char*		sKeyWinParam() { return "Window parameter"; }
+    static const char*		sKeyWinSize() { return "Window size"; }
+
+protected:
+
+    Convolver2D<T>		convolver_;
+
+    Array2DImpl<T>		window_;
+    BufferString		windowname_;
+    float			windowparam_;
+};
+
+
+template <class T> inline
+Smoother2D<T>::Smoother2D()
+    : windowparam_( mUdf(float) )
+    , window_( 1, 1 )
+{
+    convolver_.setNormalize( true );
+    convolver_.setCorrelate( false );
+    window_.set( 0, 0, 1 );
+}
+
+
+template <class T> inline
+int Smoother2D<T>::getWindowSize(int dim) const
+{ return window_.info().getSize( dim ); }
+
+
+template <class T> inline
+const char* Smoother2D<T>::getWindowName() const
+{ return windowname_.buf(); }
+
+
+template <class T> inline
+float Smoother2D<T>::getWindowParam() const
+{ return windowparam_; }
+
+
+template <class T> inline
+void Smoother2D<T>::setInput( const Array2D<T>& ni )
+{
+    convolver_.setX( ni, 0, 0 );
+}
+
+
+template <class T> inline
+void Smoother2D<T>::setOutput( Array2D<T>& no )
+{
+    convolver_.setZ( no );
+}
+
+
+template <class T> inline
+bool Smoother2D<T>::setWindow( const char* nm, float param,
+			       int sz0, int sz1 )
+{
+    PtrMan<WindowFunction> wf = WinFuncs().create( nm );
+    if ( !wf )
+	return false;
+
+    if ( wf->hasVariable() && !wf->setVariable( param ) )
+	return false;
+
+    if ( sz0<=0 || sz1<=0 )
+	return false;
+
+    window_.setSize( sz0, sz1 );
+    if ( !window_.isOK() )
+	return false;
+
+    const int hsz0 = sz0/2; const int hsz1 = sz1/2;
+    Coord pos;
+
+    for ( int idx0=0; idx0<sz0; idx0++ )
+    {
+	pos[0] = hsz0 ? ((double)(idx0-hsz0))/hsz0 : 0;
+	for ( int idx1=0; idx1<sz1; idx1++ )
+	{
+	    pos[1] = hsz1 ? ((double)(idx1-hsz1))/hsz1 : 0;
+	    window_.set( idx0, idx1, wf->getValue( pos.abs() ) );
+	}
+    }
+
+    convolver_.setY( window_, hsz0, hsz1 );
+
+    windowname_ = nm;
+    windowparam_ = wf->hasVariable() ? param : 1e30;
+
+    return true;
+}
+
+
+template <class T> inline
+void Smoother2D<T>::fillPar( IOPar& par ) const
+{
+    par.set( sKeyWinFunc(), windowname_ );
+    if ( !mIsUdf(windowparam_) )
+	par.set( sKeyWinParam(), windowparam_ );
+    par.set(sKeyWinSize(),window_.info().getSize(0),window_.info().getSize(1));
+}
+
+
+template <class T> inline
+bool Smoother2D<T>::usePar( const IOPar& par )
+{
+    int sz0, sz1;
+    if ( !par.get(sKeyWinSize(), sz0, sz1 ) )
+	return false;
+
+    const char* wn = par.find( sKeyWinFunc() );
+    float var = mUdf(float);
+    par.get( sKeyWinParam(), var );
+
+    return setWindow( wn, var, sz0, sz1 );
+}
+
+
+#define mImplSetFunc( func, vartype ) \
+template <class T> inline void Smoother2D<T>::func( vartype var ) \
+{ convolver_.func( var ); }
+
+mImplSetFunc( setProgressMeter, ProgressMeter* );
+mImplSetFunc( enableNrDoneCounting, bool );
+mImplSetFunc( enableWorkControl, bool);
+mImplSetFunc( controlWork, Task::Control);
+
+template <class T> inline
+bool Smoother2D<T>::execute()
+{
+    if ( !window_.isOK() )
+	return false;
+
+    return convolver_.execute();
+}
+
+
+template <class T> inline
+Task::Control Smoother2D<T>::getState() const
+{ return convolver_.getState(); }
+
+#endif
