d5bc57c651f31a041c001755f49926bd29124413
Bert Bril <bert.bril@dgbes.com>
2008-03-18 15:40:09 +0000

    using uiLatLong2CoordDlg to get lat/long def
    git-svn-id: https://svn.opendtect.org/od/trunk@11635 a985855d-d384-4b45-ae33-31a410998c3c

--- plugins/uiGoogleIO/uigoogleiopi.cc
+++ plugins/uiGoogleIO/uigoogleiopi.cc
@@ -9,6 +9,10 @@ static const char* rcsID = "$Id";
 #include "uigoogleexpsurv.h"
 #include "uiodmain.h"
 #include "uisurvey.h"
+#include "uilatlong2coord.h"
+#include "uimsg.h"
+#include "survinfo.h"
+#include "latlong.h"
 #include "plugins.h"
 
 extern "C" int GetuiGoogleIOPluginType()
@@ -49,10 +53,35 @@ uiGoogleIOMgr::uiGoogleIOMgr( uiODMain& a )
 }
 
 
+static bool prepLatLong( uiParent* p, SurveyInfo* si )
+{
+    if ( !si ) return false;
+    if ( si->latlong2Coord().isOK() ) return true;
+
+    uiLatLong2CoordDlg dlg( p, si->latlong2Coord(), si );
+    if ( !dlg.go() ) return false;
+
+    si->getLatlong2Coord() = dlg.ll2C();
+    if ( !si->latlong2Coord().isOK() )
+    {
+	uiMSG().error( "Sorry, your Lat/Long definition has a problem" );
+	return false;
+    }
+    if ( !si->write() )
+    {
+	uiMSG().error( "Could not write the definitions to your '.survey' file"
+		    "\nThe definition will work this OpendTect session only" );
+	return false;
+    }
+
+    return true;
+}
+
+
 void uiGoogleIOMgr::exportSurv( CallBacker* cb )
 {
     mDynamicCastGet(uiSurvey*,uisurv,cb)
-    if ( !uisurv || !uisurv->curSurvInfo() )
+    if ( !uisurv || !prepLatLong(uisurv,uisurv->curSurvInfo()) )
 	return;
 
     uiGoogleExportSurvey dlg( uisurv );
