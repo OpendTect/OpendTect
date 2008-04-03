4c670eea5da2448490f2dd8e3286834e5f7a67e1
Bert Bril <bert.bril@dgbes.com>
2008-04-03 13:48:53 +0000

    added reading and using stored PVDS
    git-svn-id: https://svn.opendtect.org/od/trunk@11776 a985855d-d384-4b45-ae33-31a410998c3c

--- src/NLA/nlacrdesc.cc
+++ src/NLA/nlacrdesc.cc
@@ -4,13 +4,14 @@
  * DATE     : June 2001
 -*/
  
-static const char* rcsID = "$Id: nlacrdesc.cc,v 1.14 2008-04-03 11:18:47 cvsbert Exp $";
+static const char* rcsID = "$Id: nlacrdesc.cc,v 1.15 2008-04-03 13:48:53 cvsbert Exp $";
 
 #include "nlacrdesc.h"
 
 #include "datacoldef.h"
 #include "errh.h"
 #include "ioman.h"
+#include "ioobj.h"
 #include "linekey.h"
 #include "posvecdataset.h"
 #include "datapointset.h"
@@ -43,13 +44,78 @@ void NLACreationDesc::clear()
 }
 
 
+static bool transferData2DPS( const PosVecDataSet& vds, DataPointSet& dps,
+			      BufferString& errmsg )
+{
+    const int nrcols = dps.nrCols();
+    TypeSet<int> tbl( nrcols, -1 );
+    for ( int idx=0; idx<2; idx++ )
+    {
+	const DataColDef::MatchLevel ml = idx	? DataColDef::Exact
+	    					: DataColDef::None;
+	for ( DataPointSet::ColID colid=0; colid<nrcols; colid++ )
+	{
+	    if ( tbl[colid] >= 0 ) continue;
+
+	    const DataColDef& dpscd = dps.colDef( colid );
+	    for ( int vdscol=0; vdscol<vds.nrCols(); vdscol++ )
+	    {
+		if ( dpscd.compare(vds.colDef(vdscol),true) )
+		    { tbl[colid] = vdscol; break; }
+	    }
+	}
+    }
+
+    for ( int idx=0; idx<tbl.size(); idx++ )
+    {
+	if ( tbl[idx] < 0 )
+	{
+	    errmsg = "Input data contains no column '";
+	    errmsg += dps.colName( idx ); errmsg += "'";
+	    return false;
+	}
+    }
+
+    const BinIDValueSet& bvs = vds.data();
+    BinIDValueSet::Pos pos;
+    DataPointSet::DataRow dr; dr.data_.setSize( nrcols );
+    while ( bvs.next(pos) )
+    {
+	const float* vals = bvs.getVals( pos );
+	for ( DataPointSet::ColID colid=0; colid<nrcols; colid++ )
+	    dr.data_[ colid ] = vals[ tbl[colid] ];
+	dr.pos_.binid_ = bvs.getBinID( pos );
+	dr.pos_.z_ = vals[0];
+	dps.addRow( dr );
+    }
+
+    dps.dataChanged();
+    return true;
+}
+
+
 const char* NLACreationDesc::prepareData( const ObjectSet<DataPointSet>& dpss,
 					  DataPointSet& traindps,
 					  DataPointSet& testdps ) const
 {
     const int nrout = dpss.size();
     if ( !nrout )
-	{ return "Internal: No input BinIDValueSets to transfer data from"; }
+	{ return "Internal: No input DataPointSets to transfer data from"; }
+
+    if ( !doextraction )
+    {
+	PtrMan<IOObj> ioobj = IOM().get( vdsid );
+	if ( !ioobj )
+	    return "Cannot find training data set specified";
+	static BufferString errmsg; PosVecDataSet vds;
+	if ( !vds.getFrom(ioobj->fullUserExpr(true),errmsg) )
+	    return errmsg.buf();
+	if ( vds.pars().isEmpty() || vds.data().isEmpty() )
+	    return "Invalid input data set specified";
+	DataPointSet& dps = const_cast<DataPointSet&>( *dpss[0] );
+	if ( !transferData2DPS(vds,dps,errmsg) )
+	    return errmsg.buf();
+    }
 
     // For direct prediction, the sets are ready. If not, add a ColumnDef
     // for each output node
@@ -67,6 +133,9 @@ const char* NLACreationDesc::prepareData( const ObjectSet<DataPointSet>& dpss,
     int totnrvec = 0;
     for ( int idps=0; idps<dpss.size(); idps++ )
 	totnrvec += dpss[idps]->size();
+
+    if ( totnrvec < 1 )
+	return "No data vectors found";
 
     // Get the data into train and test set
     Stats::RandGen::init();
