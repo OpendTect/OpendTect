61a2aab2a8eafb305fd679aa1933dffdcbb61f51
Nanne Hemstra <nanne.hemstra@dgbes.com>
2008-04-04 04:10:18 +0000

    Added missing FFT::initClass()
    git-svn-id: https://svn.opendtect.org/od/trunk@11788 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Algo/initalgo.cc
+++ src/Algo/initalgo.cc
@@ -4,27 +4,30 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne and Kristofer
  Date:          December 2007
- RCS:           $Id: initalgo.cc,v 1.8 2008-03-24 20:15:38 cvskris Exp $
+ RCS:           $Id: initalgo.cc,v 1.9 2008-04-04 04:10:18 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "initalgo.h"
-#include "windowfunction.h"
+
+#include "fft.h"
 #include "gridder2d.h"
 #include "posfilterstd.h"
+#include "windowfunction.h"
 
 void Algo::initStdClasses()
 {
+    FFT::initClass();
+
+    BartlettWindow::initClass();
     BoxWindow::initClass();
+    CosTaperWindow::initClass();
     HammingWindow::initClass();
     HanningWindow::initClass();
-    BlackmanWindow::initClass();
-    BartlettWindow::initClass();
-    CosTaperWindow::initClass();
 
-    TriangulatedGridder2D::initClass();
     InverseDistanceGridder2D::initClass();
+    TriangulatedGridder2D::initClass();
 
     Pos::RandomFilter3D::initClass();
     Pos::RandomFilter2D::initClass();
