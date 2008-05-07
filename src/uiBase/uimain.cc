a9808dff8e093558208df61dff5ca6a2cc0762d3
Nanne Hemstra <nanne.hemstra@dgbes.com>
2008-05-07 12:19:10 +0000

    init QApplication correctly
    git-svn-id: https://svn.opendtect.org/od/trunk@12025 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiBase/uimain.cc
+++ src/uiBase/uimain.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        A.H. Lammertink
  Date:          10/12/1999
- RCS:           $Id: uimain.cc,v 1.43 2008-03-19 13:15:39 cvsbert Exp $
+ RCS:           $Id: uimain.cc,v 1.44 2008-05-07 12:19:10 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
@@ -67,6 +67,18 @@ const uiFont* uiMain::font_ = 0;
 QApplication* uiMain::app_ = 0;
 uiMain*	uiMain::themain_ = 0;
 
+
+static void initQApplication()
+{
+    QApplication::setDesktopSettingsAware( true );
+    QApplication::setColorSpec( QApplication::ManyColor );
+
+    QCoreApplication::setOrganizationName( "dGB");
+    QCoreApplication::setOrganizationDomain( "opendtect.org" );
+    QCoreApplication::setApplicationName( "OpendTect" );
+}
+
+
 uiMain::uiMain( int& argc, char **argv )
     : mainobj_( 0 )
 {
@@ -88,6 +100,7 @@ uiMain::uiMain( int& argc, char **argv )
         }
 #endif
 
+    initQApplication();
     init(0,argc,argv);
 }
 
@@ -95,16 +108,7 @@ uiMain::uiMain( int& argc, char **argv )
 uiMain::uiMain( QApplication* qapp )
     : mainobj_( 0 )
 { 
-    QApplication::setDesktopSettingsAware( false );
-    QApplication::setEffectEnabled( Qt::UI_AnimateMenu, false );
-    QApplication::setEffectEnabled( Qt::UI_AnimateTooltip, false );
-    QApplication::setColorSpec( QApplication::ManyColor );
-    QApplication::setLibraryPaths( QStringList("/dev") );
-
-    QCoreApplication::setOrganizationName( "dGB");
-    QCoreApplication::setOrganizationDomain( "opendtect.org" );
-    QCoreApplication::setApplicationName( "OpendTect" );
-
+    initQApplication();
     app_ = qapp;
 }
 
