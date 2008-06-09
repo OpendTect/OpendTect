7501629469f4027129c5e4bde01da1a849b7eba6
Nageswara Rao <nageswara.rao@dgbes.com>
2008-06-09 04:05:49 +0000

    Adjust randomLine Size by using Table fill command
    git-svn-id: https://svn.opendtect.org/od/trunk@12510 a985855d-d384-4b45-ae33-31a410998c3c

--- doc/Scripts/Attrib_onRandLine.cmd
+++ doc/Scripts/Attrib_onRandLine.cmd
@@ -5,9 +5,28 @@ Mon Jan 28 11:36:39 2008
 
 #Applying Attributes on tree item Random line.
 Comment "----------Applying Attributes on tree item Random line---------"
+Case Insensitive
+
+OnError Continue
+TreeMenu "Random line" "Remove all items"
+Button "Yes"
+OnError Stop
+
+OnError Continue
+TreeMenu "Random line`Random Line 1" "Remove"
+OnError Stop
 
 TreeMenu "Random line" "New"
-TreeMenu "Random line`Random Line 1`<right-click>" "Sel*`Att*`CoherencyAtt*"
+TreeMenu "Random line`Random Line 1" "Edit nodes"
+TableFill "BinID Table" 1 1 320
+TableFill "BinID Table" 1 2 600
+TableFill "BinID Table" 2 1 500
+TableFill "BinID Table" 2 2 1000
+Input "Z start" 950
+Input "Z stop" 1700
+Ok
+ListClick "Select Data" "CoherencyAttrib"
+Ok
 Button "Make snapshot"
 Input "Select filename" "rl_400_coherencyeatt.jpg"
 Ok
@@ -47,10 +66,10 @@ Button "Make snapshot"
 Input "Select filename" "rl_frequencyatt.jpg"
 Ok
 
-#TreeMenu "Random line`Random Line 1`FrequencyAtt*" "Sel*`Att*`FrequencyFil*"
-#Button "Make snapshot"
-#Input "Select filename" "rl_frequencyfliteratt.jpg"
-#Ok
+TreeMenu "Random line`Random Line 1`FrequencyAtt*" "Sel*`Att*`FrequencyFil*"
+Button "Make snapshot"
+Input "Select filename" "rl_frequencyfliteratt.jpg"
+Ok
 
 TreeMenu "Random line`Random Line 1`FrequencyAtt*" "Sel*`Att*`Instan*"
 Button "Make snapshot"
