a23606a8d0ccde8f3bd7e8b2be890fe9b056673d
Yuancheng Liu <yuancheng.liu@dgbes.com>
2008-06-10 19:44:33 +0000

    Made creation of normals and tcs dependent on if shape creates normals and tcs.
    git-svn-id: https://svn.opendtect.org/od/trunk@12524 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visBase/visgeomindexedshape.cc
+++ src/visBase/visgeomindexedshape.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        K. Tingdahl
  Date:          August 2006
- RCS:           $Id: visgeomindexedshape.cc,v 1.12 2008-06-03 20:01:13 cvskris Exp $
+ RCS:           $Id: visgeomindexedshape.cc,v 1.13 2008-06-10 19:44:33 cvsyuancheng Exp $
 ________________________________________________________________________
 
 -*/
@@ -15,6 +15,7 @@ ________________________________________________________________________
 
 #include "viscoord.h"
 #include "visnormals.h"
+#include "vistexturecoords.h"
 
 #include <Inventor/nodes/SoIndexedTriangleStripSet.h>
 #include <Inventor/nodes/SoIndexedLineSet.h>
@@ -33,6 +34,7 @@ GeomIndexedShape::GeomIndexedShape()
     : VisualObjectImpl( true )
     , coords_( Coordinates::create() )
     , normals_( Normals::create() )
+    , texturecoords_( TextureCoords::create() )				   
     , shape_( 0 )
     , lineradius_( -1 )
     , lineconstantonscreen_( false )
@@ -43,6 +45,9 @@ GeomIndexedShape::GeomIndexedShape()
 
     normals_->ref();
     addChild( normals_->getInventorNode() );
+
+    texturecoords_->ref();
+    addChild( texturecoords_->getInventorNode() );
 }
 
 
@@ -50,6 +55,7 @@ GeomIndexedShape::~GeomIndexedShape()
 {
     coords_->unRef();
     normals_->unRef();
+    texturecoords_->unRef();
 }
 
 
@@ -87,7 +93,8 @@ void GeomIndexedShape::setSurface( Geometry::IndexedShape* ns, TaskRunner* tr )
 {
     shape_ = ns;
     shape_->setCoordList( new CoordListAdapter(*coords_),
-	    		  new NormalListAdapter(*normals_) );
+	    		  new NormalListAdapter(*normals_), 
+			  new TextureCoordListAdapter(*texturecoords_) );
     shape_->setRightHandedNormals( righthandsystem_ );
     touch( false, tr );
 }
@@ -145,80 +152,105 @@ void GeomIndexedShape::touch( bool forall, TaskRunner* tr )
     ObjectSet<SoIndexedShape> newfans;
     ObjectSet<const Geometry::IndexedGeometry> newfangeoms;
 
-    if ( shape_ )
+    if ( !shape_ )
     {
-	const ObjectSet<Geometry::IndexedGeometry>& geoms=shape_->getGeometry();
+	mRemoveOld( strip );
+	mRemoveOld( fan );
+	mRemoveOld( line );
+	return;
+    }
 
-	for ( int idx=0; idx<geoms.size(); idx++ )
+    const ObjectSet<Geometry::IndexedGeometry>& geoms=shape_->getGeometry();
+
+    for ( int idx=0; idx<geoms.size(); idx++ )
+    {
+	const Geometry::IndexedGeometry* geom = geoms[idx];
+	SoIndexedShape* shape = 0;
+	mHandleType( TriangleStrip, SoIndexedTriangleStripSet, strip )
+	else mHandleType( TriangleFan, SoIndexedTriangleFanSet, fan )
+	else mHandleType( Lines, SoIndexedLineSet, line )
+	else if ( lineradius_>0 )
 	{
-	    const Geometry::IndexedGeometry* geom = geoms[idx];
-	    SoIndexedShape* shape = 0;
-	    mHandleType( TriangleStrip, SoIndexedTriangleStripSet, strip )
-            else mHandleType( TriangleFan, SoIndexedTriangleFanSet, fan )
- 	    else if ( lineradius_>0 )
+	    mHandleType( Lines, SoIndexedLineSet3D, line );
+	    mDynamicCastGet( SoIndexedLineSet3D*, line3d, shape );
+	    if ( line3d )
 	    {
-		mHandleType( Lines, SoIndexedLineSet3D, line );
-		mDynamicCastGet( SoIndexedLineSet3D*, line3d, shape );
-		if ( line3d )
-		{
-		    line3d->radius = lineradius_;
-		    line3d->screenSize = lineconstantonscreen_;
-		    line3d->maxRadius = linemaxsize_;
-		    line3d->rightHandSystem = righthandsystem_;
-		}
+		line3d->radius = lineradius_;
+		line3d->screenSize = lineconstantonscreen_;
+		line3d->maxRadius = linemaxsize_;
+		line3d->rightHandSystem = righthandsystem_;
+	    }
+	}
+	else
+	    mHandleType( Lines, SoIndexedLineSet, line )
+
+	if ( !shape )
+	    continue;
+
+	if ( geom->ischanged_ )
+	{
+	    /* TODO: leads to crash. Probably because geom has been deleted.
+	    shape->coordIndex.setValuesPointer(
+		geom->coordindices_.size(), geom->coordindices_.arr() );
+
+	    shape->normalIndex.setValuesPointer(
+		geom->normalindices_.size(), geom->normalindices_.arr() );
+	    */
+
+	    SbBool oldstatus = shape->coordIndex.enableNotify( false );
+	    shape->coordIndex.setValues( 0,
+		geom->coordindices_.size(), geom->coordindices_.arr() );
+	    shape->coordIndex.setNum( geom->coordindices_.size() );
+	    shape->coordIndex.enableNotify( oldstatus );
+
+	    if ( shape_->createsNormals() )
+	    {
+    		oldstatus = shape->normalIndex.enableNotify( false );
+    		shape->normalIndex.setValues( 0,
+			geom->normalindices_.size(), 
+			geom->normalindices_.arr() );
+    		shape->normalIndex.setNum( geom->normalindices_.size() );
+    		shape->normalIndex.enableNotify( oldstatus );
 	    }
 	    else
-		mHandleType( Lines, SoIndexedLineSet, line )
+		shape->normalIndex.setNum( 0 );
 
-	    if ( !shape )
-		continue;
-
-	    if ( geom->ischanged_ )
+	    if ( shape_->createsTextureCoords() )
 	    {
-		/* TODO: leads to crash. Probably because geom has been deleted.
-		shape->coordIndex.setValuesPointer(
-		    geom->coordindices_.size(), geom->coordindices_.arr() );
-
-		shape->normalIndex.setValuesPointer(
-		    geom->normalindices_.size(), geom->normalindices_.arr() );
-		*/
-
-		SbBool oldstatus = shape->coordIndex.enableNotify( false );
-		shape->coordIndex.setValues( 0,
-		    geom->coordindices_.size(), geom->coordindices_.arr() );
-		shape->coordIndex.setNum( geom->coordindices_.size() );
-		shape->coordIndex.enableNotify( oldstatus );
-
-
-		oldstatus = shape->normalIndex.enableNotify( false );
-		shape->normalIndex.setValues( 0,
-		    geom->normalindices_.size(), geom->normalindices_.arr() );
-		shape->normalIndex.setNum( geom->normalindices_.size() );
-		shape->normalIndex.enableNotify( oldstatus );
-		shape->touch();
+		oldstatus = shape->textureCoordIndex.enableNotify( false );
+		shape->textureCoordIndex.setValues( 0,
+			geom->texturecoordindices_.size(), 
+			geom->texturecoordindices_.arr() );
+		shape->textureCoordIndex.setNum(
+			geom->texturecoordindices_.size() );    
+		shape->textureCoordIndex.enableNotify( oldstatus );
 	    }
+	    else
+		shape->textureCoordIndex.setNum( 0 );
 
-	    const int idy = childIndex( shape );
-	    mDynamicCastGet(SoNormalBinding*, nb, idy>0 ? getChild(idy-1) : 0);
-
-	    if ( geom->normalindices_.size() )
-	    {
-		if ( !nb )
-		{
-		    nb = new SoNormalBinding;
-		    insertChild( idy, nb );
-		}
-
-		nb->value = geom->normalbinding_==
-				 Geometry::IndexedGeometry::PerVertex 
-		     ? SoNormalBindingElement::PER_VERTEX_INDEXED
-		     : SoNormalBindingElement::PER_FACE_INDEXED;
-	    }
-	    else if ( nb )
-		removeChild( nb );
-
-	    geom->ischanged_ = false;
+	    shape->touch();
 	}
+
+	const int idy = childIndex( shape );
+	mDynamicCastGet(SoNormalBinding*, nb, idy>0 ? getChild(idy-1) : 0);
+
+	if ( geom->normalindices_.size() )
+	{
+	    if ( !nb )
+	    {
+		nb = new SoNormalBinding;
+		insertChild( idy, nb );
+	    }
+
+	    nb->value = geom->normalbinding_==
+			     Geometry::IndexedGeometry::PerVertex 
+		 ? SoNormalBindingElement::PER_VERTEX_INDEXED
+		 : SoNormalBindingElement::PER_FACE_INDEXED;
+	}
+	else if ( nb )
+	    removeChild( nb );
+
+	geom->ischanged_ = false;
     }
 
     mRemoveOld( strip );
