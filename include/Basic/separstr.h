db598862298e7ecb5af4b515c49761bbfe4e6ada
Raman Singh <raman.singh@dgbes.com>
2008-06-20 11:41:01 +0000

    added function to get PathSeparator charactor...
    git-svn-id: https://svn.opendtect.org/od/trunk@12614 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/separstr.h
+++ include/Basic/separstr.h
@@ -8,7 +8,7 @@ ________________________________________________________________________
  Author:	A.H.Bril
  Date:		May 1995
  Contents:	String with a separator between the items
- RCS:		$Id: separstr.h,v 1.11 2007-08-08 04:09:50 cvsnanne Exp $
+ RCS:		$Id: separstr.h,v 1.12 2008-06-20 11:41:01 cvsraman Exp $
 ________________________________________________________________________
 
 -*/
@@ -56,6 +56,7 @@ public:
     char		sepChar() const			{ return sep_; }
     void		setSepChar(char);
     void		replaceSepChar(char newsep);
+    static char		getPathSepChar();
 
 private:
 
