51662cc287bad262571347a8cc4b6aac1b66bd8e
cvsumesh <cvsumesh@a985855d-d384-4b45-ae33-31a410998c3c>
2008-06-23 10:15:18 +0000

    extrapolation type added.
    git-svn-id: https://svn.opendtect.org/od/trunk@12649 a985855d-d384-4b45-ae33-31a410998c3c

--- src/PreStackProcessing/prestackmuteasciio.cc
+++ src/PreStackProcessing/prestackmuteasciio.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Umesh Sinha
  Date:		June 2008
- RCS:		$Id: prestackmuteasciio.cc,v 1.1 2008-06-23 06:49:31 cvsumesh Exp $
+ RCS:		$Id: prestackmuteasciio.cc,v 1.2 2008-06-23 10:15:18 cvsumesh Exp $
 ________________________________________________________________________
 
 -*/
@@ -13,35 +13,38 @@ ________________________________________________________________________
 #include "tabledef.h"
 #include "prestackmutedef.h"
 #include "unitofmeasure.h"
-
-using namespace PreStack;
+#include "survinfo.h"
 
 static const char* extrapoltypes[] = {"Linear","Poly","Snap",0};
 
+namespace PreStack
+{
+
 Table::FormatDesc* MuteAscIO::getDesc()
 {
-    Table::FormatDesc* fd = new Table::FormatDesc("Mute");
+    Table::FormatDesc* fd = new Table::FormatDesc( "Mute" );
     const char* unitstr = SI().zIsTime() ? "Milliseconds"
-	    			           : SI().zInFeet() ? "Feet" : "Meter";
+	    			         : SI().zInFeet() ? "Feet" : "Meter";
 
-    Table::TargetInfo* posinfo = new Table::TargetInfo("X/Y", FloatInpSpec(),
-	    					       Table::Required );
-    posinfo->form(0).add( FloatInpSpec());
-    posinfo->add(posinfo->form(0).duplicate("Inl/Crl"));
+    Table::TargetInfo* posinfo = new Table::TargetInfo( "X/Y", FloatInpSpec(),
+	    					        Table::Required );
+    posinfo->form(0).add( FloatInpSpec() );
+    posinfo->add( posinfo->form(0).duplicate("Inl/Crl") );
 
     fd->bodyinfos_ += posinfo;
-    fd->bodyinfos_ += new Table::TargetInfo("Offset", FloatInpSpec(),
-	    				    Table::Required );
-    Table::TargetInfo* bti = new Table::TargetInfo("Z Values", FloatInpSpec(),
+    fd->bodyinfos_ += new Table::TargetInfo( "Offset", FloatInpSpec(),
+	    				     Table::Required );
+    Table::TargetInfo* bti = new Table::TargetInfo( "Z Values" , FloatInpSpec(),
 	    				    Table::Required,
-					    PropertyRef::surveyZType());
+					    PropertyRef::surveyZType() );
     bti->selection_.unit_ = UoMR().get( unitstr );
     fd->bodyinfos_ += bti;
 
     return fd;
 }
 
-float MuteAscIO::getUdfVal()
+
+float MuteAscIO::getUdfVal() const
 {
     if( !getHdrVals(strm_) )
 	return mUdf(float);
@@ -52,48 +55,37 @@ float MuteAscIO::getUdfVal()
 bool MuteAscIO::isXY() const
 {
     const Table::TargetInfo* xinfo = fd_.bodyinfos_[0];
-    if(!xinfo) return false;
-
-    const int sel = xinfo->selection_.form_;
-    return !sel;
+    return xinfo ? xinfo->selection_.form_ == 0 : false;
 }
 
-int MuteAscIO::getMuteDef( MuteDef& mutedef, bool extrapol, 
-			   PointBasedMathFunction::InterpolType iptype)
+
+bool MuteAscIO::getMuteDef( MuteDef& mutedef, bool extrapol, 
+			   PointBasedMathFunction::InterpolType iptype )
 {
     bool isxy = isXY();
-    int ret = getNextBodyVals( strm_ );
-    
-    PointBasedMathFunction* pointbasedmathfunc = 0;
-    
-    while( ret > 0 )
+
+    while ( true )
     {
+	const int ret = getNextBodyVals( strm_ );
+	if ( ret < 0 ) return false;
+	if ( ret == 0) break;
+
 	BinID binid;
-	if(isxy)
-	{
-	    Coord coord(getfValue(0),getfValue(1));
-	    binid = SI().transform(coord);
-	}
-	else // its inl/crl
+	if ( isxy )
+	    binid = SI().transform( Coord(getfValue(0),getfValue(1)) );
+	else
 	{
    	    binid.inl = getIntValue(0);
    	    binid.crl = getIntValue(1);
-	}	 
-	int binidpos = mutedef.indexOf(binid);
-	if(binidpos < 0)
-	{
-	    pointbasedmathfunc = new PointBasedMathFunction(iptype);
-	    mutedef.add( pointbasedmathfunc, binid );
 	}
-	else
-	{
-	    pointbasedmathfunc = &mutedef.getFn(binidpos);
-	}
-   
-	pointbasedmathfunc->add(getfValue(2),getfValue(3));
-   
-	ret = getNextBodyVals( strm_);
+
+	if ( mutedef.indexOf(binid) < 0 )
+	    mutedef.add( new PointBasedMathFunction(iptype,extrapol), binid );
+
+	mutedef.getFn(mutedef.indexOf(binid)).add( getfValue(2), getfValue(3) );
     }
 
-    return ret;
+    return true;
 }
+
+} // namespace PreStack
