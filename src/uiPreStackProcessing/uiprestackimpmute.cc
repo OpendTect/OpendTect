38bbb98ca92020007e6e413bcca556b64b873136
cvsumesh <cvsumesh@a985855d-d384-4b45-ae33-31a410998c3c>
2008-06-23 06:51:42 +0000

    implemented import utility for MuteDef...
    git-svn-id: https://svn.opendtect.org/od/trunk@12642 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/uiPreStackProcessing/uiprestackimpmute.cc
@@ -0,0 +1,105 @@
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	Umesh Sinha
+ Date:		June 2008
+ RCS:		$Id: uiprestackimpmute.cc,v 1.1 2008-06-23 06:51:42 cvsumesh Exp $
+________________________________________________________________________
+
+-*/
+#include "uiprestackimpmute.h"
+#include "prestackmutedeftransl.h"
+#include "prestackmuteasciio.h"
+#include "ctxtioobj.h"
+#include "uiseparator.h"
+#include "tabledef.h"
+#include "uifileinput.h"
+#include "oddirs.h"
+#include "uitblimpexpdatasel.h"
+#include "uiioobjsel.h"
+#include "uigeninput.h"
+#include "prestackmutedef.h"
+#include "strmprov.h"
+#include "uimsg.h"
+
+static const char* extrapoltypes[] = {"Linear","Poly","Snap",0};
+
+uiImportMute::uiImportMute(uiParent* p)
+    :uiDialog(p,uiDialog::Setup("Import Mute", "Specify Parameters",0))
+    ,ctio_(*mMkCtxtIOObj(MuteDef))
+    ,fd_(*PreStack::MuteAscIO::getDesc())
+{
+    inpfld_ = new uiFileInput(this, "Input ASCII File", 
+	                      uiFileInput::Setup().withexamine(true)
+			      .defseldir( GetDataDir() ) );
+
+    extptypefld_ = new uiGenInput(this,"Extrapolate Type",
+	                          StringListInpSpec(extrapoltypes));
+    extptypefld_->attach(alignedBelow, inpfld_ );
+
+    uiSeparator* sep = new uiSeparator( this, "H sep");
+    sep->attach(stretchedBelow, extptypefld_);
+
+    dataselfld_ = new uiTableImpDataSel( this, fd_, 0);
+    dataselfld_->attach( alignedBelow, extptypefld_ );
+    dataselfld_->attach( ensureBelow, sep);
+
+    sep = new uiSeparator( this, "H sep");
+    sep->attach( alignedBelow, dataselfld_);
+
+    ctio_.ctxt.forread = false;
+    outfld_ = new uiIOObjSel( this, ctio_, "Output Mute");
+    outfld_->attach( alignedBelow, inpfld_);
+    outfld_->attach( ensureBelow, sep);
+}
+
+uiImportMute::~uiImportMute()
+{
+    delete ctio_.ioobj; delete &ctio_;
+    delete &fd_;
+}
+#undef mErrRet
+#define mErrRet(msg) { uiMSG().error( msg ); return false; }
+
+bool uiImportMute::acceptOK(CallBacker*)
+{
+    PreStack::MuteDef mutedef;
+
+    if( !*inpfld_->fileName() )
+    mErrRet( "Please select the input file" );
+
+    StreamData sd = StreamProvider( inpfld_->fileName() ).makeIStream();
+    PreStack::MuteAscIO muteascio( fd_, *sd.istrm);
+
+    if( muteascio.getMuteDef( mutedef, true, getInterpolType() ) )
+	mErrRet( "Failed to convert into compatible data" );
+
+    MuteDefTranslator* tr = 
+	         (MuteDefTranslator*)ctio_.ioobj->getTranslator();
+    
+    if(!tr) return false;
+
+    BufferString bs;
+    bool retval = false;
+    retval = tr->store( mutedef, ctio_.ioobj, bs);
+
+    if(!retval)
+    mErrRet(bs.buf());	
+
+    return retval;
+}
+
+PointBasedMathFunction::InterpolType uiImportMute::getInterpolType()
+{
+    PointBasedMathFunction::InterpolType iptype =PointBasedMathFunction::Linear;
+    
+    const int typeval = extptypefld_->getIntValue();
+     if(typeval == 1)
+	 iptype = PointBasedMathFunction::Poly;
+     if(typeval == 2)
+	 iptype = PointBasedMathFunction::Snap;
+
+      return iptype;
+}
