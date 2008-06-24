75f3d74883b7e8d372e9be5e02c450749911041f
Raman Singh <raman.singh@dgbes.com>
2008-06-24 05:25:11 +0000

    using plf-independent isProcessAlive...
    git-svn-id: https://svn.opendtect.org/od/trunk@12656 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiTools/od_ProgressViewer.cc
+++ src/uiTools/od_ProgressViewer.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Bert Bril
  Date:          August 2001
- RCS:           $Id: od_ProgressViewer.cc,v 1.17 2008-06-19 08:24:26 cvsraman Exp $
+ RCS:           $Id: od_ProgressViewer.cc,v 1.18 2008-06-24 05:25:11 cvsraman Exp $
 ________________________________________________________________________
 
 -*/
@@ -112,7 +112,7 @@ void uiProgressViewer::appendToText()
 void uiProgressViewer::doWork( CallBacker* )
 {
     bool ateof = strm.eof();
-    if ( kill(ppid,0) )
+    if ( !isProcessAlive(ppid) )
     {
 	txtfld->append( "Program Terminated" );
 	tb_->setToolTip( quitid_, "Close" );
