137ce4f2db60ad411286854bd8937e072fe39918
cvsumesh <cvsumesh@a985855d-d384-4b45-ae33-31a410998c3c>
2008-06-27 04:40:00 +0000

    reverting back previous change
    git-svn-id: https://svn.opendtect.org/od/trunk@12693 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiODMain/uiodstdmenu.h
+++ include/uiODMain/uiodstdmenu.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Bert Bril
  Date:		Dec 2003
- RCS:		$Id: uiodstdmenu.h,v 1.43 2008-06-26 11:06:35 cvsumesh Exp $
+ RCS:		$Id: uiodstdmenu.h,v 1.44 2008-06-27 04:40:00 cvsumesh Exp $
 ________________________________________________________________________
 
 
@@ -60,6 +60,7 @@ ________________________________________________________________________
 #define mImpFaultMnuItm		(mFileImpMnu + 50)
 #define mImpPickMnuItm		(mFileImpMnu + 60)
 #define mImpWvltMnuItm		(mFileImpMnu + 70)
+#define mImpMuteDefMnuItm	(mFileImpMnu + 80)
 #define mExpSeisSEGY3DMnuItm	(mFileExpMnu + 10)
 #define mExpSeisSEGY2DMnuItm	(mFileExpMnu + 11)
 #define mExpSeisSimple3DMnuItm	(mFileExpMnu + 14)
