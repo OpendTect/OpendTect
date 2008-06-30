5bddee08e2d00ce5fcf196fb2e7e1b3b39a7f951
Bert Bril <bert.bril@dgbes.com>
2008-06-30 13:23:50 +0000

    added max inl offset to program usage
    git-svn-id: https://svn.opendtect.org/od/trunk@12711 a985855d-d384-4b45-ae33-31a410998c3c

--- spec/ODSeis/imp_bpsif.cc
+++ spec/ODSeis/imp_bpsif.cc
@@ -2,7 +2,7 @@
  * COPYRIGHT: (C) de Groot-Bril Earth Sciences B.V.
  * AUTHOR   : Bert
  * DATE     : July 2007
- * RCS      : $Id: imp_bpsif.cc,v 1.2 2008-01-14 12:03:55 cvsbert Exp $
+ * RCS      : $Id: imp_bpsif.cc,v 1.3 2008-06-30 13:23:50 cvsbert Exp $
 -*/
 
 #include "seisimpbpsif.h"
@@ -15,7 +15,7 @@ static int doWork( int argc, char** argv )
 {
     if ( argc < 3 )
     {
-	std::cerr << "Usage: " << argv[0] << " inp_BPSIF_file output_id\n";
+	std::cerr << "Usage: " << argv[0] << " inp_BPSIF_file output_id [max_inl_offs]\n";
 	return 1;
     }
 
