8b2b2912dd1cb1deaa416b09c5e0560baca11c13
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2008-07-03 13:02:18 +0000

    Refined class.
    git-svn-id: https://svn.opendtect.org/od/trunk@12729 a985855d-d384-4b45-ae33-31a410998c3c

--- src/General/keyboardevent.cc
+++ src/General/keyboardevent.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Nanne Hemstra
  Date:		July 2007
- RCS:		$Id: keyboardevent.cc,v 1.1 2007-10-26 05:33:13 cvsnanne Exp $
+ RCS:		$Id: keyboardevent.cc,v 1.2 2008-07-03 13:02:18 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -13,21 +13,25 @@ ________________________________________________________________________
 
 
 KeyboardEvent::KeyboardEvent()
-    : pos_(0,0)
+    : key_( OD::NoKey )
+    , modifier_( OD::NoButton )
+    , isrepeat_( false )
 {}
 
 
 bool KeyboardEvent::operator ==( const KeyboardEvent& ev ) const
-{ return pos_==ev.pos_; } 
+{ return key_==ev.key_ && modifier_==ev.modifier_ && isrepeat_==ev.isrepeat_; }
 
+
+bool KeyboardEvent::operator !=( const KeyboardEvent& ev ) const
+{ return !(ev==*this); }
 
 
 KeyboardEventHandler::KeyboardEventHandler()
     : keyPressed(this)
     , keyReleased(this)
     , event_(0)
-{
-}
+{}
 
 
 #define mImplKeyboardEventHandlerFn(fn,trig) \
