ca2e90083f16be88f782504f882261a46f04738b
Jaap Glas <jaap.glas@dgbes.com>
2008-08-01 11:23:23 +0000

    Functionality to make screendump of a window or desktop.
    git-svn-id: https://svn.opendtect.org/od/trunk@12905 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiTools/uiwindowgrabber.h
@@ -0,0 +1,80 @@
+#ifndef uiwindowgrabber_h
+#define uiwindowgrabber_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        J.C. Glas
+ Date:          July 2008
+________________________________________________________________________
+
+-*/
+
+#include "uidialog.h"
+#include "thread.h"
+
+class uiLabeledComboBox;
+class uiFileInput;
+class uiSliderExtra;
+class uiLabel;
+class uiMainWin;
+
+
+/*!Dialog to specify the grab window and the output image file */
+
+class uiWindowGrabDlg : public uiDialog
+{
+public:
+			uiWindowGrabDlg(uiParent*,bool desktop);
+
+    uiMainWin*		getWindow() const;
+    const char*		getFilename() const	{ return filename_.buf(); }
+    int			getQuality() const;
+
+protected:
+    uiLabeledComboBox*	windowfld_;
+    uiFileInput*	fileinputfld_;
+    uiSliderExtra*	qualityfld_;
+    uiLabel*		infofld_;
+
+    ObjectSet<uiMainWin> windowlist_;
+
+    void		updateFilter();
+    void		fileSel(CallBacker*);
+    void		addFileExtension(BufferString&);
+    bool		filenameOK() const;
+
+    bool		acceptOK(CallBacker*);
+    void		surveyChanged(CallBacker*);
+
+    const char*		getExtension() const;
+
+    static BufferString dirname_;
+    BufferString	filename_;
+};
+
+
+/*!Grabs the screen area covered by a window or the whole desktop */
+
+class uiWindowGrabber: public CallBacker
+{
+public:
+			uiWindowGrabber(uiParent*);
+
+    void		grabDesktop(bool yn)	{ desktop_ = yn; }
+    bool		go();
+
+protected:
+    uiParent*		parent_;
+    bool		desktop_;
+    uiMainWin*		grabwin_;
+    BufferString	filename_;
+    int			quality_;
+    
+    void		mkThread(CallBacker*);
+    Threads::Thread*	execthr_;
+};
+
+
+#endif
---
+++ src/uiTools/uiwindowgrabber.cc
@@ -0,0 +1,229 @@
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        J.C. Glas
+ Date:          July 2008
+________________________________________________________________________
+
+-*/
+
+#include "uiwindowgrabber.h"
+
+#include "uicombobox.h"
+#include "uifileinput.h"
+#include "uilabel.h"
+#include "uimsg.h"
+#include "uislider.h"
+
+#include "filegen.h"
+#include "filepath.h"
+#include "ioman.h"
+#include "oddirs.h"
+#include "timefun.h"
+
+BufferString uiWindowGrabDlg::dirname_ = "";
+
+static const char* imageformats[] =
+{ "jpg", "png", "bmp", "ppm", "xpm", "xbm", 0 };
+
+static const char* filters[] =
+{
+    "JPEG (*.jpg *.jpeg)",
+    "PNG (*.png)",
+    "Bitmap (*.bmp)",
+    "PPM (*.ppm)",
+    "XPM (*.xpm)",
+    "XBM (*.xbm)",
+    0
+};
+
+
+
+uiWindowGrabDlg::uiWindowGrabDlg( uiParent* p, bool desktop )
+    : uiDialog(p,uiDialog::Setup( (desktop ? "Desktop grabber" : 
+					     "Window grabber"),
+				  (desktop ? "Specify image file" :
+					     "Specify window and image file"),
+				  "0.0.0") )
+    , windowfld_(0)
+{
+    getTopLevelWindows( windowlist_ );
+    if ( !desktop && !windowlist_.isEmpty() )
+    {
+	BufferStringSet windownms;
+	for ( int idx=0; idx<windowlist_.size(); idx++ )
+	    windownms.add( windowlist_[idx]->name() );
+
+	windowfld_ = new uiLabeledComboBox( this, windownms, "Grab window" );
+    }
+
+    if ( dirname_.isEmpty() )
+	dirname_ = FilePath(GetDataDir()).add("Misc").fullPath();
+    fileinputfld_ = new uiFileInput( this, "Filename",
+				    uiFileInput::Setup()
+				    .forread(false)
+				    .defseldir(dirname_)
+	   			    .allowallextensions(false) );
+    fileinputfld_->valuechanged.notify( mCB(this,uiWindowGrabDlg,fileSel) );
+    if ( windowfld_ )
+	fileinputfld_->attach( alignedBelow, windowfld_ ); 
+    updateFilter();
+
+    qualityfld_ = new uiSliderExtra( this, "Image quality", "Quality slider" );
+    qualityfld_->attach( alignedBelow, fileinputfld_ );
+    qualityfld_->sldr()->setInterval( StepInterval<float>(0,100,1) );
+    qualityfld_->sldr()->setValue(50);
+    qualityfld_->attach( alignedBelow, fileinputfld_ );
+
+    infofld_ = new uiLabel( this, "Arrange your windows before confirming" );
+    infofld_->attach( alignedBelow, qualityfld_ );
+    
+    IOM().afterSurveyChange.notify( mCB(this,uiWindowGrabDlg,surveyChanged) );
+}
+
+
+void uiWindowGrabDlg::surveyChanged( CallBacker* )
+{
+    dirname_ = "";
+}
+
+
+void uiWindowGrabDlg::updateFilter()
+{
+    BufferString filter;
+    int idx = 0;
+    while ( imageformats[idx] )
+    {
+	if ( !filter.isEmpty() ) filter += ";;";
+	filter += filters[idx];
+	idx++;
+    }
+
+    fileinputfld_->setFilter( filter );
+}
+
+
+void uiWindowGrabDlg::fileSel( CallBacker* )
+{
+    BufferString filename = fileinputfld_->fileName();
+    addFileExtension( filename );
+    fileinputfld_->setFileName( filename );
+}
+
+
+void uiWindowGrabDlg::addFileExtension( BufferString& filename )
+{
+    FilePath fp( filename.buf() );
+    fp.setExtension( getExtension() );
+    filename = fp.fullPath();
+}
+
+
+bool uiWindowGrabDlg::filenameOK() const
+{
+    BufferString filename = fileinputfld_->fileName();
+    if ( filename.isEmpty() )
+    {
+	uiMSG().error( "Please select filename" );
+	return false;
+    }
+
+    if ( File_exists(filename) )
+    {
+	BufferString msg = "The file "; msg += filename; 
+	if ( !File_isWritable(filename) )
+	{
+	    msg += " is not writable";
+	    uiMSG().error(msg);
+	    return false;
+	}
+    }
+
+    return true;
+}
+
+
+bool uiWindowGrabDlg::acceptOK( CallBacker* )
+{
+    if ( !filenameOK() )
+       	return false;
+    
+    FilePath filepath( fileinputfld_->fileName() );
+    dirname_ = filepath.pathOnly();
+    filename_ = filepath.fullPath();
+    return true;
+}
+
+
+const char* uiWindowGrabDlg::getExtension() const
+{
+    int ifmt = -1;
+    FilePath fp( fileinputfld_->fileName() );
+    const BufferString ext( fp.extension() );
+    for ( int idx=0; imageformats[idx]; idx++ )
+    {
+	if ( ext == imageformats[idx] )
+	    { ifmt = idx; break; }
+    }
+
+    if ( ifmt < 0 )
+    {
+	const char* selectedfilter = fileinputfld_->selectedFilter();
+	for ( ; filters[ifmt]; ifmt++ )
+	{
+	    if ( !strcmp(selectedfilter,filters[ifmt]) )
+		break;
+	}
+    }
+
+    return imageformats[ifmt] ? imageformats[ifmt] : imageformats[0];
+}
+
+
+uiMainWin* uiWindowGrabDlg::getWindow() const
+{
+    const int idx = windowfld_ ? windowfld_->box()->currentItem() : 0;
+    return const_cast<uiMainWin*>( windowlist_[idx] );
+};
+
+
+int uiWindowGrabDlg::getQuality() const
+{ return qualityfld_->sldr()->getIntValue(); }
+
+
+uiWindowGrabber::uiWindowGrabber( uiParent* p )
+    : parent_(p)
+    , desktop_(false)
+    , grabwin_(0)
+    , quality_(50)
+{}
+
+
+bool uiWindowGrabber::go()
+{
+    uiWindowGrabDlg dlg( parent_, desktop_ );
+    if ( !dlg.go() )
+	return false;
+
+    grabwin_ = dlg.getWindow();
+    filename_ = dlg.getFilename();
+    quality_ = dlg.getQuality();
+
+    execthr_ = new Threads::Thread( mCB(this,uiWindowGrabber,mkThread) );
+    execthr_->detach();
+
+    return true;
+}
+
+
+void uiWindowGrabber::mkThread( CallBacker* )
+{
+    // give window manager chance to remove the uiWindowGrabDlg from screen
+    Time_sleep(0.1); 
+
+    const int zoom = desktop_ ? 0 : 1;
+    grabwin_->activateGrab( filename_, zoom, 0, quality_ );
+    Threads::Thread::threadExit();
+}
+
