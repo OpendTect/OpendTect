7d71283607198bf1fa3197c8e84edc45b18f6276
Bert Bril <bert.bril@dgbes.com>
2008-08-27 15:17:52 +0000

    set ranges to 2D default if 2D
    git-svn-id: https://svn.opendtect.org/od/trunk@13113 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiIo/uiposprovgroup.h
+++ include/uiIo/uiposprovgroup.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Bert
  Date:          Feb 2008
- RCS:           $Id: uiposprovgroup.h,v 1.6 2008-04-24 10:32:01 cvsraman Exp $
+ RCS:           $Id: uiposprovgroup.h,v 1.7 2008-08-27 15:17:52 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -27,7 +27,9 @@ public:
     {
 			Setup( bool is_2d, bool with_z )
 			    : uiPosFiltGroup::Setup(is_2d)
-			    , withz_(with_z)		{}
+			    , withz_(with_z)
+			    , cs_(!is_2d)
+			 { if ( is_2d ) cs_.set2DDef(); }
 
 	virtual	~Setup()				{}
 	mDefSetupMemb(bool,withz)
