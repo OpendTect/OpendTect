c4486fa204b53bd665b46c1691ca648edc404010
Nageswara Rao <nageswara.rao@dgbes.com>
2008-09-01 12:14:02 +0000

    Corssplots for different attributes
    git-svn-id: https://svn.opendtect.org/od/trunk@13154 a985855d-d384-4b45-ae33-31a410998c3c

--- doc/Scripts/2DScripts/2DAttributeCrossplot.cmd
+++ doc/Scripts/2DScripts/2DAttributeCrossplot.cmd
@@ -6,22 +6,12 @@ Mon Jan 28 11:36:39 2008
 Comment "----------Script for Attribute Crossplot---------------"
 
 Case Insensitive
-Menu "processing`Attributes"
+Include "$SCRIPTSDIR$/2DScripts/AllAtributes.cmd"
+Menu "Processing`Attributes"
 Window "Attribute Set 2D"
-Button "New attribute set"
-OnError Continue
-Button "No"
-OnError Stop
-Combo "Attribute group" "<All>"
-Button "Save on OK" off
-Combo "Attribute type" "Coherency"
-Button "Select*"
-ListClick "Select Data" 1 Double
-Input "Attribute name" "CoherencyAttrib"
-
 Button "Cross-Plot attributes"
 Window "Attribute cross-plotting"
-ListClick "Attributes to calculate" "CoherencyAttrib"
+ListSelect "Attributes to calculate" 1 4 On
 Combo "Line name" "i5007"
 Button "Select Location filters"
 ListButton "Filter selection" "Subsample" On
@@ -35,7 +25,39 @@ Button "Set data for X"
 TableClick "Data Table" ColHead "CoherencyAttrib"
 Button "Select as Y data"
 Button "Show crossplot"
-Snapshot "$SCRIPTSDIR$/Snapshots/2Dattribcrossplot.png" CurWin
+Snapshot "$SNAPSHOTSDIR$/2d_coherencyattr_crossplot.png" CurWin
+Window "[all] step*"
+Close
+
+Window "Attribute data"
+TableClick "Data Table" ColHead "[Y]CoherencyAttrib"
+Button "UnSelect as Y Data"
+TableClick "Data Table" ColHead "ConvolveAtt*"
+Button "Select as Y data"
+Button "Show crossplot"
+Snapshot "$SNAPSHOTSDIR$/2d_convolveattr_crossplot.png" Curwin
+Window "[all] step*"
+Close
+
+Window "Attribute data"
+TableClick "Data Table" ColHead "[Y]ConvolveAttr*"
+Button "UnSelect as Y Data"
+TableClick "Data Table" ColHead "CurvatureAttr*"
+Button "Select as Y data"
+Button "Show crossplot"
+Snapshot "$SNAPSHOTSDIR$/2d_curvatureattr_crossplot.png" CurWin
+Window "[all] step*"
+Close
+
+Window "Attribute data"
+TableClick "Data Table" ColHead "[Y]Curvature*"
+Button "UnSelect as Y Data"
+TableClick "Data Table" ColHead "DipAttr*"
+Button "Select as Y data"
+Button "Show crossplot"
+Snapshot "$SNAPSHOTSDIR$/2d_dipattr_crossplot.png"
+Window "[all] step*"
+Close
 
 Window "Attribute data"
 Button "Dismiss"
@@ -45,5 +67,9 @@ Button "Cancel"
 
 Window "Attribute Set 2D"
 Ok
+
+Menu "Survey`Select/Setup"
+Ok
+Button "No"
 
 End
