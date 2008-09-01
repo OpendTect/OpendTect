879f67eb3e22eff8ae453cf570551dfc75dced40
Nageswara Rao <nageswara.rao@dgbes.com>
2008-09-01 09:24:46 +0000

    Horizon - Isopach  Creation
    git-svn-id: https://svn.opendtect.org/od/trunk@13151 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ doc/Scripts/Horizon-Isopach.cmd
@@ -0,0 +1,28 @@
+dTect V3.2
+OpendTect commands
+Mon Jan 28 11:36:39 2008
+!
+
+Comment "------Calculating Thickness Between 2 selected Horizons--------"
+
+Case Insensitive
+
+TreeMenu "Horizon" "Load"
+ListClick "Select Horizon*" 1 Double
+Wheel "vRotate" 45
+TreeMenu "Horizon`*" "Calculate Isopach"
+Button "Select Calculate to"
+ListClick "Objects list" 4 double
+Input "Attribute name" "Demo-Isopach"
+Ok
+
+Button "Make snapshot"
+Button "Scene"
+Ok
+Input "Select filename" "$SNAPSHOTSDIR$/Demo-Isopach.jpg"
+Ok
+Wheel "vRotate" -45
+
+TreeMenu "Horizon`*" "Remove"
+
+End
