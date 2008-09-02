ecde62c232211f1e7f49526f82657260c2672bdd
Nageswara Rao <nageswara.rao@dgbes.com>
2008-09-02 11:21:20 +0000

    Generating wavelet and apply on data.
    git-svn-id: https://svn.opendtect.org/od/trunk@13171 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ doc/Scripts/ManageWavelet.cmd
@@ -0,0 +1,60 @@
+dTect V3.2
+OpendTect commands
+Mon Jun 19 10:36:39 2008
+!
+
+Case Insensitive
+
+Comment "------Generate Wavelet and Apply on Data-----------"
+
+Menu "Survey`Manage`Wavelets"
+#Window "Wavelet management"
+Button "Generate"
+Input "Central Frequency" 40
+Input "Peak amplitude" 1
+Button "Select Wavelet"
+Input "Name" "TestWavelet"
+Ok
+Ok
+Button "Dismiss"
+
+Comment "------------Adding ConvolveAttribute---------"
+
+Menu "Processing`Attributes"
+Window "Attribute Set 3D"
+Combo "Attribute Type" "Convolve"
+Button "Select Input Data"
+Button "Stored"
+ListClick "Select Data" "Median Dip Filter*" Double
+Combo "Filter type" "Wavelet"
+Button "Select Wavelet"
+ListClick "Objects list" "TestWavelet" Double
+Input "Attribute name" "Wavelet-on-MedianDip"
+Button "Save on OK" Off
+Ok
+
+Comment "----------Apply Attibute On data----------"
+
+Menu "Windows`New"
+Menu "Windows`Tile`Horizontal"
+
+TreeMenu "Tree Scene 1" "Inline" "Add"
+TreeMenu "Tree Scene 1" "Inline`*`*" "Select Attribute`Stored Cubes`Median Dip Filter*"
+
+TreeMenu "Tree Scene 2" "Inline" "Add"
+TreeMenu "Tree Scene 2" "Inline`*`*" "Select Attribute`Attributes`Wavelet-on-MedianDip"
+
+Snapshot "$SNAPSHOTSDIR$/ManageWavelet.png" "ODMain"
+
+Sleep 2
+#Window "OpendTect Main Window"
+Menu "Survey`Select/Setup"
+Ok
+Button "No"
+Menu "Survey`Manage`Wavelets"
+ListClick "Objects list" "TestWavelet"
+Button "Remove this Object"
+Button "Yes"
+Button "Dismiss"
+
+End
