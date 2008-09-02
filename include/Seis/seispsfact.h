3ac03f3cbda51ba27ec4b19ac1725d82045459c0
Bert Bril <bert.bril@dgbes.com>
2008-09-02 10:55:09 +0000

    added MultiCube type
    git-svn-id: https://svn.opendtect.org/od/trunk@13167 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Seis/seispsfact.h
+++ include/Seis/seispsfact.h
@@ -7,16 +7,18 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	A.H. Bril
  Date:		2005
- RCS:		$Id: seispsfact.h,v 1.4 2008-01-22 15:04:17 cvsbert Exp $
+ RCS:		$Id: seispsfact.h,v 1.5 2008-09-02 10:55:09 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "seispsioprov.h"
+#include "seismulticubeps.h"
 
 #define sKeySeisPS3DTranslatorGroup "Pre-Stack Seismics"
 defineTranslatorGroup(SeisPS3D,sKeySeisPS3DTranslatorGroup);
 defineTranslator(CBVS,SeisPS3D,"CBVS");
+defineTranslator(MultiCube,SeisPS3D,"MultiCube");
 
 #define sKeySeisPS2DTranslatorGroup "2D Pre-Stack Seismics"
 defineTranslatorGroup(SeisPS2D,sKeySeisPS2DTranslatorGroup);
