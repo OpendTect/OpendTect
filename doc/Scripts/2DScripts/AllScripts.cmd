fc55084d4849ac0115c4f9d856f9b718619d84af
Nageswara Rao <nageswara.rao@dgbes.com>
2008-09-05 06:26:32 +0000

    NNTrain.cmd script Added
    git-svn-id: https://svn.opendtect.org/od/trunk@13203 a985855d-d384-4b45-ae33-31a410998c3c

--- doc/Scripts/2DScripts/AllScripts.cmd
+++ doc/Scripts/2DScripts/AllScripts.cmd
@@ -14,6 +14,7 @@ Include "$SCRIPTSDIR$/2DScripts/2DExportData.cmd"
 Include "$SCRIPTSDIR$/2DScripts/2DImportData.cmd"
 Include "$SCRIPTSDIR$/2DScripts/2DHorizonGrid.cmd"
 Include "$SCRIPTSDIR$/2DScripts/CreateSteeringCube.cmd"
+Include "$SCRIPTSDIR$/2DScripts/NNTrain.cmd"
 Include "$SCRIPTSDIR$/2DScripts/Crete2DSeismicOutput.cmd.cmd"
 
 End
