c6ed62bdb8d617ac69abb341155955076aeb1d66
Nageswara Rao <nageswara.rao@dgbes.com>
2008-09-08 11:51:42 +0000

    ManageWavelet on 2D-Data
    git-svn-id: https://svn.opendtect.org/od/trunk@13232 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ doc/Scripts/2DScripts/ManageWavelet.cmd
@@ -0,0 +1,62 @@
+dTect V3.2
+OpendTect commands
+Mon Jun 19 10:36:39 2008
+!
+
+Comment "------Generate Wavelet and Apply on Data-----------"
+Case Insensitive
+
+Menu "Survey`Manage`Wavelets"
+Button "Generate"
+Input "Central Frequency" 40
+Input "Peak amplitude" 1
+Button "Select Wavelet"
+Input "Name" "2d-TestWavelet"
+Ok
+Ok
+Button "Dismiss"
+
+Comment "------------Adding ConvolveAttribute---------"
+Menu "Processing`Attributes"
+Window "Attribute Set 2D"
+Combo "Attribute group" "<All>"
+Button "Save on OK" Off
+Combo "Attribute Type" "Convolve"
+Button "Select Input Data"
+Button "Stored"
+ListClick "Select Data" "LS 5k" Double
+Combo "Filter type" "Wavelet"
+Button "Select Wavelet"
+ListClick "Objects list" "2d-TestWavelet" Double
+Input "Attribute name" "Wavelet-on-2dData"
+Ok
+#Button "Add as new"
+
+Comment "----------Apply Attibute On data----------"
+Menu "Windows`New"
+Menu "Windows`Tile`Horizontal"
+
+TreeMenu "Tree Scene 1" "2d Seismics" "Add"
+ListClick "Objects list" "LS 5k" Double
+ListClick "Select Data from List" "i5007" Double
+TreeMenu "Tree Scene 1" "2d Seismics`*`*`*" "Select*`Stored*`Seis"
+
+TreeMenu "Tree Scene 2" "2d Seismics" "Add"
+ListClick "Objects list" "LS 5k" Double
+ListClick "Select Data from List" "i5007" Double
+TreeMenu "Tree Scene 2" "2d Seismics`*`*`*" "Select*`Attributes`Wavelet*"
+Wheel "hRotate" -45
+
+Snapshot "$SNAPSHOTSDIR$/2d-ManageWavelet.png" "ODMain"
+
+Sleep 2
+Menu "Survey`Select/Setup"
+Ok
+Button "No"
+Menu "Survey`Manage`Wavelets"
+ListClick "Objects list" "2d-TestWavelet"
+Button "Remove this object"
+Button "Yes"
+Button "Dismiss"
+
+End
