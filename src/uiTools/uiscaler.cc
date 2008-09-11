b54587b42b263cf1607b966ca1cd74279e0e9632
Bert Bril <bert.bril@dgbes.com>
2008-09-11 09:03:18 +0000

    Fator -> Factor
    git-svn-id: https://svn.opendtect.org/od/trunk@13264 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiTools/uiscaler.cc
+++ src/uiTools/uiscaler.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne Hemstra
  Date:          June 2002
- RCS:           $Id: uiscaler.cc,v 1.18 2008-05-28 12:31:54 cvsnageswara Exp $
+ RCS:           $Id: uiscaler.cc,v 1.19 2008-09-11 09:03:18 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -40,7 +40,7 @@ uiScaler::uiScaler( uiParent* p, const char* lbl, bool linonly )
     DoubleInpSpec dis;
     linearfld = new uiGenInput( this, "Shift/Factor", 
 	   			DoubleInpSpec().setName("Shift",0),
-	   			DoubleInpSpec().setName("Fator",1) );
+	   			DoubleInpSpec().setName("Factor",1) );
 
     if ( !typefld )
 	ynfld->attach( leftOf, linearfld );
