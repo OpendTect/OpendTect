7a20c1142c88f77857e6affab91dce2befcea01b
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2008-09-15 10:24:34 +0000

    Added displaypropertylinks.h/.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@13275 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/displaypropertylinks.h
@@ -0,0 +1,139 @@
+#ifndef displaypropertylinks_h
+#define displaypropertylinks_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	K. Tingdahl & N. Hemstra
+ Date:		September 2008
+ RCS:		$Id: displaypropertylinks.h,v 1.1 2008-09-15 10:24:34 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+#include "factory.h"
+#include "namedobj.h"
+#include "thread.h"
+
+class IOPar;
+class DisplayPropertyHolder;
+class DisplayPropertyLink;
+
+/*!brief Manages property links between differerent displays.
+
+   Property links can be ranges, color sequences and other parameters.
+
+   The intelligence on what can be linked lies in the subclasses of
+   DisplayPropertyLink.
+
+   Displays that are able to link properties to must inherit
+   DisplayPropertyHolder and in addition must be added to the
+   DisplayLinkManager.
+
+   To create a link from a certain DisplayPropertyHolder,
+   DisplayLinkManager::createPossibleLinks should be called. This fills an
+   ObjectSet<DisplayPropertyLink>&, and one or more links in that set
+   may be enabled by calling DisplayPropertyHolder::addDisplayPropertyLink().
+
+   The link is removed by calling
+   DisplayPropertyHolder::removeDisplayPropertyLink().
+
+*/
+
+
+
+class DisplayLinkManager
+{
+public:
+    				DisplayLinkManager();
+    				~DisplayLinkManager();
+    static DisplayLinkManager&	getImpl();
+
+    int				addHolder(DisplayPropertyHolder*);
+    void			createPossibleLinks(DisplayPropertyHolder*,
+					ObjectSet<DisplayPropertyLink>&);
+    				//!<DisplayPropertyLinks become yours
+
+    int				nrDisplayPropertyLinks() const;
+    int				getDisplayPropertyLinkID(int idx) const;
+    int				addDisplayPropertyLink(DisplayPropertyLink*);
+    				/*!<PropertyLink becomes mine, returns id for
+				    new link. */
+    void			removeDisplayPropertyLink(int id);
+    DisplayPropertyLink*	getDisplayPropertyLink(int id);
+    const DisplayPropertyLink*	getDisplayPropertyLink(int id) const;
+
+    bool			usePar(const IOPar&);
+    void			fillPar(IOPar&) const;
+
+protected:
+
+    friend			class DisplayPropertyHolder;
+    void			removeHolder(DisplayPropertyHolder*);
+
+    ObjectSet<DisplayPropertyHolder> holders_;
+    int				freeholderid_;
+    mutable Threads::Mutex	lock_;
+
+    ObjectSet<DisplayPropertyLink> propertylinks_;
+    TypeSet<int>		propertylinkids_;
+    int				freelinkid_;
+};
+
+
+
+
+
+class DisplayPropertyHolder
+{
+public:
+			DisplayPropertyHolder(bool reg);
+
+    virtual		~DisplayPropertyHolder();
+
+    int			propertyHolderID() const;
+    virtual const char*	getDisplayPropertyHolderName() const;
+
+private:
+    friend		class DisplayLinkManager;
+    int			propertyholderid_;
+
+};
+
+
+class DisplayPropertyLink : public NamedObject
+{
+public:
+    mDefineFactory2ParamInClass(DisplayPropertyLink,DisplayPropertyHolder*,
+	    			DisplayPropertyHolder*,factory);
+
+    virtual const char*		type() const				= 0;
+    virtual const char*		userType() const;
+
+    virtual bool		init();
+    				//Called when added to manager, before
+				//this call, no callbacks should be set.
+				
+    bool			addHolder(DisplayPropertyHolder*);
+    void			removeHolder(DisplayPropertyHolder*);
+    bool			isValid() const;
+    virtual void		getDescription(BufferString&) const;
+
+    int				nrDisplayPropertyHolders() const;
+    const DisplayPropertyHolder* getDisplayPropertyHolder(int idx) const;
+
+protected:
+    friend			class DisplayLinkManager;
+    virtual bool		syncNow(const DisplayPropertyHolder* origin,
+	    				DisplayPropertyHolder* target);
+    				//If Target is zero, sync all but origin
+
+    virtual bool		isHolderOK(const DisplayPropertyHolder*);
+    				DisplayPropertyLink(DisplayPropertyHolder*,
+						    DisplayPropertyHolder*);
+
+    ObjectSet<DisplayPropertyHolder>	holders_;
+};
+
+#endif
---
+++ src/General/displaypropertylinks.cc
@@ -0,0 +1,261 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : Nanne Hemstra & K. Tingdahl
+ * DATE     : September 2008
+-*/
+
+static const char* rcsID = "$Id: displaypropertylinks.cc,v 1.1 2008-09-15 10:24:34 cvskris Exp $";
+
+#include "displaypropertylinks.h"
+#include "ptrman.h"
+
+mImplFactory2Param(DisplayPropertyLink,DisplayPropertyHolder*,
+		   DisplayPropertyHolder*,DisplayPropertyLink::factory);
+
+
+DisplayPropertyHolder::DisplayPropertyHolder( bool reg )
+    : propertyholderid_( -1 )
+{ if ( reg ) DisplayLinkManager::getImpl().addHolder(this); }
+
+
+DisplayPropertyHolder::~DisplayPropertyHolder()
+{ DisplayLinkManager::getImpl().removeHolder(this); }
+
+
+int DisplayPropertyHolder:: propertyHolderID() const
+{ return propertyholderid_; }
+
+
+const char* DisplayPropertyHolder::getDisplayPropertyHolderName() const
+{ return 0; }
+
+
+const char* DisplayPropertyLink::userType() const
+{ return type(); }
+
+
+bool DisplayPropertyLink::init()
+{ return syncNow( holders_[0], holders_[1] ); }
+
+
+bool DisplayPropertyLink::addHolder( DisplayPropertyHolder* dph )
+{
+    if ( !isHolderOK( dph ) )
+	return false;
+
+    holders_ += dph;
+    
+    return syncNow( holders_[0], dph );
+}
+
+
+void DisplayPropertyLink::removeHolder( DisplayPropertyHolder* dph )
+{ holders_ -= dph; }
+
+
+bool DisplayPropertyLink::isValid() const
+{ return holders_.size()>1; }
+
+
+void DisplayPropertyLink::getDescription( BufferString& desc ) const
+{ desc = userType(); }
+
+
+int DisplayPropertyLink::nrDisplayPropertyHolders() const
+{ return holders_.size(); }
+
+
+const DisplayPropertyHolder*
+DisplayPropertyLink::getDisplayPropertyHolder( int idx ) const
+{ return holders_[idx]; }
+
+
+bool DisplayPropertyLink::syncNow( const DisplayPropertyHolder* origin,
+				   DisplayPropertyHolder* target)
+{ return true; }
+
+
+bool DisplayPropertyLink::isHolderOK(const DisplayPropertyHolder*)
+{ return true; }
+
+
+DisplayPropertyLink::DisplayPropertyLink(
+	DisplayPropertyHolder* h0, DisplayPropertyHolder* h1 )
+{
+    holders_ += h0; holders_ += h1;
+}
+
+
+DisplayLinkManager::DisplayLinkManager()
+    : freeholderid_( 0 )
+    , freelinkid_( 0 )
+{}
+
+
+DisplayLinkManager::~DisplayLinkManager()
+{
+    deepErase( propertylinks_ );
+}
+
+
+DisplayLinkManager& DisplayLinkManager::getImpl()
+{
+    static PtrMan<DisplayLinkManager> mgr = new DisplayLinkManager;
+    return *mgr;
+}
+
+
+int DisplayLinkManager::addHolder( DisplayPropertyHolder* hldr )
+{
+    Threads::MutexLocker lock( lock_ );
+    const int idx = holders_.indexOf( hldr );
+    if ( idx==-1 )
+    {
+	hldr->propertyholderid_ = freeholderid_++;
+	holders_ += hldr;
+    }
+
+    return hldr->propertyHolderID();
+}
+
+
+void DisplayLinkManager::createPossibleLinks( DisplayPropertyHolder* hldr,
+				    ObjectSet<DisplayPropertyLink>& links )
+{
+    Threads::MutexLocker lock( lock_ );
+    for ( int idx=holders_.size()-1; idx>=0; idx-- )
+    {
+	if ( holders_[idx]==hldr )
+	    continue;
+
+	const BufferStringSet& names =
+	    DisplayPropertyLink::factory().getNames( false );
+
+	for ( int idy=names.size()-1; idy>=0; idy-- )
+	{
+	    DisplayPropertyLink* link =
+		DisplayPropertyLink::factory().create( names[idy]->buf(),
+						       hldr, holders_[idx] );
+	    if ( link )
+		links += link;
+	}
+    }
+}
+
+
+int DisplayLinkManager::nrDisplayPropertyLinks() const
+{
+    Threads::MutexLocker lock( lock_ );
+    return propertylinks_.size();
+}
+
+
+int DisplayLinkManager::getDisplayPropertyLinkID( int idx ) const
+{
+    Threads::MutexLocker lock( lock_ );
+    if ( !propertylinkids_.validIdx(idx) )
+	return -1;
+
+    return propertylinkids_[idx];
+}
+
+
+int DisplayLinkManager::addDisplayPropertyLink( DisplayPropertyLink* lnk )
+{
+    Threads::MutexLocker lock( lock_ );
+    for ( int idx=propertylinks_.size()-1; idx>=0; idx-- )
+    {
+	if ( strcmp(propertylinks_[idx]->type(), lnk->type() ) )
+	    continue;
+
+	//If we have at least one holder in common, we can combine them
+	bool hascommon = false;
+	for ( int idy=lnk->holders_.size(); idy>=0; idy-- )
+	{
+	    if ( propertylinks_[idx]->holders_.indexOf(lnk->holders_[idy])>=0 )
+	    {
+		hascommon = true;
+		break;
+	    }
+	}
+
+	if ( !hascommon )
+	    continue;
+
+	//Add everything that's not present in existing link from previous link
+	for ( int idy=lnk->holders_.size(); idy>=0; idy-- )
+	{
+	    if ( propertylinks_[idx]->holders_.indexOf(lnk->holders_[idy])<0 )
+	    {
+		if ( !propertylinks_[idx]->addHolder( lnk->holders_[idy] ) )
+		{
+		    pErrMsg("Something is wrong");
+		}
+	    }
+	}
+
+	delete lnk;
+
+	return propertylinkids_[idx];
+    }
+
+    lnk->init();
+    propertylinks_ += lnk;
+    const int res = freeholderid_++;
+    propertylinkids_ += res;
+
+    return res;
+}
+
+
+void DisplayLinkManager::removeDisplayPropertyLink( int id )
+{
+    Threads::MutexLocker lock( lock_ );
+    const int idx = propertylinkids_.indexOf( id );
+    if ( idx<0 )
+	return;
+
+    delete propertylinks_.remove( idx );
+    propertylinkids_.remove( idx );
+}
+
+
+const DisplayPropertyLink*
+DisplayLinkManager::getDisplayPropertyLink( int id ) const 
+{ return const_cast<DisplayLinkManager*>(this)->getDisplayPropertyLink( id ); }
+
+
+DisplayPropertyLink* DisplayLinkManager::getDisplayPropertyLink( int id )
+{
+    Threads::MutexLocker lock( lock_ );
+    const int idx = propertylinkids_.indexOf( id );
+    if ( idx<0 )
+        return 0;
+
+    return propertylinks_[idx];
+}
+
+
+void DisplayLinkManager::removeHolder(  DisplayPropertyHolder* hldr )
+{
+    Threads::MutexLocker lock( lock_ );
+    if ( holders_.indexOf( hldr )==-1 )
+	return;
+
+    for ( int idx=propertylinks_.size()-1; idx>=0; idx-- )
+    {
+	DisplayPropertyLink& link = *propertylinks_[idx];
+	for ( int idy=link.nrDisplayPropertyHolders()-1; idy>=0; idy-- )
+	{
+	    if ( link.getDisplayPropertyHolder( idy ) == hldr )
+	    {
+		link.removeHolder( hldr );
+		if ( !link.isValid() )
+		{
+		    delete propertylinks_.remove( idx );
+		    break;
+		}
+	    }
+	}
+    }
+}
