4a7c82399bb96ff180239a02b333af6870fc17de
Nageswara Rao <nageswara.rao@dgbes.com>
2008-09-19 07:12:41 +0000

    Creating 3D seismic cube with own volume subselection
    git-svn-id: https://svn.opendtect.org/od/trunk@13296 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ doc/Scripts/CreateSeismicOutput.cmd
@@ -0,0 +1,48 @@
+dTect V3.2
+OpendTect commands
+Mon Jan 28 11:36:39 2008
+!
+
+Comment "--------------Create Seismic output---------------"
+
+Case Insensitive
+Menu "Processing`Create Seismic output"
+Button "Select Quantity to output"
+Button "Stored"
+ListClick "Select Data" 5 Double
+Button "Select Volume subsel*"
+Input "Inline start" 320
+Input "Inline stop" 320
+Ok
+Button "Select Output Cube"
+Input "Name" "SeismicOutputCreate"
+Ok
+Button "Proceed"
+Ok
+Sleep 7
+
+TreeMenu "Inline" "Add"
+TreeMenu "Inline`*" "Position"
+Window "Positioning"
+Input "Inl nr" 320
+Ok
+Button "Stored"
+ListClick "Select Data" "SeismicOutputCreate" Double
+
+Menu "Survey`Manage`Seismics"
+ListClick "Objects list" "SeismicOutputCreate"
+Button "Remove this object"
+Button "Yes"
+Button "Dismiss"
+
+TreeMenu "Inline`*" "Remove"
+
+# Parameter id is not correct error
+# is coming After running this script second time
+# to rectify this problem select same survey again
+
+Menu "Survey`Select/Setup"
+Sleep 1
+Ok
+
+End
