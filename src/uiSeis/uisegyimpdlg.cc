bc00dce0515c7a3b80cb7fd4a1b9b22593e44df5
Bert Bril <bert.bril@dgbes.com>
2008-09-26 13:37:09 +0000

    use new uiSeisSel::mkCtxtIOObj
    git-svn-id: https://svn.opendtect.org/od/trunk@13362 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiSeis/uisegyimpdlg.cc
+++ src/uiSeis/uisegyimpdlg.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Bert
  Date:          Sep 2008
- RCS:           $Id: uisegyimpdlg.cc,v 1.1 2008-09-24 19:48:37 cvsbert Exp $
+ RCS:           $Id: uisegyimpdlg.cc,v 1.2 2008-09-26 13:37:09 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -13,28 +13,75 @@ ________________________________________________________________________
 
 #include "uisegydef.h"
 #include "uisegyexamine.h"
+#include "uiseistransf.h"
+#include "uiseissel.h"
 #include "uitoolbar.h"
 #include "uicombobox.h"
 #include "uibutton.h"
 #include "uibuttongroup.h"
 #include "uiseparator.h"
 #include "uigeninput.h"
+#include "uiseparator.h"
 #include "uilabel.h"
 #include "uimsg.h"
 #include "keystrs.h"
 #include "segytr.h"
 #include "seisioobjinfo.h"
+#include "ctxtioobj.h"
 
+
+uiSEGYImpDlg::Setup::Setup( Seis::GeomType gt )
+    : uiDialog::Setup("SEG-Y Import",0,"103.1.5")
+    , geom_(gt) 
+    , nrexamine_(0)     
+    , rev_(uiSEGYRead::Rev0)
+{
+}
+
+#define mAddButton(fnm,func,tip,toggle) \
+	tb_->addButton( fnm, mCB(this,uiSEGYImpDlg,func), tip, toggle )
 
 uiSEGYImpDlg::uiSEGYImpDlg( uiParent* p,
 			const uiSEGYImpDlg::Setup& su, IOPar& iop )
     : uiDialog(p,su)
     , setup_(su)
     , pars_(iop)
+    , optsfld_(0)
+    , ctio_(*uiSeisSel::mkCtxtIOObj(su.geom_))
+    , readParsReq(this)
 {
-    uiSEGYFileOpts::Setup osu( setup_.geom_, uiSEGYRead::Import,
-	    			setup_.isrev1_ );
-    optsfld_ = new uiSEGYFileOpts( this, osu, &iop );
+    ctio_.ctxt.forread = false;
+
+    uiGroup* optsgrp = 0;
+    if ( setup_.rev_ != uiSEGYRead::Rev1 )
+    {
+	optsgrp = new uiGroup( this, "Opts group" );
+	uiSEGYFileOpts::Setup osu( setup_.geom_, uiSEGYRead::Import,
+				    setup_.rev_ == uiSEGYRead::WeakRev1 );
+	optsfld_ = new uiSEGYFileOpts( optsgrp, osu, &iop );
+	optsfld_->readParsReq.notify( mCB(this,uiSEGYImpDlg,readParsCB) );
+
+	savesetupfld_ = new uiGenInput( optsgrp, "On OK, save setup as" );
+	savesetupfld_->attach( alignedBelow, optsfld_ );
+	optsgrp->setHAlignObj( savesetupfld_ );
+    }
+
+    uiSeparator* sep = optsgrp ? new uiSeparator( this, "Hor sep" ) : 0;
+
+    uiGroup* outgrp = new uiGroup( this, "Output group" );
+    transffld_ = new uiSeisTransfer( outgrp, uiSeisTransfer::Setup(setup_.geom_)
+				    .withnullfill(false)
+				    .fornewentry(true) );
+    outgrp->setHAlignObj( transffld_ );
+    if ( sep )
+    {
+	sep->attach( stretchedBelow, optsgrp );
+	outgrp->attach( alignedBelow, optsgrp );
+	outgrp->attach( ensureBelow, sep );
+    }
+
+    seissel_ = new uiSeisSel( outgrp, ctio_, uiSeisSel::Setup(setup_.geom_) );
+    seissel_->attach( alignedBelow, transffld_ );
 
     finaliseDone.notify( mCB(this,uiSEGYImpDlg,setupWin) );
 }
@@ -51,26 +98,34 @@ void uiSEGYImpDlg::setupWin( CallBacker* )
 }
 
 
+void uiSEGYImpDlg::readParsCB( CallBacker* )
+{
+    readParsReq.trigger();
+}
+
+
 uiSEGYImpDlg::~uiSEGYImpDlg()
 {
 }
 
 
-bool uiSEGYImpDlg::getFromScreen( bool permissive )
+bool uiSEGYImpDlg::getParsFromScreen( bool permissive )
 {
-    return optsfld_->fillPar( pars_, permissive );
+    return optsfld_ ? optsfld_->fillPar( pars_, permissive ) : true;
 }
 
 
 bool uiSEGYImpDlg::rejectOK( CallBacker* )
 {
-    getFromScreen( true );
+    getParsFromScreen( true );
     return true;
 }
 
 
 bool uiSEGYImpDlg::acceptOK( CallBacker* )
 {
-    return getFromScreen( false );
-}
+    if ( !getParsFromScreen(false) )
+	return false;
 
+    return true;
+}
