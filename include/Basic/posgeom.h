32bf3789611088d600f7a44e42f25968d72ee904
Bert Bril <bert.bril@dgbes.com>
2008-10-08 15:54:37 +0000

    clean-up
    git-svn-id: https://svn.opendtect.org/od/trunk@13457 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/posgeom.h
+++ include/Basic/posgeom.h
@@ -5,25 +5,25 @@
 ________________________________________________________________________
 
  CopyRight:     (C) dGB Beheer B.V.
- Author:        A.H. Bril
+ Author:        Bert
  Date:          Jan 2003
- RCS:           $Id: posgeom.h,v 1.5 2006-09-07 15:44:24 cvskris Exp $
+ RCS:           $Id: posgeom.h,v 1.6 2008-10-08 15:54:37 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
 
-#include <geometry.h>
-#include <position.h>
+#include "geometry.h"
+#include "position.h"
 
-inline Geom::Point2D<int>		pt( const BinID& bid )
+inline Geom::Point2D<int>	pt( const BinID& bid )
 				{ return Geom::Point2D<int>(bid.inl,bid.crl); }
 inline Geom::Point2D<double>	pt( const Coord& c )
 				{ return Geom::Point2D<double>(c.x,c.y); }
 
-inline BinID		bid( const Geom::Point2D<int>& p )
-			{ return BinID(p.x,p.y); }
-inline Coord		crd( const Geom::Point2D<double>& p )
-			{ return Coord(p.x,p.y); }
+inline BinID			bid( const Geom::Point2D<int>& p )
+				{ return BinID(p.x,p.y); }
+inline Coord			crd( const Geom::Point2D<double>& p )
+				{ return Coord(p.x,p.y); }
 
 
 #endif
