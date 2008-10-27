7602fc9079b60274236797833aa4fa35b7da2c10
Nageswara Rao <nageswara.rao@dgbes.com>
2008-10-27 10:43:27 +0000

    Script Added
    git-svn-id: https://svn.opendtect.org/od/trunk@13592 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ doc/Scripts/2DScripts/SceneProperties.cmd
@@ -0,0 +1,36 @@
+dTect V3.2
+OpendTect commands
+Mon Jun 7 11:36:39 2008
+!
+
+Case Insensitive
+Comment "---------Scene Properties-----------"
+
+Menu "Scenes`New"
+Menu "Scenes`Tile`Horizontal"
+
+TreeMenu "Scene 1" "Properties"
+Button "Survey box" Off
+Sleep 3
+Button "Survey box" On
+Button "Annotation text" Off
+Sleep 3
+Button "Annotation text" On
+Button "Annotation scale" Off
+Sleep 3
+Button "Annotation scale" On 
+Button "Background color"
+ColorOK Cyan 4
+Sleep 2
+Slider "Marker size Slider" 39
+Button "Mouse marker color"
+ColorOK Black 2
+Button "Apply to all scenes" On
+Ok
+
+Snapshot "$SNAPSHOTSDIR$/2d-sceneproperties.png" ODMain
+
+Menu "Survey`Select/Setup"
+Ok
+
+End
