b7777f3e05adcb7e2d447628c586bee12ede1353
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2008-11-24 15:55:11 +0000

    Adapted to changes in VelocityGridder.
    git-svn-id: https://svn.opendtect.org/od/trunk@13805 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiVolumeProcessing/uivelocitygridder.cc
+++ src/uiVolumeProcessing/uivelocitygridder.cc
@@ -4,16 +4,16 @@
  * DATE     : April 2005
 -*/
 
-static const char* rcsID = "$Id: uivelocitygridder.cc,v 1.3 2008-08-12 19:22:30 cvskris Exp $";
+static const char* rcsID = "$Id: uivelocitygridder.cc,v 1.4 2008-11-24 15:55:11 cvskris Exp $";
 
 #include "uivelocitygridder.h"
 
 #include "uiselectvelocityfunction.h"
 #include "uigridder2d.h"
+#include "uigeninput.h"
 #include "gridder2d.h"
 #include "uilabel.h"
 #include "uivolprocchain.h"
-#include "uigeninput.h"
 #include "velocitygridder.h"
 #include "volprocchain.h"
 
@@ -37,7 +37,8 @@ uiStepDialog* uiVelocityGridder::create( uiParent* p, VolProc::Step* ro )
 
 uiVelocityGridder::uiVelocityGridder( uiParent* p, VelGriddingStep* ro )
     : uiStepDialog( p, uiDialog::Setup(VelGriddingStep::sUserName(),
-		VelGriddingStep::sUserName(),"dgb:104.1.2"), ro )
+		VelGriddingStep::sUserName(),"dgb:104.1.2"),
+	   	    ro )
     , operation_( ro )
 {
     griddersel_ = new uiGridder2DSel( this, ro->getGridder() );
@@ -46,7 +47,7 @@ uiVelocityGridder::uiVelocityGridder( uiParent* p, VelGriddingStep* ro )
     uiLabel* label = new uiLabel( this, "Velocity sources" );
     label->attach( alignedBelow, griddersel_ );
 
-    velfuncsel_ = new Vel::uiFunctionSel( this, operation_->getPicks(), 0 );
+    velfuncsel_ = new Vel::uiFunctionSel( this, operation_->getSources(), 0 );
     velfuncsel_->attach( alignedBelow, label );
 }
 
@@ -61,7 +62,7 @@ bool uiVelocityGridder::acceptOK( CallBacker* cb )
 	return false;
 
     if ( !operation_ ) return true;
-    operation_->setPicks( velfuncsel_->getVelSources() );
+    operation_->setSources( velfuncsel_->getVelSources() );
     operation_->setGridder( griddersel_->getSel()->clone() );
     return true;
 }
