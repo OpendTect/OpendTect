804fc384b43a915af218ad29433620f3b04f9fd2
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2008-11-28 19:29:20 +0000

    Added prestack event export.
    git-svn-id: https://svn.opendtect.org/od/trunk@13855 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/PreStackProcessing/prestackeventascio.h
@@ -0,0 +1,68 @@
+#ifndef prestackeventascio_h
+#define prestackeventascio_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	K. Tingdahl
+ Date:		November 2008
+ RCS:		$Id: prestackeventascio.h,v 1.1 2008-11-28 19:29:20 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+#include "binidvalset.h"
+#include "task.h"
+#include "horsampling.h"
+#include "prestackevents.h"
+
+namespace PreStack
+{
+class EventManager;
+
+/*!Outputs an ascii string with all prestack event, each pick on one row. The
+columns are as follows:
+
+     Inline
+     Crossline
+     Event index (0-N per inline/crossline pair).
+     Dip, going to increasing inlines, If not dip is available, 0 is written
+     Dip, going to increasing crosslines, If not dip is available, 0 is written
+     Event quality, 0-255
+     Azimuth (0-2PI)
+     Offset
+     Depth
+     Pick quality, 0-255
+*/
+
+
+class EventExporter : public SequentialTask
+{
+public:
+    			EventExporter(std::ostream& strm,EventManager&);
+    			~EventExporter();
+    void		setHRange(const HorSampling& hrg);
+
+    od_int64		nrDone() const { return nrdone_; }
+    od_int64		totalNr() const { return locations_.totalSize(); }
+    int			nextStep();
+    const char*		nrDoneText() const;
+
+protected:
+
+    std::ostream&		strm_;
+    EventManager&		events_;
+    HorSampling			hrg_;
+
+    BinIDValueSet		locations_;
+    BinIDValueSet::Pos		pos_;
+
+    int				nrdone_;
+    int				fileidx_;
+};
+
+
+} // namespace PreStack
+
+#endif
---
+++ src/PreStackProcessing/prestackeventascio.cc
@@ -0,0 +1,94 @@
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	K. Tingdahl
+ Date:		November 2008
+ RCS:		$Id: prestackeventascio.cc,v 1.1 2008-11-28 19:29:20 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+#include "prestackeventascio.h"
+
+namespace PreStack
+{
+
+EventExporter::EventExporter( std::ostream& strm, EventManager& events )
+    : strm_( strm )
+    , events_( events )
+    , hrg_( true )
+    , nrdone_( 0 )
+    , fileidx_( 0 )
+    , locations_( 0, false )
+{
+    events_.ref();
+    events_.getLocations( locations_ );
+}
+
+
+EventExporter::~EventExporter()
+{ events_.unRef(); }
+
+
+void EventExporter::setHRange( const HorSampling& hrg )
+{
+    hrg_ = hrg;
+}
+
+
+const char* EventExporter::nrDoneText() const
+{ return "CDP's processed"; }
+
+#define mWrite( var ) strm_ << (var) << '\t';
+
+
+int EventExporter::nextStep()
+{
+    if ( !locations_.next( pos_ ) )
+	return cFinished();
+
+    nrdone_++;
+
+    const BinID bid = locations_.getBinID(pos_);
+    if ( !hrg_.includes( bid ) )
+	return cMoreToDo();
+
+    RefMan<const EventSet> eventset = events_.getEvents( bid, true, false );
+    if ( !eventset )
+	return cMoreToDo();
+
+    for ( int eventidx=0; eventidx<eventset->events_.size(); eventidx++ )
+    {
+	const Event& event = *eventset->events_[eventidx];
+	if ( event.sz_==0 )
+	    continue;
+
+	float inldip = 0, crldip = 0;
+	events_.getDip( BinIDValue( bid, event.pick_[0]), event.horid_,
+			inldip, crldip );
+
+	for ( int pickidx=0; pickidx<event.sz_; pickidx++ )
+	{
+	    mWrite( bid.inl );
+	    mWrite( bid.crl );
+	    mWrite( fileidx_ );
+	    mWrite( inldip );
+	    mWrite( crldip );
+	    mWrite( (int) event.quality_ );
+	    mWrite( event.offsetazimuth_[pickidx].azimuth() );
+	    mWrite( event.offsetazimuth_[pickidx].offset() );
+	    mWrite( event.pick_[pickidx] );
+	    mWrite( event.pickquality_ ? event.pickquality_[pickidx] : 255 );
+	    strm_ << '\n';
+	}
+
+	fileidx_++;
+    }
+
+    return cMoreToDo();
+}
+
+
+
+} // namespace PreStack
