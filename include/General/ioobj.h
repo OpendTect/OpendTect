6b0a5a54cc77b5e5a1fd53942017436bccb39525
Ranojay Sen <ranojay.sen@dgbes.com>
2008-12-29 11:15:38 +0000

    mGlobal macro added to every function declaration for exporting in DLL
    git-svn-id: https://svn.opendtect.org/od/trunk@14214 a985855d-d384-4b45-ae33-31a410998c3c

--- include/General/ioobj.h
+++ include/General/ioobj.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	A.H. Bril
  Date:		31-7-1995
- RCS:		$Id: ioobj.h,v 1.25 2008-12-29 06:01:41 cvsranojay Exp $
+ RCS:		$Id: ioobj.h,v 1.26 2008-12-29 11:15:38 cvsranojay Exp $
 ________________________________________________________________________
 
 -*/
@@ -147,12 +147,12 @@ private:
 };
 
 
-bool equalIOObj(const MultiID&,const MultiID&);
-bool areEqual(const IOObj*,const IOObj*);
-bool fullImplRemove(const IOObj&);
+mGlobal bool equalIOObj(const MultiID&,const MultiID&);
+mGlobal bool areEqual(const IOObj*,const IOObj*);
+mGlobal bool fullImplRemove(const IOObj&);
 int GetFreeMBOnDisk(const IOObj*);
 //!< If null passed or otherwise impossible returns survey directory free space
-void GetFreeMBOnDiskMsg(int,BufferString&);
+mGlobal void GetFreeMBOnDiskMsg(int,BufferString&);
 //!< Shouldn't be here. But where?
 
 
