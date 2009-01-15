84b13c2a8a0c06fb0ca862e9399436fc51ba78b3
Nanne Hemstra <nanne.hemstra@dgbes.com>
2009-01-15 04:12:22 +0000

    Update, removed flatten_seis from Maefile
    git-svn-id: https://svn.opendtect.org/od/trunk@14454 a985855d-d384-4b45-ae33-31a410998c3c

--- spec/EMAttrib/flatten_seis.cc
+++ spec/EMAttrib/flatten_seis.cc
@@ -7,7 +7,7 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID = "$Id: flatten_seis.cc,v 1.2 2008-11-25 15:35:21 cvsbert Exp $";
+static const char* rcsID = "$Id: flatten_seis.cc,v 1.3 2009-01-15 04:12:22 cvsnanne Exp $";
 
 
 
@@ -16,6 +16,7 @@ static const char* rcsID = "$Id: flatten_seis.cc,v 1.2 2008-11-25 15:35:21 cvsbe
 #include "ctxtioobj.h"
 #include "cubesampling.h"
 #include "emmanager.h"
+#include "emhorizon2d.h"
 #include "emhorizon3d.h"
 #include "emhorizonztransform.h"
 #include "emsurfaceio.h"
@@ -25,11 +26,13 @@ static const char* rcsID = "$Id: flatten_seis.cc,v 1.2 2008-11-25 15:35:21 cvsbe
 #include "plugins.h"
 #include "position.h"
 #include "ranges.h"
+#include "seis2dline.h"
+#include "seisioobjinfo.h"
 #include "seistrctr.h"
 #include "seiszaxisstretcher.h"
 
 
-static EM::Horizon3D* loadHorizon( const char* id, BufferString& err )
+static EM::Horizon* load3DHorizon( const char* id, BufferString& err )
 {
     IOM().to( MultiID(IOObjContext::getStdDirData(IOObjContext::Surf)->id) );
     PtrMan<IOObj> ioobj = IOM().get( id );
@@ -58,6 +61,35 @@ static EM::Horizon3D* loadHorizon( const char* id, BufferString& err )
 }
 
 
+static EM::Horizon* load2DHorizon( const char* id, BufferString& err )
+{
+    IOM().to( MultiID(IOObjContext::getStdDirData(IOObjContext::Surf)->id) );
+    PtrMan<IOObj> ioobj = IOM().get( id );
+    if ( !ioobj ) { err = "Horizon "; err += id; err += " not OK"; return 0; }
+
+    std::cerr << "Reading " << ioobj->name() << " ..." << std::endl;
+    EM::EMManager& em = EM::EMM();
+    EM::dgbSurfaceReader* reader =
+	new EM::dgbSurfaceReader( *ioobj, EM::Horizon2D::typeStr() );
+    if ( !reader->isOK() )
+    {
+	delete reader;
+	return 0;
+    }
+
+    EM::EMObject* emobj = EM::Horizon2D::create( EM::EMM() );
+    mDynamicCastGet(EM::Horizon2D*,horizon,emobj)
+    emobj->setMultiID( ioobj->key() );
+    reader->setOutput( *horizon );
+    reader->setReadOnlyZ( true );
+    reader->execute( &std::cerr );
+
+    horizon->ref();
+    delete reader;
+    return horizon;
+}
+
+
 #define mErrRet(msg)	{ std::cerr << msg << std::endl; return false; }
 
 
@@ -65,19 +97,26 @@ bool BatchProgram::go( std::ostream& strm )
 {
     PtrMan<IOObj> inioobj = getIOObjFromPars( "Input Seismics", false,
 		SeisTrcTranslatorGroup::ioContext(), true );
+    SeisIOObjInfo info( inioobj );
+    const bool is2d = info.is2D();
+
+    IOObjContext ctxt = SeisTrcTranslatorGroup::ioContext();
+    if ( is2d ) ctxt.deftransl = "2D";
     PtrMan<IOObj> outioobj = getIOObjFromPars( "Output Seismics", true,
-		SeisTrcTranslatorGroup::ioContext(), true );
+					       ctxt, true );
     if ( !outioobj || !inioobj )
 	mErrRet("Need input and output seismics")
 
     BufferString mid;
     pars().get( "Reference horizon", mid );
 
-    BufferString errmsg;
-    EM::Horizon3D* horizon = loadHorizon( mid.buf(), errmsg );
-    if ( errmsg != "" ) mErrRet( errmsg )
+    float refz = 0;
+    pars().get( "Reference depth", refz );
 
-    float refz; pars().get( "Reference depth", refz );
+    BufferString errmsg;
+    EM::Horizon* horizon = is2d ? load2DHorizon( mid.buf(), errmsg )
+				: load3DHorizon( mid.buf(), errmsg );
+    if ( errmsg != "" ) mErrRet( errmsg )
 
     CubeSampling cs;
     if ( !cs.hrg.usePar( pars() ) )
@@ -85,8 +124,9 @@ bool BatchProgram::go( std::ostream& strm )
 
     RefMan<EM::HorizonZTransform> zt = new EM::HorizonZTransform( horizon );
     const Interval<float> zrg = zt->getZInterval( false );
-    cs.zrg.start = zrg.start;
-    cs.zrg.stop = zrg.stop;
+    float shift = 0;
+    cs.zrg.start = zrg.start + shift;
+    cs.zrg.stop = zrg.stop + shift;
 
     SeisZAxisStretcher exec( *inioobj, *outioobj, cs, *zt, true );
     exec.execute( &strm );
