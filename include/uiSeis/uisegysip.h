0b795d750c09f13c37f99b78cd2a4479fd1bbe04
Bert Bril <bert.bril@dgbes.com>
2009-01-20 12:43:53 +0000

    added xyInFeet()
    git-svn-id: https://svn.opendtect.org/od/trunk@14525 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiSeis/uisegysip.h
+++ include/uiSeis/uisegysip.h
@@ -6,7 +6,7 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Bert
  Date:          Feb 2004
- RCS:           $Id: uisegysip.h,v 1.8 2009-01-15 16:08:41 cvsbert Exp $
+ RCS:           $Id: uisegysip.h,v 1.9 2009-01-20 12:43:53 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -19,9 +19,13 @@ mClass uiSEGYSurvInfoProvider : public uiSurvInfoProvider
 {
 public:
 
+    			uiSEGYSurvInfoProvider()
+			    : xyinft_(false)	{}
+
     const char*		usrText() const		{ return "Scan SEG-Y file(s)"; }
     uiDialog*		dialog(uiParent*);
     bool		getInfo(uiDialog*,CubeSampling&,Coord crd[3]);
+    bool		xyInFeet() const	{ return xyinft_; }
 
     IOPar*		getImportPars() const
     			{ return imppars_.isEmpty() ? 0 : new IOPar(imppars_); }
@@ -30,6 +34,7 @@ public:
 
     friend class	uiSEGYSIPMgrDlg;
     IOPar		imppars_;
+    bool		xyinft_;
 
 };
 
