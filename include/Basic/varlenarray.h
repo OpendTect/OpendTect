95ba09074b711d835bffa4d3e2225165c7ec253e
Bert Bril <bert.bril@dgbes.com>
2009-01-29 12:39:58 +0000

    Added ';' in non-msvc definition - just like the msvc definition
    git-svn-id: https://svn.opendtect.org/od/trunk@14629 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/varlenarray.h
+++ include/Basic/varlenarray.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Nanne Hemstra
  Date:		September 2007
- RCS:		$Id: varlenarray.h,v 1.4 2009-01-14 07:20:36 cvsranojay Exp $
+ RCS:		$Id: varlenarray.h,v 1.5 2009-01-29 12:39:58 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -25,7 +25,7 @@ ________________________________________________________________________
 #else
 
 # define mAllocVarLenArr( type, varnm, __size ) \
-  type varnm[__size]
+  type varnm[__size];
 # define mVarLenArr(varnm)	varnm
 
 #endif
