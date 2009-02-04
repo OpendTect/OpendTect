0ce5476d9f425ff66d2ae348c804cd6b7697ba79
Nageswara Rao <nageswara.rao@dgbes.com>
2009-02-04 08:58:06 +0000

    Scripts modified to new version.
    git-svn-id: https://svn.opendtect.org/od/trunk@14679 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ doc/Scripts/ImportSeismicAttributeSet.cmd
@@ -0,0 +1,59 @@
+dTect V3.2
+OpendTect commands
+Mon Jun 28 11:36:39 2008
+!
+
+Case Insensitive
+Comment "----------Importing Attribute Set--------"
+
+Menu "Survey`Select/Setup"
+ListClick "Select Data" "Magellane*" Double
+Include "$SCRIPTSDIR$/2DScripts/AllAtributes.cmd"
+Menu "Analysis`Attributes"
+Window "Attribute Set*"
+Button "Save attribute set"
+Input "Name" "DemoAttributeSet"
+Ok
+Ok
+Menu "Survey`Select/Setup"
+ListClick "Select Data" "F3_Demo_for_Test" Double
+Menu "Analysis`Attributes"
+Window "Attribute Set*"
+Button "Import attribute set*"
+ListClick "Select Data*" "Magellane*" Double
+ListClick "Objects list" "DemoAttributeSet" Double
+Button "Select Input Seismics*"
+ListClick "Objects list" "Median Dip Fil*" Double
+Button "Select Input Steering cube"
+ListClick "Objects list" "Steering Cube BG Detailled" Double
+Ok
+Button "Move Down"
+Button "Move Down"
+Button "Move Down"
+ListClick "Defined Attributes" "PositionAttrib"
+Button "Move Up"
+Button "Move Up"
+Button "Move Up"
+Button "Sort"
+Ok
+Menu "Survey`Select/Setup"
+Ok
+Button "No"
+
+Menu "Survey`Select/Setup"
+ListClick "Select Data" "Magellane*" Double
+Menu "Analysis`Attributes"
+Window "Attribute Set*"
+Button "Open attribute set"
+ListClick "Objects list" "DemoAttributeSet"
+Button "Rename this*"
+Input "New name" "DemoAttributeSet-new"
+ListClick "Objects list" "DemoAttributeSet-new"
+Button "Remove this*"
+Button "Yes"
+Button "Cancel"
+Button "Cancel"
+Menu "Survey`Select/Setup"
+ListClick "Select Data" "F3_Demo_for_Test" Double
+
+End
---
+++ doc/Scripts/ManageWells.cmd
@@ -0,0 +1,48 @@
+dTect V3.2
+OpendTect commands
+Mon Jun 19 10:36:39 2008
+!
+
+Case Insensitive
+Comment "---------------Importing Well------------"
+Menu "Survey`Manage`Wells"
+Window "Well file*"
+ListClick "Objects list" "F03-2"
+ListClick "Available logs" "Gamma Ray"
+Button "Export log"
+Window "Export Well*"
+Input "Depth range (m) start" 400
+Input "Depth range (m) stop" 600
+Input "Output file" "$EXPORTDIR$/GammaRay.dat"
+Ok
+Comment "---------------Importing above log------"
+Button "Add Logs"
+Window "Logs"
+Input "Input*" "$EXPORTDIR$/GammaRay.dat"
+Ok
+Window "Well file*"
+Button "Dismiss"
+Comment "---------Displaying above iported log--------"
+TreeMenu "Well" "Load"
+ListClick "Objects list" "F03-2" Double
+TreeMenu "Well`*" "Properties"
+Window "Well display*"
+Tab "Well display*" "Left Log"
+Combo "Select log" "Gamma_Ray"
+Button "Log filled" Off
+Button "Line color"
+ColorOK Red 4
+Button "Dismiss"
+Snapshot "$SNAPSHOTSDIR$/$IDX$_WellImport.png" CurWin
+TreeMenu "Well`*" "Remove"
+Menu "Survey`Manage`Wells"
+ListClick "Objects list" "F03-2"
+ListClick "Available logs" "Gamma_Ray"
+Button "Rename selected*"
+Input "New name" "Gamma_Ray_new"
+ListClick "Available logs" "Gamma_Ray_new"
+Button "Remove selected*"
+Button "Yes"
+Button "Dismiss"
+
+End
