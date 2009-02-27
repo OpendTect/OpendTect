8112a29905042ec189d6ce35bda1cf71647f7a78
Bert Bril <bert.bril@dgbes.com>
2009-02-27 14:15:52 +0000

    added getLog(const char*)
    git-svn-id: https://svn.opendtect.org/od/trunk@14921 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Well/welllogset.h
+++ include/Well/welllogset.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Bert Bril
  Date:		Aug 2003
- RCS:		$Id: welllogset.h,v 1.10 2009-01-06 10:57:11 cvsranojay Exp $
+ RCS:		$Id: welllogset.h,v 1.11 2009-02-27 14:15:52 cvsbert Exp $
 ________________________________________________________________________
 
 
@@ -33,6 +33,8 @@ public:
     Log&		getLog( int idx )	{ return *logs[idx]; }
     const Log&		getLog( int idx ) const	{ return *logs[idx]; }
     int			indexOf(const char*) const;
+    const Log*		getLog( const char* nm ) const	{ return gtLog(nm); }
+    Log*		getLog( const char* nm )	{ return gtLog(nm); }
 
     Interval<float>	dahInterval() const	{ return dahintv; }
     						//!< not def if start == undef
@@ -53,6 +55,10 @@ protected:
     			{ dahintv.start = mSetUdf(dahintv.stop); }
 
     void		updateDahIntv(const Well::Log&);
+
+    Log*		gtLog( const char* nm ) const
+			{ const int idx = indexOf(nm);
+			    return idx < 0 ? 0 : const_cast<Log*>(logs[idx]); }
 
 };
 
