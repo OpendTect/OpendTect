5c84215dfd4909b63d398c2962d1138d5f2097db
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2009-03-16 15:52:06 +0000

    First working (ie. tested) commit.
    git-svn-id: https://svn.opendtect.org/od/trunk@15065 a985855d-d384-4b45-ae33-31a410998c3c

--- src/PreStackProcessing/od_process_prestack.cc
+++ src/PreStackProcessing/od_process_prestack.cc
@@ -4,13 +4,14 @@
  * DATE     : Dec 2008
 -*/
 
-static const char* rcsID = "$Id: od_process_prestack.cc,v 1.3 2009-03-09 22:07:59 cvskris Exp $";
+static const char* rcsID = "$Id: od_process_prestack.cc,v 1.4 2009-03-16 15:52:06 cvskris Exp $";
 
 #include "batchprog.h"
 
-#include "cubesampling.h"
+#include "horsampling.h"
 #include "envvars.h"
 #include "prestackprocessor.h"
+#include "prestackprocessortransl.h"
 #include "hostdata.h"
 #include "ioman.h"
 #include "ioobj.h"
@@ -113,11 +114,11 @@ bool BatchProgram::go( std::ostream& strm )
     const double pause_sleep_time = GetEnvVarDVal( "OD_BATCH_SLEEP_TIME", 1 );
     TextStreamProgressMeter progressmeter(strm);
 
-    CubeSampling cubesampling( true );
-    cubesampling.usePar( pars() );
+    HorSampling horsampling( true );
+    const bool hashorsampling = horsampling.usePar( pars() );
 
     BufferString linekey;
-    pars().get( sKey::LineKey, linekey );
+    pars().get( PreStack::ProcessManager::sKeyLineKey(), linekey );
 
     Seis::GeomType geomtype;
     if ( !Seis::getFromPar( pars(), geomtype ) )
@@ -126,23 +127,55 @@ bool BatchProgram::go( std::ostream& strm )
 	return false;
     }
 
-    if ( geomtype!=Seis::VolPS || geomtype!=Seis::LinePS )
+    if ( geomtype!=Seis::VolPS && geomtype!=Seis::LinePS )
     {
 	errorMsg("\nGeometry is not prestack");
 	return false;
     }
 
+    MultiID setupmid;
+    if ( !pars().get( PreStack::ProcessManager::sKeySetup(), setupmid ) )
+    {
+	errorMsg( "\nCannot read setup" );
+	return false;
+    }
+
+    PtrMan<IOObj> setupioobj = IOM().get( setupmid ); 
+    if ( !setupioobj )
+    {
+	errorMsg("\nCannot create setup object");
+	return false;
+    }
+
+    mDeclareAndTryAlloc( PreStack::ProcessManager*, procman,
+	    		 PreStack::ProcessManager );
+
+    if ( !procman )
+    {
+	errorMsg( "Cannot create processor");
+	return false;
+    }
+
+    BufferString errmsg;
+    if ( !PreStackProcTranslator::retrieve( *procman, setupioobj, errmsg ) ) 
+    { 
+	errorMsg( errmsg.buf() );
+	delete procman;
+	return false;
+    } 
     
     if ( geomtype==Seis::LinePS && linekey.isEmpty() )
     {
 	errorMsg("\nNo linekey set" );
+	delete procman;
 	return false;
     }
 
     MultiID inputmid;
-    if ( !pars().get("Input", inputmid ) )
+    if ( !pars().get(PreStack::ProcessManager::sKeyInputData(), inputmid ) )
     {
 	errorMsg("\nCannot read input id");
+	delete procman;
 	return false;
     }
 
@@ -150,13 +183,15 @@ bool BatchProgram::go( std::ostream& strm )
     if ( !inputioobj )
     {
 	errorMsg("\nCannot create input object");
+	delete procman;
 	return false;
     }
 
     MultiID outputmid;
-    if ( !pars().get("Input", outputmid ) )
+    if ( !pars().get( PreStack::ProcessManager::sKeyOutputData(), outputmid ) )
     {
 	errorMsg("\nCannot read output id");
+	delete procman;
 	return false;
     }
 
@@ -164,6 +199,7 @@ bool BatchProgram::go( std::ostream& strm )
     if ( !outputioobj )
     {
 	errorMsg("\nCannot create output object");
+	delete procman;
 	return false;
     }
 
@@ -173,12 +209,28 @@ bool BatchProgram::go( std::ostream& strm )
     StepInterval<int> cdprange(0,0,1);
 
     if ( geomtype==Seis::VolPS )
-	reader3d = SPSIOPF().get3DReader( *inputioobj,
-					  cubesampling.hrg.start.inl );
+    {
+	reader3d = SPSIOPF().get3DReader( *inputioobj );
+	if ( reader3d && !hashorsampling )
+	{
+	    const PosInfo::CubeData& posdata = reader3d->posData();
+	    if ( posdata.size() )
+	    {
+		StepInterval<int> inlrg, crlrg;
+		posdata.getInlRange( inlrg );
+		posdata.getCrlRange( crlrg );
+
+		horsampling.init();
+		horsampling.setInlRange( inlrg );
+		horsampling.setCrlRange( crlrg );
+	    }
+        }
+    }
     else
     {
 	reader2d = SPSIOPF().get2DReader( *inputioobj, linekey.buf() );
-	if ( reader2d && !pars().get( "CDP Range", cdprange ) )
+	if ( reader2d &&
+	    !pars().get( PreStack::ProcessManager::sKeyCDPRange(), cdprange ) )
 	{
 	    const PosInfo::Line2DData& posdata = reader2d->posData();
 	    for ( int idx=0; idx<posdata.posns_.size(); idx++ )
@@ -194,6 +246,7 @@ bool BatchProgram::go( std::ostream& strm )
     if ( !reader3d && !reader2d )
     {
 	errorMsg("\nCannot create input reader");
+	delete procman;
 	return false;
     }
 
@@ -208,23 +261,15 @@ bool BatchProgram::go( std::ostream& strm )
     if ( !writer )
     {
 	errorMsg("\nCannot create output writer");
-	return false;
-    }
-
-    mDeclareAndTryAlloc( PreStack::ProcessManager*, procman,
-	    		 PreStack::ProcessManager );
-    if ( !procman || !procman->usePar( pars() ) )
-    {
-	errorMsg("\nCannot setup process flow");
+	delete procman;
 	return false;
     }
 
     const BinID stepout = procman->getInputStepout();
 
-
     BinID curbid; //inl not used if 2D
     BinID step;   //inl not used if 2D
-    HorSamplingIterator hiter( cubesampling.hrg );
+    HorSamplingIterator hiter( horsampling );
 
     if ( geomtype==Seis::LinePS )
     {
@@ -236,6 +281,7 @@ bool BatchProgram::go( std::ostream& strm )
 	if ( !hiter.next(curbid) )
 	{
 	    errorMsg("\nNo CDP's to process");
+	    delete procman;
 	    return false;
 	}
 
@@ -266,6 +312,13 @@ bool BatchProgram::go( std::ostream& strm )
 	procman->reset();
 	BinID relbid;
 
+	if ( !procman->prepareWork() )
+	{
+	    errorMsg("\nCannot prepare processing.");
+	    delete procman;
+	    return false;
+	}
+
 	for ( relbid.inl=-stepout.inl; relbid.inl<=stepout.inl; relbid.inl++ )
 	{
 	    for ( relbid.crl=-stepout.crl; relbid.crl<=stepout.crl;relbid.crl++)
@@ -289,48 +342,53 @@ bool BatchProgram::go( std::ostream& strm )
 	    }
 	}
 
-	if ( procman->prepareWork() || procman->process() )
+	if ( !procman->process() )
 	{
-	    const DataPack* dp =
-		DPM(DataPackMgr::FlatID()).obtain(procman->getOutput());
-	    mDynamicCastGet( const PreStack::Gather*, gather, dp );
-	    if ( gather )
+	    errorMsg("\nCannot process.");
+	    delete procman;
+	    return false;
+	}
+
+	const DataPack* dp =
+	    DPM(DataPackMgr::FlatID()).obtain(procman->getOutput());
+	mDynamicCastGet( const PreStack::Gather*, gather, dp );
+	if ( gather )
+	{
+	    const int nrtraces =
+		gather->size( !PreStack::Gather::offsetDim() );
+	    const int nrsamples =
+		gather->size( PreStack::Gather::offsetDim() );
+	    const StepInterval<double> zrg =
+		gather->posData().range( PreStack::Gather::offsetDim() );
+	    SeisTrc trc( nrsamples );
+	    trc.info().sampling.start = zrg.start;
+	    trc.info().sampling.step = zrg.step;
+
+	    if ( reader2d )
 	    {
-		const int nrtraces =
-		    gather->size( !PreStack::Gather::offsetDim() );
-		const int nrsamples =
-		    gather->size( PreStack::Gather::offsetDim() );
-		const StepInterval<double> zrg =
-		    gather->posData().range( PreStack::Gather::offsetDim() );
-		SeisTrc trc( nrsamples );
-		trc.info().sampling.start = zrg.start;
-		trc.info().sampling.step = zrg.step;
+		trc.info().nr = curbid.crl;
+		PosInfo::Line2DPos linepos;
+		if ( reader2d->posData().getPos( curbid.crl, linepos ) )
+		    trc.info().coord = linepos.coord_;
+	    }
+	    else
+	    {
+		trc.info().binid = curbid;
+		trc.info().coord = SI().transform( curbid );
+	    }
 
-		if ( reader2d )
-		{
-		    trc.info().nr = curbid.crl;
-		    PosInfo::Line2DPos linepos;
-		    if ( reader2d->posData().getPos( curbid.crl, linepos ) )
-			trc.info().coord = linepos.coord_;
-		}
-		else
-		{
-		    trc.info().binid = curbid;
-		    trc.info().coord = SI().transform( curbid );
-		}
+	    for ( int idx=0; idx<nrtraces; idx++ )
+	    {
+		trc.info().azimuth = gather->getAzimuth( idx );
+		trc.info().offset = gather->getOffset( idx );
+		for ( int idy=0; idy<nrsamples; idy++ )
+		    trc.set( idy, gather->data().get( idx, idy ), 0 );
 
-		for ( int idx=0; idx<nrtraces; idx++ )
+		if ( !writer->put( trc ) )
 		{
-		    trc.info().azimuth = gather->getAzimuth( idx );
-		    trc.info().offset = gather->getOffset( idx );
-		    for ( int idy=0; idy<nrsamples; idy++ )
-			trc.set( idy, gather->data().get( idx, idy ), 0 );
-
-		    if ( !writer->put( trc ) )
-		    {
-			errorMsg("\nCannot write output");
-			return false;
-		    }
+		    errorMsg("\nCannot write output");
+		    delete procman;
+		    return false;
 		}
 	    }
 
@@ -357,7 +415,11 @@ bool BatchProgram::go( std::ostream& strm )
     progressmeter.setFinished();
     mStrmWithProcID( "Threads closed; Writing finish status" );
 
-    if ( !comm ) return true;
+    if ( !comm )
+    {
+	delete procman;
+	return true;
+    }
 
     comm->setState( MMSockCommunic::Finished );
     bool ret = comm->sendState();
@@ -367,6 +429,7 @@ bool BatchProgram::go( std::ostream& strm )
     else
 	mStrmWithProcID( "Could not write finish status" );
 
+    delete procman;
     return ret;
 }
 
