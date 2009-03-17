f5849bf40675d0e9504b9cd7bb77ea9376658bb2
Ranojay Sen <ranojay.sen@dgbes.com>
2009-03-17 07:04:38 +0000

    winmain function for non console executable
    git-svn-id: https://svn.opendtect.org/od/trunk@15069 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Prog/prog.h
+++ include/Prog/prog.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	A.H. Bril
  Date:		5-12-1995
- RCS:		$Id: prog.h,v 1.13 2009-03-16 10:34:00 cvsranojay Exp $
+ RCS:		$Id: prog.h,v 1.14 2009-03-17 07:04:38 cvsranojay Exp $
 ________________________________________________________________________
 
  Include this file in any executable program you make. The file is actually
@@ -18,6 +18,10 @@ ________________________________________________________________________
 #include "plugins.h"
 #include "debug.h"
 
+#ifndef _CONSOLE
+# include <windows.h>
+#endif
+
 #ifdef __cpp__
 extern "C" {
 #endif
@@ -25,6 +29,20 @@ extern "C" {
     			/*!< Will not return meaningful string on Windows */
 #ifdef __cpp__
 }
+#endif
+
+
+#ifndef _CONSOLE
+
+int main( int argc, char** argv );
+
+int APIENTRY WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+		      LPTSTR lpCmdLine, int nCmdShow )
+{
+    int res = main( __argc, __argv ); // __argc and __argv defined in windows.h
+    return res;
+}
+
 #endif
 
 
