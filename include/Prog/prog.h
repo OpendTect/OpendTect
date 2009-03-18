8b192aead64aa3ea4762b24a5972fccff7dfe93f
Ranojay Sen <ranojay.sen@dgbes.com>
2009-03-18 09:05:58 +0000

    Better solution
    git-svn-id: https://svn.opendtect.org/od/trunk@15086 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Prog/prog.h
+++ include/Prog/prog.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	A.H. Bril
  Date:		5-12-1995
- RCS:		$Id: prog.h,v 1.16 2009-03-18 05:04:04 cvsranojay Exp $
+ RCS:		$Id: prog.h,v 1.17 2009-03-18 09:05:58 cvsranojay Exp $
 ________________________________________________________________________
 
  Include this file in any executable program you make. The file is actually
@@ -18,11 +18,12 @@ ________________________________________________________________________
 #include "plugins.h"
 #include "debug.h"
 
-#ifdef _win__
-#ifndef _CONSOLE
-# include <windows.h>
+#ifdef __msvc__
+# ifndef _CONSOLE
+#  include "winmain.h"
+# endif
 #endif
-#endif
+
 
 #ifdef __cpp__
 extern "C" {
@@ -32,22 +33,5 @@ extern "C" {
 #ifdef __cpp__
 }
 #endif
-
-
-#ifndef _CONSOLE
-
-int main( int argc, char** argv );
-
-#ifdef __win__
-int APIENTRY WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
-		      LPTSTR lpCmdLine, int nCmdShow )
-{
-    int res = main( __argc, __argv ); // __argc and __argv defined in windows.h
-    return res;
-}
-#endif
-
-#endif
-
 
 #endif
