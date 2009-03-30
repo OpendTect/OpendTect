76a7734d26070c28b253434bbaa652308d60872a
Raman Singh <raman.singh@dgbes.com>
2009-03-30 06:56:34 +0000

    added class SeisRandLineTo2DGrid...
    git-svn-id: https://svn.opendtect.org/od/trunk@15199 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Seis/seisrandlineto2d.h
+++ include/Seis/seisrandlineto2d.h
@@ -7,15 +7,17 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Raman Singh
  Date:		May 2008
- RCS:		$Id: seisrandlineto2d.h,v 1.4 2009-02-19 10:30:48 cvsraman Exp $
+ RCS:		$Id: seisrandlineto2d.h,v 1.5 2009-03-30 06:56:34 cvsraman Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "executor.h"
 #include "binidvalset.h"
+#include "randomlinegeom.h"
 
 class IOObj;
+class IOPar;
 class LineKey;
 class SeisTrcReader;
 class SeisTrcWriter;
@@ -37,6 +39,8 @@ public:
     od_int64		nrDone() const;
     int			nextStep();
 
+    bool		execute(std::ostream* log=0,bool isfirst=true,
+	    			bool islast=true,int delaybetwnstepsinms=0);
 protected:
 
     SeisTrcReader*	rdr_;
@@ -47,5 +51,42 @@ protected:
     BinIDValueSet::Pos	pos_;
     Seis::TableSelData&	seldata_;
 };
+
+
+mClass SeisRandLineTo2DGrid
+{
+public:
+    			SeisRandLineTo2DGrid(const IOPar&,std::ostream&);
+
+    bool		isOK()			{ return isok_; }
+    bool		createGrid();
+
+    static const char*	sKeyInputID()		{ return "Input ID"; }
+    static const char*	sKeyOutputID()		{ return "Output ID"; }
+    static const char*	sKeyOutpAttrib()	{ return "Output attribute"; }
+    static const char*	sKeyGridSpacing()	{ return "Grid Spacing"; }
+    static const char*	sKeyParPrefix()		{ return "Parallel prefix"; }
+    static const char*	sKeyPerpPrefix()	{ return "Perp prefix"; }
+    static const char*	sKeyRandomLine()	{ return "Random line"; }
+    static const char*	sKeyStartBinID()	{ return "Start BinID"; }
+    static const char*	sKeyStopBinID()		{ return "Stop BinID"; }
+
+protected:
+
+    bool		isok_;
+    std::ostream&	strm_;
+
+    IOObj*		inpobj_;
+    IOObj*		outpobj_;
+    BufferString	outpattrib_;
+    BufferString	parprefix_;
+    BufferString	perprefix_;
+    double		gridspacing_;
+
+    Geometry::RandomLine	rln_;
+
+    bool		mk2DLines(const Geometry::RandomLineSet&,bool);
+};
+
 
 #endif
