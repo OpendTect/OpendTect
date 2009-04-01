88f8ab19ea89f20b223c58815487ebd497169f56
Ranojay Sen <ranojay.sen@dgbes.com>
2009-04-01 05:26:57 +0000

    Use mBasicExtern
    git-svn-id: https://svn.opendtect.org/od/trunk@15225 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/keystrs.h
+++ include/Basic/keystrs.h
@@ -7,7 +7,8 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	A.H.Bril
  Date:		Mar 2002
- RCS:		$Id: keystrs.h,v 1.46 2009-03-24 12:33:51 cvsbert Exp $
+
+ RCS:		$Id: keystrs.h,v 1.47 2009-04-01 05:26:57 cvsranojay Exp $
 ________________________________________________________________________
 
 -*/
@@ -23,6 +24,8 @@ ________________________________________________________________________
 # define mImpl(s) /* empty */
 #endif
 
+#define mExt mBasicExtern
+
 /*!\brief is used for defining key strings that are 'global'.
 
 Some standard key strings are shared between otherwise unrelated modules.
@@ -34,92 +37,97 @@ to this namespace.
 namespace sKey
 {
 
-    extern const char*	Attribute	mImpl("Attribute");
-    extern const char*	Attributes	mImpl("Attributes");
-    extern const char*	Azimuth		mImpl("Azimuth");
-    extern const char*	Color		mImpl("Color");
-    extern const char*	Cube		mImpl("Cube");
-    extern const char*	DataType	mImpl("DataType");
-    extern const char*	Depth		mImpl("Depth");
-    extern const char*	Desc		mImpl("Description");
-    extern const char*	Factor		mImpl("Factor");
-    extern const char*	FileName	mImpl("File name");
-    extern const char*	Filter		mImpl("Filter");
-    extern const char*	FloatUdf	mImpl("1e30");
-    extern const char*	Geometry	mImpl("Geometry");
-    extern const char*	ID		mImpl("ID");
-    extern const char*	IOSelection	mImpl("I/O Selection");
-    extern const char*	Keys		mImpl("Keys");
-    extern const char*	LineKey		mImpl("Line key");
-    extern const char*	Log		mImpl("Log");
-    extern const char*	Name		mImpl("Name");
-    extern const char*	No		mImpl("No");
-    extern const char*	None		mImpl("None");
-    extern const char*	Offset		mImpl("Offset");
-    extern const char*	Output		mImpl("Output");
-    extern const char*	Pars		mImpl("Parameters");
-    extern const char*	Polygon		mImpl("Polygon");
-    extern const char*	Position	mImpl("Position");
-    extern const char*	Random		mImpl("Random");
-    extern const char*	Range		mImpl("Range");
-    extern const char*	Scale		mImpl("Scale");
-    extern const char*	Selection	mImpl("Selection");
-    extern const char*	Subsample	mImpl("Subsample");
-    extern const char*	Shortcuts	mImpl("Shortcuts");
-    extern const char*	Size		mImpl("Size");
-    extern const char*	Steering	mImpl("Steering");
-    extern const char*	StratRef	mImpl("Strat Level");
-    extern const char*	Subsel		mImpl("Subsel");
-    extern const char*	Surface		mImpl("Surface");
-    extern const char*	Survey		mImpl("Survey");
-    extern const char*	Table		mImpl("Table");
-    extern const char*	Target		mImpl("Target");
-    extern const char*	Time		mImpl("Time");
-    extern const char*	TWT		mImpl("TWT");
-    extern const char*	Title		mImpl("Title");
-    extern const char*	TraceNr		mImpl("Trace number");
-    extern const char*	Type		mImpl("Type");
-    extern const char*	Undef		mImpl("Undefined");
-    extern const char*	Value		mImpl("Value");
-    extern const char*	Wheeler		mImpl("Wheeler");
-    extern const char*	Yes		mImpl("Yes");
-    extern const char*	ZDomain		mImpl("ZDomain");
+    mExt const char*	Attribute	mImpl("Attribute");
+    mExt const char*	Attributes	mImpl("Attributes");
+    mExt const char*	Azimuth		mImpl("Azimuth");
+    mExt const char*	Color		mImpl("Color");
+    mExt const char*	Cube		mImpl("Cube");
+    mExt const char*	DataType	mImpl("DataType");
+    mExt const char*	Depth		mImpl("Depth");
+    mExt const char*	Desc		mImpl("Description");
+    mExt const char*	Factor		mImpl("Factor");
+    mExt const char*	FileName	mImpl("File name");
+    mExt const char*	Filter		mImpl("Filter");
+    mExt const char*	FloatUdf	mImpl("1e30");
+    mExt const char*	Geometry	mImpl("Geometry");
+    mExt const char*	ID		mImpl("ID");
+    mExt const char*	IOSelection	mImpl("I/O Selection");
+    mExt const char*	Keys		mImpl("Keys");
+    mExt const char*	LineKey		mImpl("Line key");
+    mExt const char*	Log		mImpl("Log");
+    mExt const char*	Name		mImpl("Name");
+    mExt const char*	No		mImpl("No");
+    mExt const char*	None		mImpl("None");
+    mExt const char*	Offset		mImpl("Offset");
+    mExt const char*	Output		mImpl("Output");
+    mExt const char*	Pars		mImpl("Parameters");
+    mExt const char*	Polygon		mImpl("Polygon");
+    mExt const char*	Position	mImpl("Position");
+    mExt const char*	Random		mImpl("Random");
+    mExt const char*	Range		mImpl("Range");
+    mExt const char*	Scale		mImpl("Scale");
+    mExt const char*	Selection	mImpl("Selection");
+    mExt const char*	Subsample	mImpl("Subsample");
+    mExt const char*	Shortcuts	mImpl("Shortcuts");
+    mExt const char*	Size		mImpl("Size");
+    mExt const char*	Steering	mImpl("Steering");
+    mExt const char*	StratRef	mImpl("Strat Level");
+    mExt const char*	Subsel		mImpl("Subsel");
+    mExt const char*	Surface		mImpl("Surface");
+    mExt const char*	Survey		mImpl("Survey");
+    mExt const char*	Table		mImpl("Table");
+    mExt const char*	Target		mImpl("Target");
+    mExt const char*	Time		mImpl("Time");
+    mExt const char*	TWT		mImpl("TWT");
+    mExt const char*	Title		mImpl("Title");
+    mExt const char*    TraceNr		mImpl("Trace number");
+    mExt const char*	Type		mImpl("Type");
+    mExt const char*	Undef		mImpl("Undefined");
+    mExt const char*	Value		mImpl("Value");
+    mExt const char*	Wheeler		mImpl("Wheeler");
+    mExt const char*	Yes		mImpl("Yes");
+    mExt const char*	ZDomain		mImpl("ZDomain");
 
-    extern const char*	Average		mImpl("Average");
-    extern const char*	Maximum		mImpl("Maximum");
-    extern const char*	Median		mImpl("Median");
-    extern const char*	Minimum		mImpl("Minimum");
-    extern const char*	StdDev		mImpl("StdDev");
-    extern const char*	Sum		mImpl("Sum");
+    mExt const char*	Average		mImpl("Average");
+    mExt const char*	Maximum		mImpl("Maximum");
+    mExt const char*	Median		mImpl("Median");
+    mExt const char*	Minimum		mImpl("Minimum");
+    mExt const char*	StdDev		mImpl("StdDev");
+    mExt const char*	Sum		mImpl("Sum");
 
-    extern const char*	BinIDSel	mImpl("BinID selection");
-    extern const char*	InlRange	mImpl("In-line range");
-    extern const char*	FirstInl	mImpl("First In-line");
-    extern const char*	LastInl		mImpl("Last In-line");
-    extern const char*	StepInl		mImpl("Step In-line");
-    extern const char*	StepOutInl	mImpl("Stepout In-line");
-    extern const char*	CrlRange	mImpl("Cross-line range");
-    extern const char*	FirstCrl	mImpl("First Cross-line");
-    extern const char*	LastCrl		mImpl("Last Cross-line");
-    extern const char*	StepCrl		mImpl("Step Cross-line");
-    extern const char*	StepOutCrl	mImpl("Stepout Cross-line");
-    extern const char*	ZRange		mImpl("Z range");
-    extern const char*  FirstTrc	mImpl("First Trace");
-    extern const char*	LastTrc		mImpl("Last Trace");
-    extern const char*  TrcRange        mImpl("Trace Range");
+    mExt const char*	BinIDSel	mImpl("BinID selection");
+    mExt const char*	InlRange	mImpl("In-line range");
+    mExt const char*	FirstInl	mImpl("First In-line");
+    mExt const char*	LastInl		mImpl("Last In-line");
+    mExt const char*	StepInl		mImpl("Step In-line");
+    mExt const char*	StepOutInl	mImpl("Stepout In-line");
+    mExt const char*	CrlRange	mImpl("Cross-line range");
+    mExt const char*	FirstCrl	mImpl("First Cross-line");
+    mExt const char*	LastCrl		mImpl("Last Cross-line");
+    mExt const char*	StepCrl		mImpl("Step Cross-line");
+    mExt const char*	StepOutCrl	mImpl("Stepout Cross-line");
+    mExt const char*	ZRange		mImpl("Z range");
+    mExt const char*    FirstTrc	mImpl("First Trace");
+    mExt const char*	LastTrc		mImpl("Last Trace");
+    mExt const char*    TrcRange        mImpl("Trace Range");
 
-    extern const char*	TmpStor		mImpl("Temporary storage location");
-    extern const char*	LogFile		mImpl("Log file");
-    extern const char*	Version		mImpl("Version");
+    mExt const char*	TmpStor		mImpl("Temporary storage location");
+    mExt const char*	LogFile		mImpl("Log file");
+    mExt const char*	Version		mImpl("Version");
 
-    extern const char*	Default		mImpl("Default");
-    extern const char*	DefCube		mImpl("Default.Cube");
-    extern const char*	DefLineSet	mImpl("Default.LineSet");
-    extern const char*	DefAttribute	mImpl("Default.Attribute");
-    extern const char*	DefLine		mImpl("Default.Line");
+	
+    mExt const char*	Default		mImpl("Default");
+    mExt const char*	DefCube		mImpl("Default.Cube");
+    mExt const char*	DefLineSet	mImpl("Default.LineSet");
+    mExt const char*	DefLine		mImpl("Default.Line");
+    mExt const char*	DefAttribute	mImpl("Default.Attribute");
+    mExt const char*	DefPS3D		mImpl("Default.PS3D Data Store");
+    mExt const char*	DefPS2D		mImpl("Default.PS2D Data Store");
+    mExt const char*	DefWavelet	mImpl("Default.Wavelet");
+
 };
 
-
+#undef mExt
 #undef mImpl
 
 #endif
