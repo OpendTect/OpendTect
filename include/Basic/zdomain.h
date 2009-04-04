775b89ef60910bb2ddd56f1db41cf3a3490f382b
Nanne Hemstra <nanne.hemstra@dgbes.com>
2009-04-04 16:23:04 +0000

    Added
    git-svn-id: https://svn.opendtect.org/od/trunk@15284 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Basic/zdomain.h
@@ -0,0 +1,26 @@
+#ifndef zdomain_h
+#define zdomain_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	Nanne Hemstra & K. Tingdahl
+ Date:		April 2009
+ RCS:		$Id: zdomain.h,v 1.1 2009-04-04 16:23:04 cvsnanne Exp $
+________________________________________________________________________
+
+-*/
+
+namespace ZDomain
+{
+    const char*		sKey()		{ return "ZDomain"; }
+    const char*		sKeyID()	{ return "ZDomain ID"; }
+    const char*		sKeyTWT()	{ return "TWT"; }
+    const char*		sKeyDepth()	{ return "Depth"; }
+    const char*		getDefault();
+};
+
+
+
+#endif
