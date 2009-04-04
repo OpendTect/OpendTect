87d1e177dcbd6cb590ba4914b792233e2baf40a8
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2009-04-04 10:57:29 +0000

    Added Icon repository.
    git-svn-id: https://svn.opendtect.org/od/trunk@15280 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiBase/uiicons.h
@@ -0,0 +1,28 @@
+#ifndef uiicons_h
+#define uiicons_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        K. Tingdahl
+ Date:          April 2009
+ RCS:           $Id: uiicons.h,v 1.1 2009-04-04 10:57:29 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+/*! Commonly used icons. */
+
+class ioPixmap;
+
+namespace Icons
+{
+    const ioPixmap&	save();
+    const ioPixmap&	saveAs();
+    const ioPixmap&	openObject();
+    const ioPixmap&	newObject();
+};
+
+
+#endif
