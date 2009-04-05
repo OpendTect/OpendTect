db94bc01342b87b9705a659daf439af11fa14952
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2009-04-05 14:52:53 +0000

    1. Started use key from process_time2depth.h 2. Made work both for time2depth and depth2time.
    git-svn-id: https://svn.opendtect.org/od/trunk@15292 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiSeis/uibatchtime2depthsetup.h
+++ include/uiSeis/uibatchtime2depthsetup.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	K. Tingdahl
  Date:		Jan 2008
- RCS:		$Id: uibatchtime2depthsetup.h,v 1.2 2009-04-03 17:19:23 cvskris Exp $
+ RCS:		$Id: uibatchtime2depthsetup.h,v 1.3 2009-04-05 14:52:53 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -26,21 +26,21 @@ class uiBatchTime2DepthSetup : public uiFullBatchDialog
 {
 public:
     		uiBatchTime2DepthSetup(uiParent*);
-		~uiBatchTime2DepthSetup();
 
 protected:
 
     bool		fillPar(IOPar&);
     bool		prepareProcessing();
+    void		dirChangeCB(CallBacker*);
+
+    uiGenInput*		directionsel_;
 
     uiVelSel*		velsel_;
 
-    CtxtIOObj&		inputctxt_;
-    uiSeisSel*		inputsel_;
+    uiSeisSel*		inputtimesel_;
+    uiSeisSel*		inputdepthsel_;
 
     uiPosSubSel*	possubsel_;
-
-    CtxtIOObj&		outputctxt_;
     uiSeisSel*		outputsel_;
 };
 
--- src/uiSeis/uibatchtime2depthsetup.cc
+++ src/uiSeis/uibatchtime2depthsetup.cc
@@ -4,7 +4,7 @@
  * DATE     : April 2005
 -*/
 
-static const char* rcsID = "$Id: uibatchtime2depthsetup.cc,v 1.3 2009-04-03 17:19:23 cvskris Exp $";
+static const char* rcsID = "$Id: uibatchtime2depthsetup.cc,v 1.4 2009-04-05 14:52:53 cvskris Exp $";
 
 #include "uibatchtime2depthsetup.h"
 
@@ -12,71 +12,99 @@ static const char* rcsID = "$Id: uibatchtime2depthsetup.cc,v 1.3 2009-04-03 17:1
 #include "ioman.h"
 #include "keystrs.h"
 #include "seistrctr.h"
+#include "process_time2depth.h"
+#include "uigeninput.h"
 #include "uipossubsel.h"
 #include "uimsg.h"
 #include "uiveldesc.h"
+#include "zdomain.h"
+#include "survinfo.h"
 
 uiBatchTime2DepthSetup::uiBatchTime2DepthSetup( uiParent* p )
     : uiFullBatchDialog( p,
 	uiFullBatchDialog::Setup("Time to depth conversion")
 	    .procprognm("process_time2depth" ) )
-    , outputctxt_( *mMkCtxtIOObj(SeisTrc) )
-    , inputctxt_( *mMkCtxtIOObj(SeisTrc) )
 {
-    setTitleText( "Time to depth conversion" );
+    setTitleText( "Time/Depth conversion" );
+
+    directionsel_ = new uiGenInput( uppgrp_, "Direction",
+	    BoolInpSpec(true, "Time to Depth", "Depth to Time", true ) );
+    directionsel_->valuechanged.notify(
+	    mCB(this,uiBatchTime2DepthSetup,dirChangeCB));
+
     IOObjContext velctxt = uiVelSel::ioContext();
     velctxt.forread = true;
     uiSeisSel::Setup velsetup( Seis::Vol );
     velsetup.seltxt( "Velocity Model" );
-
     velsel_ = new uiVelSel( uppgrp_, velctxt, velsetup );
+    velsel_->attach( alignedBelow, directionsel_ );
 
-    inputctxt_.ctxt.forread = true;
-    inputsel_ = new uiSeisSel( uppgrp_, inputctxt_,uiSeisSel::Setup(Seis::Vol));
-    inputsel_->attach( alignedBelow, velsel_ );
+    IOObjContext inputtimectxt = SeisTrcTranslatorGroup::ioContext();
+    inputtimectxt.parconstraints.add( ZDomain::sKey(), ZDomain::sKeyTWT() );
+    inputtimectxt.allowcnstrsabsent = SI().zIsTime();
+    inputtimectxt.forread = true;
+    uiSeisSel::Setup setup(Seis::Vol); setup.seltxt("Input Time Volume");
+    inputtimesel_ = new uiSeisSel( uppgrp_, inputtimectxt, setup );
+    inputtimesel_->attach( alignedBelow, velsel_ );
+
+    IOObjContext inputdepthctxt = SeisTrcTranslatorGroup::ioContext();
+    inputdepthctxt.parconstraints.add( ZDomain::sKey(), ZDomain::sKeyDepth() );
+    inputdepthctxt.allowcnstrsabsent = !SI().zIsTime();
+    inputdepthctxt.forread = true;
+    setup.seltxt("Input Depth Volume");
+    inputdepthsel_ = new uiSeisSel( uppgrp_, inputdepthctxt, setup );
+    inputdepthsel_->attach( alignedBelow, velsel_ );
 
     possubsel_ =  new uiPosSubSel( uppgrp_, uiPosSubSel::Setup(false,true) );
-    possubsel_->attach( alignedBelow, inputsel_ );
+    possubsel_->attach( alignedBelow, inputtimesel_ );
 
-    outputctxt_.ctxt.forread = false;
-    outputsel_ = new uiSeisSel(uppgrp_,outputctxt_,uiSeisSel::Setup(Seis::Vol));
+    IOObjContext outputctxt = SeisTrcTranslatorGroup::ioContext();
+    outputctxt.forread = false;
+    outputsel_ = new uiSeisSel(uppgrp_,outputctxt,uiSeisSel::Setup(Seis::Vol));
     outputsel_->attach( alignedBelow, possubsel_ );
 
     uppgrp_->setHAlignObj( possubsel_ );
 
     addStdFields( false, true );
+    dirChangeCB( 0 );
 }
 
 
-uiBatchTime2DepthSetup::~uiBatchTime2DepthSetup()
+void uiBatchTime2DepthSetup::dirChangeCB( CallBacker* )
 {
-    delete inputctxt_.ioobj; delete &inputctxt_;
-    delete outputctxt_.ioobj; delete &outputctxt_;
+    const bool istime2depth = directionsel_->getBoolValue();
+    inputtimesel_->display( istime2depth );
+    inputdepthsel_->display( !istime2depth );
+    outputsel_->setLabelText( istime2depth
+	    ? "Ouput Depth Volume"
+	    : "Output Time Volume");
 }
 
 
 bool uiBatchTime2DepthSetup::prepareProcessing()
 {
-    if ( !velsel_->commitInput() )
-    {
-	uiMSG().error("Please select a velocity volume");
+    const IOObj* outputioobj = outputsel_->ioobj(false);
+    if ( !outputioobj || !velsel_->ioobj( false ) )
 	return false;
+
+    PtrMan<IOObj> outputioobjclone = outputioobj->clone();
+
+    if ( directionsel_->getBoolValue() )
+    {
+	if ( !inputtimesel_->ioobj( false ) )
+	    return false;
+
+	outputioobjclone->pars().set( ZDomain::sKey(), ZDomain::sKeyDepth() );
+    }
+    else
+    {
+	if ( !inputdepthsel_->ioobj( false ) )
+	    return false;
+
+	outputioobjclone->pars().set( ZDomain::sKey(), ZDomain::sKeyTWT() );
     }
 
-    if ( !inputsel_->commitInput() )
-    {
-	uiMSG().error("Please select an input volume");
-	return false;
-    }
-
-    if ( !outputsel_->commitInput() )
-    {
-	uiMSG().error("Please enter an output name");
-	return false;
-    }
-
-    outputctxt_.ioobj->pars().set( sKey::ZDomain, sKey::Depth );
-    if ( !IOM().commitChanges(*outputctxt_.ioobj) )
+    if ( !IOM().commitChanges(*outputioobjclone) )
     {
 	uiMSG().error("Cannot write to database",
 		      "Check permissions in Seismics directory" );
@@ -89,15 +117,24 @@ bool uiBatchTime2DepthSetup::prepareProcessing()
 
 bool uiBatchTime2DepthSetup::fillPar( IOPar& par )
 {
-    if ( !inputctxt_.ioobj || !outputctxt_.ioobj || !velsel_->ioobj(false) )
+    if ( !outputsel_->ioobj(true) || !velsel_->ioobj( true ) )
 	return false;
 
-    par.set( "Input volume",  inputctxt_.ioobj->key() );
-    possubsel_->fillPar( par );
-    par.set( "Output volume", outputctxt_.ioobj->key() );
-    //Set depthdomain in output's omf?
+    const IOObj* input = directionsel_->getBoolValue()
+	? inputtimesel_->ioobj( true )
+	: inputdepthsel_->ioobj( true );
 
-    par.set( "Velocity model", velsel_->ioobj(true)->key() );
+    if ( !input )
+	return false;
+
+    par.set( ProcessTime2Depth::sKeyInputVolume(),  input->key() );
+    possubsel_->fillPar( par );
+    par.set( ProcessTime2Depth::sKeyOutputVolume(),
+	     outputsel_->ioobj(true)->key() );
+    par.set( ProcessTime2Depth::sKeyVelocityModel(),
+	     velsel_->ioobj(true)->key() );
+    par.setYN( ProcessTime2Depth::sKeyIsTimeToDepth(),
+	       directionsel_->getBoolValue() );
 
     return true;
 }
