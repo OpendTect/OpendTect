4e915e32f1ec2ecece0e12274b1bd88385d939a9
Ranojay Sen <ranojay.sen@dgbes.com>
2009-04-07 06:25:52 +0000

    mGlobal added
    git-svn-id: https://svn.opendtect.org/od/trunk@15330 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/zdomain.h
+++ include/Basic/zdomain.h
@@ -7,18 +7,18 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Nanne Hemstra & K. Tingdahl
  Date:		April 2009
- RCS:		$Id: zdomain.h,v 1.1 2009-04-04 16:23:04 cvsnanne Exp $
+ RCS:		$Id: zdomain.h,v 1.2 2009-04-07 06:25:52 cvsranojay Exp $
 ________________________________________________________________________
 
 -*/
 
 namespace ZDomain
 {
-    const char*		sKey()		{ return "ZDomain"; }
-    const char*		sKeyID()	{ return "ZDomain ID"; }
-    const char*		sKeyTWT()	{ return "TWT"; }
-    const char*		sKeyDepth()	{ return "Depth"; }
-    const char*		getDefault();
+    mGlobal const char*		sKey()			{ return "ZDomain"; }
+    mGlobal const char*		sKeyID()		{ return "ZDomain ID"; }
+    mGlobal const char*		sKeyTWT()		{ return "TWT"; }
+    mGlobal const char*		sKeyDepth()		{ return "Depth"; }
+    mGlobal const char*		getDefault();
 };
 
 
