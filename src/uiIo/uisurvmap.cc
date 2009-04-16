0c998cd9370ef0074ea24522934df2776ad40e24
Nanne Hemstra <nanne.hemstra@dgbes.com>
2009-04-16 05:25:37 +0000

    Enable imagesave
    git-svn-id: https://svn.opendtect.org/od/trunk@15401 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiIo/uisurvmap.cc
+++ src/uiIo/uisurvmap.cc
@@ -7,7 +7,7 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID = "$Id: uisurvmap.cc,v 1.24 2009-04-06 13:56:03 cvsnanne Exp $";
+static const char* rcsID = "$Id: uisurvmap.cc,v 1.25 2009-04-16 05:25:37 cvsnanne Exp $";
 
 #include "uisurvmap.h"
 
@@ -24,7 +24,7 @@ static const char* rcsID = "$Id: uisurvmap.cc,v 1.24 2009-04-06 13:56:03 cvsnann
 
 
 uiSurveyMap::uiSurveyMap( uiParent* parent )
-    : uiGraphicsView( parent, "Survey map view" )
+    : uiGraphicsView( parent, "Survey map view", true )
 {
     setScrollBarPolicy( true, uiGraphicsView::ScrollBarAlwaysOff );
     setScrollBarPolicy( false, uiGraphicsView::ScrollBarAlwaysOff );
