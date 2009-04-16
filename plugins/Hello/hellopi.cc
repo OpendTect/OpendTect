87fd8a5ad1a5b9e2151f0fa6bc83db27ecb85f0c
Ranojay Sen <ranojay.sen@dgbes.com>
2009-04-16 10:33:26 +0000

    mExternC added
    git-svn-id: https://svn.opendtect.org/od/trunk@15421 a985855d-d384-4b45-ae33-31a410998c3c

--- plugins/Hello/hellopi.cc
+++ plugins/Hello/hellopi.cc
@@ -4,11 +4,11 @@
  * DATE     : Sep 2003
 -*/
 
-static const char* rcsID = "$Id: hellopi.cc,v 1.5 2003-11-07 12:21:55 bert Exp $";
+static const char* rcsID = "$Id: hellopi.cc,v 1.6 2009-04-16 10:33:26 cvsranojay Exp $";
 
 #include <iostream>
 
-extern "C" const char* InitHelloPlugin( int, char** )
+extern "C"  __declspec( dllexport ) const char* InitHelloPlugin( int, char** )
 {
     std::cout << "Hello world" << std::endl;
     return 0; // All OK - no error messages
