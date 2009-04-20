cb3a838b01f1d9d6f54c0fac3c3ad63014aec87c
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2009-04-20 18:40:05 +0000

    Updated code-style and added includesRange function(s)
    git-svn-id: https://svn.opendtect.org/od/trunk@15472 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/selector.h
+++ include/Basic/selector.h
@@ -8,7 +8,7 @@ ________________________________________________________________________
  Author:	A.H.Bril
  Date:		18-10-1995
  Contents:	Selectors
- RCS:		$Id: selector.h,v 1.8 2009-02-13 13:31:14 cvsbert Exp $
+ RCS:		$Id: selector.h,v 1.9 2009-04-20 18:40:05 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -38,6 +38,13 @@ public:
     virtual Selector<T>* clone() const				= 0;
 
     virtual bool	includes(const T&) const		= 0;
+    virtual bool	canDoRange() const			{return false;}
+    virtual char	includesRange(const T& start,
+	    			      const T& stop) const	{ return -1; }
+    			/*!<\retval 0 not at all
+			    \retval 1 partly
+			    \retval 2 completely */
+
     virtual bool	include(const T&,const char* =0)       { return false; }
 
 private:
@@ -55,22 +62,25 @@ class SingleSelector : public Selector<T>
 public:
 
 			SingleSelector()			{}
-			SingleSelector( const T& t ) : val(t)	{}
+			SingleSelector( const T& t ) : val_(t)	{}
     virtual const char*	selectorType() const		{ return "Single"; }
     virtual Selector<T>* clone() const
-			{ return new SingleSelector( val ); }
+			{ return new SingleSelector( val_ ); }
 
     virtual bool	includes( const T& t ) const
-			{ return val == t; }
+			{ return val_ == t; }
+    virtual bool	canDoRange() const			{return true;}
+    virtual char	includesRange(const T& start,
+	    			      const T& stop) const;
     virtual bool	include( const T& t, const char* )
-			{ val = t; return true; }
+			{ val_ = t; return true; }
 
-    T			val;
+    T			val_;
 
 protected:
 
     virtual bool	isEq( const Selector<T>& ss ) const
-			{ return val == ((const SingleSelector<T>&)ss).val; }
+			{ return val_ == ((const SingleSelector<T>&)ss).val_; }
 
 };
 
@@ -84,22 +94,22 @@ public:
 
 			RangeSelector()			{}
 			RangeSelector( const T& t1, const T& t2 )
-			: range(t1,t2)			{}
+			: range_(t1,t2)			{}
     virtual const char*	selectorType() const		{ return "Range"; }
     virtual Selector<T>* clone() const
-			{ return new RangeSelector( range.start, range.stop ); }
+			{ return new RangeSelector(range_.start,range_.stop); }
 
     virtual bool	includes( const T& t ) const
-			{ return range.includes( t ); }
+			{ return range_.includes( t ); }
     virtual bool	include( const T& t, const char* )
-			{ range.include( t ); return true; }
+			{ range_.include( t ); return true; }
 
-    Interval<T>		range;
+    Interval<T>		range_;
 
 protected:
 
     virtual bool	isEq( const Selector<T>& rs ) const
-			{ return range == ((const RangeSelector<T>&)rs).range; }
+			{ return range_==((const RangeSelector<T>&)rs).range_; }
 
 };
 
@@ -112,28 +122,28 @@ class ArraySelector : public Selector<T>
 public:
 
 			ArraySelector()
-			: vals(0), sz(0), valsmine(true)	  {}
+			: vals_(0), sz_(0), valsmine_(true)	  {}
 			ArraySelector( const T* v, int s )
-			: vals(v), sz(s), valsmine(false)	  {}
+			: vals_(v), sz_(s), valsmine_(false)	  {}
 			ArraySelector( const ArraySelector& x )
-			: vals(x.vals), sz(x.sz), valsmine(false) {}
+			: vals_(x.vals_), sz_(x.sz_), valsmine_(false) {}
 			~ArraySelector()
-			{ if ( valsmine ) delete [] const_cast<T*>(vals); }
+			{ if ( valsmine_ ) delete [] const_cast<T*>(vals_); }
 
     virtual const char*	selectorType() const		{ return "Array"; }
     virtual Selector<T>* clone() const
-			{ return new ArraySelector( vals, sz ); }
+			{ return new ArraySelector( vals_, sz_ ); }
 
     virtual bool	includes( const T& t ) const
 			{
-			    for ( int idx=0; idx<sz; idx++ )
-				{ if ( vals[idx] == t ) return true; }
+			    for ( int idx=0; idx<sz_; idx++ )
+				{ if ( vals_[idx] == t ) return true; }
 			    return false;
 			}
-    void		manageVals( bool yn=true )	{ valsmine = yn; }
+    void		manageVals( bool yn=true )	{ valsmine_ = yn; }
 
-    const T*		vals;
-    int			sz;
+    const T*		vals_;
+    int			sz_;
 
 protected:
 
@@ -141,15 +151,28 @@ protected:
 			{
 			    const ArraySelector<T>& ass
 				= (const ArraySelector<T>&)ss;
-			    if ( sz != ass.sz ) return false;
-			    for ( int idx=0; idx<sz; idx++ )
-				{ if ( !ss.includes(vals[idx]) ) return false; }
+			    if ( sz_ != ass.sz_ ) return false;
+			    for ( int idx=0; idx<sz_; idx++ )
+				{ if ( !ss.includes(vals_[idx]) ) return false;}
 			    return true;
 			}
 
-    bool		valsmine;
+    bool		valsmine_;
 
 };
 
+
+template <class T> inline
+char SingleSelector<T>::includesRange( const T& start, const T& stop ) const
+{
+    const Interval<T> rg( start, stop );
+    if ( start==stop==val_ )
+	return 2;
+
+    if ( rg.includes( val_, true ) )
+	return 1;
+
+    return 0;
+}
 
 #endif
