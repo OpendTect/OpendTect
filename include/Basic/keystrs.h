c787cda7be834b8fd7e131ed8f197211cea9ea2a
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2009-05-05 02:21:07 +0000

    Replaced const char* with FixedString
    git-svn-id: https://svn.opendtect.org/od/trunk@15577 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/keystrs.h
+++ include/Basic/keystrs.h
@@ -8,23 +8,25 @@ ________________________________________________________________________
  Author:	A.H.Bril
  Date:		Mar 2002
 
- RCS:		$Id: keystrs.h,v 1.48 2009-04-06 07:25:31 cvsnanne Exp $
+ RCS:		$Id: keystrs.h,v 1.49 2009-05-05 02:21:07 cvskris Exp $
 ________________________________________________________________________
 
 -*/
  
  
 #include "gendefs.h"
+#include "fixedstring.h"
 
 #undef mImpl
 
 #ifdef KEYSTRS_IMPL
 # define mImpl(s) = s
+#define mExt 
 #else
 # define mImpl(s) /* empty */
+#define mExt mBasicExtern
 #endif
 
-#define mExt mBasicExtern
 
 /*!\brief is used for defining key strings that are 'global'.
 
@@ -37,90 +39,90 @@ to this namespace.
 namespace sKey
 {
 
-    mExt const char*	Attribute	mImpl("Attribute");
-    mExt const char*	Attributes	mImpl("Attributes");
-    mExt const char*	Azimuth		mImpl("Azimuth");
-    mExt const char*	Color		mImpl("Color");
-    mExt const char*	Cube		mImpl("Cube");
-    mExt const char*	DataType	mImpl("DataType");
-    mExt const char*	Depth		mImpl("Depth");
-    mExt const char*	Desc		mImpl("Description");
-    mExt const char*	Factor		mImpl("Factor");
-    mExt const char*	FileName	mImpl("File name");
-    mExt const char*	Filter		mImpl("Filter");
-    mExt const char*	FloatUdf	mImpl("1e30");
-    mExt const char*	Geometry	mImpl("Geometry");
-    mExt const char*	ID		mImpl("ID");
-    mExt const char*	IOSelection	mImpl("I/O Selection");
-    mExt const char*	Keys		mImpl("Keys");
-    mExt const char*	LineKey		mImpl("Line key");
-    mExt const char*	Log		mImpl("Log");
-    mExt const char*	Name		mImpl("Name");
-    mExt const char*	No		mImpl("No");
-    mExt const char*	None		mImpl("None");
-    mExt const char*	Offset		mImpl("Offset");
-    mExt const char*	Output		mImpl("Output");
-    mExt const char*	Pars		mImpl("Parameters");
-    mExt const char*	Polygon		mImpl("Polygon");
-    mExt const char*	Position	mImpl("Position");
-    mExt const char*	Random		mImpl("Random");
-    mExt const char*	Range		mImpl("Range");
-    mExt const char*	Scale		mImpl("Scale");
-    mExt const char*	Selection	mImpl("Selection");
-    mExt const char*	Subsample	mImpl("Subsample");
-    mExt const char*	Shortcuts	mImpl("Shortcuts");
-    mExt const char*	Size		mImpl("Size");
-    mExt const char*	Steering	mImpl("Steering");
-    mExt const char*	StratRef	mImpl("Strat Level");
-    mExt const char*	Subsel		mImpl("Subsel");
-    mExt const char*	Surface		mImpl("Surface");
-    mExt const char*	Survey		mImpl("Survey");
-    mExt const char*	Table		mImpl("Table");
-    mExt const char*	Target		mImpl("Target");
-    mExt const char*	Time		mImpl("Time");
-    mExt const char*	Title		mImpl("Title");
-    mExt const char*    TraceNr		mImpl("Trace number");
-    mExt const char*	Type		mImpl("Type");
-    mExt const char*	Undef		mImpl("Undefined");
-    mExt const char*	Value		mImpl("Value");
-    mExt const char*	Yes		mImpl("Yes");
+    mExt FixedString	Attribute 	mImpl("Attribute");
+    mExt FixedString	Attributes	mImpl("Attributes");
+    mExt FixedString	Azimuth		mImpl("Azimuth");
+    mExt FixedString	Color		mImpl("Color");
+    mExt FixedString	Cube		mImpl("Cube");
+    mExt FixedString	DataType	mImpl("DataType");
+    mExt FixedString	Depth		mImpl("Depth");
+    mExt FixedString	Desc		mImpl("Description");
+    mExt FixedString	Factor		mImpl("Factor");
+    mExt FixedString	FileName	mImpl("File name");
+    mExt FixedString	Filter		mImpl("Filter");
+    mExt FixedString	FloatUdf	mImpl("1e30");
+    mExt FixedString	Geometry	mImpl("Geometry");
+    mExt FixedString	ID		mImpl("ID");
+    mExt FixedString	IOSelection	mImpl("I/O Selection");
+    mExt FixedString	Keys		mImpl("Keys");
+    mExt FixedString	LineKey		mImpl("Line key");
+    mExt FixedString	Log		mImpl("Log");
+    mExt FixedString	Name		mImpl("Name");
+    mExt FixedString	No		mImpl("No");
+    mExt FixedString	None		mImpl("None");
+    mExt FixedString	Offset		mImpl("Offset");
+    mExt FixedString	Output		mImpl("Output");
+    mExt FixedString	Pars		mImpl("Parameters");
+    mExt FixedString	Polygon		mImpl("Polygon");
+    mExt FixedString	Position	mImpl("Position");
+    mExt FixedString	Random		mImpl("Random");
+    mExt FixedString	Range		mImpl("Range");
+    mExt FixedString	Scale		mImpl("Scale");
+    mExt FixedString	Selection	mImpl("Selection");
+    mExt FixedString	Subsample	mImpl("Subsample");
+    mExt FixedString	Shortcuts	mImpl("Shortcuts");
+    mExt FixedString	Size		mImpl("Size");
+    mExt FixedString	Steering	mImpl("Steering");
+    mExt FixedString	StratRef	mImpl("Strat Level");
+    mExt FixedString	Subsel		mImpl("Subsel");
+    mExt FixedString	Surface		mImpl("Surface");
+    mExt FixedString	Survey		mImpl("Survey");
+    mExt FixedString	Table		mImpl("Table");
+    mExt FixedString	Target		mImpl("Target");
+    mExt FixedString	Time		mImpl("Time");
+    mExt FixedString	Title		mImpl("Title");
+    mExt FixedString    TraceNr		mImpl("Trace number");
+    mExt FixedString	Type		mImpl("Type");
+    mExt FixedString	Undef		mImpl("Undefined");
+    mExt FixedString	Value		mImpl("Value");
+    mExt FixedString	Yes		mImpl("Yes");
 
-    mExt const char*	Average		mImpl("Average");
-    mExt const char*	Maximum		mImpl("Maximum");
-    mExt const char*	Median		mImpl("Median");
-    mExt const char*	Minimum		mImpl("Minimum");
-    mExt const char*	StdDev		mImpl("StdDev");
-    mExt const char*	Sum		mImpl("Sum");
+    mExt FixedString	Average		mImpl("Average");
+    mExt FixedString	Maximum		mImpl("Maximum");
+    mExt FixedString	Median		mImpl("Median");
+    mExt FixedString	Minimum		mImpl("Minimum");
+    mExt FixedString	StdDev		mImpl("StdDev");
+    mExt FixedString	Sum		mImpl("Sum");
 
-    mExt const char*	BinIDSel	mImpl("BinID selection");
-    mExt const char*	InlRange	mImpl("In-line range");
-    mExt const char*	FirstInl	mImpl("First In-line");
-    mExt const char*	LastInl		mImpl("Last In-line");
-    mExt const char*	StepInl		mImpl("Step In-line");
-    mExt const char*	StepOutInl	mImpl("Stepout In-line");
-    mExt const char*	CrlRange	mImpl("Cross-line range");
-    mExt const char*	FirstCrl	mImpl("First Cross-line");
-    mExt const char*	LastCrl		mImpl("Last Cross-line");
-    mExt const char*	StepCrl		mImpl("Step Cross-line");
-    mExt const char*	StepOutCrl	mImpl("Stepout Cross-line");
-    mExt const char*	ZRange		mImpl("Z range");
-    mExt const char*    FirstTrc	mImpl("First Trace");
-    mExt const char*	LastTrc		mImpl("Last Trace");
-    mExt const char*    TrcRange        mImpl("Trace Range");
+    mExt FixedString	BinIDSel	mImpl("BinID selection");
+    mExt FixedString	InlRange	mImpl("In-line range");
+    mExt FixedString	FirstInl	mImpl("First In-line");
+    mExt FixedString	LastInl		mImpl("Last In-line");
+    mExt FixedString	StepInl		mImpl("Step In-line");
+    mExt FixedString	StepOutInl	mImpl("Stepout In-line");
+    mExt FixedString	CrlRange	mImpl("Cross-line range");
+    mExt FixedString	FirstCrl	mImpl("First Cross-line");
+    mExt FixedString	LastCrl		mImpl("Last Cross-line");
+    mExt FixedString	StepCrl		mImpl("Step Cross-line");
+    mExt FixedString	StepOutCrl	mImpl("Stepout Cross-line");
+    mExt FixedString	ZRange		mImpl("Z range");
+    mExt FixedString    FirstTrc	mImpl("First Trace");
+    mExt FixedString	LastTrc		mImpl("Last Trace");
+    mExt FixedString    TrcRange        mImpl("Trace Range");
 
-    mExt const char*	TmpStor		mImpl("Temporary storage location");
-    mExt const char*	LogFile		mImpl("Log file");
-    mExt const char*	Version		mImpl("Version");
+    mExt FixedString	TmpStor		mImpl("Temporary storage location");
+    mExt FixedString	LogFile		mImpl("Log file");
+    mExt FixedString	Version		mImpl("Version");
 
 	
-    mExt const char*	Default		mImpl("Default");
-    mExt const char*	DefCube		mImpl("Default.Cube");
-    mExt const char*	DefLineSet	mImpl("Default.LineSet");
-    mExt const char*	DefLine		mImpl("Default.Line");
-    mExt const char*	DefAttribute	mImpl("Default.Attribute");
-    mExt const char*	DefPS3D		mImpl("Default.PS3D Data Store");
-    mExt const char*	DefPS2D		mImpl("Default.PS2D Data Store");
-    mExt const char*	DefWavelet	mImpl("Default.Wavelet");
+    mExt FixedString	Default		mImpl("Default");
+    mExt FixedString	DefCube		mImpl("Default.Cube");
+    mExt FixedString	DefLineSet	mImpl("Default.LineSet");
+    mExt FixedString	DefLine		mImpl("Default.Line");
+    mExt FixedString	DefAttribute	mImpl("Default.Attribute");
+    mExt FixedString	DefPS3D		mImpl("Default.PS3D Data Store");
+    mExt FixedString	DefPS2D		mImpl("Default.PS2D Data Store");
+    mExt FixedString	DefWavelet	mImpl("Default.Wavelet");
 
 };
 
