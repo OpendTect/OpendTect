c2e5117659dff3aba6ed168112ccd3a524050692
Bert Bril <bert.bril@dgbes.com>
2009-06-07 20:08:23 +0000

    Added subselect functions
    git-svn-id: https://svn.opendtect.org/od/trunk@15989 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Algo/statrand.h
+++ include/Algo/statrand.h
@@ -6,12 +6,13 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Bert Bril
  Date:          Sep 2006
- RCS:           $Id: statrand.h,v 1.2 2008-12-22 04:13:28 cvsranojay Exp $
+ RCS:           $Id: statrand.h,v 1.3 2009-06-07 20:08:23 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "gendefs.h"
+#include "odset.h"
 
 namespace Stats
 {
@@ -29,11 +30,45 @@ public:
     static int		getIndex(int sz);
     			//!< random index in the range [0,sz>
 
+    template <class T>
+    static void		subselect(T*,int sz,int targetsz);
+    			//!< Does not preserve order
+    static void		subselect(OD::Set&,int targetsz);
+    			//!< Does not preserve order
+
 private:
 
     static int		seed_;
 
 };
+
+
+template <class T>
+inline void Stats::RandGen::subselect( T* arr, int sz, int targetsz )
+{
+    for ( int idx=sz-1; idx>=targetsz; idx-- )
+    {
+	const int notselidx = getIndex( idx );
+	if ( notselidx != idx )
+	    Swap( arr[notselidx], arr[idx] );
+    }
+}
+
+
+inline void Stats::RandGen::subselect( OD::Set& ods, int targetsz )
+{
+    const int sz = ods.nrItems();
+    if ( sz <= targetsz ) return;
+
+    for ( int idx=sz-1; idx>=targetsz; idx-- )
+    {
+	const int notselidx = getIndex( idx );
+	if ( notselidx != idx )
+	    ods.swap( notselidx, idx );
+    }
+
+    ods.remove( targetsz, sz-1 );
+}
 
 }; // namespace Stats
 
