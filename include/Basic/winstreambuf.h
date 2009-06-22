0214b831f51333fa5c6942a16f638dcb55eb504f
Nanne Hemstra <nanne.hemstra@dgbes.com>
2009-06-22 10:05:57 +0000

    Added
    git-svn-id: https://svn.opendtect.org/od/trunk@16252 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Basic/winstreambuf.h
@@ -0,0 +1,94 @@
+#ifndef winstreambuf_h
+#define winstreambuf_h
+/*
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	Bert
+ Date:		Feb 2009
+ RCS:		$Id: winstreambuf.h,v 1.1 2009-06-22 10:05:57 cvsnanne Exp $
+________________________________________________________________________
+
+*/
+
+#define private protected
+#include <fstream>
+#undef private
+
+namespace std
+{
+
+/*!\brief Adapter to seek in filebuffers on win64.
+
+  Usage like:
+
+  std::winfilebuf fb;
+  std::istream strm( &fb );
+
+  After C:\Program Files (x86)\Microsoft Visual Studio 8\VC\include\fstream
+  Only change is: feek -> _fseeki64
+
+*/
+
+class winfilebuf : public filebuf
+{
+public:
+winfilebuf( const char* fnm, ios_base::openmode om )
+{
+    isok_ = open( fnm, om );
+}
+
+bool isOK() const	{ return isok_; }
+
+protected:
+
+#define defom (ios_base::openmode)(ios_base::in | ios_base::out)
+
+virtual pos_type seekoff( off_type _Off, ios_base::seekdir _Way,
+			  ios_base::openmode = defom )
+{
+    fpos_t _Fileposition;
+
+    if ( _Mysb::gptr() == &_Mychar && _Way == ios_base::cur && _Pcvt == 0 )
+	_Off -= (off_type)sizeof (char);
+
+    if ( _Myfile==0 || !_Endwrite() || (_Off!=0 || _Way!=ios_base::cur) &&
+	 _fseeki64(_Myfile,_Off,_Way) != 0 ||
+	 fgetpos(_Myfile,&_Fileposition) != 0 )
+	return (pos_type(_BADOFF));
+
+    if ( _Mysb::gptr() == &_Mychar )
+	_Mysb::setg( &_Mychar, &_Mychar+1, &_Mychar+1 );
+
+    return (_POS_TYPE_FROM_STATE(pos_type,_State,_Fileposition));
+}
+
+
+virtual pos_type seekpos( pos_type _Pos, ios_base::openmode = defom )
+
+{
+    fpos_t _Fileposition = _POS_TYPE_TO_FPOS_T(_Pos);
+    off_type _Off = (off_type)_Pos - _FPOSOFF(_Fileposition);
+
+    if ( _Myfile == 0 || !_Endwrite()
+	 || fsetpos(_Myfile, &_Fileposition) != 0
+	 || _Off != 0 && _fseeki64(_Myfile, _Off, SEEK_CUR) != 0
+	 || fgetpos(_Myfile, &_Fileposition) != 0)
+	return (pos_type(_BADOFF));
+
+    _State = _POS_TYPE_TO_STATE(_Pos);
+
+    if ( _Mysb::gptr() == &_Mychar)
+	 _Mysb::setg( &_Mychar, &_Mychar+1, &_Mychar+1 );
+
+    return (_POS_TYPE_FROM_STATE(pos_type,_State,_Fileposition));
+}
+
+bool	isok_;
+
+};
+
+} // namespace std
+
+
+#endif
