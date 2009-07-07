907b5e5aaf837d9b5470a7bc9d649a3a9636864d
cvsumesh <cvsumesh@a985855d-d384-4b45-ae33-31a410998c3c>
2009-07-07 09:01:11 +0000

    2D-Interpretation...
    git-svn-id: https://svn.opendtect.org/od/trunk@16465 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiMPE/horflatvieweditor.h
+++ include/uiMPE/horflatvieweditor.h
@@ -7,15 +7,19 @@ ________________________________________________________________________
  CopyRight:     (C) dGB Beheer B.V.
  Author:        Nanne Hemstra
  Date:          May 2009
- RCS:           $Id: horflatvieweditor.h,v 1.1 2009-06-22 12:37:10 cvsnanne Exp $
+ RCS:           $Id: horflatvieweditor.h,v 1.2 2009-07-07 09:01:11 cvsumesh Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "callback.h"
 #include "cubesampling.h"
+#include "multiid.h"
+#include "position.h"
 
 class MouseEventHandler;
+
+namespace Attrib { class SelSpec; }
 namespace EM { class Horizon3D; };
 namespace FlatView { class AuxDataEditor; }
 
@@ -29,8 +33,14 @@ public:
 				~HorizonFlatViewEditor();
 
     void			setCubeSampling(const CubeSampling&);
+    void			setSelSpec(const Attrib::SelSpec*,bool wva);
+    void			swapSelSpec();
+    void			set2D(bool is2d)	{ is2d_ = is2d; }
 
     FlatView::AuxDataEditor*	getEditor()	{ return editor_; }
+    void			setLineName(const char* lnm)							{ linenm_ = lnm; }
+    void			setLineSetID(const MultiID& lsetid)
+				{ lsetid_ = lsetid; }
     void			setMouseEventHandler(MouseEventHandler*);
 
 protected:
@@ -45,7 +55,13 @@ protected:
     MouseEventHandler*		mouseeventhandler_;
 
     CubeSampling		curcs_;
+    const Attrib::SelSpec* 	vdselspec_;
+    const Attrib::SelSpec*	wvaselspec_;
 
+    const char*			linenm_;
+    MultiID			lsetid_;
+
+    bool			is2d_;
 };
 
 } // namespace MPE
