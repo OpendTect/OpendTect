aab6c1803364857d349e19a8e4d879a34cd2255d
cvskarthika <cvskarthika@a985855d-d384-4b45-ae33-31a410998c3c>
2009-08-14 16:44:49 +0000

    Added beachball implementation and did some cleanup.
    git-svn-id: https://svn.opendtect.org/od/trunk@16879 a985855d-d384-4b45-ae33-31a410998c3c

--- plugins/Bouncy/bouncypi.cc
+++ plugins/Bouncy/bouncypi.cc
@@ -1,17 +1,15 @@
 /*+
  * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
- * AUTHOR   : A.H. Bril
- * DATE     : NOv 2003
+ * AUTHOR   : Karthika
+ * DATE     : Aug 2009
 -*/
 
-static const char* rcsID = "$Id: bouncypi.cc,v 1.1 2009-08-12 09:53:01 cvskarthika Exp $";
+static const char* rcsID = "$Id: bouncypi.cc,v 1.2 2009-08-14 16:44:49 cvskarthika Exp $";
 
-#include "bouncymain.h"
-#include "uimenu.h"
 #include "uimsg.h"
-#include "uiodmenumgr.h"
-
+#include "uiodmain.h"
 #include "plugins.h"
+#include "bouncymgr.h"
 
 
 mExternC mGlobal int GetBouncyPluginType()
@@ -31,36 +29,10 @@ mExternC mGlobal PluginInfo* GetBouncyPluginInfo()
 }
 
 
-class BouncyMgr :  public CallBacker
-{
-public:
-			BouncyMgr(uiODMain*);
-
-    uiODMain*		appl_;
-
-    void		doWork(CallBacker*);
-};
-
-
-BouncyMgr::BouncyMgr( uiODMain* a )
-	: appl_(a)
-{
-    uiODMenuMgr& mnumgr = appl_->menuMgr();
-    mnumgr.toolsMnu()->insertItem(
-	    new uiMenuItem("&Bouncy",mCB(this,BouncyMgr,doWork)) );
-}
-
-
-void BouncyMgr::doWork( CallBacker* cb )
-{
-    BouncyMain bm( appl_ );
-    bm.go();
-}
-
-
 mExternC mGlobal const char* InitBouncyPlugin( int, char** )
 {
-    static BouncyMgr* mgr = 0; if ( mgr ) return 0;
+    static BouncyMgr* mgr = 0; 
+    if ( mgr ) return 0;
     mgr = new BouncyMgr( ODMainWin() );
     return 0;
 }
