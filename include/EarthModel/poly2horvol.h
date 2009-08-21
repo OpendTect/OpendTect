274e545d802703d84b7f48cbf06041cba19ae621
Bert Bril <bert.bril@dgbes.com>
2009-08-21 13:43:57 +0000

    created Poly2HorVol class
    git-svn-id: https://svn.opendtect.org/od/trunk@16949 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/EarthModel/poly2horvol.h
@@ -0,0 +1,50 @@
+#ifndef poly2horvol_h
+#define poly2horvol_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Bert
+ Date:          Aug 2008
+ RCS:           $Id: poly2horvol.h,v 1.1 2009-08-21 13:43:57 cvsbert Exp $
+________________________________________________________________________
+
+-*/
+
+#include "gendefs.h"
+class MultiID;
+class TaskRunner;
+namespace Pick	{ class Set; }
+namespace EM	{ class Horizon3D; }
+
+
+/*! \brief Calculate volume between horizon and polygon */
+
+mClass Poly2HorVol
+{
+public:
+
+			Poly2HorVol( const Pick::Set* ps=0, EM::Horizon3D* h=0 )
+			    : ps_(ps), hor_(0)	{ setHorizon(h); }
+			~Poly2HorVol();
+
+    const Pick::Set*	pickSet() const		{ return ps_; }
+    EM::Horizon3D*	horizon()		{ return hor_; }
+    void		setPickSet( const Pick::Set* ps )	{ ps_ = ps; }
+    void		setHorizon(EM::Horizon3D*);
+
+    bool		setHorizon(const MultiID&,TaskRunner* tr=0);
+
+    float		getM3(float,bool upward,bool usenegvals);
+    const char*		dispText(float m3,bool zinft);
+
+protected:
+
+    const Pick::Set*	ps_;
+    EM::Horizon3D*	hor_;
+
+};
+
+
+#endif
---
+++ src/EarthModel/poly2horvol.cc
@@ -0,0 +1,156 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Bert
+ Date:          Aug 2008
+________________________________________________________________________
+
+-*/
+
+static const char* rcsID = "$Id: poly2horvol.cc,v 1.1 2009-08-21 13:43:57 cvsbert Exp $";
+
+#include "poly2horvol.h"
+
+#include "emmanager.h"
+#include "emhorizon3d.h"
+#include "emsurfacetr.h"
+#include "executor.h"
+#include "pickset.h"
+#include "survinfo.h"
+#include "polygon.h"
+#include "gridder2d.h"
+
+#include <math.h>
+
+
+Poly2HorVol::~Poly2HorVol()
+{
+    if ( hor_ )
+	hor_->unRef();
+}
+
+
+void Poly2HorVol::setHorizon( EM::Horizon3D* hor )
+{
+    if ( hor_ )
+	{ hor_->unRef(); hor_ = 0; }
+    hor_ = hor;
+    if ( hor_ )
+	hor_->ref();
+}
+
+
+bool Poly2HorVol::setHorizon( const MultiID& mid, TaskRunner* tr )
+{
+    if ( hor_ )
+	{ hor_->unRef(); hor_ = 0; }
+
+    EM::EMObject* emobj = EM::EMM().loadIfNotFullyLoaded( mid, tr );
+    mDynamicCastGet(EM::Horizon3D*,hor,emobj)
+
+    setHorizon( hor );
+    return hor_;
+}
+
+
+#define mPolyLoc(b) \
+	Geom::Point2D<float>( (float)b.inl, (float)(b.crl) )
+
+float Poly2HorVol::getM3( float vel, bool upw, bool useneg )
+{
+    if ( !hor_ || ! ps_ )
+	return mUdf(float);
+
+    ODPolygon<float> poly;
+    HorSampling hs;
+    const Pick::Location& pl0( (*ps_)[0] );
+    TypeSet<Coord> pts; TypeSet<float> zvals;
+    for ( int idx=0; idx<ps_->size(); idx++ )
+    {
+	const Pick::Location& pl( (*ps_)[idx] );
+	pts += pl.pos; zvals += pl.pos.z;
+	const BinID bid( SI().transform(pl.pos) );
+	poly.add( mPolyLoc(bid) );
+	if ( idx )
+	    hs.include( bid );
+	else
+	    hs.start = hs.stop = bid;
+    }
+
+    TriangulatedGridder2D grdr;
+    grdr.setPoints( pts ); grdr.setValues( zvals, false );
+    float avgz = 0;
+    for ( int idx=0; idx<zvals.size(); idx++ )
+	avgz += zvals[idx];
+    avgz /= zvals.size();
+
+    const int nrsect = hor_->nrSections();
+    HorSamplingIterator iter( hs );
+    BinID bid; float totth = 0;
+    while ( iter.next(bid) )
+    {
+	if ( !poly.isInside(mPolyLoc(bid),true,1e-6) )
+	    continue;
+
+	const EM::SubID subid = bid.getSerialized();
+	const Coord coord( SI().transform(bid) );
+
+	for ( int isect=0; isect<nrsect; isect++ )
+	{
+	    const float horz = hor_->getPos( isect, subid ).z;
+	    if ( mIsUdf(horz) )
+		continue;
+
+	    float polyz = avgz;
+	    bool useavgz = true;
+	    useavgz = !grdr.setGridPoint(coord) || !grdr.init();
+	    if ( useavgz )
+		polyz = avgz;
+	    else
+	    {
+		polyz = grdr.getValue();
+		if ( mIsUdf(polyz) )
+		    polyz = avgz;
+	    }
+
+	    const float th = upw ? polyz - horz : horz - polyz;
+	    if ( useneg || th > 0 )
+		{ totth += th; break; }
+	}
+    }
+
+    const float cellarea = SI().inlDistance() * hs.step.inl
+			 * SI().crlDistance() * hs.step.crl;
+    const float v = SI().zIsTime() ? vel * .5 : 1; // TWT
+    return cellarea * v * totth;
+}
+
+
+const char* Poly2HorVol::dispText( float m3, bool zinft )
+{
+    static const float bblconv = 6.2898108;
+    static const float ft3conv = 35.314667;
+
+    if ( mIsUdf(m3) )
+	return "";
+
+    float dispval = m3;
+    if ( zinft ) dispval *= ft3conv;
+    bool mega = false;
+    if ( fabs(dispval) > 1e6 )
+	{ mega = true; dispval /= 1e6; }
+
+    static BufferString txt;
+    txt = dispval; txt += mega ? "M " : " ";
+    txt += zinft ? "ft^3" : "m^3";
+    txt += " (";
+    dispval *= bblconv;
+    if ( zinft ) dispval /= ft3conv;
+    if ( dispval > 1e6 )
+	{ mega = true; dispval /= 1e6; }
+    txt += dispval; if ( mega ) txt += "M";
+    txt += " bbl)";
+
+    return txt.buf();
+}
