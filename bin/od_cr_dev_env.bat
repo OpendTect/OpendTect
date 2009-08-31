8bc5c81db92b8b7c2c0217d01b0fd1e65181fa09
Nanne Hemstra <nanne.hemstra@dgbes.com>
2009-08-31 05:47:33 +0000

    Added
    git-svn-id: https://svn.opendtect.org/od/trunk@17051 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ bin/od_cr_dev_env.bat
@@ -0,0 +1,34 @@
+cls
+@echo off
+
+set inpdir=%1
+set outdir=%2
+
+if not exist %inpdir% goto MSGINPDIR
+if not exist %inpdir%\.rel.devel goto MSGPKG
+if not exist %outdir% goto MSGOUTDIR
+
+
+FOR %%a IN ( plugins, src, include, Pmake, spec, msvc ) DO md %outdir%\%%a
+FOR %%a IN ( plugins, src, include, Pmake, spec, msvc ) DO xcopy /s %inpdir%\%%a\* %outdir%\%%a
+
+copy %inpdir%\.rel.devel %outdir%
+goto END
+
+
+:MSGINPDIR
+echo %inpdir% does not exist
+goto END
+
+:MSGOUTDIR
+echo %outdir% does not exist
+goto END
+
+:MSGPKG
+echo warning : %inpdir% does not have the development package installed
+goto END
+
+
+:END
+echo Done
+pause
