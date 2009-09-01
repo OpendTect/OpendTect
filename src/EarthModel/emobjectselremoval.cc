8415ab64f8da67e6e7b0ed0ee405c92ed646b95e
cvsumesh <cvsumesh@a985855d-d384-4b45-ae33-31a410998c3c>
2009-09-01 05:08:01 +0000

    third suggestion by Kris...
    git-svn-id: https://svn.opendtect.org/od/trunk@17069 a985855d-d384-4b45-ae33-31a410998c3c

--- src/EarthModel/emobjectselremoval.cc
+++ src/EarthModel/emobjectselremoval.cc
@@ -5,14 +5,15 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Umesh Sinha
  Date:		May 2008
- RCS:		$Id: emobjectselremoval.cc,v 1.5 2009-08-25 08:56:13 cvsumesh Exp $
+ RCS:		$Id: emobjectselremoval.cc,v 1.6 2009-09-01 05:08:01 cvsumesh Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "emobjectselremoval.h"
-#include "parametricsurface.h"
 #include "emobject.h"
+#include "parametricsurface.h"
+#include "survinfo.h"
 
 namespace EM
 {
@@ -82,16 +83,45 @@ bool EMObjectSelectionRemoval::doWork( od_int64 start, od_int64 stop,
 		startcol_ + colstep * (((boxcol+1)==nrhorgroupboxes) ?
 		nrcols_ - 1 : (boxcol+1)*16), colstep );
 
+	TypeSet<EM::SubID> ids;
+	
+	Coord3 startcord, stopcord;
+	startcord.z = SI().zRange(true).start;
+	stopcord.z = SI().zRange(true).stop;
+
+	Coord coord0 = SI().transform( BinID(rowrg.start,colrg.start) );
+	startcord.x = stopcord.x = coord0.x;
+	startcord.y = stopcord.y = coord0.y;
+
+	Coord coord1 = SI().transform( BinID(rowrg.start,colrg.stop) );
+	if ( startcord.x > coord1.x ) startcord.x = coord1.x;
+	if ( startcord.y > coord1.y ) startcord.y = coord1.y;
+	if ( coord1.x > stopcord.x ) stopcord.x = coord1.x;
+	if ( coord1.y > stopcord.y ) stopcord.y = coord1.y;
+
+	Coord coord2 = SI().transform( BinID(rowrg.stop,colrg.start) );
+	if ( startcord.x > coord2.x ) startcord.x = coord2.x;
+	if ( startcord.y > coord2.y ) startcord.y = coord2.y;
+	if ( coord2.x > stopcord.x ) stopcord.x = coord2.x;
+	if ( coord2.y > stopcord.y ) stopcord.y = coord2.y;
+
+	Coord coord3 = SI().transform( BinID(rowrg.stop,colrg.stop) );
+	if ( startcord.x > coord3.x ) startcord.x = coord3.x;
+	if ( startcord.y > coord3.y ) startcord.y = coord3.y;
+	if ( coord3.x > stopcord.x ) stopcord.x = coord3.x;
+	if ( coord3.y > stopcord.y ) stopcord.y = coord3.y;
+
+	int sel = selector_.canDoRange()
+	    ? selector_.includesRange( startcord, stopcord )
+	    : 1;
+
+	if ( !sel ) continue; // all outside
+
 	HorSampling horsampling( true );
 	horsampling.set( rowrg, colrg );
 
-	TypeSet<EM::SubID> ids;
-	TypeSet<Coord3> positions;
-	Coord3 startcord, stopcord;
-
 	HorSamplingIterator iter( horsampling );
 	BinID bid;
-	int count = 0;
 	while ( iter.next(bid) )
 	{
 	    const Coord3 examcor = emobj_.getPos( sectionid_, 
@@ -99,39 +129,10 @@ bool EMObjectSelectionRemoval::doWork( od_int64 start, od_int64 stop,
 	    if ( !examcor.isDefined() )
 		continue;
 
-	    if ( !count )
-	    {
-		startcord = stopcord = examcor;
-		count++;
+	    if ( sel == 1 && !selector_.includes(examcor) )
 		continue;
-	    }
 
-	    positions += examcor;
 	    ids += bid.getSerialized();
-	    if ( examcor.x < startcord.x ) startcord.x = examcor.x;
-	    if ( examcor.y < startcord.x ) startcord.y = examcor.y;
-	    if ( examcor.z < startcord.z ) startcord.z = examcor.z;
-	    if ( examcor.x > stopcord.x ) stopcord.x = examcor.x;
-	    if ( examcor.y > stopcord.y ) stopcord.y = examcor.y;
-	    if ( examcor.z > stopcord.z ) stopcord.z = examcor.z;
-	}
-
-	int sel = selector_.canDoRange() 
-	    ? selector_.includesRange( startcord, stopcord )
-	    : 1 ;
-
-	if ( !sel ) continue; // all outside
-
-	if ( sel == 1 )
-	{
-	    for ( int idy=ids.size()-1; idy>=0; idy-- )
-	    {
-		if ( !selector_.includes(positions[idy]) )
-		{
-		    ids.remove( idy );
-		    positions.remove( idy );
-		}
-	    }
 	}
 
 	localremovelist.append( ids );
