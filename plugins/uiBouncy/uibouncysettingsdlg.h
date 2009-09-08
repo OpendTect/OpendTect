3e6713751f025be5211f1ae6f7bca0099476b80e
cvskarthika <cvskarthika@a985855d-d384-4b45-ae33-31a410998c3c>
2009-09-08 09:03:07 +0000

    Dialog to edit the settings of the beachball.
    git-svn-id: https://svn.opendtect.org/od/trunk@17172 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ plugins/uiBouncy/uibouncysettingsdlg.h
@@ -0,0 +1,53 @@
+#ifndef uibouncysettingsdlg_h
+#define uibouncysettingsdlg_h
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : Karthika
+ * DATE     : Sep 2009
+ * ID       : $Id: uibouncysettingsdlg.h,v 1.1 2009-09-08 09:03:07 cvskarthika Exp $
+-*/
+
+#include "uigroup.h"
+
+class uiGenInput;
+class uiColorInput;
+
+namespace visBeachBall { class BallProperties; }
+
+namespace uiBouncy
+{
+
+class uiBouncySettingsDlg : public uiGroup
+{
+public:
+
+				uiBouncySettingsDlg(uiParent*,
+					visBeachBall::BallProperties* = 0);
+				~uiBouncySettingsDlg();
+
+    visBeachBall::BallProperties getBallProperties() const;
+    void			setBallProperties( 
+				    const visBeachBall::BallProperties&);
+    bool			isOK();
+    Notifier<uiBouncySettingsDlg> propertyChanged;
+
+protected:
+
+    uiGenInput*			radiusfld_;
+    uiColorInput*		color1sel_;
+    uiColorInput*		color2sel_;
+    uiGenInput*         	inlfld_;
+    uiGenInput*         	crlfld_;
+    uiGenInput*         	xfld_;
+    uiGenInput*         	yfld_;
+
+    void			changeCB(CallBacker*);
+    void			inl_crlChangedCB(CallBacker*);
+    void			x_yChangedCB(CallBacker*);
+    bool			binIDOK();
+
+};
+
+};
+
+#endif
