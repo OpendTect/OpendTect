3aa722a382c9ea60cc698a4d55640a2d97ec0ced
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2009-09-23 19:27:22 +0000

    Renamed geotesselator to horizon3dtesselator
    git-svn-id: https://svn.opendtect.org/od/trunk@17373 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Geometry/horizon3dtesselator.h
@@ -0,0 +1,71 @@
+#ifndef horizon3dtesselateor_h
+#define horizon3dtesselateor_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Yuancheng Liu
+ Date:		August 2009
+ RCS:		$Id: horizon3dtesselator.h,v 1.1 2009-09-23 19:27:22 cvskris Exp $
+________________________________________________________________________
+
+
+-*/
+
+#include "position.h"
+#include "task.h"
+
+class Coord3List;
+
+
+/* Assume given Coord3List has size nrrows*nrcols and all the points are added 
+   in  order of rows. Your spacing should be 1, 2, 4, 8 ,16, or 32. The normals
+   and their indices are optional, you could put dummy 0s for them. The normals
+   are calculated based on the spacing and they are local. The normal indices
+   are based on my own normals, hence normstartidx is provided in case you 
+   want to arrange your global indices based on your spacing or whatever. */
+
+class Horizon3DTesselator : public SequentialTask
+{
+public:
+    			Horizon3DTesselator(const Coord3List*,
+				int nrcoordcol,
+				unsigned char spacing,
+				int nrrow,int nrcol,
+				TypeSet<int>* pointci,
+				TypeSet<int>* lineci,
+				TypeSet<int>* stripci,
+				TypeSet<int>* pointni,
+				TypeSet<int>* lineni,
+				TypeSet<int>* stripni,
+				Coord3List* normals,
+				int nrnormalcols_,
+				int normstartidx);
+    int			nextStep();
+
+protected:
+    
+    void		computeNormal(int ni,int row,int col);
+    int			getNormalIdx(int crdidx);
+    
+    const Coord3List*	coords_;
+    unsigned char	spacing_;
+    int			nrcols_, nrrows_, nrcoordcols_;
+    int			nrnormalcols_;
+
+    TypeSet<int>*	pointci_;
+    TypeSet<int>*	lineci_;
+    TypeSet<int>*	stripci_;
+    TypeSet<int>*	pointni_;
+    TypeSet<int>*	lineni_;
+    TypeSet<int>*	stripni_;
+    Coord3List*		normals_;
+    int			normalstart_;
+
+    const double	cosanglexinl_, sinanglexinl_;
+};
+
+
+
+#endif
---
+++ src/Geometry/horizon3dtesselator.cc
@@ -0,0 +1,298 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : Yuancheng Liu
+ * DATE     : August 2009
+-*/
+
+static const char* rcsID = "$Id: horizon3dtesselator.cc,v 1.1 2009-09-23 19:27:22 cvskris Exp $";
+
+#include "horizon3dtesselator.h"
+
+#include "survinfo.h"
+#include "positionlist.h"
+#include "simpnumer.h"
+
+
+
+Horizon3DTesselator::Horizon3DTesselator( const Coord3List* coords,
+	int nrcoordcols, unsigned char spacing,
+	int nrrows,int nrcols, 
+	TypeSet<int>* pointci, TypeSet<int>* lineci, TypeSet<int>* stripci, 
+	TypeSet<int>* pointni, TypeSet<int>* lineni, TypeSet<int>* stripni,
+        Coord3List* normals, int nrnormalcols, int normstartidx )
+    : coords_( coords )
+    , nrrows_( nrrows )
+    , nrcols_( nrcols )  
+    , normalstart_( normstartidx )			 
+    , spacing_( spacing )
+    , pointci_( pointci )
+    , lineci_( lineci )
+    , stripci_( stripci )
+    , pointni_( pointni )
+    , lineni_( lineni )
+    , stripni_( stripni )
+    , normals_( normals )			 
+    , nrnormalcols_( nrnormalcols )
+    , cosanglexinl_( cos(SI().computeAngleXInl()) )
+    , sinanglexinl_( sin(SI().computeAngleXInl()) )		     
+    , nrcoordcols_( nrcoordcols )
+{}
+
+#define mStrip 3
+#define mLine 2
+#define mPoint 1
+
+#define mAddPosInitialTriangle( ci0, ci1, ci2 ) \
+{ \
+    isstripterminated =  false; \
+    mAddPosIndex( ci0, mStrip ); \
+    mAddPosIndex( ci1, mStrip ); \
+    mAddPosIndex( ci2, mStrip ); \
+}
+
+
+#define mTerminatePosStrip \
+if ( !isstripterminated ) \
+{ \
+    isstripterminated = true; \
+    mAddPosIndex( -1, mStrip ); \
+}
+
+
+#define mAddPosIndex( ci, obj ) \
+{ \
+    if ( obj==mStrip ) \
+    { \
+	if ( stripci_ )	(*stripci_) += ci; \
+	if ( stripni_ ) (*stripni_) += getNormalIdx(ci); \
+    } \
+    else if ( obj==mLine ) \
+    { \
+	if ( lineci_ ) (*lineci_) += ci; \
+	if ( lineni_ ) (*lineni_) += getNormalIdx(ci); \
+    } \
+    else if ( obj==mPoint ) \
+    { \
+	if ( pointci_ )	(*pointci_) += ci; \
+	if ( pointni_ ) (*pointni_) += getNormalIdx(ci); \
+    } \
+}
+
+#define m00 0
+#define m01 1
+#define m02 2
+#define m10 3
+#define m11 4
+#define m12 5
+#define m20 6
+#define m21 7
+#define m22 8
+
+int Horizon3DTesselator::nextStep()
+{
+    if ( !coords_ || !spacing_ )
+    	return SequentialTask::Finished();
+
+    const int lastrowidx = (nrrows_-1)/spacing_;
+    const int lastcolidx = (nrcols_-1)/spacing_;
+
+    for ( int ridx=0; ridx<=lastrowidx; ridx++ )
+    {
+	int ci11 = ridx*spacing_*nrcoordcols_;
+	int ci21 = ci11 + spacing_*nrcoordcols_;
+
+	bool nbdef[] = { false, false, false,		// 00   01     02
+			 false, false, false,		// 10   11(me) 12
+			 false, false, false };		// 20   21     22
+
+	nbdef[m11] = coords_->isDefined(ci11);
+	nbdef[m21] = ridx!=lastrowidx ? coords_->isDefined(ci21) : false;
+	if ( ridx )
+	{
+	    int ci01 = ci11 - spacing_*nrcoordcols_;
+	    nbdef[m01] = coords_->isDefined(ci01);
+	}
+
+	bool isstripterminated = true;
+	for ( int cidx=0; cidx<=lastcolidx; cidx++ )
+	{
+	    const int ci12 = ci11 + spacing_;
+	    const int ci22 = ci21 + spacing_;
+	    
+	    nbdef[m12] = cidx!=lastcolidx ? coords_->isDefined(ci12) : false;
+	    nbdef[m22] = (cidx==lastcolidx || ridx==lastrowidx) ? 
+		false : coords_->isDefined(ci22);
+	    
+	    int ci02 = ci12 - spacing_*nrcoordcols_;
+	    nbdef[m02] = ridx ? coords_->isDefined(ci02) : false;
+
+	    const int defsum = nbdef[m11]+nbdef[m12]+nbdef[m21]+nbdef[m22];
+	    if ( defsum<3 ) 
+	    {
+		mTerminatePosStrip;
+		if ( ridx<lastrowidx && cidx<lastcolidx && nbdef[m11] )
+		{
+		    const bool con12 = nbdef[m12] &&
+			(!ridx || (!nbdef[m01] && !nbdef[m02]) );
+		    const bool con21 = nbdef[m21] &&
+			(!cidx || (!nbdef[m10] && !nbdef[m20]) );
+
+		    if ( con12 || con21 )
+		    {
+			const int lastci = lineci_ && lineci_->size()
+			    ? (*lineci_)[lineci_->size()-1] : -1;
+
+			if ( lastci!=ci11 )
+			{
+			    if ( lastci!=-1 )
+				mAddPosIndex( -1, mLine );
+
+			    mAddPosIndex( ci11, mLine );
+			}
+
+			mAddPosIndex( con12 ? ci12 : ci21, mLine );
+		    }
+		}
+		else if ( nbdef[m11] && !nbdef[m10] && !nbdef[m12] && 
+			 !nbdef[m01] && !nbdef[m21] )
+		{
+		    mAddPosIndex( ci11, mPoint ) 
+		} 
+	    }
+	    else if ( defsum==3 )
+	    {
+		mTerminatePosStrip;
+		if ( !nbdef[m11] )
+		    mAddPosInitialTriangle( ci12, ci21, ci22 )
+		else if ( !nbdef[m21] )
+		    mAddPosInitialTriangle( ci11, ci22, ci12 )
+		else if ( !nbdef[m12] )
+		    mAddPosInitialTriangle( ci11, ci21, ci22 )
+		else
+		    mAddPosInitialTriangle( ci11, ci21, ci12 )
+		mTerminatePosStrip;
+	    }
+	    else
+	    {
+		const float diff0 = coords_->get(ci11).z-
+				    coords_->get(ci22).z;
+		const float diff1 = coords_->get(ci12).z-
+				    coords_->get(ci21).z;
+
+		const bool do11to22 = fabs(diff0) < fabs(diff1);
+		if ( do11to22 )
+		{
+		    mTerminatePosStrip;
+		    mAddPosInitialTriangle( ci21, ci22, ci11 );
+		    mAddPosIndex( ci12, mStrip );
+		    mTerminatePosStrip;
+		}
+		else
+		{
+		    if ( isstripterminated )
+		    {
+			mAddPosInitialTriangle( ci11, ci21, ci12 );
+			mAddPosIndex( ci22, mStrip );
+		    }
+		    else
+		    {
+			mAddPosIndex( ci12, mStrip );
+			mAddPosIndex( ci22, mStrip );
+		    }
+		}
+	    } 
+	
+	    nbdef[m00] = nbdef[m01]; nbdef[m01] = nbdef[m02];
+	    nbdef[m10] = nbdef[m11]; nbdef[m11] = nbdef[m12];
+	    nbdef[m20] = nbdef[m21]; nbdef[m21] = nbdef[m22];
+	    ci11 = ci12; ci21 = ci22;
+	}
+
+	mTerminatePosStrip;
+    }
+    	
+    return SequentialTask::Finished();;
+}
+
+
+void Horizon3DTesselator::computeNormal( int ni, int row, int col )
+{
+    if ( !normals_ )
+	return;
+
+    TypeSet<float> posarray, zarray;
+    for ( int idx=-spacing_; idx<=spacing_; idx++ )
+    {
+	const int currow = row+idx;
+	if ( currow<0 )
+	{
+	    idx += -currow-1;
+	    continue;
+	}
+
+	if ( currow>nrrows_ )
+	    break;
+
+	const Coord3 pos = coords_->get(currow*nrcoordcols_+col);
+	if ( pos.isDefined() )
+	{
+	    posarray += idx*SI().inlDistance();
+	    zarray += pos.z;
+	}
+    }
+	   
+    double drow = 0;
+    if ( zarray.size()>1 )
+	getGradient( posarray.arr(), zarray.arr(), zarray.size(), 0, 0, &drow );
+
+    posarray.erase(); zarray.erase();    
+    for ( int idx=-spacing_; idx<=spacing_; idx++ )
+    {
+	const int curcol = col+idx;
+	if ( curcol<0 )
+	{
+	    idx += -curcol-1;
+	    continue;
+	}
+
+	if ( curcol>nrcols_ )
+	    break;
+	
+	const Coord3 pos = coords_->get(row*nrcoordcols_+curcol);
+	if ( pos.isDefined() )
+	{
+	    posarray += idx*SI().crlDistance();
+	    zarray += pos.z;
+	}
+    }
+
+    double dcol = 0;
+    if ( zarray.size()>1 )
+	getGradient( posarray.arr(), zarray.arr(), zarray.size(), 0, 0, &dcol );
+
+   normals_->set( ni, Coord3( drow*cosanglexinl_+dcol*sinanglexinl_,
+	    	      dcol*cosanglexinl_-drow*sinanglexinl_, -1 ) );
+}
+
+
+int Horizon3DTesselator::getNormalIdx( int crdidx )
+{
+    if ( crdidx<0 )
+	return -1;
+
+    const int row = crdidx/nrcoordcols_;
+    const int col = crdidx%nrcoordcols_;
+
+    if ( !(row%spacing_) && !(col%spacing_) )
+    {
+	const int resrow = row/spacing_;
+	const int rescol = col/spacing_;
+	const int ni = resrow*nrnormalcols_ + rescol + normalstart_;
+	if ( normals_ && !normals_->isDefined(ni) )
+    	    computeNormal( ni, row, col );
+
+	return ni;
+    }
+
+    return -1;
+}
