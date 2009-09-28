2051b1a159c31081c6359d3abde2a9b2aadf9d41
Nanne Hemstra <nanne.hemstra@dgbes.com>
2009-09-28 02:42:08 +0000

    Changed, will be used when uiDialog uses QDialog again
    git-svn-id: https://svn.opendtect.org/od/trunk@17396 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiBase/i_qdialog.h
+++ include/uiBase/i_qdialog.h
@@ -7,77 +7,52 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        A.H. Lammertink
  Date:          25/05/2000
- RCS:           $Id: i_qdialog.h,v 1.5 2009-07-22 16:01:20 cvsbert Exp $
+ RCS:           $Id: i_qdialog.h,v 1.6 2009-09-28 02:42:08 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uidialog.h"
-#include "i_qobjwrap.h"
 
-#include <qobject.h>
-#include <qwidget.h>
-#include <qdialog.h> 
+#include <QObject>
+#include <QDialog> 
 
 
-mTemplTypeDefT( i_QObjWrapper, QDialog, ii_QDialog )
-
-class i_QDialog : public ii_QDialog
-{
-//    Q_OBJECT
-public:
-                        i_QDialog(uiDialog& handle, uiParent* parnt, 
-                                  const char* nm, bool modal )
-                        : ii_QDialog( handle, parnt, nm, modal )
-                        , mClient( &handle ) {}
-
-//protected slots:
-protected:
-    virtual void done( int i ) { if( mClient.doneOK(i)) ii_QDialog::done(i); }
-    virtual void accept(){ if( mClient.acceptOK()) ii_QDialog::accept(); }
-    virtual void reject(){ if( mClient.rejectOK()) ii_QDialog::reject(); }
-
-protected:
-    uiDialog*		mClient;
-};
-
-//! Helper class for uiDialog to relay Qt's 'activated' messages to uiMenuItem.
-/*!
-    Internal object, to hide Qt's signal/slot mechanism.
-*/
-class i_dialogMessenger : public QObject 
+class QDialogMessenger : public QObject 
 {
     Q_OBJECT
     friend class	uiDialog;
 
 protected:
-			i_dialogMessenger( QDialog* receiver,
-					   uiDialog*  sender  )
-			: _sender( sender )
-			, _receiver( receiver )
-			{ 
-			    connect( this    , SIGNAL( accept() ),
-				     receiver,   SLOT( accept() ) );
-			    connect( this    , SIGNAL( reject() ),
-				     receiver,   SLOT( reject() ) );
-			}
 
-    virtual		~i_dialogMessenger() {}
-  
-signals:
+QDialogMessenger( QDialog* qdlg, uiDialog* uidlg )
+    : qdialog_(qdlg)
+    , uidialog_(uidlg)
+{
+   connect( qdialog_, SIGNAL(accepted()), this, SLOT(accepted()) );
+   connect( qdialog_, SIGNAL(rejected()), this, SLOT(rejected()) );
+   connect( qdialog_, SIGNAL(finished(int)), this, SLOT(finished(int)) );
+}
 
-/*!
-    \brief signals to be relayed to QDialog. Triggerd by callback functions
-    \sa QDialog::accept()
-*/
-    void 		accept();
-    void 		reject();
+
+private slots:
+
+void accepted()
+{ // Implement when uiDialog uses QDialog again
+}
+
+void rejected()
+{ // Implement when uiDialog uses QDialog again
+}
+
+void finished( int result )
+{ // Implement when uiDialog uses QDialog again
+}
 
 private:
 
-    QDialog*		_receiver;
-    uiDialog*		_sender;
-
+    QDialog*		qdialog_;
+    uiDialog*		uidialog_;
 };
 
 #endif
