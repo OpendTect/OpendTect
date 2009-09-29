ac5e7d5222c0ba76be8f6fe710a6e7c72d21ad78
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2009-09-29 13:13:12 +0000

    Added basemap sketch.
    git-svn-id: https://svn.opendtect.org/od/trunk@17413 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/basemap.h
@@ -0,0 +1,75 @@
+#ifndef basemap_h
+#define basemap_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	K. Tingdahl
+ Date:		Sep 2009
+ RCS:		$Id: basemap.h,v 1.1 2009-09-29 13:13:12 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+#include "namedobj.h"
+
+namespace OD { class Image; }
+
+/*!Object that can be painted in a basemap. */
+
+
+mClass BaseMapObject : public NamedObject
+{
+public:
+				BaseMapObject()
+				    : changeOccurred( this )
+				{}
+
+    virtual bool		isEnabled() const;
+
+    virtual const char*		getType() const				= 0;
+
+    virtual void		getPoints(TypeSet<Coord>&) const; 
+    				/*!<Returns a number of coordinates that
+				    may form a be connected or filled. */
+    virtual float		getDepth() const			= 0;
+    				/*!<Determines what should be painted ontop of
+				    what */
+    virtual char		connectPoints() const;
+    				/*!\retval 0 - dont connect
+				   \retval 1 - connect
+				   \retval 2 - connect as polygon.
+				   \retval 3 - connect as polygon and fill */
+    virtual const Color*	getColor() const;
+
+    virtual const OD::Image*	getImage(Coord& origin,Coord& p11) const;
+    				/*!<Returns image in xy plane. p11 is the
+				    coordinate of the corner opposite of the
+				    origin. */
+    virtual const OD::Image*	getPreview(int approxdiagonal) const;
+    				/*!<Returns a preview image that has
+				    approximately the size of the specified
+				    diagonal. */
+
+    Notifier			changeOccurred;
+};
+
+
+/*!Base class for a Basemap. */
+mClass BaseMap : public CallBacker
+{
+public:
+    virtual void		addObject(BaseMapObject*) 		= 0;
+    				/*!<Object maintained by caller */
+    virtual void		removeObject(BaseMapObject*) 		= 0;
+
+    static BaseMap*		getImpl() { return impl_; }
+    static void			setImpl(BaseMap* i) { delete impl_; impl=i; }
+protected:
+
+    static BaseMap*		impl_;
+};
+
+
+#endif
