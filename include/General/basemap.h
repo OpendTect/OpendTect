00048b48e94a766a020a05caa9801836bb77ad91
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2009-09-30 07:15:49 +0000

    Added nrShapes.
    git-svn-id: https://svn.opendtect.org/od/trunk@17416 a985855d-d384-4b45-ae33-31a410998c3c

--- include/General/basemap.h
+++ include/General/basemap.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	K. Tingdahl
  Date:		Sep 2009
- RCS:		$Id: basemap.h,v 1.1 2009-09-29 13:13:12 cvskris Exp $
+ RCS:		$Id: basemap.h,v 1.2 2009-09-30 07:15:49 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -30,18 +30,21 @@ public:
 
     virtual const char*		getType() const				= 0;
 
-    virtual void		getPoints(TypeSet<Coord>&) const; 
-    				/*!<Returns a number of coordinates that
-				    may form a be connected or filled. */
     virtual float		getDepth() const			= 0;
     				/*!<Determines what should be painted ontop of
 				    what */
-    virtual char		connectPoints() const;
+
+    virtual int			nrShapes() const;
+    virtual const char*		getShapeName(int shapeidx) const;
+    virtual void		getPoints(int shapeidx,TypeSet<Coord>&) const; 
+    				/*!<Returns a number of coordinates that
+				    may form a be connected or filled. */
+    virtual char		connectPoints(int shapeidx) const;
     				/*!\retval 0 - dont connect
 				   \retval 1 - connect
 				   \retval 2 - connect as polygon.
 				   \retval 3 - connect as polygon and fill */
-    virtual const Color*	getColor() const;
+    virtual const Color*	getColor(int shapeidx) const;
 
     virtual const OD::Image*	getImage(Coord& origin,Coord& p11) const;
     				/*!<Returns image in xy plane. p11 is the
