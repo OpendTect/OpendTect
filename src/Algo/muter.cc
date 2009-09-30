d7f7ed9c5b40be8822691c32dd630b6a26e89fe2
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2009-09-30 08:11:28 +0000

    mplemented tailmute
    git-svn-id: https://svn.opendtect.org/od/trunk@17419 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Algo/muter.cc
+++ src/Algo/muter.cc
@@ -4,7 +4,7 @@
  * DATE     : April 2005
 -*/
 
-static const char* rcsID = "$Id: muter.cc,v 1.3 2009-07-22 16:01:29 cvsbert Exp $";
+static const char* rcsID = "$Id: muter.cc,v 1.4 2009-09-30 08:11:28 cvskris Exp $";
 
 #include "muter.h"
 
@@ -44,10 +44,20 @@ void Muter::topMute( ValueSeries<float>& arr, int sz, float pos ) const
 
 void Muter::tailMute( ValueSeries<float>& arr, int sz, float pos ) const
 {
-    static bool msgdone = false;
-    if ( !msgdone )
+    int endidx = pos < 0 ? (int)pos - 1 : (int)pos;
+    if ( endidx > sz-1 ) endidx = sz - 1;
+
+    float endpos = pos + taperlen_;
+    if ( endpos <= 0 ) return;
+
+    const int startidx = endidx + 1;
+    endidx = (int)endpos; if ( endidx > sz-1 ) endidx = sz - 1;
+    for ( int idx=startidx; idx<=endidx; idx++ )
     {
-	pErrMsg( "TODO: impl tail mute" );
-	msgdone = true;
+	float relpos = 1-((idx-pos) / taperlen_);
+	arr.setValue( idx, arr[idx] * 0.5 * ( 1 - cos(M_PI * relpos) ) );
     }
+
+    for ( int idx=endidx+1; idx<sz; idx++ )
+	arr.setValue( idx, 0 );
 }
