334141d28dc8f31bad4605f7c61a5b5d23ead80f
Yuancheng Liu <yuancheng.liu@dgbes.com>
2009-11-04 15:48:41 +0000

    Made a field group for saving horizons.
    git-svn-id: https://svn.opendtect.org/od/trunk@17679 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiEarthModel/uihorsavefieldgrp.h
@@ -0,0 +1,62 @@
+#ifndef uihorsavefieldgrp_h
+#define uihorsavefieldgrp_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Y. Liu
+ Date:          Nov 2009
+ RCS:           $Id: uihorsavefieldgrp.h,v 1.1 2009-11-04 15:48:41 cvsyuancheng Exp $
+________________________________________________________________________
+
+-*/
+
+
+#include "uigroup.h"
+
+namespace EM { class Horizon3D; }
+
+class MultiID;
+class uiGenInput;
+class uiIOObjSel;
+
+/*!\brief save or overwrite horizon field set up. It will create new horizon 
+    based on given horizon, if the old horizon is not given, you can read it 
+    from memory. You can also call saveHorizon() to save horizon based on your
+    choice of as new or overwrite. */
+
+mClass uiHorSaveFieldGrp : public uiGroup
+{
+public:
+				uiHorSaveFieldGrp(uiParent*,EM::Horizon3D*);
+				~uiHorSaveFieldGrp();
+
+    void			setSaveFieldName(const char*);
+    bool			displayNewHorizon() const;
+    EM::Horizon3D*		getNewHorizon() const	{ return newhorizon_; }
+
+    EM::Horizon3D*		readHorizon(const MultiID&);
+    bool			saveHorizon();
+    
+    void			setFullSurveyArray(bool yn);
+    bool			needsFullSurveyArray() const;
+    bool			acceptOK(CallBacker*);
+
+protected:
+
+    uiGenInput*			savefld_;
+    uiGenInput*			addnewfld_;
+    uiIOObjSel*			outputfld_;
+
+    EM::Horizon3D*		horizon_;
+    EM::Horizon3D*		newhorizon_;
+    bool			usefullsurvey_;
+
+    bool			createNewHorizon();
+    void			saveCB(CallBacker*);
+    void			expandToFullSurveyArray();
+};
+
+
+#endif
---
+++ src/uiEarthModel/uihorsavefieldgrp.cc
@@ -0,0 +1,255 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Y. Liu
+ Date:		Nov 2009
+________________________________________________________________________
+
+-*/
+static const char* rcsID = "$Id: uihorsavefieldgrp.cc,v 1.1 2009-11-04 15:48:41 cvsyuancheng Exp $";
+
+#include "uihorsavefieldgrp.h"
+
+#include "ctxtioobj.h"
+#include "emhorizon3d.h"
+#include "emmanager.h"
+#include "emsurfacetr.h"
+#include "executor.h"
+#include "survinfo.h"
+
+#include "uitaskrunner.h"
+#include "uigeninput.h"
+#include "uiioobjsel.h"
+#include "uimsg.h"
+
+
+
+uiHorSaveFieldGrp::uiHorSaveFieldGrp( uiParent* p, EM::Horizon3D* hor )
+    : uiGroup( p )
+    , horizon_( hor )
+    , newhorizon_( 0 ) 
+    , savefld_( 0 )
+    , addnewfld_( 0 )		     
+    , outputfld_( 0 )
+    , usefullsurvey_( false )		   
+{
+    if ( horizon_ ) horizon_->ref();
+
+    savefld_ = new uiGenInput( this, "Save horizon",
+	    		       BoolInpSpec(false,"As new","Overwrite") );
+    savefld_->valuechanged.notify( mCB(this,uiHorSaveFieldGrp,saveCB) );
+    
+    addnewfld_ = new uiGenInput( this, "Display in the scene",
+	    			 BoolInpSpec(false) );
+
+    IOObjContext ctxt = EMHorizon3DTranslatorGroup::ioContext();
+    ctxt.forread = false;
+    outputfld_ = new uiIOObjSel( this, ctxt, "Output Horizon" );
+    outputfld_->attach( alignedBelow, savefld_ );
+    addnewfld_->attach( alignedBelow, outputfld_ );
+
+    setHAlignObj( savefld_ );    
+    saveCB(0);
+}
+
+
+uiHorSaveFieldGrp::~uiHorSaveFieldGrp()
+{
+    if ( horizon_ ) horizon_->unRef();
+    if ( newhorizon_ ) newhorizon_->unRef();
+}
+
+
+void uiHorSaveFieldGrp::setSaveFieldName( const char* nm )
+{ savefld_->setName( nm ); }
+
+
+void uiHorSaveFieldGrp::saveCB( CallBacker* )
+{
+    outputfld_->display( savefld_->getBoolValue() );
+    addnewfld_->display( savefld_->getBoolValue() );
+}
+
+
+bool uiHorSaveFieldGrp::displayNewHorizon() const
+{ return savefld_->getBoolValue() && addnewfld_->getBoolValue(); }
+
+
+void uiHorSaveFieldGrp::setFullSurveyArray( bool yn )
+{
+    if ( usefullsurvey_ != yn )
+    {
+	usefullsurvey_ = yn;
+    	if ( yn )
+	    expandToFullSurveyArray();
+    }
+}
+
+
+bool uiHorSaveFieldGrp::needsFullSurveyArray() const
+{ return  usefullsurvey_; }
+
+
+#define mErrRet(msg) { if ( msg ) uiMSG().error( msg ); return false; }
+
+EM::Horizon3D* uiHorSaveFieldGrp::readHorizon( const MultiID& mid )
+{
+    EM::ObjectID oid = EM::EMM().getObjectID( mid );
+    EM::EMObject* emobj = EM::EMM().getObject( oid );
+
+    Executor* reader = 0;
+    if ( !emobj || !emobj->isFullyLoaded() )
+    {
+	reader = EM::EMM().objectLoader( mid );
+	if ( !reader ) 
+	    mErrRet( "Could not read horizon." );
+
+	uiTaskRunner dlg( this );
+	if ( !dlg.execute(*reader) )
+	{
+	    delete reader;
+	    mErrRet( "Could not read horizon." );
+	}
+
+	oid = EM::EMM().getObjectID( mid );
+	emobj = EM::EMM().getObject( oid );
+    }
+
+    mDynamicCastGet(EM::Horizon3D*,hor,emobj)
+    horizon_ = hor;
+    horizon_->ref();
+    delete reader;
+    return horizon_;
+}
+
+
+bool uiHorSaveFieldGrp::saveHorizon()
+{
+    const bool savenew = savefld_->getBoolValue();
+    PtrMan<Executor> exec = savenew ? newhorizon_->saver( ) : horizon_->saver();
+
+    if ( !exec ) mErrRet( "Cannot save horizon" );
+
+    uiTaskRunner dlg( this );
+    return dlg.execute( *exec );
+}
+
+
+bool uiHorSaveFieldGrp::acceptOK( CallBacker* )
+{
+    if ( savefld_->getBoolValue() )
+    {
+	if ( !outputfld_->commitInput() )
+    	    mErrRet(outputfld_->isEmpty() ? "Please select output horizon" : 
+		    "Cannot continue: write permission problem" )
+
+	if ( !createNewHorizon() )		
+	    return false;
+    }
+
+    return true;
+}
+
+
+bool uiHorSaveFieldGrp::createNewHorizon()
+{
+    if ( !horizon_ )
+	mErrRet( "No selected horizon, cannot create a new one." );
+
+    if ( needsFullSurveyArray() )
+	expandToFullSurveyArray();
+
+    if ( newhorizon_ )
+	newhorizon_->unRef();
+
+    EM::EMManager& em = EM::EMM();
+    const BufferString typ = EM::Horizon3D::typeStr();
+    EM::ObjectID objid = em.createObject( typ, outputfld_->getInput() );
+    
+    mDynamicCastGet(EM::Horizon3D*,horizon,em.getObject(objid));
+    if ( !horizon )
+	mErrRet( "Cannot create horizon" );
+    
+    horizon->initClass();
+    if ( newhorizon_ ) newhorizon_->unRef();
+    newhorizon_ = horizon;      
+    newhorizon_->ref();
+    newhorizon_->setMultiID( outputfld_->ioobj()->key() );
+
+    for ( int idx=0; idx<newhorizon_->geometry().nrSections(); idx++ )
+    {
+	const EM::SectionID sid = newhorizon_->geometry().sectionID( idx );
+	newhorizon_->geometry().removeSection( sid, false );
+    }
+    
+    const RowCol step = horizon_->geometry().step();
+    newhorizon_->geometry().setStep( step, step );
+
+    for ( int idx=0; idx<horizon_->geometry().nrSections(); idx++ )
+    {
+	const EM::SectionID sid = horizon_->geometry().sectionID( idx );
+	const Geometry::BinIDSurface* surf =
+	    horizon_->geometry().sectionGeometry(sid);
+        
+	EM::SectionID usedsid = sid;
+        BufferString sectnm = horizon_->sectionName( sid );
+        usedsid = newhorizon_->geometry().addSection( sectnm, false );
+
+        const RowCol start = surf->getKnotRowCol(0);
+        const RowCol stop = surf->getKnotRowCol( surf->nrKnots()-1 );
+        newhorizon_->geometry().sectionGeometry(usedsid)->
+                expandWithUdf( start, stop );
+    }
+ 
+    return true;    
+}
+
+
+void uiHorSaveFieldGrp::expandToFullSurveyArray()
+{
+    if ( !horizon_ || !horizon_->geometry().nrSections() )
+	return;
+    
+    const EM::SectionID sid = horizon_->geometry().sectionID( 0 );
+    mDynamicCastGet( Geometry::ParametricSurface*, surf,
+	    horizon_->sectionGeometry( sid ) );
+    if ( !surf || !needsFullSurveyArray() )
+	return;
+    
+    StepInterval<int> rowrg = horizon_->geometry().rowRange( sid );
+    StepInterval<int> colrg = horizon_->geometry().colRange( sid );
+    
+    const StepInterval<int> survcrlrg = SI().crlRange(true);
+    while ( colrg.start-colrg.step>=survcrlrg.start )
+    {
+	const int newcol = colrg.start-colrg.step;
+	surf->insertCol( newcol );
+	colrg.start = newcol;
+    }
+    
+    while ( colrg.stop+colrg.step<=survcrlrg.stop )
+    {
+	const int newcol = colrg.stop+colrg.step;
+	surf->insertCol( newcol );
+	colrg.stop = newcol;
+    }
+    
+    const StepInterval<int> survinlrg = SI().inlRange(true);
+    while ( rowrg.start-rowrg.step>=survinlrg.start )
+    {
+	const int newrow = rowrg.start-rowrg.step;
+	surf->insertRow( newrow );
+	rowrg.start = newrow;
+    }
+    
+    while ( rowrg.stop+rowrg.step<=survinlrg.stop )
+    {
+	const int newrow = rowrg.stop+rowrg.step;
+	surf->insertRow( newrow );
+	rowrg.stop = newrow;
+    }
+}
+
+
+
