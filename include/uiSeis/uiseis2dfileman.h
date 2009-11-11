c6d83da5fcd6580ddb57722f89c35a1eca89832d
Bert Bril <bert.bril@dgbes.com>
2009-11-11 14:38:09 +0000

    Made a separate header file for uiSeis2DFileman
    git-svn-id: https://svn.opendtect.org/od/trunk@17729 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiSeis/uiseis2dfileman.h
@@ -0,0 +1,63 @@
+#ifndef uiseis2dfileman_h
+#define uiseis2dfileman_h
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Nanne/Bert
+ Date:          April 2002/Nov 2009
+ RCS:           $Id: uiseis2dfileman.h,v 1.1 2009-11-11 14:38:09 cvsbert Exp $
+________________________________________________________________________
+
+-*/
+
+#include "uidialog.h"
+class IOObj;
+class uiListBox;
+class uiTextEdit;
+class uiToolButton;
+class uiManipButGrp;
+class Seis2DLineSet;
+class uiSeisIOObjInfo;
+
+
+class uiSeis2DFileMan : public uiDialog
+{
+public:
+
+			uiSeis2DFileMan(uiParent*,const IOObj&);
+			~uiSeis2DFileMan();
+
+    uiListBox*		getListBox( bool attrs )
+			{ return attrs ? attrfld_ : linefld_; }
+    uiManipButGrp*	getButGroup( bool attrs )
+			{ return attrs ? attrgrp_ : linegrp_; }
+
+    Seis2DLineSet*	lineset_;
+    uiSeisIOObjInfo*	objinfo_;
+
+protected:
+
+    void		fillLineBox();
+    bool		rename(const char*,BufferString&);
+    void		lineSel(CallBacker*);
+    void		attribSel(CallBacker*);
+    void		browsePush(CallBacker*);
+    void		removeAttrib(CallBacker*);
+    void		renameLine(CallBacker*);
+    void		renameAttrib(CallBacker*);
+
+    uiListBox*		linefld_;
+    uiListBox*		attrfld_;
+    uiTextEdit*		infofld_;
+    uiToolButton*	browsebut_;
+    uiManipButGrp*	linegrp_;
+    uiManipButGrp*	attrgrp_;
+
+    const bool		issidomain;
+    const bool		zistm;
+
+};
+
+
+#endif
--- src/uiSeis/uiseisfileman.cc
+++ src/uiSeis/uiseisfileman.cc
@@ -7,10 +7,11 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID = "$Id: uiseisfileman.cc,v 1.94 2009-11-06 12:01:29 cvsranojay Exp $";
+static const char* rcsID = "$Id: uiseisfileman.cc,v 1.95 2009-11-11 14:38:09 cvsbert Exp $";
 
 
 #include "uiseisfileman.h"
+#include "uiseis2dfileman.h"
 #include "uiseispsman.h"
 
 #include "cbvsreadmgr.h"
@@ -245,302 +246,12 @@ void uiSeisFileMan::browsePush( CallBacker* )
 }
 
 
-
-class uiSeis2DMan : public uiDialog
-{
-public:
-
-uiSeis2DMan( uiParent* p, const IOObj& ioobj )
-    : uiDialog(p,uiDialog::Setup("Seismic file management",
-				 "Manage 2D seismic lines",
-				 "103.1.3"))
-    , issidomain(ZDomain::isSIDomain( ioobj.pars() ))
-    , zistm((SI().zIsTime() && issidomain) || (!SI().zIsTime() && !issidomain))
-{
-    setCtrlStyle( LeaveOnly );
-
-    objinfo = new uiSeisIOObjInfo( ioobj );
-    lineset = new Seis2DLineSet( ioobj.fullUserExpr(true) );
-
-    uiGroup* topgrp = new uiGroup( this, "Top" );
-    uiLabeledListBox* lllb = new uiLabeledListBox( topgrp, "2D lines", false,
-						  uiLabeledListBox::AboveMid );
-    linelist = lllb->box();
-    linelist->selectionChanged.notify( mCB(this,uiSeis2DMan,lineSel) );
-    linelist->setMultiSelect(true);
-
-    uiManipButGrp* linebutgrp = new uiManipButGrp( lllb );
-    linebutgrp->addButton( uiManipButGrp::Rename, 
-	    		   mCB(this,uiSeis2DMan,renameLine), "Rename line" );
-    linebutgrp->attach( rightTo, linelist );
-
-    uiLabeledListBox* allb = new uiLabeledListBox( topgrp, "Attributes", true,
-						   uiLabeledListBox::AboveMid );
-    attriblist = allb->box();
-    attriblist->selectionChanged.notify( mCB(this,uiSeis2DMan,attribSel) );
-    allb->attach( rightTo, lllb );
-
-    uiManipButGrp* butgrp = new uiManipButGrp( allb );
-    butgrp->addButton( uiManipButGrp::Rename,
-	    	       mCB(this,uiSeis2DMan,renameAttrib), "Rename attribute" );
-    butgrp->addButton( uiManipButGrp::Remove,
-	    	       mCB(this,uiSeis2DMan,removeAttrib),
-		       "Remove selected attribute(s)" );
-    browsebut = butgrp->addButton( ioPixmap("browseseis.png"),
-	    	       mCB(this,uiSeis2DMan,browsePush),
-		       "Browse/edit this line" );
-    butgrp->attach( rightTo, attriblist );
-
-    uiGroup* botgrp = new uiGroup( this, "Bottom" );
-    infofld = new uiTextEdit( botgrp, "File Info", true );
-    infofld->setPrefHeightInChar( 8 );
-    infofld->setPrefWidthInChar( 50 );
-
-    uiSplitter* splitter = new uiSplitter( this, "Splitter", false );
-    splitter->addGroup( topgrp );
-    splitter->addGroup( botgrp );
-
-    fillLineBox();
-    lineSel(0);
-}
-
-
-~uiSeis2DMan()
-{
-    delete objinfo;
-    delete lineset;
-}
-
-
-void fillLineBox()
-{
-    uiListBox* lb = linelist;
-    const int curitm = lb->size() ? lb->currentItem() : 0;
-    BufferStringSet linenames;
-    objinfo->getLineNames( linenames );
-    lb->empty();
-    lb->addItems( linenames );
-    lb->setSelected( curitm );
-}
-
-
-void lineSel( CallBacker* )
-{
-    BufferStringSet sellines;
-    linelist->getSelectedItems( sellines );
-    BufferStringSet sharedattribs;
-    for ( int idx=0; idx<sellines.size(); idx++ )
-    {
-	BufferStringSet attrs;
-	objinfo->getAttribNamesForLine( sellines.get(idx), attrs );
-	if ( !idx )
-	{
-	    sharedattribs = attrs;
-	    continue;
-	}
-
-	BufferStringSet strs2rem;
-	for ( int ida=0; ida<sharedattribs.size(); ida++ )
-	{
-	    const char* str = sharedattribs.get(ida);
-	    int index = attrs.indexOf( str );
-	    if ( index<0 ) strs2rem.add( str );
-	}
-
-	for ( int ida=0; ida<strs2rem.size(); ida++ )
-	{
-	    const int index = sharedattribs.indexOf( strs2rem.get(ida) );
-	    sharedattribs.remove( index );
-	}
-    }
-
-    attriblist->empty();
-    sharedattribs.sort();
-    attriblist->addItems( sharedattribs );
-    attriblist->setSelected( 0, true );
-}
-
-
-void attribSel( CallBacker* )
-{
-    infofld->setText( "" );
-    BufferStringSet linenms, attribnms;
-    linelist->getSelectedItems( linenms );
-    attriblist->getSelectedItems( attribnms );
-    if ( linenms.isEmpty() || attribnms.isEmpty() )
-	{ browsebut->setSensitive( false ); return; }
-
-    const LineKey linekey( linenms.get(0), attribnms.get(0) );
-    const int lineidx = lineset->indexOf( linekey );
-    if ( lineidx < 0 ) { pErrMsg("Huh"); return; }
-
-    PosInfo::Line2DData l2dd;
-    if ( !lineset->getGeometry(lineidx,l2dd) || l2dd.posns_.isEmpty() )
-	return;
-
-    const int sz = l2dd.posns_.size();
-    BufferString txt( "Number of traces: " ); txt += sz;
-    const PosInfo::Line2DPos& firstpos = l2dd.posns_[0];
-    txt += "\nFirst trace: "; txt += firstpos.nr_;
-    txt += " ("; txt += firstpos.coord_.x;
-    txt += ","; txt += firstpos.coord_.y; txt += ")";
-    const PosInfo::Line2DPos& lastpos = l2dd.posns_[sz-1];
-    txt += "\nLast trace: "; txt += lastpos.nr_;
-    txt += " ("; txt += lastpos.coord_.x;
-    txt += ","; txt += lastpos.coord_.y; txt += ")";
-    txt += "\nZ-range: "; mAddZRangeTxt(l2dd.zrg_.start); txt += " - ";
-    mAddZRangeTxt(l2dd.zrg_.stop);
-    txt += " ["; mAddZRangeTxt(l2dd.zrg_.step); txt += "]";
-
-    SeisIOObjInfo sobinf( objinfo->ioObj() );
-    const int nrcomp = sobinf.nrComponents( linekey );
-    if ( nrcomp > 1 )
-	{ txt += "\nNumber of components: "; txt += nrcomp; }
-
-    const IOPar& iopar = lineset->getInfo( lineidx );
-    BufferString fname(iopar.find(sKey::FileName) );
-    FilePath fp( fname );
-    if ( !fp.isAbsolute() )
-	fp.setPath( IOObjContext::getDataDirName(IOObjContext::Seis) );
-    fname = fp.fullPath();
-
-    txt += "\nLocation: "; txt += fp.pathOnly();
-    txt += "\nFile name: "; txt += fp.fileName();
-    txt += "\nFile size: "; 
-    txt += uiObjFileMan::getFileSizeString( File_getKbSize(fname) );
-    const char* timestr = File_getTime( fname );
-    if ( timestr ) { txt += "\nLast modified: "; txt += timestr; }
-    infofld->setText( txt );
-
-    browsebut->setSensitive( true );
-}
-
-
-void browsePush( CallBacker* )
-{
-    const LineKey lk( linelist->getText(), attriblist->getText());
-    doBrowse( this, objinfo->ioObj(), &lk, true );
-}
-
-
-void removeAttrib( CallBacker* )
-{
-    BufferStringSet attribnms;
-    attriblist->getSelectedItems( attribnms );
-    if ( attribnms.isEmpty()
-      || !uiMSG().askRemove("All selected attributes will be removed.\n"
-			     "Do you want to continue?") )
-	return;
-
-    BufferStringSet sellines;
-    linelist->getSelectedItems( sellines );
-    for ( int idx=0; idx<sellines.size(); idx++ )
-    {
-	const char* linename = sellines.get(idx);
-	for ( int ida=0; ida<attribnms.size(); ida++ )
-	{
-	    LineKey linekey( linename, attribnms.get(ida) );
-	    if ( !lineset->remove(linekey) )
-		uiMSG().error( "Could not remove attribute" );
-	}
-    }
-
-    fillLineBox();
-}
-
-
-bool rename( const char* oldnm, BufferString& newnm )
-{
-    BufferString titl( "Rename '" );
-    titl += oldnm; titl += "'";
-    uiGenInputDlg dlg( this, titl, "New name", new StringInpSpec(oldnm) );
-    if ( !dlg.go() ) return false;
-    newnm = dlg.text();
-    return newnm != oldnm;
-}
-
-
-void renameLine( CallBacker* )
-{
-    BufferStringSet linenms;
-    linelist->getSelectedItems( linenms );
-    if ( linenms.isEmpty() ) return;
-
-    const char* linenm = linenms.get(0);
-    BufferString newnm;
-    if ( !rename(linenm,newnm) ) return;
-    
-    if ( linelist->isPresent(newnm) )
-    {
-	uiMSG().error( "Linename already in use" );
-	return;
-    }
-
-    if ( !lineset->renameLine( linenm, newnm ) )
-    {
-	uiMSG().error( "Could not rename line" );
-	return;
-    }
-
-    fillLineBox();
-}
-
-
-void renameAttrib( CallBacker* )
-{
-    BufferStringSet attribnms;
-    attriblist->getSelectedItems( attribnms );
-    if ( attribnms.isEmpty() ) return;
-
-    const char* attribnm = attribnms.get(0);
-    BufferString newnm;
-    if ( !rename(attribnm,newnm) ) return;
-
-    if ( attriblist->isPresent(newnm) )
-    {
-	uiMSG().error( "Attribute name already in use" );
-	return;
-    }
-
-    BufferStringSet sellines;
-    linelist->getSelectedItems( sellines );
-    for ( int idx=0; idx<sellines.size(); idx++ )
-    {
-	const char* linenm = sellines.get(idx);
-	LineKey oldlk( linenm, attribnm );
-	if ( !lineset->rename(oldlk,LineKey(linenm,newnm)) )
-	{
-	    BufferString err( "Could not rename attribute: " );
-	    err += oldlk;
-	    uiMSG().error( err );
-	    continue;
-	}
-    }
-
-    lineSel(0);
-}
-
-protected:
-
-    uiListBox*		linelist;
-    uiListBox*		attriblist;
-    uiTextEdit*		infofld;
-    uiToolButton*	browsebut;
-
-    Seis2DLineSet*	lineset;
-    uiSeisIOObjInfo*	objinfo;
-    const bool		issidomain;
-    const bool		zistm;
-
-};
-
-
 void uiSeisFileMan::man2DPush( CallBacker* )
 {
     if ( !curioobj_ ) return;
 
     const MultiID key( curioobj_->key() );
-    uiSeis2DMan dlg( this, *curioobj_ );
+    uiSeis2DFileMan dlg( this, *curioobj_ );
     dlg.go();
 
     selgrp->fullUpdate( key );
@@ -576,3 +287,272 @@ void uiSeisFileMan::manPS( CallBacker* )
     dlg.go();
 }
 
+
+
+uiSeis2DFileMan::uiSeis2DFileMan( uiParent* p, const IOObj& ioobj )
+    : uiDialog(p,uiDialog::Setup("Seismic line data management",
+				 "Manage 2D seismic lines",
+				 "103.1.3"))
+    , issidomain(ZDomain::isSIDomain( ioobj.pars() ))
+    , zistm((SI().zIsTime() && issidomain) || (!SI().zIsTime() && !issidomain))
+{
+    setCtrlStyle( LeaveOnly );
+
+    objinfo_ = new uiSeisIOObjInfo( ioobj );
+    lineset_ = new Seis2DLineSet( ioobj.fullUserExpr(true) );
+
+    uiGroup* topgrp = new uiGroup( this, "Top" );
+    uiLabeledListBox* lllb = new uiLabeledListBox( topgrp, "2D lines", false,
+						  uiLabeledListBox::AboveMid );
+    linefld_ = lllb->box();
+    linefld_->selectionChanged.notify( mCB(this,uiSeis2DFileMan,lineSel) );
+    linefld_->setMultiSelect(true);
+
+    linegrp_ = new uiManipButGrp( lllb );
+    linegrp_->addButton( uiManipButGrp::Rename, 
+	    		   mCB(this,uiSeis2DFileMan,renameLine), "Rename line");
+    linegrp_->attach( rightOf, linefld_ );
+
+    uiLabeledListBox* allb = new uiLabeledListBox( topgrp, "Attributes", true,
+						   uiLabeledListBox::AboveMid );
+    attrfld_ = allb->box();
+    attrfld_->selectionChanged.notify( mCB(this,uiSeis2DFileMan,attribSel) );
+    allb->attach( rightOf, lllb );
+
+    attrgrp_ = new uiManipButGrp( allb );
+    attrgrp_->addButton( uiManipButGrp::Rename,
+	    	       mCB(this,uiSeis2DFileMan,removeAttrib),
+		       "Remove selected attribute(s)" );
+    browsebut_ = attrgrp_->addButton( ioPixmap("browseseis.png"),
+	    	       mCB(this,uiSeis2DFileMan,browsePush),
+		       "Browse/edit this line" );
+    attrgrp_->attach( rightOf, attrfld_ );
+
+    uiGroup* botgrp = new uiGroup( this, "Bottom" );
+    infofld_ = new uiTextEdit( botgrp, "File Info", true );
+    infofld_->setPrefHeightInChar( 8 );
+    infofld_->setPrefWidthInChar( 50 );
+
+    uiSplitter* splitter = new uiSplitter( this, "Splitter", false );
+    splitter->addGroup( topgrp );
+    splitter->addGroup( botgrp );
+
+    fillLineBox();
+    lineSel(0);
+}
+
+
+uiSeis2DFileMan::~uiSeis2DFileMan()
+{
+    delete objinfo_;
+    delete lineset_;
+}
+
+
+void uiSeis2DFileMan::fillLineBox()
+{
+    uiListBox* lb = linefld_;
+    const int curitm = lb->size() ? lb->currentItem() : 0;
+    BufferStringSet linenames;
+    objinfo_->getLineNames( linenames );
+    lb->empty();
+    lb->addItems( linenames );
+    lb->setSelected( curitm );
+}
+
+
+void uiSeis2DFileMan::lineSel( CallBacker* )
+{
+    BufferStringSet sellines;
+    linefld_->getSelectedItems( sellines );
+    BufferStringSet sharedattribs;
+    for ( int idx=0; idx<sellines.size(); idx++ )
+    {
+	BufferStringSet attrs;
+	objinfo_->getAttribNamesForLine( sellines.get(idx), attrs );
+	if ( !idx )
+	{
+	    sharedattribs = attrs;
+	    continue;
+	}
+
+	BufferStringSet strs2rem;
+	for ( int ida=0; ida<sharedattribs.size(); ida++ )
+	{
+	    const char* str = sharedattribs.get(ida);
+	    int index = attrs.indexOf( str );
+	    if ( index<0 ) strs2rem.add( str );
+	}
+
+	for ( int ida=0; ida<strs2rem.size(); ida++ )
+	{
+	    const int index = sharedattribs.indexOf( strs2rem.get(ida) );
+	    sharedattribs.remove( index );
+	}
+    }
+
+    attrfld_->empty();
+    sharedattribs.sort();
+    attrfld_->addItems( sharedattribs );
+    attrfld_->setSelected( 0, true );
+}
+
+
+void uiSeis2DFileMan::attribSel( CallBacker* )
+{
+    infofld_->setText( "" );
+    BufferStringSet linenms, attribnms;
+    linefld_->getSelectedItems( linenms );
+    attrfld_->getSelectedItems( attribnms );
+    if ( linenms.isEmpty() || attribnms.isEmpty() )
+	{ browsebut_->setSensitive( false ); return; }
+
+    const LineKey linekey( linenms.get(0), attribnms.get(0) );
+    const int lineidx = lineset_->indexOf( linekey );
+    if ( lineidx < 0 ) { pErrMsg("Huh"); return; }
+
+    PosInfo::Line2DData l2dd;
+    if ( !lineset_->getGeometry(lineidx,l2dd) || l2dd.posns_.isEmpty() )
+	return;
+
+    const int sz = l2dd.posns_.size();
+    BufferString txt( "Number of traces: " ); txt += sz;
+    const PosInfo::Line2DPos& firstpos = l2dd.posns_[0];
+    txt += "\nFirst trace: "; txt += firstpos.nr_;
+    txt += " ("; txt += firstpos.coord_.x;
+    txt += ","; txt += firstpos.coord_.y; txt += ")";
+    const PosInfo::Line2DPos& lastpos = l2dd.posns_[sz-1];
+    txt += "\nLast trace: "; txt += lastpos.nr_;
+    txt += " ("; txt += lastpos.coord_.x;
+    txt += ","; txt += lastpos.coord_.y; txt += ")";
+    txt += "\nZ-range: "; mAddZRangeTxt(l2dd.zrg_.start); txt += " - ";
+    mAddZRangeTxt(l2dd.zrg_.stop);
+    txt += " ["; mAddZRangeTxt(l2dd.zrg_.step); txt += "]";
+
+    SeisIOObjInfo sobinf( objinfo_->ioObj() );
+    const int nrcomp = sobinf.nrComponents( linekey );
+    if ( nrcomp > 1 )
+	{ txt += "\nNumber of components: "; txt += nrcomp; }
+
+    const IOPar& iopar = lineset_->getInfo( lineidx );
+    BufferString fname(iopar.find(sKey::FileName) );
+    FilePath fp( fname );
+    if ( !fp.isAbsolute() )
+	fp.setPath( IOObjContext::getDataDirName(IOObjContext::Seis) );
+    fname = fp.fullPath();
+
+    txt += "\nLocation: "; txt += fp.pathOnly();
+    txt += "\nFile name: "; txt += fp.fileName();
+    txt += "\nFile size: "; 
+    txt += uiObjFileMan::getFileSizeString( File_getKbSize(fname) );
+    const char* timestr = File_getTime( fname );
+    if ( timestr ) { txt += "\nLast modified: "; txt += timestr; }
+    infofld_->setText( txt );
+
+    browsebut_->setSensitive( true );
+}
+
+
+void uiSeis2DFileMan::browsePush( CallBacker* )
+{
+    const LineKey lk( linefld_->getText(), attrfld_->getText());
+    doBrowse( this, objinfo_->ioObj(), &lk, true );
+}
+
+
+void uiSeis2DFileMan::removeAttrib( CallBacker* )
+{
+    BufferStringSet attribnms;
+    attrfld_->getSelectedItems( attribnms );
+    if ( attribnms.isEmpty()
+      || !uiMSG().askRemove("All selected attributes will be removed.\n"
+			     "Do you want to continue?") )
+	return;
+
+    BufferStringSet sellines;
+    linefld_->getSelectedItems( sellines );
+    for ( int idx=0; idx<sellines.size(); idx++ )
+    {
+	const char* linename = sellines.get(idx);
+	for ( int ida=0; ida<attribnms.size(); ida++ )
+	{
+	    LineKey linekey( linename, attribnms.get(ida) );
+	    if ( !lineset_->remove(linekey) )
+		uiMSG().error( "Could not remove attribute" );
+	}
+    }
+
+    fillLineBox();
+}
+
+
+bool uiSeis2DFileMan::rename( const char* oldnm, BufferString& newnm )
+{
+    BufferString titl( "Rename '" );
+    titl += oldnm; titl += "'";
+    uiGenInputDlg dlg( this, titl, "New name", new StringInpSpec(oldnm) );
+    if ( !dlg.go() ) return false;
+    newnm = dlg.text();
+    return newnm != oldnm;
+}
+
+
+void uiSeis2DFileMan::renameLine( CallBacker* )
+{
+    BufferStringSet linenms;
+    linefld_->getSelectedItems( linenms );
+    if ( linenms.isEmpty() ) return;
+
+    const char* linenm = linenms.get(0);
+    BufferString newnm;
+    if ( !rename(linenm,newnm) ) return;
+    
+    if ( linefld_->isPresent(newnm) )
+    {
+	uiMSG().error( "Linename already in use" );
+	return;
+    }
+
+    if ( !lineset_->renameLine( linenm, newnm ) )
+    {
+	uiMSG().error( "Could not rename line" );
+	return;
+    }
+
+    fillLineBox();
+}
+
+
+void uiSeis2DFileMan::renameAttrib( CallBacker* )
+{
+    BufferStringSet attribnms;
+    attrfld_->getSelectedItems( attribnms );
+    if ( attribnms.isEmpty() ) return;
+
+    const char* attribnm = attribnms.get(0);
+    BufferString newnm;
+    if ( !rename(attribnm,newnm) ) return;
+
+    if ( attrfld_->isPresent(newnm) )
+    {
+	uiMSG().error( "Attribute name already in use" );
+	return;
+    }
+
+    BufferStringSet sellines;
+    linefld_->getSelectedItems( sellines );
+    for ( int idx=0; idx<sellines.size(); idx++ )
+    {
+	const char* linenm = sellines.get(idx);
+	LineKey oldlk( linenm, attribnm );
+	if ( !lineset_->rename(oldlk,LineKey(linenm,newnm)) )
+	{
+	    BufferString err( "Could not rename attribute: " );
+	    err += oldlk;
+	    uiMSG().error( err );
+	    continue;
+	}
+    }
+
+    lineSel(0);
+}
