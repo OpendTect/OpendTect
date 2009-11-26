e1e7a5a9a5c760915725377bbd1c1f76b3c7d270
Ranojay Sen <ranojay.sen@dgbes.com>
2009-11-26 07:13:02 +0000

    Update
    git-svn-id: https://svn.opendtect.org/od/trunk@17896 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/msvcdefs.h
+++ include/Basic/msvcdefs.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	A.H.Bril
  Date:		Mar 2006
- RCS:		$Id: msvcdefs.h,v 1.10 2009-10-05 05:59:22 cvsnanne Exp $
+ RCS:		$Id: msvcdefs.h,v 1.11 2009-11-26 07:13:02 cvsranojay Exp $
 ________________________________________________________________________
 
  For use with Microsoft Visual C++ 8.0 and 9.0
@@ -39,6 +39,7 @@ ________________________________________________________________________
 #define strtof		strtod
 
 #define finite		_finite
+#define atoll		_atoi64
 
 // Index variable's scope is non-ANSI. This corrects that idiocy.
 #define for				if (0) ; else for
