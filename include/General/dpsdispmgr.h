babe7fe62235d5cfde1264d62299aeb8f103fed1
Satyaki Maitra <satyaki.maitra@dgbes.com>
2009-11-30 12:15:28 +0000

    DPSDispMgr shifted from datapointset.h to dpsdispmgr.h
    git-svn-id: https://svn.opendtect.org/od/trunk@17912 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/dpsdispmgr.h
@@ -0,0 +1,64 @@
+#ifndef dpsdipsmgr_h
+#define dpsdipsmgr_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Satyaki Maitra
+ Date:		Nov 2009
+ RCS:		$Id: dpsdispmgr.h,v 1.1 2009-11-30 12:15:28 cvssatyaki Exp $
+________________________________________________________________________
+
+-*/
+
+#include "callback.h"
+
+class Color;
+class DataPointSet;
+class BufferString;
+
+/*!Interface for DataPointSet Displays. Object must be locked before
+   accessing any of the other functions, and should be unlocked when
+   done.
+
+   The datapointset can be displayed in a number of viewers.
+
+   DispID not to be confused with Visid. Its used to keep an account for the
+   DataPointSetDisplayMgr only.
+*/
+
+mClass DataPointSetDisplayMgr : public CallBacker
+{
+public:
+    typedef int			DispID;
+    virtual			~DataPointSetDisplayMgr()		{}
+    virtual void		lock()					= 0;
+    virtual void		unLock()				= 0;
+
+    virtual bool		hasDisplays() const 			= 0; 
+    virtual DispID		getDisplayID(const DataPointSet&) const	= 0;
+    virtual int			getNrViewers() const			= 0;
+    virtual const char*		getViewerName(int) const		= 0;
+
+    virtual void		setDisplayCol(DispID,const Color&)	= 0;
+    virtual DispID		addDisplay(const TypeSet<int>& parents,
+	    				   const DataPointSet&)		= 0;
+    virtual void		updateDisplay(DispID id,
+				    const TypeSet<int>& parents,
+				    const DataPointSet&)		= 0;
+    virtual void		updateDisplay(DispID id,const DataPointSet&) =0;
+    virtual void		removeDisplay(DispID)			= 0;
+    const TypeSet<int>&		availableViewers() const
+    				{ return availableviewers_; }
+
+    virtual void		getIconInfo(BufferString& fnm,
+	    				    BufferString& tootltip) const = 0;
+protected:
+
+    TypeSet<int>		availableviewers_;
+};
+	    				   
+
+
+#endif
