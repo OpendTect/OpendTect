a4cae538d9753e4c5fa98234edf174937a1ed726
Bert Bril <bert.bril@dgbes.com>
2009-12-01 13:41:48 +0000

    cosmetical changes
    git-svn-id: https://svn.opendtect.org/od/trunk@17932 a985855d-d384-4b45-ae33-31a410998c3c

--- spec/ODSeisBP/read_terrex_segy.cc
+++ spec/ODSeisBP/read_terrex_segy.cc
@@ -2,9 +2,20 @@
  * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  * AUTHOR   : A.H. Bril 
  * DATE     : 2-12-2005
+
+ Reading the SEG-Y files downloadable from the ga.gov.au web site.
+ The files are indicated to be SEG-Y Rev. 0, but it just looks like that.
+ The main problem is that the coordinates are written in a weird place, as
+ floating point numbers rather than integers + scaling as the standard requires.
+ Further, we need to apply a false easting and northing, only indcated in the
+ text header.
+
+ Additionally, we want to extract the grav/mag data (although these are also
+ in the various spreadsheets, but this makes it easier to import).
+
 -*/
 
-static const char* rcsID = "$Id: read_terrex_segy.cc,v 1.1 2009-11-30 15:23:46 cvsbert Exp $";
+static const char* rcsID = "$Id: read_terrex_segy.cc,v 1.2 2009-12-01 13:41:48 cvsbert Exp $";
 
 #include "ioobj.h"
 #include "iopar.h"
@@ -12,66 +23,54 @@ static const char* rcsID = "$Id: read_terrex_segy.cc,v 1.1 2009-11-30 15:23:46 c
 #include "seistrc.h"
 #include "segytr.h"
 #include "segyhdr.h"
-#include "cubesampling.h"
-#include "seisselectionimpl.h"
 #include "ibmformat.h"
 #include "filegen.h"
 #include "strmprov.h"
-#include "ctxtioobj.h"
-#include "linekey.h"
 #include "progressmeter.h"
+#include "seisselectionimpl.h"
 
 #include "batchprog.h"
 #include <math.h>
+
+static const int nrsamples2load = 2501; // 10 seconds
+static const DataCharacteristics sampledc( DataCharacteristics::SI16 );
+static const float samplescale = 5000;
 
 
 class TerrexSEGYSeisTrcTranslator : public SEGYSeisTrcTranslator
 {
 				  isTranslator(TerrexSEGY,SeisTrc)
 public:
-    		TerrexSEGYSeisTrcTranslator( const char* s1, const char* s2 )
-		    : SEGYSeisTrcTranslator(s1,s2)		{ init(); }
-    		TerrexSEGYSeisTrcTranslator()
-		    : SEGYSeisTrcTranslator("TerrexSEGY","Terrex SEG-Y")
-								{ init(); }
 
-    void	init()
-    		{
-		    airmag_ = gravity_ = mUdf(float);
-		    fe_ = fn_ = 0;
-		}
+TerrexSEGYSeisTrcTranslator( const char* s1="TerrexSEGY",
+			     const char* s2="Terrex SEG-Y" )
+    : SEGYSeisTrcTranslator(s1,s2)
+{
+    airmag_ = gravity_ = mUdf(float);
+    fe_ = fn_ = 0;
+}
 
-    void	interpretBuf(SeisTrcInfo&);
+void interpretBuf( SeisTrcInfo& ti )
+{
+    SEGYSeisTrcTranslator::interpretBuf( ti );
+
+	// byte 21 (CDP) works, byte 181 is not OK (rather irregular):
+    ti.nr = IbmFormat::asInt( trchead_.buf+20 );
+
+	// As indicated in tape/text header:
+    ti.coord.x = fe_ + IbmFormat::asFloat( trchead_.buf+190 );
+    ti.coord.y = fn_ + IbmFormat::asFloat( trchead_.buf+194 );
+    airmag_ = IbmFormat::asFloat( trchead_.buf+198 );
+    gravity_ = IbmFormat::asFloat( trchead_.buf+202 );
+
+    ti.offset = 0;
+}
 
     float	airmag_;
     float	gravity_;
     double	fe_;
     double	fn_;
 };
-
-class TerrexLKProv : public LineKeyProvider
-{
-public:
-
-		TerrexLKProv( const char* lnm ) : lk_(lnm)	{}
-    LineKey	lineKey() const					{ return lk_; }
-
-    LineKey	lk_;
-
-};
-
-
-
-void TerrexSEGYSeisTrcTranslator::interpretBuf( SeisTrcInfo& ti )
-{
-    SEGYSeisTrcTranslator::interpretBuf( ti );
-    ti.nr = IbmFormat::asInt( trchead_.buf+20 ); // CDP is good, byte 181 sucks
-    ti.coord.x = fe_ + IbmFormat::asFloat( trchead_.buf+190 );
-    ti.coord.y = fn_ + IbmFormat::asFloat( trchead_.buf+194 );
-    ti.offset = 0;
-    airmag_ = IbmFormat::asFloat( trchead_.buf+198 );
-    gravity_ = IbmFormat::asFloat( trchead_.buf+202 );
-}
 
 
 #define mErrRet(s) { strm << s << std::endl; return false; }
@@ -102,33 +101,29 @@ bool BatchProgram::go( std::ostream& strm )
     }
 
     TerrexSEGYSeisTrcTranslator transl;
-    CubeSampling cs( false );
+    pars().get( "False Easting", transl.fe_ );
+    pars().get( "False Northing", transl.fn_ );
     if ( !transl.initRead(new StreamConn(segyfnm,Conn::Read)) )
 	mErrRet(transl.errMsg())
 
     SeisTrcWriter wrr( outioobj );
     Seis::RangeSelData* rsd = new Seis::RangeSelData;
-    rsd = new Seis::RangeSelData;
-    rsd->setIsAll( true );
-    rsd->lineKey().setLineName( linenm );
+    rsd->lineKey().setLineName( linenm ); rsd->setIsAll( true );
     wrr.setSelData( rsd );
 
-
-    pars().get( "False Easting", transl.fe_ );
-    pars().get( "False Northing", transl.fn_ );
-
-    SeisTrc trcin, trcout(2501,DataCharacteristics(DataCharacteristics::SI16));
+    SeisTrc trcin, trcout( nrsamples2load, sampledc );
     TextStreamProgressMeter pm( strm );
     while ( transl.read(trcin) )
     {
 	for ( int idx=0; idx<2501; idx++ )
-	    trcout.set( idx, 5000*trcin.get(idx,0), 0 );
+	    trcout.set( idx, samplescale*trcin.get(idx,0), 0 );
 	trcout.info() = trcin.info();
 
 	if ( wrr.put(trcout) )
 	    ++pm;
 	else
 	    mErrRet(wrr.errMsg())
+
 	if ( gravmagsd.usable() )
 	{
 	    *gravmagsd.ostrm << trcin.info().nr << '\t'
