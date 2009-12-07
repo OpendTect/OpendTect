29b2d3ac3e3bb64cab1d534e2f541522caa18ee5
Bert Bril <bert.bril@dgbes.com>
2009-12-07 14:02:21 +0000

    Introducing BendPoints2Coords class (made for SEG-Y 2D reading)
    git-svn-id: https://svn.opendtect.org/od/trunk@17984 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Algo/bendpoints2coords.h
@@ -0,0 +1,55 @@
+#ifndef bendpoints2coords_h
+#define bendpoints2coords_h
+
+/*
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Bert
+ Date:		Dec 2009
+ RCS:		$Id: bendpoints2coords.h,v 1.1 2009-12-07 14:02:21 cvsbert Exp $
+________________________________________________________________________
+
+*/
+
+#include "position.h"
+#include "typeset.h"
+#include <iosfwd>
+
+
+/*! Inter/Extra-polate bendpoints to get the coordinates.
+
+  Input is a set of coordinates and optionally ID numbers (if you don't pass
+  IDs they will be generated 0, 1, ....). Then you can give an ID (usually in
+  between the bend point IDs) and get the interpolated coordinate.
+
+  The bend points will be sorted on ID.
+
+ */
+
+mClass BendPoints2Coords
+{
+public:
+
+    			BendPoints2Coords(const TypeSet<Coord>&,
+					  const int* ids=0);
+    			BendPoints2Coords(std::istream&); //!< 'table' file
+
+    const TypeSet<Coord>& getCoords() const	{ return coords_; }
+    const TypeSet<int>&	getIDs() const		{ return nrs_; }
+
+    Coord		coordAt(float) const;
+
+    void		readFrom(std::istream&);
+
+protected:
+
+    TypeSet<int>	nrs_;
+    TypeSet<Coord>	coords_;
+
+    void		init(const TypeSet<Coord>&,const int*);
+    void		getIndexes(float,Interval<int>&) const;
+};
+
+
+#endif
---
+++ src/Algo/bendpoints2coords.cc
@@ -0,0 +1,104 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : K. Tingdahl
+ * DATE     : 9-3-1999
+-*/
+static const char* rcsID = "$Id: bendpoints2coords.cc,v 1.1 2009-12-07 14:02:21 cvsbert Exp $";
+
+#include "bendpoints2coords.h"
+#include "sorting.h"
+#include <iostream>
+
+
+BendPoints2Coords::BendPoints2Coords( const TypeSet<Coord>& crds,
+				      const int* nrs )
+{
+    init( crds, nrs );
+}
+
+
+BendPoints2Coords::BendPoints2Coords( std::istream& strm )
+{
+    readFrom( strm );
+}
+
+
+void BendPoints2Coords::readFrom( std::istream& strm )
+{
+    TypeSet<Coord> crds; TypeSet<int> nrs;
+    Coord crd; int nr;
+    while ( strm.good() )
+    {
+	strm >> nr >> crd.x >> crd.y;
+	if ( !strm.good() || (mIsZero(crd.x,1e-4) && mIsZero(crd.y,1e-4)) )
+	    break;
+	nrs += nr;
+	crds += crd;
+    }
+
+    init( crds, nrs.arr() );
+}
+
+
+void BendPoints2Coords::init( const TypeSet<Coord>& crds,
+			      const int* inpnrs )
+{
+    coords_.erase(); nrs_.erase();
+
+    const int sz = crds.size();
+    if ( sz < 1 )
+	return;
+    else if ( sz == 1 )
+    {
+	coords_ += crds[0]; nrs_ += inpnrs ? inpnrs[0] : 0;
+	return;
+    }
+    else if ( !inpnrs )
+    {
+	for ( int idx=0; idx<sz; idx++ )
+	    { coords_ += crds[idx]; nrs_ += idx; }
+	return;
+    }
+
+    TypeSet<int> nrs( inpnrs, sz );
+    mGetIdxArr(int,idxs,sz)
+    if ( !idxs ) return;
+    sort_coupled( nrs.arr(), idxs, sz );
+    for ( int idx=0; idx<sz; idx++ )
+    {
+	const int sidx = idxs[idx];
+	coords_ += crds[sidx]; nrs_ += inpnrs[sidx];
+    }
+    delete [] idxs;
+}
+
+
+Coord BendPoints2Coords::coordAt( float pos ) const
+{
+    const int sz = coords_.size();
+    if ( sz < 1 )
+	return Coord(0,0);
+    else if ( sz == 1 )
+	return coords_[0];
+
+    Interval<int> idxs; getIndexes( pos, idxs );
+    const Coord c0( coords_[idxs.start] ); const Coord c1( coords_[idxs.stop] );
+    const float frac = (pos-nrs_[idxs.start])
+		     / (nrs_[idxs.stop]-nrs_[idxs.start]);
+    return Coord( c0.x + frac * (c1.x - c0.x), c0.y + frac * (c1.y - c0.y) );
+}
+
+
+void BendPoints2Coords::getIndexes( float pos, Interval<int>& idxs ) const
+{
+    idxs.start = 0; idxs.stop = nrs_.size()-1;
+
+    while ( idxs.stop-idxs.start > 1 )
+    {
+	const int mididx = (idxs.stop+idxs.start) / 2;
+	if ( nrs_[mididx] > pos )
+	    idxs.stop = mididx;
+	else
+	    idxs.start = mididx;
+    }
+}
