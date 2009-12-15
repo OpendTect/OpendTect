8b8bf54e19047cfb7af51a74463fbb821bb10821
Ranojay Sen <ranojay.sen@dgbes.com>
2009-12-15 11:54:24 +0000

    uiSurvey selection added
    git-svn-id: https://svn.opendtect.org/od/trunk@18047 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/uiIo/uisurveyselect.cc
@@ -0,0 +1,90 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Ranojay Sen
+ Date:          Dec 2009
+________________________________________________________________________
+
+-*/
+#include "uisurveyselect.h"
+
+#include "oddirs.h"
+#include "uidialog.h"
+#include "uifileinput.h"
+#include "uilistbox.h"
+#include "uigeninput.h"
+#include "uisurvey.h"
+#include "uisetdatadir.h"
+
+
+uiSurveySelectDlg::uiSurveySelectDlg( uiParent* p )
+    : uiDialog(p,uiDialog::Setup("Survey Slection",
+				 "Select Survey",mNoHelpID))
+    , basedirnm_(GetBaseDataDir())
+{
+
+    datarootfld_ = new uiFileInput( this, "Data Root",
+			    uiFileInput::Setup(uiFileDialog::Gen,basedirnm_)
+			    .directories(true) );
+    datarootfld_->valuechanged.notify( 
+		  mCB(this,uiSurveySelectDlg,surveySelectCB) );
+
+    surveylistfld_ = new uiListBox( this,"Survey list", false, 
+				    uiLabeledListBox::AboveLeft );
+    surveylistfld_->attach( alignedBelow, datarootfld_ );
+    fillSurveyList();
+}
+
+
+uiSurveySelectDlg::~uiSurveySelectDlg()
+{
+}
+
+
+const char* uiSurveySelectDlg::getDataRoot() const
+{
+    return datarootfld_->fileName();
+}
+
+const BufferString uiSurveySelectDlg::getSurveyName() const
+{
+    return surveylistfld_->getText();
+}
+
+
+void uiSurveySelectDlg::fillSurveyList()
+{
+    BufferStringSet surveylist;
+    uiSurvey::getSurveyList( surveylist, getDataRoot() );
+    surveylistfld_->empty();
+    surveylistfld_->addItems( surveylist );
+}
+
+
+void uiSurveySelectDlg::surveySelectCB( CallBacker* )
+{
+    fillSurveyList();
+}
+
+
+uiSurveySelect::uiSurveySelect( uiParent* p )
+	: uiIOSelect(p,uiIOSelect::Setup("Select Survey"),
+		     mCB(this,uiSurveySelect,selectCB))
+{
+}
+
+
+uiSurveySelect::~uiSurveySelect()
+{
+}
+
+
+void uiSurveySelect::selectCB( CallBacker* )
+{
+    uiSurveySelectDlg dlg( this );
+    if( !dlg.go() ) return;
+
+    setInputText( dlg.getSurveyName() );
+}
+
