393e07c87b338ddc1d57b176ecacf2bcd4a7758c
cvsumesh <cvsumesh@a985855d-d384-4b45-ae33-31a410998c3c>
2009-12-23 04:28:11 +0000

    now one can get HorizonInfo for a particular ObjectID
    git-svn-id: https://svn.opendtect.org/od/trunk@18105 a985855d-d384-4b45-ae33-31a410998c3c

--- include/EarthModel/emhorizonpainter.h
+++ include/EarthModel/emhorizonpainter.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Umesh Sinha
  Date:		Mar 2009
- RCS:		$Id: emhorizonpainter.h,v 1.6 2009-08-13 10:35:54 cvsumesh Exp $
+ RCS:		$Id: emhorizonpainter.h,v 1.7 2009-12-23 04:28:11 cvsumesh Exp $
 ________________________________________________________________________
 
 -*/
@@ -24,17 +24,43 @@ ________________________________________________________________________
 
 namespace EM
 {
+    
+mClass EMObjPainterCallbackData
+{
+public:
+    		    EMObjPainterCallbackData() 
+		    {}
 
+    ObjectID	    objid_;
+    BufferString    name_;
+    bool	    enabled_;
+};
+    
+    
 mClass HorizonPainter : public CallBacker
 {
 public:
     			HorizonPainter(FlatView::Viewer&);
 			~HorizonPainter();
 
+	mStruct HorizonInfo
+    	{
+		EM::ObjectID	id_;
+		BufferString	name_;
+		bool		lineenabled_;
+		bool		seedenabled_;
+    	};
+
     void		addHorizon(const MultiID&);
     void                addHorizon(const EM::ObjectID&);
+
+    HorizonPainter::HorizonInfo* getHorizonInfo( const EM::ObjectID& );
+
     void                setHorizonIDs(const ObjectSet<MultiID>*);
     void                removeHorizon(const MultiID&);
+
+    void		enableHorizonLine(const EM::ObjectID&,bool);
+    void		enableHorizonSeed(const EM::ObjectID&,bool);
 
     void		setCubeSampling(const CubeSampling&,bool);
 
@@ -47,6 +73,9 @@ public:
     void		setLineName(const char*);
     TypeSet<int>&	getTrcNos()		{ return trcnos_; }
     TypeSet<float>&	getDistances()		{ return distances_; }
+
+    CNotifier<HorizonPainter,EMObjPainterCallbackData>	horizonadded_;
+    CNotifier<HorizonPainter,EMObjPainterCallbackData>	horizonremoved_;
 
 protected:
 
@@ -70,9 +99,10 @@ protected:
 
     int			loadinghorcount_;
     EM::ObjectID	horidtoberepainted_;
+    
+    ObjectSet<HorizonInfo>  horizoninfos_;
 
-    TypeSet<EM::ObjectID> horizonids_;
-    ObjectSet< ObjectSet<ObjectSet<FlatView::Annotation::AuxData> > > 										hormarkerlines_;
+    ObjectSet< ObjectSet<ObjectSet<FlatView::Annotation::AuxData> > > 	hormarkerlines_;
 
     ObjectSet<FlatView::Annotation::AuxData> horsmarkerseeds_;
     MarkerStyle2D	markerstyle_;
