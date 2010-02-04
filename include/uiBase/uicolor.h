1ec1ea52c063f787444e9f78c4a11afe92b7b31b
Jaap Glas <jaap.glas@dgbes.com>
2010-02-04 16:29:33 +0000

    Added getButton() function on behalf of Fault Toolbar.
    git-svn-id: https://svn.opendtect.org/od/trunk@18262 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiBase/uicolor.h
+++ include/uiBase/uicolor.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        A.H. Lammertink
  Date:          22/05/2000
- RCS:           $Id: uicolor.h,v 1.19 2009-07-22 16:01:20 cvsbert Exp $
+ RCS:           $Id: uicolor.h,v 1.20 2010-02-04 16:29:33 cvsjaap Exp $
 ________________________________________________________________________
 
 -*/
@@ -74,6 +74,8 @@ public:
 
     Notifier<uiColorInput> 	colorchanged;
     Notifier<uiColorInput> 	dodrawchanged;
+
+    uiPushButton*		getButton()	{ return colbut_; }
 
 protected:
 
