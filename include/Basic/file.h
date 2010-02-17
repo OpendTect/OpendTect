07f1b8e9bbfec0d32d29384f04b0a59fdd4ef6a1
Nanne Hemstra <nanne.hemstra@dgbes.com>
2010-02-17 04:59:31 +0000

    Added
    git-svn-id: https://svn.opendtect.org/od/trunk@18384 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Basic/file.h
@@ -0,0 +1,58 @@
+#ifndef file_h
+#define file_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Nanne Hemstra
+ Date:		January 2010
+ Contents:	File utitlities
+ RCS:		$Id: file.h,v 1.1 2010-02-17 04:59:31 cvsnanne Exp $
+________________________________________________________________________
+
+-*/
+
+#include "gendefs.h"
+
+
+/*!\brief Interface for several file and directory related services */
+
+namespace File
+{
+    mGlobal bool	exists(const char*);
+    mGlobal bool	isEmpty(const char*);
+    mGlobal bool	isFile(const char*);
+    mGlobal bool	isDirectory(const char*);
+
+    mGlobal bool	createLink(const char* from,const char* to);
+    mGlobal bool	isLink(const char*);
+    mGlobal const char*	linkTarget(const char* linkname);
+
+    mGlobal bool	isWritable(const char*);
+    mGlobal bool	makeWritable(const char*,bool recursive,bool yesno);
+    mGlobal bool	setPermissions(const char*,const char* perms,
+	    			       bool recursive);
+
+    mGlobal bool	createDir(const char*); 
+    mGlobal bool	rename(const char* oldname,const char* newname);
+    mGlobal bool	copyFile(const char* from,const char* to);
+    mGlobal bool	removeFile(const char*);
+    mGlobal bool	copyDir(const char* from,const char* to);
+    mGlobal bool	removeDir(const char*);
+
+			// Size/disk space. Returns 0 on any error
+    mGlobal int		getKbSize(const char*);
+    mGlobal int		getFreeMBytes(const char*);
+
+    mGlobal const char* timeCreated(const char*);
+    mGlobal const char*	timeLastModified(const char*);
+    mGlobal od_int64	getTimeInSeconds(const char*); //! Last modified time
+
+    mGlobal const char*	getCurrentPath();
+    mGlobal const char*	getHomePath();
+
+} // namespace File
+
+
+#endif
---
+++ src/Basic/file.cc
@@ -0,0 +1,179 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	A.H.Bril
+ Date:		3-5-1994
+ Contents:	File utitlities
+ RCS:		$Id: file.cc,v 1.1 2010-02-17 04:59:31 cvsnanne Exp $
+________________________________________________________________________
+
+-*/
+
+#include "file.h"
+#include "bufstring.h"
+
+#include <QDateTime>
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+
+
+namespace File
+{
+
+bool exists( const char* fnm )
+{ return QFile::exists( fnm ); }
+
+
+bool isEmpty( const char* fnm )
+{
+    QFileInfo qfi( fnm );
+    return qfi.size() < 1;
+}
+
+
+bool isFile( const char* fnm )
+{
+    QFileInfo qfi( fnm );
+    return qfi.isFile();
+}
+
+
+bool isDirectory( const char* fnm )
+{
+    QFileInfo qfi( fnm );
+    return qfi.isDir();
+}
+
+
+bool isLink( const char* fnm )
+{
+    QFileInfo qfi( fnm );
+    return qfi.isSymLink();
+}
+
+
+bool isWritable( const char* fnm )
+{
+    QFileInfo qfi( fnm );
+    return qfi.isWritable();
+}
+
+
+bool createDir( const char* fnm )
+{ QDir qdir; return qdir.mkpath( fnm ); }
+
+
+bool rename( const char* oldname, const char* newname )
+{ return QFile::rename( oldname, newname ); }
+
+
+bool createLink( const char* fnm, const char* linknm )
+{ return QFile::link( fnm, linknm ); }
+
+
+bool copyFile( const char* from, const char* to )
+{ return QFile::copy( from, to ); }
+
+
+bool copyDir( const char* from, const char* to )
+{
+    // TODO
+    return false;
+}
+
+
+bool removeFile( const char* fnm )
+{ return QFile::remove( fnm ); }
+
+
+bool removeDir( const char* dirnm )
+{
+    // TODO remove directories
+    return false;
+}
+
+
+bool makeWritable( const char* fnm, bool yn, bool recursive )
+{
+    // TODO
+    return false;
+}
+
+
+bool setPermissions( const char* fnm, const char* perms, bool recursive )
+{
+    // TODO
+    return false;
+}
+
+
+int getKbSize( const char* fnm )
+{
+    QFileInfo qfi( fnm );
+    od_int64 kbsz = qfi.size() / 1024;
+    return kbsz;
+}
+
+
+int getFreeMBytes( const char* fnm )
+{
+    // TODO
+    return -1;
+}
+
+static const char* sKeyTimeFormat = "ddd dd MMM yyyy , hh:mm:ss";
+
+const char* timeCreated( const char* fnm )
+{
+    static BufferString timestr;
+    QFileInfo qfi( fnm );
+    QString qstr = qfi.created().toString( sKeyTimeFormat );
+    timestr = qstr.toAscii().constData();
+    return timestr.buf();
+}
+
+
+const char* timeLastModified( const char* fnm )
+{
+    static BufferString timestr;
+    QFileInfo qfi( fnm );
+    QString qstr = qfi.lastModified().toString( sKeyTimeFormat );
+    timestr = qstr.toAscii().constData();
+    return timestr.buf();
+}
+
+
+od_int64 getTimeInSeconds( const char* fnm )
+{
+    QFileInfo qfi( fnm );
+    return qfi.lastModified().toTime_t();
+}
+
+
+const char* linkTarget( const char* linknm )
+{
+    static BufferString linkstr;
+    QFileInfo qfi( linknm );
+    linkstr = qfi.symLinkTarget().toAscii().constData();
+    return linkstr.buf();
+}
+
+
+const char* getCurrentPath()
+{
+    static BufferString pathstr;
+    pathstr = QDir::currentPath().toAscii().constData();
+    return pathstr.buf();
+}
+
+
+const char* getHomePath()
+{
+    static BufferString pathstr;
+    pathstr = QDir::homePath().toAscii().constData();
+    return pathstr.buf();
+}
+
+} // namespace File
