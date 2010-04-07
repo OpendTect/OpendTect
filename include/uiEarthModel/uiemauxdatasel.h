3b418f7b2cf905e784b425ca914a31405066897a
Nageswara Rao <nageswara.rao@dgbes.com>
2010-04-07 12:11:40 +0000

    Implemented source finle 'uiemauxdatasel.cc'.
    git-svn-id: https://svn.opendtect.org/od/trunk@18825 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiEarthModel/uiemauxdatasel.h
+++ include/uiEarthModel/uiemauxdatasel.h
@@ -7,40 +7,33 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        Nanne Hemstra
  Date:          July 2003
- RCS:           $Id: uiemauxdatasel.h,v 1.1 2010-03-30 20:56:59 cvskris Exp $
+ RCS:           $Id: uiemauxdatasel.h,v 1.2 2010-04-07 12:11:39 cvsnageswara Exp $
 ________________________________________________________________________
 
 -*/
 
-#include "uidialog.h"
-#include "uiiosurface.h"
-
-class CtxtIOObj;
-class IOObj;
-class MultiID;
-class uiGenInput;
-class uiIOObjSel;
-class uiSurfaceRead;
-class uiSurfaceWrite;
-
-namespace EM { class Surface; class SurfaceIODataSelection; class Horizon3D; }
+#include "uicompoundparsel.h"
+#include "multiid.h"
 
 
 /*! \brief Dialog for surface aux data selection. */
 
-mClass uiEMAuxDataSel : public uiGroup
+mClass uiEMAuxDataSel : public uiCompoundParSel
 {
 public:
 			uiEMAuxDataSel(uiParent*,const char* label,
-				    const MultiID* =0, const char* auxdata=0 );
+				       const MultiID* =0,
+				       const char* auxdata=0 );
     const MultiID&	getSurfaceID() const;
     const char*		getAuxDataSel() const;
 
 protected:
 
-    uiPushButton*	selectbutton_;
-    uiLabel*		label_;
-    uiGenInput*		textfld_;
+    virtual BufferString	getSummary() const;
+    bool			butPushCB(CallBacker*);
+
+    MultiID			hormid_;
+    const char*			auxdatanm_;
 };
 
 #endif
---
+++ src/uiEarthModel/uiemauxdatasel.cc
@@ -0,0 +1,78 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Nageswara
+ Date:          April 2010
+ RCS:           $Id: uiemauxdatasel.cc,v 1.1 2010-04-07 12:11:40 cvsnageswara Exp $
+________________________________________________________________________
+
+-*/
+
+#include "uiemauxdatasel.h"
+
+#include "uidialog.h"
+#include "uiiosurface.h"
+
+#include "bufstringset.h"
+#include "emsurfacetr.h"
+#include "ioman.h"
+#include "ptrman.h"
+
+uiEMAuxDataSel::uiEMAuxDataSel( uiParent* p, const char* lbl,
+       				const MultiID* hormid, const char* auxdatanm )
+    : uiCompoundParSel( p, lbl )
+    , hormid_(hormid ? *hormid : -1)
+    , auxdatanm_(auxdatanm)
+{
+    butPush.notify( mCB(this,uiEMAuxDataSel,butPushCB) );
+}
+
+
+bool uiEMAuxDataSel::butPushCB( CallBacker* )
+{
+    uiDialog dlg( this, uiDialog::Setup("Horizon/Attributes Selection","","") );
+    PtrMan<uiSurfaceRead> surfacefld = new uiSurfaceRead( &dlg,
+	    	  uiSurfaceRead::Setup(EMHorizon3DTranslatorGroup::keyword())
+		  		 .withsectionfld(false)
+	   			 .multiattribsel(false) );
+    surfacefld->setInput( hormid_ );
+    BufferStringSet attribname;
+    attribname.add( auxdatanm_ );
+    surfacefld->setSelAttributes( attribname );
+
+    if ( !dlg.go() )
+	return false;
+
+    if ( surfacefld->selIOObj() )
+	return false;
+
+    hormid_ = surfacefld->selIOObj()->key();
+    BufferStringSet selattribs;
+    surfacefld->getSelAttributes( selattribs );
+    if ( selattribs.size() <=0 )
+	return false;
+
+    auxdatanm_ = selattribs.get( 0 );
+    return true;
+}
+
+
+const MultiID& uiEMAuxDataSel::getSurfaceID() const
+{ return hormid_; }
+
+
+const char* uiEMAuxDataSel::getAuxDataSel() const
+{ return auxdatanm_; }
+
+
+BufferString uiEMAuxDataSel::getSummary() const
+{
+    PtrMan<IOObj> ioobj = IOM().get( hormid_ );
+    if ( !ioobj )
+	return "";
+
+    BufferString summary( ioobj->name() );
+    summary.add( "[" ).add( auxdatanm_ ).add( "]" );
+    return summary;
+}
