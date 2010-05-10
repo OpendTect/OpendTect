7c2d5bdccc67a158c0be31dc320eaf4c4031d713
Bert Bril <bert.bril@dgbes.com>
2010-05-10 12:08:17 +0000

    removed Solaris; added __is32bits__. Don't know what Mac OS X is, made it 64.
    git-svn-id: https://svn.opendtect.org/od/trunk@19073 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/plfdefs.h
+++ include/Basic/plfdefs.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	A.H.Bril
  Contents:	Defines that encapsulate system things
- RCS:		$Id: plfdefs.h,v 1.26 2009-07-22 16:01:14 cvsbert Exp $
+ RCS:		$Id: plfdefs.h,v 1.27 2010-05-10 12:08:17 cvsbert Exp $
 ________________________________________________________________________
 
 */
@@ -22,11 +22,9 @@ HDIR can be:
 	lux		Linux
 	win		MS Windows
 	mac		Apple Mac OSX
-	sun5		Sun Solaris 5.x
 
 Also, PLFSUBDIR should be defined. It is identical to HDIR, except for:
 Linux:   lux32 or lux64
-Solaris: sol32
 Windows: win32 or win64
 
 Then you get:
@@ -37,7 +35,6 @@ OS type:
 
 Machine:
 
-	__sun__		Sun
 	__pc__		PC
 			(Mac is just __mac__ below)
 
@@ -45,10 +42,9 @@ Platform:
 
 	__win32__	Windows 32 bits (x86)
 	__win64__	Windows 64 bits (AMD)
-	__mac__		Mac
 	__lux32__	Linux 32 bits (x86)
 	__lux64__	Linux 64 bits (AMD)
-	__sol32__	Solaris (Sparc)
+	__mac__		Mac
 
 	__plfsubdir__	String like "win32", "lux32" etc.
 	__plfname__	String like "MS Windows 32 bits", "Linux 32 bits"
@@ -56,7 +52,6 @@ Platform:
 Compiler type:
 
 	__gnuc__	GNU gcc
-	__sunc__	Sun c
 	__msvc__	MS Visual C++
 
 Language:
@@ -71,6 +66,7 @@ Always defined:
 
 	__islittle__	'true' if little endian machine, false otherwise
 	__islinux__	'true' on Linux, 'false' otherwise
+	__is32bits__	'true' on 32 bits platforms, 'false' otherwise
 	__ismac__	'true' on Mac, 'false' otherwise
 	__iswin__	'true' on Windows, 'false' otherwise
 
@@ -101,9 +97,6 @@ Always defined:
 # endif
 #endif
 
-#ifdef sun5
-# define __unix__ 1
-#endif
 #ifdef mac
 # define __unix__ 1
 #endif
@@ -134,24 +127,8 @@ Always defined:
 /*____________________________________________________________________________*/
 /* Machine type	*/
 
-#undef __sun__
 #undef __pc__
 #undef __little__
-
-#ifdef sun5
-# define __sun__ 1
-#endif
-#ifdef sparc
-# undef __sun__
-# define __sun__ 2
-#endif
-#ifdef sun
-# undef __sun__
-# define __sun__ 3
-#endif
-#ifdef __sun__
-# define __sol32__ 1
-#endif
 
 #ifdef lux
 # define __pc__ 1
@@ -176,26 +153,27 @@ Always defined:
 #ifdef __win32__
 # define __plfsubdir__	"win32"
 # define __plfname__	"MS Windows 32 bits"
+# define __is32bits__	true
 #endif
 #ifdef __win64__
 # define __plfsubdir__	"win64"
 # define __plfname__	"MS Windows 64 bits"
+# define __is32bits__	false
 #endif
 #ifdef __lux32__
 # define __plfsubdir__	"lux32"
 # define __plfname__	"Linux 32 bits"
+# define __is32bits__	true
 #endif
 #ifdef __lux64__
 # define __plfsubdir__	"lux64"
 # define __plfname__	"Linux 64 bits"
+# define __is32bits__	false
 #endif
 #ifdef __mac__
 # define __plfsubdir__	"mac"
 # define __plfname__	"Mac"
-#endif
-#ifdef __sol32__
-# define __plfsubdir__	"sol32"
-# define __plfname__	"Solaris/Sparc"
+# define __is32bits__	false
 #endif
 
 /*____________________________________________________________________________*/
@@ -219,7 +197,6 @@ Always defined:
 /* Compiler type */
 
 #undef __gnu__
-#undef __sunc__
 #undef __msvc__
 #ifdef lux
 # define __gnuc__ 1
@@ -227,11 +204,6 @@ Always defined:
 #ifdef __GNUC__
 # undef __gnuc__
 # define __gnuc__ 1
-#endif
-#ifdef sun5
-# ifndef __gnuc__
-#  define __sunc__ 1
-# endif
 #endif
 #if defined( win ) || defined( WIN32 )
 # ifndef __gnuc__
@@ -244,7 +216,7 @@ Always defined:
 #endif
 
 
-/* Finally, support some common flags */
+/* And, probably unnecessary, for external header files: */
 #ifndef NeedFunctionPrototypes
 # define NeedFunctionPrototypes 1
 #endif
