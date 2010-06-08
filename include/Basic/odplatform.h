d88f7715d5edaaabde8abe4cd46a6c06da2d9d42
Bert Bril <bert.bril@dgbes.com>
2010-06-08 10:17:13 +0000

    Removed the virtual stuff: didn't get me anywhere
    git-svn-id: https://svn.opendtect.org/od/trunk@19252 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/odplatform.h
+++ include/Basic/odplatform.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        Bert
  Date:          May 2010
- RCS:           $Id: odplatform.h,v 1.5 2010-06-03 08:22:48 cvsbert Exp $
+ RCS:           $Id: odplatform.h,v 1.6 2010-06-08 10:17:13 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -21,53 +21,50 @@ mClass Platform
 {
 public:
 
-    virtual		~Platform()		{};
+    static const Platform& local();	//!< This platform
 
-    static const Platform&	local();	//!< This platform
+    enum Type	{ Lin32, Lin64, Win32, Win64, Mac };
+		DeclareEnumUtils(Type)
 
-    enum Type		{ Lin32, Lin64, Win32, Win64, Mac };
-			DeclareEnumUtils(Type)
+    		Platform();		//!< This platform
+    		Platform( Type t )	//!< That platform
+		    : type_(t)		{}
+		Platform( const char* s, bool isshortnm )
+					{ set(s,isshortnm); }
+		Platform( bool iswin, bool is32, bool ismac=false )
+		    			{ set(iswin,is32,ismac); }
+    bool        operator ==( const Platform& p ) const
+					{ return type_ == p.type_; }
+    bool        operator ==( const Platform::Type& t ) const
+					{ return type_ == t; }
 
-    			Platform();		//!< This platform
-    			Platform( Type t )	//!< That platform
-			    : type_(t)		{}
-			Platform( const char* s, bool isshortnm )
-						{ set(s,isshortnm); }
-			Platform( bool iswin, bool is32, bool ismac=false )
-			    			{ set(iswin,is32,ismac); }
-    			Platform( const Platform& p )
-			    : type_(p.type())	{}
-    virtual bool        operator ==( const Platform& p ) const
-			{ return type() == p.type(); }
-    virtual bool        operator ==( const Platform::Type& t ) const
-			{ return type() == t; }
+    const char*	longName() const
+    			{ return eString(Type,type_); }
+    const char*	shortName() const;	//!< mac, lux32, win64, etc.
 
-    virtual const char*	longName() const
-    			{ return eString(Type,type()); }
-    virtual const char*	shortName() const;	//!< mac, lux32, win64, etc.
+    static bool	isValidName(const char*,bool isshortnm);
+    void	set(const char*,bool isshortnm);
+    inline void	set( bool iswin, bool is32, bool ismac=false )
+    		{ type_ = ismac ? Mac : (iswin	? (is32 ? Win32 : Win64)
+						: (is32 ? Lin32 : Lin64) ); }
 
-    static bool		isValidName(const char*,bool isshortnm);
-    virtual void	set(const char*,bool isshortnm);
-    inline virtual void	set( bool iswin, bool is32, bool ismac=false )
-    			{ type_ = ismac ? Mac : (iswin ? (is32 ? Win32 : Win64)
-				       : (is32 ? Lin32 : Lin64) ); }
+    inline bool	isWindows() const
+			{ return type_ == Win32 || type_ == Win64; }
+    inline bool	isLinux() const
+			{ return type_ == Lin32 || type_ == Lin64; }
+    inline bool	isMac() const
+			{ return type_ == Mac; }
 
-    inline virtual bool	isWindows() const
-			{ return type() == Win32 || type() == Win64; }
-    inline virtual bool	isLinux() const
-			{ return type() == Lin32 || type() == Lin64; }
-    inline virtual bool	isMac() const
-			{ return type() == Mac; }
+    inline bool	is32Bits() const
+			{ return type_ != Win64 && type_ != Lin64; }
 
-    inline virtual bool	is32Bits() const
-			{ return type() != Win64 && type() != Lin64; }
-
-    virtual Type	type() const		{ return type_; }
-    virtual void	setType( Type t )	{ type_ = t; }
+    inline Type	type() const		{ return type_; }
+    inline Type& type()			{ return type_; }
+    inline void	setType( Type t )	{ type_ = t; }
 
 protected:
 
-    Type		type_;
+    Type	type_;
 
 };
 
