1ccf25c9dc8cd6ce6943edcaff5547cb96738a69
Nanne Hemstra <nanne.hemstra@dgbes.com>
2010-06-11 07:06:44 +0000

    update, only do special things on Windows
    git-svn-id: https://svn.opendtect.org/od/trunk@19279 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/odcomplex.h
+++ include/Basic/odcomplex.h
@@ -7,12 +7,18 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Nanne Hemstra
  Date:		January 2010
- RCS:		$Id: odcomplex.h,v 1.1 2010-06-11 06:42:52 cvsnanne Exp $
+ RCS:		$Id: odcomplex.h,v 1.2 2010-06-11 07:06:44 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
 
 #include <complex>
+
+#ifndef __win__
+
+typedef std::complex<float> float_complex;
+
+#else
 
 namespace std
 {
@@ -21,20 +27,22 @@ template <class T>
 class odcomplex : public complex<T>
 {
 public:
-    			template <class U>
-			odcomplex( const complex<U>& c )
-			    : complex<T>(c.real(),c.imag())	{}
+    		template <class U>
+		odcomplex( const complex<U>& c )
+		    : complex<T>(c.real(),c.imag())	{}
 
-			odcomplex( const T& r = T(), const T& i = T() )
-			    : complex<T>(r,i)	{}
+		odcomplex( const T& r = T(), const T& i = T() )
+		    : complex<T>(r,i)	{}
 
-#ifdef __win__
-    T&	real()	{ return (this->_Val[_RE]); }
-    T&	imag()	{ return (this->_Val[_IM]); }
-#endif
+    T&		real()	{ return (this->_Val[_RE]); }
+    T&		imag()	{ return (this->_Val[_IM]); }
 };
 
-}
+} // namespace std
+
+typedef std::odcomplex<float> float_complex;
+
+#endif
 
 #endif
 
