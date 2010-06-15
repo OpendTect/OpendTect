99c7f6a2cfca3ba3f29dae19c487aef7e5e1bca8
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2010-06-15 19:03:46 +0000

    *** empty log message ***
    git-svn-id: https://svn.opendtect.org/od/trunk@19289 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Seis/seisposindexer.h
+++ include/Seis/seisposindexer.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        Bert
  Date:          Nov 2008
- RCS:           $Id: seisposindexer.h,v 1.6 2010-06-15 18:42:01 cvskris Exp $
+ RCS:           $Id: seisposindexer.h,v 1.7 2010-06-15 19:03:46 cvskris Exp $
 ________________________________________________________________________
 
 
@@ -15,6 +15,10 @@ ________________________________________________________________________
 
 #include "seisposkey.h"
 #include "sets.h"
+#include "thread.h"
+
+
+template <class T> class DataInterpreter;
 
 namespace Seis
 {
@@ -22,10 +26,9 @@ namespace Seis
 mClass PosKeyList
 {
 public:
-
+    virtual		~PosKeyList()			{}
     virtual od_int64	size() const			= 0;
     virtual PosKey	key(od_int64) const		= 0;
-
 };
 
 /*!\brief builds an index of a list of positions, making it easy to find a
@@ -39,7 +42,8 @@ public:
 mClass PosIndexer
 {
 public:
-				PosIndexer(const PosKeyList&,bool doindex);
+
+				PosIndexer(const PosKeyList&, bool doindex);
     virtual			~PosIndexer();
 
     od_int64			findFirst(const BinID&) const;
@@ -53,9 +57,6 @@ public:
     				//!< -1 = inl not found or empty
    				//!< -2 crl/trcnr not found
    				//!< -3 offs not found
-
-    bool			dumpTo(std::ostream&) const;
-    bool			readFrom(std::istream&);
 
     inline bool			validIdx( od_int64 idx ) const
 				{ return idx >= 0 && idx < maxidx_; }
@@ -71,7 +72,31 @@ public:
     inline Interval<int>	trcNrRange() const	{ return crlrg_; }
     inline Interval<float>	offsetRange() const	{ return offsrg_; }
 
+    bool			dumpTo(std::ostream& strm) const;
+    bool			readFrom(const char* nm, od_int64 offset,
+	    				bool all,
+	    				DataInterpreter<int>*  =0,
+					DataInterpreter<od_int64>* =0,
+					DataInterpreter<float>* =0 );
+
 protected:
+    bool			readHeader(DataInterpreter<int>*,
+					DataInterpreter<od_int64>*,
+					DataInterpreter<float>* );
+    bool			readLine(TypeSet<int>& crl,
+				    TypeSet<od_int64>&,
+				    DataInterpreter<int>*,
+				    DataInterpreter<od_int64>* ) const;
+
+    std::istream*		strm_;
+    DataInterpreter<int>*	int32interp_;
+    DataInterpreter<od_int64>*	int64interp_;
+    TypeSet<od_int64>		inlfileoffsets_;
+
+    mutable Threads::Mutex	lock_;
+    TypeSet<od_int64>		curidxset_;
+    TypeSet<int>		curcrlset_;
+    int				curinl_;
 
     const PosKeyList&		pkl_;
     bool			is2d_;
@@ -87,7 +112,7 @@ protected:
 
     void			empty();
     void			add(const PosKey&,od_int64);
-    int				getFirstIdxs(const BinID&,int&,int&) const;
+    int				getFirstIdxs(const BinID&,int&,int&);
 };
 
 
