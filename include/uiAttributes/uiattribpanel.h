5dffd0d2b4e6171b74b3d268c75b38022289de7a
Helene Huck <helene.huck@dgbes.com>
2010-07-05 10:03:18 +0000

    added uiattribpanel.h/cc + reworked Makefile
    git-svn-id: https://svn.opendtect.org/od/trunk@19454 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiAttributes/uiattribpanel.h
@@ -0,0 +1,60 @@
+#ifndef uiattribpanel_h
+#define uiattribpanel_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        H. Huck
+ Date:          Jun 2010
+________________________________________________________________________
+
+-*/
+
+#include "uiattrdesced.h"
+#include "cubesampling.h"
+#include "linekey.h"
+#include "attribdescid.h"
+
+class FlatDataPack;
+class uiFlatViewMainWin;
+
+template <class T> class Array2D;
+namespace Attrib { class EngineMan; class DescSet; class DataCubes;
+    		   class Data2DHolder; class Processor; }
+
+/*! \brief Attribute preview in a 2d viewer */
+
+class uiAttribPanel
+{
+public:
+    			uiAttribPanel(uiParent*);
+    			~uiAttribPanel();
+    void                compAndDispAttrib(Attrib::DescSet*,
+	    				  const Attrib::DescID&,
+					  const CubeSampling&,const LineKey&);
+    			//<! descset becomes mine!
+
+protected:
+
+    FlatDataPack*	computeAttrib();
+    Attrib::EngineMan*	createEngineMan();
+    virtual void	createAndDisplay2DViewer(FlatDataPack*);
+    FlatDataPack*	createFDPack(const Attrib::Data2DHolder&) const;
+    FlatDataPack*	createFDPack(Attrib::EngineMan*,
+	    			     Attrib::Processor*) const;
+
+    virtual const char*		getProcName()	{ return "Computing attribute";}
+    virtual const char*		getPackName()	{ return "Attribute pack"; }
+    virtual const char*		getPanelName()	{ return "Attribute preview"; }
+
+    uiFlatViewMainWin*		flatvwin_;
+    CubeSampling		cs_;
+    LineKey			lk_;
+    Attrib::DescID		attribid_;
+    Attrib::DescSet*    	dset_;
+    uiParent*			parent_;
+};
+
+
+#endif
---
+++ src/uiAttributes/uiattribpanel.cc
@@ -0,0 +1,157 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        H. Huck
+ Date:          Jun 2010
+________________________________________________________________________
+
+-*/
+
+#include "uiattribpanel.h"
+#include "attribparam.h"
+#include "attribsel.h"
+#include "attribdesc.h"
+#include "attribdescset.h"
+#include "attribengman.h"
+#include "attribprocessor.h"
+#include "attribfactory.h"
+#include "attribdatacubes.h"
+#include "attribdataholder.h"
+#include "attribdatapack.h"
+#include "arrayndimpl.h"
+#include "survinfo.h"
+#include "uitaskrunner.h"
+#include "uiflatviewer.h"
+#include "uiflatviewmainwin.h"
+#include "uiflatviewstdcontrol.h"
+#include "uimsg.h"
+
+using namespace Attrib;
+
+
+uiAttribPanel::uiAttribPanel( uiParent* p )
+    : attribid_( DescID::undef() )
+    , dset_( 0 )
+    , flatvwin_( 0 )
+    , parent_( p )
+{
+}
+
+
+uiAttribPanel::~uiAttribPanel()
+{
+    if ( dset_ ) delete dset_;
+    if ( flatvwin_ ) delete flatvwin_;
+}
+
+
+FlatDataPack* uiAttribPanel::computeAttrib()
+{
+    BufferString errmsg;
+    RefMan<Attrib::Data2DHolder> d2dh = new Attrib::Data2DHolder();
+    PtrMan<EngineMan> aem = createEngineMan();
+	
+    PtrMan<Processor> proc = dset_->is2D() ? 
+			    aem->createScreenOutput2D( errmsg, *d2dh ) 
+			    : aem->createDataCubesOutput( errmsg, 0  );
+    if ( !proc )
+    {
+	uiMSG().error( errmsg );
+	return 0;
+    }
+
+    proc->setName( getProcName() );
+    uiTaskRunner dlg( parent_ );
+    if ( !dlg.execute(*proc) )
+	return 0;
+
+    FlatDataPack* fdpack = dset_->is2D() ? createFDPack( *d2dh )
+					 : createFDPack( aem, proc );
+    if ( !fdpack ) return 0;
+    
+    fdpack->setName( getPackName() );
+    DPM(DataPackMgr::FlatID()).add( fdpack );
+    return fdpack;
+}
+
+
+EngineMan* uiAttribPanel::createEngineMan()
+{
+    EngineMan* aem = new EngineMan;
+
+    TypeSet<SelSpec> attribspecs;
+    SelSpec sp( 0, attribid_ );
+    attribspecs += sp;
+
+    aem->setAttribSet( dset_ );
+    aem->setAttribSpecs( attribspecs );
+    aem->setLineKey( lk_ );
+    aem->setCubeSampling( cs_ );	//should be only 1 trace
+    return aem;
+}
+
+
+FlatDataPack* uiAttribPanel::createFDPack( 
+					    const Data2DHolder& d2dh ) const
+{
+    return new Attrib::Flat2DDHDataPack( attribid_, d2dh, true );
+}
+
+
+FlatDataPack* uiAttribPanel::createFDPack( EngineMan* aem,
+						     Processor* proc ) const
+{
+    const Attrib::DataCubes* output = aem->getDataCubesOutput( *proc );
+    if ( !output ) return 0;
+    
+    output->ref();
+    FlatDataPack* fdpack = new Attrib::Flat3DDataPack( attribid_, *output, -1 );
+	    
+    output->unRef();
+    return fdpack;
+}
+
+
+void uiAttribPanel::createAndDisplay2DViewer( FlatDataPack* fdpack )
+{
+    if ( flatvwin_ )
+	flatvwin_->viewer().setPack( false, fdpack->id(), false );
+    else
+    {
+	flatvwin_ =
+	    new uiFlatViewMainWin( 0, uiFlatViewMainWin::Setup(getPanelName()));
+	uiFlatViewer& vwr = flatvwin_->viewer();
+	vwr.setPack( false, fdpack->id(), false );
+	vwr.setInitialSize( uiSize(400,600) );
+	FlatView::Appearance& app = vwr.appearance();
+	app.annot_.setAxesAnnot( true );
+	app.setDarkBG( false );
+	app.setGeoDefaults( true );
+	app.ddpars_.show( false, true );
+	flatvwin_->addControl( new uiFlatViewStdControl( flatvwin_->viewer(),
+			       uiFlatViewStdControl::Setup(0) ) );
+	flatvwin_->setDeleteOnClose( false );
+    }
+    
+    flatvwin_->show();
+}
+
+
+void uiAttribPanel::compAndDispAttrib( DescSet* dset, const DescID& mpid,
+				       const CubeSampling& cs,
+				       const LineKey& lk )
+{
+    attribid_ = mpid;
+    cs_ = cs;
+    lk_ = lk;
+    
+    if ( dset_ ) delete dset_;
+    dset_ = dset;
+
+    FlatDataPack* fdpack = computeAttrib();
+    if ( fdpack )
+	createAndDisplay2DViewer( fdpack );
+    else
+	pErrMsg( "Error during attribute computation" );
+}
