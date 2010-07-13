41d49550953cd78cca4cbb20e6b853760717a543
Bert Bril <bert.bril@dgbes.com>
2010-07-13 10:49:04 +0000

    Little rework to get most interesting fns at top, vague fns to bottom
    git-svn-id: https://svn.opendtect.org/od/trunk@19513 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/survinfo.h
+++ include/Basic/survinfo.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	A.H.Bril
  Date:		9-4-1996
- RCS:		$Id: survinfo.h,v 1.92 2010-07-08 11:16:47 cvsbert Exp $
+ RCS:		$Id: survinfo.h,v 1.93 2010-07-13 10:49:04 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -17,7 +17,6 @@ ________________________________________________________________________
 #include "ranges.h"
 #include "rcol2coord.h"
 #include "enums.h"
-
 class ascostream;
 class IOPar;
 class CubeSampling;
@@ -30,7 +29,7 @@ The surveyinfo is the primary source for ranges and steps. It also provides
 the transformation between inline/xline <-> coordinates and lat/long estimates.
 
 Note: the Z range step is only a default. It should not be used further
-because different cubes have different sample rates.
+because different cubes/lines have different sample rates.
 
 The ranges are defined for two cubes: the entire survey, and a 'working area'.
 Normally, you'll want to have the working area.
@@ -49,28 +48,47 @@ public:
 
 			~SurveyInfo();
     bool		isValid() const		{ return valid_; }
+    bool		has2D() const;
+    bool		has3D() const;
 
-    enum Pol2D      	{ No2D=0, Both2DAnd3D=1, Only2D=2 };
-    			DeclareEnumUtils(Pol2D);
-
-    const CubeSampling&	sampling( bool work ) const
-    			{ return work ? wcs_ : cs_; }
     StepInterval<int>	inlRange(bool work) const;
     StepInterval<int>	crlRange(bool work) const;
     const StepInterval<float>& zRange( bool work ) const;
     int			inlStep() const;
     int			crlStep() const;
-    float		inlDistance() const;
-    			/*!<\returns the distance for one increment in
-			    inline. */
-    float		crlDistance() const;
-    			/*!<\returns the distance for one increment in
-			    crossline. */
     float		zStep() const;
-    int			maxNrTraces(bool work) const;
+    float		inlDistance() const; //!< distance for one increment
+    float		crlDistance() const;
+    const CubeSampling&	sampling( bool work ) const
+    			{ return work ? wcs_ : cs_; }
 
-    void		setWorkRange(const CubeSampling&);
-    Notifier<SurveyInfo> workRangeChg;
+    Coord		transform( const BinID& b ) const
+			{ return b2c_.transform(b); }
+    BinID		transform(const Coord&) const;
+    			/*!<\note BinID will be snapped using work step. */
+
+    enum Unit		{ Second, Meter, Feet };
+    Unit		xyUnit() const;
+    Unit		zUnit() const;
+    inline bool		xyInFeet() const	{ return xyinfeet_;}
+    inline bool		zIsTime() const		{ return zistime_; }
+    inline bool		zInMeter() const	{ return !zistime_ &&!zinfeet_;}
+    inline bool		zInFeet() const		{ return !zistime_ && zinfeet_;}
+    const char*		getXYUnitString(bool withparens=true) const;
+    const char*		getZUnitString(bool withparens=true) const;
+    const char*		getZDomainString() const;
+    float		zFactor() const;
+    			//!< Factor between real and displayed unit in UI
+    bool		depthsInFeetByDefault() const;
+
+    Coord		minCoord(bool work) const;
+    Coord		maxCoord(bool work) const;
+    bool		isInside(const BinID&,bool work) const;
+    bool		isReasonable(const BinID&) const;
+				//!< Checks if in or near survey
+    bool		isReasonable(const Coord&) const;
+				//!< Checks if in or near survey
+    int			maxNrTraces(bool work) const;
 
     void		checkInlRange(Interval<int>&,bool work) const;
 			//!< Makes sure range is inside
@@ -81,80 +99,19 @@ public:
     bool		includes(const BinID&,const float,bool work) const;
 			//!< Returns true when pos is inside survey-range
 
-    enum Unit		{ Second, Meter, Feet };
-    Unit		xyUnit() const;
-    Unit		zUnit() const;
-    inline bool		xyInFeet() const	{ return xyinfeet_;}
-    inline bool		zIsTime() const		{ return zistime_; }
-    inline bool		zInMeter() const	{ return !zistime_ &&!zinfeet_;}
-    inline bool		zInFeet() const		{ return !zistime_ && zinfeet_;}
-    void		setXYInFeet( bool yn=true ) { xyinfeet_ = yn; }
-    void		setZUnit(bool istime,bool infeet=false);
-    const char*		getXYUnitString(bool withparens=true) const;
-    const char*		getZUnitString(bool withparens=true) const;
-    const char*		getZDomainString() const;
-    static float	defaultXYtoZScale(Unit zunit,Unit xyunit);
-    			/*!<Gives a ballpark figure of how to scale Z to
-			    make it comparable to XY. */
-    float		zScale() const;
-    			/*!<Gives a ballpark figure of how to scale Z to
-			    make it comparable to XY. */
-    float		zFactor() const;
-    			//!< Factor between real and displayed unit in UI
-    bool		depthsInFeetByDefault() const;
-    void		setSurvDataType( Pol2D typ )	{ survdatatype_ = typ; }
-    Pol2D		getSurvDataType() const	{ return survdatatype_; }
-    bool		has2D() const;
-    bool		has3D() const;
-    const char*		comment() const		{ return comment_.buf(); }
-
     void		snap(BinID&,BinID direction=BinID(0,0)) const;
 			//!< dir = 0 : auto; -1 round downward, 1 round upward
     void		snapStep(BinID&,BinID direction=BinID(0,0)) const;
     			//!< see snap() for direction
     void		snapZ(float&,int direction=0) const;
     			//!< see snap() for direction
-    Coord		transform( const BinID& b ) const
-			{ return b2c_.transform(b); }
-    BinID		transform(const Coord&) const;
-    			/*!<\note The returned BinID will be snapped according
-			  	  to the work step. */
-    const RCol2Coord&	binID2Coord() const	{ return b2c_; }
-    void		get3Pts(Coord c[3],BinID b[2],int& xline) const;
-    const LatLong2Coord& latlong2Coord() const	{ return ll2c_; }
-    bool		isClockWise() const;
-    			/*!< Orientation is determined by rotating the
-			     inline axis to the crossline axis. */
-    float		computeAngleXInl() const;
-    			/*!< It's the angle (0 to pi/2) between
-			     the X-axis and the Inl-axis (not an inline) */
-
-    Coord		minCoord(bool work) const;
-    Coord		maxCoord(bool work) const;
-    bool		isInside(const BinID&,bool work) const;
-    bool		isReasonable(const BinID&) const;
-			//!< Checks if in or near survey
-    bool		isReasonable(const Coord&) const;
-			//!< Checks if in or near survey
-
-    static const char*	sKeyInlRange();
-    static const char*	sKeyCrlRange();
-    static const char*	sKeyXRange();
-    static const char*	sKeyYRange();
-    static const char*	sKeyZRange();
-    static const char*	sKeyWSProjName();
-    static const char*	sKeyXYInFt();
-    static const char*	sKeyDpthInFt(); //!< Not used by SI, just a UI default
-    static const char*	sKeySurvDataType();
 
     const IOPar&	pars() const			{ return pars_; }
-    			// Project name will be stored
-    const char*		getWSProjName() const	{ return wsprojnm_.buf(); }
-    			// Password only in memory this session
-    const char*		getWSPwd() const	{ return wspwd_.buf(); }
-    BufferString	getDirName() const	{ return dirname; }
 
-	// Some fns moved to bottom that have 'no user servicable parts inside'
+    // Some public fns moved to bottom because they are rarely used; some fns
+    // that have 'no user servicable parts inside' are at the very bottom
+
+    enum Pol2D      	{ No2D=0, Both2DAnd3D=1, Only2D=2 };
 
 protected:
 
@@ -204,6 +161,51 @@ private:
     RCol2Coord::RCTransform	rdytr;
 
 public:
+
+	// These fns are rarely used by non-specialist classes.
+
+    void		setWorkRange(const CubeSampling&);
+    Notifier<SurveyInfo> workRangeChg;
+
+    const RCol2Coord&	binID2Coord() const	{ return b2c_; }
+    void		get3Pts(Coord c[3],BinID b[2],int& xline) const;
+    const LatLong2Coord& latlong2Coord() const	{ return ll2c_; }
+    bool		isClockWise() const;
+    			/*!< Orientation is determined by rotating the
+			     inline axis to the crossline axis. */
+    float		computeAngleXInl() const;
+    			/*!< It's the angle (0 to pi/2) between
+			     the X-axis and the Inl-axis (not an inline) */
+    void		setXYInFeet( bool yn=true ) { xyinfeet_ = yn; }
+    void		setZUnit(bool istime,bool infeet=false);
+    static float	defaultXYtoZScale(Unit,Unit);
+    			/*!<Gives a ballpark figure of how to scale XY to
+			    make it comparable to Z. */
+    float		zScale() const;
+    			/*!<Gives a ballpark figure of how to scale Z to        
+			    make it comparable to XY. */
+
+    static const char*	sKeyInlRange();
+    static const char*	sKeyCrlRange();
+    static const char*	sKeyXRange();
+    static const char*	sKeyYRange();
+    static const char*	sKeyZRange();
+    static const char*	sKeyWSProjName();
+    static const char*	sKeyXYInFt();
+    static const char*	sKeyDpthInFt(); //!< Not used by SI, just a UI default
+    static const char*	sKeySurvDataType();
+
+    BufferString	getDirName() const	{ return dirname; }
+
+    			DeclareEnumUtils(Pol2D);
+    Pol2D		survDataType() const	{ return survdatatype_; }
+    void		setSurvDataType( Pol2D typ )	{ survdatatype_ = typ; }
+
+    const char*		comment() const		{ return comment_.buf(); }
+
+    const char*		getWSProjName() const	{ return wsprojnm_.buf(); }
+    			// Password only in memory this session
+    const char*		getWSPwd() const	{ return wspwd_.buf(); }
 
 	// These fns are used by specialist classes. Know what you are doing!
 
