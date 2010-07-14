2d8a1a23ca8f6f88d918ffda1b03bf3e796651bd
Raman Singh <raman.singh@dgbes.com>
2010-07-14 06:09:58 +0000

    reworked uiSurveyMap as a subclass of uibaseMap...
    git-svn-id: https://svn.opendtect.org/od/trunk@19525 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiIo/uisurvmap.h
+++ include/uiIo/uisurvmap.h
@@ -7,28 +7,77 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        Nanne Hemstra
  Date:          June 2001
- RCS:           $Id: uisurvmap.h,v 1.18 2009-10-16 10:58:27 cvsbert Exp $
+ RCS:           $Id: uisurvmap.h,v 1.19 2010-07-14 06:09:58 cvsraman Exp $
 ________________________________________________________________________
 
 -*/
 
-#include "uigraphicsview.h"
+#include "uibasemap.h"
 
-class uiGraphicsScene;
 class SurveyInfo;
+class uiArrowItem;
+class uiLineItem;
+class uiMarkerItem;
+class uiTextItem;
+
+mClass uiSurveyBoxObject : public uiBaseMapObject
+{
+public:
+    			uiSurveyBoxObject(bool withlabels);
+
+    const char*		getType() const		{ return "SurveyBox"; }
+
+    void		updateGeometry();
+    void		setSurveyInfo(const SurveyInfo&);
+
+protected:
+
+    ObjectSet<uiMarkerItem>	vertices_;
+    ObjectSet<uiLineItem>	edges_;
+    ObjectSet<uiTextItem>	labels_;
+
+    const SurveyInfo*		survinfo_;
+
+};
 
 
-mClass uiSurveyMap : public uiGraphicsView
+mClass uiNorthArrowObject : public uiBaseMapObject
+{
+public:
+    			uiNorthArrowObject(bool withangle);
+
+    const char*		getType() const		{ return "NorthArrow"; }
+
+    void		updateGeometry();
+    void		setSurveyInfo(const SurveyInfo&);
+
+protected:
+
+    uiArrowItem*	arrow_;
+    uiLineItem*		angleline_;
+    uiTextItem*		anglelabel_;
+
+    const SurveyInfo*	survinfo_;
+
+};
+
+
+mClass uiSurveyMap : public uiBaseMap
 {
 public:
 			uiSurveyMap(uiParent*);
+
     void		drawMap(const SurveyInfo*);
 
 protected:
 
-    uiGraphicsScene*	mapscene_;
-    void		drawAngleN(float);
+    uiSurveyBoxObject*	survbox_;
+    uiNorthArrowObject*	northarrow_;
+    uiTextItem*		title_;
 
+    const SurveyInfo*	survinfo_;
+
+    void		reSizeCB(CallBacker*);
 };
 
 #endif
