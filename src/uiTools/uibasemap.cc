1933f8d02dc09e6fa6aa8f11d137d1c5b4a8d26a
Raman Singh <raman.singh@dgbes.com>
2010-07-14 06:08:56 +0000

    added uibasemap.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@19524 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/uiTools/uibasemap.cc
@@ -0,0 +1,69 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Raman K Singh
+ Date:          Jul 2010
+________________________________________________________________________
+
+-*/
+
+static const char* rcsID = "$Id: uibasemap.cc,v 1.1 2010-07-14 06:08:56 cvsraman Exp $";
+
+#include "uibasemap.h"
+#include "uigraphicsscene.h"
+#include "uigraphicsview.h"
+#include "uiworld2ui.h"
+
+
+uiBaseMapObject::uiBaseMapObject( const char* nm )
+    : BaseMapObject(nm)
+    , itemgrp_(new uiGraphicsItemGroup(true))
+    , transform_(0)
+{}
+
+
+void uiBaseMapObject::setTransform( const uiWorld2Ui* w2ui )
+{
+    transform_ = w2ui;
+}
+
+
+uiBaseMap::uiBaseMap( uiParent* p )
+    : uiGroup(p,"Basemap")
+    , view_(*new uiGraphicsView(this,"Basemap"))
+    , w2ui_(*new uiWorld2Ui)
+{
+    view_.reSize.notify( mCB(this,uiBaseMap,reSizeCB) );
+}
+
+
+uiBaseMap::~uiBaseMap()
+{
+    delete &view_;
+    deepErase( objects_ );
+}
+
+
+void uiBaseMap::addObject( BaseMapObject* obj )
+{
+    mDynamicCastGet(uiBaseMapObject*,uiobj,obj)
+    if ( !uiobj )
+	return;
+
+    view_.scene().addItem( uiobj->itemGrp() );
+    objects_ += uiobj;
+    uiobj->setTransform( &w2ui_ );
+}
+
+
+void uiBaseMap::removeObject( const BaseMapObject* obj )
+{
+    mDynamicCastGet(const uiBaseMapObject*,uiobj,obj)
+    if ( !uiobj )
+	return;
+
+    const int index = objects_.indexOf( uiobj );
+    if ( index >= 0 )
+	delete objects_.remove( index );
+}
