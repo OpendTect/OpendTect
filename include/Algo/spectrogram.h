b908d88f74158dc5f58fdd29ef9818ebe3227c7c
Yuancheng Liu <yuancheng.liu@dgbes.com>
2010-08-11 16:41:52 +0000

    Put Spectroprogram in a seperate file.
    git-svn-id: https://svn.opendtect.org/od/trunk@19683 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Algo/spectrogram.h
@@ -0,0 +1,58 @@
+#ifndef spectrogram_h
+#define spectrogram_h
+
+/*
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	K. Tingdahl
+ Date:		9-3-1999
+ RCS:		$Id: spectrogram.h,v 1.1 2010-08-11 16:41:52 cvsyuancheng Exp $
+________________________________________________________________________
+
+*/
+
+#include "fourier.h"
+
+
+/*!\brief
+Spectrogram calculates the spectrogram of a N-dimensional signal. The
+spectrogram is the square of the absolute values of the FourierTransform,
+and is the 'traditional' way to view a signal's frequency distribution.
+
+Spectrogram is not reversible.
+*/
+
+mClass Spectrogram 
+{
+public:
+			Spectrogram();
+			~Spectrogram();
+
+    bool		init();
+    bool		setInputInfo( const ArrayNDInfo& ni);
+    const ArrayNDInfo&	getInputInfo() const;
+
+    bool		real2real() const 		{ return true; }
+    bool		real2complex() const 		{ return false; }
+    bool		complex2real() const 		{ return false; }
+    bool		complex2complex() const 	{ return true; }
+    bool		biDirectional() const 		{ return false; }
+    bool		getDir() const 			{ return true; }
+    bool		setDir( bool fwd ) 		{ return fwd; }
+
+    bool		transform(const ArrayND<float>&,ArrayND<float>&);
+    bool		transform(const ArrayND<float_complex>&,
+			   	  ArrayND<float_complex>& );
+protected:
+
+    bool		isPossible(int) const	{ return true; }
+    bool		isFast(int sz) const	{ return fft_->isFast(sz); }
+
+    ArrayND<float_complex>*	tempin_;
+    ArrayND<float_complex>*	tempout_;
+    Fourier::CC*		fft_;
+};
+
+
+#endif
---
+++ src/Algo/spectrogram.cc
@@ -0,0 +1,102 @@
+/*+
+ * COPYRIGHT: (C) dGB Beheer B.V.
+ * AUTHOR   : K. Tingdahl
+ * DATE     : 9-3-1999
+-*/
+
+static const char* rcsID = "$Id: spectrogram.cc,v 1.1 2010-08-11 16:41:52 cvsyuancheng Exp $";
+
+#include "spectrogram.h"
+#include "arrayndimpl.h"
+
+    
+Spectrogram::Spectrogram()
+    : fft_( new Fourier::CC() )
+    , tempin_( 0 )
+    , tempout_( 0 )
+{}    
+
+
+Spectrogram::~Spectrogram() 
+{ 
+    delete fft_;
+    delete tempin_; 
+    delete tempout_; 
+}
+
+
+bool Spectrogram::init()
+{
+    if ( tempin_ )
+    {
+	delete tempin_;
+	delete tempout_;
+    }
+
+    const ArrayNDInfo& info = fft_->getInputInfo();
+    tempin_ = ArrayNDImpl<float_complex>::create( info );
+    tempout_= ArrayNDImpl<float_complex>::create( info );
+ 
+    fft_->setInput( tempin_->getData() );
+    fft_->setOutput( tempout_->getData() );
+
+    return true;
+}
+
+
+bool Spectrogram::setInputInfo( const ArrayNDInfo& ni )
+{ return fft_->setInputInfo( ni ); }
+
+
+const ArrayNDInfo& Spectrogram::getInputInfo() const 
+{ return fft_->getInputInfo(); }
+
+
+bool Spectrogram::transform( const ArrayND< float > &in, ArrayND< float > &out )
+{
+    const ArrayNDInfo& info = fft_->getInputInfo();
+
+    float_complex* tindata = tempin_->getData();
+    const float* indata = in.getData();
+    unsigned long size = info.getTotalSz();
+
+    for ( int idx=0; idx<size; idx++ )
+	tindata[idx] = indata[idx];
+
+    if ( !fft_->run(true) ) return false;
+
+    float_complex* toutdata = tempout_->getData();
+    float* outdata = out.getData();
+
+    for ( int idx=0; idx<size; idx++ )
+    {
+	float val = abs( toutdata[idx] );
+	outdata[idx] = val*val;
+    }
+
+    return true;
+}
+
+
+bool Spectrogram::transform( const ArrayND< float_complex > &in, 
+			     ArrayND< float_complex > &out )
+{
+    fft_->setInputInfo( in.info() );
+    fft_->setInput( in.getData() );
+    fft_->setOutput( out.getData() );
+    if ( !fft_->run(true) ) return false;
+
+    unsigned long size = in.info().getTotalSz();
+    float_complex* data = out.getData();
+
+    for ( int idx=0; idx<size; idx++ )
+    {
+	float val = abs( data[idx] );
+	data[idx] = val*val;
+    }
+
+    return true;
+}
+
+
+
