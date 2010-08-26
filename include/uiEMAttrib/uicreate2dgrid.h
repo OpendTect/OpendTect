f77e106faff59ee0c19569cb2692aa4d30fa3d16
Raman Singh <raman.singh@dgbes.com>
2010-08-26 03:56:27 +0000

    added file uicreate2dgrid.h
    git-svn-id: https://svn.opendtect.org/od/trunk@19816 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiEMAttrib/uicreate2dgrid.h
@@ -0,0 +1,165 @@
+#ifndef uicreate2dgrid_h
+#define uicreate2dgrid_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	Nanne Hemstra
+ Date:		December 2009
+ RCS:		$Id: uicreate2dgrid.h,v 1.1 2010-08-26 03:56:27 cvsraman Exp $
+________________________________________________________________________
+
+-*/
+
+
+#include "uibatchlaunch.h"
+#include "uigroup.h"
+#include "grid2d.h"
+
+class CubeSampling;
+class HorSampling;
+class IOPar;
+class uiCheckBox;
+class uiGenInput;
+class uiGrid2DMapObject;
+class uiIOObjSel;
+class uiIOObjSelGrp;
+class uiPosSubSel;
+class uiSeisSel;
+class uiSelNrRange;
+class uiSurveyMap;
+
+namespace Geometry { class RandomLine; }
+
+mClass ui2DGridLines : public uiGroup
+{
+public:
+				~ui2DGridLines();
+
+    const Grid2D*		getGridLines() const	{ return grid_; }
+    virtual void		updateRange();
+    virtual const Grid2D::Line*	getBaseLine() const	{ return 0; }
+    virtual void		getNrLinesLabelTexts(BufferString&,
+	    					     BufferString&) const  = 0;
+
+    virtual bool		fillPar(IOPar&) const;
+
+    Notifier<ui2DGridLines>	gridChanged;
+
+protected:
+
+    				ui2DGridLines(uiParent*,const HorSampling&);
+
+    virtual bool		computeGrid()			= 0;
+
+    uiGenInput*			inlprefixfld_;
+    uiGenInput*			crlprefixfld_;
+
+    Grid2D*			grid_;
+    const HorSampling&		hs_;
+};
+
+
+mClass ui2DGridLinesFromInlCrl : public ui2DGridLines
+{
+public:
+    				ui2DGridLinesFromInlCrl(uiParent*,
+							const HorSampling&);
+
+    bool			fillPar(IOPar&) const;
+    void			updateRange();
+
+protected:
+
+    bool			computeGrid();
+    void			paramsChgCB(CallBacker*);
+    void			getNrLinesLabelTexts(BufferString&,
+	    					     BufferString&) const;
+    void			modeChg(CallBacker*);
+
+    uiGenInput*			inlmodefld_;
+    uiGenInput*			crlmodefld_;
+    uiGenInput*			inlsfld_;
+    uiGenInput*			crlsfld_;
+    uiSelNrRange*		inlrgfld_;
+    uiSelNrRange*		crlrgfld_;
+};
+
+
+mClass ui2DGridLinesFromRandLine : public ui2DGridLines
+{
+public:
+    				ui2DGridLinesFromRandLine(uiParent*,
+						const HorSampling&,
+						const Geometry::RandomLine*);
+    				~ui2DGridLinesFromRandLine();
+
+    const Grid2D::Line*		getBaseLine() const	{ return baseline_; }
+    bool			fillPar(IOPar&) const;
+
+protected:
+
+    bool			computeGrid();
+    void			paramsChgCB(CallBacker*);
+    void			getNrLinesLabelTexts(BufferString&,
+	    					     BufferString&) const;
+
+    uiIOObjSel*			rdlfld_;
+    uiGenInput*			pardistfld_;
+    uiGenInput*			perdistfld_;
+
+    Grid2D::Line*		baseline_;
+};
+
+
+mClass uiCreate2DGrid : public uiFullBatchDialog
+{
+public:
+				uiCreate2DGrid(uiParent*,
+					       const Geometry::RandomLine*);
+				~uiCreate2DGrid();
+
+    bool			fillPar(IOPar&);
+    bool			prepareProcessing()	{ return true; }
+
+protected:
+
+    uiGroup*			createSeisGroup(const Geometry::RandomLine*);
+    uiGroup*			createHorizonGroup();
+    uiGroup*			createPreviewGroup();
+    void			updatePreview(CallBacker*);
+
+    uiSeisSel*			infld_;
+    uiSeisSel*			outfld_;
+    uiPosSubSel*		bboxfld_;
+    uiGenInput*			sourceselfld_;
+
+    ui2DGridLines*		inlcrlgridgrp_;
+    ui2DGridLines*		randlinegrdgrp_;
+    uiGrid2DMapObject*		preview_;
+    uiSurveyMap*		previewmap_;
+    uiLabel*			nrinlinesfld_;
+    uiLabel*			nrcrlinesfld_;
+
+    uiCheckBox*			horcheckfld_;
+    uiIOObjSelGrp*		horselfld_;
+    uiGenInput*			hornmfld_;
+
+    CubeSampling&		cs_;
+
+    void			horCheckCB(CallBacker*);
+    void			inpSelCB(CallBacker*);
+    void			outSelCB(CallBacker*);
+    void			srcSelCB(CallBacker*);
+    void			subSelCB(CallBacker*);
+    void			gridChgCB(CallBacker*);
+    void			finaliseCB(CallBacker*);
+
+    void			fillSeisPar(IOPar&);
+    void			fillHorPar(IOPar&);
+
+};
+
+
+#endif
