69aef37d255d2b2ed7df031916bc0f1185f796d7
Raman Singh <raman.singh@dgbes.com>
2010-08-26 03:50:37 +0000

    added gridcreator.h
    git-svn-id: https://svn.opendtect.org/od/trunk@19814 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/EMAttrib/gridcreator.h
@@ -0,0 +1,89 @@
+#ifndef gridcreator_h
+#define gridcreator_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Nanne Hemstra
+ Date:		January 2010
+ RCS:		$Id: gridcreator.h,v 1.1 2010-08-26 03:50:37 cvsraman Exp $
+________________________________________________________________________
+
+-*/
+
+#include "executor.h"
+
+#include "bufstring.h"
+
+class CubeSampling;
+class IOObj;
+class IOPar;
+class SeisTrcReader;
+class SeisTrcWriter;
+class TaskRunner;
+namespace EM { class Horizon2D; }
+
+
+mClass Seis2DGridCreator : public ExecutorGroup
+{
+public:
+    			Seis2DGridCreator(const IOPar&);
+			~Seis2DGridCreator();
+
+    virtual const char*	nrDoneText() const;
+
+    static const char*	sKeyInput();
+    static const char*	sKeyOutput();
+    static const char*  sKeySelType();
+    static const char*	sKeyOutpAttrib();
+    static const char*	sKeyInlSelType();
+    static const char*	sKeyCrlSelType();
+
+    static const char*	sKeyBaseLine();
+    static const char*	sKeyStartBinID();
+    static const char*	sKeyStopBinID();
+    static const char*	sKeyInlSpacing();
+    static const char*	sKeyCrlSpacing();
+
+    static const char*	sKeyInlPrefix();
+    static const char*	sKeyCrlPrefix();
+
+
+protected:
+
+    bool		init(const IOPar&);
+    bool		initFromInlCrl(const IOPar&,const IOObj&,const IOObj&,
+	    			       const CubeSampling&);
+    bool		initFromRandomLine(const IOPar&,const IOObj&,
+	    				   const IOObj&,const CubeSampling&);
+};
+
+
+mClass Horizon2DGridCreator : public ExecutorGroup
+{
+public:
+    			Horizon2DGridCreator();
+			~Horizon2DGridCreator();
+
+    virtual od_int64	totalNr() const;
+    virtual od_int64	nrDone() const;
+    virtual const char*	nrDoneText() const;
+
+    bool		init(const IOPar&,TaskRunner* tr=0);
+    bool		finish(TaskRunner* tr=0);
+
+    static const char*	sKeyInputIDs();
+    static const char*	sKeySeisID();
+    static const char*	sKeyPrefix();
+
+protected:
+
+    ObjectSet<EM::Horizon2D> horizons_;
+    od_int64		nrdone_;
+    od_int64		totalnr_;
+
+};
+
+
+#endif
