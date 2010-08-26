cf079eeedd9a854769e5382a6eac170a3b59d132
Raman Singh <raman.singh@dgbes.com>
2010-08-26 06:43:26 +0000

    added file horizonrelation.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@19825 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/EarthModel/horizonrelation.cc
@@ -0,0 +1,257 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Raman K Singh
+ Date:          Jul 2010
+________________________________________________________________________
+
+-*/
+
+static const char* rcsID = "$Id: horizonrelation.cc,v 1.1 2010-08-26 06:43:26 cvsraman Exp $";
+
+#include "horizonrelation.h"
+#include "ctxtioobj.h"
+#include "filepath.h"
+#include "iopar.h"
+#include "separstr.h"
+
+
+RelationTree::Node::Node( const MultiID& id )
+    : id_(id)
+{}
+
+
+bool RelationTree::Node::hasChild( const RelationTree::Node* node ) const
+{
+    for ( int idx=0; idx<children_.size(); idx++ )
+    {
+	if ( children_[idx] == node || children_[idx]->hasChild(node) )
+	    return true;
+    }
+
+    return false;
+}
+
+
+void RelationTree::Node::fillPar( IOPar& par ) const
+{
+    BufferStringSet childids;
+    for ( int idx=0; idx<children_.size(); idx++ )
+	childids.addIfNew( children_[idx]->id_.buf() );
+
+    par.set( id_.buf(), childids );
+}
+
+
+void RelationTree::Node::fillChildren( const FileMultiString& fms,
+       				 const RelationTree& tree )
+{
+    children_.erase();
+    for ( int idx=0; idx<fms.size(); idx++ )
+    {
+	MultiID id( fms[idx] );
+	const RelationTree::Node* node = tree.getNode( id );
+	if ( !node )
+	    continue;
+
+	children_ += node;
+    }
+}
+
+
+
+RelationTree::RelationTree( bool is2d )
+    : is2d_(is2d)
+{}
+
+
+RelationTree::~RelationTree()
+{ deepErase( nodes_ ); }
+
+
+int RelationTree::findNode( const MultiID& id ) const
+{
+    for ( int idx=0; idx<nodes_.size(); idx++ )
+    {
+	if ( nodes_[idx]->id_ == id )
+	    return idx;
+    }
+
+    return -1;
+}
+
+
+const RelationTree::Node* RelationTree::getNode( const MultiID& id ) const
+{
+    const int idx = findNode( id );
+    return idx < 0 ? 0 : nodes_[idx];
+}
+
+void RelationTree::getParents( int index, TypeSet<int>& parents ) const
+{
+    parents.erase();
+    if ( !nodes_.validIdx(index) )
+	return;
+
+    const RelationTree::Node* node = nodes_[index];
+    for ( int idx=0; idx<nodes_.size(); idx++ )
+    {
+	if ( nodes_[idx]->children_.indexOf(node) >= 0 )
+	    parents += idx;
+    }
+}
+
+
+int RelationTree::findRelation( const MultiID& id1, const MultiID& id2	) const
+{
+    const RelationTree::Node* node1 = getNode( id1 );
+    const RelationTree::Node* node2 = getNode( id2 );
+    if ( !node1 || !node2 )
+	return 0;
+
+    if ( node1->hasChild(node2) )
+	return 1;
+    if ( node2->hasChild(node1) )
+	return 2;
+
+    return 0;
+}
+
+
+void RelationTree::removeNode( const MultiID& id )
+{
+    const int index = findNode( id );
+    if ( index < 0 )
+	return;
+
+    RelationTree::Node* node = nodes_[index];
+    TypeSet<int> parents;
+    getParents( index, parents );
+    for ( int idx=0; idx<parents.size(); idx++ )
+    {
+	RelationTree::Node* parentnode = nodes_[idx];
+	for ( int cdx=0; cdx<node->children_.size(); cdx++ )
+	{
+	    const RelationTree::Node* childnode = node->children_[cdx];
+	    if ( !parentnode->hasChild(childnode) )
+		parentnode->children_ += childnode;
+	}
+    }
+
+    delete nodes_.remove( index );
+}
+
+
+void RelationTree::addRelation( const MultiID& id1, const MultiID& id2  )
+{
+    const int idx1 = findNode( id1 );
+    RelationTree::Node* node1 = idx1 < 0 ? 0 : nodes_[idx1];
+    if ( idx1 < 0 )
+    {
+	node1 = new RelationTree::Node( id1 );
+	nodes_ += node1;
+    }
+
+    const int idx2 = findNode( id2 );
+    RelationTree::Node* node2 = idx2 < 0 ? 0 : nodes_[idx2];
+    if ( idx2 < 0 )
+    {
+	node2 = new RelationTree::Node( id2 );
+	nodes_ += node2;
+    }
+
+    if ( !node1->hasChild(node2) )
+	node1->children_ += node2;
+}
+
+
+bool RelationTree::write() const
+{
+    IOPar par;
+    FilePath fp( IOObjContext::getDataDirName(IOObjContext::Surf) );
+    fp.add( "horizonrelations.txt" );
+    if ( par.read(fp.fullPath(),0) )
+	par.removeWithKey( is2d_ ? "2D" : "3D" );
+
+    IOPar subpar;
+    for ( int idx=0; idx<nodes_.size(); idx++ )
+	nodes_[idx]->fillPar( subpar );
+
+    par.mergeComp( subpar, is2d_ ? "2D" : "3D" );
+    return par.write( fp.fullPath(), 0 );
+}
+
+
+bool RelationTree::read()
+{
+    deepErase( nodes_ );
+    IOPar par;
+    FilePath fp( IOObjContext::getDataDirName(IOObjContext::Surf) );
+    fp.add( "horizonrelations.txt" );
+    if ( !par.read(fp.fullPath(),0) )
+	return false;
+
+    PtrMan<IOPar> subpar = par.subselect( is2d_ ? "2D" : "3D" );
+    if ( !subpar )
+	return false;
+
+    for ( int idx=0; idx<subpar->size(); idx++ )
+    {
+	MultiID id( subpar->getKey(idx) );
+	RelationTree::Node* node = new RelationTree::Node( id );
+	nodes_ += node;
+    }
+
+    for ( int idx=0; idx<subpar->size(); idx++ )
+    {
+	MultiID id( subpar->getKey(idx) );
+	RelationTree::Node* node = nodes_[idx];
+	if ( id != node->id_ )
+	    return false;
+	
+	FileMultiString fms( subpar->getValue(idx) );
+	node->fillChildren( fms, *this );
+    }
+
+    return true;
+}
+
+
+bool RelationTree::getSorted( const TypeSet<MultiID>& unsortedids,
+			      TypeSet<MultiID>& sortedids ) const
+{
+    for ( int idx=0; idx<unsortedids.size(); idx++ )
+    {
+	const MultiID& mid = unsortedids[idx];
+	if ( !sortedids.size() )
+	{
+	    if ( getNode(mid) )
+		sortedids += mid;
+
+	    continue;
+	}
+
+	for ( int idy=0; idy<sortedids.size(); idy++ )
+	{
+	    const MultiID& sortedmid = sortedids[idy];
+	    const int rel = findRelation( mid, sortedmid );
+	    if ( !rel )
+		break;
+	    else if ( rel == 1 )
+	    {
+		sortedids.insert( idx, mid );
+		break;
+	    }
+	    else if ( idy == sortedids.size() - 1 )
+	    {
+		sortedids += mid;
+		break;
+	    }
+	}
+    }
+
+    return sortedids.size() > 1;
+}
+
+
