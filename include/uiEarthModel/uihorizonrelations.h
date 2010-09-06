0944818d42573ba8fe7e6154817e4341466c5735
Raman Singh <raman.singh@dgbes.com>
2010-09-06 04:55:47 +0000

    removed functions which read saved relations as this job is now done by uiHorizonSortDlg
    git-svn-id: https://svn.opendtect.org/od/trunk@19941 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiEarthModel/uihorizonrelations.h
+++ include/uiEarthModel/uihorizonrelations.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Nanne Hemstra
  Date:		April 2006
- RCS:		$Id: uihorizonrelations.h,v 1.5 2009-07-22 16:01:21 cvsbert Exp $
+ RCS:		$Id: uihorizonrelations.h,v 1.6 2010-09-06 04:55:47 cvsraman Exp $
 ________________________________________________________________________
 
 -*/
@@ -36,13 +36,10 @@ protected:
     bool		is2d_;
 
     void		fillRelationField(const BufferStringSet&);
-    void		read();
-    bool		write();
 
     void		readHorizonCB(CallBacker*);
     void		checkCrossingsCB(CallBacker*);
     void		makeWatertightCB(CallBacker*);
-    bool		rejectOK(CallBacker*);
 };
 
 #endif
