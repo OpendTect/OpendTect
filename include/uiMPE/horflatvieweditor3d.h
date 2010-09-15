3e238be2d39fbbe172183f956470642c88ade58d
cvsbruno <cvsbruno@a985855d-d384-4b45-ae33-31a410998c3c>
2010-09-15 14:03:06 +0000

    removed const for getPainter
    git-svn-id: https://svn.opendtect.org/od/trunk@20048 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiMPE/horflatvieweditor3d.h
+++ include/uiMPE/horflatvieweditor3d.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  CopyRight:	(C) dGB Beheer B.V.
  Author:	Umesh Sinha
  Date:		May 2010
- RCS:		$Id: horflatvieweditor3d.h,v 1.4 2010-09-15 08:20:21 cvsbruno Exp $
+ RCS:		$Id: horflatvieweditor3d.h,v 1.5 2010-09-15 14:03:06 cvsbruno Exp $
 ________________________________________________________________________
 
 -*/
@@ -41,7 +41,7 @@ public:
     void		setSelSpec(const Attrib::SelSpec*,bool wva);
 
     FlatView::AuxDataEditor* getEditor()                { return editor_; }
-    const EM::HorizonPainter3D*	getPainter()		{ return horpainter_; }
+    EM::HorizonPainter3D* getPainter() const		{ return horpainter_; }
     void		setMouseEventHandler(MouseEventHandler*);
 
     void		enableLine(bool);
