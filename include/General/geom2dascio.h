b6274cb34f9fab474717b344b9fc130486f2d386
Satyaki Maitra <satyaki.maitra@dgbes.com>
2010-09-30 06:39:33 +0000

    geom2dascio.cc/h added
    git-svn-id: https://svn.opendtect.org/od/trunk@20171 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/geom2dascio.h
@@ -0,0 +1,34 @@
+#ifndef geom2dascio_h
+#define geom2dascio_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Satyaki Maitra
+ Date:		Sep 2010
+ RCS:		$Id: geom2dascio.h,v 1.1 2010-09-30 06:39:33 cvssatyaki Exp $
+________________________________________________________________________
+
+-*/
+
+
+#include "tableascio.h"
+
+namespace PosInfo { class Line2DData; }
+namespace Table { class FormatDesc; }
+
+mClass Geom2dAscIO : public Table::AscIO
+{
+public:
+				Geom2dAscIO(const Table::FormatDesc&,
+					    std::istream&);
+    static Table::FormatDesc*	getDesc();
+    bool			getData(PosInfo::Line2DData&);
+
+protected:
+
+    std::istream&	strm_;
+};
+
+#endif
---
+++ src/General/geom2dascio.cc
@@ -0,0 +1,47 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : Satyaki Maitra
+ * DATE     : September 2010
+-*/
+
+static const char* rcsID = " $";
+
+#include "geom2dascio.h"
+#include "posinfo2d.h"
+#include "tabledef.h"
+
+Geom2dAscIO::Geom2dAscIO( const Table::FormatDesc& fd, std::istream& strm )
+    : Table::AscIO( fd )
+    , strm_( strm )
+{
+}
+
+
+Table::FormatDesc* Geom2dAscIO::getDesc()
+{
+    Table::FormatDesc* fd = new Table::FormatDesc( "Geom2D" );
+    fd->bodyinfos_ += new Table::TargetInfo( "Trace Nr", IntInpSpec() );
+    fd->bodyinfos_ += Table::TargetInfo::mkHorPosition( true );
+
+    return fd;
+}
+
+
+bool Geom2dAscIO::getData( PosInfo::Line2DData& geom )
+{
+    if ( !getHdrVals(strm_) )
+	return false;
+     while ( true )
+     {
+	 const int ret = getNextBodyVals( strm_ );
+	 if ( ret < 0 ) return false;
+	 if ( ret == 0 ) break;
+
+	 PosInfo::Line2DPos pos( getIntValue(0) );
+	 pos.coord_.x = getdValue( 1 );
+	 pos.coord_.y = getdValue( 2 );
+	 geom.add( pos );
+     }
+
+     return geom.positions().size();
+}
