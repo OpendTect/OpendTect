f9c6b767769cc071539aa0ff871ba473b9279a19
Helene Huck <helene.huck@dgbes.com>
2010-10-04 18:04:41 +0000

    added extra function in uiSimiSteeringSel
    git-svn-id: https://svn.opendtect.org/od/trunk@20217 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiAttributes/uisimilarityattrib.h
+++ include/uiAttributes/uisimilarityattrib.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        N. Hemstra
  Date:          May 2005
- RCS:           $Id: uisimilarityattrib.h,v 1.9 2010-09-30 15:14:44 cvshelene Exp $
+ RCS:           $Id: uisimilarityattrib.h,v 1.10 2010-10-04 18:04:41 cvshelene Exp $
 ________________________________________________________________________
 
 -*/
@@ -66,6 +66,7 @@ protected:
 	   
 	    bool	willSteer() const;	
 	    bool	wantBrowseDip() const;
+	    int		browseDipIdxInList() const;
 				                                             
 	    Notifier<uiSimiSteeringSel>	typeSelected;
 
