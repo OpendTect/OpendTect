71f115019e3bd7e6cca395703933eee177946482
Bert Bril <bert.bril@dgbes.com>
2010-10-12 12:07:40 +0000

    Layer now no longer hooked to Property's
    git-svn-id: https://svn.opendtect.org/od/trunk@20342 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Strat/stratlayer.h
+++ include/Strat/stratlayer.h
@@ -7,16 +7,15 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Bert
  Date:		Sep 2010
- RCS:		$Id: stratlayer.h,v 1.8 2010-10-06 15:40:52 cvsbert Exp $
+ RCS:		$Id: stratlayer.h,v 1.9 2010-10-12 12:07:40 cvsbert Exp $
 ________________________________________________________________________
 
 
 -*/
 
 #include "compoundkey.h"
-#include "property.h"
-class Property;
-class PropertySet;
+#include "typeset.h"
+class PropertyRef;
 
 namespace Strat
 {
@@ -24,10 +23,9 @@ class LeafUnitRef;
 
 /*!\brief data for a layer.
 
-  Layers are atached to a UnitRef. They have Property's, and the first
-  of the properties is always 'Thickness'. Initially, this Property will be
-  a ValueProperty. You can later replace this with another type, but
-  thou shalt not remove it!
+  Layers are atached to a UnitRef. To understand the values, you need access to
+  the governing PropertyRefSet, usually attached to the LayerSequence that
+  the Layer is part of.
  
  */
 
@@ -37,30 +35,33 @@ public:
 
     typedef CompoundKey	ID;
 
-			Layer(const LeafUnitRef&,float thickness=0);
+			Layer(const LeafUnitRef&);
 
     const LeafUnitRef&	unitRef() const;
     inline void		setRef( const LeafUnitRef& r )	{ ref_ = &r; }
 
-    PropertySet&	properties()		{ return props_; }
-    const PropertySet&	properties() const	{ return props_; }
-    Property&		thickness()		{ return props_.get(0); }
-    const Property&	thickness() const	{ return props_.get(0); }
+    inline float	zTop() const		{ return vals_[0]; }
+    inline float	thickness() const	{ return vals_[1]; }
+    inline int		nrValues() const	{ return vals_.size(); }
+    float		value(int) const;	//!< returns undef if necessary
+    inline void		setZTop( float v )	{ vals_[0] = v; }
+    inline void		setThickness( float v )	{ vals_[1] = v; }
+    void		setValue(int,float);	//!< automatically adds space
 
-    float		zTop() const		{ return ztop_; }
-    float		depth() const;		//!< ztop + thickness/2
+    inline float	zBot() const	{ return zTop() + thickness(); }
+    inline float	depth() const	{ return zTop() + 0.5*thickness(); }
 
     ID			id() const;		//!< unitRef().fullCode()
 
+    const float*	values() const		{ return vals_.arr(); }
+
+    static const PropertyRef& topDepthRef();
     static const PropertyRef& thicknessRef();
 
 protected:
 
     const LeafUnitRef*	ref_;
-    PropertySet		props_;
-    float		ztop_;
-
-    friend class	LayerModel;
+    TypeSet<float>	vals_;
 
 };
 
