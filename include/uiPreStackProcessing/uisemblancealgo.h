88b77f85515e085b6355fbc97d3daebdac91e6d9
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2010-11-09 22:23:08 +0000

    Added uiSemblanceAlgorithm
    git-svn-id: https://svn.opendtect.org/od/trunk@20588 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiPreStackProcessing/uisemblancealgo.h
@@ -0,0 +1,39 @@
+#ifndef uisemblancealgo_h
+#define uisemblancealgo_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	K. Tingdahl
+ Date:		Nov 2010
+ RCS:		$Id: uisemblancealgo.h,v 1.1 2010-11-09 22:23:08 cvskris Exp $
+________________________________________________________________________
+
+
+-*/
+
+#include "factory.h"
+#include "uidialog.h"
+
+
+namespace PreStack
+{
+
+class SemblanceAlgorithm;
+
+/*! Base class for algorithms that computes semblance along a moveout */
+mClass uiSemblanceAlgorithm : public uiDialog
+{
+public:
+    			mDefineFactory1ParamInClass(uiSemblanceAlgorithm,
+						    uiParent*,factory);
+    virtual bool	populateUI(const SemblanceAlgorithm*) 		= 0;
+    virtual bool	populateObject(SemblanceAlgorithm*) const 	= 0;
+protected:
+    			uiSemblanceAlgorithm(uiParent*,const char* helpid);
+};
+
+}; //namespace
+
+#endif
---
+++ src/uiPreStackProcessing/uisemblancealgo.cc
@@ -0,0 +1,24 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Nov 2010
+-*/
+
+static const char* rcsID = "$Id: uisemblancealgo.cc,v 1.1 2010-11-09 22:23:08 cvskris Exp $";
+
+#include "uisemblancealgo.h"
+#include "ptrman.h"
+
+namespace PreStack
+{
+
+mImplFactory1Param( uiSemblanceAlgorithm, uiParent*,
+		    uiSemblanceAlgorithm::factory);
+
+uiSemblanceAlgorithm::uiSemblanceAlgorithm( uiParent* p, const char* helpid )
+    : uiDialog( p,
+	  uiDialog::Setup("Setup","Semblance parameters",helpid).canceltext(0))
+{}
+
+
+}; //namespace
