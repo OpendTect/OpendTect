84676857e3d3e488c154f6d0638156b185c8126a
Bert Bril <bert.bril@dgbes.com>
2010-11-12 10:17:54 +0000

    Moving time series stuff to Algo
    git-svn-id: https://svn.opendtect.org/od/trunk@20625 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Algo/timeser.h
@@ -0,0 +1,37 @@
+#ifndef timeser_H
+#define timeser_H
+
+/*@+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	A.H. Bril
+ Date:		24-3-1996
+ RCS:		$Id: timeser.h,v 1.1 2010-11-12 10:17:54 cvsbert Exp $
+________________________________________________________________________
+
+-*/
+ 
+#include <gendefs.h>
+
+#ifdef __cpp__
+extern "C" {
+#endif
+
+mGlobal void BFhighpass(int npoles,float f3db,int n,const float* arrin,float* arrout);
+mGlobal void BFlowpass (int npoles,float f3db,int n,const float* arrin,float* arrout);
+mGlobal void AntiAlias(float frac,int n,const float* arrin,float* arrout);
+mGlobal void Convolve(int lx,int ifx,const float* x,int ly,int ify,const float* y,
+               int lz,int ifz,float* z);
+mGlobal void Resample(int stepsize,int ns,const float* arrin,float* arrout);
+mGlobal void Hilbert(int n,float* x,float* y);
+
+
+#define mHalfHilbertLength 30
+
+#ifdef __cpp__
+}
+#endif
+
+
+#endif
---
+++ src/Algo/timeser.c
@@ -0,0 +1,247 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : Dave Hale / A.H. Bril
+ * DATE     : 24-3-1996
+ * FUNCTION : Time series manipulation
+
+ Only AntiAlias and ReSample are Bert's, the rest is Dave's (albeit with
+ another interface).
+
+-*/
+
+static const char* rcsID = "$Id: timeser.c,v 1.1 2010-11-12 10:17:54 cvsbert Exp $";
+
+
+#include "timeser.h"
+#include "mallocdefs.h"
+#include <math.h>
+#include <string.h>
+
+
+static float PowerOf( float x, float y )
+{
+    int isneg = x < 0 ? 1 : 0;
+     float ret ;
+
+    if ( x == 0 ) return y ? 0 : 1;
+    if ( isneg ) x = -x;
+
+     ret = exp( y * log(x) );
+    return isneg ? -ret : ret;
+}
+
+static void designBF( float fpass, float apass, float fstop, float astop,
+		      int* npoles, float* f3db )
+{
+    double wpass, wstop, fnpoles, w3db;
+
+    wpass = 2 * tan( M_PI * fpass );
+    wstop = 2 * tan( M_PI * fstop );
+
+    if ( fstop>fpass )
+    {
+	fnpoles = log( (1.0/(apass*apass)-1.0) / (1.0/(astop*astop)-1.0) )
+		/ log( wpass*wpass / (wstop*wstop) );
+	w3db = wpass / PowerOf( (double)(1.0/(apass*apass)-1.0),
+				(double)(0.5/fnpoles) );
+    }
+    else
+    {
+	fnpoles = log( (1.0/(apass*apass)-1.0) / (1.0/(astop*astop)-1.0) )
+		/ log( wstop*wstop / (wpass*wpass) );
+	w3db = wpass * PowerOf( (double)(1.0/(apass*apass)-1.0),
+				(double)(0.5/fnpoles) );
+    }
+
+    *npoles = 1 + (int)fnpoles;
+    *f3db = atan(0.5*w3db) / M_PI;
+}
+
+
+void BFhighpass( int npoles, float f3db,
+		 int n, const float* arrin, float* arrout )
+{
+    int jpair, j;
+    float r, scale, theta, a, b1, b2, pj, pjm1, pjm2, qjm1, qjm2;
+
+    r = 2 * tan( M_PI * fabs(f3db) );
+    if ( ! (npoles%2) )
+	for ( j=0; j<n; j++ ) arrout[j] = arrin[j];
+    else
+    {
+	scale = r + 2;
+	a = 2 / scale;
+	b1 = (r - 2) / scale;
+	pj = qjm1 = 0;
+	for ( j=0; j<n; j++ )
+	{
+	    pjm1 = pj;
+	    pj = arrin[j];
+	    arrout[j] = a * (pj-pjm1) - b1*qjm1;
+	    qjm1 = arrout[j];
+	}
+    }
+
+    for ( jpair=0; jpair<npoles/2; jpair++ )
+    {
+	theta = M_PI * (2*jpair+1) / (2*npoles);
+	scale = 4 + 4*r*sin(theta) + r*r;
+	a = 4 / scale;
+	b1 = (2*r*r - 8) / scale;
+	b2 = (4 - 4*r*sin(theta) + r*r) / scale;
+	pjm1 = pj = qjm2 = qjm1 = 0;
+	for (j=0; j<n; j++)
+	{
+	    pjm2 = pjm1;
+	    pjm1 = pj;
+	    pj = arrout[j];
+	    arrout[j] = a * (pj-2*pjm1+pjm2) - b1*qjm1 - b2*qjm2;
+	    qjm2 = qjm1;
+	    qjm1 = arrout[j];
+	}
+    }
+}
+
+
+void BFlowpass( int npoles, float f3db,
+		int n, const float* arrin, float* arrout )
+{
+    int jpair,j;
+    float r,scale,theta,a,b1,b2,pj,pjm1,pjm2,qjm1,qjm2;
+
+    r = 2.0*tan(M_PI*fabs(f3db));
+    if ( !(npoles%2) )
+	for ( j=0; j<n; j++ ) arrout[j] = arrin[j];
+    else
+    {
+	scale = r + 2;
+	a = r / scale;
+	b1 = (r - 2) / scale;
+	pj = qjm1 = 0;
+	for ( j=0; j<n; j++ )
+	{
+	    pjm1 = pj;
+	    pj = arrin[j];
+	    arrout[j] = a * (pj+pjm1) - b1*qjm1;
+	    qjm1 = arrout[j];
+	}
+    }
+
+    for ( jpair=0; jpair<npoles/2; jpair++ )
+    {
+	theta = M_PI * (2*jpair+1) / (2*npoles);
+	scale = 4 + 4 * r * sin(theta) + r*r;
+	a = r * r / scale;
+	b1 = (2*r*r - 8) / scale;
+	b2 = (4 - 4*r*sin(theta) + r*r)/scale;
+	pjm1 = pj = qjm2 = qjm1 = 0;
+	for (j=0; j<n; j++)
+	{
+	    pjm2 = pjm1;
+	    pjm1 = pj;
+	    pj = arrout[j];
+	    arrout[j] = a * (pj+2.0*pjm1+pjm2) - b1*qjm1 - b2*qjm2;
+	    qjm2 = qjm1;
+	    qjm1 = arrout[j];
+	}
+    }
+}
+
+
+void AntiAlias( float frac, int n, const float* arrin, float* arrout )
+{
+    int iwt, ival, width, hwidth, inpidx;
+    float h;
+    float* wts;
+
+    if ( frac < 0 ) frac = -frac;
+    width = 2 * (1 / frac + .5);
+    if ( width < 2 || frac >= 1 )
+    {
+	memcpy( arrout, arrin, n * sizeof(float) );
+	return;
+    }
+    if ( (width % 2) == 0 )
+	width++;
+    hwidth = width / 2;
+
+    wts = mMALLOC(width,float);
+    for ( iwt=0; iwt<width; iwt++ )
+	wts[iwt] = 2 * frac * (1 + cos( M_PI * iwt / hwidth - M_PI ));
+
+    for ( ival=0; ival<n; ival++ )
+    {
+	arrout[ival] = 0;
+	for ( iwt=0; iwt<width; iwt++ )
+	{
+	    inpidx = ival - hwidth + iwt;
+	    arrout[ival] +=
+		wts[iwt] *
+		(inpidx >= 0 ? (inpidx < n ? arrin[inpidx] : arrin[n-1])
+			     : arrin[0]);
+	}
+    }
+
+    mFREE(wts);
+}
+
+
+void Convolve( int lx, int ifx, const float* x, int ly, int ify, const float* y,
+	       int lz, int ifz, float* z )
+/*
+Compute z = x convolved with y; i.e.,
+
+           ifx+lx-1
+    z[i] =   sum    x[j]*y[i-j]  ;  i = ifz,...,ifz+lz-1
+            j=ifx
+*/
+{
+    int ilx=ifx+lx-1, ily=ify+ly-1, ilz=ifz+lz-1, i, j, jlow, jhigh;
+    float sum;
+    
+    x -= ifx;  y -= ify;  z -= ifz;
+    for ( i=ifz; i<=ilz; ++i )
+    {
+	jlow = i-ily; if ( jlow < ifx ) jlow = ifx;
+	jhigh = i-ify; if ( jhigh > ilx ) jhigh = ilx;
+	for ( j=jlow, sum=0; j<=jhigh; ++j )
+	    sum += x[j]*y[i-j];
+	z[i] = sum;
+    }
+}
+
+
+void Resample( int stepsize, int n, const float* arrin, float* arrout )
+{
+    int isamp;
+    int nsamp = n / stepsize;
+
+    for ( isamp=0;isamp < nsamp; isamp++ )
+        arrout[isamp]= arrin[ isamp*stepsize ];
+}
+
+
+#define mHilbertLength 2*mHalfHilbertLength+1
+
+void Hilbert( int n, float* x, float* y )
+{
+    static int madeh = 0;
+    static float h[mHilbertLength];
+    static int hlen = mHalfHilbertLength;
+    int i;
+    float taper;
+
+    if ( !madeh )
+    {
+	h[hlen] = 0;
+	for ( i=1; i<=hlen; i++ )
+	{
+	    taper = 0.54 + 0.46 * cos( M_PI*(float)i / (float)(hlen) );
+	    h[hlen+i] = taper * ( -(float)(i%2)*2.0 / (M_PI*(float)(i)) );
+	    h[hlen-i] = -h[hlen+i];
+	}
+	madeh = 1;
+    }
+
+    Convolve( mHilbertLength, -hlen, h, n, 0, x, n, 0, y );
+}
