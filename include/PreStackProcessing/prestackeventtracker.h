666d635f02de7a2f28a315d657763fb496adc751
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2010-11-17 15:15:32 +0000

    Added prestackeventtracker.h/.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@20657 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/PreStackProcessing/prestackeventtracker.h
@@ -0,0 +1,69 @@
+#ifndef prestackeventtracker_h
+#define prestackeventtracker_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	K. Tingdahl
+ Date:		November 2010
+ RCS:		$Id: prestackeventtracker.h,v 1.1 2010-11-17 15:15:32 cvskris Exp $
+________________________________________________________________________
+
+
+-*/
+
+#include "factory.h"
+#include "odmemory.h"
+#include "multiid.h"
+#include "bufstring.h"
+
+
+namespace PreStack
+{
+class Gather;
+class Event;
+class MuteDef;
+
+/*!Baseclass for algorithms that track pre-stack events on a gather. */
+
+mClass EventTracker
+{
+public:
+			mDefineFactoryInClass(EventTracker,factory);
+    virtual void	reInit();
+			//!<Should be called after each survey change
+			
+    virtual void	setMute(bool inner,MuteDef*,OD::PtrPolicy);
+    virtual bool	setMute(bool inner,const MultiID&);
+
+    virtual bool	trackEvents(const Gather&,ObjectSet<Event>&) const = 0;
+
+    virtual		~EventTracker();
+    virtual bool	usePar(const IOPar&);
+    virtual void	fillPar(IOPar&) const;
+
+    const char*		errMsg() { return errmsg_.str(); }
+
+    static const char*	sKeyInnerMute() { return "Inner mute"; }
+    static const char*	sKeyOuterMute() { return "Outer mute"; }
+
+protected:			
+    				EventTracker();
+    void			removeMutes();
+
+    MuteDef*			innermute_;
+    MuteDef*			outermute_;
+    bool			ownsinnermute_;
+    bool			ownsoutermute_;
+
+    MultiID			innermuteid_;
+    MultiID			outermuteid_;
+
+    BufferString		errmsg_;
+};
+
+
+}; //namespace
+
+#endif
---
+++ src/PreStackProcessing/prestackeventtracker.cc
@@ -0,0 +1,134 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : K. Tingdahl
+ * DATE     : March 2007
+-*/
+
+static const char* rcsID = "$Id: prestackeventtracker.cc,v 1.1 2010-11-17 15:15:32 cvskris Exp $";
+
+#include "prestackeventtracker.h"
+
+#include "ioman.h"
+#include "prestackmutedef.h"
+#include "prestackmutedeftransl.h"
+
+namespace PreStack
+{
+
+
+EventTracker::EventTracker()
+    : ownsinnermute_( false )
+    , ownsoutermute_( false )
+    , innermute_( 0 )
+    , outermute_( 0 )
+{}
+
+
+EventTracker::~EventTracker()
+{
+    removeMutes();
+}
+
+
+void EventTracker::reInit()
+{ removeMutes(); }
+
+
+void EventTracker::setMute( bool inner, MuteDef* mutedef,
+			    OD::PtrPolicy ptrpolicy )
+{
+    MuteDef*& mymute = inner ? innermute_ : outermute_;
+    bool& myownership = inner ? ownsinnermute_ : ownsoutermute_;
+    MultiID& myid = inner ? innermuteid_ : outermuteid_;
+
+    if ( mymute && myownership )
+	delete mymute;
+
+    mymute = 0;
+    if ( !mutedef )
+	return;
+
+    if ( ptrpolicy==OD::UsePtr )
+    {
+	mymute = mutedef;
+	myownership = false;
+    }
+    else if ( ptrpolicy==OD::CopyPtr )
+    {
+	mymute = new MuteDef(*mutedef);
+	myownership = true;
+    }
+    else
+    {
+	mymute = mutedef;
+	myownership = true;
+    }
+}
+
+#define mErrRet( msg ) { errmsg_ = msg; return false; }
+
+
+bool EventTracker::setMute( bool inner, const MultiID& mid )
+{
+    MultiID& myid = inner ? innermuteid_ : outermuteid_;
+    myid.setEmpty();
+
+    PreStack::MuteDef* mutedef = 0;
+
+    if ( !mid.isEmpty() )
+    {
+	PtrMan<IOObj> ioobj = IOM().get( mid );
+	if ( !ioobj )
+	    mErrRet("Cannot find MuteDef ID in Object Manager");
+	mutedef = new PreStack::MuteDef;
+
+	if ( !mutedef )
+	    mErrRet("Cannot create new mute definition");
+				     
+	if ( !MuteDefTranslator::retrieve( *mutedef, ioobj, errmsg_ ) )
+	{
+	    delete mutedef;
+	    mErrRet(errmsg_);
+	}
+    }
+
+    setMute( inner, mutedef, OD::TakeOverPtr );
+    myid = mid;
+
+    return true;
+}
+
+
+
+void EventTracker::removeMutes()
+{
+    if ( ownsinnermute_ ) delete innermute_;
+    if ( ownsoutermute_ ) delete outermute_;
+
+    ownsinnermute_ = 0;
+    ownsoutermute_ = 0;
+};
+
+
+void EventTracker::fillPar( IOPar& par ) const
+{
+    par.set( sKeyInnerMute(), innermuteid_ );
+    par.set( sKeyOuterMute(), outermuteid_ );
+}
+
+
+bool EventTracker::usePar( const IOPar& par )
+{
+    MultiID imid, omid;
+    if ( !par.get( sKeyInnerMute(), imid ) ||
+	 !par.get( sKeyOuterMute(), omid ) )
+    {
+	mErrRet( "Cannot parse mutes" );
+    }
+
+    return setMute( true, imid ) && setMute( false, omid );
+}
+
+
+} //namespace
+
