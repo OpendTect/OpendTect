fa8d78406b144526866f78d58dbdf8d46b1775b2
cvskarthika <cvskarthika@a985855d-d384-4b45-ae33-31a410998c3c>
2010-11-18 08:33:03 +0000

    Removed SoMFImage class (it is outdated by SoMFImagei32). Added Coin bug fix for setValues method in macro.
    git-svn-id: https://svn.opendtect.org/od/trunk@20661 a985855d-d384-4b45-ae33-31a410998c3c

--- src/SoOD/SoMFImage.cc
+++ src/SoOD/SoMFImage.cc
@@ -7,154 +7,117 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID = "$Id: SoMFImage.cc,v 1.9 2010-10-26 21:23:11 cvskarthika Exp $";
+static const char* rcsID = "$Id: SoMFImage.cc,v 1.10 2010-11-18 08:33:03 cvskarthika Exp $";
 
 
 #include "SoMFImage.h"
 
 #include <Inventor/errors/SoReadError.h>
 
-SO_MFIELD_SOURCE( SoMFImage, SbImage, const SbImage& );
+// bug fix for problem while copying SbImagei32 in setValues
+// remove the macros below once the bug is fixed in Coin
 
-void SoMFImage::initClass()
-{
-    SO_MFIELD_INIT_CLASS( SoMFImage, SoMField );
+// ******************** remove from this point ********************
+#undef SO_MFIELD_VALUE_SOURCE
+
+// from SoSubField.h with the only difference:
+// ( _valtype_ ) cast removed from newvals[i] in the setValues method
+#define SO_MFIELD_VALUE_SOURCE(_class_, _valtype_, _valref_) \
+    int \
+_class_::fieldSizeof(void) const \
+{ \
+          return sizeof(_valtype_); \
+} \
+ \
+void * \
+_class_::valuesPtr(void) \
+{ \
+          return (void *)this->values; \
+} \
+ \
+void \
+_class_::setValuesPtr(void * ptr) \
+{ \
+          this->values = (_valtype_ *)ptr; \
+} \
+ \
+int \
+_class_::find(_valref_ value, SbBool addifnotfound) \
+{ \
+          evaluate(); \
+          for (int i=0; i < this->num; i++) if (this->values[i] == value) return i; \
+         \
+          if (addifnotfound) this->set1Value(this->num, value); \
+          return -1; \
+} \
+ \
+void \
+_class_::setValues(const int start, const int numarg, const _valtype_ * newvals) \
+{ \
+          if (start+numarg > this->maxNum) this->allocValues(start+numarg); \
+          else if (start+numarg > this->num) this->num = start+numarg; \
+         \
+          for (int i=0; i < numarg; i++) \
+            this->values[i+start] = newvals[i]; \
+          this->valueChanged(); \
+} \
+ \
+void \
+_class_::set1Value(const int idx, _valref_ value) \
+{ \
+          if (idx+1 > this->maxNum) this->allocValues(idx+1); \
+          else if (idx+1 > this->num) this->num = idx+1; \
+          this->values[idx] = value; \
+          this->valueChanged(); \
+} \
+ \
+void \
+_class_::setValue(_valref_ value) \
+{ \
+          this->allocValues(1); \
+          this->values[0] = value; \
+          this->valueChanged(); \
+} \
+ \
+SbBool \
+_class_::operator==(const _class_ & field) const \
+{ \
+          if (this == &field) return TRUE; \
+          if (this->getNum() != field.getNum()) return FALSE; \
+         \
+          const _valtype_ * const lhs = this->getValues(0); \
+          const _valtype_ * const rhs = field.getValues(0); \
+          for (int i = 0; i < this->num; i++) if (lhs[i] != rhs[i]) return FALSE; \
+          return TRUE; \
+} \
+ \
+/*! \COININTERNAL */ \
+void \
+_class_::deleteAllValues(void) \
+{ \
+          this->setNum(0); \
+} \
+ \
+/*! This method is used for moving values around internally within a multivalue field. It needs to be overridden in each field so it automatically takes care of running copy contructors where necessary. */ \
+void \
+_class_::copyValue(int to, int from) \
+{ \
+          this->values[to] = this->values[from]; \
 }
 
-
-#define mErrRet { SoReadError::post(in, prematuremsg ); return false; }
-
-
-SbBool SoMFImage::read1Value( SoInput* in, int index )
-{
-    SbVec3s size;
-    int nc;
-
-    static const char* prematuremsg = "Premature end of file";
-
-    if ( !in->read( size[0] ) || !in->read( size[1] ) || !in->read( size[2] ) ||
-         !in->read( nc ) )
-	mErrRet;
-
-    if ( size[0]<0 || size[1]<0 || size[2]<0 || nc<0 || nc>4 )
-    {
-	SoReadError::post( in, "Invalid image specification %dx%dx%dx%d",
-			   size[0], size[1], size[2], nc );
-	return false;
-    }
-
-    const int buffersize = size[0] * size[1] * size[2] * nc;
-
-    if ( !buffersize && (size[0] || size[1] || size[2] || nc) )
-    {
-	SoReadError::post( in, "Invalid image specification %dx%dx%dx%d",
-			   size[0], size[1], size[2], nc );
-	return false;
-    }
-
-
-    if ( !buffersize ) 
-    {
-	values[index].setValue( SbVec3s(0,0,0), 0, NULL );
-	return true;
-    }
-
-    values[index].setValue( size, nc, 0 );
-    unsigned char* pixblock = values[index].getValue( size, nc );
-
-    if ( in->isBinary() && in->getIVVersion() >= 2.1f)
-    {
-	if ( !in->readBinaryArray( pixblock,buffersize ) )
-	    mErrRet;
-    }
-    else
-    {
-	int byte = 0;
-	const int numpixels = size[0] * size[1] * size[2];
-	for ( int i=0; i<numpixels; i++ )
-	{
-	    unsigned int l;
-	    if ( !in->read( l ) )
-		mErrRet;
-
-	    for ( int j=0; j<nc; j++ )
-		pixblock[byte++] =
-		    (unsigned char) ((l >> (8 * (nc-j-1))) & 0xFF);
-	}
-    }
-
-    return true;
-}
-
-
-#define mWriteSpace if ( !binary ) out->write(' ')
-
-void SoMFImage::write1Value( SoOutput* out, int index ) const
-{
-    int nc;
-    SbVec3s size;
-    unsigned char * pixblock = values[index].getValue( size, nc );
-
-    const bool binary = out->isBinary();
-
-    out->write( size[0] ); mWriteSpace;
-    out->write( size[1] ); mWriteSpace;
-    out->write( size[2] ); mWriteSpace;
-    out->write( nc );
-
-
-    if ( out->isBinary() )
-    {
-	int buffersize = size[0] * size[1] * size[2] * nc;
-	if ( buffersize )
-	{
-	    out->writeBinaryArray( pixblock, buffersize );
-	    int padsize = ((buffersize + 3) / 4) * 4 - buffersize;
-	    if ( padsize )
-	    {
-		unsigned char pads[3] = {'\0','\0','\0'};
-		out->writeBinaryArray( pads, padsize );
-	    }
-	}
-    }
-    else
-    {
-	out->write( '\n' );
-	out->indent();
-
-	int numpixels = size[0] * size[1] * size[2];
-	for ( int i=0; i<numpixels; i++ )
-	{
-	    unsigned int data = 0;
-	    for ( int j=0; j<nc; j++ )
-	    {
-		if (j) data <<= 8;
-		data |= (uint32_t)(pixblock[i * nc + j]);
-	    }
-
-	    out->write( data );
-	    if ( ((i+1)%8 == 0) && (i+1 != numpixels))
-	    {
-		out->write( '\n' );
-		out->indent();
-	    }
-	    else
-	    {
-		out->write( ' ' );
-	    }
-	}
-    }
-}
-
+// ******************** remove upto here ********************
 
 
 SO_MFIELD_SOURCE( SoMFImagei32, SbImagei32, const SbImagei32& );
+
 
 void SoMFImagei32::initClass()
 {
     SO_MFIELD_INIT_CLASS( SoMFImagei32, SoMField );
 }
 
+
+#define mErrRet { SoReadError::post(in, prematuremsg ); return false; }
 
 SbBool SoMFImagei32::read1Value( SoInput* in, int index )
 {
@@ -225,6 +188,8 @@ SbBool SoMFImagei32::read1Value( SoInput* in, int index )
     return true;
 }
 
+
+#define mWriteSpace if ( !binary ) out->write(' ')
 
 void SoMFImagei32::write1Value( SoOutput* out, int index ) const
 {
