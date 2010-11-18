96cd4175d767f3492b5d25169b1f844cb1ec62cc
cvskarthika <cvskarthika@a985855d-d384-4b45-ae33-31a410998c3c>
2010-11-18 08:41:05 +0000

    Removed SoMFImage class (outdated by SoMFImagei32).
    git-svn-id: https://svn.opendtect.org/od/trunk@20662 a985855d-d384-4b45-ae33-31a410998c3c

--- include/SoOD/SoMFImage.h
+++ include/SoOD/SoMFImage.h
@@ -7,33 +7,17 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        K. Tingdahl
  Date:          Dec 2005
- RCS:           $Id: SoMFImage.h,v 1.6 2010-09-09 08:26:27 cvskarthika Exp $
+ RCS:           $Id: SoMFImage.h,v 1.7 2010-11-18 08:41:05 cvskarthika Exp $
 ________________________________________________________________________
 
 -*/
 
 
 #include <Inventor/fields/SoSubField.h>
-#include <Inventor/SbImage.h>
 
 #include <SbImagei32.h>
 
 #include "soodbasic.h"
-
-/*!
-The SoMFImage class is a container for SbImage values.
-
-This field is used where nodes, engines or other field containers needs to store
-multiple 2d or 3d images. */ 
-
-mClass SoMFImage : public SoMField
-{
-    SO_MFIELD_HEADER( SoMFImage, SbImage, const SbImage& );
-
-public:
-    static void		initClass();
-};
-
 
 /*!
 The SoMFImagei32 class is a container for SbImagei32 values.
