b322ea9eb4d98e17be542f06609f78579bcf40ae
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2010-11-23 16:06:20 +0000

    Added NotSavedPrompter.
    git-svn-id: https://svn.opendtect.org/od/trunk@20714 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiTools/uinotsaveddlg.h
@@ -0,0 +1,123 @@
+#ifndef uinotsaveddlg_h
+#define uinotsaveddlg_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        K. Tingdahl
+ Date:          November 2010
+ RCS:           $Id: uinotsaveddlg.h,v 1.1 2010-11-23 16:06:20 cvskris Exp $
+________________________________________________________________________
+
+-*/
+
+
+#include "bufstring.h"
+#include "callback.h"
+
+class uiNotSavedDlg;
+class uiParent;
+class NotSavedPrompterData;
+
+
+/*!Presents a list of items that have not been saved, giving the user the
+   opportunity to save them.
+\code
+MyClass::MyClass()
+{
+    NotSavedPrompter::NSP().promptSaving.notify(
+        mCB(this,MyClass,askSaveCB) );
+}
+
+MyClass::~MyClass()
+{
+    NotSavedPrompter::NSP().promptSaving.remove(
+        mCB(this,MyClass,askSaveCB) );
+}
+
+void MyClass::askSaveCB( CallBacker* )
+{
+    if ( not_saved )
+    {
+        BufferString str("Object type \"" );
+	str += name(); str += "\"";
+
+	PtrMan<IOObj> ioobj = IOM().get( mid );
+	const bool issaveas = (bool) ioobj;
+
+        NotSavedPrompter::NSP().addObject( new NotSavedPrompter::Data(
+	    str.buf(), mCB(this,MyClass,saveCB()), issaveas, 0 ) );
+    }
+}
+
+
+void MyClass::saveCB( CallBacker* )
+{
+    if ( NotSavedPrompter::NSP().isSaveAs() )
+    {
+        uiParent* uip = NotSavedPrompter::NSP().getParent();
+	//Start dlg with uiparent and get new name
+    }
+
+    if ( successfulSave )
+        NotSavedPrompter::NSP().reportSuccessfullSave()
+}
+
+\endcode
+*/
+
+   
+mClass NotSavedPrompter : public CallBacker
+{
+public:
+    static NotSavedPrompter&	NSP(); //gives instance
+
+    Notifier<NotSavedPrompter>	promptSaving;
+    				/*!<Will trigger from when users should save all
+				    unsaved objects, normally at survey change
+				    or shutdown. */
+    void	addObject(const char* str,const CallBack& savecb,bool issaveas,
+			  const void* dataptr );
+    		/*!<Lets the object know that you have an object that should
+		    be added to the list of unsaved objects. Normally called
+		    when triggered by promptSaving.
+		    \param str	    Description of the object (e.g. "Horizon A")
+		    \param savecb   Callback that will save the object
+		    \param issaveas true if savecb will prompt user for a name
+		    \param dataptr  Pointer that can be retrieved during savecb
+		*/
+				
+    bool 	isSaveAs() const;
+		/*!<\returns the issaveas status of the currently active object
+		     \note Only valid during a call from a cb
+			   given in addObject() */
+    const void*	getCurrentObjectData() const;
+    		/*!<\returns the dataptr of the currently active object.
+		    \note Only valid during a call from a cb given in
+			  addObject() */
+    uiParent*	getParent();
+		/*!<\returns a pointer to the save-dialog, which can be used
+		       when creating a dialog in a callback given in addObject.
+		   \note Only valid during a call from a cb given in
+			  addObject() */
+
+    void	reportSuccessfullSave();
+    		/*!<Let the dialog know that the current object was successfully
+		    saved.
+		   \note Only valid during a call from a cb given in
+			  addObject() */
+    		
+		NotSavedPrompter();
+    bool	doTrigger(uiParent*,bool withcancel);
+    		//!<Invoke the system. Returns false if cancel has been pressed.
+
+protected:
+    friend class uiNotSavedDlg;
+
+    ObjectSet<NotSavedPrompterData>		objects_;
+    uiNotSavedDlg*				dlg_;
+};
+
+
+#endif
---
+++ src/uiTools/uinotsaveddlg.cc
@@ -0,0 +1,147 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Kristofer Tingdahl
+ Date:          November 2010
+________________________________________________________________________
+
+-*/
+static const char* rcsID = "$Id: uinotsaveddlg.cc,v 1.1 2010-11-23 16:06:20 cvskris Exp $";
+
+#include "uinotsaveddlg.h"
+
+#include "uidialog.h"
+#include "uimain.h"
+#include "uibutton.h"
+#include "uilabel.h"
+
+
+struct NotSavedPrompterData
+{
+public:
+    NotSavedPrompterData(const char* str,const CallBack& cb,bool issaveas,
+			 const void* dataptr )
+	: string_(str), cb_(cb), issaveas_(issaveas)
+	, dataptr_( dataptr )
+    {}
+    BufferString    string_;
+    CallBack        cb_;
+    bool            issaveas_;
+    const void*     dataptr_;
+};
+
+
+
+class uiNotSavedDlg : public uiDialog
+{
+public:
+    uiNotSavedDlg(uiParent* p, NotSavedPrompter& prompter, bool withcancel )
+	: uiDialog( p, uiDialog::Setup( "Not Saved",
+		    "The following objects are not saved", mNoHelpID ) )
+	, prompter_( prompter )
+    {
+	if ( !withcancel ) setCancelText( 0 );
+
+	uiLabel* lastlabel;
+
+	for ( int idx=0; idx<prompter_.objects_.size(); idx++ )
+	{
+	    uiLabel* label =
+		new uiLabel( this, prompter_.objects_[idx]->string_ );
+	    if ( idx ) label->attach( alignedBelow, lastlabel );
+	    lastlabel = label;
+	    uiPushButton* curbutton = new uiPushButton( this, "Save now",
+		    mCB(this,uiNotSavedDlg,buttonCB),
+		    prompter_.objects_[idx]->issaveas_ );
+	    curbutton->attach( rightOf, label );
+	    buttons_ += curbutton;
+	}
+    }
+
+    void	reportSuccessfullSave()
+    {
+	buttons_[activebutton_]->setText( "Saved" );
+	buttons_[activebutton_]->setSensitive( false );
+    }
+    void	buttonCB(CallBacker* cb)
+    {
+	activebutton_ = buttons_.indexOf( (uiPushButton*) cb );
+	prompter_.objects_[activebutton_]->cb_.doCall( &prompter_ );
+    }
+
+    const void*				getCurrentObjectData() const
+    {
+	return prompter_.objects_.validIdx(activebutton_)
+	    ? prompter_.objects_[activebutton_]->dataptr_
+	    : 0;
+    }
+    bool 				isSaveAs() const
+    {
+	return prompter_.objects_.validIdx(activebutton_)
+	    ? prompter_.objects_[activebutton_]->issaveas_
+	    : false;
+    }
+
+
+protected:
+    int					activebutton_;
+    ObjectSet<uiPushButton>		buttons_;
+    NotSavedPrompter&			prompter_;
+};
+
+
+NotSavedPrompter& NotSavedPrompter::NSP()
+{
+    static PtrMan<NotSavedPrompter> res = new NotSavedPrompter;
+    return *res;
+}
+
+
+NotSavedPrompter::NotSavedPrompter()
+    : promptSaving( this )
+    , dlg_( 0 )
+{}
+
+
+bool NotSavedPrompter::doTrigger( uiParent* parent, bool withcancel )
+{
+    promptSaving.trigger();
+    if ( !objects_.size() )
+	return true;
+
+    dlg_ = new uiNotSavedDlg( parent, *this, withcancel );
+    bool retval = dlg_->go();
+    delete dlg_;
+    dlg_ = 0;
+
+    deepErase( objects_ );
+
+    return retval;
+}
+
+
+void NotSavedPrompter::addObject( const char* str,const CallBack& cb,
+	bool issaveas, const void* dataptr )
+{
+    objects_ += new NotSavedPrompterData( str, cb, issaveas, dataptr );
+}
+
+
+
+void NotSavedPrompter::reportSuccessfullSave()
+{
+    if ( dlg_ ) dlg_->reportSuccessfullSave();
+}
+
+
+uiParent* NotSavedPrompter::getParent()
+{ return dlg_; }
+
+
+const void* NotSavedPrompter::getCurrentObjectData() const
+{ return dlg_ ? dlg_->getCurrentObjectData() : 0; }
+
+
+bool NotSavedPrompter::isSaveAs() const
+{ return dlg_ ? dlg_->isSaveAs() : false; }
