eeef8ca59a0cba8c2c5f16532c4ea7335e959356
cvsbruno <cvsbruno@a985855d-d384-4b45-ae33-31a410998c3c>
2010-12-14 08:56:23 +0000

    removed from file manager
    git-svn-id: https://svn.opendtect.org/od/trunk@20940 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiSeis/uiseisfileman.h
+++ include/uiSeis/uiseisfileman.h
@@ -6,7 +6,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        N. Hemstra
  Date:          April 2002
- RCS:           $Id: uiseisfileman.h,v 1.24 2010-12-14 08:50:32 cvsbruno Exp $
+ RCS:           $Id: uiseisfileman.h,v 1.25 2010-12-14 08:56:23 cvsbruno Exp $
 ________________________________________________________________________
 
 -*/
@@ -31,7 +31,6 @@ protected:
     void		dump2DPush(CallBacker*);
     void		browsePush(CallBacker*);
     void		copyPush(CallBacker*);
-    void		importFromOtherSurvPush(CallBacker*);
     void		man2DPush(CallBacker*);
     void		manPS(CallBacker*);
     void		makeDefault(CallBacker*);
