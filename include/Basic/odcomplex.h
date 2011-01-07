b25e6516c9d5ca243933f453a7620afa8705bd03
cvsbruno <cvsbruno@a985855d-d384-4b45-ae33-31a410998c3c>
2011-01-07 12:54:27 +0000

    added undef type
    git-svn-id: https://svn.opendtect.org/od/trunk@21093 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/odcomplex.h
+++ include/Basic/odcomplex.h
@@ -7,12 +7,13 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Nanne Hemstra
  Date:		January 2010
- RCS:		$Id: odcomplex.h,v 1.2 2010-06-11 07:06:44 cvsnanne Exp $
+ RCS:		$Id: odcomplex.h,v 1.3 2011-01-07 12:54:27 cvsbruno Exp $
 ________________________________________________________________________
 
 -*/
 
 #include <complex>
+#include "undefval.h"
 
 #ifndef __win__
 
@@ -40,9 +41,26 @@ public:
 
 } // namespace std
 
+
 typedef std::odcomplex<float> float_complex;
 
 #endif
+
+namespace Values
+{
+template<>
+class Undef<float_complex>
+{
+public:
+    static float_complex val()		{ return float_complex(1e30,1e30); }
+    static bool	hasUdf() 		{ return true; }
+    static bool	isUdf(float_complex f) 	{ return __mIsUndefined(f.real())
+						 || __mIsUndefined(f.imag()); }
+    static void	setUdf(float_complex& f) { f.real() = (float)__mUndefValue;
+					   f.imag() = (float)__mUndefValue; }
+};
+
+};//namespace Values
 
 #endif
 
