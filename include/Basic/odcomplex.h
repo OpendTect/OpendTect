3d54765472494f38ab229a24298402fcf7b0fa18
Bert Bril <bert.bril@dgbes.com>
2011-01-10 14:57:33 +0000

    Hmmm. This should solve compiler picky-ness.
    git-svn-id: https://svn.opendtect.org/od/trunk@21115 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/odcomplex.h
+++ include/Basic/odcomplex.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Nanne Hemstra
  Date:		January 2010
- RCS:		$Id: odcomplex.h,v 1.5 2011-01-10 14:49:21 cvsbert Exp $
+ RCS:		$Id: odcomplex.h,v 1.6 2011-01-10 14:57:33 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -26,28 +26,25 @@ class Undef<float_complex>
 public:
 
     static bool			hasUdf() 		{ return true; }
-    static float_complex	val();
-    static bool			isUdf(float_complex);
-    static void			setUdf(float_complex&);
+    static float_complex	val()
+				{
+				    return float_complex( (float)__mUndefValue,
+					    		  (float)__mUndefValue);
+				}
+    static bool			isUdf( float_complex f )
+				{
+				    const float r = f.real();
+				    const float i = f.imag();
+				    return __mIsUndefined(r)
+					|| __mIsUndefined(i);
+				}
+    static void			setUdf( float_complex& f )
+				{
+				    f.real( (float)__mUndefValue );
+				    f.imag( (float)__mUndefValue );
+				}
 
 };
-
-inline float_complex Undef<float_complex>::val()
-{
-    return float_complex( (float)__mUndefValue, (float)__mUndefValue );
-}
-
-bool Undef<float_complex>::isUdf( float_complex f )
-{
-    const float r = f.real(); const float i = f.imag();
-    return __mIsUndefined(r) || __mIsUndefined(i);
-}
-
-void Undef<float_complex>::setUdf( float_complex& f )
-{
-    f.real( (float)__mUndefValue ); f.imag( (float)__mUndefValue );
-}
-
 
 
 };//namespace Values
