85f41528ba40b3b27512ed2bf8c3f5ddd7bf91e2
Bert Bril <bert.bril@dgbes.com>
2011-01-13 14:51:49 +0000

    Added attrib transl
    git-svn-id: https://svn.opendtect.org/od/trunk@21139 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Strat/strattransl.h
@@ -0,0 +1,20 @@
+#ifndef strattransl_h
+#define strattransl_h
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Bert
+ Date:		Jan 2011
+ RCS:		$Id: strattransl.h,v 1.1 2011-01-13 14:51:49 cvsbert Exp $
+________________________________________________________________________
+
+-*/
+
+#include "transl.h"
+
+mDeclEmptyTranslatorBundle(StratLayerSequenceGenDesc,od,"sgd")
+mDeclEmptyTranslatorBundle(StratLayerSequenceAttribSet,od,"lsa")
+
+
+#endif
