3068271280182db917760aa7f4aa6f5645026892
Yuancheng Liu <yuancheng.liu@dgbes.com>
2011-01-14 16:12:16 +0000

    Fixed an rounding error message.
    git-svn-id: https://svn.opendtect.org/od/trunk@21151 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiODMain/uiodapplmgr.cc
+++ src/uiODMain/uiodapplmgr.cc
@@ -7,7 +7,7 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID = "$Id: uiodapplmgr.cc,v 1.403 2010-12-29 19:20:53 cvsyuancheng Exp $";
+static const char* rcsID = "$Id: uiodapplmgr.cc,v 1.404 2011-01-14 16:12:16 cvsyuancheng Exp $";
 
 #include "uiodapplmgr.h"
 #include "uiodapplmgraux.h"
@@ -455,14 +455,14 @@ bool uiODApplMgr::getNewData( int visid, int attrib )
 		{
 		    StepInterval<double> rg0 = f3ddp->posData().range(true);
 		    StepInterval<double> rg1 = f3ddp->posData().range(false);
-		    int newsz0 = rg0.nrSteps()+1;
-		    int newsz1 = rg1.nrSteps()+1;
-		    if ( rg0.start+(newsz0-1)*rg0.step < rg0.stop ) newsz0++;
-		    if ( rg1.start+(newsz1-1)*rg1.step < rg1.stop ) newsz1++;
+		    const int newsz0 = rg0.nrSteps()+1;
+		    const int newsz1 = rg1.nrSteps()+1;
 		    const int reso = pdd->getResolution()+1;
 		    const int oldsz0 = pdd->getChannels()->getSize(1) / reso;
 		    const int oldsz1 = pdd->getChannels()->getSize(2) / reso;
-		    if ( oldsz0 && oldsz1 && (newsz0!=oldsz0 || newsz1!=oldsz1))
+		    const int diff0 = abs(newsz0-oldsz0);
+		    const int diff1 = abs(newsz1-oldsz1);
+		    if ( oldsz0 && oldsz1 && (diff0>1 || diff1>1) )
 		    { 
 			BufferString msg = "The current data has different ";
 			msg += "step from the loaded attribute(s), if ";
