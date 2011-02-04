ed2964011ac5520fc33e0ac7d96529eb8c9c874d
Bert Bril <bert.bril@dgbes.com>
2011-02-04 11:06:31 +0000

    Untested, uncompiled - time for Bruno to step in
    git-svn-id: https://svn.opendtect.org/od/trunk@21365 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Algo/statgrubbs.h
@@ -0,0 +1,76 @@
+#ifndef statgrubbs_h
+#define statgrubbs_h
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Bert/Bruno
+ Date:          Feb 2011
+ RCS:           $Id: statgrubbs.h,v 1.1 2011-02-04 11:06:31 cvsbert Exp $
+________________________________________________________________________
+
+-*/
+
+#include "gendefs.h"
+#include <math.h>
+
+namespace Stats
+{
+
+mClass Grubbs
+{
+public:
+
+    template <class T,class SzTp>
+    static T	getMax(T*,SzTp sz,SzTp& index_of_max);
+
+};
+
+
+template <class T,class SzTp>
+inline T Stats::Grubbs::getMax( T* arr, SzTp sz, SzTp& idxof )
+{
+    if ( sz < 3 )
+	{ idxof = 0; return sz > 0 ? 0 : mUdf(T); }
+    SzTp startidx = 0;
+    for ( ; startidx<sz && mIsUdf(arr[startidx]); startidx++ )
+	/* just skip undefs at start */;
+    if ( startidx+3 > sz )
+	{ idxof = startidx; return 0; }
+
+    T maxval = arr[startidx]; T minval = maxval, sum = maxval;
+    SzTp minidx = startidx, maxidx = startidx;
+    SzTp nonudfsz = 1;
+    for ( SzTp idx=startidx+1; idx<sz; idx++ )
+    {
+	const T val = arr[idx];
+	if ( mIsUdf(val) ) continue;
+
+	if ( maxval < val ) { maxidx = idx; maxval = val; }
+	if ( minval > val ) { minidx = idx; minval = val; }
+	sum += val;
+	nonudfsz++;
+    }
+    if ( maxval == minval || nonudfsz < 3 )
+	{ idxof = startidx; return 0; }
+
+    const T avg = sum / nonudfsz;
+    sum = 0;
+    for ( SzTp idx=startidx; idx<sz; idx++ )
+    {
+	const T val = arr[idx];
+	if ( mIsUdf(val) ) continue;
+
+	const T delta = avg - arr[idx];
+	sum += delta * delta;
+    }
+    const T stdev = sqrt( sum / nonudfsz );
+
+    const T diffmin = avg - minval;
+    const T diffmax = maxval - avg;
+    return (diffmin > diffmax ? diffmin : diffmax) / stdev;
+}
+
+} // namespace Stats
+
+#endif
