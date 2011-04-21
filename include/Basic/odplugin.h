0fb0664500fe1853469a713caa137e91badd3d23
Bert Bril <bert.bril@dgbes.com>
2011-04-21 09:44:17 +0000

    leave the 'Fn' from the macro name
    git-svn-id: https://svn.opendtect.org/od/trunk@21925 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/odplugin.h
+++ include/Basic/odplugin.h
@@ -6,7 +6,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Bert
  Date:		Apr 2011
- RCS:		$Id: odplugin.h,v 1.1 2011-04-21 09:35:56 cvsbert Exp $
+ RCS:		$Id: odplugin.h,v 1.2 2011-04-21 09:44:17 cvsbert Exp $
 ________________________________________________________________________
 
 -*/
@@ -18,7 +18,7 @@ extern "C" {
 
 
 /* The following function MUST be defined: */
-#define mDefODInitPluginFn(pinm) \
+#define mDefODInitPlugin(pinm) \
     mExternC const char* Init##pinm##Plugin(int,char**); \
     mExternC const char* Init##pinm##Plugin( int argc, char** argv )
 
