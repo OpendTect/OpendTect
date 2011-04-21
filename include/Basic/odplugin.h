7e64e67289969e97037f892681b321d1909bf428
Bert Bril <bert.bril@dgbes.com>
2011-04-21 09:35:56 +0000

    Added
    git-svn-id: https://svn.opendtect.org/od/trunk@21924 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Basic/odplugin.h
@@ -0,0 +1,38 @@
+#ifndef odplugin_h
+#define odplugin_h
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Bert
+ Date:		Apr 2011
+ RCS:		$Id: odplugin.h,v 1.1 2011-04-21 09:35:56 cvsbert Exp $
+________________________________________________________________________
+
+-*/
+
+extern "C" {
+#include "pluginbase.h"
+}
+#include "gendefs.h"
+
+
+/* The following function MUST be defined: */
+#define mDefODInitPluginFn(pinm) \
+    mExternC const char* Init##pinm##Plugin(int,char**); \
+    mExternC const char* Init##pinm##Plugin( int argc, char** argv )
+
+/* The following function SHOULD be defined: */
+#define mDefODPluginInfo(pinm) \
+    mExternC PluginInfo* Get##pinm##PluginInfo(); \
+    mExternC PluginInfo* Get##pinm##PluginInfo()
+
+/* Define ONLY if your plugin needs early loading (i.e. before any UI)
+   This is common for 'Batch'-type plugins.
+ */
+#define mDefODPluginEarlyLoad(pinm) \
+    mExternC int Get##pinm##PluginType(); \
+    mExternC int Get##pinm##PluginType() { return PI_AUTO_INIT_EARLY; }
+
+
+#endif
