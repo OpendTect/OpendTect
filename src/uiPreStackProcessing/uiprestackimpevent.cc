aa338320bea86d472c381405052d339157bd5f54
Raman Singh <raman.singh@dgbes.com>
2011-05-25 04:54:24 +0000

    added uiprestackimpevent.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@22171 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/uiPreStackProcessing/uiprestackimpevent.cc
@@ -0,0 +1,73 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Raman K Singh
+ Date:          May 2011
+________________________________________________________________________
+
+-*/
+
+static const char* rcsID = "$Id: uiprestackimpevent.cc,v 1.1 2011-05-25 04:54:24 cvsraman Exp $";
+
+#include "uiprestackimpevent.h"
+
+#include "ctxtioobj.h"
+#include "executor.h"
+#include "ioman.h"
+#include "ioobj.h"
+#include "prestackeventascio.h"
+#include "prestackeventio.h"
+#include "prestackeventtransl.h"
+#include "uifileinput.h"
+#include "uimsg.h"
+#include "uiioobjsel.h"
+#include "uitaskrunner.h"
+#include "uitblimpexpdatasel.h"
+
+#include <fstream>
+
+namespace PreStack
+{
+
+uiEventImport::uiEventImport( uiParent* p )
+    : uiDialog( p, uiDialog::Setup("Prestack event export",0,"dgb:104.0.3") )
+    , fd_(*EventAscIO::getDesc())
+{
+    filefld_ = new uiFileInput( this, "Input ASCII file",
+	    			uiFileInput::Setup(0).forread(true)
+						     .withexamine(true) );
+
+    dataselfld_ = new uiTableImpDataSel( this, fd_, "105.0.5" );
+    dataselfld_->attach( alignedBelow, filefld_ );
+
+    IOObjContext ctxt( PSEventTranslatorGroup::ioContext() );
+    ctxt.forread = false;
+    outputfld_ = new uiIOObjSel( this, ctxt, "Prestack Events" );
+    outputfld_->attach( alignedBelow, dataselfld_ );
+}
+
+
+bool uiEventImport::acceptOK( CallBacker* )
+{
+    if ( !filefld_->fileName() )
+    {
+	uiMSG().error("No input file selected");
+	return false;
+    }
+
+    if ( !outputfld_->ioobj() )
+	return false;
+
+    RefMan<EventManager> mgr = new EventManager;
+    mgr->setStorageID( outputfld_->key(), false );
+    EventImporter importer( filefld_->fileName(), fd_, *mgr );
+    uiTaskRunner tr( this );
+    if ( !tr.execute(importer) )
+	return false;
+
+    EventWriter writer( outputfld_->getIOObj(), *mgr );
+    return tr.execute( writer );
+}
+
+}; //namespace
