1b8b018ab78e58dfa59a558cb52cfc11c9b7eda6
cvsbruno <cvsbruno@a985855d-d384-4b45-ae33-31a410998c3c>
2011-05-25 15:44:44 +0000

    added
    git-svn-id: https://svn.opendtect.org/od/trunk@22178 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/raytracerrunner.h
@@ -0,0 +1,51 @@
+#ifndef raytracerrunner_h
+#define raytracerrunner_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Bruno
+ Date:          May 2011
+ RCS:           $Id: raytracerrunner.h,v 1.1 2011-05-25 15:44:43 cvsbruno Exp $
+________________________________________________________________________
+
+-*/
+
+#include "ailayer.h"
+#include "executor.h"
+#include "ranges.h"
+#include "raytrace1d.h"
+
+mClass RayTracerRunner : public Executor
+{
+public:
+    				RayTracerRunner(const ObjectSet<AIModel>&,
+						const TypeSet<float>& offs,
+						const RayTracer1D::Setup&);
+
+    int                         nextStep();
+    od_int64                    totalNr() const { return aimodels_.size(); }
+    od_int64                    nrDone() const  { return nrdone_; }
+    const char*                 message() const { return "Running Ray tracers";}
+
+    //available after excution
+    RayTracer1D*		rayTracer(int idx);
+    StepInterval<float>		getTotalRaySampling(bool usenmo);
+
+    const char*			errMsg() const 	{ return errmsg_.buf(); }
+
+protected:
+
+    int 			nrdone_;
+    RayTracer1D::Setup          raysetup_;
+    TypeSet<float>              offsets_;
+
+    BufferString		errmsg_;
+
+    const ObjectSet<AIModel>&	aimodels_;
+    ObjectSet<RayTracer1D> 	raytracers_;
+};
+
+#endif
+
---
+++ src/General/raytracerrunner.cc
@@ -0,0 +1,54 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Bruno
+ Date:          May 2011
+________________________________________________________________________
+
+-*/
+static const char* rcsID = "$Id: raytracerrunner.cc,v 1.1 2011-05-25 15:44:44 cvsbruno Exp $";
+
+
+#include "raytracerrunner.h"
+
+RayTracerRunner::RayTracerRunner( const ObjectSet<AIModel>& aims, 
+				    const TypeSet<float>& offs, 
+				    const RayTracer1D::Setup& su )
+    : Executor("Ray tracer runner") 
+    , aimodels_(aims)
+    , raysetup_(su)
+    , offsets_(offs)
+{}
+
+
+#define mErrRet(msg) { errmsg_ = msg; return ErrorOccurred(); }
+
+int RayTracerRunner::nextStep() 
+{
+    if ( nrdone_ == 0 )
+    {
+	if ( aimodels_.isEmpty() )
+	    mErrRet( "No AI model set" );
+
+	if ( offsets_.isEmpty() )
+	    offsets_ += 0;
+
+	deepErase( raytracers_ );
+    }
+    const AIModel& aim = *aimodels_[nrdone_];
+    if ( aim.isEmpty() )
+	{ nrdone_ ++; return Executor::MoreToDo(); }
+
+    RayTracer1D* rt1d = new RayTracer1D( raysetup_ );
+    rt1d->setModel( true, aim );
+    rt1d->setOffsets( offsets_ );
+    if ( !rt1d->execute() )
+	mErrRet( rt1d->errMsg() );
+
+    raytracers_ += rt1d;
+
+    return MoreToDo();
+}
+
+
