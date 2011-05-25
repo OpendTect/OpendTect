c1939b67c0c7010066ec05ac89dfc6cf29fb5653
Raman Singh <raman.singh@dgbes.com>
2011-05-25 04:53:06 +0000

    added UI for PS event import
    git-svn-id: https://svn.opendtect.org/od/trunk@22170 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiPreStackProcessing/uiprestackimpevent.h
@@ -0,0 +1,45 @@
+#ifndef uiprestackimpevent_h
+#define uiprestackimpevent_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Raman K Singh
+ Date:          May 2011
+ RCS:           $Id: uiprestackimpevent.h,v 1.1 2011-05-25 04:53:06 cvsraman Exp $
+________________________________________________________________________
+
+-*/
+
+
+#include "uidialog.h"
+
+class uiFileInput;
+class uiIOObjSel;
+class uiTableImpDataSel;
+namespace Table { class FormatDesc; }
+
+namespace PreStack
+{
+
+
+mClass uiEventImport : public uiDialog
+{
+public:
+    			uiEventImport(uiParent*);
+
+protected:
+    bool		acceptOK(CallBacker*);
+
+    Table::FormatDesc&	fd_;
+
+    uiFileInput*	filefld_;
+    uiTableImpDataSel*	dataselfld_;
+    uiIOObjSel*		outputfld_;
+};
+
+
+}; //namespace
+
+#endif
