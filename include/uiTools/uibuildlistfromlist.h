ae37ef2f1da9e3ce95d823b9523f7ed5c24bed3b
Bert Bril <bert.bril@dgbes.com>
2011-06-14 11:45:53 +0000

    added uibuildlistfromlist
    git-svn-id: https://svn.opendtect.org/od/trunk@22259 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiTools/uibuildlistfromlist.h
@@ -0,0 +1,102 @@
+#ifndef uibuildlistfromlist_h
+#define uibuildlistfromlist_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Bert
+ Date:          June 2011
+ RCS:           $Id: uibuildlistfromlist.h,v 1.1 2011-06-14 11:45:53 cvsbert Exp $
+________________________________________________________________________
+
+-*/
+
+
+#include "uigroup.h"
+#include "bufstringset.h"
+
+class uiListBox;
+class uiToolButton;
+
+/*!\brief Base class for element allowing the building of lists of items
+          from available items.
+
+ Let's say you want to make a list of persons. These can be male or female, and
+ dep on the gender you want to be able to define different properties.
+
+ uiBuildListFromList::Setup su( false, "person", true );
+
+ Once you create e.g. a female, the option 'female' itself should stay in the
+ 'available' list. So, singleuseitems is false. Here, the order of the defined
+ items is important so we set 'itemsmovable' to true. The constructor will now
+ set default tooltips for add, edit and remove button like "Add person".
+
+ Required:
+ * Both sets of names cannot have double entries.
+ * You only get requests for add, edit and remove. You have to manage
+   underlying objects yourself. Moreover, you have to use 'removeItem' and
+   'addItem' to keep the display in sync.
+
+  */
+
+mClass uiBuildListFromList : public uiGroup
+{
+public:
+
+    mClass Setup
+    {
+    public:
+			Setup(bool singleuseitems,const char* itemtypename,
+			      bool itemsmovable=false);
+
+	mDefSetupMemb(bool,singleuse);
+	mDefSetupMemb(bool,movable);
+	mDefSetupMemb(BufferString,itemtype);
+	mDefSetupMemb(BufferString,addtt);
+	mDefSetupMemb(BufferString,edtt);
+	mDefSetupMemb(BufferString,rmtt);
+
+    };
+
+			uiBuildListFromList(uiParent*,const Setup&,
+					    const char* grpnm=0);
+    bool		haveUserChange() const		{ return usrchg_; }
+
+protected:
+
+    Setup		setup_;
+    bool		usrchg_;
+
+    uiListBox*		avfld_;
+    uiListBox*		deffld_;
+    uiToolButton*	edbut_;
+    uiToolButton*	rmbut_;
+    uiToolButton*	moveupbut_;
+    uiToolButton*	movedownbut_;
+
+    uiToolButton*	lowestStdBut();
+    const char*		curAvSel() const;		//!< null = no selection
+    const char*		curDefSel() const;		//!< null = no selection
+
+    virtual void	editReq(bool isadd)				= 0;
+    virtual void	removeReq()					= 0;
+    virtual const char*	avFromDef(const char*) const			= 0;
+    virtual void	itemSwitch(const char*,const char*) const	{}
+
+    void		setAvailable(const BufferStringSet&); //!< at start
+    void		removeItem(); 
+    void		setItemName(const char*); 
+    void		addItem(const char*); 
+
+    bool		isadd_;
+
+    virtual void	defSelChg(CallBacker*);
+    void		addCB( CallBacker* )		{ editReq( true ); }
+    void		edCB( CallBacker* )		{ editReq( false ); }
+    void		rmCB( CallBacker* )		{ removeReq(); }
+
+};
+
+
+#endif
---
+++ src/uiTools/uibuildlistfromlist.cc
@@ -0,0 +1,153 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        N. Hemstra
+ Date:          March 2003
+________________________________________________________________________
+
+-*/
+static const char* rcsID = "$Id: uibuildlistfromlist.cc,v 1.1 2011-06-14 11:45:53 cvsbert Exp $";
+
+#include "uibuildlistfromlist.h"
+#include "uilistbox.h"
+#include "uitoolbutton.h"
+
+
+uiBuildListFromList::Setup::Setup( bool sing, const char* itmtp, bool mv )
+    : singleuse_(sing)
+    , movable_(mv)
+    , itemtype_(itmtp)
+{
+    if ( itemtype_.isEmpty() )
+	itemtype_ = "item";
+    addtt_.add( "Add " ).add( itemtype_ );
+    edtt_.add( "Edit " ).add( itemtype_ );
+    rmtt_.add( "Remove " ).add( itemtype_ );
+}
+
+
+uiBuildListFromList::uiBuildListFromList( uiParent* p,
+			const uiBuildListFromList::Setup& su, const char* nm )
+    : uiGroup(p,nm?nm:"List-from-list build group")
+    , setup_(su)
+    , usrchg_(false)
+    , movedownbut_(0)
+{
+    BufferString curtxt( "Available " ); curtxt.add(setup_.itemtype_).add("s");
+    avfld_ = new uiListBox( this, curtxt );
+    avfld_->doubleClicked.notify( mCB(this,uiBuildListFromList,addCB) );
+
+    uiToolButton* addbut = new uiToolButton( this, uiToolButton::RightArrow,
+		    setup_.addtt_, mCB(this,uiBuildListFromList,addCB) );
+    addbut->attach( centeredRightOf, avfld_ );
+
+    curtxt = "Defined "; curtxt.add(setup_.itemtype_).add("s");
+    deffld_ = new uiListBox( this, curtxt );
+    deffld_->attach( rightTo, avfld_ );
+    deffld_->attach( ensureRightOf, addbut );
+    deffld_->selectionChanged.notify(
+	    		mCB(this,uiBuildListFromList,defSelChg) );
+    deffld_->doubleClicked.notify( mCB(this,uiBuildListFromList,edCB) );
+
+    edbut_ = new uiToolButton( this, "edit.png",
+		    setup_.edtt_, mCB(this,uiBuildListFromList,edCB) );
+    edbut_->attach( rightOf, deffld_ );
+    rmbut_ = new uiToolButton( this, "trashcan.png",
+		    setup_.rmtt_, mCB(this,uiBuildListFromList,rmCB) );
+    rmbut_->attach( alignedBelow, edbut_ );
+
+    //TODO implement move buttons and handling
+
+    setHAlignObj( deffld_ );
+    defSelChg(0);
+}
+
+
+void uiBuildListFromList::setAvailable( const BufferStringSet& avnms )
+{
+    avfld_->setEmpty();
+    avfld_->addItems( avnms );
+}
+
+
+void uiBuildListFromList::defSelChg( CallBacker* )
+{
+    const int selidx = deffld_->currentItem();
+    if ( !setup_.singleuse_ && selidx >= 0 )
+	avfld_->setCurrentItem( avFromDef(deffld_->getText()) );
+
+    const bool havesel = selidx >= 0;
+    edbut_->setSensitive( havesel );
+    rmbut_->setSensitive( havesel );
+    if ( movedownbut_ )
+    {
+	const int sz = deffld_->size();
+	const bool canmove = sz > 1;
+	moveupbut_->setSensitive( canmove && selidx > 0 );
+	movedownbut_->setSensitive( canmove && selidx < sz - 1 );
+    }
+}
+
+
+void uiBuildListFromList::removeItem()
+{
+    int itmidx = deffld_->currentItem();
+    if ( itmidx < 0 ) return;
+    if ( setup_.singleuse_ )
+	avfld_->insertItem( avFromDef(deffld_->textOfItem(itmidx)), 0 );
+
+    deffld_->removeItem( itmidx );
+    usrchg_ = true;
+
+    itmidx--; if ( itmidx < 0 ) itmidx = 0;
+    deffld_->setCurrentItem( itmidx );
+    defSelChg( 0 ); // explicit because the UI selection hasn't changed
+}
+
+
+void uiBuildListFromList::setItemName( const char* newnm )
+{
+    const int itmidx = deffld_->currentItem();
+    if ( itmidx < 0 ) return;
+    const BufferString orgnm( deffld_->textOfItem(itmidx) );
+    if ( orgnm != newnm )
+    {
+	deffld_->setItemText( itmidx, newnm );
+	usrchg_ = true;
+    }
+}
+
+
+void uiBuildListFromList::addItem( const char* itmnm )
+{
+    deffld_->addItem( itmnm );
+    const int itmidx = deffld_->size() - 1;
+    if ( setup_.singleuse_ )
+    {
+	const int avidx = avfld_->indexOf( avFromDef(itmnm) );
+	avfld_->removeItem( avidx );
+    }
+    deffld_->setCurrentItem( itmidx );
+    usrchg_ = true;
+}
+
+
+uiToolButton* uiBuildListFromList::lowestStdBut()
+{
+    return movedownbut_ ? movedownbut_ : rmbut_;
+}
+
+
+const char* uiBuildListFromList::curDefSel() const
+{
+    const int itmidx = deffld_->currentItem();
+    return itmidx < 0 ? 0 : deffld_->textOfItem(itmidx);
+}
+
+
+const char* uiBuildListFromList::curAvSel() const
+{
+    const int itmidx = avfld_->currentItem();
+    return itmidx < 0 ? 0 : avfld_->textOfItem(itmidx);
+}
