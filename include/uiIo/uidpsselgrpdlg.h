7a2e4ed6f9c413842b894e9ce5c95fd27611d407
Satyaki Maitra <satyaki.maitra@dgbes.com>
2011-06-16 10:16:44 +0000

    uidpsselgrpdlg.cc/h Things shifted form uidatapointsetcrossplotwin.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@22282 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiIo/uidpsselgrpdlg.h
@@ -0,0 +1,43 @@
+#ifndef uidpsselgrpdlg_h
+#define uidpsselgrpdlg_h
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        Satyaki Maitra
+ Date:          June 2011
+ RCS:           $Id: uidpsselgrpdlg.cc,: 
+________________________________________________________________________
+
+-*/
+
+#include "uidialog.h"
+
+class uiDataPointSetCrossPlotter;
+class uiTable;
+class SelectionGrp;
+
+class uiDPSSelGrpDlg : public uiDialog
+{
+public:
+
+			uiDPSSelGrpDlg(uiDataPointSetCrossPlotter&,
+				       const BufferStringSet&);
+protected:
+
+    int					curselgrp_;
+    uiTable*				tbl_;
+    uiDataPointSetCrossPlotter&		plotter_;
+    ObjectSet<SelectionGrp>&		selgrps_;
+
+    void				setCurSelGrp(CallBacker*);
+    void				changeSelGrbNm(CallBacker*);
+    void				addSelGrp(CallBacker*);
+    void				importSelectionGrps(CallBacker*);
+    void				exportSelectionGrps(CallBacker*);
+    void				remSelGrp(CallBacker*);
+    void				changeColCB(CallBacker*);
+    void				scaleSelections(CallBacker*);
+};
+
+# endif
---
+++ src/uiIo/uidpsselgrpdlg.cc
@@ -0,0 +1,185 @@
+/*+
+________________________________________________________________________
+
+ CopyRight:     (C) dGB Beheer B.V.
+ Author:        Satyaki Maitra
+ Date:          June 2011
+ RCS:           $Id: uidpsselgrpdlg.cc,v 1.1 2011-06-16 10:16:44 cvssatyaki Exp $: 
+________________________________________________________________________
+
+-*/
+static const char* rcsID = "";
+
+#include "uidpsselgrpdlg.h"
+
+#include "uicolor.h"
+#include "uidatapointsetcrossplot.h"
+#include "uiimpexpselgrp.h"
+#include "uitable.h"
+#include "uitoolbutton.h"
+
+#include "randcolor.h"
+
+
+uiDPSSelGrpDlg::uiDPSSelGrpDlg( uiDataPointSetCrossPlotter& p,
+				const BufferStringSet& colnames )
+    : uiDialog( p.parent(), uiDialog::Setup("Selection Settings",0, "111.0.4")
+				    .savebutton(!p.isADensityPlot())
+				    .savetext("Select on Ok").modal(false) )
+    , plotter_( p )
+    , selgrps_(p.selectionGrps())
+{
+    setPrefHeight( 300 );
+    TypeSet<int> colids;
+    const DataPointSet& dps = plotter_.dps();
+
+    uiTable::Setup su( selgrps_.size(), 2 );
+    su.rowdesc("Selection Group").selmode(uiTable::Single);
+    tbl_ = new uiTable( this, su, "Selection Groups" );
+    tbl_->setColumnReadOnly( 1, true );
+    tbl_->doubleClicked.notify( mCB(this,uiDPSSelGrpDlg,changeColCB) );
+    tbl_->rowInserted.notify( mCB(this,uiDPSSelGrpDlg,addSelGrp) );
+    tbl_->valueChanged.notify( mCB(this,uiDPSSelGrpDlg,changeSelGrbNm) );
+    tbl_->selectionChanged.notify( mCB(this,uiDPSSelGrpDlg,setCurSelGrp) );
+    tbl_->setColumnLabel( 0, "Name" );
+    tbl_->setColumnLabel( 1, "Color" );
+    for ( int idx=0; idx<selgrps_.size(); idx++ )
+    {
+	tbl_->setText( RowCol(idx,0), selgrps_[idx]->name() );
+	tbl_->setColor( RowCol(idx,1), selgrps_[idx]->col_ );
+    }
+
+    curselgrp_ = tbl_->currentRow() < 0 ? 0 : tbl_->currentRow();
+    uiPushButton* addgrpbut = new uiPushButton( this, "Add group", 
+	    mCB(this,uiDPSSelGrpDlg,addSelGrp), true );
+    addgrpbut->attach( alignedBelow, tbl_ );
+    
+    uiPushButton* remgrpbut = new uiPushButton( this, "Remove group", 
+	    mCB(this,uiDPSSelGrpDlg,remSelGrp), true );
+    remgrpbut->attach( rightTo, addgrpbut );
+    
+    uiPushButton* expgrpbut = new uiPushButton( this, "Save groups", 
+	    mCB(this,uiDPSSelGrpDlg,exportSelectionGrps), true );
+    expgrpbut->attach( rightTo, remgrpbut );
+    
+    uiPushButton* impgrpbut = new uiPushButton( this, "Open groups", 
+	    mCB(this,uiDPSSelGrpDlg,importSelectionGrps), true );
+    impgrpbut->attach( rightTo, expgrpbut );
+    
+    uiPushButton* scalesgbut =
+	new uiPushButton( this, "Scale selected selections", 
+			  mCB(this,uiDPSSelGrpDlg,scaleSelections), true );
+    scalesgbut->attach( rightTo, impgrpbut );
+}
+
+
+void uiDPSSelGrpDlg::changeSelGrbNm( CallBacker* )
+{
+
+    if ( tbl_->currentRow() < 0 || !selgrps_.validIdx(tbl_->currentRow()) )
+	return;
+
+    for ( int idx=0; idx<tbl_->nrRows(); idx++ )
+    {
+	SelectionGrp* selgrp = selgrps_[ idx ];
+	selgrp->setName( tbl_->text(RowCol(idx,0)) );
+	selgrp->col_ = tbl_->getColor( RowCol(idx,1) );
+    }
+
+    setCurSelGrp(0);
+}
+
+
+void uiDPSSelGrpDlg::setCurSelGrp( CallBacker* )
+{
+    curselgrp_ = tbl_->currentRow();
+    plotter_.setCurSelGrp( curselgrp_ < 0 ? 0 : curselgrp_ );
+}
+
+
+void uiDPSSelGrpDlg::addSelGrp( CallBacker* cb )
+{
+    tbl_->insertRows( tbl_->nrRows(), 1 );
+    tbl_->setColumnReadOnly( 1, true );
+    RowCol newcell = RowCol( tbl_->nrRows()-1, 1 );
+    tbl_->setColor( RowCol(newcell.row,1), getRandomColor() );
+    BufferString selgrpnm( "No " );
+    static int selgrpnr = 2;
+    selgrpnm += selgrpnr;
+    selgrpnr++;
+    tbl_->setText( RowCol(newcell.row,0), selgrpnm );
+    selgrps_ +=
+	new SelectionGrp( selgrpnm, tbl_->getColor(RowCol(newcell.row,1)) );
+
+    setCurSelGrp(0);
+}
+
+void uiDPSSelGrpDlg::importSelectionGrps( CallBacker* )
+{
+    uiReadSelGrp dlg( this, plotter_ );
+    NotifyStopper ns( tbl_->valueChanged );
+    if ( dlg.go() )
+    {
+	while ( tbl_->nrRows() )
+	    tbl_->removeRow(0);
+
+	tbl_->setNrRows( selgrps_.size() );
+	for ( int idx=0; idx<selgrps_.size(); idx++ )
+	{
+	    BufferString temp(selgrps_[idx]->name());
+	    tbl_->setText( RowCol(idx,0), temp.buf() );
+	    tbl_->setColor( RowCol(idx,1), selgrps_[idx]->col_ );
+	}
+    }
+
+    setCurSelGrp(0);
+}
+
+
+void uiDPSSelGrpDlg::exportSelectionGrps( CallBacker* )
+{
+    const bool showboth =
+	plotter_.isY2Selectable() && plotter_.isY1Selectable();
+    uiExpSelectionArea::Setup su( plotter_.axisHandler(0)->name().buf(), 
+				  plotter_.axisHandler(1)->name().buf(),
+				  plotter_.axisHandler(2)
+				  ? plotter_.axisHandler(2)->name().buf():0);
+    uiExpSelectionArea dlg( this, plotter_.selectionGrps(), su );
+    dlg.go();
+}
+
+
+void uiDPSSelGrpDlg::remSelGrp( CallBacker* )
+{
+    if ( tbl_->nrRows() <= 1 ) return;
+
+    selgrps_.remove( tbl_->currentRow() );
+    tbl_->removeRow( tbl_->currentRow() );
+    
+    setCurSelGrp(0);
+    plotter_.reDrawSelections();
+}
+
+
+void uiDPSSelGrpDlg::changeColCB( CallBacker* )
+{
+    if ( tbl_->currentRow() < 0 ) return;
+
+    RowCol rc = tbl_->notifiedCell();
+    if ( !rc.col ) return;
+
+    Color newcol = tbl_->getColor( rc );
+    if ( selectColor(newcol,this,"Marker color") )
+    {
+	selgrps_[rc.row]->col_ = newcol;
+	tbl_->setColor( rc, newcol );
+    }
+
+    plotter_.reDrawSelections();
+}
+
+
+void uiDPSSelGrpDlg::scaleSelections( CallBacker* )
+{
+    plotter_.uiPointSet().addScaledSelColumn();
+}
