3c55166bd9a08422ce299aea867d2d5ce88e4d3c
cvsbruno <cvsbruno@a985855d-d384-4b45-ae33-31a410998c3c>
2011-08-05 15:00:53 +0000

    translators for elasticpropsel
    git-svn-id: https://svn.opendtect.org/od/trunk@22570 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/elasticpropseltransl.h
@@ -0,0 +1,20 @@
+#ifndef elasticpropseltransl_h
+#define elasticpropseltransl_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Bruno
+ Date:		Aug 2011
+ RCS:		$Id: elasticpropseltransl.h,v 1.1 2011-08-05 15:00:53 cvsbruno Exp $
+________________________________________________________________________
+
+-*/
+
+#include "transl.h"
+
+mDeclEmptyTranslatorBundle(ElasticPropSelection,od,"elas")
+
+#endif
+
