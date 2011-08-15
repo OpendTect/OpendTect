025c8a43ea2306c87819334e2bad352713b7542c
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2011-08-15 12:34:51 +0000

    Added getPoints
    git-svn-id: https://svn.opendtect.org/od/trunk@22600 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Algo/gridder2d.h
+++ include/Algo/gridder2d.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Y.C.Liu & K. Tingdahl
  Date:		January 2008
- RCS:		$Id: gridder2d.h,v 1.19 2011-04-30 17:32:03 cvskris Exp $
+ RCS:		$Id: gridder2d.h,v 1.20 2011-08-15 12:34:51 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -57,6 +57,7 @@ public:
 			    values in setValues. Don't re-set it unless they
 			    have changes, as it may lead to substantial
 			    computations. */
+    const TypeSet<Coord>* getPoints() const { return points_; }
     virtual bool	setGridPoint(const Coord&);
 			/*!This is where we want to compute a value */
     virtual bool	init()					= 0;
