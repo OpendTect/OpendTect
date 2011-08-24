c91aac8491318146886fbba32d1039aa697eedb4
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2011-08-24 12:32:24 +0000

    Interface usability improvements.
    git-svn-id: https://svn.opendtect.org/od/trunk@22637 a985855d-d384-4b45-ae33-31a410998c3c

--- plugins/uiVoxelConnectivityFilter/uivoxelconnectivityfilter.cc
+++ plugins/uiVoxelConnectivityFilter/uivoxelconnectivityfilter.cc
@@ -7,7 +7,7 @@ _______________________________________________________________________________
 _______________________________________________________________________________
 
  -*/
-static const char* rcsID = "$Id: uivoxelconnectivityfilter.cc,v 1.1 2011-08-11 09:46:19 cvskris Exp $";
+static const char* rcsID = "$Id: uivoxelconnectivityfilter.cc,v 1.2 2011-08-24 12:32:24 cvskris Exp $";
 
 #include "uivoxelconnectivityfilter.h"
 
@@ -15,18 +15,11 @@ static const char* rcsID = "$Id: uivoxelconnectivityfilter.cc,v 1.1 2011-08-11 0
 #include "voxelconnectivityfilter.h"
 #include "uigeninput.h"
 #include "uimsg.h"
+#include "uiseparator.h"
 #include "uivolprocchain.h"
 
 namespace VolProc
 {
-
-void uiVoxelConnectivityFilter::initClass()
-{
-    uiChain::factory().addCreator( createInstance,
-	    VoxelConnectivityFilter::sFactoryKeyword(),
-	    VoxelConnectivityFilter::sFactoryDisplayName() );
-}
-
 
 uiStepDialog* uiVoxelConnectivityFilter::createInstance( uiParent* p,
 							 Step* step )
@@ -38,42 +31,76 @@ uiStepDialog* uiVoxelConnectivityFilter::createInstance( uiParent* p,
 }
 
 
+#define mCutBelow 0
+#define mCutAbove 1
+#define mCutOutside 2
+
+
 uiVoxelConnectivityFilter::uiVoxelConnectivityFilter( uiParent* p,
 	VoxelConnectivityFilter* step )
     : uiStepDialog( p, VoxelConnectivityFilter::sFactoryDisplayName(), step )
 {
-    rangefld_ = new uiGenInput( this, "Clip-range",
-	    			FloatInpIntervalSpec(false) );
-    rangefld_->setValue( step->getAcceptRange() );
+    const char* cutofftypes[] = { "Input less than", "Input more than", "Input outside", 0 };
+    cutofftypefld_ = new uiGenInput( this, "Clip",
+                              StringListInpSpec( cutofftypes ) );
+    const Interval<float>& acceptrange = step->getAcceptRange();
+
+    cutoffvalfld_ = new uiGenInput( this, "Value", FloatInpSpec() );
+    cutoffvalfld_->attach( alignedBelow, cutofftypefld_ );
+    cutoffrangefld_ = new uiGenInput( this, "Range", FloatInpIntervalSpec(false) );
+    cutoffrangefld_->attach( alignedBelow, cutofftypefld_ );
+    cutoffrangefld_->setValue( acceptrange );
+
+    if ( !mIsUdf(acceptrange.start) && !mIsUdf(acceptrange.stop) )
+    {
+	cutofftypefld_->setValue( mCutOutside );
+    }
+    else if ( !mIsUdf(acceptrange.start) )
+    {
+	cutofftypefld_->setValue( mCutBelow );
+	cutoffvalfld_->setValue( acceptrange.start );
+    }
+    else
+    {
+	cutofftypefld_->setValue( mCutAbove );
+	cutoffvalfld_->setValue( acceptrange.stop );
+    }
+
+    cutofftypefld_->valuechanged.notify(
+          mCB( this, uiVoxelConnectivityFilter, updateFieldsCB) );
+
+    uiSeparator* sep = new uiSeparator( this );
+    sep->attach( stretchedBelow, cutoffrangefld_ );
 
     connectivityfld_ = new uiGenInput( this, "Connectivity",
 	    StringListInpSpec( VoxelConnectivityFilter::ConnectivityNames() ) );
     connectivityfld_->setValue( (int) step->getConnectivity() );
-    connectivityfld_->attach( alignedBelow, rangefld_ );
+    connectivityfld_->attach( alignedBelow, cutoffrangefld_ );
+    connectivityfld_->attach( ensureBelow, sep );
 
-    minbodysizefld_ = new uiGenInput( this, "Minimum size",
+    minbodysizefld_ = new uiGenInput( this, "Minimum body size",
 	    IntInpSpec( step->getMinimumBodySize() ) );
     minbodysizefld_->attach( alignedBelow, connectivityfld_ );
 
-    acceptoutputfld_ = new uiGenInput( this, "Accept output",
+    acceptoutputfld_ = new uiGenInput( this, "Inside",
 	    StringListInpSpec( VoxelConnectivityFilter::AcceptOutputNames() ) );
     acceptoutputfld_->setValue( (int) step->getAcceptOutput() );
     acceptoutputfld_->valuechanged.notify(
 	    mCB( this, uiVoxelConnectivityFilter, updateFieldsCB) );
     acceptoutputfld_->attach( alignedBelow, minbodysizefld_ );
 
-    acceptvaluefld_ = new uiGenInput( this, "Accept value",
+    acceptvaluefld_ = new uiGenInput( this, "Inside value",
 	    FloatInpSpec( step->getAcceptValue() ) );
     acceptvaluefld_->attach( alignedBelow, acceptoutputfld_ );
 
-    rejectoutputudffld_ = new uiGenInput( this, "Reject output",
+    rejectoutputudffld_ = new uiGenInput( this, "Outside",
 	    BoolInpSpec(mIsUdf(step->getRejectValue() ),
 			"Undefined value", "Value" ) );
     rejectoutputudffld_->attach( alignedBelow, acceptvaluefld_ );
     rejectoutputudffld_->valuechanged.notify(
 	    mCB( this, uiVoxelConnectivityFilter, updateFieldsCB) );
 
-    rejectoutputvalfld_ = new uiGenInput( this, "Reject output value",
+    rejectoutputvalfld_ = new uiGenInput( this, "Outside value",
 	    FloatInpSpec(step->getRejectValue() ) );
     rejectoutputvalfld_->attach( alignedBelow, rejectoutputudffld_ );
 
@@ -83,6 +110,9 @@ uiVoxelConnectivityFilter::uiVoxelConnectivityFilter( uiParent* p,
 
 void uiVoxelConnectivityFilter::updateFieldsCB( CallBacker* )
 {
+    cutoffvalfld_->display( cutofftypefld_->getIntValue()!=mCutOutside );
+    cutoffrangefld_->display( cutofftypefld_->getIntValue()==mCutOutside );
+
     VoxelConnectivityFilter::AcceptOutput output;
     VoxelConnectivityFilter::parseEnumAcceptOutput( acceptoutputfld_->text(),
 	     					    output );
@@ -93,11 +123,35 @@ void uiVoxelConnectivityFilter::updateFieldsCB( CallBacker* )
 
 bool uiVoxelConnectivityFilter::acceptOK( CallBacker* )
 {
-    const Interval<float> range = rangefld_->getFInterval();
-    if ( mIsUdf(range.start) && mIsUdf(range.stop) )
+    Interval<float> range;
+    if ( cutofftypefld_->getIntValue()==mCutOutside )
     {
-	uiMSG().error("Undefined range");
-	return false;
+	range = cutoffrangefld_->getFInterval();
+	if ( mIsUdf(range.start) || mIsUdf(range.stop) )
+	{
+	    uiMSG().error("Cut range not set");
+	    return false;
+	}
+    }
+    else 
+    {
+	const float cutoffval = cutoffvalfld_->getfValue();
+	if ( mIsUdf(cutoffval) )
+	{
+	    uiMSG().error( "Cut value value not set" );
+	    return false;
+	}
+
+	if ( cutofftypefld_->getIntValue()==mCutBelow )
+	{
+	    range.start = cutoffval;
+	    range.stop = mUdf(float);
+	}
+	else
+	{
+	    range.start = mUdf(float);
+	    range.stop = cutoffval;
+	}
     }
 
     if ( mIsUdf(minbodysizefld_->getIntValue() ) ||
--- plugins/uiVoxelConnectivityFilter/uivoxelconnectivityfilter.h
+++ plugins/uiVoxelConnectivityFilter/uivoxelconnectivityfilter.h
@@ -7,24 +7,26 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Kristofer Tingdahl
  Date:		May 2011
- RCS:		$Id: uivoxelconnectivityfilter.h,v 1.1 2011-08-11 09:46:19 cvskris Exp $
+ RCS:		$Id: uivoxelconnectivityfilter.h,v 1.2 2011-08-24 12:32:24 cvskris Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "uivolprocstepdlg.h"
+#include "voxelconnectivityfilter.h"
 #include "enums.h"
 #include "factory.h"
 
 namespace VolProc
 {
-class VoxelConnectivityFilter;
 
 mClass uiVoxelConnectivityFilter : public uiStepDialog
 {
 public:
-    mDefaultFactoryInstanciationBase( "VoxelConnectivityFilter",
-				      "Voxel Connection Filter" );
+    mDefaultFactoryInstanciationBase(
+	    VolProc::VoxelConnectivityFilter::sFactoryKeyword(),
+	    VolProc::VoxelConnectivityFilter::sFactoryDisplayName())
+	    mDefaultFactoryInitClassImpl( uiStepDialog, createInstance );
     static uiStepDialog*	createInstance(uiParent*,Step*);
 
     				uiVoxelConnectivityFilter(uiParent*,
@@ -36,7 +38,10 @@ protected:
 
     void		updateFieldsCB(CallBacker*);
 
-    uiGenInput*		rangefld_;
+    uiGenInput*		cutofftypefld_;
+    uiGenInput*		cutoffvalfld_;
+    uiGenInput*		cutoffrangefld_;
+
     uiGenInput*		connectivityfld_;
     uiGenInput*		minbodysizefld_;
     uiGenInput*		acceptoutputfld_;
