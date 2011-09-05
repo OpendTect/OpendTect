b2bc965aed3e8a540d8a63e7b93a71204b03ee86
Satyaki Maitra <satyaki.maitra@dgbes.com>
2011-09-05 10:51:41 +0000

    uidatapointsetman.h added
    git-svn-id: https://svn.opendtect.org/od/trunk@22677 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/uiIo/uidatapointsetman.h
@@ -0,0 +1,35 @@
+#ifndef uicrossplotman_h
+#define uicrossplotman_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Satyaki Maitra
+ Date:          August 2011
+ RCS:           $Id: uidatapointsetman.h,v 1.1 2011-09-05 10:51:41 cvssatyaki Exp $
+________________________________________________________________________
+
+-*/
+
+#include "uiobjfileman.h"
+
+/*! \brief
+CrossPlot manager
+*/
+
+mClass uiDataPointSetMan : public uiObjFileMan
+{
+public:
+    				uiDataPointSetMan(uiParent*);
+				~uiDataPointSetMan();
+
+protected:
+
+    void			mergePush(CallBacker*);
+
+    void			mkFileInfo();
+
+};
+
+#endif
