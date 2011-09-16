7fcef5691bb2306863495d72d7180af7111a3048
Bert Bril <bert.bril@dgbes.com>
2011-09-16 09:47:35 +0000

    Added
    git-svn-id: https://svn.opendtect.org/od/trunk@22766 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/Basic/callback.cc
@@ -0,0 +1,79 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : Bert
+ * DATE     : Sep 2011
+-*/
+
+static const char* rcsID = "$Id: callback.cc,v 1.1 2011-09-16 09:47:35 cvsbert Exp $";
+
+#include "callback.h"
+
+
+void CallBack::doCall( CallBacker* cber )
+{
+    if ( obj_ && fn_ )
+	(obj_->*fn_)( cber );
+    else if ( sfn_ )
+	sfn_( cber );
+}
+
+void CallBackSet::doCall( CallBacker* obj, const bool* enabledflag,
+			  CallBacker* exclude )
+{
+    const bool enab = true;
+    const bool& enabled = enabledflag ? *enabledflag : enab;
+    if ( !enabled ) return;
+
+    TypeSet<CallBack> cbscopy = *this;
+    for ( int idx=0; idx<cbscopy.size(); idx++ )
+    {
+	CallBack& cb = cbscopy[idx];
+	if ( indexOf(cb)==-1 )
+	    continue;
+
+	if ( !exclude || cb.cbObj()!=exclude )
+	    cb.doCall( obj );
+    }
+}
+
+
+void CallBackSet::removeWith( CallBacker* cbrm )
+{
+    for ( int idx=0; idx<size(); idx++ )
+    {
+	CallBack& cb = (*this)[idx];
+	if ( cb.cbObj() == cbrm )
+	    { remove( idx ); idx--; }
+    }
+}
+
+
+void CallBackSet::removeWith( CallBackFunction cbfn )
+{
+    for ( int idx=0; idx<size(); idx++ )
+    {
+	CallBack& cb = (*this)[idx];
+	if ( cb.cbFn() == cbfn )
+	    { remove( idx ); idx--; }
+    }
+}
+
+
+void CallBackSet::removeWith( StaticCallBackFunction cbfn )
+{
+    for ( int idx=0; idx<size(); idx++ )
+    {
+	CallBack& cb = (*this)[idx];
+	if ( cb.scbFn() == cbfn )
+	    { remove( idx ); idx--; }
+    }
+}
+
+
+void i_Notifier::removeWith( CallBacker* cb )
+{
+    if ( cber_ == cb )
+	{ cbs_.erase(); cber_ = 0; return; }
+
+    cbs_.removeWith( cb );
+}
---
+++ src/Basic/errh.cc
@@ -0,0 +1,145 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : Bert
+ * DATE     : Sep 2011
+-*/
+
+static const char* rcsID = "$Id: errh.cc,v 1.1 2011-09-16 09:47:35 cvsbert Exp $";
+
+#include "errh.h"
+#include "strmprov.h"
+#include "file.h"
+#include "filepath.h"
+#include "oddirs.h"
+#include "envvars.h"
+#include <iostream>
+#include <fstream>
+
+mGlobal const char* logMsgFileName();
+mGlobal std::ostream& logMsgStrm();
+bool ErrMsgClass::printProgrammerErrs =
+# ifdef __debug__
+    true;
+# else
+    false;
+# endif
+static BufferString logmsgfnm;
+mBasicGlobal int gLogFilesRedirectCode = -1;
+// Not set. 0 = stderr, 1 = log file
+
+
+mBasicGlobal const char* logMsgFileName()
+{
+    return logmsgfnm.buf();
+}
+
+
+#ifndef __win__
+	#define mErrRet(s){ strm = &std::cerr;  \
+	*strm << "Cannot open file for log messages:\n\t" << s << std::endl; \
+	return *strm; } 
+#else 
+	#define mErrRet(s){ strm = new std::ofstream( "nul" ); \
+	*strm << "Cannot open file for log messages:\n\t" << s << std::endl; \
+	return *strm; }
+#endif
+	
+  
+mBasicGlobal std::ostream& logMsgStrm()
+{
+    if ( gLogFilesRedirectCode < 1 )
+	return std::cerr;
+
+    static std::ostream* strm = 0;
+    if ( strm ) return *strm;
+
+    if ( GetEnvVarYN("OD_LOG_STDERR") )
+	{ strm = &std::cerr; return *strm; }
+
+    const char* basedd = GetBaseDataDir();
+    if ( !File::isDirectory(basedd) )
+	mErrRet( "Directory for data storage is invalid" )
+
+    FilePath fp( basedd );
+    fp.add( "LogFiles" );
+    const BufferString dirnm = fp.fullPath();
+    if ( !File::exists(dirnm) )
+	File::createDir( dirnm );
+    if ( !File::isDirectory(dirnm) )
+	mErrRet( "Cannot create proper directory for log file" )
+
+    const FilePath pfp( GetPersonalDir() );
+    BufferString fnm( pfp.fileName() );
+    const char* odusr = GetSoftwareUser();
+    if ( odusr && *odusr )
+	{ fnm += "_"; fnm += odusr; }
+    BufferString datestr = Time::getDateTimeString();
+    replaceCharacter( datestr.buf(), ' ', '-' );
+    replaceCharacter( datestr.buf(), ':', '.' );
+    fnm += "_"; fnm += datestr.buf();
+    fnm += ".txt";
+
+    fp.add( fnm );
+    logmsgfnm = fp.fullPath();
+    StreamData sd = StreamProvider( logmsgfnm ).makeOStream( false );
+    if ( !sd.usable() )
+    {
+	BufferString msg( "Cannot create log file '" );
+	msg += logmsgfnm; msg += "'";
+	logmsgfnm = "";
+	mErrRet( msg );
+    }
+
+    strm = sd.ostrm;
+    return *strm;
+}
+
+
+void UsrMsg( const char* msg, MsgClass::Type t )
+{
+    if ( !MsgClass::theCB().willCall() )
+	logMsgStrm() << msg << std::endl;
+    else
+    {
+	MsgClass obj( msg, t );
+	MsgClass::theCB().doCall( &obj );
+    }
+}
+
+
+void ErrMsg( const char* msg, bool progr )
+{
+    if ( !ErrMsgClass::printProgrammerErrs && progr ) return;
+
+    if ( !MsgClass::theCB().willCall() )
+    {
+	if ( progr )
+	    std::cerr << "(PE) " << msg << std::endl;
+	else if ( msg && *msg )
+	{
+	    const char* start = *msg == '[' ? "" : "Err: ";
+	    logMsgStrm() << start << msg << std::endl;
+	}
+    }
+    else
+    {
+	ErrMsgClass obj( msg, progr );
+	MsgClass::theCB().doCall( &obj );
+    }
+}
+
+
+CallBack& MsgClass::theCB( const CallBack* cb )
+{
+    static CallBack thecb;
+    if ( cb ) thecb = *cb;
+    return thecb;
+}
+
+
+const char* MsgClass::nameOf( MsgClass::Type typ )
+{
+    static const char* strs[] =
+    	{ "Information", "Message", "Warning", "Error", "PE", 0 };
+    return strs[ (int)typ ];
+}
