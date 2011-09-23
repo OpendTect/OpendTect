21cf9faae1b0523e73e87276f9d7b5b30871f6cc
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2011-09-23 13:11:35 +0000

    Added odmemory.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@22849 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/Basic/odmemory.cc
@@ -0,0 +1,32 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        K. Tingdahl
+ Date:          Sep 2011
+________________________________________________________________________
+
+-*/
+static const char* rcsID = "$Id: odmemory.cc,v 1.1 2011-09-23 13:11:35 cvskris Exp $";
+
+#include "odmemory.h"
+
+#include "malloc.h" 
+#include "iopar.h" 
+#include "string2.h" 
+
+void OD::dumpMemInfo( IOPar& res )
+{
+    struct mallinfo info = mallinfo();
+
+    res.set( "Total heap size", getBytesString( info.arena ) );
+    res.set( "Small block size", getBytesString( info.usmblks ) );
+    res.set( "Free small blocks", getBytesString( info.fsmblks ) );
+    res.set( "Ordinary block allocation",
+	     getBytesString( info.uordblks+info.ordblks*8) );
+    res.set( "Free ordinary Ordinary block", getBytesString( info.fordblks ) );
+    res.set( "MMapped size", getBytesString( info.hblkhd ) );
+    res.set( "Total used memory",
+	    getBytesString(info.uordblks+info.ordblks*8+info.usmblks+
+			   info.hblkhd ));
+}
