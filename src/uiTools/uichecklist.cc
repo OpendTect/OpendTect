1bbf7ceeb9aa31cfac06d191b07844d07329733b
Nanne Hemstra <nanne.hemstra@dgbes.com>
2011-11-18 21:48:42 +0000

    Attached finaliseDone notifer to mainObject
    git-svn-id: https://svn.opendtect.org/od/trunk@23183 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiTools/uichecklist.cc
+++ src/uiTools/uichecklist.cc
@@ -7,7 +7,7 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID = "$Id: uichecklist.cc,v 1.5 2011-11-17 15:55:56 cvsbert Exp $";
+static const char* rcsID = "$Id: uichecklist.cc,v 1.6 2011-11-18 21:48:42 cvsnanne Exp $";
 
 #include "uichecklist.h"
 #include "uibutton.h"
@@ -22,7 +22,7 @@ uiCheckList::uiCheckList( uiParent* p, const char* t1, const char* t2,
 {
     addBox( t1 );
     addBox( t2 );
-    finaliseDone.notify( mCB(this,uiCheckList,initGrp) );
+    mainObject()->finaliseDone.notify( mCB(this,uiCheckList,initGrp) );
 }
 
 
@@ -35,7 +35,7 @@ uiCheckList::uiCheckList( uiParent* p, const BufferStringSet& bss,
     for ( int idx=0; idx<bss.size(); idx++ )
 	addBox( bss.get(idx), forcehor );
 
-    finaliseDone.notify( mCB(this,uiCheckList,initGrp) );
+    mainObject()->finaliseDone.notify( mCB(this,uiCheckList,initGrp) );
 }
 
 
