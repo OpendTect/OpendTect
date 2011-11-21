efd09af3427eb1104e67a0d3cd6746854e2a9a6c
Bert Bril <bert.bril@dgbes.com>
2011-11-21 12:58:36 +0000

    Added oddlsite
    git-svn-id: https://svn.opendtect.org/od/trunk@23186 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Network/oddlsite.h
@@ -0,0 +1,81 @@
+#ifndef oddlsite_h
+#define oddlsite_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Bert
+ Date:          Nov 2011
+ RCS:           $Id: oddlsite.h,v 1.1 2011-11-21 12:58:36 cvsbert Exp $
+________________________________________________________________________
+
+-*/
+
+#include "callback.h"
+#include "bufstring.h"
+class BufferStringSet;
+class TaskRunner;
+class DataBuffer;
+class ODHttp;
+
+
+/*!\brief Access to an http site to get the contents of files.
+ 
+  The URL defaults to opendtect.org. The subdir is going to be the current
+  work directory on the site. You can change it at any time (unlike the host).
+
+  There are two main services: get one file, or get a bunch of files. To get
+  one file, you request it. Set some kind of status bar, because it won't
+  return until either an error occurs (e.g. a timeout is exceeded),
+  or the file is there.  This is meant for small files and minimum hassle.
+
+  If you need bigger files and/or you want users to be able to interrupt, then
+  you need the version with the TaskRunner.
+
+ */
+
+class ODDLSite : public CallBacker
+{
+public:
+
+			ODDLSite(const char* the_host,float timeout_sec=0);
+			~ODDLSite();
+    bool		isOK() const			{ return !isfailed_; }
+    bool		reConnect();
+    			//!< usable for 'Re-try'. Done automatically if needed.
+
+    const char*		host() const			{ return host_; }
+    const char*		subDir() const			{ return subdir_; }
+    void		setSubDir( const char* s )	{ subdir_ = s; }
+    void		setTimeOut(float,bool storeinsettings);
+
+    const char*		errMsg() const			{ return errmsg_; }
+
+    bool		getFile(const char* fnm,const char* outfnm=0);
+    			//!< Without a file name, get the DataBuffer
+    DataBuffer*		obtainResultBuf();
+    			//!< the returned databuf becomes yours
+
+    bool		getFiles(const BufferStringSet& fnms,
+				  const char* outputdir,TaskRunner&);
+
+protected:
+
+    BufferString	host_;
+    BufferString	subdir_;
+    float		timeout_;
+
+    ODHttp&		odhttp_;
+    mutable BufferString errmsg_;
+    mutable bool	isfailed_;
+    DataBuffer*		databuf_;
+
+    void		reqFinish(CallBacker*);
+    BufferString	getFileName(const char*) const;
+
+};
+
+
+
+#endif
---
+++ src/Network/oddlsite.cc
@@ -0,0 +1,128 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Bert
+ Date:          Nov 2011
+________________________________________________________________________
+
+-*/
+static const char* rcsID = "$Id: oddlsite.cc,v 1.1 2011-11-21 12:58:36 cvsbert Exp $";
+
+#include "oddlsite.h"
+#include "odhttp.h"
+#include "timefun.h"
+#include "databuf.h"
+#include "filepath.h"
+#include "thread.h"
+#include "settings.h"
+
+static const char* sKeyTimeOut = "Download.Timout";
+
+
+ODDLSite::ODDLSite( const char* h, float t )
+    : odhttp_(*new ODHttp)
+    , timeout_(t)
+    , host_(h)
+    , databuf_(0)
+{
+    if ( timeout_ <= 0 )
+    {
+	Settings::common().get( sKeyTimeOut, timeout_ );
+	if ( timeout_ <= 0 )
+	    timeout_ = 5; // internet is quick these days
+    }
+
+    if ( host_.isEmpty() )
+	host_ = "opendtect.org";
+    odhttp_.requestFinished.notify( mCB(this,ODDLSite,reqFinish) );
+    reConnect();
+}
+
+
+ODDLSite::~ODDLSite()
+{
+    delete databuf_;
+    delete &odhttp_;
+}
+
+
+void ODDLSite::setTimeOut( float t, bool sett )
+{
+    timeout_ = t;
+    if ( sett )
+    {
+	Settings::common().set( sKeyTimeOut, timeout_ );
+	Settings::common().write();
+    }
+}
+
+
+bool ODDLSite::reConnect()
+{
+    if ( odhttp_.state() == ODHttp::Unconnected )
+	odhttp_.setHost( host_ );
+
+    Time::Counter tc; tc.start();
+    while ( odhttp_.state() < ODHttp::Sending )
+    {
+	Threads::sleep( 0.1 );
+	if ( tc.elapsed() > 1000 * timeout_ )
+	{
+	    errmsg_ = "Cannot open connection to ";
+	    errmsg_.add( host_ ).add ( ":\n" );
+	    if ( odhttp_.state() == ODHttp::HostLookup )
+		errmsg_.add ( ":\nHost name lookup timeout" );
+	    if ( odhttp_.state() == ODHttp::Connecting )
+		errmsg_.add ( ":\nHost doesn't respond" );
+	    else
+		errmsg_.add ( ":\nInternet connection not available" );
+	    return false;
+	}
+    }
+    return true;
+}
+
+
+bool ODDLSite::getFile( const char* relfnm, const char* outfnm )
+{
+    //TODO
+    // if outfnm non-null and not empty, open it for write
+    // otherwise, fill the buffer
+
+    errmsg_ = "TODO: getFile not implemented yet";
+    return false;
+}
+
+
+DataBuffer* ODDLSite::obtainResultBuf()
+{
+    DataBuffer* ret = databuf_;
+    databuf_ = 0;
+    return ret;
+}
+
+
+bool ODDLSite::getFiles( const BufferStringSet& fnms, const char* outputdir,
+			 TaskRunner& tr )
+{
+    errmsg_ = "TODO: getFiles not implemented yet";
+    return false;
+}
+
+
+void ODDLSite::reqFinish( CallBacker* )
+{
+    //TODO implement
+}
+
+
+BufferString ODDLSite::getFileName( const char* relfnm ) const
+{
+    BufferString ret;
+    if ( subdir_.isEmpty() )
+	ret = relfnm;
+    else
+	ret = FilePath( subdir_ ).add( relfnm ).fullPath();
+    return ret;
+}
