6c8be024a3322f674a8dac0f8548f7675b182605
Yuancheng Liu <yuancheng.liu@dgbes.com>
2012-01-04 18:19:07 +0000

    Optimized the code.
    git-svn-id: https://svn.opendtect.org/od/trunk@23442 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiEarthModel/uibodyregiondlg.cc
+++ src/uiEarthModel/uibodyregiondlg.cc
@@ -4,7 +4,7 @@
  * DATE     : October 2011
 -*/
 
-static const char* rcsID = "$Id: uibodyregiondlg.cc,v 1.7 2011-12-28 04:33:32 cvsranojay Exp $";
+static const char* rcsID = "$Id: uibodyregiondlg.cc,v 1.8 2012-01-04 18:19:07 cvsyuancheng Exp $";
 
 #include "uibodyregiondlg.h"
 
@@ -46,10 +46,10 @@ static const char* collbls[] = { "Name", "Side", 0 };
 #define cNameCol 0
 #define cSideCol 1
 
-class OptImplicitBodyRegionExtractor : public ParallelTask
+class ImplicitBodyRegionExtractor : public ParallelTask
 {
 public:
-OptImplicitBodyRegionExtractor( const TypeSet<MultiID>& surflist, 
+ImplicitBodyRegionExtractor( const TypeSet<MultiID>& surflist, 
 	const TypeSet<char>& sides, const CubeSampling& cs, Array3D<float>& res)
     : res_( res )
     , cs_( cs )
@@ -96,23 +96,26 @@ OptImplicitBodyRegionExtractor( const TypeSet<MultiID>& surflist,
 		computeFltOuterRange( *flt, sides[idx] );
 	    }
 	}
+
+	computeHorOuterRange();
     }
 
 
-~OptImplicitBodyRegionExtractor()
+~ImplicitBodyRegionExtractor()
 {
     deepUnRef( hors_ );
     deepUnRef( flts_ );
     deepErase( expflts_ );
 }
 
-
 od_int64 nrIterations() const   	{ return cs_.nrZ(); }
 const char* message() const		{ return "Extracting implicit body"; }
 
-
 bool doWork( od_int64 start, od_int64 stop, int threadid )
 {
+    const int lastinlidx = cs_.nrInl()-1;
+    const int lastcrlidx = cs_.nrCrl()-1;
+    const int lastzidx = cs_.nrZ()-1;
     const int horsz = hors_.size();
     const int fltsz = fsides_.size();
     
@@ -135,7 +138,23 @@ bool doWork( od_int64 start, od_int64 stop, int threadid )
 
     for ( int idz=start; idz<=stop && shouldContinue(); idz++, addToNrDone(1) )
     {
+	if ( !idz || idz==lastzidx )
+	    continue;
+
 	const double curz = cs_.zrg.atIndex( idz );
+	bool outsidehorrg = false;
+	for ( int hidx=0; hidx<horoutrgs_.size(); hidx++ )
+	{
+	    if ( horoutrgs_[hidx].includes(curz,false) )
+	    {
+		outsidehorrg = true;
+		break;
+	    }
+	}
+
+	if ( outsidehorrg )
+	    continue;
+
 	if ( fltsz )
 	{
 	    for ( int cidx=0; cidx<3; cidx++ )
@@ -158,6 +177,8 @@ bool doWork( od_int64 start, od_int64 stop, int threadid )
 	{
 	    const int inlidx = cs_.hrg.inlIdx(bid.inl);
 	    const int crlidx = cs_.hrg.crlIdx(bid.crl);	    
+	    if (!inlidx || !crlidx || inlidx==lastinlidx || crlidx==lastcrlidx)
+		continue;
 	    
 	    bool infltrg = true;
 	    for ( int idy=0; idy<fltsz; idy++ )
@@ -275,7 +296,62 @@ bool inFaultRange( const BinID& pos, int curidx,
 	poly.add( c_[3] );
     }
 
-    return poly.isInside( Geom::Point2D<float>(pos.inl,pos.crl), true, 0 );
+    return poly.isInside(Geom::Point2D<float>(pos.inl,pos.crl),true,0);
+}
+
+
+void computeHorOuterRange()
+{
+    Interval<double> hortopoutrg(mUdf(float),mUdf(float));
+    Interval<double> horbotoutrg(mUdf(float),mUdf(float));
+    for ( int idx=0; idx<hors_.size(); idx++ )
+    {
+	const Geometry::BinIDSurface* surf = 
+	    hors_[idx]->geometry().sectionGeometry(hors_[idx]->sectionID(0));
+	const Array2D<float>* depth = surf ? surf->getArray() : 0;
+	const int sz = depth ? depth->info().getTotalSz() : 0;
+	if ( !sz ) continue;
+
+	const float* data = depth->getData();
+	Interval<float> zrg;
+	bool defined = false;
+	for ( int idz=0; idz<sz; idz++ )
+	{
+	    if ( mIsUdf(data[idz]) )
+		continue;
+
+	    if ( !defined )
+	    {
+		zrg.start = zrg.stop = data[idz];
+		defined = true;
+	    }
+	    else
+		zrg.include( data[idz], false );
+	}
+
+	if ( !defined )
+	    continue;
+
+	if ( hsides_[idx]==mBelow )
+	{
+	    if ( hortopoutrg.isUdf() )
+		hortopoutrg.set(cs_.zrg.start,zrg.start);
+	    else if ( hortopoutrg.stop>zrg.start )
+		hortopoutrg.stop = zrg.start;
+	}
+	else
+	{
+	    if ( horbotoutrg.isUdf() )
+		horbotoutrg.set(zrg.stop,cs_.zrg.stop);
+	    else if ( horbotoutrg.start<zrg.stop )
+		hortopoutrg.start = zrg.stop;
+	}
+    }
+
+    if ( !hortopoutrg.isUdf() && hortopoutrg.start<hortopoutrg.stop )
+    	horoutrgs_ += hortopoutrg;
+    if ( !horbotoutrg.isUdf() && horbotoutrg.start<horbotoutrg.stop )
+    	horoutrgs_ += horbotoutrg;
 }
 
 
@@ -332,13 +408,202 @@ TypeSet<char>					fsides_;
 ObjectSet<Geometry::ExplFaultStickSurface>	expflts_;
 TypeSet< Interval<int> >			insidergs_;
 TypeSet< Interval<int> >			outsidergs_;
+TypeSet< Interval<double> >			horoutrgs_;
 };
 
 
-class ImplicitBodyRegionExtractor : public ParallelTask
+uiBodyRegionDlg::uiBodyRegionDlg( uiParent* p, EM::MarchingCubesSurface& emcs )
+    : uiDialog( p, Setup("Body region constructor", "Surrounding boundaries", 
+		mNoHelpID) )
+    , emcs_( emcs )		  
+    , ctio_( EMBodyTranslatorGroup::ioContext() )		  
+{
+    setCtrlStyle( DoAndStay );
+    //setHelpID( "dgb:104.0.4" );
+
+    subvolfld_ =  new uiPosSubSel( this,  uiPosSubSel::Setup( !SI().has3D(),
+		true).withstep(false).seltxt("Volume subselection") );
+
+    table_ = new uiTable( this, uiTable::Setup(4).rowgrow(true).fillrow(true)
+	    .rightclickdisabled(true).selmode(uiTable::Single), "Edges" );
+    table_->attach( alignedBelow, subvolfld_ );
+    table_->setColumnLabels( collbls ); 
+    table_->setLeftMargin( 0 );
+    table_->setSelectionBehavior( uiTable::SelectRows );
+    table_->setColumnResizeMode( uiTable::ResizeToContents );
+    table_->setRowResizeMode( uiTable::Interactive );
+    table_->setColumnStretchable( cNameCol, true );
+
+    addhorbutton_ = new uiPushButton( this, "&Add horizon",
+	    mCB(this,uiBodyRegionDlg,addSurfaceCB), false );
+    addhorbutton_->attach( rightOf, table_ );
+    
+    addfltbutton_ = new uiPushButton( this, "&Add fault",
+	    mCB(this,uiBodyRegionDlg,addSurfaceCB), false );
+    addfltbutton_->attach( alignedBelow, addhorbutton_ );
+
+    removebutton_ = new uiPushButton( this, "&Remove", 
+	    mCB(this,uiBodyRegionDlg,removeSurfaceCB), false );
+    removebutton_->attach( alignedBelow, addfltbutton_ );
+    removebutton_->setSensitive( false );
+
+    ctio_.ctxt.forread = false;
+    outputfld_ = new uiIOObjSel( this, ctio_, "Output region body" );
+    outputfld_->attach( alignedBelow, table_ ); 
+}
+
+
+uiBodyRegionDlg::~uiBodyRegionDlg()
+{}
+
+
+void uiBodyRegionDlg::addSurfaceCB( CallBacker* cb )
+{
+    mDynamicCastGet( uiPushButton*, but, cb );
+    const bool isflt = addfltbutton_==cb;
+    if ( !isflt && addhorbutton_!=cb )
+	return;
+
+    PtrMan<CtxtIOObj> objio =  isflt ? mMkCtxtIOObj(EMFault3D)
+				     : mMkCtxtIOObj(EMHorizon3D);
+    PtrMan<uiIOObjSelDlg> dlg = new uiIOObjSelDlg( this, *objio, 0, true );
+    if ( !dlg->go() )
+	return;
+
+    for ( int idx=0; idx<dlg->nrSel(); idx++ )
+    {
+	const MultiID& mid = dlg->selected( idx );
+	if ( surfacelist_.indexOf(mid)!=-1 )
+	    continue;
+	
+	PtrMan<IOObj> ioobj = IOM().get( mid );
+	addSurfaceTableEntry( *ioobj, isflt, 0 );
+    }
+}
+
+
+void uiBodyRegionDlg::addSurfaceTableEntry( const IOObj& ioobj,	bool isfault, 
+	char side )
+{
+    const int row = surfacelist_.size();
+    if ( row==table_->nrRows() )
+	table_->insertRows( row, 1 );
+
+    BufferStringSet sidenms;
+    if ( isfault )
+    {
+	sidenms.add("Between min Inline and fault");
+	sidenms.add("Between max Inline and fault");
+	sidenms.add("Between min Crossline and fault");
+	sidenms.add("Between max Crossline and fault");
+    }
+    else
+    {
+	sidenms.add("Follow Z increase side (Below)");
+	sidenms.add("Follow Z decrease side (Above)");
+    }
+    
+    uiComboBox* sidesel = new uiComboBox( 0, sidenms, 0 );
+    sidesel->setCurrentItem( side==-1 ? 0 : 1 );
+    
+    table_->setCellObject( RowCol(row,cSideCol), sidesel );
+    table_->setText( RowCol(row,cNameCol), ioobj.name() );
+    table_->setCellReadOnly( RowCol(row,cNameCol), true );
+    
+    surfacelist_ += ioobj.key();
+
+    removebutton_->setSensitive( surfacelist_.size() );
+}
+
+
+void uiBodyRegionDlg::removeSurfaceCB( CallBacker* )
+{
+    const int currow = table_->currentRow();
+    if ( currow==-1 ) return;
+
+    if ( currow<surfacelist_.size() )
+	surfacelist_.remove( currow );
+
+    table_->removeRow( currow );
+    removebutton_->setSensitive( surfacelist_.size() );
+}
+
+
+bool uiBodyRegionDlg::acceptOK( CallBacker* cb )
+{
+    if ( !surfacelist_.size() )
+    {
+	uiMSG().warning("Please select at least one boundary");
+	return false;
+    }
+
+    if ( !outputfld_->commitInput() )
+    {
+	uiMSG().error( "Cannot create the output body" );
+	return false;
+    }
+
+    return createImplicitBody();
+}
+
+
+bool uiBodyRegionDlg::createImplicitBody()
+{
+    MouseCursorChanger mcc( MouseCursor::Wait );
+
+    TypeSet<char> sides;
+    for ( int idx=0; idx<surfacelist_.size(); idx++ )
+    {
+	mDynamicCastGet(uiComboBox*, selbox, 
+		table_->getCellObject(RowCol(idx,cSideCol)) );    
+    	sides += selbox->currentItem();
+    }
+    
+    CubeSampling cs = subvolfld_->envelope();
+    cs.zrg.start -= cs.zrg.step;
+    cs.zrg.stop += cs.zrg.step;
+    cs.hrg.start.inl -= cs.hrg.step.inl;
+    cs.hrg.stop.inl += cs.hrg.step.inl;
+    cs.hrg.start.crl -= cs.hrg.step.crl;
+    cs.hrg.stop.crl += cs.hrg.step.crl;
+
+    mDeclareAndTryAlloc( Array3DImpl<float>*, arr,
+	    Array3DImpl<float> (cs.nrInl(),cs.nrCrl(),cs.nrZ()) );
+    if ( !arr )
+	return false;
+    
+    uiTaskRunner taskrunner( this );
+    ImplicitBodyRegionExtractor ext( surfacelist_, sides, cs, *arr );
+    if ( !taskrunner.execute(ext) )
+    {
+	uiMSG().error("Extracting body region failed.");
+	return false;
+    }
+
+    ::MarchingCubesSurface& mcs = emcs_.surface();
+    mcs.removeAll();
+
+    mcs.setVolumeData( 0, 0, 0, *arr, 0, &taskrunner);
+    
+    emcs_.setInlSampling(
+	    SamplingData<int>(cs.hrg.start.inl,cs.hrg.step.inl) );
+    emcs_.setCrlSampling(
+	    SamplingData<int>(cs.hrg.start.crl,cs.hrg.step.crl) );
+    emcs_.setZSampling( SamplingData<float>(cs.zrg.start,cs.zrg.step) );
+
+    emcs_.setMultiID( ctio_.ioobj->key() );
+    emcs_.setName( ctio_.ioobj->name() );
+    emcs_.setFullyLoaded( true );
+    emcs_.setChangedFlag();
+
+    return true;
+}
+
+//not used
+class BinidWiseImplicitBodyRegionExtractor : public ParallelTask
 {
 public:
-ImplicitBodyRegionExtractor( const TypeSet<MultiID>& surflist, 
+BinidWiseImplicitBodyRegionExtractor( const TypeSet<MultiID>& surflist, 
 	const TypeSet<char>& sides, const CubeSampling& cs, Array3D<float>& res)
     : res_( res )
     , sides_( sides )
@@ -349,7 +614,7 @@ ImplicitBodyRegionExtractor( const TypeSet<MultiID>& surflist,
 	fltsurplgs_.allowNull( true );
     }
 
-~ImplicitBodyRegionExtractor()
+~BinidWiseImplicitBodyRegionExtractor()
 {
     deepUnRef( hors_ );
     deepErase( fltsurplgs_ );
@@ -619,196 +884,6 @@ const TypeSet<char>&			sides_;
 ObjectSet<EM::Horizon>			hors_;
 ObjectSet< ODPolygon<float> >		fltsurplgs_;//vertical flts
 };
-
-
-uiBodyRegionDlg::uiBodyRegionDlg( uiParent* p, EM::MarchingCubesSurface& emcs )
-    : uiDialog( p, uiDialog::Setup("Body region constructor",
-		"Surrounding boundaries", mNoHelpID) )
-    , emcs_( emcs )		  
-    , ctio_( EMBodyTranslatorGroup::ioContext() )		  
-{
-    setCtrlStyle( DoAndStay );
-    //setHelpID( "dgb:104.0.4" );
-
-    subvolfld_ =  new uiPosSubSel( this,  uiPosSubSel::Setup( !SI().has3D(),
-		true).withstep(false).seltxt("Volume subselection") );
-
-    table_ = new uiTable( this, uiTable::Setup(4).rowgrow(true).fillrow(true)
-	    .rightclickdisabled(true).selmode(uiTable::Single), "Edges" );
-    table_->attach( alignedBelow, subvolfld_ );
-    table_->setColumnLabels( collbls ); 
-    table_->setLeftMargin( 0 );
-    table_->setSelectionBehavior( uiTable::SelectRows );
-    table_->setColumnResizeMode( uiTable::ResizeToContents );
-    table_->setRowResizeMode( uiTable::Interactive );
-    table_->setColumnStretchable( cNameCol, true );
-
-    addhorbutton_ = new uiPushButton( this, "&Add horizon",
-	    mCB(this,uiBodyRegionDlg,addSurfaceCB), false );
-    addhorbutton_->attach( rightOf, table_ );
-    
-    addfltbutton_ = new uiPushButton( this, "&Add fault",
-	    mCB(this,uiBodyRegionDlg,addSurfaceCB), false );
-    addfltbutton_->attach( alignedBelow, addhorbutton_ );
-
-    removebutton_ = new uiPushButton( this, "&Remove", 
-	    mCB(this,uiBodyRegionDlg,removeSurfaceCB), false );
-    removebutton_->attach( alignedBelow, addfltbutton_ );
-    removebutton_->setSensitive( false );
-
-    ctio_.ctxt.forread = false;
-    outputfld_ = new uiIOObjSel( this, ctio_, "Output region body" );
-    outputfld_->attach( alignedBelow, table_ ); 
-}
-
-
-uiBodyRegionDlg::~uiBodyRegionDlg()
-{}
-
-
-void uiBodyRegionDlg::addSurfaceCB( CallBacker* cb )
-{
-    mDynamicCastGet( uiPushButton*, but, cb );
-    const bool isflt = addfltbutton_==cb;
-    if ( !isflt && addhorbutton_!=cb )
-	return;
-
-    PtrMan<CtxtIOObj> objio =  isflt ? mMkCtxtIOObj(EMFault3D)
-				     : mMkCtxtIOObj(EMHorizon3D);
-    PtrMan<uiIOObjSelDlg> dlg = new uiIOObjSelDlg( this, *objio, 0, true );
-    if ( !dlg->go() )
-	return;
-
-    for ( int idx=0; idx<dlg->nrSel(); idx++ )
-    {
-	const MultiID& mid = dlg->selected( idx );
-	if ( surfacelist_.indexOf(mid)!=-1 )
-	    continue;
-	
-	PtrMan<IOObj> ioobj = IOM().get( mid );
-	addSurfaceTableEntry( *ioobj, isflt, 0 );
-    }
-}
-
-
-void uiBodyRegionDlg::addSurfaceTableEntry( const IOObj& ioobj,	bool isfault, 
-	char side )
-{
-    const int row = surfacelist_.size();
-    if ( row==table_->nrRows() )
-	table_->insertRows( row, 1 );
-
-    BufferStringSet sidenms;
-    if ( isfault )
-    {
-	sidenms.add("Between min Inline and fault");
-	sidenms.add("Between max Inline and fault");
-	sidenms.add("Between min Crossline and fault");
-	sidenms.add("Between max Crossline and fault");
-    }
-    else
-    {
-	sidenms.add("Follow Z increase side (Below)");
-	sidenms.add("Follow Z decrease side (Above)");
-    }
-    
-    uiComboBox* sidesel = new uiComboBox( 0, sidenms, 0 );
-    sidesel->setCurrentItem( side==-1 ? 0 : 1 );
-    
-    table_->setCellObject( RowCol(row,cSideCol), sidesel );
-    table_->setText( RowCol(row,cNameCol), ioobj.name() );
-    table_->setCellReadOnly( RowCol(row,cNameCol), true );
-    
-    surfacelist_ += ioobj.key();
-
-    removebutton_->setSensitive( surfacelist_.size() );
-}
-
-
-void uiBodyRegionDlg::removeSurfaceCB( CallBacker* )
-{
-    const int currow = table_->currentRow();
-    if ( currow==-1 ) return;
-
-    if ( currow<surfacelist_.size() )
-	surfacelist_.remove( currow );
-
-    table_->removeRow( currow );
-    removebutton_->setSensitive( surfacelist_.size() );
-}
-
-
-bool uiBodyRegionDlg::acceptOK( CallBacker* cb )
-{
-    if ( !surfacelist_.size() )
-    {
-	uiMSG().warning("Please select at least one boundary");
-	return false;
-    }
-
-    if ( !outputfld_->commitInput() )
-    {
-	uiMSG().error( "Cannot create the output body" );
-	return false;
-    }
-
-    createImplicitBody();
-    return false;
-}
-
-
-bool uiBodyRegionDlg::createImplicitBody()
-{
-    MouseCursorChanger mcc( MouseCursor::Wait );
-
-    TypeSet<char> sides;
-    for ( int idx=0; idx<surfacelist_.size(); idx++ )
-    {
-	mDynamicCastGet(uiComboBox*, selbox, 
-		table_->getCellObject(RowCol(idx,cSideCol)) );    
-    	sides += selbox->currentItem();
-    }
-    
-    CubeSampling cs = subvolfld_->envelope();
-    cs.zrg.start -= cs.zrg.step;
-    cs.zrg.stop += cs.zrg.step;
-    cs.hrg.start.inl -= cs.hrg.step.inl;
-    cs.hrg.stop.inl += cs.hrg.step.inl;
-    cs.hrg.start.crl -= cs.hrg.step.crl;
-    cs.hrg.stop.crl += cs.hrg.step.crl;
-
-    mDeclareAndTryAlloc( Array3DImpl<float>*, arr,
-	    Array3DImpl<float> (cs.nrInl(),cs.nrCrl(),cs.nrZ()) );
-    if ( !arr )
-	return false;
-    
-    OptImplicitBodyRegionExtractor ext( surfacelist_, sides, cs, *arr );
-    //ImplicitBodyRegionExtractor ext( surfacelist_, sides, cs, *arr );
-    if ( !ext.execute() )
-    {
-	uiMSG().error("Extracting body region failed.");
-	return false;
-    }
-
-    ::MarchingCubesSurface& mcs = emcs_.surface();
-    mcs.removeAll();
-
-    uiTaskRunner taskrunner( this );
-    mcs.setVolumeData( 0, 0, 0, *arr, 0, &taskrunner);
-    
-    emcs_.setInlSampling(
-	    SamplingData<int>(cs.hrg.start.inl,cs.hrg.step.inl) );
-    emcs_.setCrlSampling(
-	    SamplingData<int>(cs.hrg.start.crl,cs.hrg.step.crl) );
-    emcs_.setZSampling( SamplingData<float>(cs.zrg.start,cs.zrg.step) );
-
-    emcs_.setMultiID( ctio_.ioobj->key() );
-    emcs_.setName( ctio_.ioobj->name() );
-    emcs_.setFullyLoaded( true );
-    emcs_.setChangedFlag();
-
-    return true;
-}
 
 
 
