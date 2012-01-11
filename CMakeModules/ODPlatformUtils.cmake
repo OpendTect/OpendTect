4fd6cad4f75930421f3a44431b157ac5696c5951
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-01-11 16:25:59 +0000

    Removed setting of __debug__ on msvc, as that is done there automatically
    git-svn-id: https://svn.opendtect.org/od/trunk@23525 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODPlatformUtils.cmake
+++ CMakeModules/ODPlatformUtils.cmake
@@ -2,12 +2,14 @@
 #
 #	CopyRight:	dGB Beheer B.V.
 # 	Jan 2012	K. Tingdahl
-#	RCS :		$Id: ODPlatformUtils.cmake,v 1.2 2012-01-11 13:54:12 cvskris Exp $
+#	RCS :		$Id: ODPlatformUtils.cmake,v 1.3 2012-01-11 16:25:59 cvskris Exp $
 #_______________________________________________________________________________
 
-IF( ${CMAKE_BUILD_TYPE} MATCHES Debug)
-    ADD_DEFINITIONS("-D__debug__")
-ENDIF()
+IF(UNIX)
+    IF( ${CMAKE_BUILD_TYPE} MATCHES Debug)
+        ADD_DEFINITIONS("-D__debug__")
+    ENDIF()
+ENDIF(UNIX)
 
 IF(APPLE)
     ADD_DEFINITIONS("-D__mac__ -Dmac")
@@ -15,6 +17,7 @@ IF(APPLE)
     FIND_LIBRARY(STDCPP_LIBRARY stdc++ REQUIRED )
     SET(EXTRA_LIBS ${APP_SERVICES_LIBRARY} )
 ELSEIF(WIN32)
+    ADD_DEFINITIONS("-D_CRT_SECURE_NO_WARNINGS")
 ELSE()
     ADD_DEFINITIONS("-Dlux")
 ENDIF()
