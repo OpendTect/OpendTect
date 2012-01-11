52f987f2734efa5b1cab2b508f1482696dae2783
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-01-11 12:48:32 +0000

    Various changes.
    git-svn-id: https://svn.opendtect.org/od/trunk@23518 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODMacroUtils.cmake
+++ CMakeModules/ODMacroUtils.cmake
@@ -2,14 +2,16 @@
 #
 #	CopyRight:	dGB Beheer B.V.
 # 	Jan 2012	K. Tingdahl
-#	RCS :		$Id: ODMacroUtils.cmake,v 1.1 2012-01-11 11:43:19 cvskris Exp $
+#	RCS :		$Id: ODMacroUtils.cmake,v 1.2 2012-01-11 12:48:32 cvskris Exp $
 #_______________________________________________________________________________
 
 MACRO( OD_INIT_MODULE )
 
 #Add all module dependencies
 IF(DEPS)
-    OD_ADD_DEPS( ${DEPS} )
+    FOREACH( DEP ${DEPS} )
+	OD_ADD_DEPS( ${DEP} )
+    ENDFOREACH()
 ENDIF()
 
 #Add Qt-stuff
@@ -18,13 +20,15 @@ IF(OD_USEQT)
 
     IF(${OD_USEQT} MATCHES "Core" )
 	LIST(APPEND ${${LIB_NAME}_INCLUDEPATH}
+	    ${QT_QTNETWORK_INCLUDE_DIR}
 	    ${QT_QTCORE_INCLUDE_DIR} ${QTDIR}/include )
 	SET(OWN_QTLIBS ${QT_QTCORE_LIBRARY})
     ENDIF()
 
     IF(${OD_USEQT} MATCHES "Sql" )
 	LIST(APPEND ${${LIB_NAME}_INCLUDEPATH}
-	    ${QT_QTSQL_INCLUDE_DIR} ${QTDIR}/include )
+	    ${QT_QTSQL_INCLUDE_DIR}
+	    ${QTDIR}/include )
 	SET(OWN_QTLIBS ${QT_QTSQL_LIBRARY})
     ENDIF()
 
@@ -35,14 +39,14 @@ IF(OD_USEQT)
 	SET(OWN_QTLIBS ${QT_QTGUI_LIBRARY})
     ENDIF()
 
-    IF( MOC_HEADERS )
-	FOREACH( HEADER ${MOC_HEADERS} )
-	    LIST(APPEND MOC_INPUT
+    IF( QT_MOC_HEADERS )
+	FOREACH( HEADER ${QT_MOC_HEADERS} )
+	    LIST(APPEND QT_MOC_INPUT
 		${OpendTect_SOURCE_DIR}/include/${LIB_NAME}/${HEADER})
 	ENDFOREACH()
 
-	QT4_WRAP_CPP (MOC_OUTFILES ${MOC_INPUT})
-    ENDIF( MOC_HEADERS )
+	QT4_WRAP_CPP (QT_MOC_OUTFILES ${QT_MOC_INPUT})
+    ENDIF( QT_MOC_HEADERS )
 ENDIF(OD_USEQT)
 
 #Add current module to include-path
@@ -55,7 +59,7 @@ INCLUDE_DIRECTORIES( ${${LIB_NAME}_INCLUDEPATH} )
 SET( ${LIB_NAME}_DEPS ${${LIB_NAME}_DEPS} PARENT_SCOPE )
 
 #Setup libraries & its deps
-ADD_LIBRARY( ${LIB_NAME} SHARED ${LIB_SOURCES} ${MOC_OUTFILES} )
+ADD_LIBRARY( ${LIB_NAME} SHARED ${LIB_SOURCES} ${QT_MOC_OUTFILES} )
 target_link_libraries(
         ${LIB_NAME}
         ${${LIB_NAME}_DEPS}
@@ -76,25 +80,21 @@ ENDIF(EXEC_SOURCES)
 
 ENDMACRO(OD_INIT_MODULE)
 
-MACRO( OD_ADD_DEPS DEPLIST )
+MACRO( OD_ADD_DEPS DEP )
+#Check if dep is already in the list
+LIST(FIND ${LIB_NAME}_DEPS ${DEP} INDEX)
+IF(${INDEX} EQUAL -1)
+    #Add dependency
+    LIST(APPEND ${LIB_NAME}_DEPS ${DEP})
+    
+    #Add dependencies of dependencies
+    FOREACH( DEPLIB ${${DEP}_DEPS} )
+	OD_ADD_DEPS( ${DEPLIB} )
+    ENDFOREACH()
 
-FOREACH( DEP ${DEPLIST} )
-
-    #Check if dep is already in the list
-    LIST(FIND ${LIB_NAME}_DEPS ${DEP} INDEX)
-    IF(${INDEX} EQUAL -1)
-	#Add dependency
-	LIST(APPEND ${LIB_NAME}_DEPS ${DEP})
-	
-	#Add dependencies of dependencies
-	FOREACH( DEPLIB ${${DEP}_DEPS} )
-	    OD_ADD_DEPS( ${DEPLIB} )
-	ENDFOREACH()
-
-	#Add dependencies to include-path
-	LIST(APPEND ${LIB_NAME}_INCLUDEPATH
-		    ${OpendTect_SOURCE_DIR}/include/${DEP})
-    ENDIF()
-ENDFOREACH()
+    #Add dependencies to include-path
+    LIST(APPEND ${LIB_NAME}_INCLUDEPATH
+		${OpendTect_SOURCE_DIR}/include/${DEP})
+ENDIF()
 
 ENDMACRO(OD_ADD_DEPS)
