061e78ece228ff6b70072e1c124a1d303724e573
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-01-12 07:22:21 +0000

    Added ODUtils
    git-svn-id: https://svn.opendtect.org/od/trunk@23537 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ CMakeModules/ODUtils.cmake
@@ -0,0 +1,9 @@
+#_______________________Pmake__________________________________________________
+#
+#	CopyRight:	dGB Beheer B.V.
+# 	Jan 2012	K. Tingdahl
+#	RCS :		$Id: ODUtils.cmake,v 1.1 2012-01-12 07:22:21 cvskris Exp $
+#_______________________________________________________________________________
+
+ADD_DEFINITIONS(
+"'-DmDeclareRcsID(id)=static const char* __attribute__ ((unused)) rcsID(id)'")
