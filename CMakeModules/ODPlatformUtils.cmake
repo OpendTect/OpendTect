3a7ea616184ea89e3a097505142a776cafe84ddf
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-01-12 06:35:02 +0000

    Added windows libraries
    git-svn-id: https://svn.opendtect.org/od/trunk@23535 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODPlatformUtils.cmake
+++ CMakeModules/ODPlatformUtils.cmake
@@ -2,23 +2,25 @@
 #
 #	CopyRight:	dGB Beheer B.V.
 # 	Jan 2012	K. Tingdahl
-#	RCS :		$Id: ODPlatformUtils.cmake,v 1.3 2012-01-11 16:25:59 cvskris Exp $
+#	RCS :		$Id: ODPlatformUtils.cmake,v 1.4 2012-01-12 06:35:02 cvskris Exp $
 #_______________________________________________________________________________
 
 IF(UNIX)
     IF( ${CMAKE_BUILD_TYPE} MATCHES Debug)
         ADD_DEFINITIONS("-D__debug__")
     ENDIF()
+    IF(APPLE)
+        ADD_DEFINITIONS("-D__mac__ -Dmac")
+        FIND_LIBRARY(APP_SERVICES_LIBRARY ApplicationServices )
+        FIND_LIBRARY(STDCPP_LIBRARY stdc++ REQUIRED )
+        SET(EXTRA_LIBS ${APP_SERVICES_LIBRARY} )
+    ELSE()
+        ADD_DEFINITIONS("-Dlux")
+    ENDIF()
 ENDIF(UNIX)
 
-IF(APPLE)
-    ADD_DEFINITIONS("-D__mac__ -Dmac")
-    FIND_LIBRARY(APP_SERVICES_LIBRARY ApplicationServices )
-    FIND_LIBRARY(STDCPP_LIBRARY stdc++ REQUIRED )
-    SET(EXTRA_LIBS ${APP_SERVICES_LIBRARY} )
-ELSEIF(WIN32)
+IF(WIN32)
     ADD_DEFINITIONS("-D_CRT_SECURE_NO_WARNINGS")
-ELSE()
-    ADD_DEFINITIONS("-Dlux")
+    SET(EXTRA_LIBS "ws2_32" "shlwapi")
 ENDIF()
 
