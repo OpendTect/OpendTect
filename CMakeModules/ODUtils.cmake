48ca8d92409f13a5774caf1536df0170e0e91af0
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-01-12 14:14:38 +0000

    Remove mDeclareRcsID
    git-svn-id: https://svn.opendtect.org/od/trunk@23549 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODUtils.cmake
+++ CMakeModules/ODUtils.cmake
@@ -2,8 +2,6 @@
 #
 #	CopyRight:	dGB Beheer B.V.
 # 	Jan 2012	K. Tingdahl
-#	RCS :		$Id: ODUtils.cmake,v 1.1 2012-01-12 07:22:21 cvskris Exp $
+#	RCS :		$Id: ODUtils.cmake,v 1.2 2012-01-12 14:14:38 cvskris Exp $
 #_______________________________________________________________________________
 
-ADD_DEFINITIONS(
-"'-DmDeclareRcsID(id)=static const char* __attribute__ ((unused)) rcsID(id)'")
