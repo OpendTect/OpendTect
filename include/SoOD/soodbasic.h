c7d5fff2c1ab5069ae0c7679e26ba9ffc7d6670c
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-02-01 15:34:59 +0000

    Added definition of __win__
    git-svn-id: https://svn.opendtect.org/od/trunk@23679 a985855d-d384-4b45-ae33-31a410998c3c

--- include/SoOD/soodbasic.h
+++ include/SoOD/soodbasic.h
@@ -6,15 +6,19 @@
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        Nanne Hemstra
  Date:          January 2009
- RCS:           $Id: soodbasic.h,v 1.3 2011-08-29 05:50:50 cvsranojay Exp $
+ RCS:           $Id: soodbasic.h,v 1.4 2012-02-01 15:34:59 cvskris Exp $
  ________________________________________________________________________
 
 -*/
 
-#if defined(WIN32) && defined(SOOD_EXPORTS)
-# define dll_export __declspec( dllexport )
-#else
-# define dll_export
+#if defined(WIN32)
+# undef __win__
+# define __win__ 1
+# if defined(SOOD_EXPORTS)
+#  define dll_export __declspec( dllexport )
+# else
+#  define dll_export
+# endif
 #endif
 
 #ifndef mClass
