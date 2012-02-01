d69636806cfc070e4100e1d5e570e33a2b1b5fed
Yuancheng Liu <yuancheng.liu@dgbes.com>
2012-02-01 19:56:07 +0000

    Fix in #define's, did not compile on linux.
    git-svn-id: https://svn.opendtect.org/od/trunk@23686 a985855d-d384-4b45-ae33-31a410998c3c

--- include/SoOD/soodbasic.h
+++ include/SoOD/soodbasic.h
@@ -6,18 +6,17 @@
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        Nanne Hemstra
  Date:          January 2009
- RCS:           $Id: soodbasic.h,v 1.4 2012-02-01 15:34:59 cvskris Exp $
+ RCS:           $Id: soodbasic.h,v 1.5 2012-02-01 19:56:07 cvsyuancheng Exp $
  ________________________________________________________________________
 
 -*/
 
+#define dll_export
 #if defined(WIN32)
 # undef __win__
 # define __win__ 1
 # if defined(SOOD_EXPORTS)
 #  define dll_export __declspec( dllexport )
-# else
-#  define dll_export
 # endif
 #endif
 
