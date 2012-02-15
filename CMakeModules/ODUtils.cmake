bd7b1d4262546d8acd0bd6ef82f72671dad479cc
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-02-15 10:01:05 +0000

    Moved non-platform stuff from ODPlatform
    git-svn-id: https://svn.opendtect.org/od/trunk@23802 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODUtils.cmake
+++ CMakeModules/ODUtils.cmake
@@ -2,9 +2,25 @@
 #
 #	CopyRight:	dGB Beheer B.V.
 # 	Jan 2012	K. Tingdahl
-#	RCS :		$Id: ODUtils.cmake,v 1.3 2012-01-23 20:52:35 cvskris Exp $
+#	RCS :		$Id: ODUtils.cmake,v 1.4 2012-02-15 10:01:05 cvskris Exp $
 #_______________________________________________________________________________
 
 #Empty ModDeps-file
-FILE(WRITE ${OpendTect_SOURCE_DIR}/Pmake/ModDeps.od "")
+SET( OD_MODDEPS_FILE ${CMAKE_BINARY_DIR}/Pmake/ModDeps.od )
+FILE(WRITE ${OD_MODDEPS_FILE} "")
+
+#Discover 64 or 32 bits
+IF(CMAKE_SIZEOF_VOID_P MATCHES "8")
+    SET( OD_64BIT 1 )
+ENDIF()
+
+#Discover 64 or 32 bits
+IF( ${CMAKE_BUILD_TYPE} MATCHES Debug)
+    SET( OD_DEBUG 1 )
+    SET( OD_OUTPUTDIR "G" )
+ELSE()
+    SET( OD_DEBUG )
+    SET( OD_OUTPUTDIR "O" )
+ENDIF()
+
 
