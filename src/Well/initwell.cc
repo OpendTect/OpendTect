b623a73d66ad44f96adb3b813084d4bc6cebf34f
Nanne Hemstra <nanne.hemstra@dgbes.com>
2012-02-24 23:15:31 +0000

    Added wellposprovider
    git-svn-id: https://svn.opendtect.org/od/trunk@23961 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Well/initwell.cc
+++ src/Well/initwell.cc
@@ -4,13 +4,14 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        Nageswara
  Date:          July 2010
- RCS:           $Id: initwell.cc,v 1.3 2011-08-23 14:51:33 cvsbert Exp $
+ RCS:           $Id: initwell.cc,v 1.4 2012-02-24 23:15:31 cvsnanne Exp $
 ________________________________________________________________________
 
 -*/
 
 #include "moddepmgr.h"
 #include "wellt2dtransform.h"
+#include "wellposprovider.h"
 
 
 mDefModInitFn(Well)
@@ -18,4 +19,5 @@ mDefModInitFn(Well)
     mIfNotFirstTime( return );
 
     WellT2DTransform::initClass();
+    Pos::WellProvider3D::initClass();
 }
