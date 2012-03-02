65fc16b792e99a6b68d7dc5a9dfb3e6f554efe85
Raman Singh <raman.singh@dgbes.com>
2012-03-02 11:47:04 +0000

    reworked: separate modes for submission and merging
    git-svn-id: https://svn.opendtect.org/od/trunk@24044 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiSeis/od_ClusterProc.cc
+++ src/uiSeis/od_ClusterProc.cc
@@ -7,14 +7,17 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID = "$Id: od_ClusterProc.cc,v 1.4 2009-07-22 16:01:41 cvsbert Exp $";
+static const char* rcsID = "$Id: od_ClusterProc.cc,v 1.5 2012-03-02 11:47:04 cvsraman Exp $";
 
 #include "uiclusterproc.h"
 #include "uimain.h"
 
 #include "envvars.h"
+#include "executor.h"
+#include "file.h"
 #include "filepath.h"
 #include "ioman.h"
+#include "ioobj.h"
 #include "iopar.h"
 #include "keystrs.h"
 #include "plugins.h"
@@ -25,27 +28,46 @@ static const char* rcsID = "$Id: od_ClusterProc.cc,v 1.4 2009-07-22 16:01:41 cvs
 
 #include <iostream>
 
+#define mPrintHelpMsg \
+    std::cerr << "Usage: " << argv[0] << " parfile [-dosubmit] [-nodelete]" \
+	    << std::endl;
 
 int main( int argc, char ** argv )
 {
     od_putProgInfo( argc, argv );
 
-    const int bgadd = argc > 1 && !strcmp(argv[1],"-bg") ? 1 : 0;
-    if ( argc+bgadd < 2 )
+    BufferString parfilenm;
+    bool withdelete = true;
+    bool dosubmit = false;
+
+    for ( int idx=1; idx<argc; idx++ )
     {
-	std::cerr << "Usage: " << argv[0] << " parfile" << std::endl;
+	if ( !strcmp(argv[idx],"-nodelete") )
+	{ withdelete = false; continue; }
+	if ( !strcmp(argv[idx],"-dosubmit") )
+	{ dosubmit = true; continue; }
+	if ( *argv[idx] != '-' )
+	{ parfilenm = argv[idx]; continue; }
+
+	std::cerr << "Unrecognized option : " << argv[idx] << std::endl;
+	mPrintHelpMsg;
 	ExitProgram( 1 );
     }
 
-    FilePath fp( argv[ 1 + bgadd ] );
-    const BufferString parfnm( fp.fullPath() );
-    StreamProvider spin( parfnm );
+    if ( parfilenm.isEmpty() )
+    {
+	mPrintHelpMsg;
+	ExitProgram( 1 );
+    }
+
+    StreamProvider spin( parfilenm );
     StreamData sdin = spin.makeIStream();
     if ( !sdin.usable() )
     {
 	std::cerr << argv[0] << ": Cannot open parameter file" << std::endl;
 	ExitProgram( 1 );
     }
+
     IOPar iop; iop.read( *sdin.istrm, sKey::Pars );
     if ( iop.size() == 0 )
     {
@@ -62,13 +84,23 @@ int main( int argc, char ** argv )
     PIM().setArgs( argc, argv );
     PIM().loadAuto( false );
 
-    uiMain app( argc, argv );
-    uiClusterProc* cp = new uiClusterProc( 0, iop );
+    if ( dosubmit )
+    {
+	uiMain app( argc, argv );
+	uiClusterProc* cp = new uiClusterProc( 0, iop );
 
-    app.setTopLevel( cp );
-    cp->show();
+	app.setTopLevel( cp );
+	cp->show();
 
-    int ret = app.exec();
-    ExitProgram( ret );
-    return ret;
+	int ret = app.exec();
+	ExitProgram( ret );
+    }
+
+    std::cout << "Merging output ... " << std::endl;
+    TextTaskRunner tr( std::cout );
+    BufferString msg;
+    const bool result = uiClusterProc::mergeOutput( iop, &tr, msg, withdelete );
+    std::cout << msg << std::endl;
+    ExitProgram( result ? 0 : 1 );
+    return 0;
 }
--- src/uiSeis/uiclusterproc.cc
+++ src/uiSeis/uiclusterproc.cc
@@ -4,7 +4,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        Raman Singh
  Date:          April 2008
- RCS:		$Id: uiclusterproc.cc,v 1.3 2010-04-23 05:10:56 cvsnanne Exp $
+ RCS:		$Id: uiclusterproc.cc,v 1.4 2012-03-02 11:47:04 cvsraman Exp $
 ________________________________________________________________________
 
 -*/
@@ -13,78 +13,328 @@ ________________________________________________________________________
 
 #include "dirlist.h"
 #include "timer.h"
+#include "file.h"
 #include "filepath.h"
+#include "ioobj.h"
+#include "ioman.h"
+#include "iopar.h"
+#include "manobjectset.h"
+#include "seistrctr.h"
+#include "strmdata.h"
+#include "strmoper.h"
+#include "survinfo.h"
 #include "strmprov.h"
 #include "envvars.h"
 #include "keystrs.h"
-#include "seismerge.h"
+#include "seissingtrcproc.h"
 #include "settings.h"
 
+#include "uiclusterjobprov.h"
 #include "uilabel.h"
+#include "uimsg.h"
 #include "uiprogressbar.h"
 #include "uitaskrunner.h"
+#include "uitextedit.h"
 #include <iostream>
+
+
+static int getExitStatus( const char* logfile, BufferString& msg )
+{
+    StreamData sd = StreamProvider(logfile).makeIStream();
+    if ( !sd.usable() )
+	return -1;
+
+    StrmOper::seek( *sd.istrm, -50, std::ios::end );
+    char buf[51];
+    if ( !StrmOper::readBlock(*sd.istrm,(void*) buf,50) )
+	return -1;
+
+    buf[50] = '\0';
+    char* ptr = buf + 49;
+    while ( --ptr && ptr > buf )
+	if ( *ptr == '\n' )
+	    break;
+
+    msg = ptr;
+    ptr = strstr( msg.buf(), "finished with code" );
+    if ( !ptr )
+    {
+	msg.setEmpty();
+	return -1;
+    }
+
+    ptr += 19;
+    *(ptr + 1) = '\0';
+    return atoi( ptr );
+}
+
+
+struct ClusterJobInfo
+{
+    			ClusterJobInfo(const char* logfnm, const char* desc )
+			    : status_(-2),logfnm_(logfnm), desc_(desc) {}
+
+    int			status_;	// 0=finished,1=error,-2=notsubmitted,
+    					// -1=submitted
+    BufferString	logfnm_;
+    BufferString	desc_;
+};
+
+
+class ClusterJobSubmitter : public ParallelTask
+{
+public:
+ClusterJobSubmitter( ObjectSet<ClusterJobInfo>& jobs, const char* cmd )
+    : ParallelTask("Cluster Job Submitter")
+    , cmd_(cmd)
+    , jobs_(jobs)
+{
+    for ( int idx=0; idx<jobs_.size(); idx++ )
+    {
+	if ( jobs_[idx]->status_ == 1 || jobs_[idx]->status_ == -2 )
+	    jobstodo_ += idx;
+    }
+}
+
+od_int64 nrIterations() const
+{ return jobstodo_.size(); }
+
+bool doWork( od_int64 start, od_int64 stop, int )
+{
+    for ( int idx=start; idx<=stop; idx++ )
+    {
+	BufferString jobcmd( cmd_ );
+	jobcmd += " \'";
+	FilePath fp( jobs_[jobstodo_[idx]]->logfnm_ );
+	fp.setExtension( ".scr" );
+	jobcmd += fp.fullPath();
+	jobcmd += "\' &";
+	if ( system(jobcmd) )
+	    continue;
+	
+	jobs_[jobstodo_[idx]]->status_ = -1;
+	addToNrDone(1);
+    }
+
+    return true;
+}
+
+protected:
+
+    BufferString	cmd_;
+    ObjectSet<ClusterJobInfo>&	jobs_;
+    TypeSet<int>	jobstodo_;
+};
+
+
+class ClusterProc : public NamedObject
+{
+public:
+ClusterProc( const IOPar& pars )
+    : pars_(pars)
+    , jobs_(false)
+{
+}
+
+bool init()
+{
+    FixedString res = pars_.find( sKey::Survey );
+    if ( !res.isEmpty() && SI().getDirName() != res )
+	IOMan::setSurvey( res.str() );
+
+    FixedString scriptdir = pars_.find( "Script dir" );
+    if ( scriptdir.isEmpty() || !File::isDirectory(scriptdir.str()) )
+	return false;
+
+    DirList scriptfiles( scriptdir, DirList::FilesOnly, "*.scr" );
+    for ( int idx=0; idx<scriptfiles.size(); idx++ )
+    {
+	FilePath fp( scriptfiles.fullPath(idx) );
+	fp.setExtension( ".par" );
+	IOPar iop;
+	if ( !iop.read(fp.fullPath(),sKey::Pars) )
+	    continue;
+
+	FixedString desc = iop.find( sKey::Desc );
+	fp.setExtension( ".log" );
+	jobs_ += new ClusterJobInfo( fp.fullPath(), desc.str() );
+    }
+
+    return true;
+}
+
+
+int totalNrJobs()
+{ return jobs_.size(); };
+
+void checkProgress( int& nrjobsfinished, int& nrjobswitherr, BufferString& msg )
+{
+    nrjobsfinished = 0;
+    nrjobswitherr = 0;
+    for ( int idx=0; idx<jobs_.size(); idx++ )
+    {
+	int status = jobs_[idx]->status_;
+	if ( !status )
+	{
+	    nrjobsfinished++;
+	    continue;
+	}
+	else if ( status == 1 )
+	{
+	    nrjobswitherr++;
+	    continue;
+	}
+	else if ( status == -2 )
+	    continue;
+
+	const int exitstatus = getExitStatus( jobs_[idx]->logfnm_.buf(), msg );
+	if ( exitstatus < 0 )
+	    continue;
+
+	jobs_[idx]->status_ = exitstatus ? 1 : 0;
+    }
+}
+
+
+bool submitJobs( TaskRunner* tr )
+{
+    FixedString submitcmd = pars_.find( "Command" );
+    if ( submitcmd.isEmpty() )
+	return false;
+
+    ClusterJobSubmitter jobsubmitter( jobs_, submitcmd.str() );
+    if ( !tr->execute(jobsubmitter) )
+	return false;
+
+    return true;
+}
+
+protected:
+
+    const IOPar&		pars_;
+    ManagedObjectSet<ClusterJobInfo>	jobs_;
+};
 
 
 uiClusterProc::uiClusterProc( uiParent* p, const IOPar& iop )
     : uiDialog(p,uiDialog::Setup("Cluster Processing","Progress window",""))
     , pars_(iop)
     , scriptdirnm_(iop.find(uiClusterProc::sKeyScriptDir()))
+    , proc_(*new ClusterProc(iop))
 {
-    progbar_ = new uiProgressBar( this );
-    progbar_->setPrefWidth( 500 );
+    msgfld_ = new uiTextEdit( this, "Processing progress", true );
+    msgfld_->setPrefHeightInChar( 7 );
+    msgfld_->setPrefWidth( 500 );
 
-    dirlist_ = new DirList( scriptdirnm_.buf(), DirList::FilesOnly, "*.scr" );
-    totalnr_ = dirlist_->size();
+    progbar_ = new uiProgressBar( this );
+    progbar_->attach( alignedBelow, msgfld_ );
+    progbar_->attach( widthSameAs, msgfld_ );
+
+    proc_.init();
+    totalnr_ = proc_.totalNrJobs();
     progbar_->setTotalSteps( totalnr_ );
 
-    BufferString labeltxt( "No. of jobs left: " );
-    labeltxt += totalnr_;
-    progfld_ = new uiLabel( this, labeltxt.buf() );
-    progfld_->attach( alignedBelow, progbar_ );
+    BufferString labeltxt( "Number of jobs finished: " );
+    labeltxt += "XXXX out of XXXX (XXXX with error)";
+    label_ = new uiLabel( this, labeltxt.buf() );
+    label_->attach( alignedBelow, progbar_ );
+
+    uiTaskRunner dlg( this );
+    proc_.submitJobs( &dlg );
 
     timer_ = new Timer("uiClusterProc timer");
     timer_->tick.notify( mCB(this,uiClusterProc,progressCB) );
-    timer_->start( 100, false );
+    timer_->start( 500, false );
 }
 
 
 uiClusterProc::~uiClusterProc()
 {
-std::cerr << "Deleting the window now... " << std::endl;
-    delete dirlist_; delete timer_;
+    delete timer_;
+    delete &proc_;
 }
 
 
 void uiClusterProc::progressCB( CallBacker* )
 {
-    dirlist_->update();
-    const int nrleft = dirlist_->size();
-    progbar_->setProgress( totalnr_ - nrleft );
-    BufferString labeltxt( "No. of jobs left: " );
-    labeltxt += nrleft;
-    progfld_->setText( labeltxt );
-    if ( totalnr_ && !nrleft )
+    int nrjobsdone, nrjobswitherr;
+    BufferString msg;
+    proc_.checkProgress( nrjobsdone, nrjobswitherr, msg );
+    progbar_->setProgress( nrjobsdone + nrjobswitherr );
+    if ( !msg.isEmpty() )
+	msgfld_->append( msg.buf() );
+
+    BufferString labeltxt( "Number of jobs finished: " );
+    labeltxt += nrjobsdone + nrjobswitherr;
+    labeltxt += " out of "; labeltxt += totalnr_;
+    labeltxt += " ("; labeltxt += nrjobswitherr;
+    labeltxt += "with error)";
+    label_->setText( labeltxt );
+    if ( totalnr_ && (nrjobsdone + nrjobswitherr) == totalnr_ )
     {
 	timer_->stop();
-	doPostProcessing();
+	uiTaskRunner dlg( this );
+	if ( nrjobswitherr )
+	{
+	    BufferString ques; ques += nrjobswitherr;
+	    ques += " job(s) either failed or finished with error, ";
+	    ques += "do you still want to proceed with merging the output?";
+	    const int resp = uiMSG().question( ques.buf(), "Merge anyway",
+		    		"Re-submit failed jobs", "Abort" );
+	    if ( resp == -1 )
+		return;
+
+	    if ( !resp )
+	    {
+		proc_.submitJobs( &dlg );
+		timer_->start( 500, false );
+		return;
+	    }
+	}
+
+	mergeOutput( pars_, &dlg, msg );
+	label_->setText( msg.buf() );
     }
 }
 
 
-void uiClusterProc::doPostProcessing()
+#define mErrRet(s) { msg = s; return false; }
+bool uiClusterProc::mergeOutput( const IOPar& pars, TaskRunner* tr,
+				 BufferString& msg, bool withdelete )
 {
-std::cerr << "Post processing now...." << std::endl;
-    PtrMan<SeisMerger> exec = new SeisMerger( pars_ );
+    MultiID key;
+    if ( !pars.get(uiClusterJobProv::sKeyOutputID(),key) )
+	mErrRet("Missing ID of Temporary storage in the parameters file")
+    PtrMan<IOObj> inobj = IOM().get( key );
+    if ( !pars.get("Output.ID",key) )
+	mErrRet("Missing ID of output dataset in the parameters file")
+    PtrMan<IOObj> outobj = IOM().get( key );
+    if ( !inobj || !outobj )
+	mErrRet("Cannot open Output" ) 
+    PtrMan<SeisSingleTraceProc> exec = new SeisSingleTraceProc( inobj, outobj,
+	    	"Data transfer", &pars, "Writing results to output cube" );
+
     if ( !exec )
+	return false;
+
+    if ( !tr->execute(*exec) )
+	mErrRet("Failed to merge output data")
+    else
+	msg = "Finished merging output data";
+
+    if ( !withdelete )
+	return true;
+    
+    MultiID tempid;
+    if ( pars.get("Output.0.Seismic.ID",tempid) )
     {
-	std::cerr << "Failed to create Merger .. " << std::endl;
-	return;
+	IOM().to( SeisTrcTranslatorGroup::ioContext().getSelKey() );
+	IOM().permRemove( tempid );
     }
 
-std::cerr << "Created Merger .. " << std::endl;
-    exec->stacktrcs_ = false;
-    uiTaskRunner dlg( this );
-    dlg.execute( *exec );
-std::cerr << "Finished post-processing... " << std::endl;
+    FixedString tmpdir = pars.find( sKey::TmpStor );
+    if ( tmpdir && File::isDirectory(tmpdir.str()) )
+	File::removeDir( tmpdir.str() );
+
+    return true;
 }
