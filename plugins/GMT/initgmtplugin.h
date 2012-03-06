c098a32f4aef67f2584b9630cdeb3816d55a5f2d
Nageswara Rao <nageswara.rao@dgbes.com>
2012-03-06 06:21:56 +0000

    Added new file
    git-svn-id: https://svn.opendtect.org/od/trunk@24071 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ plugins/GMT/initgmtplugin.h
@@ -0,0 +1,20 @@
+#ifndef initgmtplugin_h
+#define initgmtplugin_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Nageswara
+ Date:          March 2012
+ RCS:           $Id: initgmtplugin.h,v 1.1 2012-03-06 06:21:56 cvsnageswara Exp $
+________________________________________________________________________
+
+*/
+#include "commondefs.h"
+
+namespace GMTPlugin
+{
+    mGlobal void initStdClasses();
+}
+#endif
