12c7fb784873803438196595b28b42368ecda5a6
Bert Bril <bert.bril@dgbes.com>
2012-04-12 14:04:38 +0000

    Implemented new odsysmem stuff
    git-svn-id: https://svn.opendtect.org/od/trunk@24473 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Basic/odmemory.cc
+++ src/Basic/odmemory.cc
@@ -7,40 +7,92 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID = "$Id: odmemory.cc,v 1.3 2011-09-26 15:11:25 cvskris Exp $";
+static const char* rcsID = "$Id: odmemory.cc,v 1.4 2012-04-12 14:04:38 cvsbert Exp $";
 
+#include "odsysmem.h"
 #include "odmemory.h"
 
 #ifdef lux
-#include "malloc.h" 
+#include "strmoper.h" 
+#include <fstream>
+#endif
+#ifdef mac
+#include <unistd.h>
+#include <mach/mach_init.h>
+#include <mach/mach_host.h>
+#include <mach/host_info.h>
 #endif
 
 #include "iopar.h" 
 #include "string2.h" 
 
-bool OD::canDumpMemInfo()
+bool OD::haveMemInfo()
 {
 #ifdef lux
     return true;
-#else
+#endif
+#ifdef mac
+    return true;
+#endif
+#ifdef win
     return false;
 #endif
 }
 
 void OD::dumpMemInfo( IOPar& res )
 {
-#ifdef lux
-    struct mallinfo info = mallinfo();
+    float total, free;
+    getSystemMemory( total, free );
+    total /= 1024; free /= 1024;
+    int itot = mNINT(total); int ifree = mNINT(free);
+    res.set( "Total memory (kB)", itot );
+    res.set( "Free memory (kB)", ifree );
+}
 
-    res.set( "Total heap size", getBytesString( info.arena ) );
-    res.set( "Small block size", getBytesString( info.usmblks ) );
-    res.set( "Free small blocks", getBytesString( info.fsmblks ) );
-    res.set( "Ordinary block allocation",
-	     getBytesString( info.uordblks+info.ordblks*8) );
-    res.set( "Free ordinary Ordinary block", getBytesString( info.fordblks ) );
-    res.set( "MMapped size", getBytesString( info.hblkhd ) );
-    res.set( "Total used memory",
-	    getBytesString(info.uordblks+info.ordblks*8+info.usmblks+
-			   info.hblkhd ));
+
+#ifdef lux
+static float getMemFromStr( char* str, const char* ky )
+{
+    char* ptr = strstr( str, ky );
+    if ( !ptr ) return 0;
+    ptr += strlen( ky );
+    mSkipBlanks(ptr);
+    char* endptr = ptr; mSkipNonBlanks(endptr);
+    *endptr = '\0';
+    float ret = toFloat( ptr );
+    *endptr = '\n';
+    return ret;
+}
+#endif
+
+
+#define mErrRet { total = free = 0; return; }
+
+void OD::getSystemMemory( float& total, float& free )
+{
+#ifdef lux
+
+    std::ifstream strm( "/proc/meminfo" );
+    BufferString filecont;
+    if ( !StrmOper::readFile(strm,filecont) )
+	mErrRet
+
+    total = getMemFromStr( filecont.buf(), "MemTotal:" );
+    free = getMemFromStr( filecont.buf(), "MemFree:" );
+
+#endif
+#ifdef mac
+    vm_statistics_data_t vm_info;
+    mach_msg_type_number_t info_count;
+
+    info_count = HOST_VM_INFO_COUNT;
+    if ( host_statistics(mach_host_self(),HOST_VM_INFO,
+		(host_info_t)&vm_info,&info_count) )
+	mErrRet
+
+    total = (vm_info.active_count + vm_info.inactive_count +
+	    vm_info.free_count + vm_info.wire_count) * vm_page_size;
+    free = vm_info.free_count * vm_page_size;
+
 #endif
 }
