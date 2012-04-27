4e0e95f1652328a4ee107e7e3fe48004c7bf1521
Yuancheng Liu <yuancheng.liu@dgbes.com>
2012-04-27 18:51:58 +0000

    Fixed an error.
    git-svn-id: https://svn.opendtect.org/od/trunk@24649 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visBase/visgeomindexedshape.cc
+++ src/visBase/visgeomindexedshape.cc
@@ -7,7 +7,7 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID = "$Id: visgeomindexedshape.cc,v 1.32 2012-04-26 21:25:25 cvsyuancheng Exp $";
+static const char* rcsID = "$Id: visgeomindexedshape.cc,v 1.33 2012-04-27 18:51:58 cvsyuancheng Exp $";
 
 #include "visgeomindexedshape.h"
 
@@ -516,7 +516,7 @@ void GeomIndexedShape::setAttribData( const DataPointSet& set,TaskRunner* tr)
     {
 	const float* ptr = vals.getVals( pos );
 	const int coordidx = mNINT(ptr[col]);
-	const float val = ptr[col+1];
+	const float val = ptr[col-1];
 
 	if ( coordidx>=cache.size() )
 	{
