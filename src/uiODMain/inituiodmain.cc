a1e3e1ff227e3f9d2a6d383ac1640d7986e5e81f
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-05-01 13:59:01 +0000

    Added inituiodmain.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@24671 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/uiODMain/inituiodmain.cc
@@ -0,0 +1,23 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Nanne and Kristofer
+ Date:          December 2007
+________________________________________________________________________
+
+-*/
+static const char* rcsID = "$Id: inituiodmain.cc,v 1.1 2012-05-01 13:59:01 cvskris Exp $";
+
+#include "moddepmgr.h"
+
+#include "odsession.h"
+
+mDefModInitFn(uiODMain)
+{
+    mIfNotFirstTime( return );
+    
+    ODSessionTranslatorGroup::initClass();
+    dgbODSessionTranslator::initClass();
+
+}
