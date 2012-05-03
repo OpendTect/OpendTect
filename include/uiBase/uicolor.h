ea70ab3eb1bcd1ab6d2e840237627923bab07979
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-05-03 06:40:58 +0000

    Made variable global.
    git-svn-id: https://svn.opendtect.org/od/trunk@24722 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiBase/uicolor.h
+++ include/uiBase/uicolor.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        A.H. Lammertink
  Date:          22/05/2000
- RCS:           $Id: uicolor.h,v 1.23 2011-04-08 12:36:46 cvsbert Exp $
+ RCS:           $Id: uicolor.h,v 1.24 2012-05-03 06:40:58 cvskris Exp $
 ________________________________________________________________________
 
 -*/
@@ -30,7 +30,7 @@ mGlobal bool  	selectColor(Color&,uiParent* parnt=0,const char* seltxt=0,
 
 // To be used by cmddriver to select a color while closing the QColorDialog
 mGlobal void		setExternalColor( const Color& );
-static Color*	externalcolor = 0;
+mGlobal Color*		externalcolor = 0;
 
 
 /*! \brief small element for color selection. Has no text label.
