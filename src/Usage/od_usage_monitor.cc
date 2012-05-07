44c5782cba479ab3ae8bec7ae171258c11905e07
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-05-07 11:57:18 +0000

    Added call to ensureLoaded().
    git-svn-id: https://svn.opendtect.org/od/trunk@24771 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Usage/od_usage_monitor.cc
+++ src/Usage/od_usage_monitor.cc
@@ -4,15 +4,17 @@
  * DATE     : Mar 2009
 -*/
 
-static const char* rcsID mUnusedVar = "$Id: od_usage_monitor.cc,v 1.5 2012-05-02 15:11:52 cvskris Exp $";
+static const char* rcsID mUnusedVar = "$Id: od_usage_monitor.cc,v 1.6 2012-05-07 11:57:18 cvskris Exp $";
 
 #include "batchprog.h"
 #include "odusgserver.h"
 #include "iopar.h"
-
+#include "moddepmgr.h"
 
 bool BatchProgram::go( std::ostream& strm )
 {
+    OD::ModDeps().ensureLoaded( "Usage" );
+    
     IOPar* usgpars = Usage::Server::getPars();
     if ( !usgpars || usgpars->isEmpty() ) return true;
 
