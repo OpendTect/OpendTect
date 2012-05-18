a6c8d0c2958e37b11b7b55a629c5677de6f6e11f
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-05-18 10:14:02 +0000

    Renamed odver.h.in to odversion.h.in
    git-svn-id: https://svn.opendtect.org/od/trunk@24859 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODInternal.cmake
+++ CMakeModules/ODInternal.cmake
@@ -2,12 +2,12 @@
 #
 #	CopyRight:	dGB Beheer B.V.
 # 	Jan 2012	K. Tingdahl
-#	RCS :		$Id: ODInternal.cmake,v 1.4 2012-05-18 09:06:36 cvskris Exp $
+#	RCS :		$Id: ODInternal.cmake,v 1.5 2012-05-18 10:14:02 cvskris Exp $
 #_______________________________________________________________________________
 
 
-#Configure odver.h
-configure_file ( ${OpendTect_DIR}/include/Basic/odver.h.in ${OpendTect_DIR}/include/Basic/odver.h )
+#Configure odversion.h
+configure_file ( ${OpendTect_DIR}/include/Basic/odversion.h.in ${OpendTect_DIR}/include/Basic/odversion.h )
 
 #Install cmake things.
 install ( DIRECTORY CMakeModules DESTINATION .
