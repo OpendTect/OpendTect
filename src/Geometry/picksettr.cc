d8d2aa276d239011c312ad05237a766967a5f3dc
Raman Singh <raman.singh@dgbes.com>
2012-05-29 03:41:56 +0000

    removed vintage 3.2 stuff
    git-svn-id: https://svn.opendtect.org/od/trunk@24949 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Geometry/picksettr.cc
+++ src/Geometry/picksettr.cc
@@ -7,7 +7,7 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID mUnusedVar = "$Id: picksettr.cc,v 1.28 2012-05-29 03:32:43 cvsraman Exp $";
+static const char* rcsID mUnusedVar = "$Id: picksettr.cc,v 1.29 2012-05-29 03:41:56 cvsraman Exp $";
 
 #include "picksetfact.h"
 #include "pickset.h"
@@ -91,53 +91,16 @@ const char* dgbPickSetTranslator::read( Pick::Set& ps, Conn& conn,
     ps.setName( conn.ioobj ? (const char*)conn.ioobj->name() : "" );
 
     Pick::Location loc;
+    IOPar iopar; iopar.getFrom( astrm );
+    ps.usePar( iopar );
 
-    if ( astrm.hasKeyword("Ref") ) // TODO: Remove in v3.4
+    astrm.next();
+    while ( !atEndOfSection(astrm) )
     {
-	// In old format we can find mulitple pick sets. Just gather them all
-	// in the pick set
-	for ( int ips=0; !atEndOfSection(astrm); ips++ )
-	{
-	    astrm.next();
-	    if ( astrm.hasKeyword(sKey::Color()) )
-	    {
-		ps.disp_.color_.use( astrm.value() );
-		astrm.next();
-	    }
-	    if ( astrm.hasKeyword(sKey::Size()) )
-	    {
-		ps.disp_.pixsize_ = astrm.getIValue();
-		astrm.next();
-	    }
-	    if ( astrm.hasKeyword(Pick::Set::sKeyMarkerType()) )
-	    {
-		ps.disp_.markertype_ = astrm.getIValue();
-		astrm.next();
-	    }
-	    while ( !atEndOfSection(astrm) )
-	    {
-		if ( !loc.fromString( astrm.keyWord() ) )
-		    break;
-		ps += loc;
-		astrm.next();
-	    }
-	    while ( !atEndOfSection(astrm) ) astrm.next();
-	    astrm.next();
-	}
-    }
-    else // New format
-    {
-	IOPar iopar; iopar.getFrom( astrm );
-	ps.usePar( iopar );
+	if ( loc.fromString( astrm.keyWord(), true, checkdir ) )
+	    ps += loc;
 
 	astrm.next();
-	while ( !atEndOfSection(astrm) )
-	{
-	    if ( loc.fromString( astrm.keyWord(), true, checkdir ) )
-		ps += loc;
-
-	    astrm.next();
-	}
     }
 
     return ps.size() ? 0 : "No valid picks found";
