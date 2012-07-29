03947274b37f782a44e905c14925410e985bce48
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-07-29 21:43:31 +0000

    Changed to own intranet-server.
    git-svn-id: https://svn.opendtect.org/od/trunk@25609 a985855d-d384-4b45-ae33-31a410998c3c

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,15 +1,12 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-##
-## # The following are required to submit to the CDash dashboard:
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
+## # The following are required to uses Dart and the Cdash dashboard
 
 set(CTEST_PROJECT_NAME "OpendTect")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=OpendTect")
+set(CTEST_DROP_SITE "intranet")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=OpendTect")
 set(CTEST_DROP_SITE_CDASH TRUE)
