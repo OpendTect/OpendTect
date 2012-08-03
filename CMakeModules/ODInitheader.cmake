a8e54e771ddb13afc16083912d0adf5a70ddf0dd
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-08-03 11:31:30 +0000

    Made it work with spec-folders.
    git-svn-id: https://svn.opendtect.org/od/trunk@25703 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODInitheader.cmake
+++ CMakeModules/ODInitheader.cmake
@@ -2,7 +2,7 @@
 #
 #	CopyRight:	dGB Beheer B.V.
 # 	Jan 2012	K. Tingdahl
-#	RCS :		$Id: ODInitheader.cmake,v 1.3 2012-02-27 07:35:30 cvskris Exp $
+#	RCS :		$Id: ODInitheader.cmake,v 1.4 2012-08-03 11:31:30 cvskris Exp $
 #_______________________________________________________________________________
 
 # OD_CREATE_INIT_HEADER
@@ -18,11 +18,17 @@ MACRO( OD_CREATE_INIT_HEADER )
     IF ( OD_IS_PLUGIN )
 	SET( INCLUDEDIR ${CMAKE_SOURCE_DIR}/plugins/${OD_MODULE_NAME} )
     ELSE()
-	SET( INCLUDEDIR ${CMAKE_SOURCE_DIR}/include/${OD_MODULE_NAME} )
+	if ( EXISTS ${CMAKE_SOURCE_DIR}/include/${OD_MODULE_NAME} )
+	    SET( INCLUDEDIR ${CMAKE_SOURCE_DIR}/include/${OD_MODULE_NAME} )
+	else()
+	    if ( EXISTS ${CMAKE_SOURCE_DIR}/spec/${OD_MODULE_NAME} )
+		SET( INCLUDEDIR ${CMAKE_SOURCE_DIR}/spec/${OD_MODULE_NAME} )
+	    endif()
+	endif()
     ENDIF()
 
     IF ( EXISTS ${INCLUDEDIR} )
-	SET( INITHEADER ${INCLUDEDIR}/init${OD_MODULE_NAME_LOWER}.h )
+	SET( INITHEADER ${INCLUDEDIR}/${OD_MODULE_NAME_LOWER}mod.h )
 
 	CONFIGURE_FILE( ${OpendTect_DIR}/CMakeModules/templates/initheader.h.in 
 			${INITHEADER} )
