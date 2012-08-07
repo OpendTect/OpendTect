a367ee609a35c78ed2666cf7a00506d4f449a4bc
Mahant Mothey <mahant.mothey@dgbes.com>
2012-08-07 04:27:46 +0000

    ...
    git-svn-id: https://svn.opendtect.org/od/trunk@25737 a985855d-d384-4b45-ae33-31a410998c3c

--- plugins/Hello/hellopi.cc
+++ plugins/Hello/hellopi.cc
@@ -4,13 +4,13 @@
  * DATE     : Sep 2003 / Apr 2011
 -*/
 
-static const char* rcsID mUnusedVar = "$Id: hellopi.cc,v 1.12 2012-08-07 04:23:04 cvsmahant Exp $";
+static const char* rcsID mUnusedVar = "$Id: hellopi.cc,v 1.13 2012-08-07 04:27:46 cvsmahant Exp $";
 
 #include "odplugin.h"
 #include <iostream>
 
 mDefODInitPlugin(Hello)
 {
-    std::cout << "Hello world! Speed 1 THz" << std::endl;
+    std::cout << "Hello world" << std::endl;
     return 0; // All OK - no error messages
 }
