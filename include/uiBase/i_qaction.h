18bec7d37fd48cd2a15bd8ba458d6f071281d266
Nageswara Rao <nageswara.rao@dgbes.com>
2012-08-23 12:12:34 +0000

    Qt class is replaced with macro.
    git-svn-id: https://svn.opendtect.org/od/trunk@25839 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiBase/i_qaction.h
+++ include/uiBase/i_qaction.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        Nanne Hemstra
  Date:          May 2007
- RCS:           $Id: i_qaction.h,v 1.5 2011-04-21 13:09:13 cvsbert Exp $
+ RCS:           $Id: i_qaction.h,v 1.6 2012-08-23 12:12:34 cvsnageswara Exp $
 ________________________________________________________________________
 
 -*/
@@ -22,13 +22,13 @@ ________________________________________________________________________
 /*!
     Internal object, to hide Qt's signal/slot mechanism.
 */
-class i_ActionMessenger : public QObject 
+class i_ActionMessenger : public QObject
 {
     Q_OBJECT
     friend class	uiAction;
 
 protected:
-i_ActionMessenger( QAction* sndr, uiAction* receiver )
+i_ActionMessenger( mQtclass(QAction*) sndr, uiAction* receiver )
     : sender_( sndr )
     , receiver_( receiver )
 { 
@@ -42,7 +42,7 @@ virtual	~i_ActionMessenger() {}
 private:
 
     uiAction*		receiver_;
-    QAction*		sender_;
+    mQtclass(QAction*)	sender_;
 
 private slots:
 
