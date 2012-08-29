bf44b7ea8e787d8ecb3f04deaacd531357ae998a
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-08-29 08:18:42 +0000

    Made plugin macro declare exports.
    git-svn-id: https://svn.opendtect.org/od/trunk@25928 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/odplugin.h
+++ include/Basic/odplugin.h
@@ -6,13 +6,14 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Bert
  Date:		Apr 2011
- RCS:		$Id: odplugin.h,v 1.3 2012-08-03 13:00:13 cvskris Exp $
+ RCS:		$Id: odplugin.h,v 1.4 2012-08-29 08:18:42 cvskris Exp $
 ________________________________________________________________________
 
 -*/
 
-extern "C" {
 #include "basicmod.h"
+
+extern "C" {
 #include "pluginbase.h"
 }
 #include "gendefs.h"
@@ -20,20 +21,20 @@ extern "C" {
 
 /* The following function MUST be defined: */
 #define mDefODInitPlugin(pinm) \
-    mExternC(Basic) const char* Init##pinm##Plugin(int,char**); \
-    mExternC(Basic) const char* Init##pinm##Plugin( int argc, char** argv )
+    mExternC(pinm) const char* Init##pinm##Plugin(int,char**); \
+    mExternC(pinm) const char* Init##pinm##Plugin( int argc, char** argv )
 
 /* The following function SHOULD be defined: */
 #define mDefODPluginInfo(pinm) \
-    mExternC(Basic) PluginInfo* Get##pinm##PluginInfo(); \
-    mExternC(Basic) PluginInfo* Get##pinm##PluginInfo()
+    mExternC(pinm) PluginInfo* Get##pinm##PluginInfo(); \
+    mExternC(pinm) PluginInfo* Get##pinm##PluginInfo()
 
 /* Define ONLY if your plugin needs early loading (i.e. before any UI)
    This is common for 'Batch'-type plugins.
  */
 #define mDefODPluginEarlyLoad(pinm) \
-    mExternC(Basic) int Get##pinm##PluginType(); \
-    mExternC(Basic) int Get##pinm##PluginType() { return PI_AUTO_INIT_EARLY; }
+    mExternC(pinm) int Get##pinm##PluginType(); \
+    mExternC(pinm) int Get##pinm##PluginType() { return PI_AUTO_INIT_EARLY; }
 
 
 #endif
