0cca288e37206d237037a0da0ac64e1745199842
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-08-30 05:05:57 +0000

    Added better platform detection to macro.
    git-svn-id: https://svn.opendtect.org/od/trunk@25946 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Prog/batchmod.h
+++ include/Prog/batchmod.h
@@ -7,18 +7,20 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	A.H.Bril
  Date:		Mar 2006
- RCS:		$Id: batchmod.h,v 1.1 2012-08-03 13:02:51 cvskris Exp $
+ RCS:		$Id: batchmod.h,v 1.2 2012-08-30 05:05:57 cvskris Exp $
 ________________________________________________________________________
 
 
 -*/
 
-#ifdef __msvc__
-# define dll_export	__declspec( dllexport )
-# define dll_import	__declspec( dllimport )
-#else
-# define dll_export
-# define dll_import
+#ifndef dll_export
+# if defined ( __win64__ ) || defined ( __win32__ )
+#  define dll_export	__declspec( dllexport )
+#  define dll_import	__declspec( dllimport )
+# else
+#  define dll_export
+#  define dll_import
+# endif
 #endif
 
 #if defined(Batch_EXPORTS) || defined(BATCH_EXPORTS)
