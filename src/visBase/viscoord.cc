6f6e73df5865f4b2285e0fa392b3ffc0f3baf517
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-09-05 11:56:40 +0000

    More osg-stuff.
    git-svn-id: https://svn.opendtect.org/od/trunk@26058 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visBase/viscoord.cc
+++ src/visBase/viscoord.cc
@@ -7,16 +7,19 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID mUnusedVar = "$Id: viscoord.cc,v 1.47 2012-08-10 03:50:08 cvsaneesh Exp $";
+static const char* rcsID mUnusedVar = "$Id: viscoord.cc,v 1.48 2012-09-05 11:56:40 cvskris Exp $";
 
 #include "viscoord.h"
 
 #include "errh.h"
 #include "vistransform.h"
 #include "UTMPosition.h"
+#include "visnormals.h"
 
 #include <Inventor/nodes/SoCoordinate3.h>
 #include <Inventor/nodes/SoGroup.h>
+
+#include <osg/Array>
 
 mCreateFactoryEntry( visBase::Coordinates );
 
@@ -24,21 +27,31 @@ namespace visBase
 {
 
 Coordinates::Coordinates()
-    : coords_( new SoCoordinate3 )
+    : coords_( doOsg() ? 0 : new SoCoordinate3 )
     , transformation_( 0 )
     , utmposition_( 0 )
-    , root_( new SoGroup )
+    , root_( doOsg() ? 0 : new SoGroup )
+    , osgcoords_( doOsg() ? new osg::Vec3Array : 0 )
 {
-    root_->ref();
-    root_->addChild( coords_ );
-    unusedcoords_ += 0;
-    //!<To compensate for that the first coord is set by default by OI
+    if ( osgcoords_ )
+	mGetOsgVecArr(osgcoords_)->ref();
+    else
+    {
+	root_->ref();
+	root_->addChild( coords_ );
+	unusedcoords_ += 0;
+	//!<To compensate for that the first coord is set by default by OI
+
+    }
 }
 
 
 Coordinates::~Coordinates()
 {
-    root_->unref();
+    if ( osgcoords_ )
+	mGetOsgVecArr(osgcoords_)->unref();
+    
+    if ( root_ ) root_->unref();
     if ( transformation_ ) transformation_->unRef();
 }
 
@@ -48,16 +61,23 @@ void Coordinates::copyFrom( const Coordinates& nc )
     Threads::MutexLocker lock( mutex_ );
     Threads::MutexLocker nclock( nc.mutex_ );
 
-    coords_->point = nc.coords_->point;
-    if ( nc.utmposition_ )
+    if ( doOsg() )
     {
-	if ( !utmposition_ )
+	*mGetOsgVecArr(osgcoords_) = *mGetOsgVecArr(nc.osgcoords_);
+    }
+    else
+    {
+	coords_->point = nc.coords_->point;
+	if ( nc.utmposition_ )
 	{
-	    utmposition_ = new UTMPosition;
-	    root_->insertChild( utmposition_, 0 );
-	}
+	    if ( !utmposition_ )
+	    {
+		utmposition_ = new UTMPosition;
+		root_->insertChild( utmposition_, 0 );
+	    }
 
-	utmposition_->utmposition = nc.utmposition_->utmposition;
+	    utmposition_->utmposition = nc.utmposition_->utmposition;
+	}
     }
 
     unusedcoords_ = nc.unusedcoords_;
@@ -128,15 +148,19 @@ Coord Coordinates::getLocalTranslation() const
 }
 
 
+#define mArrSize \
+    (doOsg() ? mGetOsgVecArr(osgcoords_)->size() : coords_->point.getNum())
+
 int Coordinates::size(bool includedeleted) const
-{ return coords_->point.getNum()-(includedeleted ? 0 : unusedcoords_.size()); }
+{
+    return mArrSize -(includedeleted ? 0 : unusedcoords_.size()); }
 
 
 int Coordinates::nextID( int previd ) const
 {
     Threads::MutexLocker lock( mutex_ );
 
-    const int sz = coords_->point.getNum();
+    const int sz = mArrSize;
 
     int res = previd+1;
     while ( res<sz )
@@ -163,7 +187,7 @@ int Coordinates::addPos( const Coord3& pos )
     }
     else
     {
-	res = coords_->point.getNum();
+	res = mArrSize;
     }
 
     Coord3 postoset = pos;
@@ -179,8 +203,14 @@ int Coordinates::addPos( const Coord3& pos )
 	    postoset.y -= utmoffset[1];
 	}
     }
-    
-    coords_->point.set1Value( res, SbVec3f((float) postoset.x,
+
+    if ( doOsg() )
+	mGetOsgVecArr(osgcoords_)->push_back(
+		osg::Vec3f( (float) postoset.x,
+			    (float) postoset.y,
+			    (float) postoset.z));
+    else
+	coords_->point.set1Value( res, SbVec3f((float) postoset.x,
 				(float) postoset.y,(float) postoset.z) );
 
     return res;
@@ -189,7 +219,14 @@ int Coordinates::addPos( const Coord3& pos )
 
 void Coordinates::insertPos( int idx, const Coord3& pos )
 {
+    if ( doOsg() )
+    {
+	pErrMsg( "Not implemented" );
+	return;
+	
+    }
     Threads::MutexLocker lock( mutex_ );
+    
     coords_->point.insertSpace( idx, 1 );
     for ( int idy=unusedcoords_.size()-1; idy>=0; idy-- )
     {
@@ -203,7 +240,12 @@ void Coordinates::insertPos( int idx, const Coord3& pos )
 
 Coord3 Coordinates::getPos( int idx, bool scenespace ) const
 {
-    SbVec3f scenepos = coords_->point[idx];
+    const float* scenepos;
+    if ( doOsg() )
+        scenepos = (*mGetOsgVecArr(osgcoords_))[idx].ptr();
+    else
+	scenepos = coords_->point[idx].getValue();
+    
     Coord3 res( scenepos[0], scenepos[1], scenepos[2] );
     if ( res.isDefined() )
     {
@@ -225,11 +267,16 @@ Coord3 Coordinates::getPos( int idx, bool scenespace ) const
 bool Coordinates::isDefined( int idx ) const
 {
     Threads::MutexLocker lock( mutex_ );
-    if ( idx<0 || idx>=coords_->point.getNum() ||
+    if ( idx<0 || idx>=mArrSize ||
 	 unusedcoords_.indexOf( idx )!=-1 )
 	return false;
 
-    const SbVec3f coord = coords_->point[idx];
+    const float* coord;
+    if ( doOsg() )
+	coord = (*mGetOsgVecArr(osgcoords_))[idx].ptr();
+    else
+	coord = coords_->point[idx].getValue();
+    
     return !mIsUdf(coord[2]) && !mIsUdf(coord[1]) && !mIsUdf(coord[0]);
 }
 
@@ -243,7 +290,7 @@ void Coordinates::setPos( int idx, const Coord3& pos )
 
 void Coordinates::setPosWithoutLock( int idx, const Coord3& pos )
 {
-    for ( int idy=coords_->point.getNum(); idy<idx; idy++ )
+    for ( int idy=mArrSize; idy<idx; idy++ )
 	unusedcoords_ += idy;
 
     Coord3 postoset = pos;
@@ -274,7 +321,17 @@ void Coordinates::setPosWithoutLock( int idx, const Coord3& pos )
 	}
     }
     
-    coords_->point.set1Value( idx, SbVec3f((float) postoset.x,
+    if ( doOsg() )
+    {
+	if ( idx>=mGetOsgVecArr(osgcoords_)->size() )
+	    mGetOsgVecArr(osgcoords_)->resize( idx+1 );
+	
+	(*mGetOsgVecArr(osgcoords_))[idx] =
+	    osg::Vec3f((float) postoset.x,(float) postoset.y,(float)postoset.z);
+	    
+    }
+    else
+	coords_->point.set1Value( idx, SbVec3f((float) postoset.x,
 				(float) postoset.y,(float) postoset.z) );
 
     const int unusedidx = unusedcoords_.indexOf(idx);
@@ -286,7 +343,7 @@ void Coordinates::setPosWithoutLock( int idx, const Coord3& pos )
 void Coordinates::removePos( int idx, bool keepidxafter )
 {
     Threads::MutexLocker lock( mutex_ );
-    const int nrcoords = coords_->point.getNum();
+    const int nrcoords = mArrSize;
     if ( idx>=nrcoords || idx<0 )
     {
 	pErrMsg("Invalid index");
@@ -295,14 +352,24 @@ void Coordinates::removePos( int idx, bool keepidxafter )
 
     if ( idx==nrcoords-1 )
     {
-	coords_->point.deleteValues( idx );
+	if ( doOsg() )
+	    mGetOsgVecArr(osgcoords_)->resize( idx );
+	else
+	    coords_->point.deleteValues( idx );
 	unusedcoords_ -= idx;
     }
     else if ( keepidxafter )
 	unusedcoords_ += idx;
     else
     {
-	coords_->point.deleteValues( idx, 1 );
+	if ( doOsg() )
+	{
+	    mGetOsgVecArr(osgcoords_)->erase(
+				 mGetOsgVecArr(osgcoords_)->begin() + idx );
+	}
+	else
+	    coords_->point.deleteValues( idx, 1 );
+	
 	for ( int idy=unusedcoords_.size()-1; idy>=0; idy-- )
 	{
 	    if ( unusedcoords_[idy]>idx )
@@ -315,10 +382,13 @@ void Coordinates::removePos( int idx, bool keepidxafter )
 void Coordinates::removeAfter( int idx )
 {
     Threads::MutexLocker lock( mutex_ );
-    if ( idx<-1 || idx>=coords_->point.getNum()-1 )
+    if ( idx<-1 || idx>=mArrSize-1 )
 	return;
 
-    coords_->point.deleteValues( idx+1 );
+    if ( doOsg() )
+	mGetOsgVecArr(osgcoords_)->resize( idx+1 );
+    else
+	coords_->point.deleteValues( idx+1 );
     for ( int idy=0; idy<unusedcoords_.size(); idy++ )
     {
 	if ( unusedcoords_[idy]>idx )
@@ -351,7 +421,7 @@ SoNode* Coordinates::gtInvntrNode() { return root_; }
 
 void Coordinates::setAllZ( const float* vals, int sz, float zscale )
 {
-    if ( sz != coords_->point.getNum() )
+    if ( sz != mArrSize )
 	coords_->point.setNum( sz );
 
     float* zvals = ((float*) coords_->point.startEditing())+2;
@@ -382,7 +452,7 @@ void Coordinates::setAllZ( const float* vals, int sz, float zscale )
 
 void Coordinates::getPositions(TypeSet<Coord3>& res) const
 {
-    for ( int idx=0; idx<coords_->point.getNum(); idx++ )
+    for ( int idx=0; idx<mArrSize; idx++ )
 	res += getPos(idx);
 }
 
@@ -390,7 +460,7 @@ void Coordinates::getPositions(TypeSet<Coord3>& res) const
 void Coordinates::setPositions( const TypeSet<Coord3>& pos)
 {
     const bool oldstatus = coords_->point.enableNotify( false );
-    for ( int idx=0; idx<coords_->point.getNum(); idx++ )
+    for ( int idx=0; idx<mArrSize; idx++ )
     {
 	if ( unusedcoords_.indexOf(idx)!=-1 )
 	    continue;
@@ -415,7 +485,92 @@ void Coordinates::setPositions( const Coord3* pos, int sz, int start )
     coords_->point.touch();
 }
 
+    
+    
+CoinFloatVertexAttribList::CoinFloatVertexAttribList(Coordinates& c, Normals* n)
+    : coords_( c )
+    , normals_( n )
+{
+    coords_.ref();
+    if ( normals_ ) normals_->ref();
+}
 
+    
+CoinFloatVertexAttribList::~CoinFloatVertexAttribList()
+{
+    coords_.unRef();
+    if ( normals_ ) normals_->unRef();
+}
+    
+
+int CoinFloatVertexAttribList::size() const
+{
+    return coords_.size();
+}
+    
+    
+bool CoinFloatVertexAttribList::setSize(int sz,bool cpdata)
+{
+    if ( sz>size() )
+    {
+	coords_.setPos( sz-1, Coord3::udf() );
+	if ( normals_ ) normals_->setNormal(sz-1, Coord3::udf() );
+    }
+    else if ( sz>size() )
+    {
+	coords_.removeAfter( sz-1 );
+    }
+    
+    return true;
+}
+
+    
+ void	CoinFloatVertexAttribList::setCoord(int idx,const float* pos)
+{
+    const Coord3 coord( pos[0], pos[1], pos[2] );
+    coords_.setPos( idx, coord );
+}
+    
+    
+void	CoinFloatVertexAttribList::getCoord( int idx, float* res ) const
+{
+    const Coord3 coord = coords_.getPos( idx );
+    res[0] = (float) coord.x;
+    res[1] = (float) coord.y;
+    res[2] = (float) coord.z;
+}
+
+    
+void CoinFloatVertexAttribList::setNormal( int idx, const float* pos )
+{
+    if ( !normals_ )
+	return;
+    
+    const Coord3 coord( pos[0], pos[1], pos[2] );
+    normals_->setNormal( idx, coord );
+}
+    
+    
+void	CoinFloatVertexAttribList::getNormal( int idx, float* res ) const
+{
+    if ( !normals_ )
+	return;
+    
+    const Coord3 coord = normals_->getNormal( idx );
+    res[0] = (float) coord.x;
+    res[1] = (float) coord.y;
+    res[2] = (float) coord.z;
+}
+
+    
+void CoinFloatVertexAttribList::setTCoord(int,const float*)
+{}
+
+
+void CoinFloatVertexAttribList::getTCoord(int,float*) const
+{}
+
+	
 
 CoordListAdapter::CoordListAdapter( Coordinates& c )
     : coords_( c )
@@ -428,6 +583,7 @@ CoordListAdapter::~CoordListAdapter()
 {
     coords_.unRef();
 }
+    
 
 int CoordListAdapter::nextID( int previd ) const
 { return coords_.nextID( previd ); }
