5e47ddc449cfa3eaf8f9b84408171fa5d8b53862
Mahant Mothey <mahant.mothey@dgbes.com>
2012-09-06 10:31:38 +0000

    ...........
    git-svn-id: https://svn.opendtect.org/od/trunk@26077 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiIo/uidatapointsetcrossplot.h
+++ include/uiIo/uidatapointsetcrossplot.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        Bert
  Date:          Mar 2008
- RCS:           $Id: uidatapointsetcrossplot.h,v 1.45 2012-09-06 10:29:08 cvsmahant Exp $
+ RCS:           $Id: uidatapointsetcrossplot.h,v 1.46 2012-09-06 10:31:38 cvsmahant Exp $
 ________________________________________________________________________
 
 -*/
@@ -251,7 +251,7 @@ public:
 
     void			setUserDefDrawType(bool dodrw,bool isy2,
 	    						bool drwln = false);
-    void                        setUserDefPolyLine(TypeSet<uiWorldPoint>&,bool);
+    void			setUserDefPolyLine(TypeSet<uiWorldPoint>&,bool);
     void			drawUserDefPolyLine(bool);
     void 			sortPts(TypeSet<uiWorldPoint>&);
 
