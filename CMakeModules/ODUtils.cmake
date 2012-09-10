da9f2facd18d77eae6a546829cf9ac7345053e6e
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-09-10 10:20:22 +0000

    Prepared to switch to bin/$plf/Debug outputs with Unix makefiles.
    git-svn-id: https://svn.opendtect.org/od/trunk@26110 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODUtils.cmake
+++ CMakeModules/ODUtils.cmake
@@ -2,55 +2,60 @@
 #
 #	CopyRight:	dGB Beheer B.V.
 # 	Jan 2012	K. Tingdahl
-#	RCS :		$Id: ODUtils.cmake,v 1.21 2012-06-08 11:36:04 cvsraman Exp $
+#	RCS :		$Id: ODUtils.cmake,v 1.22 2012-09-10 10:20:22 cvskris Exp $
 #_______________________________________________________________________________
 
-IF ( CMAKE_BUILD_TYPE STREQUAL "" )
-    SET ( DEBUGENV $ENV{DEBUG} )
-    IF ( DEBUGENV AND
-	( (${DEBUGENV} MATCHES "yes" ) OR
-	  (${DEBUGENV} MATCHES "Yes" ) OR
-	  (${DEBUGENV} MATCHES "YES" ) ) )
-	SET ( CMAKE_BUILD_TYPE "Debug" CACHE STRING "Debug or Release" FORCE )
-    ELSE()
-	SET ( CMAKE_BUILD_TYPE "Release" CACHE STRING "Debug or Release" FORCE)
-    ENDIF()
+if ( CMAKE_GENERATOR STREQUAL "Unix Makefiles" )
+    if ( CMAKE_BUILD_TYPE STREQUAL "" )
+	set ( DEBUGENV $ENV{DEBUG} )
+	if ( DEBUGENV AND
+	    ( (${DEBUGENV} MATCHES "yes" ) OR
+	      (${DEBUGENV} MATCHES "Yes" ) OR
+	      (${DEBUGENV} MATCHES "YES" ) ) )
+	    set ( CMAKE_BUILD_TYPE "Debug" CACHE STRING "Debug or Release" FORCE )
+	else()
+	    set ( CMAKE_BUILD_TYPE "Release" CACHE STRING "Debug or Release" FORCE)
+	endif()
 
-    MESSAGE( STATUS "Setting CMAKE_BUILD_TYPE to ${CMAKE_BUILD_TYPE}" )
-ENDIF()
+	MESSAGE( STATUS "Setting CMAKE_BUILD_TYPE to ${CMAKE_BUILD_TYPE}" )
+    endif()
+
+#    set( OD_BUILDSUBDIR "/${CMAKE_BUILD_TYPE}" )
+#    To be enabled 
+endif()
 
 ADD_DEFINITIONS("-D__cmake__")
 
-SET ( OD_EXEC_OUTPUT_RELPATH bin/${OD_PLFSUBDIR} )
-SET ( OD_EXEC_OUTPUT_PATH ${OD_BINARY_BASEDIR}/${OD_EXEC_OUTPUT_RELPATH} )
-SET ( OD_EXEC_INSTALL_PATH ${OD_EXEC_OUTPUT_RELPATH} )
+set ( OD_EXEC_OUTPUT_RELPATH "bin/${OD_PLFSUBDIR}${OD_BUILDSUBDIR}" )
+set ( OD_EXEC_OUTPUT_PATH "${OD_BINARY_BASEDIR}/${OD_EXEC_OUTPUT_RELPATH}" )
+set ( OD_EXEC_INSTALL_PATH "${OD_EXEC_OUTPUT_RELPATH}" )
 
-SET ( OD_MAIN_EXEC od_main )
-SET ( OD_ATTRIB_EXECS od_process_attrib od_process_attrib_em )
-SET ( OD_VOLUME_EXECS od_process_volume )
-SET ( OD_PRESTACK_EXECS od_process_prestack )
-SET ( OD_ZAXISTRANSFORM_EXECS od_process_time2depth )
+set ( OD_MAIN_EXEC od_main )
+set ( OD_ATTRIB_EXECS od_process_attrib od_process_attrib_em )
+set ( OD_VOLUME_EXECS od_process_volume )
+set ( OD_PRESTACK_EXECS od_process_prestack )
+set ( OD_ZAXISTRANSFORM_EXECS od_process_time2depth )
 
 #Macro for going through a list of modules and adding them
-MACRO ( OD_ADD_MODULES )
-    SET( DIR ${ARGV0} )
+macro ( OD_ADD_MODULES )
+    set( DIR ${ARGV0} )
 
-    FOREACH( OD_MODULE_NAME ${ARGV} )
-	IF ( NOT ${OD_MODULE_NAME} STREQUAL ${DIR} )
+    foreach( OD_MODULE_NAME ${ARGV} )
+	if ( NOT ${OD_MODULE_NAME} STREQUAL ${DIR} )
 	    add_subdirectory( ${DIR}/${OD_MODULE_NAME} )
-	ENDIF()
-    ENDFOREACH()
-ENDMACRO()
+	endif()
+    endforeach()
+endmacro()
 
 
 # Macro for going through a list of modules and adding them
 # as optional targets
-MACRO ( OD_ADD_OPTIONAL_MODULES )
-    SET( DIR ${ARGV0} )
+macro ( OD_ADD_OPTIONAL_MODULES )
+    set( DIR ${ARGV0} )
 
-    FOREACH( OD_MODULE_NAME ${ARGV} )
-	IF ( NOT ${OD_MODULE_NAME} MATCHES ${DIR} )
+    foreach( OD_MODULE_NAME ${ARGV} )
+	if ( NOT ${OD_MODULE_NAME} MATCHES ${DIR} )
 	    add_subdirectory( ${DIR}/${OD_MODULE_NAME} EXCLUDE_FROM_ALL )
-	ENDIF()
-    ENDFOREACH()
-ENDMACRO()
+	endif()
+    endforeach()
+endmacro()
