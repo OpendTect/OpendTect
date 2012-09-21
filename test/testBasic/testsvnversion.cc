6b5cdf57bc3c4b1bebbd7b2f70b5d3c9c9e58224
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-09-21 19:08:55 +0000

    Added testsvnversion.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@26335 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ test/testBasic/testsvnversion.cc
@@ -0,0 +1,29 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : K. Tingdahl
+ * DATE     : July 2012
+ * FUNCTION : 
+-*/
+
+static const char* rcsID mUnusedVar = "$Id$";
+
+#include "genc.h"
+
+#include <iostream>
+
+
+int main( int narg, char** argv )
+{
+    const int svnversion = GetSubversionRevision();
+    const char* svnurl = GetSubversionUrl();
+
+    if ( svnversion<1 || !svnurl || !*svnurl )
+    {
+	std::cerr << "Invalid svn revision. "
+		"Cmake could probably not find svn command-client. "
+		"Take a look in CMakeModules/ODSubversion.cmake.\n";
+	return 1;
+    }
+
+    return 0;
+}
