27afdfc6b23abd7e415db8d032454488cc83260f
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-09-21 18:12:23 +0000

    Added defaults to svn-information.
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@26332 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODSubversion.cmake
+++ CMakeModules/ODSubversion.cmake
@@ -10,7 +10,12 @@
 include(FindSubversion)
 
 # extract working copy information for SOURCE_DIR into MY_XXX variables
-Subversion_WC_INFO( ${CMAKE_SOURCE_DIR} MY)
+if ( Subversion_FOUND )
+    Subversion_WC_INFO( ${CMAKE_SOURCE_DIR} MY)
+else()
+    set ( MY_WC_REVISION 0 )
+    set ( MY_WC_URL "" )
+endif()
 
 set ( INC_DIR ${CMAKE_SOURCE_DIR}/include/Basic )
 set ( TMPFILE ${INC_DIR}/svnversion.h.tmp )
