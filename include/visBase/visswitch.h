3315ff1705942e5481ad25c91215abdb9d905108
Yuancheng Liu <yuancheng.liu@dgbes.com>
2012-09-25 21:49:45 +0000

    add SoSwith and SoSeparator
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@26417 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/visBase/visswitch.h
@@ -0,0 +1,62 @@
+#ifndef visswitch_h
+#define visswitch_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Yuancheng Liu
+ Date:		Sep 2012
+ RCS:		$Id$
+________________________________________________________________________
+
+
+-*/
+
+#include "visdata.h"
+
+class SoSeparator;
+class SoSwitch;
+
+namespace visBase
+{
+
+/*!SoSeparator and SoSwitch from Coin. */
+
+mClass Switch : public DataObject
+{
+public:
+
+    static Switch*		create() mCreateDataObj(Switch);
+
+    void			turnOn(bool yn);
+    void			addChild(SoNode*);
+    void			removeChild(SoNode*);
+
+protected:
+    				~Switch();
+    SoSwitch*			switch_;
+    virtual SoNode*		gtInvntrNode();
+
+};
+
+mClass Separator : public DataObject
+{
+public:
+
+    static Separator*		create() mCreateDataObj(Separator);
+
+    void			addChild(SoNode*);
+    void			removeChild(SoNode*);
+
+protected:
+    				~Separator();
+    SoSeparator*		separator_;
+    virtual SoNode*		gtInvntrNode();
+};
+
+
+}
+
+#endif
+
