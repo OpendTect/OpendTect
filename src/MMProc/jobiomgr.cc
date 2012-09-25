a3e5ac3d018337e5df9fe41e283f59e55d330613
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-09-25 11:53:20 +0000

    Removed function declaration in cc-file (it is in a header-file now).
    git-svn-id: https://svn.opendtect.org/od/trunk@26392 a985855d-d384-4b45-ae33-31a410998c3c

--- src/MMProc/jobiomgr.cc
+++ src/MMProc/jobiomgr.cc
@@ -389,7 +389,7 @@ bool JobIOMgr::startProg( const char* progname,
 #ifdef __win__
 
 extern const BufferString& getTempBaseNm();
-extern "C" const char* GetSurveyName();
+
 extern int MMJob_getTempFileNr();
 
 FilePath getConvFilePath( const HostData& hd, const FilePath& fp )
