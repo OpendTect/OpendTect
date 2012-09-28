05c48f150549ddfedf57cb6e1681005d46915a40
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-09-28 11:08:38 +0000

    Changed project name.
    git-svn-id: https://svn.opendtect.org/od/trunk@26499 a985855d-d384-4b45-ae33-31a410998c3c

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,5 +8,5 @@ set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "intranet")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=OpendTect")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=${CTEST_PROJECT_NAME}")
 set(CTEST_DROP_SITE_CDASH TRUE)
