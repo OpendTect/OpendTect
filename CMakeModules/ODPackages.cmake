4ae571440ef8a0bab9c3a9178403fd4101cd9c86
dgb@dgbes.com <dgb@dgbes.com@a985855d-d384-4b45-ae33-31a410998c3c>
2012-10-03 13:11:22 +0000

    Added do_install target and made packages dependent on it.
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@26591 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODPackages.cmake
+++ CMakeModules/ODPackages.cmake
@@ -4,6 +4,10 @@
 # Date:		August 2012		
 #RCS:           $Id: ODPackages.cmake,v 1.6 2012/09/11 12:26:50 cvsnageswara Exp $
 
+add_custom_target( do_install ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target install
+        --config ${CMAKE_BUILD_TYPE} 
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR} )
+
 add_custom_target( packages  ${CMAKE_COMMAND} 
         -DOpendTect_VERSION_MAJOR=${OpendTect_VERSION_MAJOR} 
         -DOpendTect_VERSION_MINOR=${OpendTect_VERSION_MINOR} 
@@ -15,5 +19,6 @@ add_custom_target( packages  ${CMAKE_COMMAND}
 	-DQTDIR=${QTDIR}
 	-DCOINDIR=${COINDIR}
         -P ${PROJECT_SOURCE_DIR}/CMakeModules/packagescripts/ODMakePackages.cmake 
+	DEPENDS do_install
         COMMENT "Creating packages" ) 
 
