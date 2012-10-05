0647281e0534d2711a0baa7ffdc64cae1eafa42c
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-10-05 09:51:12 +0000

    Fixed compiler warning.
    git-svn-id: https://svn.opendtect.org/od/trunk@26615 a985855d-d384-4b45-ae33-31a410998c3c

--- src/SoOD/SoColTabMultiTexture2.cc
+++ src/SoOD/SoColTabMultiTexture2.cc
@@ -695,7 +695,7 @@ const unsigned char* SoColTabMultiTexture2::createImage( SbVec2s& size, int& nc 
 
     bool hastransperancy = false;
     int coltabstart = 0;
-    bool nrimagesused = 0;
+    int nrimagesused = 0;
     for ( int idx=0; idx<numimages; idx++ )
     {
 	SbVec2i32 lsz;
