4ed478cda6c679ff73de9d48ac89248fe051c1b8
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-10-05 09:17:43 +0000

    Changed refcount implementation macro to give more flexibility in usage.
    git-svn-id: https://svn.opendtect.org/od/trunk@26612 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/refcount.h
+++ include/Basic/refcount.h
@@ -124,14 +124,14 @@ mObjectSetApplyToAllFunc( deepUnRefNoDelete,
 
 //Macro to implement a refman class
 #define mDefRefMan( clss, reffunc, unreffunc ) \
-mDefPtrMan1(clss, if ( ptr_ ) ptr_->reffunc(), if ( ptr_ ) ptr_->unreffunc() ) \
+mDefPtrMan1(clss, if ( ptr_ ) reffunc, if ( ptr_ ) unreffunc ) \
 inline clss(const clss<T>& p) : ptr_( 0 ) {  set(p.ptr_); } \
 inline clss<T>& operator=(const clss<T>& p) { set(p.ptr_); return *this; }\
-mDefPtrMan2(clss, if (ptr_) ptr_->reffunc(), if ( ptr_ ) ptr_->unreffunc() )\
-mDefPtrMan3(clss, if (ptr_) ptr_->reffunc(), if ( ptr_ ) ptr_->unreffunc() )
+mDefPtrMan2(clss, if (ptr_) reffunc, if ( ptr_ ) unreffunc )\
+mDefPtrMan3(clss, if (ptr_) reffunc, if ( ptr_ ) unreffunc )
 
 //Implement RefMan
-mDefRefMan( RefMan, ref, unRef )
+mDefRefMan( RefMan, ptr_->ref(), ptr_->unRef() )
 
 
 
