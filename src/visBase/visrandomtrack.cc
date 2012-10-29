0cebcf1e3291028e5f2fdff6f2cc23fa7f4c5978
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-10-29 11:00:34 +0000

    Renamed TypeSet::remove(int) to TypeSet::removeSingle and TypeSet::remove(int,int) to TypeSet::removeRange(int,int).
    git-svn-id: https://svn.opendtect.org/od/trunk@27125 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visBase/visrandomtrack.cc
+++ src/visBase/visrandomtrack.cc
@@ -225,7 +225,7 @@ void RandomTrack::removeKnot( int idx )
 	return;
     }
 
-    knots.remove( idx );
+    knots.removeSingle( idx );
     rebuild();
 
     if ( dragger )
@@ -455,7 +455,7 @@ void RandomTrack::rebuild()
 	TriangleStripSet* strip = sections[idx];
 	removeChild( strip->getInventorNode() );
 	strip->unRef();
-	sections.remove( idx );
+	sections.removeSingle( idx );
     }
 
     if ( sections.isEmpty() ) return;
