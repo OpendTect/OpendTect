8340442483e2829cbdac365a62ce661d5db825a4
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-10-29 11:12:36 +0000

    Renamed TypeSet::remove(int) to TypeSet::removeSingle and TypeSet::remove(int,int) to TypeSet::removeRange(int,int).
    git-svn-id: https://svn.opendtect.org/od/trunk@27134 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Basic/posinfo2d.cc
+++ src/Basic/posinfo2d.cc
@@ -95,7 +95,7 @@ void PosInfo::Line2DData::remove( int nr )
 {
     bool found; int idx = gtIndex( nr, found );
     if ( !found ) return;
-    posns_.remove( idx );
+    posns_.removeSingle( idx );
 }
 
 
@@ -104,7 +104,7 @@ void PosInfo::Line2DData::limitTo( Interval<int> trcrg )
     trcrg.sort();
     for ( int idx=0; idx<posns_.size(); idx++ )
 	if ( posns_[idx].nr_ < trcrg.start || posns_[idx].nr_ > trcrg.stop )
-	    posns_.remove( idx-- );
+	    posns_.removeSingle( idx-- );
 }
 
 
