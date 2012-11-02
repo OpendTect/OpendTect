fc64fe9b2c1add468f18f3e0d09e500a160c7ed0
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-11-02 14:26:23 +0000

    Initial add.
    git-svn-id: https://svn.opendtect.org/od/trunk@27300 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/generalexport.h
@@ -0,0 +1,21 @@
+#ifndef seisexport_h
+#define seisexport_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	A.H. Bril
+ Date:		27-1-98
+ RCS:		$Id: seiswrite.h 26297 2012-09-21 00:16:36Z nanne.hemstra@dgbes.com $
+________________________________________________________________________
+
+-*/
+
+
+#include <arraynd.h>
+
+mExportTemplClassInst( General, Array2D<float> );
+
+#endif
+
