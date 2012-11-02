38ab0f464ca3cc3b9289d572f9734e987d1e0fae
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-11-02 09:51:08 +0000

    Updated to latest mod-include-template.
    git-svn-id: https://svn.opendtect.org/od/trunk@27281 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Prog/batchmod.h
+++ include/Prog/batchmod.h
@@ -13,10 +13,19 @@ ________________________________________________________________________
 
 -*/
 
+#if defined( __win64__ ) || defined ( __win32__ )
+# define do_import_export
+#else
+# ifdef do_import_export
+#  undef do_import_export
+# endif
+#endif
+
 #ifndef dll_export
-# if defined ( __win64__ ) || defined ( __win32__ )
+# if defined( do_import_export )
 #  define dll_export	__declspec( dllexport )
 #  define dll_import	__declspec( dllimport )
+#  define dll_extern	extern
 # else
 #  define dll_export
 #  define dll_import
@@ -24,10 +33,27 @@ ________________________________________________________________________
 #endif
 
 #if defined(Batch_EXPORTS) || defined(BATCH_EXPORTS)
+# define do_export_Batch
+#else
+# if defined ( do_export_Batch )
+#  undef do_export_Batch
+# endif
+#endif
+
+
+#if defined( do_export_Batch )
 # define Export_Batch	dll_export
+# define Extern_Batch
 #else
 # define Export_Batch	dll_import
+# define Extern_Batch	dll_extern
 #endif
 
+#if defined ( do_import_export )
+
+#include "networkmod.h"
 
 #endif
+
+#endif
+
