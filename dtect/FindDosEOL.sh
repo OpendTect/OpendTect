81bc7e02cdd55f313f3162cbcc104105a5a6bb08
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-11-13 12:03:44 +0000

    Made script somewhat nicer.
    git-svn-id: https://svn.opendtect.org/od/trunk@27401 a985855d-d384-4b45-ae33-31a410998c3c

--- dtect/FindDosEOL.sh
+++ dtect/FindDosEOL.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
+#
+# Copyright (C): dGB Beheer B. V.
+#
+# FindDosEOL.sh - finds files with DOS end-of-lines.
+#
 
-files=`cat $1 | xargs grep -l $'\r'`
+if [ $# -ne 1 ]
+then
+  echo "Usage: `basename $0` <listfile>"
+  exit 1
+fi
+
+listfile=$1
+
+if [ ! -f $listfile ];
+then
+   echo "File $listfile does not exists."
+   exit 1
+fi
+
+files=`cat $listfile | xargs grep -l $'\r'`
 if [ -z "$files" ];then
    echo "No DOS EOL found!"
 else
