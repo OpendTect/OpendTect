dfa6d626c5a70fc55be37020caf189b165ae02d2
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-11-13 09:47:24 +0000

    New attempt with DOS EOL test.
    git-svn-id: https://svn.opendtect.org/od/trunk@27395 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ dtect/FindDosEOL.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+files=`cat $1 | xargs grep -l $'\r\n'`
+OUT=$?
+if [ $OUT -eq 0 ];then
+   echo "No DOS EOL found!"
+else
+   echo "DOS EOL found in: "
+   echo $files
+   exit 1
+fi
+
