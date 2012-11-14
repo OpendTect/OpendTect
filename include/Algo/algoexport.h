4ca7c6e16087bbc5edb4d6ea7d157659c703e03e
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-11-14 14:40:59 +0000

    Initial add.
    git-svn-id: https://svn.opendtect.org/od/trunk@27440 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Algo/algoexport.h
@@ -0,0 +1,24 @@
+#ifndef algoexport_h
+#define algoexport_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	A.H. Bril
+ Date:		27-1-98
+ RCS:		$Id: seiswrite.h 26297 2012-09-21 00:16:36Z nanne.hemstra@dgbes.com $
+________________________________________________________________________
+
+-*/
+
+
+
+#ifdef __cpp__
+
+#include "mathfunc.h"
+Extern_Algo template class mExp(Algo) MathFunction<float,float>;
+#endif
+
+#endif
+
