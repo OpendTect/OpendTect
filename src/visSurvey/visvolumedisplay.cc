b98d189961bfa1b9669a3cd98663f70c6f450097
Nanne Hemstra <nanne.hemstra@dgbes.com>
2012-11-15 15:15:57 +0000

    When duplicating, also duplicate coltab properties #0001649
    git-svn-id: https://svn.opendtect.org/od/trunk@27466 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visSurvey/visvolumedisplay.cc
+++ src/visSurvey/visvolumedisplay.cc
@@ -1046,7 +1046,7 @@ visSurvey::SurveyObject* VolumeDisplay::duplicate( TaskRunner* tr ) const
 	vd->addIsoSurface();
 	vd->isosurfsettings_[idx] = isosurfsettings_[idx];
     }
-    
+
     vd->init();
     vd->showVolRen( isVolRenShown() );
 
@@ -1054,6 +1054,8 @@ visSurvey::SurveyObject* VolumeDisplay::duplicate( TaskRunner* tr ) const
 
     vd->setSelSpec( 0, as_ );
     vd->setDataVolume( 0, cache_, tr );
+    vd->setColTabMapperSetup( 0, scalarfield_->getColTabMapper().setup_, tr );
+    vd->setColTabSequence( 0, scalarfield_->getColTabSequence(), tr );
     return vd;
 }
 
