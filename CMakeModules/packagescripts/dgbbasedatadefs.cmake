522842875c19f22d9460c73f029fb52faa6cab15
Nageswara Rao <nageswara.rao@dgbes.com>
2012-11-15 09:20:29 +0000

    New entry.
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@27451 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ CMakeModules/packagescripts/dgbbasedatadefs.cmake
@@ -0,0 +1,11 @@
+#!/bin/csh
+#(C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+# Description:  CMake script to define basedata package variables
+# Author:       Nageswara
+# Date:         Nov 2012
+#RCS:           $Id$
+
+#//TODO Modify script to work on all platforms.
+SET( LIBLIST )
+SET( EXECLIST  )
+SET( PACK "dgbbasedata")
