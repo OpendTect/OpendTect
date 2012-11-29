4ba5f40c1ff2a705f43f8e4e4e6db5108569fa88
Nageswara Rao <nageswara.rao@dgbes.com>
2012-11-29 03:58:18 +0000

    NEw entry. Installing stuff related to devel package.
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@27678 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ CMakeModules/ODInstallSources.cmake
@@ -0,0 +1,41 @@
+#_______________________Pmake__________________________________________________
+#
+#	CopyRight:	dGB Beheer B.V.
+# 	Dec 2012	Nageswara
+#	RCS :		$Id$
+#_______________________________________________________________________________
+
+#TODO Add date
+set ( YEAR 2012 )
+configure_file( ${CMAKE_SOURCE_DIR}/data/install_files/unixscripts/license_devel.txt ${CMAKE_SOURCE_DIR}/CMakeModules/templates/license.txt.in )
+
+macro( add_licensetext DIRNAME DIRPATH )
+    FILE( GLOB FILES ${CMAKE_SOURCE_DIR}/${DIRPATH}/${DIRNAME}/* )
+    foreach( FIL ${FILES} )
+	FILE( READ ${FIL} temp )
+	FILE( WRITE ${CMAKE_INSTALL_PREFIX}/${DIRPATH}/${DIRNAME}/tempfile
+	      "${lic_temp}\n" )
+	FILE( APPEND ${CMAKE_INSTALL_PREFIX}/${DIRPATH}/${DIRNAME}/tempfile
+	      "${temp}\n" )
+	get_filename_component( FILENAME ${FIL} NAME )
+	FILE( RENAME
+		${CMAKE_INSTALL_PREFIX}/${DIRPATH}/${DIRNAME}/tempfile 
+		${CMAKE_INSTALL_PREFIX}/${DIRPATH}/${DIRNAME}/${FILENAME} )
+    endforeach()
+endmacro( add_licensetext )
+
+FILE( READ ${CMAKE_SOURCE_DIR}/CMakeModules/templates/license.txt.in
+	   lic_temp )
+INCLUDE( ${CMAKE_SOURCE_DIR}/CMakeModules/packagescripts/develdefs.cmake )
+message( "${INCLIBLIST}" )
+foreach( INCLUDEDIR ${INCLIBLIST} )
+    add_licensetext( "${INCLUDEDIR}" "include" )
+endforeach()
+
+foreach( SRCDIR ${SRCLIBLIST} )
+    add_licensetext( "${SRCDIR}" "src" )
+endforeach()
+
+foreach( PLUGINDIR ${PLUGINS} )
+    add_licensetext( "${PLUGINDIR}" "plugins" )
+endforeach()
