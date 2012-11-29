9d81b6924c0492bbf3025e85633dc482ce7ad912
Nageswara Rao <nageswara.rao@dgbes.com>
2012-11-29 10:58:03 +0000

    Removed commented line.
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@27685 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODPackages.cmake
+++ CMakeModules/ODPackages.cmake
@@ -4,16 +4,15 @@
 # Date:		August 2012		
 #RCS:           $Id: ODPackages.cmake,v 1.6 2012/09/11 12:26:50 cvsnageswara Exp $
 
-add_custom_target( do_install ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target install
-        --config ${CMAKE_BUILD_TYPE} 
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR} )
+add_custom_target( do_install ${CMAKE_COMMAND}
+		   --build ${CMAKE_BINARY_DIR} --target install
+                   WORKING_DIRECTORY ${CMAKE_BINARY_DIR} )
 
 add_custom_target( packages  ${CMAKE_COMMAND} 
         -DOpendTect_VERSION_MAJOR=${OpendTect_VERSION_MAJOR} 
         -DOpendTect_VERSION_MINOR=${OpendTect_VERSION_MINOR} 
         -DOpendTect_VERSION_PATCH=${OpendTect_VERSION_PATCH} 
         -DOD_PLFSUBDIR=${OD_PLFSUBDIR} 
-#        -DPACKAGE_DEFINITIONS=${PACKAGE_DEFINITIONS} 
         -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} 
 	-DPSD=${PROJECT_SOURCE_DIR}
 	-DQTDIR=${QTDIR}
