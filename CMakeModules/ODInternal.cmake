6abba5aaac3ab680cd2ef88e317dde8236e60f85
Nageswara Rao <nageswara.rao@dgbes.com>
2012-11-29 08:56:42 +0000

    Install unix scripts.
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@27682 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODInternal.cmake
+++ CMakeModules/ODInternal.cmake
@@ -7,15 +7,49 @@
 
 #Install cmake things.
 install ( DIRECTORY CMakeModules DESTINATION .
-	  PATTERN "CVS" EXCLUDE )
+	  PATTERN ".svn" EXCLUDE )
 
 #Install plugin example
 install( DIRECTORY ${CMAKE_SOURCE_DIR}/doc/Programmer/pluginexample DESTINATION doc/Programmer
-	 PATTERN "CVS" EXCLUDE )
+	 PATTERN ".svn" EXCLUDE )
 
 #Install batchprogram example
 install( DIRECTORY ${CMAKE_SOURCE_DIR}/doc/Programmer/batchprogexample
 		   DESTINATION doc/Programmer
-		   PATTERN "CVS" EXCLUDE )
+		   PATTERN ".svn" EXCLUDE )
+
+#install data folder
+install( DIRECTORY ${CMAKE_SOURCE_DIR}/data 
+	 DESTINATION .
+	 PATTERN "install_files" EXCLUDE
+	 PATTERN ".svn" EXCLUDE )
+
+if ( UNIX )
+    file ( GLOB TEXTFILES
+	   ${CMAKE_SOURCE_DIR}/data/install_files/unixscripts/*.txt )
+    file( GLOB PROGRAMS
+	  ${CMAKE_SOURCE_DIR}/data/install_files/unixscripts/* )
+    list ( REMOVE_ITEM PROGRAMS
+	   ${CMAKE_SOURCE_DIR}/data/install_files/unixscripts/.svn )
+    foreach ( TEXTFILE ${TEXTFILES} )
+	list ( REMOVE_ITEM PROGRAMS ${TEXTFILE} )
+    endforeach()
+
+    install ( PROGRAMS ${PROGRAMS} DESTINATION . )
+    install ( FILES ${TEXTFILES} DESTINATION . )
+endif( UNIX )
+
+if ( APPLE )
+   #Put in Info.plist
+endif( APPLE )
+
+if ( WIN32 )
+#install runtime libraries
+endif ( WIN32 )
+
+add_custom_target( sources ${CMAKE_COMMAND}
+	-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+	-P ${CMAKE_SOURCE_DIR}/CMakeModules/ODInstallSources.cmake 
+	 COMMENT "Installing sources" )
 
 include ( ODSubversion )
