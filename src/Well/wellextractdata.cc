ec90493d934596bc253c1405174cc1734741342f
Arnaud Huck <arnaud.huck@dgbes.com>
2012-12-03 00:04:14 +0000

    Bugfix 1664 - logsampler did not use the provided zstep
    git-svn-id: https://svn.opendtect.org/od/trunk@27730 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Well/wellextractdata.cc
+++ src/Well/wellextractdata.cc
@@ -1040,14 +1040,17 @@ bool Well::LogSampler::doPrepare( int thread )
 	return false;
     }	
 
-    TypeSet<float> dahs; Interval<float> dahrg; 
-    dahrg.start = zrgisintime_ ? d2t_->getDah(zrg_.start) : zrg_.start;
-    dahrg.stop = zrgisintime_ ? d2t_->getDah(zrg_.stop) : zrg_.stop;
+    TypeSet<float> dahs; Interval<float> dahrg;
+    const float dahstart = track_.getDahForTVD( zrg_.start, mUdf(float) );
+    const float dahstop = track_.getDahForTVD( zrg_.stop, mUdf(float) );
+    dahrg.start = zrgisintime_ ? d2t_->getDah(zrg_.start) : dahstart;
+    dahrg.stop = zrgisintime_ ? d2t_->getDah(zrg_.stop) : dahstop;
     float dah = dahrg.start;
     float time = mUdf(float);
     if ( extrintime_ )
     {
-	time = zrgisintime_ ? zrg_.start : d2t_->getTime( dah, track_ );
+	time = zrgisintime_ ? track_.getDahForTVD(zrg_.start) :
+	   		      d2t_->getTime( dah, track_ );
 	time -= zstep_;
     }
     else
@@ -1094,7 +1097,6 @@ bool Well::LogSampler::doWork( od_int64 start, od_int64 stop, int nrthreads )
 }
 
 
-#define mWinSz extrintime_ ? log->dahStep(true)*20 : SI().zStep()
 bool Well::LogSampler::doLog( int logidx )
 {
     const Well::Log* log = logset_.validIdx( logidx ) ? logset_[logidx] : 0;
@@ -1102,11 +1104,30 @@ bool Well::LogSampler::doLog( int logidx )
 
     for ( int idz=0; idz<data_->info().getSize(1); idz++ ) 
     {
-	const float dah = data_->get( 0, idz );
-	const float winsz = mWinSz;
-	const float lval = samppol_ == Stats::TakeNearest ? 
-	    	  log->getValue(dah,true) 
-		: LogDataExtracter::calcVal(*log,dah,winsz,samppol_);
+	float dah = data_->get( 0, idz );
+	float lval = mUdf(float);
+
+	if ( samppol_ == Stats::TakeNearest )
+	    lval = log->getValue(dah,true);
+	else
+	{
+	    float winsz = SI().zStep();
+	    if ( extrintime_ )
+	    {
+		const float startdahwin = idz == 0 ? dah :
+		    ( data_->get(0,idz)+data_->get(0,idz-1) ) / 2;
+		const float stopdahwin = idz < data_->info().getSize(1) ?
+		    ( data_->get(0,idz)+data_->get(0,idz+1) ) / 2 : dah;
+		const float starttime = d2t_->getTime(startdahwin, track_ );
+		const float stoptime = d2t_->getTime( stopdahwin, track_ );
+		winsz = stoptime - starttime;
+		dah = d2t_->getDah( ( stoptime + starttime ) / 2 );
+	    }
+	    else if ( !mIsUdf(zstep_) )
+		winsz = zstep_;
+
+	    lval = LogDataExtracter::calcVal(*log,dah,winsz,samppol_);
+	}
 	data_->set( logidx+1, idz, lval );
     }
 
