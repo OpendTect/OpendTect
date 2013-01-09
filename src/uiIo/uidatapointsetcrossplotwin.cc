dd1d3ec4324d6bc701fdc7dbe960af09247bc0d5
Mahant Mothey <mahant.mothey@dgbes.com>
2013-01-09 04:20:23 +0000

    Fix for minptsfordensity_.
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@28115 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiIo/uidatapointsetcrossplotwin.cc
+++ src/uiIo/uidatapointsetcrossplotwin.cc
@@ -77,6 +77,12 @@ uiDataPointSetCrossPlotWin::uiDataPointSetCrossPlotWin( uiDataPointSet& uidps )
     Settings& setts = Settings::common();
     if ( !setts.get(sKeyMinDPPts(),minptsfordensity_) )
 	minptsfordensity_ = cMinPtsForDensity;
+    if ( minptsfordensity_ <= 0 || mIsUdf(minptsfordensity_) )
+    {
+	setts.set( sKeyMinDPPts(), cMinPtsForDensity ); setts.write();
+	minptsfordensity_ = cMinPtsForDensity;
+    }
+
     const int nrpts = plotter_.y2_.axis_ ? uidps.pointSet().nrActive()*2
 					 : uidps.pointSet().nrActive();
     const float perc = (float)( 100/(1 + nrpts/minptsfordensity_) );
@@ -529,6 +535,11 @@ void uiDataPointSetCrossPlotWin::eachChg( CallBacker* )
     Settings& setts = Settings::common();
     if ( !setts.get(sKeyMinDPPts(),minptsfordensity_) )
 	minptsfordensity_ = cMinPtsForDensity;
+    if ( minptsfordensity_ <= 0 || mIsUdf(minptsfordensity_) )
+    {
+	setts.set( sKeyMinDPPts(), cMinPtsForDensity ); setts.write();
+	minptsfordensity_ = cMinPtsForDensity;
+    }
 
     if ( estpts > minptsfordensity_ && !plotter_.isADensityPlot() )
     {
