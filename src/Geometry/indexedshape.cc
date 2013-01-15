b0d91ef522fceeba54477af19a08be7f63ff81fb
Yuancheng Liu <yuancheng.liu@dgbes.com>
2013-01-15 20:58:38 +0000

    Code style change.
    git-svn-id: https://svn.opendtect.org/od/trunk@28194 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Geometry/indexedshape.cc
+++ src/Geometry/indexedshape.cc
@@ -58,8 +58,7 @@ RangePrimitiveSet* RangePrimitiveSet::create()
 
     
     
-void PrimitiveSetCreator::setCreator(
-				Geometry::PrimitiveSetCreator* c )
+void PrimitiveSetCreator::setCreator( Geometry::PrimitiveSetCreator* c )
 {
     creator_ = c;
 }
