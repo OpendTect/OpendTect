b0614905d8829afa4e7108147d20a4f1d9ba4285
salil.agarwal@dgbes.com <salil.agarwal@dgbes.com@a985855d-d384-4b45-ae33-31a410998c3c>
2013-01-17 08:56:55 +0000

    object of uiwelldisplay wasn't deleting which is causing crash Fixed it.
    git-svn-id: https://svn.opendtect.org/od/trunk@28222 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiWell/uiwelldisplay.cc
+++ src/uiWell/uiwelldisplay.cc
@@ -188,7 +188,11 @@ uiWellDisplayWin::uiWellDisplayWin(uiParent* p, Well::Data& wd )
 
 
 void uiWellDisplayWin::closeWin( CallBacker* )
-{ close(); }
+{
+    delete welldisp_;
+    welldisp_ = 0;
+    close(); 
+}
 
 
 void uiWellDisplayWin::dispInfoMsg( CallBacker* cb )
