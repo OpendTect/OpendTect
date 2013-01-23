a39ee1d1695aea5eb1ba4cfc85a559535831329e
Nageswara Rao <nageswara.rao@dgbes.com>
2013-01-23 05:41:45 +0000

    Solution name changed.
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@28307 a985855d-d384-4b45-ae33-31a410998c3c

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,10 +3,10 @@
 ## project to incorporate the testing dashboard.
 ## # The following are required to uses Dart and the Cdash dashboard
 
-set(CTEST_PROJECT_NAME "OpendTect-4.4")
+set(CTEST_PROJECT_NAME "OpendTect-4_4")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "intranet")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=${CTEST_PROJECT_NAME}")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=OpendTect-4.4")
 set(CTEST_DROP_SITE_CDASH TRUE)
