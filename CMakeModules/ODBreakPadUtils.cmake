ade391f0c728c7daa65159d963c879cb165fed7d
Aneesh Tiwari <aneesh.tiwari@dgbes.com>
2013-01-24 10:07:57 +0000

    some changes (previous commit didn't work properly)
    git-svn-id: https://svn.opendtect.org/od/trunk@28331 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODBreakPadUtils.cmake
+++ CMakeModules/ODBreakPadUtils.cmake
@@ -6,19 +6,21 @@
 #_______________________________________________________________________________
 
 
-set( BREAKPAD_DIR "" cache path "BREAKPAD Location" )
+set( BREAKPAD_DIR "" CACHE PATH "BREAKPAD Location" )
 
 macro(OD_SETUP_BREAKPAD)
-    if ( OD_USEBREAKPAD )
-	if( WIN32 )
-	    find_library( BREAKPADLIB names BreakPad paths ${BREAKPAD_DIR}/client/windows/Debug/lib REQUIRED )
-	    find_library( BREAKPADCOMMONLIB names BreakPadCommon paths ${BREAKPAD_DIR}/client/windows/Debug/lib REQUIRED )
-	    find_library( BREAKPADCLIENTLIB names BreakPadClient paths ${BREAKPAD_DIR}/client/windows/Debug/lib REQUIRED )
-	endif( WIN32 )
+
+	if(OD_USEBREAKPAD)
+		if( WIN32 )
+			find_library( BREAKPADLIB NAMES BreakPad PATHS ${BREAKPAD_DIR}/client/windows/Debug/lib REQUIRED )
+			find_library( BREAKPADCOMMONLIB NAMES BreakPadCommon PATHS ${BREAKPAD_DIR}/client/windows/Debug/lib REQUIRED )
+			find_library( BREAKPADCLIENTLIB NAMES BreakPadClient PATHS ${BREAKPAD_DIR}/client/windows/Debug/lib REQUIRED )
+		endif( WIN32 )
 		
-	set(OD_BREAKPADLIBS ${BREAKPADLIB} ${BREAKPADCOMMONLIB} ${BREAKPADCLIENTLIB} )
-	add_definitions( -DHAS_BREAKPAD )
-	list(APPEND OD_MODULE_INCLUDESYSPATH ${BREAKPAD_DIR})
-	list(APPEND OD_MODULE_EXTERNAL_LIBS ${OD_BREAKPADLIBS} )
-    endif( OD_USEBREAKPAD )
+		set(OD_BREAKPADLIBS ${BREAKPADLIB} ${BREAKPADCOMMONLIB} ${BREAKPADCLIENTLIB} )
+		add_definitions( -DHAS_BREAKPAD )
+		list(APPEND OD_MODULE_INCLUDESYSPATH ${BREAKPAD_DIR})
+		list(APPEND OD_MODULE_EXTERNAL_LIBS ${OD_BREAKPADLIBS} )
+	endif(OD_USEBREAKPAD)
+
 endmacro(OD_SETUP_BREAKPAD)
