3a01d16015d1320165dd6987781bc61e022a6477
Nanne Hemstra <nanne.hemstra@dgbes.com>
2013-01-28 19:20:37 +0000

    Added
    git-svn-id: https://svn.opendtect.org/od/trunk@28372 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ plugins/MatlabLink/matlabstep.h
@@ -0,0 +1,51 @@
+#ifndef matlabstep_h
+#define matlabstep_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Nanne Hemstra
+ Date:		February 2013
+ RCS:		$Id$
+________________________________________________________________________
+
+-*/
+
+#include "matlablinkmod.h"
+#include "volprocchain.h"
+
+namespace VolProc
+{
+
+mExpClass(MatlabLink) MatlabStep : public Step
+{
+public:
+			mDefaultFactoryInstantiation( Step,
+				MatlabStep,
+				"MatlabStep",
+				"MATLAB" );
+
+    void		fillPar(IOPar&) const;
+    bool		usePar(const IOPar&);
+
+    bool		needsInput() const 		{ return true; }
+    bool		canInputAndOutputBeSame() const	{ return true; }
+    bool		needsFullVolume() const		{ return true; }
+    bool		areSamplesIndependent() const	{ return false; }
+    const char*		errMsg() const		{ return errmsg_.str(); }
+
+    Task*		createTask();
+
+protected:
+
+			MatlabStep();
+			~MatlabStep();
+
+    FixedString		errmsg_;
+};
+
+} // namespace VolProc
+
+#endif
+
