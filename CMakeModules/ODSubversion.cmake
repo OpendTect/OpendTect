8e20d2acdcf9cf201c6e86ca7afa7accc02f0f7d
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2013-02-18 13:06:36 +0000

    Merge from od4.4 (D35).
    git-svn-id: https://svn.opendtect.org/od/trunk@28552 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODSubversion.cmake
+++ CMakeModules/ODSubversion.cmake
@@ -5,6 +5,9 @@
 #	RCS :		$Id$
 #_______________________________________________________________________________
 
+if ( EXISTS ${CMAKE_SOURCE_DIR}/.svn )
+    set ( OD_FROM_SVN 1 )
+endif()
 
 # the FindSubversion.cmake module is part of the standard distribution
 if ( WIN32 )
@@ -17,27 +20,35 @@ endif()
 include(FindSubversion)
 
 # extract working copy information for SOURCE_DIR into MY_XXX variables
-if ( Subversion_FOUND )
-    Subversion_WC_INFO( ${CMAKE_SOURCE_DIR} MY)
+if ( Subversion_FOUND AND OD_FROM_SVN )
+    Subversion_WC_INFO( ${CMAKE_SOURCE_DIR} MY )
 else()
     set ( MY_WC_REVISION 0 )
     set ( MY_WC_URL "" )
 endif()
 
 set ( INC_DIR ${CMAKE_SOURCE_DIR}/include/Basic )
-set ( TMPFILE ${INC_DIR}/svnversion.h.tmp )
+set ( INC_FILE ${INC_DIR}/svnversion.h )
 
-# write a file with the SVNVERSION define
-file( WRITE ${TMPFILE} 
-     "// Generated automatically by CMakeModules/ODSubversion.cmake\n"
-     "//\n"
-     "#ifndef mSVN_VERSION\n"
-     "#define mSVN_VERSION ${MY_WC_REVISION}\n"
-     "#define mSVN_URL \"${MY_WC_URL}\"\n"
-     "#endif\n")
+if ( NOT EXISTS ${INC_FILE} )
+    set ( OD_CREATE_INC_FILE 1 )
+endif()
 
-# copy the file to the final header only if the version changes
-# reduces needless rebuilds
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                        ${TMPFILE} ${INC_DIR}/svnversion.h )
-file ( REMOVE ${TMPFILE} )
+if ( OD_FROM_SVN OR OD_CREATE_INC_FILE )
+    set ( TMPFILE ${INC_DIR}/svnversion.h.tmp )
+
+    # write a file with the SVNVERSION define
+    file( WRITE ${TMPFILE} 
+	 "// Generated automatically by CMakeModules/ODSubversion.cmake\n"
+	 "//\n"
+	 "#ifndef mSVN_VERSION\n"
+	 "#define mSVN_VERSION ${MY_WC_REVISION}\n"
+	 "#define mSVN_URL \"${MY_WC_URL}\"\n"
+	 "#endif\n")
+
+    # copy the file to the final header only if the version changes
+    # reduces needless rebuilds
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
+			    ${TMPFILE} ${INC_DIR}/svnversion.h )
+    file ( REMOVE ${TMPFILE} )
+endif()
