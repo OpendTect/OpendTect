b15914f9ee295560fd13bfc6f9a8704eaf962dd2
Nageswara Rao <nageswara.rao@dgbes.com>
2013-02-26 12:07:16 +0000

    Added executables to definition.
    git-svn-id: https://svn.opendtect.org/od/trunk@28634 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/packagescripts/dgbhcdefs.cmake
+++ CMakeModules/packagescripts/dgbhcdefs.cmake
@@ -5,8 +5,8 @@
 # Date:         August 2012
 #RCS:           $Id$
 
-#//TODO Modify script to work on all platforms.
 SET( LIBLIST HorizonCube uiHorizonCube uiHorizonCubeSlider )
-SET( EXECLIST od_convert_chronostrat od_filter_horizoncube od_hc2steer od_process_horizoncube
-	      od_trim_hor_atfaults od_grid_horizoncube )
+SET( EXECLIST od_convert_chronostrat od_filter_horizoncube od_hc2steer
+	      od_process_horizoncube od_trim_hor_atfaults od_grid_horizoncube
+	      od_process_attrib_hc )
 SET( PACK "dgbhc" )
