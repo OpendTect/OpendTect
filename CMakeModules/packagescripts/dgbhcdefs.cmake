773e8f27f2ba0ce9aceb3ae58463a44b32161b68
Nageswara Rao <nageswara.rao@dgbes.com>
2013-02-27 05:08:45 +0000

    Removed commented lines.
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@28648 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/packagescripts/dgbhcdefs.cmake
+++ CMakeModules/packagescripts/dgbhcdefs.cmake
@@ -5,8 +5,7 @@
 # Date:         August 2012
 #RCS:           $Id: dgbhcdefs.cmake,v 1.4 2012/09/06 05:50:52 cvsnageswara Exp $
 
-#//TODO Modify script to work on all platforms.
 SET( LIBLIST HorizonCube uiHorizonCube uiHorizonCubeSlider )
-SET( EXECLIST od_convert_chronostrat od_filter_horizoncube od_hc2steer od_process_horizoncube od_trim_hor_atfaults )
-#SET( PACKAGE_DIR "/dsk/d21/nageswara/dev/buildtest/lux64" )
+SET( EXECLIST od_convert_chronostrat od_filter_horizoncube od_hc2steer
+	      od_process_horizoncube od_trim_hor_atfaults )
 SET( PACK "dgbhc" )
