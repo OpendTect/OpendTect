d5bc1119942b67538904a46fbfb512087413c324
Ranojay Sen <ranojay.sen@dgbes.com>
2013-03-19 11:18:51 +0000

    Fixed re-deletion of objects, if already removed with  ManagedObjectSet::removeSingle()
    Summary: removed the delete statements, as the objects will automatically be deleted in removesingle function
    
    Test Plan: compiled and executed OD, did not encounter any crash yet.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    CC: kristofer
    
    Maniphest Tasks: T31
    
    Differential Revision: https://phabricator.dgbes.com:8443/D222
    
    git-svn-id: https://svn.opendtect.org/od/trunk@28884 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Basic/posinfo.cc
+++ src/Basic/posinfo.cc
@@ -313,7 +313,7 @@ void PosInfo::CubeData::limitTo( const HorSampling& hsin )
     {
 	PosInfo::LineData* ld = (*this)[iidx];
 	if ( !hs.inlOK(ld->linenr_) )
-	{ ld = removeSingle( iidx ); delete ld; continue; }
+	{ removeSingle( iidx ); continue; }
 
 	int nrvalidsegs = 0;
 	for ( int iseg=ld->segments_.size()-1; iseg>=0; iseg-- )
@@ -356,7 +356,7 @@ void PosInfo::CubeData::limitTo( const HorSampling& hsin )
 	}
 
 	if ( !nrvalidsegs )
-	{ ld = removeSingle( iidx ); delete ld; }
+	    removeSingle( iidx );
     }
 }
 
