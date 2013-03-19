8fba01d8d226619d95f2418c95cd4acd98af6657
Bert Bril <bert.bril@dgbes.com>
2013-03-19 12:48:17 +0000

    Fixed crash when no layer at SRD
    Summary: layerIdxAtZ can return -1, then firstidx was -1, then seq.layers()[-1] is accessed. The code around it was ... a bit sub-standard so I cleaned it up.
    
    Test Plan: Tested in od and od4.4 - no more crash
    
    Reviewers: helene
    
    Reviewed By: helene
    
    Differential Revision: https://phabricator.dgbes.com:8443/D235
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@28892 a985855d-d384-4b45-ae33-31a410998c3c

--- src/WellAttrib/stratsynth.cc
+++ src/WellAttrib/stratsynth.cc
@@ -455,7 +455,6 @@ const char* StratSynth::errMsg() const
 }
 
 
-#define mLayerBlockSize 1000
 static const float cMaximumVpWaterVel = 1510.f;
 
 
@@ -465,7 +464,7 @@ bool StratSynth::fillElasticModel( const Strat::LayerModel& lm,
     const Strat::LayerSequence& seq = lm.sequence( seqidx ); 
     const ElasticPropSelection& eps = lm.elasticPropSel();
     const PropertyRefSelection& props = lm.propertyRefs();
-    if ( !eps.isValidInput( &errmsg_ ) )
+    if ( !eps.isValidInput(&errmsg_) )
 	return false; 
 
     ElasticPropGen elpgen( eps, props );
@@ -473,48 +472,46 @@ bool StratSynth::fillElasticModel( const Strat::LayerModel& lm,
     const ElasticPropertyRef& pvref = eps.getPropertyRef(ElasticFormula::PVel); 
     const ElasticPropertyRef& svref = eps.getPropertyRef(ElasticFormula::SVel); 
     const float srddepth = -1*0; // SHOULD COME from SI() - depth, not elevation
-    const Strat::Layer* lay = 0;
-    const int firstidx = seq.startDepth() >= srddepth ? 0 :
-					     seq.layerIdxAtZ( srddepth, false );
+    int firstidx = 0;
+    if ( seq.startDepth() < srddepth )
+    {
+	firstidx = seq.layerIdxAtZ( srddepth, false );
+	if ( firstidx < 0 )
+	    firstidx = 0;
+    }
+
     for ( int idx=firstidx; idx<seq.size(); idx++ )
     {
-	lay = seq.layers()[idx];
+	const Strat::Layer* lay = seq.layers()[idx];
 	float thickness = lay->thickness();
 	if ( idx == firstidx )
-	{
 	    thickness -= srddepth - lay->zTop();
-	    if ( mIsZero( thickness, 1e-4 ) )
-		continue;
+	if ( thickness < 1e-4 )
+	    continue;
+
+	float dval  = elpgen.getVal( denref, lay->values(), props.size() );
+	float pval = elpgen.getVal( pvref, lay->values(), props.size() );
+	float sval = elpgen.getVal( svref, lay->values(), props.size() );
+	const bool dudf = mIsUdf(dval); const bool pudf = mIsUdf(dval);
+	const bool sudf = mIsUdf(dval);
+	const int nrudf = (dudf?1:0) + (pudf?1:0) + (sudf?1:0);
+	if ( nrudf > 0 )
+	{
+	    BufferString msg(
+		    "Cannot derive layer propert", nrudf>1?"ies:":"y:" );
+#	    define mAddUdfProp(t,s) if ( t##udf ) msg.add( " " ).add( s )
+	    mAddUdfProp(d,"'Density'"); mAddUdfProp(p,"'P-wave velocity'");
+	    mAddUdfProp(s,"'S-wave velocity'");
+	    msg.add( ".\nPlease check the definition" );
+	    if ( nrudf > 1 ) msg.add( "s" );
+	    errmsg_ = msg; return false;
 	}
-	float dval, pval, sval;
-	dval  = elpgen.getVal( denref, lay->values(), props.size() );
-	pval = elpgen.getVal( pvref, lay->values(), props.size() );
-	sval = elpgen.getVal( svref, lay->values(), props.size() );
 
 	// Detect water - reset Vs
 	if ( pval < cMaximumVpWaterVel )
 	    sval = 0;
 	
-	ElasticLayer ail ( thickness, pval, sval, dval );
-	BufferString msg( "Can not derive synthetic layer property " );
-	bool isudf = mIsUdf( dval ) || mIsUdf( pval ) || mIsUdf( sval );
-	if ( mIsUdf( dval ) )
-	{
-	    msg += "'Density'";
-	}
-	if ( mIsUdf( pval ) )
-	{
-	    msg += "'P-Wave'";
-	}
-	if ( mIsUdf( sval ) )
-	{
-	    msg += "'S-Wave'";
-	}
-	msg += ". \n Please check its definition.";
-	if ( isudf )
-	    { errmsg_ = msg; return false; }
-
-	aimodel += ail;
+	aimodel += ElasticLayer( thickness, pval, sval, dval );
     }
 
     return true;
