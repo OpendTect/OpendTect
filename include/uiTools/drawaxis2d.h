eb7bb0a5d727cb85b5a1a91746ea62486e9e5643
Mahant Mothey <mahant.mothey@dgbes.com>
2013-04-02 09:28:08 +0000

    Documentation.
    git-svn-id: https://svn.opendtect.org/od/trunk@29063 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiTools/drawaxis2d.h
+++ include/uiTools/drawaxis2d.h
@@ -27,29 +27,26 @@ class uiTextItem;
 class uiLineItem;
 template <class T> class StepInterval;
 
-/*\!Draw simple axis for 2D axis.
-
-    This class is designed for normal axis drawing and is not applicable to
-    axises which are oblique to the X/Y axis of window. The area for drawing
-    axis can be on the same canvas with the main graph area or on different 
-    canvas. Refer to uiCrossplot or uiDistributionMap for an example.
-
-    Axis values is marked at every step. Grid lines can also be drawn at 
-    each step. Before calling the drawing routines, make sure the proper line
-    pattern and pen color are set for the 'ioDrawTool' and there is enough
-    room in the window to draw the axis and number annotation.
-    
-    USAGE:
-    1) call setupAxis() to set up the position for drawing axis and a pointer
-       to uiWorld2Ui which provides the corrdinate conversion functions.
-       The axis range and step is calculated automatically from the class
-       uiWorld2Ui.
-    2) If the default range and step are not to be used for axis annotation,
-       call setFixedDataRangeandStep() to override after calling setupAxis().
-    3) Call the actual draw functions
-
- */
-
+/*!\brief To draw simple axes for a 2D coordinate system.
+  
+  This class is designed for normal axis drawing and is not applicable to axes
+  which are oblique to the X/Y axis of window. The area for drawing axis can be
+  on the same canvas with the main graph area or on different canvas. Refer to
+  uiCrossplot or uiDistributionMap for an example.
+  
+  Axis values are marked at every step. Grid lines can also be drawn at each
+  step. Before calling the drawing routines, make sure the proper line pattern
+  and pen color are set for the 'ioDrawTool' and there is enough room in the
+  window to draw the axis and number annotation.
+  
+  USAGE:
+  1) Call setupAxis() to set up the position for drawing axis and a pointer
+  to uiWorld2Ui which provides the coordinate conversion functions. The axis
+  range and step is calculated automatically from the class uiWorld2Ui.
+  2) If the default range and step are not to be used for axis annotation,
+  call setFixedDataRangeandStep() to override after calling setupAxis().
+  3) Call the actual draw functions
+*/
 
 mExpClass(uiTools) uiGraphicsSceneAxis
 {
@@ -104,6 +101,10 @@ protected:
 };
 
 
+/*!
+\brief Manages uiGraphicsSceneAxis.
+*/
+
 mExpClass(uiTools) uiGraphicsSceneAxisMgr
 {
 public:
@@ -154,8 +155,6 @@ protected:
     uiRectItem*			leftmask_;
     uiRectItem*			rightmask_;
 };
-
-
 
 #endif
 
