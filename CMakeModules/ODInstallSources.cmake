9b07480e5336881471b85d53693eb9bbd9d87459
Nageswara Rao <nageswara.rao@dgbes.com>
2013-04-10 09:59:13 +0000

    We may lost some files while copying files. So, svn checkout is good option than copying file.
    Summary: copy option was replaced by 'svn checkout'.
    
    Test Plan: Tested on lux64
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D237
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@29191 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODInstallSources.cmake
+++ CMakeModules/ODInstallSources.cmake
@@ -9,11 +9,47 @@
 set ( YEAR 2012 )
 configure_file( ${CMAKE_SOURCE_DIR}/data/install_files/unixscripts/license_devel.txt ${CMAKE_SOURCE_DIR}/CMakeModules/templates/license.txt.in )
 
+set( ROOT "https://svn.opendtect.org/od" )
+IF( NOT DEFINED ISTAG )
+    MESSAGE( FATAL_ERROR "ISTAG is not defined" )
+ENDIF()
+
+IF( "${ISTAG}" STREQUAL "" )
+    MESSAGE( FATAL_ERROR "ISTAG is not defined" )
+ENDIF()
+
+IF( NOT DEFINED OD_BRANCH )
+    MESSAGE( FATAL_ERROR "OD_BRANCH is not defined" )
+ENDIF()
+
+IF( "${OD_BRANCH}" STREQUAL "" )
+    MESSAGE( FATAL_ERROR "OD_BRANCH is not defined" )
+ENDIF()
+
+IF( "${ISTAG}" STREQUAL "N" OR "${ISTAG}" STREQUAL "n" )
+    set( ROOT "${ROOT}/branches" )
+ELSEIF( "${ISTAG}" STREQUAL "Y" OR "${ISTAG}" STREQUAL "y" )
+    set( ROOT "${ROOT}/tags" )
+ENDIF()
+
+set( ROOT "${ROOT}/${OD_BRANCH}" )
 macro( add_licensetext DIRNAME DIRPATH )
     MESSAGE( "Installing ${DIRPATH}/${DIRNAME} " )
-    FILE( GLOB HFILES ${CMAKE_SOURCE_DIR}/${DIRPATH}/${DIRNAME}/*.h )
-    FILE( GLOB SFILES ${CMAKE_SOURCE_DIR}/${DIRPATH}/${DIRNAME}/*.cc )
-    SET( FILES ${HFILES} ${SFILES} )
+
+message( "root:${ROOT}/${DIRPATH}/${DIRNAME}" )
+message( "${CMAKE_INSTALL_PREFIX}/${DIRPATH}/${DIRNAME}" )
+
+    execute_process( COMMAND svn checkout ${ROOT}/${DIRPATH}/${DIRNAME} ${CMAKE_INSTALL_PREFIX}/${DIRPATH}/${DIRNAME} RESULT_VARIABLE STATUS )
+    IF( NOT ${STATUS} EQUAL "0" )
+	MESSAGE( FATAL_ERROR "Failed to checkout ${ROOT}/${DIRPATH}/${DIRNAME}" )
+    ENDIF()
+
+    FILE( REMOVE_RECURSE ${CMAKE_INSTALL_PREFIX}/${DIRPATH}/${DIRNAME}/.svn)
+
+    FILE( GLOB HFILES ${CMAKE_INSTALL_PREFIX}/${DIRPATH}/${DIRNAME}/*.h )
+    FILE( GLOB SFILES ${CMAKE_INSTALL_PREFIX}/${DIRPATH}/${DIRNAME}/*.cc )
+    FILE( GLOB CFILES ${CMAKE_INSTALL_PREFIX}/${DIRPATH}/${DIRNAME}/*.c )
+    SET( FILES ${HFILES} ${SFILES} ${CFILES} )
 
     foreach( FIL ${FILES} )
 	FILE( READ ${FIL} temp )
@@ -31,6 +67,13 @@ endmacro( add_licensetext )
 FILE( READ ${CMAKE_SOURCE_DIR}/CMakeModules/templates/license.txt.in
 	   lic_temp )
 INCLUDE( ${CMAKE_SOURCE_DIR}/CMakeModules/packagescripts/develdefs.cmake )
+
+FILE( REMOVE_RECURSE  ${CMAKE_INSTALL_PREFIX}/include
+		      ${CMAKE_INSTALL_PREFIX}/src
+		      ${CMAKE_INSTALL_PREFIX}/plugins
+		      ${CMAKE_INSTALL_PREFIX}/spec
+		      ${CMAKE_INSTALL_PREFIX}/tests )
+
 foreach( INCLUDEDIR ${INCLIBLIST} )
     add_licensetext( "${INCLUDEDIR}" "include" )
 endforeach()
@@ -45,4 +88,8 @@ endforeach()
 
 foreach( SPECDIR ${SPECSOURCES} )
     add_licensetext( "${SPECDIR}" "spec" )
+endforeach()
+
+foreach( TESTDIR ${TESTS} )
+    add_licensetext( "${TESTDIR}" "tests" )
 endforeach()
