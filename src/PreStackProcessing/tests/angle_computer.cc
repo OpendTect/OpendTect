348f25d260ea7e835279a4f6abb45f1ac9098d82
Aneesh Tiwari <aneesh.tiwari@dgbes.com>
2013-04-16 08:36:32 +0000

    Template for angle_computer.cc
    Summary:
    This is a skeleton of a test program to test the angle computer. My idea is that you (initially) hardcode a multiID to read from the F3 data, as well as a hard-coded TraceID.
    
    You would then compute the angles, and do a few checks at a few depths/offset that the angles are correct.
    
    Later, I want to do a few things:
    
    1. I wish to add put the MultiID and the TraceID in a parameter file.
    2. I wish to use the test-survey that Nageswara and Arnaud are making (T15).
    
    Please also note that when D124 is approved, the executable will be named test_angle_computer.
    
    Test Plan: This does not compile. It is my wish that you commandeer this revision (so it becomes yours) and implement it. I'll be happy to review it for you when you reach that stage.
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Maniphest Tasks: T8
    
    Differential Revision: https://phabricator.dgbes.com:8443/D130
    
    git-svn-id: https://svn.opendtect.org/od/trunk@29267 a985855d-d384-4b45-ae33-31a410998c3c

--- src/PreStackProcessing/tests/angle_computer.cc
+++ src/PreStackProcessing/tests/angle_computer.cc
@@ -8,13 +8,80 @@ static const char* rcsID mUsedVar = "$Id$";
 
 #include "batchprog.h"
 
-#include "prestackanglecomputer.h"
+#include "moddepmgr.h"
 #include "multiid.h"
+#include "prestackanglecomputer.h"
+#include "prestackgather.h"
+#include "windowfunction.h"
 #include <iostream>
+
+
+#define mCheckVal(ofsidx,zidx,val) \
+    if ( !mIsEqual(angles->data().get(ofsidx,zidx),val,1e-2) ) return false
+
+
+bool isAngleOK(PreStack::Gather* angles)
+{
+    mCheckVal( 0, 0, 0 );
+    mCheckVal( 0, 46, 0 );
+    mCheckVal( 1, 10, 0.5877 );
+    mCheckVal( 1, 40, 0.1411 );
+    mCheckVal( 2, 20, 0.5612 );
+    mCheckVal( 2, 30, 0.3757 );
+    mCheckVal( 3, 20, 0.7551 );
+    mCheckVal( 3, 30, 0.5341 );
+    mCheckVal( 4, 10, 1.2063 );
+    mCheckVal( 4, 40, 0.5166 );
+    mCheckVal( 5, 0, 1.5416 );
+    mCheckVal( 5, 46, 0.5627 );
+
+    return true;
+}
 
 
 bool BatchProgram::go( std::ostream &strm )
 {
-	// ToDo
-	return true; 
+    OD::ModDeps().ensureLoaded( "Velocity" );
+    RefMan<PreStack::VelocityBasedAngleComputer> computer = 
+				    new PreStack::VelocityBasedAngleComputer;
+
+    computer->setMultiID( MultiID(100010,34) );
+
+    if ( !computer->isOK() )
+    {
+	std::cerr<<" Angle computer is not OK.\n";
+	return false;
+    }
+
+    StepInterval<double> zrange(0,1.848,0.04), offsetrange(0,2500,500);
+    FlatPosData fp;
+    fp.setRange( true, offsetrange );
+    fp.setRange( false, zrange );
+    computer->setOutputSampling( fp );
+    computer->setTraceID( BinID(425,775) );
+
+    IOPar iopar;
+    iopar.set( PreStack::AngleComputer::sKeySmoothType(), 
+	       PreStack::AngleComputer::TimeAverage );
+    iopar.set( PreStack::AngleComputer::sKeyWinFunc(), HanningWindow::sName() );
+    iopar.set( PreStack::AngleComputer::sKeyWinParam(), 0.95f );
+    iopar.set( PreStack::AngleComputer::sKeyWinLen(), 10 );
+
+    computer->setSmoothingPars( iopar );
+
+    PtrMan<PreStack::Gather> angles = computer->computeAngles();
+    if ( !angles )
+    {
+	std::cerr << "Computer did not succeed in making angle data\n";
+	return false;
+    }
+
+    if ( !isAngleOK(angles) )
+    {
+	std::cerr << "Computer computed wrong values\n";
+	return false;
+    }
+
+    return true;
 }
+
