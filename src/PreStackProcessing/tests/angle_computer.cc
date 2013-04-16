c1c579fdcd3e3fc379e5e12adbaca86db3ad1208
Aneesh Tiwari <aneesh.tiwari@dgbes.com>
2013-04-16 05:57:34 +0000

    There is a problem while adding file using phabricator on my system. So committing this file. Will update it for review using phabricator ( hope it works )
    git-svn-id: https://svn.opendtect.org/od/trunk@29257 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/PreStackProcessing/tests/angle_computer.cc
@@ -0,0 +1,20 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Dec 2008
+-*/
+
+static const char* rcsID mUsedVar = "$Id$"; 
+
+#include "batchprog.h"
+
+#include "prestackanglecomputer.h"
+#include "multiid.h"
+#include <iostream>
+
+
+bool BatchProgram::go( std::ostream &strm )
+{
+	// ToDo
+	return true; 
+}
