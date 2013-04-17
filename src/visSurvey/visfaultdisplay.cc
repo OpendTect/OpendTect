22a5876a30d6c62ae9bad0abe3498264d170a9af
Yuancheng Liu <yuancheng.liu@dgbes.com>
2013-04-17 16:06:42 +0000

    Fault intersection display update
    Summary: Not updated when Fault plane display is not checked.
    
    Test Plan: Tested on od and od4.4 based on bug 1829.
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D469
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@29314 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visSurvey/visfaultdisplay.cc
+++ src/visSurvey/visfaultdisplay.cc
@@ -527,8 +527,7 @@ void FaultDisplay::updateIntersectionDisplay()
     {
 	setLineRadius( intersectiondisplay_ );
 
-	const bool dodisplay = areIntersectionsDisplayed() &&
-			       arePanelsDisplayed();
+	const bool dodisplay = areIntersectionsDisplayed();
 	if ( dodisplay )
 	    intersectiondisplay_->touch( false );
 
