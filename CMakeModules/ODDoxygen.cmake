a17cd6544e665502b3acc6574ac1a2329638b6f8
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2013-04-25 11:06:21 +0000

    Re-instated plain doc target. Sitemap file will be generated by upload script.
    git-svn-id: https://svn.opendtect.org/od/trunk@29495 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODDoxygen.cmake
+++ CMakeModules/ODDoxygen.cmake
@@ -44,13 +44,9 @@ macro( OD_BUILD_DOCUMENTATION )
 
     OD_ADD_SOURCE_FILES( ${TEMPLATE} ${FOOTER} )
 
-    add_custom_target ( doxygen
+    add_custom_target ( doc
 			COMMAND ${DOXYGEN_EXECUTABLE} ${OD_DOXYGEN_FILE}
 			SOURCES ${OD_DOXYGEN_FILE} )
-    add_custom_target ( doc 
-			COMMAND ${CMAKE_SOURCE_DIR}/dtect/make_classdoc_sitemap.csh ${OD_DOXYGEN_PATH}/html
-			DEPENDS doxygen
-			SOURCES ${OD_DOXYGEN_FILE} ${CMAKE_SOURCE_DIR}/dtect/make_classdoc_sitemap.csh )
     install ( DIRECTORY doc/Programmer/Generated/html DESTINATION doc/Programmer/Generated )
 endmacro()
 
