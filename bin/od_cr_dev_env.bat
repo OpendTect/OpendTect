10c7a24e4a6f1fd0d5e2b01c47c8094dabc039a8
Raman Singh <raman.singh@dgbes.com>
2013-05-02 12:18:39 +0000

    Port of D621 to od.
    Summary: same as D621.
    
    Test Plan: same as D621.
    
    Reviewers: nageswara
    
    Reviewed By: nageswara
    
    Differential Revision: https://phabricator.dgbes.com:8443/D622
    
    git-svn-id: https://svn.opendtect.org/od/trunk@29599 a985855d-d384-4b45-ae33-31a410998c3c

--- bin/od_cr_dev_env.bat
+++ bin/od_cr_dev_env.bat
@@ -5,15 +5,11 @@ set inpdir=%1
 set outdir=%2
 
 if not exist %inpdir% goto MSGINPDIR
-if not exist %inpdir%\.rel.devel goto MSGPKG
+if not exist %inpdir%\relinfo\ver.devel_win64.txt if not exist %inpdir%\relinfo\ver.devel_win32.txt goto MSGPKG
 if not exist %outdir% goto MSGOUTDIR
 
-
-FOR %%a IN ( plugins, src, include, Pmake, spec, msvc10, data ) DO md %outdir%\%%a
-FOR %%a IN ( plugins, src, include, Pmake, spec, msvc10, data ) DO xcopy /s %inpdir%\%%a\* %outdir%\%%a
-
-copy %inpdir%\.rel.devel %outdir%
-
+md %outdi%
+xcopy /s %inpdir%\doc\Programmer\pluginexample\* %outdir%\
 if exist %outdir%\plugins\win32 rd /S /Q %outdir%\plugins\win32\libs 
 if exist %outdir%\plugins\win64 rd /S /Q %outdir%\plugins\win64\libs 
 goto END
