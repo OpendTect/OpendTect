b02b095ee51ebd12cd0c621c1d2df8dffb474392
Helene Huck <helene.huck@dgbes.com>
2013-05-02 07:31:38 +0000

    port fix for multicomponent display in histogram & amplitude spectrum to od4.4
    Summary:
    port fix for multicomponent display in histogram & amplitude spectrum
    to od4.4 (rev D576 accepted yesterday, fix mantis issue 1844)
    
    Test Plan: tested in od4.4
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D582
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@29575 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiODMain/uioddatatreeitem.cc
+++ src/uiODMain/uioddatatreeitem.cc
@@ -393,43 +393,65 @@ void uiODDataTreeItem::handleMenuCB( CallBacker* cb )
 	visserv->showAttribTransparencyDlg( displayID(), attribNr() );
 	menu->setIsHandled( true );
     }
-    else if ( mnuid==statisticsitem_.id )
+    else if ( mnuid==statisticsitem_.id || mnuid==amplspectrumitem_.id )
     {
-	const DataPack::ID dpid = visserv->getDataPackID( displayID(),
-							  attribNr() );
-	const DataPackMgr::ID dmid = visserv->getDataPackMgrID( displayID() );
-	uiStatsDisplay::Setup su; su.countinplot( false );
-	uiStatsDisplayWin* dwin =
-	    new uiStatsDisplayWin( applMgr()->applService().parent(), su,
-		    		   1, false );
-	dwin->statsDisplay()->setDataPackID( dpid, dmid );
-	dwin->setDataName( DPM(dmid).nameOf(dpid)  );
-	dwin->setDeleteOnClose( true );
-	dwin->show();
-	menu->setIsHandled( true );
-    }
-    else if ( mnuid==amplspectrumitem_.id )
-    {
-	const DataPack::ID dpid = visserv->getDataPackID( displayID(),
-							  attribNr() );
-	const DataPackMgr::ID dmid = visserv->getDataPackMgrID( displayID() );
-	const bool isselmodeon = visserv->isSelectionModeOn();
+	const int visid = displayID();
+	const int attribid = attribNr();
+	DataPack::ID dpid = visserv->getDataPackID( visid, attribid );
+	const DataPackMgr::ID dmid = visserv->getDataPackMgrID( visid );
 
-	if ( !isselmodeon )
+	const Attrib::SelSpec* as = visserv->getSelSpec( visid, attribid );
+	const FixedString dpname = DPM(DataPackMgr::FlatID()).nameOf( dpid );
+	if ( as && dpname != as->userRef() )
 	{
-	    uiAmplSpectrum* asd = new uiAmplSpectrum(
-					applMgr()->applService().parent() );
-	    asd->setDeleteOnClose( true );
-	    asd->setDataPackID( dpid, dmid );
-	    asd->show();
+	    const int nrpacks = DPM(DataPackMgr::FlatID()).packs().size();
+	    for ( int idx=0; idx<nrpacks; idx++ )
+	    {
+		const int tmpdtpackid =
+				DPM(DataPackMgr::FlatID()).packs()[idx]->id();
+		const FixedString tmpnm =
+		    		DPM(DataPackMgr::FlatID()).nameOf(tmpdtpackid);
+		if ( tmpnm == as->userRef() )
+		{
+		    dpid = tmpdtpackid;
+		    break;
+		}
+	    }
+	}
+	if ( mnuid==statisticsitem_.id )
+	{
+	    uiStatsDisplay::Setup su; su.countinplot( false );
+	    uiStatsDisplayWin* dwin =
+		new uiStatsDisplayWin( applMgr()->applService().parent(), su,
+				       1, false );
+	    dwin->statsDisplay()->setDataPackID( dpid, dmid );
+	    dwin->setDataName( DPM(dmid).nameOf(dpid)  );
+	    dwin->setDeleteOnClose( true );
+	    dwin->show();
+	    menu->setIsHandled( true );
 	}
 	else
 	{
+	    const bool isselmodeon = visserv->isSelectionModeOn();
+	    if ( !isselmodeon )
+	    {
+		if ( mnuid==amplspectrumitem_.id )
+		{
+		    uiAmplSpectrum* asd = new uiAmplSpectrum(
+					    applMgr()->applService().parent() );
+		    asd->setDeleteOnClose( true );
+		    asd->setDataPackID( dpid, dmid );
+		    asd->show();
+		}
+		else
+		{
+		    
+		}
+	    }
 
+	    menu->setIsHandled( true );
 	}
-	
-	menu->setIsHandled( true );
-    }
+}
     else if ( mnuid==view2dwvaitem_.id || mnuid==view2dvditem_.id )
     {
 	ODMainWin()->viewer2DMgr().displayIn2DViewer( displayID(), attribNr(),
