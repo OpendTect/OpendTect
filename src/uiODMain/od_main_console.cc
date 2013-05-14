7c9a3f1d80aff402516d2ed86ce7f63cb3d39fa9
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2013-05-14 10:34:23 +0000

    Added od_main_console.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@29779 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/uiODMain/od_main_console.cc
@@ -0,0 +1,12 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        A.H.Bril
+ Date:          Mar 2002
+ ID:		$Id$
+________________________________________________________________________
+
+-*/
+
+#include "od_main.cc"
