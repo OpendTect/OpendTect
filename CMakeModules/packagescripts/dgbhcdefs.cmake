098549db037dba70186825920fe4a54dedcc64e0
Nageswara Rao <nageswara.rao@dgbes.com>
2013-05-22 03:51:27 +0000

    'od_hc2steer' executable does not exist in dgb4.6, so it is removed from definition.
    Summary: od_hc2steer does not exist.
    
    Test Plan: Fixed erroor 'od_hc2steer not found' while preparing packages.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    CC: nanne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D880
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.6@29944 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/packagescripts/dgbhcdefs.cmake
+++ CMakeModules/packagescripts/dgbhcdefs.cmake
@@ -4,8 +4,7 @@
 # Date:         August 2012
 #RCS:           $Id$
 
-SET( LIBLIST HorizonCube uiHorizonCube uiHorizonCubeSlider )
-SET( EXECLIST od_convert_chronostrat od_filter_horizoncube od_hc2steer
-	      od_process_horizoncube od_trim_hor_atfaults od_grid_horizoncube
-	      od_process_attrib_hc )
-SET( PACK "dgbhc" )
+set( LIBLIST HorizonCube uiHorizonCube uiHorizonCubeSlider )
+set( EXECLIST od_convert_chronostrat od_filter_horizoncube od_process_horizoncube
+	      od_trim_hor_atfaults od_grid_horizoncube od_process_attrib_hc )
+set( PACK "dgbhc" )
