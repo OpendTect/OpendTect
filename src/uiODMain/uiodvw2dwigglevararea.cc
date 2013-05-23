a598db3276df38ebcf243e821429b191316f22bc
Mahant Mothey <mahant.mothey@dgbes.com>
2013-05-23 09:34:59 +0000

    Port of D920.
    Summary: Same as D920.
    
    Test Plan: Same as D920.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D916
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@29974 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiODMain/uiodvw2dwigglevararea.cc
+++ src/uiODMain/uiodvw2dwigglevararea.cc
@@ -20,6 +20,9 @@ ________________________________________________________________________
 #include "uiodviewer2d.h"
 #include "uiodviewer2dmgr.h"
 #include "uitaskrunner.h"
+#include "filepath.h"
+#include "ioobj.h"
+#include "keystrs.h"
 
 #include "attribdatacubes.h"
 #include "attribdatapack.h"
@@ -303,6 +306,21 @@ bool uiODVW2DWiggleVarAreaTreeItem::handleSelMenu( int mnuid )
     if ( newid == DataPack::cNoID() ) return true;
 
     viewer2D()->setSelSpec( &selas, true );
+
+    PtrMan<IOObj> ioobj = attrserv->getIOObj( selas );
+    if ( ioobj )
+    {
+	FilePath fp( ioobj->fullUserExpr(true) );
+	fp.setExtension( "par" );
+	IOPar iop;
+	if ( iop.read(fp.fullPath(),sKey::Pars) && !iop.isEmpty() )
+	{
+	    ColTab::MapperSetup mapper;
+	    mapper.usePar( iop );
+	    vwr.appearance().ddpars_.wva_.mappersetup_ = mapper;
+	}
+    }
+
     viewer2D()->setUpView( newid, true );
 
     return false;
