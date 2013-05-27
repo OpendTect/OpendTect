b9fab820da9359345b10171f2d5ddb51978c98c3
Aneesh Tiwari <aneesh.tiwari@dgbes.com>
2013-05-27 10:17:31 +0000

    Corrected the names of the libraries used for breakpad
    Summary:
    Corrected the names of the libraries in ODBreakPadUtils.cmake.
    Now cmake gets the libraries and we don't need to specify manually.
    Also changed indentation for better readability.
    
    Test Plan: Tested by running cmake. It gets all the required libraries when we specify the BREAKPAD_DIR is correctly specified.
    
    Reviewers: nageswara
    
    Reviewed By: nageswara
    
    CC: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D941
    
    git-svn-id: https://svn.opendtect.org/od/trunk@30026 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODBreakPadUtils.cmake
+++ CMakeModules/ODBreakPadUtils.cmake
@@ -17,9 +17,27 @@ macro(OD_SETUP_BREAKPAD)
 
 	if(OD_ENABLE_BREAKPAD)
 		if( WIN32 )
-			find_library( BREAKPADLIB NAMES BreakPad PATHS ${BREAKPAD_DIR}/client/windows/Debug/lib REQUIRED )
-			find_library( BREAKPADCOMMONLIB NAMES BreakPadCommon PATHS ${BREAKPAD_DIR}/client/windows/Debug/lib REQUIRED )
-			find_library( BREAKPADCLIENTLIB NAMES BreakPadClient PATHS ${BREAKPAD_DIR}/client/windows/Debug/lib REQUIRED )
+			find_library( BREAKPADLIB_DEBUG NAMES exception_handler 
+				      PATHS ${BREAKPAD_DIR}/client/windows/Debug/lib 
+				      REQUIRED )
+			find_library( BREAKPADLIB_RELEASE NAMES exception_handler 
+				      PATHS ${BREAKPAD_DIR}/client/windows/Release/lib 
+				      REQUIRED )
+			find_library( BREAKPADCOMMONLIB_DEBUG NAMES common 
+				      PATHS ${BREAKPAD_DIR}/client/windows/Debug/lib 
+				      REQUIRED )
+			find_library( BREAKPADCOMMONLIB_RELEASE NAMES common 
+				      PATHS ${BREAKPAD_DIR}/client/windows/Release/lib 
+				      REQUIRED )
+			find_library( BREAKPADCLIENTLIB_DEBUG NAMES crash_generation_client 
+				      PATHS ${BREAKPAD_DIR}/client/windows/Debug/lib 
+				      REQUIRED )
+			find_library( BREAKPADCLIENTLIB_RELEASE NAMES crash_generation_client 
+				      PATHS ${BREAKPAD_DIR}/client/windows/Release/lib 
+				      REQUIRED )
+			 OD_MERGE_LIBVAR( BREAKPADLIB )
+			 OD_MERGE_LIBVAR( BREAKPADCOMMONLIB )
+			 OD_MERGE_LIBVAR( BREAKPADCLIENTLIB )
 		endif( WIN32 )
 		
 		set(OD_BREAKPADLIBS ${BREAKPADLIB} ${BREAKPADCOMMONLIB} ${BREAKPADCLIENTLIB} )
