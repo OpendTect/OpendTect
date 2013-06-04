e9bfedf9966c82691feac73a6452ede3162b2f73
Nanne Hemstra <nanne.hemstra@dgbes.com>
2013-06-04 21:35:31 +0000

    Improved error message when selecting invalid horizon (od4.6)
    Summary: Bug 0001938
    
    Test Plan: Selected valid and invalid horizons. Messages are better now.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D1078
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.6@30172 a985855d-d384-4b45-ae33-31a410998c3c

--- src/EarthModel/emioobjinfo.cc
+++ src/EarthModel/emioobjinfo.cc
@@ -209,8 +209,11 @@ bool IOObjInfo::getTrcRanges( TypeSet< StepInterval<int> >& trcranges ) const
 
 const char* IOObjInfo::getSurfaceData( SurfaceIOData& sd ) const
 {
-    if ( !isOK() )
+    if ( !ioobj_ )
 	return "Cannot find surface in object database";
+
+    if ( !ioobj_->implExists(true) )
+	return "Cannot find file on disk";
 
     if ( !isSurface() )
     {
