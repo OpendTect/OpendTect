03b62dd5b148c487eb1dba51a8826135ebce7a61
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2013-06-21 10:49:42 +0000

    Fixed deadlock in notifyIfNotNotified.
    git-svn-id: https://svn.opendtect.org/od/trunk@30412 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Basic/callback.cc
+++ src/Basic/callback.cc
@@ -267,9 +267,7 @@ void NotifierAccess::notify( const CallBack& cb, bool first )
 void NotifierAccess::notifyIfNotNotified( const CallBack& cb )
 {
     Threads::Locker lckr( cbs_.lock_ );
-
-    if ( !cbs_.isPresent(cb) )
-	notify(cb);
+    cbs_.addIfNew( cb );
 }
 
 
