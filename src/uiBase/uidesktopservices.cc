7ea579f8209c89e5ea8dac051b0747c06dc8d5b8
Raman Singh <raman.singh@dgbes.com>
2013-07-04 03:54:08 +0000

    od4.4: Fix for running external apps from OD.
    Summary: See D1387.
    
    Test Plan: See D1387.
    
    Reviewers: nageswara
    
    Reviewed By: nageswara
    
    Differential Revision: https://phabricator.dgbes.com:8443/D1398
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.4@30543 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiBase/uidesktopservices.cc
+++ src/uiBase/uidesktopservices.cc
@@ -13,11 +13,24 @@ static const char* rcsID = "$Id$";
 
 #include "bufstring.h"
 #include "debugmasks.h"
+#include "envvars.h"
 #include "uimsg.h"
 
 #include <QDesktopServices>
 #include <QUrl>
 
+
+static const char* sKeySysLibPath = "OD_SYSTEM_LIBRARY_PATH";
+static const char* sKeyLDLibPath =
+#ifdef __lux__
+"LD_LIBRARY_PATH";
+#else
+# ifdef __mac__
+"DYLD_LIBRARY_PATH";
+# else
+0;
+# endif
+#endif
 
 bool uiDesktopServices::openUrl( const char* url )
 {
@@ -44,5 +57,15 @@ bool uiDesktopServices::openUrl( const char* url )
 	msg += qurl.toString().toAscii().data();
 	DBG::message( msg );
     }
-    return QDesktopServices::openUrl( qurl );
+
+    const BufferString syslibpath( GetEnvVar(sKeySysLibPath) );
+    const BufferString odenvlibpath( sKeyLDLibPath ? GetEnvVar(sKeyLDLibPath)
+	    					   : 0 );
+    if ( odenvlibpath.isEmpty() )
+	return QDesktopServices::openUrl( qurl );
+
+    SetEnvVar( sKeyLDLibPath, syslibpath.buf() );
+    const bool res = QDesktopServices::openUrl( qurl );
+    SetEnvVar( sKeyLDLibPath, odenvlibpath.buf() );
+    return res;
 }
