4b39a0ef784ab7b5d6dbbed844a39623f304aefe
Ding Zheng <ding.zheng@dgbes.com>
2013-07-18 07:28:19 +0000

    Added test for string precision in ascii
    Test Plan: Compiles and Tests
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D1266
    
    git-svn-id: https://svn.opendtect.org/od/trunk@30812 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Basic/tests/string.cc
+++ src/Basic/tests/string.cc
@@ -60,6 +60,47 @@ bool testBytes2String( bool quiet )
     return true;
 }
 
+#define mTestStringPrecision( val, strval, isfloat )\
+{\
+    char buff[255];\
+    BufferString valfmtstr;\
+    BufferString testname;\
+    BufferString valbfstr;\
+    if ( isfloat )\
+    {\
+	testname = "Float precision ";\
+	testname += val;\
+	valfmtstr = getStringFromFloat( val, buff, 7 );\
+	valbfstr =  Conv::to<const char*>((float)val);\
+    }\
+    else\
+    {\
+	testname = "Double precision ";\
+	testname += val;\
+	valbfstr =  Conv::to<const char*>((double)val);\
+	valfmtstr = getStringFromDouble( val, buff, 15 );\
+    }\
+    mRunTest( testname.buf(), valfmtstr && valbfstr && (valfmtstr ==valbfstr) &&\
+    ( strcmp( valbfstr.buf(),strval) == 0 ) );\
+}
+
+bool testStringPrecisionInAscII( bool quiet )
+{
+    mTestStringPrecision( 0.0001, "0.0001", true );
+    mTestStringPrecision( 12.3456785, "12.34568", true );
+    mTestStringPrecision( 123.4567852, "123.4568", true );
+    mTestStringPrecision( 1234.567352, "1234.567", true );
+    mTestStringPrecision( 12345.67352, "12345.67", true );
+    mTestStringPrecision( 123456.7552, "123456.8", true );
+    mTestStringPrecision( 0.10010001002030415,"0.1001000100203", false );
+    mTestStringPrecision( 12.32568023782987356,"12.3256802378299", false );
+    mTestStringPrecision( 123.2568023782987353,"123.256802378299", false );
+    mTestStringPrecision( 1232.568023782927357,"1232.56802378293", false );
+    mTestStringPrecision( 12325.68023782981352,"12325.6802378298", false );
+    mTestStringPrecision( 123256.8023782984888,"123256.802378298", false );
+    return true;
+}
+
 
 int main( int narg, char** argv )
 {
@@ -67,8 +108,10 @@ int main( int narg, char** argv )
     
     const bool quiet = CommandLineParser().hasKey( sKey::Quiet() );
 
-
     if ( !testBytes2String(quiet) )
+	ExitProgram( 1 );
+
+    if ( !testStringPrecisionInAscII(quiet) )
 	ExitProgram( 1 );
 
     ExitProgram( 0 );
