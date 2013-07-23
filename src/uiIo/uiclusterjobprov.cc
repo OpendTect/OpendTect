05733dbcaf366eab97a6017a4a1f019d7e9c4566
Nanne Hemstra <nanne.hemstra@dgbes.com>
2013-07-23 02:53:41 +0000

    Fixed missing double quote to end string.
    git-svn-id: https://svn.opendtect.org/od/trunk@30860 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiIo/uiclusterjobprov.cc
+++ src/uiIo/uiclusterjobprov.cc
@@ -108,7 +108,7 @@ static bool writeScriptFile( const char* scrfnm, const char* prognm,
     *sd.ostrm << fp.fullPath().buf() << std::endl;
     *sd.ostrm << "set exitcode = $status" << std::endl;
     *sd.ostrm << "echo \""; *sd.ostrm << desc;
-    *sd.ostrm << " finished with code ${exitcode} >>\\" << std::endl;
+    *sd.ostrm << " finished with code ${exitcode} >> \"" << std::endl;
     fp.setExtension( ".log" );
     *sd.ostrm << fp.fullPath().buf() << std::endl;
     *sd.ostrm << "exit ${exitcode}" << std::endl;
