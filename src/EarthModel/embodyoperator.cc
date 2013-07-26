45dee4141f960334012076aa5ef3fcce629ad81c
Yuancheng Liu <yuancheng.liu@dgbes.com>
2013-07-26 22:26:31 +0000

    od4.6: Fixed an error when create an implicit body
    Summary: One case has wrong sign
    
    Test Plan: Created body from polygonbody in F3 and isopach display looks ok
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D1310
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.6@30933 a985855d-d384-4b45-ae33-31a410998c3c

--- src/EarthModel/embodyoperator.cc
+++ src/EarthModel/embodyoperator.cc
@@ -109,17 +109,13 @@ float getVal( char p0, char p1, float v0, float v1 ) const
     {
 	if ( p1==mInsideVal )
 	{
-	    if ( action_==BodyOperator::Union )
+	    if ( action_==BodyOperator::Minus )
 	    {
-		res = useval ? (mMAX(v0,v1)) : mInsideVal;
-	    }
-	    else if ( action_==BodyOperator::IntSect )
-	    {
-		res = useval ? (mMAX(v0,v1)) : mInsideVal;
+		res = useval ? b1_.threshold_-v1 : mOutsideVal;
 	    }
 	    else
 	    {
-		res = useval ? -v1 : mOutsideVal;
+		res = useval ? (mMAX(v0,v1)) : mInsideVal;
 	    }
 	}
 	else if ( p1==mOnBodyVal )
@@ -134,22 +130,18 @@ float getVal( char p0, char p1, float v0, float v1 ) const
 	    }
 	    else
 	    {
-		res = useval ? 0.01f : mOutsideVal;
+		res = useval ? b0_.threshold_+0.01f : mOutsideVal;
 	    }
 	}
 	else
 	{
-	    if ( action_==BodyOperator::Union )
-	    {
-		res = useval ? v0 : mInsideVal;
-	    }
-	    else if ( action_==BodyOperator::IntSect )
+	    if ( action_==BodyOperator::IntSect )
 	    {
 		res = useval ? v1 : mOutsideVal;
 	    }
 	    else
 	    {
-		res = useval ? v0 : mOutsideVal;
+		res = useval ? v0 : mInsideVal;
 	    }
 	}
     }
@@ -167,31 +159,16 @@ float getVal( char p0, char p1, float v0, float v1 ) const
 	    }
 	    else
 	    {
-		res = useval ? -v1 : mOutsideVal;
+		res = useval ? b1_.threshold_-v1 : mOutsideVal;
 	    }
 	}
 	else if ( p1==mOnBodyVal )
 	{
-	    if ( action_==BodyOperator::Union )
-	    {
-		res = mOnBodyVal;
-	    }
-	    else if ( action_==BodyOperator::IntSect )
-	    {
-		res = mOnBodyVal;
-	    }
-	    else
-	    {
-		res = useval ? 0.01f : mOutsideVal;
-	    }
+	    res = mOnBodyVal;
 	}
 	else
 	{
-	    if ( action_==BodyOperator::Union )
-	    {
-		res = mOnBodyVal;
-	    }
-	    else if ( action_==BodyOperator::IntSect )
+	    if ( action_==BodyOperator::IntSect )
 	    {
 		res = useval ? v1 : mOutsideVal;
 	    }
@@ -209,10 +186,6 @@ float getVal( char p0, char p1, float v0, float v1 ) const
 	    {
 		res = useval ? v1 : mInsideVal;
 	    }
-	    else if ( action_==BodyOperator::IntSect )
-	    {
-		res = useval ? v0 : mOutsideVal;
-	    }
 	    else
 	    {
 		res = useval ? v0 : mOutsideVal;
@@ -224,10 +197,6 @@ float getVal( char p0, char p1, float v0, float v1 ) const
 	    {
 		res = mOnBodyVal;
 	    }
-	    else if ( action_==BodyOperator::IntSect )
-	    {
-		res = useval ? v0 : mOutsideVal;
-	    }
 	    else
 	    {
 		res = useval ? v0 : mOutsideVal;
@@ -235,18 +204,7 @@ float getVal( char p0, char p1, float v0, float v1 ) const
 	}
 	else
 	{
-	    if ( action_==BodyOperator::Union )
-	    {
-		res = useval ? (mMIN(v0,v1)) : mOutsideVal;
-	    }
-	    else if ( action_==BodyOperator::IntSect )
-	    {
-		res = useval ? (mMIN(v0,v1)) : mOutsideVal;
-	    }
-	    else
-	    {
-		res = useval ? v0 : mOutsideVal;
-	    }
+	    res = useval ? (mMIN(v0,v1)) : mOutsideVal;
 	}
     }
 
