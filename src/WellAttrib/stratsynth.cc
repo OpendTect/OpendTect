bfaf31ecaf49ddedffcc9b101870e4c9378a2fb7
Satyaki Maitra <satyaki.maitra@dgbes.com>
2013-08-29 06:44:40 +0000

    [od] Gather management reshuffled
    Summary: gather management shifted from GatherSetDataPack to PreStackSyntheticData. As the gathers are passed as const ObjectSet<Gather>& in the constructor of GatherSetDataPack & it was deepErased in destructor
    
    Test Plan: Tested in Tst_Prof
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D1757
    
    git-svn-id: https://svn.opendtect.org/od/trunk@31292 a985855d-d384-4b45-ae33-31a410998c3c

--- src/WellAttrib/stratsynth.cc
+++ src/WellAttrib/stratsynth.cc
@@ -278,10 +278,6 @@ SyntheticData* StratSynth::addDefaultSynthetic()
     genparams_.synthtype_ = SynthGenParams::ZeroOffset;
     genparams_.createName( genparams_.name_ );
     SyntheticData* sd = addSynthetic();
-
-    mDynamicCastGet(PostStackSyntheticData*,psd,sd);
-    if ( psd )  generateOtherQuantities( *psd, layMod() );
-
     return sd;
 }
 
@@ -643,8 +639,12 @@ SyntheticData* StratSynth::generateSD( const SynthGenParams& synthgenpar )
     for ( int idx=0; idx<aimodels_.size(); idx++ )
 	maxsz = mMAX( aimodels_[idx].size(), maxsz );
 
-    if ( maxsz == 0 || maxsz == 1 )
+    if ( maxsz == 0 )
 	return 0;
+
+    if ( maxsz == 1 )
+	mErrRet( "Model has only one layer, please add another layer.", 
+		return 0; );
 
     if ( !TaskRunner::execute( tr_, synthgen) )
 	return 0;
@@ -1353,13 +1353,28 @@ PreStackSyntheticData::PreStackSyntheticData( const SynthGenParams& sgp,
     DataPackMgr::ID pmid = DataPackMgr::CubeID();
     DPM( pmid ).add( &dp );
     datapackid_ = DataPack::FullID( pmid, dp.id());
+    ObjectSet<PreStack::Gather>& gathers = dp.getGathers();
+    for ( int idx=0; idx<gathers.size(); idx++ )
+    {
+	DPM(DataPackMgr::FlatID()).obtain( gathers[idx]->id() );
+	gathers[idx]->setName( name() );
+    }
 }
 
 
 PreStackSyntheticData::~PreStackSyntheticData()
 {
+    mDynamicCastGet(PreStack::GatherSetDataPack&,gsetdp,datapack_)
+    ObjectSet<PreStack::Gather>& gathers = gsetdp.getGathers();
+    for ( int idx=0; idx<gathers.size(); idx++ )
+	DPM(DataPackMgr::FlatID()).release( gathers[idx] );
     if ( angledp_ )
+    {
+	ObjectSet<PreStack::Gather>& anglegathers = angledp_->getGathers();
+	for ( int idx=0; idx<anglegathers.size(); idx++ )
+	    DPM(DataPackMgr::FlatID()).release( anglegathers[idx] );
 	DPM( DataPackMgr::CubeID() ).release( angledp_->id() );
+    }
 }
 
 
@@ -1415,7 +1430,11 @@ void PreStackSyntheticData::createAngleData( const ObjectSet<RayTracer1D>& rts,
 	TypeSet<float> azimuths;
 	gather->getAzimuths( azimuths );
 	anglegather->setAzimuths( azimuths );
+	BufferString angledpnm( name(), "(Angle Data)" );
+	anglegather->setName( angledpnm );
+	anglegather->setBinID( gather->getBinID() );
 	anglegathers += anglegather;
+	DPM(DataPackMgr::FlatID()).addAndObtain( anglegather );
     }
 
     angledp_ = new PreStack::GatherSetDataPack( name(), anglegathers );
