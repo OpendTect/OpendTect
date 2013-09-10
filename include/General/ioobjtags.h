8b11b10678f19159546b629b09387e3a952705a3
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2013-09-10 06:38:25 +0000

    Renamed from velocitytags.h/.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@31473 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/ioobjtags.h
@@ -0,0 +1,67 @@
+#ifndef velocitytag_h
+#define velocitytag_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	K. Tingdahl
+ Date:		Sep 2013
+ RCS:		$Id: ibmformat.h 28906 2013-03-20 08:26:48Z mahant.mothey@dgbes.com $
+________________________________________________________________________
+
+-*/
+
+#include "generalmod.h"
+#include "commondefs.h"
+
+class IOObj;
+class VelocityDesc;
+class MultiID;
+class BufferString;
+
+/*! Writes velocity information to an IOObj. */
+mGlobal(General) bool SetVelocityTag(IOObj&,const VelocityDesc&);
+
+
+/*! Removes velocity information from an IOObj. */
+mGlobal(General) bool RemoveVelocityTag(IOObj&);
+    
+
+/*! Reads velocity information from an IOObj. */
+mGlobal(General) bool GetVelocityTag(const IOObj&,VelocityDesc&);
+
+
+/*! Writes velocity volume information to an IOObj. */
+mGlobal(General) bool SetVelocityVolumeTag(IOObj&,const MultiID&);
+
+
+/*! Removes velocity volume information from an IOObj. */
+mGlobal(General) bool RemoveVelocityVolumeTag(IOObj&);
+    
+
+/*! Reads velocity volume information from an IOObj. */
+mGlobal(General) bool GetVelocityVolumeTag(const IOObj&,MultiID&);
+
+
+/*! Writes zdomain information to an IOObj. */
+mGlobal(General) bool SetZDomainTag(IOObj&,const char* zdomain);
+
+
+/*! Writes depth information to an IOObj. */
+mGlobal(General) bool SetDepthTag(IOObj&,const MultiID* velocity=0);
+
+
+/*! Writes time information to an IOObj. */
+mGlobal(General) bool SetTimeTag(IOObj&,const MultiID* velocity=0);
+
+
+/*! Reads zdomain information from an IOObj. */
+mGlobal(General) bool GetZDomainTag(const IOObj&,BufferString&);
+
+
+/*! Removes zdomain information from an IOObj. */
+mGlobal(General) bool RemoveZDomainTag(IOObj&);
+
+#endif
+
---
+++ src/General/ioobjtags.cc
@@ -0,0 +1,100 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : A.H. Bril
+ * DATE     : Sep 2013
+-*/
+
+static const char* rcsID mUsedVar = "$Id: ibmformat.cc 30517 2013-07-02 13:07:14Z bert.bril@dgbes.com $";
+
+#include "ioobjtags.h"
+
+#include "veldesc.h"
+#include "ioobj.h"
+#include "ioman.h"
+#include "zdomain.h"
+
+
+
+bool SetVelocityTag( IOObj& ioobj, const VelocityDesc& desc )
+{
+    desc.fillPar( ioobj.pars() );
+    
+    return IOM().commitChanges( ioobj );
+}
+
+
+bool GetVelocityTag( const IOObj& ioobj, VelocityDesc& desc )
+{
+    return desc.usePar( ioobj.pars() );
+}
+
+
+bool RemoveVelocityTag( IOObj& ioobj )
+{
+    VelocityDesc::removePars( ioobj.pars() );
+    return IOM().commitChanges( ioobj );
+}
+
+
+bool SetVelocityVolumeTag( IOObj& ioobj, const MultiID& velvol )
+{
+    ioobj.pars().set( VelocityDesc::sKeyVelocityVolume(), velvol );
+    return IOM().commitChanges( ioobj );
+}
+
+
+bool GetVelocityVolumeTag( const IOObj& ioobj, MultiID& velvol )
+{
+    return ioobj.pars().get( VelocityDesc::sKeyVelocityVolume(), velvol );
+}
+
+
+bool RemoveVelocityVolumeTag( IOObj& ioobj )
+{
+    if ( !ioobj.pars().hasKey( VelocityDesc::sKeyVelocityVolume() ) )
+	return true;
+
+    ioobj.pars().remove( VelocityDesc::sKeyVelocityVolume() );
+    return IOM().commitChanges( ioobj );
+}
+
+
+bool SetZDomainTag( IOObj& ioobj, const char* zdomain ) 
+{
+    ioobj.pars().set( ZDomain::sKey(), zdomain );
+    return IOM().commitChanges( ioobj );
+}
+
+
+bool SetDepthTag( IOObj& ioobj, const MultiID* velocity )
+{
+    if ( velocity && !SetVelocityVolumeTag( ioobj, *velocity ) )
+	return false;
+
+    return SetZDomainTag( ioobj, ZDomain::sKeyDepth() );
+}
+
+
+bool SetTimeTag( IOObj& ioobj, const MultiID* velocity )
+{
+    if ( velocity && !SetVelocityVolumeTag( ioobj, *velocity ) )
+	return false;
+
+    return SetZDomainTag( ioobj, ZDomain::sKeyDepth() );
+}
+
+
+bool GetZDomainTag( const IOObj& ioobj, BufferString& res )
+{
+    return ioobj.pars().get( ZDomain::sKey(), res );
+}
+
+
+bool RemoveZDomainTag( IOObj& ioobj, BufferString& res )
+{
+    if ( !ioobj.pars().hasKey( ZDomain::sKey() ) )
+	return true;
+
+    ioobj.pars().remove( ZDomain::sKey() );
+    return IOM().commitChanges( ioobj );
+}
