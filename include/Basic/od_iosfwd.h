2e9c78afb0395548355726f06fd2b28e2f895609
Bert Bril <bert.bril@dgbes.com>
2013-09-23 20:04:24 +0000

    oops forgot to commit
    git-svn-id: https://svn.opendtect.org/od/trunk@31650 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/Basic/od_iosfwd.h
@@ -0,0 +1,24 @@
+#ifndef od_iosfwd_h
+#define od_iosfwd_h
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Bert
+ Date:		Sep 2013
+ RCS:		$Id$
+________________________________________________________________________
+
+-*/
+
+class od_stream;
+class od_istream;
+class od_ostream;
+class od_istrstream;
+class od_ostrstream;
+
+typedef od_uint64	od_stream_Count;
+typedef od_int64	od_stream_Pos;
+
+
+#endif
