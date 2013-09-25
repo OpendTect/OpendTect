4d3fd713b1a4a1d348e5573377e8f746c68b7e53
salil.agarwal@dgbes.com <salil.agarwal@dgbes.com@a985855d-d384-4b45-ae33-31a410998c3c>
2013-09-25 04:48:09 +0000

    od4.6: Bug fix in well track import from ascii.
    Summary:
    Port of D2081
    If one of track or MD is not defined in a file then well is not imported properly. In that case (if one of them is defined)track and MD will be equal.
    Bug fix for id 2163
    
    Test Plan: same as D2081
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D2085
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.6@31672 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Well/wellimpasc.cc
+++ src/Well/wellimpasc.cc
@@ -422,11 +422,15 @@ bool Well::TrackAscIO::getData( Well::Data& wd, bool tosurf ) const
 	    continue;
 
 	c.z = getdValue(2);
-	const float newdah = getfValue( 3 );
+	float newdah = getfValue( 3 );
 	const bool havez = !mIsUdf(c.z);
 	const bool havedah = !mIsUdf(newdah);
 	if ( !havez && !havedah )
 	    continue;
+        else if ( !havez && havedah )
+            c.z = newdah;
+        else if ( havez && !havedah )
+            newdah = mCast( float, c.z );
 
 	if ( wd.track().size() == 0 )
 	{
