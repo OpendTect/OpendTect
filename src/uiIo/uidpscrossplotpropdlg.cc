a04922d9990b4b0147d03a2d42d11979c8964e2f
Mahant Mothey <mahant.mothey@dgbes.com>
2013-09-26 07:01:50 +0000

    Increased no. of points.
    git-svn-id: https://svn.opendtect.org/od/trunk@31694 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiIo/uidpscrossplotpropdlg.cc
+++ src/uiIo/uidpscrossplotpropdlg.cc
@@ -540,7 +540,7 @@ void computePts( bool isy2 )
 	    vert.axis_->range().step );
     MathExpression* mathobj = isy2 ? mathobj1_ : mathobj_;
     const bool& linedrawn = isy2 ? line2drawn_ : line1drawn_;
-    const int nrpts = linedrawn ? 2 : 1000;
+    const int nrpts = linedrawn ? 70 : 1000;
     const Interval<float> xrge = horz.rg_;
     const float step = fabs( (xrge.stop-xrge.start)/(nrpts-1) );
     TypeSet<uiWorldPoint> pts; pts.setCapacity( nrpts );
