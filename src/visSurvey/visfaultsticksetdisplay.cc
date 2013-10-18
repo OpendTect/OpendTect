8ef82bfde558865083dc85ec838991376e096731
Raman Singh <raman.singh@dgbes.com>
2013-10-18 07:24:34 +0000

    od4.6: Fixed potential crash in faultstickset display.
    Summary: See D2245.
    
    Test Plan: See D2245.
    
    Reviewers: mahant
    
    Reviewed By: mahant
    
    Differential Revision: https://phabricator.dgbes.com:8443/D2262
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.6@32016 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visSurvey/visfaultsticksetdisplay.cc
+++ src/visSurvey/visfaultsticksetdisplay.cc
@@ -96,16 +96,14 @@ FaultStickSetDisplay::FaultStickSetDisplay()
 
 FaultStickSetDisplay::~FaultStickSetDisplay()
 {
+    setSceneEventCatcher( 0 );
+    showManipulator( false );
     if ( scene_ && scene_->getPolySelection() &&
 	 scene_->getPolySelection()->polygonFinished() )
     {
 	const CallBack cb = mCB(this, FaultStickSetDisplay, polygonFinishedCB);
 	scene_->getPolySelection()->polygonFinished()->remove( cb );
     }
-
-    setSceneEventCatcher( 0 );
-    showManipulator( false );
-
 
     if ( viseditor_ )
 	viseditor_->unRef();
