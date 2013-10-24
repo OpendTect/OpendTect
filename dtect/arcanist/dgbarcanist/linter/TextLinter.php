b8e7003818059305cf6c91e2a3d125f549dfdd2e
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2013-10-24 06:18:21 +0000

    Added keyword search and tab-replacement.
    git-svn-id: https://svn.opendtect.org/od/trunk@32110 a985855d-d384-4b45-ae33-31a410998c3c

--- dtect/arcanist/dgbarcanist/linter/TextLinter.php
+++ dtect/arcanist/dgbarcanist/linter/TextLinter.php
@@ -7,15 +7,18 @@
  */
 final class TextLinter extends ArcanistLinter {
 
-  const LINT_DOS_NEWLINE            = 1;
-  const LINT_TAB_LITERAL            = 2;
-  const LINT_LINE_WRAP              = 3;
-  const LINT_EOF_NEWLINE            = 4;
-  const LINT_BAD_CHARSET            = 5;
-  const LINT_TRAILING_WHITESPACE    = 6;
-  const LINT_NO_COMMIT              = 7;
+  const LINT_DOS_NEWLINE		= 1;
+  const LINT_TAB_LITERAL		= 2;
+  const LINT_LINE_WRAP			= 3;
+  const LINT_EOF_NEWLINE		= 4;
+  const LINT_BAD_CHARSET		= 5;
+  const LINT_TRAILING_WHITESPACE	= 6;
+  const LINT_NO_COMMIT			= 7;
+  const LINT_SPACE_ALIGNMENT		= 8;
+  const LINT_FORBIDDEN_WORD		= 9;
 
   private $maxLineLength = 80;
+  private $nrautofixes = 0;
 
   public function getLinterPriority() {
     return 0.5;
@@ -35,35 +38,55 @@ final class TextLinter extends ArcanistLinter {
   }
 
   public function getLintSeverityMap() {
+    if ( $this->nrautofixes>1 )
+      return array(
+	self::LINT_TRAILING_WHITESPACE =>ArcanistLintSeverity::SEVERITY_AUTOFIX,
+      );
+
     return array(
       self::LINT_TRAILING_WHITESPACE => ArcanistLintSeverity::SEVERITY_AUTOFIX,
+      self::LINT_SPACE_ALIGNMENT     => ArcanistLintSeverity::SEVERITY_AUTOFIX,
     );
   }
 
   public function getLintNameMap() {
     return array(
-      self::LINT_DOS_NEWLINE          => pht('DOS Newlines'),
-      self::LINT_TAB_LITERAL          => pht('Tab Literal'),
-      self::LINT_LINE_WRAP            => pht('Line Too Long'),
-      self::LINT_EOF_NEWLINE          => pht('File Does Not End in Newline'),
-      self::LINT_BAD_CHARSET          => pht('Bad Charset'),
-      self::LINT_TRAILING_WHITESPACE  => pht('Trailing Whitespace'),
-      self::LINT_NO_COMMIT            => pht('Explicit %s', '@no'.'commit'),
+      self::LINT_DOS_NEWLINE		=> pht('DOS Newlines'),
+      self::LINT_TAB_LITERAL		=> pht('Tab Literal'),
+      self::LINT_LINE_WRAP		=> pht('Line Too Long'),
+      self::LINT_EOF_NEWLINE		=> pht('File Does Not End in Newline'),
+      self::LINT_BAD_CHARSET		=> pht('Bad Charset'),
+      self::LINT_TRAILING_WHITESPACE	=> pht('Trailing Whitespace'),
+      self::LINT_NO_COMMIT		=> pht('Explicit %s', '@no'.'commit'),
+      self::LINT_SPACE_ALIGNMENT	=> pht('Spaces used instead of tabs'),
+      self::LINT_FORBIDDEN_WORD 	=> pht('Forbidden words'),
     );
   }
 
   public function lintPath($path) {
+    $this->nrautofixes = 0;
     if (!strlen($this->getData($path))) {
       // If the file is empty, don't bother; particularly, don't require
       // the user to add a newline.
       return;
     }
 
-    //$this->lintNewlines($path);
-//
-    //if ($this->didStopAllLinters()) {
-      //return;
-    //}
+    $iscmake = strpos( $path, ".cmake" )!==false;
+    $isphp = strpos( $path, ".php" )!==false;
+
+    $this->lintNewlines($path);
+
+    if ($this->didStopAllLinters()) {
+      return;
+    }
+
+    if ( !$isphp ) {
+      $this->lintForbiddenStrings($path);
+
+      if ($this->didStopAllLinters()) {
+	return;
+      }
+    }
 
     $this->lintCharset($path);
 
@@ -74,10 +97,15 @@ final class TextLinter extends ArcanistLinter {
     $this->lintEOFNewline($path);
     $this->lintTrailingWhitespace($path);
 
-    $iscmake = strpos( $path, ".cmake" )!==false;
-
-    if (!$iscmake)
+    if (!$iscmake) {
       $this->lintLineLength($path);
+
+      if ($this->didStopAllLinters()) {
+	return;
+      }
+    }
+
+    $this->lintSpaceAlignment($path);
 
     if ($this->getEngine()->getCommitHookMode()) {
       $this->lintNoCommit($path);
@@ -105,8 +133,8 @@ final class TextLinter extends ArcanistLinter {
       if ( $text[$idx]=="\t" ) {
 	$curpos = strlen($res);
 	$nrtabs = (int) ($curpos/$tab_stop);
-	$stop = ($nrtabs+1)*$tab_stop;
-	$nrspaces = $stop-$curpos;
+	$newsize = ($nrtabs+1)*$tab_stop;
+	$nrspaces = $newsize-$curpos;
 	for ( $idy=$nrspaces-1; $idy>=0; $idy-- ) {
 	  $res = $res." ";
 	}
@@ -129,19 +157,24 @@ final class TextLinter extends ArcanistLinter {
       $expandedline = $this->tab_expand( $line );
       if (strlen($expandedline) > $width) {
 
-	$isrcs = strpos( $expandedline, '$Id' );
+      $isrcs = strpos( $expandedline, '$Id' );
 
-	if ( $isrcs==false ) {
-	    $this->raiseLintAtLine(
-	      $line_idx + 1,
-	      1,
-	      self::LINT_LINE_WRAP,
-	      'This line is '.number_format(strlen($expandedline)).
-	      ' characters long, '.
-	      'but the convention is '.$width.' characters.',
-	      $expandedline);
+      if ( $isrcs==false ) {
+	$this->raiseLintAtLine(
+	  $line_idx + 1,
+	  1,
+	  self::LINT_LINE_WRAP,
+	  'This line is '.number_format(strlen($expandedline)).
+	  ' characters long, '.
+	  'but the convention is '.$width.' characters.',
+	  $expandedline);
+
 	}
       }
+    }
+
+    if ($this->isMessageEnabled(self::LINT_LINE_WRAP)) {
+      $this->stopAllLinters();
     }
   }
 
@@ -154,6 +187,10 @@ final class TextLinter extends ArcanistLinter {
         "Files must end in a newline.",
         '',
         "\n");
+    }
+
+    if ($this->isMessageEnabled(self::LINT_EOF_NEWLINE)) {
+      $this->stopAllLinters();
     }
   }
 
@@ -188,6 +225,96 @@ final class TextLinter extends ArcanistLinter {
     }
   }
 
+  protected function lintForbiddenStrings($path) {
+
+    $bad = array( 'sqrt(' );
+    //$bad = array( 'sqrt', 'std::ostream', 'std::istream' );
+
+    $data = $this->getData( $path );
+    $hasbadword = false;
+
+    foreach ( $bad as $keyword ) {
+      $offset = strpos( $data, $keyword );
+      if ($offset !== false) {
+	$this->raiseLintAtOffset(
+	  $offset,
+	  self::LINT_FORBIDDEN_WORD,
+	  'This file contains the string "'.$keyword.'", which is on the '.
+	  'list of words not allowed in the code' );
+	$hasbadword = false;
+      }
+    }
+
+    if ($this->isMessageEnabled(self::LINT_FORBIDDEN_WORD) ) {
+      $this->stopAllLinters();
+    }
+  }
+
+  protected function tab_collapse( $string, $tabstop ) {
+    $changedline = $string;
+
+    $length = strlen( $changedline );
+    $firstchar = 0;
+    $last = $tabstop-1;
+
+    for ( $idx=$length-1; $idx>=$last; $idx-- ) {
+
+      if ( ($idx+1)%$tabstop )
+	 continue;
+
+      $nrspaces = 0;
+      for ( $idy=0; $idy<$tabstop; $idy++ )
+      {
+	    if ( $changedline[$idx-$idy]==' ' )
+		$nrspaces++;
+	    else
+		break;
+      }
+
+      if ( $nrspaces<2 )
+	    continue;
+
+      $lastchartocopy = $idx-$nrspaces;
+      $copysize = $lastchartocopy + 1;
+
+      $newstring = "";
+
+      if ( $copysize>0 )
+	$newstring .= substr( $changedline, 0, $copysize );
+
+      $newstring .= "\t";
+      $newstring .= substr( $changedline, $idx+1 );
+      $changedline = $newstring;
+    }
+
+    return $changedline;
+  }
+
+
+  protected function lintSpaceAlignment($path) {
+    $lines = explode("\n", $this->getData($path));
+
+    $change = false;
+    foreach ($lines as $line_idx => $line) {
+
+      $expanded = $this->tab_expand($line);
+      $changedline = $this->tab_collapse( $expanded, 8 );
+      if ( strcmp( $line, $changedline ) ) {
+	$this->raiseLintAtLine(
+		$line_idx + 1,
+		1,
+		self::LINT_SPACE_ALIGNMENT,
+		'This line is uses spaces in place for tabs',
+		$line,
+		$changedline );
+	$change = true;
+      }
+    }
+
+    if ( $change )
+      $this->nrautofixes++;
+  }
+
   protected function lintTrailingWhitespace($path) {
     $data = $this->getData($path);
 
@@ -213,6 +340,8 @@ final class TextLinter extends ArcanistLinter {
         $string,
         '');
     }
+
+    $this->nrautofixes++;
   }
 
   private function lintNoCommit($path) {
