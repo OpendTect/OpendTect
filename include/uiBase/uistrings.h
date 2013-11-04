687be45e7cdcfc4cb5a548800664843eb8d32cd9
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2013-11-04 08:08:37 +0000

    Added Save and Save as default.
    git-svn-id: https://svn.opendtect.org/od/trunk@32248 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiBase/uistrings.h
+++ include/uiBase/uistrings.h
@@ -7,7 +7,7 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:        A.H. Lammertink
  Date:          25/08/1999
- RCS:           $Id: uiobj.h 32104 2013-10-23 20:11:53Z kristofer.tingdahl@dgbes.com $
+ RCS:           $Id$
 ________________________________________________________________________
 
 -*/
@@ -17,7 +17,11 @@ ________________________________________________________________________
 
 //Common strings used in OD. Should be extended
 
+inline FixedString sSave()		{ return "&Save"; }
 inline FixedString sSaveAs()            { return "Save &as ..."; }
+inline FixedString sSaveAsDefault()     { return "Save as &default ..."; }
+
+
 
 
 #endif
