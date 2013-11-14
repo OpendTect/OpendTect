OpendTect/OpendTect

commit 50bc2866c9f6caf7f16f2dd134108d3ace33f826

Fixed casting warning on mac.
Test Plan: Visually inspected only. Warnings are gone.

Reviewers: bert

Reviewed By: bert

Differential Revision: https://phabricator.dgbes.com:8443/D2372

git-svn-id: https://svn.opendtect.org/od/trunk@32249 a985855d-d384-4b45-ae33-31a410998c3c

src/Algo/linear.cc
@@ -12,9 +12,9 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "math2.h"
 #include "geometry.h"
 
-#define mArrVal(arr) (*(float*)(arr + idx * offs))
+#define mArrVal(arr) (arr[idx * offs])
 
-static void calcLS( LinStats2D& ls, const char* xvals, const char* yvals,
+static void calcLS( LinStats2D& ls, const float* xvals, const float* yvals,
 		    int nrpts, int offs )
 {
     double sumx = 0, sumy = 0;
@@ -76,22 +76,21 @@ static void calcLS( LinStats2D& ls, const char* xvals, const char* yvals,
     else
     {
 	ls.sd.ax = (float) ( Math::Sqrt( sumd2 / ((nrpts-2) * sumx2) ) );
-	ls.sd.a0 = (float) ( Math::Sqrt( (sumx2 * sumd2) / 
-									 (nrpts * (nrpts-2) * sumx2) ) );
+	ls.sd.a0 = (float) ( Math::Sqrt( (sumx2 * sumd2) /
+                                         (nrpts * (nrpts-2) * sumx2) ) );
     }
 }
 
 
 void LinStats2D::use( const float* xvals, const float* yvals, int nrpts )
 {
-    calcLS( *this, (const char*) xvals, (const char*) yvals,
-	    nrpts, sizeof(float) );
+    calcLS( *this, xvals, yvals, nrpts, sizeof(float) );
 }
 
 
 void LinStats2D::use( const Geom::Point2D<float>* vals, int nrpts )
 {
     if ( nrpts < 1 ) return;
-    calcLS( *this, (const char*) &vals[0].x, (const char*) &vals[0].y, nrpts,
+    calcLS( *this, &vals[0].x, &vals[0].y, nrpts,
 	    sizeof(Geom::Point2D<float>) );
 }

commit f4cb56f65a82850eb8945d4171c61570e4ea2bb5

od: fixed wrong offset reading in LinStats2D
Summary:
fixed bug introduced by D2372: Was reading a char, now reading a float, thus the offset should be
divided by sizeof(float)

Test Plan: tested in well tie cross-correlation and cross-plot window

Reviewers: kristofer

Reviewed By: kristofer

CC: bert

Differential Revision: https://phabricator.dgbes.com:8443/D2479

git-svn-id: https://svn.opendtect.org/od/trunk@32426 a985855d-d384-4b45-ae33-31a410998c3c

src/Algo/linear.cc
@@ -12,7 +12,7 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "math2.h"
 #include "geometry.h"
 
-#define mArrVal(arr) (arr[idx * offs])
+#define mArrVal(arr) ( arr[idx * offs / sizeof(float)] )
 
 static void calcLS( LinStats2D& ls, const float* xvals, const float* yvals,
 		    int nrpts, int offs )