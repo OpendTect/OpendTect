cdedbdbc842db40327d7e62409a7526afb884a40
Aneesh Tiwari <aneesh.tiwari@dgbes.com>
2013-11-19 10:03:55 +0000

    [od4.6] Fix for mantis id 2202. LAS import: meter vs feet handling is wrong
    Summary: see D2531
    
    Test Plan: see D2531
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D2532
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.6@32467 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiWell/uiwelldlgs.cc
+++ src/uiWell/uiwelldlgs.cc
@@ -1170,11 +1170,10 @@ void uiLoadLogsDlg::lasSel( CallBacker* )
     Interval<float> usrzrg = lfi.zrg;
     if ( isft )
     {
-	const UnitOfMeasure* zun = UnitOfMeasure::surveyDefDepthUnit();
-	if ( !mIsUdf(lfi.zrg.start) && zun )
-	    usrzrg.start = zun->userValue( lfi.zrg.start );
-	if ( !mIsUdf(lfi.zrg.stop) && zun )
-	    usrzrg.stop = zun->userValue( lfi.zrg.stop );
+	if ( !mIsUdf(lfi.zrg.start) ) 
+	    usrzrg.start *= mToFeetFactorF;
+	if ( !mIsUdf(lfi.zrg.stop) ) 
+	    usrzrg.stop *= mToFeetFactorF;
     }
     intvfld->setValue( usrzrg );
 }
@@ -1191,11 +1190,10 @@ bool uiLoadLogsDlg::acceptOK( CallBacker* )
     const bool zinft = !intvunfld->getBoolValue();
     if ( zinft )
     {
-	const UnitOfMeasure* zun = UnitOfMeasure::surveyDefDepthUnit();
-	if ( !mIsUdf(usrzrg.start) && zun )
-	    usrzrg.start = zun->internalValue( usrzrg.start );
-	if ( !mIsUdf(usrzrg.stop) && zun )
-	    usrzrg.stop = zun->internalValue( usrzrg.stop );
+	if ( !mIsUdf(usrzrg.start) ) 
+	    usrzrg.start *= mFromFeetFactorF;
+	if ( !mIsUdf(usrzrg.stop) ) 
+	    usrzrg.stop *= mFromFeetFactorF;
     }
     lfi.zrg.setFrom( usrzrg );
 
