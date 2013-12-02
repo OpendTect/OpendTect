05208ecf82ec92bf419014be5a3c8e77443e35bb
Raman Singh <raman.singh@dgbes.com>
2013-12-02 06:10:46 +0000

    od4.6: Fix for undefined lines in crossplots.
    Summary: While importing crossplots, an undefined line was always added at the end. Now this check avoids that.
    
    Test Plan: tested by exporting a crossplot and importing it back.
    
    Reviewers: mahant
    
    Reviewed By: mahant
    
    Differential Revision: https://phabricator.dgbes.com:8443/D2610
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.6@32607 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiIo/uiimppvds.cc
+++ src/uiIo/uiimppvds.cc
@@ -147,7 +147,7 @@ bool getLine()
       && fd_.bodyinfos_[0]->selection_.isInFile(1))
     {
 	coord_.x = getdValue( 0 ); coord_.y = getdValue( 1 );
-	if ( formOf(false,0) == 1 )
+	if ( formOf(false,0) == 1 && coord_.isDefined() )
 	    coord_ = SI().transform( BinID(mNINT32(coord_.x),mNINT32(coord_.y)) );
     }
     else
@@ -220,11 +220,14 @@ bool uiImpPVDS::getData( std::istream& strm, Table::FormatDesc& fd,
 
     while ( true )
     {
-	DataPointSet::Pos dpspos( aio.coord_, aio.z_ );
-	if ( is2d_ ) dpspos.nr_ = aio.trcnr_;
-	DataPointSet::DataRow dr( dpspos );
-	dr.data_ = aio.datavals_;
-	dps.addRow( dr );
+	if ( aio.coord_.isDefined() )
+	{
+	    DataPointSet::Pos dpspos( aio.coord_, aio.z_ );
+	    if ( is2d_ ) dpspos.nr_ = aio.trcnr_;
+	    DataPointSet::DataRow dr( dpspos );
+	    dr.data_ = aio.datavals_;
+	    dps.addRow( dr );
+	}
 
 	if ( !aio.getLine() )
 	    break;
