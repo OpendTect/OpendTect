d2d12d945518efb718602f997707605c086d37ba
Nanne Hemstra <nanne.hemstra@dgbes.com>
2013-12-04 17:21:54 +0000

    Prepare for Qt5
    Summary: In Qt5 zlib.h is in another directory
    
    Test Plan: Cmake runs without errors with both Qt5 and Qt4, and Basic compiles and links
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Maniphest Tasks: T96
    
    Differential Revision: https://phabricator.dgbes.com:8443/D2643
    
    git-svn-id: https://svn.opendtect.org/od/trunk@32648 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODZlibUtils.cmake
+++ CMakeModules/ODZlibUtils.cmake
@@ -7,7 +7,12 @@
 
 MACRO( OD_SETUP_ZLIB )
     if ( NOT DEFINED APPLE ) #QT never includes zlib on Mac
-	if ( QT_QTCORE_INCLUDE_DIR )
+	if ( Qt5Core_FOUND )
+	    if ( EXISTS "${QTDIR}/include/QtZlib/zlib.h" )
+		set( ZLIB_INCLUDE_DIR ${QTDIR}/include/QtZlib )
+		set ( ZLIB_LIBRARY ${Qt5Core_LIBRARIES} )
+	    endif()
+	elseif ( QT_QTCORE_INCLUDE_DIR )
 	    if ( EXISTS "${QT_QTCORE_INCLUDE_DIR}/zlib.h" )
 		set ( ZLIB_INCLUDE_DIR ${QT_QTCORE_INCLUDE_DIR} )
 		set ( ZLIB_LIBRARY ${QT_QTCORE_LIBRARY} )
