4aa63923502e946e17c976c9bf5c5ca493debd29
Nanne Hemstra <nanne.hemstra@dgbes.com>
2013-12-10 17:26:42 +0000

    remove <iostream> inclusion
    git-svn-id: https://svn.opendtect.org/od/trunk@32728 a985855d-d384-4b45-ae33-31a410998c3c

--- src/VolumeProcessing/tests/volprocchain.cc
+++ src/VolumeProcessing/tests/volprocchain.cc
@@ -2,7 +2,7 @@
  * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  * AUTHOR   : K. Tingdahl
  * DATE     : July 2012
- * FUNCTION : 
+ * FUNCTION :
 -*/
 
 static const char* rcsID mUsedVar = "$Id$";
@@ -14,26 +14,24 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "commandlineparser.h"
 #include "keystrs.h"
 
-#include <iostream>
-
 
 bool testConnectionSetup( bool quiet )
 {
     RefMan<VolProc::Chain> chain = new VolProc::Chain;
-    
+
     VolProc::VolumeReader* volreader = new VolProc::VolumeReader;
     chain->addStep( volreader );
-    
+
     VolProc::BodyFiller* bodyfiller = new VolProc::BodyFiller;
     chain->addStep( bodyfiller );
-    
+
     chain->addConnection(
 	VolProc::Chain::Connection( volreader->getID(),
-				    volreader->getOutputSlotID( 0 ),
+				    volreader->getOutputSlotID(0),
 				    bodyfiller->getID(),
-				    bodyfiller->getInputSlotID( 0 ) ) );
-    
-    
+				    bodyfiller->getInputSlotID(0) ) );
+
+
     return true;
 }
 
@@ -45,7 +43,7 @@ int main( int narg, char** argv )
 
     CommandLineParser parser;
     const bool quiet = parser.hasKey( sKey::Quiet() );
-    
+
     if ( !testConnectionSetup(quiet) )
 	ExitProgram( 1 );
 
