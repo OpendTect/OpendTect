1a2ed729095972a76a2c18b25dd4727774b15a4e
salil.agarwal@dgbes.com <salil.agarwal@dgbes.com@a985855d-d384-4b45-ae33-31a410998c3c>
2013-12-20 10:02:26 +0000

    od4.6: crash fix while importing a SEGY file and some error occurred.
    Summary:
    bug fix for 2408
    http://bugs.dgbes.com/view.php?id=2408
    see D2790
    
    Test Plan: see D2790
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D2791
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.6@32812 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiSeis/uisegysip.cc
+++ src/uiSeis/uisegysip.cc
@@ -13,6 +13,7 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "uisegyread.h"
 #include "uilabel.h"
 #include "uimsg.h"
+#include "uimain.h"
 #include "segyscanner.h"
 #include "segytr.h"
 #include "segyhdr.h"
@@ -77,20 +78,24 @@ uiDialog* uiSEGYSurvInfoProvider::dialog( uiParent* p )
     return new uiSEGYSIPMgrDlg( this, p, su );
 }
 
-#define mErrRet(s) { uiMSG().error(s); return; }
+#define mShowErr(s) \
+    uiMainWin* mw = uiMSG().setMainWin( uiMain::theMain().topLevel() ); \
+    uiMSG().error(s); \
+    uiMSG().setMainWin(mw);
 
 static void showReport( const SEGY::Scanner& scanner )
 {
     const BufferString fnm( GetProcFileName("SEGY_survey_scan.txt" ) );
     StreamData sd( StreamProvider(fnm).makeOStream() );
     if ( !sd.usable() )
-	mErrRet("Cannot open temporary file in Proc directory")
+    {	mShowErr("Cannot open temporary file in Proc directory"); return; }
     IOPar iop; 
     scanner.getReport( iop );
     if ( !iop.write(*sd.ostrm,IOPar::sKeyDumpPretty()) )
     {
 	sd.close();
-	mErrRet("Cannot write to temporary file in Proc directory")
+	mShowErr("Cannot write to temporary file in Proc directory"); 
+	return;
     }
 
     sd.close();
@@ -115,7 +120,7 @@ bool uiSEGYSurvInfoProvider::getInfo( uiDialog* d, CubeSampling& cs,
 
     const char* errmsg = scanner->posInfoDetector().getSurvInfo(cs.hrg,crd);
     if ( errmsg && *errmsg )
-	{ uiMSG().error( errmsg ); return false; }
+	{ mShowErr( errmsg ); return false; }
 
     cs.zrg = scanner->zRange();
     const SEGYSeisTrcTranslator* tr = scanner->translator();
