30f0c3aa09793c5332172553dddae6453a36a5fb
Bert Bril <bert.bril@dgbes.com>
2014-01-10 22:53:36 +0000

    Broader test range, very informative output ...
    git-svn-id: https://svn.opendtect.org/od/trunk@32986 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Basic/tests/memcopying.cc
+++ src/Basic/tests/memcopying.cc
@@ -1,67 +1,70 @@
 #include "odmemory.h"
 #include "timefun.h"
 #include "testprog.h"
-#include <string.h>
+#include "thread.h"
 
-#define mArrSz 200000000
-#define mNrRuns 5
+#define mTotNrElems 1000000000
+
+#define mPrTime(s) \
+	curms = Time::getMilliSeconds(); \
+	od_cout() << od_tab << s << ": " << curms - prevms << od_endl
 
 
 int main( int argc, char** argv )
 {
     mInitTestProg();
 
-    float* vals = new float [mArrSz];
-    float* copy = new float [mArrSz];
-    for ( int idx=0; idx<mArrSz; idx++ )
-	vals[idx] = idx * 0.001f;
+    od_cout() << "\nTotal number of floats: " << mTotNrElems << od_newline
+	      << "Processors: " << Threads::getNrProcessors() << od_newline
+	      << od_endl;
 
-    int prevms = Time::getMilliSeconds();
-
-    for ( int irun=0; irun<mNrRuns; irun++ )
+    for ( int arrsz=10; arrsz<=1000000000; arrsz*=10 )
     {
-	for ( int idx=0; idx<mArrSz; idx++ )
-	    copy[idx] = vals[idx];
+	float* vals = new float [arrsz];
+	float* copy = new float [arrsz];
+	const int nrruns = mTotNrElems / arrsz;
+	od_cout() << "Arrsz=" << arrsz << " (" << nrruns << " runs)" << od_endl;
+
+	int prevms = Time::getMilliSeconds();
+
+	for ( int irun=0; irun<nrruns; irun++ )
+	{
+	    for ( int idx=0; idx<arrsz; idx++ )
+		copy[idx] = vals[idx];
+	}
+
+	int mPrTime( "SimpleLoop" );
+
+	prevms = Time::getMilliSeconds();
+	for ( int irun=0; irun<nrruns; irun++ )
+	{
+	    const float* curptr = vals;
+	    float* cpptr = copy;
+	    for ( const float* stopptr = vals + arrsz; curptr != stopptr;
+		    curptr++, cpptr++  )
+		*cpptr = *curptr;
+	}
+
+	mPrTime( "PtrLoop" );
+
+	prevms = Time::getMilliSeconds();
+	for ( int irun=0; irun<nrruns; irun++ )
+	    OD::memCopy( copy, vals, arrsz * sizeof(float) );
+
+	mPrTime( "OD::memCopy" );
+
+	prevms = Time::getMilliSeconds();
+	for ( int irun=0; irun<nrruns; irun++ )
+	{
+	    MemCopier<float> mcp( copy, vals, arrsz );
+	    mcp.execute();
+	}
+
+	mPrTime( "MemCopier" );
+
+	delete [] vals;
+	delete [] copy;
     }
-
-#define mPrTime(s) \
-    curms = Time::getMilliSeconds(); \
-    od_cout() << s << ": " << curms - prevms << od_endl
-
-    int mPrTime( "Simple loop" );
-
-    prevms = Time::getMilliSeconds();
-    for ( int irun=0; irun<mNrRuns; irun++ )
-    {
-	const float* curptr = vals;
-	float* cpptr = copy;
-	for ( const float* stopptr = vals + mArrSz; curptr != stopptr;
-		curptr++, cpptr++  )
-	    *cpptr = *curptr;
-    }
-
-    mPrTime( "Ptr loop" );
-
-    prevms = Time::getMilliSeconds();
-    for ( int irun=0; irun<mNrRuns; irun++ )
-	OD::memCopy( copy, vals, mArrSz * sizeof(float) );
-
-    mPrTime( "OD::memCopy" );
-
-    prevms = Time::getMilliSeconds();
-    for ( int irun=0; irun<mNrRuns; irun++ )
-	memcpy( copy, vals, mArrSz * sizeof(float) );
-
-    mPrTime( "memcpy" );
-
-    prevms = Time::getMilliSeconds();
-    for ( int irun=0; irun<mNrRuns; irun++ )
-    {
-	MemCopier<float> mcp( copy, vals, mArrSz );
-	mcp.execute();
-    }
-
-    mPrTime( "MemCopier" );
 
     return ExitProgram( 0 );
 }
