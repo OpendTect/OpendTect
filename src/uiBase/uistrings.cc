a197fef4c36a85ae96721a65a22e18a518ca860a
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2014-02-13 08:20:16 +0000

    Added uistrings.cc
    git-svn-id: https://svn.opendtect.org/od/trunk@33330 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/uiBase/uistrings.cc
@@ -0,0 +1,14 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        K. Tingdahl
+ Date:          Feb 2014
+________________________________________________________________________
+
+-*/
+static const char* rcsID mUsedVar = "$Id$";
+
+#include "uistrings.h"
+
+
