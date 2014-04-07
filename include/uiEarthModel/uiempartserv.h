83bda19c3026d27bc77cc9022be67555d2531ee6
Nanne Hemstra <nanne.hemstra@dgbes.com>
2014-04-07 18:11:45 +0000

    code layout change. no functional changes
    git-svn-id: https://svn.opendtect.org/od/trunk@34058 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiEarthModel/uiempartserv.h
+++ include/uiEarthModel/uiempartserv.h
@@ -27,21 +27,20 @@ class CubeSampling;
 class DataPointSet;
 class HorSampling;
 class SurfaceInfo;
-class uiExportHorizon;
 class uiExportFault;
-class uiImportHorizon;
+class uiExportHorizon;
 class uiImportFault3D;
+class uiImportHorizon;
 class uiVariogramDisplay;
 
+namespace EM { class EMObject; class EMManager; class SurfaceIODataSelection; };
 namespace Pick { class Set; }
 namespace PosInfo { class Line2DData; }
 
 template <class T> class Array2D;
 
-namespace EM { class EMObject; class EMManager; class SurfaceIODataSelection; };
 
 /*!
-\ingroup uiEarthModel
 \brief Earth Model UI Part Server
 */
 
@@ -78,80 +77,80 @@ public:
 
     void		displayEMObject(const MultiID&);
     bool		fillHoles(const EM::ObjectID&,bool);
-    			/*!<return bool is overwrite old horizon or not. */
+			/*!<return bool is overwrite old horizon or not. */
     bool		filterSurface(const EM::ObjectID&);
-    			/*!<return bool is overwrite old horizon or not. */
+			/*!<return bool is overwrite old horizon or not. */
     void		fillPickSet(Pick::Set&,MultiID);
     void		deriveHor3DFrom2D(const EM::ObjectID&);
-    bool		askUserToSave(const EM::ObjectID&,bool withcancel) const;
-    			/*!< If object has changed, user is asked whether
+    bool		askUserToSave(const EM::ObjectID&,bool withcancl) const;
+			/*!< If object has changed, user is asked whether
 			    to save it or not, and if so, the object is saved.
 			    Returns false when save option is cancelled. */
 
     HorSampling		horizon3DDisplayRange() const	{ return selectedrg_; }
     void		setHorizon3DDisplayRange(const HorSampling&);
-    			/*!<Users can change the display range, hor 3D only. */
+			/*!<Users can change the display range, hor 3D only. */
 
     void		selectHorizons(ObjectSet<EM::EMObject>&,bool is2d);
-    			//!<Returned set is reffed and must be unrefed by caller
+			//!<Returned set is reffed and must be unrefed by caller
     void		selectFaults(ObjectSet<EM::EMObject>&,bool is2d);
-    			//!<Returned set is reffed and must be unrefed by caller
+			//!<Returned set is reffed and must be unrefed by caller
     void		selectFaultStickSets(ObjectSet<EM::EMObject>&);
-    			//!<Returned set is reffed and must be unrefed by caller
+			//!<Returned set is reffed and must be unrefed by caller
     void		selectBodies(ObjectSet<EM::EMObject>&);
-    			//!<Returned set is reffed and must be unrefed by caller
+			//!<Returned set is reffed and must be unrefed by caller
     bool		showLoadAuxDataDlg(const EM::ObjectID&);
     int			loadAuxData(const EM::ObjectID&,const char*,
 				    bool removeold=true);
-    			/*!<Loads the specified data into memory and returns
+			/*!<Loads the specified data into memory and returns
 			    its auxdatanr. */
 
     bool		showLoadFaultAuxDataDlg(const EM::ObjectID&);
     bool		storeFaultAuxData(const EM::ObjectID& id,
-	    				  BufferString& auxdatanm,
+					  BufferString& auxdatanm,
 					  const Array2D<float>& data);
     void		manageSurfaces(const char* typ);
     bool		loadSurface(const MultiID&,
-	    			    const EM::SurfaceIODataSelection* s=0);
+				    const EM::SurfaceIODataSelection* s=0);
     void		getSurfaceInfo(ObjectSet<SurfaceInfo>&);
     static void         getAllSurfaceInfo(ObjectSet<SurfaceInfo>&,bool);
     void		getSurfaceDef3D(const TypeSet<EM::ObjectID>&,
-	    			        BinIDValueSet&,
+				        BinIDValueSet&,
 				        const HorSampling&) const;
     void		getSurfaceDef2D(const ObjectSet<MultiID>&,
-	    				const ObjectSet<PosInfo::Line2DData>&,
-	    				BufferStringSet&,const MultiID&,
+					const ObjectSet<PosInfo::Line2DData>&,
+					BufferStringSet&,const MultiID&,
 					TypeSet<Coord>&,
 					TypeSet< Interval<float> >&);
 
     bool		storeObject(const EM::ObjectID&,
-	    			    bool storeas=false) const;
+				    bool storeas=false) const;
     bool		storeObject(const EM::ObjectID&,bool storeas,
 				    MultiID& storagekey,
 				    float shift=0) const;
     bool		storeAuxData(const EM::ObjectID&,
-	    			     BufferString& auxdataname,
-	    			     bool storeas=false) const;
+				     BufferString& auxdataname,
+				     bool storeas=false) const;
     int			setAuxData(const EM::ObjectID&,
-	    			   DataPointSet&,const char* nm,int valnr,
+				   DataPointSet&,const char* nm,int valnr,
 				   float shift);
     bool		getAuxData(const EM::ObjectID&,int auxdatanr,
-	    			   DataPointSet&, float& shift) const;
+				   DataPointSet&, float& shift) const;
     bool		getAllAuxData(const EM::ObjectID&,DataPointSet&,
-	    			      TypeSet<float>* shfs=0,
+				      TypeSet<float>* shfs=0,
 				      const CubeSampling* cs=0) const;
     bool		interpolateAuxData(const EM::ObjectID&,const char* nm,
-	    				   DataPointSet& res);
+					   DataPointSet& res);
     bool		filterAuxData(const EM::ObjectID&,const char* nm,
-	    			      DataPointSet& res);
+				      DataPointSet& res);
     bool		computeVariogramAuxData(const EM::ObjectID&,const char*,
-	    					DataPointSet&);
+						DataPointSet&);
     bool		attr2Geom(const EM::ObjectID&,const char* nm,
-	    			  const DataPointSet&);
+				  const DataPointSet&);
     bool		geom2Attr(const EM::ObjectID&);
 
     const char*		genRandLine(int opt);
-    bool 		dispLineOnCreation()	{ return disponcreation_; }
+    bool		dispLineOnCreation()	{ return disponcreation_; }
 
     void		removeUndo();
 
@@ -171,11 +170,11 @@ public:
 protected:
 
     void		selectSurfaces(ObjectSet<EM::EMObject>&,
-	    			       const char* type);
+				       const char* type);
     bool		loadAuxData(const EM::ObjectID&,const TypeSet<int>&,
 				    bool removeold=true);
     bool		changeAuxData(const EM::ObjectID&,const char* nm,
-	    			      bool interp,DataPointSet& res);
+				      bool interp,DataPointSet& res);
     void		importReadyCB(CallBacker*);
     void		survChangedCB(CallBacker*);
 
@@ -189,7 +188,7 @@ protected:
     uiExportFault*	expfltdlg_;
     uiExportFault*	expfltstickdlg_;
 
-    HorSampling		selectedrg_;    
+    HorSampling		selectedrg_;
     bool		disponcreation_;
 
     ObjectSet<uiVariogramDisplay>	variodlgs_;
