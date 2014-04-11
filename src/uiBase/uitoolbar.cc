OpendTect/OpendTect

commit da371e20441b6c6c7d32250a1616606894df6d2c

Fix for flatviewers not being deleted.
Summary: A number of instances of od are not deleting the flatviewers on closure, leaving the processing queues open, giving an error message on exit.

Test Plan: Error messags on exit are gone.

Reviewers: mahant

Reviewed By: mahant

Differential Revision: https://phabricator.dgbes.com:8443/D1867

git-svn-id: https://svn.opendtect.org/od/trunk@31427 a985855d-d384-4b45-ae33-31a410998c3c

src/uiTools/uicolortable.cc
@@ -277,6 +277,7 @@ void uiColorTable::createFields( uiParent* parnt, bool vert )
 
 uiColorTable::~uiColorTable()
 {
+    delete canvas_;
     delete &coltabseq_;
     delete &mapsetup_;
 }

include/uiTools/uiobjectitemviewwin.h
@@ -58,9 +58,10 @@ public:
     };
 
 			uiObjectItemViewWin(uiParent*,const Setup&);
+			~uiObjectItemViewWin();
 
 
-    int 		nrItems() const { return mainviewer_->nrItems(); } 
+    int 		nrItems() const { return mainviewer_->nrItems(); }
     uiObjectItemView*	mainViewer() 	{ return mainviewer_; }
 
     void 		addObject(uiObject* grp,uiObject* infogrp=0);

src/uiTools/uiobjectitemviewwin.cc
@@ -76,6 +76,13 @@ uiObjectItemViewWin::uiObjectItemViewWin(uiParent* p, const Setup& su)
 }
 
 
+uiObjectItemViewWin::~uiObjectItemViewWin()
+{
+    delete mainviewer_;
+    delete infobar_;
+}
+
+
 void uiObjectItemViewWin::addObject( uiObject* obj, uiObject* infoobj )
 {
     uiObjectItem* itm = new uiObjectItem( obj );

commit d4937e631e1f376979e0a2f8d37c797d4ce75e0d

Bugfix for crash when enabling ToolTipNameGuide of CmdDriver.
Summary:
ToolTipNameGuide crashes are caused by undeleted uiObjects whose Qt part have
already been destructed. In this case after opening and closing the 
"Attribute cross-plotting" dialog and its child dialog "Attribute data".
Objects added to a uiToolbar are neither deleted by the dialog nor by the
toolbar, while the interface addOject(uiObject*) more or less suggests the
latter.

Test Plan:
Enabling ToolTipNameGuide after after opening and closing the cross-plot
dialogs does not yield crash anymore. However, now exiting Opendtect caused
crash in uiColorTableToolBar, because canvas_ is explicitly deleted in the
uiColorTable base class as well, as part of D1867 (fix by Kris for flatviewers
not being deleted). Although I got no crashes when removing it entirely,
I moved it to the ~uiColorTableGroup destructor for safety. Unclear what
workflow precisely caused the problems solved by D1867. Any remarks?

Reviewers: kristofer

Reviewed By: kristofer

Differential Revision: https://phabricator.dgbes.com:8443/D3616

git-svn-id: https://svn.opendtect.org/od/trunk@34128 a985855d-d384-4b45-ae33-31a410998c3c

src/uiBase/uitoolbar.cc
@@ -67,6 +67,7 @@ uiToolBar::uiToolBar( uiParent* parnt, const char* nm, ToolBarArea tba,
 uiToolBar::~uiToolBar()
 {
     removeAllActions();
+    deepErase( addedobjects_ );
 
     delete qtoolbar_;
     delete msgr_;
@@ -251,7 +252,7 @@ void uiToolBar::setToolBarMenuAction( uiAction* action )
 void uiToolBar::clear()
 {
     removeAllActions();
-    addedobjects_.erase();
+    deepErase( addedobjects_ );
 }
 
 

src/uiTools/uicolortable.cc
@@ -281,7 +281,6 @@ void uiColorTable::createFields( uiParent* parnt, bool vert )
 
 uiColorTable::~uiColorTable()
 {
-    delete canvas_;
     delete &coltabseq_;
     delete &mapsetup_;
 }
@@ -578,7 +577,9 @@ void uiColorTableGroup::init( bool vertical, bool nominmax )
 
 
 uiColorTableGroup::~uiColorTableGroup()
-{}
+{
+    delete canvas_;
+}
 
 
 