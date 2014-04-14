de52dbb40c36e3a14b404561a321e36dfaa0016d
Nanne Hemstra <nanne.hemstra@dgbes.com>
2014-04-14 19:03:17 +0000

    Added basemapwell
    git-svn-id: https://svn.opendtect.org/od/trunk@34159 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ plugins/Basemap/basemapwell.cc
@@ -0,0 +1,64 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Nanne Hemstra
+ Date:		January 2014
+________________________________________________________________________
+
+-*/
+
+static const char* rcsID mUsedVar = "$Id$";
+
+
+#include "basemapwell.h"
+
+#include "ioman.h"
+#include "ioobj.h"
+#include "ptrman.h"
+#include "welldata.h"
+#include "wellreader.h"
+
+namespace Basemap
+{
+
+WellObject::WellObject( const MultiID& mid )
+    : BaseMapObject(0)
+    , wellmid_(mid)
+    , wd_(*new Well::Data)
+{
+    PtrMan<IOObj> ioobj = IOM().get( wellmid_ );
+    if ( ioobj ) setName( ioobj->name() );
+
+    Well::Reader rdr( ioobj->fullUserExpr(true), wd_ );
+    rdr.getInfo();
+}
+
+
+WellObject::~WellObject()
+{
+    delete &wd_;
+}
+
+
+void WellObject::updateGeometry()
+{ changed.trigger(); }
+
+
+int WellObject::nrShapes() const	{ return 1; }
+
+const char* WellObject::getShapeName(int shapeidx) const
+{ return name(); }
+
+
+void WellObject::getPoints( int shapeidx, TypeSet<Coord>& pts ) const
+{ pts.add( wd_.info().surfacecoord ); }
+
+
+const MarkerStyle2D* WellObject::getMarkerStyle( int ) const
+{ return &ms_; }
+
+void WellObject::setMarkerStyle( int, const MarkerStyle2D& ms )
+{ ms_ = ms; }
+
+} // namespace Basemap
---
+++ plugins/Basemap/basemapwell.h
@@ -0,0 +1,49 @@
+#ifndef basemapwell_h
+#define basemapwell_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Nanne Hemstra
+ Date:		January 2014
+ RCS:		$Id$
+________________________________________________________________________
+
+-*/
+
+#include "basemapmod.h"
+#include "basemap.h"
+#include "draw.h"
+#include "multiid.h"
+
+namespace Well { class Data; }
+
+namespace Basemap
+{
+
+mExpClass(Basemap) WellObject : public BaseMapObject
+{
+public:
+			WellObject(const MultiID&);
+			~WellObject();
+
+    const char*		getType() const		{ return "Well"; }
+    void		updateGeometry();
+
+    int			nrShapes() const;
+    const char*		getShapeName(int shapeidx) const;
+    void		getPoints(int shapeidx,TypeSet<Coord>&) const;
+    const MarkerStyle2D* getMarkerStyle(int shapeidx) const;
+    void		setMarkerStyle(int shapeidx,const MarkerStyle2D&);
+
+private:
+    MultiID		wellmid_;
+    MarkerStyle2D	ms_;
+
+    Well::Data&		wd_;
+};
+
+} // namespace Basemap
+
+#endif
