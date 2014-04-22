782fda94c08cacaea4862359d17ba6099fc13ed8
Bert Bril <bert.bril@dgbes.com>
2014-04-22 16:13:59 +0000

    Added
    git-svn-id: https://svn.opendtect.org/od/trunk@34230 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/General/mathformulatransl.h
@@ -0,0 +1,19 @@
+#ifndef mathformulatransl_h
+#define mathformulatransl_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Bert
+ Date:		Apr 2014
+ RCS:		$Id$
+________________________________________________________________________
+
+-*/
+
+#include "transl.h"
+
+mDeclEmptyTranslatorBundle(General,MathFormula,od,"formula")
+
+#endif
