4fe41555d2689f2a0d500dfcdb8c60524490813f
Nageswara Rao <nageswara.rao@dgbes.com>
2014-05-06 04:17:38 +0000

    Ported D3769 revision. Changed CTEST_PROJECT_NAME and CTEST_DROP_LOCATION variables.
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@34431 a985855d-d384-4b45-ae33-31a410998c3c

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,10 +3,10 @@
 ## project to incorporate the testing dashboard.
 ## # The following are required to uses Dart and the Cdash dashboard
 
-set(CTEST_PROJECT_NAME "OpendTect")
+set(CTEST_PROJECT_NAME "OpendTect-5_0")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "intranet")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=OpendTect")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=OpendTect-5.0")
 set(CTEST_DROP_SITE_CDASH TRUE)
