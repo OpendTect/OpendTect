269dd5e5a333637e316899957e0b24fb80be37d4
Nageswara Rao <nageswara.rao@dgbes.com>
2014-05-12 04:18:31 +0000

    od5.0: Added GLCM plugin to base definition.
    Summary:
    Added GLCM plugin to base definition.
    od_batch_launcher is not available for windows platforms.
    
    Test Plan: Tested locally.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Subscribers: nanne, raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D3845
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@34552 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/packagescripts/basedefs.cmake
+++ CMakeModules/packagescripts/basedefs.cmake
@@ -18,11 +18,15 @@ set( EXECLIST od_cbvs_browse od_glxinfo od_osgfileviewer lmhostid
 	      od_process_velocityconv od_process_volume od_ProgressViewer od_DispMsg
 	      od_FileBrowser od_SEGYExaminer od_SeisMMBatch od_ClusterProc
 	      od_process_2dgrid od_remexec od_remoteservice od_stratamp od_isopach
-	      od_ReportIssue od_uiReportIssue od_gmtexec od_madexec od_batch_launcher)
+	      od_ReportIssue od_uiReportIssue od_gmtexec od_madexec)
+if ( UNIX )
+    set( EXECLIST ${EXECLIST} od_batch_launcher )
+endif()
+
 set( PLUGINS HorizonAttrib VoxelConnectivityFilter uiHorizonAttrib uiPreStackViewer
 	     uiGoogleIO CmdDriver uiVoxelConnectivityFilter
 	     GMT uiGMT uiImpGPR Madagascar uiMadagascar
-	     MadagascarAttribs uiMadagascarAttribs )
+	     MadagascarAttribs uiMadagascarAttribs GLCM uiGLCM )
 #Only for windows base package
 set( WINEXECLIST od_start_dtect od_main_console )
 
