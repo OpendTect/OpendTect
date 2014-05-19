7c2ced87038be08772ee225cfc2b9682897ae99b
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2014-05-19 12:11:49 +0000

    Updated test.
    git-svn-id: https://svn.opendtect.org/od/trunk@34747 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Basic/tests/geometry.cc
+++ src/Basic/tests/geometry.cc
@@ -15,20 +15,28 @@ static const char* rcsID mUsedVar = "$Id$";
 
 static bool testRectangle()
 {
-    Geom::Rectangle<int> normalrect( 0, 0, 1, 1 );
-    Geom::Rectangle<int> reversedrect( 1, 1, 0, 0 );
+    const Geom::Rectangle<int> normalrect( 0, 0, 1, 1 );
+    const Geom::Rectangle<int> reversedrect( 1, 1, 0, 0 );
 
-    Geom::Rectangle<int> test = reversedrect;
+    const Geom::Rectangle<int> normalinput( -1, -1, 2, 2 );
+    const Geom::Rectangle<int> reversedinput( 2, 2, -1, -1 );
+
+
+    Geom::Rectangle<int> test = reversedinput;
     test.limitTo( normalrect );
     mRunStandardTest( test==reversedrect,
 		      "Reversed limit by normal rectangle" );
+
+    test = reversedinput;
     test.limitTo( reversedrect );
     mRunStandardTest( test==reversedrect,
 		      "Reversed limit by reversed rectangle");
 
-    test = normalrect;
+    test = normalinput;
     test.limitTo( normalrect );
     mRunStandardTest( test==normalrect, "Normal limit by normal rectangle" );
+
+    test = normalinput;
     test.limitTo( reversedrect );
     mRunStandardTest( test==normalrect, "Normal limit by reversed rectangle" );
 
