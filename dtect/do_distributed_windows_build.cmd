d7c71cbbb642630f6cfc0fb0be9e226c3cb0b6ee
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2014-06-16 12:42:03 +0000

    Moved build-profile to dtect folder.
    git-svn-id: https://svn.opendtect.org/od/trunk@35311 a985855d-d384-4b45-ae33-31a410998c3c

--- dtect/do_distributed_windows_build.cmd
+++ dtect/do_distributed_windows_build.cmd
@@ -1,4 +1,9 @@
 @echo off
+SETLOCAL
 set PATH=%PATH%;C:\appman\apps\ninja\
 
-buildconsole /COMMAND="ninja -j70" /PROFILE=CMakeModules/increadibuild_profile.xml
+SET MYPATH=%~p0
+
+echo buildconsole /COMMAND="ninja -j70" /PROFILE=%MYPATH%increadibuild_profile.xml
+
+ENDLOCAL
