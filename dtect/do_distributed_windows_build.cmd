250a788aa336983747dc3cd364b9803e62d0db6b
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2014-06-17 05:06:28 +0000

    Removed erronious echo.
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@35316 a985855d-d384-4b45-ae33-31a410998c3c

--- dtect/do_distributed_windows_build.cmd
+++ dtect/do_distributed_windows_build.cmd
@@ -4,6 +4,6 @@ set PATH=%PATH%;C:\appman\apps\ninja\
 
 SET MYPATH=%~p0
 
-echo buildconsole /COMMAND="ninja -j70" /PROFILE=%MYPATH%increadibuild_profile.xml
+buildconsole /COMMAND="ninja -j70" /PROFILE=%MYPATH%increadibuild_profile.xml
 
 ENDLOCAL
