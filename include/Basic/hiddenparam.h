ecf4e361ee39bb5c5dbf3b27465402e97d63d3a6
Nanne Hemstra <nanne.hemstra@dgbes.com>
2014-07-03 17:54:47 +0000

    Use a spinlock in stead of a mutex lock
    git-svn-id: https://svn.opendtect.org/od/trunk@35530 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/hiddenparam.h
+++ include/Basic/hiddenparam.h
@@ -22,23 +22,23 @@ binary compability issues.
 
   If you want to add the variable of type V to class O, do the following
   in the source-file:
-  
+
   \code
   HiddenParam<O,V>	myparammanager( undef_val );
   \endcode
-  
+
   You can then access the variable in the source-file by:
-  
+
   \code
   myparammanager.setParam( this, new_value );
   \endcode
-  
-  and retrieve it by 
-  
+
+  and retrieve it by
+
   \code
   V value = myparammanager.getParam( this );
   \endcode
-  
+
   \note V cannot be boolean. Use char instead. V must be 'simple' enough to be
   stored in a type-set. Also note you may not be able to call the
   removeParam (if the class does not already have a destructor), so
@@ -50,17 +50,17 @@ binary compability issues.
 */
 
 template <class O, class V>
-mClass(Basic) HiddenParam 
+mClass(Basic) HiddenParam
 {
 public:
-    		HiddenParam( const V& undefval )
-		    : undef_( undefval )		{}
-    void	setParam( O* obj, const V& val );
-    const V&	getParam( const O* obj ) const;
+		HiddenParam( const V& undefval )
+		    : lock_(true)
+		    , undef_(undefval)		{}
 
-    bool	hasParam( const O* ) const;
-
-    void	removeParam( O* );
+    void	setParam(O* obj,const V& val);
+    const V&	getParam(const O* obj) const;
+    bool	hasParam(const O* obj) const;
+    void	removeParam(O* obj);
 
 protected:
 
@@ -123,6 +123,5 @@ void HiddenParam<O,V>::removeParam( O* obj )
     params_.removeSingle( idx );
     objects_.removeSingle( idx );
 }
-
 
 #endif
