487d3113a153b9b99ed822f10da4f56b89b4fb8b
Bart de Groot <bart.degroot@dgbes.com>
2014-07-03 07:52:39 +0000

    uiHello
    Summary: removed & signs
    
    Test Plan: review
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Subscribers: kristofer, helene
    
    Differential Revision: https://phabricator.dgbes.com:8443/D4698
    
    git-svn-id: https://svn.opendtect.org/od/trunk@35510 a985855d-d384-4b45-ae33-31a410998c3c

--- plugins/uiHello/uihellopi.cc
+++ plugins/uiHello/uihellopi.cc
@@ -59,7 +59,7 @@ public:
 uiHelloMgr::uiHelloMgr( uiODMain& a )
 	: appl(a)
 {
-    uiAction* newitem = new uiAction( "&Display Hello Message ...",
+    uiAction* newitem = new uiAction( "Display Hello Message ...",
 					  mCB(this,uiHelloMgr,dispMsg) );
     appl.menuMgr().utilMnu()->insertItem( newitem );
 }
