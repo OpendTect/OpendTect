5d51176bf10e728de1ce7b9179e2d18f1c2e0a10
Nageswara Rao <nageswara.rao@dgbes.com>
2014-07-07 12:05:16 +0000

    od4.6: Removed copyright message insertion from source code.
    Summary: Removed copyright message insertion while installing source files.
    
    Test Plan: Tested on windows machine, able to step into function definitions.
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Subscribers: raman, ranojay
    
    Maniphest Tasks: T199
    
    Differential Revision: https://phabricator.dgbes.com:8443/D4805
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.6@35566 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODInstallSources.cmake
+++ CMakeModules/ODInstallSources.cmake
@@ -5,23 +5,9 @@
 #	RCS :		$Id$
 #_______________________________________________________________________________
 
-#TODO Add date
-file( READ ${CMAKE_SOURCE_DIR}/CMakeModules/templates/license.txt.in lic_temp )
 file( STRINGS ${CMAKE_SOURCE_DIR}/CMakeModules/sourcefiles_od.txt DEVELSTUFF )
 foreach( FIL ${DEVELSTUFF} )
     get_filename_component( FPATH ${FIL} PATH )
     file( INSTALL DESTINATION ${CMAKE_INSTALL_PREFIX}/${FPATH}
 	          TYPE FILE FILES ${CMAKE_SOURCE_DIR}/${FIL} )
-    get_filename_component( FEXT ${FIL} EXT )
-    if( ("${FEXT}" STREQUAL ".h") OR ("${FEXT}" STREQUAL ".cc") OR ("${FEXT}" STREQUAL ".c"))
-	get_filename_component( FNAME ${FIL} NAME )
-	file( READ ${CMAKE_INSTALL_PREFIX}/${FPATH}/${FNAME} temp )
-	file( WRITE ${CMAKE_INSTALL_PREFIX}/${FPATH}/tempfile
-	      "${lic_temp}\n" )
-	file( APPEND ${CMAKE_INSTALL_PREFIX}/${FPATH}/tempfile
-	      "${temp}\n" )
-	file( RENAME
-		${CMAKE_INSTALL_PREFIX}/${FPATH}/tempfile 
-		${CMAKE_INSTALL_PREFIX}/${FPATH}/${FNAME} )
-    endif()
 endforeach()
