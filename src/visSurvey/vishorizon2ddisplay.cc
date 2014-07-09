d0c1c82175b5f74854bc1f28c2daa4f4fde8c52a
Ranojay Sen <ranojay.sen@dgbes.com>
2014-07-09 12:40:26 +0000

    od: Possible crash fix in 2D horizon updator (Mantis crash report 3009)
    Summary:
    Using Null pointer checks to avoid crash.
    http://bugs.dgbes.com/view.php?id=3009
    
    Test Plan: Could not reproduce, so could not test the crash.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Subscribers: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D4850
    
    git-svn-id: https://svn.opendtect.org/od/trunk@35611 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visSurvey/vishorizon2ddisplay.cc
+++ src/visSurvey/vishorizon2ddisplay.cc
@@ -408,6 +408,10 @@ void Horizon2DDisplay::updateSection( int idx, const LineRanges* lineranges )
 		    emobject_->sectionGeometry(sid));
     const LineRanges* lrgs = redo ? &linergs : lineranges;
     visBase::PolyLine3D* pl = lines_.validIdx(idx) ? lines_[idx] : 0;
+
+    if ( !rcs || !pl )
+	return;
+
     Horizon2DDisplayUpdater updater( rcs, lrgs, pl, ps,
 				     zaxistransform_, linenames );
     updater.execute();
