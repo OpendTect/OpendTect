f2a17eddca8f9edab66cf4db1fe3b4c5393562f5
Jaap Glas <jaap.glas@dgbes.com>
2014-07-29 08:46:30 +0000

    Bugfix: Non-shader volume display was printing OSG matrix info in console. (PORTED FROM od TO od5.0 UNMODIFIED)
    Summary:
    Overrule OSG's default notify level if not set by OSG environment variables.
    Tightened from NOTICE to WARN when in debug mode, and disabled if not.
    
    Test Plan:
    Debug version was used to test all notify levels before finalizing the
    settings for both debug and non-debug version. Also tried environment
    variables to overrule them.
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D4976
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@35799 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visBase/initvisbase.cc
+++ src/visBase/initvisbase.cc
@@ -15,11 +15,19 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "indexedshape.h"
 
 #include <osgGeo/LayeredTexture>
+#include <osg/Notify>
 
 
 mDefModInitFn(visBase)
 {
     mIfNotFirstTime( return );
+
+    if( !getenv("OSG_NOTIFY_LEVEL") && !getenv("OSGNOTIFYLEVEL") )
+#ifdef __debug__
+	setNotifyLevel( osg::WARN );
+#else
+	setNotifyLevel( osg::ALWAYS );	// which strangely enough means never.
+#endif
 
     int maxsize = -1;
     mSettUse( get, "dTect", "Max texture size override", maxsize );
