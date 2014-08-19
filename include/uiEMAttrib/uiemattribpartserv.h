OpendTect/OpendTect

commit 2362ed1aaa4d3b80310bcd156159c38834e6dffd

od5.0: Fix for horizon shift dialog shifting the other horizon when selected from the tree
Summary: see D5211

Test Plan: see D5211

Reviewers: raman

Reviewed By: raman

Differential Revision: https://phabricator.dgbes.com:8443/D5214

git-svn-id: https://svn.opendtect.org/od/branches/od5.0@36010 a985855d-d384-4b45-ae33-31a410998c3c

include/uiEMAttrib/uiemattribpartserv.h
@@ -61,8 +61,10 @@ public:
 				{ descset_ = ads; }
 
     void			showHorShiftDlg(const EM::ObjectID&,
-	    			    const BoolTypeSet& attrenabled,
-				    float initialshift,bool canaddattrib);
+	    					const int& visid,
+						const BoolTypeSet& attrenabled,
+						float initialshift,
+						bool canaddattrib);
     void			fillHorShiftDPS(ObjectSet<DataPointSet>&,
 	    				TaskRunner*);
 
@@ -74,6 +76,7 @@ public:
     float			getShift() const;
     void			setAttribID( Attrib::DescID id )
     				{ attribid_ = id; }
+    int				getShiftedObjectVisID() const;
     void			setAttribIdx(int);
     Attrib::DescID		attribID() const	{ return attribid_; }
     int				attribIdx() const	{ return attribidx_; }

include/uiEMAttrib/uihorizonshiftdlg.h
@@ -30,6 +30,7 @@ mClass(uiEMAttrib) uiHorizonShiftDialog : public uiDialog
 {
 public:
 			uiHorizonShiftDialog(uiParent*,const EM::ObjectID& id,
+					     const int& visid,
 					     const Attrib::DescSet&,
 					     float initialshift,
 					     bool cancalcattrib);
@@ -42,6 +43,8 @@ public:
     int				curShiftIdx() const;
     bool			doStore() const;
     const EM::ObjectID&		emID() const		{ return emid_; }
+    const int&			visID() const		{ return visid_; }
+    
     const char*			getAttribName() const;
     const char*			getAttribBaseName() const;
 
@@ -56,11 +59,11 @@ protected:
     void			attribChangeCB(CallBacker*);
     void			calcAttrib(CallBacker*);
     void			shiftCB(CallBacker*);
-    bool	acceptOK(CallBacker*);
+    bool			acceptOK(CallBacker*);
 
     uiAttrSel*			attrinpfld_;
     uiGenInput*			rangeinpfld_;
-    uiSlider*		slider_;
+    uiSlider*			slider_;
     uiPushButton*		calbut_;
     uiCheckBox*			storefld_;
     uiGenInput*			namefld_;
@@ -69,6 +72,7 @@ protected:
     StepInterval<float>		calcshiftrg_;
     EM::Horizon3D*		emhor3d_;
     EM::ObjectID		emid_;
+    int				visid_;
 };
 
 #endif

src/uiEMAttrib/uiemattribpartserv.cc
@@ -127,6 +127,7 @@ StepInterval<float> uiEMAttribPartServer::shiftRange() const
 
 
 void uiEMAttribPartServer::showHorShiftDlg( const EM::ObjectID& id,
+					    const int& visid,
 					    const BoolTypeSet& attrenabled,
 					    float initialshift,
 					    bool canaddattrib)
@@ -136,8 +137,9 @@ void uiEMAttribPartServer::showHorShiftDlg( const EM::ObjectID& id,
     initialshift_ = initialshift;
     initialattribstatus_ = attrenabled;
     setAttribIdx( mUdf(int) );
-    horshiftdlg_ = new uiHorizonShiftDialog( appserv().parent(), id, *descset_,
-	    				     initialshift, canaddattrib );
+    horshiftdlg_ = new uiHorizonShiftDialog( appserv().parent(), id, visid,
+					     *descset_, initialshift,
+					     canaddattrib );
     horshiftdlg_->calcAttribPushed.notify(
 	    mCB(this,uiEMAttribPartServer,calcDPS) );
     horshiftdlg_->horShifted.notify(
@@ -246,3 +248,7 @@ void uiEMAttribPartServer::horShifted( CallBacker* cb )
     sendEvent( uiEMAttribPartServer::evHorizonShift() );
 }
 
+int uiEMAttribPartServer::getShiftedObjectVisID() const
+{
+    return horshiftdlg_ ? horshiftdlg_->visID() : -1;
+}

src/uiEMAttrib/uihorizonshiftdlg.cc
@@ -33,6 +33,7 @@ const char* uiHorizonShiftDialog::sDefaultAttribName()
 
 uiHorizonShiftDialog::uiHorizonShiftDialog( uiParent* p,
 					    const EM::ObjectID& emid,
+					    const int& visid,
 					    const Attrib::DescSet& descset,
 					    float initialshift,
 					    bool cancalcattrib )
@@ -42,6 +43,7 @@ uiHorizonShiftDialog::uiHorizonShiftDialog( uiParent* p,
     , calcshiftrg_(mUdf(float),mUdf(float),mUdf(float))
     , emhor3d_(0)
     , emid_(emid)
+    , visid_(visid)
     , storefld_(0)
     , namefld_(0)
     , calbut_(0)
@@ -71,8 +73,15 @@ uiHorizonShiftDialog::uiHorizonShiftDialog( uiParent* p,
     EM::EMObject* emobj = EM::EMM().getObject( emid_ );
     mDynamicCastGet(EM::Horizon3D*,emhor3d,emobj)
     emhor3d_ = emhor3d;
-    emhor3d_->ref();
 
+    if ( emhor3d_ )
+    {
+	emhor3d_->ref();
+	BufferString title = setup().wintitle_.getFullString();
+	title.add( " - " ).add( emhor3d_->name() );
+	setCaption( title );
+    }
+    
     if ( cancalcattrib )
     {
 	attrinpfld_ = new uiAttrSel( this, descset, "Select Attribute" );

src/uiODMain/uiodapplmgr.cc
@@ -1211,6 +1211,8 @@ bool uiODApplMgr::handleEMAttribServEv( int evid )
 {
     const int visid = visserv_->getEventObjId();
     const int attribidx = emattrserv_->attribIdx();
+    const int shiftvisid = emattrserv_->getShiftedObjectVisID();
+
     if ( evid == uiEMAttribPartServer::evCalcShiftAttribute() )
     {
 	const Attrib::SelSpec as( emattrserv_->getAttribBaseNm(),
@@ -1220,11 +1222,12 @@ bool uiODApplMgr::handleEMAttribServEv( int evid )
     else if ( evid == uiEMAttribPartServer::evHorizonShift() )
     {
 	const int textureidx = emattrserv_->textureIdx();
-	visserv_->setTranslation( visid, Coord3(0,0,emattrserv_->getShift()) );
+	visserv_->setTranslation( shiftvisid,
+				    Coord3(0,0,emattrserv_->getShift()) );
 	if ( !mIsUdf(attribidx) )
-	    visserv_->selectTexture( visid, attribidx, textureidx );
+	    visserv_->selectTexture( shiftvisid, attribidx, textureidx );
 
-	uiTreeItem* parent = sceneMgr().findItem( visid );
+	uiTreeItem* parent = sceneMgr().findItem( shiftvisid );
 	if ( parent )
 	    parent->updateColumnText( uiODSceneMgr::cNameColumn() );
     }
@@ -1282,19 +1285,19 @@ bool uiODApplMgr::handleEMAttribServEv( int evid )
 		}
 
 		parent->removeChild( itm );
-		visserv_->removeAttrib( visid, emattrserv_->attribIdx() );
+		visserv_->removeAttrib( shiftvisid, emattrserv_->attribIdx() );
 	    }
 	}
 
 	if ( !isok )
 	{
-	    visserv_->setTranslation( visid,
+	    visserv_->setTranslation( shiftvisid,
 		    Coord3(0,0,emattrserv_->initialShift() ) );
 
 	    for ( int idx=0; idx<enableattrib.size(); idx++ )
-		visserv_->enableAttrib( visid, idx, enableattrib[idx] );
+		visserv_->enableAttrib( shiftvisid, idx, enableattrib[idx] );
 
-	    uiTreeItem* parent = sceneMgr().findItem( visid );
+	    uiTreeItem* parent = sceneMgr().findItem( shiftvisid );
 	    if ( parent )
 		parent->updateColumnText( uiODSceneMgr::cNameColumn() );
 	}
@@ -1305,7 +1308,7 @@ bool uiODApplMgr::handleEMAttribServEv( int evid )
 		if ( idx==attribidx )
 		{
 		    DataPointSet data( false, true );
-		    visserv_->getRandomPosCache( visid, attribidx, data );
+		    visserv_->getRandomPosCache( shiftvisid, attribidx, data );
 		    if ( data.isEmpty() )
 			continue;
 
@@ -1323,7 +1326,7 @@ bool uiODApplMgr::handleEMAttribServEv( int evid )
 			    data.bivSet().removeVal( idy );
 		    }
 
-		    visserv_->setRandomPosData( visid, attribidx, &data );
+		    visserv_->setRandomPosData( shiftvisid, attribidx, &data );
 		}
 	    }
 	}

src/uiODMain/uiodhortreeitem.cc
@@ -545,8 +545,8 @@ void uiODHorizonTreeItem::handleMenuCB( CallBacker* cb )
 	if ( mIsUdf( curshift ) ) curshift = 0;
 
 	emattrserv->setDescSet( attrserv->curDescSet(false) );
-	emattrserv->showHorShiftDlg( emid_, isenabled, curshift,
-				     visserv_->canAddAttrib( visid, 1) );
+	emattrserv->showHorShiftDlg( emid_, visid, isenabled, curshift,
+				     visserv_->canAddAttrib(visid,1) );
     }
     else if ( mnuid==hordatamnuitem_.id )
     {

commit 5353c20dd4cda828d4dd850c89484aaa50a62285

od5.0: Restored API compatibility broken in revision D5214
Summary: Using overloaded functions to maintain API compatibility

Test Plan:
1) compiles
2) Shifted horizon up/down - works

Reviewers: raman

Reviewed By: raman

Differential Revision: https://phabricator.dgbes.com:8443/D5245

git-svn-id: https://svn.opendtect.org/od/branches/od5.0@36022 a985855d-d384-4b45-ae33-31a410998c3c

include/uiEMAttrib/uiemattribpartserv.h
@@ -65,6 +65,10 @@ public:
 						const BoolTypeSet& attrenabled,
 						float initialshift,
 						bool canaddattrib);
+    void			showHorShiftDlg(const EM::ObjectID&,
+						const BoolTypeSet& attrenabled,
+						float initialshift,
+						bool canaddattrib);
     void			fillHorShiftDPS(ObjectSet<DataPointSet>&,
 	    				TaskRunner*);
 

include/uiEMAttrib/uihorizonshiftdlg.h
@@ -34,6 +34,10 @@ public:
 					     const Attrib::DescSet&,
 					     float initialshift,
 					     bool cancalcattrib);
+			uiHorizonShiftDialog(uiParent*,const EM::ObjectID& id,
+					     const Attrib::DescSet&,
+					     float initialshift,
+					     bool cancalcattrib);
 			~uiHorizonShiftDialog();
     const EM::Horizon3D&	horizon3D()		{ return *emhor3d_; }
     StepInterval<float>	shiftRg() const;

src/uiEMAttrib/uiemattribpartserv.cc
@@ -152,6 +152,31 @@ void uiEMAttribPartServer::showHorShiftDlg( const EM::ObjectID& id,
 }
 
 
+void uiEMAttribPartServer::showHorShiftDlg( const EM::ObjectID& id,
+					    const BoolTypeSet& attrenabled,
+					    float initialshift,
+					    bool canaddattrib)
+{
+    sendEvent( uiEMAttribPartServer::evShiftDlgOpened() );
+
+    initialshift_ = initialshift;
+    initialattribstatus_ = attrenabled;
+    setAttribIdx( mUdf(int) );
+    horshiftdlg_ = new uiHorizonShiftDialog( appserv().parent(), id, 
+					     *descset_, initialshift,
+					     canaddattrib );
+    horshiftdlg_->calcAttribPushed.notify(
+	    mCB(this,uiEMAttribPartServer,calcDPS) );
+    horshiftdlg_->horShifted.notify(
+	    mCB(this,uiEMAttribPartServer,horShifted) );
+    horshiftdlg_->windowClosed.notify(
+	    mCB(this,uiEMAttribPartServer,shiftDlgClosed));
+
+    horshiftdlg_->go();
+    horshiftdlg_->setDeleteOnClose( true );
+}
+
+
 void uiEMAttribPartServer::shiftDlgClosed( CallBacker* )
 {
     if ( horshiftdlg_->uiResult()==1 )

src/uiEMAttrib/uihorizonshiftdlg.cc
@@ -107,6 +107,81 @@ uiHorizonShiftDialog::uiHorizonShiftDialog( uiParent* p,
 }
 
 
+uiHorizonShiftDialog::uiHorizonShiftDialog( uiParent* p,
+					    const EM::ObjectID& emid,
+					    const Attrib::DescSet& descset,
+					    float initialshift,
+					    bool cancalcattrib )
+    : uiDialog(p,uiDialog::Setup("Horizon shift",mNoDlgTitle,
+                                 mODHelpKey(mHorizonShiftDialogHelpID) ).
+				  modal(false) )
+    , calcshiftrg_(mUdf(float),mUdf(float),mUdf(float))
+    , emhor3d_(0)
+    , emid_(emid)
+    , visid_(-1)
+    , storefld_(0)
+    , namefld_(0)
+    , calbut_(0)
+    , attrinpfld_(0)
+    , calcAttribPushed(this)
+    , horShifted(this)
+{
+    const float curshift = initialshift*SI().zDomain().userFactor();
+    shiftrg_ = StepInterval<float> (curshift-100,curshift+100,10);
+
+    BufferString lbl( "Shift Range ", SI().getZUnitString() );
+    rangeinpfld_ = new uiGenInput( this, lbl, FloatInpIntervalSpec(shiftrg_) );
+    rangeinpfld_->valuechanged.notify(
+	    mCB(this,uiHorizonShiftDialog,rangeChangeCB) );
+
+    lbl = BufferString( "Shift ", SI().getZUnitString() );
+    slider_ = new uiSlider(
+	    this, uiSlider::Setup(lbl).withedit(true), "Horizon slider" );
+    slider_->attach( alignedBelow, rangeinpfld_ );
+
+    // TODO: Calculate slider range from horizon's z-range
+    slider_->setScale( shiftrg_.step, shiftrg_.start );
+    slider_->setInterval( shiftrg_ );
+    slider_->setValue( curshift );
+    slider_->valueChanged.notify( mCB(this,uiHorizonShiftDialog,shiftCB) );
+
+    EM::EMObject* emobj = EM::EMM().getObject( emid_ );
+    mDynamicCastGet(EM::Horizon3D*,emhor3d,emobj)
+    emhor3d_ = emhor3d;
+
+    if ( emhor3d_ )
+    {
+	emhor3d_->ref();
+	BufferString title = setup().wintitle_.getFullString();
+	title.add( " - " ).add( emhor3d_->name() );
+	setCaption( title );
+    }
+    
+    if ( cancalcattrib )
+    {
+	attrinpfld_ = new uiAttrSel( this, descset, "Select Attribute" );
+	attrinpfld_->attach( alignedBelow, slider_ );
+	attrinpfld_->selectionDone.notify(
+		mCB(this,uiHorizonShiftDialog,attribChangeCB) );
+
+	calbut_ = new uiPushButton( this, "Calculate", false );
+	calbut_->attach( rightTo, attrinpfld_ );
+	calbut_->activated.notify( mCB(this,uiHorizonShiftDialog,calcAttrib) );
+
+	storefld_ = new uiCheckBox( this, "Store Horizons on pressing Ok" );
+	storefld_->attach( alignedBelow, attrinpfld_ );
+	storefld_->setChecked( false );
+	storefld_->activated.notify(
+		mCB(this,uiHorizonShiftDialog,setNameFldSensitive) );
+
+	namefld_ = new uiGenInput( this, "Attribute Basename",
+				   StringInpSpec(sDefaultAttribName()) );
+	namefld_->attach( alignedBelow, storefld_ );
+	attribChangeCB( 0 );
+    }
+}
+
+
 uiHorizonShiftDialog::~uiHorizonShiftDialog()
 {
     emhor3d_->unRef();