260b1657dc97681ef7ec7340a24fed89dead2b44
Nageswara Rao <nageswara.rao@dgbes.com>
2014-08-20 08:56:08 +0000

    od5.0: Crash fixed while displaying corrupted horizon on flattened scene.
    Summary: Fixed crash.
    
    Test Plan: No crah after this change.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D5256
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@36045 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiEarthModel/uiempartserv.cc
+++ src/uiEarthModel/uiempartserv.cc
@@ -1312,7 +1312,10 @@ ZAxisTransform* uiEMPartServer::getHorizonZAxisTransform( bool is2d )
     const MultiID hormid = horfld->key();
     EM::ObjectID emid = getObjectID( hormid );
     if ( emid<0 || !isFullyLoaded(emid) )
-	loadSurface( hormid );
+    {
+	if ( !loadSurface( hormid ) )
+	    return 0;
+    }
 
     emid = getObjectID( hormid );
     if ( emid<0 ) return 0;
