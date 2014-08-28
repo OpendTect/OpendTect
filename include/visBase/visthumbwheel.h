OpendTect/OpendTect

commit 1d70a845f9b5a92347795d1cfbc7d0ccf8ad61ca

od5.0: Reversed zoom-wheel zoom direction
Summary:
Made it consistent with v4.6

Users expect:
Zoom-in: Mouse-wheel backward, scene-wheel down
Zoom-out: Mouse-wheel forward, scene-wheel up

Does this make sense and is the implementation at the right place?

Test Plan:
Tested zooming via scene and mouse wheels. Zooming as expected.
Only the scene-wheel moves in the opposite direction if I use the mouse-wheel. No clue where to change that.

Reviewers: jaap

Reviewed By: jaap

Differential Revision: https://phabricator.dgbes.com:8443/D5179

git-svn-id: https://svn.opendtect.org/od/branches/od5.0@35953 a985855d-d384-4b45-ae33-31a410998c3c

src/uiOSG/ui3dviewer.cc
@@ -260,7 +260,7 @@ void ui3DViewerBody::setupHUD()
 
     hudview_ = new osgViewer::View;
     hudview_->setCamera( hudcamera );
-    offscreenrenderhudswitch_->removeChild( 
+    offscreenrenderhudswitch_->removeChild(
 	0, offscreenrenderhudswitch_->getNumChildren() );
     offscreenrenderhudswitch_->addChild( hudscene_->osgNode() );
     hudview_->setSceneData( offscreenrenderhudswitch_ );
@@ -308,7 +308,7 @@ void ui3DViewerBody::setupHUD()
     {
 	visscenecoltab_ = visBase::SceneColTab::create();
 	hudscene_->addObject( visscenecoltab_ );
-	
+
 	FontData ftdata;
 	ftdata.setPointSize( 18 );
 	visscenecoltab_->setAnnotFont( ftdata );
@@ -433,7 +433,6 @@ osgViewer::CompositeViewer* ui3DViewerBody::getCompositeViewer()
 
     return viewer.get();
 }
-
 
 
 osg::Camera* ui3DViewerBody::getOsgCamera()
@@ -588,7 +587,7 @@ void ui3DViewerBody::thumbWheelRotationCB(CallBacker* cb )
         osg::ref_ptr<osgGeo::TrackballManipulator> manip =
 	static_cast<osgGeo::TrackballManipulator*>(
                                                 view_->getCameraManipulator() );
-        manip->changeDistance( deltaangle/M_PI );
+	manip->changeDistance( -deltaangle/M_PI );
 
     }
 }
@@ -985,7 +984,7 @@ void ui3DViewerBody::uiRotate( float angle, bool horizontal )
 
 void ui3DViewerBody::notifyManipulatorMovement( float dh, float dv, float df )
 {
-    distancethumbwheel_->setAngle( df * M_PI+distancethumbwheel_->getAngle() );
+    distancethumbwheel_->setAngle( -df * M_PI+distancethumbwheel_->getAngle() );
     horthumbwheel_->setAngle( dh + horthumbwheel_->getAngle() );
     verthumbwheel_->setAngle( dv + verthumbwheel_->getAngle() );
 }
@@ -1458,7 +1457,7 @@ bool ui3DViewer::usePar( const IOPar& par )
 	setBackgroundColor( newcol );
     }
 
-    
+
     StereoType stereotype;
     if ( parseEnum( par, sKeyStereo(), stereotype ) )
 	setStereoType( stereotype );

commit 542d0e2d94aedf4ab126747f1113c9bfef099024

Zoom wheel was turning in reverse direction when zooming by means of mouse dragging in the scene.
Summary:
Zoom wheel was turning in reverse direction when zooming by means of mouse
dragging in the scene (if not above zoom wheel). Caused by code change in
D5179, which made zoom wheel turn in right direction for zooming by means
of mouse wheel. Turns out the big discrete steps of the mouse wheel caused
the same optical illusion as spoke wheels rolling backwards in old movies.
Solved by animating the zoom wheel rotation in case of discrete zooming.

Test Plan:
Zoom wheel turns in right direction when zooming by means of mouse dragging
in the scene, and shows an animated rotation in the right direction when
zooming by means of scrolling the mouse wheel (at any speed).

Reviewers: kristofer

Reviewed By: kristofer

Subscribers: kristofer, nanne

Differential Revision: https://phabricator.dgbes.com:8443/D5347

git-svn-id: https://svn.opendtect.org/od/trunk@36157 a985855d-d384-4b45-ae33-31a410998c3c

include/visBase/visthumbwheel.h
@@ -38,7 +38,8 @@ public:
     void			setAnnotationColor(const Color&);
 
     float			getAngle() const;
-    void			setAngle(float);
+    void			setAngle(float angle,float rotationtime=0.0);
+				//!<angle in rad, rotation time in seconds
     CNotifier<ThumbWheel,float>	rotation;
 				//!<passes rotation since last notification
 

src/uiOSG/ui3dviewer.cc
@@ -984,7 +984,14 @@ void ui3DViewerBody::uiRotate( float angle, bool horizontal )
 
 void ui3DViewerBody::notifyManipulatorMovement( float dh, float dv, float df )
 {
-    distancethumbwheel_->setAngle( -df * M_PI+distancethumbwheel_->getAngle() );
+    osgGeo::TrackballManipulator* manip =
+	static_cast<osgGeo::TrackballManipulator*>(
+						view_->getCameraManipulator() );
+
+    const float rotationtime = manip && manip->isDiscreteZooming() ? 0.2 : 0.0;
+    distancethumbwheel_->setAngle( df*M_PI + distancethumbwheel_->getAngle(),
+				   rotationtime );
+
     horthumbwheel_->setAngle( dh + horthumbwheel_->getAngle() );
     verthumbwheel_->setAngle( dv + verthumbwheel_->getAngle() );
 }

src/visBase/visthumbwheel.cc
@@ -93,9 +93,9 @@ float ThumbWheel::getAngle() const
 }
 
 
-void ThumbWheel::setAngle( float a )
+void ThumbWheel::setAngle( float angle, float rotationtime )
 {
-    thumbwheel_->setAngle( a );
+    thumbwheel_->setAngle( angle, rotationtime );
 }
 
 