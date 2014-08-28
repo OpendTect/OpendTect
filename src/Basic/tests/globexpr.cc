84433121b6209ff50f9732a47a7304c29b4557df
Bert Bril <bert.bril@dgbes.com>
2014-08-28 11:36:58 +0000

    Added test_globexpr
    git-svn-id: https://svn.opendtect.org/od/trunk@36165 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/Basic/tests/globexpr.cc
@@ -0,0 +1,31 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : K. Tingdahl
+ * DATE     : July 2012
+ * FUNCTION :
+-*/
+
+static const char* rcsID mUsedVar = "$Id$";
+
+#include "nrbytes2string.h"
+#include "testprog.h"
+#include "globexpr.h"
+
+
+
+int main( int argc, char** argv )
+{
+    mInitTestProg();
+
+    if ( quiet || argc != 3 )
+	return ExitProgram( 0 );
+
+    BufferString tomatch( argv[1] );
+    GlobExpr ge( argv[2] );
+
+    od_cout() << "'" << argv[1]
+	<< (ge.matches(argv[1]) ? "' matches '" : "' doesn't match '")
+	<< argv[2] << "'" << od_endl;
+
+    return ExitProgram( 0 );
+}
