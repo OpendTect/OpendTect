b948a252c4d66049925c49a6d442f1a79f829ef7
Nanne Hemstra <nanne.hemstra@dgbes.com>
2014-08-29 19:19:14 +0000

    Renamed Stored Volume to Input Volume
    git-svn-id: https://svn.opendtect.org/od/trunk@36181 a985855d-d384-4b45-ae33-31a410998c3c

--- include/VolumeProcessing/volprocvolreader.h
+++ include/VolumeProcessing/volprocvolreader.h
@@ -27,38 +27,37 @@ namespace VolProc
 \brief Reads in a volume. Will replace previous values if data is present in
 the read volume.
 */
-    
+
 mExpClass(VolumeProcessing) VolumeReader : public Step
 {
 public:
-    			mDefaultFactoryInstantiation( Step, VolumeReader,
-				"VolumeReader", "Stored Volume" );
-    			~VolumeReader();
+			mDefaultFactoryInstantiation( Step, VolumeReader,
+				"VolumeReader", "Input Volume" );
+			~VolumeReader();
 
-    bool		needsInput() const			{ return false;}
-    bool 		setVolumeID(const MultiID&);
-    const MultiID&	getVolumeID() const			{ return mid_; }
+    bool		needsInput() const		{ return false;}
+    bool		setVolumeID(const MultiID&);
+    const MultiID&	getVolumeID() const		{ return mid_; }
 
     void		fillPar(IOPar&) const;
     bool		usePar(const IOPar&);
 
-    bool		canInputAndOutputBeSame() const		{ return true; }
-    bool		needsFullVolume() const 		{ return false;}
+    bool		canInputAndOutputBeSame() const	{ return true; }
+    bool		needsFullVolume() const		{ return false;}
 
 protected:
-    Task*			createTask();
+    Task*		createTask();
 
-    bool			prefersBinIDWise() const        { return false; }
+    bool		prefersBinIDWise() const        { return false; }
 
-    static const char*		sKeyVolumeID()	{ return "Volume ID"; }
+    static const char*	sKeyVolumeID()			{ return "Volume ID"; }
 
     MultiID				mid_;
     ObjectSet<SeisTrcReader>		readers_;
     ObjectSet<SeisTrcTranslator>	translators_;
 };
 
-}; //namespace
-
+} // namespace VolProc
 
 #endif
 
