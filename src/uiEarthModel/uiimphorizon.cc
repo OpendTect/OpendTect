8b46443c9df454dac61135816954e58516e11a5d
Nageswara Rao <nageswara.rao@dgbes.com>
2014-09-05 12:03:58 +0000

    https://phabricator.dgbes.com:8443/D5481. od5.0: Should return if it (list) is empty.
    git-svn-id: https://svn.opendtect.org/od/tags/od5.0.0-beta2@36304 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiEarthModel/uiimphorizon.cc
+++ src/uiEarthModel/uiimphorizon.cc
@@ -255,7 +255,7 @@ void uiImportHorizon::addAttribCB( CallBacker* )
 
 void uiImportHorizon::rmAttribCB( CallBacker* )
 {
-    if ( !attrlistfld_->isEmpty() )
+    if ( attrlistfld_->isEmpty() )
 	return;
 
     int selidx = attrlistfld_->currentItem();
