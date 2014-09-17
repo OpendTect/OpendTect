8c6e32a16d5e07f132e62801ba4c07eeeafad9be
Bert Bril <bert.bril@dgbes.com>
2014-09-17 13:33:58 +0000

    od5.0: Adding 'suggestedName' to IOObj write options protocol
    Summary: When a user specifies a name for an object in Petrel, then that name is likely but not necessarily the same as the name (s)he wants in OpendTect. Therefore, added notifiers caught by uiIOObjSelGroup to set the name to the suggested one.
    
    Test Plan:
    Tried it for Petrel UI only, but that works.
    Not tested API compatibility, but I only added stuff so that should be OK.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Maniphest Tasks: T198
    
    Differential Revision: https://phabricator.dgbes.com:8443/D5703
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@36517 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiIo/uiioobjselwritetransl.cc
+++ src/uiIo/uiioobjselwritetransl.cc
@@ -28,6 +28,7 @@ uiIOObjTranslatorWriteOpts::uiIOObjTranslatorWriteOpts( uiParent* p,
 							const Translator& trl )
     : uiGroup(p,BufferString("Write options group for ",trl.getDisplayName()))
     , transl_(trl)
+    , suggestedNameAvailble(this)
 {
 }
 
@@ -38,6 +39,7 @@ uiIOObjSelWriteTranslator::uiIOObjSelWriteTranslator( uiParent* p,
     , ctxt_(*new IOObjContext(ctio.ctxt))
     , selfld_(0)
     , lbl_(0)
+    , suggestedNameAvailble(this)
 {
     optflds_.allowNull( true );
     const TranslatorGroup& trgrp = *ctio.ctxt.trgroup;
@@ -69,6 +71,8 @@ uiIOObjSelWriteTranslator::uiIOObjSelWriteTranslator( uiParent* p,
 	{
 	    uiIOObjTranslatorWriteOpts* fld =
 		uiIOObjTranslatorWriteOpts::create( this, *trs_[idx] );
+	    fld->suggestedNameAvailble.notify(
+			mCB(this,uiIOObjSelWriteTranslator,nmAvCB) );
 	    optflds_ += fld;
 	    if ( fld && !firstoptfld )
 		firstoptfld = fld;
@@ -128,6 +132,13 @@ bool uiIOObjSelWriteTranslator::isEmpty() const
 	    return false;
 
     return true;
+}
+
+
+const char* uiIOObjSelWriteTranslator::suggestedName() const
+{
+    uiIOObjTranslatorWriteOpts* optfld = getCurOptFld();
+    return optfld ? optfld->suggestedName() : "";
 }
 
 
