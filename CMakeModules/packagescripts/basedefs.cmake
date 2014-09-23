1b92200d70182801319645aa70a2b1720f7ebbc3
Nageswara Rao <nageswara.rao@dgbes.com>
2014-09-23 05:25:25 +0000

    od5.0: Included 'od_process_2dto3d' and 'od_BatchHosts' in basepackage definition.
    Summary:
    Added 'od_process_2dto3d' and 'od_BatchHosts' executables in
    basepackage definition
    
    Test Plan: Tested.
    
    Reviewers: raman, ranojay
    
    Reviewed By: ranojay
    
    Subscribers: ranojay
    
    Differential Revision: https://phabricator.dgbes.com:8443/D5749
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@36594 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/packagescripts/basedefs.cmake
+++ CMakeModules/packagescripts/basedefs.cmake
@@ -18,7 +18,8 @@ set( EXECLIST od_cbvs_browse od_glxinfo od_osgfileviewer lmhostid
 	      od_process_velocityconv od_process_volume od_ProgressViewer od_DispMsg
 	      od_FileBrowser od_SEGYExaminer od_SeisMMBatch od_ClusterProc
 	      od_process_2dgrid od_remexec od_remoteservice od_stratamp od_isopach
-	      od_ReportIssue od_uiReportIssue od_gmtexec od_madexec)
+	      od_ReportIssue od_uiReportIssue od_gmtexec od_madexec
+	      od_process_2dto3d od_BatchHosts )
 if ( UNIX )
     set( EXECLIST ${EXECLIST} od_batch_launcher )
 endif()
