f3f2dc4106436426f12ea34281cd39c19cdf03ab
Nanne Hemstra <nanne.hemstra@dgbes.com>
2014-09-30 11:40:18 +0000

    od5.0: Removed &'s
    Summary:
    &'s in menu item names were causing problems. See:
    http://bugs.dgbes.com/view.php?id=3375
    
    Test Plan: Full menu is back again.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D5839
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@36698 a985855d-d384-4b45-ae33-31a410998c3c

--- plugins/uiHorizonAttrib/uihorattribpi.cc
+++ plugins/uiHorizonAttrib/uihorattribpi.cc
@@ -115,15 +115,15 @@ uiHorAttribPIMgr::uiHorAttribPIMgr( uiODMain* a )
 void uiHorAttribPIMgr::updateMenu( CallBacker* )
 {
     uiODMenuMgr& mnumgr = appl_->menuMgr();
-    uiActionSeparString gridprocstr( "Create &Horizon Output" );
+    uiActionSeparString gridprocstr( "Create Horizon Output" );
     uiAction* itm = mnumgr.procMnu()->findAction( gridprocstr );
     if ( !itm || !itm->getMenu() ) return;
 
     if ( SI().has3D() )
-	itm->getMenu()->insertItem( new uiAction("&Stratal Amplitude ...",
+	itm->getMenu()->insertItem( new uiAction("Stratal Amplitude ...",
 				     mCB(this,uiHorAttribPIMgr,makeStratAmp)) );
 
-    itm->getMenu()->insertItem( new uiAction("&Isopach ...",
+    itm->getMenu()->insertItem( new uiAction("Isopach ...",
 			    mCB(this,uiHorAttribPIMgr,doIsopachThruMenu)) );
 }
 
