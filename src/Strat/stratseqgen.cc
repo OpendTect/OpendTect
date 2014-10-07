24f92f9a421706e33b27629f8673e3bc40e3f0b7
Arnaud Huck <arnaud.huck@dgbes.com>
2014-10-07 15:31:38 +0000

    od5.0: Fix for re-evaluation of math property
    Summary: see D5241
    
    Test Plan: tested in SynthRock
    
    Reviewers: bert
    
    Reviewed By: bert
    
    Differential Revision: https://phabricator.dgbes.com:8443/D5945
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@36808 a985855d-d384-4b45-ae33-31a410998c3c

--- src/Strat/stratseqgen.cc
+++ src/Strat/stratseqgen.cc
@@ -506,6 +506,7 @@ bool Strat::SingleLayerGenerator::genMaterial( Strat::LayerSequence& seq,
 
     Layer* newlay = new Layer( unit() );
     newlay->setContent( content() );
+	props_.prepareUsage();
 
     TypeSet<int> indexesofprsmath;
     TypeSet<int> correspondingidxinprops;
