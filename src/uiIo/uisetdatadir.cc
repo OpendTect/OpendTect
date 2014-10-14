31cb1418bca83e064b5b19e3065cba62fcb1b2a0
Arnaud Huck <arnaud.huck@dgbes.com>
2014-10-14 06:55:09 +0000

    od4.6: Changed error message when the selected dtect_data is not writable
    Summary: see D5995
    
    Test Plan: see D5995
    
    Reviewers: helene
    
    Reviewed By: helene
    
    Differential Revision: https://phabricator.dgbes.com:8443/D5996
    
    git-svn-id: https://svn.opendtect.org/od/branches/od4.6@36878 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiIo/uisetdatadir.cc
+++ src/uiIo/uisetdatadir.cc
@@ -54,8 +54,8 @@ static const char* doSetRootDataDir( const char* inpdatadir )
 
 uiSetDataDir::uiSetDataDir( uiParent* p )
 	: uiDialog(p,uiDialog::Setup("Set Data Directory",
-		    		     "Specify a data storage directory",
-		    		     "8.0.1"))
+				     "Specify a data storage directory",
+				     "8.0.1"))
 	, curdatadir_(GetBaseDataDir())
 {
     const bool oldok = IOMan::isValidDataRoot( curdatadir_ );
@@ -173,7 +173,7 @@ bool uiSetDataDir::setRootDataDir( uiParent* par, const char* inpdatadir )
 #ifdef __win__
 	BufferString progfiles=GetSpecialFolderLocation(CSIDL_PROGRAM_FILES);
 
-	if ( ( progfiles.size() && 
+	if ( ( progfiles.size() &&
 	       !strncasecmp(progfiles, datadir, strlen(progfiles)) )
 	  || strstr( datadir, "Program Files" )
 	  || strstr( datadir, "program files" )
@@ -190,6 +190,9 @@ bool uiSetDataDir::setRootDataDir( uiParent* par, const char* inpdatadir )
     {
 	if ( !File::isDirectory(datadir) )
 	    mErrRet( "A file (not a directory) with this name already exists" )
+
+	if ( !File::isWritable(datadir) )
+	    mErrRet( "Selected directory is not writable" )
 
 	if ( File::exists(omffnm) )
 	{
@@ -260,12 +263,12 @@ void uiSetDataDir::offerUnzipSurv( uiParent* par, const char* datadir )
     if ( !uigc.go() || uigc.choice() == 0 )
 	return;
 
-    if ( (havedemosurv && uigc.choice() == 2) || 
+    if ( (havedemosurv && uigc.choice() == 2) ||
          (!havedemosurv && uigc.choice() == 1))
     {
         uiFileDialog dlg( par, true, "", "*.zip", "Select zip file" );
         dlg.setDirectory( datadir );
-        if ( !dlg.go() ) 
+        if ( !dlg.go() )
             return;
 
         zipfilenm = dlg.fileName();
