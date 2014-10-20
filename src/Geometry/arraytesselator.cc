d5e4db285e889768a2fbce9cd8bd4d17a4b12b2b
Ding Zheng <ding.zheng@dgbes.com>
2014-10-20 13:29:49 +0000

    Changes in arraytesselator
    Summary:
    1. Moved implementation code to the new file arraytesselator.cc
    2. Made code style changes.
    3. Change triangle from triangle strip to triangles
    
    Test Plan: Compiles and Tests fine
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D6003
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@36973 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/Geometry/arraytesselator.cc
@@ -0,0 +1,160 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        D.Zheng
+ Date:          Sept 2014
+________________________________________________________________________
+
+-*/
+static const char* rcsID mUsedVar = "$Id$";
+
+#include "arraytesselator.h"
+
+
+namespace Geometry
+{
+
+ArrayTesselator::ArrayTesselator( const float* data, int rowsz, int colsz, 
+				  const StepInterval<int>& rrg, 
+				  const StepInterval<int>& crg )
+    : data_( data )
+    , datarowsize_( rowsz )
+    , datacolsize_( colsz )
+    , rowrange_( rrg )
+    , colrange_( crg )
+{}		      
+
+
+ArrayTesselator::ArrayTesselator( const Array2D<float>& data,
+				  const StepInterval<int>& rrg, 
+				  const StepInterval<int>& crg )
+    : data_( data.getData() )
+    , datarowsize_( data.info().getSize(0) )
+    , datacolsize_( data.info().getSize(1) )
+    , rowrange_( rrg )
+    , colrange_( crg )
+{}		      
+
+
+#define mGlobleIdx(row,col) row*datacolsize_+col 
+
+#define mAddTriangle( ci0, ci1, ci2 ) \
+stripcis_ += ci0; \
+stripcis_ += ci1; \
+stripcis_ += ci2
+
+
+bool ArrayTesselator::doWork( od_int64 start, od_int64 stop, int )
+{
+    const int glastrowidx = datarowsize_ - 1;
+    const int glastcolidx = datacolsize_ - 1;
+    const int colsz = colrange_.nrSteps()+1;
+
+    for ( od_int64 idx=start; idx<=stop && shouldContinue(); idx++ )
+    {
+	const int currow = mCast(int,(idx/colsz)*rowrange_.step + 
+							    rowrange_.start);
+	const int curcol = mCast(int,(idx%colsz)*colrange_.step + 
+							    colrange_.start);
+	if ( currow > glastrowidx || curcol > glastcolidx )
+	    continue;
+
+	const bool islastrow = currow == glastrowidx;
+	const bool islastcol = curcol == glastcolidx;
+	int nextrow = currow + rowrange_.step;
+  	if ( nextrow>glastrowidx ) nextrow = glastrowidx;
+  	int nextcol = curcol + colrange_.step;
+ 	if ( nextcol>glastcolidx ) nextcol = glastcolidx;
+
+	const int c11 = mGlobleIdx(currow,curcol);
+	const int c12 = mGlobleIdx(currow,nextcol);
+	const int c21 = mGlobleIdx(nextrow,curcol);
+	const int c22 = mGlobleIdx(nextrow,nextcol);
+
+	bool def11 = !mIsUdf(data_[c11]);
+	bool def12 = islastcol ? false : !mIsUdf(data_[c12]);
+	bool def21 = islastrow ? false : !mIsUdf(data_[c21]);
+	bool def22 = (islastrow || islastcol) ? false : !mIsUdf(data_[c22]);
+	const int nrdefined = def11 + def12 + def21 + def22;
+	if ( !nrdefined )
+	    continue;
+	
+	if ( nrdefined>2 )
+	{
+    	    if ( nrdefined==4 )
+    	    {
+		mAddTriangle(c21,c22,c11);
+		mAddTriangle(c11,c22,c12);
+	    }
+	    else if ( !def11 )
+	    {
+		mAddTriangle(c21,c22,c12);
+	    }
+	    else if ( !def12 )
+	    {
+		mAddTriangle(c21,c22,c11);
+	    }
+	    else if ( !def21 )
+	    {
+		mAddTriangle(c11,c22,c12);
+	    }
+	    else
+	    {
+		mAddTriangle(c11,c21,c12);
+	    }
+
+	}
+	else if ( def11 )
+	{
+	    const int prerow = currow - rowrange_.step;
+	    const int precol = curcol - colrange_.step;
+
+	    const int c01 = mGlobleIdx( prerow, curcol );
+    	    const int c10 = mGlobleIdx( currow, precol );
+	    bool def01 = prerow<0 ? false : !mIsUdf(data_[c01]);
+	    bool def10 = precol<0 ? false : !mIsUdf(data_[c10]);
+	    if ( nrdefined==1 )
+	    {
+		if ( !def01 && !def10 )
+		    pointcis_ += c11;
+	    }
+	    else
+	    {
+		if ( def12 && !def01 )
+		{
+		    const int c02 = mGlobleIdx(prerow,nextcol);
+		    bool def02 = prerow<0 || islastcol ? false 
+						       : !mIsUdf(data_[c02]);
+		    if ( !def02 )
+		    {
+			linecis_ += c11;
+			linecis_ += c12;
+		    }
+		}
+		else if ( def21 && !def10 )
+		{
+		    const int c20 = mGlobleIdx(nextrow,precol);
+		    bool def20 = islastrow || precol<0 ? false 
+						       : !mIsUdf(data_[c20]);
+		    if ( !def20 )
+		    {
+			linecis_ += c11;
+			linecis_ += c21;
+		    }
+		}
+	    }
+	}
+
+	addToNrDone( 1 );
+    }
+
+    return true;
+} 
+
+
+
+
+
+
+};
