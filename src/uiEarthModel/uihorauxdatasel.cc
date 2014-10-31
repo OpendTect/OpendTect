8f7f6bc826c4e248c23c31006924c6d6f0a0a251
Nanne Hemstra <nanne.hemstra@dgbes.com>
2014-10-31 17:34:10 +0000

    Code layout changes
    git-svn-id: https://svn.opendtect.org/od/trunk@37119 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiEarthModel/uihorauxdatasel.cc
+++ src/uiEarthModel/uihorauxdatasel.cc
@@ -27,85 +27,91 @@ static const char* rcsID mUsedVar = "$Id$";
 class uiHorizonAuxDataDlg: public uiDialog
 {
 public:
-		uiHorizonAuxDataDlg( uiParent* p, 
-			const uiHorizonAuxDataSel::HorizonAuxDataInfo& info )
-		: uiDialog( p, uiDialog::Setup("Horizon data selection", 0,
-                                            mNoHelpKey ))
-		, auxidx_( 0 )
-		, selmid_( 0 )
-		, auxinfo_( new uiHorizonAuxDataSel::HorizonAuxDataInfo(info) )
-		{
-		    uiGroup* grp = new uiGroup( this, "Group" );
-		    uiLabel* horlabel = new uiLabel(grp, 
-                                                    uiStrings::sHorizons(true));
-		    horlistfld_ = new uiListBox( grp );
-		    horlistfld_->addItems( auxinfo_->hornms_ );
-		    horlistfld_->attach( ensureBelow, horlabel );
-		    horlistfld_->setHSzPol( uiObject::Wide );
-		    horlistfld_->selectionChanged.notify( 
-			    mCB(this,uiHorizonAuxDataDlg,selChg) );
-		    
-		    datalistfld_ = new uiListBox( grp );
-		    datalistfld_->attach( rightTo, horlistfld_ );
-		    datalistfld_->setHSzPol( uiObject::Wide );
-		    datalistfld_->selectionChanged.notify(
-			    mCB(this, uiHorizonAuxDataDlg, auxidxChg) );
-		    
-		    uiLabel* directlabel = new uiLabel( grp, "Available data" );
-		    directlabel->attach( alignedAbove, datalistfld_ );
-		    directlabel->attach( rightTo, horlabel );
-		}
-		
-		~uiHorizonAuxDataDlg()	{ delete auxinfo_; }
-const MultiID&	selected() const	{ return selmid_; }
-int		dataidx()		{ return auxidx_; }
-void		setSelection( const MultiID& mid, int auxidx )
-    		{
-		    if ( auxidx_==auxidx && selmid_==mid )
-			return;
+uiHorizonAuxDataDlg( uiParent* p,
+		     const uiHorizonAuxDataSel::HorizonAuxDataInfo& info )
+    : uiDialog( p, uiDialog::Setup("Horizon data selection",0,mNoHelpKey ))
+    , auxidx_( 0 )
+    , selmid_( 0 )
+    , auxinfo_( new uiHorizonAuxDataSel::HorizonAuxDataInfo(info) )
+{
+    uiGroup* grp = new uiGroup( this, "Group" );
+    uiLabel* horlabel = new uiLabel(grp,
+				    uiStrings::sHorizons(true));
+    horlistfld_ = new uiListBox( grp );
+    horlistfld_->addItems( auxinfo_->hornms_ );
+    horlistfld_->attach( ensureBelow, horlabel );
+    horlistfld_->setHSzPol( uiObject::Wide );
+    horlistfld_->selectionChanged.notify(
+	    mCB(this,uiHorizonAuxDataDlg,selChg) );
 
-		    auxidx_ = auxidx;
-		    if ( auxidx_>=0 )
-			datalistfld_->setCurrentItem( auxidx_ );
+    datalistfld_ = new uiListBox( grp );
+    datalistfld_->attach( rightTo, horlistfld_ );
+    datalistfld_->setHSzPol( uiObject::Wide );
+    datalistfld_->selectionChanged.notify(
+	    mCB(this, uiHorizonAuxDataDlg, auxidxChg) );
 
-		    const bool hchanged = !selmid_.isEmpty() && selmid_==mid;
-		    selmid_ = mid;
-		    for ( int idx=0; idx<auxinfo_->mids_.size(); idx++ )
-		    {
-			if ( auxinfo_->mids_[idx]==selmid_ )
-			{
-			    horlistfld_->setCurrentItem( idx );
-			    return;
-			}
-		    }	    
-		    
-		    if ( hchanged ) selChg( 0 );
-		}
+    uiLabel* directlabel = new uiLabel( grp, "Available data" );
+    directlabel->attach( alignedAbove, datalistfld_ );
+    directlabel->attach( rightTo, horlabel );
+}
+
+~uiHorizonAuxDataDlg()
+{ delete auxinfo_; }
+
+const MultiID&	selected() const
+{ return selmid_; }
+
+int dataidx()
+{ return auxidx_; }
+
+
+void setSelection( const MultiID& mid, int auxidx )
+{
+    if ( auxidx_==auxidx && selmid_==mid )
+	return;
+
+    auxidx_ = auxidx;
+    if ( auxidx_>=0 )
+	datalistfld_->setCurrentItem( auxidx_ );
+
+    const bool hchanged = !selmid_.isEmpty() && selmid_==mid;
+    selmid_ = mid;
+    for ( int idx=0; idx<auxinfo_->mids_.size(); idx++ )
+    {
+	if ( auxinfo_->mids_[idx]==selmid_ )
+	{
+	    horlistfld_->setCurrentItem( idx );
+	    return;
+	}
+    }
+
+    if ( hchanged ) selChg( 0 );
+}
 
 protected:
-void		auxidxChg( CallBacker* )
-		{ auxidx_ = datalistfld_->currentItem(); }
+void auxidxChg( CallBacker* )
+{ auxidx_ = datalistfld_->currentItem(); }
 
-void		selChg( CallBacker* )
-		{
-		    const int hidx = horlistfld_->currentItem();
-		    if ( !auxinfo_->mids_.size() || hidx<0 )
-			return;
+void selChg( CallBacker* )
+{
+    const int hidx = horlistfld_->currentItem();
+    if ( !auxinfo_->mids_.size() || hidx<0 )
+	return;
 
-		    int oldidx =  auxinfo_->mids_[hidx]!=selmid_ ? 0 : auxidx_;
-		    selmid_ = auxinfo_->mids_[hidx];
+    int oldidx =  auxinfo_->mids_[hidx]!=selmid_ ? 0 : auxidx_;
+    selmid_ = auxinfo_->mids_[hidx];
 
-		    datalistfld_->setEmpty();
-		    datalistfld_->addItems( auxinfo_->auxdatanms_[hidx] ); 
-		    if ( oldidx>=0 )
-			datalistfld_->setCurrentItem( oldidx );
-		}
+    datalistfld_->setEmpty();
+    datalistfld_->addItems( auxinfo_->auxdatanms_[hidx] );
+    if ( oldidx>=0 )
+	datalistfld_->setCurrentItem( oldidx );
+}
 
-uiListBox*	horlistfld_;
-uiListBox*	datalistfld_;
-int		auxidx_;
-MultiID		selmid_;
-uiHorizonAuxDataSel::HorizonAuxDataInfo* auxinfo_;
+    uiListBox*			horlistfld_;
+    uiListBox*			datalistfld_;
+    int				auxidx_;
+    MultiID			selmid_;
+    uiHorizonAuxDataSel::HorizonAuxDataInfo* auxinfo_;
 };
 
 
@@ -117,11 +123,11 @@ uiHorizonAuxDataSel::HorizonAuxDataInfo::HorizonAuxDataInfo( bool load )
     PtrMan<CtxtIOObj> allhorio =  mMkCtxtIOObj(EMHorizon3D);
     const IODir iodir( allhorio->ctxt.getSelKey() );
     const IODirEntryList horlist( iodir, allhorio->ctxt );
-	
+
     for ( int idx=0; idx<horlist.size(); idx++ )
     {
 	const IOObj* obj = horlist[idx]->ioobj_;
-	EM::IOObjInfo eminfo( obj->key() ); 
+	EM::IOObjInfo eminfo( obj->key() );
 	BufferStringSet attrnms;
 	eminfo.getAttribNames( attrnms );
 	if ( attrnms.size() )
@@ -143,14 +149,14 @@ uiHorizonAuxDataSel::HorizonAuxDataInfo::HorizonAuxDataInfo(
 }
 
 
-uiHorizonAuxDataSel::HorizonAuxDataInfo::~HorizonAuxDataInfo() 
-{ 
-    mids_.erase(); 
-    auxdatanms_.erase(); 
+uiHorizonAuxDataSel::HorizonAuxDataInfo::~HorizonAuxDataInfo()
+{
+    mids_.erase();
+    auxdatanms_.erase();
 }
 
 
-uiHorizonAuxDataSel::uiHorizonAuxDataSel( uiParent* p, const MultiID& mid, 
+uiHorizonAuxDataSel::uiHorizonAuxDataSel( uiParent* p, const MultiID& mid,
 	int auxidx, const HorizonAuxDataInfo* auxinfo )
     : uiGroup( p )
     , selmid_( mid )
@@ -161,8 +167,8 @@ uiHorizonAuxDataSel::uiHorizonAuxDataSel( uiParent* p, const MultiID& mid,
     selbut_ = new uiPushButton( this, uiStrings::sSelect(true),
 	    mCB(this,uiHorizonAuxDataSel,selCB), false );
     selbut_->attach( rightOf, horfld_ );
- 
-    StringListInpSpec str; 
+
+    StringListInpSpec str;
     PtrMan<IOObj> obj = IOM().get(mid);
     const bool hasobj = !mid.isEmpty() && obj;
     if ( hasobj )
@@ -172,17 +178,17 @@ uiHorizonAuxDataSel::uiHorizonAuxDataSel( uiParent* p, const MultiID& mid,
 	eminfo.getAttribNames( attrnms );
 	for ( int idx=0; idx<attrnms.size(); idx++ )
 	    str.addString( attrnms.get(idx) );
-	
+
 	horfld_->setText( obj->name() );
-    }    
-    
-    auxfld_ = new uiGenInput( this, tr("Horizon Data"), str ); 
+    }
+
+    auxfld_ = new uiGenInput( this, tr("Horizon Data"), str );
     auxfld_->attach( rightOf, selbut_ );
     auxfld_->setPrefWidthInChar( 60 );
     auxfld_->valuechanged.notify(
 	    mCB(this,uiHorizonAuxDataSel,auxidxChg) );
     if ( hasobj && auxidx>=0 ) auxfld_->setValue(auxidx);
-    
+
     setHAlignObj( horfld_ );
     if ( auxinfo )
     {
@@ -198,21 +204,21 @@ uiHorizonAuxDataSel::uiHorizonAuxDataSel( uiParent* p, const MultiID& mid,
 }
 
 
-void uiHorizonAuxDataSel::auxidxChg( CallBacker* ) 
+void uiHorizonAuxDataSel::auxidxChg( CallBacker* )
 { auxidx_ = auxfld_->getIntValue(); }
 
 
-void uiHorizonAuxDataSel::selCB( CallBacker* ) 
-{ 
+void uiHorizonAuxDataSel::selCB( CallBacker* )
+{
     dlg_->setSelection( selmid_, auxidx_ );
     dlg_->go();
 
     selmid_ = dlg_->selected();
     PtrMan<IOObj> obj = IOM().get(selmid_);
     if ( !obj )	return;
- 
-    horfld_->setText( obj->name() );	    
-    
+
+    horfld_->setText( obj->name() );
+
     EM::IOObjInfo eminfo( selmid_ );
     BufferStringSet attrnms;
     eminfo.getAttribNames( attrnms );
@@ -220,9 +226,9 @@ void uiHorizonAuxDataSel::selCB( CallBacker* )
     StringListInpSpec str;
     for ( int idx=0; idx<attrnms.size(); idx++ )
 	str.addString( attrnms.get(idx) );
-    auxfld_->clear(); 
+    auxfld_->clear();
     auxfld_->newSpec( str, 0 );
-					    
+
     auxidx_ = dlg_->dataidx();
     auxfld_->setValue( auxidx_ );
 }
