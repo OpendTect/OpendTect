10042a0e87a83cf54d85249b3c07ddab6e2025ea
Jaap Glas <jaap.glas@dgbes.com>
2014-11-04 16:45:47 +0000

    Bugfix: initial red-channel attribute (z-value) of color blended horizon texture was displayed corrupted when using shaders (Mantis 3479). (PORTING FROM od TO od5.0 UNCHANGED)
    Summary:
    Initial red-channel attribute (z-value) of color blended horizon texture was
    displayed corrupted when using shaders. Textures objects are initialized with
    ColTabTextureChannel2RGBA first, which is overruled by RGBATextureChannel2RGBA
    in case of color blending. These two do have different number of texture bands,
    functionality which is new in OD-OSG. This makes that texture data needs to be
    remapped if an initial attribute was mapped already. Horizons do have such an
    initial attribute.
    
    Test Plan:
    Color-blended horizons now display their initial z-value attribute in red
    correctly, both with and without using shaders.
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D6218
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@37155 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visBase/vistexturechannels.cc
+++ src/visBase/vistexturechannels.cc
@@ -864,6 +864,8 @@ bool TextureChannels::setMappedData( int channel, int version,
 
 bool TextureChannels::setChannels2RGBA( TextureChannel2RGBA* nt )
 {
+    const int oldnrtexturebands = nrTextureBands();
+
     if ( tc2rgba_ )
     {
 	tc2rgba_->setChannels( 0 );
@@ -878,7 +880,12 @@ bool TextureChannels::setChannels2RGBA( TextureChannel2RGBA* nt )
 	tc2rgba_->ref();
 
 	for ( int channel=0; channel<nrChannels(); channel++ )
+	{
+	    if ( oldnrtexturebands != nrTextureBands() )
+		reMapData( channel, true, 0 );
+
 	    update( channel );
+	}
     }
 
     return true;
