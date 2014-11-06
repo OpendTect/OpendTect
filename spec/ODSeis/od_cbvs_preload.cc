d9935056d4b5587e04d1e4d3e1581a704c44d4f8
Nanne Hemstra <nanne.hemstra@dgbes.com>
2014-11-06 19:03:20 +0000

    Added for testing
    git-svn-id: https://svn.opendtect.org/od/trunk@37183 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ spec/ODSeis/od_cbvs_preload.cc
@@ -0,0 +1,73 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : A.H. Bril
+ * DATE     : 2000
+ * RCS      : $Id$
+-*/
+
+static const char* rcsID mUsedVar = "$Id$";
+
+#include "seistrc.h"
+#include "seiscbvs.h"
+#include "seisselectionimpl.h"
+#include "ioman.h"
+#include "ioobj.h"
+#include "moddepmgr.h"
+#include "ptrman.h"
+#include "timefun.h"
+
+#include <iostream>
+#include <math.h>
+
+#include "prog.h"
+#include "seisioobjinfo.h"
+#include "seisparallelreader.h"
+
+
+int main( int argc, char** argv )
+{
+    SetProgramArgs( argc, argv );
+    if ( argc < 2 )
+    {
+	std::cerr << "Usage: " << argv[0]
+	     << " objectid method\n";
+	std::cerr << "method: 0-parallel 1-sequential"
+		  << std::endl;
+	ExitProgram( 1 );
+    }
+
+    OD::ModDeps().ensureLoaded( "Seis" );
+    const MultiID seismid( argv[1] );
+    PtrMan<IOObj> ioobj = IOM().get( seismid );
+    if ( !ioobj )
+    {
+	std::cerr << "Cannot read seismic data with ID " << seismid.buf()
+		  << std::endl;
+	ExitProgram( 1 );
+    }
+
+    std::cerr << "Preloading " << ioobj->name() << std::endl;
+    SeisIOObjInfo info( *ioobj );
+    TrcKeyZSampling tkzs; tkzs.init();
+    info.getRanges( tkzs );
+
+    const int method = atoi( argv[2] );
+
+    Time::Counter counter;
+    counter.start();
+    if ( method==0 )
+    {
+	Seis::ParallelReader rdr( *ioobj, tkzs );
+	rdr.execute();
+    }
+    else
+    {
+	TypeSet<int> comps; comps += 0;
+	Seis::SequentialReader rdr( *ioobj, comps, tkzs );
+	rdr.execute();
+    }
+
+    std::cerr << "Total time: " << counter.elapsed() << std::endl;
+
+    ExitProgram( 0 ); return 0;
+}
