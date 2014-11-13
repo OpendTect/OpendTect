e409c4ac1d6c36905bfa528435895b9ca54ba99a
Bert Bril <bert.bril@dgbes.com>
2014-11-13 17:15:57 +0000

    Just a bit of layout improvement
    git-svn-id: https://svn.opendtect.org/od/trunk@37247 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Well/welldisp.h
+++ include/Well/welldisp.h
@@ -26,8 +26,8 @@ ________________________________________________________________________
 namespace Well
 {
 
-inline const char* sKey2DDispProp()  { return "2D Display"; }
-inline const char* sKey3DDispProp() { return "3D Display"; }
+inline const char* sKey2DDispProp()	{ return "2D Display"; }
+inline const char* sKey3DDispProp()	{ return "3D Display"; }
 
 /*!
 \brief Display properties of a well.
@@ -39,18 +39,17 @@ public:
 
 			DisplayProperties(const char* subj = sKey3DDispProp());
 			DisplayProperties(const Well::DisplayProperties& dp)
-			{ *this = dp;}			   
+			{ *this = dp;}
 
     virtual		~DisplayProperties();
 
-    DisplayProperties& 	operator = (const DisplayProperties& dp);
+    DisplayProperties&	operator = (const DisplayProperties& dp);
 
     mStruct(Well) BasicProps
     {
 			BasicProps( int sz=1 )
-			    : size_(sz)			
-			    , color_(Color(0,0,255))
-			    {}
+			    : size_(sz)
+			    , color_(Color(0,0,255))	{}
 
 	Color		color_;
 	int		size_;
@@ -61,17 +60,17 @@ public:
 	void		useRightPar(const IOPar&);
 	void		fillLeftPar(IOPar&) const;
 	void		fillRightPar(IOPar&) const;
-	virtual const char* subjectName() const		= 0;
 
+	virtual const char* subjectName() const		= 0;
 
     protected:
 
 	virtual void	doUsePar(const IOPar&)		{}
 	virtual void	doFillPar(IOPar&) const		{}
-	virtual void	doUseLeftPar(const IOPar&) {}
-	virtual void	doFillRightPar(IOPar&) const {}
-	virtual void	doUseRightPar(const IOPar&){}
-	virtual void	doFillLeftPar(IOPar&) const{}
+	virtual void	doUseLeftPar(const IOPar&)	{}
+	virtual void	doFillRightPar(IOPar&) const	{}
+	virtual void	doUseRightPar(const IOPar&)	{}
+	virtual void	doFillLeftPar(IOPar&) const	{}
 
     };
 
@@ -80,11 +79,11 @@ public:
 			Track()
 			    : BasicProps(1)
 			    , dispabove_(true)
-			    , dispbelow_(true)	
+			    , dispbelow_(true)
 			    , font_(10)
-		       	    {}
+			    {}
 
-	virtual const char* subjectName() const		{ return "Track"; }
+	virtual const char* subjectName() const	{ return "Track"; }
 
 	bool		dispabove_;
 	bool		dispbelow_;
@@ -94,6 +93,7 @@ public:
 
 	virtual void	doUsePar(const IOPar&);
 	virtual void	doFillPar(IOPar&) const;
+
     };
 
     mStruct(Well) Markers : public BasicProps
@@ -101,18 +101,19 @@ public:
 
 			Markers()
 			    : BasicProps(3)
-			    , shapeint_(0)	
-			    , cylinderheight_(1)			
+			    , shapeint_(0)
+			    , cylinderheight_(1)
 			    , issinglecol_(false)
 			    , font_(10)
-			    , samenmcol_(true)	 
+			    , samenmcol_(true)
 			    {}
 
 	virtual const char* subjectName() const	{ return "Markers"; }
+
 	int		shapeint_;
 	int		cylinderheight_;
-	bool 		issinglecol_;
-	FontData 	font_;
+	bool		issinglecol_;
+	FontData	font_;
 	Color		nmcol_;
 	bool		samenmcol_;
 	BufferStringSet	selmarkernms_;
@@ -129,46 +130,46 @@ public:
 			    : cliprate_(0)
 			    , fillname_("none")
 			    , fillrange_(mUdf(float),mUdf(float))
-			    , isleftfill_(false) 
+			    , isleftfill_(false)
 			    , isrightfill_(false)
 		            , isdatarange_(true)
-			    , islogarithmic_(false) 
-			    , islogreverted_(false) 
+			    , islogarithmic_(false)
+			    , islogreverted_(false)
 			    , issinglecol_(false)
-			    , iswelllog_(true)	
+			    , iswelllog_(true)
 			    , name_("none")
-		            , logwidth_(250 * 
-				((int)(SI().xyInFeet() ? mToFeetFactorF:1)))	
+		            , logwidth_(250 *
+				((int)(SI().xyInFeet() ? mToFeetFactorF:1)))
 			    , range_(mUdf(float),mUdf(float))
 			    , repeat_(5)
 			    , repeatovlap_(50)
 		            , seiscolor_(Color::White())
 			    , seqname_("Rainbow")
-			    , iscoltabflipped_(false)	
+			    , iscoltabflipped_(false)
 			    , style_( 0 )
-			    {}		 
+			    {}
 
-	virtual const char* subjectName() const 	{ return "Log"; }
+	virtual const char* subjectName() const	{ return "Log"; }
 
 	BufferString	name_;
 	BufferString	fillname_;
 	bool		iswelllog_;
-	float           cliprate_;      
-	Interval<float> range_;        
-	Interval<float> fillrange_;       
-	bool 		isleftfill_;				 
-	bool 		isrightfill_;				 
+	float           cliprate_;
+	Interval<float> range_;
+	Interval<float> fillrange_;
+	bool		isleftfill_;
+	bool		isrightfill_;
 	bool            islogarithmic_;
-	bool 		islogreverted_; 
+	bool		islogreverted_;
 	bool            issinglecol_;
 	bool            isdatarange_;
-	bool 		iscoltabflipped_;
+	bool		iscoltabflipped_;
 	int             repeat_;
 	float           repeatovlap_;
 	Color           linecolor_;
-	Color 		seiscolor_;
+	Color		seiscolor_;
 	BufferString    seqname_;
-	int 		logwidth_;
+	int		logwidth_;
 	int		style_;
 
     protected:
@@ -189,12 +190,15 @@ public:
     static DisplayProperties&	defaults();
     static void		commitDefaults();
 
-    mStruct(Well) LogCouple 	{ Log left_; Log right_; };
+    mStruct(Well) LogCouple { Log left_, right_; };
     ObjectSet<LogCouple> logs_;
 
-    virtual const char* subjectName() const 	{ return subjectname_.buf(); }
+    virtual const char* subjectName() const	{ return subjectname_.buf(); }
+
 protected:
-    BufferString 	subjectname_;
+
+    BufferString	subjectname_;
+
 };
 
 } // namespace
