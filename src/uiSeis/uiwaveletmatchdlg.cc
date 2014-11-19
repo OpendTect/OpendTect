b04073a1e3cf77568a89c547a4385aad8bd8c5d4
Nanne Hemstra <nanne.hemstra@dgbes.com>
2014-11-19 19:33:50 +0000

    Solve toeplitz system. Still need to QC.
    git-svn-id: https://svn.opendtect.org/od/trunk@37293 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiSeis/uiwaveletmatchdlg.cc
+++ src/uiSeis/uiwaveletmatchdlg.cc
@@ -57,7 +57,7 @@ uiWaveletMatchDlg::uiWaveletMatchDlg( uiParent* p )
 
     IntInpSpec filterrg( 21 ); filterrg.setLimits( StepInterval<int>(3,99,2) );
     filterszfld_ = new uiGenInput( this, "Filter length", filterrg );
-    filterszfld_->valuechanged.notify( mCB(this,uiWaveletMatchDlg,filterSzCB) );
+    filterszfld_->valuechanging.notify( mCB(this,uiWaveletMatchDlg,filterSzCB));
     filterszfld_->attach( rightOf, wvlt1fld_ );
 
     wvltoutfld_ = new uiWaveletSel( this, false, "Output Wavelet" );
@@ -87,6 +87,51 @@ void uiWaveletMatchDlg::filterSzCB( CallBacker* )
 }
 
 
+static void solveSymToeplitzsystem( int systdim, float* r, float* g,
+				    float* f, float* a)
+{
+    if ( r[0] < 0.001 && r[0] > -0.001 ) return; //check mIsZero(r[0],0.001)
+
+    if ( systdim<1 )
+	return;
+
+    a[0] = 1;
+    float v = r[0];
+    f[0] = g[0]/r[0];
+
+    for ( int j=1; j<systdim; j++ )
+    {
+	a[j] = 0;
+	f[j] = 0;
+	float tmpvar = 0;		// corresponds to e in Clearbout, FGDP
+	for ( int i=0; i<j; i++ )
+	    tmpvar += a[i]*r[j-i];
+
+	float coef = tmpvar/v;		// corresponds to c in Clearbout, FGDP
+	v -= coef*tmpvar;
+	if ( v < 0.001 && v > -0.001 )
+	    v = 0.001;
+
+	for ( int i=0; i<=j/2; i++ )
+	{
+	    float bot = a[j-i]-coef*a[i];
+	    a[i] -= coef*a[j-i];
+	    a[j-i] = bot;
+	}
+
+	/* use a and v above to get f[i], i = 0,1,2,...,j */
+
+	float w; int i;
+	for ( i=0,w=0; i<j; i++ )
+	    w += f[i]*r[j-i];
+
+	coef = (w-g[j])/v;
+	for ( i=0; i<=j; i++ )
+	    f[i] -= coef*a[j-i];
+    }
+}
+
+
 bool uiWaveletMatchDlg::calcFilter()
 {
     // Call to genericCrossCorrelation didn't compile: CDash Error
@@ -104,14 +149,27 @@ bool uiWaveletMatchDlg::calcFilter()
     const int refsz = refwvlt->size();
     genericCrossCorrelation( refsz, 0, wref,
 			     refsz, 0, wref,
-			     filtersz, -filtersz/2, autoref );
+			     filtersz, 0, autoref );
 
     const float* wtar = tarwvlt->samples();
     const int tarsz = tarwvlt->size();
     genericCrossCorrelation( refsz, 0, wref,
 			     tarsz, 0, wtar,
 			     filtersz, 0, crossref );
+
+    mAllocVarLenArr( float, wiener, filtersz );
+    mAllocVarLenArr( float, spiker, filtersz );
+    solveSymToeplitzsystem( filtersz, autoref, crossref, wiener, spiker );
+
+    Interval<float> intv( -filtersz/2, filtersz/2 );
+    wvltoutdisp_->setVals( intv, wiener, filtersz );
+
+    mAllocVarLenArr(float,wqc,filtersz)
+    GenericConvolve(refsz,0,wref,filtersz,0,wiener,filtersz,0,wqc);
+    wvlt0disp_->setY2Vals( intv, wqc, filtersz );
+
     */
+
     return true;
 }
 
