63f468493dccec9ecbde8d621d710de7dbeaf515
Nanne Hemstra <nanne.hemstra@dgbes.com>
2014-12-02 14:26:25 +0000

    od5.0: Fixed layout problem
    Summary:
    See D6435
    See http://bugs.dgbes.com/view.php?id=2555
    
    Test Plan: See D6435
    
    Reviewers: helene
    
    Reviewed By: helene
    
    Subscribers: helene
    
    Differential Revision: https://phabricator.dgbes.com:8443/D6448
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@37416 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiAttributes/uieventattrib.cc
+++ src/uiAttributes/uieventattrib.cc
@@ -43,7 +43,7 @@ static const char* outpstrs[] =
     "Peakedness",
     "Steepness",
     "Asymmetry",
-    0	
+    0
 };
 
 
@@ -51,8 +51,9 @@ mInitAttribUI(uiEventAttrib,Event,"Event",sKeyPatternGrp())
 
 
 uiEventAttrib::uiEventAttrib( uiParent* p, bool is2d )
-        : uiAttrDescEd(p,is2d, mODHelpKey(mEventAttribHelpID) )
-	  
+    : uiAttrDescEd(p,is2d, mODHelpKey(mEventAttribHelpID) )
+    , tonextlblfld(0)
+
 {
     inpfld = createInpFld( is2d );
 
@@ -66,28 +67,24 @@ uiEventAttrib::uiEventAttrib( uiParent* p, bool is2d )
     evtypefld->attach( alignedBelow, issinglefld );
     evtypefld->valuechanged.notify( mCB(this,uiEventAttrib,isGateSel) );
     evtypefld->display(false);
-    
-    tonextlblfld = new uiLabel( this,
-	    		"Compute distance between 2 consecutive events" );
-    tonextlblfld->attach( leftAlignedBelow, evtypefld );
 
-    outampfld = new uiGenInput( this, "",
-	       BoolInpSpec(true,"Compute distance between 2 consecutive events",
-		   		"Output Amplitude") );
+    outampfld = new uiGenInput( this, "Compute",
+	       BoolInpSpec(true,"Distance between 2 consecutive events",
+				"Output Amplitude") );
     outampfld->valuechanged.notify( mCB(this,uiEventAttrib,outAmpSel) );
-    outampfld->attach( leftAlignedBelow, evtypefld );
+    outampfld->attach( alignedBelow, evtypefld );
 
     tonextfld = new uiGenInput( this, "starting from",
-	    			BoolInpSpec(true,"Top","Bottom") );
-    tonextfld->attach( centeredBelow, tonextlblfld );
-    tonextfld->display(false);
-    
+				BoolInpSpec(true,"Top","Bottom") );
+    tonextfld->attach( alignedBelow, outampfld );
+    tonextfld->display( false );
+
     outpfld = new uiGenInput( this, "Output", StringListInpSpec(outpstrs) );
     outpfld->attach( alignedBelow, issinglefld);
 
     gatefld = new uiGenInput( this, gateLabel(),
-	    		      FloatInpIntervalSpec().setName("Z start",0)
-			      			    .setName("Z stop",1) );
+			      FloatInpIntervalSpec().setName("Z start",0)
+						    .setName("Z stop",1) );
     gatefld->attach( alignedBelow, tonextfld);
     gatefld->display(false);
 
@@ -100,27 +97,29 @@ void uiEventAttrib::isSingleSel( CallBacker* )
     const bool issingle = issinglefld-> getBoolValue();
     const int val = evtypefld-> getIntValue();
     const bool iszc = !issingle && ( val==3 || val==4 || val==5 );
+    if ( iszc ) outampfld->setValue( true );
     const bool outamp = !outampfld-> getBoolValue();
     evtypefld->display( !issingle );
-    tonextlblfld->display( iszc );
     tonextfld->display( !issingle && val != 6 && val != 7 && !outamp );
     gatefld->display( !issingle && ( val == 6 || val == 7 ) );
     outpfld->display( issingle );
-    outampfld->display( !issingle && !iszc );
+    outampfld->display( !issingle );
+    outampfld->setSensitive( !iszc );
 }
 
 
 void uiEventAttrib::isGateSel( CallBacker* )
 {
-    const int val = evtypefld-> getIntValue();
+    const int val = evtypefld->getIntValue();
     const bool issingle = issinglefld-> getBoolValue();
     const bool tgdisplay =  (val == 6 || val == 7 ) ? true : false;
     const bool iszc = !issingle && ( val==3 || val==4 || val==5 );
+    if ( iszc ) outampfld->setValue( true );
     const bool outamp = !outampfld-> getBoolValue();
     gatefld->display( tgdisplay && !issingle );
     tonextfld->display( !tgdisplay && !issingle && !outamp );
-    tonextlblfld->display( iszc );
-    outampfld->display( !issingle && !iszc );
+    outampfld->display( !issingle );
+    outampfld->setSensitive( !iszc );
 }
 
 
@@ -139,13 +138,13 @@ bool uiEventAttrib::setParameters( const Attrib::Desc& desc )
     if ( desc.attribName() != Attrib::Event::attribName() )
 	return false;
 
-    mIfGetBool( Attrib::Event::issingleeventStr(), issingleevent, 
+    mIfGetBool( Attrib::Event::issingleeventStr(), issingleevent,
 	        issinglefld->setValue(issingleevent) );
     mIfGetBool( Attrib::Event::tonextStr(), tonext,
 		tonextfld->setValue(tonext) );
     mIfGetBool( Attrib::Event::outampStr(), outamp,
 		outampfld->setValue(!outamp) );
-    mIfGetInt( Attrib::Event::eventTypeStr(), eventtype, 
+    mIfGetInt( Attrib::Event::eventTypeStr(), eventtype,
 	        evtypefld->setValue(eventtype) );
     mIfGetFloatInterval( Attrib::Event::gateStr(), gate,
 			 gatefld->setValue(gate) );
