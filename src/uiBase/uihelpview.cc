5acb6108c80637a4f9ba738461cd33c2146919a6
Nageswara Rao <nageswara.rao@dgbes.com>
2014-12-12 09:27:27 +0000

    od: Help > Documentation > Programmer option is working
    Summary: See D6503
    
    Test Plan: Tested.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Subscribers: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D6553
    
    git-svn-id: https://svn.opendtect.org/od/trunk@37542 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiBase/uihelpview.cc
+++ src/uiBase/uihelpview.cc
@@ -16,6 +16,7 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "filepath.h"
 #include "file.h"
 #include "uidesktopservices.h"
+#include "odinst.h"
 #include "odversion.h"
 #include "odnetworkaccess.h"
 #include "keystrs.h"
@@ -98,23 +99,18 @@ void DevDocHelp::initClass()
 
 BufferString DevDocHelp::getUrl() const
 {
-    FilePath basefile = mGetProgrammerDocDir();
-    basefile.add( "index.html" );
-
-    if ( File::exists( basefile.fullPath()) )
-	return BufferString( fileprot, basefile.fullPath(FilePath::Unix) );
-
-    BufferString url = "http://www.opendtect.org/";
-    url.add( toString(mODVersion) );
-    url.add( "/doc/Programmer/index.html" );
-
-    uiString networkmsg;
-    mDefineStaticLocalObject( bool, isonline, = Network::ping(url,networkmsg) );
-
-    if ( !isonline )
+    const BufferString classpkgver( ODInst::getPkgVersion("classdoc") );
+    const bool foundclspkg = !classpkgver.isEmpty() &&
+			     !classpkgver.find( "error" );
+    if ( !foundclspkg )
 	return BufferString( sKey::EmptyString() );
 
-    return url;
+    FilePath basefile = mGetProgrammerDocDir();
+    basefile.add( "index.html" );
+    if ( !File::exists( basefile.fullPath()) )
+	return BufferString( sKey::EmptyString() );
+
+    return BufferString( fileprot, basefile.fullPath(FilePath::Unix) );
 }
 
 
