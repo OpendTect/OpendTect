6360d06a657f1a5543be33e62ec48a58a98be7aa
Ranojay Sen <ranojay.sen@dgbes.com>
2014-12-12 10:54:37 +0000

    od: Typo Fixed
    Summary: Fixed spelling mistake
    
    Test Plan: spelling corrected.
    
    Reviewers: nageswara
    
    Reviewed By: nageswara
    
    Subscribers: kristofer, raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D6555
    
    git-svn-id: https://svn.opendtect.org/od/trunk@37543 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiTools/uiproxydlg.cc
+++ src/uiTools/uiproxydlg.cc
@@ -39,7 +39,7 @@ uiProxyDlg::uiProxyDlg( uiParent* p )
     portfld_->box()->setInterval( 1, 65535 );
 
     authenticationfld_ =
-	new uiCheckBox( this, tr("Use uthentication") );
+	new uiCheckBox( this, tr("Use authentication") );
     authenticationfld_->activated.notify( 
 				    mCB(this,uiProxyDlg,useProxyCB) );
     authenticationfld_->attach( alignedBelow, hostfld_ );
