3576590c465b1918604d94a360de6f7975b58a86
Raman Singh <raman.singh@dgbes.com>
2015-01-05 06:24:22 +0000

    od5.0: Displaying component names while selecting 2D input attribute
    Summary: See http://bugs.dgbes.com/view.php?id=2976. The list of component names was not populated because of the difference in working of SeisTrcReader for 2D and 3D. Now, using the SeisIOObjInfo class solves this. Let SeisIOObjInfo figure out how to handle 2D/3D.
    
    Test Plan: Component names for 2D stored attributes are available now.
    
    Reviewers: mahant
    
    Reviewed By: mahant
    
    Subscribers: helene
    
    Differential Revision: https://phabricator.dgbes.com:8443/D6686
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@37714 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiAttributes/uiattrsel.cc
+++ src/uiAttributes/uiattrsel.cc
@@ -26,7 +26,7 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "ctxtioobj.h"
 #include "datainpspec.h"
 #include "ptrman.h"
-#include "seisread.h"
+#include "seisioobjinfo.h"
 #include "seistrctr.h"
 #include "linekey.h"
 #include "cubesampling.h"
@@ -413,14 +413,8 @@ void uiAttrSelDlg::cubeSel( CallBacker* c )
 
     compfld_->box()->setCurrentItem(0);
     const MultiID key( ioobjkey.buf() );
-    PtrMan<IOObj> ioobj = IOM().get( key );
-    SeisTrcReader rdr( ioobj );
-    if ( !rdr.prepareWork(Seis::PreScan) ) return;
-
-    SeisTrcTranslator* transl = rdr.seisTranslator();
-    if ( !transl ) return;
     BufferStringSet compnms;
-    transl->getComponentNames( compnms );
+    SeisIOObjInfo::getCompNames( key, compnms );
     compfld_->box()->setEmpty();
 
     const bool hascomps = compnms.size()>1;
