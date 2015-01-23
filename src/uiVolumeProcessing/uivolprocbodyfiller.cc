3af898c928c5876560d34c4917721e08ed2b69a8
Yuancheng Liu <yuancheng.liu@dgbes.com>
2015-01-23 15:13:01 +0000

    od5.0: Removed Transparent option, UI change
    Summary:
    In body painter settings, the transparent option is the same as if inside value or outside value are undef. So, remove this option to make it more clear for users.
    
    Ref: bug 2648
    
    Test Plan: Just UI change
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D6880
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@37950 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiVolumeProcessing/uivolprocbodyfiller.cc
+++ src/uiVolumeProcessing/uivolprocbodyfiller.cc
@@ -28,44 +28,34 @@ void uiBodyFiller::initClass()
     str += BodyFiller::sKeyOldType();
 
     uiStepDialog::factory().addCreator( createInstance, str,
-	    			        sFactoryDisplayName() );
-}    
+				        sFactoryDisplayName() );
+}
 
 
 uiBodyFiller::uiBodyFiller( uiParent* p, BodyFiller* mp )
     : uiStepDialog( p, BodyFiller::sFactoryDisplayName(), mp )
     , bodyfiller_( mp )
+    , useinsidefld_(0)
+    , useoutsidefld_(0)
 {
     setHelpKey( mODHelpKey(mBodyFillerHelpID) );
 
     IOObjContext ctxt = mIOObjContext( EMBody );
     ctxt.forread = true;
-    uinputselfld_ = new uiIOObjSel( this, ctxt, "Input Body" );
+    uinputselfld_ = new uiIOObjSel( this, ctxt, "Input body" );
     uinputselfld_->selectionDone.notify( mCB(this,uiBodyFiller,bodySel) );
     if ( mp )
 	uinputselfld_->setInput( mp->getSurfaceID() );
-    
-    const bool showinside = !mIsUdf( mp->getInsideValue() ); 
-    useinsidefld_ = new uiGenInput( this, "Inside",
-	    BoolInpSpec( showinside, "Value", "Transparent" ) );
-    useinsidefld_->attach( alignedBelow, uinputselfld_ );
-    useinsidefld_->valuechanged.notify( mCB(this,uiBodyFiller,updateFlds) );
-    insidevaluefld_ = new uiGenInput( this, "Inside-Value",
-	    FloatInpSpec( showinside ? mp->getInsideValue() : -2000 ) );
-    insidevaluefld_->attach( alignedBelow, useinsidefld_ );
-    
-    const bool showoutside = !mIsUdf( mp->getOutsideValue() ); 
-    useoutsidefld_ = new uiGenInput( this, "Outside",
-	    BoolInpSpec( showoutside, "Value", "Transparent" ) );
-    useoutsidefld_->attach( alignedBelow, insidevaluefld_ );
-    useoutsidefld_->valuechanged.notify(
-	    mCB(this,uiBodyFiller, updateFlds) );
-    outsidevaluefld_ = new uiGenInput( this, "Outside-Value",
-	    FloatInpSpec( showoutside ? mp->getOutsideValue() : 2000 ) );
-    outsidevaluefld_->attach( alignedBelow, useoutsidefld_ );
+
+    insidevaluefld_ = new uiGenInput( this, "Inside value",
+	    FloatInpSpec( mp ? mp->getInsideValue() : 1e30 ) );
+    insidevaluefld_->attach( alignedBelow, uinputselfld_ );
+
+    outsidevaluefld_ = new uiGenInput( this, "Outside value",
+	    FloatInpSpec( mp ? mp->getOutsideValue() : 1e30 ) );
+    outsidevaluefld_->attach( alignedBelow, insidevaluefld_ );
 
     addNameFld( outsidevaluefld_ );
-    updateFlds( 0 );
 }
 
 
@@ -83,10 +73,7 @@ void uiBodyFiller::bodySel( CallBacker* )
 
 
 void uiBodyFiller::updateFlds( CallBacker* )
-{
-    insidevaluefld_->display( useinsidefld_->getBoolValue() );
-    outsidevaluefld_->display( useoutsidefld_->getBoolValue() );
-}
+{}
 
 
 uiStepDialog* uiBodyFiller::createInstance(uiParent* parent,Step* ps)
@@ -103,54 +90,23 @@ bool uiBodyFiller::acceptOK( CallBacker* cb )
     if ( !uiStepDialog::acceptOK( cb ) )
 	return false;
 
-    const IOObj* ioobj = uinputselfld_->ioobj();
-    if ( !ioobj ) return false;
-
-    bodyfiller_->setSurface( ioobj->key() );
-
-    if ( useinsidefld_->getBoolValue() && !useoutsidefld_->getBoolValue() )
+    if ( mIsUdf(insidevaluefld_->getfValue()) &&
+	 mIsUdf(outsidevaluefld_->getfValue()) )
     {
-	if ( mIsUdf(insidevaluefld_->getfValue()) ) 
-	{
-	    uiMSG().error("Set the inside value");
-	    return false;
-	}
-
-	bodyfiller_->setInsideOutsideValue( 
-		insidevaluefld_->getfValue(), mUdf(float) );
-    }
-
-    if ( useoutsidefld_->getBoolValue() && !useinsidefld_->getBoolValue() )
-    {
-	if ( mIsUdf(outsidevaluefld_->getfValue()) ) 
-	{
-	    uiMSG().error("Set the outside value");
-	    return false;
-	}
-
-	bodyfiller_->setInsideOutsideValue( 
-		mUdf(float), outsidevaluefld_->getfValue() );
-    }
-
-
-    if ( useinsidefld_->getBoolValue() && useoutsidefld_->getBoolValue() )
-    {
-	if ( mIsUdf(insidevaluefld_->getfValue()) &&
-      	     mIsUdf(outsidevaluefld_->getfValue()) ) 
-	{
-	    uiMSG().error("Set at lease one value");
-	    return false;
-	}
-
-	bodyfiller_->setInsideOutsideValue( 
-		insidevaluefld_->getfValue(), outsidevaluefld_->getfValue() );
-    }
-
-    if ( !useinsidefld_->getBoolValue() && !useoutsidefld_->getBoolValue() )
-    {
-	uiMSG().error("Select either Inside or Outside value to proceed.");
+	uiMSG().error("Set at least one defined value");
 	return false;
     }
+
+    const IOObj* ioobj = uinputselfld_->ioobj();
+    if ( !ioobj )
+    {
+	uiMSG().error("Invalid empty input body");
+	return false;
+    }
+
+    bodyfiller_->setSurface( ioobj->key() );
+    bodyfiller_->setInsideOutsideValue( insidevaluefld_->getfValue(),
+					outsidevaluefld_->getfValue() );
 
     return true;
 }
