7703ac447c7054b6003020c726e1e20f71407d8a
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2015-01-27 14:33:53 +0000

    Removed Version_DETAIL
    Summary:
    CMake does supports PROJECT_VERSION_MAJOR, <PROJECT-NAME>_VERSION_MAJOR
    PROJECT_VERSION_MINOR, <PROJECT-NAME>_VERSION_MINOR
    PROJECT_VERSION_PATCH, <PROJECT-NAME>_VERSION_PATCH
    PROJECT_VERSION_TWEAK, <PROJECT-NAME>_VERSION_TWEAK
    
    http://www.cmake.org/cmake/help/v3.0/command/project.html
    
    Detail is not a part of that.
    
    Test Plan: I don't know if it works. As od5.0 has version A.B.C, we should stick to that.
    
    Reviewers: nageswara
    
    Reviewed By: nageswara
    
    Subscribers: bert
    
    Differential Revision: https://phabricator.dgbes.com:8443/D6906
    
    git-svn-id: https://svn.opendtect.org/od/trunk@37957 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODPackages.cmake
+++ CMakeModules/ODPackages.cmake
@@ -16,7 +16,6 @@ macro( OD_ADD_PACKAGES_TARGET )
     add_custom_target( packages  ${CMAKE_COMMAND} 
 	    -DOpendTect_VERSION_MAJOR=${OpendTect_VERSION_MAJOR} 
 	    -DOpendTect_VERSION_MINOR=${OpendTect_VERSION_MINOR} 
-	    -DOpendTect_VERSION_DETAIL=${OpendTect_VERSION_DETAIL}
 	    -DOpendTect_VERSION_PATCH=${OpendTect_VERSION_PATCH} 
 	    "-DOD_THIRD_PARTY_LIBS=\"${OD_THIRD_PARTY_LIBS}\""
 	    "-DOD_THIRD_PARTY_LIBS_DEBUG=\"${OD_THIRD_PARTY_LIBS_DEBUG}\""
