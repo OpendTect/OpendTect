6faad70c24f00bc89cfe11847c0ba974a3e322ac
Aneesh Tiwari <aneesh.tiwari@dgbes.com>
2015-02-03 15:25:05 +0000

    Removed sKey::HorizonCube
    git-svn-id: https://svn.opendtect.org/od/trunk@38023 a985855d-d384-4b45-ae33-31a410998c3c

--- include/Basic/keystrs.h
+++ include/Basic/keystrs.h
@@ -63,7 +63,6 @@ namespace sKey
     inline FixedString Geometry()	{ return "Geometry"; }
     inline FixedString GeomID()		{ return "GeomID"; }
     inline FixedString Horizon()	{ return "Horizon"; }
-    inline FixedString HorizonCube()	{ return "HorizonCube"; }
     inline FixedString ID()		{ return "ID"; }
     inline FixedString Input()		{ return "Input"; }
     inline FixedString Inline()		{ return "In-line"; }
