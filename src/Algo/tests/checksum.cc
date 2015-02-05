45837e54d3936fde2a0df2f35fe1d871073580ac
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2015-02-05 12:14:59 +0000

    Added test programs for checksum and system-id.
    Summary: Added system-id to crash-reports.
    
    Test Plan: Test programs work.
    
    Reviewers: ranojay
    
    Reviewed By: ranojay
    
    Differential Revision: https://phabricator.dgbes.com:8443/D6988
    
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@38051 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ src/Algo/tests/checksum.cc
@@ -0,0 +1,32 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : K. Tingdahl
+ * DATE     : Jan 2015
+ * FUNCTION :
+-*/
+
+static const char* rcsID mUsedVar = "$Id$";
+
+#include "checksum.h"
+#include "testprog.h"
+
+
+bool testChecksum()
+{
+    mRunStandardTest(
+	    checksum64( (unsigned char*) "123456789",9,0 )==0xe9c6d914c4b8d9ca,
+	    "64 bit checksum" );
+
+    return true;
+}
+
+
+int main( int argc, char** argv )
+{
+    mInitTestProg();
+
+    if ( !testChecksum() )
+	ExitProgram( 1 );
+
+    return ExitProgram( 0 );
+}
