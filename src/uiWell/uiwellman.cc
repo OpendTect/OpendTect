OpendTect/OpendTect

commit 05fe1195ab9ce6288b56b3e764b4e5a42ef9c204

od5.0: Standardized well-related labels in the interface
Summary:
many different labels were used since the base keys were hardly ever used, changed that.
Also changed the default temporary model model since to disambiguate it from the replacement velocity.

Fixes: http://bugs.dgbes.com/view.php?id=3850

Test Plan: popped up the various windows, looks fine

Reviewers: helene

Reviewed By: helene

Differential Revision: https://phabricator.dgbes.com:8443/D7031

git-svn-id: https://svn.opendtect.org/od/branches/od5.0@38078 a985855d-d384-4b45-ae33-31a410998c3c

src/uiWell/uiwellman.cc
@@ -495,7 +495,8 @@ void uiWellMan::defD2T( bool chkshot )
 	      !wtr.putInfoAndTrack() )
     {
 	if ( !errmsg.isEmpty() ) errmsg.addNewLine();
-	errmsg.add( "Cannot write new replacement velocity to disk" );
+	errmsg.add( "Cannot write new " ).add( Well::Info::sKeykbelev() );
+	errmsg.add( " to disk" );
 	wd->info().replvel = oldreplvel;
     }
 
@@ -778,7 +779,7 @@ void uiWellMan::exportLogs( CallBacker* )
 
 #define mAddWellInfo(key,str) \
     if ( !str.isEmpty() ) \
-    { txt += key; txt += ": "; txt += str; txt += "\n"; }
+    { txt.add( key ).add( colonstr ).add( str ).addNewLine(); }
 
 
 void uiWellMan::mkFileInfo()
@@ -796,6 +797,7 @@ void uiWellMan::mkFileInfo()
     const Well::Info& info = curwd.info();
     const Well::Track& track = curwd.track();
 
+    FixedString colonstr( ": " );
     const BufferString posstr( info.surfacecoord.toString(), " ",
 		SI().transform(info.surfacecoord).toString() );
     mAddWellInfo(Well::Info::sKeycoord(),posstr)
@@ -806,53 +808,55 @@ void uiWellMan::mkFileInfo()
 	const UnitOfMeasure* zun = UnitOfMeasure::surveyDefDepthUnit();
 	if ( !mIsZero(rdelev,1e-4) && !mIsUdf(rdelev) )
 	{
-	    txt += "Reference Datum Elevation (KB): ";
-	    txt += zun ? zun->userValue(rdelev) : rdelev;
+	    txt.add( Well::Info::sKeykbelev() ).add( colonstr );
+	    txt.add( zun ? zun->userValue(rdelev) : rdelev );
 	    if ( zun ) txt.add( zun->symbol() );
-	    txt.add( "\n" );
+	    txt.addNewLine();
 	}
 
 	const float td = track.dahRange().stop;
 	if ( !mIsZero(td,1e-3f) && !mIsUdf(td) )
 	{
-	    txt += "Total Depth (TD): ";
-	    txt += zun ? zun->userValue(td) : td;
+	    txt.add( Well::Info::sKeyTD() ).add( colonstr );
+	    txt.add( zun ? zun->userValue(td) : td );
 	    if ( zun ) txt.add( zun->symbol() );
-	    txt.add( "\n" );
+	    txt.addNewLine();
 	}
 
 	const float srdelev = info.srdelev;
 	const float srd = mCast(float,SI().seismicReferenceDatum());
 	if ( !mIsZero(srd,1e-4f) )
 	{
-	    txt.add( "Seismic Reference Datum (SRD): " );
+	    txt.add( SurveyInfo::sKeySeismicRefDatum() ).add( colonstr );
 	    txt.add( zun ? zun->userValue(srd) : srd );
 	    if ( zun ) txt.add( zun->symbol() );
-	    txt.add( "\n" );
+	    txt.addNewLine();
 	}
 	if ( !mIsEqual(srd,srdelev,1e-2f) &&
 	     !mIsUdf(srdelev) && !mIsZero(srdelev,1e-2f) )
 	{
-	    txt.add( "Warning: Seismic Reference Datum (SRD) from well " );
-	    txt.add( "is no longer supported.\n" );
-	    txt.add("This later is ignored, using SRD from survey settings.\n");
+	    txt.add( "Warning: " ).add( SurveyInfo::sKeySeismicRefDatum() );
+	    txt.add( " from well " ).add( "is no longer supported." );
+	    txt.addNewLine().add( "This later is ignored, " );
+	    txt.add( "using SRD from survey settings." ).addNewLine();
 	}
 
 	const float replvel = info.replvel;
 	if ( !mIsUdf(replvel) )
 	{
-	     txt += "Replacement velocity (from KB to SRD): ";
-	     txt += zun ? zun->userValue(replvel) : replvel;
-	     txt += UnitOfMeasure::zUnitAnnot( false, true, false );
-	     txt += "/s\n";
+	     txt.add( Well::Info::sKeyreplvel() ).add( colonstr );
+	     txt.add( zun ? zun->userValue(replvel) : replvel );
+	     txt.add( UnitOfMeasure::zUnitAnnot(false,true,false) );
+	     txt.add( "/s" ).addNewLine();
 	}
 
 	const float groundelev = info.groundelev;
 	if ( !mIsUdf(groundelev) )
 	{
-	    txt += "Ground level elevation (GL): ";
-	    txt += zun ? zun->userValue(groundelev) : groundelev;
-	    txt += zun->symbol(); txt += "\n";
+	    txt.add( Well::Info::sKeygroundelev() ).add( colonstr );
+	    txt.add( zun ? zun->userValue(groundelev) : groundelev );
+	    if ( zun ) txt.add( zun->symbol() );
+	    txt.addNewLine();
 	}
     }
 

include/Well/welldata.h
@@ -71,6 +71,7 @@ public:
     static const char*	sKeyreplvel();
     static const char*	sKeygroundelev();
     static int		legacyLogWidthFactor();
+    static const char*	sKeyTD();
 
 };
 

include/uiWell/uid2tmodelgrp.h
@@ -50,6 +50,9 @@ public:
     bool		wantAsCSModel() const;
     BufferString	dataSourceName() const;
 
+    static const char*	sKeyTemporaryVel();
+    static float	getDefaultTemporaryVelocity();
+
 protected:
 
     Setup		setup_;

src/Well/well.cc
@@ -23,11 +23,14 @@ const char* Well::Info::sKeyoper()	{ return "Operator"; }
 const char* Well::Info::sKeystate()	{ return "State"; }
 const char* Well::Info::sKeycounty()	{ return "County"; }
 const char* Well::Info::sKeycoord()	{ return "Surface coordinate"; }
-const char* Well::Info::sKeykbelev()	{ return "Reference Datum elevation"; }
+const char* Well::Info::sKeykbelev(){ return "Reference Datum elevation [KB]"; }
 const char* Well::Info::sKeyOldelev()	{ return "Surface elevation"; }
-const char* Well::Info::sKeySRD()	{ return "Seismic Reference Datum"; }
-const char* Well::Info::sKeyreplvel()	{ return "Replacement velocity"; }
-const char* Well::Info::sKeygroundelev(){ return "Ground level elevation"; }
+const char* Well::Info::sKeySRD()
+{ return SurveyInfo::sKeySeismicRefDatum(); }
+const char* Well::Info::sKeyreplvel()
+{ return "Replacement velocity [from KB to SRD]"; }
+const char* Well::Info::sKeygroundelev(){ return "Ground Level elevation [GL]";}
+const char* Well::Info::sKeyTD()	{ return "Total Depth [TD]"; }
 
 int Well::Info::legacyLogWidthFactor()
 {

src/Well/wellascio.cc
@@ -274,7 +274,7 @@ bool TrackAscIO::getData( Data& wd, float kbelev, float td ) const
 	return false;
 
     if ( mIsUdf(kbelev) && mIsUdf(kbelevinfile) )
-	mErrRet( BufferString("Reference Datum Elevation was not provided"
+	mErrRet( BufferString(Well::Info::sKeykbelev(), " was not provided"
 		     " and cannot be computed") )
 
     if ( !computeMissingValues(pos,mdvals,kbelevinfile) )
@@ -604,6 +604,7 @@ static void checkReplacementVelocity( Well::Info& info, double vreplinfile,
     if ( mIsUdf(vreplinfile) )
 	return;
 
+    FixedString replvelbl( Well::Info::sKeyreplvel() );
     if ( !mIsEqual((float)vreplinfile,info.replvel,mDefEpsF) )
     {
 	if ( mIsEqual(info.replvel,Well::getDefaultVelocity(),mDefEpsF) )
@@ -612,16 +613,16 @@ static void checkReplacementVelocity( Well::Info& info, double vreplinfile,
 	}
 	else
 	{
-	    msg.set( "Input error with the replacement velocity" ).addNewLine();
+	    msg.set( "Input error with the " ).add( replvelbl ).addNewLine();
 	    const UnitOfMeasure* uomdepth = UnitOfMeasure::surveyDefDepthUnit();
 	    const BufferString veluomlbl(
-		    UnitOfMeasure::surveyDefDepthUnitAnnot(true,false) );
+		    UnitOfMeasure::surveyDefDepthUnitAnnot(true,false), "/s" );
 
-	    msg.add( "Your time-depth model suggests "
-		     "a replacement velocity of " );
+	    msg.add( "Your time-depth model suggests a " );
+	    msg.add( replvelbl ).add( " of " );
 	    msg.add( toString(mScaledValue(vreplinfile,uomdepth), 2 ) );
-	    msg.add( veluomlbl ).addNewLine();
-	    msg.add( "but the replacement velocity was set to: " );
+	    msg.add( veluomlbl ).addNewLine().add( "but the " );
+	    msg.add( replvelbl ).add( " was set to: " );
 	    msg.add( toString(mScaledValue(info.replvel,uomdepth), 2) );
 	    msg.add( veluomlbl );
 	}
@@ -793,7 +794,8 @@ Table::FormatDesc* BulkTrackAscIO::getDesc()
     fd->bodyinfos_ += Table::TargetInfo::mkDepthPosition( true );
     fd->bodyinfos_ +=
 	new Table::TargetInfo( "MD", FloatInpSpec(), Table::Optional );
-    fd->bodyinfos_ += new Table::TargetInfo( "Well ID (UWI)", Table::Optional );
+    fd->bodyinfos_ += new Table::TargetInfo( Well::Info::sKeyuwid(),
+					     Table::Optional );
     return fd;
 }
 

src/uiIo/uisurvinfoed.cc
@@ -24,6 +24,7 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "statrand.h"
 #include "survinfo.h"
 #include "systeminfo.h"
+#include "unitofmeasure.h"
 
 #include "uibutton.h"
 #include "uicombobox.h"
@@ -41,8 +42,18 @@ static const char* rcsID mUsedVar = "$Id$";
 
 extern "C" const char* GetBaseDataDir();
 
-static const char* sKeySRDMeter = "Seismic Reference Datum (m) ";
-static const char* sKeySRDFeet = "Seismic Reference Datum (ft) ";
+static const char* sKeySRD( bool infeet )
+{
+    const UnitOfMeasure* uomsi = UoMR().get( "Meter" );
+    const UnitOfMeasure* uomimp = UoMR().get( "Feet" );
+    const UnitOfMeasure* uom = uomimp && infeet ? uomimp : uomsi;
+    BufferString lbl( SurveyInfo::sKeySeismicRefDatum() );
+    lbl.add( "(" ).add( uom ? uom->symbol() : "m"  ).add( ")" );
+
+    return lbl;
+}
+
+
 
 uiSurveyInfoEditor::uiSurveyInfoEditor( uiParent* p, SurveyInfo& si,
 					bool isnew )
@@ -259,9 +270,9 @@ void uiSurveyInfoEditor::mkRangeGrp()
     if ( depthinft && si_.zIsTime() )
 	srd *= mToFeetFactorD;
 
-    refdatumfld_ = new uiGenInput( rangegrp_,
-			depthinft ? sKeySRDFeet : sKeySRDMeter,
-			DoubleInpSpec(srd) );
+
+    refdatumfld_ = new uiGenInput( rangegrp_, sKeySRD( depthinft ),
+				   DoubleInpSpec(srd) );
     refdatumfld_->attach( alignedBelow, depthdispfld_ );
 
     rangegrp_->setHAlignObj( inlfld_ );
@@ -843,11 +854,10 @@ void uiSurveyInfoEditor::depthDisplayUnitSel( CallBacker* )
 {
     const BufferString labeltext = refdatumfld_->titleText().getFullString();
     const bool showdepthinft = !depthdispfld_->getBoolValue();
-    const bool needsupdate =
-	labeltext != ( !showdepthinft ? sKeySRDMeter : sKeySRDFeet );
+    const bool needsupdate = labeltext != sKeySRD( showdepthinft );
     if ( !needsupdate ) return;
 
-    refdatumfld_->setTitleText( !showdepthinft ? sKeySRDMeter : sKeySRDFeet );
+    refdatumfld_->setTitleText( sKeySRD( showdepthinft ) );
     double refdatum = refdatumfld_->getdValue( 0.0 );
     refdatum *= showdepthinft ? mToFeetFactorD : mFromFeetFactorD;
     refdatumfld_->setValue( refdatum );

src/uiWell/uid2tmodelgrp.cc
@@ -28,6 +28,14 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "welltrack.h"
 #include "od_helpids.h"
 
+const char* uiD2TModelGroup::sKeyTemporaryVel()
+{ return "Temporary model velocity"; }
+
+
+float uiD2TModelGroup::getDefaultTemporaryVelocity()
+{ return getGUIDefaultVelocity() * 1.25f; }
+
+
 uiD2TModelGroup::uiD2TModelGroup( uiParent* p, const Setup& su )
     : uiGroup(p,"D2TModel group")
     , velfld_(0)
@@ -40,9 +48,9 @@ uiD2TModelGroup::uiD2TModelGroup( uiParent* p, const Setup& su )
 				uiFileInput::Setup().withexamine(true) );
     if ( setup_.fileoptional_ )
     {
-	BufferString velllbl( "Temporary model velocity ");
-	velllbl.add( VelocityDesc::getVelUnit( true ) );
-	const float vel = getGUIDefaultVelocity();
+	BufferString velllbl( sKeyTemporaryVel() );
+	velllbl.addSpace().add( VelocityDesc::getVelUnit( true ) );
+	const float vel = getDefaultTemporaryVelocity();
 	filefld_->setWithCheck( true ); filefld_->setChecked( true );
 	filefld_->checked.notify( mCB(this,uiD2TModelGroup,fileFldChecked) );
 	velfld_ = new uiGenInput( this, velllbl, FloatInpSpec(vel) );

src/uiWell/uisimplemultiwell.cc
@@ -75,13 +75,13 @@ uiSimpleMultiWellCreate::uiSimpleMultiWellCreate( uiParent* p )
     tbl_->setColumnLabel( 2, BufferString("[Y",xunstr,"]") );
     const char* zunstr = zinft_ ? " (ft" : " (m";
     tbl_->setColumnLabel( 3, BufferString("[KB",zunstr,")]") );
-    tbl_->setColumnToolTip( 3, "Reference Datum elevation" );
+    tbl_->setColumnToolTip( 3, Well::Info::sKeykbelev() );
     tbl_->setColumnLabel( 4, BufferString("[TD",zunstr,")]") );
-    tbl_->setColumnToolTip( 4, "Total Depth" );
+    tbl_->setColumnToolTip( 4, Well::Info::sKeyTD() );
     tbl_->setColumnLabel( 5, BufferString("[GL",zunstr,")]") );
-    tbl_->setColumnToolTip( 5, "Ground level elevation" );
+    tbl_->setColumnToolTip( 5, Well::Info::sKeygroundelev() );
     tbl_->setColumnLabel( 6, "[UWI]" );
-    tbl_->setColumnToolTip( 6, "Unique Well Identifier" );
+    tbl_->setColumnToolTip( 6, Well::Info::sKeyuwid() );
 
     uiPushButton* pb = new uiPushButton( this, "Read file",
 	    mCB(this,uiSimpleMultiWellCreate,rdFilePush), false );
@@ -89,9 +89,9 @@ uiSimpleMultiWellCreate::uiSimpleMultiWellCreate( uiParent* p )
 
     if ( SI().zIsTime() )
     {
-	const float defvel = getGUIDefaultVelocity();
-	velfld_ = new uiGenInput( this, BufferString("Velocity",zunstr,"/s)"),
-				  FloatInpSpec(defvel) );
+	const float defvel = uiD2TModelGroup::getDefaultTemporaryVelocity();
+	BufferString velbl( uiD2TModelGroup::sKeyTemporaryVel(),zunstr,"/s)" );
+	velfld_ = new uiGenInput( this, velbl, FloatInpSpec(defvel) );
 	velfld_->attach( rightTo, pb );
 	velfld_->attach( rightBorder );
     }
@@ -174,12 +174,13 @@ uiSimpleMultiWellCreateReadData( uiSimpleMultiWellCreate& p )
     fd_.bodyinfos_ += new Table::TargetInfo( "Well name", Table::Required );
     fd_.bodyinfos_ += Table::TargetInfo::mkHorPosition( true );
     Table::TargetInfo* ti = Table::TargetInfo::mkDepthPosition( false );
-    ti->setName( "KB Elevation" ); fd_.bodyinfos_ += ti;
+    ti->setName( Well::Info::sKeykbelev() ); fd_.bodyinfos_ += ti;
     ti = Table::TargetInfo::mkDepthPosition( false );
-    ti->setName( "TD" ); fd_.bodyinfos_ += ti;
+    ti->setName( Well::Info::sKeyTD() ); fd_.bodyinfos_ += ti;
     ti = Table::TargetInfo::mkDepthPosition( false );
-    ti->setName( "Ground Level Elevation" ); fd_.bodyinfos_ += ti;
-    fd_.bodyinfos_ += new Table::TargetInfo( "Well ID (UWI)", Table::Optional );
+    ti->setName( Well::Info::sKeygroundelev() ); fd_.bodyinfos_ += ti;
+    fd_.bodyinfos_ += new Table::TargetInfo( Well::Info::sKeyuwid(),
+					     Table::Optional );
 
     dataselfld_ = new uiTableImpDataSel( this, fd_,
                                        mODHelpKey(mTableImpDataSelwellsHelpID));

commit 8c45767cd97fc38ae98c4df5ca2dcc8c231a4a7c

od5.0: Fixed wrong key used in settings of replacement velocity
Summary: as in title. Regression introduced in D7031

Test Plan: compiles

Reviewers: helene

Reviewed By: helene

Differential Revision: https://phabricator.dgbes.com:8443/D7050

git-svn-id: https://svn.opendtect.org/od/branches/od5.0@38099 a985855d-d384-4b45-ae33-31a410998c3c

src/uiWell/uiwellman.cc
@@ -495,7 +495,7 @@ void uiWellMan::defD2T( bool chkshot )
 	      !wtr.putInfoAndTrack() )
     {
 	if ( !errmsg.isEmpty() ) errmsg.addNewLine();
-	errmsg.add( "Cannot write new " ).add( Well::Info::sKeykbelev() );
+	errmsg.add( "Cannot write new " ).add( Well::Info::sKeyreplvel() );
 	errmsg.add( " to disk" );
 	wd->info().replvel = oldreplvel;
     }