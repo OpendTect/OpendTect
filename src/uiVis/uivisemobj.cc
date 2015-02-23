8dbd670861a2d6db931c1c34f163f0143ab65f7b
Aneesh Tiwari <aneesh.tiwari@dgbes.com>
2015-02-23 07:19:15 +0000

    Fixed crash https://phabricator.dgbes.com:8443/D7136
    git-svn-id: https://svn.opendtect.org/od/trunk@38219 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiVis/uivisemobj.cc
+++ src/uiVis/uivisemobj.cc
@@ -173,7 +173,7 @@ uiVisEMObject::uiVisEMObject( uiParent* uip, const EM::ObjectID& emid,
 	emod->setZAxisTransform( zt, 0 );
 
 	uiTaskRunner dlg( uiparent_ );
-	if ( !emod->setEMObject(emid, &dlg ) || !dlg.execResult() ) mRefUnrefRet
+	if ( !emod->setEMObject(emid, &dlg) ) mRefUnrefRet
 
 	    visserv_->addObject( emod, sceneid, true );
 	displayid_ = emod->id();
