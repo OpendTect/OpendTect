e132d6f7257c774039c89db7e1eb54e26a2e444b
Nanne Hemstra <nanne.hemstra@dgbes.com>
2015-03-05 19:45:03 +0000

    include arrayndimpl
    git-svn-id: https://svn.opendtect.org/od/trunk@38392 a985855d-d384-4b45-ae33-31a410998c3c

--- plugins/MATLABLink/matlabstep.cc
+++ plugins/MATLABLink/matlabstep.cc
@@ -11,7 +11,7 @@ static const char* rcsID mUsedVar = "$Id$";
 
 #include "matlabstep.h"
 
-#include "arraynd.h"
+#include "arrayndimpl.h"
 #include "filepath.h"
 #include "iopar.h"
 #include "keystrs.h"
