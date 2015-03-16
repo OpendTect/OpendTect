OpendTect/OpendTect

commit 3a67c65d832f40f63c40665c25098bec18832d2f

All calls to TextureChannels::set/getSize(.) got attrib argument, and removed old interface.
Summary:
All calls to TextureChannels::set/getSize(.) got attrib argument, and removed
old interface.

Test Plan:
It compiles, and I carefully looked to insert the right attrib argument value.
Testing is difficult since all attribute layers still have equal sizes.

Reviewers: kristofer

Reviewed By: kristofer

Subscribers: kristofer

Differential Revision: https://phabricator.dgbes.com:8443/D5622

git-svn-id: https://svn.opendtect.org/od/trunk@36431 a985855d-d384-4b45-ae33-31a410998c3c

src/visSurvey/visplanedatadisplay.cc
@@ -871,9 +871,9 @@ void PlaneDataDisplay::setVolumeDataPackNoCache( int attrib,
     if ( nrAttribs()>1 )
     {
 	const int oldchannelsz0 =
-			(channels_->getSize(1)+resolution_) / (resolution_+1);
+		  (channels_->getSize(attrib,1)+resolution_) / (resolution_+1);
 	const int oldchannelsz1 =
-			(channels_->getSize(2)+resolution_) / (resolution_+1);
+		  (channels_->getSize(attrib,2)+resolution_) / (resolution_+1);
 
 	//check current attribe sizes
 	int newsz0 = 0, newsz1 = 0;
@@ -1060,7 +1060,7 @@ void PlaneDataDisplay::updateFromDisplayIDs( int attrib, TaskRunner* tr )
 	    cp = OD::TakeOverPtr;
 	}
 
-	channels_->setSize( 1, sz0, sz1 );
+	channels_->setSize( attrib, 1, sz0, sz1 );
 	channels_->setUnMappedData( attrib, idx, arr, cp, tr );
     }
 
@@ -1294,8 +1294,9 @@ void PlaneDataDisplay::annotateNextUpdateStage( bool yn )
     {
 	updatestageinfo_.oldcs_ = getTrcKeyZSampling( false, true );
 	updatestageinfo_.oldorientation_ = orientation_;
-	updatestageinfo_.oldimagesize_.x = channels_->getSize(2);
-	updatestageinfo_.oldimagesize_.y = channels_->getSize(1);
+	// Needs refinement when introducing variably sized texture layers
+	updatestageinfo_.oldimagesize_.x = channels_->getSize(0,2);
+	updatestageinfo_.oldimagesize_.y = channels_->getSize(0,1);
 	updatestageinfo_.refreeze_ = true;
     }
     else if ( updatestageinfo_.refreeze_ )

include/visBase/vistexturechannels.h
@@ -77,10 +77,6 @@ public:
     void			setSize(int channel,int sz0,int sz1,int sz2);
     int				getSize(int channel,unsigned char dim) const;
 
-    				//Old, don't use in new code. Will be removed
-    void			setSize(int sz0,int sz1,int sz2);
-    int				getSize(unsigned char dim) const;
-
     void			setNrComponents(int channel,int nrcomp);
     int				getNrComponents(int channel) const;
 

src/visBase/visflatviewer.cc
@@ -165,7 +165,7 @@ void FlatViewer::handleChange( unsigned int dt)
 			cp = OD::TakeOverPtr;
 		    }
 
-		    channels_->setSize( 1, rowsz, colsz );
+		    channels_->setSize( 0, 1, rowsz, colsz );
 		    channels_->setUnMappedData( 0, 0, arr, cp, 0 );
 
 		    appearance().ddpars_.vd_.ctab_ =

src/visBase/vishorizontexturehandler.cc
@@ -280,7 +280,7 @@ void HorizonTextureHandler::updateTexture(int channel,int sectionid,
     const int nrrows = rrg.nrSteps()+1;
     const int nrcols = crg.nrSteps()+1;
 
-    channels_->setSize( 1, nrrows, nrcols );
+    channels_->setSize( channel, 1, nrrows, nrcols );
 
     ObjectSet<float> versiondata;
     versiondata.allowNull( true );

src/visBase/vistexturechannels.cc
@@ -578,22 +578,6 @@ int TextureChannels::getSize( int channel, unsigned char dim ) const
 }
 
 
-void TextureChannels::setSize( int s0, int s1, int s2 )
-{
-    for ( int channel=0; channel<channelinfo_.size(); channel++ )
-	setSize( channel, s0, s1, s2 );
-}
-
-
-int TextureChannels::getSize( unsigned char dim ) const
-{
-    if ( channelinfo_.size() )
-	return getSize( 0, dim );
-
-    return -1;
-}
-
-
 bool TextureChannels::turnOn( bool yn )
 {
     bool res = isOn();

src/visSurvey/visfaultdisplay.cc
@@ -1185,8 +1185,8 @@ void FaultDisplay::setRandomPosDataInternal( int attrib,
     }
 
     delete texuredatas_.replace( attrib, texturedata );
-    channels_->setSize( 1, texturedata->info().getSize(0),
-			   texturedata->info().getSize(1) );
+    channels_->setSize( attrib, 1, texturedata->info().getSize(0),
+			texturedata->info().getSize(1) );
     channels_->setUnMappedData( attrib, 0, texturedata->getData(),
 				OD::UsePtr, tr );
     validtexture_ = true;
@@ -1211,7 +1211,7 @@ void FaultDisplay::showSelectedSurfaceData()
 	if ( !data )
 	    continue;
 
-	channels_->setSize( 1, data->info().getSize(0),
+	channels_->setSize( lastattridx, 1, data->info().getSize(0),
 			       data->info().getSize(1) );
 	channels_->setUnMappedData( lastattridx--, 0, data->getData(),
 				    OD::UsePtr, 0 );

src/visSurvey/visrandomtrackdisplay.cc
@@ -616,7 +616,7 @@ void RandomTrackDisplay::setData( int attrib, const SeisTrcBuf& trcbuf )
 	const Array2D<float>& array = dprdm->data();
 	const int sz0 = 1 + (array.info().getSize(0)-1) * (resolution_+1);
 	const int sz1 = 1 + (array.info().getSize(1)-1) * (resolution_+1);
-	channels_->setSize( 1, sz0, sz1 );
+	channels_->setSize( attrib, 1, sz0, sz1 );
 
 	if ( resolution_==0 )
 	    channels_->setUnMappedData(attrib,sidx,array.getData(),

commit 10abc06a4fe34ffe61a37487006334074cf4fce7

Bugfix: Multi-attribs with different data sizes were not all displayed at the right scale after session restore of inl/crl/z plane (Mantis 4021).
Summary:
Bugfix: Multi-attribs with different data sizes were not all displayed at the
right scale after session restore of inl/crl/z plane (Mantis 4021). Caused by
revision r36431/D5622 which extended all TextureChannel::setSize(.) calls
with an attrib argument. Poor understanding of the existing code made me pass
the current attrib value instead of the required 0 (mea culpa).

Test Plan:
Dagang_HC_Test1 survey. Save session of inline scene with two attribs among
which Fault_Likelihood_Thinned_from_Seismic_FULL_5s. This attrib is restored
at the right scale again after bugfix.

Reviewers: ding

Reviewed By: ding

Differential Revision: https://phabricator.dgbes.com:8443/D7322

git-svn-id: https://svn.opendtect.org/od/trunk@38516 a985855d-d384-4b45-ae33-31a410998c3c

src/visSurvey/visplanedatadisplay.cc
@@ -790,9 +790,9 @@ void PlaneDataDisplay::setVolumeDataPackNoCache( int attrib,
     if ( nrAttribs()>1 )
     {
 	const int oldchannelsz0 =
-		  (channels_->getSize(attrib,1)+resolution_) / (resolution_+1);
+		  (channels_->getSize(0,1)+resolution_) / (resolution_+1);
 	const int oldchannelsz1 =
-		  (channels_->getSize(attrib,2)+resolution_) / (resolution_+1);
+		  (channels_->getSize(0,2)+resolution_) / (resolution_+1);
 
 	//check current attribe sizes
 	int newsz0 = 0, newsz1 = 0;