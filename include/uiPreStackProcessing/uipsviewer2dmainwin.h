5605f53002fbb2947404256c1f60fa8487926f8d
Bart de Groot <bart.degroot@dgbes.com>
2015-03-26 08:47:43 +0000

    git-svn-id: https://svn.opendtect.org/od/trunk@38641 a985855d-d384-4b45-ae33-31a410998c3c

--- include/uiPreStackProcessing/uipsviewer2dmainwin.h
+++ include/uiPreStackProcessing/uipsviewer2dmainwin.h
@@ -55,7 +55,8 @@ public:
 			~uiViewer2DMainWin();
 
     virtual void 	start()		{ show(); }
-    virtual void        setWinTitle( const char* t )    { setCaption(t); }
+    virtual void        setWinTitle( const uiString t )    
+				    { setCaption(t); }
 
     virtual uiMainWin*  dockParent()    { return this; }
     virtual uiParent*   viewerParent()  { return this; }
