39daaa16ab299ec72d27f9ebe67e132d944792b1
Jaap Glas <jaap.glas@dgbes.com>
2015-03-27 11:00:46 +0000

    Previous commit did not even compile. Very sloppy. Mea culpa.
    git-svn-id: https://svn.opendtect.org/od/branches/od5.0@38661 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiEMAttrib/uiimphorizon2d.cc
+++ src/uiEMAttrib/uiimphorizon2d.cc
@@ -217,7 +217,7 @@ uiImportHorizon2D::uiImportHorizon2D( uiParent* p )
 			"Horizon(s) to import", OD::ChooseAtLeastOne );
     horbox->attach( alignedBelow, inpfld_ );
     horselfld_ = horbox->box();
-    horselfld_->allowDuplicates( false );
+    horselfld_->setAllowDuplicates( false );
     horselfld_->selectionChanged.notify(mCB(this,uiImportHorizon2D,formatSel));
 
     uiPushButton* addbut = new uiPushButton( this, "Add new",
