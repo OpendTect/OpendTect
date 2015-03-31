OpendTect/OpendTect

commit 4e1b47d2d92f3d8dbf78d091e2ff64be566e1ae2

Fixed compiler warnings
Summary:
1. atan was used instead of Math::Atan2
2. conversion to degrees istead of using function for that. 
3. unneeded variable angle.

Test Plan: Compiles fine. @henrique is requested to verify that i works

Reviewers: bert

Reviewed By: bert

Subscribers: nanne, henrique

Maniphest Tasks: T149

Differential Revision: https://phabricator.dgbes.com:8443/D7433

git-svn-id: https://svn.opendtect.org/od/trunk@38699 a985855d-d384-4b45-ae33-31a410998c3c

plugins/Basemap/basemapgeom2d.cc
@@ -61,13 +61,9 @@ void Geom2DObject::setMultiID( const MultiID& mid )
 
 float Geom2DObject::getTextRotation() const
 {
-    const float dy = bptcoords_[1].y - bptcoords_[0].y;
-    const float dx = bptcoords_[1].x - bptcoords_[0].x;
-    float angle = 0;
-    if ( !mIsZero(dx,mDefEps) )
-	return angle = 180.0/M_PI*std::atan( dy/dx );
-    else
-	return angle = 90;
+    const float dy = (float) (bptcoords_[1].y - bptcoords_[0].y);
+    const float dx = (float) (bptcoords_[1].x - bptcoords_[0].x);
+    return Math::toDegrees( Math::Atan2(dy,dx) );
 }
 
 void Geom2DObject::setLineStyle( int shapeidx, const LineStyle& ls )

commit b3590cfc8f3edc2f0414e4d184f1ccab6aff2afc

od: Fixed upsided down labels caused by patch D7433
Summary:
Using Math::Atan2(float,float) as asked.

@Bert I let the conversion to degrees out of this function as you asked.

Test Plan: Worked fine. Labels are being displayed as expected.

Reviewers: bert

Reviewed By: bert

Subscribers: nanne, kristofer, bert

Maniphest Tasks: T149

Differential Revision: https://phabricator.dgbes.com:8443/D7443

git-svn-id: https://svn.opendtect.org/od/trunk@38708 a985855d-d384-4b45-ae33-31a410998c3c

plugins/Basemap/basemapgeom2d.cc
@@ -59,12 +59,26 @@ void Geom2DObject::setMultiID( const MultiID& mid )
 }
 
 
+Alignment Geom2DObject::getAlignment( int shapeix ) const
+{
+    const double dy = bptcoords_[1].y - bptcoords_[0].y;
+    const double dx = bptcoords_[1].x - bptcoords_[0].x;
+
+    const bool isalignedtoleft = (dy<0.f && dx<0.f) || (dy>0.f && dx<0.f);
+    return Alignment( isalignedtoleft ? Alignment::Left : Alignment::Right,
+		      Alignment::VCenter );
+}
+
+
 float Geom2DObject::getTextRotation() const
 {
-    const float dy = (float) (bptcoords_[1].y - bptcoords_[0].y);
-    const float dx = (float) (bptcoords_[1].x - bptcoords_[0].x);
-    return Math::toDegrees( Math::Atan2(dy,dx) );
+    const double dy = bptcoords_[1].y - bptcoords_[0].y;
+    const double dx = bptcoords_[1].x - bptcoords_[0].x;
+
+    const bool needscorrection = (dy<0. && dx<0.) || (dy>0. && dx<0.);
+    return mCast(float,(Math::Atan2(dy,dx) + (needscorrection ? M_PI : 0.)));
 }
+
 
 void Geom2DObject::setLineStyle( int shapeidx, const LineStyle& ls )
 {

plugins/Basemap/basemapgeom2d.h
@@ -31,6 +31,7 @@ public:
     const MultiID&	    getMultiID() const	{ return mid_; }
     virtual const char*     getType() const		{ return "Geom2D"; }
     virtual float	    getTextRotation() const;
+    virtual Alignment	    getAlignment(int shapeidx) const;
     virtual void	    updateGeometry();
 
     virtual int		    nrShapes() const;

src/uiTools/uibasemap.cc
@@ -198,10 +198,10 @@ void uiBaseMapObject::update()
 	    mDynamicCastGet(uiTextItem*,itm,itemgrp_.getUiItem(itemnr));
 	    itm->setText( shapenm );
 	    itm->setPos( crds[0] );
-	    Alignment al = bmobject_->getAlignment( idx );
+	    const Alignment al = bmobject_->getAlignment( idx );
 	    itm->setAlignment( al );
 
-	    float angle = bmobject_->getTextRotation();
+	    const float angle = Math::toDegrees( bmobject_->getTextRotation() );
 	    itm->setRotation( angle );
 
 	    itemnr++;