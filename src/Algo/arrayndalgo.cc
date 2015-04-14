f3de043d4e6ef96050b675d04ecdeee16b78eb22
arnauddgbes <arnaud.huck@dgbes.com>
2015-04-14 13:38:33 +0200

    od6.0: Added generic polynomial trend extractor and removal
    Summary: see D7479
    
    Test Plan: see D7479
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D7530

--- src/Algo/arrayndalgo.cc
+++ src/Algo/arrayndalgo.cc
@@ -7,12 +7,14 @@
 static const char* rcsID mUsedVar = "$Id$";
 
 #include "arrayndalgo.h"
+#include "qrsolv.h"
 #include "windowfunction.h"
 
 
 DefineEnumNames( ArrayNDWindow, WindowType, 0, "Windowing type")
 { "Box", "Hamming", "Hanning", "Blackman", "Bartlett",
   "CosTaper5", "CosTaper10", "CosTaper20", 0 };
+
 
 
 ArrayNDWindow::ArrayNDWindow( const ArrayNDInfo& sz, bool rectangular,
@@ -58,28 +60,28 @@ bool ArrayNDWindow::setType( ArrayNDWindow::WindowType wintype )
 
     switch( wintype )
     {
-    	case ArrayNDWindow::CosTaper5:
+	case ArrayNDWindow::CosTaper5:
             winnm = "CosTaper";
             paramval = 0.95;
             break;
-    	case ArrayNDWindow::CosTaper10:
+	case ArrayNDWindow::CosTaper10:
             winnm = "CosTaper";
             paramval = 0.90;
             break;
-    	case ArrayNDWindow::CosTaper20:
+	case ArrayNDWindow::CosTaper20:
             winnm = "CosTaper";
             paramval = 0.80;
             break;
         default:
             break;
-            
+
     }
 
     return setType( winnm, paramval );
 }
 
 
-bool ArrayNDWindow::setType( const char* winnm, float val ) 
+bool ArrayNDWindow::setType( const char* winnm, float val )
 {
     if ( !buildWindow(winnm,val) )
 	return false;
@@ -94,7 +96,7 @@ bool ArrayNDWindow::buildWindow( const char* winnm, float val )
 {
     const od_int64 totalsz = size_.getTotalSz();
     if ( totalsz <= 0 ) return false;
-    window_ = new float[totalsz];  
+    window_ = new float[totalsz];
     const int ndim = size_.getNDim();
     ArrayNDIter position( size_ );
 
@@ -108,14 +110,14 @@ bool ArrayNDWindow::buildWindow( const char* winnm, float val )
     {
 	for ( unsigned long off=0; off<totalsz; off++ )
 	{
-	    float dist = 0;    
+	    float dist = 0;
 
 	    for ( int idx=0; idx<ndim; idx++ )
 	    {
 		int sz =  size_.getSize(idx);
 		int halfsz = sz / 2;
 		float distval = (halfsz==0) ? 0 :
-		    		( (float) (position[idx] - halfsz) / halfsz );
+				( (float) (position[idx] - halfsz) / halfsz );
 		dist += distval * distval;
 	    }
 
@@ -123,7 +125,7 @@ bool ArrayNDWindow::buildWindow( const char* winnm, float val )
 
 	    window_[off] = windowfunc->getValue( dist );
 	    position.next();
-	}	
+	}
     }
     else
     {
@@ -141,10 +143,148 @@ bool ArrayNDWindow::buildWindow( const char* winnm, float val )
 
 	    window_[off] = windowval;
 	    position.next();
-	}	
+	}
     }
-	
+
 
     delete windowfunc;
     return true;
 }
+
+
+
+DefineEnumNames( PolyTrend, Order, 0, "Polynomial Order")
+{ "None", "Order0", "Order1", "Order2", 0 };
+
+
+
+PolyTrend::PolyTrend()
+    : order_(None)
+{}
+
+
+bool PolyTrend::operator==( const PolyTrend& p ) const
+{
+    if ( order_ != p.order_ )
+	return false;
+
+    return mIsEqual(f0_,p.f0_,mDefEps) && mIsEqual(f1_,p.f1_,mDefEps) &&
+	   mIsEqual(f2_,p.f2_,mDefEps) && mIsEqual(f11_,p.f11_,mDefEps) &&
+	   mIsEqual(f12_,p.f12_,mDefEps) && mIsEqual(f22_,p.f22_,mDefEps) &&
+	   posc_ == p.posc_;
+}
+
+
+void PolyTrend::initOrder0( const TypeSet<double>& vals )
+{
+    const int sz = vals.size();
+    f0_ = 0.;
+    for ( int idx=0; idx<sz; idx++ )
+	f0_ += vals[idx];
+
+    if ( sz < 2 )
+	return;
+
+    f0_ /= (double)sz;
+    order_ = Order0;
+}
+
+
+void PolyTrend::initOrder1( const TypeSet<Coord>& pos,
+			    const TypeSet<double>& vals )
+{
+    initCenter( pos );
+
+    const int sz = vals.size();
+    if ( sz < 3 )
+	initOrder0( vals );
+
+    Array2DImpl<double> a( sz, 3 );
+    Array1DImpl<double> b( sz );
+    for ( int idx=0; idx<sz; idx++ )
+    {
+	const double dx = pos[idx].x - posc_.x;
+	const double dy = pos[idx].y - posc_.y;
+	a.set( idx, 0, 1. );
+	a.set( idx, 1, dx );
+	a.set( idx, 2, dy );
+	b.set( idx, vals[idx] );
+    }
+
+    QRSolver<double> solver( a );
+    if ( !solver.isFullRank() )
+    {
+	f1_ = f2_ = 0;
+	initOrder0( vals );
+	return;
+    }
+
+    const Array1DImpl<double>* f = solver.apply( b );
+    f0_ = f->get( 0 );
+    f1_ = f->get( 1 );
+    f2_ = f->get( 2 );
+    delete f;
+    order_ = Order1;
+}
+
+
+void PolyTrend::initOrder2( const TypeSet<Coord>& pos,
+			    const TypeSet<double>& vals )
+{
+    initCenter( pos );
+
+    const int sz = vals.size();
+    if ( sz < 6 )
+	initOrder1( pos, vals );
+
+    Array2DImpl<double> a( sz, 6 );
+    Array1DImpl<double> b( sz );
+    for ( int idx=0; idx<sz; idx++ )
+    {
+	const double dx = pos[idx].x - posc_.x;
+	const double dy = pos[idx].y - posc_.y;
+	a.set( idx, 0, 1. );
+	a.set( idx, 1, dx );
+	a.set( idx, 2, dy );
+	a.set( idx, 3, dx*dx );
+	a.set( idx, 4, dx*dy );
+	a.set( idx, 5, dy*dy );
+	b.set( idx, vals[idx] );
+    }
+
+    QRSolver<double> solver( a );
+    if ( !solver.isFullRank() )
+    {
+	f11_ = f12_ = f22_ = 0.;
+	initOrder1( pos, vals );
+	return;
+    }
+
+    const Array1DImpl<double>* f = solver.apply( b );
+    f0_ = f->get( 0 );
+    f1_ = f->get( 1 );
+    f2_ = f->get( 2 );
+    f11_ = f->get( 3 );
+    f12_ = f->get( 4 );
+    f22_ = f->get( 5 );
+    delete f;
+    order_ = Order2;
+}
+
+
+void PolyTrend::initCenter( const TypeSet<Coord>& pos )
+{
+    const int sz = pos.size();
+    for ( int idx=0; idx<sz; idx++ )
+    {
+	posc_.x += pos[idx].x;
+	posc_.y += pos[idx].y;
+    }
+
+    if ( sz < 2 )
+	return;
+
+    posc_.x /= (double)sz;
+    posc_.y /= (double)sz;
+}
+
