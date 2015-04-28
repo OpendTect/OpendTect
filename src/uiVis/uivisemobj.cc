c562643be5a32c7369f5c10679e77ecbe9152f4b
dgbyuancheng <yuancheng.liu@dgbes.com>
2015-04-28 08:55:30 -0500

    od5.0: Fixed a loading bug
    Summary:
    Loading horizon 2D failed because of the following extra in the code: 
    (!emobj->isEmpty() && !dlg.execResult())
    
    Not sure why people would add those in previous version. My dvs_annot does not functioning well at this moment.
    
    Test Plan: Worked now on horizon 2D loading
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D7654

--- src/uiVis/uivisemobj.cc
+++ src/uiVis/uivisemobj.cc
@@ -187,8 +187,7 @@ uiVisEMObject::uiVisEMObject( uiParent* uip, const EM::ObjectID& emid,
 	emod->setZAxisTransform( zt, 0 );
 
 	uiTaskRunner dlg( uiparent_ );
-	if ( !emod->setEMObject(emid, &dlg) ||
-	     (!emobj->isEmpty() && !dlg.execResult()) )
+	if ( !emod->setEMObject(emid, &dlg) )
 	    mRefUnrefRet
 
 	visserv_->addObject( emod, sceneid, true );
