6a3456cfeef0fcc4c99150e85c70153811bcdde9
Kristofer Tingdahl <kristofer@tingdahl.net>
2015-05-02 00:04:53 +0200

    Made use of _strdup

--- include/Basic/msvcdefs.h
+++ include/Basic/msvcdefs.h
@@ -36,6 +36,7 @@ ________________________________________________________________________
 
 #define strtoll		_strtoi64
 #define strtoull	_strtoui64
+#define strdup		_strdup
 #define strtof		strtod
 
 #define finite		_finite
