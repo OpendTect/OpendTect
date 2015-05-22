91911a2c356695d799568f283834699aa8f22618
Bert Bril <ahbril@users.noreply.github.com>
2015-05-22 08:48:45 +0200

    od5.0: Last step in solving Z0 problem
    Summary:
    Now that the Z0 problem is solved, we should no longer make the traces to write 'write ready'.
    Moreover, this caused an actual 1-sample-off problem.
    
    Test Plan: Tested in all branches, also with Petrel.
    
    Reviewers: helene
    
    Reviewed By: helene
    
    Differential Revision: https://phabricator.dgbes.com:8443/D7866

--- src/Seis/seiswrite.cc
+++ src/Seis/seiswrite.cc
@@ -40,7 +40,7 @@ SeisTrcWriter::SeisTrcWriter( const IOObj* ioob, const GeomIDProvider* l )
 	, lineauxiopar_(*new IOPar)
 	, gidp_(l)
 	, worktrc_(*new SeisTrc)
-	, makewrready_(true)
+	, makewrready_(false)
 	, linedata_(0)
 {
     init();
@@ -52,7 +52,7 @@ SeisTrcWriter::SeisTrcWriter( const char* fnm, bool is_2d, bool isps )
 	, lineauxiopar_(*new IOPar)
 	, gidp_(0)
 	, worktrc_(*new SeisTrc)
-	, makewrready_(true)
+	, makewrready_(false)
 	, linedata_(0)
 {
     init();
