58a6524f1ec9e41366876348a806fb54b97dd9f3
Henrique Mageste <henrique.mageste@dgbes.com>
2015-05-25 10:29:22 -0300

    od6.0: New map scale or just scale for uiGraphicsScenes (Basemaps, 2DViewers, etc)
    Summary:
    New scale for uiGraphics Scene made from polygons.
    
    Two variables should be manage in order to make this object work properly:
    
    pxlength = length in pixels of the scale;
    length = length in any arbitrary coordinate
    
    Test Plan: Worked when added to Basemaps.
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Maniphest Tasks: T149
    
    Differential Revision: https://phabricator.dgbes.com:8443/D7880

---
+++ include/uiBase/uigraphicsscalebar.h
@@ -0,0 +1,63 @@
+#ifndef uigraphicsscalebar_h
+#define uigraphicsscalebar_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Henrique Mageste
+ Date:		May 2015
+ RCS:		$Id: $
+________________________________________________________________________
+
+-*/
+
+#include "uibasemod.h"
+#include "uigraphicsitem.h"
+#include "draw.h"
+
+class uiRectItem;
+class uiTextItem;
+class SurveyInfo;
+
+/*!
+\brief It adds a graphic scale made from polygons
+*/
+
+mExpClass(uiBase) uiScaleBarItem : public uiGraphicsItemGroup
+{
+public:
+			uiScaleBarItem(int pxwidth=100,int pxheight=6);
+			~uiScaleBarItem();
+
+    inline int		getPxHeight() const		{ return pxheight_; }
+    inline int		getLength() const		{ return length_; }
+    inline int		getPxWidth() const		{ return pxwidth_; }
+    inline void		setPxHeight(int pxh)		{ pxheight_ = pxh; }
+    inline void		setLength(int len)		{ length_ = len; }
+    inline void		setPxWidth(int pxwidth)		{ pxwidth_ = pxwidth; }
+
+    void		update();
+
+protected:
+    int			length_;
+    int			pxwidth_;
+    int			pxheight_;
+
+    uiRectItem*		upperright_;
+    uiRectItem*		uppermid_;
+    uiRectItem*		upperleft_;
+    uiRectItem*		lowerright_;
+    uiRectItem*		lowermid_;
+    uiRectItem*		lowerleft_;
+    uiTextItem*		startnr_;
+    uiTextItem*		midnr_;
+    uiTextItem*		stopnr_;
+
+private:
+    void		initDefaultScale();
+    void		setPolygons(int,int);
+
+};
+#endif
+
