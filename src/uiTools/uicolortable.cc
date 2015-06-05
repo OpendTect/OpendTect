f2e9a17b39784b1f6e0bc14554e7c7b79f91aeeb
Nanne Hemstra <hemstra@users.noreply.github.com>
2015-06-05 16:18:20 -0300

    Enabled coltab flipping from menu

--- src/uiTools/uicolortable.cc
+++ src/uiTools/uicolortable.cc
@@ -402,7 +402,7 @@ void uiColorTable::canvasClick( CallBacker* )
 	return;
 
     PtrMan<uiMenu> mnu = new uiMenu( parent_, uiStrings::sAction() );
-    if ( hasseq )
+    if ( hasseq || hasmapper )
     {
 	uiAction* itm =
 	    new uiAction("Flipped", mCB(this,uiColorTable,doFlip));
