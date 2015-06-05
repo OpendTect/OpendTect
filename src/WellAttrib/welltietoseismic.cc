62e37bc4118c3d3b12b39d65bde9861cd8c31dd7
Satyaki <satyaki.maitra@dgbes.com>
2015-06-05 11:53:30 +0530

    od5.0 : Fix for synthetic creation in well to tie seismics
    Summary: Fix for http://bugs.dgbes.com/view.php?id=4349 & http://bugs.dgbes.com/view.php?id=4364
    
    Test Plan: Synthetics can be generated
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D7960

--- src/WellAttrib/welltietoseismic.cc
+++ src/WellAttrib/welltietoseismic.cc
@@ -34,6 +34,7 @@ static const char* rcsID mUsedVar = "$Id$";
 
 #include "hiddenparam.h"
 
+static const char* sKeyAdvancedRayTracer()	{ return "FullRayTracer"; }
 
 namespace WellTie
 {
@@ -493,6 +494,15 @@ bool DataPlayer::doFullSynthetics( const Wavelet& wvlt )
     gen.setOutSampling( data_.getTraceRange() );
     gen.enableFourierDomain( !GetEnvVarYN("DTECT_CONVOLVE_USETIME") );
     IOPar par;
+    FixedString defrayparstr = sKeyAdvancedRayTracer();
+    const BufferStringSet& facnms = RayTracer1D::factory().getNames();
+    if ( !facnms.isEmpty() )
+    {
+	const int typeidx = facnms.indexOf( defrayparstr );
+	FixedString facnm( typeidx>=0 ? facnms.get(typeidx) : facnms.get(0) );
+	par.set( sKey::Type(), facnm );
+    }
+
     gen.usePar( par );
     TaskRunner* tr = data_.trunner_;
     if ( !TaskRunner::execute(tr,gen) )
