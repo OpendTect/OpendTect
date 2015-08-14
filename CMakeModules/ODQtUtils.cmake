9b5c026c6aafe7885e81247ff93f42ba5229f88a
Kristofer Tingdahl <kristofer@tingdahl.net>
2015-08-14 12:44:45 +0200

    Fixed configure problems in dgb repository.

--- CMakeModules/ODQtUtils.cmake
+++ CMakeModules/ODQtUtils.cmake
@@ -9,7 +9,10 @@ set( QTDIR "" CACHE PATH "QT Location" )
 option ( OD_NO_QT "Turn off all QT" NO )
 
 ##Create launcher for linguist
-configure_file( CMakeModules/templates/linguist.csh.in dtect/linguist.csh @ONLY )
+set( LINGUIST_LAUNCHER "CMakeModules/templates/linguist.csh.in" )
+if ( EXISTS ${LINGUIST_LAUNCHER} )
+    configure_file( ${LINGUIST_LAUNCHER} dtect/linguist.csh @ONLY )
+endif()
 
 macro(ADD_TO_LIST_IF_NEW LISTNAME ITEMNAME)
     list( FIND ${LISTNAME} "${ITEMNAME}" ITMINDEX )
