faa89943066f21c6f8fc79179eb4d33f2f5a5065
Nanne Hemstra <hemstra@users.noreply.github.com>
2015-08-20 10:24:12 -0300

    Added

---
+++ include/uiIo/uirandomlineman.h
@@ -0,0 +1,38 @@
+#ifndef uirandomlineman_h
+#define uirandomlineman_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Nanne Hemstra
+ Date:           2015
+ RCS:           $Id$
+________________________________________________________________________
+
+-*/
+
+#include "uiiomod.h"
+#include "uiobjfileman.h"
+
+/*! \brief
+RandomLine manager
+*/
+
+
+mExpClass(uiIo) uiRandomLineMan : public uiObjFileMan
+{ mODTextTranslationClass(uiRandomLineMan);
+public:
+				uiRandomLineMan(uiParent*);
+				~uiRandomLineMan();
+
+    mDeclInstanceCreatedNotifierAccess(uiRandomLineMan);
+
+protected:
+
+    void			mkFileInfo();
+
+};
+
+#endif
+
