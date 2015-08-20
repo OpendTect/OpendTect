4bafa84309e95343a8049f5f145411386e1d06eb
Nanne Hemstra <hemstra@users.noreply.github.com>
2015-08-20 16:45:11 -0300

    Better layout

--- src/uiSeis/uiseislinesel.cc
+++ src/uiSeis/uiseislinesel.cc
@@ -62,7 +62,7 @@ uiSeis2DLineChoose::uiSeis2DLineChoose( uiParent* p, OD::ChoiceMode cm )
 		    mCB(this,uiSeis2DLineChoose,objInserted) );
 	}
 
-	butgrp->attach( leftOf, listfld_ );
+	butgrp->attach( centeredLeftOf, listfld_ );
     }
 }
 
@@ -115,6 +115,8 @@ void uiSeis2DLineChoose::init( OD::ChoiceMode cm )
 	lbchoiceio_->storeRequested.notify(
 				mCB(this,uiSeis2DLineChoose,writeChoiceReq) );
     }
+
+    setHAlignObj( listfld_ );
 }
 
 
