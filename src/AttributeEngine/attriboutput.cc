60f4c7436d09a773aa787c8a09d65594d52f41c3
Raman Singh <raman.singh@dgbes.com>
2015-08-21 17:20:13 +0530

    Fix for resampling of output

--- src/AttributeEngine/attriboutput.cc
+++ src/AttributeEngine/attriboutput.cc
@@ -475,6 +475,24 @@ void SeisTrcStorOutput::collectData( const DataHolder& data, float refstep,
 	    }
 	}
     }
+
+    if ( !mIsEqual(desiredvolume_.zsamp_.step,trc_->info().sampling.step,1e-6) )
+    {
+	StepInterval<float> reqzrg = desiredvolume_.zsamp_;
+	reqzrg.limitTo( trc_->zRange() );
+	const int nrsamps = mCast( int, reqzrg.nrfSteps() + 1 );
+	for ( int icomp=0; icomp<trc_->data().nrComponents(); icomp++ )
+	{
+	    SeisTrc temptrc( *trc_ );
+	    trc_->info().sampling.step = desiredvolume_.zsamp_.step;
+	    trc_->data().getComponent(icomp)->reSize( nrsamps );
+	    for ( int isamp=0; isamp<nrsamps; isamp++ )
+	    {
+		float t = reqzrg.start + isamp * reqzrg.step;
+		trc_->set( isamp, temptrc.getValue(t,icomp), icomp );
+	    }
+	}
+    }
 }
 
 
