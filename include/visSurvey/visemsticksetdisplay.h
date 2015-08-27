83f5bda3ea67fd2c994ed45256e2ff8c0bf163c6
dingzheng-dgb <dingzheng-dgb@users.noreply.github.com>
2015-08-27 13:27:10 +0200

    od6.0- New class visEmStickSetDisplay
    Summary:
    The new class is used for display knots( defined as markerset in this class ) in visFaultDisplay and visFaultStickSetDisplay. Perhaps the name is not good. 
    I have a little confuse, in general we call knot, in fault and faultstickset display, we call stick( see menu). So here I cal stickset too.
    
    This class will be the inherited by visFaultdisplay and visFaultSticksetDisplay 
    
    Please let me know if the indention is OK. From my side the indention is 4 spaces.
    
    Test Plan: Compiles fine and Test works in classes  visFaultdisplay and visFaultSticksetDisplay.
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D8413

---
+++ include/visSurvey/visemsticksetdisplay.h
@@ -0,0 +1,87 @@
+#ifndef visemsticksetdisplay_h
+#define visemsticksetdisplay_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Kristofer Tingdahl
+ Date:		4-11-2002
+ RCS:		$Id$
+________________________________________________________________________
+
+-*/
+
+#include "vissurveymod.h"
+#include "viscoord.h"
+
+namespace EM 
+{ 
+    class Fault; 
+    typedef od_int32 ObjectID; 
+}
+
+namespace visBase 
+{  
+    class MarkerSet; 
+    class EventCatcher;
+    class Transformation; 
+    class PolygonSelection;
+}
+
+namespace Geometry { class FaultStickSet; }
+namespace Survey { class Geometry3D; }
+
+namespace visSurvey
+{
+class Scene;
+
+mExpClass(visSurvey) StickSetDisplay 
+{ 
+public:
+				StickSetDisplay(bool);
+				~StickSetDisplay();
+    void			setDisplayTransformation(const mVisTrans*);
+    const mVisTrans*		getDisplayTransformation() const;
+    void			polygonSelectionCB();
+    void			updateStickMarkerSet();
+    void	    		getMousePosInfo(const visBase::EventInfo&,
+					       Coord3& xyzpos,BufferString& val,
+					       BufferString& info) const;
+protected:
+    Geometry::FaultStickSet*	faultStickSetGeometry(int);
+    void			stickSelectionCB(CallBacker*,
+						 const Survey::Geometry3D*);
+    void			setCurScene( visSurvey::Scene* scene )
+					   { ownerscene_ = scene; }
+    const visSurvey::Scene*	getCurScene() { return ownerscene_; }
+    bool			matchMarker(int,int,const Coord3,const Coord3,
+					    const Coord3);
+    EM::Fault*			fault_;
+    const mVisTrans*		displaytransform_;
+    bool			ctrldown_;
+    bool			showmanipulator_;
+    bool			stickselectmode_;
+    bool			displaysticks_;
+    bool			hideallknots_;
+    bool			faultstickset_;
+    bool			pickmarker_;
+    visSurvey::Scene*		ownerscene_;
+    visBase::EventCatcher*	eventcatcher_;
+
+    ObjectSet<visBase::MarkerSet>   knotmarkersets_;
+    struct StickIntersectPoint
+    {
+	Coord3			pos_;
+	int			sid_;
+	int			sticknr_;
+    };
+    ObjectSet<StickIntersectPoint>  stickintersectpoints_;
+
+};
+
+};
+
+
+#endif
+
