3fd3dfb246a67315f473314d647a14cd982a32a2
Nanne Hemstra <hemstra@users.noreply.github.com>
2015-09-15 16:23:38 -0300

    Fix for horizon display on random lines

--- src/visSurvey/vishorizondisplay.cc
+++ src/visSurvey/vishorizondisplay.cc
@@ -1662,7 +1662,7 @@ void HorizonDisplay::drawHorizonOnRandomTrack( const TypeSet<Coord>& trclist,
 		    continue;
 		}
 	    }
-	    else if ( idxps.size()>1 )
+	    else if ( idxps.size()>0 )
 	    {
 		mAddLinePrimitiveSet;
 		idxps.erase();
