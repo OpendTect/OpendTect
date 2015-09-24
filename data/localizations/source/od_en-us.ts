645012c5549dc2475cbf093b45991ffa43fe6bfc
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2015-09-24 23:15:30 +0200

    Updated plural table.

--- data/localizations/source/od_en-us.ts
+++ data/localizations/source/od_en-us.ts
@@ -14,7 +14,7 @@
 <context>
     <name>TextTranslatorLanguage</name>
     <message numerus="yes">
-        <location filename="../../../src/Basic/texttranslator.cc" line="+58"/>
+        <location filename="../../../src/Basic/texttranslator.cc" line="+59"/>
         <source>Language Name</source>
         <translation>
             <numerusform>English</numerusform>
@@ -67,7 +67,15 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location line="+155"/>
+        <location line="+170"/>
+        <source>ColorTable</source>
+        <translation>
+            <numerusform>Color Table</numerusform>
+            <numerusform>Color Tables</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+45"/>
         <source>input file</source>
         <translation>
             <numerusform>input file</numerusform>
@@ -75,7 +83,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location line="+6"/>
+        <location line="+9"/>
         <source>output file</source>
         <translation>
             <numerusform>output file</numerusform>
@@ -83,7 +91,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location line="+18"/>
+        <location line="+29"/>
         <source>Fault</source>
         <translation>
             <numerusform>Fault</numerusform>
@@ -115,7 +123,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location line="+29"/>
+        <location line="+36"/>
         <source>Marker</source>
         <translation>
             <numerusform>Marker</numerusform>
@@ -123,7 +131,15 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location line="+84"/>
+        <location line="+72"/>
+        <source>Selection</source>
+        <translation>
+            <numerusform>Selection</numerusform>
+            <numerusform>Selections</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+34"/>
         <source>Wavenumber</source>
         <translation>
             <numerusform>Wavenumber</numerusform>
@@ -167,7 +183,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location line="-47"/>
+        <location line="-63"/>
         <source>Seismic</source>
         <translation>
             <numerusform>Seismic</numerusform>
@@ -175,7 +191,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location line="+20"/>
+        <location line="+23"/>
         <source>Setting</source>
         <translation>
             <numerusform>Setting</numerusform>
@@ -183,7 +199,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location line="+21"/>
+        <location line="+34"/>
         <source>Wavelet</source>
         <translation>
             <numerusform>Wavelet</numerusform>
@@ -196,6 +212,214 @@
         <translation>
             <numerusform>Well</numerusform>
             <numerusform>Wells</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../../../include/Basic/uistrings.h" line="+161"/>
+        <source>Attribute</source>
+        <translation>
+            <numerusform>Attribute</numerusform>
+            <numerusform>Attributes</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+5"/>
+        <source>Body</source>
+        <translation>
+            <numerusform>Body</numerusform>
+            <numerusform>Bodies</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+25"/>
+        <source>Code</source>
+        <translation>
+            <numerusform>Code</numerusform>
+            <numerusform>Codes</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+2"/>
+        <source>Color</source>
+        <translation>
+            <numerusform>Color</numerusform>
+            <numerusform>Colors</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+4"/>
+        <source>Coordinate</source>
+        <translation>
+            <numerusform>Coordinate</numerusform>
+            <numerusform>Coordinates</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+4"/>
+        <source>Correlation</source>
+        <translation>
+            <numerusform>Correlation</numerusform>
+            <numerusform>Correlations</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+4"/>
+        <source>Cross-line</source>
+        <translation>
+            <numerusform>Cross-line</numerusform>
+            <numerusform>Cross-lines</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+22"/>
+        <source>Error</source>
+        <translation>
+            <numerusform>Error</numerusform>
+            <numerusform>Errors</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+7"/>
+        <source>Factor</source>
+        <translation>
+            <numerusform>Factor</numerusform>
+            <numerusform>Factors</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+6"/>
+        <source>Filter</source>
+        <translation>
+            <numerusform>Filter</numerusform>
+            <numerusform>Filters</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+14"/>
+        <source>In-line</source>
+        <translation>
+            <numerusform>In-line</numerusform>
+            <numerusform>In-lines</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+16"/>
+        <source>Log</source>
+        <translation>
+            <numerusform>Log</numerusform>
+            <numerusform>Logs</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+14"/>
+        <source>Mute</source>
+        <translation>
+            <numerusform>Mute</numerusform>
+            <numerusform>Mutes</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+1"/>
+        <source>Name</source>
+        <translation>
+            <numerusform>Name</numerusform>
+            <numerusform>Names</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+25"/>
+        <source>Parameter</source>
+        <translation>
+            <numerusform>Parameter</numerusform>
+            <numerusform>Parameters</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+3"/>
+        <source>Position</source>
+        <translation>
+            <numerusform>Position</numerusform>
+            <numerusform>Positions</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+7"/>
+        <source>Range</source>
+        <translation>
+            <numerusform>Range</numerusform>
+            <numerusform>Ranges</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+1"/>
+        <source>Random Line</source>
+        <translation>
+            <numerusform>Random Line</numerusform>
+            <numerusform>Random Lines</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+18"/>
+        <source>Scene</source>
+        <translation>
+            <numerusform>Scene</numerusform>
+            <numerusform>Scenes</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+27"/>
+        <source>Step</source>
+        <translation>
+            <numerusform>Step</numerusform>
+            <numerusform>Steps</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+8"/>
+        <source>Survey</source>
+        <translation>
+            <numerusform>Survey</numerusform>
+            <numerusform>Surveys</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+9"/>
+        <source>Trace</source>
+        <translation>
+            <numerusform>Trace</numerusform>
+            <numerusform>Traces</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+7"/>
+        <source>Unit</source>
+        <translation>
+            <numerusform>Unit</numerusform>
+            <numerusform>Units</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+5"/>
+        <source>Value</source>
+        <translation>
+            <numerusform>Value</numerusform>
+            <numerusform>Values</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+29"/>
+        <source>Z-slice</source>
+        <translation>
+            <numerusform>Z-slice</numerusform>
+            <numerusform>Z-slices</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location line="+1"/>
+        <source>Z value</source>
+        <translation>
+            <numerusform>Z value</numerusform>
+            <numerusform>Z values</numerusform>
         </translation>
     </message>
 </context>
