2c21a76e59c2298fcc3c863cb5ee2b0411e9da9b
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2015-09-24 21:42:50 +0200

    Changed number-format of floats.

--- src/Basic/texttranslator.cc
+++ src/Basic/texttranslator.cc
@@ -19,6 +19,8 @@ static const char* rcsID mUsedVar = "$Id$";
 #ifndef OD_NO_QT
 # include <QTranslator>
 # include <QLocale>
+#else
+# include "keystrs.h"
 #endif
 
 void TextTranslateMgr::GetLocalizationDir( FilePath& res )
@@ -62,6 +64,8 @@ TextTranslatorLanguage::TextTranslatorLanguage( const char* localename )
 	name.translate( maintrans, *languagename_ );
 	//Force be a part of the plural setup
     }
+
+    locale_->setNumberOptions( QLocale::OmitGroupSeparator );
 #endif
 }
 
@@ -84,7 +88,11 @@ TextTranslatorLanguage::getTranslator( const char* appl ) const
 
 BufferString TextTranslatorLanguage::getLocaleName() const
 {
+#ifndef OD_NO_QT
     return BufferString( locale_->name() );
+#else
+    return sKey::EmptyString();
+#endif
 }
 
 
