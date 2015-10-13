c753356bcd99942fc7ca4f200c3a646a90f8de7b
Jaap Glas <jaap.glas@dgbes.com>
2015-10-13 14:54:14 +0200

    Bugfix: act mode cursor did not refresh after middle-mouse view mode action
    Summary:
    Act mode cursor did not refresh after middle-mouse view mode action.
    Phabricator D8794 (issue 2) "Now act mode cursor is refreshed when coming
    back from view mode" was incomplete, because it only covered switches of
    the view/act mode button.
    
    Test Plan:
    Act mode cursor now returns both after view mode middle-mouse actions and
    view/act mode button switches.
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D8977

--- src/uiOSG/ui3dviewer.cc
+++ src/uiOSG/ui3dviewer.cc
@@ -847,7 +847,6 @@ void ui3DViewerBody::setViewMode( bool yn, bool trigger )
 	scene_->setPickable( !yn );
 
     setViewModeCursor( HoverCursor );
-    actmodecursor_.shape_ = MouseCursor::NotSet;
 
     if ( trigger )
 	handle_.viewmodechanged.trigger( handle_ );
@@ -867,7 +866,10 @@ void ui3DViewerBody::setViewModeCursor( ViewModeCursor viewmodecursor )
     else if ( viewmodecursor==HoverCursor && isViewMode() )
 	cursor.shape_ = MouseCursor::PointingHand;
     else
+    {
 	cursor.shape_ = MouseCursor::Arrow;
+	actmodecursor_.shape_ = MouseCursor::NotSet;
+    }
 
     mQtclass(QCursor) qcursor;
     uiCursorManager::fillQCursor( cursor, qcursor );
