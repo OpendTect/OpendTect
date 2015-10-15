573c73e90cdfa114b77d21aebdecbb837b983f9c
dgbyuancheng <yuancheng.liu@dgbes.com>
2015-10-15 09:17:31 -0500

    od6.0: A code style change
    Summary: as title
    
    Test Plan: no test needed
    
    Reviewers: nanne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D9019

--- src/Algo/conncomponents.cc
+++ src/Algo/conncomponents.cc
@@ -34,10 +34,9 @@ CC2DExtracor( const Array3D<bool>& inp )
 const TypeSet<TypeSet<TypeSet<int> > >&	getCubeComps()	{ return slicecomps_; }
 
 protected:
+
 od_int64 nrIterations() const	{ return input_.info().getSize(2); }
-uiString uiMessage() const	{
-			    return tr("Computing 2D connected  components");
-				}
+uiString uiMessage() const { return tr("Computing 2D connected	components"); }
 
 bool doWork( od_int64 start, od_int64 stop, int threadid )
 {
