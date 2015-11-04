f4ce9b2279e890ef4e58022969d67b67defd4f0e
Satyaki <satyaki.maitra@dgbes.com>
2015-11-04 11:06:10 +0530

    Same as D9119

--- src/uiPreStackProcessing/uiprestackanglemute.cc
+++ src/uiPreStackProcessing/uiprestackanglemute.cc
@@ -182,12 +182,12 @@ void uiAngleCompAdvParsDlg::createAngleCompFields()
     smoothwinlengthlbl_ = new uiLabel( this, SI().getUiZUnitString(false) );
     smoothwinlengthlbl_->attach( rightOf, smoothwinlengthfld_ );
 
-    freqf3fld_ = new uiGenInput( this, tr("Freq F3"), FloatInpSpec() );
+    freqf3fld_ = new uiGenInput( this, tr("Frequency F3"), FloatInpSpec() );
     freqf3fld_->attach( alignedBelow, smoothtypefld_ );
     freqf3lbl_ = new uiLabel( this, tr("Hz") );
     freqf3lbl_->attach( rightOf, freqf3fld_ );
 
-    freqf4fld_ = new uiGenInput( this, tr("Freq F4"), FloatInpSpec() );
+    freqf4fld_ = new uiGenInput( this, tr("Frequency F4"), FloatInpSpec() );
     freqf4fld_->attach( alignedBelow, freqf3fld_ );
     freqf4lbl_ = new uiLabel( this, tr("Hz") );
     freqf4lbl_->attach( rightOf, freqf4fld_ );
