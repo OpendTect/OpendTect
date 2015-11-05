d51928f887b490b72e68e139068c85e1c2ed4e4f
Nanne Hemstra <hemstra@users.noreply.github.com>
2015-11-05 16:39:59 -0200

    Fixed compiler error

--- src/uiEMAttrib/uicreate2dgrid.cc
+++ src/uiEMAttrib/uicreate2dgrid.cc
@@ -89,7 +89,7 @@ void update()
 
     if ( !grid_ ) return;
 
-    OD::LineStyle ls;
+    LineStyle ls;
     ls.width_ = 5;
     int graphicszval = 2;
     for ( int idx=0; idx<grid_->size(true); idx++ )
