b1bf4a0ea9a84617181af12088528d9c7ad2a49a
Ranojay Sen <ranojay.sen@dgbes.com>
2015-11-06 11:37:32 +0530

    First working copy of uiGVFreehandAreaSelectionTool

---
+++ include/uiTools/uigvfreehandareaselectiontool.h
@@ -0,0 +1,67 @@
+#ifndef uigvfreehandareaselectiontool_h
+#define uigvfreehandareaselectiontool_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Ranojay
+ Date:          Nov 2015
+ RCS:           $Id$
+________________________________________________________________________
+
+-*/
+
+#include "uitoolsmod.h"
+#include "callback.h"
+
+/*! \brief Tool that handles freehand selection of areas on graphics views.
+
+  Enabling the tool will make sure that when the user starts dragging a polygon
+  can be constructed by the user. When dragging starts, while dragging, and
+  when it stops you can get notifications.
+
+ */
+
+class uiGraphicsView;
+class uiPolygonItem;
+template <class T> class ODPolygon;
+
+mExpClass(uiTools) uiGVFreehandAreaSelectionTool : public CallBacker
+{
+public:
+
+			uiGVFreehandAreaSelectionTool(uiGraphicsView&);
+			~uiGVFreehandAreaSelectionTool();
+
+    void		enable();
+    void		disable();
+    bool		isEnabled() const { return enabled_; }
+
+    void		setPolygonMode(bool);
+    bool		isPolygonMode() const;
+
+    const ODPolygon<int>& selection() const { return odpolygon_; }
+
+    Notifier<uiGVFreehandAreaSelectionTool>	started;
+    Notifier<uiGVFreehandAreaSelectionTool>	pointAdded;
+    Notifier<uiGVFreehandAreaSelectionTool>	stopped;
+
+protected:
+
+    uiGraphicsView&	gv_;
+    uiPolygonItem*	polygonselitem_;
+    ODPolygon<int>&	odpolygon_;
+
+    
+    bool		ispolygonmode_;
+    bool		enabled_;
+
+    void		mousePressCB(CallBacker*);
+    void		mouseReleaseCB(CallBacker*);
+    void		mouseMoveCB(CallBacker*);
+};
+
+
+#endif
+
---
+++ src/uiTools/uigvfreehandareaselectiontool.cc
@@ -0,0 +1,122 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Ranojay
+ Date:          Nov 2015
+________________________________________________________________________
+
+-*/
+static const char* rcsID mUsedVar = "$Id$";
+
+#include "uigvfreehandareaselectiontool.h"
+#include "uigraphicsview.h"
+#include "uigraphicsscene.h"
+#include "uigraphicsitemimpl.h"
+
+#include "mouseevent.h"
+#include "polygon.h"
+
+
+uiGVFreehandAreaSelectionTool::uiGVFreehandAreaSelectionTool(uiGraphicsView& gv)
+    : gv_(gv)
+    , ispolygonmode_(true)
+    , enabled_(true)
+    , polygonselitem_(0)
+    , odpolygon_(*new ODPolygon<int>())
+    , started(this)
+    , pointAdded(this)
+    , stopped(this)
+{
+    polygonselitem_ = gv_.scene().addItem( new uiPolygonItem() );
+    polygonselitem_->setPenColor( Color(255,0,0) );
+
+    mAttachCB( gv_.getMouseEventHandler().movement,
+		uiGVFreehandAreaSelectionTool::mouseMoveCB );
+    
+    mAttachCB( gv_.getMouseEventHandler().buttonPressed,
+		uiGVFreehandAreaSelectionTool::mousePressCB );
+
+    mAttachCB( gv_.getMouseEventHandler().buttonReleased,
+		uiGVFreehandAreaSelectionTool::mouseReleaseCB );
+}
+
+
+uiGVFreehandAreaSelectionTool::~uiGVFreehandAreaSelectionTool()
+{
+    detachAllNotifiers();
+    delete &odpolygon_;
+}
+
+
+void uiGVFreehandAreaSelectionTool::enable()
+{
+    enabled_ = true;
+    polygonselitem_->setVisible( enabled_ );
+}
+
+
+void uiGVFreehandAreaSelectionTool::disable()
+{
+    enabled_ = false;
+    polygonselitem_->setVisible( enabled_ );
+}
+
+
+void uiGVFreehandAreaSelectionTool::setPolygonMode( bool ispoly )
+{
+    ispolygonmode_ = ispoly;
+}
+
+
+bool uiGVFreehandAreaSelectionTool::isPolygonMode() const
+{
+    return ispolygonmode_;
+}
+
+
+void uiGVFreehandAreaSelectionTool::mousePressCB( CallBacker* )
+{
+    odpolygon_.setEmpty();
+}
+
+
+void uiGVFreehandAreaSelectionTool::mouseReleaseCB( CallBacker* )
+{
+    //TODO
+}
+
+
+void uiGVFreehandAreaSelectionTool::mouseMoveCB( CallBacker* )
+{
+    const MouseEvent& ev = gv_.getMouseEventHandler().event();
+    if ( ev.leftButton() && isEnabled() )
+    {
+	if ( isPolygonMode() )
+	{
+	    const Geom::Point2D<int>& pos = ev.pos();
+	    if ( odpolygon_.size() > 3 )
+	    {
+		if ( !odpolygon_.isInside(pos,true,0) )
+		    odpolygon_.add( pos );
+	    }
+	    else
+		odpolygon_.add( pos );
+	}
+	else
+	{
+	    if ( odpolygon_.isEmpty() )
+		odpolygon_.add( ev.pos() );
+
+	    const Geom::Point2D<int>& startpos = odpolygon_.getVertex( 0 );
+	    odpolygon_.setEmpty();
+	    odpolygon_.add( startpos );
+	    odpolygon_.add( Geom::Point2D<int>(startpos.x,ev.pos().y) );
+	    odpolygon_.add( ev.pos() );
+	    odpolygon_.add( Geom::Point2D<int>(ev.pos().x,startpos.y) );
+	    odpolygon_.add( startpos );
+	}
+    }
+   
+    polygonselitem_->setPolygon( odpolygon_ );
+}
