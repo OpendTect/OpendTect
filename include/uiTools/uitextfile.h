1ea856ec6cf42bcc337daba00e11d2c08cb0650b
Nanne Hemstra <hemstra@users.noreply.github.com>
2015-11-23 16:10:12 -0200

    Make sure reload button has reload icon

--- include/uiTools/uitextfile.h
+++ include/uiTools/uitextfile.h
@@ -122,6 +122,7 @@ protected:
     void		dismiss(CallBacker*);
     bool		rejectOK(CallBacker*);
     bool		acceptOK(CallBacker*);
+    void		finalizeCB(CallBacker*);
 
     void		fileNmChgd(CallBacker*);
     void		init(const Setup&,const uiTextFile::Setup&,const char*);
