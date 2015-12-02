e4ca9f489a903d64858de2ea4ac0dcdb9c480696
Kristofer Tingdahl <kristofer@tingdahl.net>
2015-12-02 13:37:59 +0100

    Fixed merge problem.

--- src/uiTools/uiscaler.cc
+++ src/uiTools/uiscaler.cc
@@ -154,7 +154,6 @@ void uiScaler::typeSel( CallBacker* )
 	linearfld->setSensitive( typ == 0 );
     if ( basefld ) basefld->display( typ > 0 );
 }
-<<<<<<< HEAD
 
 
 void uiScaler::fillPar( IOPar& iop ) const
