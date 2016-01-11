OpendTect/OpendTect

commit 7a0d76206afa95e5a8c0b0863e00516f086683f6

od6.0-bug fixing 5067 about deleting pick in displaying at section
Summary:
This is the same revision of D9443 in od trunk.
This is a bug after delete a pick at the display status of displaying at section, the display on/off array is not updated in osggeo markerset class.
then the displayed marker of markerset is wrong. in the option of displaying at section, we just display part of markers not all, so the onoff array
is wrong, the displayed marker also is wrong.

Test Plan: Compiles and Testing fine. The bug is fixed.

Reviewers: kristofer

Reviewed By: kristofer

Differential Revision: https://phabricator.dgbes.com:8443/D9444

include/visBase/vismarkerset.h
@@ -111,6 +111,7 @@ public:
     void		clearMarkers();
     			//!<Removes all markers
     void		turnMarkerOn(unsigned int idx,bool);
+    bool		markerOn(unsigned int);
 
     int			findClosestMarker(const Coord3&, bool scenespace=false);
     int			findMarker(const Coord3&, const Coord3& eps,

src/visBase/vismarkerset.cc
@@ -135,6 +135,7 @@ void MarkerSet::removeMarker( int idx )
     if ( material_ ) material_->removeColor( idx );
     if ( coords_ ) coords_->removePos( idx, false );
     if ( coords_ ) coords_->dirty();
+    markerset_->removeMarkerOnOff( idx );
     markerset_->forceRedraw( true );
     
 }
@@ -355,6 +356,12 @@ void MarkerSet::turnMarkerOn( unsigned int idx,bool yn )
 }
 
 
+bool MarkerSet::markerOn( unsigned int idx )
+{
+    return markerset_->markerOn( idx );
+}
+
+
 int MarkerSet::findClosestMarker( const Coord3& tofindpos, 
 				 const bool scenespace )
 {
@@ -395,8 +402,10 @@ int MarkerSet::addPos( const Coord3& crd, bool draw )
     if ( coords_ )
 	index = coords_->addPos( crd );
     
+    markerset_->turnMarkerOn( index, draw );
+
     if ( draw && markerset_ )
-        markerset_->forceRedraw( true );
+	markerset_->forceRedraw( true );
 
     return index;
 }
@@ -406,6 +415,9 @@ void MarkerSet::setPos( int idx, const Coord3& crd, bool draw )
 {
     if ( coords_ )
 	coords_->setPos( idx, crd );
+
+    markerset_->turnMarkerOn( idx, draw );
+
     if ( draw && markerset_ )
 	markerset_->forceRedraw( true );
 }
@@ -415,6 +427,8 @@ void MarkerSet::insertPos( int idx, const Coord3& crd, bool draw )
 {
     if ( coords_ ) 
 	coords_->insertPos( idx, crd );
+
+    markerset_->turnMarkerOn( idx, draw );
     if ( draw && markerset_ )
 	markerset_->forceRedraw( true );
 }

include/visSurvey/vispicksetdisplay.h
@@ -107,6 +107,9 @@ protected:
     void			polygonFinishedCB(CallBacker*);
     void			updateSelections(
 					    const visBase::PolygonSelection*);
+    bool			updateMarkerAtSection(const SurveyObject*,int);
+    void			updateLineAtSection();
+
     virtual bool		removeSelections(TaskRunner*);
     virtual bool		draggerNormal() const;
     virtual void		setDraggerNormal(const Coord3&);

src/visSurvey/vispicksetdisplay.cc
@@ -194,9 +194,9 @@ void PickSetDisplay::setPosition( int idx, const Pick::Location& loc, bool add )
 	return;
     }
     if ( add )
-	markerset_->insertPos( idx, loc.pos_, false );
+	markerset_->insertPos( idx, loc.pos_, true );
     else
-	markerset_->setPos( idx, loc.pos_, false );
+	markerset_->setPos( idx, loc.pos_, true );
     if ( set_->disp_.markertype_ == MarkerStyle3D::Arrow ||
 	 set_->disp_.markertype_ == MarkerStyle3D::Plane )
     	markerset_->setSingleMarkerRotation( getDirection(loc), idx );
@@ -513,55 +513,69 @@ void PickSetDisplay::otherObjectsMoved(
     if ( showall_ && invalidpicks_.isEmpty() )
 	return;
 
-    TypeSet<Coord3> polycoords;
     for ( int idx=0; idx<markerset_->getCoordinates()->size(); idx++ )
     {
-	Coord3 pos = set_->validIdx(idx) ? (*set_)[idx].pos_ : Coord3::udf();
-	if ( datatransform_ ) pos.z = datatransform_->transform( pos );
-
-	if ( scene_ )
-	    scene_->getUTM2DisplayTransform()->transform( pos );
-
-	bool newstatus;
-	if ( !pos.isDefined() )
-	    newstatus = false;
-	else if ( showall_ )
-	    newstatus = true;
-	else
+	for ( int idy=0; idy<objs.size(); idy++ )
 	{
-	    newstatus = false;
-	    for ( int idy=0; idy<objs.size(); idy++ )
-	    {
-		mDynamicCastGet(const EMObjectDisplay*,emobj,objs[idy])
-		if ( emobj && emobj->displayedOnlyAtSections() )
-		    continue;
-
-		const float dist = objs[idy]->calcDist( pos );
-		if ( dist < objs[idy]->maxDist() )
-		{
-		    newstatus = true;
-		    break;
-		}
-	    }
+	    if ( updateMarkerAtSection(objs[idy],idx) )
+		invalidpicks_ += idx;
+	    else
+		invalidpicks_ -= idx;
 	}
+    }
+    updateLineAtSection();
+}
 
-	markerset_->turnMarkerOn( idx, newstatus );
-	if ( newstatus )
-	{
-	    invalidpicks_ -= idx;
-	    polycoords += markerset_->getCoordinates()->getPos( idx );
-	}
-	else
-	    invalidpicks_ += idx;
 
+bool PickSetDisplay::updateMarkerAtSection( const SurveyObject* obj, int idx )
+{
+    if ( !obj ) return false;
+
+    Coord3 pos = set_->validIdx(idx) ? (*set_)[idx].pos_ : Coord3::udf();
+    if ( !pos.isDefined()) return false;
+
+    if ( datatransform_ ) 
+	pos.z = datatransform_->transform( pos );
+
+    if ( scene_ )
+	scene_->getUTM2DisplayTransform()->transform( pos );
+
+    bool onsection = false;
+    if ( !pos.isDefined() )
+	return false;
+    else if ( showall_ )
+	onsection = true;
+    else
+    {
+	mDynamicCastGet( const EMObjectDisplay*,emobj,obj )
+	if ( emobj && emobj->displayedOnlyAtSections() )
+	    return false;
+
+	const float dist = obj->calcDist( pos );
+	if ( dist<obj->maxDist() )
+	    onsection = true;
     }
 
-    if ( polyline_ )
-    {
+    markerset_->turnMarkerOn( idx, onsection );
+    return onsection;
+}
+
+
+void PickSetDisplay::updateLineAtSection()
+{
+   if ( polyline_ )
+   {
+       TypeSet<Coord3> polycoords;
+       for ( int idx = 0; idx<markerset_->getCoordinates()->size(); idx++ )
+	   polycoords += markerset_->getCoordinates()->getPos(idx);
+
 	polyline_->removeAllPoints();
 	int pidx = 0;
 	for ( pidx=0; pidx<polycoords.size(); pidx++ )
-	    polyline_->addPoint( polycoords[pidx] );
+	{
+	    if ( markerset_->markerOn(pidx) )
+		polyline_->addPoint( polycoords[pidx] );
+	}
 
 	if ( pidx && set_->disp_.connect_ == Pick::Set::Disp::Close )
 	    polyline_->setPoint( pidx, polyline_->getPoint(0) );

commit 6df88d9727c3fcd322011e9cedc36c9bc8b0a1d2

od-bug fixing markerset not displayed caused by revision D9444(committed two days ago).
Summary:
The bug is found by using polygon selection tool selecting fault and fault stick, the selection is not displayed.
The reason is that I added one calling in revision D9444: markerset_->turnMarkerOn( index, draw ); in addPos(0,setPos and insertPos() in vismarkerset.
so if the draw argument is false, the marker will be not displayed.

The revision D9444 is good. but I should do corresponded changes in other codes if the argument "draw" is set as false.
the solution fixed "draw" issue by searching all addPos, setPos, and insertPos in od and dgb.

Test Plan: Compiles and Test fine.

Reviewers: kristofer

Reviewed By: kristofer

Differential Revision: https://phabricator.dgbes.com:8443/D9479

include/visBase/vismarkerset.h
@@ -112,6 +112,7 @@ public:
     			//!<Removes all markers
     void		turnMarkerOn(unsigned int idx,bool);
     bool		markerOn(unsigned int);
+    void		turnAllMarkersOn(bool);
 
     int			findClosestMarker(const Coord3&, bool scenespace=false);
     int			findMarker(const Coord3&, const Coord3& eps,

src/visBase/vismarkerset.cc
@@ -362,6 +362,12 @@ bool MarkerSet::markerOn( unsigned int idx )
 }
 
 
+void MarkerSet::turnAllMarkersOn( bool yn )
+{
+    markerset_->turnAllMarkersOn( yn );
+}
+
+
 int MarkerSet::findClosestMarker( const Coord3& tofindpos, 
 				 const bool scenespace )
 {

src/visSurvey/visemobjdisplay.cc
@@ -709,6 +709,7 @@ void EMObjectDisplay::updatePosAttrib( int attrib )
 	markerset->addPos( pos, false );
     }
 
+    markerset->turnAllMarkersOn( true );
     markerset->forceRedraw( true );
     markerset->getCoordinates()->removeAfter( pids->size()-1 );
 }

src/visSurvey/visemsticksetdisplay.cc
@@ -156,7 +156,11 @@ void StickSetDisplay::polygonSelectionCB()
 
 #define mForceDrawMarkerSet()\
 for ( int idx = 0; idx<knotmarkersets_.size(); idx++ )\
+for ( int idx = 0; idx<knotmarkersets_.size(); idx++ )\
+{\
+    knotmarkersets_[idx]->turnAllMarkersOn( true );\
     knotmarkersets_[idx]->forceRedraw( true );\
+}\
 
 
 void StickSetDisplay::updateStickMarkerSet()

src/visSurvey/vispseventdisplay.cc
@@ -420,6 +420,7 @@ void PSEventDisplay::updateDisplay( ParentAttachedObject* pao )
 		vals += (markercolor_==Quality ? psevent->quality_
 					       : getMoveoutComp(offsets,picks));
 	    }
+    	    eventmarkerset_->turnAllMarkersOn( true );
 	    eventmarkerset_->forceRedraw( true );
 	}
 
@@ -591,6 +592,7 @@ void PSEventDisplay::updateDisplay( ParentAttachedObject* pao )
 		    pao->lines_->getCoordinates()->addPos( pos );
 	    }
 
+    	    pao->markerset_->turnAllMarkersOn( true );
 	    pao->markerset_->forceRedraw( true );
 
 	    const int size = pao->lines_ ? pao->lines_->getCoordinates()->size()