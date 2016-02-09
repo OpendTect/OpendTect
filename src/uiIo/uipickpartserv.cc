002492140b1848ebfa889005004de7e329b149a4
arnauddgbes <arnaud.huck@dgbes.com>
2016-02-09 09:33:55 +0100

    fix for old picksets (without type) not being listed as picksets

--- src/uiIo/uipickpartserv.cc
+++ src/uiIo/uipickpartserv.cc
@@ -165,8 +165,10 @@ bool uiPickPartServer::loadSets( TypeSet<MultiID>& psids, bool poly )
 {
     PtrMan<CtxtIOObj> ctio = mMkCtxtIOObj(PickSet);
     ctio->ctxt_.forread_ = true;
-    ctio->ctxt_.toselect_.require_.set( sKey::Type(),
-			poly ? sKey::Polygon() : sKey::PickSet() );
+    if ( poly )
+	ctio->ctxt_.toselect_.require_.set( sKey::Type(), sKey::Polygon() );
+    else
+	ctio->ctxt_.toselect_.dontallow_.set( sKey::Type(), sKey::Polygon() );
 
     uiIOObjSelDlg::Setup sdsu; sdsu.multisel( true );
     uiIOObjSelDlg dlg( parent(), sdsu, *ctio );
