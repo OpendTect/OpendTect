95749d4ad60a90f4eb1e72b47ab6af613fa6b87f
Bert Bril <ahbril@users.noreply.github.com>
2016-02-16 14:28:43 +0100

    EM::StoredObjAccess: made test program run on default test survey

--- src/EarthModel/tests/emstoredobjaccess.cc
+++ src/EarthModel/tests/emstoredobjaccess.cc
@@ -10,11 +10,10 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "od_ostream.h"
 #include "multiid.h"
 
-#define mErrRet(s) { od_cout() << s << od_endl; return false; }
+#define mErrRet(s) { od_ostream::logStream() << s << od_endl; return false; }
 
 static bool initLoader( EM::StoredObjAccess& soa )
 {
-/*
     if ( !soa.add( MultiID("100020.3") ) ) // Hor3D: Demo 6 --> FS8
 	mErrRet( soa.getError(0) );
     if ( !soa.add( MultiID("100020.53") ) ) // Body: Slumps-2b
@@ -26,13 +25,9 @@ static bool initLoader( EM::StoredObjAccess& soa )
 	mErrRet( "ID 100020.99795 should give error" );
     soa.dismiss( "100020.99795" );
 
-*/
     return true;
 }
 
-
-#undef mErrRet
-#define mErrRet(s) { od_cout() << s << od_endl; ExitProgram(1); }
 
 #include "testprog.h"
 #include "emhorizon3d.h"
@@ -52,7 +47,6 @@ int main( int argc, char** argv )
     if ( !initLoader(soa) )
 	ExitProgram(1);
 
-/*
     Executor* exec = soa.reader();
     TextTaskRunner ttr( od_cout() );
     if ( !ttr.execute(*exec) )
@@ -61,51 +55,39 @@ int main( int argc, char** argv )
     delete exec;
 
     mDynamicCastGet(EM::Horizon3D*,hor3d,soa.object(0))
-    if ( !hor3d )
-	mErrRet("object(0) is null or not Hor3D")
+    mRunStandardTest(hor3d,"object(0) must be Hor3D")
     mDynamicCastGet(EM::Body*,body,soa.object(1))
-    if ( !body )
-	mErrRet("object(1) is null or not Body")
+    mRunStandardTest(body,"object(1) must be Body")
     mDynamicCastGet(EM::FaultStickSet*,fss,soa.object(2))
-    if ( !fss )
-	mErrRet("object(2) is null or not FaultStickSet")
+    mRunStandardTest(fss,"object(2) must be FaultStickSet")
 
-    const Coord crd_ix_400_600( 613123, 6081263 );
-    const float zval = hor3d->getZValue( crd_ix_400_600 );
-*/
-    /*
-       od_cout() << "Z Val at 400/600: " << zval << od_endl;
-       -> Z Val at 400/600: 0.65230799
-    */
-/*
-    if ( zval < 0.65 || zval > 0.66 )
-	mErrRet("Hor Z Value not as expected")
+    const Coord crd_ix_400_900( 620620, 6081472 );
+    const float zval = hor3d->getZValue( crd_ix_400_900 );
+    mRunStandardTest(zval>0.549 && zval<0.6,"Horizon Z Value check")
+       /*
+       od_cout() << "Z Val at 400/900: " << zval << od_endl;
+       -> Z Val at 400/900: 0.54925388
+       */
 
     TrcKeyZSampling cs;
-    if ( !body->getBodyRange(cs) )
-	mErrRet("Cannot get Body ranges")
-    if ( cs.hsamp_.start_.inl() < 390 || cs.hsamp_.stop_.crl() > 870 )
-	mErrRet("Body range not as expected")
-
-*/
-
-    /*
+    mRunStandardTest( body->getBodyRange(cs),"Get body ranges")
+    mRunStandardTest( cs.hsamp_.start_.inl()>390 && cs.hsamp_.stop_.crl()<870,
+	    		"Check on body ranges" )
+       /*
        od_cout() << "Inl/Crl rg: "
 	<< cs.hsamp_.start_.inl() << '-' << cs.hsamp_.stop_.inl() << ", "
 	<< cs.hsamp_.start_.crl() << '-' << cs.hsamp_.stop_.crl() << od_endl;
 	-> Inl/Crl rg: 392-474, 810-864
-    */
+       */
 
-/*
     const EM::FaultStickSetGeometry& fssgeom = fss->geometry();
-    if ( fssgeom.nrSticks(0) != 4 || fssgeom.nrKnots(0,0) != 7 )
-	mErrRet("Nr Sticks or knots not as expected")
-*/
-    /*
+    mRunStandardTest( fssgeom.nrSticks(0) == 4 && fssgeom.nrKnots(0,0) == 7,
+	    		"Check nr Sticks and knots of Stick in FaultStickSet" );
+       /*
 	od_cout() << fssgeom.nrSticks(0) << " sticks, "
 		<< fssgeom.nrKnots(0,0) << " knots." << od_endl;
        -> 4 sticks, 7 knots.
-    */
+       */
 
     return ExitProgram( 0 );
 }
