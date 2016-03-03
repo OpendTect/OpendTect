OpendTect/OpendTect

commit 1223a07dead7e754dea5d0c71d74dac6d65c9c86

od6.0: Reimplemented pointInTriangle3D
Summary: New method should be more stable

Test Plan: Works well on new fault painter algo

Reviewers: nanne

Differential Revision: https://phabricator.dgbes.com:8443/D9354

include/Algo/trigonometry.h
@@ -171,8 +171,21 @@ inline bool pointInTriangle2D( const Coord& p, const Coord& a, const Coord& b,
 inline bool pointInTriangle3D( const Coord3& p, const Coord3& a,
 			const Coord3& b, const Coord3& c, double epsilon )
 {
+    Coord3 ap = a - p;
+    ap = ap.normalize();
+    Coord3 bp = b - p;
+    bp = bp.normalize();
+    Coord3 cp = c - p;
+    cp = cp.normalize();
+    const double d1 = ap.dot( bp );
+    const double d2 = bp.dot( cp );
+    const double d3 = cp.dot( ap );
+    const double angle = Math::ACos(d1) + Math::ACos(d2) + Math::ACos(d3);
+    return mIsEqual(angle,M_2PI,epsilon);
+
+    /*Method 2
     return sameSide3D(p,a,b,c,epsilon) && sameSide3D(p,b,a,c,epsilon) &&
-	   sameSide3D(p,c,a,b,epsilon);
+	   sameSide3D(p,c,a,b,epsilon);*/
 }
 
 

commit a5f67faf680094e6035f21d12b6e90812c070197

[od] Bugfix: Polygon body volume was estimated as 0m3 (OD-140)
Summary:
Bugfix: Polygon body volume was estimated as 0m3 (OD-140). Also its displayed
triangles where not totally enclosing the body in that case. Bug introduced
by revision D9354 from Yuancheng, who has reimplemented pointInTriangle3D(.)
in trigonometry.cc using an angular method. It was said to work well for a new
fault painter algorithm, but existing code also using this function was not
tested. Such a fundamental change in a low-level algorithm should not have
been accepted in a release version without proper testing! Deciding which
implementation works best for which application (and why) will take too much
time now. Current solution is to use the old implementation with old code
and the new implementation for the fault painter.

Test Plan:
Polygon body volume is returning the same non-zero value as in od5.0, and
the displayed triangles are nicely enclosing the body again. I must admit
I did not test the new fault painter.

Reviewers: yuancheng

Reviewed By: yuancheng

Differential Revision: https://phabricator.dgbes.com:8443/D9813

include/Algo/trigonometry.h
@@ -169,23 +169,28 @@ inline bool pointInTriangle2D( const Coord& p, const Coord& a, const Coord& b,
 
 /*!<Only when four points are coplanar. */
 inline bool pointInTriangle3D( const Coord3& p, const Coord3& a,
-			const Coord3& b, const Coord3& c, double epsilon )
+			const Coord3& b, const Coord3& c, double epsilon,
+			bool useangularmethod=false )
 {
+    if ( !useangularmethod )
+    {
+	return sameSide3D(p,a,b,c,epsilon) && sameSide3D(p,b,a,c,epsilon) &&
+	       sameSide3D(p,c,a,b,epsilon);
+    }
+
     Coord3 ap = a - p;
     ap = ap.normalize();
     Coord3 bp = b - p;
     bp = bp.normalize();
     Coord3 cp = c - p;
     cp = cp.normalize();
+
     const double d1 = ap.dot( bp );
     const double d2 = bp.dot( cp );
     const double d3 = cp.dot( ap );
     const double angle = Math::ACos(d1) + Math::ACos(d2) + Math::ACos(d3);
-    return mIsEqual(angle,M_2PI,epsilon);
 
-    /*Method 2
-    return sameSide3D(p,a,b,c,epsilon) && sameSide3D(p,b,a,c,epsilon) &&
-	   sameSide3D(p,c,a,b,epsilon); */
+    return mIsEqual(angle,M_2PI,epsilon);
 }
 
 