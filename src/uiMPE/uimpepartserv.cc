4d6b127ae4ac5de6cdd77be7018315ec73c7126f
Jaap Glas <jaap.glas@dgbes.com>
2016-03-14 15:20:56 +0100

    [od] Solved (PE) messages on stuff not removed by tracking setup on exit.
    Summary:
    Solved (PE) messages on queues, datapacks and hidden parameter not removed
    by tracking setup on exit. This was caused by bluntly setting setupgrp_=0
    before the destructor of uiMPEPartServer was called. Looks like there is no
    reason (anymore) to do this. The one in the mCloseReturn macro was added in
    revision r29123/D1611 by Mahant. The crash he solved does no longer appear
    either with or without. The other two were inserted 7 years ago by Umesh
    without any proper explanation in the log message. For safety, let's only
    change this in [od] until it turns out really necessary in [od6.0].
    
    Test Plan:
    All (PE) messages gone after this fix. Pick new horizon on inline and exit
    or switch survey immediately. New horizon created in either 3D scene or 2D
    viewer. The latter covers Mahant's revision r29123/D1611. No crashes.
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D9881

--- src/uiMPE/uimpepartserv.cc
+++ src/uiMPE/uimpepartserv.cc
@@ -334,14 +334,12 @@ void uiMPEPartServer::nrHorChangeCB( CallBacker* )
 }
 
 
-#define mCloseReturn { setupgrp_ = 0; return; }
-
 void uiMPEPartServer::trackerWinClosedCB( CallBacker* cb )
 {
     cleanSetupDependents();
     seedswithoutattribsel_ = false;
 
-    if ( trackercurrentobject_ == -1 ) mCloseReturn;
+    if ( trackercurrentobject_ == -1 ) return;
 
     const int trackerid = getTrackerID( trackercurrentobject_ );
     if ( trackerid == -1 )
@@ -350,14 +348,14 @@ void uiMPEPartServer::trackerWinClosedCB( CallBacker* cb )
 	initialundoid_ = mUdf(int);
 	seedhasbeenpicked_ = false;
 	setupbeingupdated_ = false;
-	mCloseReturn;
+	return;
     }
 
     MPE::EMTracker* tracker = MPE::engine().getTracker( trackerid );
-    if ( !tracker ) mCloseReturn;
+    if ( !tracker ) return;
 
     MPE::EMSeedPicker* seedpicker = tracker->getSeedPicker( true );
-    if ( !seedpicker ) mCloseReturn;
+    if ( !seedpicker ) return;
 
     if ( setupbeingupdated_ )
     {
@@ -366,7 +364,7 @@ void uiMPEPartServer::trackerWinClosedCB( CallBacker* cb )
 	trackercurrentobject_ = -1;
 	setupbeingupdated_ = false;
 	sendEvent( ::uiMPEPartServer::evSetupClosed() );
-	mCloseReturn;
+	return;
     }
 
     if ( !seedhasbeenpicked_ )
@@ -377,7 +375,7 @@ void uiMPEPartServer::trackerWinClosedCB( CallBacker* cb )
 	if ( !res )
 	{
 	    noTrackingRemoval();
-	    mCloseReturn;
+	    return;
 	}
     }
 
@@ -385,14 +383,12 @@ void uiMPEPartServer::trackerWinClosedCB( CallBacker* cb )
 	if ( !seedhasbeenpicked_ )
 	{
 	    seedswithoutattribsel_ = true;
-	    mCloseReturn;
+	    return;
 	}
 
     seedpicker->seedToBeAddedRemoved.remove(
 			   mCB(this,uiMPEPartServer,aboutToAddRemoveSeed) );
     seedpicker->seedAdded.remove( mCB(this,uiMPEPartServer,seedAddedCB) );
-
-    setupgrp_ = 0;
 
     if ( !seedhasbeenpicked_ || !seedpicker->doesModeUseVolume() )
 	sendEvent( uiMPEPartServer::evStartSeedPick() );
@@ -445,10 +441,7 @@ void uiMPEPartServer::noTrackingRemoval()
     seedhasbeenpicked_ = false;
 
     if ( cursceneid_ == -1 )
-    {
 	setupbeingupdated_ = false;
-	setupgrp_ = 0;
-    }
 
     MPE::engine().trackeraddremove.enable();
     MPE::engine().trackeraddremove.trigger();
