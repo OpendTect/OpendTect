f644444f5f22557606e0f90547ce7dbb064ecbca
Satyaki <satyaki.maitra@dgbes.com>
2016-03-22 15:05:50 +0530

    od : Fix for broken synthetics after fluid replacement D9929

--- src/WellAttrib/stratsynth.cc
+++ src/WellAttrib/stratsynth.cc
@@ -794,7 +794,7 @@ bool StratSynth::createElasticModels()
     if ( !modelsvalid )
 	return false;
 
-    return adjustElasticModel( layMod(), aimodels_, false );
+    return adjustElasticModel( layMod(), aimodels_, useed_ );
 }
 
 
