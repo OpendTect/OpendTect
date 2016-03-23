c394fc7ce594f3825875af5449bc7907dc2a1cdd
Bert Bril <ahbril@users.noreply.github.com>
2016-03-24 00:47:06 +0100

    CDash silencing: will this help?

--- src/General/pickset.cc
+++ src/General/pickset.cc
@@ -701,8 +701,8 @@ ObjectSet<Pick::Location>& Pick::List::locations()
 
 ObjectSet<const Pick::Location>& Pick::List::locations() const
 {
-    return const_cast<ObjectSet<const Pick::Location>&>(
-	    reinterpret_cast<const ObjectSet<const Pick::Location>&>( locs_ ) );
+    void* ptr = const_cast<ObjectSet<Pick::Location>*>( &locs_ );
+    return *((ObjectSet<const Pick::Location>*)ptr);
 }
 
 
