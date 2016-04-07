d31995548696fec3f5303782fc48e36508e24792
dingzheng-dgb <dingzheng-dgb@users.noreply.github.com>
2016-04-07 08:33:08 +0200

    fix windows compile warning

--- src/EarthModel/emhor2dto3d.cc
+++ src/EarthModel/emhor2dto3d.cc
@@ -175,7 +175,7 @@ void Hor2DTo3D::addSections( const TrcKeySampling& hs )
 
 void Hor2DTo3D::fillSections()
 {
-    const float mindist = SI().xyInFeet() ? 400. : 100.;
+    const float mindist = SI().xyInFeet() ? 400.0f : 100.0f;
     for ( int isd=0; isd<sd_.size(); isd++ )
     {
 	Coord lastpos = Coord::udf();
