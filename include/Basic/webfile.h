300369b7b9694eac5207c6d638571540aba5733b
Bert Bril <ahbril@users.noreply.github.com>
2016-04-13 18:17:13 +0200

    aCDash accused me of not checking in the this file. Well, here it is!

---
+++ include/Basic/webfile.h
@@ -0,0 +1,36 @@
+#ifndef webfile_h
+#define webfile_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Bert
+ Date:		April 2016
+________________________________________________________________________
+
+-*/
+
+#include "basicmod.h"
+#include "gendefs.h"
+
+class BufferString;
+
+
+namespace File
+{
+
+			// These functions want reasonably common URI's,
+			// no nulls, empties or crap stuff
+
+typedef bool		(*ExistsFn)(const char*);
+typedef od_int64	(*GetSizeFn)(const char*);
+typedef bool		(*GetContentFn)(const char*,BufferString&);
+
+mGlobal(Basic) void	setWebHandlers(ExistsFn,GetSizeFn,GetContentFn);
+
+
+} // namespace File
+
+
+#endif
