7349b9f1253b23242282713ecc7cfcd1de99f42d
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2016-04-15 17:05:14 +0200

    Added source codes of backoffice server.

---
+++ src/Network/backoffice-server/googleapp/dlsites.php
@@ -0,0 +1,42 @@
+<?php
+
+header('Content-Type: text/plain');
+use google\appengine\api\modules\ModulesService;
+
+
+$platform = "";
+$id = "";
+$address = "";
+
+if ( array_key_exists( 'REMOTE_ADDR', $_SERVER ) )
+    $address = $_SERVER['REMOTE_ADDR'];
+
+if ( array_key_exists( 'p', $_GET ) )
+    $platform = $_GET['p'];
+
+if ( array_key_exists( 'i', $_GET ) )
+    $id = $_GET['i'];
+
+$filearray = $_GET;
+
+$datestring = date("Y-m-d\TH:i:s\Z");
+
+$filearray['date'] = $datestring;
+$filearray['address'] = $address;
+
+$memcache = new Memcached;
+$uniquenr = $memcache->increment('uniquenr');
+
+$outputdir = "gs://opendtect-backend/dlsites/tmp/";
+$filenamebase = "dlstats-".$datestring."-".$uniquenr;
+$filename = $filenamebase.".txt";
+
+$outputfile = $outputdir.$filename;
+
+file_put_contents( $outputfile, json_encode( $filearray ) );
+
+echo "cloud.opendtect.org"."\n";
+echo "download.opendtect.org"."\n";
+
+?>
+
