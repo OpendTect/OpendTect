5ffa899a4f8aa4717bd9c847ba686a7401ae34ce
dingzheng-dgb <dingzheng-dgb@users.noreply.github.com>
2016-04-20 14:04:39 +0200

    [OD]OD-209 [OD]OD-209 CTRL + S in a 2D Viewer does not save the horizon, Part3
    Summary: Added a new class: uiODvw2demtreeitem which will be the base classes of some emobject tree item.
    
    Test Plan: Tests with other [OD]OD-209 revision working fine
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D10112

---
+++ include/uiODMain/uiodvw2demtreeitem.h
@@ -0,0 +1,41 @@
+#ifndef uiodvw2demtreeitem_h
+#define uiodvw2demtreeitem_h
+
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	DZH
+ Date:		Apr 2016
+________________________________________________________________________
+
+-*/
+
+
+
+#include "uiodmainmod.h"
+#include "uiodvw2dtreeitem.h"
+#include "emposid.h"
+
+mExpClass(uiODMain) uiODVw2DEMTreeItem : public uiODVw2DTreeItem
+{mODTextTranslationClass(uiODVw2DEMTreeItem);
+public:
+			uiODVw2DEMTreeItem(const EM::ObjectID&);
+			~uiODVw2DEMTreeItem();
+    
+    EM::ObjectID	emObjectID() const	{ return emid_; }
+
+protected:
+
+    EM::ObjectID        emid_;
+    void		doSave();
+    void		doSaveAs();
+    void		renameVisObj();
+
+private:
+    void		doStoreObject(bool);
+
+};
+
+
+#endif
---
+++ src/uiODMain/uiodvw2demtreeitem.cc
@@ -0,0 +1,93 @@
+/*+
+________________________________________________________________________
+
+ CopyRight:	(C) dGB Beheer B.V.
+ Author:	DZH
+ Date:		Apr 2016
+________________________________________________________________________
+
+-*/
+
+#include "uiodvw2demtreeitem.h"
+
+#include "uitreeitemmanager.h"
+#include "uiodapplmgr.h"
+#include "uiodviewer2dmgr.h"
+#include "uiempartserv.h"
+#include "uimpepartserv.h"
+#include "uivispartserv.h"
+
+#include "uiodvw2dhor2dtreeitem.h"
+#include "uiodvw2dhor3dtreeitem.h"
+
+#include "mpeengine.h"
+#include "commondefs.h"
+#include "emmanager.h"
+#include "emobject.h"
+#include "ptrman.h"
+
+
+uiODVw2DEMTreeItem::uiODVw2DEMTreeItem( const EM::ObjectID& emid )
+    : uiODVw2DTreeItem(uiString::emptyString())
+    , emid_( emid )
+{
+}
+
+
+uiODVw2DEMTreeItem::~uiODVw2DEMTreeItem()
+{}
+
+
+void uiODVw2DEMTreeItem::doSave()
+{
+    bool savewithname = false;
+    if ( !EM::EMM().getMultiID( emid_ ).isEmpty() )
+	savewithname = !IOM().get( EM::EMM().getMultiID(emid_) );
+    doStoreObject( savewithname );
+
+    if ( MPE::engine().hasTracker(emid_) )
+    {
+	uiMPEPartServer* mps = applMgr()->mpeServer();
+	if ( mps ) 
+	    mps->saveSetup( applMgr()->EMServer()->getStorageID(emid_) );
+    }
+
+}
+
+
+void uiODVw2DEMTreeItem::doSaveAs()
+{
+    doStoreObject( true );
+
+    if ( MPE::engine().hasTracker(emid_) )
+    {
+	uiMPEPartServer* mps = applMgr()->mpeServer();
+	if ( mps ) 
+	{
+	   const MultiID oldmid = applMgr()->EMServer()->getStorageID( emid_ );
+	   mps->prepareSaveSetupAs( oldmid );
+	   mps->saveSetupAs( EM::EMM().getObject(emid_)->multiID() );
+	}
+    }
+}
+
+
+void uiODVw2DEMTreeItem::doStoreObject( bool saveas )
+{
+    applMgr()->EMServer()->storeObject( emid_, saveas );
+    renameVisObj();
+}
+
+
+void uiODVw2DEMTreeItem::renameVisObj()
+{
+    const MultiID midintree = applMgr()->EMServer()->getStorageID( emid_ );
+    TypeSet<int> visobjids;
+    applMgr()->visServer()->findObject( midintree, visobjids );
+    name_ = ::toUiString( applMgr()->EMServer()->getName( emid_ ) );
+    for ( int idx = 0; idx<visobjids.size(); idx++ )
+	applMgr()->visServer()->setObjectName( visobjids[idx], name_ );
+    uiTreeItem::updateColumnText(uiODViewer2DMgr::cNameColumn());
+    applMgr()->visServer()->triggerTreeUpdate();
+}
+
