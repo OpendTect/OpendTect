a035473b60c475226c1dd26bf8a248662f3f19dd
Bert Bril <ahbril@users.noreply.github.com>
2016-04-28 12:44:08 +0200

    AutoSaver: progress, noting tested yet.

--- include/General/autosaver.h
+++ include/General/autosaver.h
@@ -40,76 +40,130 @@ namespace OD
 class AutoSaveMgr;
 
 
-mExpClass(General) AutoSaver : public Monitorable
+/*!\brief Object that can be saved at any time. */
+
+mExpClass(General) Saveable : public Monitorable
 {
 public:
 
-			AutoSaver(const Monitorable&);
-			~AutoSaver();
+			Saveable(const Monitorable&);
+			~Saveable();
     const Monitorable&	monitored() const		{ return obj_; }
 
     mImplSimpleMonitoredGetSet(inline,key,setKey,MultiID,key_)
-    mImplSimpleMonitoredGetSet(inline,nrSeconds,setNrSeconds,int,nrseconds_)
     inline		mImplSimpleMonitoredGet(isFinished,bool,objdeleted_)
 
-			// These functions can be called from any thread
-    virtual BufferString getFingerPrint() const		= 0;
-    virtual bool	store(const IOObj&) const	= 0;
-    virtual void	remove(const IOObj&) const;
-
-    void		userSaveOccurred();
-    bool		saveNow()		{ return doWork(true); }
+    virtual bool	store(const IOObj&) const;
     uiString		errMsg() const		{ return errmsg_; }
 
 protected:
 
     const Monitorable&	obj_;
     MultiID		key_;
-    int			nrseconds_;
     bool		objdeleted_;
-
-    BufferString	prevfingerprint_;
-    IOStream*		prevstoreioobj_;
-    int			savenr_;
-    int			lastsaveclockseconds_;
-    mutable int		curclockseconds_;
     mutable uiString	errmsg_;
 
-    bool		doWork(bool forcesave);
+			// This function can be called from any thread
+    virtual bool	doStore(const IOObj&) const	= 0;
 
 private:
 
     void		objDel(CallBacker*);
-    void		removePrevStored();
 
+};
+
+
+/*!\brief Object that can be added to OD::AUTOSAVE(). */
+
+mExpClass(General) AutoSaveable : public Saveable
+{
+public:
+
+			AutoSaveable(const Monitorable&);
+			~AutoSaveable();
+
+    mImplSimpleMonitoredGetSet(inline,nrSecondsBetweenSaves,
+				      setNrSecondsBetweenSaves,
+				      int,nrclocksecondsbetweensaves_)
+
+			// These functions can be called from any thread
+    virtual BufferString getFingerPrint() const		= 0;
+
+    void		userSaveOccurred() const;
+    bool		saveNow() const			{ return doWork(true); }
+
+    virtual bool	store(const IOObj&) const;
+    virtual void	remove(const IOObj&) const;
+
+protected:
+
+    mutable BufferString prevfingerprint_;
+    mutable IOStream*	prevstoreioobj_;
+    mutable int		savenr_;
+    mutable int		nrclocksecondsbetweensaves_;
+    mutable int		lastsaveclockseconds_;
+    mutable int		curclockseconds_;
+
+    bool		doWork(bool forcesave) const;
+
+private:
+
+    void		removePrevStored() const;
     bool		needsAct(int) const;
-    bool		act();
+    bool		act() const;
     friend class	AutoSaveMgr;
 
 };
 
 
-/*!\brief Auto-save manager. Singleton class. */
+/*!\brief Auto-save manager. Singleton class.
+
+  Work is done in its own thread.
+  Saveable will automatically be removed when they are deleted.
+
+  */
 
 mExpClass(General) AutoSaveMgr : public CallBacker
 { mODTextTranslationClass(AutoSaveMgr)
 public:
 
-    void		add(AutoSaver*);
+    bool		isActive(bool bydefault=false) const;
+    void		setActive(bool,bool makedefault=true);
+    int			nrSecondsBetweenSaves() const;
+    void		setNrSecondsBetweenSaves(int);
 
-    static AutoSaveMgr&	getInst();
+    void		add(AutoSaveable*);
+
+			// triggered from mgr's thread. CB obj is the saver.
+    Notifier<AutoSaveMgr> saveDone;
+    Notifier<AutoSaveMgr> saveFailed;
 
 private:
 
 			AutoSaveMgr();
+			~AutoSaveMgr();
 
-    ObjectSet<AutoSaver> savers_;
-    int			nrcycles_; // one per second
-    Threads::Lock	lock_;
-    Threads::Thread	thread_;
+    ObjectSet<AutoSaveable> savers_;
+    mutable Threads::Lock lock_;
+    Threads::Thread*	thread_;
+    bool		appexits_;
+    bool		active_;
 
-    void		go(CallBacker*);
     void		appExits(CallBacker*);
+    void		saverDel(CallBacker*);
+    void		survChg( CallBacker* )	    { setEmpty(); }
+
+    void		go();
+    static inline void	goCB(CallBacker*);
+
+public:
+
+    // Probably not useful 4 u:
+
+    static AutoSaveMgr&	getInst();		    // use OD::AUTOSAVE()
+    void		remove(AutoSaveable*);	    // done 4 u on destruction
+    void		setEmpty();		    // why would u?
+    Threads::Thread&	thread()		    { return *thread_; }
 
 };
 
--- src/General/autosaver.cc
+++ src/General/autosaver.cc
@@ -9,26 +9,81 @@
 #include "iodir.h"
 #include "iostrm.h"
 #include "ioman.h"
-#include "iopar.h"
+#include "settings.h"
 #include "separstr.h"
-#include "timer.h"
+#include "timefun.h"
 #include "msgh.h"
 
+static const int defnrsecondsbetweensaves_ = 300;
+static const char* sKeyIsActiveByDefault = "AutoSave.Active";
+static const char* sKeyNrSecondsBetweenSaves = "AutoSave.Cycle Time";
 
-OD::AutoSaver::AutoSaver( const Monitorable& obj )
+static void setDefaultNrSecondsBetweenSaves( int nrsecs )
+{
+    Settings::common().set( sKeyNrSecondsBetweenSaves, nrsecs );
+}
+
+static int defaultNrSecondsBetweenSaves()
+{
+    int nrsecs = defnrsecondsbetweensaves_;
+    Settings::common().get( sKeyNrSecondsBetweenSaves, nrsecs );
+    return nrsecs;
+}
+
+static void setIsActiveByDefault( bool yn )
+{
+    Settings::common().setYN( sKeyIsActiveByDefault, yn );
+}
+
+static bool isActiveByDefault()
+{
+    bool yn = true;
+    Settings::common().getYN( sKeyNrSecondsBetweenSaves, yn );
+    return yn;
+}
+
+
+OD::Saveable::Saveable( const Monitorable& obj )
     : obj_(obj)
-    , nrseconds_(300)
     , objdeleted_(false)
-    , savenr_(1)
-    , prevstoreioobj_(0)
 {
     mAttachCB( const_cast<Monitorable&>(obj).objectToBeDeleted(),
-	       AutoSaver::objDel );
+	       Saveable::objDel );
     mTriggerInstanceCreatedNotifier();
 }
 
 
-OD::AutoSaver::~AutoSaver()
+OD::Saveable::~Saveable()
+{
+    detachAllNotifiers();
+    sendDelNotif();
+}
+
+
+void OD::Saveable::objDel( CallBacker* )
+{
+    mLock4Write();
+    objdeleted_ = true;
+}
+
+
+bool OD::Saveable::store( const IOObj& ioobj ) const
+{
+    return doStore( ioobj );
+}
+
+
+OD::AutoSaveable::AutoSaveable( const Monitorable& obj )
+    : Saveable(obj)
+    , nrclocksecondsbetweensaves_(defaultNrSecondsBetweenSaves())
+    , savenr_(1)
+    , prevstoreioobj_(0)
+{
+    mTriggerInstanceCreatedNotifier();
+}
+
+
+OD::AutoSaveable::~AutoSaveable()
 {
     detachAllNotifiers();
     sendDelNotif();
@@ -36,20 +91,23 @@ OD::AutoSaver::~AutoSaver()
 }
 
 
-void OD::AutoSaver::objDel( CallBacker* )
+bool OD::AutoSaveable::store( const IOObj& ioobj ) const
 {
-    mLock4Write();
-    objdeleted_ = true;
+    if ( !doStore(ioobj) )
+	return false;
+
+    userSaveOccurred();
+    return true;
 }
 
 
-void OD::AutoSaver::remove( const IOObj& ioobj ) const
+void OD::AutoSaveable::remove( const IOObj& ioobj ) const
 {
     ioobj.implRemove();
 }
 
 
-void OD::AutoSaver::removePrevStored()
+void OD::AutoSaveable::removePrevStored() const
 {
     if ( !prevstoreioobj_ )
 	return;
@@ -61,32 +119,33 @@ void OD::AutoSaver::removePrevStored()
 }
 
 
-bool OD::AutoSaver::needsAct( int clockseconds ) const
+bool OD::AutoSaveable::needsAct( int clockseconds ) const
 {
     mLock4Read();
     if ( objdeleted_ || (!mLock2Write() && objdeleted_) )
 	return false;
 
     curclockseconds_ = clockseconds;
-    return curclockseconds_ - lastsaveclockseconds_ >= nrseconds_;
+    return curclockseconds_ - lastsaveclockseconds_
+	>= nrclocksecondsbetweensaves_;
 }
 
 
-bool OD::AutoSaver::act()
+bool OD::AutoSaveable::act() const
 {
     return isFinished() || doWork(false);
 }
 
 
 
-void OD::AutoSaver::userSaveOccurred()
+void OD::AutoSaveable::userSaveOccurred() const
 {
     removePrevStored();
     lastsaveclockseconds_ = curclockseconds_ + 1;
 }
 
 
-bool OD::AutoSaver::doWork( bool forcesave )
+bool OD::AutoSaveable::doWork( bool forcesave ) const
 {
     AccessLockHandler lockhandler( obj_ );	// locks the object
     mLock4Read();				// locks me
@@ -105,7 +164,7 @@ bool OD::AutoSaver::doWork( bool forcesave )
     newstoreioobj->pars().update( sKey::CrFrom(), key_ );
     newstoreioobj->pars().update( sKey::CrInfo(), "Auto-saved" );
     newstoreioobj->updateCreationPars();
-    if ( !store(*newstoreioobj) )
+    if ( !doStore(*newstoreioobj) )
     {
 	delete newstoreioobj; newstoreioobj = 0;
 	prevfingerprint_.setEmpty();
@@ -134,48 +193,162 @@ OD::AutoSaveMgr& OD::AutoSaveMgr::getInst()
 
 
 OD::AutoSaveMgr::AutoSaveMgr()
-    : nrcycles_(0)
-    , thread_(mCB(this,AutoSaveMgr,go),"AutoSave Manager")
+    : appexits_(false)
+    , active_(isActiveByDefault())
+    , saveDone(this)
+    , saveFailed(this)
 {
+    mAttachCB( IOM().surveyToBeChanged, AutoSaveMgr::setEmpty );
     mAttachCB( IOM().applicationClosing, AutoSaveMgr::appExits );
+    thread_ = new Threads::Thread( mSCB(AutoSaveMgr::goCB), "AutoSave Manager");
 }
 
 
-void OD::AutoSaveMgr::add( AutoSaver* saver )
+OD::AutoSaveMgr::~AutoSaveMgr()
 {
+    thread_->waitForFinish();
+    delete thread_;
+}
+
+
+void OD::AutoSaveMgr::add( AutoSaveable* saver )
+{
+    if ( !saver )
+	return;
+
+    mAttachCB( saver->objectToBeDeleted(), AutoSaveMgr::saverDel );
     Threads::Locker locker( lock_ );
     savers_ += saver;
 }
 
 
-void OD::AutoSaveMgr::appExits( CallBacker* )
+void OD::AutoSaveMgr::setEmpty()
 {
-    detachAllNotifiers();
-    thread_.waitForFinish();
+    Threads::Locker locker( lock_ );
+    deepErase( savers_ );
 }
 
 
-void OD::AutoSaveMgr::go( CallBacker* )
+bool OD::AutoSaveMgr::isActive( bool fordef ) const
+{
+    if ( fordef )
+	return isActiveByDefault();
+
+    Threads::Locker locker( lock_ );
+    return active_;
+}
+
+
+void OD::AutoSaveMgr::setActive( bool yn, bool mkdef )
+{
+    if ( mkdef )
+	setIsActiveByDefault( yn );
+
+    Threads::Locker locker( lock_ );
+    active_ = yn;
+}
+
+
+int OD::AutoSaveMgr::nrSecondsBetweenSaves() const
+{
+    return defaultNrSecondsBetweenSaves();
+}
+
+
+void OD::AutoSaveMgr::setNrSecondsBetweenSaves( int nrsecs )
+{
+    if ( nrsecs < 1 )
+	{ setActive( false ); return; }
+
+    setDefaultNrSecondsBetweenSaves( nrsecs );
+    Threads::Locker locker( lock_ );
+    for ( int isvr=0; isvr<savers_.size(); isvr++ )
+	savers_[isvr]->setNrSecondsBetweenSaves( nrsecs );
+}
+
+
+void OD::AutoSaveMgr::saverDel( CallBacker* cb )
+{
+    mDynamicCastGet(AutoSaveable*,autosaver,cb)
+    if ( !autosaver )
+    {
+	if ( !cb )
+	    { pErrMsg( "Huh" ); return; }
+	pErrMsg( "Add a sendDelNotif() to your destructor" );
+	autosaver = (AutoSaveable*)cb;
+    }
+}
+
+
+void OD::AutoSaveMgr::remove( AutoSaveable* autosaver )
 {
     Threads::Locker locker( lock_ );
+    const int idxof = savers_.indexOf( autosaver );
+    if ( idxof < 0 )
+	return; // no pErrMsg so you can always remove them
 
-    const bool isverbose = DBG::isOn( DBG_IO );
+    savers_.removeSingle( idxof );
+}
+
+
+void OD::AutoSaveMgr::appExits( CallBacker* )
+{
+    setEmpty();
+    Threads::Locker locker( lock_ );
+    detachAllNotifiers();
+    appexits_ = true;
+    locker.unlockNow();
+}
+
+
+void OD::AutoSaveMgr::goCB( CallBacker* )
+{
+    getInst().go();
+}
+
+
+void OD::AutoSaveMgr::go()
+{
+    const int time0ms = Time::getMilliSeconds();
+    int prevnrcycles = -1;
+
     while ( true )
     {
-	nrcycles_++;
-	for ( int idx=0; idx<savers_.size(); idx++ )
+	bool isactive; ObjectSet<AutoSaveable> svrs;
+	Threads::Locker locker( lock_ );
+	    if ( appexits_ )
+		break;
+	    isactive = active_;
+	    svrs = savers_;
+	locker.unlockNow();
+
+	const int mselapsed = Time::getMilliSeconds() - time0ms;
+	const int nrcycles = mselapsed / 1000;
+	if ( nrcycles == prevnrcycles )
+	    { Threads::sleep( 1000 - mselapsed % 1000 ); continue; }
+	prevnrcycles = nrcycles;
+	if ( !isactive )
+	    continue;
+
+	ObjectSet<AutoSaveable> finishedsvrs;
+	for ( int isvr=0; isvr<svrs.size(); isvr++ )
 	{
-	    AutoSaver* saver = savers_[idx];
+	    AutoSaveable* saver = svrs[isvr];
 	    if ( saver->isFinished() )
-		{ delete savers_.removeSingle(idx); idx--; }
-	    else if ( saver->needsAct(nrcycles_) )
+		finishedsvrs += saver;
+	    else if ( saver->needsAct(nrcycles) )
+		(saver->act() ? saveDone : saveFailed).trigger( saver );
+	}
+
+	if ( !finishedsvrs.isEmpty() )
+	{
+	    locker.reLock();
+	    for ( int isvr=0; isvr<finishedsvrs.size(); isvr++ )
 	    {
-		if ( !saver->act() )
-		    ErrMsg( BufferString("Auto-save failed:\n",
-					 saver->errMsg().getFullString()) );
-		else if ( isverbose )
-		    UsrMsg( BufferString("Auto-save succeeded for: ",
-					 saver->key()) );
+		AutoSaveable* saver = finishedsvrs[isvr];
+		const int idxof = savers_.indexOf( saver );
+		if ( idxof >= 0 )
+		    delete savers_.removeSingle( idxof );
 	    }
 	}
     }
