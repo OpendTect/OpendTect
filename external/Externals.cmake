aa9b80a46fc9466a4d25f6569d3e52e9bcbb49a0
Raman Singh <raman.singh@dgbes.com>
2016-04-29 17:19:29 +0530

    Fixed revision number for doc_csh in 6.0.3

--- external/Externals.cmake
+++ external/Externals.cmake
@@ -1,4 +1,4 @@
 include ( "CMakeModules/ODExternals.cmake" )
 
 DEFINE_SVN_EXTERNAL( external/osgGeo https://github.com/opendtect/osgGeo.git/branches/osgGeo-1.2 1445 )
-DEFINE_SVN_EXTERNAL( external/doc_csh https://github.com/opendtect/opendtectdoc.git/branches/od6.0/od_userdoc/Project/Advanced/CSH HEAD )
+DEFINE_SVN_EXTERNAL( external/doc_csh https://github.com/opendtect/opendtectdoc.git/branches/od6.0/od_userdoc/Project/Advanced/CSH 323 )
