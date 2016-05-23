583b02f645fe32789d16618be6eac5fe30dfc913
Nanne Hemstra <hemstra@users.noreply.github.com>
2016-05-22 21:44:37 -0500

    replaced spaces by tabs

--- include/uiBase/uifiledlg.h
+++ include/uiBase/uifiledlg.h
@@ -37,17 +37,17 @@ public:
     };
 
 
-                        uiFileDialog(uiParent*,bool forread,
+			uiFileDialog(uiParent*,bool forread,
 				     const char* fname=0,
 				     const char* filter=0,
 			const uiString& caption=uiString::emptyString());
 
-                        uiFileDialog(uiParent*,Mode mode=AnyFile,
+			uiFileDialog(uiParent*,Mode mode=AnyFile,
 				     const char* fname=0,
 				     const char* filter=0,
 			const uiString& caption=uiString::emptyString());
     enum Type		{ Gen, Img, Txt, Html };
-                        uiFileDialog(uiParent*,Type,
+			uiFileDialog(uiParent*,Type,
 				     const char* fname=0,
 			const uiString& caption=uiString::emptyString());
 						//!< Always AnyFile
@@ -73,7 +73,7 @@ public:
     void		setDefaultExtension(const char*);
     const char*		getDefaultExtension() const;
 
-    int                 go();
+    int			go();
 
     static const char*	filesep_;
 
