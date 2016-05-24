856df9f8fdf7863d74ef8df99de12d6456f7ed2d
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2016-05-24 14:36:41 +0200

    Added handling of already resolved address when forwarding requests.

--- src/Network/backend-server/googleapp/dlsites.php
+++ src/Network/backend-server/googleapp/dlsites.php
@@ -10,7 +10,14 @@ $platform = "";
 $id = "";
 $address = "";
 
-if ( array_key_exists( 'REMOTE_ADDR', $_SERVER ) )
+/*First, attempt to see if address is set in _GET.
+  This may be the case if the request is forwarded to us,
+  and the address is already captured. Otherwise, capture
+  the address from _SEVER */
+
+if ( array_key_exists( 'REMOTE_ADDR', $_GET ) )
+    $address = $_GET['REMOTE_ADDR'];
+else if ( array_key_exists( 'REMOTE_ADDR', $_SERVER ) )
     $address = $_SERVER['REMOTE_ADDR'];
 
 if ( array_key_exists( 'p', $_GET ) )
