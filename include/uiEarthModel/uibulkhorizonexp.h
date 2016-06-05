33f73d1265715d250105acf9b4957aad05a00fd1
Nanne Hemstra <hemstra@users.noreply.github.com>
2016-06-04 21:19:29 -0500

    Added class for bulk horizon export. Nothing implemented yet.

---
+++ include/uiEarthModel/uibulkhorizonexp.h
@@ -0,0 +1,27 @@
+#ifndef uibulkhorizonexp_h
+#define uibulkhorizonexp_h
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : Nanne Hemstra
+ * DATE     : May 2016
+-*/
+
+#include "uiearthmodelmod.h"
+#include "uidialog.h"
+
+class uiFileInput;
+
+mExpClass(uiEarthModel) uiBulkHorizonExport : public uiDialog
+{ mODTextTranslationClass(uiBulkHorizonExport);
+public:
+			uiBulkHorizonExport(uiParent*);
+			~uiBulkHorizonExport();
+
+protected:
+
+    bool		acceptOK(CallBacker*);
+
+    uiFileInput*	outfld_;
+};
+
+#endif
---
+++ src/uiEarthModel/uibulkhorizonexp.cc
@@ -0,0 +1,44 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Nanne Hemstra
+ Date:          May 2016
+________________________________________________________________________
+
+-*/
+
+
+#include "uibulkhorizonexp.h"
+
+#include "uibutton.h"
+#include "uifiledlg.h"
+#include "uifileinput.h"
+#include "uimsg.h"
+#include "uitaskrunner.h"
+#include "od_helpids.h"
+
+
+uiBulkHorizonExport::uiBulkHorizonExport( uiParent* p )
+    : uiDialog(p,uiDialog::Setup(uiStrings::phrExport(tr("Multiple Horizons")),
+				 mNoDlgTitle,
+				 mODHelpKey(mBulkHorizonExportHelpID) )
+			    .modal(false))
+{
+    setOkText( uiStrings::sExport() );
+
+    outfld_ = new uiFileInput( this, 
+		      uiStrings::sOutputASCIIFile(),
+		      uiFileInput::Setup().withexamine(false) );
+}
+
+
+uiBulkHorizonExport::~uiBulkHorizonExport()
+{
+}
+
+
+bool uiBulkHorizonExport::acceptOK( CallBacker* )
+{
+    return false;
+}
