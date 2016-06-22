740e5692d8c323428e63ee61186f68fa4e09b943
Satyaki <satyaki.maitra@dgbes.com>
2016-06-22 11:25:48 +0530

    Fix for CDash warning

--- src/uiViewer2D/view2dpickset.cc
+++ src/uiViewer2D/view2dpickset.cc
@@ -267,7 +267,7 @@ void VW2DPickSet::drawAll()
 		BufferString dipval;
 		pl.getText( "Dip" , dipval );
 		SeparString dipstr( dipval );
-		float distance;
+		double distance;
 		const TrcKeyZSampling& vwr2dtkzs = regfdp->sampling();
 		if ( !regfdp->is2D() )
 		{
