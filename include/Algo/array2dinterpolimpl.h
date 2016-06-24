f5b509399a60acf7b81987132a8a87c500e51df7
Yuancheng Liu <yuancheng.liu@dgbes.com>
2016-06-24 11:06:23 -0500

    Improved TriangleInterpolator for gridding. Added polyTrend.

--- include/Algo/array2dinterpolimpl.h
+++ include/Algo/array2dinterpolimpl.h
@@ -14,6 +14,7 @@ ________________________________________________________________________
 
 #include "algomod.h"
 #include "array2dinterpol.h"
+#include "binid.h"
 #include "rowcol.h"
 #include "threadlock.h"
 
@@ -27,21 +28,21 @@ class Extension2DInterpolExecutor;
 \brief Interpolates 2D arrays using inverse distance method.
 
   Parameters:
-  
-  1. searchradius - 
+
+  1. searchradius -
 		sets the search radius. Should have the same unit as
 		given in setRowStep and getColStep.
 		If undefined, all defined nodes will be used, and no other
 		settings will be used.
-  2. stepsize/nrsteps - 
-		sets how many nodes that will be done in each step. Each step 
+  2. stepsize/nrsteps -
+		sets how many nodes that will be done in each step. Each step
 		will only use points defined in previous steps (not really true
 		as shortcuts are made). In general, larger steps gives faster
 		interpolation, but lower quality. If stepsize is 10, and nrsteps
 		is 10, a border of maximum 100 nodes will be interpolated around
 		the defined positions.
-  3. cornersfirst - 
-		if true, algorithm will only interpolate nodes that has the 
+  3. cornersfirst -
+		if true, algorithm will only interpolate nodes that has the
 		same number of support (i.e. neigbors) within +-stepsize, before
 		reevaluating which nodes to do next. Enabling cornersfirst will
 		give high quality output at the expense of speed.
@@ -50,7 +51,7 @@ class Extension2DInterpolExecutor;
 mExpClass(Algo) InverseDistanceArray2DInterpol : public Array2DInterpol
 { mODTextTranslationClass(InverseDistanceArray2DInterpol);
 public:
-		mDefaultFactoryInstantiation(Array2DInterpol, 
+		mDefaultFactoryInstantiation(Array2DInterpol,
 			InverseDistanceArray2DInterpol,
 			"InverseDistance", tr("Inverse distance") )
 
@@ -72,7 +73,7 @@ public:
 
     void	setCornersFirst(bool n)		{ cornersfirst_ = n; }
     bool	getCornersFirst() const		{ return cornersfirst_; }
-    bool	nothingToFill() const		{ return nrIterations()<1; }  
+    bool	nothingToFill() const		{ return nrIterations()<1; }
 
     virtual bool	fillPar(IOPar&) const;
     virtual bool	usePar(const IOPar&);
@@ -92,13 +93,13 @@ protected:
     od_int64		getNextIdx();
     void		reportDone(od_int64);
 
-    				//settings
+				//settings
     int				nrsteps_;
     int				stepsize_;
     bool			cornersfirst_;
     float			searchradius_;
 
-    				//workflow control stuff
+				//workflow control stuff
     int				nrthreads_;
     int				nrthreadswaiting_;
     bool			waitforall_;
@@ -107,9 +108,9 @@ protected:
     int				stepidx_;
 
     int				nrinitialdefined_;
-    int				totalnr_;	
+    int				totalnr_;
 
-    				//Working arrays
+				//Working arrays
     bool*			curdefined_;
     bool*			nodestofill_;
     TypeSet<od_int64>		definedidxs_;		//Only when no radius
@@ -117,12 +118,12 @@ protected:
     TypeSet<float>		neighborweights_;	//Only when radius
 
 
-    				//Per support size				
+				//Per support size
     TypeSet<od_int64>		addedwithcursuport_;
     int				nraddedthisstep_;
     od_int64			prevsupportsize_;
 
-    				//perstep
+				//perstep
     TypeSet<od_int64>		todothisstep_;
     TypeSet<od_int64>		nrsources_;	//linked to todothisstep or zero
 };
@@ -139,19 +140,19 @@ public:
 			TriangulationArray2DInterpol,
 			 "Triangulation", ::toUiString(sFactoryKeyword()))
 
-    		TriangulationArray2DInterpol();
-    		~TriangulationArray2DInterpol();
+		TriangulationArray2DInterpol();
+		~TriangulationArray2DInterpol();
 
     bool	setArray(Array2D<float>&,TaskRunner*);
     bool	canUseArrayAccess() const	{ return true; }
     bool	setArray(ArrayAccess&,TaskRunner*);
-    bool	nothingToFill() const		{ return nrIterations()<1; } 
+    bool	nothingToFill() const		{ return nrIterations()<1; }
 
     bool	doInterpolation() const		{ return dointerpolation_; }
     void	doInterpolation(bool yn)	{ dointerpolation_ = yn; }
     float	getMaxDistance() const		{ return maxdistance_; }
     void	setMaxDistance(float r)		{ maxdistance_ = r; }
-    
+
     virtual bool	fillPar(IOPar&) const;
     virtual bool	usePar(const IOPar&);
 
@@ -168,21 +169,24 @@ protected:
     virtual bool	initFromArray(TaskRunner*);
     void		getNextNodes(TypeSet<od_int64>&);
 
-    				//triangulation stuff
+				//triangulation stuff
     bool			dointerpolation_;
     float			maxdistance_;
     DAGTriangleTree*		triangulation_;
     Triangle2DInterpolator*	triangleinterpolator_;
     TypeSet<int>		coordlistindices_;
 
-    				//Working arrays
+				//Working arrays
     bool*			curdefined_;
     bool*			nodestofill_;
     od_int64			curnode_;
     Threads::Lock		curnodelock_;
 
-    				//Work control
+				//Work control
     od_int64			totalnr_;
+    int				corneridx_[4];
+    float			cornerval_[4];
+    BinID			cornerbid_[4];
 };
 
 
@@ -194,14 +198,14 @@ mExpClass(Algo) ExtensionArray2DInterpol : public Array2DInterpol
 { mODTextTranslationClass(ExtensionArray2DInterpol);
 public:
 		mDefaultFactoryInstantiation(Array2DInterpol,
-		    ExtensionArray2DInterpol, "Extension", 
+		    ExtensionArray2DInterpol, "Extension",
 		     ::toUiString(sFactoryKeyword()))
 
-    		ExtensionArray2DInterpol();
-    		~ExtensionArray2DInterpol();
+		ExtensionArray2DInterpol();
+		~ExtensionArray2DInterpol();
 
     bool	canUseArrayAccess() const	{ return true; }
-    
+
     void	setNrSteps(int n)		{ nrsteps_ = n; }
     od_int64	getNrSteps() const		{ return nrsteps_; }
 
@@ -214,11 +218,11 @@ protected:
 
     bool	doWork(od_int64,od_int64,int);
     od_int64	nrIterations() const		{ return 1; }
-    
-    od_int64	nrsteps_;   
-    
-    Extension2DInterpolExecutor* executor_; 
-    friend class Extension2DInterpolExecutor;    
+
+    od_int64	nrsteps_;
+
+    Extension2DInterpolExecutor* executor_;
+    friend class Extension2DInterpolExecutor;
 };
 
 #endif
