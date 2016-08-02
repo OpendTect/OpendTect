2d8576a951cea850f534925cb1013ca63d3e1829
Satyaki <satyaki.maitra@dgbes.com>
2016-08-02 12:41:07 +0530

    od6.0 : Fix for saved attribute set opened in SynthRock Crossplot
    Summary: Now there is a new ui which prompts selection of datapacks. https://jira.dgbes.com:8443/browse/OD-865
    
    Test Plan: Tested with saving and restoring attribute set in SR crossplot. Works with multiple inputs. No regression in normal attribute set cases
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D10658

--- src/uiAttributes/uistoredattrreplacer.cc
+++ src/uiAttributes/uistoredattrreplacer.cc
@@ -22,10 +22,15 @@ static const char* rcsID mUsedVar = "$Id$";
 #include <string.h>
 
 #include "uiattrinpdlg.h"
+#include "uicombobox.h"
 #include "uimsg.h"
 
+#include "hiddenparam.h"
 
 using namespace Attrib;
+
+static HiddenParam< uiStoredAttribReplacer,TypeSet<DataPack::FullID>* >
+			dpfids( 0 );
 
 uiStoredAttribReplacer::uiStoredAttribReplacer( uiParent* parent,
 						DescSet* attrset )
@@ -36,6 +41,7 @@ uiStoredAttribReplacer::uiStoredAttribReplacer( uiParent* parent,
     , noofseis_(0)
     , noofsteer_(0)
 {
+    dpfids.setParam( this, new TypeSet<DataPack::FullID>() );
     getStoredIds();
 
     for ( int idx=0; idx<storedids_.size(); idx++ )
@@ -57,6 +63,7 @@ uiStoredAttribReplacer::uiStoredAttribReplacer( uiParent* parent,
     , noofseis_(0)
     , noofsteer_(0)
 {
+    dpfids.setParam( this, new TypeSet<DataPack::FullID>() );
     usePar( *iopar );
 
     for ( int idx=0; idx<storedids_.size(); idx++ )
@@ -66,6 +73,14 @@ uiStoredAttribReplacer::uiStoredAttribReplacer( uiParent* parent,
 	else
 	    noofseis_++;
     }
+}
+
+
+uiStoredAttribReplacer::~uiStoredAttribReplacer()
+{
+    TypeSet<DataPack::FullID>* fids = dpfids.getParam( this );
+    dpfids.removeParam( this );
+    delete fids;
 }
 
 
@@ -175,10 +190,9 @@ static bool hasSpace( const char* txt )
 }
 
 
-void uiStoredAttribReplacer::setStoredKey( IOPar* par, const char* key )
+static void setDefinitionKey( IOPar& par, const char* key )
 {
-    if ( !par || !key ) return;
-    const char* defstring = par->find( "Definition" );
+    const char* defstring = par.find( "Definition" );
     BufferString defstr( defstring );
     char* maindefstr = defstr.find( "id=" );
     if ( !maindefstr )
@@ -195,7 +209,14 @@ void uiStoredAttribReplacer::setStoredKey( IOPar* par, const char* key )
     if ( usequotes ) defstr += "\"";
     defstr += finalpartstr;
 
-    par->set( "Definition", defstr );
+    par.set( "Definition", defstr );
+}
+
+
+void uiStoredAttribReplacer::setStoredKey( IOPar* par, const char* key )
+{
+    if ( !par || !key ) return;
+    setDefinitionKey( *par, key );
 }
 
 
@@ -272,6 +293,101 @@ void uiStoredAttribReplacer::go()
 }
 
 
+void uiStoredAttribReplacer::setDataPackIDs(
+	const TypeSet<DataPack::FullID>& dpids )
+{
+    *dpfids.getParam(this) = dpids;
+}
+
+
+class uiDataPackReplacerDlg : public uiDialog
+{ mODTextTranslationClass(uiDataPackReplacerDlg)
+public:
+uiDataPackReplacerDlg( uiParent* p,
+		  TypeSet<uiStoredAttribReplacer::StoredEntry>& storedids,
+		  const TypeSet<DataPack::FullID>& dpids )
+    : uiDialog(p,uiDialog::Setup(tr("Select data for input"),tr(""),mNoHelpKey))
+    , storedids_(storedids)
+    , attrset_(0)
+    , attrdspar_(0)
+    , dpfids_(dpids)
+{
+    BufferStringSet dpnms;
+    for ( int dpidx=0; dpidx<dpids.size(); dpidx++ )
+	dpnms.add( DataPackMgr::nameOf(dpids[dpidx]) );
+
+    for ( int inpidx=0; inpidx<storedids_.size(); inpidx++ )
+    {
+	uiStoredAttribReplacer::StoredEntry& storedkey = storedids_[inpidx];
+	if ( storedkey.has2Ids() )
+	    continue;
+
+	uiLabeledComboBox* lastinpfld = inpflds_.last();
+	uiString lbl = tr("Input for '%1'").arg( storedkey.storedref_ );
+	inpflds_ += new uiLabeledComboBox( this, dpnms, lbl );
+	if ( lastinpfld )
+	    inpflds_.last()->attach( alignedBelow, lastinpfld );
+    }
+}
+
+bool acceptOK( CallBacker* )
+{
+    int inpfldidx = 0;
+    if ( !attrset_ && !attrdspar_ )
+    {
+	pErrMsg( "Either pass a DescSet or an IOPar" );
+	return false;
+    }
+
+    for ( int idx=0; idx<storedids_.size(); idx++ )
+    {
+	uiStoredAttribReplacer::StoredEntry& storedid = storedids_[idx];
+	if ( storedid.has2Ids() )
+	    continue;
+
+	uiComboBox* inpfld = inpflds_[inpfldidx]->box();
+	const int seldpidx = inpfld->currentItem();
+	BufferString dpidstr( "#" );
+	dpidstr += dpfids_[seldpidx];
+	FixedString dpnm( DataPackMgr::nameOf(dpfids_[seldpidx]) );
+	if ( attrset_ )
+	{
+	    Desc* ad = attrset_->getDesc( storedid.firstid_ );
+	    ad->changeStoredID( dpidstr );
+	    ad->setUserRef( dpnm );
+	}
+	else
+	{
+	    IOPar* descpar = attrdspar_->subselect( storedid.firstid_.asInt() );
+	    setDefinitionKey( *descpar, dpidstr );
+	    descpar->set( "UserRef", dpnm );
+	    BufferString idstr;
+	    idstr+= storedid.firstid_.asInt();
+	    attrdspar_->mergeComp( *descpar, idstr );
+	}
+
+	inpfldidx++;
+    }
+
+    return true;
+}
+
+void setAttribDescSet( Attrib::DescSet* ds )	{ attrset_ = ds; }
+
+void setAttribDescSetPar( IOPar* par )		{ attrdspar_ = par; }
+
+
+protected:
+
+const TypeSet<DataPack::FullID>&			dpfids_;
+TypeSet<uiStoredAttribReplacer::StoredEntry>&		storedids_;
+ObjectSet<uiLabeledComboBox>				inpflds_;
+Attrib::DescSet*					attrset_;
+IOPar*							attrdspar_;
+
+};
+
+
 void uiStoredAttribReplacer::handleSingleInput()
 {
     if ( storedids_.isEmpty() ) return;
@@ -293,6 +409,21 @@ void uiStoredAttribReplacer::handleSingleInput()
 	    seisinpidx += idx;
 	}
     }
+
+    if ( !dpfids.getParam(this)->isEmpty() )
+    {
+	uiDataPackReplacerDlg dlg( parent_, storedids_, *dpfids.getParam(this));
+	if ( attrset_ )
+	    dlg.setAttribDescSet( attrset_ );
+	else
+	    dlg.setAttribDescSetPar( iopar_ );
+
+	if ( !dlg.go() && attrset_ )
+	    attrset_->removeAll( true );
+
+	return;
+    }
+
 
     uiAttrInpDlg dlg( parent_, seisinprefs, steerinprefs, is2d_ );
     if ( !dlg.go() )
