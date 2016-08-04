OpendTect/OpendTect

commit a6e19692e7b48159519e605ffd55832f42db74ad

[OD6.0] Ensure several visualization callback function into main threads
Summary: This revision is ported from revision D10489.

Test Plan:
Tested in F3-demo in auto tracking, re-tracking, remove seeds, pick seeds in 3d horizon tracking on randomline and inline. it works fine.
Tested also in 2dview by tracking 2d horizon. it works as normal.
So far I have not found crash.

Reviewers: nanne, kristofer

Reviewed By: kristofer

Differential Revision: https://phabricator.dgbes.com:8443/D10490

include/visSurvey/visemobjdisplay.h
@@ -19,6 +19,7 @@ ________________________________________________________________________
 #include "emposid.h"
 #include "visobject.h"
 #include "vissurvobj.h"
+#include "emobject.h"
 
 class ZAxisTransform;
 
@@ -34,6 +35,32 @@ namespace visBase
 
 namespace visSurvey
 {
+struct EMChangeData
+{
+    ObjectSet<const EM::EMObjectCallbackData>	emcallbackdata_;
+    Threads::Lock				lock_;
+
+    void    addCallBackData( const EM::EMObjectCallbackData* data )
+    {
+	Threads::Locker locker( lock_ );
+	emcallbackdata_ += data;
+    }
+
+    const EM::EMObjectCallbackData*   getCallBackData( int idx ) const
+    {
+	if ( idx<emcallbackdata_.size() )
+	    return emcallbackdata_[idx];
+	return 0;    
+    }
+
+    void    clearData()
+    {
+	Threads::Locker locker( lock_ );
+	emcallbackdata_.setEmpty();
+    }
+
+    int	    size() const { return emcallbackdata_.size(); }
+};
 
 class MPEEditor;
 class EdgeLineSetDisplay;
@@ -138,6 +165,7 @@ protected:
     void			polygonFinishedCB(CallBacker*);
 
     virtual void		updateSelections();
+    void			handleEmChange(const EM::EMObjectCallbackData&);
 
     Notifier<EMObjectDisplay>	hasmoved;
     Notifier<EMObjectDisplay>	locknotifier;

src/visSurvey/vishorizondisplay.cc
@@ -68,6 +68,9 @@ static HiddenParam<HorizonDisplay,Threads::Mutex*> locker_( 0 );
 static HiddenParam<HorizonDisplay,char> newseeds_( false );
 static HiddenParam<HorizonDisplay,visBase::PointSet*> sectionlockedpts_( 0 );
 
+static HiddenParam<HorizonDisplay,visSurvey::EMChangeData*>
+                                            emchangedata_( 0 );
+
 
 class LockedPointsCalculator: public ParallelTask
 {
@@ -177,7 +180,7 @@ HorizonDisplay::HorizonDisplay()
 
     newseeds_.setParam( this, false );
     sectionlockedpts_.setParam( this, 0 );
-
+    emchangedata_.setParam( this, new EMChangeData );
 }
 
 
@@ -217,6 +220,11 @@ HorizonDisplay::~HorizonDisplay()
 
     delete locker_.getParam( this );
     locker_.removeParam( this );
+    
+    EMChangeData* horchangedata = emchangedata_.getParam( this );
+    if ( horchangedata )
+      horchangedata->clearData();
+    emchangedata_.removeParam( this );
 
     if ( sectionlockedpts_.getParam( this) )
 	sectionlockedpts_.getParam(this)->unRef();
@@ -1101,10 +1109,35 @@ void HorizonDisplay::setIntersectLineMaterial( visBase::Material* nm )
 
 void HorizonDisplay::emChangeCB( CallBacker* cb )
 {
-    if ( !cb ) return;
+    if ( cb )
+    {
+	mCBCapsuleUnpack(const EM::EMObjectCallbackData&,cbdata,cb);
+	emchangedata_.getParam( this )->addCallBackData( &cbdata );
+    }
     
-    EMObjectDisplay::emChangeCB( cb );
-    mCBCapsuleUnpack(const EM::EMObjectCallbackData&,cbdata,cb);
+    mEnsureExecutedInMainThread( HorizonDisplay::emChangeCB );
+
+    EMChangeData* horchangedata = emchangedata_.getParam(this);
+    if ( !horchangedata ) return;
+
+    for ( int idx=0; idx<horchangedata->size(); idx++ )
+    {
+	const EM::EMObjectCallbackData* cbdata = 
+        horchangedata->getCallBackData( idx );
+	if ( !cbdata )
+	    continue;
+	EMObjectDisplay::handleEmChange( *cbdata );
+	handleEmChange( *cbdata );
+    }
+
+    horchangedata->clearData();
+    updateSingleColor();   
+    newseeds_ = true;
+}
+
+
+void HorizonDisplay::handleEmChange( const EM::EMObjectCallbackData& cbdata )
+{
     if ( cbdata.event==EM::EMObjectCallbackData::PositionChange )
     {
 	validtexture_ = false;
@@ -1127,7 +1160,14 @@ void HorizonDisplay::emChangeCB( CallBacker* cb )
 	    if ( selections_ && selections_->getMaterial() )
 		selections_->getMaterial()->setColor(
 		hor3d->getSelectionColor() );
-	    updateLockedPointsColor();
+	    if ( lockedpts_ && lockedpts_->getMaterial() )
+		lockedpts_->getMaterial()->setColor( hor3d->getLockColor() );
+	    if ( sectionlockedpts_.getParam(this) )
+	    {
+		if ( sectionlockedpts_.getParam(this)->getMaterial() )
+		    sectionlockedpts_.getParam(this)->getMaterial()->setColor( 
+		    hor3d->getLockColor() );
+	    }
 	}
     }
     else if ( cbdata.event==EM::EMObjectCallbackData::SelectionChange )
@@ -1143,10 +1183,7 @@ void HorizonDisplay::emChangeCB( CallBacker* cb )
 	mDynamicCastGet( const EM::Horizon3D*, hor3d, emobject_ )
 	const bool locked = hor3d ? hor3d->hasLockedNodes() : 0;
 	if ( locked && !displayonlyatsections_ ) 
-	{
-	    updateLockedPointsColor();
 	    return; 
-	}
 
 	if ( !locked )
 	{
@@ -1163,8 +1200,6 @@ void HorizonDisplay::emChangeCB( CallBacker* cb )
 		sectionlockedpts_.getParam(this)->turnOn( true );
 	}
     }
-    updateSingleColor();   
-    newseeds_ = true;
 }
 
 
@@ -1183,8 +1218,6 @@ void HorizonDisplay::updateLockedPointsColor()
 	    hor3d->getLockColor() );
     }
 }
-
-
 
 Color HorizonDisplay::singleColor() const
 {

include/visSurvey/vishorizondisplay.h
@@ -207,7 +207,7 @@ private:
     EM::PosID			findClosestNode(const Coord3&) const;
     void			createDisplayDataPacks(
 					int attrib,const DataPointSet*);
-
+    
     void			removeSectionDisplay(const EM::SectionID&);
     visBase::VisualObject*	createSection(const EM::SectionID&) const;
     bool			addSection(const EM::SectionID&,TaskRunner*);
@@ -227,6 +227,7 @@ private:
 
     void			initSelectionDisplay(bool erase);
     virtual void		updateSelections();
+    void			handleEmChange(const EM::EMObjectCallbackData&);
     void			updateLockedPointsColor();
 
     bool				allowshading_;

src/uiVis/uimpeman.cc
@@ -929,7 +929,10 @@ void uiMPEMan::cleanPatchDisplay()
 void uiMPEMan::undo()
 {
     if ( !getSelectedDisplay() || !getSelectedDisplay()->isSelected() )
-	return;
+    {
+	if ( !getSelected2DDisplay() || !getSelected2DDisplay()->isSelected() )
+	    return;
+    }
 
     MouseCursorChanger mcc( MouseCursor::Wait );
     uiString errmsg;
@@ -957,7 +960,10 @@ void uiMPEMan::undo()
 void uiMPEMan::redo()
 {
     if ( !getSelectedDisplay() || !getSelectedDisplay()->isSelected() )
-	return;
+    {
+	if ( !getSelected2DDisplay() || !getSelected2DDisplay()->isSelected() )
+	    return;
+    }
 
     MouseCursorChanger mcc( MouseCursor::Wait );
     uiString redoerrmsg;

src/visSurvey/visemobjdisplay.cc
@@ -12,7 +12,6 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "visemobjdisplay.h"
 
 #include "emmanager.h"
-#include "emobject.h"
 #include "iopar.h"
 #include "ioobj.h"
 #include "ioman.h"
@@ -35,10 +34,15 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "vistexturechannel2rgba.h"
 #include "vispolygonselection.h"
 #include "zaxistransform.h"
+#include "hiddenparam.h"
 
 
 #define mSelColor Color( 0, 255, 0 )
 #define mDefaultSize 4
+
+static HiddenParam<visSurvey::EMObjectDisplay,visSurvey::EMChangeData*>
+					    emchangedata_( 0 );
+
 
 namespace visSurvey
 {
@@ -82,6 +86,7 @@ EMObjectDisplay::EMObjectDisplay()
     drawstyle_->setLineStyle( defls );
 
     getMaterial()->setAmbience( 0.8 );
+    emchangedata_.setParam( this, new EMChangeData );
 }
 
 
@@ -115,6 +120,12 @@ EMObjectDisplay::~EMObjectDisplay()
     }
 
     clearSelections();
+
+    EMChangeData* emchangeddata = emchangedata_.getParam( this );
+    if ( emchangeddata )
+	emchangeddata->clearData();
+    emchangedata_.removeParam(this);
+
 }
 
 
@@ -510,11 +521,34 @@ EM::SectionID EMObjectDisplay::getSectionID( const TypeSet<int>* path ) const
 
 void EMObjectDisplay::emChangeCB( CallBacker* cb )
 {
-    bool triggermovement = false;
+    if ( cb )
+    {
+	mCBCapsuleUnpack( const EM::EMObjectCallbackData&, cbdata, cb );
+	emchangedata_.getParam( this )->addCallBackData( &cbdata );
+    }
+    
+    mEnsureExecutedInMainThread( EMObjectDisplay::emChangeCB );
+
+    EMChangeData* emchangedata = emchangedata_.getParam(this);
+    if ( !emchangedata ) return;
+
+    for ( int idx=0; idx<emchangedata->size(); idx++ )
+    {
+	const EM::EMObjectCallbackData* cbdata = 
+	    emchangedata->getCallBackData(idx);
+	if ( !cbdata )
+	    continue;
+	handleEmChange( *cbdata );
+    }
+    emchangedata->clearData();
+ }
 
 
-    mCBCapsuleUnpack(const EM::EMObjectCallbackData&,cbdata,cb);
-    if ( cbdata.event==EM::EMObjectCallbackData::SectionChange )
+void EMObjectDisplay::handleEmChange( const EM::EMObjectCallbackData& cbdata )
+{
+
+   bool triggermovement = false;
+   if ( cbdata.event==EM::EMObjectCallbackData::SectionChange )
     {
 	const EM::SectionID sectionid = cbdata.pid0.sectionID();
 	if ( emobject_->sectionIndex(sectionid)>=0 )

src/visSurvey/vishorizon2ddisplay.cc
@@ -41,6 +41,9 @@ static HiddenParam< Horizon2DDisplay,visBase::MarkerSet* > intersectmkset_(0);
 static HiddenParam< Horizon2DDisplay,char > updateintsectmarkers_(true);
 static HiddenParam< Horizon2DDisplay,int > nr2dlines_(0);
 static HiddenParam< Horizon2DDisplay,Line2DInterSectionSet* > ln2dset_(0);
+static HiddenParam<visSurvey::Horizon2DDisplay,visSurvey::EMChangeData*>
+                                            emchangedata_( 0 );
+
 
 Horizon2DDisplay::Horizon2DDisplay()
 {
@@ -57,6 +60,7 @@ Horizon2DDisplay::Horizon2DDisplay()
     mkset->setMaterial( new visBase::Material );
     mkset->setMarkerStyle( MarkerStyle3D::Sphere );
     mkset->setScreenSize( 4.0 );
+    emchangedata_.setParam( this, new EMChangeData );
 }
 
 
@@ -77,6 +81,10 @@ Horizon2DDisplay::~Horizon2DDisplay()
     ln2dset_.removeParam( this );
 
     nr2dlines_.removeParam( this );
+    EMChangeData* emchangeddata = emchangedata_.getParam(this);
+    if ( emchangeddata )
+        emchangeddata->clearData();
+    emchangedata_.removeParam( this );
 }
 
 
@@ -500,14 +508,33 @@ bool Horizon2DDisplay::shouldDisplayIntersections(
 
 void Horizon2DDisplay::emChangeCB( CallBacker* cb )
 {
-    updateintsectmarkers_.setParam( this, true );
-    EMObjectDisplay::emChangeCB( cb );
-    mCBCapsuleUnpack(const EM::EMObjectCallbackData&,cbdata,cb);
-    if ( cbdata.event==EM::EMObjectCallbackData::PrefColorChange )
+    if ( cb )
     {
-	getMaterial()->setColor( emobject_->preferredColor() );
-	setLineStyle( emobject_->preferredLineStyle() );
+       mCBCapsuleUnpack( const EM::EMObjectCallbackData&, cbdata, cb );
+       emchangedata_.getParam(this)->addCallBackData( &cbdata );
     }
+
+    mEnsureExecutedInMainThread( Horizon2DDisplay::emChangeCB );
+
+    EMChangeData* hor2dchangedata = emchangedata_.getParam(this);
+    if ( !hor2dchangedata ) return;
+
+    updateintsectmarkers_.setParam( this, true );
+    for ( int idx=0; idx<hor2dchangedata->size(); idx++ )
+    {
+      const EM::EMObjectCallbackData* cbdata=
+          hor2dchangedata->getCallBackData( idx );
+      if ( !cbdata )
+          continue;
+      EMObjectDisplay::handleEmChange( *cbdata );
+      if ( cbdata->event==EM::EMObjectCallbackData::PrefColorChange )
+      {
+          getMaterial()->setColor( emobject_->preferredColor() );
+          setLineStyle( emobject_->preferredLineStyle() );
+      }
+    }
+
+    hor2dchangedata->clearData();
 }
 
 

commit 6a9d713d2cb757290840693a5971b928bfa8cd36

[OD6.0]OD-874 Horizon Auto-tracking: a lag between actions and display
Summary:
This bug was introduced by revision D10490. The solution implements a copy of
EM::EMObjectCallbackData to solve the bug.

Test Plan: Tested in F3-demo by doing the steps described in the Jira issue. it works fine

Reviewers: raman

Reviewed By: raman

Differential Revision: https://phabricator.dgbes.com:8443/D10673

include/EarthModel/emobject.h
@@ -53,6 +53,16 @@ public:
 		    , event( EMObjectCallbackData::Undef )
 		{}
 
+
+		EMObjectCallbackData( const EMObjectCallbackData& data )
+		    : pid0( data.pid0 )
+		    , pid1( data.pid1 )
+		    , attrib( data.attrib )
+		    , flagfor2dviewer( data.flagfor2dviewer )
+		    , event( data.event )
+		{}
+
+
     enum Event { Undef, PositionChange, PosIDChange, PrefColorChange, Removal,
    		 AttribChange, SectionChange, NameChange, SelectionChange,
    		 LockChange, BurstAlert } event;

include/visSurvey/visemobjdisplay.h
@@ -56,7 +56,7 @@ struct EMChangeData
     void    clearData()
     {
 	Threads::Locker locker( lock_ );
-	emcallbackdata_.setEmpty();
+	deepErase( emcallbackdata_ );
     }
 
     int	    size() const { return emcallbackdata_.size(); }

src/visSurvey/vishorizondisplay.cc
@@ -1112,7 +1112,8 @@ void HorizonDisplay::emChangeCB( CallBacker* cb )
     if ( cb )
     {
 	mCBCapsuleUnpack(const EM::EMObjectCallbackData&,cbdata,cb);
-	emchangedata_.getParam( this )->addCallBackData( &cbdata );
+	emchangedata_.getParam( this )->addCallBackData( 
+	    new EM::EMObjectCallbackData(cbdata) );
     }
     
     mEnsureExecutedInMainThread( HorizonDisplay::emChangeCB );