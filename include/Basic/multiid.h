9cdd569d05238246022edbe2875736f222346a37
Bert Bril <ahbril@users.noreply.github.com>
2016-09-06 01:41:11 +0200

    MultiID ... is a DBKey ...

--- include/Basic/multiid.h
+++ include/Basic/multiid.h
@@ -11,66 +11,9 @@ ________________________________________________________________________
 
 -*/
 
-#include "basicmod.h"
-#include "compoundkey.h"
-#include "string2.h"
-#include "fixedstring.h"
+#include "dbkey.h"
 
+typedef DBKey MultiID;
 
-/*!\brief Compound key consisting of ints */
-
-mExpClass(Basic) MultiID : public CompoundKey
-{
-public:
-
-    typedef int		SubID;
-
-			MultiID(const FixedString& s)
-			: CompoundKey(s)	{}
-			MultiID( const char* s=0 )
-			: CompoundKey(s)	{}
-			MultiID( const MultiID& mid )
-			: CompoundKey(mid)	{}
-			MultiID( SubID id )
-			{ add(id); }
-			MultiID(SubID,SubID);
-
-    inline MultiID&	operator =( const MultiID& mi )
-			{ impl_ = mi.impl_; return *this; }
-    inline MultiID&	operator =( const CompoundKey& ck )
-			{ impl_ = ck.buf(); return *this; }
-    inline MultiID&	operator =( const FixedString& fs )
-			{ impl_ = fs.str(); return *this; }
-    inline MultiID&	operator =( const char* s )
-			{ impl_ = s; return *this; }
-
-    inline bool		operator==( const MultiID& m ) const
-			{ return impl_ == m.impl_; }
-    inline bool		operator==( const char* s ) const
-			{ return impl_ == s; }
-
-    inline SubID	subID( IdxType idx ) const
-			{ return key(idx).toInt(); }
-    inline void		setSubID( IdxType idx, SubID id )
-			{ setKey( idx, toString(id) ); }
-    SubID		getIDAt(int lvl) const;
-    SubID		leafID() const;
-    MultiID		parent() const;
-
-    inline MultiID&	set( const MultiID& oth )
-			{ *this = oth; return *this; }
-    inline MultiID&	set( const char* s )
-			{ *this = s; return *this; }
-    inline MultiID&	add( SubID id )
-			{ *this += toString(id); return *this; }
-
-    od_int64		toInt64() const;
-    static MultiID	fromInt64(od_int64);
-
-    static const MultiID& udf();
-    inline void		setUdf()		{ *this = udf(); }
-    bool		isUdf() const;
-
-};
 
 #endif
