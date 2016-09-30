bbcdec22282652d57cac0dc0029c7c4a19b1b20e
Nanne Hemstra <hemstra@users.noreply.github.com>
2016-09-30 16:49:50 -0500

    removed setOkText and setCancelText. Is already done by the setCtrlStyle call.

--- src/uiODMain/uiodhelpmenumgr.cc
+++ src/uiODMain/uiodhelpmenumgr.cc
@@ -188,8 +188,6 @@ uiODLegalInfo(uiParent* p)
     textfld_ = new uiTextEdit( this );
     textfld_->attach( alignedBelow, textsel_ );
 
-    setCancelText( uiString::emptyString() );
-    setOkText( uiStrings::sClose() );
     selChgCB( 0 );
 }
 
