33d6590a2f38f76d609c20fe53c1bd764bb585de
Nanne Hemstra <hemstra@users.noreply.github.com>
2016-12-20 11:22:56 -0600

    Added a missing feet to meter conversion. More changes needed for code cleanup.

--- src/EarthModel/poly2horvol.cc
+++ src/EarthModel/poly2horvol.cc
@@ -133,6 +133,9 @@ float Poly2HorVol::getM3( float vel, bool upw, bool useneg )
     const float cellarea = SI().inlDistance() * hs.step_.inl() * xyfactor
 			 * SI().crlDistance() * hs.step_.crl() * xyfactor;
     const float v = SI().zIsTime() ? vel * .5f : 1; // TWT
+    if ( SI().zInFeet() )
+	totth *= mFromFeetFactorF;
+
     return cellarea * v * totth;
 }
 
