6431828cf45abcc7ea001a48459e3dad8a263774
arnauddgbes <arnaud.huck@dgbes.com>
2017-02-01 11:16:17 +0100

    od6.0: Added appropriate uiMessage for BinIDWiseTask
    Summary: with an empty message such tasks will not report their start time status during progress. Fixed that.
    
    Test Plan: start time correctly shows up in log files.
    
    Reviewers: helene
    
    Reviewed By: helene
    
    Differential Revision: https://phabricator.dgbes.com:8443/D11383

--- src/VolumeProcessing/volprocstep.cc
+++ src/VolumeProcessing/volprocstep.cc
@@ -21,10 +21,14 @@ class BinIDWiseTask : public ParallelTask
 public:
 
 		BinIDWiseTask( Step& ro )
-		    : step_( ro ), totalnr_( -1 ) { setName(ro.userName()); }
+		    : step_( ro ), totalnr_( -1 )
+		{
+		    setName(ro.userName());
+		    msg_ = tr("Executing Volume Builder Task");
+		}
 
-    uiString	uiMessage() const	{ return errmsg_; }
-    uiString	uiNrDoneText() const	{ return tr("Positions done"); }
+    uiString	uiMessage() const	{ return msg_; }
+    uiString	uiNrDoneText() const	{ return ParallelTask::sTrcFinished(); }
 
 protected:
 
@@ -83,13 +87,13 @@ protected:
     bool	doPrepare( int nrthreads )
 		{
 		    const bool res = step_.prepareComp( nrthreads );
-		    if ( !res ) errmsg_ = step_.errMsg();
+		    if ( !res ) msg_ = step_.errMsg();
 		    return res;
 		}
 
     Step&		step_;
     mutable int		totalnr_;
-    uiString		errmsg_;
+    uiString		msg_;
 };
 
 } // namespace VolProc
