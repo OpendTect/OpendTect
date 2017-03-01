c7a0d26c325c11fb25b58c8a978c06da47856f7b
Nanne Hemstra <hemstra@users.noreply.github.com>
2017-03-01 12:46:44 -0600

    Bugfix in ValSeriesMathFunc::getValue. ValueSeriesInterpolator should receive a maxidx, not the size. Fixes OD-1494. Reviewed by Ding.

--- src/Algo/mathfunc.cc
+++ src/Algo/mathfunc.cc
@@ -26,7 +26,7 @@ ValSeriesMathFunc::ValSeriesMathFunc( const ValueSeries<float>& vs, int sz )
 
 float ValSeriesMathFunc::getValue( float x ) const
 {
-    ValueSeriesInterpolator<float> interp( sz_ );
+    ValueSeriesInterpolator<float> interp( sz_-1 );
     if ( (-1<x && x<0) || (sz_<x && x<sz_+1) )
 	interp.extrapol_ = true;
 
