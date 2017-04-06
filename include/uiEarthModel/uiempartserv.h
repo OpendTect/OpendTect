02ed9cec7073174568711bcdfb462324a85ecafa
dgbprajjavalsingh <prajjaval.singh@dgbes.com>
2017-04-06 09:48:40 +0530

    Missing header file for D11633

--- include/uiEarthModel/uiempartserv.h
+++ include/uiEarthModel/uiempartserv.h
@@ -68,8 +68,8 @@ public:
     bool		importBulkFaults();
     bool		importFaultStickSet();
     void		import2DFaultStickset();
-    bool		exportFault();
-    bool		exportFaultStickSet();
+    bool		exportFault(bool single=true);
+    bool		exportFaultStickSet(bool single=true);
     void		createHorWithConstZ(bool is2d);
 
     MultiID		getStorageID(const EM::ObjectID&) const;
