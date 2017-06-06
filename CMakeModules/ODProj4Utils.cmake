b0aaad185017bc96616744cef968bf35792a7e02
Nageswara Rao <nageswara.rao@dgbes.com>
2017-06-06 11:23:50 +0530

    Ported from od6.2; Related to commit 'a1b35ef226d8a7e15095f0773381e5d7ba80fe69'

--- CMakeModules/ODProj4Utils.cmake
+++ CMakeModules/ODProj4Utils.cmake
@@ -30,8 +30,7 @@ macro( OD_ADD_PROJ4 )
 
     set ( PROJ4_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/external/proj4/src
 			     ${CMAKE_BINARY_DIR}/external/proj4/src )
-    set ( PROJ4_LIBRARY_PATH
-	${CMAKE_BINARY_DIR}/external/proj4/${CMAKE_BUILD_TYPE} )
+    set ( PROJ4_LIBRARY_PATH ${CMAKE_BINARY_DIR}/external/proj4/lib)
 endmacro()
 
 macro(OD_SETUP_PROJ4)
@@ -48,30 +47,32 @@ macro(OD_SETUP_PROJ4)
 	list ( APPEND OD_${OD_MODULE_NAME}_RUNTIMEPATH ${PROJ4_LIBRARY_PATH} )
 
 	if ( OD_INSTALL_DEPENDENT_LIBS )
-	    foreach ( BUILD_TYPE Debug Release )
+	    set( LIB proj )
+	    if ( ${CMAKE_BUILD_TYPE} STREQUAL "Debug" )
+		set( LIB ${LIB}d )
+	    endif()
+	    include(${PROJ4_LIBRARY_PATH}/../FindProjLibVersion.cmake)
 
-		set( LIB proj )
+	    if( UNIX OR APPLE )
+		if( APPLE )
+		    #installing Mac proj4 files(like libproj.12.dylib etc. )
+		    set( PROJ4LIBBUILDVER ${PROJ4_LIBRARY_PATH}/lib${LIB}.${PROJ_BUILD_VERSION}.dylib )
+		    set( PROJ4LIB lib${LIB}.${PROJ_API_VERSION}.dylib)
 
-		OD_FILTER_LIBRARIES( ${LIB} ${BUILD_TYPE} )
-
-		get_filename_component( PROJ4LIBNAME ${LIB} NAME_WE )
-		    if( UNIX OR APPLE )
-		        if( APPLE )
-			    #installing Mac proj4 files(like libproj.12.dylib etc. )
-			    file( GLOB ALLLIBS "${PROJ4_LIBRARY_PATH}/${LIB}.[0-9][0-9]*.dylib" )
-	
-			else()
-			    #installing linux proj4 files(like libproj.so.12 etc. )
-			    file( GLOB ALLLIBS "${PROJ4_LIBRARY_PATH}/${LIB}.so.[0-9][0-9]*" )
-			endif()
-			list( APPEND ARGS ${ALLLIBS} )
-			list( GET ARGS 0 PROJ4LIB )
-			OD_INSTALL_LIBRARY( ${PROJ4LIB} ${BUILD_TYPE} )
-
-			set( PROJ4LIB "" )
-		    endif()
-
-	    endforeach()
+		else()
+		    #installing linux proj4 files(like libproj.so.12 etc. )
+		    set( PROJ4LIBBUILDVER ${PROJ4_LIBRARY_PATH}/lib${LIB}.so.${PROJ_BUILD_VERSION} )
+		    set( PROJ4LIB lib${LIB}.so.${PROJ_API_VERSION})
+		endif()
+		if ( ${CMAKE_BUILD_TYPE} STREQUAL "Release" )
+		    install( PROGRAMS ${PROJ4LIBBUILDVER} DESTINATION ${OD_LIB_INSTALL_PATH_RELEASE}
+			     RENAME ${PROJ4LIB} CONFIGURATIONS ${CMAKE_BUILD_TYPE} )
+		else()
+		    install( PROGRAMS ${PROJ4LIBBUILDVER} DESTINATION ${OD_LIB_INSTALL_PATH_DEBUG}
+			     RENAME ${PROJ4LIB} CONFIGURATIONS ${CMAKE_BUILD_TYPE} )
+		endif()
+	    set( PROJ4LIB ${PROJ4LIB} PARENT_SCOPE)
+	    endif()
 	endif( OD_INSTALL_DEPENDENT_LIBS )
     endif()
 
--- CMakeModules/packagescripts/basedatadefs.cmake
+++ CMakeModules/packagescripts/basedatadefs.cmake
@@ -10,7 +10,7 @@ SET( LIBLIST omf Attribs BasicSurvey ColTabs MouseControls BatchPrograms
 	     RockPhysics ModDeps.od Strat
 	     prodlist.txt Vendors
 	     BatchHosts_example_for_nodes_of_a_linux_server.txt
-	     BatchHosts_example_for_nodes_of_a_windows_server.txt Scripts )
+	     BatchHosts_example_for_nodes_of_a_windows_server.txt Scripts CRS )
 SET( EXECLIST  )
 SET( PACK "basedata")
 
