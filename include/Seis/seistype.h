a2a2331f1a186bc1c5955f83188436d8c11d2fba
Bert Bril <ahbril@users.noreply.github.com>
2017-06-22 10:28:32 +0200

    Removed unnecessary mGlobal causing warning=error on VS2017

--- include/Seis/seistype.h
+++ include/Seis/seistype.h
@@ -22,7 +22,7 @@ class BufferStringSet;
 namespace Seis
 {
 
-    mGlobal(Seis) enum SelType	{ Range, Table, Polygon };
+    enum SelType		{ Range, Table, Polygon };
     mGlobal(Seis) const char*	nameOf(SelType);
     mGlobal(Seis) SelType	selTypeOf(const char*);
 
