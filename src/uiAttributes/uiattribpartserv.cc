57186bcfab45a2d89847819a128f26e1096c2044
Nanne Hemstra <hemstra@users.noreply.github.com>
2017-08-01 17:16:36 -0500

    Fixed problem of losing access to segy data as described in OD-1759

--- src/uiAttributes/uiattribpartserv.cc
+++ src/uiAttributes/uiattribpartserv.cc
@@ -745,7 +745,7 @@ const RegularSeisDataPack* uiAttribPartServer::createOutput(
 	    cache = 0;
 
 	const bool isz = tkzs.isFlat()&&tkzs.defaultDir() == TrcKeyZSampling::Z;
-	if ( isz )
+	if ( !preloadeddatapack && isz )
 	{
 	    uiString errmsg;
 	    Desc* nonconsttargetdesc = const_cast<Desc*>( targetdesc );
