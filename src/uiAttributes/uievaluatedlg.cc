fe99b4ddacd5c3800ba6a432d097a6ef14c20704
Nanne Hemstra <hemstra@users.noreply.github.com>
2017-10-05 15:59:37 -0500

    User refs are now constructed using incoming userref, not the attribute name.

--- src/uiAttributes/uievaluatedlg.cc
+++ src/uiAttributes/uievaluatedlg.cc
@@ -375,7 +375,7 @@ void uiEvaluateDlg::calcPush( CallBacker* )
     if ( !desc )
 	return;
 
-    BufferString userchosenref = desc->userRef();
+    const BufferString userchosenref = desc->userRef();
     const int nrsteps = nrstepsfld->box()->getIntValue();
     for ( int idx=0; idx<nrsteps; idx++ )
     {
@@ -387,8 +387,7 @@ void uiEvaluateDlg::calcPush( CallBacker* )
 	BufferString defstr; newad->getDefStr( defstr ); //Only for debugging
 
 	const char* lbl = pargrp->getLabel();
-	BufferString usrref = newad->attribName();
-	usrref += " - "; usrref += lbl;
+	const BufferString usrref( userchosenref.buf(), " - ", lbl );
 	newad->setUserRef( usrref );
 	if ( newad->selectedOutput()>=newad->nrOutputs() )
 	{
