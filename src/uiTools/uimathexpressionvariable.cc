2f991913c56d3c3796f8e852bfeba1e44673cecb
dgbprajjavalsingh <prajjaval.singh@dgbes.com>
2017-10-17 10:10:34 +0530

    od6.2: OD-1813, increased the size of input field for maths formula
    Summary: Earlier field had to be resized by dragging the dialoge border, now the field is wide enough to hold large formula
    
    Test Plan: Tested visually in 6.2
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D12129

--- src/uiTools/uimathexpressionvariable.cc
+++ src/uiTools/uimathexpressionvariable.cc
@@ -48,6 +48,7 @@ uiMathExpressionVariable::uiMathExpressionVariable( uiParent* p,
     inpgrp_ = new uiGroup( this, "Input group" );
     inpfld_ = new uiComboBox( inpgrp_, BufferString("input ",varidx_+1) );
     const uiString lblstr = tr("For input number %1 use").arg(varidx_+1);
+    inpfld_->setHSzPol(uiObject::WideMax);
     inplbl_ = new uiLabel( inpgrp_, lblstr, inpfld_ );
     inplbl_->setPrefWidthInChar( 35 );
     inplbl_->setAlignment( Alignment::Right );
@@ -61,10 +62,8 @@ uiMathExpressionVariable::uiMathExpressionVariable( uiParent* p,
 	subinpfld_->selectionChanged.notify(
 			    mCB(this,uiMathExpressionVariable,subInpChg) );
     }
-
     constfld_ = new uiGenInput( inpgrp_, tr("Value for 'c0'"), FloatInpSpec() );
     constfld_->attach( alignedWith, inpfld_ );
-
     if ( withunit )
     {
 	uiUnitSel::Setup uussu( PropertyRef::Other, tr("convert to:") );
