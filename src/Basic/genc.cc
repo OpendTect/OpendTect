56b93655985e02aa237c84178049db93a621e8d0
arnauddgbes <arnaud.huck@dgbes.com>
2017-10-23 09:38:33 +0200

    od: Reverting the fix in Basic from D12136

--- src/Basic/genc.cc
+++ src/Basic/genc.cc
@@ -201,10 +201,8 @@ static float getFloatFromString( const char* str, char** endptr )
     bool isok = false;
     const QString qstr( str );
     const float ret = qstr.toFloat( &isok );
-    if ( !isok )
-	return strtof( (char*)str, endptr );
-
-    *endptr = (char*)(str + 1); // just fake it, as long as it's not str
+    if ( isok )
+	*endptr = (char*)(str + 1); // just fake it, as long as it's not str
 
     return ret;
 
