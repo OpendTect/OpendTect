b71fe24ecdfad52fe187bebd48f994efe2e7ddd4
arnauddgbes <arnaud.huck@dgbes.com>
2017-11-06 14:53:10 +0100

    od: simplified FindDosEOL.sh, improved FindNoNewlineAtEndOfFile.sh

--- testscripts/FindDosEOL.sh
+++ testscripts/FindDosEOL.sh
@@ -5,71 +5,54 @@
 # FindDosEOL.sh - finds files with DOS end-of-lines.
 #
 
-if [ $# -lt 1 ]
-then
+progname=$0
+
+if [ $# -gt 1 ] && [ $1 == "--listfile" ]; then
+  listfile=$2
+  if [ ! -f $listfile ]; then
+    echo "File $listfile does not exists."
+  fi
+elif [ $# -lt 1 ]; then
   echo "Usage: `basename $0` <listfile> [--wdir basedir]"
+  echo
+  echo ${progname} - Checks that all lines of all files end with a Unix-style new line character
+  echo Returns 0 if all lines of all files end with a unix-style new line character, otherwise 1
   exit 1
-fi
-
-listfile=$1
-
-if [ ! -f $listfile ];
-then
-   echo "File $listfile does not exists."
-   exit 1
 fi
 
 scriptdir=`dirname $0`
 
 if [ -e "${scriptdir}/GetNrProc" ]; then
-    nrcpus=`${scriptdir}/GetNrProc`
+  nrcpus=`${scriptdir}/GetNrProc`
 else
-    nrcpus=8
+  nrcpus=8
 fi
 
 srcdir=`dirname $scriptdir`
-if [ $# == 3 ];
-then
-   srcdir=$3
+if [ ${*: -2:1} == "--wdir" ] && [ -d "$BASH_ARGV" ]; then
+  srcdir=$BASH_ARGV
 fi
 
-cd $srcdir
-srcfiles=`cat $listfile | grep \\.ico -v | grep \\mod.h -v | grep \\Basic/buildinfo.h -v | grep \\odversion.h -v | grep _helpids.h -v | grep qpaths.cc -v`
-for onefile in $srcfiles;
-   do
-      onefile="$srcdir/$onefile"
-      if [ ! -e "$onefile" ];then
-         echo "File not found: $onefile"
-         exit 1
-      fi
-done
-files=`echo $srcfiles | xargs -P ${nrcpus} -n 200 grep -l $'\r'`
-allfiles=$files
-
+srcfiles=`cat $listfile | grep \\.ico -v | grep \\mod.h -v | grep \\Basic/buildinfo.h -v | grep \\odversion.h -v | grep _helpids.h -v | grep qpaths.cc -v | awk -v srcdir=${srcdir} '{print srcdir"/"$1;}'`
 bindir=`dirname $listfile`
 bindir=`dirname $bindir`
-cd $bindir
-binfiles=`cat $listfile | grep \\mod.h`
-for onefile in $binfiles;
-   do
-      onefile="$bindir/$onefile"
-      if [ ! -e "$onefile" ];then
-         echo "File not found: $onefile"
-         exit 1
-      fi
+binfiles=`cd ${bindir}; cat $listfile | grep \\mod.h | awk -v dirnm=${bindir} '{print dirnm"/"$1;}'`
+inputfiles=${srcfiles}" "${binfiles}
+
+for onefile in ${inputfiles};
+do
+  if [ ! -f "${onefile}" ];then
+    echo "File not found: $onefile"
+    exit 1
+  fi
 done
-files=`echo $binfiles | xargs -P ${nrcpus} -n 200 grep -l $'\r'`
-if [ -z "$allfiles" ];then
-   allfiles="$files"
-else
-   allfiles="$allfiles $files"
+files=`echo ${inputfiles} | xargs -P ${nrcpus} -n 200 grep -l $'\r'`
+
+if [ ! -z "$files" ];then
+  echo "DOS EOL found in: "
+  echo $files
+  exit 1
 fi
 
-if [ -z "$allfiles" ];then
-   echo "No DOS EOL found!"
-else
-   echo "DOS EOL found in: "
-   echo $allfiles
-   exit 1
-fi
+exit ${status}
 
