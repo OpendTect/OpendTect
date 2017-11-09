19eeea84d682955994075866aa00a57eee926060
arnauddgbes <arnaud.huck@dgbes.com>
2017-11-09 08:44:55 +0100

    od6.0: Fixed test program test_netsocket
    Summary: the test timesout when run by cmake as its launches another executable test_echoserver, which is only claused (abnormaly) with the timeout. Thus added an explicit kill send through the socket, as done by test_netreqechoserver
    
    Test Plan: all test programs from Network module pass successfully when run using cmake testing.
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Subscribers: ranojay
    
    Differential Revision: https://phabricator.dgbes.com:8443/D12171

--- src/Network/tests/netreq.cc
+++ src/Network/tests/netreq.cc
@@ -15,6 +15,7 @@ static const char* rcsID mUsedVar = "$Id$";
 
 #include "netreqconnection.h"
 #include "netreqpacket.h"
+#include "netserver.h"
 
 
 #define mLargePayload 50000000
@@ -165,7 +166,7 @@ public:
 	if ( sendkill )
 	{
 	    Network::RequestPacket killpacket;
-	    killpacket.setStringPayload("Kill");
+	    killpacket.setStringPayload( Network::Server::sKeyKillword() );
 	    killpacket.setIsNewRequest();
 
 	    mRunStandardTestWithError( conn.sendPacket( killpacket ),
@@ -256,16 +257,16 @@ int main(int argc, char** argv)
     mInitTestProg();
     ApplicationData app;
 
-    Tester runner;
-    runner.port_ = 1025;
-    clparser.getVal( "port", runner.port_, true );
-    runner.hostname_ = "localhost";
-    runner.prefix_ = "[singlethreaded] ";
+    PtrMan<Tester> runner = new Tester;
+    runner->port_ = 1025;
+    clparser.getVal( "port", runner->port_, true );
+    runner->hostname_ = "localhost";
+    runner->prefix_ = "[singlethreaded] ";
 
     BufferString echoapp = "test_netreqechoserver";
     clparser.getVal( "serverapp", echoapp );
 
-    BufferString args( "--port ", runner.port_ );
+    BufferString args( "--port ", runner->port_ );
     args.add( " --quiet " );
 
     if ( !clparser.hasKey("noechoapp") && !ExecODProgram( echoapp, args.buf() ))
@@ -276,11 +277,13 @@ int main(int argc, char** argv)
 
     Threads::sleep( 1 );
 
-    if ( !runner.runTest(false,false) )
+    if ( !runner->runTest(false,false) )
 	ExitProgram( 1 );
 
-    CallBack::addToMainThread( mCB( &runner,Tester, runEventLoopTest) );
+    CallBack::addToMainThread( mCB(runner,Tester,runEventLoopTest) );
     const int retval = app.exec();
+
+    runner = 0;
 
     ExitProgram( retval );
 }
