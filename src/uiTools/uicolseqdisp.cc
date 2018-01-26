e33c502a66357803d264c4d252ca2db0256478e6
Nanne Hemstra <hemstra@users.noreply.github.com>
2018-01-26 16:36:58 -0600

    MouseWheel events are handled by the NavigationMouseEventHandler. Not sure why. Needs some rework. At least, scrolling on the color canvas works now.

--- src/uiTools/uicolseqdisp.cc
+++ src/uiTools/uicolseqdisp.cc
@@ -181,7 +181,8 @@ void uiColSeqDisp::initCB( CallBacker* )
     MouseEventHandler& meh = getMouseEventHandler();
     mAttachCB( meh.buttonPressed, uiColSeqDisp::mousePressCB );
     mAttachCB( meh.buttonReleased, uiColSeqDisp::mouseReleaseCB );
-    mAttachCB( meh.wheelMove, uiColSeqDisp::mouseWheelCB );
+    mAttachCB( getNavigationMouseEventHandler().wheelMove,
+	       uiColSeqDisp::mouseWheelCB );
 
     KeyboardEventHandler& keh = getKeyboardEventHandler();
     mAttachCB( keh.keyReleased, uiColSeqDisp::keybCB );
@@ -265,18 +266,17 @@ void uiColSeqDisp::handleMouseBut( bool ispressed )
 
 void uiColSeqDisp::mouseWheelCB( CallBacker* )
 {
-    MouseEventHandler& meh = getMouseEventHandler();
+    MouseEventHandler& meh = getNavigationMouseEventHandler();
     if ( meh.isHandled() )
 	return;
     const MouseEvent& event = meh.event();
     if ( event.isWithKey() )
 	return;
 
-    //TODO how does the wheel stuff work?
-    // if ( wheel-up )
-	// mMouseTrigger( upReq )
-    // else if ( wheel-down )
-	// mMouseTrigger( downReq )
+    if ( event.angle() > 0 )
+	mMouseTrigger( upReq )
+    else if ( event.angle() < 0 )
+	mMouseTrigger( downReq )
 }
 
 
