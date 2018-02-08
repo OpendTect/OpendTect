f3ac9d75371f87df626c576978680e253aa0c014
Raman Singh <raman.singh@dgbes.com>
2018-02-08 12:18:58 +0530

    Added the Localized Velocity Fan Filter on request of StatOil

---
+++ include/uiAttributes/uidipfilterattrib.h
@@ -0,0 +1,53 @@
+#pragma once
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Nanne Hemstra
+ Date:          July 2001
+ RCS:           $Id$
+________________________________________________________________________
+
+-*/
+
+#include "uiattributesmod.h"
+#include "uiattrdesced.h"
+
+class uiAttrSel;
+class uiGenInput;
+class uiLabeledSpinBox;
+
+/*! \brief DipFilter Attribute description editor */
+
+mExpClass(uiAttributes) uiDipFilterAttrib : public uiAttrDescEd
+{ mODTextTranslationClass(uiDipFilterAttrib);
+public:
+
+			uiDipFilterAttrib(uiParent*,bool);
+
+    virtual void	getEvalParams(TypeSet<EvalParam>&) const;
+
+protected:
+
+    uiAttrSel*		inpfld_;
+    uiLabeledSpinBox*	szfld_;
+    uiGenInput*		fltrtpfld_;
+    uiGenInput*		velfld_;
+    uiGenInput*		azifld_;
+    uiGenInput*		aziintfld_;
+    uiGenInput*		taperfld_;
+
+    bool		setParameters(const Attrib::Desc&);
+    bool		setInput(const Attrib::Desc&);
+
+    bool		getParameters(Attrib::Desc&);
+    uiRetVal		getInput(Attrib::Desc&);
+
+    void		panelbutCB(CallBacker*);
+    void		fkWinCloseCB(CallBacker*);
+    void		filtSel(CallBacker*);
+    void		aziSel(CallBacker*);
+
+			mDeclReqAttribUIFns
+};
