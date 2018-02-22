b1343dcd5f5c67d9e75e65e4318e027104a0c3aa
arnauddgbes <arnaud.huck@dgbes.com>
2018-02-22 13:32:09 +0100

    od: text stream progress meter was ignoring finished status

--- src/Basic/progressmeter.cc
+++ src/Basic/progressmeter.cc
@@ -238,7 +238,7 @@ void TextStreamProgressMeter::operator++()
 void TextStreamProgressMeter::setNrDone( od_int64 nrdone )
 {
     mSetLock();
-    if ( nrdone<=nrdone_ )
+    if ( finished_ || nrdone<=nrdone_ )
 	return;
 
     addProgress( (int)(nrdone-nrdone_) );
