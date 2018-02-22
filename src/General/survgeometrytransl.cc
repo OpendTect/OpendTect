73c81ed75f787d443545c84ac6c1d07a4c93e5d4
Raman Singh <raman.singh@dgbes.com>
2018-02-22 13:11:59 +0530

    Fixed deprecated warning

--- src/General/survgeometrytransl.cc
+++ src/General/survgeometrytransl.cc
@@ -73,7 +73,7 @@ Survey::Geometry* dgbSurvGeom2DTranslator::readGeometry( const IOObj& ioobj,
     ascistream astrm( strm );
     const bool hasheader = astrm.hasStandardHeader();
     if ( !hasheader )
-        strm.setPosition( 0 );
+        strm.reOpen();
     else
     {
 	if ( astrm.atEOS() )
