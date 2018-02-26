e0ed3cc6ab596146d92c74720041c91b66b83fd5
arnauddgbes <arnaud.huck@dgbes.com>
2018-02-26 17:16:17 +0100

    od6.2: Fixed a memory leak

--- src/Seis/seistrctr.cc
+++ src/Seis/seistrctr.cc
@@ -93,6 +93,7 @@ SeisTrcTranslator::SeisTrcTranslator( const char* nm, const char* unm )
 SeisTrcTranslator::~SeisTrcTranslator()
 {
     cleanUp();
+    delete compnms_;
     delete &trcblock_;
     delete &pinfo_;
     delete &warnings_;
