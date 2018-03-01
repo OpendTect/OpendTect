83e847a9c842c90c263aa9cd802f3d35c7426c71
Nanne Hemstra <hemstra@users.noreply.github.com>
2018-03-01 08:55:19 -0600

    [od6.2] Fix for 2D ZAxistransform of points
    Summary: PointSets are not transformed in a Wheeler scene when HorizonCube is 2D. canTransformSurv is now telling me whether the transform is 2D or 3D. Then needed to loop through all 2D lines to find the nearest TrcKey. Is this the most efficient way?
    
    Test Plan: PointSets displayed correctly in 2D wheeler scene
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D12378

--- src/visSurvey/vislocationdisplay.cc
+++ src/visSurvey/vislocationdisplay.cc
@@ -13,6 +13,7 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "pickset.h"
 #include "picksettr.h"
 #include "selector.h"
+#include "survgeom2d.h"
 
 #include "visevent.h"
 #include "vismaterial.h"
@@ -520,7 +521,42 @@ bool LocationDisplay::transformPos( Pick::Location& loc ) const
 {
     if ( !datatransform_ ) return true;
 
-    const float newdepth = datatransform_->transform( loc.pos_ );
+    float newdepth = mUdf(float);
+    if ( datatransform_->canTransformSurv(TrcKey::std3DSurvID()) )
+	newdepth = datatransform_->transformTrc( loc.trcKey(), loc.z() );
+    else if ( datatransform_->canTransformSurv(TrcKey::std2DSurvID()) )
+    {
+	if ( loc.trcKey().is2D() )
+	    newdepth = datatransform_->transformTrc( loc.trcKey(), loc.z() );
+	else // Pre v6.0 Pickset without TrcKey information
+	{
+	    BufferStringSet nms; TypeSet<Pos::GeomID> ids;
+	    Survey::GM().getList( nms, ids, true );
+	    TrcKey ntk = TrcKey::udf();
+	    float ndist = mUdf(float);
+	    for ( int idx=0; idx<ids.size(); idx++ )
+	    {
+		mDynamicCastGet(const Survey::Geometry2D*,geom2d,
+				Survey::GM().getGeometry(ids[idx]))
+		if ( !geom2d )
+		    continue;
+
+		float dist;
+		const TrcKey tk = geom2d->nearestTrace( loc.pos_, &dist );
+		if ( dist>ndist || dist>geom2d->averageTrcDist() )
+		    continue;
+
+		ndist = dist;
+		ntk = tk;
+	    }
+
+	    if ( ntk.isUdf() )
+		return false;
+
+	    newdepth = datatransform_->transformTrc( ntk, loc.pos_.z );
+	}
+    }
+
     if ( mIsUdf(newdepth) )
 	return false;
 
