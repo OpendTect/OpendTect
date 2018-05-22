9ff3f614aaac98f49119af766d37de1d50a88cfb
arnauddgbes <arnaud.huck@dgbes.com>
2018-05-22 16:58:28 +0200

    od: avoid reset of hdf5 to system libraries when re-running cmake

--- plugins/HDF5/PreInit.cmake
+++ plugins/HDF5/PreInit.cmake
@@ -4,12 +4,17 @@
 #       Feb 2018	Bert
 #_______________________________________________________________________________
 
-find_package( HDF5 1.10 QUIET COMPONENTS CXX )
 
-if ( HDF5_FOUND )
-  set( OD_PLUGINS ${OD_PLUGINS} HDF5 )
-  if ( HDF5_ROOT )
-    unset( HDF5_ROOT CACHE )
+if ( NOT EXISTS ${HDF5_hdf5_LIBRARY_RELEASE} )
+  find_package( HDF5 1.10 QUIET COMPONENTS CXX )
+  if ( HDF5_FOUND )
+    set( OD_PLUGINS ${OD_PLUGINS} HDF5 )
+    if ( HDF5_ROOT )
+      unset( HDF5_ROOT CACHE )
+    endif()
   endif()
+else()
+  set( OD_PLUGINS ${OD_PLUGINS} HDF5 )
 endif()
+
 
