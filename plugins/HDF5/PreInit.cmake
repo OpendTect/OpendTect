0dcc5c1649e854edfe0689ad75161488ca05bbb6
arnauddgbes <arnaud.huck@dgbes.com>
2018-05-23 10:16:04 +0200

    od: solved linker errors for HDF5 after reconfiguration. Removed unnecessary BUILD_HDF5_SUPPORT. HDF5 location is found from system location or external path using HDF5_ROOT (cmake variable) provided that the version allows it (>=1.10)

--- plugins/HDF5/PreInit.cmake
+++ plugins/HDF5/PreInit.cmake
@@ -4,17 +4,21 @@
 #       Feb 2018	Bert
 #_______________________________________________________________________________
 
-
-if ( NOT EXISTS ${HDF5_hdf5_LIBRARY_RELEASE} )
-  find_package( HDF5 1.10 QUIET COMPONENTS CXX )
-  if ( HDF5_FOUND )
-    set( OD_PLUGINS ${OD_PLUGINS} HDF5 )
-    if ( HDF5_ROOT )
-      unset( HDF5_ROOT CACHE )
+if ( EXISTS ${HDF5_hdf5_LIBRARY_RELEASE} )
+  get_filename_component( hdf5_path ${HDF5_hdf5_LIBRARY_RELEASE} DIRECTORY )
+  if ( IS_DIRECTORY ${hdf5_path} )
+    get_filename_component( hdf5_path ${hdf5_path} DIRECTORY )
+    if ( IS_DIRECTORY ${hdf5_path} )
+      set ( HDF5_ROOT ${hdf5_path} )
     endif()
   endif()
-else()
-  set( OD_PLUGINS ${OD_PLUGINS} HDF5 )
 endif()
 
+find_package( HDF5 1.10 QUIET COMPONENTS CXX )
+if ( HDF5_ROOT )
+  unset( HDF5_ROOT CACHE )
+endif()
 
+if ( HDF5_FOUND )
+  set( OD_PLUGINS ${OD_PLUGINS} HDF5 )
+endif()
