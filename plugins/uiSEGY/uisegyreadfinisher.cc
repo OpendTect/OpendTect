4200266704004dfda4d61dd6105a2d7ccebc4d4c
Prajjaval Singh <prajjaval.singh@dgbes.com>
2018-05-23 18:28:23 +0530

    Now can launch multiple batch process for same volume in SEG-Y

--- plugins/uiSEGY/uisegyreadfinisher.cc
+++ plugins/uiSEGY/uisegyreadfinisher.cc
@@ -633,7 +633,7 @@ bool uiSEGYReadFinisher::do2D( const IOObj& inioobj, const IOObj& outioobj,
 bool uiSEGYReadFinisher::doBatch( bool doimp )
 {
     const BufferString jobname( doimp ? "Import SEG-Y (" : "Scan SEG-Y (",
-				objname_, ")" );
+				outFld(doimp)->getInput(), ")" );
     batchfld_->setJobName( jobname );
     IOPar& jobpars = batchfld_->jobSpec().pars_;
     const bool isps = Seis::isPS( fs_.geomType() );
