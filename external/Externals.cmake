b37a44602c7a689428d993b8b577c5f3032a9182
Raman Singh <raman.singh@dgbes.com>
2018-06-19 11:38:24 +0530

    Created a new branch od6.4

--- external/Externals.cmake
+++ external/Externals.cmake
@@ -1,5 +1,5 @@
 include ( "CMakeModules/ODExternals.cmake" )
 
-DEFINE_GIT_EXTERNAL( osgGeo https://github.com/OpendTect/osgGeo.git osgGeo-1.3 )
+DEFINE_GIT_EXTERNAL( osgGeo https://github.com/OpendTect/osgGeo.git osgGeo-1.4 )
 DEFINE_GIT_EXTERNAL( proj4 https://github.com/OpendTect/proj4.git master )
-DEFINE_SVN_EXTERNAL( doc_csh https://github.com/OpendTect/opendtectdoc.git/branches/od6.2/od_userdoc/Project/Advanced/CSH ${CMAKE_SOURCE_DIR}/external HEAD )
+DEFINE_SVN_EXTERNAL( doc_csh https://github.com/OpendTect/opendtectdoc.git/branches/od6.4/od_userdoc/Project/Advanced/CSH ${CMAKE_SOURCE_DIR}/external HEAD )
