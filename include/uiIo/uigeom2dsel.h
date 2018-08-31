9fb7b4d67463c4e06f56bc44623dbdf87e176542
Nanne Hemstra <hemstra@users.noreply.github.com>
2018-08-31 15:08:40 -0500

    Added

---
+++ include/uiIo/uigeom2dsel.h
@@ -0,0 +1,24 @@
+#pragma once
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Nanne Hemstra
+ Date:		January 2018
+ RCS:		$Id: $
+________________________________________________________________________
+
+-*/
+
+#include "uiiomod.h"
+#include "uiioobjsel.h"
+
+mExpClass(uiIo) uiGeom2DSel : public uiIOObjSel
+{
+public:
+                        uiGeom2DSel(uiParent*,bool forread,
+                                const uiString& seltxt=uiString::empty());
+                        ~uiGeom2DSel();
+
+protected:
+};
