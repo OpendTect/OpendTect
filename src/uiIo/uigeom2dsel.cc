e9b054fbccc4a6da625829e409f47dd85d2e8051
Nanne Hemstra <hemstra@users.noreply.github.com>
2018-08-31 15:08:56 -0500

    Added

---
+++ src/uiIo/uigeom2dsel.cc
@@ -0,0 +1,45 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Nanne Hemstra
+ Date:		January 2018
+________________________________________________________________________
+
+-*/
+
+static const char* rcsID mUsedVar = "$Id: Exp $";
+
+
+#include "uigeom2dsel.h"
+
+#include "survgeometrytransl.h"
+
+static IOObjContext getContext( bool forread )
+{
+    IOObjContext ret = mIOObjContext(SurvGeom2D);
+    ret.forread_ = forread;
+    return ret;
+}
+
+
+static uiIOObjSel::Setup getSetup( bool forread, const uiString& seltxt )
+{
+    uiString st = seltxt;
+    if ( st.isEmpty() )
+	st = forread ? od_static_tr("uiGeom2DSel","Input Geometry")
+		     : od_static_tr("uiGeom2DSel","Output Geometry");
+    uiIOObjSel::Setup su( st );
+    return su;
+}
+
+
+uiGeom2DSel::uiGeom2DSel( uiParent* p, bool forread, const uiString& seltxt )
+    : uiIOObjSel(p,getContext(forread),getSetup(forread,seltxt))
+{
+}
+
+
+uiGeom2DSel::~uiGeom2DSel()
+{
+}
