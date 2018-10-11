4fb0f910bce8e7b5c19f6af64e0bf8c3bee010a9
Raman Singh <raman.singh@dgbes.com>
2018-10-11 10:24:15 +0530

    Added option to chose order of OK/Cancel buttons

--- include/uiTools/uivarwizarddlg.h
+++ include/uiTools/uivarwizarddlg.h
@@ -26,7 +26,7 @@ public:
 			// one says 'Finish', the other 'Go'.
 
 			uiVarWizardDlg(uiParent*,const uiDialog::Setup&,IOPar&,
-					Position);
+					Position,bool revbuttons=true);
 			~uiVarWizardDlg();
 
 protected:
