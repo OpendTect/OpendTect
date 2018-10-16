110be98cb69f673c6b70ef7351af60ab1d4f9ce4
Bert Bril <ahbril@users.noreply.github.com>
2018-10-16 11:46:05 +0200

    made polyFit more robust

--- include/Algo/polyfit.h
+++ include/Algo/polyfit.h
@@ -15,64 +15,61 @@ ________________________________________________________________________
 #include "typeset.h"
 
 
-/*!\brief fits an Nth order polynomial to X, Y data series.
+/*!\brief fits an Nth order polynomial to an X,Y data series. Guaranteed return
+  is a TypeSet v of size N+1, that you can actually use as in:
+
+  y = v[0] + v[1] * x + v[2] * x^2 + ... + v[N] * x^N
 
   Partly taken from Manas Sharma's post on bragitoff.com
  */
 
 
 template <class fT>
-TypeSet<fT> polyFit( const fT* x, const fT* y, int nrpts, int reqorder )
+TypeSet<fT> polyFit( const fT* x, const fT* y, int nrpts, int order )
 {
-    int order = reqorder;
+    TypeSet<fT> ret( order+1, (fT)0 );
     if ( order > nrpts-1 )
     {
 	if ( nrpts < 1 )
-	    return TypeSet<fT>( reqorder, 0 );
+	    return ret;
 	order = nrpts - 1;
 	if ( order < 1 )
-	{
-	    TypeSet<fT> ret( reqorder, 0 );
-	    ret[0] = *y;
-	    return ret;
-	}
+	    { ret[0] = *y; return ret; }
     }
 
-    const int nrsigma = 2*order + 1;
-    fT sigma[nrsigma];
-    for ( int isigma=0; isigma<nrsigma; isigma++ )
+    const int nrxpows = 2*order + 1;
+    fT xpows[nrxpows];
+    for ( int ixpow=0; ixpow<nrxpows; ixpow++ )
     {
-	sigma[isigma] = (fT)0;
+	xpows[ixpow] = (fT)0;
 	for ( int ipt=0; ipt<nrpts; ipt++ )
-	    sigma[isigma] += std::pow( x[ipt], isigma );
+	    xpows[ixpow] += std::pow( x[ipt], ixpow );
     }
 
     fT normmat[order+1][order+2];
     for ( int iorder=0; iorder<=order; iorder++ )
 	for ( int jorder=0; jorder<=order; jorder++ )
-	    normmat[iorder][jorder] = sigma[iorder+jorder];
+	    normmat[iorder][jorder] = xpows[iorder+jorder];
 
-    fT sigmaxn[ order+1 ];
+    fT yxpows[ order+1 ];
     for ( int iorder=0; iorder<=order; iorder++ )
     {
-	sigmaxn[iorder] = (fT)0;
+	yxpows[iorder] = (fT)0;
 	for ( int ipt=0; ipt<nrpts; ipt++ )
-	    sigmaxn[iorder] += std::pow( x[ipt], iorder ) * y[ipt];
+	    yxpows[iorder] += std::pow( x[ipt], iorder ) * y[ipt];
     }
 
     for ( int iorder=0; iorder<=order; iorder++ )
-	normmat[iorder][order+1] = sigmaxn[iorder];
+	normmat[iorder][order+1] = yxpows[iorder];
 
     const int nreq = order + 1;
     for ( int ieq=0; ieq<nreq; ieq++ )
     {
 	for ( int jeq=ieq+1;jeq<nreq;jeq++)
 	{
-	    if ( std::fabs(normmat[ieq][ieq]) >= std::fabs(normmat[jeq][ieq]) )
-		continue;
-
-	    for ( int idx=0; idx<=nreq; idx++ )
-		std::swap( normmat[ieq][idx], normmat[jeq][idx] );
+	    if ( normmat[ieq][ieq] < normmat[jeq][ieq] )
+		for ( int idx=0; idx<=nreq; idx++ )
+		    std::swap( normmat[ieq][idx], normmat[jeq][idx] );
 	}
     }
 
@@ -86,17 +83,15 @@ TypeSet<fT> polyFit( const fT* x, const fT* y, int nrpts, int reqorder )
 	}
     }
 
-    const auto retnrcoeffs = reqorder + 1;
     const auto nrcoeffs = order + 1;
-    TypeSet<fT> coeffs( retnrcoeffs, (fT)0 );
     for ( int icoeff=nrcoeffs-1; icoeff!=-1; icoeff-- )
     {
-	coeffs[icoeff] = normmat[icoeff][nrcoeffs];
+	ret[icoeff] = normmat[icoeff][nrcoeffs];
 	for ( int ieq=0; ieq<nreq; ieq++ )
 	    if ( icoeff != ieq )
-		coeffs[icoeff] -= normmat[icoeff][ieq] * coeffs[ieq];
-	coeffs[icoeff] /= normmat[icoeff][icoeff];
+		ret[icoeff] -= normmat[icoeff][ieq] * ret[ieq];
+	ret[icoeff] /= normmat[icoeff][icoeff];
     }
 
-    return coeffs;
+    return ret;
 }
