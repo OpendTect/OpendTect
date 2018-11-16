f2fd7b9ed9f07414beb746e85ae9abc6b0ea5dea
arnauddgbes <arnaud.huck@dgbes.com>
2018-11-16 13:02:56 +0100

    od: added python function for reading the header information of well-based hdf5 file

--- bin/python/odpy/hdf5.py
+++ bin/python/odpy/hdf5.py
@@ -25,3 +25,51 @@ def getIntValue( infos, ky ):
 
 def getDValue( infos, ky ):
   return float(getAttr( infos, ky ))
+
+def getWellInfo( filenm ):
+  h5file = h5py.File( filenm, "r" )
+  infods = getInfoDataSet( h5file )
+  type = getText(infods,"Type")
+
+  ex_sz = getIntValue(infods,"Examples.Size") 
+  idx = 0
+  examples = list()
+  while idx < ex_sz:
+    example_sz = getIntValue(infods,"Examples."+str(idx)+".Size")
+    example_id = list()
+    idy = 0
+    while idy < example_sz:
+      example_id.append(getText(infods,"Examples."+str(idx)+".ID."+str(idy)))
+      idy += 1
+    example = {
+      "name": getText(infods,"Examples."+str(idx)+".Log"),
+      "id": example_id,
+      "survey": getText(infods,"Examples."+str(idx)+".Survey")
+    }
+    examples.append( example )
+    idx += 1
+
+  inp_sz = getIntValue(infods,"Input.Size")
+  idx = 0
+  input = list()
+  while idx < inp_sz:
+    inp = {
+      "name": getText(infods,"Input."+str(idx)+".Logs"),
+      "survey": getText(infods,"Input."+str(idx)+".Survey")
+    }
+    input.append( inp )
+    idx += 1
+
+  zstep = getDValue(infods,"Z step") 
+  stepout = getIntValue(infods,"Stepout") 
+  marker = (getText(infods,"Top marker"), getText(infods,"Bottom marker"))
+  isinterpol = getBoolValue(infods,"Edge extrapolation")
+  h5file.close()
+  return {
+    'examples': examples,
+    'input': input,
+    'zstep': zstep,
+    'stepout': stepout,
+    'marker': marker,
+    'interpolated': isinterpol
+  }
