7aa855578535fddeb6a85995304d319297b44656
arnauddgbes <arnaud.huck@dgbes.com>
2018-11-29 14:42:44 +0100

    od: maybe python log messaging persitant

--- bin/python/odpy/common.py
+++ bin/python/odpy/common.py
@@ -6,34 +6,43 @@
 # tools common to all odpy scripts
 #
 
+import os
 import platform
 import sys
 
-log_file = sys.stdout
-dbg_file = sys.stderr
+class LogManager(object):
+  def __init__(self):
+    self.log_file = sys.stdout
+    self.dbg_file = sys.stdout
 
-
-def dbg_msg( msg ):
-  dbg_file.write( msg + "\n" )
-
-def set_log_file( fnm ):
-  if not fnm:
-    log_file = sys.stderr
-  else:
-    if os.path.isfile( fnm ):
-      newlogfile = open( fnm, "a" )
+  def set_log_file( self, fnm ):
+    if not fnm:
+      self.log_file = sys.stderr
     else:
-      newlogfile = open( fnm, "w" )
-    if newlogfile.closed:
-      dbg_msg( "Could not open '" + fnm + "'" )
-    else:
-      log_file = newlogfile
-      dbg_msg( "Log file set to '" + fnm + "'" )
+      if os.path.isfile( fnm ):
+        newlogfile = open( fnm, "a" )
+      else:
+        newlogfile = open( fnm, "w" )
+      if newlogfile.closed:
+        dbg_msg( "Could not open '" + fnm + "'" )
+      else:
+        self.log_file = newlogfile
+        self.std_msg( "Log file set to '" + fnm + "'" )
 
-def log_msg( msg ):
-  log_file.write( msg + "\n" )
-  
+  def std_msg( self, msg ):
+    print( msg, file=sys.stdout )
+
+  def log_msg( self, msg ):
+    print( msg, file=self.log_file )
+
+  def dbg_msg( self, msg ):
+    print( msg, file=self.dbg_file )
+
+  def err_msg( self, msg ):
+    print( msg, file=sys.stderr )
+
 
 if platform.python_version() < "3":
   dbg_msg( "odpy requires at least Python 3" )
   exit( 1 )
+
