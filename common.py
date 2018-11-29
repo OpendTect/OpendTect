6e4254455ddba6544ef4a5e42ec925a8a7c1b83a
Bert Bril <ahbril@users.noreply.github.com>
2018-11-29 10:59:02 +0100

    Made log file name available, also in stand-alone programs

--- common.py
+++ common.py
@@ -9,9 +9,31 @@
 import platform
 import sys
 
+log_file = sys.stdout
+dbg_file = sys.stderr
+
+
 def dbg_msg( msg ):
-  sys.stderr.write( msg + "\n" )
+  dbg_file.write( msg + "\n" )
+
+def set_log_file( fnm ):
+  if not fnm:
+    log_file = sys.stderr
+  else:
+    if os.path.isfile( fnm ):
+      newlogfile = open( fnm, "a" )
+    else:
+      newlogfile = open( fnm, "w" )
+    if newlogfile.closed:
+      dbg_msg( "Could not open '" + fnm + "'" )
+    else:
+      log_file = newlogfile
+      dbg_msg( "Log file set to '" + fnm + "'" )
+
+def log_msg( msg ):
+  log_file.write( msg + "\n" )
+  
 
 if platform.python_version() < "3":
-  dbg_msg( "odpy requires at least pyhon 3" )
+  dbg_msg( "odpy requires at least Python 3" )
   exit( 1 )
