e913f330e11c798d01c8573a79afdc9014c0ec1a
arnauddgbes <arnaud.huck@dgbes.com>
2018-11-30 11:59:46 +0100

    od: improvements in python LogManager.

--- common.py
+++ common.py
@@ -11,9 +11,17 @@ import platform
 import sys
 
 class LogManager(object):
-  def __init__(self):
+  def __init__(self, argv):
     self.log_file = sys.stdout
+    self.std_file = sys.stdout
     self.dbg_file = sys.stdout
+    self.err_file = sys.stderr
+    if len(argv)>1 and not 'htm' in argv[1]:
+      stdoutfnm = argv[1]
+      self.set_stdout( stdoutfnm )
+    if len(argv)>2:
+      logfnm = argv[2]
+      self.set_log_file( logfnm )
 
   def set_log_file( self, fnm ):
     if not fnm:
@@ -21,16 +29,28 @@ class LogManager(object):
     else:
       if os.path.isfile( fnm ):
         newlogfile = open( fnm, "a" )
+        self.std_msg( "Log file '" + fnm + "' opened in append mode" )
       else:
         newlogfile = open( fnm, "w" )
+        self.std_msg( "Log file opened in write mode" )
       if newlogfile.closed:
-        dbg_msg( "Could not open '" + fnm + "'" )
+        self.err_msg( "Could not open '" + fnm + "'" )
       else:
         self.log_file = newlogfile
         self.std_msg( "Log file set to '" + fnm + "'" )
 
+  def set_stdout( self, fnm ):
+    if fnm == "null":
+      return
+    if os.path.isfile( fnm ):
+      newlogfile = open( fnm, "a" )
+    else:
+      newlogfile = open( fnm, "w" )
+    if not newlogfile.closed:
+      self.std_file = newlogfile
+
   def std_msg( self, msg ):
-    print( msg, file=sys.stdout )
+    print( msg, file=self.std_file )
 
   def log_msg( self, msg ):
     print( msg, file=self.log_file )
@@ -39,7 +59,7 @@ class LogManager(object):
     print( msg, file=self.dbg_file )
 
   def err_msg( self, msg ):
-    print( msg, file=sys.stderr )
+    print( msg, file=self.err_file )
 
 
 if platform.python_version() < "3":
