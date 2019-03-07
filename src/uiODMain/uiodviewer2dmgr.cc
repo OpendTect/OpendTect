b83fd023873432fc30a9803d7fe648a6f905975f
Nanne Hemstra <hemstra@users.noreply.github.com>
2019-03-07 15:13:41 -0600

    Changed the way nearest trace is found. JIRA: OD-2254

--- src/uiODMain/uiodviewer2dmgr.cc
+++ src/uiODMain/uiodviewer2dmgr.cc
@@ -50,6 +50,7 @@ ________________________________________________________________________
 #include "probeimpl.h"
 #include "probemanager.h"
 #include "randomlinegeom.h"
+#include "scaler.h"
 #include "seisioobjinfo.h"
 #include "survinfo.h"
 #include "survgeom2d.h"
@@ -964,14 +965,16 @@ Line2DInterSection::Point uiODViewer2DMgr::intersectingLineID(
 
     const StepInterval<double> vwrxrg =
 	vwr2d->viewwin()->viewer().posRange( true );
-    const int posidx = vwrxrg.getIndex( pos );
-    if ( posidx<0 )
+    StepInterval<int> vwrtrcrg = vwr2d->getTrcKeyZSampling().hsamp_.trcRange();
+    const LinScaler x2trc( vwrxrg.start, vwrtrcrg.start,
+			   vwrxrg.stop, vwrtrcrg.stop );
+    const double trcnrd = x2trc.scale( pos );
+    const int trcnr = mNINT32( trcnrd );
+    if ( !vwrtrcrg.includes(trcnr,false) )
 	return udfintpoint;
 
-    StepInterval<int> vwrtrcrg = vwr2d->getTrcKeyZSampling().hsamp_.trcRange();
-    const uiWorldPoint wperpixel =
-	vwr2d->viewwin()->viewer(0).getWorld2Ui().worldPerPixel();
-    const float eps  = mCast(float,wperpixel.x_) * sEPSPixWidth;
+    double mindiff = mUdf(double);
+    int minidx = -1;
     GeomIDSet datagids;
     getVWR2DDataGeomIDs( vwr2d, datagids );
     for ( int idx=0; idx<int2d->size(); idx++ )
@@ -980,13 +983,15 @@ Line2DInterSection::Point uiODViewer2DMgr::intersectingLineID(
 	if ( !datagids.isPresent(intpoint.otherid_) )
 	    continue;
 
-	const int inttrcidx = vwrtrcrg.getIndex( intpoint.mytrcnr_ );
-	const double inttrcpos = vwrxrg.atIndex( inttrcidx );
-	if ( mIsEqual(pos,inttrcpos,eps) )
-	    return intpoint;
+	const double diff = Math::Abs( (double)(intpoint.mytrcnr_-trcnrd) );
+	if ( diff > mindiff )
+	    continue;
+
+	mindiff = diff;
+	minidx = idx;
     }
 
-    return udfintpoint;
+    return minidx==-1 ? udfintpoint : int2d->getPoint( minidx );
 }
 
 
