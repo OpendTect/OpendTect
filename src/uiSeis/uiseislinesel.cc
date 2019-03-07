c7446e1c8e003757a34d70a0098afd8d1713b6aa
Nanne Hemstra <hemstra@users.noreply.github.com>
2019-03-07 15:54:36 -0600

    If only 1 line available, auto select it.

--- src/uiSeis/uiseislinesel.cc
+++ src/uiSeis/uiseislinesel.cc
@@ -250,6 +250,12 @@ uiSeis2DLineSel::uiSeis2DLineSel( uiParent* p, bool multisel )
 	    geomids_.add( geomids[ idxs[idx] ] );
 	}
     }
+
+    if ( lnms_.size()==1 )
+    {
+	setAll( true );
+	updateSummary();
+    }
 }
 
 
