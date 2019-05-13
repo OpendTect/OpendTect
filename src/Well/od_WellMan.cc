902ed6e0b40b0de8b1409192e19a7ac66bfcc85b
Bert <bert.bril@dgbes.com>
2019-05-14 00:01:08 +0200

    od_WellMan: Implemented some basic things

--- src/Well/od_WellMan.cc
+++ src/Well/od_WellMan.cc
@@ -1,42 +1,107 @@
 /*+
  * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  * AUTHOR   : Bert
- * DATE     : May 2019
+ * DATE     : Nov 2018
 -*/
 
 
-#include "commandlineparser.h"
-#include "moddepmgr.h"
-#include "od_ostream.h"
-#include "odver.h"
-#include "prog.h"
+#include "serverprogtool.h"
 #include "wellmanager.h"
+#include "commandlineparser.h"
+#include "keystrs.h"
+#include "odjson.h"
+#include "prog.h"
 
-static int protocolnr_ = 1;
-static const char* sVersionCmd = "version";
 
+static const int cProtocolNr = 1;
 
-static int printUsage()
+static const char* sListWellsCmd	= "list-wells";
+static const char* sListLogsCmd		= "list-logs";
+static const char* sReadLogCmd		= "read-log";
+
+class WellServerTool : public ServerProgTool
 {
-    od_cout() << "Needs some params" << od_endl;
-    return ExitProgram( 0 );
+public:
+
+		    WellServerTool(int,char**);
+
+    void	    listWells();
+    void	    listLogs(const DBKey&);
+    void	    readLog(const DBKey&,const char*);
+
+protected:
+
+    BufferString    getSpecificUsage() const override;
+
+};
+
+
+WellServerTool::WellServerTool( int argc, char** argv )
+    : ServerProgTool(argc,argv,"Well")
+{
+    initParsing( cProtocolNr );
+}
+
+
+void WellServerTool::listWells()
+{
+    DBKeySet wellids;
+    Well::MGR().getAll( wellids, false );
+    set( sKey::ID(mPlural), wellids );
+    respondInfo( true );
+}
+
+
+void WellServerTool::listLogs( const DBKey& wellid )
+{
+    BufferStringSet lognms;
+    Well::MGR().getLogNames( wellid, lognms );
+    set( sKey::Name(mPlural), lognms );
+    respondInfo( true );
+}
+
+
+void WellServerTool::readLog( const DBKey& wellid, const char* lognm )
+{
+    set( "TODO", "TODO" );
+    respondInfo( true );
+}
+
+
+BufferString WellServerTool::getSpecificUsage() const
+{
+    BufferString ret;
+    addToUsageStr( ret, sListWellsCmd, "" );
+    addToUsageStr( ret, sListLogsCmd, "well_id" );
+    addToUsageStr( ret, sReadLogCmd, "well_id log_name" );
+    return ret;
 }
 
 
 int main( int argc, char** argv )
 {
-    OD::SetRunContext( OD::BatchProgCtxt );
-    SetProgramArgs( argc, argv );
-    OD::ModDeps().ensureLoaded( "Well" );
-    CommandLineParser clp;
+    WellServerTool st( argc, argv );
+    auto& clp = st.clp();
 
-    if ( clp.nrArgs() < 1 )
-	return printUsage();
-    else if ( clp.hasKey( sVersionCmd ) )
+    if ( clp.hasKey(sListWellsCmd) )
+	st.listWells();
+
+    DBKey wellid;
+    if ( clp.hasKey(sListLogsCmd) )
     {
-	od_cout() << protocolnr_ << "@" << GetFullODVersion() << od_endl;
-	return ExitProgram( 0 );
+	clp.setKeyHasValue( sListLogsCmd, 1 );
+	clp.getDBKey( sListLogsCmd, wellid );
+	st.listLogs( wellid );
+    }
+    else if ( clp.hasKey(sReadLogCmd) )
+    {
+	clp.setKeyHasValue( sReadLogCmd, 2 );
+	clp.getDBKey( sReadLogCmd, wellid );
+	BufferString lognm;
+	clp.getVal( sReadLogCmd, lognm, false, 2 );
+	st.readLog( wellid, lognm );
     }
 
+    pFreeFnErrMsg( "Should not reach" );
     return ExitProgram( 0 );
 }
