10ba87abe1546278a1912bb0d47afd83be69fcd1
Khushnood Qadir <khushnood@dgbindia26.mumbai.dgbes.com>
2019-08-02 16:08:56 +0530

    od6.4: Added seisdatapackfromdps files to OD from dgb6.4
    Summary: seisdatapackfromdps was previously a member of fault tracker, but since it is used in SVF as well, it was decided it is best if we move it to od.
    
    Test Plan: It is a working file needed in SVF.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D12897

---
+++ src/Seis/seisdatapackfromdps.cc
@@ -0,0 +1,87 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Raman Singh
+ Date:		November 2017
+________________________________________________________________________
+
+-*/
+
+
+#include "seisdatapackfromdps.h"
+
+#include "arrayndimpl.h"
+#include "bufstringset.h"
+#include "datapointset.h"
+#include "seisdatapack.h"
+
+#define mZColID 9999
+
+SeisDataPackFromDPS::SeisDataPackFromDPS( const DataPointSet& dps,
+						RegularSeisDataPack& dp,
+					  const BufferStringSet& colnames )
+    : ParallelTask("Extracting Seismic Cube")
+    , dps_(dps),dp_(dp)
+{
+    for ( int idx=0; idx<colnames.size(); idx++ )
+    {
+	DataPointSet::ColID colid = dps_.indexOf( colnames.get(idx) );
+	if ( colid < 0 )
+	{
+	    if ( colnames.get(idx) == "Z" )
+		colid = mZColID;
+	    else
+		continue;
+	}
+
+	int compidx = dp_.getComponentIdx( colnames.get(idx) );
+	if ( compidx < 0 && !dp_.addComponent(colnames.get(idx)) )
+	    continue;
+
+	compidx = dp_.getComponentIdx( colnames.get(idx) );
+	selcols_ += colid;
+	selcomps_ += compidx;
+    }
+}
+
+
+od_int64 SeisDataPackFromDPS::nrIterations() const
+{
+    return dps_.size();
+}
+
+
+bool SeisDataPackFromDPS::doPrepare( int nrthreads )
+{
+    return true;
+}
+
+
+bool SeisDataPackFromDPS::doWork( od_int64 start, od_int64 stop, int threadidx )
+{
+    const StepInterval<float>& zsamp = dp_.getZRange();
+    const int nrsamps = zsamp.nrSteps() + 1;
+    for ( int compidx=0; compidx<selcomps_.size(); compidx++ )
+    {
+	Array3DImpl<float>& arr = dp_.data( selcomps_[compidx] );
+	float* data = arr.getData();
+	if ( !data )
+	    continue;
+
+	DataPointSet::ColID colid = selcols_[compidx];
+	for ( int idx=mCast(int,start); idx<=stop; idx++ )
+	{
+	    const TrcKey tkpos( dps_.binID(idx) );
+	    const int globidx = dp_.getGlobalIdx( tkpos );
+	    if ( globidx < 0 )
+		continue;
+
+	    const float zval = dps_.z( idx );
+	    data[globidx*nrsamps + zsamp.getIndex(zval)] =
+		colid == mZColID ? zval : dps_.value( colid, idx );
+	}
+    }
+
+    return true;
+}
