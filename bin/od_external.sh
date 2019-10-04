1cd7e53762b7b8679b5bb919a1520f37e04d0455
arnauddgbes <arnaud.huck@dgbes.com>
2019-10-04 12:47:06 +0200

    od6.4: tensorboard may not start with some locale settings

--- bin/od_external.sh
+++ bin/od_external.sh
@@ -4,6 +4,7 @@ if [ "$#" -lt 1 ]; then
     echo "Usage: command [ARGS]"
     exit 1	
 fi
+
 if [[ ! -z "${OD_INTERNAL_CLEANPATH}" ]]; then
     export PATH=${OD_INTERNAL_CLEANPATH}
 fi
@@ -14,4 +15,10 @@ else
 fi
 
 unset DTECT_APPL
+
+#Tensorboard won't start if LC_ALL is not set
+if [ -z $LC_ALL ] && [ ! -z ${LANG} ]; then
+    export LC_ALL=${LANG}
+fi
+
 $*
