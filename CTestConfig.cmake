0fdfe9df37fcb8dc510f02d61cfb641202d1d16b
Arnaud Huck <arnaud@dgb34.enschede.dgbes.com>
2019-10-17 09:57:26 +0200

    update to 6.7 branch name

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,11 +3,11 @@
 ## project to incorporate the testing dashboard.
 ## # The following are required to uses Dart and the Cdash dashboard
 
-set(CTEST_PROJECT_NAME "OpendTect")
+set(CTEST_PROJECT_NAME "OpendTect-6_5")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "intranet")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=OpendTect")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=OpendTect-6.5")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
