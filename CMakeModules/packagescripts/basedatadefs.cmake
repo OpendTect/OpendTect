c47df2a3e6d7652a325dbd648874962b433acd8c
arnauddgbes <arnaud.huck@dgbes.com>
2019-11-26 17:11:17 +0100

    od6.4: added data/Python folder to base package list

--- CMakeModules/packagescripts/basedatadefs.cmake
+++ CMakeModules/packagescripts/basedatadefs.cmake
@@ -7,7 +7,7 @@
 SET( LIBLIST omf Attribs BasicSurvey ColTabs MouseControls BatchPrograms
 	     BatchHosts_example FileFormats UnitsOfMeasure Properties odSettings
 	     EnvVars ShortCuts *.ico *.png *.html icons.Default
-	     RockPhysics ModDeps.od Strat
+	     RockPhysics ModDeps.od Strat Python
 	     prodlist.txt Vendors
 	     BatchHosts_example_for_nodes_of_a_linux_server.txt
 	     BatchHosts_example_for_nodes_of_a_windows_server.txt Scripts CRS )
