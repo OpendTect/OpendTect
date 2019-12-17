c28f2f96fa011a9219277f6b39fbfe6341151d06
Arjan Burggraaf <arjan.burggraaf@dgbes.com>
2019-12-17 15:15:29 +0100

    Fix to last commit

--- src/Network/backend-server/backendscripts/docsites.php
+++ src/Network/backend-server/backendscripts/docsites.php
@@ -43,7 +43,7 @@ if ( array_key_exists( $versionkey, $_GET ) &&
 	{
 	    $docurl = $docurl.'/6.6.0/doc/'.$module.'/Default.htm';
 	}
-	if ( $version>=640 )
+	else if ( $version>=640 )
 	{
 	    $docurl = $docurl.'/6.4.0/doc/'.$module.'/Default.htm';
 	}
