b886cf975f7f8fb8babbe7810eb34a3246e36d61
Prajjaval Singh <prajjaval.singh@dgbes.com>
2020-02-25 11:45:07 +0530

    od : Added a protection against user modified strings in file
    Summary: A file contained string which seems to be modified by user. This synthrock model was causing stackoverflow crash. OD-2527
    
    Test Plan: Not checked, synthrock crashed
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Subscribers: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D13194

--- include/General/elasticpropsel.h
+++ include/General/elasticpropsel.h
@@ -48,12 +48,20 @@ public:
     void			fillPar(IOPar&) const;
     bool			usePar(const IOPar&);
 
+    uiString			  errMsg() { return errmsg_; }
+
 protected:
 
     ElasticPropertyRef&		gt(ElasticFormula::Type) const;
     ElasticPropertyRef&		gt(int idx) const;
 
+    bool			checkForValidSelPropsDesc(
+					    const ElasticFormula&,
+					    BufferStringSet& faultynms,
+					    BufferStringSet& corrnms);
     void			mkEmpty();
+
+    uiString			errmsg_;
 
 };
 
