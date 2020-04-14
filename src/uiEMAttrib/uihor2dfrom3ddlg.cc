dd8996be2dda85d0b526e4c314f9288e43432bc6
Arnaud Huck <arnaud.huck@dgbes.com>
2020-04-14 08:31:29 +0200

    od: spelling mistake

--- src/uiEMAttrib/uihor2dfrom3ddlg.cc
+++ src/uiEMAttrib/uihor2dfrom3ddlg.cc
@@ -112,7 +112,7 @@ EM::Horizon2D* uiHor2DFrom3DDlg::create2dHorizon( const char* horizonnm )
 bool uiHor2DFrom3DDlg::checkFlds()
 {
     if ( !hor3dsel_->getObjSel()->commitInput() )
-	mErrRet( tr("Pease select a valid 3D Horizon. ") )
+	mErrRet( tr("Please select a valid 3D Horizon. ") )
     if ( !linesetinpsel_->nrSelected() )
 	mErrRet( uiStrings::phrPlsSelectAtLeastOne( uiStrings::s2DLine() ) )
     if ( !out2dfld_->getObjSel()->commitInput() )
