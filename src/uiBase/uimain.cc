b352901e2ed6f20a46fa248d1b929600d29f5c4c
Nageswara Rao <nageswara.rao@dgbes.com>
2020-04-17 19:48:36 +0530

    od6.6: Defined  Qt variable to manage UI in high DPI displays.
    Summary: Qt::AA_EnableHighDpiScaling iattribute s defined to manage high resolution displays.
    
    Test Plan: Defined environment variable QT_AUTO_SCREEN_SCALE_FACTOR=1 on  high resolution windows laptop. OpendTect installer displayed without any issues.
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Subscribers: raman, lloyd, wayne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D13258

--- src/uiBase/uimain.cc
+++ src/uiBase/uimain.cc
@@ -359,6 +359,7 @@ static void qtMessageOutput( QtMsgType type, const char* msg )
 void uiMain::preInit()
 {
     QApplication::setDesktopSettingsAware( true );
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #if QT_VERSION >= 0x050400
     // Attributes added with Qt5.3 and Qt5.4
     if ( QApplication::testAttribute(Qt::AA_UseDesktopOpenGL) ||
