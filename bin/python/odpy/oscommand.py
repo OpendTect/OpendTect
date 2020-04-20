d30c3546d67ba77e95b0f0e0d188d0892d712a00
Arnaud Huck <arnaud.huck@dgbes.com>
2020-04-20 17:49:44 +0200

    odpy: added some documentation

--- bin/python/odpy/oscommand.py
+++ bin/python/odpy/oscommand.py
@@ -1,10 +1,10 @@
-#
-# (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
-# AUTHOR   : A. Huck
-# DATE     : Apr 2019
-#
-# tools for managing OS commands
-#
+"""Tools for managing OS commands
+
+(C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+AUTHOR   : A. Huck
+DATE     : Apr 2019
+
+"""
 
 import sys
 import os
@@ -16,11 +16,74 @@ import json
 from odpy.common import isWin, getExecPlfDir, get_log_stream, get_std_stream, std_msg, sTimeUnitString
 
 def getODCommand(execnm,args=None):
+  """OpendTect command
+
+  For a given OpendTect executable, get its full command line
+
+  Parameters
+  ----------
+  execnm : list
+           [Executable name]
+  arg : dict, optional
+        Dictionary with the members 'dtectdata' and 'survey' as 
+        single element lists, and/or 'dtectexec' (see odpy.getODSoftwareDir)
+
+  Returns
+  -------
+  list
+    OpendTect command line
+
+  Notes
+  -----
+  The two standard arguments providing the current survey location
+  will be added to the command line arguments.
+
+  Examples
+  --------
+  >>>args = {
+    'dtectdata': ['D:\\ODData'],
+    'survey': ['F3_Demo']
+  }
+  >>>getODCommand()
+  ['C:\\Program Files\\OpendTect\\6.6.0\\bin\win64\Release\\od_process_attrib',
+  '--dataroot',
+  'D:\\ODData',
+  '--survey',
+  'F3_Demo']
+
+  """
+
   cmd = list()
   cmd.append( os.path.join(getExecPlfDir(args),execnm) )
   return appendDtectArgs( cmd, args )
 
 def appendDtectArgs( cmd, args=None ):
+  """Append OpendTect arguments
+
+  Parameters
+  ----------
+  cmd : list
+        List to which the returned elements will be added
+  arg : dict, optional
+        Dictionary with the members 'dtectdata' and 'survey' as 
+        single element lists, and/or 'dtectexec' (see odpy.getODSoftwareDir)
+
+  Returns
+  -------
+  list
+    Input list with the added data root and survey directory name if provided
+
+  Examples
+  --------
+  >>>args = {
+    'dtectdata': ['D:\\ODData'],
+    'survey': ['F3_Demo']
+  }
+  >>>appendDtectArgs( args )
+  ['--dataroot', 'D:\\ODData', '--survey', 'F3_Demo']
+
+  """
+
   if args == None:
     return cmd
   if 'dtectdata' in args:
@@ -32,9 +95,39 @@ def appendDtectArgs( cmd, args=None ):
   return cmd
 
 def getPythonExecNm():
+  """Python executable name
+
+  shortcut to sys.executable
+  
+  """
+
   return sys.executable
 
 def getPythonCommand(scriptfile,posargs=None,dict=None,args=None):
+  """Python command line
+
+  Parameters
+  ----------
+  scriptfile : str
+               Python script or executable
+  posargs : list, optional
+            Positional arguments for that script (default is None)
+  dict : str, optional
+         Value for the argument with key "--dict":
+         String from which a json object can be parsed (default is None)
+  args : dict
+        Dictionary with the members 'dtectdata' and 'survey' as 
+        single element lists, and/or 'dtectexec' (see odpy.getODSoftwareDir)
+        The dictionary may also contain the members 'proclog' and 'syslog' as 
+        strings, each being the full path to a log file.
+
+  Returns
+  -------
+  list
+    Fully formed command line to be executed by the python executable
+
+  """
+
   cmd = list()
   cmd.append( getPythonExecNm() )
   cmd.append( scriptfile )
@@ -55,26 +148,118 @@ def getPythonCommand(scriptfile,posargs=None,dict=None,args=None):
   return cmd
 
 def execCommand( cmd, background=False ):
+  """Command execution
+
+  Launches the execution of a command with its arguments.
+
+  Parameters
+  ----------
+  cmd : list
+        command to be executed with its arguments.
+  background : bool, optional
+                The command is forked if True (default is False).
+
+  Returns
+  -------
+  str
+    The stdout print of the subprocess.CompletedProcess (default)
+  or
+  A psutil.Popen object (background=True)
+
+  Notes
+  -----
+  This is an interface to the subprocess module.
+
+  """
+
   if background:
     return startDetached( cmd )
   else:
     return startAndWait( cmd )
 
 def isRunning( proc ):
+  """
+
+  Parameters
+  ----------
+  psutil.Popen
+    Process to be checked
+
+  Returns
+  -------
+  bool
+    True if the process is running.
+
+  """
+
   if proc == None:
     return
   proc.poll()
   return proc.is_running()
 
 def pauseProcess( proc ):
+  """Pause a running process
+
+  Parameters
+  ----------
+  psutil.Popen
+    Process to be checked
+
+  Returns
+  -------
+  Set and return subprocess.Popen.returncode
+
+  """
+
   if isinstance(proc,psutil.Popen):
     return proc.suspend()
 
 def resumeProcess( proc ):
+  """Resume a paused process
+
+  Parameters
+  ----------
+  psutil.Popen
+    Process to be checked
+
+  Returns
+  -------
+  Set and return subprocess.Popen.returncode
+
+  """
+
   if isinstance(proc,psutil.Popen):
     return proc.resume()
 
 def printProcessTime(procnm,isstart,print_fn=std_msg,withprocline=True):
+  """Print processing timestamp
+
+  Parameters
+  ----------
+  procnm : str
+            Process name
+  isstart : bool
+            Status: Start or end of this process.
+  print_fn : function, optional
+              function to be used for the printing of the timestamp
+              (default is odpy.std_msg, which default to stderr)
+  withprocline : bool, optional
+                 Print the timestamp preceeded by a label string (default is True)
+
+  Examples
+  --------
+  >>>printProcessTime( 'od_process', True )
+  Process: 'od_process'
+  Started: Mon 20 Apr 2020, 17:23:32
+
+  >>>printProcessTime( 'od_process', False )
+  Process: 'od_process'
+  Finished: Mon 20 Apr 2020, 17:23:37
+
+  >>>printProcessTime( 'od_process', False, withprocline=False )
+  Finished: Mon 20 Apr 2020, 17:23:37
+
+  """
   if withprocline:
     procstr = 'Process: \'' + procnm + '\''
     print_fn( procstr )
@@ -85,13 +270,45 @@ def printProcessTime(procnm,isstart,print_fn=std_msg,withprocline=True):
   print_fn( retstr, sTimeUnitString() )
 
 def kill( proc ):
+  """Terminate a process
+
+  Parameters
+  ----------
+  psutil.Popen
+    Object view of the process
+
+  Returns
+  -------
+  Set and return subprocess.Popen.returncode
+
+  """
+
   with proc.oneshot():
     proc.resume()
     proc.terminate()
     proc.wait(3)
 
-# INTERNAL, you should not need to use those:
 def startAndWait( cmd ):
+  """Run a command with subprocess
+
+  Parameters
+  ----------
+  cmd : list
+        command to be executed with its arguments.
+
+  Returns
+  -------
+  str
+    stdout from the executed and retrieved subprocess.CompletedProcess
+
+  Notes
+  -----
+  The command is executed by the run function of the subprocess module.
+  stdout is captured with a PIPE from the child process.
+  stderr is captured and forwarded to odpy.syslog_logger.
+
+  """
+
   try:
     completedproc = subprocess.run( cmd, check=True, stdout=subprocess.PIPE, \
                                     stderr=get_std_stream() ).stdout
@@ -101,16 +318,37 @@ def startAndWait( cmd ):
   return completedproc
 
 def startDetached( cmd ):
- try:
-   if isWin():
-     runningproc = psutil.Popen( cmd, stdout=get_log_stream(), \
-                                      stderr=get_std_stream() )
-   else:
-     runningproc = psutil.Popen( cmd, start_new_session=True, \
+  """Run a command in the background with psutil
+
+  Parameters
+  ----------
+  cmd : list
+        command to be executed with its arguments.
+
+  Returns
+  -------
+  psutil.Popen
+    Object view of the process
+
+  Notes
+  -----
+  The command is executed by the Popen function of the psutil module.
+  Since the command is run as a daemon, it does not block the execution of the script.
+  stdout is captured and forwarded to odpy.proclog_logger.
+  stderr is captured and forwarded to odpy.syslog_logger.
+
+  """
+
+  try:
+    if isWin():
+      runningproc = psutil.Popen( cmd, stdout=get_log_stream(), \
+                                       stderr=get_std_stream() )
+    else:
+      runningproc = psutil.Popen( cmd, start_new_session=True, \
                                      stdout=get_log_stream(), \
                                      stderr=get_std_stream() )
- except subprocess.CalledProcessError as err:
-   std_msg( 'Failed: ', err )
-   raise
- return runningproc
+  except subprocess.CalledProcessError as err:
+    std_msg( 'Failed: ', err )
+    raise
+  return runningproc
 
