a552a9e05f32b04cbdbe33f4a076e1ad71bc22ee
Arnaud Huck <arnaud.huck@dgbes.com>
2020-05-09 15:12:57 +0200

    Added cmake support for HDF5 >= 1.10.6 [devel]
    Also supporting builds with contain only shared libraries,
    since this new option is introduced in v1.10.6

--- plugins/ODHDF5/PreModInit.cmake
+++ plugins/ODHDF5/PreModInit.cmake
@@ -4,10 +4,14 @@
 # 	Feb 2018	Bert
 #_______________________________________________________________________________
 
-list( APPEND OD_MODULE_INCLUDESYSPATH ${HDF5_INCLUDE_DIRS} )
+list( APPEND OD_MODULE_INCLUDESYSPATH ${HDF5_INCLUDE_DIR} )
 list( APPEND OD_MODULE_EXTERNAL_LIBS ${HDF5_LIBRARIES} )
 
 if ( WIN32 )
   GETHDF5COMPDEF()
   add_definitions( -D${HDF5_COMPILEDEF} )
+  if ( "${HDF5_VERSION}" STREQUAL "1.12.0" )
+    set ( CMAKE_CXX_FLAGS "/wd4268 ${CMAKE_CXX_FLAGS}" )
+  endif()
 endif()
+
