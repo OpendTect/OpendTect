2633fc1e3df95c70b2dff9cf7efab75a271e719d
Arjan Burggraaf <arjan.burggraaf@dgbes.com>
2020-06-17 09:06:58 +0200

    Ported from master: python testscript
    Summary: Added new pythoncomm.cc file to od6.6.
    
    Test Plan: Tested on Linux by running run_test.csh with the needed arguments. Working as expected.
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Differential Revision: https://phabricator.dgbes.com:8443/D13328

---
+++ src/Basic/tests/pythoncomm.cc
@@ -0,0 +1,85 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : K. Tingdahl
+ * DATE     : July 2012
+ * FUNCTION :
+-*/
+
+
+#include "testprog.h"
+
+#include "dirlist.h"
+#include "genc.h"
+#include "file.h"
+#include "filepath.h"
+#include "od_iostream.h"
+#include "oddirs.h"
+#include "oscommand.h"
+#include "pythonaccess.h"
+
+#include <iostream>
+#include "settingsaccess.h"
+
+#define mTestDirNm "Test Dir"
+
+BufferString createTestDir()
+{
+    const FilePath fp( FilePath::getTempDir(),
+			FilePath::getTempFileName( mTestDirNm,"" ) );
+    File::createDir( fp.fullPath() );
+    const FilePath filefp( fp.fullPath(),
+			    FilePath::getTempFileName("python","txt") );
+    od_ostream strm( filefp.fullPath() );
+    strm << "Testing deletion via python command line";
+    strm.close();
+
+    return fp.fullPath();
+}
+
+bool testRemoveDir( const BufferString& path, bool expectedres )
+{
+    bool ret = OD::pythonRemoveDir( path, true ).isOK();
+    if ( ret )
+    {
+	const DirList dl( FilePath::getTempDir(), DirList::DirsOnly );
+
+	for ( int idx=0; idx<dl.size(); idx++ )
+	{
+	    FilePath fp = dl.fullPath(idx);
+
+	    const BufferString dirnm = fp.dir();
+
+	    if ( dirnm.isEqual(mTestDirNm) )
+		ret = false;
+	}
+    }
+
+    const BufferString desc = expectedres ? "Writable folder deleted"
+					  : "Read-only folder not deleted";
+    mRunStandardTest( ret == expectedres, desc )
+
+    return ret;
+}
+
+int main( int argc, char** argv )
+{
+    mInitTestProg();
+
+    if ( !OD::PythA().isUsable().isOK() )
+    {
+	if ( !quiet )
+	    od_cout() << "Python link is not usable" << od_endl;
+	return 1;
+    }
+
+    const BufferString path = createTestDir();
+    File::makeReadOnly( path, true );
+    if ( testRemoveDir(path,false) )
+	return 1;
+
+    File::makeWritable( path, true, true );
+    if ( !testRemoveDir(path,true) )
+	return 1;
+
+    return 0;
+}
