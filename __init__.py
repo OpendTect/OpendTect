bb30d7bf44a07fa528d5f71f51f8d7b8026571ff
Arjan Burggraaf <arjan.burggraaf@dgbes.com>
2020-06-18 09:03:32 +0200

    Updated odpy docstrings for API documentation
    Summary: Updated odpy docstring for API documentation following new Python Docstrings Policy document.
    
    Test Plan: Checked for readability of docstrings in code and in the new odpy API documentation
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Subscribers: wayne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D13329

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """Generic Python tools for OpendTect
 
-(C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
-AUTHOR   : A. Huck
-DATE     : Aug. 2018
+Copyright (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+  * AUTHOR   : A. Huck
+  * DATE     : Aug. 2018
 
 """
