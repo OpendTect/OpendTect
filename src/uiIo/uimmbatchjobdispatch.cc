fc5d19b4ecb0d02755da4600d268768d4f7aa94a
Arnaud Huck <arnaud.huck@dgbes.com>
2020-06-30 12:32:57 +0200

    Fixed incorrect DTECT_DATA for SeisMMBatch executable

--- src/uiIo/uimmbatchjobdispatch.cc
+++ src/uiIo/uimmbatchjobdispatch.cc
@@ -37,6 +37,7 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "ioman.h"
 #include "survinfo.h"
 #include "plugins.h"
+#include "plfdefs.h"
 
 
 
@@ -64,7 +65,7 @@ bool uiMMBatchJobDispatcher::initMMProgram( int argc, char** argv,
     if ( jobpars.isEmpty() )
     {
 	od_cout() << argv[0] << ": Invalid parameter file"
-                  << parfnm << od_endl;
+		  << parfnm << od_endl;
 	return false;
     }
     strm.close();
@@ -72,9 +73,14 @@ bool uiMMBatchJobDispatcher::initMMProgram( int argc, char** argv,
     if ( bgadd )
 	ForkProcess();
 
-    const char* res = jobpars.find( sKey::Survey() );
-    if ( res && *res && SI().getDirName() != res )
-	IOMan::setSurvey( res );
+    const char* res = jobpars.find( sKey::DataRoot() );
+    if ( res && *res && SI().getDataDirName() != res )
+	SetEnvVar( __iswin__ ? "DTECT_WINDATA" : "DTECT_DATA", res );
+
+    res = jobpars.find( sKey::Survey() );
+    if ( res && *res && SI().getDirName() != res && !IOMan::setSurvey(res) )
+	return false;
+
     PIM().loadAuto( false );
     jobpars.set( sKey::FileName(), parfnm );
 
