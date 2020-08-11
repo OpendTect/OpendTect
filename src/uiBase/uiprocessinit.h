45e9bb94b8ccb15bdfdd471cfe837fa032dbc2e6
Nageswara Rao <nageswara.rao@dgbes.com>
2020-08-11 12:12:38 +0530

    Ported this file from master to fix compilation errors on MAC  platform.

---
+++ src/uiBase/uiprocessinit.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Kris / Bert
+ Date:          September 2013 / August 2018
+________________________________________________________________________
+
+-*/
+
+#include "uibasemod.h"
+#include "gendefs.h"
+
+namespace OD
+{
+    mGlobal(uiBase) void uiInitProcessStatus(); // does nothing except on Mac
+}
