16f23568cece3b84821d923259e9aecc0ebe6a07
Nanne Hemstra <nanne.hemstra@dgbes.com>
2020-08-30 22:55:08 -0500

    Fixed a few errors in well log merge workflow.

--- include/uiWell/uiwelllogmerge.h
+++ include/uiWell/uiwelllogmerge.h
@@ -29,56 +29,52 @@ mExpClass(uiWell) uiWellLogMerger : public uiDialog
 { mODTextTranslationClass(uiWellLogMerger);
 public:
 
-		uiWellLogMerger(uiParent*,
-				const TypeSet<MultiID>&,
-				const BufferStringSet* chosenlognms=nullptr);
-		~uiWellLogMerger();
+				uiWellLogMerger(uiParent*,
+				    const TypeSet<MultiID>&,
+				    const BufferStringSet* lognms=nullptr);
+				~uiWellLogMerger();
 
     bool			haveNewLogs() const	{ return havenew_; }
 
-    void			setOutputLogName(const char* nm);
+    void			setOutputLogName(const char*);
     const char*			getOutputLogName() const;
 
 private:
 
-    uiGroup*			mergeparamgrp_;
-    uiGroup*			mergedlogdispgrp_;
-    uiGenInput*			logfld_;
+    uiGenInput*			logtypefld_ = nullptr;
     uiGenInput*			overlapfld_;
-    uiGenInput*			extrapolatefld_;
-    uiGenInput*			intrapolatefld_;
-    uiListBox*			loglistfld_;
+    uiCheckBox*			extrapolatefld_;
+    uiCheckBox*			intrapolatefld_;
+    uiListBox*			loglistfld_ = nullptr;
     uiListBox*			selectedlogsfld_;
-    uiGroup*			selbuttons_;
     uiGroup*			movebuttons_;
-    uiGroup*			changelogdispbut_;
-    uiToolButton*		toselect_;
-    uiToolButton*		fromselect_;
+    uiToolButton*		toselect_ = nullptr;
+    uiToolButton*		fromselect_ = nullptr;
     uiToolButton*		moveupward_;
     uiToolButton*		movedownward_;
-    uiToolButton*		nextlog_;
-    uiToolButton*		prevlog_;
+    uiToolButton*		nextlog_ = nullptr;
+    uiToolButton*		prevlog_ = nullptr;
     uiGenInput*			srfld_;
-    uiGenInput*			nmfld_;
+    uiGenInput*			nmfld_ = nullptr;
     uiUnitSel*			outunfld_;
     uiWellLogDisplay*		logdisp_;
 
     Well::LogMerger::OverlapAction	action_ = Well::LogMerger::UseAverage;
 
     const TypeSet<MultiID>	wellids_;
-    ObjectSet<Well::Log>	outlgs_;
+    ObjectSet<Well::Log>	outlogs_;
     BufferStringSet		chosenlognms_;
     float			zsampling_;
     bool			havenew_;
-    bool			needselectlogfld_ = false;
 
-    void			createMergeParaGrp();
-    void			createMergedLogDispGrp();
+    void			finalizeCB(CallBacker*);
+    uiGroup*			createParGrp(bool);
+    uiGroup*			createLogDispGrp();
 
     void			updateLogViewer();
-    void			merge();
-    bool			writeNewLog();
-    void			convertMergedLog();
+    bool			merge();
+    bool			write();
+    bool			convert();
 
     void			logSetCB(CallBacker*);
     void			overlapCB(CallBacker*);
