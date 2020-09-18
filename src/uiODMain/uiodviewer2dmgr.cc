77a692c2cb7aee78a357a152a22ce739ae93fe15
Nanne Hemstra <nanne.hemstra@dgbes.com>
2020-09-18 11:43:35 -0500

    Possible fix for crash when opening 2D viewer
    Summary:
    Added few extra null pointer checks
    JIRA: OD-2523
    
    Test Plan: Could not reproduce crash, but this might solve it.
    
    Reviewers: nageswara
    
    Reviewed By: nageswara
    
    Differential Revision: https://phabricator.dgbes.com:8443/D13472

--- src/uiODMain/uiodviewer2dmgr.cc
+++ src/uiODMain/uiodviewer2dmgr.cc
@@ -703,7 +703,7 @@ void uiODViewer2DMgr::create2DViewer( const uiODViewer2D& curvwr2d,
     else if ( curvwr.isVisible(false) )
 	vwr2d->setUpView( vwr2d->createDataPack(false), false );
 
-    if ( vwr2d->viewControl() )
+    if ( vwr2d->viewControl() && control )
 	vwr2d->viewControl()->setEditMode( control->isEditModeOn() );
 
     for ( int idx=0; idx<vwr2d->viewwin()->nrViewers(); idx++ )
@@ -716,7 +716,9 @@ void uiODViewer2DMgr::create2DViewer( const uiODViewer2D& curvwr2d,
     if ( geom2dids_.size() > 0 )
 	vwr2d->viewwin()->viewer().setSeisGeomidsToViewer( geom2dids_ );
     attachNotifiersAndSetAuxData( vwr2d );
-    vwr2d->viewControl()->setEditMode( curvwr2d.viewControl()->isEditModeOn() );
+
+    if ( vwr2d->viewControl() && control )
+	vwr2d->viewControl()->setEditMode( control->isEditModeOn() );
 }
 
 
