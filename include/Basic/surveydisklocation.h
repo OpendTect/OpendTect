2712ffbba882f9f791aa9cf295c4a2cf4ea5bebe
Nanne Hemstra <nanne.hemstra@dgbes.com>
2020-10-08 16:01:43 -0500

    Introduce SurveyDiskLocation [devel]

---
+++ include/Basic/surveydisklocation.h
@@ -0,0 +1,78 @@
+#pragma once
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Bert
+ Date:		July 2018
+________________________________________________________________________
+
+-*/
+
+#include "bufstring.h"
+
+class BufferStringSet;
+class FilePath;
+class SurveyInfo;
+namespace Survey { class GeometryManager; }
+
+
+/*!\brief Survey location on disk.
+
+  The survey itself is the only OpendTect database 'object' that has no ID by
+  itself. Thus, if you have to work accross surveys, this object has to be the
+  ID of a survey. To make things like 'relocate' easier, the path is split in a
+  base path (usualy the 'Data Root') and the survey directory name. If any
+  of the two is empty, it will be taken from surrent survey.
+
+  When working accross surveys, you can get the corresponding SurveyInfo and
+  GeometryManager, cached for you.
+
+ */
+
+mExpClass(Basic) SurveyDiskLocation
+{
+public:
+			SurveyDiskLocation()	{} //!< current
+			SurveyDiskLocation(const char* dirnm,const char* bp=0);
+			SurveyDiskLocation(const FilePath& fulldir);
+    bool		operator ==(const SurveyDiskLocation&) const;
+    bool		operator !=( const SurveyDiskLocation& oth ) const
+			{ return !(*this == oth); }
+
+    BufferString	basePath() const;
+    void		setBasePath( const char* bp )	{ basepath_ = bp; }
+    BufferString	dirName() const;
+    void		setDirName( const char* dn )	{ dirname_ = dn; }
+
+    void		set(const char* fullpath);
+    void		set(const FilePath&);
+    BufferString	fullPath() const;
+    BufferString	surveyName() const;
+    BufferString	fullPathFor(const char* fnm) const;
+
+    bool		isCurrentSurvey() const;
+    void		setToCurrentSurvey(bool hard=true);
+
+    const SurveyInfo&	surveyInfo() const;
+    const Survey::GeometryManager&
+			geometryManager() const;
+
+    bool		isEmpty() const;    //!< current survey, soft path
+    void		setEmpty();	    //!< current survey, soft path
+    bool		exists() const;
+
+    void		fillPar(IOPar&,bool force=false) const;
+    bool		usePar(const IOPar&);
+
+    static const SurveyDiskLocation&	currentSurvey();
+    static void		listSurveys(BufferStringSet&,const char* basepath=0);
+			//!< returns subdirectory names (not full paths)
+
+protected:
+
+    BufferString	basepath_;	//!< The 'data root'
+    BufferString	dirname_;	//!< The subdirectory name
+
+};
---
+++ src/Basic/surveydisklocation.cc
@@ -0,0 +1,260 @@
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:        Bert
+ Date:          July 2018
+________________________________________________________________________
+
+-*/
+
+#include "surveydisklocation.h"
+
+#include "ascstream.h"
+#include "dirlist.h"
+#include "filepath.h"
+#include "iopar.h"
+#include "keystrs.h"
+#include "oddirs.h"
+#include "od_istream.h"
+#include "survgeom.h"
+#include "survinfo.h"
+
+const SurveyDiskLocation& SurveyDiskLocation::currentSurvey()
+{
+    static SurveyDiskLocation empty;
+    return empty;
+}
+
+
+SurveyDiskLocation::SurveyDiskLocation( const char* dirnm, const char* bp )
+    : basepath_(bp && *bp ? bp : GetBaseDataDir())
+    , dirname_(dirnm)
+{
+}
+
+
+SurveyDiskLocation::SurveyDiskLocation( const FilePath& fp )
+{
+    set( fp );
+}
+
+
+bool SurveyDiskLocation::operator ==( const SurveyDiskLocation& oth ) const
+{
+    if ( dirName() != oth.dirName() )
+	return false;
+
+    if ( basePath() == oth.basePath() )
+	return true;
+
+    FilePath fp( basePath() ), ofp( oth.basePath() );
+    fp.makeCanonical(); ofp.makeCanonical();
+    return fp == ofp;
+}
+
+
+BufferString SurveyDiskLocation::basePath() const
+{
+    return basepath_.isEmpty() ? SI().diskLocation().basepath_ : basepath_;
+}
+
+
+BufferString SurveyDiskLocation::dirName() const
+{
+    return dirname_.isEmpty() ? SI().diskLocation().dirname_ : dirname_;
+}
+
+
+void SurveyDiskLocation::set( const char* fullpath )
+{
+    if ( !fullpath || !*fullpath )
+	setEmpty();
+    else
+	set( FilePath(fullpath) );
+}
+
+
+void SurveyDiskLocation::set( const FilePath& fp )
+{
+    basepath_ = fp.pathOnly();
+    dirname_ = fp.fileName();
+}
+
+
+bool SurveyDiskLocation::isCurrentSurvey() const
+{
+    return isEmpty() || *this == currentSurvey();
+}
+
+
+bool SurveyDiskLocation::isEmpty() const
+{
+    return dirname_.isEmpty() && basepath_.isEmpty();
+}
+
+
+void SurveyDiskLocation::setEmpty()
+{
+    dirname_.setEmpty();
+    basepath_.setEmpty();
+}
+
+
+bool SurveyDiskLocation::exists() const
+{
+    return File::isDirectory( fullPath() );
+}
+
+
+void SurveyDiskLocation::setToCurrentSurvey( bool hard )
+{
+    if ( !hard )
+	setEmpty();
+    else
+    {
+	basepath_ = currentSurvey().basePath();
+	dirname_ = currentSurvey().dirName();
+    }
+}
+
+
+void SurveyDiskLocation::listSurveys( BufferStringSet& dirnms, const char* bp )
+{
+    const BufferString basepath( bp && *bp
+				 ? bp : currentSurvey().basePath().str() );
+    Survey::getDirectoryNames( dirnms, false, basepath );
+}
+
+
+void SurveyDiskLocation::fillPar( IOPar& iop, bool force ) const
+{
+    if ( isCurrentSurvey() )
+    {
+	if ( force )
+	    iop.set( sKey::Survey(), fullPath() );
+	else
+	    iop.set( sKey::Survey(), BufferString::empty() );
+	return;
+    }
+
+    const SurveyDiskLocation& cursurvsdl = currentSurvey();
+    if ( basePath() == cursurvsdl.basePath() )
+	iop.set( sKey::Survey(), dirName() );
+    else
+	iop.set( sKey::Survey(), fullPath() );
+}
+
+
+bool SurveyDiskLocation::usePar( const IOPar& iop )
+{
+    BufferString survdir;
+    if ( !iop.get(sKey::Survey(),survdir) )
+	return false;
+
+    if ( survdir.isEmpty() )
+    {
+	setToCurrentSurvey();
+	return true;
+    }
+
+    FilePath fp( survdir );
+    const SurveyDiskLocation sdl( fp );
+    if ( sdl.exists() )
+	{ *this = sdl; return true; }
+    else
+    {
+	const SurveyDiskLocation& cursdl = currentSurvey();
+	fp.set( cursdl.basePath() ).add( survdir );
+	const SurveyDiskLocation relsdl( fp );
+	if ( relsdl.exists() )
+	    { *this = relsdl; return true; }
+	else
+	{
+	    fp.set( cursdl.basePath() ).add( sdl.dirName() );
+	    const SurveyDiskLocation rebasedsdl( fp );
+	    if ( rebasedsdl.exists() )
+		{ *this = rebasedsdl; return true; }
+	}
+    }
+
+    return false;
+}
+
+
+BufferString SurveyDiskLocation::fullPath() const
+{
+    return FilePath( basePath(), dirName() ).fullPath();
+}
+
+
+BufferString SurveyDiskLocation::fullPathFor( const char* fnm ) const
+{
+    if ( !fnm || !*fnm )
+	return fullPath();
+
+    return FilePath( basePath(), dirName(), fnm ).fullPath();
+}
+
+
+BufferString SurveyDiskLocation::surveyName() const
+{
+    BufferString ret( surveyInfo().getName() );
+    if ( ret.isEmpty() )
+	ret = dirname_;
+    return ret;
+}
+
+
+const SurveyInfo& SurveyDiskLocation::surveyInfo() const
+{
+    if ( isCurrentSurvey() )
+	return SI();
+
+    static ObjectSet<SurveyInfo> infos_;
+    static TypeSet<od_int64> timestamps_;
+    static Threads::Lock infolock;
+
+    Threads::Locker locker( infolock );
+    const BufferString fullpath = fullPath();
+    const FilePath fp( fullpath, SurveyInfo::sKeySetupFileName() );
+    const BufferString survinfofnm = fp.fullPath();
+
+    SurveyInfo* newinfo = 0;
+    od_int64 timestamp = mUdf( od_int64 );
+    if ( File::exists(survinfofnm) )
+    {
+	timestamp = File::getTimeInSeconds( survinfofnm );
+	for ( int idx=0; idx<infos_.size(); idx++ )
+	{
+	    if ( infos_[idx]->diskLocation() == *this )
+	    {
+		if ( timestamps_[idx] <= timestamp )
+		    return *infos_[idx];
+
+		delete infos_.removeSingle( idx );
+		break;
+	    }
+	}
+
+	newinfo = SurveyInfo::read( fullpath );
+    }
+
+    if ( !newinfo )
+	return SurveyInfo::empty();
+
+    infos_ += newinfo;
+    timestamps_ += timestamp;
+    return *newinfo;
+}
+
+
+const Survey::GeometryManager& SurveyDiskLocation::geometryManager() const
+{
+    if ( isCurrentSurvey() )
+	return Survey::GM();
+
+    //TODO implement this cache
+    static Survey::GeometryManager ret;
+    return ret;
+}
