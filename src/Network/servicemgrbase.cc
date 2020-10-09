26591adb0658bc2b23d31c19260ff92f28ab3742
Arnaud Huck <arnaud.huck@dgbes.com>
2020-10-09 10:09:30 +0200

    Fixed applying of request for python environment change [devel]

--- src/Network/servicemgrbase.cc
+++ src/Network/servicemgrbase.cc
@@ -453,18 +453,8 @@ bool ServiceMgrBase::canParseAction( const char* action, uiRetVal& uirv )
 bool ServiceMgrBase::canParseRequest( const OD::JSON::Object& request,
 				    uiRetVal& uirv )
 {
-    if ( request.isPresent(sKeySurveyChangeEv()) )
-    {
-	uirv = survChangedReq( *request.getObject(sKeySurveyChangeEv()) );
-	return true;
-    }
-    if ( request.isPresent(sKeyPyEnvChangeEv()) )
-    {
-	uirv = pythEnvChangedReq( *request.getObject(sKeyPyEnvChangeEv()) );
-	return true;
-    }
-
-    return false;
+    return request.isPresent( sKeySurveyChangeEv() ) ||
+        request.isPresent( sKeyPyEnvChangeEv() );
 }
 
 
@@ -476,8 +466,17 @@ uiRetVal ServiceMgrBase::doHandleAction( const char* action )
 }
 
 
-uiRetVal ServiceMgrBase::doHandleRequest( const OD::JSON::Object& )
+uiRetVal ServiceMgrBase::doHandleRequest( const OD::JSON::Object& request )
 {
+    if ( request.isPresent(sKeySurveyChangeEv()) )
+    {
+        return survChangedReq( *request.getObject( sKeySurveyChangeEv() ) );
+    }
+    if ( request.isPresent(sKeyPyEnvChangeEv()) )
+    {
+        return pythEnvChangedReq( *request.getObject( sKeyPyEnvChangeEv() ) );
+    }
+
     return uiRetVal::OK();
 }
 
@@ -491,8 +490,7 @@ uiRetVal ServiceMgrBase::pythEnvChangedReq( const OD::JSON::Object& reqobj )
 	if ( pytha.activatefp_ )
 	    prevactivatefp = *pytha.activatefp_;
 	const BufferString prevvirtenvnm( pytha.virtenvnm_ );
-	const FilePath activatefp(
-				reqobj.getStringValue(sKey::FileName()) );
+	const FilePath activatefp = reqobj.getFilePath( sKey::FileName() );
 	const BufferString virtenvnm( reqobj.getStringValue(sKey::Name() ) );
 	if ( prevactivatefp != activatefp || prevvirtenvnm != virtenvnm )
 	{
