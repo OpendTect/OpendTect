4c5b33528d4369c130e71066bc0d0ac1d9fc0470
Nageswara Rao <nageswara.rao@dgbes.com>
2020-11-05 15:52:31 +0530

    Fixed launching 'HostID information' window on MAC platform.

--- src/General/generalinfo.cc
+++ src/General/generalinfo.cc
@@ -27,7 +27,13 @@ bool	getHostIDs( BufferStringSet& hostids,
     hostids.setEmpty();
     errmsg.setEmpty();
 
+#ifndef __mac__
     File::Path lmutilfp( GetSoftwareDir(false), "bin", GetPlfSubDir() );
+#else
+    File::Path lmutilfp( GetSoftwareDir(false), "Resources", "bin",
+	    	         GetPlfSubDir() );
+#endif
+
     lmutilfp.add( "lm.dgb" ).add( "lmutil" );
 
 #ifdef __win__
