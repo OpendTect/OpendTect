2a42f58e8f2ce3ab681302c9fe029f7e8b4f43a3
Arnaud Huck <arnaud.huck@dgbes.com>
2020-11-20 18:16:53 +0100

    updated repository name for osgGeo [devel]

--- external/Externals.cmake
+++ external/Externals.cmake
@@ -3,5 +3,5 @@ include ( "CMakeModules/ODExternals.cmake" )
 DEFINE_GIT_EXTERNAL( proj4 https://github.com/OpendTect/proj4.git 5.1.0 )
 
 if ( NOT OD_NO_OSG )
-    DEFINE_GIT_EXTERNAL( osgGeo https://github.com/OpendTect/osgGeo.git master )
+    DEFINE_GIT_EXTERNAL( osgGeo https://github.com/OpendTect/osgGeo.git main )
 endif()
