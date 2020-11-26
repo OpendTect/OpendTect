7dc9e96a66efbc51dd626e4ffb6f21c11b057f52
Arnaud Huck <arnaud.huck@dgbes.com>
2020-11-26 21:20:28 +0100

    Fixed several errors in execution of python scripts [devel]
    Manually quoting the script argument was redundant, as Qt takes
    care of it. Double work breaks the execution of the script.
    
    Also fixed the logic of the test program itself, which from far
    did not report all possible failures.

--- src/Basic/tests/pythoncomm.cc
+++ src/Basic/tests/pythoncomm.cc
@@ -8,6 +8,7 @@
 
 #include "testprog.h"
 
+#include "dirlist.h"
 #include "genc.h"
 #include "file.h"
 #include "filepath.h"
@@ -20,100 +21,6 @@
 #include "settingsaccess.h"
 
 #define mTestDirNm "Test Dir"
-
-/* TODO activate??
-
-static bool getData( od_istream& pullstrm, float* vals,
-			int expectednrvals, const char* whatstr )
-{
-    int nrvals;
-    int nrerrs = 0;
-    while ( true )
-    {
-	nrvals = 0;
-	pullstrm.getBin( nrvals );
-	const bool badstrm = pullstrm.isBad();
-	if ( nrvals == 0 || badstrm )
-	{
-	    nrerrs++;
-	    std::istream& stdstrm = pullstrm.stdStream();
-	    if ( stdstrm.bad() || nrerrs > 100 )
-	    {
-		tstStream(false) << "bad strm, or too many fails" << od_endl;
-		BufferString errmsg;
-		pullstrm.addErrMsgTo( errmsg );
-		tstStream(true) << errmsg << od_endl;
-		return false;
-	    }
-	    stdstrm.clear();
-	    Threads::sleep( 0.02 );
-	    continue;
-	}
-	else if ( nrvals == 0 )
-	{
-	    tstStream(false) << "got 0 vals, continuing loop" << od_endl;
-	    Threads::sleep( 0.02 );
-	    continue;
-	}
-	mRunStandardTestWithError( nrvals==expectednrvals,
-				   BufferString(whatstr," (nr vals)"),
-				   BufferString("nrvals=",nrvals) )
-	break;
-   }
-   mRunStandardTest( pullstrm.getBin(vals,nrvals*sizeof(float)),
-			BufferString(whatstr," (slurp)") )
-   return true;
-}
-
-static bool testComm( od_ostream& pushstrm, od_istream& pullstrm )
-{
-    int nrvals; float vals[10], outvals[2];
-
-    nrvals = 3; vals[0] = 2.f; vals[1] = vals[2] = 5.f;
-    pushstrm.addBin( nrvals );
-    mRunStandardTest( pushstrm.addBin(vals,nrvals*sizeof(float)),
-			"First vals put" )
-
-    if ( !getData(pullstrm,outvals,2,"first") )
-	return false;
-    mRunStandardTestWithError( outvals[0]>3.99f&&outvals[0]<4.01f,
-	"First Average calculated", BufferString("outvals[0]=",outvals[0]) )
-
-    nrvals = 6; vals[3] = 8.f; vals[4] = vals[5] = 5.f;
-    pushstrm.addBin( nrvals );
-    mRunStandardTest( pushstrm.addBin(vals,nrvals*sizeof(float)),
-			"2nd vals put" )
-    if ( !getData(pullstrm,outvals,2,"second") )
-	return false;
-    mRunStandardTestWithError( outvals[0]>4.99f&&outvals[0]<5.01f,
-	"2nd Average calculated", BufferString("outvals[0]=",outvals[0]) )
-
-    return true;
-}
-
-int mTestMainFnName( int argc, char** argv )
-{
-    mInitTestProg();
-
-    OS::MachineCommand mc( OD::PythonAccess::sPythonExecNm(true) );
-    mc.addArg( "-u" );
-    mc.addArg( GetPythonScript("test_ioserv.py") );
-    OS::CommandExecPars execpars( OS::RunInBG );
-    execpars.createstreams( true );
-    File::Path scriptfp;
-    PtrMan<OS::CommandLauncher> cl = OD::PythA().getLauncher( mc, scriptfp );
-    cl->execute( execpars );
-    sleepSeconds( 1 );
-    if ( !scriptfp.isEmpty() )
-	File::remove( scriptfp.fullPath() );
-
-    if ( !testComm(*cl.getStdInput(),*cl.getStdOutput()) )
-	return 1;
-
-    return 0;
-}
-
-*/
 
 BufferString createTestDir()
 {
@@ -128,20 +35,48 @@ BufferString createTestDir()
     return fp.fullPath();
 }
 
-bool testRemoveDir( const BufferString& path, bool expectedres )
+
+bool testRemoveDir( const char* path, bool expectedres )
 {
-    bool ret = OD::pythonRemoveDir( path, true ).isOK();
-    if ( ret )
+    const uiRetVal uirv = OD::pythonRemoveDir( path, true );
+    BufferString err = toString(uirv);
+
+    const File::Path fp( path );
+    const BufferString dirnm = fp.dir();
+    const DirList dl( File::Path::getTempDir(), File::DirsInDir );
+    if ( expectedres )
     {
-	BufferStringSet dirnms;
-	File::listDir( File::Path::getTempDir(), File::DirsInDir, dirnms );
-	if ( dirnms.indexOf(mTestDirNm) >= 0 )
-	    ret = false;
+	bool missing = true;
+	for ( int idx=0; idx<dl.size(); idx++ )
+	{
+	    if ( dl.get(idx) == dirnm )
+	    {
+		missing = false;
+		break;
+	    }
+	}
+	if ( !missing )
+	    err += "Folder reported deleted but still present";
+    }
+    else
+    {
+	bool present = false;
+	for ( int idx=0; idx<dl.size(); idx++ )
+	{
+	    if ( dl.get(idx) == dirnm )
+	    {
+		present = true;
+		break;
+	    }
+	}
+	if ( present )
+	    err += "Folder to be tested is missing";
     }
 
-    mRunStandardTest( ret == expectedres,
-	    expectedres ? "Writable folder deleted"
-			: "Read-only folder not deleted" )
+    const bool ret = uirv.isOK() && err.isEmpty();
+    const BufferString desc = expectedres ? "Delete writable folder"
+					  : "Don't delete read-only folder";
+    mRunStandardTestWithError( ret == expectedres, desc, err );
 
     return ret;
 }
@@ -150,10 +85,11 @@ int mTestMainFnName( int argc, char** argv )
 {
     mInitTestProg();
 
-    if ( !OD::PythA().isUsable().isOK() )
+    const uiRetVal uirv = OD::PythA().isUsable();
+    if ( !uirv.isOK() )
     {
-	if ( !quiet_ )
-	    od_cout() << "Python link is not usable" << od_endl;
+	logStream() << "Python link is not usable" << od_newline;
+	logStream() << toString(uirv) << od_endl;
 	return 1;
     }
 
