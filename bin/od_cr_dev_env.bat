e13a1f1cb5d0cf261556658e9c2880221e9cbcdb
Arnaud Huck <arnaud.huck@dgbes.com>
2021-01-07 08:58:26 +0100

    cmake changes to make the setup for development of plugins work again [devel]
    Large update of the CreateLaunchers.cmake file, and subsequent changes to make
    the creation of a plugin development environment work smoothly, especially on windows.
    
    One noticeable addition is that a script is created to launch each build executable,
    per configuration.

--- bin/od_cr_dev_env.bat
+++ bin/od_cr_dev_env.bat
@@ -1,26 +1,29 @@
 cls
-@echo off
+@ECHO OFF
 
 set inpdir=%1
 set outdir=%2
 
 if not exist %inpdir% goto MSGINPDIR
 if not exist %inpdir%\relinfo\ver.devel_win64.txt if not exist %inpdir%\relinfo\ver.devel_win32.txt goto MSGPKG
-if not exist %outdir% goto MSGOUTDIR
+if not exist %outdir% md %outdir%
 
-md %outdi%
-xcopy /s %inpdir%\doc\Programmer\pluginexample\* %outdir%\
+xcopy /s /y %inpdir%\doc\Programmer\pluginexample\* %outdir%\
 if exist %outdir%\plugins\win32 rd /S /Q %outdir%\plugins\win32\libs 
-if exist %outdir%\plugins\win64 rd /S /Q %outdir%\plugins\win64\libs 
+if exist %outdir%\plugins\win64 rd /S /Q %outdir%\plugins\win64\libs
+if not exist "%outdir%\plugins" (
+    xcopy /s /i /y %inpdir%\plugins\Tut\CMakeLists*.txt %outdir%\plugins\Tut
+    xcopy /s /i /y %inpdir%\plugins\Tut\*.h %outdir%\plugins\Tut
+    xcopy /s /i /y %inpdir%\plugins\Tut\*.cc %outdir%\plugins\Tut
+    xcopy /s /i /y %inpdir%\plugins\uiTut\CMakeLists*.txt %outdir%\plugins\uiTut
+    xcopy /s /i /y %inpdir%\plugins\uiTut\*.h %outdir%\plugins\uiTut
+    xcopy /s /i /y %inpdir%\plugins\uiTut\*.cc %outdir%\plugins\uiTut
+)
 goto END
 
 
 :MSGINPDIR
 echo %inpdir% does not exist
-goto END
-
-:MSGOUTDIR
-echo %outdir% does not exist
 goto END
 
 :MSGPKG
@@ -29,4 +32,8 @@ goto END
 
 
 :END
+set inpdir=%inpdir:\=/%
+set inpdir=%inpdir:"=%
+echo OpendTect_DIR:PATH=%inpdir% > %outdir%\CMakeCache.txt
+echo CMAKE_BUILD_TYPE:STRING=Debug >> %outdir%\CMakeCache.txt
 echo Done
