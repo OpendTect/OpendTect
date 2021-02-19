2dec906c2fa1b30ac45222e7f03af66a9f0dcbd6
Nanne Hemstra <nanne.hemstra@dgbes.com>
2021-02-19 11:35:23 -0600

    Added SurveyInfo::hasProjection() [devel]

--- src/Basic/survinfo.cc
+++ src/Basic/survinfo.cc
@@ -1361,6 +1361,12 @@ ConstRefMan<Coords::CoordSystem> SurveyInfo::getCoordSystem() const
 }
 
 
+bool SurveyInfo::hasProjection() const
+{
+    return coordsystem_ && coordsystem_->isProjection();
+}
+
+
 bool SurveyInfo::xyInFeet() const
 {
     return mXYInFeet();
