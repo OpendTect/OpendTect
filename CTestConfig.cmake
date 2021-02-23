3e985397b6d5c75aab47c36e172e3576ca7d8d76
Arnaud Huck <arnaud.huck@dgbes.com>
2021-02-23 11:57:08 +0100

    Updated CDash site [devel]

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,6 +7,6 @@ set(CTEST_PROJECT_NAME "OpendTect-6_7_rel")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "intranet")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=OpendTect-6.7_rel")
+set(CTEST_DROP_SITE "cdash.dgbes.com")
+set(CTEST_DROP_LOCATION "/submit.php?project=OpendTect-6.7_rel")
 set(CTEST_DROP_SITE_CDASH TRUE)
