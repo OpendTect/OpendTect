27aec0dc2e384508b16ab655b7a39b11698d3725
Arnaud Huck <arnaud.huck@dgbes.com>
2021-03-05 10:52:45 +0100

    Minor changes to base plugins [devel]

--- plugins/uiTut/uitutpi.cc
+++ plugins/uiTut/uitutpi.cc
@@ -73,8 +73,8 @@ private:
 uiTutMgr::uiTutMgr()
     : uiPluginInitMgr()
     , wellmnuitmhandler_(visSurvey::WellDisplay::sFactoryKeyword(),
-		 *appl().applMgr().visServer(),m3Dots(tr("Tut Well Tools")),
-		 mCB(this,uiTutMgr,doWells),nullptr,cTutIdx)
+		     *appl().applMgr().visServer(),m3Dots(tr("Tut Well Tools")),
+		     mCB(this,uiTutMgr,doWells),nullptr,cTutIdx)
 {
     init();
 }
