d107960eb48375c71eb516b27e4caf0e4a72f213
Arnaud Huck <arnaud.huck@dgbes.com>
2021-03-19 15:34:32 +0100

    Improved OpendTect isolation script on Linux [devel]
    The script was relying on the presence of the environment
    variables OD_INTERNAL_CLEANPATH and OD_SYSTEM_LIBRARY_PATH to
    reset the environment before sending a command.
    These variables may not be set, for instance they are not set
    by the installer which does not execute the init_dtect script.
    
    The new implementation uses DTECT_APPL if set to remove any path
    in $PATH and $LD_LIBRARY_PATH that would start with $DTECT_APPL.
    
    When DTECT_APPL is not set, it gets set to the installation
    containing the script od_external.sh
    
    Should fix the dynamic linking error when the installer starts on
    Linux, which tries to use the Qt libraries of the OpendTect installation
    instead of using its own libraries.

--- bin/od_external.sh
+++ bin/od_external.sh
@@ -1,23 +1,56 @@
 #!/bin/bash
+# od_external.sh
+# Removes pre-existing OpendTect environment
+# if any before executing a command
 
 if [ "$#" -lt 1 ]; then
     echo "Usage: command [ARGS]"
     exit 1	
 fi
 
-if [[ ! -z "${OD_INTERNAL_CLEANPATH}" ]]; then
-    export PATH=${OD_INTERNAL_CLEANPATH}
-fi
-if [[ -z "${OD_SYSTEM_LIBRARY_PATH}" ]]; then
-    unset LD_LIBRARY_PATH
-else
-    export LD_LIBRARY_PATH=${OD_SYSTEM_LIBRARY_PATH}
+if [[ -z "${DTECT_APPL}" ]]; then
+    execpath=`realpath "${0}"`
+    execappl=$(dirname "${execpath}")
+    export DTECT_APPL="$(dirname "${execappl}")"
 fi
 
+ld_cleanup () {
+    local inppaths="${1}"
+    local addedpath="${2}"
+    if [[ -n "${inppaths}" ]]; then
+	if [[ -n "${addedpath}" ]]; then
+	    inppaths="${inppaths}":"${addedpath}"
+	fi
+    elif [[ -n "${addedpath}" ]]; then
+	inppaths="${addedpath}"
+    fi
+
+    if [[ -z "${inppaths}" ]] || [[ -z "${DTECT_APPL}" ]]; then
+	return
+    fi
+
+    local func_result=""
+    IFS=':' read -r -a ldpaths <<< "${inppaths}"
+    for index in "${!ldpaths[@]}"
+    do
+	if [[ "${ldpaths[index]}" != "${DTECT_APPL}"* ]]; then
+	    if [[ -n "${func_result}" ]]; then
+		export func_result="${func_result}":"${ldpaths[index]}"
+	    else
+		export func_result="${ldpaths[index]}"
+	    fi	    
+	fi
+    done
+    echo "${func_result}"
+}
+
+export PATH="$(ld_cleanup "${PATH}" "${OD_INTERNAL_CLEANPATH}")"
+export LD_LIBRARY_PATH="$(ld_cleanup "${LD_LIBRARY_PATH}" "${OD_SYSTEM_LIBRARY_PATH}")"
+if [[ -z "${LD_LIBRARY_PATH}" ]]; then unset LD_LIBRARY_PATH; fi
 unset DTECT_APPL
 
 #Tensorboard won't start if LC_ALL is not set
-if [ -z $LC_ALL ] && [ ! -z ${LANG} ]; then
+if [[ -z $LC_ALL ]] && [[ -n ${LANG} ]]; then
     export LC_ALL=${LANG}
 fi
 
