d8f667dbb7b661bcc92b39dfeae0777c4d659a94
Prajjaval Singh <prajjaval.singh@dgbes.com>
2021-04-01 13:56:32 +0530

    added missing files

---
+++ include/General/geojson.h
@@ -0,0 +1,112 @@
+#pragma once
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Prajjaval Singh
+ Date:		March 2021
+ RCS:		$Id$
+________________________________________________________________________
+-*/
+
+#include "generalmod.h"
+#include "odjson.h"
+#include "color.h"
+#include "pickset.h"
+#include "googlexmlwriter.h"
+#include "giswriter.h"
+#include "ptrman.h"
+#include "survinfo.h"
+
+namespace Coords { class CoordSystem; }
+typedef TypeSet<Coord3> coord3dset;
+typedef TypeSet<Coord> coord2dset;
+
+
+namespace OD
+{
+
+mExpClass(General) GeoJsonTree : public JSON::Object
+{ mODTextTranslationClass(GeoJsonTree)
+
+public:
+
+    typedef JSON::Object	Object;
+    typedef JSON::Array		Array;
+
+			GeoJsonTree()
+			    : coordsys_(SI().getCoordSystem())
+			    , featarr_(0)
+			    , polyarr_(0)
+			    , topobj_(new Object())
+			{}
+			GeoJsonTree( const GeoJsonTree& oth )
+			    : Object(oth)
+			    , filename_(oth.filename_)
+			    , coordsys_( SI().getCoordSystem() )
+			    , topobj_(new Object())
+			    , featarr_(0)
+			    , polyarr_(0)
+			{}
+			GeoJsonTree( const Object& obj )
+			    : Object(obj)
+			    , coordsys_(SI().getCoordSystem())
+			    , topobj_(new Object())
+			    , featarr_(0)
+			    , polyarr_(0)
+			{}
+    virtual		~GeoJsonTree()			{}
+
+			// will do GeoJSon check
+    uiRetVal		use(od_istream&);
+    uiRetVal		use(const char* fnm);
+
+    BufferString	crsName() const;
+
+    static const char*	sKeyName()			{ return "name"; }
+    static const char*	sKeyType()			{ return "type"; }
+    static const char*	sKeyFeatures()			{ return "features"; }
+    static const char*	sKeyProperties()		{ return "properties"; }
+    static const char*	sKeyGeometry()			{ return "geometry"; }
+    static const char*	sKeyCoord()			{ return "coordinates";}
+    static const char*	sKeyCRS()			{ return "crs"; }
+
+			// use when constructing from general JSON Object
+    void		doGeoJSonCheck(uiRetVal&);
+
+    ValueSet*		createJSON(BufferString geomtyp,
+			   const coord2dset& crdset, const BufferStringSet& nms,
+			    ConstRefMan<Coords::CoordSystem>,
+			    const BufferString& iconnm=BufferString::empty() );
+    ValueSet*		createJSON(BufferString geomtyp,
+			   const coord3dset& crdset, const BufferStringSet& nms,
+			   ConstRefMan<Coords::CoordSystem>,
+			    const BufferString& iconnm=BufferString::empty());
+    ValueSet*		createJSON(BufferString geomtyp,
+			      const pickset&,ConstRefMan<Coords::CoordSystem>,
+			    const BufferString& iconnm=BufferString::empty());
+    //void		setProperties(const GISWriter::Property&);
+
+protected:
+
+    BufferString	filename_;
+    bool		isfeatpoint_ = true;
+    bool		isfeatpoly_  = false;
+    bool		isfeatmulti_ = false;
+
+    void		addCoord(const Coord3& coords, Array& poly);
+    void		addCoord(const Coord& coords, Array& poly);
+    bool		isAntiMeridianCrossed(const coord3dset&);
+    Array*		createFeatArray(BufferString);
+    Array*		createFeatCoordArray(Array* featarr, BufferString typ,
+							GISWriter::Property);
+    Object*		createCRSArray(Array* featarr);
+    ConstRefMan<Coords::CoordSystem>	    coordsys_;
+    Array*		featarr_;
+    Array*		polyarr_;
+    Object*		topobj_;
+    void		setCRS(ConstRefMan<Coords::CoordSystem>);
+
+};
+
+} // namespace OD
---
+++ src/General/geojson.cc
@@ -0,0 +1,259 @@
+/*+
+ * (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ * AUTHOR   : Prajjaval Singh
+ * DATE     : March 2021
+-*/
+
+#include "geojson.h"
+#include "od_istream.h"
+#include "latlong.h"
+#include "picklocation.h"
+#include "survinfo.h"
+#include "giswriter.h"
+#include "coordsystem.h"
+
+
+uiRetVal OD::GeoJsonTree::use( const char* fnm )
+{
+    od_istream strm( fnm );
+    return use( strm );
+}
+
+
+uiRetVal OD::GeoJsonTree::use( od_istream& strm )
+{
+    filename_.set( strm.fileName() );
+
+    uiRetVal uirv = read( strm );
+    if ( !uirv.isOK() )
+	return uirv;
+
+    doGeoJSonCheck( uirv );
+    return uirv;
+}
+
+
+#define mCheckPresence(obj,ky,fn) \
+    const auto* ky = (obj)->fn( #ky ); \
+    if ( !ky ) \
+    { \
+	uirv.set( missing_key_str.arg( #ky ).arg( filename_ ) ); \
+	return; \
+    }
+
+void OD::GeoJsonTree::doGeoJSonCheck( uiRetVal& uirv )
+{
+    uirv.setEmpty();
+    uiString missing_key_str( tr("Missing %1 key in GeoJSON file '%2'") );
+
+    mCheckPresence( this, crs, getObject )
+    mCheckPresence( crs, properties, getObject )
+    mCheckPresence( this, features, getArray )
+    if ( features->isEmpty() )
+	uirv.set( tr("No features in GeoJSON file '%1'").arg( filename_ ) );
+}
+
+
+BufferString OD::GeoJsonTree::crsName() const
+{
+    return getObject( "crs" )
+	 ->getObject( "properties" )
+	 ->getStringValue( sKeyName() );
+}
+
+
+void  OD::GeoJsonTree::addCoord( const Coord3& coord, Array& poly )
+{
+
+    const LatLong ll( LatLong::transform(coord.coord(), true, coordsys_) );
+    if ( isfeatpoly_ )
+    {
+	Array* coordarr = poly.add( new Array(OD::JSON::Number) );
+	coordarr->add( ll.lng_ ).add( ll.lat_ ).add( coord.z );
+    }
+    else
+	poly.add( ll.lng_ ).add( ll.lat_ ).add( coord.z );
+}
+
+
+void  OD::GeoJsonTree::addCoord( const Coord& coord, Array& poly )
+{
+    const LatLong ll( LatLong::transform( coord, true, coordsys_ ) );
+    if ( isfeatpoly_ )
+    {
+	Array* coordarr = poly.add( new Array(OD::JSON::Number) );
+	coordarr->add( ll.lng_ ).add( ll.lat_ );
+    }
+    else
+	poly.add( ll.lng_ ).add( ll.lat_ );
+}
+
+
+#define mCreateFeatArray(geomtyp) \
+{ \
+    isfeatpoint_ = geomtyp == "Point"; \
+    isfeatpoly_ = (geomtyp == "Polygon")  || (geomtyp == "MultiPolygon"); \
+    topobj_->set("type", "FeatureCollection"); \
+    featarr_ = topobj_->set( "features", new Array(true) ); \
+} \
+
+#define mAddCoord \
+for ( int idx=0; idx<nms.size(); idx++ ) \
+{ \
+    GISWriter::Property property; \
+    property.objnm_ = *nms[idx]; \
+    polyarr_ = createFeatCoordArray( featarr_, geomtyp, property ); \
+    Array* poly(0); \
+    if (isfeatpoly_) \
+	poly = polyarr_->add( new Array( false ) ); \
+    for( int cidx=0; cidx<crdset.size(); cidx++ ) \
+    { \
+	if (!isfeatpoint_) \
+	{ \
+	    if ( !isfeatpoly_ ) \
+		poly = polyarr_->add( new Array( OD::JSON::Number ) ); \
+	    addCoord( crdset[cidx], *poly ); \
+	} \
+	else \
+	    addCoord( crdset[cidx], *polyarr_ ); \
+    } \
+} \
+
+
+void OD::GeoJsonTree::setCRS( ConstRefMan<Coords::CoordSystem> crs )
+{
+    coordsys_ = crs;
+}
+
+
+OD::GeoJsonTree::Object* OD::GeoJsonTree::createCRSArray( Array* crsarr )
+{
+    Object* featobj = crsarr->add( new Object );
+    featobj->set( "type", "name" );
+
+    Object* propobj = featobj->set( "properties", new Object );
+    propobj->set( "name", coordsys_->getURNString() );
+    return propobj;
+}
+
+
+OD::GeoJsonTree::Array* OD::GeoJsonTree::createFeatCoordArray( Array* featarr,
+	    BufferString typ , GISWriter::Property property )
+{
+    if ( !featarr )
+	return nullptr;
+
+    Object* featobj = featarr->add( new Object );
+    featobj->set( "type", "Feature" );
+
+    Object* propobj = featobj->set( "properties", new Object );
+
+    Object* styleobj = featobj->set( "style", new Object );
+    const Color clr = property.color_;
+    propobj->set( "fill", clr.getStdStr(false, -1) );
+    propobj->set( "stroke-width", property.width_ );
+    propobj->set( "fill-opacity", clr.t() );
+
+    Object* crsobj = featobj->set( "crs", new Object );
+    crsobj->set( "type", "name" );
+    Object* crspropobj = crsobj->set( "properties", new Object );
+    crspropobj->set( "name", coordsys_->getURNString() );
+
+    Object* geomobj = featobj->set( "geometry", new Object );
+    geomobj->set( "type", typ );
+    return geomobj->set( "coordinates", isfeatpoint_ ?
+			    new Array(OD::JSON::Number) : new Array( false ) );
+}
+
+
+OD::GeoJsonTree::ValueSet* OD::GeoJsonTree::createJSON( BufferString geomtyp,
+	    const coord2dset& crdset, const BufferStringSet& nms,
+	    ConstRefMan<Coords::CoordSystem> crs, const BufferString& iconnm  )
+{
+    if ( topobj_->isEmpty() )
+	mCreateFeatArray( geomtyp )
+
+    setCRS( crs );
+
+    mAddCoord;
+
+    return topobj_->clone();
+}
+
+
+OD::GeoJsonTree::ValueSet* OD::GeoJsonTree::createJSON( BufferString geomtyp,
+	    const coord3dset& crdset, const BufferStringSet& nms,
+	    ConstRefMan<Coords::CoordSystem> crs, const BufferString& iconnm )
+{
+
+    if ( topobj_->isEmpty() )
+	mCreateFeatArray( geomtyp )
+
+    setCRS( crs );
+
+    mAddCoord;
+
+    return topobj_->clone();
+}
+
+
+OD::GeoJsonTree::ValueSet* OD::GeoJsonTree::createJSON( BufferString geomtyp,
+    const pickset& pckset, ConstRefMan<Coords::CoordSystem> crs,
+    const BufferString& iconnm )
+{
+    if ( topobj_->isEmpty() )
+	mCreateFeatArray( geomtyp )
+
+    setCRS( crs );
+
+    for ( int idx=0; idx<pckset.size(); idx++ )
+    {
+	const Pick::Set* pick = pckset.get( idx );
+	if ( !pick )
+	    continue;
+
+	GISWriter::Property property;
+	property.color_ = pick->disp_.color_;
+	property.width_ = pick->disp_.pixsize_*0.1;
+	property.objnm_ = pick->name();
+	property.iconnm_ = iconnm;
+	polyarr_ = createFeatCoordArray( featarr_, geomtyp, property );
+	ObjectSet<const Pick::Location> crdset;
+	pick->getLocations( crdset );
+	Array* poly(0);
+	if ( isfeatpoly_ )
+	    poly = polyarr_->add( new Array(false) );
+
+	for ( int cidx=0; cidx<crdset.size(); cidx++ )
+	{
+	    if ( !isfeatpoint_ )
+	    {
+		if ( !isfeatpoly_ )
+		    poly = polyarr_->add( new Array(OD::JSON::Number) );
+		addCoord( crdset[cidx]->pos(), *poly );
+	    }
+	    else
+		addCoord( crdset[cidx]->pos(), *polyarr_ );
+	}
+    }
+
+    return topobj_->clone();
+}
+
+
+bool OD::GeoJsonTree::isAntiMeridianCrossed( const coord3dset& crdset )
+{
+    for ( auto crd : crdset)
+    {
+	const LatLong ll( LatLong::transform( crd.coord(), true, coordsys_) );
+	if ( ll.lng_ < 0 )
+	    break;
+    }
+    return true;
+}
+
+
+//void OD::GeoJsonTree::setProperties( const GISWriter::Property& property )
+//{
+//    property_ = property;
+//}
