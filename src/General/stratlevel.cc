e6ff4ca7a4d8bc6e7c829ee88f6ec6e9fe3a66a1
Nanne Hemstra <nanne.hemstra@dgbes.com>
2021-04-21 16:21:28 -0500

    Make sure default strat tree is loaded.
    Summary:
    Call Strat::loadDefaultTree() after every survey change.
    Couldn't think of a better place. Any other ideas?
    
    Right now, you need to open the Strat manager window to load the default tree. So, if you open a marker table for a well, the regional markers are not selected.
    
    Test Plan: Start OpendTect and directly open the well manager. Select a well, and open the marker dialog. Regional markers are now set correctly.
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Differential Revision: https://phabricator.dgbes.com:8443/D13873

--- src/General/stratlevel.cc
+++ src/General/stratlevel.cc
@@ -48,17 +48,17 @@ public:
 
 LevelSetMgr()
 {
-    IOM().surveyChanged.notify( mCB(this,LevelSetMgr,doNull) );
+    mAttachCB( IOM().surveyChanged, LevelSetMgr::surveyChangedCB );
 }
 
 ~LevelSetMgr()
 {
-    doNull( 0 );
+    detachAllNotifiers();
 }
 
-void doNull( CallBacker* )
+void surveyChangedCB( CallBacker* )
 {
-    deepErase( lss_ );
+    lss_.erase();
 }
 
 void createSet()
@@ -92,7 +92,7 @@ LevelSet& curSet()
     return *lss_[lss_.size()-1];
 }
 
-    ObjectSet<LevelSet>	lss_;
+    ManagedObjectSet<LevelSet>	lss_;
 };
 
 
