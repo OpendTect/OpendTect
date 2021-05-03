4cdeb1de9de27850cfea168b84f535dc6ee6d426
Nanne Hemstra <nanne.hemstra@dgbes.com>
2021-05-03 09:23:38 -0500

    Removed rcsID from uiCmdDriver, uiNLA, uiStrat, uiSysAdm [devel]

--- src/uiStrat/uistratlayseqgendesc.cc
+++ src/uiStrat/uistratlayseqgendesc.cc
@@ -7,7 +7,6 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID mUsedVar = "$Id$";
 
 #include "uistratlaycontent.h"
 #include "uistratbasiclayseqgendesc.h"
