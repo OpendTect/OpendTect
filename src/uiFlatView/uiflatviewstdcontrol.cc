baa2fa9c4c76168b934c076c4dcf5621ef15bef7
Nanne Hemstra <nanne.hemstra@dgbes.com>
2021-05-03 09:24:02 -0500

    Removed rcsID from uiFlatView [devel]

--- src/uiFlatView/uiflatviewstdcontrol.cc
+++ src/uiFlatView/uiflatviewstdcontrol.cc
@@ -7,7 +7,6 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID mUsedVar = "$Id$";
 
 #include "uiflatviewstdcontrol.h"
 #include "uibitmapdisplay.h"
