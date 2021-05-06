ac72427dd3bd5a5203b5ace7d96f5109a3eea5b7
Nanne Hemstra <nanne.hemstra@dgbes.com>
2021-05-06 16:45:34 -0500

    Removed RCS line from include/uiTools [devel]

--- include/uiTools/uiconstvel.h
+++ include/uiTools/uiconstvel.h
@@ -6,7 +6,6 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	A. Huck
  Date:		January 2017
- RCS:		$Id$
 ________________________________________________________________________
 
 -*/
--- include/uiTools/uihostiddlg.h
+++ include/uiTools/uihostiddlg.h
@@ -6,7 +6,6 @@ ________________________________________________________________________
  (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
  Author:	Wayne Mogg
  Date:		April 2021
- RCS:		$Id$
 ________________________________________________________________________
 -*/
 
