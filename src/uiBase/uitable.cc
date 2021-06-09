7fd420cdce9ce9fea36e640c5a7c7b1cda4415c5
Raman Singh <raman.singh@dgbes.com>
2021-06-09 13:14:53 -0500

    added uiTable::setMouseTracking [devel]
    Can be disabled when no action is desired on mouse move inside the table

--- src/uiBase/uitable.cc
+++ src/uiBase/uitable.cc
@@ -1282,6 +1282,12 @@ void uiTable::editCell( const RowCol& rc, bool replace )
 }
 
 
+void uiTable::setMouseTracking( bool yn )
+{
+    body_->setMouseTracking( yn );
+}
+
+
 void uiTable::popupMenu( CallBacker* )
 {
     const int xcursorpos = QCursor::pos().x();
