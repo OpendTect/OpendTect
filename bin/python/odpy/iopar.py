fe2eded3fd2c239034d79562543f9e8411cc242b
Arnaud Huck <arnaud.huck@dgbes.com>
2021-06-25 16:57:33 +0200

    Fixed error in odpy.common.getODSoftwareDir
    If the input dictionary is not null and contains the
    key 'dtectexec', four levels of the corresponding file path
    need to be removed in order to really return the root of the
    OpendTect software installation, and not the path to the
    binaries.
    
    Also improved odpy.common.getODArgs(), so it reads
    the survey location from the setting files of the current
    user.
    
    [Jira: OD-3185 and OD-3225]

--- bin/python/odpy/iopar.py
+++ bin/python/odpy/iopar.py
@@ -6,14 +6,10 @@
 # IOPar tools
 #
 
-import odpy.common
+import os
 
-
-def read_line( fp, bin ):
-  if bin:
-    words = fp.readline().decode("utf8").split(":")
-  else:
-    words = fp.readline().split(":")
+def read_line( words ):
+  words = words.split(':')
   cleanwords = [x.strip() for x in words]
   nrwords = len(cleanwords)
   ret = list()
@@ -21,4 +17,18 @@ def read_line( fp, bin ):
     ret.append( cleanwords[0] )
   if nrwords > 1:
     ret.append( ':'.join(cleanwords[1:nrwords]) )
+  else:
+    ret.append( ':'.join('') )
   return ret
+
+def read_from_iopar( fnm, searchkey ):
+  if not os.path.exists(fnm):
+    return None
+
+  with open( fnm, 'r' ) as fp:
+    for line in fp:
+      key,val = read_line(line)
+      if key == searchkey:
+        return val
+
+  return None
