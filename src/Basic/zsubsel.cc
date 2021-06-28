OpendTect/OpendTect

commit ee35b0487d09caf9d32de88b4f7c90c9b22d8c9c

Fixed failure to restore Z Range in Machine Learning extraction
Summary:
An IOPar saved by Survey::FullSubSel could not be used to
initialize the Pos::Provider in uiIo because of incompatible keys.
With this fix the IOPar of both classes are compatible again.

Also added several operators that will be needed to fix OD-3107,
especially the == operator in SeisSel.

Test Plan:
Saved Machine Learning data extraction parameters in
an Img2Img workflow. The user-interface did not show the Z range
stored in the saved file, now it does, either by opening a
parameters file from a legacy version, or with the current code.

Reviewers: raman

Reviewed By: raman

Subscribers: nanne

Differential Revision: https://phabricator.dgbes.com:8443/D13825

src/Basic/zsubsel.cc
@@ -452,9 +452,20 @@ static const char* sKeyZSS = "Z Subsel";
 
 void Survey::FullZSubSel::fillPar( IOPar& iop ) const
 {
-    iop.set( IOPar::compKey(sKeyZSS,sKey::Size()), size() );
+    const int sz = size();
+    if ( sz == 1 )
+    {
+	if ( is2D() )
+	    iop.set( sKey::GeomID(), GeomID() );
+	else
+	    iop.removeWithKey( sKey::GeomID() );
+	iop.set( sKey::ZRange(), zRange() );
+	return;
+    }
+
+    iop.set( IOPar::compKey(sKeyZSS,sKey::Size()), sz );
     iop.setYN( IOPar::compKey(sKeyZSS,sKey::Is2D()), is2D() );
-    for ( auto idx=0; idx<size(); idx++ )
+    for ( int idx=0; idx<sz; idx++ )
     {
 	const BufferString baseky( IOPar::compKey(sKeyZSS,idx) );
 	iop.set( IOPar::compKey(baseky,sKey::GeomID()), geomids_.get(idx) );
@@ -484,8 +495,11 @@ void Survey::FullZSubSel::usePar( const IOPar& inpiop, const SurveyInfo* si )
     if ( !iop || iop->isEmpty() )
     {
 	//Seis::SelData format
-	const bool iopis2d = !inpiop.isPresent( sKey::SurveyID() );
-	if ( iopis2d )
+	bool is2d; SubSel::GeomID gid;
+	if ( !SubSel::getInfo(inpiop,is2d,gid) )
+	    return;
+
+	if ( is2d )
 	{
 	    int idx = 0;
 	    while( true )
@@ -500,8 +514,7 @@ void Survey::FullZSubSel::usePar( const IOPar& inpiop, const SurveyInfo* si )
 		else
 		    break;
 
-		bool is2d = true; GeomID gid;
-		Survey::SubSel::getInfo( *iop, is2d, gid );
+		SubSel::getInfo( *iop, is2d, gid );
 		{
 		    z_steprg_type zrg;
 		    if ( iop->get(sKey::ZRange(),zrg) )

include/Basic/bufstringset.h
@@ -38,6 +38,7 @@ public:
 			{ return new BufferStringSet(*this); }
     virtual		~BufferStringSet()	{}
     bool		operator ==(const BufferStringSet&) const;
+    bool		operator !=(const BufferStringSet&) const;
 
     inline size_type	size() const		{ return strs_.size(); }
     inline bool		isEmpty() const		{ return strs_.isEmpty(); }

include/Basic/dbkey.h
@@ -133,6 +133,7 @@ public:
     inline DBKeySet&	operator =( const DBKeySet& oth )
 			{ deepCopy( dbkys_, oth.dbkys_ ); return *this; }
     bool		operator ==(const DBKeySet&) const;
+    bool		operator !=(const DBKeySet&) const;
 
     inline size_type	size() const		{ return dbkys_.size(); }
     inline bool		isEmpty() const		{ return dbkys_.isEmpty(); }

include/Seis/seisseldata.h
@@ -75,6 +75,9 @@ public:
     static SelData*	get(const IOPar&,const SurveyInfo* si=nullptr);
     static SelData*	get(const DBKey&);
 
+    bool		operator ==(const SelData&) const;
+    bool		operator !=(const SelData&) const;
+
     virtual Type	type() const			= 0;
     virtual bool	is2D() const			{ return false; }
     virtual bool	isAll() const			{ return false; }

src/Basic/bufstring.cc
@@ -571,17 +571,26 @@ BufferStringSet::BufferStringSet( const char* s1, const char* s2,
 }
 
 
-bool BufferStringSet::operator ==( const BufferStringSet& bss ) const
+bool BufferStringSet::operator ==( const BufferStringSet& oth ) const
 {
-    if ( size() != bss.size() )
-	return false;
+    if ( &oth == this )
+	return true;
 
     const size_type sz = size();
+    if ( sz != oth.size() )
+	return false;
+
     for ( idx_type idx=0; idx<sz; idx++ )
-	if ( get(idx) != bss.get(idx) )
+	if ( get(idx) != oth.get(idx) )
 	    return false;
 
     return true;
+}
+
+
+bool BufferStringSet::operator !=( const BufferStringSet& oth ) const
+{
+    return !(*this == oth);
 }
 
 

src/Basic/dbkey.cc
@@ -156,6 +156,9 @@ DBKey& DBKey::operator =( const DBKey& oth )
 
 bool DBKey::operator ==( const DBKey& oth ) const
 {
+    if ( &oth == this )
+	return true;
+
     if ( !IDWithGroup<int,int>::operator ==(oth) )
 	return false;
 
@@ -324,6 +327,9 @@ uiString DBKey::toUiString() const
 
 bool DBKeySet::operator ==( const DBKeySet& oth ) const
 {
+    if ( &oth == this )
+	return true;
+
     const size_type sz = size();
     if ( sz != oth.size() )
 	return false;
@@ -334,6 +340,12 @@ bool DBKeySet::operator ==( const DBKeySet& oth ) const
 	    return false;
     }
     return true;
+}
+
+
+bool DBKeySet::operator !=( const DBKeySet& oth ) const
+{
+    return !(*this == oth);
 }
 
 

src/Seis/seisseldata.cc
@@ -33,6 +33,29 @@ typedef StepInterval<pos_type> pos_steprg_type;
 typedef StepInterval<z_type> z_steprg_type;
 
 
+bool Seis::SelData::operator ==( const Seis::SelData& oth ) const
+{
+    if ( &oth == this )
+	return true;
+
+    if ( type() != oth.type() || is2D() != oth.is2D() ||
+	 isAll() != oth.isAll() )
+	return false;
+
+    IOPar thisiop, othiop;
+    fillPar( thisiop );
+    oth.fillPar( othiop );
+
+    return thisiop == othiop;
+}
+
+
+bool Seis::SelData::operator !=( const Seis::SelData& oth ) const
+{
+    return !(*this == oth);
+}
+
+
 const char* Seis::SelData::sNrLinesKey()
 {
     return Survey::FullHorSubSel::sNrLinesKey();

commit 04e2b07d3c66829fd7326764e51bad36413aa23e

Fixed crash in ML data extraction for 2D data
Removing some of the code introduced with D13825, while working on OD-3107.
No longer making a difference between single and multi-line selections.
Not sure why the code that had to be introduced back then is no longer
necessary.

src/Basic/zsubsel.cc
@@ -453,16 +453,6 @@ static const char* sKeyZSS = "Z Subsel";
 void Survey::FullZSubSel::fillPar( IOPar& iop ) const
 {
     const int sz = size();
-    if ( sz == 1 )
-    {
-	if ( is2D() )
-	    iop.set( sKey::GeomID(), geomID() );
-	else
-	    iop.removeWithKey( sKey::GeomID() );
-	iop.set( sKey::ZRange(), zRange() );
-	return;
-    }
-
     iop.set( IOPar::compKey(sKeyZSS,sKey::Size()), sz );
     iop.setYN( IOPar::compKey(sKeyZSS,sKey::Is2D()), is2D() );
     for ( int idx=0; idx<sz; idx++ )
@@ -506,10 +496,8 @@ void Survey::FullZSubSel::usePar( const IOPar& inpiop, const SurveyInfo* si )
     if ( !iop || iop->isEmpty() )
     {
 	//Seis::SelData format
-	bool is2d; SubSel::GeomID gid;
-	if ( !SubSel::getInfo(inpiop,is2d,gid) )
-	    return;
-
+	bool is2d = false; SubSel::GeomID gid;
+	SubSel::getInfo( inpiop, is2d, gid );
 	if ( is2d )
 	{
 	    int idx = 0;
@@ -555,7 +543,7 @@ void Survey::FullZSubSel::usePar( const IOPar& inpiop, const SurveyInfo* si )
 
     si_ = si;
     setEmpty();
-    for ( auto idx=0; idx<sz; idx++ )
+    for ( int idx=0; idx<sz; idx++ )
     {
 	const BufferString baseky( toString(idx) );
 	GeomID gid; z_steprg_type zrg;