b22342dc2bed39e3f2ef1f0c2f4219932b0efde9
Arnaud Huck <arnaud.huck@dgbes.com>
2021-07-07 17:40:17 +0200

    Fixed rare case of python execution error [devel]
    std::cerr may be contaminated with PE message from OpendTect.
    That should not be a reason for failure

--- src/Basic/pythonaccess.cc
+++ src/Basic/pythonaccess.cc
@@ -420,6 +420,9 @@ bool OD::PythonAccess::isEnvUsable( const FilePath* pythonenvfp,
     if ( !res )
 	return false;
 
+    if ( res && laststderr_.contains("(PE) HiddenParam") )
+	laststderr_.setEmpty();
+
     const bool testscriptout = scriptexpectedout && *scriptexpectedout;
     res = doscript ?  (testscriptout ? laststdout_ ==
 				       FixedString(scriptexpectedout)
