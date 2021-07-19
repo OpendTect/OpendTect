446e6edec33f06304684e7c1b1a5272d20a23aff
Prajjaval Singh <prajjaval.singh@dgbes.com>
2021-07-19 09:50:55 +0200

    Correctly copying selected attributes from Available List [Multi Attribute Processing]
    Summary: Earlier the attribute id set was not set to empty before new ids were added. This led to loss of few attributes when selectred attribute were moved to right for processing (OD-3285)
    
    Test Plan:
    Create a attribute set, similarity, sembelance and reference
    
    Open multi attribute window
    
    Close multi attribute window
    
    Open Attribute window and delete the attribute created in previous step
    
    Recreate all the three, delete one and recreate the same again under different name
    
    Re-open the multi attribute window
    
    Select all and copy them to the right. All should copy
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Subscribers: raman, lloyd, wayne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D14161

--- src/uiAttributes/uimultoutsel.cc
+++ src/uiAttributes/uimultoutsel.cc
@@ -246,6 +246,7 @@ bool uiMultiAttribSel::is2D() const
 void uiMultiAttribSel::fillAttribFld()
 {
     attribfld_->setEmpty();
+    allids_.setEmpty();
     if ( !descset_ )
 	return;
 
@@ -302,7 +303,8 @@ void uiMultiAttribSel::updateSelFld()
     for ( int idx=0; idx<selids_.size(); idx++ )
     {
 	const Attrib::Desc* desc = descset_->getDesc( selids_[idx] );
-	if (!desc ) continue;
+	if (!desc )
+	    continue;
 
 	selfld_->addItem( toUiString(desc->userRef()) );
     }
