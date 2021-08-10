aa5280a49d5f40b88dc1fe4f979ce360b19c924d
Arnaud Huck <arnaud.huck@dgbes.com>
2021-08-10 12:38:19 +0200

    Fixed detection of Qt5 WebEngine in QT_DTECT_WEBENGINE [cmake-devel]
    When the USE_QtWebEngine was set to false in the cache, it never
    got updated by the macro call.

--- CMakeModules/ODQtUtils.cmake
+++ CMakeModules/ODQtUtils.cmake
@@ -251,7 +251,9 @@ macro( OD_ADD_QT )
 endmacro(OD_ADD_QT)
 
 macro( QT_DTECT_WEBENGINE )
-    if ( DEFINED Dtect_QtWebEngine OR "${USE_QtWebEngine}" )
+    if ( (DEFINED Dtect_QtWebEngine OR "${USE_QtWebEngine}") OR
+	 (NOT DEFINED Dtect_QtWebEngine AND DEFINED CACHE{USE_QtWebEngine} AND
+	  NOT "$CACHE{USE_QtWebEngine}") )
 	find_package( Qt5 QUIET COMPONENTS WebEngineWidgets )
 	if ( Qt5WebEngineWidgets_FOUND )
 	    set( USE_QtWebEngine ON CACHE BOOL "Build with Qt5 WebEngineWidgets" FORCE )
