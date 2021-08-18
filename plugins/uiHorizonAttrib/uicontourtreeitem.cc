a262f248b8864d8ad12d31c24419317ea2173927
Prajjaval Singh <prajjaval.singh@dgbes.com>
2021-08-18 07:34:17 +0200

    Now contour elevation working
    Summary:
    Contour now changes its elevation from -180 to 165 in step of 15 default value is 0.
    Earlier we were not using the rotation property of osg::Text class.  This is now solved. OD-3363
    
    Test Plan: Tested by displaying contours in 3D Scene and rotating their lables
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Subscribers: raman, lloyd, wayne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D14224

--- plugins/uiHorizonAttrib/uicontourtreeitem.cc
+++ plugins/uiHorizonAttrib/uicontourtreeitem.cc
@@ -539,8 +539,9 @@ void uiContourTreeItemContourGenerator::addContourLabel(
 
 class uiContourParsDlg : public uiDialog
 { mODTextTranslationClass(uiContourParsDlg);
-public:
 
+static const int defelevval = 0;
+public:
 uiContourParsDlg( uiParent* p, const char* attrnm, const Interval<float>& rg,
 		  const StepInterval<float>& intv, const OD::LineStyle& ls,
 		  int sceneid )
@@ -622,15 +623,13 @@ uiContourParsDlg( uiParent* p, const char* attrnm, const Interval<float>& rg,
     degreeslblfld_ = new uiLabel( this, toUiString("(deg)") );
     degreeslblfld_->attach( rightOf, elevationfld_ );
 
-    elevationfld_->box()->setSpecialValueText( "Off" );
-    elevationfld_->box()->setInterval( -5, 75, 5 );
+    elevationfld_->box()->setInterval( -180, 165, 15 );
+    elevationfld_->box()->setValue( defelevval );
     elevationfld_->box()->doSnap( true );
     mAttachCB( elevationfld_->box()->valueChanging,
 					    uiContourParsDlg::elevationChg );
     mAttachCB( elevationfld_->box()->valueChanged,
 					    uiContourParsDlg::uiDisplayCB );
-    disableLabelElevation();
-
     mAttachCB( postFinalise(), uiContourParsDlg::finaliseCB );
 }
 
@@ -682,6 +681,12 @@ const FontData& getFontData() const
 }
 
 
+float getRotation() const
+{
+    return elevationfld_->box()->getFValue();
+}
+
+
 void setLabelAlignment( visBase::Text::Justification alignment )
 {
     if ( alignment == visBase::Text::BottomRight )
@@ -706,7 +711,7 @@ int getLabelAlignment() const
 
 void disableLabelElevation()
 {
-    elevationfld_->box()->setValue( elevationfld_->box()->minValue() );
+    elevationfld_->box()->setValue( defelevval );
 }
 
 
@@ -738,6 +743,7 @@ protected:
 bool acceptOK( CallBacker* )
 {
     intervalChanged.trigger();
+    propertyChanged.trigger();
     return false;
 }
 
@@ -1165,7 +1171,6 @@ void uiContourTreeItem::showPropertyDlg()
 	    propdlg_->setFontData( labels_->text(0)->getFontData() );
 	    propdlg_->setLabelAlignment( (visBase::Text::Justification)
 				   labels_->text(0)->getJustification() );
-	    propdlg_->disableLabelElevation();
 	}
     }
 
@@ -1221,10 +1226,12 @@ void uiContourTreeItem::propChangeCB( CallBacker* cb )
     {
 	showlabels_ = dlg->showLabels();
 	labels_->turnOn( lines_->isOn() && showlabels_ );
-
 	labels_->setFontData( dlg->getFontData() );
+	const Coord3 axis ( 0, 0, 1 );
+	const float rotangrad = Math::toRadians( dlg->getRotation() );
 	for ( int idx=0; idx<labels_->nrTexts(); idx++ )
 	{
+	    labels_->text(idx)->setRotation( rotangrad, axis );
 	    labels_->text(idx)->setJustification(
 		    (visBase::Text::Justification) dlg->getLabelAlignment() );
 	    labels_->text(idx)->setColor( color_ );
@@ -1256,6 +1263,7 @@ void uiContourTreeItem::startCreateUICContours()
 	lines_->turnOn( showcontours );
 	if ( labels_ )
 	    labels_->turnOn( showlabels );
+
 	areas_ = ctrgtr.getAreas();
 
 	bool validfound = false;
