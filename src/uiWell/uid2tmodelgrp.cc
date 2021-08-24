7e667e9f629e96ebeafcc2de8bc31f29f2ec5088
Nanne Hemstra <nanne.hemstra@dgbes.com>
2021-08-24 16:51:13 -0500

    Removed rcsID from src/uiWell and src/uiWellAttrib [devel]

--- src/uiWell/uid2tmodelgrp.cc
+++ src/uiWell/uid2tmodelgrp.cc
@@ -7,7 +7,6 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID mUsedVar = "$Id$";
 
 #include "uid2tmodelgrp.h"
 #include "uitblimpexpdatasel.h"
