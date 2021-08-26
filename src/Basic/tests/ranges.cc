38475b2a3ff7cab56446063c2b4a5b0a38000634
waynegm <wayne.mogg@gmail.com>
2021-08-26 14:07:06 +1000

    Fix failing test for StepInterval::niceInterval function [devel]
    Summary: Changes ported from main branch
    
    Test Plan: Tested on Linux
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Differential Revision: https://phabricator.dgbes.com:8443/D14259

--- src/Basic/tests/ranges.cc
+++ src/Basic/tests/ranges.cc
@@ -6,10 +6,8 @@
 -*/
 
 
-#include "thread.h"
 #include "testprog.h"
 #include "math2.h"
-#include "genc.h"
 #include "ranges.h"
 
 
@@ -18,19 +16,21 @@ bool test_niceInterval()
     StepInterval<float> si;
     mRunStandardTest(si.niceInterval(1)==StepInterval<float>(),
 							"si().niceInterval(1)");
-    si.set(2, 2, 1);
-    mRunStandardTest(si.niceInterval(10)==StepInterval<float>(2.f, 2.f, 0.05f),
+    si.set(2.f, 2.f, 1.f);
+    mRunStandardTest(si.niceInterval(10)==StepInterval<float>(1.6f, 2.4f, 0.1f),
 					    "si(2, 2, 1).niceInterval(10)");
-    si.set(2, 98, 1);
-    mRunStandardTest(si.niceInterval(10)==StepInterval<float>(0, 100, 10),
+    si.set(2.f, 98.f, 1.f);
+    mRunStandardTest(si.niceInterval(10)==StepInterval<float>(0.f, 100.f, 10.f),
 					    "si(2, 98, 1).niceInterval(10)");
-    si.set(98, 2, 1);
-    mRunStandardTest(si.niceInterval(10)==StepInterval<float>(100, 0, 10),
+    si.set(98.f, 2.f, 1.f);
+    mRunStandardTest(si.niceInterval(10)==StepInterval<float>(100.f, 0.f, 10.f),
 					    "si(98, 2, 1).niceInterval(10)");
-    mRunStandardTest(si.niceInterval(10, false)==StepInterval<float>(0,100,10),
+    mRunStandardTest(
+		si.niceInterval(10, false)==StepInterval<float>(0.f,100.f,10.f),
 					"si(98, 2, 1).niceInterval(10, false)");
-    si.set(-2, 98, 1);
-    mRunStandardTest(si.niceInterval(10)==StepInterval<float>(-10, 100, 10),
+    si.set(-2.f, 98.f, 1.f);
+    mRunStandardTest(
+		si.niceInterval(10)==StepInterval<float>(-10.f, 100.f, 10.f),
 					    "si(-2, 98, 1).niceInterval(10)");
     return true;
 }
