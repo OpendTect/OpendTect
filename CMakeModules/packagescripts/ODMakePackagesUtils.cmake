6cdb0b026f91aa13bc5145a69ad235a699632fb9
Nageswara Rao <nageswara.rao@dgbes.com>
2021-10-12 17:58:03 +0530

    Minor edit

--- CMakeModules/packagescripts/ODMakePackagesUtils.cmake
+++ CMakeModules/packagescripts/ODMakePackagesUtils.cmake
@@ -131,7 +131,7 @@ macro ( CREATE_PACKAGE PACKAGE_NAME )
 		file( COPY ${COPYFROMLIBDIR}/${EXE}.exe
 		      DESTINATION ${COPYTOLIBDIR} )
 	    elseif( APPLE )
-execute_process( COMMAND ${OpendTect_DIR}/data/install_files/macscripts/chfwscript ${COPYFROMEXEDIR}/${EXE} )
+		execute_process( COMMAND ${OpendTect_DIR}/data/install_files/macscripts/chfwscript ${COPYFROMEXEDIR}/${EXE} )
 		file( COPY ${COPYFROMEXEDIR}/${EXE}
 		      DESTINATION ${COPYTOEXEDIR} )
 	    else()
