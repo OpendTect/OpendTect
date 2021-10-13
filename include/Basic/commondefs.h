9ef4525a8b26c679bcd512b18a5034095f8f0f61
Arnaud Huck <arnaud.huck@dgbes.com>
2021-10-13 14:53:38 +0200

    Ported implementation of mUseType from od6.7 [devel]

--- include/Basic/commondefs.h
+++ include/Basic/commondefs.h
@@ -149,7 +149,15 @@ inline T& getNonConst( const T& t )
 #define mNonConst(x)    getNonConst( x )
 #define mSelf()         mNonConst( *this )
 
-#define mUseType(scope,typ)		typedef scope::typ typ
+#ifdef __win32__
+# define mUseType(scope,typ)            typedef scope::typ typ
+#else
+# define mUseType(scope,typ)            typedef typename scope::typ typ
+#endif
+//In template classes:
+#define mUseTemplType(scope,typ)        typedef typename scope::typ typ
+
+#define mGetDynamicCast(typ,in) dynamic_cast< typ >( in );
 # define mDynamicCast(typ,out,in)	out = dynamic_cast< typ >( in );
 # define mDynamicCastGet(typ,out,in)	typ mDynamicCast(typ,out,in)
 
