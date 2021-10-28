7ab98f56dccda1e8b2c67a110ab63bbe7729c3d6
Nageswara Rao <nageswara.rao@dgbes.com>
2021-10-28 13:55:46 +0530

    Supressed warnings 'function but is not marked 'override'' on MAC[cmake-devel]
    As API is frozen we can not fix above mentioned warning. Hence added a flag to supress warnings on MAC.

--- CMakeModules/ODPlatformUtils.cmake
+++ CMakeModules/ODPlatformUtils.cmake
@@ -64,6 +64,8 @@ if( UNIX ) #Apple and Linux
 
 	if ( (GCC_VERSION VERSION_GREATER 4.2.1) OR (NOT DEFINED CMAKE_COMPILER_IS_GNUCC) )
 	    set ( CMAKE_CXX_FLAGS "-Wdelete-non-virtual-dtor ${CMAKE_CXX_FLAGS}" )
+	    # This flag is defined because API s frozen
+	    set ( CMAKE_CXX_FLAGS "-Wno-inconsistent-missing-override ${CMAKE_CXX_FLAGS}" )
 	endif()
 
     else() # Not Apple
