82793a42957bc4ecae6a8e119c7ff317a37bedd6
Nageswara Rao <nageswara.rao@dgbes.com>
2021-11-17 12:45:11 +0530

    Check out and compilation of ODCharts moved to dgb. [cmake-devel]
    Summary: ODCharts is part of PRO package. So, check out and compilation of ODCharts moved to dgb.
    
    Test Plan: ODCharts not checked out while configuraion od od(main)
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Subscribers: raman, lloyd, wayne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D14471

--- external/Externals.cmake
+++ external/Externals.cmake
@@ -2,7 +2,7 @@ include ( "CMakeModules/ODExternals.cmake" )
 
 DEFINE_GIT_EXTERNAL( odpy https://github.com/OpendTect/odpy.git od6.6 )
 DEFINE_GIT_EXTERNAL( proj4 https://github.com/OpendTect/proj4.git 5.1.0 )
+
 if ( NOT OD_NO_OSG )
     DEFINE_GIT_EXTERNAL( osgGeo https://github.com/OpendTect/osgGeo.git osgGeo-1.4 )
 endif()
-DEFINE_GIT_EXTERNAL( ODCharts https://github.com/OpendTect/ODCharts.git charts6.6 )
