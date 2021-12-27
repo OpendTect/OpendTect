49afea301c2bc32c156fad44912bfd474e6b0fbc
Arnaud Huck <arnaud.huck@dgbes.com>
2021-12-27 10:06:53 +0100

    Running conda list on windows to list the python packages
    Summary:
    The python activator app is not successfull in executing the command
    'conda list' in a virtual environment, because it contains both a script called
    conda.bat and and executable called conda.exe in the path. CreateProcessA windows
    function cannot determine which one to execute.
    Hence adding the .bat extension and now executing the command 'conda.bat list' on
    windows when using the python activator app.
    Also removed all ifdef, replaced by __iswin__ which is always defined, set to true
    or false.
    
    Test Plan:
    The test button in the python settings window now lists all
    python packages on windows including pytorch, and the ML control center no
    longer complains that pytorch is missing.
    Will need minimal testing on Unix.
    
    Reviewers: wayne
    
    Reviewed By: wayne
    
    Subscribers: nageswara
    
    Differential Revision: https://phabricator.dgbes.com:8443/D14567

--- src/Basic/pythonaccess.cc
+++ src/Basic/pythonaccess.cc
@@ -1357,26 +1357,26 @@ BufferString OD::PythonAccess::getPacmanExecNm() const
     if ( activatefp_ )
     {
 	FilePath packmanexe( *activatefp_ );
-#ifdef __win__
-	if ( !GetPythonActivatorExe().isEmpty() )
-	    packmanexe.add( "condabin" )
-		      .add( "activate" );
-#endif
-	packmanexe.setFileName( "conda" );
-#ifdef __win__
-	packmanexe.setExtension( "exe" );
-#endif
-	if ( packmanexe.exists() )
-	    return packmanexe.baseName();
+	if ( __iswin__ && !GetPythonActivatorExe().isEmpty() )
+	    packmanexe.add( "condabin" ).add( "activate" );
 
-#ifdef __win__
-	packmanexe.setFileName( nullptr ).setFileName( nullptr )
-		  .add( "Scripts" );
-#endif
+	packmanexe.setFileName( "conda" );
+	if ( __iswin__ )
+	    packmanexe.setExtension( "bat" );
+
+	if ( packmanexe.exists() )
+	    return packmanexe.fileName();
+
+	if ( __iswin__ )
+	{
+	    packmanexe.setFileName( nullptr ).setFileName( nullptr )
+		      .add( "Scripts" );
+	}
+
 	packmanexe.setFileName( "pip" );
-#ifdef __win__
-	packmanexe.setExtension( "exe" );
-#endif
+	if ( __iswin__ )
+	    packmanexe.setExtension( "exe" );
+
 	if ( packmanexe.exists() )
 	    return packmanexe.baseName();
 
@@ -1387,11 +1387,7 @@ BufferString OD::PythonAccess::getPacmanExecNm() const
 	return BufferString( "pip" ); //Fallback
     }
 
-#ifdef __win__
-    return BufferString( "pip" );
-#else
-    return BufferString( "pip3" );
-#endif
+    return BufferString( __iswin__ ? "pip" : "pip3" );
 }
 
 
