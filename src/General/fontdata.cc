c2ffad76d204d1dfd8c8628398b3ab7b93cd1080
Nanne Hemstra <nanne.hemstra@dgbes.com>
2022-02-25 17:24:53 -0600

    Fixed font style issue in font selection dialog
    Summary:
    Finally found out that the bug is on our side because we don't store
    the font style.
    OD-3828
    
    Test Plan: QFontDialog now shows the correct style. Needs testing on Windows.
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D14756

--- src/General/fontdata.cc
+++ src/General/fontdata.cc
@@ -6,6 +6,8 @@
 
 
 #include "fontdata.h"
+
+#include "hiddenparam.h"
 #include "separstr.h"
 
 
@@ -27,12 +29,16 @@ static const int numwghts[] =
 { 25, 50, 63, 75, 87, 0 };
 
 
+static const char* sRegularStyle()	{ return "Regular"; }
+static HiddenParam<FontData,BufferString> hp_fontstylenames( sRegularStyle() );
+
 FontData::FontData( int ptsz, const char* fam, Weight wght, bool ital )
     : family_(fam)
     , pointsize_(ptsz)
     , weight_(wght)
     , italic_(ital)
 {
+    hp_fontstylenames.setParam( this, sRegularStyle() );
 }
 
 
@@ -43,6 +49,7 @@ FontData::FontData( const char* fms )
     , italic_(defaultItalic())
 {
     getFrom( fms );
+    hp_fontstylenames.setParam( this, sRegularStyle() );
 }
 
 
@@ -52,13 +59,25 @@ bool FontData::operator ==( const FontData& oth ) const
 	return true;
 
     return family_ == oth.family_ && pointsize_ == oth.pointsize_ &&
-	   weight_ == oth.weight_ && italic_ == oth.italic_;
+	   weight_ == oth.weight_ && italic_ == oth.italic_ &&
+	   styleName() == oth.styleName();
 }
 
 
 bool FontData::operator !=( const FontData& oth ) const
 {
     return !(*this == oth);
+}
+
+
+FontData& FontData::operator =( const FontData& fd )
+{
+    family_ = fd.family_;
+    pointsize_ = fd.pointsize_;
+    weight_ = fd.weight_;
+    italic_ = fd.italic_;
+    setStyleName( fd.styleName() );
+    return *this;
 }
 
 
@@ -73,6 +92,7 @@ FontData::Weight FontData::enumWeight( int w )
     if ( !numwghts[idx] ) idx--;
     return (FontData::Weight)idx;
 }
+
 
 const char* const* FontData::universalFamilies() { return universalfamilies; }
 const char* const* FontData::defaultKeys()	 { return defaultkeys; }
@@ -93,8 +113,19 @@ bool FontData::defaultItalic()			{ return defaultitalic; }
 void FontData::setDefaultFamily( const char* f)	{ defaultfamily = f; }
 void FontData::setDefaultPointSize( int ps )	{ defaultpointsize = ps; }
 void FontData::setDefaultWeight( Weight w )	{ defaultweight = w; }
-void FontData::setDefaultItalic( bool yn )      { defaultitalic = yn; }
+void FontData::setDefaultItalic( bool yn )	{ defaultitalic = yn; }
 
+
+void FontData::setStyleName( const char* stylenm )
+{
+    hp_fontstylenames.setParam( this, stylenm );
+}
+
+
+const char* FontData::styleName() const
+{
+    return hp_fontstylenames.getParam(this).buf();
+}
 
 
 bool FontData::getFrom( const char* s )
@@ -107,6 +138,7 @@ bool FontData::getFrom( const char* s )
     if ( nr > 1 ) pointsize_ = fms.getIValue( 1 );
     if ( nr > 2 ) parseEnumWeight( fms[2], weight_ );
     if ( nr > 3 ) italic_ = toBool(fms[3],false);
+    if ( nr > 4 ) setStyleName( fms[4] );
 
     return true;
 }
@@ -119,5 +151,6 @@ void FontData::putTo( BufferString& s ) const
     fms += pointsize_;
     fms += FontData::getWeightString(weight_);
     fms += getYesNoString( italic_ );
+    fms += styleName();
     s = fms;
 }
