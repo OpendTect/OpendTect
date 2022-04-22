4c019932e1586177e54274aba18e709783e9e216
Nanne Hemstra <nanne.hemstra@dgbes.com>
2022-04-22 17:23:11 -0500

    CDash stuff

--- include/uiODMain/uiodvw2dpicksettreeitem.h
+++ include/uiODMain/uiodvw2dpicksettreeitem.h
@@ -13,6 +13,7 @@ ________________________________________________________________________
 #include "uiodmainmod.h"
 #include "uiodvw2dtreeitem.h"
 #include "emposid.h"
+#include "pickset.h"
 
 class VW2DPickSet;
 
