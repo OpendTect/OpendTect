2f96da2a0721be553ed9c23259358eca92ef19da
Nanne Hemstra <nanne.hemstra@dgbes.com>
2022-05-05 11:19:23 -0500

    Improved plugin info message [devel]

--- plugins/uiSEGY/uisegypi.cc
+++ plugins/uiSEGY/uisegypi.cc
@@ -45,9 +45,9 @@ mDefODPluginInfo(uiSEGY)
     mDefineStaticLocalObject( PluginInfo, retpi,(
 	"SEG-Y base",
 	"OpendTect",
-	"dGB (Bert)",
+	"dGB",
 	"=od",
-	"Makes SEG-Y handlers available in odMain") );
+	"Support SEG-Y I/O in OpendTect") );
     return &retpi;
 }
 
@@ -55,7 +55,6 @@ mDefODPluginInfo(uiSEGY)
 class uiSEGYMgr	: public uiPluginInitMgr
 { mODTextTranslationClass(uiSEGYMgr);
 public:
-
 			uiSEGYMgr();
 			~uiSEGYMgr();
 
