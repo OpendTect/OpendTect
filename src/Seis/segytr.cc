39449c2f2cd4e3d955b022dc7e6185ae1955517e
Nanne Hemstra <nanne.hemstra@dgbes.com>
2022-05-26 10:24:57 -0500

    Added a nullptr check
    Summary: Might fix OD-3978, OD-3888, OD-3578, OD-2182
    
    Test Plan: Code compiles. Could not reproduce the crash
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D14981

--- src/Seis/segytr.cc
+++ src/Seis/segytr.cc
@@ -520,7 +520,8 @@ bool SEGYSeisTrcTranslator::commitSelections_()
     fileopts_.forread_ = forread;
     fileopts_.setGeomType( Seis::geomTypeOf( is_2d, is_prestack ) );
 
-    inpcd_ = inpcds_[0]; outcd_ = outcds_[0];
+    inpcd_ = inpcds_[0];
+    outcd_ = outcds_[0];
     if ( mIsEqual(outsd_.start,insd_.start,mDefEps)
       && mIsEqual(outsd_.step,insd_.step,mDefEps) )
 	useinpsd_ = true;
@@ -637,6 +638,9 @@ bool SEGYSeisTrcTranslator::skipThisTrace( SeisTrcInfo& ti, int& nrbadtrcs )
 		  .arg( maxnrconsecutivebadtrcs );
 	mPosErrRet(str);
     }
+
+    if ( !inpcd_ )
+	return false;
 
     sConn().iStream().ignore( innrsamples_*mBPS(inpcd_) );
     if ( !readTraceHeadBuffer() )
