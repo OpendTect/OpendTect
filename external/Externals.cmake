f53e67a4ae584550b0e54d7c6fa40fa6f92092e8
Raman Singh <raman.singh@dgbes.com>
2022-06-15 11:58:08 -0500

    Added BUILD_PROJ cmake variable to explicitly build proj in external

--- external/Externals.cmake
+++ external/Externals.cmake
@@ -2,7 +2,7 @@ include ( "CMakeModules/ODExternals.cmake" )
 
 DEFINE_GIT_EXTERNAL( odpy https://github.com/OpendTect/odpy.git main )
 if ( NOT EXISTS "${PROJ_DIR}" )
-    if ( NOT OD_NO_PROJ )
+    if ( BUILD_PROJ )
 	DEFINE_GIT_EXTERNAL( proj https://github.com/OSGeo/PROJ.git 9.0.0 )
     endif()
 endif()
