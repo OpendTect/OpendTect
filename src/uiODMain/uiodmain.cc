f81dd1f159ab7bd877d7ce31ccb31e2b33ce7b6b
Arnaud Huck <arnaud.huck@dgbes.com>
2022-06-16 15:43:01 +0200

    Made the product selection window more contextual
    Moving the product selection code away from uiODMain,
    to be issued from inside a plugin (uidGBTools).
    First big advantage is that the window no longer shows up in
    an empty layout when no commercial product is installed.
    
    Started od_main from a build with only od_main -> no window
    Started od_main after building uidGBTools -> same behaviour as current.
    
    [Jira: OD-4009]

--- src/uiODMain/uiodmain.cc
+++ src/uiODMain/uiodmain.cc
@@ -29,7 +29,6 @@ ________________________________________________________________________
 #include "uiodscenemgr.h"
 #include "uiodviewer2dmgr.h"
 #include "uipixmap.h"
-#include "uipluginsel.h"
 #include "uiseispartserv.h"
 #include "uisetdatadir.h"
 #include "uisplashscreen.h"
@@ -70,7 +69,12 @@ ________________________________________________________________________
 #include <iostream>
 
 
-extern "C" const char* GetSettingsDataDir();
+extern "C" {
+
+    mGlobal(uiTools) bool doProductSelection(uiParent*,bool&,uiRetVal&);
+
+}
+
 
 static uiODMain* manODMainWin( uiODMain* i )
 {
@@ -209,15 +213,14 @@ int ODMain( uiMain& app )
     manODMainWin( odmain );
 
     checkScreenRes();
-
-    const bool skippluginsel = GetEnvVarYN( "OD_SKIP_PLUGIN_SEL" );
-    if ( !skippluginsel )
+    uiRetVal uirv;
+    bool skippluginsel = false;
+    if ( !doProductSelection(odmain,skippluginsel,uirv) )
     {
-	uiPluginSel dlg( odmain );
-	dlg.setPopupArea( uiMainWin::Auto );
-	dlg.setActivateOnFirstShow();
-	if ( dlg.go() == uiDialog::Rejected )
-	    return 1;
+	if ( !uirv.isOK() )
+	    uiMSG().error( uirv );
+
+	return 1;
     }
 
     SetProgramRestarter( ODMainProgramRestarter );
