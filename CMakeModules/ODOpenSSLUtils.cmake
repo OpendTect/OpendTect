5cd15e5c305d35e411bbb32e9a95d9ae1f84bcad
Arnaud Huck <arnaud.huck@dgbes.com>
2022-06-20 14:10:04 +0200

    Fix for OpenSSL cmake detection [devel]

--- CMakeModules/ODOpenSSLUtils.cmake
+++ CMakeModules/ODOpenSSLUtils.cmake
@@ -4,8 +4,7 @@
 # 	Oct 2020	A.Huck
 #_______________________________________________________________________________
 
-macro( OD_FIND_OPENSSLCOMP COMP )
-
+macro( OD_ADD_QTOPENSSL_HINT )
     if ( DEFINED Qt5_DIR )
 	OD_FIND_QTDIR()
 	get_filename_component( QTINSTDIR ${QTDIR} REALPATH )
@@ -20,37 +19,38 @@ macro( OD_FIND_OPENSSLCOMP COMP )
 	    endif( WIN32 )
 	endif()
     endif( DEFINED Qt5_DIR )
-
-    find_package( OpenSSL 1.1.1 QUIET COMPONENTS ${COMP} )
-
-endmacro( OD_FIND_OPENSSLCOMP )
+endmacro( OD_ADD_QTOPENSSL_HINT )
 
 macro( OD_FIND_OPENSSL )
-    OD_FIND_OPENSSLCOMP( Crypto )
-    OD_FIND_OPENSSLCOMP( SSL )
+    if ( NOT TARGET OpenSSL::SSL OR NOT TARGET OpenSSL::Crypto )
+	find_package( OpenSSL 1.1.1 QUIET COMPONENTS SSL Crypto )
+	if ( NOT TARGET OpenSSL::SSL OR NOT TARGET OpenSSL::Crypto )
+	    OD_ADD_QTOPENSSL_HINT()
+	    unset( OPENSSL_FOUND )
+	    unset( OPENSSL_INCLUDE_DIR CACHE )
+	    unset( OPENSSL_CRYPTO_LIBRARY CACHE )
+	    unset( OPENSSL_SSL_LIBRARY CACHE )
+	    unset( OPENSSL_SSL_VERSION CACHE )
+	    find_package( OpenSSL 1.1.1 QUIET COMPONENTS SSL Crypto )
+	endif()
+    endif()
 endmacro(OD_FIND_OPENSSL)
 
 macro( OD_SETUP_OPENSSLCOMP COMP )
-    if ( NOT DEFINED OPENSSL_${COMP}_LIBRARY} )
 
-	if ( TARGET OpenSSL::${COMP} )
-	    OD_READ_TARGETINFO( OpenSSL::${COMP} )
-	    if ( EXISTS "${SOURCEFILE}" )
-		get_filename_component( SOURCEFILE ${SOURCEFILE} REALPATH )
-		add_definitions( -D__OpenSSL_${COMP}_LIBRARY__="${SOURCEFILE}" )
-	    endif()
-	else()
-	    message( WARNING "OpenSSL component ${COMP} is not available.")
+    if ( TARGET OpenSSL::${COMP} )
+	OD_READ_TARGETINFO( OpenSSL::${COMP} )
+	if ( EXISTS "${SOURCEFILE}" )
+	    get_filename_component( SOURCEFILE ${SOURCEFILE} REALPATH )
+	    add_definitions( -D__OpenSSL_${COMP}_LIBRARY__="${SOURCEFILE}" )
 	endif()
-
+    else()
+	message( WARNING "OpenSSL component ${COMP} is not available.")
     endif()
+
 endmacro( OD_SETUP_OPENSSLCOMP )
 
 macro( OD_SETUP_CRYPTO )
-
-    if ( OD_USECRYPTO OR OD_LINKCRYPTO )
-	OD_SETUP_OPENSSLCOMP( Crypto )
-    endif()
 
     if ( EXISTS "${OPENSSL_CRYPTO_LIBRARY}" )
 	if ( OD_LINKCRYPTO )
@@ -58,6 +58,7 @@ macro( OD_SETUP_CRYPTO )
 	    list( APPEND OD_MODULE_EXTERNAL_LIBS OpenSSL::Crypto )
 	elseif ( OD_USECRYPTO )
 	    list( APPEND OD_MODULE_EXTERNAL_RUNTIME_LIBS OpenSSL::Crypto )
+	    OD_SETUP_OPENSSLCOMP( Crypto )
 	endif()
     endif()
 
@@ -65,16 +66,13 @@ endmacro( OD_SETUP_CRYPTO )
 
 macro( OD_SETUP_OPENSSL )
 
-    if ( OD_USEOPENSSL OR OD_LINKOPENSSL )
-	OD_SETUP_OPENSSLCOMP( SSL )
-    endif()
-
     if ( EXISTS "${OPENSSL_SSL_LIBRARY}" )
 	if ( OD_LINKOPENSSL )
 	    list( APPEND OD_MODULE_INCLUDESYSPATH ${OpenSSL_INCLUDE_DIR} )
 	    list( APPEND OD_MODULE_EXTERNAL_LIBS OpenSSL::SSL )
 	elseif( OD_USEOPENSSL )
 	    list( APPEND OD_MODULE_EXTERNAL_RUNTIME_LIBS OpenSSL::SSL )
+	    OD_SETUP_OPENSSLCOMP( SSL )
 	endif()
     endif()
 
