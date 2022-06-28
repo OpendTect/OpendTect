a61f68e1b066005f0b26a860025f39d2f7f28297
Khushnood <khushnood.qadir@dgbes.com>
2022-06-28 16:32:03 +0530

    Mnemonic filter now includes only the mnemonics available in the project
    Summary: Mnemonic filter used to show all the mnemonics in our database. This has now been restricted to those used in the project.
    
    Test Plan: tested in well table
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Subscribers: lloyd, wayne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D15061

--- src/uiWell/uiwellfiltergrp.cc
+++ src/uiWell/uiwellfiltergrp.cc
@@ -20,6 +20,7 @@ ________________________________________________________________________
 #include "uimnemonicsel.h"
 #include "uistatusbar.h"
 #include "uitoolbutton.h"
+#include "welllogset.h"
 #include "wellman.h"
 #include "welltransl.h"
 
@@ -267,8 +268,12 @@ void uiWellFilterGrp::noLogFilterCB( CallBacker* )
 void uiWellFilterGrp::mnemFilterCB( CallBacker* )
 {
     MnemonicSelection mns;
+    MnemonicSelection availmns;
+    for ( const auto* wd : *wds_ )
+	wd->logs().getAllAvailMnems(availmns);
+
     BufferStringSet suitablelogs;
-    uiMultiMnemonicsSel dlg( this, mns );
+    uiMultiMnemonicsSel dlg( this, mns, &availmns );
     if ( !dlg.go() )
 	return;
 
