7df3e99f5f4e56288ae486c52c8458031127fd97
Arnaud Huck <arnaud.huck@dgbes.com>
2022-07-07 09:57:14 +0200

    Added few tools to OD::PythonAccess::ModuleInfo [devel]

--- include/Basic/pythonaccess.h
+++ include/Basic/pythonaccess.h
@@ -86,6 +86,8 @@ namespace OD
 	{
 	    public:
 				ModuleInfo(const char*);
+	    bool		operator ==(const ModuleInfo&) const;
+	    bool		operator !=(const ModuleInfo&) const;
 
 	    BufferString	displayStr(bool withver=true) const;
 
@@ -105,7 +107,10 @@ namespace OD
 			//<! Initial value of PYTHONPATH before OD starts
 
 	uiRetVal	hasModule(const char* modname,
-				  const char* minversion=0) const;
+				  const char* minversion=nullptr) const;
+	static uiRetVal	hasModule(const ObjectSet<ModuleInfo>&,
+				  const char* modname,
+				  const char* minversion=nullptr);
 	uiRetVal	getModules(ManagedObjectSet<ModuleInfo>&);
 	void		setForScript(const char* scriptnm,
 				     OS::MachineCommand&) const;
