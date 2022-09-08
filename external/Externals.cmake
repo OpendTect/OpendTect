ab2f6f45beee43866c0de0e55f7343a519604b60
Arnaud Huck <arnaud.huck@dgbes.com>
2022-09-08 09:13:02 +0200

    Updated externals to use 7.0 specific branches [devel]

--- external/Externals.cmake
+++ external/Externals.cmake
@@ -7,11 +7,11 @@
 
 include ( "CMakeModules/ODExternals.cmake" )
 
-DEFINE_GIT_EXTERNAL( odpy https://github.com/OpendTect/odpy.git main )
+DEFINE_GIT_EXTERNAL( odpy https://github.com/OpendTect/odpy.git od7.0 )
 if ( BUILD_PROJ )
-    DEFINE_GIT_EXTERNAL( proj https://github.com/OSGeo/PROJ.git 9.0.0 )
+    DEFINE_GIT_EXTERNAL( proj https://github.com/OSGeo/PROJ.git 9.0.1 )
 endif()
 
 if ( NOT OD_NO_OSG )
-    DEFINE_GIT_EXTERNAL( osgGeo https://github.com/OpendTect/osgGeo.git main )
+    DEFINE_GIT_EXTERNAL( osgGeo https://github.com/OpendTect/osgGeo.git osgGeo-1.5 )
 endif()
