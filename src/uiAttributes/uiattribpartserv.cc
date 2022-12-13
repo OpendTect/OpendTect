1c087cbadf253120bb71be444f54c11049d3d1df
waynegm <wayne.mogg@gmail.com>
2022-12-13 15:04:25 +1000

    Fix OD4321 Crash when displaying time-slices outside the pre-loaded range
    Summary:
    Fixes OD-4321  Working on time-slices outside the pre-loaded range hangs the system
    Changes ported from 7.0 branch
    
    Test Plan: Tested on Linux
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Differential Revision: https://phabricator.dgbes.com:8443/D15410

--- src/uiAttributes/uiattribpartserv.cc
+++ src/uiAttributes/uiattribpartserv.cc
@@ -884,6 +884,9 @@ ConstRefMan<RegularSeisDataPack> uiAttribPartServer::createOutput(
     {
 	if ( preloadeddatapack )
 	{
+	    if ( !preloadeddatapack->sampling().includes(tkzs) )
+		return nullptr;
+
 	    ObjectSet<const RegularSeisDataPack> cubeset;
 	    cubeset += preloadeddatapack;
 	    return aem->getDataPackOutput( cubeset );
