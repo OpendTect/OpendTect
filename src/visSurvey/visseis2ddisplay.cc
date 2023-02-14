f4f05c29ed2e4f8aa56300f6e3cdbe120e6af315
Nanne Hemstra <nanne.hemstra@dgbes.com>
2023-02-14 17:39:44 -0600

    Performance improvements for 2D data displays
    Summary:
    Avoid uiTaskRunner popup when mapping data
    Use bendpointfinder from Line2DData
    
    Jira: OD-4440
    
    Test Plan: Displayed 10 large 2D lines. Performance is a lot better
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D15592

--- src/visSurvey/visseis2ddisplay.cc
+++ src/visSurvey/visseis2ddisplay.cc
@@ -484,7 +484,7 @@ void Seis2DDisplay::updateChannels( int attrib, TaskRunner* taskr )
 
 	channels_->setSize( attrib, 1, sz0, sz1 );
 	channels_->setUnMappedVSData( attrib, idx, stor,
-			ownsstor ? OD::TakeOverPtr : OD::UsePtr, taskr );
+			ownsstor ? OD::TakeOverPtr : OD::UsePtr, nullptr );
     }
 
     channels_->turnOn( true );
@@ -528,10 +528,7 @@ void Seis2DDisplay::createTransformedDataPack( int attrib, TaskRunner* taskr )
 void Seis2DDisplay::updatePanelStripPath()
 {
     TraceDisplayInfo& tdi = trcdisplayinfo_;
-    BendPointFinder2D finder( tdi.alltrcpos_.arr(), tdi.alltrcpos_.size(), 1.0);
-    finder.execute();
-
-    const TypeSet<int>& bends = finder.bendPoints();
+    const TypeSet<int>& bends = geometry_.getBendPoints();
     tdi.alljoints_.erase();
 
     for ( int idx=0; idx<bends.size(); idx++ )
