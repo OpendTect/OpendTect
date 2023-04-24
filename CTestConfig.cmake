66d486d4b17dad1e2ae2cf86d4dc1f8ab73339a3
Arnaud Huck <arnaud.huck@dgbes.com>
2023-04-24 14:46:11 +0200

    Updated branch name to od7.0_rel [devel]

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,13 +1,15 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
 
-set(CTEST_PROJECT_NAME "OpendTect-7.0")
+set(CTEST_PROJECT_NAME "OpendTect-7.0_rel")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.dgbes.com")
-set(CTEST_DROP_LOCATION "/submit.php?project=OpendTect-7.0")
+set(CTEST_DROP_LOCATION "/submit.php?project=OpendTect-7.0_rel")
 set(CTEST_DROP_SITE_CDASH TRUE)
-
