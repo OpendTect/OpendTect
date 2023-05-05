f39b28c25eeef04747669b5c7e28871388ecbdac
waynegm <wayne.mogg@gmail.com>
2023-05-05 16:14:17 +1000

    Fix OD4610 Horizon loading for some horizons is very slow in v7
    Summary:
    Fixes OD-4610 Horizon loading for some horizons is very slow in v7
    Changes ported from 7.0 branch
    
    Test Plan: Tested on Linux
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D15844

--- src/EarthModel/emsurfaceio.cc
+++ src/EarthModel/emsurfaceio.cc
@@ -346,9 +346,6 @@ bool dgbSurfaceReader::readHeaders( StreamConn& conn, const char* filetype )
     const bool is2d = res;
     setLinesTrcRngs( trcranges );
 
-    for ( int idx=0; idx<nrAuxVals(); idx++ )
-	auxdatasel_ += idx;
-
     par_->get( sKeyDBInfo(), dbinfo_ );
 
     if ( version_==1 )
