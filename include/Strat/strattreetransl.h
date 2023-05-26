79bacd721979808f6f62769277d44ad407b83770
Arnaud Huck <arnaud.huck@dgbes.com>
2023-05-26 14:07:27 +0200

    Fixed file operations on Stratigraphic framework
    Summary:
    Rename-remove-read-write operations on Stratigraphic Framework did not
    work fully, as they were only doing operations on the main file of the implementation,
    the sfw file.
    One must reimplement several functions of that Translator class to make it happen. However
    since the existing classes are declared using macros, creating a new set of classes to
    replace that translator group.
    
    [Jira: OD-4710]
    
    Test Plan:
    rename, remove, read/write operation now apply to all available files of the
    selected object (tested from the Open Stratigraphic Framework dialog).
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Subscribers: raman
    
    Differential Revision: https://phabricator.dgbes.com:8443/D15951

--- include/Strat/strattreetransl.h
+++ include/Strat/strattreetransl.h
@@ -12,3 +12,28 @@ ________________________________________________________________________
 #include "transl.h"
 
 mDeclEmptyTranslatorBundle(Strat,StratTree,od,"sfw")
+
+mExpClass(Strat) StratTreeNTranslatorGroup : public TranslatorGroup
+{			isTranslatorGroup(StratTreeN)
+			mDefEmptyTranslatorGroupConstructor(StratTreeN)
+    const char*		defExtension() const override	{ return "sfw"; }
+};
+
+mExpClass(Strat) StratTreeNTranslator : public Translator
+{ mODTextTranslationClass(StratTreeNTranslator)
+public:
+			mDefEmptyTranslatorBaseConstructor(StratTreeN)
+
+    bool		implRename(const IOObj*,const char*) const override;
+    bool		implRemove(const IOObj*,bool) const override;
+    bool		implSetReadOnly(const IOObj*,bool) const override;
+
+    static const char*	associatedFileExt();
+    static uiString	sStratTree()
+			{ return tr("Stratigraphic Tree"); }
+};
+
+mExpClass(Strat) odStratTreeNTranslator : public StratTreeNTranslator
+{			isTranslator(od,StratTreeN)
+			 mDefEmptyTranslatorConstructor(od,StratTreeN)
+};
