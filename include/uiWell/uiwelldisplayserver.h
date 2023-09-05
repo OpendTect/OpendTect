da2dbf0f1ebf7c40031a2ca985f3baec9c330dde
Khushnood Qadir <khushnood.qadir@dgbes.com>
2023-09-05 12:28:10 +0530

    Removing filter icon from well charts
    Summary:
    Well Charts had a filter button that redirected to Well table filter apart from the usual Well Charts filter. This second filter button is redundant and there shouldn't be a backward relationship from well charts to well table. This revision removes it.
    
    Jira Issue: OD-4069
    
    Test Plan: tested in well table log plots, log tools and basic manager log plots
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Subscribers: nanne, lloyd, wayne
    
    Differential Revision: https://phabricator.dgbes.com/D16331

--- include/uiWell/uiwelldisplayserver.h
+++ include/uiWell/uiwelldisplayserver.h
@@ -76,8 +76,7 @@ public:
 					const BufferStringSet& markernms,
 					const DBKeySet& sel_ids,
 					const BufferStringSet& sel_lognms,
-					const BufferStringSet& sel_markernms,
-					const CallBack& filtercb)
+					const BufferStringSet& sel_markernms)
 				{ return nullptr; }
     virtual uiMainWin*		createLogViewWinCB(uiParent*,
 					const ObjectSet<Well::Data>&,
@@ -85,8 +84,7 @@ public:
 					const BufferStringSet& markernms,
 					const DBKeySet& sel_ids,
 					const MnemonicSelection& sel_mns,
-					const BufferStringSet& sel_markernms,
-					const CallBack& filtercb)
+					const BufferStringSet& sel_markernms)
 				{ return nullptr; }
 
     virtual uiLogDisplayGrp*	createLogDisplayGrp(uiParent*)
