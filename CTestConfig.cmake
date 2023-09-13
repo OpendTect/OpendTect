311227f49927c63a52f1b8a5b613257d28b53587
Arnaud Huck <arnaud.huck@dgbes.com>
2023-09-13 12:25:09 +0200

    CDash configuration update [devel]
    Using http for submitting the build summary to CDash,
    as we are using a self-signed certificate.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,6 +9,6 @@
 set(CTEST_PROJECT_NAME "OpendTect")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
 
-set(CTEST_SUBMIT_URL "https://cdash.dgbes.com/submit.php?project=OpendTect")
+set(CTEST_SUBMIT_URL "http://cdash.dgbes.com/submit.php?project=OpendTect")
 
 set(CTEST_DROP_SITE_CDASH TRUE)
