b02839b4b99404a9b5a965173bf76128dee9b71d
Nanne Hemstra <nanne.hemstra@dgbes.com>
2023-09-22 17:06:04 -0500

    Fixed Math::NrSignificantDecimals
    Summary:
    and added to testprogram
    A project with 0.1ms sampling gave wrong number of decimals in some gui's.
    
    Test Plan: Tested test programs and checked uiseissubsel gui in OpendTect
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com/D16422

--- src/Basic/tests/math2.cc
+++ src/Basic/tests/math2.cc
@@ -87,12 +87,48 @@ bool testNiceNumber()
 }
 
 
+bool testNrSignificantDecimals()
+{
+    double stepms = 0.1;
+    double steps = stepms / 1000.;
+    mRunStandardTest(Math::NrSignificantDecimals(stepms)==1,
+		     "nrDec(stepms=0.1)");
+    mRunStandardTest(Math::NrSignificantDecimals(steps*1000.)==1,
+		     "nrDec(steps*1000.)");
+    mRunStandardTest(Math::NrSignificantDecimals(stepms/10.)==2,
+		     "nrDec(stepms/10.)");
+    mRunStandardTest(Math::NrSignificantDecimals(stepms/100.)==3,
+		     "nrDec(stepms/100.)");
+    mRunStandardTest(Math::NrSignificantDecimals(stepms/1000.)==4,
+		     "nrDec(stepms/1000.)");
+
+    mRunStandardTest(Math::NrSignificantDecimals(12345)==0,"nrDec(12345)");
+    mRunStandardTest(Math::NrSignificantDecimals(12345.99999)==0,
+		     "nrDec(12345.99999)");
+    mRunStandardTest(Math::NrSignificantDecimals(1234.599999)==1,
+		     "nrDec(1234.599999)");
+    mRunStandardTest(Math::NrSignificantDecimals(123.4599999)==2,
+		     "nrDec(123.4599999)");
+    mRunStandardTest(Math::NrSignificantDecimals(12.34599999)==3,
+		     "nrDec(12.34599999)");
+    mRunStandardTest(Math::NrSignificantDecimals(1.234599999)==4,
+		     "nrDec(1.234599999)");
+    mRunStandardTest(Math::NrSignificantDecimals(0.1234599999)==5,
+		     "nrDec(0.1234599999)");
+    mRunStandardTest(Math::NrSignificantDecimals(0.01234599999)==6,
+		     "nrDec(0.01234599999)");
+    return true;
+}
+
+
 int mTestMainFnName( int argc, char** argv )
 {
     mInitTestProg();
 
-    if (       !testBits()
-	    || !testNiceNumber() )
+    if ( !testBits() ||
+	 !testNiceNumber() ||
+	 !testNrSignificantDecimals()
+       )
 	return 1;
 
     return 0;
