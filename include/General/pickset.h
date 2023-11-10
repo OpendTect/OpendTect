bf0815c9be07fb870353d21694644ffa085fafe2
Nanne Hemstra <nanne.hemstra@dgbes.com>
2023-11-10 12:41:33 -0600

    Update startidxs when removing points from pointset

--- include/General/pickset.h
+++ include/General/pickset.h
@@ -77,7 +77,8 @@ public:
     float		getZ(int idx) const;
     int			size() const		{ return locations_.size(); }
     bool		isEmpty() const		{ return locations_.isEmpty(); }
-    void		setEmpty()		{ locations_.setEmpty(); }
+    void		setEmpty()
+			{ locations_.setEmpty(); startidxs_.setEmpty(); }
     bool		validIdx(int idx) const;
     bool		setCapacity(int sz);
     bool		setSize(int,const Coord3& defval=Coord3::udf());
