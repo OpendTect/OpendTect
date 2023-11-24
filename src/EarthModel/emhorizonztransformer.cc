327699e550663e0331e874fe7f1e13d90652ed28
Prajjaval Singh <prajjaval@ad.enschede.dgbes.com>
2023-11-24 10:35:37 +0100

    reverted the changes for flattening the horizon

--- src/EarthModel/emhorizonztransformer.cc
+++ src/EarthModel/emhorizonztransformer.cc
@@ -55,19 +55,19 @@ int HorizonZTransformer::nextStep()
     if ( posid.isUdf() )
 	return Executor::Finished();
 
-    Coord3 crd = tarhor_.getPos( posid );
-    if ( !isforward_ && !mIsUdf(crd.z) )
-	crd.z -= refz_;
+    float z = (float)tarhor_.getPos( posid ).z;
+    if ( !isforward_ && !mIsUdf(z) )
+	z -= refz_;
 
     const Pos::IdxPair bid = BinID::fromInt64( posid.subID() );
     mDynamicCastGet(const EM::Horizon2D*,hor2d,&tarhor_)
-    const TrcKey tk( bid, hor2d );
+	const TrcKey tk( bid, hor2d );
 
-    float newz = zat_.transformTrc( tk, crd.z );
+    float newz = zat_.transformTrc( tk, z );
     if ( isforward_ && !mIsUdf(newz) )
 	newz += refz_;
 
-    outputhor_->setPos( posid.subID(), Coord3(crd.coord(),newz), false );
+    outputhor_->setPos( posid.subID(), Coord3(0,0,newz), false );
     nrdone_++;
     return Executor::MoreToDo();
 }
