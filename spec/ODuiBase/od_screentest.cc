1d6d33e168cd9a0fb7279b5cb8a3e081c9e6de32
Arnaud Huck <arnaud.huck@dgbes.com>
2023-11-28 14:16:26 +0100

    Renamed the spec/uiBase module [devel]
    Module names must be unique accross the entire project.

---
+++ spec/ODuiBase/od_screentest.cc
@@ -0,0 +1,121 @@
+/*+
+________________________________________________________________________
+
+ Copyright:	(C) 1995-2022 dGB Beheer B.V.
+ License:	https://dgbes.com/licensing
+________________________________________________________________________
+
+-*/
+
+#include "uidialog.h"
+#include "uigroup.h"
+
+#include "uilabel.h"
+#include "uilineedit.h"
+#include "uimain.h"
+
+#include "moddepmgr.h"
+#include "prog.h"
+
+#include <QApplication>
+#include <QDebug>
+#include <QScreen>
+
+
+class uiScreenGrp : public uiGroup
+{
+public:
+			uiScreenGrp(uiParent*,QScreen*);
+protected:
+    QScreen*		qscreen_;
+};
+
+
+#define mAddField( txt, fn ) \
+{ \
+    auto* le = new uiLineEdit( this, txt ); \
+    if ( txt ) \
+	new uiLabel( this, toUiString(txt), le ); \
+    if ( attachobj ) \
+	le->attach( alignedBelow, attachobj ); \
+    attachobj = le; \
+    \
+    QString dbgstr; \
+    QDebug dbg( &dbgstr ); \
+    dbg << qscreen_->fn; \
+    le->setText( BufferString(dbgstr) ); \
+}
+
+uiScreenGrp::uiScreenGrp( uiParent* p, QScreen* qscreen )
+    : uiGroup(p,"Screen Group")
+    , qscreen_(qscreen)
+{
+    uiObject* attachobj = nullptr;
+    mAddField( "Name", name() );
+    mAddField( "Manufacturer", manufacturer() );
+    mAddField( "Model", model() );
+    mAddField( "Serial Number", serialNumber() );
+    mAddField( "Geometry", geometry() );
+    mAddField( "Virtual Geometry", virtualGeometry() );
+    mAddField( "Av. Geometry", availableGeometry() );
+    mAddField( "Av. Virt Geometry", availableVirtualGeometry() );
+    mAddField( "Size", size() );
+    mAddField( "Virtual Size", virtualSize() );
+    mAddField( "Av. Size", availableSize() );
+    mAddField( "Av. Virt Size", availableVirtualSize() );
+    mAddField( "Depth", depth() );
+    mAddField( "Refresh Rate", refreshRate() );
+    mAddField( "Device Pixel Ratio", devicePixelRatio() );
+    mAddField( "Logical Dots per Inch", logicalDotsPerInch() );
+    mAddField( "Logical Dots per Inch X", logicalDotsPerInchX() );
+    mAddField( "Logical Dots per Inch Y", logicalDotsPerInchY() );
+    mAddField( "Physical Dots per Inch", physicalDotsPerInch() );
+    mAddField( "Physical Dots per Inch X", physicalDotsPerInchX() );
+    mAddField( "Physical Dots per Inch Y", physicalDotsPerInchY() );
+}
+
+
+class uiScreenDlg : public uiDialog
+{
+public:
+			uiScreenDlg(uiParent*);
+
+protected:
+};
+
+
+
+uiScreenDlg::uiScreenDlg( uiParent* p )
+    : uiDialog(p,Setup(toUiString("Layout Test"),mNoDlgTitle,mTODOHelpKey))
+{
+    QList<QScreen*> screens = QApplication::screens();
+    uiGroup* prevgrp = nullptr;
+    for ( auto* screen : screens )
+    {
+	auto* grp = new uiScreenGrp( this, screen );
+	if ( prevgrp )
+	    grp->attach( rightTo, prevgrp );
+
+	prevgrp = grp;
+    }
+}
+
+
+
+int mProgMainFnName( int argc, char** argv )
+{
+    mInitProg( OD::UiProgCtxt )
+    SetProgramArgs( argc, argv, false );
+    uiMain app( argc, argv );
+
+    OD::ModDeps().ensureLoaded( "General" );
+
+    PIM().loadAuto( false );
+    OD::ModDeps().ensureLoaded( "uiBase" );
+    PtrMan<uiDialog> topdlg = new uiScreenDlg( nullptr );
+    app.setTopLevel( topdlg );
+    PIM().loadAuto( true );
+    topdlg->show();
+
+    return app.exec();
+}
