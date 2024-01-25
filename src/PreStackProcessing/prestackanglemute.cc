79ab39fe5a8f877da99bd3a15b2455925ed1b1cd
Arnaud Huck <arnaud.huck@dgbes.com>
2024-01-25 11:16:30 +0100

    Fixed a warning [devel]

--- src/PreStackProcessing/prestackanglemute.cc
+++ src/PreStackProcessing/prestackanglemute.cc
@@ -513,7 +513,8 @@ bool AngleMute::doWork( od_int64 start, od_int64 stop, int thread )
     ElasticModelSet emodels;
     auto* layers = new ElasticModelOv();
     emodels.add( layers );
-    bool nonemuted, allmuted;
+    bool nonemuted = false;
+    bool allmuted = false;
     TypeSet<float> offsets;
     offsets.setCapacity( 100, true );
     for ( int idx=mCast(int,start); idx<=stop; idx++, addToNrDone(1) )
