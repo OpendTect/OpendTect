8d5ca23af129eb25ef139e1be0d38bce33eda6bb
Nanne Hemstra <nanne.hemstra@dgbes.com>
2024-01-26 17:01:10 -0600

    Fixes for new uiFileSel [devel]

--- src/uiTools/uifilesel.cc
+++ src/uiTools/uifilesel.cc
@@ -145,7 +145,9 @@ void uiFileSel::init( const uiString& lbltxt )
     // needs to be done before finalising
     setFileNames( setup_.initialselection_ );
 
-    mAttachCB( fnmfld_->editingFinished, uiFileSel::inputChgCB );
+    if ( setup_.isForRead() )
+	mAttachCB( fnmfld_->editingFinished, uiFileSel::inputChgCB );
+
     mAttachCB( fnmfld_->returnPressed, uiFileSel::fnmEnteredCB );
     mAttachCB( postFinalize(), uiFileSel::finalizeCB );
 }
@@ -308,6 +310,7 @@ void uiFileSel::inputChgCB( CallBacker* )
     setButtonStates();
     if ( protfld_ )
     {
+	const int prevprot = protfld_->currentItem();
 	const BufferString prot = selectedProtocol();
 	int idx = prot.isEmpty() ? 0 : factnms_.indexOf( prot.buf() );
 	if ( idx<0 )
@@ -317,7 +320,8 @@ void uiFileSel::inputChgCB( CallBacker* )
 	}
 
 	protfld_->setCurrentItem( idx );
-	protChgCB( nullptr );
+	if ( prevprot != idx )
+	    protChgCB( nullptr );
     }
 
     filepars_.set( sKey::FileName(), fileName() );
