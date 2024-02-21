a23849abe970637ab6b683e17d2551adce543720
Raman Singh <raman.singh@dgbes.com>
2024-02-21 14:57:32 -0600

    Some minor fixes for Attrib vs Log Xplot GUI
    Summary:
    1. The default step value in feet survey is now 1ft instead of the feet equivalent of 1m.
    2. Radius field is greyed out when no attributes are selected, as it is only useful for extracting attributes around the well.
    
    Test Plan: Tested Log-Log xplot. The GUI field appear as expected. Data extraction works as before.
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Differential Revision: https://phabricator.dgbes.com/D16865

--- src/uiWellAttrib/uiwelllogextract.cc
+++ src/uiWellAttrib/uiwelllogextract.cc
@@ -79,6 +79,7 @@ uiWellLogExtractGrp::uiWellLogExtractGrp( uiParent* p,
     asu.lblpos( uiListBox::AboveMid );
     attrsfld_ = new uiListBox( this, asu );
     attrsfld_->display( setup.withattrib_, true );
+    attrsfld_->itemChosen.notify( mCB(this,uiWellLogExtractGrp,attribSelCB) );
     welllogselfld_->attach( ensureBelow, attrsfld_ );
 
     const float inldist = SI().inlDistance();
@@ -117,14 +118,23 @@ void uiWellLogExtractGrp::releaseDPS()
 
 
 const DataPointSet* uiWellLogExtractGrp::getDPS() const
-{ return curdps_; }
+{
+    return curdps_;
+}
 
 
 void uiWellLogExtractGrp::setDescSet( const Attrib::DescSet* newads )
 {
     ads_  = newads;
     adsChg();
+    attribSelCB( nullptr );
     welllogselfld_->update();
+}
+
+
+void uiWellLogExtractGrp::attribSelCB( CallBacker* )
+{
+    radiusfld_->setSensitive( attrsfld_->nrChosen() > 0 );
 }
 
 
@@ -188,10 +198,14 @@ bool uiWellLogExtractGrp::extractWellData( const TypeSet<MultiID>& ioobjids,
 					     ObjectSet<DataPointSet>& dpss )
 {
     Well::TrackSampler wts( ioobjids, dpss, SI().zIsTime() );
-    wts.for2d_ = false; wts.lognms_ = lognms;
-    wts.locradius_ = !radiusfld_ ? 0.f : radiusfld_->getFValue();
+    wts.for2d_ = false;
+    wts.lognms_ = lognms;
     wts.mkdahcol_ = true;
     wts.params_ = welllogselfld_->params();
+    wts.locradius_ = 0.f;
+    if ( radiusfld_ && attrsfld_->nrChosen() > 0 )
+	wts.locradius_ = radiusfld_->getFValue();
+
     if ( !wts.params_.isInTime() )
     {
 	wts.params_.zstep_ = getConvertedValue( wts.params_.zstep_,
