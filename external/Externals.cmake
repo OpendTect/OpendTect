2bd0a995332ec47da8f567cbd8ac90e3ac179891
Arnaud Huck <arnaud.huck@dgbes.com>
2024-03-06 16:36:24 +0100

    Revert "Remove marshmallow external [devel]"
    This reverts commit 3f50f7daca95ab38140dcfe216d6cadd191c70f1.

--- external/Externals.cmake
+++ external/Externals.cmake
@@ -9,6 +9,7 @@ include ( "CMakeModules/ODExternals.cmake" )
 
 DEFINE_GIT_EXTERNAL( odpy https://github.com/OpendTect/odpy.git main )
 DEFINE_GIT_EXTERNAL( safety https://github.com/pyupio/safety.git 2.3.5 TAG )
+DEFINE_GIT_EXTERNAL( marshmallow https://github.com/marshmallow-code/marshmallow.git dev )
 if ( NOT OD_NO_OSG )
     DEFINE_GIT_EXTERNAL( osgGeo https://github.com/OpendTect/osgGeo.git main )
 endif()
