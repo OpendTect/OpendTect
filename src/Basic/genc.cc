872851bd17cb05c85ef8f7e78ad549b3fd5bf1d5
Raman Singh <raman.singh@dgbes.com>
2024-03-29 09:24:39 -0500

    Added InQAEnvironment()
    Summary: An environment for features that need longer testing than a single patch release cycle.
    
    Test Plan: Compiles.
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Subscribers: nanne
    
    Differential Revision: https://phabricator.dgbes.com/D17017

--- src/Basic/genc.cc
+++ src/Basic/genc.cc
@@ -1082,3 +1082,10 @@ mExternC(Basic) void CloseBindings()
     PIM().unLoadAll();
     const_cast<OD::ModDepMgr&>( OD::ModDeps() ).closeAll();
 }
+
+
+bool InQAEnvironment()
+{
+    static const bool inqaenv = GetEnvVarYN( "OD_ENABLE_QA_ENVIRONMENT" );
+    return inqaenv;
+}
