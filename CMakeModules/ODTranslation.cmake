1ca65f8e61e7ad804f7b6794711e9f6c5f1f7584
Arnaud Huck <arnaud.huck@dgbes.com>
2024-11-13 14:34:23 +0100

    Removed linguist.csh [devel]
    Setting LD_LIBRARY_PATH to run the Qt tool 'linguist' is no longer
    required since at least Qt v5.9.6, thus removing the csh wrapper script, that
    was not used by the cmake target update_translations anyway.

--- CMakeModules/ODTranslation.cmake
+++ CMakeModules/ODTranslation.cmake
@@ -7,26 +7,17 @@
 
 macro( OD_ADD_TRANSLATIONS )
 
-    if ( TARGET Qt${QT_VERSION_MAJOR}::lrelease )
-	get_target_property( QT_RELEASE_EXECUTABLE Qt${QT_VERSION_MAJOR}::lupdate
-				    IMPORTED_LOCATION )
-    endif()
-
     if ( UNIX AND TARGET Qt${QT_VERSION_MAJOR}::lupdate )
 	get_target_property( QT_LUPDATE_EXECUTABLE Qt${QT_VERSION_MAJOR}::lupdate
-				    IMPORTED_LOCATION )
+			     IMPORTED_LOCATION )
 
-	if ( QT_LUPDATE_EXECUTABLE AND EXISTS "${QT_LUPDATE_EXECUTABLE}" )
-	    set( LINGUIST_LAUNCHER "CMakeModules/templates/linguist.csh.in" )
-	    if ( EXISTS "${LINGUIST_LAUNCHER}" )
-		configure_file( "${LINGUIST_LAUNCHER}" dtect/linguist.csh @ONLY )
-	    endif()
-
+	if ( EXISTS "${QT_LUPDATE_EXECUTABLE}" )
 	    if ( NOT TARGET update_translations )
 		set( CMAKE_FOLDER "Base" )
 		add_custom_target( update_translations
-			COMMAND ${OpendTect_DIR}/dtect/update_translations.csh
-				${OpendTect_DIR} ${CMAKE_SOURCE_DIR} ${CMAKE_BINARY_DIR}
+			COMMAND "${OpendTect_DIR}/dtect/update_translations.csh"
+				"${OpendTect_DIR}" "${CMAKE_SOURCE_DIR}"
+				"${CMAKE_BINARY_DIR}"
 				"od"
 				"${QT_LUPDATE_EXECUTABLE}"
 			COMMENT "Updating plural localizations" )
