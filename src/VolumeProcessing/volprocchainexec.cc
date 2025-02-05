f703e6ee18ffabb66b257ee9ce2e43e4ea89aabb
Raman Singh <raman.singh@dgbes.com>
2025-02-05 15:29:10 -0600

    Using the VolProc::Step errMsg()

--- src/VolumeProcessing/volprocchainexec.cc
+++ src/VolumeProcessing/volprocchainexec.cc
@@ -705,6 +705,7 @@ bool VolProc::ChainExecutor::Epoch::doPrepare( ProgressMeter* progmeter )
 	if ( !newtask )
 	{
 	    pErrMsg("Could not create task");
+	    errmsg_ = currentstep->errMsg();
 	    return false;
 	}
 
