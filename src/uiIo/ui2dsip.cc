d1ace84a413db43b2450526e252abfbf9eb089ba
Arnaud Huck <arnaud.huck@dgbes.com>
2025-03-12 12:13:27 +0100

    Fixed cmake error when using OD_INSTALL_DATADIR_MOD [devel]
    Summary:
    OD_INSTALL_DATADIR_MOD could not be used by an external plugins repository, if the OpendTect
    SDK installation was read-only. It should write its files to the 'data' folder of that external
    plugins repository, and not inside the SDK installation.
    The new default location for writing these files is the CMAKE_BINARY_DIR, which thus points to the
    external repository build directory, not the OpendTect SDK. This location can optionally be set
    by providing an optional second argument to the macro OD_INSTALL_DATADIR_MOD.
    
    To make it work, one needed to also change this implementation that reads these files from their
    data folder. The three current static functions thus have to read the data folder of the OpendTect
    SDK installation, and the data folder of the external repository build. This is done in the same
    way as the plugin manager loads these foreign libraries, by reading the environment variable OD_USER_PLUGIN_DIR.
    
    Test Plan:
    Configured the OpendTect project: The generated files remain created in this build's 'data' subfolder
    Configured the Tutorial plugin exercise, by adding a cmake call to OD_INSTALL_DATADIR_MOD in both plugins.
    Their generated files are now created in the corresponding build directory.
    Started od_main in gdb, checked that the filepaths used to look for the InitXXXSel external function are valid for:
    OpendTect modules
    OpendTect plugins
    External plugins.
    
    Reviewers: wayne, raman
    
    Reviewed By: raman
    
    Subscribers: raman, nanne
    
    Differential Revision: https://phabricator.dgbes.com/D17712

--- src/uiIo/ui2dsip.cc
+++ src/uiIo/ui2dsip.cc
@@ -23,10 +23,11 @@ ________________________________________________________________________
 #include "dirlist.h"
 #include "filepath.h"
 #include "hiddenparam.h"
+#include "moddepmgr.h"
 #include "od_helpids.h"
 #include "oddirs.h"
+#include "plugins.h"
 #include "posinfo2d.h"
-#include "sharedlibs.h"
 #include "survgeom2d.h"
 #include "survinfo.h"
 #include "tabledef.h"
@@ -269,17 +270,33 @@ bool uiSurvInfoProvider::getRanges( TrcKeyZSampling& cs, Coord crd[3],
 
 void uiSurvInfoProvider::addPluginsInfoProviders()
 {
-    const FilePath sipdatafp( mGetSWDirDataDir(), "SurveyProviders" );
-    if ( !sipdatafp.exists() )
-	return;
+    BufferStringSet configfiles;
+    const FilePath datafp( GetSetupDataFileName(ODSetupLoc_SWDirOnly,
+			   "SurveyProviders",false) );
+    if ( datafp.exists() )
+    {
+	const DirList dl( datafp.fullPath(), File::FilesInDir, "*.txt" );
+	for ( int idx=0; idx<dl.size(); idx++ )
+	    configfiles.add( dl.get( idx ).buf() );
+    }
+
+    const FilePath pluginsdatafpdir(
+			GetSetupDataFileName(ODSetupLoc_UserPluginDirOnly,
+					     "SurveyProviders",false) );
+    if ( pluginsdatafpdir.exists() )
+    {
+	const DirList dl( pluginsdatafpdir.fullPath(), File::FilesInDir,
+			  "*.txt" );
+	for ( int idx=0; idx<dl.size(); idx++ )
+	    configfiles.add( dl.get( idx ).buf() );
+    }
 
     using VoidVoidFn = void(*)(void);
-    const DirList dl( sipdatafp.fullPath(), File::FilesInDir, "*.txt" );
     BufferString libname;
     libname.setBufSize( 256 );
-    for ( int idx=0; idx<dl.size(); idx++ )
+    for ( const auto* configfile : configfiles )
     {
-	const FilePath dirname( dl.get( idx ).buf() );
+	const FilePath dirname( configfile->buf() );
 	const BufferString piname = dirname.baseName();
 	if ( piname.isEmpty() )
 	    continue;
@@ -287,7 +304,19 @@ void uiSurvInfoProvider::addPluginsInfoProviders()
 	libname.setEmpty();
 	SharedLibAccess::getLibName( piname.buf(), libname.getCStr(),
 				     libname.bufSize() );
-	const FilePath fp( GetLibPlfDir(), libname );
+	FilePath fp;
+	const OD::ModDep* moddep = OD::ModDeps().find( piname.str() );
+	if ( moddep )
+	{
+	    fp.set( GetLibPlfDir() ).add( libname.str() );
+	}
+	else
+	{
+	    const PluginManager::Data* pidata = PIM().findData( libname.str() );
+	    if ( pidata )
+		fp.set( PIM().getFileName(*pidata) );
+	}
+
 	if ( !fp.exists() )
 	    continue;
 
