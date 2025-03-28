4865899e78af696edf6dc71ea2661fd4fa64be02
Raman Singh <raman.singh@dgbes.com>
2025-03-28 09:34:22 -0500

    Fix for duplicate entries in the survey info provider list
    Summary: Also applied the changes to SeisTrcTranslators and FileSelectors.
    
    Test Plan: No more duplicate entries in the survey provider list.
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Subscribers: nanne
    
    Differential Revision: https://phabricator.dgbes.com/D17762

--- src/uiBase/uifileselector.cc
+++ src/uiBase/uifileselector.cc
@@ -297,18 +297,18 @@ void uiFileSelToolProvider::addPluginFileSelProviders()
     {
 	const DirList dl( datafp.fullPath(), File::FilesInDir, "*.txt" );
 	for ( int idx=0; idx<dl.size(); idx++ )
-	    configfiles.add( dl.get( idx ).buf() );
+	    configfiles.addIfNew( dl.get( idx ).buf() );
     }
 
     const FilePath pluginsdatafpdir(
 			GetSetupDataFileName(ODSetupLoc_UserPluginDirOnly,
 					     "FileSelProviders",false) );
-    if ( pluginsdatafpdir.exists() )
+    if ( pluginsdatafpdir.exists() && pluginsdatafpdir!=datafp )
     {
 	const DirList dl( pluginsdatafpdir.fullPath(), File::FilesInDir,
 			  "*.txt" );
 	for ( int idx=0; idx<dl.size(); idx++ )
-	    configfiles.add( dl.get( idx ).buf() );
+	    configfiles.addIfNew( dl.get( idx ).buf() );
     }
 
     using VoidVoidFn = void(*)(void);
