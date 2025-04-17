cb81e5ef416a3d39e762aa4e6076a9b016246f68
Nanne Hemstra <nanne.hemstra@dgbes.com>
2025-04-17 16:46:44 -0500

    A bit of rework
    Summary:
    - Added underscores to class members
    - Pass some values by reference instead of by copy
    
    Test Plan: Code compiles. No functionality change
    
    Reviewers: raman
    
    Reviewed By: raman
    
    Differential Revision: https://phabricator.dgbes.com/D17844

--- include/uiTools/annotbuffill.h
+++ include/uiTools/annotbuffill.h
@@ -9,10 +9,11 @@ ________________________________________________________________________
 -*/
 
 #include "uitoolsmod.h"
+
 #include "uigeom.h"
+#include "uiworld2ui.h"
 #include "draw.h"
 
-class uiWorld2Ui;
 class uiRGBArray;
 
 /*! \brief: Fills an uiRGBArray ( image buffer ) used to draw annotations 
@@ -39,32 +40,33 @@ public:
     //TODO add later on sth for the polygones
 
     
-			AnnotBufferFiller(const uiWorld2Ui* w=0);
+			AnnotBufferFiller(const uiWorld2Ui&);
 			~AnnotBufferFiller();
 
     void		addLineInfo(const OD::LineStyle&,
-				    TypeSet<dPoint>,bool,bool);
-    void		setW2UI( const uiWorld2Ui* w )	{ w2u_ = w; }
+				    const TypeSet<dPoint>&,
+				    bool ishgrid,bool isvgrid);
+    void		setW2UI(const uiWorld2Ui&);
     void		fillBuffer(const uiWorldRect&,uiRGBArray&) const;
-    void		fillInterWithBufArea(const uiWorldRect&,const LineInfo*,
+    void		fillInterWithBufArea(const uiWorldRect&,const LineInfo&,
 	    				     uiRGBArray&) const;
 
     void		eraseGridLines(bool);
-    bool		dispannotlines_;
-    bool		disphorgdlines_;
-    bool		dispvertgdlines_;
+    bool		dispannotlines_				= true;
+    bool		disphorgdlines_				= true;
+    bool		dispvertgdlines_			= true;
 
 protected:
 
-    const uiWorld2Ui*	w2u_;
+    uiWorld2Ui		w2u_;
     ObjectSet<LineInfo> annotlines_;
     ObjectSet<LineInfo> horgdlines_;
     ObjectSet<LineInfo> vertgdlines_;
 
     void		setPoint(const iPoint&,int,uiRGBArray&) const;
     void		setLine(const iPoint&,const iPoint&,
-				const LineInfo*,uiRGBArray&) const;
-    bool		isLineOutside(const LineInfo*,const uiWorldRect&) const;
+				const LineInfo&,uiRGBArray&) const;
+    bool		isLineOutside(const LineInfo&,const uiWorldRect&) const;
     dPoint		computeIntersect(const dPoint&,const dPoint&,
 					 const uiWorldRect&)const;
     void		dummytest();
