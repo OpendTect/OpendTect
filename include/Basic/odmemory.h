b9b5be8399e5be28c5ab703b954c0e71f772d388
Khushnood Qadir <khushnood.qadir@dgbes.com>
2026-01-23 18:56:39 +0100

    Potential CDash fix: Template specialization for floating_type

--- include/Basic/odmemory.h
+++ include/Basic/odmemory.h
@@ -10,10 +10,9 @@ ________________________________________________________________________
 
 #include "commondefs.h"
 #include "paralleltask.h"
+#include "valseries.h"
 
 #include <cmath>
-
-template <class T> class ValueSeries;
 
 
 namespace OD
@@ -431,10 +430,23 @@ bool MemValReplacer<T>::doWork( od_int64 start, od_int64 stop, int )
     if ( ptr_ )
 	return setPtr( start, stop-start+1 );
 
-    bool isfromnan = false;
-    if constexpr ( std::is_floating_point_v<T> )
-	isfromnan = std::isnan( fromval_ );
+    for ( od_int64 idx=start; idx<=stop; idx++ )
+    {
+	if ( vs_->value(idx)==fromval_ )
+	    vs_->setValue( idx, toval_ );
+    }
 
+    return true;
+}
+
+
+template <> inline
+bool MemValReplacer<float>::doWork( od_int64 start, od_int64 stop, int )
+{
+    if ( ptr_ )
+	return setPtr( start, stop-start+1 );
+
+    const bool isfromnan = std::isnan( fromval_ );
     if ( isfromnan )
     {
 	for ( od_int64 idx=start; idx<=stop; idx++ )
@@ -456,15 +468,40 @@ bool MemValReplacer<T>::doWork( od_int64 start, od_int64 stop, int )
 }
 
 
-template <class T> inline
-bool MemValReplacer<T>::setPtr( od_int64 start, od_int64 size )
+template <> inline
+bool MemValReplacer<double>::doWork( od_int64 start, od_int64 stop, int )
 {
-    T* ptr = ptr_ + start;
-    const T* stopptr = ptr + size;
-    bool isfromnan = false;
-    if constexpr ( std::is_floating_point_v<T> )
-	isfromnan = std::isnan( fromval_ );
+    if ( ptr_ )
+	return setPtr( start, stop-start+1 );
 
+    const bool isfromnan = std::isnan( fromval_ );
+    if ( isfromnan )
+    {
+	for ( od_int64 idx=start; idx<=stop; idx++ )
+	{
+	    if ( std::isnan(vs_->value(idx)) )
+		vs_->setValue( idx, toval_ );
+	}
+    }
+    else
+    {
+	for ( od_int64 idx=start; idx<=stop; idx++ )
+	{
+	    if ( vs_->value(idx)==fromval_ )
+		vs_->setValue( idx, toval_ );
+	}
+    }
+
+    return true;
+}
+
+
+template <> inline
+bool MemValReplacer<float>::setPtr( od_int64 start, od_int64 size )
+{
+    float* ptr = ptr_ + start;
+    const float* stopptr = ptr + size;
+    const bool isfromnan = std::isnan( fromval_ );
     if ( isfromnan )
     {
 	while ( ptr != stopptr )
@@ -484,6 +521,54 @@ bool MemValReplacer<T>::setPtr( od_int64 start, od_int64 size )
 
 	    ptr++;
 	}
+    }
+
+    return true;
+}
+
+
+template <> inline
+bool MemValReplacer<double>::setPtr( od_int64 start, od_int64 size )
+{
+    double* ptr = ptr_ + start;
+    const double* stopptr = ptr + size;
+    const bool isfromnan = std::isnan( fromval_ );
+    if ( isfromnan )
+    {
+	while ( ptr != stopptr )
+	{
+	    if ( std::isnan(*ptr) )
+		*ptr = toval_;
+
+	    ptr++;
+	}
+    }
+    else
+    {
+	while ( ptr != stopptr )
+	{
+	    if ( *ptr==fromval_ )
+		*ptr = toval_;
+
+	    ptr++;
+	}
+    }
+
+    return true;
+}
+
+
+template <class T> inline
+bool MemValReplacer<T>::setPtr( od_int64 start, od_int64 size )
+{
+    T* ptr = ptr_ + start;
+    const T* stopptr = ptr + size;
+    while ( ptr != stopptr )
+    {
+	if ( *ptr==fromval_ )
+	    *ptr = toval_;
+
+	ptr++;
     }
 
     return true;
