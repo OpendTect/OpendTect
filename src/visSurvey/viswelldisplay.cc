e01982e7f9f2002328ca9d7cb3a89ba8adefa0a2
Khushnood Qadir <khushnood.qadir@dgbes.com>
2026-04-20 15:59:30 +0200

    Fixed crashes related to well display in 3D scene
    Summary:
    Stabilize 3D well log rebuild sequence:
    Adjusted log refresh flow so per-side log data is cleared before reapplying log properties and data.
    This keeps fill/color-table state consistent during redraw and avoids stale buffer combinations.
    
    Make well visual updates main-thread safe:
    Guarded WellDisplay::fullRedraw() and WellDisplay::updateMarkers() to execute on the main thread.
    Prevents OSG scene graph updates from worker-thread callbacks during well reload.
    
    Test Plan: Open Advanced Well Manager > Add multiple wells to 3D from the tree > Reload selected/all wells from the table.
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Subscribers: lloyd, wayne
    
    Differential Revision: https://phabricator.dgbes.com/D18536

--- src/visSurvey/viswelldisplay.cc
+++ src/visSurvey/viswelldisplay.cc
@@ -220,6 +220,8 @@ void WellDisplay::fillLogParams(
 
 void WellDisplay::fullRedraw( CallBacker* )
 {
+    mEnsureExecutedInMainThread( WellDisplay::fullRedraw );
+
     mGetWD(return);
     if ( !well_ || !wd )
 	return;
@@ -319,6 +321,8 @@ void WellDisplay::getTrackPos( const Well::Data* wd,
 
 void WellDisplay::updateMarkers( CallBacker* )
 {
+    mEnsureExecutedInMainThread( WellDisplay::updateMarkers );
+
     if ( !well_ )
 	return;
 
@@ -551,9 +555,11 @@ void WellDisplay::setLogDisplay( visBase::Well::Side side )
 
     visBase::Well::LogParams lp;
     fillLogParams( lp, side );
-    lp.logidx_ = logidx;  lp.side_ = side;
+    lp.logidx_ = logidx;
+    lp.side_ = side;
     lp.filllogidx_ = wd->logs().indexOf( lp.fillname_ );
 
+    well_->clearLog( side );
     setLogProperties( lp );
     setLogData( lp, mustBeFilled(lp) );
 }
