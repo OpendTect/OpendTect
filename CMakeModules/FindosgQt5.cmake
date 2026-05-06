11ab16fefad1f0136a8cc4eb53b06d61d424b622
Arnaud Huck <arnaud.huck@dgbes.com>
2026-05-06 11:26:44 +0200

    Updated Find*.cmake modules
    Summary:
    Merged the OpendTect version of these modules with CMake 4.3.2.
    The main goal is to provide compatibility with zlib 1.3.1.2 and 1.3.2+ releases. Only cmake 4.2.0+ modules are compatible with them.
    
    Reference: https://github.com/Kitware/CMake/commit/b2c059e68f32034acb612d0418ef45ac7b8eed5d
    
    Test Plan:
    configured the project and compiled all targets on Linux.
    The release test will be when building OpendTect releases
    
    Reviewers: khushnood
    
    Reviewed By: khushnood
    
    Subscribers: nikolay
    
    Differential Revision: https://phabricator.dgbes.com/D18596

--- CMakeModules/FindosgQt5.cmake
+++ CMakeModules/FindosgQt5.cmake
@@ -1,45 +1,134 @@
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
-# file Copyright.txt or https://cmake.org/licensing for details.
+# file LICENSE.rst or https://cmake.org/licensing for details.
 
-#.rst:
-# FindosgQt
-# ---------
-#
-#
-#
-# This is part of the Findosg* suite used to find OpenSceneGraph
-# components.  Each component is separate and you must opt in to each
-# module.  You must also opt into OpenGL and OpenThreads (and Producer
-# if needed) as these modules won't do it for you.  This is to allow you
-# control over your own system piece by piece in case you need to opt
-# out of certain components or change the Find behavior for a particular
-# module (perhaps because the default FindOpenGL.cmake module doesn't
-# work with your system as an example).  If you want to use a more
-# convenient module that includes everything, use the
-# FindOpenSceneGraph.cmake instead of the Findosg*.cmake modules.
-#
-# Locate osgQt5 This module defines
-#
-# OSGQT_FOUND - Was osgQt found? OSGQT_INCLUDE_DIR - Where to find the
-# headers OSGQT_LIBRARIES - The libraries to link for osgQt (use this)
-#
-# OSGQT_LIBRARY - The osgQt library OSGQT_LIBRARY_DEBUG - The osgQt
-# debug library
-#
-# $OSGDIR is an environment variable that would correspond to the
-# ./configure --prefix=$OSGDIR used in building osg.
-#
-# Created by Eric Wing.  Modified to work with osgQt by Robert Osfield,
-# January 2012.
+#[=======================================================================[.rst:
+FindosgQt
+---------
 
-# Header files are presumed to be included like
-# #include <osg/PositionAttitudeTransform>
-# #include <osgQt/GraphicsWindowQt>
+Finds the osgQt utility library from the OpenSceneGraph toolkit.
 
-include( Findosg_functions )
+.. note::
+
+  In most cases, it's recommended to use the :module:`FindOpenSceneGraph` module
+  instead and list osgQt as a component.  This will automatically handle
+  dependencies such as the OpenThreads and core osg libraries:
+
+  .. code-block:: cmake
+
+    find_package(OpenSceneGraph COMPONENTS osgQt)
+
+This module is used internally by :module:`FindOpenSceneGraph` to find the
+osgQt library.	It is not intended to be included directly during typical
+use of the :command:`find_package` command.  However, it is available as a
+standalone module for advanced use cases where finer control over detection is
+needed.  For example, to find the osgQt explicitly or bypass automatic
+component detection:
+
+.. code-block:: cmake
+
+  find_package(osgQt)
+
+OpenSceneGraph and osgQt headers are intended to be included in C++ project
+source code as:
+
+.. code-block:: c++
+  :caption: ``example.cxx``
+
+  #include <osg/PositionAttitudeTransform>
+  #include <osgQt/GraphicsWindowQt>
+  // ...
+
+When working with the OpenSceneGraph toolkit, other libraries such as OpenGL may
+also be required.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This module defines the following variables:
+
+``osgQt_FOUND``
+  .. versionadded:: 3.3
+
+  Boolean indicating whether the osgQt library of the OpenSceneGraph toolkit
+  was found.
+
+``OSGQT_LIBRARIES``
+  The libraries needed to link against to use osgQt.
+
+``OSGQT_LIBRARY``
+  A result variable that is set to the same value as the ``OSGQT_LIBRARIES``
+  variable.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``OSGQT_INCLUDE_DIR``
+  The include directory containing headers needed to use osgQt.
+
+``OSGQT_LIBRARY_DEBUG``
+  The path to the osgQt debug library.
+
+Hints
+^^^^^
+
+This module accepts the following variables:
+
+``OSGDIR``
+  Environment variable that can be set to help locate the OpenSceneGraph
+  toolkit, including its osgQt library, when installed in a custom
+  location.  It should point to the OpenSceneGraph installation prefix used when
+  it was configured, built, and installed: ``./configure --prefix=$OSGDIR``.
+
+Deprecated Variables
+^^^^^^^^^^^^^^^^^^^^
+
+The following variables are provided for backward compatibility:
+
+``OSGQT_FOUND``
+  .. deprecated:: 4.2
+    Use ``osgQt_FOUND``, which has the same value.
+
+  Boolean indicating whether the osgQt library of the OpenSceneGraph toolkit
+  was found.
+
+Examples
+^^^^^^^^
+
+Finding osgQt explicitly with this module and creating an interface
+:ref:`imported target <Imported Targets>` that encapsulates its usage
+requirements for linking it to a project target:
+
+.. code-block:: cmake
+
+  find_package(osgQt)
+
+  if(osgQt_FOUND AND NOT TARGET osgQt::osgQt)
+    add_library(osgQt::osgQt INTERFACE IMPORTED)
+    set_target_properties(
+      osgQt::osgQt
+      PROPERTIES
+	INTERFACE_INCLUDE_DIRECTORIES "${OSGQT_INCLUDE_DIR}"
+	INTERFACE_LINK_LIBRARIES "${OSGQT_LIBRARIES}"
+    )
+  endif()
+
+  target_link_libraries(example PRIVATE osgQt::osgQt)
+
+See Also
+^^^^^^^^
+
+* The :module:`FindOpenSceneGraph` module to find OpenSceneGraph toolkit.
+#]=======================================================================]
+
+# Created by Eric Wing.
+# Modified to work with osgQt by Robert Osfield, January 2012.
+
+include(${CMAKE_CURRENT_LIST_DIR}/Findosg_functions.cmake)
 OSG_FIND_PATH	(OSGQT osgQt/GraphicsWindowQt)
-OSG_FIND_LIBRARY(OSGQT osgQt5)
+OSG_FIND_LIBRARY(OSGQT osgQt)
 
-include( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(osgQt DEFAULT_MSG
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(osgQt DEFAULT_MSG
     OSGQT_LIBRARY OSGQT_INCLUDE_DIR)
