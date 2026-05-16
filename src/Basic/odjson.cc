dd497fdbcca9294c5c4509d4264ef0c8e33f682e
Nanne Hemstra <nanne.hemstra@dgbes.com>
2026-05-15 22:12:08 -0500

    Potentially a fix for using \\server as dataroot
    JSON did not convert // back to \\
    
    Added some protections to getCanonical

--- src/Basic/odjson.cc
+++ src/Basic/odjson.cc
@@ -199,6 +199,18 @@ static BufferString getPathStr( const FilePath& fp )
     return ret;
 }
 
+
+static void setPathStr( const char* str, FilePath& fp )
+{
+    BufferString ret = str;
+    fp.set( str );
+    if ( __iswin__ && !fp.isURI() )
+	ret.replace( "/", "\\" );
+
+    fp.set( ret );
+}
+
+
 } // namespace JSON
 
 } // namespace OD
@@ -308,7 +320,7 @@ FilePath OD::JSON::ValArr::getFilePath( idx_type idx ) const
 
     FilePath ret;
     if ( type_ == String )
-	ret.set( strings().get(idx) );
+	setPathStr( strings().get(idx), ret );
 
     return ret;
 }
@@ -490,7 +502,7 @@ FilePath OD::JSON::ValueSet::getFilePath( idx_type idx ) const
 
     FilePath ret;
     if ( DataType(val->type_) == String )
-	ret.set( val->str() );
+	setPathStr( val->str(), ret );
 
     return ret;
 }
