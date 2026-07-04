fa6af66662bbaaf50cf99e1cd245ad695c39ac82
Arnaud Huck <arnaud.huck@dgbes.com>
2026-07-04 10:06:42 +0200

    Reverted a change in od_external.bat
    Introduced by: https://github.com/OpendTect/OpendTect/commit/0776dff6f0ff7c6cbfa015eb791ac6fec7a5ce2e
    
    See the installer project for more details
    
    [Jira: ODINST-80]

--- bin/od_external.bat
+++ bin/od_external.bat
@@ -10,6 +10,7 @@ IF %argC% LSS 1 (
     GOTO :eof
 )
 
+SET DTECT_APPL=
 IF DEFINED HOMEDRIVE IF DEFINED HOMEPATH IF EXIST %HOMEDRIVE%%HOMEPATH% (
     chdir /D %HOMEDRIVE%%HOMEPATH%
     Set "WindowsAppDir=%HOMEDRIVE%%HOMEPATH%\AppData\Local\Microsoft\WindowsApps"
