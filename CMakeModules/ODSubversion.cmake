e06757b18bfaf1f74f57655fba6fda909a3d9e39
Arnaud Huck <arnaud.huck@dgbes.com>
2026-08-07 10:26:36 +0200

    Minor CMake changes [devel]
    Added "" around the variables that represent a filepath, which may
    contain spaces

--- CMakeModules/ODSubversion.cmake
+++ CMakeModules/ODSubversion.cmake
@@ -5,17 +5,17 @@
 #________________________________________________________________________
 #
 
-if ( EXISTS ${CMAKE_SOURCE_DIR}/.git )
+if ( IS_DIRECTORY "${CMAKE_SOURCE_DIR}/.git" )
     set ( OD_FROM_GIT TRUE )
 endif()
-if ( EXISTS ${CMAKE_SOURCE_DIR}/.svn )
+if ( IS_DIRECTORY "${CMAKE_SOURCE_DIR}/.svn ")
     set ( OD_FROM_SVN TRUE )
 endif()
 
 if ( OD_FROM_SVN )
     # the FindSubversion.cmake module is part of the standard distribution
     if ( WIN32 )
-	set ( CMAKE_SYSTEM_PROGRAM_PATH ${CMAKE_SYSTEM_PROGRAM_PATH}
+	list( APPEND CMAKE_SYSTEM_PROGRAM_PATH
 		"C:/Program Files/SlikSvn/bin"
 		"C:/Program Files (x86)/SlikSvn/bin" )
 	#Add more likely paths if need be
@@ -29,37 +29,37 @@ set ( VCS_BRANCH_DEF )
 
 if ( Subversion_FOUND AND OD_FROM_SVN )
     # extract working copy information for SOURCE_DIR into MY_XXX variables
-    Subversion_WC_INFO( ${CMAKE_SOURCE_DIR} MY )
-    set ( UPDATE_CMD ${Subversion_SVN_EXECUTABLE} update )
-    set ( VCS_VERSION ${MY_WC_REVISION} )
+    Subversion_WC_INFO( "${CMAKE_SOURCE_DIR}" MY )
+    set( UPDATE_CMD "${Subversion_SVN_EXECUTABLE}" update )
+    set( VCS_VERSION ${MY_WC_REVISION} )
 elseif( OD_FROM_GIT )
-    set ( VCS_VERSION 0 )
+    set( VCS_VERSION 0 )
 
     find_package(Git)
 
     if( GIT_FOUND )
 	# Get the latest abbreviated commit hash of the working branch
 	execute_process(
-	  COMMAND ${GIT_EXECUTABLE} log -1 --format=%h
-	  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+	  COMMAND "${GIT_EXECUTABLE}" log -1 --format=%h
+	  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
 	  OUTPUT_VARIABLE VCS_VERSION
 	  OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
 
 	execute_process(
-	  COMMAND git rev-parse --abbrev-ref HEAD
-	  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+	  COMMAND "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
+	  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
 	  OUTPUT_VARIABLE VCS_BRANCH
 	  OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
 
 	if ( VCS_BRANCH STREQUAL "main" )
-	    set ( VCS_BRANCH_DEF "#define mVCS_DEVEL" )
+	    set( VCS_BRANCH_DEF "#define mVCS_DEVEL" )
 	else()
-	    set ( VCS_BRANCH_DEF "#define mVCS_STABLE" )
+	    set( VCS_BRANCH_DEF "#define mVCS_STABLE" )
 	endif()
 
-	set ( UPDATE_CMD ${GIT_EXECUTABLE} pull )
+	set( UPDATE_CMD "${GIT_EXECUTABLE}" pull )
     endif()
 endif()
 
@@ -77,7 +77,7 @@ macro( OD_SETUP_EXTERNALS )
 			"-DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}" )
 	endif()
 
-	execute_process( COMMAND ${CMAKE_COMMAND}
+	execute_process( COMMAND "${CMAKE_COMMAND}"
 		    ${EXTERNALCMDARGS} -DUPDATE=No
 		    -P "${CMAKE_SOURCE_DIR}/external/Externals.cmake"
 	    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
@@ -96,7 +96,7 @@ macro( OD_SETUP_EXTERNALS )
 
 	set( CMAKE_FOLDER "Other" )
 	add_custom_target( update ${UPDATE_CMD} 
-		COMMAND ${CMAKE_COMMAND}
+		COMMAND "${CMAKE_COMMAND}"
 		${EXTERNALCMDARGS} -DUPDATE=Yes
 		-P external/Externals.cmake
 		WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
